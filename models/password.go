35fa252bbd459054cc1e7c1730a89c9e63c2755c
Dimitri Roche <dimroc@gmail.com>
2017-12-06 15:36:10 -0500

    Add ability to check password
    Signed-off-by: Steve Ellis <email@steveell.is>

--- models/password.go
+++ models/password.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
 
@@ -9,17 +10,31 @@ import (
 )
 
 type Password struct {
-	Hash  []byte `storm:"id"`
+	Hash []byte `storm:"id"`
 	Salt []byte
 }
 
 func NewPassword(phrase string) Password {
+	salt := generateSalt()
+	return Password{
+		Hash: generateHash(phrase, salt),
+		Salt: salt,
+	}
+}
+
+func (self Password) Check(phrase string) bool {
+	return bytes.Compare(generateHash(phrase, self.Salt), self.Hash) == 0
+}
+
+func generateHash(phrase string, salt []byte) []byte {
+	return pbkdf2.Key([]byte(phrase), salt, 262144, 32, sha256.New)
+}
+
+func generateSalt() []byte {
 	salt := make([]byte, 32)
 	_, err := rand.Read(salt)
 	if err != nil {
 		logger.Fatal(err)
 	}
-
-	hash := pbkdf2.Key([]byte(phrase), salt, 262144, 32, sha256.New)
-	return Password{hash, salt}
+	return salt
 }
--- models/password_test.go
+++ models/password_test.go
@@ -1,6 +1,7 @@
 package models_test
 
 import (
+	"encoding/base64"
 	"testing"
 
 	"github.com/smartcontractkit/chainlink-go/internal/cltest"
@@ -21,4 +22,13 @@ func TestNewPassword(t *testing.T) {
 
 	store.ORM.All(&passwords)
 	assert.Equal(t, 1, len(passwords))
+}
+
+func TestPasswordCheck(t *testing.T) {
+	hash, _ := base64.StdEncoding.DecodeString("/qlGjiuorAcE4tFURJzq4vG+SEaYb0KExvGpfMZ0jF0=")
+	salt, _ := base64.StdEncoding.DecodeString("+FTNZKzpDKl5BYTCp0CD/LYiHtzEQCph3s/UHKUZHyQ=")
+	password := models.Password{Hash: hash, Salt: salt}
+
+	assert.True(t, password.Check(phrase))
+	assert.False(t, password.Check("NotThePassword"))
 }
