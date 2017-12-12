8675b13beb38f34bf6d05ccdb47ca45306e577c8
Steve Ellis <email@steveell.is>
2017-12-11 19:41:57 -0800

    add test for null value EthBytes32 encoding

--- models/adapters/eth_bytes32_test.go
+++ models/adapters/eth_bytes32_test.go
@@ -5,21 +5,44 @@ import (
 
 	"github.com/smartcontractkit/chainlink-go/models/adapters"
 	"github.com/stretchr/testify/assert"
+	null "gopkg.in/guregu/null.v3"
 )
 
 func TestEthereumBytes32Formatting(t *testing.T) {
 	tests := []struct {
-		value    string
+		value    null.String
 		expected string
 	}{
-		{"16800.00", "31363830302e3030000000000000000000000000000000000000000000000000"},
-		{"", "0000000000000000000000000000000000000000000000000000000000000000"},
-		{"Hello World!", "48656c6c6f20576f726c64210000000000000000000000000000000000000000"},
-		{"string that is waaAAAaaay toooo long!!!!!", "737472696e672074686174206973207761614141416161617920746f6f6f6f20"},
+		{
+			null.StringFrom("16800.00"),
+			"31363830302e3030000000000000000000000000000000000000000000000000",
+		},
+		{
+			null.StringFrom(""),
+			"0000000000000000000000000000000000000000000000000000000000000000",
+		},
+		{
+			null.StringFrom("Hello World!"),
+			"48656c6c6f20576f726c64210000000000000000000000000000000000000000",
+		},
+		{
+			null.StringFrom("string that is waaAAAaaay toooo long!!!!!"),
+			"737472696e672074686174206973207761614141416161617920746f6f6f6f20",
+		},
+		{
+			null.StringFrom("¡Hola Mundo!"),
+			"c2a1486f6c61204d756e646f2100000000000000000000000000000000000000",
+		},
+		{
+			null.StringFromPtr(nil),
+			"0000000000000000000000000000000000000000000000000000000000000000",
+		},
 	}
 
 	for _, test := range tests {
-		past := adapters.RunResultWithValue(test.value)
+		past := adapters.RunResult{
+			Output: adapters.Output{"value": test.value},
+		}
 		adapter := adapters.EthBytes32{}
 		result := adapter.Perform(past)
 
