6752bea55c7f6aad2636cf8b292dcf1bf7fdfbe9
Steve Ellis <email@steveell.is>
2017-12-27 15:40:23 -0500

    move ethereum client into store package
    Signed-off-by: Dimitri Roche <dimroc@gmail.com>

---
+++ store/eth.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts"
+)
+
+type Eth struct {
+	Caller
+}
+
+type Caller interface {
+	Call(result interface{}, method string, args ...interface{}) error
+}
+
+func (self *Eth) GetNonce(account accounts.Account) (uint64, error) {
+	var result string
+	err := self.Call(&result, "eth_getTransactionCount", account.Address.Hex())
+	if err != nil {
+		return 0, err
+	}
+	if strings.ToLower(result[0:2]) == "0x" {
+		result = result[2:]
+	}
+	return strconv.ParseUint(result, 16, 64)
+}
+
+func (self *Eth) SendRawTx(hex string) (string, error) {
+	var result string
+	err := self.Call(&result, "eth_sendRawTransaction", hex)
+	return result, err
+}
