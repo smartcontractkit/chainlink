ebd053bdb2cd9f38d29c5ef8d4b300ab76ec2d21
Steve Ellis <email@steveell.is>
2017-12-28 17:22:44 -0500

    add ability for adapters to mark runs pending
    Signed-off-by: Dimitri Roche <dimroc@gmail.com>

--- adapters/no_op.go
+++ adapters/no_op.go
@@ -10,3 +10,9 @@ type NoOp struct{}
 func (self *NoOp) Perform(input models.RunResult, _ *store.Store) models.RunResult {
 	return models.RunResult{}
 }
+
+type NoOpPend struct{}
+
+func (self *NoOpPend) Perform(input models.RunResult, _ *store.Store) models.RunResult {
+	return models.RunResultPending(input)
+}
