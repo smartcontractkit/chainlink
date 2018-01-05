51180783139ae4f0883a62e1e88e19249194f4ea
Steve Ellis <email@steveell.is>
2018-01-05 12:31:17 -0500

    make the contract controlled by one account
    - add method for requesting data
    - add yarn for JS dependency management
    - include common helper functions for tests
    
    Signed-off-by: Dimitri Roche <dimroc@gmail.com>

--- solidity/contracts/ChainLink.sol
+++ solidity/contracts/ChainLink.sol
@@ -1,14 +1,26 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
-contract ChainLink {
+import "./zeppelin/Ownable.sol";
+contract ChainLink is Ownable {
 
   bytes32 public value;
+  uint public nonce;
+  event Request(
+    uint indexed nonce,
+    address indexed to,
+    bytes4 indexed fid
+  );
 
   function ChainLink() public {
     value = "Hello World!";
   }
 
-  function setValue(bytes32 newValue) public {
-    value = newValue;
+  function setValue(bytes32 _value) public {
+    value = _value;
+  }
+
+  function requestData(address _address, bytes4 _fid) public {
+    Request(nonce, _address, _fid);
+    nonce += 1;
   }
 }
--- solidity/test/ChainLink_test.js
+++ solidity/test/ChainLink_test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('./support/helpers.js')
+
 contract('ChainLink', () => {
   let ChainLink = artifacts.require("./contracts/ChainLink.sol");
   let oracle;
@@ -8,14 +10,50 @@ contract('ChainLink', () => {
     oracle = await ChainLink.new();
   });
 
-  it("returns the value it was initialized with", async () => {
-    let value = await oracle.value.call();
-    assert.equal("Hello World!", web3.toUtf8(value));
+
+  describe("initialization", () => {
+    it("returns the value it was initialized with", async () => {
+      let value = await oracle.value.call();
+      assert.equal("Hello World!", web3.toUtf8(value));
+    });
   });
 
-  it("sets the value", async () => {
-    await oracle.setValue("Hello Jupiter");
-    let value = await oracle.value.call();
-    assert.equal("Hello Jupiter", web3.toUtf8(value));
+  describe("#setValue", () => {
+    it("sets the value", async () => {
+      await oracle.setValue("Hello Jupiter");
+      let value = await oracle.value.call();
+      assert.equal("Hello Jupiter", web3.toUtf8(value));
+    });
+  });
+
+  describe("#transferOwnership", () => {
+    it("can change the owner", async () => {
+      let newOwner = "0x80e29acb842498fe6591f020bd82766dce619d43"
+      await oracle.transferOwnership(newOwner);
+      let owner = await oracle.owner.call();
+      assert.isTrue(web3.isAddress(owner));
+      assert.equal(newOwner, owner);
+    });
+  });
+
+  describe("#requestData", () => {
+    it("logs an event", async () => {
+      let fID = "0x12345678";
+      let to = "0x80e29acb842498fe6591f020bd82766dce619d43";
+      let tx = await oracle.requestData(to, fID);
+
+      assert.equal(1, tx.receipt.logs.length)
+
+      let log = tx.receipt.logs[0]
+      assert.equal(to, hexToAddress(log.topics[2]))
+    });
+
+    it("increments the nonce", async () => {
+      let fID = "0x12345678";
+      let to = "0x80e29acb842498fe6591f020bd82766dce619d43";
+      let tx = await oracle.requestData(to, fID);
+      let nonce = await oracle.nonce.call();
+      assert.isTrue(bigNum(1).eq(nonce));
+    });
   });
 });
