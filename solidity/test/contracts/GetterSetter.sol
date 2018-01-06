24b49cc804ba4f55da0d4222acd1d152342023b7
Steve Ellis <email@steveell.is>
2018-01-05 20:06:54 -0500

    make contract requests use a nonce
    Signed-off-by: Dimitri Roche <dimroc@gmail.com>

--- solidity/test/contracts/GetterSetter.sol
+++ solidity/test/contracts/GetterSetter.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.4.18;
 
 contract GetterSetter {
   bytes32 public value;
+  uint256 public nonce;
 
-  function setValue(bytes32 _value) public {
+  function setValue(uint256 _nonce, bytes32 _value) public {
+    nonce = _nonce;
     value = _value;
   }
 }
