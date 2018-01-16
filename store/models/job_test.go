ad8c9dc0e2df8cebaf5bd81ff279466cf1a1fda3
Dimitri Roche <dimroc@gmail.com>
2018-01-16 18:56:09 -0500

    Move JobRun into job.go

--- store/models/job_test.go
+++ store/models/job_test.go
@@ -1,11 +1,13 @@
 package models_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/smartcontractkit/chainlink/adapters"
 	"github.com/smartcontractkit/chainlink/internal/cltest"
+	"github.com/smartcontractkit/chainlink/services"
 	"github.com/smartcontractkit/chainlink/store/models"
 	"github.com/smartcontractkit/chainlink/utils"
 	"github.com/stretchr/testify/assert"
@@ -66,4 +68,42 @@ func TestJobEnded(t *testing.T) {
 			assert.Equal(t, test.want, job.Ended(test.current))
 		})
 	}
+}
+
+func TestRetrievingJobRunsWithErrorsFromDB(t *testing.T) {
+	t.Parallel()
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
+
+	job := models.NewJob()
+	jr := job.NewRun()
+	jr.Result = models.RunResultWithError(fmt.Errorf("bad idea"))
+	err := store.Save(jr)
+	assert.Nil(t, err)
+
+	run := &models.JobRun{}
+	err = store.One("ID", jr.ID, run)
+	assert.Nil(t, err)
+	assert.True(t, run.Result.HasError())
+	assert.Equal(t, "bad idea", run.Result.Error())
+}
+
+func TestTaskRunsToRun(t *testing.T) {
+	t.Parallel()
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
+
+	j := models.NewJob()
+	j.Tasks = []models.Task{
+		{Type: "NoOp"},
+		{Type: "NoOpPend"},
+		{Type: "NoOp"},
+	}
+	assert.Nil(t, store.SaveJob(j))
+	jr := j.NewRun()
+	assert.Equal(t, jr.TaskRuns, jr.UnfinishedTaskRuns())
+
+	err := services.ExecuteRun(jr, store)
+	assert.Nil(t, err)
+	assert.Equal(t, jr.TaskRuns[1:], jr.UnfinishedTaskRuns())
 }
