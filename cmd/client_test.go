3950589aec365331425b54edf4bb08ac4bd34777
Dimitri Roche <dimroc@gmail.com>
2018-01-18 08:01:57 -0500

    Improve testing of cmds with RenderMock

--- cmd/client_test.go
+++ cmd/client_test.go
@@ -6,9 +6,29 @@ import (
 
 	"github.com/smartcontractkit/chainlink/cmd"
 	"github.com/smartcontractkit/chainlink/internal/cltest"
+	"github.com/smartcontractkit/chainlink/store/models"
+	"github.com/smartcontractkit/chainlink/web"
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/cli"
 )
+
+func TestClientGetJobs(t *testing.T) {
+	app, cleanup := cltest.NewApplication()
+	defer cleanup()
+
+	j1 := cltest.NewJob()
+	app.Store.SaveJob(j1)
+	j2 := cltest.NewJob()
+	app.Store.SaveJob(j2)
+
+	r := &cltest.RendererMock{}
+	client := cmd.Client{r, app.Store.Config}
+
+	assert.Nil(t, client.GetJobs(nil))
+	jobs := *r.Renders[0].(*[]models.Job)
+	assert.Equal(t, 2, len(jobs))
+	assert.Equal(t, j1.ID, jobs[0].ID)
+}
 
 func TestClientShowJob(t *testing.T) {
 	app, cleanup := cltest.NewApplication()
@@ -16,21 +36,27 @@ func TestClientShowJob(t *testing.T) {
 	job := cltest.NewJob()
 	app.Store.SaveJob(job)
 
-	client := cmd.Client{cmd.RendererNoOp{}, app.Store.Config}
+	r := &cltest.RendererMock{}
+	client := cmd.Client{r, app.Store.Config}
 
 	set := flag.NewFlagSet("test", 0)
 	set.Parse([]string{job.ID})
 	c := cli.NewContext(nil, set, nil)
 	assert.Nil(t, client.ShowJob(c))
+	assert.Equal(t, 1, len(r.Renders))
+	assert.Equal(t, job.ID, r.Renders[0].(*web.JobPresenter).ID)
 }
 
 func TestClientShowJobNotFound(t *testing.T) {
 	app, cleanup := cltest.NewApplication()
 	defer cleanup()
-	client := cmd.Client{cmd.RendererNoOp{}, app.Store.Config}
+
+	r := &cltest.RendererMock{}
+	client := cmd.Client{r, app.Store.Config}
 
 	set := flag.NewFlagSet("test", 0)
 	set.Parse([]string{"bogus-ID"})
 	c := cli.NewContext(nil, set, nil)
 	assert.NotNil(t, client.ShowJob(c))
+	assert.Empty(t, r.Renders)
 }
