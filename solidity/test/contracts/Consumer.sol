442f6e09dca4383e430d1d1d778e65e12f69f52a
Dimitri Roche <dimroc@gmail.com>
2018-01-18 16:22:38 -0800

    ensure consuming contract is securely updated
    Signed-off-by: Steve Ellis <email@steveell.is>

--- solidity/test/contracts/Consumer.sol
+++ solidity/test/contracts/Consumer.sol
@@ -5,16 +5,32 @@ import "../../contracts/ChainLink.sol";
 contract Consumer {
   ChainLink private chainLink;
   uint256 private nonce;
+  bytes32 public currentPrice;
 
   function Consumer(address _chainLink) public {
     chainLink = ChainLink(_chainLink);
   }
 
   function requestEthereumPrice() public {
-    bytes4 fid = bytes4(keccak256("callback(uint256,bytes32)"));
+    bytes4 fid = bytes4(keccak256("fulfill(uint256,bytes32)"));
     nonce = chainLink.requestData(this, fid, "https://etherprice.com/api", "recent,usd");
   }
 
-  function callback(uint256 _nonce, bytes32 _data) public {
+  function fulfill(uint256 _nonce, bytes32 _data)
+    public
+    checkSender
+    checkNonce(_nonce)
+  {
+    currentPrice = _data;
+  }
+
+  modifier checkSender() {
+    require(msg.sender == address(chainLink));
+    _;
+  }
+
+  modifier checkNonce(uint256 _nonce) {
+    require(nonce == _nonce);
+    _;
   }
 }
