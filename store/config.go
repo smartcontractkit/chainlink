0864f176c5ae6bf5fc4078ebebdb4ff9be60e469
Dimitri Roche <dimroc@gmail.com>
2018-02-04 08:37:58 -0500

    Switch to debug log level w cmd line and env

--- store/config.go
+++ store/config.go
@@ -10,20 +10,22 @@ import (
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/smartcontractkit/env"
+	"go.uber.org/zap/zapcore"
 )
 
 type Config struct {
-	RootDir             string  `env:"ROOT" envDefault:"~/.chainlink"`
-	BasicAuthUsername   string  `env:"USERNAME" envDefault:"chainlink"`
-	BasicAuthPassword   string  `env:"PASSWORD" envDefault:"twochains"`
-	EthereumURL         string  `env:"ETH_URL" envDefault:"http://localhost:8545"`
-	ChainID             uint64  `env:"ETH_CHAIN_ID" envDefault:"0"`
-	PollingSchedule     string  `env:"POLLING_SCHEDULE" envDefault:"*/15 * * * * *"`
-	ClientNodeURL       string  `env:"CLIENT_NODE_URL" envDefault:"http://localhost:8080"`
-	EthMinConfirmations uint64  `env:"ETH_MIN_CONFIRMATIONS" envDefault:"12"`
-	EthGasBumpThreshold uint64  `env:"ETH_GAS_BUMP_THRESHOLD" envDefault:"12"`
-	EthGasBumpWei       big.Int `env:"ETH_GAS_BUMP_GWEI" envDefault:"5000000000"`
-	EthGasPriceDefault  big.Int `env:"ETH_GAS_PRICE_DEFAULT" envDefault:"20000000000"`
+	LogLevel            LogLevel `env:"LOG_LEVEL" envDefault:"info"`
+	RootDir             string   `env:"ROOT" envDefault:"~/.chainlink"`
+	BasicAuthUsername   string   `env:"USERNAME" envDefault:"chainlink"`
+	BasicAuthPassword   string   `env:"PASSWORD" envDefault:"twochains"`
+	EthereumURL         string   `env:"ETH_URL" envDefault:"http://localhost:8545"`
+	ChainID             uint64   `env:"ETH_CHAIN_ID" envDefault:"0"`
+	PollingSchedule     string   `env:"POLLING_SCHEDULE" envDefault:"*/15 * * * * *"`
+	ClientNodeURL       string   `env:"CLIENT_NODE_URL" envDefault:"http://localhost:8080"`
+	EthMinConfirmations uint64   `env:"ETH_MIN_CONFIRMATIONS" envDefault:"12"`
+	EthGasBumpThreshold uint64   `env:"ETH_GAS_BUMP_THRESHOLD" envDefault:"12"`
+	EthGasBumpWei       big.Int  `env:"ETH_GAS_BUMP_GWEI" envDefault:"5000000000"`
+	EthGasPriceDefault  big.Int  `env:"ETH_GAS_PRICE_DEFAULT" envDefault:"20000000000"`
 }
 
 func NewConfig() Config {
@@ -48,7 +50,8 @@ func (c Config) KeysDir() string {
 
 func parseEnv(cfg interface{}) error {
 	return env.ParseWithFuncs(cfg, env.CustomParsers{
-		reflect.TypeOf(big.Int{}): bigIntParser,
+		reflect.TypeOf(big.Int{}):  bigIntParser,
+		reflect.TypeOf(LogLevel{}): levelParser,
 	})
 }
 
@@ -58,4 +61,14 @@ func bigIntParser(str string) (interface{}, error) {
 		return i, fmt.Errorf("Unable to parse %v into *big.Int(base 10)", str)
 	}
 	return *i, nil
+}
+
+func levelParser(str string) (interface{}, error) {
+	var lvl LogLevel
+	err := lvl.Set(str)
+	return lvl, err
+}
+
+type LogLevel struct {
+	zapcore.Level
 }
