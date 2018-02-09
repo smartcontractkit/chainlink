fc6070fcda057742ee2bb5eb7b066e7583cde141
Steve Ellis <email@steveell.is>
2018-02-08 18:27:24 -0800

    clean up test and remove duplication

--- services/notification_listener_test.go
+++ services/notification_listener_test.go
@@ -2,7 +2,6 @@ package services_test
 
 import (
 	"encoding/json"
-	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -36,39 +35,28 @@ func TestNotificationListenerStart(t *testing.T) {
 
 func TestNotificationListenerAddJob(t *testing.T) {
 	t.Parallel()
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
 
 	initrAddress := cltest.NewEthAddress()
-	eventTopic := "0x06f4bf36b4e011a5c499cef1113c2d166800ce4013f6c2509cab1a0e92b83fb2"
-	nonce := "0x402e9676507fe53f4bbb5d771a6597c663bf72ae82bb707356a2614f83fd5b15"
-	jobID := "ced8524ac7c14c05aae482e02a098b87"
-	jobIDHex := "0x6365643835323461633763313463303561616534383265303261303938623837"
 
 	tests := []struct {
 		name       string
 		initType   string
 		logAddress common.Address
 		wantCount  int
-		wantJSON   models.JSON
 		data       hexutil.Bytes
 	}{
-		{"basic eth log", "ethlog", initrAddress, 1,
-			cltest.JSONFromString(
-				`{"address":"%v","blockHash":"0x0000000000000000000000000000000000000000000000000000000000000000","blockNumber":"0x0","data":"0x","logIndex":"0x0","topics":["%v","%v","%v"],"transactionHash":"0x0000000000000000000000000000000000000000000000000000000000000000","transactionIndex":"0x0"}`,
-				strings.ToLower(initrAddress.String()), eventTopic, nonce, jobIDHex), hexutil.Bytes{}},
-		{"non-matching eth log", "ethlog", cltest.NewEthAddress(), 0, models.JSON{}, hexutil.Bytes{}},
-		{"basic cllog", "chainlinklog", initrAddress, 1,
-			cltest.JSONFromString(
-				`{"value":"100","address":"%v","functionId":"76005c26","dataPrefix":"%v"}`, initrAddress.String(), nonce),
-			cltest.StringToRunLogPayload(`{"value":"100"}`)},
-		{"cllog non-matching", "chainlinklog", cltest.NewEthAddress(), 0, models.JSON{}, hexutil.Bytes{}},
+		{"basic eth log", "ethlog", initrAddress, 1, hexutil.Bytes{}},
+		{"non-matching eth log", "ethlog", cltest.NewEthAddress(), 0, hexutil.Bytes{}},
+		{"basic cllog", "chainlinklog", initrAddress, 1, cltest.StringToRunLogPayload(`{"value":"100"}`)},
+		{"cllog non-matching", "chainlinklog", cltest.NewEthAddress(), 0, hexutil.Bytes{}},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			RegisterTestingT(t)
 
-			store, cleanup := cltest.NewStore()
-			defer cleanup()
 			nl := services.NotificationListener{Store: store}
 			defer nl.Stop()
 			nl.Start()
@@ -82,7 +70,6 @@ func TestNotificationListenerAddJob(t *testing.T) {
 				Type:    test.initType,
 				Address: initrAddress,
 			}}
-			j.ID = jobID
 			assert.Nil(t, store.SaveJob(j))
 
 			nl.AddJob(j)
@@ -90,28 +77,10 @@ func TestNotificationListenerAddJob(t *testing.T) {
 			logChan <- cltest.NewEthNotification(strpkg.EventLog{
 				Address: test.logAddress,
 				Data:    test.data,
-				Topics: []hexutil.Bytes{
-					cltest.StringToBytes(eventTopic),
-					cltest.StringToBytes(nonce),
-					cltest.StringToBytes(jobIDHex),
-				},
+				Topics:  []hexutil.Bytes{cltest.StringToBytes("00"), cltest.StringToBytes("01"), cltest.StringToBytes("22")},
 			})
 
-			if test.wantCount == 0 {
-				Consistently(func() []models.JobRun {
-					jrs, _ := store.JobRunsFor(j)
-					return jrs
-				}).Should(HaveLen(test.wantCount))
-			} else {
-				var jrs []models.JobRun
-				Eventually(func() []models.JobRun {
-					jrs, _ = store.JobRunsFor(j)
-					return jrs
-				}).Should(HaveLen(test.wantCount))
-				jr := jrs[0]
-				params := jr.TaskRuns[0].Task.Params
-				assert.JSONEq(t, test.wantJSON.String(), params.String())
-			}
+			cltest.WaitForRuns(t, j, store, test.wantCount)
 
 			assert.True(t, eth.AllCalled())
 		})
