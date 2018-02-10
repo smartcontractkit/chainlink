290758e800c4c1171903dd0d28128167284d7eac
Thomas Hodges <me@thodges.com>
2018-02-10 12:15:36 -0600

    Godocs for key_store.

--- store/key_store.go
+++ store/key_store.go
@@ -9,10 +9,12 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+// KeyStore manages a key storage directory on disk.
 type KeyStore struct {
 	*keystore.KeyStore
 }
 
+// NewKeyStore creates a keystore for the given directory.
 func NewKeyStore(keyDir string) *KeyStore {
 	ks := keystore.NewKeyStore(
 		keyDir,
@@ -23,10 +25,14 @@ func NewKeyStore(keyDir string) *KeyStore {
 	return &KeyStore{ks}
 }
 
+// HasAccounts returns true if there are accounts located at the keystore
+// directory.
 func (ks *KeyStore) HasAccounts() bool {
 	return len(ks.Accounts()) > 0
 }
 
+// Unlock uses the given password to try to unlock accounts located in the
+// keystore directory.
 func (ks *KeyStore) Unlock(phrase string) error {
 	for _, account := range ks.Accounts() {
 		err := ks.KeyStore.Unlock(account, phrase)
@@ -37,6 +43,7 @@ func (ks *KeyStore) Unlock(phrase string) error {
 	return nil
 }
 
+// SignTx uses the unlocked account to sign the given transaction.
 func (ks *KeyStore) SignTx(tx *types.Transaction, chainID uint64) (*types.Transaction, error) {
 	return ks.KeyStore.SignTx(
 		ks.GetAccount(),
@@ -44,6 +51,7 @@ func (ks *KeyStore) SignTx(tx *types.Transaction, chainID uint64) (*types.Transa
 	)
 }
 
+// GetAccount returns the unlocked account in the KeyStore object.
 func (ks *KeyStore) GetAccount() accounts.Account {
 	return ks.Accounts()[0]
 }
