ca71f8fc82255e78d6edcb933f459d29d0e7734d
Steve Ellis <email@steveell.is>
2018-02-10 21:36:29 -0800

    add an HttpPost adapter

--- adapters/http_test.go
+++ adapters/http_test.go
@@ -1,40 +1,87 @@
 package adapters_test
 
 import (
-	"net/url"
 	"testing"
 
 	gock "github.com/h2non/gock"
 	"github.com/smartcontractkit/chainlink/adapters"
+	"github.com/smartcontractkit/chainlink/internal/cltest"
 	"github.com/smartcontractkit/chainlink/store/models"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHttpGetNotAUrlError(t *testing.T) {
+func TestHttpNotAUrlError(t *testing.T) {
 	t.Parallel()
-	u, err := url.Parse("NotAUrl")
-	assert.Nil(t, err)
 
-	httpGet := adapters.HttpGet{URL: models.WebURL{u}}
-	input := models.RunResult{}
-	result := httpGet.Perform(input, nil)
-	assert.Equal(t, models.JSON{}, result.Output)
-	assert.NotNil(t, result.Error)
+	tests := []struct {
+		name    string
+		adapter adapters.Adapter
+	}{
+		{"HttpGet", &adapters.HttpGet{URL: cltest.MustParseWebURL("NotAURL")}},
+		{"HttpPost", &adapters.HttpGet{URL: cltest.MustParseWebURL("NotAURL")}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.adapter.Perform(models.RunResult{}, nil)
+			assert.Equal(t, models.JSON{}, result.Output)
+			assert.NotNil(t, result.Error)
+		})
+	}
 }
 
 func TestHttpGetResponseError(t *testing.T) {
 	defer gock.Off()
-	u, err := url.Parse(`https://example.com/api`)
-	assert.Nil(t, err)
+	url := cltest.MustParseWebURL(`https://example.com/api`)
 
-	gock.New(u.String()).
+	gock.New(url.String()).
 		Get("").
 		Reply(400).
 		JSON(`Invalid request`)
 
-	httpGet := adapters.HttpGet{URL: models.WebURL{u}}
+	httpGet := adapters.HttpGet{URL: url}
 	input := models.RunResult{}
 	result := httpGet.Perform(input, nil)
 	assert.Equal(t, models.JSON{}, result.Output)
-	assert.NotNil(t, result.Error)
+	assert.NotNil(t, result.Error())
+}
+
+func TestHttpPostAdapterPerform(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name        string
+		status      int
+		want        string
+		wantExists  bool
+		wantErrored bool
+		response    string
+	}{
+		{"success", 200, "so meta", true, false, `so meta`},
+		{"success but error in body", 200, `{"error": "so meta"}`, true, false, `{"error": "so meta"}`},
+		{"success with HTML", 200, `<html>so meta</html>`, true, false, `<html>so meta</html>`},
+		{"server error", 500, "", false, true, `big error`},
+	}
+
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			input := models.RunResultWithValue("modern")
+			wantedBody := `{"value":"modern"}`
+			mock, cleanup := cltest.NewHTTPMockServer(t, test.status, wantedBody, "POST", test.response)
+			defer cleanup()
+
+			hpa := adapters.HttpPost{URL: cltest.MustParseWebURL(mock.URL)}
+			result := hpa.Perform(input, store)
+
+			val, err := result.Get("value")
+			assert.Nil(t, err)
+			assert.Equal(t, test.want, val.String())
+			assert.Equal(t, test.wantExists, val.Exists())
+			assert.Equal(t, test.wantErrored, result.HasError())
+			assert.Equal(t, false, result.Pending)
+		})
+	}
 }
