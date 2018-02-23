2c7bf36f277296023bdaa0dd5c4d43c85fcf957a
Jonny Huxtable <jonnyh@outlook.com>
2018-02-23 18:06:39 +0000

    If password env variable is set and there's no accounts, create one on boot

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func Run(client *cmd.Client, args ...string) {
 				cli.StringFlag{
 					Name:  "password, p",
 					Usage: "password for the node's account",
+					EnvVar: "PASSWORD",
 				},
 				cli.BoolFlag{
 					Name:  "debug, d",
