8b38b7dc598a0da0f438a1ed7767333d1efdb4ec
Thomas Hodges <me@thodges.com>
2018-03-27 08:44:02 -0500

    Added event log to GetterSetter contract and tests.

--- solidity/contracts/examples/GetterSetter.sol
+++ solidity/contracts/examples/GetterSetter.sol
@@ -6,8 +6,12 @@ contract GetterSetter {
   uint256 public getUint256;
   uint256 public requestId;
 
+  event SetBytes32(address indexed from, bytes32 indexed value);
+  event SetUint256(address indexed from, uint256 indexed value);
+
   function setBytes32(bytes32 _value) public {
     getBytes32 = _value;
+    SetBytes32(msg.sender, _value);
   }
 
   function requestedBytes32(uint256 _requestId, bytes32 _value) public {
@@ -17,6 +21,7 @@ contract GetterSetter {
 
   function setUint256(uint256 _value) public {
     getUint256 = _value;
+    SetUint256(msg.sender, _value);
   }
 
   function requestedUint256(uint256 _requestId, uint256 _value) public {
