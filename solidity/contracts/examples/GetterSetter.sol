39cb75e3628e287a923088c14882f37e638a6644
Steve Ellis <email@steveell.is>
2018-04-03 22:20:22 -0400

    improve bytes nesting functionality
    - bump truffle version
    
    Signed-off-by: Dimitri Roche <dimroc@gmail.com>

--- solidity/contracts/examples/GetterSetter.sol
+++ solidity/contracts/examples/GetterSetter.sol
@@ -11,7 +11,7 @@ contract GetterSetter {
 
   function setBytes32(bytes32 _value) public {
     getBytes32 = _value;
-    SetBytes32(msg.sender, _value);
+    emit SetBytes32(msg.sender, _value);
   }
 
   function requestedBytes32(uint256 _requestId, bytes32 _value) public {
@@ -21,7 +21,7 @@ contract GetterSetter {
 
   function setUint256(uint256 _value) public {
     getUint256 = _value;
-    SetUint256(msg.sender, _value);
+    emit SetUint256(msg.sender, _value);
   }
 
   function requestedUint256(uint256 _requestId, uint256 _value) public {
