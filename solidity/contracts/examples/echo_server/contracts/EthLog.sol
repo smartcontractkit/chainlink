e60e9681a540498af55df14e29b297eaeaca90d8
Thomas Hodges <me@thodges.com>
2018-04-04 12:12:41 -0500

    Remove examples from solidity directory.

--- solidity/contracts/examples/echo_server/contracts/EthLog.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.18;
-
-contract EthLog {
-  event LogEvent(bytes32 indexed jobId);
-
-  function logEvent() public {
-    LogEvent("hello_chainlink");
-  }
-}
--- solidity/contracts/examples/uptime_sla/migrations/2_oracle.js
+++
@@ -1,5 +0,0 @@
-let Oracle = artifacts.require("../node_modules/smartcontractkit/chainlink/solidity/contracts/Oracle.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Oracle);
-};
