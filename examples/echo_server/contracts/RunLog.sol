898aa4fbec76cee1e66eafc8b383358a41f7755b
Steve Ellis <email@steveell.is>
2018-04-19 09:50:28 -0700

    update integration test contracts

--- examples/echo_server/contracts/RunLog.sol
+++ examples/echo_server/contracts/RunLog.sol
@@ -3,26 +3,26 @@ pragma solidity ^0.4.21;
 import "../../../solidity/contracts/Chainlinked.sol";
 
 contract RunLog is Chainlinked {
-  uint256 private requestId;
+  bytes32 private requestId;
 
   function RunLog(address _oracle) public {
     setOracle(_oracle);
   }
 
   function request() public {
-    Chainlink.Run memory run = newRun("MY_JOB_ID", this, "fulfill(uint256,bytes32)");
+    ChainlinkLib.Run memory run = newRun("MY_JOB_ID", this, "fulfill(uint256,bytes32)");
     run.add("msg", "hello_chainlink");
     requestId = chainlinkRequest(run);
   }
 
-  function fulfill(uint256 _requestId, bytes32 _data)
+  function fulfill(bytes32 _requestId, bytes32 _data)
     public
     onlyOracle
     checkRequestId(_requestId)
   {
   }
 
-  modifier checkRequestId(uint256 _requestId) {
+  modifier checkRequestId(bytes32 _requestId) {
     require(requestId == _requestId);
     _;
   }
