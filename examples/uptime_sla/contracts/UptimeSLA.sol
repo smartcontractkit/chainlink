ca0ef9d6144afebb3e15af45e00a5f4489c27c09
Dimitri Roche <dimroc@gmail.com>
2018-04-21 10:55:13 -0400

    Update examples to use new solidity cbor encoding

--- examples/uptime_sla/contracts/UptimeSLA.sol
+++ examples/uptime_sla/contracts/UptimeSLA.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 import "../../../solidity/contracts/Chainlinked.sol";
-import "../../../solidity/contracts/Chainlink.sol";
+import "../../../solidity/contracts/ChainlinkLib.sol";
 
 contract UptimeSLA is Chainlinked {
   uint256 constant uptimeThreshold = 9999;
@@ -9,38 +9,41 @@ contract UptimeSLA is Chainlinked {
   uint256 private endAt;
   address private client;
   address private serviceProvider;
-  uint256 public requestId;
+  bytes32 public externalId;
   uint256 public uptime;
 
   function UptimeSLA(
     address _client,
     address _serviceProvider,
+    address _link,
     address _oracle,
     bytes32 _jobId
   ) public payable {
     client = _client;
     serviceProvider = _serviceProvider;
     endAt = block.timestamp + 30 days;
-    oracle = Oracle(_oracle);
     jobId = _jobId;
+
+    setLinkToken(_link);
+    setOracle(_oracle);
   }
 
   function updateUptime(string _when) public {
-    Chainlink.Run memory run = newRun(jobId, this, "report(uint256,uint256)");
+    ChainlinkLib.Run memory run = newRun(jobId, this, "report(bytes32,uint256)");
     run.add("url", "https://status.heroku.com/api/ui/availabilities");
     string[] memory path = new string[](4);
     path[0] = "data";
     path[1] = _when;
     path[2] = "attributes";
     path[3] = "calculation";
-    run.add("path", path);
-    requestId = chainlinkRequest(run);
+    run.addStringArray("path", path);
+    externalId = chainlinkRequest(run);
   }
 
-  function report(uint256 _requestId, uint256 _uptime)
+  function report(bytes32 _externalId, uint256 _uptime)
     public
     onlyOracle
-    checkRequestId(_requestId)
+    checkRequestId(_externalId)
   {
     uptime = _uptime;
     if (_uptime < uptimeThreshold) {
@@ -50,8 +53,8 @@ contract UptimeSLA is Chainlinked {
     }
   }
 
-  modifier checkRequestId(uint256 _requestId) {
-    require(requestId == _requestId);
+  modifier checkRequestId(bytes32 _externalId) {
+    require(externalId == _externalId);
     _;
   }
 
