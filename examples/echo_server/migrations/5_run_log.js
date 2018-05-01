51b6a7a8566416c8df226ce55eaf8feb5756e073
Dimitri Roche <dimroc@gmail.com>
2018-05-01 08:01:53 -0400

    examples/echo_server transfers link in separate migration

--- examples/echo_server/migrations/5_run_log.js
+++ examples/echo_server/migrations/5_run_log.js
@@ -4,7 +4,4 @@ let RunLog = artifacts.require("./RunLog.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(RunLog, LinkToken.address, Oracle.address);
-  LinkToken.deployed().then(function(instance) {
-    return instance.transfer(RunLog.address, 5000000000000000000);
-  }).then(console.log, console.log);
 };
