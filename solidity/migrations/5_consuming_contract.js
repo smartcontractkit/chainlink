751a6bb43e37ba1fef2cbb40c88513501434c64e
Thomas Hodges <me@thodges.com>
2018-05-02 16:56:54 -0500

    Added jobid to constructor and will automatically add to node.

--- solidity/migrations/5_consuming_contract.js
+++ solidity/migrations/5_consuming_contract.js
@@ -1,7 +1,15 @@
-var Consumer = artifacts.require("./Consumer.sol");
-var Oracle = artifacts.require("./Oracle.sol");
-var LinkToken = artifacts.require("./LinkToken.sol");
+let chainlinkDeployer = require("../chainlink_deployer.js");
+let Consumer = artifacts.require("./Consumer.sol");
+let Oracle = artifacts.require("./Oracle.sol");
+let LinkToken = artifacts.require("./LinkToken.sol");
+let fs = require('fs');
 
-module.exports = function(deployer) {
-  deployer.deploy(Consumer, LinkToken.address, Oracle.address);
+module.exports = function(truffleDeployer) {
+  chainlinkDeployer.job("../http_consumer_job.json", function(error, response, body) {
+    console.log(`Deploying Consumer:`)
+    console.log(`\tjob: ${body.id}`);
+    truffleDeployer.deploy(Consumer, LinkToken.address, Oracle.address, body.id);
+  }, function(error) {
+    console.log("chainlink error:", error);
+  });
 };
