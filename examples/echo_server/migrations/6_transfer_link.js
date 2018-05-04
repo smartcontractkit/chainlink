61d41db66e8857057bd09310a5461e40fdd71366
Dimitri Roche <dimroc@gmail.com>
2018-05-04 08:35:18 -0400

    Update echo_server RunLog to take jobId in ctor

--- examples/echo_server/migrations/6_transfer_link.js
+++ examples/echo_server/migrations/6_transfer_link.js
@@ -2,11 +2,11 @@ let LinkToken = artifacts.require("../node_modules/smartcontractkit/chainlink/so
 let RunLog = artifacts.require("./RunLog.sol");
 let devnetAddress = "0x9CA9d2D5E04012C9Ed24C0e513C9bfAa4A2dD77f";
 
-module.exports = async function(deployer) {
-  await LinkToken.deployed().then(async function(linkInstance) {
+module.exports = function(deployer) {
+  LinkToken.deployed().then(async function(linkInstance) {
     await RunLog.deployed().then(async function(runLogInstance) {
       await linkInstance.transfer(runLogInstance.address, web3.toWei(1000));
       await linkInstance.transfer(devnetAddress, web3.toWei(1000));
-    });
+    }).catch(console.log);
   });
 };
