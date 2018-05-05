100b9724e546e953aeae3b1d9594504cdd64c8f0
Thomas Hodges <me@thodges.com>
2018-05-05 15:48:32 -0500

    Added MINIMUM_CONTRACT_PAYMENT to debug output on run

--- store/config_test.go
+++ store/config_test.go
@@ -15,4 +15,5 @@ func TestStore_ConfigDefaults(t *testing.T) {
 	assert.Equal(t, uint64(0), config.ChainID)
 	assert.Equal(t, *big.NewInt(20000000000), config.EthGasPriceDefault)
 	assert.Equal(t, "0x514910771AF9Ca656af840dff83E8264EcF986CA", common.HexToAddress(config.LinkContractAddress).String())
+	assert.Equal(t, *big.NewInt(1000000000000000000), config.MinimumContractPayment)
 }
