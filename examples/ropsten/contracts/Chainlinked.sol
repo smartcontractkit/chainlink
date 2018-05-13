faaef9eb952bd65420843c77c53f91284cd7848b
Thomas Hodges <me@thodges.com>
2018-05-13 06:51:33 -0500

    Fix Chainlinked imports

--- examples/ropsten/contracts/Chainlinked.sol
+++ examples/ropsten/contracts/Chainlinked.sol
@@ -1,27 +1,15 @@
 pragma solidity ^0.4.23;
 
 import "./ChainlinkLib.sol";
-
-contract LinkToken {
-  // ERC20 interface
-  function transfer(address to, uint tokens) public returns (bool success);
-  function approve(address spender, uint tokens) public returns (bool success);
-  function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-  // ERC677 interface
-  function transferAndCall(address receiver, uint amount, bytes data) public returns (bool success);
-}
-
-contract Oracle {
-  function cancel(uint256 _internalId) public;
-}
+import "./Oracle.sol";
+import "github.com/smartcontractkit/LinkToken/contracts/LinkToken.sol";
+import "github.com/smartcontractkit/solidity-cborutils/contracts/CBOR.sol";
 
 contract Chainlinked {
   using ChainlinkLib for ChainlinkLib.Run;
   using CBOR for Buffer.buffer;
 
   uint256 constant clArgsVersion = 1;
-  bytes4 constant oracleFid = bytes4(keccak256("requestData(uint256,bytes32,address,bytes4,bytes32,bytes)"));
 
   LinkToken internal link;
   Oracle internal oracle;
@@ -31,33 +19,20 @@ contract Chainlinked {
     bytes32 _jobId,
     address _callbackAddress,
     string _callbackFunctionSignature
-  ) internal returns (ChainlinkLib.Run memory) {
+  ) internal pure returns (ChainlinkLib.Run memory) {
     ChainlinkLib.Run memory run;
-    Buffer.init(run.buf, 128);
-    run.jobId = _jobId;
-    run.callbackAddress = _callbackAddress;
-    run.callbackFunctionId = bytes4(keccak256(_callbackFunctionSignature));
-    run.buf.startMap();
-
-    return run;
+    return run.initialize(_jobId, _callbackAddress, _callbackFunctionSignature);
   }
 
   function chainlinkRequest(ChainlinkLib.Run memory _run, uint256 _wei)
-  internal
-  returns(bytes32)
+    internal
+    returns(bytes32)
   {
-    bytes32 requestId = keccak256(this, requests++);
-    bytes memory requestDataABI = abi.encodeWithSelector(
-      oracleFid,
-      clArgsVersion,
-      _run.jobId,
-      _run.callbackAddress,
-      _run.callbackFunctionId,
-      requestId,
-      _run.close());
-    require(link.transferAndCall(oracle, _wei, requestDataABI));
-
-    return requestId;
+    requests += 1;
+    _run.requestId = bytes32(requests);
+    _run.close();
+    require(link.transferAndCall(oracle, _wei, _run.encodeForOracle(clArgsVersion)));
+    return _run.requestId;
   }
 
   function LINK(uint256 _amount) internal pure returns (uint256) {
