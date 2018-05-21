8497cb1dd23d9218f0ef9daf5bf2f60724d6ed38
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-05-21 10:53:07 -0700

    Use type alias for Link currency

--- store/assets/currencies.go
+++ store/assets/currencies.go
@@ -6,34 +6,41 @@ import (
 )
 
 // Link contains a field to represent the smallest units of LINK
-type Link struct {
-	wei *big.Int
-}
+type Link big.Int
 
 // NewLink returns a new struct to represent LINK from it's smallest unit
-func NewLink(w *big.Int) Link {
-	return Link{wei: w}
+func NewLink(w int64) *Link {
+	return (*Link)(big.NewInt(w))
 }
 
-func (l Link) String() string {
+func (l *Link) String() string {
 	b := &big.Int{}
 	b.SetString("1000000000000000000", 10)
 	r := &big.Rat{}
-	r.SetFrac(l.wei, b)
+	r.SetFrac((*big.Int)(l), b)
 	return fmt.Sprintf("%v", r.FloatString(18))
 }
 
+// SetInt64 delegates to *big.Int.SetInt64
+func (l *Link) SetInt64(w int64) *Link {
+	return (*Link)((*big.Int)(l).SetInt64(w))
+}
+
+// SetString delegates to *big.Int.SetString
+func (l *Link) SetString(s string, base int) (*Link, bool) {
+	w, ok := (*big.Int)(l).SetString(s, base)
+	return (*Link)(w), ok
+}
+
 // MarshalText implements the encoding.TextMarshaler interface.
-func (l Link) MarshalText() ([]byte, error) {
-	return l.wei.MarshalText()
+func (l *Link) MarshalText() ([]byte, error) {
+	return (*big.Int)(l).MarshalText()
 }
 
 // UnmarshalText implements the encoding.TextUnmarshaler interface.
 func (l *Link) UnmarshalText(text []byte) error {
-	b := &big.Int{}
-	if _, ok := b.SetString(string(text), 10); !ok {
+	if _, ok := l.SetString(string(text), 10); !ok {
 		return fmt.Errorf("assets: cannot unmarshal %q into a *assets.Link", text)
 	}
-	l.wei = b
 	return nil
 }
