e759435e4eb3d41ca27a28459e7220cbea465d5d
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-05-24 10:55:43 -0700

    Combine GUI as root dev dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   moduleDirectories: [
     'node_modules',
-    '<rootDir>/src/',
-    '<rootDir>/support/'
+    '<rootDir>/gui/src/',
+    '<rootDir>/gui/support/'
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: [
-    '<rootDir>/dist/',
-    '<rootDir>/tmp/',
+    '<rootDir>/gui/dist/',
+    '<rootDir>/gui/tmp/',
     '<rootDir>/node_modules/'
   ]
 }
