d80447459785ef9c9566d783d7df83ade0037c12
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-05-30 11:35:44 -0700

    Remove redux-logger. Use devtools instead

--- gui/src/connectors/redux/index.js
+++ gui/src/connectors/redux/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
 import reducer from './reducers'
 
 if (typeof window === 'undefined') {
@@ -8,10 +7,6 @@ if (typeof window === 'undefined') {
 }
 
 const middleware = [thunkMiddleware]
-if (process.env.NODE_ENV !== 'test') {
-  middleware.push(createLogger())
-}
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default () => (
