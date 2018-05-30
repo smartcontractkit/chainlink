68619fa805d560b14a90d15649512c6af59bc604
Steve Ellis <email@steveell.is>
2018-05-29 22:16:52 -0700

    return full contract object, not just address

--- solidity/app/deploy.js
+++ solidity/app/deploy.js
@@ -1,18 +1,32 @@
+const Web3 = require('web3')
+
 let compile = require('./compile.js')
 let personal = require('./personal.js')
 let utils = require('./utils.js')
 let wallet = require('./wallet.js')
+let web3 = new Web3('http://localhost:18545')
+
+function getBytecode (contract) {
+  return contract.evm.bytecode.object.toString()
+}
+
+function objectify (compiled, address) {
+  let contract = new web3.eth.Contract(compiled.abi, address)
+  contract.address = address
+  return contract
+}
 
 module.exports = async function deploy (filename) {
-  let bytecode = compile(filename)
+  let compiled = compile(filename)
 
   await personal.send({to: wallet.address, value: utils.toWei(1)})
+  let data = '0x' +
+    getBytecode(compiled) +
+    '0000000000000000000000004b274dfcd56656742A55ad54549b3770c392aA87'
   let txHash = await wallet.send({
     gas: 2000000,
-    data: '0x' + bytecode + '0000000000000000000000004b274dfcd56656742A55ad54549b3770c392aA87'
+    data: data
   })
-  return utils.getTxReceipt(txHash).then((receipt) => {
-    let address = receipt.contractAddress
-    return address
-  })
+  let receipt = await utils.getTxReceipt(txHash)
+  return objectify(compiled, receipt.contractAddress)
 }
