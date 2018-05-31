c489cedf009c5712b3316d2d33fd77a32f6b598e
Steve Ellis <email@steveell.is>
2018-05-30 21:14:57 -0700

    use new deployer with truffle

--- solidity/app/deploy.js
+++ solidity/app/deploy.js
@@ -2,7 +2,6 @@ const Eth = require('ethjs')
 const TruffleContract = require('truffle-contract')
 
 let compile = require('./compile.js')
-let personal = require('./personal.js')
 let wallet = require('./wallet.js')
 
 function getBytecode (contract) {
@@ -18,13 +17,14 @@ async function contractify (compiled, address) {
 module.exports = async function deploy (filename) {
   let compiled = compile(filename)
 
-  await personal.send({to: wallet.address, value: clUtils.toWei(1)})
-  let data = '0x' +
-    getBytecode(compiled) +
-    '0000000000000000000000004b274dfcd56656742A55ad54549b3770c392aA87'
+  let fundingTx = await clUtils.send({
+    to: wallet.address,
+    value: clUtils.toWei(1)
+  })
+  await clUtils.getTxReceipt(fundingTx)
   let txHash = await wallet.send({
     gas: 2000000,
-    data: data
+    data: '0x' + getBytecode(compiled)
   })
   let receipt = await clUtils.getTxReceipt(txHash)
   return contractify(compiled, receipt.contractAddress)
