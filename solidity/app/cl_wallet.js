ccd8548822be5cc8ed9e7f185ec4fcd140010330
John Barker <jebarker@gmail.com>
2018-06-01 18:07:58 -0700

    use default truffle key for contract deployment
    - add method to make wallet easily configurable
    
    Signed-off-by: Steve Ellis <email@steveell.is>

---
+++ solidity/app/cl_wallet.js
@@ -0,0 +1,24 @@
+require('./cl_utils.js')
+
+const Tx = require('ethereumjs-tx')
+const Wallet = require('ethereumjs-wallet')
+
+global.clWallet = global.clWallet || {
+  send: async function send (params) {
+    let eth = clUtils.eth
+    let defaults = {
+      nonce: await eth.getTransactionCount(this.address),
+      chainId: 0
+    }
+    let tx = new Tx(Object.assign(defaults, params))
+    tx.sign(this.privateKey)
+    let txHex = tx.serialize().toString('hex')
+    return eth.sendRawTransaction(txHex)
+  },
+  setDefaultKey: function setDefaultKey(key) {
+    this.privateKey = Buffer.from(key, 'hex')
+    const wallet = Wallet.fromPrivateKey(this.privateKey)
+    this.address = wallet.getAddress().toString('hex')
+  }
+}
+clWallet.setDefaultKey('4d6cf3ce1ac71e79aa33cf481dedf2e73acb548b1294a70447c960784302d2fb')
