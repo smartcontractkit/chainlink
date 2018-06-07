b794e1c40c0a20a305122da56a08882e1490fee7
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-06-07 11:00:05 -0400

    Make sync assertions after await

--- gui/__tests__/containers/Jobs.test.js
+++ gui/__tests__/containers/Jobs.test.js
@@ -40,16 +40,15 @@ describe('containers/Job', () => {
 
     const wrapper = mountJobs()
 
-    await syncFetch(wrapper).then(() => {
-      expect(wrapper.text()).toContain('c60b9927eeae43168ddbe92584937b1b')
-      expect(wrapper.text()).toContain('web')
-      expect(wrapper.text()).toContain('2018-05-10T00:41:54.531043837Z')
+    await syncFetch(wrapper)
+    expect(wrapper.text()).toContain('c60b9927eeae43168ddbe92584937b1b')
+    expect(wrapper.text()).toContain('web')
+    expect(wrapper.text()).toContain('2018-05-10T00:41:54.531043837Z')
 
-      expect(wrapper.text()).toContain('Ethereum10.12k')
-      expect(wrapper.text()).toContain('Link7.46k')
+    expect(wrapper.text()).toContain('Ethereum10.12k')
+    expect(wrapper.text()).toContain('Link7.46k')
 
-      expect(wrapper.text()).toContain('Jobs1')
-    })
+    expect(wrapper.text()).toContain('Jobs1')
   })
 
   it('can page through the list of jobs', async () => {
@@ -61,27 +60,24 @@ describe('containers/Job', () => {
 
     const wrapper = mountJobs({pageSize: 1})
 
-    await syncFetch(wrapper).then(() => {
-      expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
-      expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
-    })
+    await syncFetch(wrapper)
+    expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
+    expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
 
     const pageTwoResponse = jsonApiJobSpecsFactory([{ id: 'ID-ON-SECOND-PAGE' }], 2)
     global.fetch.getOnce('/v2/specs?page=2&size=1', pageTwoResponse)
     clickNextPage(wrapper)
 
-    await syncFetch(wrapper).then(() => {
-      expect(wrapper.text()).not.toContain('ID-ON-FIRST-PAGE')
-      expect(wrapper.text()).toContain('ID-ON-SECOND-PAGE')
-    })
+    await syncFetch(wrapper)
+    expect(wrapper.text()).not.toContain('ID-ON-FIRST-PAGE')
+    expect(wrapper.text()).toContain('ID-ON-SECOND-PAGE')
 
     global.fetch.getOnce('/v2/specs?page=1&size=1', pageOneResponse)
     clickPreviousPage(wrapper)
 
-    await syncFetch(wrapper).then(() => {
-      expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
-      expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
-    })
+    await syncFetch(wrapper)
+    expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
+    expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
   })
 
   it('displays an error message when the network requests fail', async () => {
@@ -91,16 +87,15 @@ describe('containers/Job', () => {
 
     const wrapper = mountJobs()
 
-    await syncFetch(wrapper).then(() => {
-      expect(wrapper.text()).toContain(
-        'There was an error fetching the jobs. Please reload the page.'
-      )
-      expect(wrapper.text()).toContain(
-        'Ethereumerror fetching balance'
-      )
-      expect(wrapper.text()).toContain(
-        'Linkerror fetching balance'
-      )
-    })
+    await syncFetch(wrapper)
+    expect(wrapper.text()).toContain(
+      'There was an error fetching the jobs. Please reload the page.'
+    )
+    expect(wrapper.text()).toContain(
+      'Ethereumerror fetching balance'
+    )
+    expect(wrapper.text()).toContain(
+      'Linkerror fetching balance'
+    )
   })
 })
