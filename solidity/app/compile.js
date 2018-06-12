be38cfbe75e77f53e0e5aa0a0ea3fe0c2eead2fc
John Barker <jebarker@gmail.com>
2018-06-12 13:19:53 -0400

    Improve deploy script path lookup and allow it to be customized
      * Do path lookup relative to script
      * Add relative path of file to compile
      * Allow adding paths through SOLIDITY_INCLUDE environment variable
      (separated by :)

--- solidity/app/compile.js
+++ solidity/app/compile.js
@@ -2,27 +2,12 @@ const solc = require('solc')
 const fs = require('fs')
 const path = require('path')
 
-const lookupPaths = [
-  './',
-  './contracts/',
-  './node_modules/',
-  './node_modules/linkToken/contracts/'
-]
+const INCLUDE_PATHS = ['./']
+const SCRIPT_PATH = path.dirname(process.argv[1])
 
-function solCompile (filename) {
-  function lookupIncludeFile (filename) {
-    for (let path of lookupPaths) {
-      const fullPath = path + filename
-      if (fs.existsSync(fullPath)) {
-        return {contents: fs.readFileSync(fullPath).toString()}
-      }
-    }
-    console.log('Unable to load', filename)
-    return null
-  }
-
+function solidityCompile (filename, lookupIncludeFile) {
   const inputBasename = path.basename(filename).toString()
-  const solInput = {
+  const input = {
     language: 'Solidity',
     sources: {[inputBasename]: {'urls': [filename]}},
     settings: {
@@ -33,8 +18,35 @@ function solCompile (filename) {
       }
     }
   }
-  const output = solc.compileStandardWrapper(JSON.stringify(solInput), lookupIncludeFile)
-  return JSON.parse(output)
+
+  const solOutput = solc.compileStandardWrapper(JSON.stringify(input), lookupIncludeFile)
+  return JSON.parse(solOutput)
+}
+
+function environmentPaths () {
+  return (process.env['SOLIDITY_INCLUDE'] || '').split(/:/)
+}
+
+function compile (filename) {
+  // Include the relative path of the specified file in the lookup paths
+  let lookupPaths = INCLUDE_PATHS.concat(path.dirname(filename))
+
+  // Add any paths defined in the environment
+  lookupPaths = lookupPaths.concat(environmentPaths())
+
+  function lookupIncludeFile (includeFile) {
+    for (let lookupPath of lookupPaths) {
+      // Do all path lookups relative to the script
+      const fullPath = path.resolve(SCRIPT_PATH, lookupPath, includeFile)
+      if (fs.existsSync(fullPath)) {
+        return {contents: fs.readFileSync(fullPath).toString()}
+      }
+    }
+
+    throw new Error(`Unable to load ${includeFile} searched in ${lookupPaths.join(' ')}`)
+  }
+
+  return solidityCompile(filename, lookupIncludeFile)
 }
 
 function checkCompilerErrors (errors) {
@@ -65,7 +77,7 @@ function getContract (output, contractName) {
 
 module.exports = (filename) => {
   const contractName = path.basename(filename).toString()
-  const compiled = solCompile(filename)
+  const compiled = compile(filename)
   checkCompilerErrors(compiled.errors)
   return getContract(compiled, contractName)
 }
