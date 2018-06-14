57a99b1dd22810e25186e244aba899109590d6bf
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-06-14 10:41:54 -0700

    Apply simple lint fixes to solidity js
    Signed-off-by: Steve Ellis <email@steveell.is>

--- solidity/migrations/5_consuming_contract.js
+++ solidity/migrations/5_consuming_contract.js
@@ -1,24 +1,24 @@
-const clmigration = require("../clmigration.js");
-const request = require("request-promise");
-const Consumer = artifacts.require("./Consumer.sol");
-const Oracle = artifacts.require("./Oracle.sol");
-const LinkToken = artifacts.require("./LinkToken.sol");
+const clmigration = require('../clmigration.js')
+const request = require('request-promise')
+const Consumer = artifacts.require('./Consumer.sol')
+const Oracle = artifacts.require('./Oracle.sol')
+const LinkToken = artifacts.require('./LinkToken.sol')
 
-let url = "http://chainlink:twochains@localhost:6688/v2/specs";
+let url = 'http://chainlink:twochains@localhost:6688/v2/specs'
 let job = {
-  "initiators": [{ "type": "runlog" }],
-  "tasks": [
-    { "type": "httpGet" },
-    { "type": "jsonParse" },
-    { "type": "multiply", "times": 100 },
-    { "type": "ethuint256" },
-    { "type": "ethtx" }
+  'initiators': [{ 'type': 'runlog' }],
+  'tasks': [
+    { 'type': 'httpGet' },
+    { 'type': 'jsonParse' },
+    { 'type': 'multiply', 'times': 100 },
+    { 'type': 'ethuint256' },
+    { 'type': 'ethtx' }
   ]
 }
 
-module.exports = clmigration(async function(truffleDeployer) {
-  let body = await request.post(url, {json: job});
-  console.log(`Deploying Consumer:`);
-  console.log(`\tjob: ${body.id}`);
-  await truffleDeployer.deploy(Consumer, LinkToken.address, Oracle.address, body.id);
-});
+module.exports = clmigration(async function (truffleDeployer) {
+  let body = await request.post(url, {json: job})
+  console.log(`Deploying Consumer:`)
+  console.log(`\tjob: ${body.id}`)
+  await truffleDeployer.deploy(Consumer, LinkToken.address, Oracle.address, body.id)
+})
