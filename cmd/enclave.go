307633b3011ddfd4e48de7af450250ff949ebe8c
John Barker <jebarker@gmail.com>
2018-06-19 15:44:01 -0400

    Chainlink SGX adapter stubs!
    This PR adds support for a build of chainlink that includes an
    alternative HTTP adapter that executes from within an enclave. This SGX
    HTTP adapter is currently just a stub.
    
    Build tags are used to disable SGX code when it is undesirable, since it
    needs the SGX SDK installed and can only really be built on Linux.
    
    Other details:
      1) SGX is disabled by default across the board
      2) SGX mode is built in simulation mode by default
      2) Integration tests do not use SGX
      3) Includes a docker compose setup for running locally

---
+++ cmd/enclave.go
@@ -0,0 +1,14 @@
+// +build !sgx_enclave
+
+package cmd
+
+import (
+	"github.com/smartcontractkit/chainlink/logger"
+)
+
+// InitEnclave is a stub in non SGX enabled builds.
+func InitEnclave() error {
+	logger.Infow("SGX enclave *NOT* loaded")
+	logger.Infow("This version of chainlink was not built with support for SGX tasks")
+	return nil
+}
---
+++ cmd/enclave_sgx.go
@@ -0,0 +1,24 @@
+// +build sgx_enclave
+
+package cmd
+
+/*
+#cgo LDFLAGS: -L../sgx/target/release/ -ladapters
+#include "../sgx/libadapters/adapters.h"
+*/
+import "C"
+import (
+	"fmt"
+
+	"github.com/smartcontractkit/chainlink/logger"
+)
+
+// InitEnclave initialized the SGX enclave for use by adapters
+func InitEnclave() error {
+	_, err := C.init_enclave()
+	if err != nil {
+		return fmt.Errorf("error initializing SGX enclave: %+v", err)
+	}
+	logger.Infow("SGX Enclave Loaded")
+	return nil
+}
