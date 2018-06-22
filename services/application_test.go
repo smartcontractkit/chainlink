5772ecce81d13d80dce8294b8dc9f649aef505c8
Steve Ellis <email@steveell.is>
2018-06-22 01:07:21 -0700

    add job run executor fed by a channel via store
    - allows runs to be initiated from wherever the store is made available

--- services/application_test.go
+++ services/application_test.go
@@ -3,18 +3,21 @@
 package services_test
 
 import (
+	"fmt"
 	"syscall"
 	"testing"
+	"time"
 
-	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega"
 	"github.com/smartcontractkit/chainlink/internal/cltest"
+	"github.com/smartcontractkit/chainlink/store/models"
+	"github.com/smartcontractkit/chainlink/utils"
+	"github.com/stretchr/testify/assert"
 	"github.com/tevino/abool"
 )
 
 func TestServices_ApplicationSignalShutdown(t *testing.T) {
-	RegisterTestingT(t)
-	app, cleanup := cltest.NewApplication()
-	defer cleanup()
+	app, _ := cltest.NewApplication()
 
 	completed := abool.New()
 	app.Exiter = func(code int) {
@@ -24,7 +27,29 @@ func TestServices_ApplicationSignalShutdown(t *testing.T) {
 	app.Start()
 	syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
 
-	Eventually(func() bool {
+	gomega.NewGomegaWithT(t).Eventually(func() bool {
 		return completed.IsSet()
-	}).Should(BeTrue())
+	}).Should(gomega.BeTrue())
+}
+
+func TestApplication_ResumeSleptRuns(t *testing.T) {
+	app, cleanup := cltest.NewApplicationWithKeyStore()
+	defer cleanup()
+	ethMock := app.MockEthClient()
+	ethMock.Register("eth_getTransactionCount", utils.Uint64ToHex(0))
+
+	j := models.NewJob()
+	i := models.Initiator{Type: models.InitiatorWeb}
+	j.Initiators = []models.Initiator{i}
+	json := fmt.Sprintf(`{"until":"%v"}`, utils.ISO8601UTC(time.Now().Add(time.Second)))
+	j.Tasks = []models.TaskSpec{cltest.NewTask("sleep", json)}
+	assert.NoError(t, app.Store.Save(&j))
+
+	jr := j.NewRun(i)
+	jr.Status = models.RunStatusPendingSleep
+	assert.NoError(t, app.Store.Save(&jr))
+
+	assert.NoError(t, app.ResumeSleptRuns())
+	input := <-app.Store.RunChannel
+	assert.Equal(t, jr.ID, input.JobRunID)
 }
