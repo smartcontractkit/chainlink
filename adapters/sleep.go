3cb9bcbe0b17c0556b4d95459946f239d7dbb24b
Steve Ellis <email@steveell.is>
2018-06-22 01:11:21 -0700

    remove max duration for sleep adapter

--- adapters/sleep.go
+++ adapters/sleep.go
@@ -1,9 +1,6 @@
 package adapters
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/smartcontractkit/chainlink/store"
 	"github.com/smartcontractkit/chainlink/store/models"
 )
@@ -13,17 +10,12 @@ type Sleep struct {
 	EndAt models.Time `json:"until"`
 }
 
-const maxDuration = 30 * 24 * time.Hour
-
 // Perform returns the input RunResult after waiting for the specified EndAt time.
 func (adapter *Sleep) Perform(input models.RunResult, store *store.Store) models.RunResult {
 	duration := adapter.EndAt.DurationFromNow()
 	if duration <= 0 {
 		input.Status = models.RunStatusCompleted
 		return input
-	}
-	if duration > maxDuration {
-		return input.WithError(fmt.Errorf("Sleep Adapter: %v is greater than max duration %v", duration, maxDuration))
 	}
 
 	input.Status = models.RunStatusPendingSleep
