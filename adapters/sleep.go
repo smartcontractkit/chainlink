2fc71af83004756a4b79f7df461663c7756f5d87
Steve Ellis <email@steveell.is>
2018-06-25 16:53:19 -0700

    rename attribute for consistency

--- adapters/sleep.go
+++ adapters/sleep.go
@@ -7,12 +7,12 @@ import (
 
 // Sleep adapter allows a job to do nothing for some amount of wall time.
 type Sleep struct {
-	EndAt models.Time `json:"until"`
+	Until models.Time `json:"until"`
 }
 
-// Perform returns the input RunResult after waiting for the specified EndAt time.
+// Perform returns the input RunResult after waiting for the specified Until parameter.
 func (adapter *Sleep) Perform(input models.RunResult, store *store.Store) models.RunResult {
-	duration := adapter.EndAt.DurationFromNow()
+	duration := adapter.Until.DurationFromNow()
 	if duration <= 0 {
 		input.Status = models.RunStatusCompleted
 		return input
