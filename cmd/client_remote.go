fe4514771cdbb4b1df9ed3f6ccc68a790737bc38
Dimitri Roche <dimroc@gmail.com>
2018-07-16 09:27:40 -0400

    Update all remote GET commands to use new authentication

--- cmd/client_remote.go
+++ cmd/client_remote.go
@@ -25,6 +25,8 @@ import (
 	"go.uber.org/multierr"
 )
 
+var errUnauthorized = errors.New("401 Unauthorized")
+
 // DisplayAccountBalance renders a table containing the active account address
 // with it's ETH & LINK balance
 func (cli *Client) DisplayAccountBalance(c *clipkg.Context) error {
@@ -47,12 +49,7 @@ func (cli *Client) ShowJobSpec(c *clipkg.Context) error {
 	if !c.Args().Present() {
 		return cli.errorOut(errors.New("Must pass the job id to be shown"))
 	}
-	cfg := cli.Config
-	resp, err := utils.BasicAuthGet(
-		cfg.BasicAuthUsername,
-		cfg.BasicAuthPassword,
-		cfg.ClientNodeURL+"/v2/specs/"+c.Args().First(),
-	)
+	resp, err := cli.RemoteClient.Get("/v2/specs/" + c.Args().First())
 	if err != nil {
 		return cli.errorOut(err)
 	}
@@ -63,9 +60,6 @@ func (cli *Client) ShowJobSpec(c *clipkg.Context) error {
 
 // GetJobSpecs returns all job specs.
 func (cli *Client) GetJobSpecs(c *clipkg.Context) error {
-	cfg := cli.Config
-	requestURI := cfg.ClientNodeURL + "/v2/specs"
-
 	page := 0
 	if c != nil && c.IsSet("page") {
 		page = c.Int("page")
@@ -73,7 +67,7 @@ func (cli *Client) GetJobSpecs(c *clipkg.Context) error {
 
 	var links jsonapi.Links
 	var jobs []models.JobSpec
-	err := cli.getPage(requestURI, page, &jobs, &links)
+	err := cli.getPage("/v2/specs", page, &jobs, &links)
 	if err != nil {
 		return err
 	}
@@ -141,15 +135,10 @@ func (cli *Client) CreateJobRun(c *clipkg.Context) error {
 
 // BackupDatabase streams a backup of the node's db to the passed filepath.
 func (cli *Client) BackupDatabase(c *clipkg.Context) error {
-	cfg := cli.Config
 	if !c.Args().Present() {
 		return cli.errorOut(errors.New("Must pass the path to save the backup"))
 	}
-	resp, err := utils.BasicAuthGet(
-		cfg.BasicAuthUsername,
-		cfg.BasicAuthPassword,
-		cfg.ClientNodeURL+"/v2/backup",
-	)
+	resp, err := cli.RemoteClient.Get("/v2/backup")
 	if err != nil {
 		return cli.errorOut(err)
 	}
@@ -197,9 +186,6 @@ func (cli *Client) AddBridge(c *clipkg.Context) error {
 
 // GetBridges returns all bridges.
 func (cli *Client) GetBridges(c *clipkg.Context) error {
-	cfg := cli.Config
-	requestURI := cfg.ClientNodeURL + "/v2/bridge_types"
-
 	page := 0
 	if c != nil && c.IsSet("page") {
 		page = c.Int("page")
@@ -207,7 +193,7 @@ func (cli *Client) GetBridges(c *clipkg.Context) error {
 
 	var links jsonapi.Links
 	var bridges []models.BridgeType
-	err := cli.getPage(requestURI, page, &bridges, &links)
+	err := cli.getPage("/v2/bridge_types", page, &bridges, &links)
 	if err != nil {
 		return err
 	}
@@ -215,8 +201,6 @@ func (cli *Client) GetBridges(c *clipkg.Context) error {
 }
 
 func (cli *Client) getPage(requestURI string, page int, model interface{}, links *jsonapi.Links) error {
-	cfg := cli.Config
-
 	uri, err := url.Parse(requestURI)
 	if err != nil {
 		return err
@@ -227,7 +211,7 @@ func (cli *Client) getPage(requestURI string, page int, model interface{}, links
 	}
 	uri.RawQuery = q.Encode()
 
-	resp, err := utils.BasicAuthGet(cfg.BasicAuthUsername, cfg.BasicAuthPassword, uri.String())
+	resp, err := cli.RemoteClient.Get(uri.String())
 	if err != nil {
 		return cli.errorOut(err)
 	}
@@ -238,15 +222,10 @@ func (cli *Client) getPage(requestURI string, page int, model interface{}, links
 
 // ShowBridge returns the info for the given Bridge name.
 func (cli *Client) ShowBridge(c *clipkg.Context) error {
-	cfg := cli.Config
 	if !c.Args().Present() {
 		return cli.errorOut(errors.New("Must pass the name of the bridge to be shown"))
 	}
-	resp, err := utils.BasicAuthGet(
-		cfg.BasicAuthUsername,
-		cfg.BasicAuthPassword,
-		cfg.ClientNodeURL+"/v2/bridge_types/"+c.Args().First(),
-	)
+	resp, err := cli.RemoteClient.Get("/v2/bridge_types/" + c.Args().First())
 	if err != nil {
 		return cli.errorOut(err)
 	}
@@ -299,9 +278,9 @@ func fromFile(arg string) (*bytes.Buffer, error) {
 
 // deserializeAPIResponse is distinct from deserializeResponse in that it supports JSONAPI responses with Links
 func (cli *Client) deserializeAPIResponse(resp *http.Response, dst interface{}, links *jsonapi.Links) error {
-	b, err := parseResponse(resp)
+	b, err := cli.parseResponse(resp)
 	if err != nil {
-		return cli.errorOut(err)
+		return err
 	}
 	if err = web.ParsePaginatedResponse(b, dst, links); err != nil {
 		return cli.errorOut(err)
@@ -310,14 +289,25 @@ func (cli *Client) deserializeAPIResponse(resp *http.Response, dst interface{}, 
 }
 
 func (cli *Client) deserializeResponse(resp *http.Response, dst interface{}) error {
-	b, err := parseResponse(resp)
+	b, err := cli.parseResponse(resp)
 	if err != nil {
-		return cli.errorOut(err)
+		return err
 	}
 	if err = json.Unmarshal(b, &dst); err != nil {
 		return cli.errorOut(err)
 	}
 	return nil
+}
+
+func (cli *Client) parseResponse(resp *http.Response) ([]byte, error) {
+	b, err := parseResponse(resp)
+	if err == errUnauthorized {
+		return nil, cli.errorOut(multierr.Append(err, fmt.Errorf("Retry after deleting your cookie at %s/cookie", cli.Config.RootDir)))
+	}
+	if err != nil {
+		return nil, cli.errorOut(err)
+	}
+	return b, err
 }
 
 func parseResponse(resp *http.Response) ([]byte, error) {
@@ -326,7 +316,7 @@ func parseResponse(resp *http.Response) ([]byte, error) {
 		return b, multierr.Append(errors.New(resp.Status), err)
 	}
 	if resp.StatusCode == 401 {
-		return b, fmt.Errorf("%s: %s", resp.Status, "Try deleting your cookie and trying again")
+		return b, errUnauthorized
 	} else if resp.StatusCode >= 400 {
 		return b, errors.New(resp.Status)
 	}
