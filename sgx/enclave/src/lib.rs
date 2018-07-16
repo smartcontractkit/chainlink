bbecfedfad95a3c06ab41e5f2c073d728ed97ee4
Steve Ellis <email@steveell.is>
2018-07-16 19:24:56 -0400

    add multiply adapter into rust
    Signed-off-by: John Barker <jebarker@gmail.com>

--- sgx/enclave/src/lib.rs
+++ sgx/enclave/src/lib.rs
@@ -1,17 +1,21 @@
 #![crate_name = "enclave"]
 #![crate_type = "staticlib"]
+
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 #![feature(alloc)]
 
 extern crate base64;
 extern crate num;
-extern crate serde;
-extern crate serde_json;
 extern crate sgx_types;
 #[cfg(not(target_env = "sgx"))]
 #[macro_use] extern crate sgx_tstd as std;
 extern crate wasmi;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 #[macro_use] mod util;
 mod wasm;
@@ -108,38 +112,62 @@ fn wasm(
     Ok(())
 }
 
-pub extern "C" fn sgx_multiply(multiplicand_ptr: *const u8, multiplicand_len: usize, multiplier_ptr: *const u8, multiplier_len: usize) -> sgx_status_t {
-    let multiplicand = string_from_cstr_with_len(multiplicand_ptr, multiplicand_len).unwrap();
-    let multiplier = string_from_cstr_with_len(multiplier_ptr, multiplier_len).unwrap();
+pub extern "C" fn sgx_multiply(adapter_str_ptr: *const u8, adapter_str_len: usize, input_str_ptr: *const u8, input_str_len: usize) -> sgx_status_t {
+    let adapter_str = string_from_cstr_with_len(adapter_str_ptr, adapter_str_len).unwrap();
+    let adapter: serde_json::Value = match serde_json::from_str(&adapter_str) {
+        Ok(result) => result,
+        Err(_err) => return sgx_status_t::SGX_ERROR_INVALID_PARAMETER,
+    };
+
+    let input_str = string_from_cstr_with_len(input_str_ptr, input_str_len).unwrap();
+    let input = match parse_run_result_json(&input_str) {
+        Ok(result) => result,
+        Err(_err) => return sgx_status_t::SGX_ERROR_INVALID_PARAMETER,
+    };
+
+    let multiplier = match &adapter["times"] {
+        serde_json::Value::String(v) => v.clone(),
+        serde_json::Value::Number(v) => format!("{}", v),
+        _ => return sgx_status_t::SGX_ERROR_INVALID_PARAMETER,
+    };
+
+
+    let multiplicand = match &input.data["value"] {
+        serde_json::Value::String(v) => v.clone(),
+        serde_json::Value::Number(v) => format!("{}", v),
+        _ => return sgx_status_t::SGX_ERROR_INVALID_PARAMETER,
+    };
 
     println!("Performing MULTIPLY from within enclave with {:?} * {:?} = {:?}",
              multiplicand, multiplier, multiply(&multiplicand, &multiplier));
+
     sgx_status_t::SGX_SUCCESS
 }
 
 fn multiply(multiplicand_str: &str, multiplier_str: &str) -> Result<String, std::num::ParseIntError> {
     let multiplicand = match i128::from_str_radix(multiplicand_str, 10) {
-        Ok(a) => a,
-        Err(err) => return Err(err)
+        Ok(result) => result,
+        Err(err) => return Err(err),
     };
     let multiplier = match i128::from_str_radix(multiplier_str, 10) {
-        Ok(a) => a,
-        Err(err) => return Err(err)
+        Ok(result) => result,
+        Err(err) => return Err(err),
     };
 
     Ok(format!("{:?}", multiplicand * multiplier))
 }
 
-#[cfg(test)]
-mod test {
-    use std::ffi::CString;
-    use *;
+fn parse_run_result_json(input: &str) -> Result<RunResult, serde_json::Error> {
+    let result: RunResult = serde_json::from_str(input)?;
+    Ok(result)
+}
 
-    #[test]
-    fn test_multiply() {
-      assert_eq!(multiply("1", "1"), Ok("1".to_string()));
-      assert_eq!(multiply("13", "19"), Ok("247".to_string()));
-
-      assert!(multiply("", "19").is_err());
-    }
+#[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+struct RunResult {
+    job_run_id: String,
+    data: serde_json::Value,
+    status: Option<String>,
+    error_message: Option<String>,
+    amount: Option<u64>,
 }
