dc99649a2554452c85795ecc9287fd01aa401e6e
John Barker <jebarker@gmail.com>
2018-07-17 19:01:29 -0400

    Add capture of output from WASM execution

--- sgx/enclave/src/lib.rs
+++ sgx/enclave/src/lib.rs
@@ -1,65 +1,124 @@
 #![crate_name = "enclave"]
 #![crate_type = "staticlib"]
-
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
-#[macro_use]
-extern crate lazy_static;
+extern crate base64;
 extern crate num;
 extern crate serde;
 extern crate serde_json;
 extern crate sgx_types;
+#[cfg(not(target_env = "sgx"))]
 #[macro_use]
 extern crate sgx_tstd as std;
 extern crate sgxwasm;
 extern crate wasmi;
 
+mod util;
 mod wasm;
 
 use sgx_types::*;
-use std::string::String;
-use std::slice;
+use util::{copy_string_to_cstr_ptr, string_from_cstr_with_len};
 
 #[no_mangle]
 pub extern "C" fn sgx_http_get(url_ptr: *const u8, url_len: usize) -> sgx_status_t {
-    let url_slice = unsafe { slice::from_raw_parts(url_ptr, url_len) };
-    let url = String::from_utf8(url_slice.to_vec()).unwrap();
+    let url = string_from_cstr_with_len(url_ptr, url_len).unwrap();
 
     println!("Performing HTTP GET from within enclave with {:?}", url);
     sgx_status_t::SGX_SUCCESS
 }
 
 #[no_mangle]
-pub extern "C" fn sgx_http_post(url_ptr: *const u8, url_len: usize, body_ptr: *const u8, body_len: usize) -> sgx_status_t {
-    let url_slice = unsafe { slice::from_raw_parts(url_ptr, url_len) };
-    let url = String::from_utf8(url_slice.to_vec()).unwrap();
+pub extern "C" fn sgx_http_post(
+    url_ptr: *const u8,
+    url_len: usize,
+    body_ptr: *const u8,
+    body_len: usize,
+) -> sgx_status_t {
+    let url = string_from_cstr_with_len(url_ptr, url_len).unwrap();
+    let body = string_from_cstr_with_len(body_ptr, body_len).unwrap();
 
-    let body_slice = unsafe { slice::from_raw_parts(body_ptr, body_len) };
-    let body = String::from_utf8(body_slice.to_vec()).unwrap();
-
-    println!("Performing HTTP POST from within enclave with {:?}: {:?}", url, body);
+    println!(
+        "Performing HTTP POST from within enclave with {:?}: {:?}",
+        url, body
+    );
     sgx_status_t::SGX_SUCCESS
 }
 
 #[no_mangle]
-pub extern "C" fn sgx_wasm(wasmt_ptr: *const u8, wasmt_len: usize) -> sgx_status_t {
-    let wasmt_slice = unsafe { slice::from_raw_parts(wasmt_ptr, wasmt_len) };
-    let wasmt = String::from_utf8(wasmt_slice.to_vec()).unwrap();
+pub extern "C" fn sgx_wasm(
+    wasmt_ptr: *const u8,
+    wasmt_len: usize,
+    arguments_ptr: *const u8,
+    arguments_len: usize,
+    result_ptr: *mut u8,
+    result_capacity: usize,
+    result_len: *mut usize,
+) -> sgx_status_t {
+    match wasm(
+        wasmt_ptr,
+        wasmt_len,
+        arguments_ptr,
+        arguments_len,
+        result_ptr,
+        result_capacity,
+        result_len,
+    ) {
+        Ok(_) => sgx_status_t::SGX_SUCCESS,
+        _ => sgx_status_t::SGX_ERROR_UNEXPECTED,
+    }
+}
 
-    let data = &[
-        0, 97, 115, 109, // \0ASM - magic
-        1, 0, 0, 0       //  0x01 - version
-    ];
+enum WasmError {
+    FromUtf8Error(std::string::FromUtf8Error),
+    WasmError(wasm::Error),
+    OutputCStrError(util::OutputCStrError),
+}
 
-    match wasmi::Module::from_buffer(data) {
-        Ok(r) => {
-            //println!("module: {:?}", r);
-            return sgx_status_t::SGX_SUCCESS
-        },
-        Err(err) => {
-            println!("Error executing wasm: {:?}", err);
-            return sgx_status_t::SGX_ERROR_WASM_INTERPRETER_ERROR;
-        },
-    };
+impl From<std::string::FromUtf8Error> for WasmError {
+    fn from(e: std::string::FromUtf8Error) -> Self {
+        WasmError::FromUtf8Error(e)
+    }
+}
+
+impl From<wasm::Error> for WasmError {
+    fn from(e: wasm::Error) -> Self {
+        WasmError::WasmError(e)
+    }
+}
+
+impl From<util::OutputCStrError> for WasmError {
+    fn from(e: util::OutputCStrError) -> Self {
+        WasmError::OutputCStrError(e)
+    }
+}
+
+fn wasm(
+    wasmt_ptr: *const u8,
+    wasmt_len: usize,
+    arguments_ptr: *const u8,
+    arguments_len: usize,
+    result_ptr: *mut u8,
+    result_capacity: usize,
+    result_len: *mut usize,
+) -> Result<(), WasmError> {
+
+    let wasmt = string_from_cstr_with_len(wasmt_ptr, wasmt_len)?;
+    println!("wasmt: {:?}", wasmt);
+
+    let arguments = string_from_cstr_with_len(arguments_ptr, arguments_len)?;
+    println!("arguments: {:?}", arguments);
+
+    let output = wasm::exec(&wasmt, &arguments)?;
+    println!("output: {:?}", output);
+
+    // FIXME: This prints the value as I32(1) which is not super useful right now
+    copy_string_to_cstr_ptr(
+        &format!("{:?}", output),
+        result_ptr,
+        result_capacity,
+        result_len,
+    )?;
+
+    Ok(())
 }
--- sgx/enclave/src/wasm.rs
+++ sgx/enclave/src/wasm.rs
@@ -1,222 +1,77 @@
-// Copyright (C) 2017-2018 Baidu, Inc. All Rights Reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-//  * Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in
-//    the documentation and/or other materials provided with the
-//    distribution.
-//  * Neither the name of Baidu, Inc., nor the names of its
-//    contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+use base64;
+use num::traits;
+use num::Num;
+use sgxwasm;
+use std::num;
+use wasmi::{self, ImportsBuilder, ModuleInstance, NopExternals};
 
-use std::prelude::v1::*;
-use std::sync::SgxMutex;
-use std::ptr;
-
-use serde_json;
-
-use sgxwasm::{self, SpecDriver, boundary_value_to_runtime_value, result_covert};
-
-use sgx_types::*;
-use std::slice;
-
-use wasmi::{self, ModuleInstance, ImportsBuilder, RuntimeValue, Error as InterpreterError, Module};
-
-lazy_static!{
-    static ref SPECDRIVER: SgxMutex<SpecDriver> = SgxMutex::new(SpecDriver::new());
+#[derive(Debug)]
+pub enum Error {
+    Base64DecoderError(base64::DecodeError),
+    EmptyResultError,
+    ParseIntError(num::ParseIntError),
+    ParseFloatError(traits::ParseFloatError),
+    WasmError(sgxwasm::Error),
+    WasmTrap(wasmi::Trap),
 }
 
-#[no_mangle]
-pub extern "C"
-fn sgxwasm_init() -> sgx_status_t {
-    let mut sd = SPECDRIVER.lock().unwrap();
-    *sd = SpecDriver::new();
-    sgx_status_t::SGX_SUCCESS
-}
-
-fn wasm_invoke(module : Option<String>, field : String, args : Vec<RuntimeValue>)
-              -> Result<Option<RuntimeValue>, InterpreterError> {
-    let mut program = SPECDRIVER.lock().unwrap();
-    let module = program.module_or_last(module.as_ref().map(|x| x.as_ref()))
-                        .expect(&format!("Expected program to have loaded module {:?}", module));
-    module.invoke_export(&field, &args, program.spec_module())
-}
-
-fn wasm_get(module : Option<String>, field : String)
-            -> Result<Option<RuntimeValue>, InterpreterError> {
-    let program = SPECDRIVER.lock().unwrap();
-    let module = match module {
-        None => {
-                 program
-                 .module_or_last(None)
-                 .expect(&format!("Expected program to have loaded module {:?}",
-                        "None"
-                 ))
-        },
-        Some(str) => {
-                 program
-                 .module_or_last(Some(&str))
-                 .expect(&format!("Expected program to have loaded module {:?}",
-                         str
-                 ))
-        }
-    };
-
-    let global = module.export_by_name(&field)
-                       .ok_or_else(|| {
-                           InterpreterError::Global(format!("Expected to have export with name {}", field))
-                       })?
-                       .as_global()
-                       .cloned()
-                       .ok_or_else(|| {
-                           InterpreterError::Global(format!("Expected export {} to be a global", field))
-                       })?;
-     Ok(Some(global.get()))
-}
-
-fn try_load_module(wasm: &[u8]) -> Result<Module, InterpreterError> {
-    wasmi::Module::from_buffer(wasm).map_err(|e| InterpreterError::Instantiation(format!("Module::from_buffer error {:?}", e)))
-}
-
-fn wasm_try_load(wasm: Vec<u8>) -> Result<(), InterpreterError> {
-    let ref mut spec_driver = SPECDRIVER.lock().unwrap();
-    let module = try_load_module(&wasm[..])?;
-    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())?;
-    instance
-        .run_start(spec_driver.spec_module())
-        .map_err(|trap| InterpreterError::Instantiation(format!("ModuleInstance::run_start error on {:?}", trap)))?;
-    Ok(())
-}
-
-fn wasm_load_module(name: Option<String>, module: Vec<u8>)
-                    -> Result<(), InterpreterError> {
-    let ref mut spec_driver = SPECDRIVER.lock().unwrap();
-    let module = try_load_module(&module[..])?;
-    let instance = ModuleInstance::new(&module, &**spec_driver)
-        .map_err(|e| InterpreterError::Instantiation(format!("ModuleInstance::new error on {:?}", e)))?
-        .run_start(spec_driver.spec_module())
-        .map_err(|trap| InterpreterError::Instantiation(format!("ModuleInstance::run_start error on {:?}", trap)))?;
-
-    spec_driver.add_module(name, instance.clone());
-
-    Ok(())
-}
-
-fn wasm_register(name: &Option<String>, as_name: String)
-                    -> Result<(), InterpreterError> {
-    let ref mut spec_driver = SPECDRIVER.lock().unwrap();
-    spec_driver.register(name, as_name)
-}
-
-#[no_mangle]
-pub extern "C"
-fn sgxwasm_run_action(req_bin : *const u8, req_length: usize,
-                      result_bin : *mut u8, result_max_len: usize) -> sgx_status_t {
-
-    let req_slice = unsafe { slice::from_raw_parts(req_bin, req_length) };
-    let action_req: sgxwasm::SgxWasmAction = serde_json::from_slice(req_slice).unwrap();
-
-    let response;
-    let return_status;
-
-    match action_req {
-        sgxwasm::SgxWasmAction::Invoke{module,field,args}=> {
-            let args = args.into_iter()
-                           .map(|x| boundary_value_to_runtime_value(x))
-                           .collect::<Vec<RuntimeValue>>();
-            let r = wasm_invoke(module, field, args);
-            let r = result_covert(r);
-            response = serde_json::to_string(&r).unwrap();
-            match r {
-                Ok(_) => {
-                    return_status = sgx_status_t::SGX_SUCCESS;
-                },
-                Err(_) => {
-                    return_status = sgx_status_t::SGX_ERROR_WASM_INTERPRETER_ERROR;
-               }
-            }
-        },
-        sgxwasm::SgxWasmAction::Get{module,field} => {
-            let r = wasm_get(module, field);
-            let r = result_covert(r);
-            response = serde_json::to_string(&r).unwrap();
-            match r {
-                Ok(_v) => {
-                    return_status = sgx_status_t::SGX_SUCCESS;
-                },
-                Err(_x) => {
-                    return_status = sgx_status_t::SGX_ERROR_WASM_INTERPRETER_ERROR;
-                }
-            }
-        },
-        sgxwasm::SgxWasmAction::LoadModule{name,module} => {
-            let r = wasm_load_module(name.clone(), module);
-            response = serde_json::to_string(&r).unwrap();
-            match r {
-                Ok(_) => {
-                    return_status = sgx_status_t::SGX_SUCCESS;
-                },
-                Err(_x) => {
-                    return_status = sgx_status_t::SGX_ERROR_WASM_LOAD_MODULE_ERROR;
-                }
-            }
-        },
-        sgxwasm::SgxWasmAction::TryLoad{module} => {
-            let r = wasm_try_load(module);
-            response = serde_json::to_string(&r).unwrap();
-            match r {
-                Ok(()) => {
-                    return_status = sgx_status_t::SGX_SUCCESS;
-                },
-                Err(_x) => {
-                    return_status = sgx_status_t::SGX_ERROR_WASM_TRY_LOAD_ERROR;
-                }
-            }
-        },
-        sgxwasm::SgxWasmAction::Register{name, as_name} => {
-            let r = wasm_register(&name, as_name.clone());
-            response = serde_json::to_string(&r).unwrap();
-            match r {
-                Ok(()) => {
-                    return_status = sgx_status_t::SGX_SUCCESS;
-                },
-                Err(_x) => {
-                    return_status = sgx_status_t::SGX_ERROR_WASM_REGISTER_ERROR;
-                }
-            }
-        }
-    }
-
-    //println!("len = {}, Response = {:?}", response.len(), response);
-
-    if response.len() < result_max_len {
-        unsafe {
-            ptr::copy_nonoverlapping(response.as_ptr(),
-                                     result_bin,
-                                     response.len());
-        }
-        return return_status;
-    }
-    else{
-        //println!("Result len = {} > buf size = {}", response.len(), result_max_len);
-        return sgx_status_t::SGX_ERROR_WASM_BUFFER_TOO_SHORT;
+impl From<base64::DecodeError> for Error {
+    fn from(e: base64::DecodeError) -> Self {
+        Error::Base64DecoderError(e)
     }
 }
 
+impl From<sgxwasm::Error> for Error {
+    fn from(e: sgxwasm::Error) -> Self {
+        Error::WasmError(e)
+    }
+}
+
+impl From<wasmi::Trap> for Error {
+    fn from(e: wasmi::Trap) -> Self {
+        Error::WasmTrap(e)
+    }
+}
+
+impl From<num::ParseIntError> for Error {
+    fn from(e: num::ParseIntError) -> Self {
+        Error::ParseIntError(e)
+    }
+}
+
+impl From<traits::ParseFloatError> for Error {
+    fn from(e: traits::ParseFloatError) -> Self {
+        Error::ParseFloatError(e)
+    }
+}
+
+pub fn exec(encoded_program: &str, arguments: &str) -> Result<wasmi::RuntimeValue, Error> {
+    println!("exec: {:?}", encoded_program);
+    let data = base64::decode(encoded_program)?;
+    println!("data: {:?}", data);
+    // FIXME: Compiler can't find the From trait for the below?
+    let module = wasmi::Module::from_buffer(data) //?;
+        .expect("module error");
+    println!("module loaded");
+    let module_ref = ModuleInstance::new(&module, &ImportsBuilder::default()) //?;
+        .expect("module_ref error");
+    println!("module_ref loaded");
+    let instance = module_ref.run_start(&mut NopExternals) //?;
+        .expect("run_start error");
+
+    println!("instance loaded");
+    // FIXME: assumes a single value argument that is a float
+    let value = <f64 as Num>::from_str_radix(arguments, 10)?;
+
+    println!("value loaded {:?}", value);
+    let arguments = [wasmi::RuntimeValue::I64(value as i64)];
+
+    println!("loading permission: {:?}", instance);
+    match instance
+        .invoke_export("perform", &arguments, &mut NopExternals)
+        .expect("invoke_export")
+    {
+        Some(v) => Ok(v),
+        _ => Err(Error::EmptyResultError),
+    }
+}
