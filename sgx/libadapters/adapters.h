f5340f3fba32e2139817c834882589286b5abdc6
Steve Ellis <email@steveell.is>
2018-07-17 15:08:49 -0400

    return data from SGX
    Signed-off-by: John Barker <jebarker@gmail.com>

--- sgx/libadapters/adapters.h
+++ sgx/libadapters/adapters.h
@@ -1,5 +1,5 @@
 void init_enclave();
 char * http_get(char *url);
 char * http_post(char *url, char *body);
-char * multiply(char *adapter, char *input);
+char * multiply(char *adapter, char *input, char *result);
 void wasm(char *wasm, char *arguments, char *result, int result_capacity, int *result_len);
