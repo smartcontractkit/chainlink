aedb7a26946824dc6f2f2af5155675d1235cf578
NavyAdmiral <keccaksha3@gmail.com>
2018-07-18 22:44:14 +0400

    linted and edited

--- gui/src/components/Footer.js
+++ gui/src/components/Footer.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import { withRouteData } from "react-static";
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import { withRouteData } from 'react-static'
 import Card from '@material-ui/core/Card'
 
 const styles = theme => ({
   style: {
     textAlign: "center",
-    padding: "20px",
+    padding: theme.spacing.unit * 2.5,
     position: "fixed",
     left: "0",
     bottom: "0",
-    height: "60px",
     width: "100%"
   }
-});
+})
 
 const Footnote = ({ classes, version, sha }) => {
   return (
@@ -23,7 +22,7 @@ const Footnote = ({ classes, version, sha }) => {
           Chainlink Node {version} at commit {sha}
         </Typography>
       </Card>
-  );
-};
+  )
+}
 
-export default withRouteData(withStyles(styles)(Footnote));
+export default withRouteData(withStyles(styles)(Footnote))
