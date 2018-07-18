dd963f27a6af7068c02e6a9a443fe815519741d9
NavyAdmiral <keccaksha3@gmail.com>
2018-07-18 23:39:28 +0400

    yarn lint

--- gui/src/components/BridgeForm.js
+++ gui/src/components/BridgeForm.js
@@ -1,107 +1,107 @@
-import axios from "axios";
-import React, { Fragment } from "react";
-import { withFormik, Form } from "formik";
-import * as Yup from "yup";
-import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import { TextField, Typography } from "@material-ui/core";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios'
+import React, { Fragment } from 'react'
+import { withFormik, Form } from 'formik'
+import * as Yup from 'yup'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import { TextField, Typography } from '@material-ui/core'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const styles = theme => ({
   textfield: {
     paddingTop: theme.spacing.unit * 1.25,
-    width: "270px"
+    width: '270px'
   },
   card: {
     paddingBottom: theme.spacing.unit * 2
   },
   form: {
-    position: "relative", 
-    textAlign: "center"
+    position: 'relative',
+    textAlign: 'center'
   }
-});
+})
 
 const App = ({ errors, touched, isSubmitting, classes, handleChange }) => (
   <Fragment>
     <br />
     <Form className={classes.form} noValidate>
       <div>
-        {touched.name && errors.name && <Typography color="error">{errors.name}</Typography>}
+        {touched.name && errors.name && <Typography color='error'>{errors.name}</Typography>}
         <TextField
           onChange={handleChange}
           className={classes.textfield}
-          label="Type Bridge Name"
-          type="name"
-          name="name"
-          placeholder="name"
+          label='Type Bridge Name'
+          type='name'
+          name='name'
+          placeholder='name'
         />
       </div>
       <div>
-        {touched.url && errors.url && <Typography color="error">{errors.url}</Typography>}
+        {touched.url && errors.url && <Typography color='error'>{errors.url}</Typography>}
         <TextField
           onChange={handleChange}
           className={classes.textfield}
-          label="Type Bridge URL"
-          type="url"
-          name="url"
-          placeholder="url"
+          label='Type Bridge URL'
+          type='url'
+          name='url'
+          placeholder='url'
         />
       </div>
       <div>
-        {touched.confirmations && errors.confirmations && <Typography color="error">{errors.confirmations}</Typography>}
+        {touched.confirmations && errors.confirmations && <Typography color='error'>{errors.confirmations}</Typography>}
         <TextField
           onChange={handleChange}
           className={classes.textfield}
-          label="Type Confirmations"
-          type="confirmations"
-          name="confirmations"
-          placeholder="confirmations"
+          label='Type Confirmations'
+          type='confirmations'
+          name='confirmations'
+          placeholder='confirmations'
         />
       </div>
-      <Button color="primary" type="submit" disabled={isSubmitting}>
+      <Button color='primary' type='submit' disabled={isSubmitting}>
         Build Bridge
       </Button>
       <ToastContainer />
     </Form>
   </Fragment>
-);
+)
 
 const BridgeForm = withFormik({
-  mapPropsToValues({ name, url, confirmations }) {
+  mapPropsToValues ({ name, url, confirmations }) {
     return {
-      name: name || "",
-      url: url || "",
-      confirmations: confirmations || ""
-    };
+      name: name || '',
+      url: url || '',
+      confirmations: confirmations || ''
+    }
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required("Name is required"),
+    name: Yup.string().required('Name is required'),
     url: Yup.string()
-      .required("URL is required"),
+      .required('URL is required'),
     confirmations: Yup.number()
-      .positive("Should be a positive number")
-      .typeError("Should be a number")
+      .positive('Should be a positive number')
+      .typeError('Should be a number')
   }),
-  handleSubmit(values) {
-    const formattedValues = JSON.parse(JSON.stringify(values).replace("confirmations", "defaultConfirmations"));
-    formattedValues.defaultConfirmations = parseInt(formattedValues.defaultConfirmations) || 0;
+  handleSubmit (values) {
+    const formattedValues = JSON.parse(JSON.stringify(values).replace('confirmations', 'defaultConfirmations'))
+    formattedValues.defaultConfirmations = parseInt(formattedValues.defaultConfirmations) || 0
     axios
-      .post("/v2/bridge_types", formattedValues, {
+      .post('/v2/bridge_types', formattedValues, {
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         auth: {
-          username: "chainlink",
-          password: "twochains"
+          username: 'chainlink',
+          password: 'twochains'
         }
       })
       .then(res =>
         toast.success(`Bridge ${res.data.name} created`, {
           position: toast.POSITION.BOTTOM_RIGHT
         })
-      );
+      )
   }
-})(App);
+})(App)
 
-export default withStyles(styles)(BridgeForm);
+export default withStyles(styles)(BridgeForm)
--- gui/src/containers/Create.js
+++ gui/src/containers/Create.js
@@ -1,69 +1,69 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import BridgeForm from "components/BridgeForm"
-import JobForm from "components/JobForm"
-import { Card } from '@material-ui/core';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import BridgeForm from 'components/BridgeForm'
+import JobForm from 'components/JobForm'
+import { Card } from '@material-ui/core'
 
 const TabContainer = props => {
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
+    <Typography component='div' style={{ padding: 8 * 3 }}>
       {props.children}
     </Typography>
-  );
+  )
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  children: PropTypes.node.isRequired
+}
 
 const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
-    },
-    card: {
-        paddingBottom: theme.spacing.unit * 2,
-    }
-});
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  },
+  card: {
+    paddingBottom: theme.spacing.unit * 2
+  }
+})
 
 class WrappedTabs extends React.Component {
   state = {
-    value: 0,
+    value: 0
   };
 
   handleChange = (event, value) => {
-    this.setState({ value });
+    this.setState({ value })
   };
 
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
+  render () {
+    const { classes } = this.props
+    const { value } = this.state
 
     return (
-        <div className={classes.root}>
-        <br/>
+      <div className={classes.root}>
+        <br />
         <Card className={classes.card}>
-        <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}>
-            <Tab label="Create Bridge" />
-            <Tab label="Create Job" />
-          </Tabs>
-        </AppBar>
-        {value === 0 && <TabContainer><BridgeForm/></TabContainer>}
-        {value === 1 && <TabContainer><JobForm/></TabContainer>}
+          <AppBar position='static'>
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab label='Create Bridge' />
+              <Tab label='Create Job' />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <TabContainer><BridgeForm /></TabContainer>}
+          {value === 1 && <TabContainer><JobForm /></TabContainer>}
         </Card>
       </div>
-    );
+    )
   }
 }
 
 WrappedTabs.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(WrappedTabs);
+export default withStyles(styles)(WrappedTabs)
