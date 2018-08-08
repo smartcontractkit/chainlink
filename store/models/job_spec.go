38457a73d304a6895b0fc6bba26435b04e769e0e
Steve Ellis <email@steveell.is>
2018-08-08 10:34:32 -0700

    move service agreements to their own file

--- store/models/job_spec.go
+++ store/models/job_spec.go
@@ -3,7 +3,6 @@ package models
 import (
 	"encoding/json"
 	"fmt"
-	"math/big"
 	"regexp"
 	"strings"
 	"time"
@@ -339,78 +338,4 @@ func NormalizeSpecJSON(s string) (string, error) {
 	}
 
 	return utils.NormalizedJSONString(js)
-}
-
-// ServiceAgreement connects job specifications with on-chain encumbrances.
-type ServiceAgreement struct {
-	Encumbrance Encumbrance `json:"encumbrance" storm:"inline"`
-	ID          string      `json:"id" storm:"id,unique"`
-	JobSpecID   string      `json:"jobSpecID"`
-	jobSpec     JobSpec
-}
-
-// NewServiceAgreementFromRequest builds a new ServiceAgreement.
-func NewServiceAgreementFromRequest(sar ServiceAgreementRequest) (ServiceAgreement, error) {
-	sa := ServiceAgreement{}
-
-	sa.Encumbrance = sar.Encumbrance
-	sa.jobSpec = sar.JobSpec
-
-	b, err := utils.HexToBytes(sa.Encumbrance.ABI(), sar.Digest)
-	if err != nil {
-		return sa, err
-	}
-	digest, err := utils.Keccak256(b)
-	if err != nil {
-		return sa, err
-	}
-	sa.ID = common.ToHex(digest)
-
-	return sa, nil
-}
-
-// ServiceAgreementRequest represents a service agreement as requested over the wire.
-type ServiceAgreementRequest struct {
-	JobSpec     JobSpec
-	Encumbrance Encumbrance
-	Digest      string
-}
-
-// UnmarshalJSON fulfills Go's built in JSON unmarshaling interface.
-func (sar *ServiceAgreementRequest) UnmarshalJSON(input []byte) error {
-	var jsr JobSpecRequest
-	if err := json.Unmarshal(input, &jsr); err != nil {
-		return err
-	}
-	js, err := NewJobFromRequest(jsr)
-	if err != nil {
-		return err
-	}
-	var en Encumbrance
-	if err := json.Unmarshal(input, &en); err != nil {
-		return err
-	}
-
-	sar.JobSpec = js
-	sar.Digest = js.Digest
-	sar.Encumbrance = en
-
-	return nil
-}
-
-// Encumbrance connects job specifications with on-chain encumbrances.
-type Encumbrance struct {
-	Payment    *big.Int `json:"payment"`
-	Expiration *big.Int `json:"expiration"`
-}
-
-// ABI returns the encumbrance ABI encoded as a hex string.
-func (e Encumbrance) ABI() string {
-	if e.Payment == nil {
-		e.Payment = big.NewInt(0)
-	}
-	if e.Expiration == nil {
-		e.Expiration = big.NewInt(0)
-	}
-	return fmt.Sprintf("%064x%064x", e.Payment, e.Expiration)
 }
