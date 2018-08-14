ae07f1b9e978ddeb601213c3efcfa77332cdad84
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-08-13 17:24:56 -0700

    Mark Coordinator#getId as pure
    Signed-off-by: John Barker <jebarker@gmail.com>

--- solidity/contracts/Coordinator.sol
+++ solidity/contracts/Coordinator.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.24;
 contract Coordinator {
 
   function getId(uint256 _payment, uint256 _expiration, bytes32 _jobSpecId)
-    public returns (bytes32)
+    public pure returns (bytes32)
   {
     return keccak256(_payment, _expiration, _jobSpecId);
   }
