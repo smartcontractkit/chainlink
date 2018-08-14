7ce6c4561346a5dbef347856cfff2a0b085dd286
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-08-13 17:00:48 -0700

    Add initiateServiceAgreement and getServiceAgreement to Coordinator
    Add ability to save and retrieve a service agreement from the blockchain
    by ID.
    
    [Finishes #158585432]
    
    Signed-off-by: John Barker <jebarker@gmail.com>

--- solidity/contracts/Coordinator.sol
+++ solidity/contracts/Coordinator.sol
@@ -3,10 +3,36 @@ pragma solidity ^0.4.24;
 // Coordinator handles oracle service aggreements between one or more oracles.
 contract Coordinator {
 
+  struct ServiceAgreement {
+    uint256 payment;
+    uint256 expiration;
+    bytes32 jobSpecID;
+  }
+
+  mapping(bytes32 => ServiceAgreement) internal serviceAgreements;
+
   function getId(uint256 _payment, uint256 _expiration, bytes32 _jobSpecId)
     public pure returns (bytes32)
   {
     return keccak256(abi.encodePacked(_payment, _expiration, _jobSpecId));
   }
 
+  function initiateServiceAgreement(uint256 _payment, uint256 _expiration, bytes32 _jobSpecId)
+    public
+  {
+    bytes32 id = getId(_payment, _expiration, _jobSpecId);
+
+    serviceAgreements[id] = ServiceAgreement(
+      _payment,
+      _expiration,
+      _jobSpecId
+    );
+  }
+
+  function getServiceAgreement(bytes32 _id)
+    public returns (bytes memory)
+  {
+    ServiceAgreement sa = serviceAgreements[_id];
+    return abi.encode(sa.payment, sa.expiration, sa.jobSpecID);
+  }
 }
