6af7c49f94b9b8d059086caacfae87e068ef1e6a
NavyAdmiral <keccaksha3@gmail.com>
2018-08-21 21:32:29 +0400

    fix failing tests

--- gui/src/components/JobList.js
+++ gui/src/components/JobList.js
@@ -59,12 +59,14 @@ export class JobList extends Component {
   }
 
   componentDidUpdate (prevProps) {
-    const prevJobPage = prevProps.match.params.jobPage
-    const currentJobPage = this.props.match.params.jobPage
-
-    if (prevJobPage !== currentJobPage) {
-      const { pageSize, fetchJobs } = this.props
-      fetchJobs(currentJobPage, pageSize)
+    if(prevProps.match && this.props.match){
+      const prevJobPage = prevProps.match.params.jobPage
+      const currentJobPage = this.props.match.params.jobPage
+  
+      if (prevJobPage !== currentJobPage) {
+        const { pageSize, fetchJobs } = this.props
+        fetchJobs(currentJobPage, pageSize)
+      }
     }
   }
 
