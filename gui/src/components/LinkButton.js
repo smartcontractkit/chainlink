dc901cbce3b9268c739dbb6597c0a4925f945d14
Dimitri Roche <dimroc@gmail.com>
2018-08-28 08:41:28 -0400

    Introduce LinkButton: looks like a link, functions like a button

---
+++ gui/src/components/LinkButton.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import StyledButton from '@material-ui/core/Button'
+
+export default (props) => (
+  <StyledButton {...props} style={{ backgroundColor: 'transparent' }} />
+)
