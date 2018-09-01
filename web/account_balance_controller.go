fcaf0554cdc716f375f791a398522e0b6f052abc
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-08-31 21:43:54 -0500

    add withdraw method and tests for account balance controller, add cli call to withdraw

--- web/account_balance_controller.go
+++ web/account_balance_controller.go
@@ -1,12 +1,16 @@
 package web
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/manyminds/api2go/jsonapi"
 	"github.com/smartcontractkit/chainlink/services"
+	"github.com/smartcontractkit/chainlink/store/assets"
+	"github.com/smartcontractkit/chainlink/store/models"
 	"github.com/smartcontractkit/chainlink/store/presenters"
+	"github.com/smartcontractkit/chainlink/utils"
 )
 
 // AccountBalanceController returns information for the active account
@@ -41,8 +45,32 @@ func (abc *AccountBalanceController) Show(c *gin.Context) {
 	}
 }
 
-// Withdraw will withdraw some LINK to an authorized address
-func (abc *AccountBalanceController) Withdraw(c *gin.Context) {
+var naz = assets.NewLink(1)
 
-	c.JSON(200, gin.H{"txhash": "TXHASH"})
+// Withdraw sends LINK from the configured oracle contract to the given address
+func (abc *AccountBalanceController) Withdraw(c *gin.Context) {
+	store := abc.App.Store
+	txm := store.TxManager
+	wr := models.WithdrawalRequest{}
+	oca := *store.Config.OracleContractAddress
+
+	if err := c.ShouldBindJSON(&wr); err != nil {
+		publicError(c, 400, err)
+	} else if wr.Amount.Cmp(naz) < 0 {
+		publicError(c, 400, fmt.Errorf("Must withdraw at least %v LINK", naz.String()))
+	} else if wr.Address == utils.ZeroAddress { // address is unmarshalled to ZeroAddres if invalid
+		publicError(c, 400, errors.New("Invalid withdrawal address"))
+	} else if account, err := store.KeyStore.GetAccount(); err != nil {
+		c.AbortWithError(500, err)
+	} else if linkBalance, err := txm.GetLinkBalance(account.Address); err != nil {
+		c.AbortWithError(500, err)
+	} else if linkBalance.Cmp(wr.Amount) < 0 {
+		publicError(c, 400, fmt.Errorf("Insufficient link balance. Withdrawal Amount: %v Link Balance: %v", wr.Amount.String(), linkBalance.String()))
+	} else if tx, err := txm.CreateTx(oca, nil); err != nil {
+		c.AbortWithError(500, err)
+		// functionSelector := models.HexToFunctionSelector("0x70a08231") // balanceOf(address)
+		// data, err := utils.HexToBytes(functionSelector.String(), common.ToHex(common.LeftPadBytes(address.Bytes(), utils.EVMWordByteLen)))
+	} else {
+		c.JSON(200, tx.Hash)
+	}
 }
