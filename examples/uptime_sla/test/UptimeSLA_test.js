6316251ae5f8038096380ccb4365d1df709044bf
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-09-06 12:59:21 -0700

    Convert uptime sla helpers to ES6

--- examples/uptime_sla/test/UptimeSLA_test.js
+++ examples/uptime_sla/test/UptimeSLA_test.js
@@ -1,109 +1,122 @@
-'use strict';
-
-require('./support/helpers.js')
+import cbor from 'cbor'
+import util from 'ethereumjs-util'
+import {
+  assertActionThrows,
+  days,
+  eth,
+  fastForwardTo,
+  functionSelector,
+  getEvents,
+  getLatestEvent,
+  getLatestTimestamp,
+  newAddress,
+  oracleNode,
+  requestDataBytes,
+  requestDataFrom
+} from './support/helpers'
 
 contract('UptimeSLA', () => {
-  let Link = artifacts.require("../../../solidity/contracts/LinkToken.sol");
-  let Oracle = artifacts.require("../../../solidity/contracts/Oracle.sol");
-  let SLA = artifacts.require("UptimeSLA.sol");
-  let specId = "4c7b7ffb66b344fbaa64995af81e355a";
-  let deposit = 1000000000;
-  let link, oc, sla, client, serviceProvider, startAt;
+  let Link = artifacts.require('../../../solidity/contracts/LinkToken.sol')
+  let Oracle = artifacts.require('../../../solidity/contracts/Oracle.sol')
+  let SLA = artifacts.require('UptimeSLA.sol')
+  let specId = '4c7b7ffb66b344fbaa64995af81e355a'
+  let deposit = 1000000000
+  let link, oc, sla, client, serviceProvider, startAt
 
   beforeEach(async () => {
     client = newAddress()
-    serviceProvider = newAddress();
-    link = await Link.new();
-    oc = await Oracle.new(link.address, {from: oracleNode});
+    serviceProvider = newAddress()
+    link = await Link.new()
+    oc = await Oracle.new(link.address, {from: oracleNode})
     sla = await SLA.new(client, serviceProvider, link.address, oc.address, specId, {
       value: deposit
-    });
-    link.transfer(sla.address, web3.toWei(1, 'ether'));
-    startAt = await getLatestTimestamp();
-  });
+    })
+    link.transfer(sla.address, web3.toWei(1, 'ether'))
+    startAt = await getLatestTimestamp()
+  })
 
   describe("before updates", () => {
     it("does not release money to anyone", async () => {
-      assert.equal(await eth.getBalance(sla.address), deposit);
-      assert.equal(await eth.getBalance(client), 0);
-      assert.equal(await eth.getBalance(serviceProvider), 0);
-    });
-  });
+      assert.equal(await eth.getBalance(sla.address), deposit)
+      assert.equal(await eth.getBalance(client), 0)
+      assert.equal(await eth.getBalance(serviceProvider), 0)
+    })
+  })
 
   describe("#updateUptime", () => {
     it("triggers a log event in the Oracle contract", async () => {
-      let tx = await sla.updateUptime("0");
+      let tx = await sla.updateUptime("0")
 
-      let events = await getEvents(oc);
+      let events = await getEvents(oc)
       assert.equal(1, events.length)
 
       let event = events[0]
-      assert.equal(web3.toUtf8(event.args.specId), specId);
+      assert.equal(web3.toUtf8(event.args.specId), specId)
 
-      let decoded = cbor.decodeFirstSync(util.toBuffer(event.args.data));
+      let decoded = cbor.decodeFirstSync(util.toBuffer(event.args.data))
       assert.deepEqual(
         decoded,
-        {"url":"https://status.heroku.com/api/ui/availabilities","path":["data","0","attributes","calculation"]}
+        {'url': 'https://status.heroku.com/api/ui/availabilities', 'path': ['data', '0', 'attributes', 'calculation']}
       )
-    });
-  });
+    })
+  })
 
-  describe("#fulfillData", () => {
-    let response = "0x00000000000000000000000000000000000000000000000000000000000f8c4c";
-    let requestId;
+  describe('#fulfillData', () => {
+    let response = '0x00000000000000000000000000000000000000000000000000000000000f8c4c'
+    let requestId
 
     beforeEach(async () => {
-      await sla.updateUptime("0");
-      let event = await getLatestEvent(oc);
+      await sla.updateUptime('0')
+      let event = await getLatestEvent(oc)
       requestId = event.args.internalId
-    });
+    })
 
     context("when the value is below 9999", async () => {
-      let response = "0x000000000000000000000000000000000000000000000000000000000000270e";
+      let response = "0x000000000000000000000000000000000000000000000000000000000000270e"
 
-      it("sends the deposit to the client", async () => {
+      it('sends the deposit to the client', async () => {
         await oc.fulfillData(requestId, response, {from: oracleNode})
 
-        assert.equal(await eth.getBalance(sla.address), 0);
-        assert.equal(await eth.getBalance(client), deposit);
-        assert.equal(await eth.getBalance(serviceProvider), 0);
-      });
-    });
+        assert.equal(await eth.getBalance(sla.address), 0)
+        assert.equal(await eth.getBalance(client), deposit)
+        assert.equal(await eth.getBalance(serviceProvider), 0)
+      })
+    })
 
     context("when the value is 9999 or above", () => {
-      let response = "0x000000000000000000000000000000000000000000000000000000000000270f";
+      let response = "0x000000000000000000000000000000000000000000000000000000000000270f"
 
-      it("does not move the money", async () => {
+      it('does not move the money', async () => {
         await oc.fulfillData(requestId, response, {from: oracleNode})
 
-        assert.equal(await eth.getBalance(sla.address), deposit);
-        assert.equal(await eth.getBalance(client), 0);
-        assert.equal(await eth.getBalance(serviceProvider), 0);
-      });
+        assert.equal(await eth.getBalance(sla.address), deposit)
+        assert.equal(await eth.getBalance(client), 0)
+        assert.equal(await eth.getBalance(serviceProvider), 0)
+      })
 
-      context("and a month has passed", () => {
+      context('and a month has passed', () => {
         beforeEach(async () => {
-          await fastForwardTo(startAt + days(30));
-        });
+          await fastForwardTo(startAt + days(30))
+        })
 
-        it("gives the money back to the service provider", async () => {
+        it('gives the money back to the service provider', async () => {
           await oc.fulfillData(requestId, response, {from: oracleNode})
 
-          assert.equal(await eth.getBalance(sla.address), 0);
-          assert.equal(await eth.getBalance(client), 0);
-          assert.equal(await eth.getBalance(serviceProvider), deposit);
-        });
-      });
-    });
+          assert.equal(await eth.getBalance(sla.address), 0)
+          assert.equal(await eth.getBalance(client), 0)
+          assert.equal(await eth.getBalance(serviceProvider), deposit)
+        })
+      })
+    })
 
-    context("when the consumer does not recognize the request ID", () => {
+    context('when the consumer does not recognize the request ID', () => {
       beforeEach(async () => {
-        let fid = functionSelector("report(uint256,bytes32)");
-        let args = requestDataBytes(specId, sla.address, fid, "xid", "");
-        await requestDataFrom(oc, link, 0, args);
-        let event = await getLatestEvent(oc);
-        requestId = event.args.internalId;
-      });
+        let fid = functionSelector("report(uint256,bytes32)")
+        let args = requestDataBytes(specId, sla.address, fid, "xid", "")
+        await requestDataFrom(oc, link, 0, args)
+        let event = await getLatestEvent(oc)
+        requestId = event.args.internalId
+      })
 
       it("does not accept the data provided", async () => {
         let originalUptime = await sla.uptime()
@@ -111,15 +124,15 @@ contract('UptimeSLA', () => {
         let newUptime = await sla.uptime()
 
         assert.isTrue(originalUptime.equals(newUptime))
-      });
-    });
+      })
+    })
 
-    context("when called by anyone other than the oracle contract", () => {
-      it("does not accept the data provided", async () => {
+    context('when called by anyone other than the oracle contract', () => {
+      it('does not accept the data provided', async () => {
         await assertActionThrows(async () => {
           await sla.report(requestId, response, {from: oracleNode})
-        });
-      });
-    });
-  });
-});
+        })
+      })
+    })
+  })
+})
--- examples/uptime_sla/test/support/helpers.js
+++ examples/uptime_sla/test/support/helpers.js
@@ -1,231 +1,320 @@
-BigNumber = require('bignumber.js');
-abi = require('ethereumjs-abi');
-cbor = require("cbor");
-moment = require('moment');
-util = require('ethereumjs-util');
+const BigNumber = require('bignumber.js')
+const abi = require('ethereumjs-abi')
+const cbor = require('cbor')
+const moment = require('moment')
+const util = require('ethereumjs-util')
+
+let eth,
+  Eth,
+  accounts,
+  Accounts,
+  oracleNode,
+  stranger,
+  consumer,
+  emptyAddress,
+  sealBlock,
+  sendTransaction,
+  getBalance,
+  bigNum,
+  toWei,
+  tokens,
+  intToHex,
+  hexToInt,
+  hexToAddress,
+  unixTime,
+  seconds,
+  minutes,
+  hours,
+  days,
+  keccak256,
+  logTopic,
+  getLatestBlock,
+  getLatestTimestamp,
+  fastForwardTo,
+  getEvents,
+  eventsOfType,
+  getEventsOfType,
+  getLatestEvent,
+  assertActionThrows,
+  encodeUint256,
+  encodeAddress,
+  encodeBytes,
+  checkPublicABI,
+  functionSelector,
+  randomHex,
+  newAddress,
+  requestDataBytes,
+  requestDataFrom
 
 (() => {
-  eth = web3.eth;
+  eth = web3.eth
 
   before(async function () {
-    accounts = await eth.accounts;
-    Accounts = accounts.slice(1);
+    accounts = await eth.accounts
+    Accounts = accounts.slice(1)
 
-    oracleNode = Accounts[0];
-    stranger = Accounts[1];
-    consumer = Accounts[2];
-  });
+    oracleNode = Accounts[0]
+    stranger = Accounts[1]
+    consumer = Accounts[2]
+  })
 
-  Eth = function sendEth(method, params) {
-    params = params || [];
+  Eth = function sendEth (method, params) {
+    params = params || []
 
     return new Promise((resolve, reject) => {
       web3.currentProvider.sendAsync({
-        jsonrpc: "2.0",
+        jsonrpc: '2.0',
         method: method,
         params: params || [],
         id: new Date().getTime()
-      }, function sendEthResponse(error, response) {
+      }, function sendEthResponse (error, response) {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(response.result);
+          resolve(response.result)
         };
-      }, () => {}, () => {});
-    });
-  };
-
-  emptyAddress = '0x0000000000000000000000000000000000000000';
-
-  sealBlock = async function sealBlock() {
-    return Eth('evm_mine');
-  };
-
-  sendTransaction = async function sendTransaction(params) {
-    return await eth.sendTransaction(params);
+      }, () => {}, () => {})
+    })
   }
 
-  getBalance = async function getBalance(account) {
-    return bigNum(await eth.getBalance(account));
+  emptyAddress = '0x0000000000000000000000000000000000000000'
+
+  sealBlock = async function () {
+    return Eth('evm_mine')
   }
 
-  bigNum = function bigNum(number) {
-    return new BigNumber(number);
+  sendTransaction = async function (params) {
+    return await eth.sendTransaction(params)
   }
 
-  toWei = function toWei(number) {
-    return web3.toWei(number);
+  getBalance = async function (account) {
+    return bigNum(await eth.getBalance(account))
   }
 
-  tokens = function tokens(number) {
-    return bigNum(number * 10**18);
+  bigNum = function (number) {
+    return new BigNumber(number)
   }
 
-  intToHex = function intToHex(number) {
-    return '0x' + bigNum(number).toString(16);
+  toWei = function (number) {
+    return web3.toWei(number)
   }
 
-  hexToAddress = function hexToAddress(string) {
-    return '0x' + string.slice(string.length - 40);
+  tokens = function (number) {
+    return bigNum(number * 10 ** 18)
   }
 
-  unixTime = function unixTime(time) {
-    return moment(time).unix();
+  intToHex = function (number) {
+    return '0x' + bigNum(number).toString(16)
   }
 
-  seconds = function seconds(number) {
-    return number;
-  };
-
-  minutes = function minutes(number) {
-    return number * 60;
-  };
-
-  hours = function hours(number) {
-    return number * minutes(60);
-  };
-
-  days = function days(number) {
-    return number * hours(24);
-  };
-
-  keccak256 = function keccak256(string) {
-    return web3.sha3(string);
+  hexToInt = function (string) {
+    return web3.toBigNumber(string)
   }
 
-  logTopic = function logTopic(string) {
-    let hash = keccak256(string);
-    return '0x' + hash.slice(26);
+  hexToAddress = function (string) {
+    return '0x' + string.slice(string.length - 40)
   }
 
-  getLatestBlock = async function getLatestBlock() {
-    return await eth.getBlock('latest', false);
-  };
+  unixTime = function (time) {
+    return moment(time).unix()
+  }
 
-  getLatestTimestamp = async function getLatestTimestamp () {
+  seconds = function (number) {
+    return number
+  }
+
+  minutes = function (number) {
+    return number * 60
+  }
+
+  hours = function (number) {
+    return number * minutes(60)
+  }
+
+  days = function (number) {
+    return number * hours(24)
+  }
+
+  keccak256 = function (string) {
+    return web3.sha3(string)
+  }
+
+  logTopic = function (string) {
+    let hash = keccak256(string)
+    return '0x' + hash.slice(26)
+  }
+
+  getLatestBlock = async function () {
+    return await eth.getBlock('latest', false)
+  }
+
+  getLatestTimestamp = async function () {
     let latestBlock = await getLatestBlock()
-    return web3.toDecimal(latestBlock.timestamp);
-  };
+    return web3.toDecimal(latestBlock.timestamp)
+  }
 
-  fastForwardTo = async function fastForwardTo(target) {
-    let now = await getLatestTimestamp();
-    assert.isAbove(target, now, "Cannot fast forward to the past");
-    let difference = target - now;
-    await Eth("evm_increaseTime", [difference]);
-    await sealBlock();
-  };
+  fastForwardTo = async function (target) {
+    let now = await getLatestTimestamp()
+    assert.isAbove(target, now, 'Cannot fast forward to the past')
+    let difference = target - now
+    await Eth('evm_increaseTime', [difference])
+    await sealBlock()
+  }
 
-  getEvents = function getEvents(contract) {
+  getEvents = function (contract) {
     return new Promise((resolve, reject) => {
       contract.allEvents().get((error, events) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(events);
+          resolve(events)
         };
-      });
-    });
-  };
+      })
+    })
+  }
 
-  eventsOfType = function eventsOfType(events, type) {
-    let filteredEvents = [];
+  eventsOfType = function (events, type) {
+    let filteredEvents = []
     for (event of events) {
-      if (event.event === type) filteredEvents.push(event);
+      if (event.event === type) filteredEvents.push(event)
     }
-    return filteredEvents;
-  };
+    return filteredEvents
+  }
 
-  getEventsOfType = async function getEventsOfType(contract, type) {
-    return eventsOfType(await getEvents(contract), type);
-  };
+  getEventsOfType = async function (contract, type) {
+    return eventsOfType(await getEvents(contract), type)
+  }
 
-  getLatestEvent = async function getLatestEvent(contract) {
-    let events = await getEvents(contract);
-    return events[events.length - 1];
-  };
+  getLatestEvent = async function (contract) {
+    let events = await getEvents(contract)
+    return events[events.length - 1]
+  }
 
-  assertActionThrows = function assertActionThrows(action) {
+  assertActionThrows = function (action) {
     return Promise.resolve().then(action)
       .catch(error => {
-        assert(error, "Expected an error to be raised");
-        assert(error.message, "Expected an error to be raised");
-        return error.message;
+        assert(error, 'Expected an error to be raised')
+        assert(error.message, 'Expected an error to be raised')
+        return error.message
       })
       .then(errorMessage => {
-        assert(errorMessage, "Expected an error to be raised");
-        invalidOpcode = errorMessage.includes("invalid opcode")
-        reverted = errorMessage.includes("VM Exception while processing transaction: revert")
-        assert.isTrue(invalidOpcode || reverted, 'expected error message to include "invalid JUMP" or "revert"');
+        assert(errorMessage, 'Expected an error to be raised')
+        const invalidOpcode = errorMessage.includes('invalid opcode')
+        const reverted = errorMessage.includes('VM Exception while processing transaction: revert')
+        assert.isTrue(invalidOpcode || reverted, 'expected error message to include "invalid JUMP" or "revert"')
         // see https://github.com/ethereumjs/testrpc/issues/39
         // for why the "invalid JUMP" is the throw related error when using TestRPC
       })
-  };
-
-  encodeUint256 = function encodeUint256(int) {
-    let zeros = "0000000000000000000000000000000000000000000000000000000000000000";
-    let payload = int.toString(16);
-    return (zeros + payload).slice(payload.length);
   }
 
-  encodeAddress = function encodeAddress(address) {
-    return '000000000000000000000000' + address.slice(2);
+  encodeUint256 = function (int) {
+    let zeros = '0000000000000000000000000000000000000000000000000000000000000000'
+    let payload = int.toString(16)
+    return (zeros + payload).slice(payload.length)
   }
 
-  encodeBytes = function encodeBytes(bytes) {
-    let zeros = "0000000000000000000000000000000000000000000000000000000000000000";
-    let padded = bytes.padEnd(64, 0);
-    let length = encodeUint256(bytes.length / 2);
-    return length + padded;
+  encodeAddress = function (address) {
+    return '000000000000000000000000' + address.slice(2)
   }
 
-  checkPublicABI = function checkPublicABI(contract, expectedPublic) {
-    let actualPublic = [];
+  encodeBytes = function (bytes) {
+    let zeros = '0000000000000000000000000000000000000000000000000000000000000000'
+    let padded = bytes.padEnd(64, 0)
+    let length = encodeUint256(bytes.length / 2)
+    return length + padded
+  }
+
+  checkPublicABI = function (contract, expectedPublic) {
+    let actualPublic = []
     for (method of contract.abi) {
-      if (method.type == 'function') actualPublic.push(method.name);
+      if (method.type == 'function') actualPublic.push(method.name)
     };
 
     for (method of actualPublic) {
-      let index = expectedPublic.indexOf(method);
+      let index = expectedPublic.indexOf(method)
       assert.isAtLeast(index, 0, (`#${method} is NOT expected to be public`))
     }
 
     for (method of expectedPublic) {
-      let index = actualPublic.indexOf(method);
+      let index = actualPublic.indexOf(method)
       assert.isAtLeast(index, 0, (`#${method} is expected to be public`))
     }
-  };
+  }
 
-  functionSelector = function functionSelector(signature) {
-    return "0x" + web3.sha3(signature).slice(2).slice(0, 8);
-  };
+  functionSelector = function (signature) {
+    return '0x' + web3.sha3(signature).slice(2).slice(0, 8)
+  }
 
   // https://codepen.io/code_monk/pen/FvpfI
-  randomHex = function randomHex(len) {
-    var maxlen = 8;
-    var min = Math.pow(16,Math.min(len,maxlen)-1);
-    var max = Math.pow(16,Math.min(len,maxlen)) - 1;
-    var n   = Math.floor( Math.random() * (max-min+1) ) + min;
-    var r   = n.toString(16);
-    while ( r.length < len ) {
-      r = r + randomHex(len - maxlen);
+  randomHex = function (len) {
+    var maxlen = 8
+    var min = Math.pow(16, Math.min(len, maxlen) - 1)
+    var max = Math.pow(16, Math.min(len, maxlen)) - 1
+    var n = Math.floor(Math.random() * (max - min + 1)) + min
+    var r = n.toString(16)
+    while (r.length < len) {
+      r = r + randomHex(len - maxlen)
     }
-    return r;
-  };
+    return r
+  }
 
-  newAddress = function newAddress() {
-    return "0x" + randomHex(40);
-  };
+  newAddress = () => {
+    return '0x' + randomHex(40)
+  }
 
-  requestDataBytes = function requestDataBytes(jobId, to, fHash, runId, data) {
-    let types = ["address", "uint256", "uint256", "bytes32", "address", "bytes4", "bytes32", "bytes"];
-    let values = [0, 0 ,1, jobId, to, fHash, runId, data];
-    let funcSelector = functionSelector("requestData(address,uint256,uint256,bytes32,address,bytes4,bytes32,bytes)");
-    let encoded = abi.rawEncode(types, values);
-    return funcSelector + encoded.toString("hex");
-  };
+  requestDataBytes = function requestDataBytes (jobId, to, fHash, runId, data) {
+    let types = ['address', 'uint256', 'uint256', 'bytes32', 'address', 'bytes4', 'bytes32', 'bytes']
+    let values = [0, 0, 1, jobId, to, fHash, runId, data]
+    let funcSelector = functionSelector('requestData(address,uint256,uint256,bytes32,address,bytes4,bytes32,bytes)')
+    let encoded = abi.rawEncode(types, values)
+    return funcSelector + encoded.toString('hex')
+  }
 
-  requestDataFrom = function requestDataFrom(oc, link, amount, args) {
-    return link.transferAndCall(oc.address, amount, args);
-  };
+  requestDataFrom = function requestDataFrom (oc, link, amount, args) {
+    return link.transferAndCall(oc.address, amount, args)
+  }
+})()
 
-})();
+export {
+  Accounts,
+  Eth,
+  accounts,
+  assertActionThrows,
+  bigNum,
+  checkPublicABI,
+  consumer,
+  days,
+  emptyAddress,
+  encodeAddress,
+  encodeBytes,
+  encodeUint256,
+  eth,
+  eventsOfType,
+  fastForwardTo,
+  functionSelector,
+  getBalance,
+  getEvents,
+  getEventsOfType,
+  getLatestBlock,
+  getLatestEvent,
+  getLatestTimestamp,
+  hexToAddress,
+  hexToInt,
+  hours,
+  intToHex,
+  keccak256,
+  logTopic,
+  minutes,
+  newAddress,
+  oracleNode,
+  randomHex,
+  requestDataBytes,
+  requestDataFrom,
+  sealBlock,
+  seconds,
+  sendTransaction,
+  stranger,
+  toWei,
+  tokens,
+  unixTime
+}
