60aee3172f719609945d4a9733b04a97371223ac
Dimitri Roche <dimroc@gmail.com>
2018-09-11 18:24:11 -0400

    Ability to handle already updated dbs
    Signed-off-by: Steve Ellis <email@steveell.is>

--- store/models/migrations/migration1536696950/migrate_test.go
+++ store/models/migrations/migration1536696950/migrate_test.go
@@ -41,3 +41,22 @@ func TestMigrate_MigrateRunResultAmount1536521223(t *testing.T) {
 	require.Equal(t, jr2.Result.Amount, assets.NewLink(1000000000000000000))
 	require.Equal(t, jr2.TaskRuns[0].Result.Amount, assets.NewLink(1000000000000000000))
 }
+
+func TestMigrate_MigrateRunResultAmount1536521223_asString(t *testing.T) {
+	input := cltest.LoadJSON("../../../../internal/fixtures/bolt/jobrun_string_amount.json")
+	var jr migration1536696950.JobRun
+	require.NoError(t, json.Unmarshal(input, &jr))
+
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
+
+	require.NoError(t, store.Save(&jr))
+
+	migration := migration1536696950.Migration{}
+	require.NoError(t, migration.Migrate(store.ORM))
+
+	var jr2 migration1536696950.JobRun
+	require.NoError(t, store.One("ID", jr.ID, &jr2))
+	require.Equal(t, jr2.Result.Amount, assets.NewLink(1000000000000000000))
+	require.Equal(t, jr2.TaskRuns[0].Result.Amount, assets.NewLink(1000000000000000000))
+}
