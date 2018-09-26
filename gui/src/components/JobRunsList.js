a62e1581e5a9561e37de7c53ab4ea5793e41ed9c
Steve Ellis <email@steveell.is>
2018-09-26 14:15:53 -0400

    make entire job runs table scroll

--- gui/src/components/JobRunsList.js
+++ gui/src/components/JobRunsList.js
@@ -4,14 +4,24 @@ import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import JobRunsHead from 'components/JobRunsHead'
 import JobRunsRow from 'components/JobRunsRow'
+import Card from '@material-ui/core/Card'
+import { withStyles } from '@material-ui/core/styles'
 
-const JobRunsList = ({jobSpecId, runs}) => (
-  <Table>
-    <JobRunsHead />
-    <TableBody>
-      {runs.map(r => <JobRunsRow key={r.id} jobSpecId={jobSpecId} {...r} />)}
-    </TableBody>
-  </Table>
+const styles = theme => ({
+  jobRunsCard: {
+    overflow: 'auto'
+  }
+})
+
+const JobRunsList = ({jobSpecId, runs, classes}) => (
+  <Card className={classes.jobRunsCard}>
+    <Table>
+      <JobRunsHead />
+      <TableBody>
+        {runs.map(r => <JobRunsRow key={r.id} jobSpecId={jobSpecId} {...r} />)}
+      </TableBody>
+    </Table>
+  </Card>
 )
 
 JobRunsList.propTypes = {
@@ -19,4 +29,4 @@ JobRunsList.propTypes = {
   runs: PropTypes.array.isRequired
 }
 
-export default JobRunsList
+export default withStyles(styles)(JobRunsList)
