c497011e115c289ecf43db1385f4b95d50aac853
Thomas Hodges <me@thodges.com>
2018-10-03 12:27:50 -0500

    Add Link address helper to Chainlinked contract

--- solidity/contracts/Chainlinked.sol
+++ solidity/contracts/Chainlinked.sol
@@ -70,6 +70,14 @@ contract Chainlinked {
     link = LinkToken(_link);
   }
 
+  function getLinkToken()
+    internal
+    view
+    returns (address)
+  {
+    return address(link);
+  }
+
   function newChainlinkWithENS(address _ens, bytes32 _node)
     internal
     returns (address, address)
