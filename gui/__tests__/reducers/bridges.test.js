bbd38c51f14090b011322ea2b0aaf06b5893303f
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-10-08 12:18:44 -0400

    Reduce bridge index & show into single reducer
    - Use dictionary with current page instead of array
    - Consistent with other index + show pages
    - Use selector pattern to reconstruct data for rendering

--- gui/__tests__/reducers/bridges.test.js
+++ gui/__tests__/reducers/bridges.test.js
@@ -2,15 +2,18 @@ import reducer from 'connectors/redux/reducers'
 import {
   REQUEST_BRIDGES,
   RECEIVE_BRIDGES_SUCCESS,
-  RECEIVE_BRIDGES_ERROR
+  RECEIVE_BRIDGES_ERROR,
+  REQUEST_BRIDGE,
+  RECEIVE_BRIDGE_SUCCESS,
+  RECEIVE_BRIDGE_ERROR
 } from 'actions'
 
 describe('bridges reducer', () => {
-  it('should return the initial state', () => {
+  it('returns the initial state', () => {
     const state = reducer(undefined, {})
 
     expect(state.bridges).toEqual({
-      items: [],
+      items: {},
       currentPage: [],
       count: 0,
       networkError: false
@@ -19,7 +22,10 @@ describe('bridges reducer', () => {
 
   it('REQUEST_BRIDGES disables the network error', () => {
     const action = {type: REQUEST_BRIDGES}
-    const state = reducer(undefined, action)
+    const previousState = {
+      bridges: {networkError: true}
+    }
+    const state = reducer(previousState, action)
 
     expect(state.bridges.networkError).toEqual(false)
   })
@@ -27,11 +33,17 @@ describe('bridges reducer', () => {
   it('RECEIVE_BRIDGES_SUCCESS stores the bridge items and the current page', () => {
     const action = {
       type: RECEIVE_BRIDGES_SUCCESS,
-      items: [{name: 'a'}, {name: 'b'}]
+      items: [
+        {id: 'a', name: 'A'},
+        {id: 'b', name: 'B'}
+      ]
     }
     const state = reducer(undefined, action)
 
-    expect(state.bridges.items).toEqual([{name: 'a'}, {name: 'b'}])
+    expect(state.bridges.items).toEqual({
+      a: {id: 'a', name: 'A'},
+      b: {id: 'b', name: 'B'}
+    })
     expect(state.bridges.currentPage).toEqual(['a', 'b'])
     expect(state.bridges.networkError).toEqual(false)
   })
@@ -42,6 +54,48 @@ describe('bridges reducer', () => {
     }
     const action = {
       type: RECEIVE_BRIDGES_ERROR,
+      networkError: true
+    }
+    const state = reducer(previousState, action)
+
+    expect(state.bridges.networkError).toEqual(true)
+  })
+
+  it('REQUEST_BRIDGE disables the network error', () => {
+    const previousState = {
+      bridges: {networkError: true}
+    }
+    const action = {type: REQUEST_BRIDGE}
+    const state = reducer(previousState, action)
+
+    expect(state.bridges.networkError).toEqual(false)
+  })
+
+  it('RECEIVE_BRIDGE_SUCCESS adds to the list of items', () => {
+    const action = {
+      type: RECEIVE_BRIDGE_SUCCESS,
+      item: {
+        id: 'a',
+        name: 'A'
+      }
+    }
+    const previousState = {
+      bridges: {items: []}
+    }
+    const state = reducer(previousState, action)
+
+    expect(state.bridges.items.a).toEqual({
+      id: 'a',
+      name: 'A'
+    })
+  })
+
+  it('RECEIVE_BRIDGE_ERROR assigns a network error', () => {
+    const previousState = {
+      bridges: {networkError: false}
+    }
+    const action = {
+      type: RECEIVE_BRIDGE_ERROR,
       networkError: true
     }
     const state = reducer(previousState, action)
--- gui/src/connectors/redux/reducers/bridges.js
+++ gui/src/connectors/redux/reducers/bridges.js
@@ -1,11 +1,14 @@
 import {
   REQUEST_BRIDGES,
   RECEIVE_BRIDGES_SUCCESS,
-  RECEIVE_BRIDGES_ERROR
+  RECEIVE_BRIDGES_ERROR,
+  REQUEST_BRIDGE,
+  RECEIVE_BRIDGE_SUCCESS,
+  RECEIVE_BRIDGE_ERROR
 } from 'actions'
 
 const initialState = {
-  items: [],
+  items: {},
   currentPage: [],
   count: 0,
   networkError: false
@@ -20,18 +23,50 @@ export default (state = initialState, action = {}) => {
         {networkError: false}
       )
     case RECEIVE_BRIDGES_SUCCESS: {
+      const newItems = action.items.reduce(
+        (acc, i) => {
+          acc[i.id] = i
+          return acc
+        },
+        {}
+      )
+
       return Object.assign(
         {},
         state,
         {
-          items: Object.assign([], state.items, action.items),
-          currentPage: action.items.map(b => b.name),
+          items: Object.assign({}, state.items, newItems),
+          currentPage: action.items.map(b => b.id),
           count: action.count,
           networkError: false
         }
       )
     }
     case RECEIVE_BRIDGES_ERROR:
+      return Object.assign(
+        {},
+        state,
+        {networkError: !!action.networkError}
+      )
+    case REQUEST_BRIDGE:
+      return Object.assign(
+        {},
+        state,
+        {networkError: false}
+      )
+    case RECEIVE_BRIDGE_SUCCESS:
+      return Object.assign(
+        {},
+        state,
+        {
+          items: Object.assign(
+            {},
+            state.items,
+            {[action.item.id]: action.item}
+          )
+        }
+      )
+    case RECEIVE_BRIDGE_ERROR:
       return Object.assign(
         {},
         state,
