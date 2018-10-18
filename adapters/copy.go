5e50ad68603c4974297179825480a9290d7d99c6
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-10-18 17:05:29 -0500

    modify Copy adapter to accept dot notation in existing field

--- adapters/copy.go
+++ adapters/copy.go
@@ -8,18 +8,12 @@ import (
 // Copy obj keys refers to which value to copy inside `data`,
 // each obj value refers to where to copy the value to inside `data`
 type Copy struct {
-	CopyPath []string `json:"copyPath"`
-	Path     JSONPath `json:"path"`
+	CopyPath JSONPath `json:"copyPath"`
 }
 
 // Perform returns the copied values from the desired mapping within the `data` JSON object
 func (c *Copy) Perform(input models.RunResult, store *store.Store) models.RunResult {
-	var jp JSONParse
-	if len(c.CopyPath) > 0 {
-		jp = JSONParse{Path: c.CopyPath}
-	} else {
-		jp = JSONParse{Path: c.Path}
-	}
+	jp := JSONParse{Path: c.CopyPath}
 
 	data, err := input.Data.Add("value", input.Data.String())
 	if err != nil {
