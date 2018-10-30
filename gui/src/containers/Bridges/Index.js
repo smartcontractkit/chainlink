dfcb2720778bea7e764a0d59e667d20a8fea7859
NavyAdmiral <keccaksha3@gmail.com>
2018-10-30 02:05:18 -0400

    Bridge Index less classes more functions

--- gui/src/containers/Bridges/Index.js
+++ gui/src/containers/Bridges/Index.js
@@ -11,49 +11,42 @@ import matchRouteAndMapDispatchToProps from 'utils/matchRouteAndMapDispatchToPro
 import bridgesSelector from 'selectors/bridges'
 import { fetchBridges } from 'actions'
 
-export class Index extends Component {
-  componentDidMount () {
-    this.props.fetchBridges(1, this.props.pageSize)
-  }
-
-  render () {
-    const { bridges, bridgeCount, pageSize, bridgesError, fetchBridges } = this.props
-
-    return (
-      <div>
-        <Grid container spacing={8} alignItems='center'>
-          <Grid item xs={9}>
-            <Title>Bridges</Title>
-          </Grid>
-          <Grid item xs={3}>
-            <Grid container justify='flex-end'>
-              <Grid item>
-                <Button
-                  variant='outlined'
-                  color='primary'
-                  component={ReactStaticLinkComponent}
-                  to={'/bridges/new'}
-                >
-                  New Bridge
+export const Index = (props) => {
+  const { bridges, bridgeCount, pageSize, bridgesError, fetchBridges, history, match } = props
+  return <div>
+    <Grid container spacing={8} alignItems='center'>
+      <Grid item xs={9}>
+        <Title>Bridges</Title>
+      </Grid>
+      <Grid item xs={3}>
+        <Grid container justify='flex-end'>
+          <Grid item>
+            <Button
+              variant='outlined'
+              color='primary'
+              component={ReactStaticLinkComponent}
+              to={'/bridges/new'}
+            >
+              New Bridge
                 </Button>
-              </Grid>
-            </Grid>
           </Grid>
         </Grid>
-        <Grid container spacing={40}>
-          <Grid item xs={12}>
-            <BridgeList
-              bridges={bridges}
-              bridgeCount={bridgeCount}
-              pageSize={pageSize}
-              error={bridgesError}
-              fetchBridges={fetchBridges}
-            />
-          </Grid>
-        </Grid>
-      </div>
-    )
-  }
+      </Grid>
+    </Grid>
+    <Grid container spacing={40}>
+      <Grid item xs={12}>
+        <BridgeList
+          bridges={bridges}
+          bridgeCount={bridgeCount}
+          pageSize={pageSize}
+          error={bridgesError}
+          fetchBridges={fetchBridges}
+          history={history}
+          match={match}
+        />
+      </Grid>
+    </Grid>
+  </div>
 }
 
 Index.propTypes = {
