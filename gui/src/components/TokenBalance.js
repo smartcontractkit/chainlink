dbdb2a6db9ec17162a31974b61b2e11f228371e4
NavyAdmiral <keccaksha3@gmail.com>
2018-10-30 01:11:46 -0400

    Provide more precision around LINK and ETH balances

--- gui/src/components/TokenBalance.js
+++ gui/src/components/TokenBalance.js
@@ -9,24 +9,25 @@ const WEI_PER_TOKEN = new BigNumber(10 ** 18)
 const formatBalance = (val) => {
   const b = new BigNumber(val)
   const tokenBalance = b.dividedBy(WEI_PER_TOKEN).toNumber()
-
-  return numeral(tokenBalance).format('0.20a')
+  return {formatted: numeral(tokenBalance).format('0.200000a'), unformatted: tokenBalance}
 }
 
 const TokenBalance = ({title, value, className, error}) => {
   let val
+  let unformattedVal
   if (error) {
     val = error
   } else if (value == null) {
     val = '...'
   } else {
-    val = formatBalance(value)
+    val = formatBalance(value).formatted
+    unformattedVal = formatBalance(value).unformatted
   }
-
   return (
     <MetaInfo
       title={title}
       value={val}
+      unformattedValue={unformattedVal}
       className={className}
     />
   )
