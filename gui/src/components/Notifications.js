6292ef1042b0f6778dcd952a7bde147d5f85d099
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-10-31 15:42:45 -0700

    Remove success presenter for notifications

--- gui/src/components/Notifications.js
+++ gui/src/components/Notifications.js
@@ -18,17 +18,6 @@ export class Notifications extends React.Component {
     return <p key={i}>{err.detail}</p>
   }
 
-  successPresenter = (success, i) => {
-    const isJobRun = success => success.data && success.data.type === 'runs'
-    const attributes = success.data.attributes
-
-    if (isJobRun(success)) {
-      return <p key={i}>
-        Job <Link to={`/jobs/${attributes.jobId}/runs/id/${attributes.id}`}>{attributes.id}</Link> was successfully run
-      </p>
-    }
-  }
-
   render () {
     const { errors, successes, classes } = this.props
     return (
@@ -47,12 +36,9 @@ export class Notifications extends React.Component {
         }
         {successes.length > 0 && (
           <Flash success className={classes.flash}>
-            {successes.map((succ, i) => {
-              if (succ.type === 'component') {
-                return <p key={i}>{succ.component(succ.props)}</p>
-              }
-              return this.successPresenter(succ, i)
-            })}
+            {successes.map(({component, props}, i) => (
+              <p key={i}>{component(props)}</p>
+            ))}
           </Flash>
         )}
       </React.Fragment>
