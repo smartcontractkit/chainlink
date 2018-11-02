24499b0bf1a62c38ec71d767cd4ed48b353b2883
Dimitri Roche <dimroc@gmail.com>
2018-11-02 17:45:32 -0400

    Fix SGX_ENABLED env variable in chainlink-launcher-sgx

--- chainlink-launcher-sgx.sh
+++ chainlink-launcher-sgx.sh
@@ -14,7 +14,7 @@ fi
 
 trap "kill -- -$$ 2>/dev/null || true" SIGINT SIGTERM EXIT
 
-if [ "$SGXENABLED" = "yes" ] && [ "$SGX_SIMULATION" != "yes" ]; then
+if [ "$SGX_ENABLED" = "yes" ] && [ "$SGX_SIMULATION" != "yes" ]; then
   /opt/intel/sgxpsw/aesm/aesm_service &
   aesm_pid=$!
 fi
@@ -24,7 +24,7 @@ fi
 chainlink "$@" | cat &
 chainlink_pid=$!
 
-if [ "$SGXENABLED" = "yes" ] && [ "$SGX_SIMULATION" != "yes" ]; then
+if [ "$SGX_ENABLED" = "yes" ] && [ "$SGX_SIMULATION" != "yes" ]; then
   while sleep 10; do
     kill -0 $aesm_pid 2>/dev/null
     kill -0 $chainlink_pid 2>/dev/null
