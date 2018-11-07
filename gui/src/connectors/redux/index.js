f96be4bc7321844346bcf6016bab773cb11d73dc
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-11-07 15:27:21 -0800

    Add optional redux logger for test debugger
    LOG_REDUX=true yarn test

--- gui/src/connectors/redux/index.js
+++ gui/src/connectors/redux/index.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import logger from 'redux-logger'
 import reducer from './reducers'
 
 if (typeof window === 'undefined') {
   global.window = {}
 }
 
-const middleware = [thunkMiddleware]
+let middleware = [thunkMiddleware]
+if (process.env.LOG_REDUX === 'true') {
+  middleware = middleware.concat(logger)
+}
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default () => (
