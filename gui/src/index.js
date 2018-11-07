98709c1db1ca7d8eb490855bcdc66d3d8a4eb2ae
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-11-07 11:34:51 -0800

    Add finally shim for older browser support

--- gui/src/index.js
+++ gui/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import promiseFinally from 'promise.prototype.finally'
+import JavascriptTimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import App from './App'
 import theme from './theme'
 
-// Human readable date & time
-import JavascriptTimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
+promiseFinally.shim(Promise)
 
 window.JavascriptTimeAgo = JavascriptTimeAgo
-
 JavascriptTimeAgo.locale(en)
 
 // Export top level component as JSX (for static rendering)
