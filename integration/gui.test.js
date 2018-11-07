5268a8d7d3c2df4505fb35c46f442b3cd6cbb9ac
Dimitri Roche <dimroc@gmail.com>
2018-11-07 11:23:06 -0500

    Assert creation of a job

---
+++ integration/gui.test.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer')
+
+describe('Integration', () => {
+  let browser, page
+  beforeAll(async () => {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+
+  it('creates a job that runs', async () => {
+    await page.goto('http://localhost:6688')
+    await expect(page).toMatch('Chainlink')
+    await expect(page).toFill('form input[id=email]', 'notreal@fakeemail.ch')
+    await expect(page).toFill('form input[id=password]', 'twochains')
+    await expect(page).toClick('form button')
+    await expect(page).toMatch('Jobs')
+    await expect(page).toClick('a', { text: 'New Job' })
+    await expect(page).toMatch('New Job')
+
+    const jobJson = `{
+      "initiators": [{"type": "web"}],
+      "tasks": [{"type": "NoOp"}]
+    }`
+    await expect(page).toFill('form textarea', jobJson)
+    await expect(page).toClick('button', { text: 'Create Job' })
+    await expect(page).toMatch('successfully created')
+  })
+})
