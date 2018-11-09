81398f114285bb617f98b66817963b38e17aa075
Steve Ellis <email@steveell.is>
2018-11-09 18:44:56 -0500

    remove ChainlinkLib's dependency on Oracle.sol

--- solidity/contracts/interfaces/OracleInterface.sol
+++ solidity/contracts/interfaces/OracleInterface.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.4.24;
 
 interface OracleInterface {
-  function fulfillData(uint256 _internalId, bytes32 _data) external returns (bool);
-  function cancel(bytes32 _externalId) external;
+  function fulfillData(uint256 internalId, bytes32 data) external returns (bool);
+  function cancel(bytes32 externalId) external;
+  function requestData(
+    address sender,
+    uint256 amount,
+    uint256 version,
+    bytes32 specId,
+    address callbackAddress,
+    bytes4 callbackFunctionId,
+    bytes32 externalId,
+    bytes data
+  ) external;
 }
