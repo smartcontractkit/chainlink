f905955cdfa02ecb82f456eb861d5740ef91c6c3
John Barker <jebarker@gmail.com>
2018-11-09 19:43:06 -0700

    Use a pool to keep track of SgxEnclave handles
    Create a pool of SgxEnclave handles that are lazily initialized when a
    call is made. This should allow for some parallelism in enclave calls.

--- sgx/libadapters/src/lib.rs
+++ sgx/libadapters/src/lib.rs
@@ -1,31 +1,38 @@
 #![feature(libc)]
 
 extern crate errno;
+#[macro_use]
+extern crate lazy_static;
 extern crate libc;
+extern crate pool;
 extern crate sgx_types;
 extern crate sgx_urts;
 extern crate utils;
-#[macro_use]
-extern crate lazy_static;
 
+use pool::Pool;
 use sgx_types::*;
 use sgx_urts::SgxEnclave;
+use std::cell::RefCell;
 use std::sync::{Arc, Mutex};
 
 pub mod multiply;
 pub mod wasm;
 
+// ENCLAVE_FILE is the path to the signed enclave
 static ENCLAVE_FILE: &str = "enclave.signed.so";
 
+// MAX_ECALLS controls the maximum number of concurrent enclave calls can be made
+static MAX_ECALLS: usize = 2;
+
+type LazyEnclave = Arc<Mutex<RefCell<Option<SgxEnclave>>>>;
+type LazyEnclavePool = Arc<Mutex<Pool<LazyEnclave>>>;
+
 lazy_static! {
-    static ref ENCLAVE: Arc<Mutex<SgxEnclave>> = {
-        Arc::new(Mutex::new(enclave_init().unwrap_or_else(|err| {
-            panic!("Failed to initialize the enclave: {}", err);
-        })))
-    };
+    static ref ENCLAVE_POOL: LazyEnclavePool =
+        Arc::new(Mutex::new(Pool::with_capacity(MAX_ECALLS, 0, || Arc::new(Mutex::new(RefCell::new(None))))));
 }
 
-fn enclave_init() -> SgxResult<SgxEnclave> {
+fn new_enclave() -> SgxResult<SgxEnclave> {
     let mut launch_token: sgx_launch_token_t = [0; 1024];
     let mut launch_token_updated: i32 = 0;
     let debug = 1;
@@ -44,8 +51,25 @@ fn enclave_init() -> SgxResult<SgxEnclave> {
 
 // use_enclave takes a closure and passes in an enclave ID for making enclave calls in a thread
 // safe way
-pub fn use_enclave<F>(ctx: F) 
+pub fn use_enclave<F>(ctx: F)
     where F: Fn(u64) {
-    let enclave = ENCLAVE.lock().unwrap();
-    ctx(enclave.geteid())
+
+    let lazy_enclave: LazyEnclave = {
+        Arc::clone(&ENCLAVE_POOL.lock().unwrap().checkout().unwrap())
+    };
+
+    let cell: &RefCell<Option<SgxEnclave>> = &*lazy_enclave.lock().unwrap();
+    let enclave_id = || -> u64 {
+        if let Some(ref e) = *cell.borrow() {
+            return e.geteid();
+        }
+
+        let e = new_enclave().unwrap();
+        println!("new enclave {:?}", e);
+        let eid = e.geteid();
+        *cell.borrow_mut() = Some(e);
+        eid
+    }();
+
+    ctx(enclave_id)
 }
