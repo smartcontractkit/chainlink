9d72d74d78b2d2796e7c8865076a8b6943d23683
Steve Ellis <email@steveell.is>
2018-11-12 10:01:11 -0800

    Revert "use function selectors instead of hashed strings"
    This reverts commit 03cd92fe7585ea01ff09c5cdaf5168adff07957d.

--- solidity/contracts/examples/MaliciousChainlinked.sol
+++ solidity/contracts/examples/MaliciousChainlinked.sol
@@ -23,19 +23,19 @@ contract MaliciousChainlinked {
   function newRun(
     bytes32 _specId,
     address _callbackAddress,
-    bytes4 _callbackFunction
+    string _callbackFunctionSignature
   ) internal pure returns (MaliciousChainlinkLib.Run memory) {
     MaliciousChainlinkLib.Run memory run;
-    return run.initialize(_specId, _callbackAddress, _callbackFunction);
+    return run.initialize(_specId, _callbackAddress, _callbackFunctionSignature);
   }
 
   function newWithdrawRun(
     bytes32 _specId,
     address _callbackAddress,
-    bytes4 _callbackFunction
+    string _callbackFunctionSignature
   ) internal pure returns (MaliciousChainlinkLib.WithdrawRun memory) {
     MaliciousChainlinkLib.WithdrawRun memory run;
-    return run.initializeWithdraw(_specId, _callbackAddress, _callbackFunction);
+    return run.initializeWithdraw(_specId, _callbackAddress, _callbackFunctionSignature);
   }
 
   function chainlinkRequest(MaliciousChainlinkLib.Run memory _run, uint256 _wei)
@@ -45,7 +45,7 @@ contract MaliciousChainlinked {
     requests += 1;
     _run.requestId = bytes32(requests);
     _run.close();
-    require(link.transferAndCall(oracle, _wei, encodeForOracle(_run)), "Unable to transferAndCall to oracle");
+    require(link.transferAndCall(oracle, _wei, _run.encodeForOracle(clArgsVersion)), "Unable to transferAndCall to oracle");
     emit ChainlinkRequested(_run.requestId);
     unfulfilledRequests[_run.requestId] = true;
     return _run.requestId;
@@ -58,7 +58,7 @@ contract MaliciousChainlinked {
     requests += 1;
     _run.requestId = bytes32(requests);
     _run.closeWithdraw();
-    require(link.transferAndCall(oracle, _wei, encodeWithdrawForOracle(_run)), "Unable to transferAndCall to oracle");
+    require(link.transferAndCall(oracle, _wei, _run.encodeWithdrawForOracle(clArgsVersion)), "Unable to transferAndCall to oracle");
     emit ChainlinkRequested(_run.requestId);
     unfulfilledRequests[_run.requestId] = true;
     return _run.requestId;
@@ -74,31 +74,6 @@ contract MaliciousChainlinked {
 
   function setLinkToken(address _link) internal {
     link = LinkTokenInterface(_link);
-  }
-
-  function encodeForOracle(MaliciousChainlinkLib.Run memory _run)
-    internal view returns (bytes memory)
-  {
-    return abi.encodeWithSelector(
-      oracle.requestData.selector,
-      address(this), // overridden by onTokenTransfer
-      100 ether,     // overridden by onTokenTransfer
-      clArgsVersion,
-      _run.specId,
-      _run.callbackAddress,
-      _run.callbackFunctionId,
-      _run.requestId,
-      _run.buf.buf);
-  }
-
-  function encodeWithdrawForOracle(MaliciousChainlinkLib.WithdrawRun memory _run)
-    internal view returns (bytes memory)
-  {
-    return abi.encodeWithSelector(
-      oracle.withdraw.selector,
-      _run.callbackAddress,
-      _run.amount,
-      _run.buf.buf);
   }
 
   modifier checkChainlinkFulfillment(bytes32 _requestId) {
