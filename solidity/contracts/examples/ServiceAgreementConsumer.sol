f3618395830e2f16468dac63e6943af67c0111f6
Steve Ellis <email@steveell.is>
2018-11-18 12:54:21 -0800

    move comments to variable names
    - remove unused code/tests

--- solidity/contracts/examples/ServiceAgreementConsumer.sol
+++ solidity/contracts/examples/ServiceAgreementConsumer.sol
@@ -6,11 +6,6 @@ contract ServiceAgreementConsumer is Chainlinked {
   bytes32 internal sAId;
   bytes32 public currentPrice;
 
-  event RequestFulfilled(
-    bytes32 indexed requestId,
-    bytes32 indexed data
-  );
-
   constructor(address _link, address _coordinator, bytes32 _sAId) public {
     setLinkToken(_link);
     setOracle(_coordinator);
@@ -20,9 +15,7 @@ contract ServiceAgreementConsumer is Chainlinked {
   function requestEthereumPrice(string _currency) public {
     ChainlinkLib.Run memory run = newRun(sAId, this, this.fulfill.selector);
     run.add("url", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,JPY");
-    string[] memory path = new string[](1);
-    path[0] = _currency;
-    run.addStringArray("path", path);
+    run.add("path", _currency);
     serviceRequest(run, LINK(1));
   }
 
@@ -30,16 +23,6 @@ contract ServiceAgreementConsumer is Chainlinked {
     public
     checkChainlinkFulfillment(_requestId)
   {
-    emit RequestFulfilled(_requestId, _price);
     currentPrice = _price;
-  }
-
-  function cancelRequest(bytes32 _requestId) public {
-    cancelChainlinkRequest(_requestId);
-  }
-
-  function withdrawLink() public {
-    LinkTokenInterface link = LinkTokenInterface(chainlinkToken());
-    require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
   }
 }
