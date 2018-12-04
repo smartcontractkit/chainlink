800e61598f04085e3cdd6d1d751cf2c75b554cc6
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-12-03 16:21:32 -0800

    Combine About & Configuration pages
    [Finishes #162370656]

--- gui/src/containers/Configuration.js
+++ gui/src/containers/Configuration.js
@@ -1,31 +1,70 @@
 import React from 'react'
+import { useHooks, useEffect } from 'use-react-hooks'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { withSiteData } from 'react-static'
+import { withRouteData } from 'react-static'
 import Grid from '@material-ui/core/Grid'
-import Title from 'components/Title'
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Divider from '@material-ui/core/Divider'
+import PaddedCard from 'components/PaddedCard'
 import ConfigList from 'components/ConfigList'
 import Content from 'components/Content'
 import { fetchConfiguration } from 'actions'
 import configsSelector from 'selectors/configs'
 import matchRouteAndMapDispatchToProps from 'utils/matchRouteAndMapDispatchToProps'
-import { useHooks, useEffect } from 'use-react-hooks'
 
 export const Configuration = useHooks(props => {
   useEffect(() => { props.fetchConfiguration() }, [])
-  return <Content>
-    <Title>Configuration</Title>
-    <Grid container spacing={40}>
-      <Grid item xs={12}>
-        <ConfigList
-          configs={props.configs}
-          error={props.error}
-        />
+
+  return (
+    <Content>
+      <Grid container spacing={40}>
+        <Grid item xs={8}>
+          <Card>
+            <CardContent>
+              <Typography variant='h5' color='secondary'>
+                Configuration
+              </Typography>
+            </CardContent>
+
+            <Divider />
+
+            <ConfigList
+              configs={props.configs}
+              error={props.error}
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Grid container spacing={40}>
+            <Grid item xs={12}>
+              <PaddedCard>
+                <Typography variant='h5' color='secondary'>
+                  Version
+                </Typography>
+                <Typography variant='body1'>
+                  {props.version}
+                </Typography>
+              </PaddedCard>
+            </Grid>
+            <Grid item xs={12}>
+              <PaddedCard>
+                <Typography variant='h5' color='secondary'>
+                  SHA
+                </Typography>
+                <Typography variant='body1'>
+                  {props.sha}
+                </Typography>
+              </PaddedCard>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
-    </Grid>
-  </Content>
-}
-)
+    </Content>
+  )
+})
 
 Configuration.propTypes = {
   configs: PropTypes.array.isRequired,
@@ -49,4 +88,4 @@ export const ConnectedConfiguration = connect(
   matchRouteAndMapDispatchToProps({fetchConfiguration})
 )(Configuration)
 
-export default withSiteData(ConnectedConfiguration)
+export default withRouteData(ConnectedConfiguration)
