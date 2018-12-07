be794b4415958b8a88f75e9adedbee447a67f8a1
John Barker <jebarker@gmail.com>
2018-12-06 19:02:15 -0600

    Add event to RunLog contract and event counter assertion

--- integration/contracts/RunLog.sol
+++ integration/contracts/RunLog.sol
@@ -5,6 +5,8 @@ import "Chainlinked.sol";
 contract RunLog is Chainlinked {
   uint256 constant private ORACLE_PAYMENT = 1 * LINK; // solium-disable-line zeppelin/no-arithmetic-operations
 
+  event FulfilledEvent(bytes32 data);
+
   constructor(address _link, address _oracle) public {
     setLinkToken(_link);
     setOracle(_oracle);
@@ -18,7 +20,7 @@ contract RunLog is Chainlinked {
 
   function fulfill(bytes32 _externalId, bytes32 _data)
     public
-    checkChainlinkFulfillment(_externalId)
-  {
+    checkChainlinkFulfillment(_externalId) {
+    emit FulfilledEvent(_data);
   }
 }
