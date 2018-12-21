cdaed7e0ed82972bd5a0a65268b8f3491a73ea4e
Thomas Hodges <me@thodges.com>
2018-12-21 11:42:52 -0600

    Add Chainlinked contract to ZeppelinOS

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -13,7 +13,22 @@ module.exports = {
       host: "127.0.0.1",
       port: 18545,
       network_id: "*",
-      gas: 4700000
+      gas: 4700000,
+      gasPrice: 5e9,
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: '3'
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 28545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: '4'
     }
   }
 };
