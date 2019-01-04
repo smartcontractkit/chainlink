2940a7b6eb4a2e817c989947ede4df35657cda99
Steve Ellis <email@steveell.is>
2019-01-04 16:11:48 -0500

    remove need separate ganache process

--- solidity/run_blockchain_mock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-this_dir=`dirname $0`
-$this_dir/node_modules/.bin/ganache-cli \
-    -m 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat' \
-    -p 18545 "$@"
