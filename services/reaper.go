30bb55b40356287133d2036dc44e3ab73c97d1e0
Dimitri Roche <dimroc@gmail.com>
2019-01-04 16:48:38 -0500

    Reaper deletes session through orm rather than directly

--- services/reaper.go
+++ services/reaper.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/smartcontractkit/chainlink/logger"
 	"github.com/smartcontractkit/chainlink/store"
-	"github.com/smartcontractkit/chainlink/store/models"
-	"github.com/smartcontractkit/chainlink/store/orm"
 )
 
 type storeReaper struct {
@@ -23,19 +21,9 @@ func NewStoreReaper(store *store.Store) SleeperTask {
 }
 
 func (sr *storeReaper) Work() {
-	var sessions []models.Session
 	offset := time.Now().Add(-sr.config.ReaperExpiration()).Add(-sr.config.SessionTimeout())
-	stale := models.Time{offset}
-	err := sr.store.Range("LastUsed", models.Time{}, stale, &sessions)
-	if err != nil && err != orm.ErrorNotFound {
+	err := sr.store.DeleteStaleSessions(offset)
+	if err != nil {
 		logger.Error("unable to reap stale sessions: ", err)
-		return
-	}
-
-	for _, s := range sessions {
-		err := sr.store.DeleteStruct(&s)
-		if err != nil {
-			logger.Error("unable to delete stale session: ", err)
-		}
 	}
 }
