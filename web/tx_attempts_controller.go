99a8fc175bd5e2c08a6d804d80eb1f31fcf8a1d8
Steve Ellis <email@steveell.is>
2019-01-15 15:50:28 -0800

    add basic API for showing transaction attempts

--- web/tx_attempts_controller.go
+++ web/tx_attempts_controller.go
@@ -1,9 +1,12 @@
 package web
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
+	"github.com/manyminds/api2go/jsonapi"
 	"github.com/smartcontractkit/chainlink/services"
 	"github.com/smartcontractkit/chainlink/store/orm"
 )
@@ -30,5 +33,21 @@ func (tac *TxAttemptsController) Index(c *gin.Context) {
 		c.AbortWithError(500, err)
 	} else {
 		c.Data(200, MediaType, buffer)
+	}
+}
+
+// Show returns the details of a Transasction Attempt.
+// Example:
+//  "<application>/txattempts/:TxHash"
+func (tac *TxAttemptsController) Show(c *gin.Context) {
+	hash := common.HexToHash(c.Param("TxHash"))
+	if ta, err := tac.App.GetStore().FindTxAttempt(hash); err == orm.ErrorNotFound {
+		c.AbortWithError(404, errors.New("Transaction Attempt not found"))
+	} else if err != nil {
+		c.AbortWithError(500, err)
+	} else if doc, err := jsonapi.Marshal(ta); err != nil {
+		c.AbortWithError(500, err)
+	} else {
+		c.Data(200, MediaType, doc)
 	}
 }
