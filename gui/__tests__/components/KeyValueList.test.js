1dd888b7dc0eed3f4943c3cc81a2ffc78a151e17
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-01-16 15:47:32 -0800

    KeyValueList can titleize keys

--- gui/__tests__/components/KeyValueList.test.js
+++ gui/__tests__/components/KeyValueList.test.js
@@ -23,10 +23,30 @@ describe('components/KeyValueList', () => {
       ['CHAINLINK_DEV', 'true'],
       ['DATABASE_TIMEOUT', 1000]
     ]
+
     let wrapper = render(<KeyValueList entries={entries} />)
     expect(wrapper.text()).toContain('CHAINLINK_DEV')
     expect(wrapper.text()).toContain('true')
     expect(wrapper.text()).toContain('DATABASE_TIMEOUT')
     expect(wrapper.text()).toContain('1000')
+  })
+
+  it('can titleize keys', () => {
+    const entries = [
+      ['gasLimit', 50000],
+      ['hash', 'abc123']
+    ]
+
+    let withoutTitleize = render(<KeyValueList entries={entries} />)
+    expect(withoutTitleize.text()).toContain('gasLimit')
+    expect(withoutTitleize.text()).toContain('hash')
+    expect(withoutTitleize.text()).toContain('50000')
+    expect(withoutTitleize.text()).toContain('abc123')
+
+    let withTitleize = render(<KeyValueList entries={entries} titleize />)
+    expect(withTitleize.text()).toContain('Gas Limit')
+    expect(withTitleize.text()).toContain('Hash')
+    expect(withTitleize.text()).toContain('50000')
+    expect(withTitleize.text()).toContain('abc123')
   })
 })
