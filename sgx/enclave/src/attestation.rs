16cade036fd4b9af142128183205f2274dbff851
John Barker <jebarker@gmail.com>
2019-01-17 15:54:39 -0700

    Trimming down quote code and breaking out into functions

--- sgx/enclave/src/attestation.rs
+++ sgx/enclave/src/attestation.rs
@@ -1,8 +1,7 @@
-use hex;
 use sgx_rand::{Rng, os::SgxRng};
 use sgx_tcrypto::{self as tcrypto, SgxEccHandle};
 use sgx_types::*;
-use std::io::{Write, Read, BufReader};
+use std::io::{self, Read};
 use std::ptr;
 use std::string::String;
 use std::untrusted::fs;
@@ -26,83 +25,77 @@ extern "C" {
                 p_quote_len        : *mut u32) -> sgx_status_t;
 }
 
-pub fn report() -> Result<sgx_report_t, sgx_status_t> {
-    let ecc_handle = SgxEccHandle::new();
-    let _result = ecc_handle.open();
-    let (prv_k, pub_k) = ecc_handle.create_key_pair().unwrap();
-    
-    // Workflow:
-    // (1) ocall to get the target_info structure (ti) and epid group id (eg)
-    // (2) call sgx_create_report with ti+data, produce an sgx_report_t
-    // (3) ocall to sgx_get_quote to generate (*mut sgx-quote_t, uint32_t)
-
-    // (1) get ti + eg
-    let mut ti : sgx_target_info_t = sgx_target_info_t::default();
-    let mut eg : sgx_epid_group_id_t = sgx_epid_group_id_t::default();
+fn quote() -> Result<(sgx_target_info_t, sgx_epid_group_id_t), sgx_status_t> {
+    let mut target_info : sgx_target_info_t = sgx_target_info_t::default();
+    let mut epid_group_id : sgx_epid_group_id_t = sgx_epid_group_id_t::default();
     let mut rt : sgx_status_t = sgx_status_t::SGX_ERROR_UNEXPECTED;
 
-    let res = unsafe {
+    let result = unsafe {
         ocall_sgx_init_quote(&mut rt as *mut sgx_status_t,
-                             &mut ti as *mut sgx_target_info_t,
-                             &mut eg as *mut sgx_epid_group_id_t)
+                             &mut target_info as *mut sgx_target_info_t,
+                             &mut epid_group_id as *mut sgx_epid_group_id_t)
     };
 
-    println!("eg = {:?}", eg);
+    println!("epid_group_id = {:?}", epid_group_id);
 
-    if res != sgx_status_t::SGX_SUCCESS {
-        return Err(res);
+    if result != sgx_status_t::SGX_SUCCESS {
+        return Err(result);
     }
 
     if rt != sgx_status_t::SGX_SUCCESS {
         return Err(rt);
     }
 
-    // (2) Generate the report
-    // Fill ecc256 public key into report_data
+    return Ok((target_info, epid_group_id))
+}
+
+fn new_report(public_key: &sgx_ec256_public_t) -> sgx_report_data_t {
     let mut report_data: sgx_report_data_t = sgx_report_data_t::default();
-    let mut pub_k_gx = pub_k.gx.clone();
+
+    // Fill ecc256 public key into report_data
+    let mut pub_k_gx = public_key.gx.clone();
     pub_k_gx.reverse();
-    let mut pub_k_gy = pub_k.gy.clone();
+    let mut pub_k_gy = public_key.gy.clone();
     pub_k_gy.reverse();
     report_data.d[..32].clone_from_slice(&pub_k_gx);
     report_data.d[32..].clone_from_slice(&pub_k_gy);
 
-    let rep = match tse::rsgx_create_report(&ti, &report_data) {
-        Ok(r) =>{
-            println!("Report creation => success {:?}", r.body.mr_signer.m);
-            Some(r)
-        },
-        Err(e) =>{
-            println!("Report creation => failed {:?}", e);
-            None
-        },
+    report_data
+}
+
+fn keypair() -> Result<(sgx_ec256_private_t, sgx_ec256_public_t), sgx_status_t> {
+    let ecc_handle = SgxEccHandle::new();
+    let _result = ecc_handle.open();
+    ecc_handle.create_key_pair()
+}
+
+fn quote_nonce() -> io::Result<sgx_quote_nonce_t> {
+    let mut quote_nonce = sgx_quote_nonce_t { rand : [0;16] };
+    let mut os_rng = SgxRng::new()?;
+    os_rng.fill_bytes(&mut quote_nonce.rand);
+    Ok(quote_nonce)
+}
+
+pub fn report() -> Result<sgx_report_t, sgx_status_t> {
+    let (_, public_key) = keypair()?;
+    let report_data = new_report(&public_key);
+    let (target_info, _) = quote()?;
+    let report = tse::rsgx_create_report(&target_info, &report_data)?;
+
+    let quote_nonce = match quote_nonce() {
+        Ok(n) => n,
+        Err(_) => return Err(sgx_status_t::SGX_ERROR_UNEXPECTED)
     };
 
-    let mut quote_nonce = sgx_quote_nonce_t { rand : [0;16] };
-    let mut os_rng = SgxRng::new().unwrap();
-    os_rng.fill_bytes(&mut quote_nonce.rand);
-    println!("rand finished");
     let mut qe_report = sgx_report_t::default();
     const RET_QUOTE_BUF_LEN : u32 = 2048;
     let mut return_quote_buf : [u8; RET_QUOTE_BUF_LEN as usize] = [0;RET_QUOTE_BUF_LEN as usize];
     let mut quote_len : u32 = 0;
 
-    // (3) Generate the quote
-    // Args:
-    //       1. sigrl: ptr + len
-    //       2. report: ptr 432bytes
-    //       3. linkable: u32, unlinkable=0, linkable=1
-    //       4. spid: sgx_spid_t ptr 16bytes
-    //       5. sgx_quote_nonce_t ptr 16bytes
-    //       6. p_sig_rl + sigrl size ( same to sigrl)
-    //       7. [out]p_qe_report need further check
-    //       8. [out]p_quote
-    //       9. quote_size
-    let p_report = (&rep.unwrap()) as * const sgx_report_t;
+    let p_report = &report as * const sgx_report_t;
     let quote_type = sgx_quote_sign_type_t::SGX_LINKABLE_SIGNATURE;
+    let spid : sgx_spid_t = sgx_spid_t::default();
 
-    println!("loading SPID");
-    let spid : sgx_spid_t = load_spid("spid.txt");
 
     let p_spid = &spid as *const sgx_spid_t;
     let p_nonce = &quote_nonce as * const sgx_quote_nonce_t;
@@ -111,13 +104,14 @@ pub fn report() -> Result<sgx_report_t, sgx_status_t> {
     let maxlen = RET_QUOTE_BUF_LEN;
     let p_quote_len = &mut quote_len as *mut u32;
 
+    let mut rt : sgx_status_t = sgx_status_t::SGX_ERROR_UNEXPECTED;
     let result = unsafe {
         ocall_get_quote(&mut rt as *mut sgx_status_t,
                 ptr::null(),
                 0,
                 p_report,
                 quote_type,
-                ptr::null(), // p_spid,
+                p_spid,
                 p_nonce,
                 p_qe_report,
                 p_quote,
@@ -144,9 +138,9 @@ pub fn report() -> Result<sgx_report_t, sgx_status_t> {
     }
 
     // Check if the qe_report is produced on the same platform
-    if ti.mr_enclave.m != qe_report.body.mr_enclave.m ||
-       ti.attributes.flags != qe_report.body.attributes.flags ||
-       ti.attributes.xfrm  != qe_report.body.attributes.xfrm {
+    if target_info.mr_enclave.m != qe_report.body.mr_enclave.m ||
+       target_info.attributes.flags != qe_report.body.attributes.flags ||
+       target_info.attributes.xfrm  != qe_report.body.attributes.xfrm {
         println!("qe_report does not match current target_info!");
         return Err(sgx_status_t::SGX_ERROR_UNEXPECTED);
     }
@@ -154,10 +148,10 @@ pub fn report() -> Result<sgx_report_t, sgx_status_t> {
     println!("qe_report check passed");
 
     // Debug
-    // for i in 0..quote_len {
-    //     print!("{:02X}", unsafe {*p_quote.offset(i as isize)});
-    // }
-    // println!("");
+    for i in 0..quote_len {
+        print!("{:02X}", unsafe {*p_quote.offset(i as isize)});
+    }
+    println!("");
 
     // Check qe_report to defend against replay attack
     // The purpose of p_qe_report is for the ISV enclave to confirm the QUOTE
@@ -186,12 +180,4 @@ pub fn report() -> Result<sgx_report_t, sgx_status_t> {
     //let (attn_report, sig, cert) = get_report_from_intel(ias_sock, quote_vec);
     // Ok((attn_report, sig, cert))
     Err(sgx_status_t::SGX_ERROR_UNEXPECTED)
-}
-
-fn load_spid(filename: &str) -> sgx_spid_t {
-    let mut spidfile = fs::File::open(filename).expect("cannot open spid file");
-    let mut contents = String::new();
-    spidfile.read_to_string(&mut contents).expect("cannot read the spid file");
-
-    hex::decode_spid(&contents)
 }
