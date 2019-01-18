aa2fc6b13c264bf886bcaa0638e574a5acbcc5bd
John Barker <jebarker@gmail.com>
2019-01-18 12:27:42 -0700

    Assert on newly returned report

--- services/attestation/attestation_test.go
+++ services/attestation/attestation_test.go
@@ -3,13 +3,28 @@
 package attestation
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type AttestationReport struct {
+	Report struct {
+		KeyID []int `json:"key_id"`
+		Mac   []int `json:"mac"`
+	} `json:"report"`
+}
+
 func TestReport(t *testing.T) {
 	result, err := Report()
 	assert.NoError(t, err)
-	assert.Equal(t, `{"report":{"key_id":[255,0,255,0,255,0,255,0,255,0,255,0,255,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"mac":[167,99,107,85,185,122,149,93,161,30,126,255,36,101,174,248]}}`, result)
+
+	var report AttestationReport
+	err = json.Unmarshal([]byte(result), &report)
+	assert.NoError(t, err)
+
+	// Report now contains a nonce so we can only assert on its structure
+	assert.Len(t, report.Report.KeyID, 32)
+	assert.Len(t, report.Report.Mac, 16)
 }
