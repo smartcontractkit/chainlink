3300f6d762ef66c0b611c79f5a97382e157fd1d6
Alex Coventry <coventry@gmail.com>
2019-01-18 15:53:52 -0500

    Better APIs for golang VRF code

--- services/vrf/vrf.go
+++ services/vrf/vrf.go
@@ -12,12 +12,14 @@ package vrf
 // Usage
 // -----
 //
-// A secret key sk should be securely sampled uniformly from {0, ..., Order}.
+// A secret key sk should be securely sampled uniformly from {0, ..., Order-1}.
 // The public key associated with it can be calculated from it by
-// bn256.ScalarMult(Generator,sk).
+// bn256.ScalarMult(Generator,sk). A convenience function for this is provided
+// by GenerateKeyPair().
 //
-// To generate random output from a big.Int seed, pass sk and the seed to
-// GenerateProof, and use the Output field of the returned Proof object.
+// To generate a random output, pass the keypair from GenerateKeyPair and a
+// *big.Int seed to GenerateProof, and use the Output field of the returned
+// Proof object.
 //
 // To verify a Proof object p, run p.Verify(), or pass its fields to the
 // corresponding arguments of isValidVRFOutput on the VRF solidity contract, to
@@ -25,6 +27,8 @@ package vrf
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
 	"math/big"
@@ -35,14 +39,43 @@ import (
 	"github.com/smartcontractkit/chainlink/utils"
 )
 
-func bigFromBase10(s string) *big.Int {
-	n, _ := new(big.Int).SetString(s, 10)
-	return n
+// Int compensates for awkward big.Int API. This package is fast; we don't need
+// to bash things in-place.
+type integer struct{ i *big.Int }
+
+func bi(i uint) integer { return integer{big.NewInt(int64(i))} }
+
+var zero, one, two, three, four = bi(0), bi(1), bi(2), bi(3), bi(4)
+
+func i() *big.Int                              { return new(big.Int) }
+func (x integer) add(s integer) integer        { return integer{i().Add(x.i, s.i)} }
+func (x integer) div(divisor integer) integer  { return integer{i().Div(x.i, divisor.i)} }
+func (x integer) equal(y integer) bool         { return x.i.Cmp(y.i) == 0 }
+func (x integer) lessThan(y integer) bool      { return x.i.Cmp(y.i) == -1 }
+func (x integer) greaterThan(y integer) bool   { return x.i.Cmp(y.i) == 1 }
+func (x integer) exp(exponent integer) integer { return integer{i().Exp(x.i, exponent.i, P.i)} }
+func (x integer) lsh(bits uint) integer        { return integer{i().Lsh(x.i, bits)} }
+func (x integer) rsh(bits uint) integer        { return integer{i().Rsh(x.i, bits)} }
+func (x integer) mul(y integer) integer        { return integer{i().Mul(x.i, y.i)} }
+func (x integer) mod(y integer) integer        { return integer{i().Mod(x.i, y.i)} }
+func (x integer) sub(y integer) integer        { return integer{i().Sub(x.i, y.i)} }
+func (x integer) modSqrt() integer             { return integer{i().ModSqrt(x.i, P.i)} }
+func (x integer) and(mask integer) integer     { return integer{i().And(x.i, mask.i)} }
+func (x integer) bitLen() uint                 { return uint(x.i.BitLen()) }
+func (x integer) copy(y integer)               { x.i.Set(y.i) }
+
+func integerFromBytes(b []byte) integer { return integer{i().SetBytes(b)} }
+
+func bigFromBase10(s string) integer {
+	n, ok := new(big.Int).SetString(s, 10)
+	if !ok {
+		panic("failed to allocate big.Int")
+	}
+	return integer{n}
 }
 
 // The following variables are not exported, despite being capitalized, from
 // github.com/ethereum/go-ethereum/crypto/bn256/cloudfare/constants.go
-
 // These values don't match the published BN256 values, because the bn256
 // package does not implement the BN256 curve, but the Alt-BN128 curve.
 
@@ -54,54 +87,28 @@ var P = bigFromBase10(
 var Order = bigFromBase10(
 	"21888242871839275222246405745257275088548364400416034343698204186575808495617")
 
-// Compensate for awkward big.Int API.
-var bi = big.NewInt
-var zero, one, two, three, four = bi(0), bi(1), bi(2), bi(3), bi(4)
-
-func i() *big.Int                                    { return new(big.Int) }
-func add(addend1, addend2 *big.Int) *big.Int         { return i().Add(addend1, addend2) }
-func div(dividend, divisor *big.Int) *big.Int        { return i().Div(dividend, divisor) }
-func equal(left, right *big.Int) bool                { return left.Cmp(right) == 0 }
-func exp(base, exponent, modulus *big.Int) *big.Int  { return i().Exp(base, exponent, modulus) }
-func lsh(num *big.Int, bits uint) *big.Int           { return i().Lsh(num, bits) }
-func mul(multiplicand, multiplier *big.Int) *big.Int { return i().Mul(multiplicand, multiplier) }
-func mod(dividend, divisor *big.Int) *big.Int        { return i().Mod(dividend, divisor) }
-func sub(minuend, subtrahend *big.Int) *big.Int      { return i().Sub(minuend, subtrahend) }
-
-// Sadly, the hidden interfaces in bn256 require us to duplicate some
-// functionality.
-
-var (
-	// (P-1)/2: Half Fermat's Little Theorem exponent
-	eulersCriterionPower = div(sub(P, one), two)
-	// (P+1)/4: As long as P%4==3 and n=x^2 in GF(P), n^((P+1)/4)=±x
-	sqrtPower = div(add(P, one), four)
-)
+// (P-1)/2: Half Fermat's Little Theorem exponent
+var eulersCriterionPower = P.sub(one).rsh(1)
 
 // isSquare returns true iff x = y^2 for some y in GF(p)
-func isSquare(x *big.Int) bool {
-	return equal(one, exp(x, eulersCriterionPower, P))
-}
-
-// squareRoot returns a s.t. a^2=x. Assumes x is a square
-func squareRoot(x *big.Int) *big.Int {
-	return exp(x, sqrtPower, P)
+func isSquare(x integer) bool {
+	return x.exp(eulersCriterionPower).equal(one)
 }
 
 // ySquared returns x^3+3 mod P
-func ySquared(x *big.Int) *big.Int {
-	return mod(add(exp(x, three, P), three), P)
+func ySquared(x integer) integer {
+	return x.exp(three).add(three).mod(P)
 }
 
 // IsCurveXOrdinate returns true iff there is y s.t. y^2=x^3+3
-func isCurveXOrdinate(x *big.Int) bool {
+func isCurveXOrdinate(x integer) bool {
 	return isSquare(ySquared(x))
 }
 
-func packUint256s(xs ...*big.Int) ([]byte, error) {
+func packUint256s(xs ...integer) ([]byte, error) {
 	mem := bytes.Buffer{}
 	for _, x := range xs {
-		word, err := utils.EVMWordBigInt(x)
+		word, err := utils.EVMWordBigInt(x.i)
 		if err != nil {
 			return []byte{}, err
 		}
@@ -122,7 +129,7 @@ func packUint256s(xs ...*big.Int) ([]byte, error) {
 }
 
 // newCurvePoint returns the bn256.G1 point corresponding to (x, y)
-func newCurvePoint(x, y *big.Int) (*bn256.G1, error) {
+func newCurvePoint(x, y integer) (*bn256.G1, error) {
 	p := new(bn256.G1)
 	packed, err := packUint256s(x, y)
 	if err != nil {
@@ -155,58 +162,58 @@ func CoordsFromPoint(p *bn256.G1) (*big.Int, *big.Int) {
 
 // hashUint256s returns a uint256 representing the hash of the concatenated byte
 // representations of the inputs
-func hashUint256s(xs ...*big.Int) (*big.Int, error) {
+func hashUint256s(xs ...integer) (integer, error) {
 	packed, err := packUint256s(xs...)
 	if err != nil {
-		return &big.Int{}, err
+		return zero, err
 	}
 	hash, err := utils.Keccak256(packed)
 	if err != nil {
-		return &big.Int{}, err
+		return zero, err
 	}
-	return i().SetBytes(hash), nil
+	return integerFromBytes(hash), nil
 }
 
 // maskHash returns hashUint256s(xs...) & mask
-func maskHash(mask *big.Int, xs ...*big.Int) (*big.Int, error) {
+func maskHash(mask integer, xs ...integer) (integer, error) {
 	x, err := hashUint256s(xs...)
 	if err != nil {
-		return &big.Int{}, err
+		return zero, err
 	}
-	return x.And(x, mask), nil
+	return x.and(mask), nil
 }
 
 // zqHash hashes xs uniformly into {0, ..., q-1}
-func zqHash(q *big.Int, xs ...*big.Int) (*big.Int, error) {
+func zqHash(q integer, xs ...integer) (integer, error) {
 	if len(xs) < 1 {
 		panic("can't take hash of empty list. You might have forgotten argument q")
 	}
-	if q.BitLen() > 256 {
+	if q.bitLen() > 256 {
 		panic(fmt.Errorf(
 			"will only generate 256 bits of entropy, need %v",
-			q.BitLen()))
+			q.bitLen()))
 	}
 	// Bits which can be used in representation of a number less than q.
 	// 2^(q.BitLen)-1
-	orderMask := sub(lsh(one, uint(q.BitLen())), one)
+	orderMask := one.lsh(uint(q.bitLen())).sub(one)
 	rv, err := maskHash(orderMask, xs...)
 	if err != nil {
-		return &big.Int{}, err
+		return zero, err
 	}
 	// Hash recursively until rv < q. P(success per iteration) >= 0.5, so
 	// number of extra hashes is geometrically distributed, with mean < 1.
-	for rv.Cmp(q) != -1 {
+	for rv.greaterThan(q) {
 		nrv, err := maskHash(orderMask, rv)
 		if err != nil {
-			return &big.Int{}, err
+			return zero, err
 		}
-		rv.Set(nrv)
+		rv.copy(nrv)
 	}
 	return rv, nil
 }
 
 // hashToCurve is a one-way hash function onto the curve
-func hashToCurve(px, py, input *big.Int) (*bn256.G1, error) {
+func hashToCurve(px, py, input integer) (*bn256.G1, error) {
 	x, err := zqHash(P, px, py, input)
 	if err != nil {
 		return &bn256.G1{}, err
@@ -216,31 +223,163 @@ func hashToCurve(px, py, input *big.Int) (*bn256.G1, error) {
 		if err != nil {
 			return &bn256.G1{}, err
 		}
-		x.Set(nx)
+		x.copy(nx)
 	}
-	return newCurvePoint(x, squareRoot(ySquared(x)))
+	return newCurvePoint(x, ySquared(x).modSqrt())
 }
 
 // scalarFromCurve returns a hash for the curve points. Corresponds to the hash
 // computed in Curve.sol#scalarFromCurve
-func scalarFromCurve(ps ...*bn256.G1) (*big.Int, error) {
-	coordinates := make([]*big.Int, (len(ps)+1)*2)
+func scalarFromCurve(ps ...*bn256.G1) (integer, error) {
+	coordinates := make([]integer, (len(ps)+1)*2)
 	gx, gy := CoordsFromPoint(Generator)
-	coordinates[0] = gx
-	coordinates[1] = gy
+	coordinates[0] = integer{gx}
+	coordinates[1] = integer{gy}
 	for ordidx, p := range ps {
 		x, y := CoordsFromPoint(p)
-		coordinates[2*ordidx+2] = x
-		coordinates[2*ordidx+3] = y
+		coordinates[2*ordidx+2] = integer{x}
+		coordinates[2*ordidx+3] = integer{y}
 	}
 	return zqHash(Order, coordinates...)
 }
 
 // linearComination returns c*p1+s*p2
-func linearComination(c *big.Int, p1 *bn256.G1, s *big.Int, p2 *bn256.G1) *bn256.G1 {
+func linearComination(c integer, p1 *bn256.G1, s integer, p2 *bn256.G1) *bn256.G1 {
 	return new(bn256.G1).Add(
-		new(bn256.G1).ScalarMult(p1, c),
-		new(bn256.G1).ScalarMult(p2, s))
+		new(bn256.G1).ScalarMult(p1, c.i),
+		new(bn256.G1).ScalarMult(p2, s.i))
+}
+
+// KeyPair represents a public/private keypair
+type KeyPair struct {
+	Public *bn256.G1
+	secret *big.Int
+}
+
+// GenerateKeyPair returns a public/private keypair generated from
+// cryptographically strong pseudorandomness, or an error on failure.
+func GenerateKeyPair() (*KeyPair, error) {
+	rv := new(KeyPair)
+	var err error
+	rv.secret, err = rand.Int(rand.Reader, Order.sub(one).i)
+	if err != nil {
+		return rv, err
+	}
+	rv.Public = new(bn256.G1).ScalarMult(Generator, rv.secret)
+	return rv, nil
+}
+
+// Marshal should not be used on a KeyPair
+func (keypair *KeyPair) Marshal(out []byte) {
+	panic("don't use this, use MarshalEncryptedKeyPair instead")
+}
+
+// Unmarshal should not be used on a KeyPair
+func (keypair *KeyPair) Unmarshal(in []byte) {
+	panic("don't use this, use UnmarshalEncryptedKeyPair or UnmarshalPublicKey instead")
+}
+
+// dummyCipher is a blank cipher to be returned on an error condition
+type dummyCipher struct{}
+
+func (*dummyCipher) BlockSize() int          { panic("never use this function") }
+func (*dummyCipher) Encrypt(dst, src []byte) { panic("never use this function") }
+func (*dummyCipher) Decrypt(dst, src []byte) { panic("never use this function") }
+
+func createKeyCipher(salt, passPhrase []byte) (cipher.Block, error) {
+	// XXX: Append is over-writing the encrypted key!!! Take a copy, or
+	// send one in.
+	key, err := utils.Keccak256(append(salt, passPhrase...))
+	if err != nil {
+		return &dummyCipher{}, nil
+	}
+	cipherRV, err := aes.NewCipher(key)
+	if err != nil {
+		return &dummyCipher{}, nil
+	}
+	return cipherRV, nil
+}
+
+// MarshalEncryptedKeyPair marshals keypair with Secret encrypted by passPhrase,
+// or returns an error on failure.
+//
+// The format is (Public.x || Public.y || encrypted secret), where all fields
+// are 256 bits, || is concatenation, and Public.{x,y} are big-endian affine
+// coordinates of KeyPair.Public.
+//
+// The encryption key is keccak256(Public.x || Public.y || passPhrase), and the
+// plaintext of the encrypted secret is KeyPair.secret as big-endian byte array.
+func (keypair *KeyPair) MarshalEncryptedKeyPair(passPhrase []byte) ([]byte, error) {
+	if (integer{keypair.secret}).lessThan(zero) ||
+		(integer{keypair.secret}).greaterThan(Order.sub(one)) {
+		return []byte{}, fmt.Errorf(
+			"Secret key %v is not in {1,...Order-1}", keypair.secret)
+	}
+	rv := keypair.Public.Marshal()                 // Marshal public key in plaintext
+	cipher, err := createKeyCipher(rv, passPhrase) // Salt with encryption key with public key.
+	if err != nil {
+		return []byte{}, err
+	}
+	cipherText := make([]byte, 32)
+	byteDeficit := (256 - keypair.secret.BitLen()) / 8
+	// Bytes returns big-endian (higher-order bytes first), so pre-pad with
+	// 0's to bring input length up to 256 bits/32 bytes.
+	prefix := make([]byte, byteDeficit)
+	for i := range prefix {
+		prefix[i] = 0
+	}
+	secretInput := append(prefix, keypair.secret.Bytes()...)
+	if (len(secretInput) != 32) ||
+		!integerFromBytes(secretInput).equal(integer{keypair.secret}) {
+		panic("failed to encode secret with correct length")
+	}
+	cipher.Encrypt(cipherText[:16], secretInput[:16])
+	testOutput := make([]byte, 16)
+	cipher.Decrypt(testOutput, cipherText[:16])
+	cipher.Encrypt(cipherText[16:], secretInput[16:])
+	result := append(rv, cipherText...)
+	return result, nil
+}
+
+// UnmarshalEncryptedKeyPair returns a KeyPair after decrypting the secret key with passPhrase
+func UnmarshalEncryptedKeyPair(encKeyPair, passPhrase []byte) (*KeyPair, error) {
+	if len(encKeyPair) != 96 {
+		return &KeyPair{}, fmt.Errorf("serialized key pair should be 3 uint256's")
+	}
+	rv := new(KeyPair)
+	publicKey, err := UnmarshalPublicKey(encKeyPair)
+	encSecretKey := make([]byte, 32) // Copy because createKeyCipher
+	copy(encSecretKey, encKeyPair[64:])
+	if err != nil {
+		return &KeyPair{}, err
+	}
+	rv.Public = publicKey
+	marshaledPublicKeyAsSalt := encKeyPair[:len(encKeyPair)-len(encSecretKey)]
+	cipher, err := createKeyCipher(marshaledPublicKeyAsSalt, passPhrase)
+	if err != nil {
+		return &KeyPair{}, err
+	}
+	secretOutput := make([]byte, 32)
+	cipher.Decrypt(secretOutput[:16], encSecretKey[:16])
+	cipher.Decrypt(secretOutput[16:], encSecretKey[16:])
+	rv.secret = integerFromBytes(secretOutput).i
+
+	return rv, nil
+}
+
+// UnmarshalPublicKey returns the unencrypted public key of a marshaled KeyPair
+func UnmarshalPublicKey(encKeyPair []byte) (*bn256.G1, error) {
+	rv := new(bn256.G1)
+	_, err := rv.Unmarshal(encKeyPair)
+	if err != nil {
+		return &bn256.G1{}, err
+	}
+	return rv, nil
+}
+
+// String generates a human-readable representation of the KeyPair, blinding the secret key
+func String(k *KeyPair) string {
+	return fmt.Sprintf("KeyPair{Public:%v, secret:<redacted>}", k.Public)
 }
 
 // Proof represents a proof that Gamma was constructed from the Seed
@@ -254,15 +393,15 @@ type Proof struct {
 // given publicKey and seed
 func (proof *Proof) VerifyProof() (bool, error) {
 	px, py := CoordsFromPoint(proof.PublicKey)
-	h, err := hashToCurve(px, py, proof.Seed)
+	h, err := hashToCurve(integer{px}, integer{py}, integer{proof.Seed})
 	if err != nil {
 		return false, err
 	}
 	// publicKey = secretKey*Generator. See GenerateProof for u, v, m, s
 	// c*secretKey*Generator + (m - c*secretKey)*Generator = m*Generator = u
-	uPrime := linearComination(proof.C, proof.PublicKey, proof.S, Generator)
+	uPrime := linearComination(integer{proof.C}, proof.PublicKey, integer{proof.S}, Generator)
 	// c*secretKey*h + (m - c*secretKey)*h = m*h = v
-	vPrime := linearComination(proof.C, proof.Gamma, proof.S, h)
+	vPrime := linearComination(integer{proof.C}, proof.Gamma, integer{proof.S}, h)
 	cPrime, _ := scalarFromCurve(
 		h, proof.PublicKey, proof.Gamma, uPrime, vPrime)
 	if err != nil {
@@ -272,45 +411,45 @@ func (proof *Proof) VerifyProof() (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	return (proof.C.Cmp(cPrime) == 0) &&
-			(proof.Output.Cmp(i().SetBytes(output)) == 0),
+	return integer{proof.C}.equal(cPrime) &&
+			integer{proof.Output}.equal(integerFromBytes(output)),
 		nil
 }
 
 // makeProof proof generates the actual proof, modulo the actual random output
-func makeProof(secretKey, seed *big.Int) (*Proof, error) {
-	publicKey := new(bn256.G1).ScalarMult(Generator, secretKey)
-	px, py := CoordsFromPoint(publicKey)
-	h, err := hashToCurve(px, py, seed)
+func makeProof(keypair *KeyPair, seed integer) (*Proof, error) {
+	px, py := CoordsFromPoint(keypair.Public)
+	h, err := hashToCurve(integer{px}, integer{py}, seed)
 	if err != nil {
 		return &Proof{}, err
 	}
-	gamma := new(bn256.G1).ScalarMult(h, secretKey)
-	m, err := rand.Int(rand.Reader, Order)
+	gamma := new(bn256.G1).ScalarMult(h, keypair.secret)
+	baseM, err := rand.Int(rand.Reader, Order.i)
 	if err != nil {
 		return &Proof{}, err
 	}
-	u := new(bn256.G1).ScalarMult(Generator, m)
-	v := new(bn256.G1).ScalarMult(h, m)
-	c, err := scalarFromCurve(h, publicKey, gamma, u, v)
+	m := integer{baseM}
+	u := new(bn256.G1).ScalarMult(Generator, m.i)
+	v := new(bn256.G1).ScalarMult(h, m.i)
+	c, err := scalarFromCurve(h, keypair.Public, gamma, u, v)
 	if err != nil {
 		return &Proof{}, err
 	}
-	// s = (m - c*secretKey) % Order
-	s := mod(sub(m, mul(c, secretKey)), Order)
+	// s = (m - c*secretKey1) % Order
+	s := m.sub(c.mul(integer{keypair.secret})).mod(Order)
 	return &Proof{
-		PublicKey: publicKey,
+		PublicKey: keypair.Public,
 		Gamma:     gamma,
-		C:         c,
-		S:         s,
-		Seed:      seed,
+		C:         c.i,
+		S:         s.i,
+		Seed:      seed.i,
 	}, nil
 }
 
 // GenerateProof returns gamma, plus proof that gamma was constructed from seed
 // as mandated from the given secretKey, with public key secretKey*Generator
-func GenerateProof(secretKey, seed *big.Int) (*Proof, error) {
-	proof, err := makeProof(secretKey, seed)
+func GenerateProof(keypair *KeyPair, seed *big.Int) (*Proof, error) {
+	proof, err := makeProof(keypair, integer{seed})
 	if err != nil {
 		return &Proof{}, err
 	}
--- services/vrf/vrf_test.go
+++ services/vrf/vrf_test.go
@@ -3,20 +3,24 @@ package vrf
 import (
 	"fmt"
 	"math"
-	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto/bn256"
 	"github.com/stretchr/testify/assert"
 )
 
+var insecureKeyPair = KeyPair{
+	Public: new(bn256.G1).Add(Generator, Generator),
+	secret: two.i,
+}
+
 func TestVRF_isSquare(t *testing.T) {
 	assert.True(t, isSquare(four))
-	assert.False(t, isSquare(sub(P, one)), "P-1 is not square in GF(P)")
+	assert.False(t, isSquare(P.sub(one)), "P-1 is not square in GF(P)")
 }
 
 func TestVRF_squareRoot(t *testing.T) {
-	assert.Equal(t, two, squareRoot(four), "4^{(P-1)/4} = 2 in GF(P)")
+	assert.Equal(t, two, four.modSqrt(), "4^{(P-1)/4} = 2 in GF(P)")
 }
 
 func TestVRF_ySquared(t *testing.T) {
@@ -31,29 +35,29 @@ func TestVRF_isCurveXOrdinate(t *testing.T) {
 
 func TestVRF_CoordsFromPoint(t *testing.T) {
 	x, y := CoordsFromPoint(Generator)
-	assert.Equal(t, x, one, "Wrong x ordinate from Generator")
-	assert.Equal(t, y, two, "Wrong y ordinate from Generator")
+	assert.Equal(t, x, one.i, "Wrong x ordinate from Generator")
+	assert.Equal(t, y, two.i, "Wrong y ordinate from Generator")
 }
 
-func bigFromHex(s string) *big.Int {
+func bigFromHex(s string) integer {
 	n, ok := i().SetString(s, 16)
 	if !ok {
 		panic(fmt.Errorf(`failed to convert "%s" as hex to big.Int`, s))
 	}
-	return n
+	return integer{n}
 }
 
 func TestVRF_zqHash(t *testing.T) {
 	log2Mod := 5.0
-	modulus := bi(int64(math.Pow(2, log2Mod)))
-	bitMask := bi(int64(math.Pow(2, log2Mod+1) - 1))
+	modulus := bi(uint(math.Pow(2, log2Mod)))
+	bitMask := bi(uint(math.Pow(2, log2Mod+1) - 1))
 	reHashTriggeringSeed := zero
 	hash, err := hashUint256s(reHashTriggeringSeed)
 	if err != nil {
 		panic(err)
 	}
-	hash.And(hash, bitMask)
-	assert.Equal(t, 1, hash.Cmp(modulus),
+	hash = hash.and(bitMask)
+	assert.True(t, hash.greaterThan(modulus),
 		`need an example which hashes to something bigger than the
 modulus, to test the rehash logic.`)
 	zqH, err := zqHash(modulus, reHashTriggeringSeed)
@@ -64,7 +68,7 @@ modulus, to test the rehash logic.`)
 }
 
 func TestVRF_hashToCurve(t *testing.T) {
-	reHashTriggeringInput := []*big.Int{one, two, bi(5)}
+	reHashTriggeringInput := []integer{one, two, bi(5)}
 	x, err := zqHash(P, reHashTriggeringInput...)
 	if err != nil {
 		panic(err)
@@ -77,12 +81,12 @@ point on the curve, to exercise rehash logic.`)
 	if err != nil {
 		panic(err)
 	}
-	x, y := CoordsFromPoint(p)
+	px, py := CoordsFromPoint(p)
 	eX := "247154f2ce523897365341b03669e1061049e801e8750ae708e1cb02f36cb225"
 	eY := "16e1157d5b94324127e094abe222a05a5c47be3124254a6aa047d5e1f2d864ea"
-	assert.Equal(t, bigFromHex(eX), x,
+	assert.Equal(t, bigFromHex(eX), integer{px},
 		"x ordinate of hashToCurve case tested in VRF_test.js has changed")
-	assert.Equal(t, bigFromHex(eY), y,
+	assert.Equal(t, bigFromHex(eY), integer{py},
 		"y ordinate of hashToCurve case tested in VRF_test.js has changed")
 }
 
@@ -100,16 +104,35 @@ func TestVRF_scalarFromCurve(t *testing.T) {
 func pointsEqual(p1, p2 *bn256.G1) bool {
 	s1, _ := scalarFromCurve(p1)
 	s2, _ := scalarFromCurve(p2)
-	return s1.Cmp(s2) == 0
+	return s1.equal(s2)
+}
+
+func TestVRF_Marshalling(t *testing.T) {
+	assert.Panics(t, func() { insecureKeyPair.Marshal([]byte{}) }, "Marshal should be disabled")
+	assert.Panics(t, func() { insecureKeyPair.Unmarshal([]byte{}) }, "Unmarshal should be disabled")
+	passPhrase := []byte("sekret")
+	m, err := insecureKeyPair.MarshalEncryptedKeyPair(passPhrase)
+	if err != nil {
+		panic(err)
+	}
+	up, err := UnmarshalPublicKey(m)
+	if err != nil {
+		panic(err)
+	}
+	assert.True(t, pointsEqual(insecureKeyPair.Public, up),
+		"Unmarshalling should return correct public key.")
+	u, err := UnmarshalEncryptedKeyPair(m, passPhrase)
+	if err != nil {
+		panic(err)
+	}
+	assert.True(t, pointsEqual(insecureKeyPair.Public, u.Public) &&
+		(integer{u.secret}).equal((integer{insecureKeyPair.secret})),
+		"Unmarshalling should return the same key pair")
 }
 
 func TestVRF_GenerateProof(t *testing.T) {
-	insecureKeyPair := KeyPair{
-		Public: new(bn256.G1).Add(Generator, Generator),
-		secret: two,
-	}
 	seed := zero
-	proof, err := GenerateProof(&insecureKeyPair, seed)
+	proof, err := GenerateProof(&insecureKeyPair, seed.i)
 	if err != nil {
 		panic(err)
 	}
@@ -117,17 +140,13 @@ func TestVRF_GenerateProof(t *testing.T) {
 	gammaX, gammaY := CoordsFromPoint(proof.Gamma)
 	gX := "26feb384a4a3f28742d0e0e0f5458474ba54ef9816d4d31f3bf538dfcf67cf3f"
 	gY := "1eaed2431dd78ad75dd0c9f013cabff4f1d8c4c83cda79fff3855c988a3606d8"
-	assert.Equal(t, bigFromHex(gX), gammaX,
+	assert.Equal(t, bigFromHex(gX), integer{gammaX},
 		"x ordinate of gamma tested in VRF_test.js has changed.")
-	assert.Equal(t, bigFromHex(gY), gammaY,
+	assert.Equal(t, bigFromHex(gY), integer{gammaY},
 		"y ordinate of gamma tested in VRF_test.js has changed.")
 	verification, err := proof.VerifyProof()
 	if err != nil {
 		panic(err)
 	}
 	assert.True(t, verification, "Generated proof should verify.")
-}
-
-func TestBytes(t *testing.T) {
-	fmt.Println("bytes")
 }
