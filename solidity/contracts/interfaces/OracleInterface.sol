e7568310a599141a26f313f8eff7efb47b4c64f0
Thomas <me@thodges.com>
2019-01-25 14:49:00 -0800

    Update solidity/contracts/interfaces/OracleInterface.sol
    Co-Authored-By: se3000 <email@steveell.is>

--- solidity/contracts/interfaces/OracleInterface.sol
+++ solidity/contracts/interfaces/OracleInterface.sol
@@ -5,7 +5,7 @@ interface OracleInterface {
     uint256 requestId,
     uint256 payment,
     address callbackAddress,
-    bytes4 _callbackFunctionId,
+    bytes4 callbackFunctionId,
     uint256 expiration,
     bytes32 data
   ) external returns (bool);
