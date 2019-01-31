9b361ad19efdb18132d40bf5d2ea6fb850180a37
Dimitri Roche <dimroc@gmail.com>
2019-01-30 20:44:43 -0500

    Add file locking strategy to ensure single running node

--- store/orm/locking_strategies.go
+++ store/orm/locking_strategies.go
@@ -2,17 +2,28 @@ package orm
 
 import (
 	"fmt"
+	"net/url"
+	"path/filepath"
+	"sync"
 	"time"
+
+	"github.com/gofrs/flock"
 )
 
 // NewLockingStrategy returns the appropriate dialect for the
 // passed connection string.
 func NewLockingStrategy(dialect DialectName, path string) (LockingStrategy, error) {
+	uri, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	path = uri.Path
 	switch dialect {
 	case DialectPostgres:
 		return UnlockedLockingStrategy{}, nil
 	case DialectSqlite:
-		return UnlockedLockingStrategy{}, nil
+		directory := filepath.Dir(path)
+		return NewFileLockingStrategy(filepath.Join(directory, "chainlink.lock")), nil
 	}
 
 	return nil, fmt.Errorf("unable to create locking strategy for dialect %s and path %s", dialect, path)
@@ -22,7 +33,7 @@ func NewLockingStrategy(dialect DialectName, path string) (LockingStrategy, erro
 // resource, usually a file or db.
 type LockingStrategy interface {
 	Lock(timeout time.Duration) error
-	Unlock()
+	Unlock() error
 }
 
 // UnlockedLockingStrategy is a strategy that's always unlocked.
@@ -32,4 +43,48 @@ type UnlockedLockingStrategy struct{}
 func (s UnlockedLockingStrategy) Lock(timeout time.Duration) error { return nil }
 
 // Unlock is a noop.
-func (s UnlockedLockingStrategy) Unlock() {}
+func (s UnlockedLockingStrategy) Unlock() error { return nil }
+
+// FileLockingStrategy uses the write lock for a file on disk to ensure
+// exclusive operations.
+type FileLockingStrategy struct {
+	path     string
+	fileLock *flock.Flock
+	m        *sync.Mutex
+}
+
+// NewFileLockingStrategy creates a new instance of FileLockingStrategy
+// at the passed path.
+func NewFileLockingStrategy(path string) LockingStrategy {
+	return FileLockingStrategy{
+		path:     path,
+		fileLock: flock.New(path),
+		m:        &sync.Mutex{},
+	}
+}
+
+// Lock returns immediately and assumes is always unlocked.
+func (s FileLockingStrategy) Lock(timeout time.Duration) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	var err error
+	locked := make(chan struct{})
+	go func() {
+		err = s.fileLock.Lock()
+		close(locked)
+	}()
+	select {
+	case <-locked:
+	case <-time.After(timeout):
+		return fmt.Errorf("file locking strategy timed out for %s", s.path)
+	}
+	return err
+}
+
+// Unlock is a noop.
+func (s FileLockingStrategy) Unlock() error {
+	s.m.Lock()
+	defer s.m.Unlock()
+	return s.fileLock.Unlock()
+}
