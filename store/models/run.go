7275c118db36d7ed00f890c5215be9bb25615deb
Dimitri Roche <dimroc@gmail.com>
2019-02-05 12:22:28 -0500

    changes to TaskRun#TaskSpec are not automatically saved

--- store/models/run.go
+++ store/models/run.go
@@ -133,7 +133,7 @@ type TaskRun struct {
 	Result               RunResult `json:"result"`
 	ResultID             uint      `json:"-"`
 	Status               RunStatus `json:"status"`
-	TaskSpec             TaskSpec  `json:"task"`
+	TaskSpec             TaskSpec  `json:"task" gorm:"association_autoupdate:false;association_autocreate:false"`
 	TaskSpecID           uint      `json:"-" gorm:"index;not null REFERENCES task_specs(id)"`
 	MinimumConfirmations uint64    `json:"minimumConfirmations"`
 	CreatedAt            time.Time `json:"-" gorm:"index"`
