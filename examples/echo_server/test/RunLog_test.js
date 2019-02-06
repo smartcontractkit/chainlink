5b565315ea68dc0064876c757707b9bab32dcf55
Steve Ellis <email@steveell.is>
2019-02-06 11:57:33 -0500

    cut down dependencies of RunLog example's tests

--- examples/echo_server/test/RunLog_test.js
+++ examples/echo_server/test/RunLog_test.js
@@ -1,24 +1,21 @@
 import {resolve, join } from 'path'
-import {
-  deploy,
-  linkContract,
-  toWei
-} from '../../../solidity/test/support/helpers'
 
 contract('RunLog', () => {
-  const arbitraryJobID =
-        "0x0000000000000000000000000000000000000000000000000000000000000001"
-  let link, logger, oc;
+  const LinkToken = artifacts.require('LinkToken')
+  const Oracle = artifacts.require('Oracle')
+  const RunLog = artifacts.require('RunLog')
+  const arbitraryJobID = "0x0000000000000000000000000000000000000000000000000000000000000001"
+  let link, logger, oc
 
   beforeEach(async () => {
-    link = await linkContract()
-    oc = await deploy('Oracle.sol', link.address)
-    logger = await deploy('RunLog.sol', link.address, oc.address, arbitraryJobID)
-    await link.transfer(logger.address, toWei("1"));
+    link = await LinkToken.new()
+    oc = await Oracle.new(link.address)
+    logger = await RunLog.new(link.address, oc.address, arbitraryJobID)
+    await link.transfer(logger.address, web3.utils.toWei("1"));
   });
 
   it("has a limited public interface", async () => {
     let tx = await logger.request();
-    assert.equal(4, tx.receipt.logs.length);
+    assert.equal(4, tx.receipt.rawLogs.length);
   });
 });
