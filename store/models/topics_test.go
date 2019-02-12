5327f9d694a5ce2ad0a5688ee06241d7df12a97c
Steve Ellis <email@steveell.is>
2019-02-12 00:39:47 -0500

    document the differences in OracleRequest logs

--- store/models/topics_test.go
+++ store/models/topics_test.go
@@ -12,13 +12,13 @@ func TestTopicFiltersForRunLog(t *testing.T) {
 	t.Parallel()
 
 	jobID := "4a1eb0e8df314cb894024a38991cff0f"
-	topics, err := models.TopicFiltersForRunLog([]common.Hash{models.RunLogTopic0}, jobID)
+	topics, err := models.TopicFiltersForRunLog([]common.Hash{models.RunLogTopic0original}, jobID)
 
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(topics))
 	assert.Equal(
 		t,
-		[]common.Hash{models.RunLogTopic0},
+		[]common.Hash{models.RunLogTopic0original},
 		topics[models.RequestLogTopicSignature])
 
 	assert.Equal(
@@ -34,28 +34,32 @@ func TestTopicFiltersForRunLog_Error(t *testing.T) {
 	t.Parallel()
 
 	jobID := "Q!1eb0e8df314cb894024a38991cff0f"
-	topics, err := models.TopicFiltersForRunLog([]common.Hash{models.RunLogTopic0}, jobID)
+	topics, err := models.TopicFiltersForRunLog([]common.Hash{models.RunLogTopic0original}, jobID)
 
 	assert.Error(t, err)
 	assert.Equal(t, [][]common.Hash{}, topics)
 }
 
-func TestRunTopic0(t *testing.T) {
-	assert.Equal(t, "0x6d6db1f8fe19d95b1d0fa6a4bce7bb24fbf84597b35a33ff95521fac453c1529", models.RunLogTopic0.Hex())
+func TestRunLogTopic0original(t *testing.T) {
+	assert.Equal(t, "0x6d6db1f8fe19d95b1d0fa6a4bce7bb24fbf84597b35a33ff95521fac453c1529", models.RunLogTopic0original.Hex())
 }
 
-func TestRunTopic20190123(t *testing.T) {
-	assert.Equal(t, "0xe9cf09ba23a60c27cfb5ad84043dba257ed0ccea7f0095ff7054ec8088ce5871", models.RunLogTopic20190123.Hex())
+func TestRunLogTopic20190123withFulfillmentParams(t *testing.T) {
+	assert.Equal(t, "0xe9cf09ba23a60c27cfb5ad84043dba257ed0ccea7f0095ff7054ec8088ce5871", models.RunLogTopic20190123withFullfillmentParams.Hex())
 }
 
-func TestRunTopic20190207(t *testing.T) {
-	assert.Equal(t, "0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65", models.RunLogTopic20190207.Hex())
+func TestRunLogTopic20190207withoutIndexes(t *testing.T) {
+	assert.Equal(t, "0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65", models.RunLogTopic20190207withoutIndexes.Hex())
 }
 
-func TestOracleFulfillmentFunctionID0(t *testing.T) {
-	assert.Equal(t, "0x76005c26", models.OracleFulfillmentFunctionID0)
+func TestOracleFulfillmentFunctionID0original(t *testing.T) {
+	assert.Equal(t, "0x76005c26", models.OracleFullfillmentFunctionID0original)
 }
 
-func TestOracleFulfillmentFunctionID20190123(t *testing.T) {
-	assert.Equal(t, "0xeea57e70", models.OracleFulfillmentFunctionID20190123)
+func TestOracleFulfillmentFunctionID20190123withFulfillmentParams(t *testing.T) {
+	assert.Equal(t, "0xeea57e70", models.OracleFulfillmentFunctionID20190123withFulfillmentParams)
+}
+
+func TestOracleFulfillmentFunctionID20190128withoutCast(t *testing.T) {
+	assert.Equal(t, "0x4ab0d190", models.OracleFulfillmentFunctionID20190128withoutCast)
 }
