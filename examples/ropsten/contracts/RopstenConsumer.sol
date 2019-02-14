ee62289a77bfee8ddf1c09accf794c6e9247a179
Thomas Hodges <me@thodges.com>
2019-02-13 20:07:09 -0600

    Update Ropsten example contracts

--- examples/ropsten/contracts/RopstenConsumer.sol
+++ examples/ropsten/contracts/RopstenConsumer.sol
@@ -207,6 +207,10 @@ library CBOR {
 
 // File: ../solidity/contracts/Chainlink.sol
 
+/**
+ * @title Library for common Chainlink functions
+ * @dev Uses imported CBOR library for encoding to buffer
+ */
 library Chainlink {
   uint256 internal constant defaultBufferSize = 256;
 
@@ -220,6 +224,15 @@ library Chainlink {
     Buffer.buffer buf;
   }
 
+  /**
+   * @notice Initializes a Chainlink request
+   * @dev Sets the ID, callback address, and callback function signature on the request
+   * @param self The uninitialized request
+   * @param _id The Job Specification ID
+   * @param _callbackAddress The callback address
+   * @param _callbackFunction The callback function signature
+   * @return The initialized request
+   */
   function initialize(
     Request memory self,
     bytes32 _id,
@@ -233,13 +246,25 @@ library Chainlink {
     return self;
   }
 
-  function setBuffer(Request memory self, bytes data)
+  /**
+   * @notice Sets the data for the buffer without encoding CBOR on-chain
+   * @dev CBOR can be closed with curly-brackets {} or they can be left off
+   * @param self The initialized request
+   * @param _data The CBOR data
+   */
+  function setBuffer(Request memory self, bytes _data)
     internal pure
   {
-    Buffer.init(self.buf, data.length);
-    Buffer.append(self.buf, data);
+    Buffer.init(self.buf, _data.length);
+    Buffer.append(self.buf, _data);
   }
 
+  /**
+   * @notice Adds a string value to the request with a given key name
+   * @param self The initialized request
+   * @param _key The name of the key
+   * @param _value The string value to add
+   */
   function add(Request memory self, string _key, string _value)
     internal pure
   {
@@ -247,6 +272,12 @@ library Chainlink {
     self.buf.encodeString(_value);
   }
 
+  /**
+   * @notice Adds a bytes value to the request with a given key name
+   * @param self The initialized request
+   * @param _key The name of the key
+   * @param _value The bytes value to add
+   */
   function addBytes(Request memory self, string _key, bytes _value)
     internal pure
   {
@@ -254,6 +285,12 @@ library Chainlink {
     self.buf.encodeBytes(_value);
   }
 
+  /**
+   * @notice Adds a int256 value to the request with a given key name
+   * @param self The initialized request
+   * @param _key The name of the key
+   * @param _value The int256 value to add
+   */
   function addInt(Request memory self, string _key, int256 _value)
     internal pure
   {
@@ -261,6 +298,12 @@ library Chainlink {
     self.buf.encodeInt(_value);
   }
 
+  /**
+   * @notice Adds a uint256 value to the request with a given key name
+   * @param self The initialized request
+   * @param _key The name of the key
+   * @param _value The uint256 value to add
+   */
   function addUint(Request memory self, string _key, uint256 _value)
     internal pure
   {
@@ -268,6 +311,12 @@ library Chainlink {
     self.buf.encodeUInt(_value);
   }
 
+  /**
+   * @notice Adds an array of strings to the request with a given key name
+   * @param self The initialized request
+   * @param _key The name of the key
+   * @param _values The array of string values to add
+   */
   function addStringArray(Request memory self, string _key, string[] memory _values)
     internal pure
   {
@@ -406,6 +455,11 @@ library SafeMath {
 
 // File: ../solidity/contracts/Chainlinked.sol
 
+/**
+ * @title The Chainlinked contract
+ * @notice Contract writers can inherit this contract in order to create requests for the
+ * Chainlink network
+ */
 contract Chainlinked {
   using Chainlink for Chainlink.Request;
   using SafeMath for uint256;
@@ -428,6 +482,13 @@ contract Chainlinked {
   event ChainlinkFulfilled(bytes32 indexed id);
   event ChainlinkCancelled(bytes32 indexed id);
 
+  /**
+   * @notice Creates a request that can hold additional parameters
+   * @param _specId The Job Specification ID that the request will be created for
+   * @param _callbackAddress The callback address that the response will be sent to
+   * @param _callbackFunctionSignature The callback function signature to use for the callback address
+   * @return A Chainlink Request struct in memory
+   */
   function newRequest(
     bytes32 _specId,
     address _callbackAddress,
@@ -437,6 +498,13 @@ contract Chainlinked {
     return req.initialize(_specId, _callbackAddress, _callbackFunctionSignature);
   }
 
+  /**
+   * @notice Creates a Chainlink request to the stored oracle address
+   * @dev Calls `chainlinkRequestTo` with the stored oracle address
+   * @param _req The initialized Chainlink Request
+   * @param _payment The amount of LINK to send for the request
+   * @return The request ID
+   */
   function chainlinkRequest(Chainlink.Request memory _req, uint256 _payment)
     internal
     returns (bytes32)
@@ -444,6 +512,16 @@ contract Chainlinked {
     return chainlinkRequestTo(oracle, _req, _payment);
   }
 
+  /**
+   * @notice Creates a Chainlink request to the specified oracle address
+   * @dev Generates and stores a request ID, increments the local nonce, and uses `transferAndCall` to
+   * send LINK which creates a request on the target oracle contract.
+   * Emits ChainlinkRequested event.
+   * @param _oracle The address of the oracle for the request
+   * @param _req The initialized Chainlink Request
+   * @param _payment The amount of LINK to send for the request
+   * @return The request ID
+   */
   function chainlinkRequestTo(address _oracle, Chainlink.Request memory _req, uint256 _payment)
     internal
     returns (bytes32 requestId)
@@ -458,6 +536,16 @@ contract Chainlinked {
     return requestId;
   }
 
+  /**
+   * @notice Allows a request to be cancelled if it has not been fulfilled
+   * @dev Requires keeping track of the expiration value emitted from the oracle contract.
+   * Deletes the request from the `pendingRequests` mapping.
+   * Emits ChainlinkCancelled event.
+   * @param _requestId The request ID
+   * @param _payment The amount of LINK sent for the request
+   * @param _callbackFunc The callback function specified for the request
+   * @param _expiration The time of the expiration for the request
+   */
   function cancelChainlinkRequest(
     bytes32 _requestId,
     uint256 _payment,
@@ -472,14 +560,26 @@ contract Chainlinked {
     requested.cancelOracleRequest(_requestId, _payment, _callbackFunc, _expiration);
   }
 
+  /**
+   * @notice Sets the stored oracle address
+   * @param _oracle The address of the oracle contract
+   */
   function setOracle(address _oracle) internal {
     oracle = ChainlinkRequestInterface(_oracle);
   }
 
+  /**
+   * @notice Sets the LINK token address
+   * @param _link The address of the LINK token contract
+   */
   function setLinkToken(address _link) internal {
     link = LinkTokenInterface(_link);
   }
 
+  /**
+   * @notice Retrieves the stored address of the LINK token
+   * @return The address of the LINK token
+   */
   function chainlinkToken()
     internal
     view
@@ -488,6 +588,10 @@ contract Chainlinked {
     return address(link);
   }
 
+  /**
+   * @notice Retrieves the stored address of the oracle contract
+   * @return The address of the oracle contract
+   */
   function oracleAddress()
     internal
     view
@@ -496,13 +600,25 @@ contract Chainlinked {
     return address(oracle);
   }
 
+  /**
+   * @notice Allows for a request which was created on another contract to be fulfilled
+   * on this contract
+   * @param _oracle The address of the oracle contract that will fulfill the request
+   * @param _requestId The request ID used for the response
+   */
   function addExternalRequest(address _oracle, bytes32 _requestId)
     internal
-    isPendingRequest(_requestId)
+    notPendingRequest(_requestId)
   {
     pendingRequests[_requestId] = _oracle;
   }
-
+ 
+  /**
+   * @notice Sets the stored oracle and LINK token contracts with the addresses resolved by ENS
+   * @dev Accounts for subnodes having different resolvers
+   * @param _ens The address of the ENS contract
+   * @param _node The ENS node hash
+   */
   function setChainlinkWithENS(address _ens, bytes32 _node)
     internal
   {
@@ -514,6 +630,10 @@ contract Chainlinked {
     setOracleWithENS();
   }
 
+  /**
+   * @notice Sets the stored oracle contract with the address resolved by ENS
+   * @dev This may be called on its own as long as `setChainlinkWithENS` has been called previously
+   */
   function setOracleWithENS()
     internal
   {
@@ -522,6 +642,13 @@ contract Chainlinked {
     setOracle(resolver.addr(oracleSubnode));
   }
 
+  /**
+   * @notice Encodes the request to be sent to the oracle contract
+   * @dev The Chainlink node expects values to be in order for the request to be picked up. Order of types
+   * will be validated in the oracle contract.
+   * @param _req The initialized Chainlink Request
+   * @return The bytes payload for the `transferAndCall` method
+   */
   function encodeRequest(Chainlink.Request memory _req)
     internal
     view
@@ -539,11 +666,21 @@ contract Chainlinked {
       _req.buf.buf);
   }
 
+  /**
+   * @notice Ensures that the fulfillment is valid for this contract
+   * @dev Use if the contract developer prefers methods instead of modifiers for validation
+   * @param _requestId The request ID for fulfillment
+   */
   function fulfillChainlinkRequest(bytes32 _requestId)
     internal
     recordChainlinkFulfillment(_requestId)
   {}
 
+  /**
+   * @dev Reverts if the sender is not the oracle of the request.
+   * Emits ChainlinkFulfilled event.
+   * @param _requestId The request ID for fulfillment
+   */
   modifier recordChainlinkFulfillment(bytes32 _requestId) {
     require(msg.sender == pendingRequests[_requestId], "Source must be the oracle of the request");
     delete pendingRequests[_requestId];
@@ -551,8 +688,12 @@ contract Chainlinked {
     _;
   }
 
-  modifier isPendingRequest(bytes32 _requestId) {
-    require(pendingRequests[_requestId] == address(0), "Request is already fulfilled");
+  /**
+   * @dev Reverts if the request is already pending
+   * @param _requestId The request ID for fulfillment
+   */
+  modifier notPendingRequest(bytes32 _requestId) {
+    require(pendingRequests[_requestId] == address(0), "Request is already pending");
     _;
   }
 }
