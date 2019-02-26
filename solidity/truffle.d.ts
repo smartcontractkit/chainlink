768a5715d3a95cc1f750e14a0f1d2cfeeb5d77e1
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-02-26 12:01:08 -0800

    Add tslint
    - yarn `tslint`

--- solidity/truffle.d.ts
+++ solidity/truffle.d.ts
@@ -1,18 +1,21 @@
-declare type _contractTest = (accounts: string[]) => void;
-declare function contract(name: string, test: _contractTest): void;
+declare type _contractTest = (accounts: string[]) => void
+declare function contract(name: string, test: _contractTest): void
+
+/* tslint:disable-next-line:interface-name */
 declare interface TransactionMeta {
   from: string,
 }
 
-declare interface Contract<T> {
-  "new"(): Promise<T>,
+declare interface IContract<T> {
+  'new'(): Promise<T>,
   deployed(): Promise<T>,
   at(address: string): T,
 }
 
-declare interface ChainlinkedInstance {
-}
+/* tslint:disable-next-line:interface-name no-empty-interface */
+declare interface ChainlinkedInstance {}
 
+/* tslint:disable-next-line:interface-name */
 interface Artifacts {
   require(name: string): Contract<ChainlinkedInstance>,
 }
