83543fb953a647a407b736d9992d2022e01d83f7
John Barker <jebarker@gmail.com>
2019-03-04 15:29:36 -0700

    Return historic rate if block hasn't advanced

--- solidity/contracts/LinkEx.sol
+++ solidity/contracts/LinkEx.sol
@@ -9,15 +9,15 @@ contract LinkEx {
   uint256 private rate;
   uint256 private rateHeight;
 
-  function currentRate() view public returns (uint256) {
-    if (rateHeight != 0 && block.number == rateHeight) {
+  function currentRate() public view returns (uint256) {
+    if (rateHeight != 0 && block.number > rateHeight) {
       return rate;
     }
     return historicRate;
   }
 
   function update(uint256 _rate) public {
-    if (rateHeight != 0 && block.number == rateHeight) {
+    if (rateHeight != 0 && block.number != rateHeight) {
       return;
     }
     historicRate = rate;
