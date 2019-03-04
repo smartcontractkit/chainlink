a868364f29d42def54ecc086e0bd373d341b4e30
John Barker <jebarker@gmail.com>
2019-03-04 15:08:53 -0700

    Add solidity test for LinkEx

---
+++ solidity/test/TestLinkEx.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24 <0.6.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/LinkEx.sol";
+
+contract TestLinkEx {
+  function testCurrentRate() public {
+    LinkEx ex = new LinkEx();
+
+    Assert.equal(ex.currentRate(), 0, "Fresh contract should have 0 initial rate");
+  }
+
+  function testUpdate() public {
+    LinkEx ex = new LinkEx();
+
+    uint rate = 8616460799;
+    ex.update(rate);
+    Assert.equal(ex.currentRate(), rate, "Contract should return rate just set");
+  }
+}
