0be24b431965fd6034e24b51fff427eeb63b8030
John Barker <jebarker@gmail.com>
2019-03-05 16:17:33 -0700

    Use eslint, which provides correctness checks and uses prettifier
    When `prettifier` was added, `standard` was replaced directly with
    `prettifier`. However `prettifier` only does formatting checks.
    
    Eslint was configured to use `prettifier` as a plugin, so using that
    instead.

--- examples/echo_server/send_ethlog_transaction.js
+++ examples/echo_server/send_ethlog_transaction.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-var Web3 = require('web3'),
-  contract = require('truffle-contract'),
-  path = require('path')
+var Web3 = require('web3')
+var contract = require('truffle-contract')
+var path = require('path')
 EthLogJSON = require(path.join(__dirname, 'build/contracts/EthLog.json'))
 
 var provider = new Web3.providers.HttpProvider('http://localhost:18545')
