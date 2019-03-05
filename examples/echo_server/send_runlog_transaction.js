a1c727633d7aacec595627fae125283a49eb9a5d
John Barker <jebarker@gmail.com>
2019-03-05 16:17:33 -0700

    Use eslint, which provides correctness checks and uses prettifier
    When `prettifier` was added, `standard` was replaced directly with
    `prettifier`. However `prettifier` only does formatting checks.
    
    Eslint was configured to use `prettifier` as a plugin, so using that
    instead.

--- examples/echo_server/send_runlog_transaction.js
+++ examples/echo_server/send_runlog_transaction.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-var Web3 = require('web3'),
-  contract = require('truffle-contract'),
-  path = require('path')
+var Web3 = require('web3')
+var contract = require('truffle-contract')
+var path = require('path')
 RunLogJSON = require(path.join(__dirname, 'build/contracts/RunLog.json'))
 
 var provider = new Web3.providers.HttpProvider('http://localhost:18545')
--- examples/uptime_sla/send_sla_transaction.js
+++ examples/uptime_sla/send_sla_transaction.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-var Web3 = require('web3'),
-  contract = require('truffle-contract'),
-  path = require('path')
+var Web3 = require('web3')
+var contract = require('truffle-contract')
+var path = require('path')
 UptimeSLAJSON = require(path.join(__dirname, 'build/contracts/UptimeSLA.json'))
 
 var provider = new Web3.providers.HttpProvider('http://localhost:18545')
