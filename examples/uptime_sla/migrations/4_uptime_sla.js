dd0e2a324b514792afc1c87dea3531b7cad27b4b
John Barker <jebarker@gmail.com>
2019-03-06 10:31:49 -0700

    Restore clmigration for UptimeSLA

--- examples/uptime_sla/migrations/4_uptime_sla.js
+++ examples/uptime_sla/migrations/4_uptime_sla.js
@@ -1,3 +1,4 @@
+const clmigration = require('../clmigration.js')
 const request = require('request-promise').defaults({ jar: true })
 const UptimeSLA = artifacts.require('UptimeSLA')
 const Oracle = artifacts.require('Oracle')
@@ -19,7 +20,7 @@ let job = {
   ]
 }
 
-module.exports = async function(truffleDeployer) {
+module.exports = clmigration(async function(truffleDeployer) {
   let client = '0x542B68aE7029b7212A5223ec2867c6a94703BeE3'
   let serviceProvider = '0xB16E8460cCd76aEC437ca74891D3D358EA7d1d88'
 
@@ -39,4 +40,4 @@ module.exports = async function(truffleDeployer) {
     body.id,
     { value: 1000000000 }
   )
-}
+})
