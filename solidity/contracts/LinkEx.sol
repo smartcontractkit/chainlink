0d25d200dea33eb434b3cac819fefbb55e67be81
Thomas Hodges <me@thodges.com>
2019-03-12 10:23:52 -0500

    Establish price feed interface

--- solidity/contracts/LinkEx.sol
+++ solidity/contracts/LinkEx.sol
@@ -1,22 +1,24 @@
 pragma solidity 0.4.24;
 
+import "./interfaces/LinkExInterface.sol";
+
 /**
  * @title The LINK exchange contract
  */
-contract LinkEx {
+contract LinkEx is LinkExInterface {
 
   uint256 private historicRate;
   uint256 private rate;
   uint256 private rateHeight;
 
-  function currentRate() public view returns (uint256) {
+  function currentRate() external view returns (uint256) {
     if (isFutureBlock()) {
       return rate;
     }
     return historicRate;
   }
 
-  function update(uint256 _rate) public {
+  function update(uint256 _rate) external {
     if (isFutureBlock()) {
       historicRate = rate;
       rateHeight = block.number;
