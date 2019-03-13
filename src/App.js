1189704c4f637b0515449df3e267a1c0eb39fce2
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-13 09:36:33 -0700

    Display job runs list on index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import './App.css'
+import JobRunsList from './components/JobRunsList'
+
+const getJobRuns = () => fetch('/api/v1/job_runs').then(r => r.json())
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { jobRuns: null }
+  }
+
+  componentDidMount() {
+    getJobRuns().then(jr => {
+      this.setState({ jobRuns: jr })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>LINK Stats</h1>
         </header>
+
+        <JobRunsList jobRuns={this.state.jobRuns} />
       </div>
     )
   }
