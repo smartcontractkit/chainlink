ed6a51a3f330c2d9dd34676d952056edf87949db
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-14 15:39:16 -0700

    Only seed job runs when empty

--- src/seed.ts
+++ src/seed.ts
@@ -1,17 +1,19 @@
 import { JobRun } from "./entity/JobRun"
 
 const seed = async (dbConnection: any) => {
-  console.log("Inserting a new job run into the database...")
-  const jobRunA = new JobRun()
-  jobRunA.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
-  await dbConnection.manager.save(jobRunA)
-  console.log("Saved a new job run with id: " + jobRunA.id)
+  const count = await dbConnection.manager.count(JobRun)
 
-  console.log("Inserting a new job run into the database...")
-  const jobRunB = new JobRun()
-  jobRunB.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
-  await dbConnection.manager.save(jobRunB)
-  console.log("Saved a new job run with id: " + jobRunB.id)
+  if (count === 0) {
+    const jobRunA = new JobRun()
+    jobRunA.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
+    await dbConnection.manager.save(jobRunA)
+    console.log("Saved a new job run with id: " + jobRunA.id)
+
+    const jobRunB = new JobRun()
+    jobRunB.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
+    await dbConnection.manager.save(jobRunB)
+    console.log("Saved a new job run with id: " + jobRunB.id)
+  }
 }
 
 export default seed
