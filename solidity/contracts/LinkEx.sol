cdf6dfc7b76f9ddcd9e3ca316190aa1223745a55
Thomas Hodges <me@thodges.com>
2019-03-14 09:19:18 -0500

    Owner can add oracles

--- solidity/contracts/LinkEx.sol
+++ solidity/contracts/LinkEx.sol
@@ -1,15 +1,24 @@
 pragma solidity 0.4.24;
 
 import "./interfaces/LinkExInterface.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title The LINK exchange contract
  */
-contract LinkEx is LinkExInterface {
+contract LinkEx is LinkExInterface, Ownable {
+
+  mapping(address => bool) public authorizedNodes;
 
   uint256 private historicRate;
   uint256 private rate;
   uint256 private rateHeight;
+  address[] private oracles;
+
+  function addOracle(address _oracle) external onlyOwner {
+    setFulfillmentPermission(_oracle, true);
+    oracles.push(_oracle);
+  }
 
   function currentRate() external view returns (uint256) {
     if (isFutureBlock()) {
@@ -28,5 +37,9 @@ contract LinkEx is LinkExInterface {
 
   function isFutureBlock() internal view returns (bool) {
     return block.number > rateHeight;
+  }
+
+  function setFulfillmentPermission(address _oracle, bool _status) private {
+    authorizedNodes[_oracle] = _status;
   }
 }
