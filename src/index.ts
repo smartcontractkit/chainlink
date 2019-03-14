fca5b678cebf3042b519594406a60c36c044af3b
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-14 15:36:22 -0700

    Extract server & seed functions into separate files

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,9 @@
 import "reflect-metadata"
 import { createConnection } from "typeorm"
-import express from "express"
-import { JobRun } from "./entity/JobRun"
-
-const PORT = process.env.SERVER_PORT || 8080
-
-const startServer = (dbConnection: any) => {
-  const app = express()
-  app.set("port", PORT);
-
-  const server = require("http").Server(app)
-  const io = require("socket.io")(server)
-
-  server.listen(PORT, () => {
-    console.log(`server started, listening on port ${PORT}`)
-  })
-
-  app.use(express.static('client/build'))
-
-  app.get('/api/v1/job_runs', async (req, res) => {
-    const jobRuns = await dbConnection.manager.find(JobRun)
-
-    return res.send(jobRuns)
-  })
-
-  io.on('connection', (socket: any) => {
-    console.log('a user connected')
-
-    socket.on('disconnect', () => {
-      console.log('a user disconnected');
-    })
-  })
-}
-
-const seedDb = async (dbConnection: any) => {
-  console.log("Inserting a new job run into the database...")
-  const jobRunA = new JobRun()
-  jobRunA.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
-  await dbConnection.manager.save(jobRunA)
-  console.log("Saved a new job run with id: " + jobRunA.id)
-
-  console.log("Inserting a new job run into the database...")
-  const jobRunB = new JobRun()
-  jobRunB.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
-  await dbConnection.manager.save(jobRunB)
-  console.log("Saved a new job run with id: " + jobRunB.id)
-}
+import seed from "./seed"
+import server from "./server"
 
 createConnection().then(async dbConnection => {
-  seedDb(dbConnection)
-  startServer(dbConnection)
+  seed(dbConnection)
+  server(dbConnection)
 }).catch(error => console.log(error))
