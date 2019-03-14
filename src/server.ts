e96013a302869e000674c4edf65d7b5e457ff394
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-14 15:36:22 -0700

    Extract server & seed functions into separate files

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import { JobRun } from "./entity/JobRun"
+
+const PORT = process.env.SERVER_PORT || 8080
+
+const server = (dbConnection: any) => {
+  let connections = 0
+  const express = require('express')
+  const app = express()
+  app.set("port", PORT);
+
+  const server = require("http").Server(app)
+  const io = require("socket.io")(server)
+
+  server.listen(PORT, () => {
+    console.log(`server started, listening on port ${PORT}`)
+  })
+
+  app.use(express.static('public'))
+
+  app.get('/api/v1/job_runs', async (req, res) => {
+    const jobRuns = await dbConnection.manager.find(JobRun)
+
+    return res.send(jobRuns)
+  })
+
+  io.on('connection', (socket: any) => {
+    console.log('a user connected')
+
+    socket.on('disconnect', () => {
+      console.log('a user disconnected');
+    })
+  })
+}
+
+export default server
