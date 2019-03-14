9aa43f538af5d047ebfa6d2507eda6c2fa35d48c
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-14 15:58:26 -0700

    Improve spacing & typography of latest runs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import Header from './components/Header'
 import Home from './containers/Home'
 import { withStyles } from '@material-ui/core/styles'
 
-const styles = () => {
+const styles = (theme: any) => {
   return {
     main: {
-      marginTop: 90
+      marginTop: 90,
+      paddingLeft: theme.spacing.unit * 5,
+      paddingRight: theme.spacing.unit * 5
     }
   }
 }
 
 const App = (props: any) => {
   return (
-    <Grid container>
+    <Grid container spacing={24}>
       <Grid item xs={12}>
         <Header />
 
-        <main className={props.classes.main}>
+        <Paper className={props.classes.main} elevation={0}>
           <Home />
-        </main>
+        </Paper>
       </Grid>
     </Grid>
   )
