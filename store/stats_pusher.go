288e1e89b793c4eff10225749a4e8805ac9998dc
Dimitri Roche <dimroc@gmail.com>
2019-03-17 12:17:24 -0400

    replace multiple done chans w a context tree:
    https://blog.golang.org/context

--- store/stats_pusher.go
+++ store/stats_pusher.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"context"
 	"fmt"
 	"net/url"
 	"sync"
@@ -39,7 +40,7 @@ func (noopStatsPusher) Send([]byte)  {}
 type websocketStatsPusher struct {
 	boot    *sync.Mutex
 	conn    *websocket.Conn
-	done    chan struct{}
+	cancel  context.CancelFunc
 	send    chan []byte
 	sleeper utils.Sleeper
 	started bool
@@ -66,10 +67,11 @@ func (w *websocketStatsPusher) Start() error {
 		return nil
 	}
 
-	w.done = make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	w.cancel = cancel
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	go w.connectAndWritePump(wg, w.done)
+	go w.connectAndWritePump(ctx, wg)
 	wg.Wait()
 	w.started = true
 	return nil
@@ -92,25 +94,27 @@ const (
 // Inspired by https://github.com/gorilla/websocket/blob/master/examples/chat/client.go
 // lexical confinement of done chan allows multiple connectAndWritePump routines
 // to clean up independent of itself by reducing shared state. i.e. a passed done, not w.done.
-func (w *websocketStatsPusher) connectAndWritePump(wg *sync.WaitGroup, done chan struct{}) {
+func (w *websocketStatsPusher) connectAndWritePump(parentCtx context.Context, wg *sync.WaitGroup) {
 	wg.Done()
+	logger.Info("Connecting to linkstats at ", w.url.String())
 
 	for {
 		select {
-		case <-done:
+		case <-parentCtx.Done():
 			return
 		case <-time.After(w.sleeper.After()):
-			if err := w.connect(); err != nil {
+			connectionCtx, cancel := context.WithCancel(parentCtx)
+			defer cancel()
+
+			if err := w.connect(connectionCtx); err != nil {
 				logger.Warn("Failed to connect to linkstats (", w.url.String(), "): ", err)
 				break
 			}
 
 			logger.Info("Connected to linkstats at ", w.url.String())
 			w.sleeper.Reset()
-
-			serverDone := make(chan struct{})
-			go w.readPumpForControlMessages(serverDone)
-			w.writePump(done, serverDone)
+			go w.readPumpForControlMessages(cancel)
+			w.writePump(connectionCtx)
 		}
 	}
 }
@@ -120,7 +124,7 @@ var (
 )
 
 // Inspired by https://github.com/gorilla/websocket/blob/master/examples/chat/client.go#L82
-func (w *websocketStatsPusher) writePump(done chan struct{}, serverDone chan struct{}) {
+func (w *websocketStatsPusher) writePump(ctx context.Context) {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
 		ticker.Stop()
@@ -128,9 +132,7 @@ func (w *websocketStatsPusher) writePump(done chan struct{}, serverDone chan str
 	}()
 	for {
 		select {
-		case <-done:
-			return
-		case <-serverDone:
+		case <-ctx.Done():
 			return
 		case message, open := <-w.send:
 			if !open { // channel closed
@@ -176,8 +178,8 @@ func (w *websocketStatsPusher) writePump(done chan struct{}, serverDone chan str
 	}
 }
 
-func (w *websocketStatsPusher) connect() error {
-	conn, _, err := websocket.DefaultDialer.Dial(w.url.String(), nil)
+func (w *websocketStatsPusher) connect(ctx context.Context) error {
+	conn, _, err := websocket.DefaultDialer.DialContext(ctx, w.url.String(), nil)
 	if err != nil {
 		return fmt.Errorf("websocketStatsPusher#connect: %v", err)
 	}
@@ -192,7 +194,9 @@ var expectedCloseMessages = []int{websocket.CloseGoingAway, websocket.CloseAbnor
 // intention of handling control messages, like server disconnect.
 // https://stackoverflow.com/a/48181794/639773
 // https://github.com/gorilla/websocket/blob/master/examples/chat/client.go#L56
-func (w *websocketStatsPusher) readPumpForControlMessages(serverDone chan struct{}) {
+func (w *websocketStatsPusher) readPumpForControlMessages(cancel context.CancelFunc) {
+	defer cancel()
+
 	w.conn.SetReadLimit(maxMessageSize)
 	_ = w.conn.SetReadDeadline(time.Now().Add(pongWait))
 	w.conn.SetPongHandler(func(string) error {
@@ -206,8 +210,7 @@ func (w *websocketStatsPusher) readPumpForControlMessages(serverDone chan struct
 			if websocket.IsUnexpectedCloseError(err, expectedCloseMessages...) {
 				logger.Warn(fmt.Sprintf("readPumpForControlMessages: %v", err))
 			}
-			close(serverDone)
-			break
+			return
 		}
 	}
 }
@@ -222,9 +225,8 @@ func (w *websocketStatsPusher) Close() error {
 	w.boot.Lock()
 	defer w.boot.Unlock()
 
-	if w.done != nil {
-		close(w.done)
-		w.done = nil
+	if w.started {
+		w.cancel()
 	}
 	w.started = false
 	return nil
