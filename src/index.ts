5ada3be0300ab7bf9afc082fa2541173a1022dbf
Dimitri Roche <dimroc@gmail.com>
2019-03-19 09:09:56 -0400

    merge TYPEORM_* env vars onto existing ormconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
-import "reflect-metadata"
-import { createConnection, ConnectionOptions, ConnectionOptionsReader } from "typeorm"
-import seed from "./seed"
-import server from "./server"
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+import seed from './seed'
+import server from './server'
+import options from '../ormconfig.json'
 
-const nodeEnv = process.env.NODE_ENV || 'development'
-const ormconfigPath = `ormconfig.${nodeEnv}.json`
+const overridableKeys = ['host', 'port', 'username', 'password', 'database']
 
-const loadOptions = async (configName: string): Promise<ConnectionOptions> => {
-  const reader = new ConnectionOptionsReader({configName})
-  return reader.get("default")
+// Loads the following ENV vars, giving them precedence.
+// i.e. TYPEORM_PORT will replace "port" in ormconfig.json.
+const loadOptions = (): PostgresConnectionOptions => {
+  let envOptions: { [key: string]: string } = {}
+  for (let v of overridableKeys) {
+    const envVar = process.env[`TYPEORM_${v.toUpperCase()}`]
+    if (envVar) {
+      envOptions[v] = envVar
+    }
+  }
+  return {
+    ...options,
+    ...envOptions
+  } as PostgresConnectionOptions
 }
 
-loadOptions(ormconfigPath).then(options => {
-  createConnection(options).then(async dbConnection => {
+createConnection(loadOptions())
+  .then(async dbConnection => {
     seed(dbConnection)
     server(dbConnection)
   })
-}).catch(error => console.log(error))
+  .catch(error => console.log(error))
