ae108932a27dc9659a9612c0a3f68ff6d2de4ef1
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-21 17:53:38 -0700

    Send query to job runs API request

--- client/src/actions/jobRuns.ts
+++ client/src/actions/jobRuns.ts
@@ -2,9 +2,9 @@ import { Dispatch } from 'redux'
 import * as api from '../api'
 import { JobRunsAction } from '../reducers/jobRuns'
 
-const getJobRuns = () => {
+const getJobRuns = (query: string | undefined) => {
   return (dispatch: Dispatch<any>) => {
-    api.getJobRuns().then((r: IJobRun[]) => {
+    api.getJobRuns(query).then((r: IJobRun[]) => {
       const action = { type: 'UPSERT_JOB_RUNS', items: r } as JobRunsAction
       dispatch(action)
     })
