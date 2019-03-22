65814be1e54919cd433bf41c008a76a1bd88f1c3
John Barker <jebarker@gmail.com>
2019-03-22 15:19:28 -0600

    Don't register sync callback if linkstatsURL is nil

--- store/store.go
+++ store/store.go
@@ -220,7 +220,7 @@ func (Clock) After(d time.Duration) <-chan time.Time {
 }
 
 func initializeORM(config Config) (*orm.ORM, error) {
-	orm, err := orm.NewORM(config.NormalizedDatabaseURL(), config.DatabaseTimeout())
+	orm, err := orm.NewORM(config.NormalizedDatabaseURL(), config.DatabaseTimeout(), config.LinkstatsURL())
 	if err != nil {
 		return nil, err
 	}
