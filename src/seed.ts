2fee76168c4ee5c76d100a19f14d7df0380fbdd6
Dimitri Roche <dimroc@gmail.com>
2019-03-22 17:35:58 -0400

    encapsulate server controllers and db connection

--- src/seed.ts
+++ src/seed.ts
@@ -1,20 +1,23 @@
-import { JobRun } from "./entity/JobRun"
+import { getDb } from './database'
+import { Connection } from 'typeorm'
+import { JobRun } from './entity/JobRun'
 
-const seed = async (dbConnection: any) => {
+const seed = async () => {
+  const dbConnection = getDb()
   const count = await dbConnection.manager.count(JobRun)
 
   if (count === 0) {
     const jobRunA = new JobRun()
-    jobRunA.requestId = "66eb9365-6c0c-487c-9297-7b1b44d87711"
-    jobRunA.jobId = "d9b0dd13-091f-4f55-b718-d9e725ab96dd" 
+    jobRunA.requestId = '66eb9365-6c0c-487c-9297-7b1b44d87711'
+    jobRunA.jobId = 'd9b0dd13-091f-4f55-b718-d9e725ab96dd'
     await dbConnection.manager.save(jobRunA)
-    console.log("Saved a new job run with id: " + jobRunA.id)
+    console.log('Saved a new job run with id: ' + jobRunA.id)
 
     const jobRunB = new JobRun()
-    jobRunB.requestId =  "81369a4d-76db-45a5-9192-869a023eced0"
-    jobRunB.jobId =   "dbbb5305-5ec9-46e8-9bab-0891d2ad4578"
+    jobRunB.requestId = '81369a4d-76db-45a5-9192-869a023eced0'
+    jobRunB.jobId = 'dbbb5305-5ec9-46e8-9bab-0891d2ad4578'
     await dbConnection.manager.save(jobRunB)
-    console.log("Saved a new job run with id: " + jobRunB.id)
+    console.log('Saved a new job run with id: ' + jobRunB.id)
   }
 }
 
