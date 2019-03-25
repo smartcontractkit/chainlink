99b02d97b0254f9d2cf7940261fddf7592ff93d6
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-25 11:44:49 -0700

    Add lint to top level project

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupTestFrameworkScriptFile: "<rootDir>/jest.setup.ts",
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/client/"
-  ]
-};
+  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.ts',
+  testPathIgnorePatterns: ['/node_modules/', '/client/']
+}
