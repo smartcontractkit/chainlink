97384f9aa70473683daad1656a3075d169bc4cc9
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-25 10:30:42 -0700

    Extract Query type

--- client/src/actions/jobRuns.ts
+++ client/src/actions/jobRuns.ts
@@ -1,8 +1,9 @@
 import { Dispatch } from 'redux'
 import * as api from '../api'
 import { JobRunsAction } from '../reducers/jobRuns'
+import { Query } from '../reducers/search'
 
-const getJobRuns = (query: string | undefined) => {
+const getJobRuns = (query: Query) => {
   return (dispatch: Dispatch<any>) => {
     api.getJobRuns(query).then((r: IJobRun[]) => {
       const action = { type: 'UPSERT_JOB_RUNS', items: r } as JobRunsAction
--- client/src/api.ts
+++ client/src/api.ts
@@ -1,8 +1,9 @@
 import 'whatwg-fetch'
+import { Query } from './reducers/search'
 
 const base = () => `${document.location.protocol}//${document.location.host}`
 
-const getJobRuns = async (query: string | undefined): Promise<IJobRun[]> => {
+const getJobRuns = async (query: Query): Promise<IJobRun[]> => {
   const url = new URL('/api/v1/job_runs', base())
   if (query) { url.searchParams.set('query', query) }
 
