4c7cc276636f63fcd0fb5c36a45bcbc333d6a533
Dimitri Roche <dimroc@gmail.com>
2019-03-25 11:56:28 -0400

    add jest testing to server

---
+++ src/controllers/jobRuns.test.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import http from 'http'
+import request from 'supertest'
+import jobRuns from './jobRuns'
+import seed from '../seed'
+import { createDbConnection } from '../database'
+import { clearDb } from '../testdatabase'
+
+const controller = express()
+controller.use('/api/v1', jobRuns)
+
+let server: http.Server
+beforeAll(async () => {
+  await createDbConnection()
+  server = controller.listen(null)
+})
+afterAll(() => {
+  server.close()
+})
+beforeEach(async () => {
+  await clearDb()
+})
+
+describe('with no runs', () => {
+  it('returns empty', async () => {
+    const response = await request(server).get(`/api/v1/job_runs`)
+    expect(response.status).toEqual(200)
+  })
+})
+
+describe('with runs', () => {
+  beforeEach(async () => {
+    await seed()
+  })
+
+  it('returns runs', async () => {
+    const response = await request(server).get(`/api/v1/job_runs`)
+    expect(response.status).toEqual(200)
+  })
+})
