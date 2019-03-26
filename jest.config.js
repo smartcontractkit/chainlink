530a83805fa4ac8f093961b8c292887a6596ce42
Dimitri Roche <dimroc@gmail.com>
2019-03-26 17:46:40 -0400

    create jobrun on clnode push, move server tests to src/__tests__

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupTestFrameworkScriptFile: '<rootDir>/jest.setup.ts',
-  testPathIgnorePatterns: ['/node_modules/', '/client/']
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/client/',
+    'src/__tests__/testdatabase.ts'
+  ]
 }
