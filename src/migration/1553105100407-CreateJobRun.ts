0775929a6fbc70491910330b9f34a66b35345da4
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-26 15:30:14 -0700

    Remove drop table from migration

--- src/migration/1553105100407-CreateJobRun.ts
+++ src/migration/1553105100407-CreateJobRun.ts
@@ -3,12 +3,11 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class CreateJobRun1553105100407 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`CREATE TABLE "job_run" (
-        "id" SERIAL NOT NULL,
-        "jobRunId" character varying NOT NULL,
-        "jobId" character varying NOT NULL,
-        "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
-        CONSTRAINT "PK_96fe0b041b8bc157dcec25bd8ef" PRIMARY KEY ("id")
-      )`)
+      "id" character varying NOT NULL,
+      "jobId" character varying NOT NULL,
+      "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+      CONSTRAINT "PK_96fe0b041b8bc157dcec25bd8ef" PRIMARY KEY ("id")
+    )`)
     await queryRunner.query(`CREATE INDEX job_id_idx ON "job_run" ("jobId")`)
   }
 
