53f58262ba1daa1ec2d15300926bb97713fc979e
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-27 18:35:40 -0700

    Add branded link component

---
+++ client/src/components/Link.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles'
+import { Link as RouterLink } from '@reach/router'
+import classNames from 'classnames'
+
+const styles = ({ palette }: Theme) =>
+  createStyles({
+    link: {
+      color: palette.primary.main,
+      textDecoration: 'none'
+    }
+  })
+
+interface IProps extends WithStyles<typeof styles> {
+  children: React.ReactNode | string
+  to: string
+  className?: string
+}
+
+const Link = ({ to, children, classes, className }: IProps) => {
+  return (
+    <RouterLink to={to} className={classNames(classes.link, className)}>
+      {children}
+    </RouterLink>
+  )
+}
+
+export default withStyles(styles)(Link)
