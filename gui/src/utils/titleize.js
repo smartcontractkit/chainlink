286863b60a181fd3329b9b660cb07da030aad083
NavyAdmiral <keccaksha3@gmail.com>
2019-03-28 01:16:03 -0400

    Replace all underscores

--- gui/src/utils/titleize.js
+++ gui/src/utils/titleize.js
@@ -3,6 +3,6 @@ export default input => {
   const normalized = input || ''
   return normalized
     .toLowerCase()
-    .replace('_', ' ')
+    .replace(/_/g, ' ')
     .replace(/(?:^|\s|-)\S/g, x => x.toUpperCase())
 }
