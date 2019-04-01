9ef9c1a51cfa761da0931ce0b40483fa47d74c79
Dimitri Roche <dimroc@gmail.com>
2019-04-01 16:25:34 -0400

    add revert command

---
+++ src/bin/migrator.ts
@@ -0,0 +1,46 @@
+import yargs from 'yargs'
+import { Connection } from 'typeorm'
+import { createDbConnection, closeDbConnection, getDb } from '../database'
+
+const migrate = async () => {
+  return bootstrap(async (db: Connection) => {
+    const pendingMigrations = await db.runMigrations()
+    for (let m of pendingMigrations) {
+      console.log('ran', m)
+    }
+  })
+}
+
+const revert = async () => {
+  return bootstrap(async (db: Connection) => {
+    await db.undoLastMigration()
+  })
+}
+
+const bootstrap = async (cb: any) => {
+  await createDbConnection()
+  const db = getDb()
+  try {
+    await cb(db)
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await closeDbConnection()
+  }
+}
+
+const _ = yargs
+  .usage('Usage: $0 <command> [options]')
+  .command({
+    command: 'migrate',
+    describe: 'Run migrations',
+    handler: migrate
+  })
+  .command({
+    command: 'revert',
+    describe: 'Revert last migration',
+    handler: revert
+  })
+  .help('h')
+  .alias('h', 'help')
+  .demandCommand(1).argv // final argv call invokes command
