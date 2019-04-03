6068d559e929bc0292088ec118fa2dcb9da34ca7
John Barker <jebarker@gmail.com>
2019-04-03 13:19:38 -0600

    Foreign keys on jobRunID should be BIGINT

--- src/migration/1554246743893-CreateInitiator.ts
+++ src/migration/1554246743893-CreateInitiator.ts
@@ -4,7 +4,7 @@ export class AddInitiators1554246743893 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`CREATE TABLE "initiator" (
       "id" BIGSERIAL PRIMARY KEY,
-      "jobRunId" BIGSERIAL REFERENCES "job_run" ("id") ON DELETE CASCADE,
+      "jobRunId" BIGINT REFERENCES "job_run" ("id") ON DELETE CASCADE,
       "requestId" character varying NOT NULL,
       "txHash" character varying NOT NULL,
       "requester" character varying NOT NULL,
