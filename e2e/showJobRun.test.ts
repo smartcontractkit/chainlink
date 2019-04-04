f6b50cf90c6f89d3023f338a3da995675d4e7c7e
Dimitri Roche <dimroc@gmail.com>
2019-04-04 11:56:51 -0400

    add e2e tests leveraging puppeteer

---
+++ e2e/showJobRun.test.ts
@@ -0,0 +1,38 @@
+import { Server } from 'http'
+import { Browser, launch, Page } from 'puppeteer'
+import expect from 'expect-puppeteer'
+import { createDbConnection, closeDbConnection } from '../src/database'
+import { clearDb } from '../src/__tests__/testdatabase'
+import seed, { JOB_RUN_A_ID, JOB_RUN_B_ID } from '../src/seed'
+import server from '../src/server'
+
+const startServer = async () => {
+  await createDbConnection()
+  await clearDb()
+  await seed()
+  return server()
+}
+
+describe('End to end', () => {
+  let browser: Browser, page: Page, server: Server
+  beforeAll(async () => {
+    browser = await launch({
+      devtools: false,
+      headless: true,
+      args: ['--no-sandbox']
+    })
+    page = await browser.newPage()
+    server = await startServer()
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()))
+  })
+
+  afterAll(async () => {
+    return Promise.all([browser.close(), server.close(), closeDbConnection()])
+  })
+
+  it('can run tests', async () => {
+    await page.goto('http://localhost:8080')
+    await expect(page).toMatch(JOB_RUN_A_ID)
+    await expect(page).toMatch(JOB_RUN_B_ID)
+  })
+})
