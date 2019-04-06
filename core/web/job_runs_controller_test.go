24cdecdaab390c46d8df1202968447a55e3ee6ee
Steve Ellis <email@steveell.is>
2019-04-06 11:52:48 -0400

    move web package into core directory

--- core/web/job_runs_controller_test.go
+++ core/web/job_runs_controller_test.go
@@ -8,10 +8,10 @@ import (
 	"time"
 
 	"github.com/manyminds/api2go/jsonapi"
+	"github.com/smartcontractkit/chainlink/core/web"
 	"github.com/smartcontractkit/chainlink/internal/cltest"
 	"github.com/smartcontractkit/chainlink/store/models"
 	"github.com/smartcontractkit/chainlink/store/presenters"
-	"github.com/smartcontractkit/chainlink/web"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
