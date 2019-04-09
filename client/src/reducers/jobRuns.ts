694ac5ec8ec8c4a4bdfdb3aae3938187f8fb75ad
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-09 14:09:37 -0700

    Support INIT action with & without devtools
    [Fixes #164575831]

--- client/src/reducers/jobRuns.ts
+++ client/src/reducers/jobRuns.ts
@@ -10,12 +10,14 @@ export interface INormalizedData {
 export type JobRunsAction =
   | { type: 'UPSERT_JOB_RUNS'; data: INormalizedData }
   | { type: 'UPSERT_JOB_RUN'; data: INormalizedData }
+  | { type: '@@redux/INIT' }
   | { type: '@@INIT' }
 
 const INITIAL_STATE = { items: undefined }
 
 export default (state: IState = INITIAL_STATE, action: JobRunsAction) => {
   switch (action.type) {
+    case '@@redux/INIT':
     case '@@INIT':
       return INITIAL_STATE
     case 'UPSERT_JOB_RUNS':
