83fd666198f6e2546b7ce9eb0a6d8fd439edc78b
John Barker <jebarker@gmail.com>
2019-04-10 12:26:43 -0600

    Remove commented out EnableLogging in migrate_test

--- core/web/bridge_types_controller.go
+++ core/web/bridge_types_controller.go
@@ -26,6 +26,8 @@ func (btc *BridgeTypesController) Create(c *gin.Context) {
 		c.AbortWithError(http.StatusInternalServerError, err)
 	} else if bta, bt, err := models.NewBridgeType(btr); err != nil {
 		publicError(c, StatusCodeForError(err), err)
+	} else if err := services.ValidateAdapter(btr, btc.App.GetStore()); err != nil {
+		publicError(c, StatusCodeForError(err), err)
 	} else if err := btc.App.GetStore().CreateBridgeType(bt); err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 	} else if doc, err := jsonapi.Marshal(bta); err != nil {
