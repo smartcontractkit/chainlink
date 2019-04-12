ed31a6b2019f39ac63a26c9b5931faab09766043
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-11 17:53:01 -0700

    Fix & test hydration of task runs
    - Add assertion for saved task runs in realtime test
    - Make task_run#jobRunId NOT NULL

--- src/__tests__/realtime.test.ts
+++ src/__tests__/realtime.test.ts
@@ -4,6 +4,7 @@ import { start as startServer, DEFAULT_TEST_PORT } from '../support/server'
 import { closeDbConnection, getDb } from '../database'
 import fixture from './fixtures/JobRun.fixture.json'
 import { JobRun } from '../entity/JobRun'
+import { TaskRun } from '../entity/TaskRun'
 
 const ENDPOINT = `ws://localhost:${DEFAULT_TEST_PORT}`
 
@@ -17,29 +18,40 @@ describe('realtime', () => {
     return Promise.all([server.close(), closeDbConnection()])
   })
 
-  it('can handle malformed JSON & create a job run for valid JSON', (done: any) => {
-    expect.assertions(2)
+  it('can handle malformed JSON & create a job run for valid JSON', async (done: any) => {
+    expect.assertions(5)
+
+    const db = await getDb()
 
     const wsA = new WebSocket(ENDPOINT)
     wsA.on('open', () => {
       wsA.send('{invalid json}')
     })
-    wsA.on('message', (data: any) => {
+    wsA.on('message', async (data: any) => {
       const result = JSON.parse(data)
       expect(result.status).toEqual(422)
-    })
 
-    const wsB = new WebSocket(ENDPOINT)
-    wsB.on('open', () => {
-      wsB.send(JSON.stringify(fixture))
-    })
-    wsB.on('message', (data: any) => {
-      const result = JSON.parse(data)
-      expect(result.status).toEqual(201)
+      const count = await db.manager.count(JobRun)
+      expect(count).toEqual(0)
 
-      wsA.close()
-      wsB.close()
-      done()
+      const wsB = new WebSocket(ENDPOINT)
+      wsB.on('open', () => {
+        wsB.send(JSON.stringify(fixture))
+      })
+      wsB.on('message', async (data: any) => {
+        const result = JSON.parse(data)
+        expect(result.status).toEqual(201)
+
+        const jobRunCount = await db.manager.count(JobRun)
+        expect(jobRunCount).toEqual(1)
+
+        const taskRunCount = await db.manager.count(TaskRun)
+        expect(taskRunCount).toEqual(1)
+
+        wsA.close()
+        wsB.close()
+        done()
+      })
     })
   })
 })
