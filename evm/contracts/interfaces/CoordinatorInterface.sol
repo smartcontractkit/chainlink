222376ddafae00a6407155bb8253a9bda274140e
James Towle <jalextowle@berkeley.edu>
2019-04-17 00:42:05 -0600

    Refactored initiateServiceAgreement to have struct parameters

--- evm/contracts/interfaces/CoordinatorInterface.sol
+++ evm/contracts/interfaces/CoordinatorInterface.sol
@@ -1,15 +1,26 @@
 pragma solidity 0.4.24;
+pragma experimental ABIEncoderV2;
 
-interface CoordinatorInterface {
+contract CoordinatorInterface {
+
+  struct ServiceAgreement {
+    uint256 payment;
+    uint256 expiration;
+    uint256 endAt;
+    address[] oracles;
+    bytes32 requestDigest;
+  }
+
+  struct OracleSignatures {
+    uint8[] vs;
+    bytes32[] rs;
+    bytes32[] ss;
+  }
+
   function initiateServiceAgreement(
-    uint256 payment,
-    uint256 expiration,
-    uint256 endAt,
-    address[] oracles,
-    uint8[] vs,
-    bytes32[] rs,
-    bytes32[] ss,
-    bytes32 requestDigest
-  ) external returns (bytes32);
+    ServiceAgreement memory _agreement, 
+    OracleSignatures memory _signatures
+  ) public returns (bytes32);
+
   function fulfillOracleRequest(bytes32 requestId, bytes32 data) external returns (bool);
 }
