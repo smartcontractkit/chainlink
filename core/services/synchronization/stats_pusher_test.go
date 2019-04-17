26abb14f82918bb9f46cac9047f099e8779126c9
Dimitri Roche <dimroc@gmail.com>
2019-04-17 13:54:13 -0400

    intro simple StatsPusher test

---
+++ core/services/synchronization/stats_pusher_test.go
@@ -0,0 +1,38 @@
+package synchronization_test
+
+import (
+	"testing"
+
+	"github.com/smartcontractkit/chainlink/core/internal/cltest"
+	"github.com/smartcontractkit/chainlink/core/services/synchronization"
+	"github.com/smartcontractkit/chainlink/core/store/models"
+	"github.com/smartcontractkit/chainlink/core/store/orm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStatsPusher(t *testing.T) {
+	store, cleanup := cltest.NewStore()
+	defer cleanup()
+
+	wsserver, wscleanup := cltest.NewEventWebSocketServer(t)
+	defer wscleanup()
+
+	pusher := synchronization.NewStatsPusher(store.ORM, wsserver.URL)
+	pusher.Start()
+	defer pusher.Close()
+
+	j := cltest.NewJobWithWebInitiator()
+	require.NoError(t, store.CreateJob(&j))
+
+	jr := j.NewRun(j.Initiators[0])
+	require.NoError(t, store.CreateJobRun(&jr))
+
+	assert.Equal(t, 1, lenSyncEvents(t, store.ORM))
+}
+
+func lenSyncEvents(t *testing.T, orm *orm.ORM) int {
+	var count int
+	require.NoError(t, orm.DB.Model(&models.SyncEvent{}).Count(&count).Error)
+	return count
+}
