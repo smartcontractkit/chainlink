1950cf2ea926c1250ba7805937ad4f0ef0571219
Dimitri Roche <dimroc@gmail.com>
2019-04-17 14:17:39 -0400

    move Clock,Aferer,Nower from services to utils:
    For easier reuse in other packages, to prevent import cycles.

---
+++ core/utils/interfaces.go
@@ -0,0 +1,35 @@
+package utils
+
+import "time"
+
+// Nower is an interface that fulfills the Now method,
+// following the behavior of time.Now.
+type Nower interface {
+	Now() time.Time
+}
+
+// Afterer is an interface that fulfills the After method,
+// following the behavior of time.After.
+type Afterer interface {
+	After(d time.Duration) <-chan time.Time
+}
+
+// AfterNower is an interface that fulfills the `After()` and `Now()`
+// methods.
+type AfterNower interface {
+	After(d time.Duration) <-chan time.Time
+	Now() time.Time
+}
+
+// Clock is a basic type for scheduling events in the application.
+type Clock struct{}
+
+// Now returns the current time.
+func (Clock) Now() time.Time {
+	return time.Now()
+}
+
+// After returns the current time if the given duration has elapsed.
+func (Clock) After(d time.Duration) <-chan time.Time {
+	return time.After(d)
+}
