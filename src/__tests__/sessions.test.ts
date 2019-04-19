ef36daae7f96ff7611911ccc0e3ccf969d4a17ef
John Barker <jebarker@gmail.com>
2019-04-19 10:34:59 -0600

    Update authenticate tests

--- src/__tests__/sessions.test.ts
+++ src/__tests__/sessions.test.ts
@@ -1,7 +1,7 @@
 import { Connection } from 'typeorm'
 import { closeDbConnection, getDb } from '../database'
 import { createClient } from '../entity/Client'
-import { parseAuthenticationRequest, authenticateRequest } from '../sessions'
+import { authenticate } from '../sessions'
 
 describe('sessions', () => {
   let db: Connection
@@ -12,44 +12,24 @@ describe('sessions', () => {
     await closeDbConnection()
   })
 
-  describe('parseAuthenticationRequest', () => {
-    it('saves access key and secret', () => {
-      const request = parseAuthenticationRequest(
-        '{"accessKey": "917", "secret": "awkward"}'
-      )
-
-      expect(request.accessKey).toEqual('917')
-      expect(request.secret).toEqual('awkward')
-    })
-  })
-
   describe('authenticate', () => {
     it('returns the session', async (done: any) => {
       const [client, secret] = await createClient(db, 'valid-client')
-      const session = await authenticateRequest(db, {
-        accessKey: client.accessKey,
-        secret: secret
-      })
+      const session = await authenticate(db, client.accessKey, secret)
       expect(session).toBeDefined()
       expect(session.clientId).toEqual(client.id)
       done()
     })
 
     it('returns null if no client exists', async (done: any) => {
-      const result = await authenticateRequest(db, {
-        accessKey: '',
-        secret: ''
-      })
+      const result = await authenticate(db, '', '')
       expect(result).toBeNull()
       done()
     })
 
     it('returns null if the secret is incorrect', async (done: any) => {
       const [client, _] = await createClient(db, 'invalid-client')
-      const result = await authenticateRequest(db, {
-        accessKey: client.accessKey,
-        secret: 'wrong-secret'
-      })
+      const result = await authenticate(db, client.accessKey, 'wrong-secret')
       expect(result).toBeNull()
       done()
     })
