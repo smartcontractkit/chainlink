570f67074329c9faaede37b0d553460a364a1d07
John Barker <jebarker@gmail.com>
2019-04-22 15:22:52 -0600

    Add test for auth headers being passed in on wsclient connect

--- core/services/synchronization/web_socket_client_test.go
+++ core/services/synchronization/web_socket_client_test.go
@@ -1,11 +1,15 @@
 package synchronization_test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
 	"github.com/smartcontractkit/chainlink/core/services/synchronization"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -58,5 +62,27 @@ func TestWebSocketClient_Send(t *testing.T) {
 	wsclient.Send([]byte(expectation))
 	cltest.CallbackOrTimeout(t, "receive stats", func() {
 		require.Equal(t, expectation, <-wsserver.Received)
+	})
+}
+
+func TestWebSocketClient_Authentiation(t *testing.T) {
+	headerChannel := make(chan http.Header)
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("handler", w, r)
+		headerChannel <- r.Header
+	}
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	url := cltest.MustParseURL(server.URL)
+	url.Scheme = "ws"
+	wsclient := synchronization.NewWebSocketClient(url, "accessKey", "secret")
+	require.NoError(t, wsclient.Start())
+	defer wsclient.Close()
+
+	cltest.CallbackOrTimeout(t, "receive authentication headers", func() {
+		headers := <-headerChannel
+		assert.Equal(t, []string{"accessKey"}, headers["X-Explore-Chainlink-AccessKey"])
+		assert.Equal(t, []string{"secret"}, headers["X-Explore-Chainlink-Secret"])
 	})
 }
