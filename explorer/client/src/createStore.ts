5da57fddebd147a807bc946e00d18648d352d69e
Dimitri Roche <dimroc@gmail.com>
2019-04-22 10:42:23 -0400

    remove redux-logger since it was stuck on redux 3.6

--- explorer/client/src/createStore.ts
+++ explorer/client/src/createStore.ts
@@ -1,14 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
-import * as createLogger from 'redux-logger'
 import reducer from './reducers'
 
 let middleware = [thunkMiddleware]
-if (process.env.LOG_REDUX === 'true') {
-  const logger = (createLogger as any)()
-  middleware = middleware.concat(logger)
-}
 const composeEnhancers = composeWithDevTools({})
 
 export default () =>
--- operator_ui/src/connectors/redux/index.js
+++ operator_ui/src/connectors/redux/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import logger from 'redux-logger'
 import reducer from './reducers'
 
 if (typeof window === 'undefined') {
@@ -8,9 +7,6 @@ if (typeof window === 'undefined') {
 }
 
 let middleware = [thunkMiddleware]
-if (process.env.LOG_REDUX === 'true') {
-  middleware = middleware.concat(logger)
-}
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default () =>
