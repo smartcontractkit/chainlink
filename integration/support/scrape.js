5d4f3d33304274329ad6e5e7282648441ba36018
Steve Ellis <email@steveell.is>
2019-04-27 23:59:57 -0400

    improve puppeteer scraping helper

--- integration/support/scrape.js
+++ integration/support/scrape.js
@@ -19,22 +19,19 @@ module.exports = {
   // if any, it will refresh the page until the match is found or timeout
   // occurs.
   scrape: async (page, regex) => {
-    let match = await waitWithTimeout(
-      async () => {
-        const content = await page.content()
-        let match = content
-          .replace(/\s+/g, ' ')
-          .trim()
-          .match(regex)
-        if (match !== null) {
-          return match
-        }
-        page.reload()
-        return false
-      },
-      'scrape',
-      30000
-    )
-    return match
+    checkPage = async () => {
+      const content = await page.content()
+      let match = content
+        .replace(/\s+/g, ' ')
+        .trim()
+        .match(regex)
+      if (match) {
+        return match
+      }
+      await page.reload()
+      return await checkPage(page, regex)
+    }
+    let match = await waitWithTimeout(checkPage, 'scrape', 30000)
+    return await match()
   }
 }
