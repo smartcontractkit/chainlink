00d2dc813fc2262392056159fd0bddf5d996e5ed
Steve Ellis <email@steveell.is>
2019-04-29 16:42:01 -0400

    create ChainlinkClient alias for Chainlinked.sol

---
+++ evm/contracts/ChainlinkClient.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.24;
+
+import "./Chainlinked.sol";
+
+/**
+ * @title The ChainlinkClient contract
+ * @notice Contract writers can inherit this contract in order to create requests for the
+ * Chainlink network. ChainlinkClient is an alias of the Chainlinked contract.
+ */
+contract ChainlinkClient is Chainlinked {
+}
