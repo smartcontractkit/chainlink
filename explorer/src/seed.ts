b629e5693c4337aeb47f291726bb492272d1c0c3
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-05-07 14:07:54 -0700

    Seed static chainlink_node credentials in explorer

--- explorer/src/seed.ts
+++ explorer/src/seed.ts
@@ -1,17 +1,31 @@
 import { ChainlinkNode } from './entity/ChainlinkNode'
-import { Connection } from 'typeorm'
-import { createChainlinkNode } from '../src/entity/ChainlinkNode'
 import { createJobRun } from './factories'
 import { getDb } from './database'
+
+// Unhashed Secret: 6l69Z1c2Qws5AlUuwG8Gy2vQJmX0FpVDBbPaUdxPp8nhMjrFtbHxFUl7MBFSHboo
+const CORE_NODE_HASHED_SECRET =
+  'c0fe784d9aa5e3bdc78adcde938fa7d9ede40f542ee9f4c4d626da8c8b406a41'
+const CORE_NODE_SALT = '5dhpzBypVnddiYNRRz0eIO9Y6ZBqqv8D'
+const CORE_NODE_ACCESS_KEY = 'bL1wMDLp4GVJ5p5n'
 
 export default async () => {
   const db = await getDb()
   const count = await db.manager.count(ChainlinkNode)
 
   if (count === 0) {
-    const [node, _] = await createChainlinkNode(db, 'default')
+    const node = await db.manager.save(buildChainlinkNode('default'))
 
     await createJobRun(db, node)
     await createJobRun(db, node)
   }
+}
+
+const buildChainlinkNode = (name: string) => {
+  const node = new ChainlinkNode()
+  node.name = 'default'
+  node.accessKey = CORE_NODE_ACCESS_KEY
+  node.salt = CORE_NODE_SALT
+  node.hashedSecret = CORE_NODE_HASHED_SECRET
+
+  return node
 }
