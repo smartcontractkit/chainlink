efb03e8f94098b7d2972f4e1c096174b12881dd1
Steve Ellis <email@steveell.is>
2019-05-10 17:03:35 -0400

    move internal models over to use multiple oracles
    - switch over to ChainlinkClient API
    - cut out methods that get complicated with multiple oracles

--- evm/contracts/examples/ConversionRate.sol
+++ evm/contracts/examples/ConversionRate.sol
@@ -1,40 +1,34 @@
 pragma solidity 0.4.24;
 
-import "../Chainlinked.sol";
+import "../ChainlinkClient.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract ConversionRate is Chainlinked, Ownable {
+contract ConversionRate is ChainlinkClient, Ownable {
   uint256 constant private ORACLE_PAYMENT = 1 * LINK; // solium-disable-line zeppelin/no-arithmetic-operations
 
-  address public getOracle;
   uint256 public currentRate;
-  bytes32 public getJobId;
+  bytes32[] public jobIds;
+  address[] public oracles;
 
-  constructor(address _link, address _oracle, bytes32 _jobId) public {
-    setLinkToken(_link);
-    updateOracle(_oracle);
-    updateJobId(_jobId);
-    getJobId = _jobId;
+  constructor(address _link, address[] _oracles, bytes32[] _jobIds) public {
+    setChainlinkToken(_link);
+    jobIds = _jobIds;
+    oracles = _oracles;
   }
 
   function update() public {
-    chainlinkRequest(newRequest(getJobId, this, this.updateCallback.selector), ORACLE_PAYMENT);
+    Chainlink.Request memory request;
+    for (uint i = 0; i < oracles.length; i++) {
+      request = buildChainlinkRequest(jobIds[i], this, this.chainlinkCallback.selector);
+      sendChainlinkRequestTo(oracles[i], request, ORACLE_PAYMENT);
+    }
   }
 
-  function updateCallback(bytes32 _requestId, uint256 _rate)
+  function chainlinkCallback(bytes32 _requestId, uint256 _rate)
     public
-    recordChainlinkFulfillment(_requestId)
   {
+    validateChainlinkCallback(_requestId);
     currentRate = _rate;
-  }
-
-  function updateOracle(address _oracle) public onlyOwner {
-    getOracle = _oracle;
-    setOracle(_oracle);
-  }
-
-  function updateJobId(bytes32 _jobId) public onlyOwner {
-    getJobId = _jobId;
   }
 
 }
