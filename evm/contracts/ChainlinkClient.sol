50dde7c7dc6c81cd5edcfa837724f2570d6e09f8
Thomas Hodges <me@thodges.com>
2019-05-10 15:00:07 -0500

    Use Pointer in ChainlinkClient

--- evm/contracts/ChainlinkClient.sol
+++ evm/contracts/ChainlinkClient.sol
@@ -5,6 +5,7 @@ import "./ENSResolver.sol";
 import "./interfaces/ENSInterface.sol";
 import "./interfaces/LinkTokenInterface.sol";
 import "./interfaces/ChainlinkRequestInterface.sol";
+import "./interfaces/PointerInterface.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 /**
@@ -22,6 +23,7 @@ contract ChainlinkClient {
   uint256 constant private ARGS_VERSION = 1;
   bytes32 constant private ENS_TOKEN_SUBNAME = keccak256("link");
   bytes32 constant private ENS_ORACLE_SUBNAME = keccak256("oracle");
+  address constant private LINK_TOKEN_POINTER = 0xC89bD4E1632D3A43CB03AAAd5262cbe4038Bc571;
 
   ENSInterface private ens;
   bytes32 private ensNode;
@@ -126,6 +128,14 @@ contract ChainlinkClient {
    */
   function setChainlinkToken(address _link) internal {
     link = LinkTokenInterface(_link);
+  }
+
+  /**
+   * @notice Sets the Chainlink token address for the public
+   * network as given by the Pointer contract
+   */
+  function setPublicChainlinkToken() internal {
+    setChainlinkToken(PointerInterface(LINK_TOKEN_POINTER).getAddress());
   }
 
   /**
