8b4b8fa88ec6cd2a082f2427431925463a6925b8
John Barker <jebarker@gmail.com>
2019-05-10 20:21:20 -0600

    alloc feature is now on by default

--- sgx/enclave/src/lib.rs
+++ sgx/enclave/src/lib.rs
@@ -2,7 +2,6 @@
 #![crate_type = "staticlib"]
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
-#![feature(alloc)]
 
 extern crate base64;
 extern crate bigdecimal;
