4e70869f4351de6a2964cdad45e3dcb545961efe
Steve Ellis <email@steveell.is>
2019-05-11 19:15:14 -0400

    basic aggregation for conversion rate contract

--- evm/contracts/examples/ConversionRate.sol
+++ evm/contracts/examples/ConversionRate.sol
@@ -1,14 +1,22 @@
 pragma solidity 0.4.24;
 
 import "../ChainlinkClient.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract ConversionRate is ChainlinkClient, Ownable {
+contract ConversionRate is ChainlinkClient {
   uint256 constant private ORACLE_PAYMENT = 1 * LINK; // solium-disable-line zeppelin/no-arithmetic-operations
+
+  struct Answer {
+    uint256 expectedResponses;
+    uint256[] responses;
+  }
 
   uint256 public currentRate;
   bytes32[] public jobIds;
   address[] public oracles;
+  uint256 private requestCounter = 1;
+  uint256 private latestAnswer;
+  mapping(bytes32 => uint256) private requestMap;
+  mapping(uint256 => Answer) private answers;
 
   constructor(address _link, address[] _oracles, bytes32[] _jobIds) public {
     setChainlinkToken(_link);
@@ -16,19 +24,67 @@ contract ConversionRate is ChainlinkClient, Ownable {
     oracles = _oracles;
   }
 
-  function update() public {
+  function update()
+    public
+  {
     Chainlink.Request memory request;
+    bytes32 requestId;
+
     for (uint i = 0; i < oracles.length; i++) {
       request = buildChainlinkRequest(jobIds[i], this, this.chainlinkCallback.selector);
-      sendChainlinkRequestTo(oracles[i], request, ORACLE_PAYMENT);
+      requestId = sendChainlinkRequestTo(oracles[i], request, ORACLE_PAYMENT);
+      requestMap[requestId] = requestCounter;
+    }
+    answers[requestCounter].expectedResponses = oracles.length;
+    requestCounter = requestCounter.add(1);
+  }
+
+  function chainlinkCallback(bytes32 _clRequestId, uint256 _rate)
+    public
+  {
+    validateChainlinkCallback(_clRequestId);
+
+    uint256 requestId = requestMap[_clRequestId];
+    delete requestMap[_clRequestId];
+    answers[requestId].responses.push(_rate);
+
+    updateRecords(requestId);
+  }
+
+  function updateRecords(uint256 _requestId)
+    private
+    checkAllResponsesReceived(_requestId)
+  {
+    updateRate(_requestId);
+    delete answers[_requestId];
+  }
+
+  function updateRate(uint256 _requestId)
+    private
+    checkLatestAnswer(_requestId)
+  {
+    uint256 sumQuotients;
+    uint256 sumRemainders;
+    Answer memory answer = answers[_requestId];
+    for (uint i = 0; i < answer.expectedResponses; i++) {
+      uint256 response = answer.responses[i];
+      sumQuotients = sumQuotients.add(response.div(answer.expectedResponses)); // aggregate responses and protect from overflows
+      sumRemainders = sumRemainders.add(response % answer.expectedResponses); 
+    }
+    currentRate = sumQuotients.add(sumRemainders.div(answer.expectedResponses)); // recover lost accuracy from result 
+    latestAnswer = _requestId;
+  }
+
+  modifier checkAllResponsesReceived(uint256 _requestId) {
+    if (answers[_requestId].responses.length == answers[_requestId].expectedResponses) {
+      _;
     }
   }
 
-  function chainlinkCallback(bytes32 _requestId, uint256 _rate)
-    public
-  {
-    validateChainlinkCallback(_requestId);
-    currentRate = _rate;
+  modifier checkLatestAnswer(uint256 _requestId) {
+    if (latestAnswer < _requestId) {
+      _;
+    }
   }
 
 }
--- evm/test/ConversionRate.test.ts
+++ evm/test/ConversionRate.test.ts
@@ -2,17 +2,16 @@ import * as h from './support/helpers'
 import { assertBigNum } from './support/matchers'
 const personas = h.personas
 
-contract.only('ConverstionRate', () => {
+contract('ConverstionRate', () => {
   const SOURCE_PATH: string = 'ConversionRate.sol'
-  const jobId = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000000'
-  let link, oc, rate
+  const jobId1 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000001'
+  const jobId2 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000002'
+  const jobId3 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000003'
+  let link, rate, oc1, oc2, oc3, oracles
 
   beforeEach(async () => {
     link = await h.linkContract()
-    oc = await h.deploy('Oracle.sol', link.address)
-    await oc.transferOwnership(personas.Neil, { from: personas.Default })
-    rate = await h.deploy(SOURCE_PATH, link.address, [oc.address], [jobId])
-    await rate.transferOwnership(personas.Carol, { from: personas.Default })
+    oc1 = await h.deploy('Oracle.sol', link.address)
   })
 
   it('has a limited public interface', () => {
@@ -22,36 +21,66 @@ contract.only('ConverstionRate', () => {
       'jobIds',
       'oracles',
       'update',
-      // Owable
-      'owner',
-      'renounceOwnership',
-      'transferOwnership'
     ])
   })
 
   describe('#update', () => {
     const response = 100
 
-    beforeEach(async () => {
-      await link.transfer(rate.address, h.toWei('1', 'ether'), {from: personas.Default})
+    context('with one oracle', () => {
+      beforeEach(async () => {
+        rate = await h.deploy(SOURCE_PATH, link.address, [oc1.address], [jobId1])
 
-      const current = await rate.currentRate.call()
-      assertBigNum(h.bigNum(0), current)
+        await link.transfer(rate.address, h.toWei('1', 'ether'))
+
+        const current = await rate.currentRate.call()
+        assertBigNum(h.bigNum(0), current)
+      })
+
+      it('triggeers a request to the oracle and accepts a response', async () => {
+        const requestTx = await rate.update()
+
+        const log = requestTx.receipt.rawLogs[3]
+        assert.equal(oc1.address, log.address)
+        const request = h.decodeRunRequest(log)
+
+        await h.fulfillOracleRequest(oc1, request, response)
+
+        const current = await rate.currentRate.call()
+        assertBigNum(response, current)
+      })
     })
 
-    it('triggeers a request to the oracle and accepts a response', async () => {
-      const requestTx = await rate.update()
+    context('with multiple oracles', () => {
+      beforeEach(async () => {
+        oc2 = await h.deploy('Oracle.sol', link.address)
+        oc3 = await h.deploy('Oracle.sol', link.address)
+        oracles = [oc1, oc2, oc3]
 
-      const log = requestTx.receipt.rawLogs[3]
-      assert.equal(oc.address, log.address)
-      const request = h.decodeRunRequest(log)
+        rate = await h.deploy(SOURCE_PATH, link.address, oracles.map(o => o.address), [jobId1, jobId2, jobId3])
 
-      await h.fulfillOracleRequest(oc, request, response, {
-         from: personas.Neil
-       })
+        await link.transfer(rate.address, h.toWei('3', 'ether'))
 
-      const current = await rate.currentRate.call()
-      assertBigNum(response, current)
+        const current = await rate.currentRate.call()
+        assertBigNum(h.bigNum(0), current)
+      })
+
+      it('triggeers a request to the oracle and accepts a response', async () => {
+        const requestTx = await rate.update()
+        const responses = [101, 102, 103]
+
+        for (let i = 0; i < oracles.length; i++) {
+            const oracle = oracles[i]
+            const log = requestTx.receipt.rawLogs[(i * 4) + 3]
+            assert.equal(oracle.address, log.address)
+            const request = h.decodeRunRequest(log)
+
+            await h.fulfillOracleRequest(oracle, request, responses[i])
+        }
+
+        const current = await rate.currentRate.call()
+        assertBigNum(102, current)
+      })
     })
   })
 })
