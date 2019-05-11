abdaa89da27257dd3585b8832234d93125b76e52
Thomas Hodges <me@thodges.com>
2019-05-11 12:56:59 -0500

    Add box to CI

--- evm/box/migrations/2_mycontract_migration.js
+++ evm/box/migrations/2_mycontract_migration.js
@@ -1,19 +1,19 @@
-let MyContract = artifacts.require("MyContract");
-let LinkToken = artifacts.require("LinkToken");
-let Oracle = artifacts.require("Oracle");
+let MyContract = artifacts.require('MyContract')
+let LinkToken = artifacts.require('LinkToken')
+let Oracle = artifacts.require('Oracle')
 
 module.exports = (deployer, network) => {
   // Local (development) networks need their own deployment of the LINK
   // token and the Oracle contract
-  if(!network.startsWith("live")) {
+  if (!network.startsWith('live')) {
     deployer.deploy(LinkToken).then(() => {
       return deployer.deploy(Oracle, LinkToken.address).then(() => {
-        return deployer.deploy(MyContract, LinkToken.address);
-      });
-    });
+        return deployer.deploy(MyContract, LinkToken.address)
+      })
+    })
   } else {
     // For live networks, use the 0 address to allow the ChainlinkRegistry
     // contract automatically retrieve the correct address for you
-    deployer.deploy(MyContract, "0x0000000000000000000000000000000000000000");
+    deployer.deploy(MyContract, '0x0000000000000000000000000000000000000000')
   }
-};
+}
