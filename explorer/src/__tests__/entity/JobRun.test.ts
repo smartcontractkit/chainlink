e6edf5dbe51d6b26f4d4dd3bce7932ffc240ee5f
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-05-14 11:17:48 -0700

    Extract queries within explorer

--- explorer/src/__tests__/entity/JobRun.test.ts
+++ explorer/src/__tests__/entity/JobRun.test.ts
@@ -3,7 +3,7 @@ import { closeDbConnection, getDb } from '../../database'
 import { Connection } from 'typeorm'
 import { createChainlinkNode } from '../../entity/ChainlinkNode'
 import ethtxFixture from '../fixtures/JobRun.ethtx.fixture.json'
-import { fromString, search } from '../../entity/JobRun'
+import { fromString } from '../../entity/JobRun'
 
 let db: Connection
 
@@ -79,119 +79,5 @@ describe('fromString', () => {
     } catch (err) {
       expect(err).toBeDefined()
     }
-  })
-})
-
-describe('search', () => {
-  beforeEach(async () => {
-    const [chainlinkNode, _] = await createChainlinkNode(
-      db,
-      'job-run-search-chainlink-node'
-    )
-
-    const jrA = fromString(JSON.stringify(fixture))
-    jrA.chainlinkNodeId = chainlinkNode.id
-    jrA.createdAt = new Date('2019-04-08T01:00:00.000Z')
-    await db.manager.save(jrA)
-
-    const fixtureB = Object.assign({}, fixture, {
-      jobId: 'f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb',
-      runId: 'f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb'
-    })
-    const jrB = fromString(JSON.stringify(fixtureB))
-    jrB.chainlinkNodeId = chainlinkNode.id
-    jrB.createdAt = new Date('2019-04-09T01:00:00.000Z')
-    jrB.txHash = 'fixtureBTxHash'
-    jrB.requester = 'fixtureBRequester'
-    jrB.requestId = 'fixtureBRequestID'
-    await db.manager.save(jrB)
-  })
-
-  it('returns all results when no query is supplied', async () => {
-    let results
-    results = await search(db, { searchQuery: undefined })
-    expect(results).toHaveLength(2)
-    results = await search(db, { searchQuery: null })
-    expect(results).toHaveLength(2)
-  })
-
-  it('returns results in descending order by createdAt', async () => {
-    const results = await search(db, { searchQuery: undefined })
-    expect(results[0].runId).toEqual('f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb')
-    expect(results[1].runId).toEqual('f1xtureAaaaaaaaaaaaaaaaaaaaaaaaa')
-  })
-
-  it('can set a limit', async () => {
-    const results = await search(db, { searchQuery: undefined, limit: 1 })
-    expect(results[0].runId).toEqual('f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb')
-    expect(results).toHaveLength(1)
-  })
-
-  it('can set a page with a 1 based index', async () => {
-    let results
-
-    results = await search(db, {
-      limit: 1,
-      page: 1,
-      searchQuery: undefined
-    })
-    expect(results[0].runId).toEqual('f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb')
-
-    results = await search(db, {
-      limit: 1,
-      page: 2,
-      searchQuery: undefined
-    })
-    expect(results[0].runId).toEqual('f1xtureAaaaaaaaaaaaaaaaaaaaaaaaa')
-  })
-
-  it('returns no results for blank search', async () => {
-    const results = await search(db, { searchQuery: '' })
-    expect(results).toHaveLength(0)
-  })
-
-  it('returns one result for an exact match on jobId', async () => {
-    const results = await search(db, {
-      searchQuery: 'f1xtureAaaaaaaaaaaaaaaaaaaaaaaaa'
-    })
-    expect(results).toHaveLength(1)
-  })
-
-  it('returns one result for an exact match on jobId and runId', async () => {
-    const results = await search(db, {
-      searchQuery: `${'f1xtureAaaaaaaaaaaaaaaaaaaaaaaaa'} aeb2861d306645b1ba012079aeb2e53a`
-    })
-    expect(results).toHaveLength(1)
-  })
-
-  it('returns two results when two matching runIds are supplied', async () => {
-    const results = await search(db, {
-      searchQuery: `${'f1xtureAaaaaaaaaaaaaaaaaaaaaaaaa'} ${'f1xtureBbbbbbbbbbbbbbbbbbbbbbbbb'}`
-    })
-    expect(results).toHaveLength(2)
-  })
-
-  it('returns one result for an exact match on requester', async () => {
-    const requester = 'fixtureBRequester'
-    const results = await search(db, { searchQuery: requester })
-    expect(results).toHaveLength(1)
-  })
-
-  it('returns one result for a case insensitive match on requester', async () => {
-    const requester = 'FIXTUREBREQUESTER'
-    const results = await search(db, { searchQuery: requester })
-    expect(results).toHaveLength(1)
-  })
-
-  it('returns one result for an exact match on requestId', async () => {
-    const requestId = 'fixtureBRequestID'
-    const results = await search(db, { searchQuery: requestId })
-    expect(results).toHaveLength(1)
-  })
-
-  it('returns one result for an exact match on txHash', async () => {
-    const txHash = 'fixtureBTxHash'
-    const results = await search(db, { searchQuery: txHash })
-    expect(results).toHaveLength(1)
   })
 })
--- explorer/src/entity/JobRun.ts
+++ explorer/src/entity/JobRun.ts
@@ -2,13 +2,12 @@ import {
   Column,
   Connection,
   Entity,
-  SelectQueryBuilder,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn
 } from 'typeorm'
 import { TaskRun } from './TaskRun'
-import { ChainlinkNode, IChainlinkNodePresenter } from './ChainlinkNode'
+import { ChainlinkNode } from './ChainlinkNode'
 
 @Entity()
 export class JobRun {
@@ -92,61 +91,11 @@ export const fromString = (str: string): JobRun => {
   return jr
 }
 
-export interface ISearchParams {
-  searchQuery?: string
-  page?: number
-  limit?: number
-}
-
-const searchBuilder = (
-  db: Connection,
-  params: ISearchParams
-): SelectQueryBuilder<JobRun> => {
-  let query = db.getRepository(JobRun).createQueryBuilder('job_run')
-
-  if (params.searchQuery != null) {
-    const searchTokens = params.searchQuery.split(/\s+/)
-    query = query
-      .where('job_run.runId IN(:...searchTokens)', { searchTokens })
-      .orWhere('job_run.jobId IN(:...searchTokens)', { searchTokens })
-      .orWhere('job_run.requester IN(:...searchTokens)', { searchTokens })
-      .orWhere('job_run.requestId IN(:...searchTokens)', { searchTokens })
-      .orWhere('job_run.txHash IN(:...searchTokens)', { searchTokens })
-  }
-
-  if (params.limit != null) {
-    query = query.limit(params.limit)
-  }
-
-  if (params.page !== undefined) {
-    const offset = (params.page - 1) * params.limit
-    query = query.offset(offset)
-  }
-
-  return query
-    .leftJoinAndSelect('job_run.chainlinkNode', 'chainlink_node')
-    .orderBy('job_run.createdAt', 'DESC')
-}
-
-export const search = async (
-  db: Connection,
-  params: ISearchParams
-): Promise<JobRun[]> => {
-  return searchBuilder(db, params).getMany()
-}
-
-export const count = async (
-  db: Connection,
-  params: ISearchParams
-): Promise<number> => {
-  return searchBuilder(db, params).getCount()
-}
-
 export const saveJobRunTree = async (db: Connection, jobRun: JobRun) => {
   await db.manager.transaction(async manager => {
     const builder = manager.createQueryBuilder()
 
-    const response = await builder
+    await builder
       .insert()
       .into(JobRun)
       .values(jobRun)
