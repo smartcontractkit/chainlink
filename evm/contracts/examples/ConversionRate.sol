df88c90b121f6e13d113ec2b1932bb7a2534c8c5
Steve Ellis <email@steveell.is>
2019-05-15 11:40:16 -0400

    rename requests to answers for clarity's sake

--- evm/contracts/examples/ConversionRate.sol
+++ evm/contracts/examples/ConversionRate.sol
@@ -13,9 +13,9 @@ contract ConversionRate is ChainlinkClient {
   uint256 public currentRate;
   bytes32[] public jobIds;
   address[] public oracles;
-  uint256 private requestCounter = 1;
-  uint256 private latestAnswer;
-  mapping(bytes32 => uint256) private requestMap;
+  uint256 private answerCounter = 1;
+  uint256 private latestCompletedAnswer;
+  mapping(bytes32 => uint256) private requestAnswers;
   mapping(uint256 => Answer) private answers;
 
   constructor(address _link, address[] _oracles, bytes32[] _jobIds) public {
@@ -33,10 +33,10 @@ contract ConversionRate is ChainlinkClient {
     for (uint i = 0; i < oracles.length; i++) {
       request = buildChainlinkRequest(jobIds[i], this, this.chainlinkCallback.selector);
       requestId = sendChainlinkRequestTo(oracles[i], request, ORACLE_PAYMENT);
-      requestMap[requestId] = requestCounter;
+      requestAnswers[requestId] = answerCounter;
     }
-    answers[requestCounter].expectedResponses = oracles.length;
-    requestCounter = requestCounter.add(1);
+    answers[answerCounter].expectedResponses = oracles.length;
+    answerCounter = answerCounter.add(1);
   }
 
   function chainlinkCallback(bytes32 _clRequestId, uint256 _rate)
@@ -44,45 +44,48 @@ contract ConversionRate is ChainlinkClient {
   {
     validateChainlinkCallback(_clRequestId);
 
-    uint256 requestId = requestMap[_clRequestId];
-    delete requestMap[_clRequestId];
-    answers[requestId].responses.push(_rate);
+    uint256 answerId = requestAnswers[_clRequestId];
+    delete requestAnswers[_clRequestId];
+    answers[answerId].responses.push(_rate);
 
-    updateRecords(requestId);
+    updateRecords(answerId);
   }
 
-  function updateRecords(uint256 _requestId)
+  function updateRecords(uint256 _answerId)
     private
-    checkAllResponsesReceived(_requestId)
+    checkAllResponsesReceived(_answerId)
   {
-    updateRate(_requestId);
-    delete answers[_requestId];
+    emit Here(1111, _answerId);
+    updateRate(_answerId);
+    delete answers[_answerId];
   }
 
-  function updateRate(uint256 _requestId)
+  event Here(uint256 a, uint256 b);
+  function updateRate(uint256 _answerId)
     private
-    checkLatestAnswer(_requestId)
+    checkLatestAnswer(_answerId)
   {
     uint256 sumQuotients;
     uint256 sumRemainders;
-    Answer memory answer = answers[_requestId];
+    Answer memory answer = answers[_answerId];
     for (uint i = 0; i < answer.expectedResponses; i++) {
       uint256 response = answer.responses[i];
       sumQuotients = sumQuotients.add(response.div(answer.expectedResponses)); // aggregate responses and protect from overflows
       sumRemainders = sumRemainders.add(response % answer.expectedResponses); 
     }
     currentRate = sumQuotients.add(sumRemainders.div(answer.expectedResponses)); // recover lost accuracy from result 
-    latestAnswer = _requestId;
+    latestCompletedAnswer = _answerId;
   }
 
-  modifier checkAllResponsesReceived(uint256 _requestId) {
-    if (answers[_requestId].responses.length == answers[_requestId].expectedResponses) {
+  modifier checkAllResponsesReceived(uint256 _answerId) {
+    emit Here(answers[_answerId].responses.length, answers[_answerId].expectedResponses);
+    if (answers[_answerId].responses.length == answers[_answerId].expectedResponses) {
       _;
     }
   }
 
-  modifier checkLatestAnswer(uint256 _requestId) {
-    if (latestAnswer < _requestId) {
+  modifier checkLatestAnswer(uint256 _answerId) {
+    if (latestCompletedAnswer < _answerId) {
       _;
     }
   }
