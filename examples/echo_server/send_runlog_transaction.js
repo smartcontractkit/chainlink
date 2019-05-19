fd110a1ac36b0ed338e0d9a3620ac8a6b162fbda
Dimitri Roche <dimroc@gmail.com>
2019-05-19 19:01:34 -0400

    fix echo_server's send_*log_transaction.js to work w updated pkgs

--- examples/echo_server/send_runlog_transaction.js
+++ examples/echo_server/send_runlog_transaction.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
-var Web3 = require('web3')
-var contract = require('truffle-contract')
-var path = require('path')
+const Web3 = require('web3')
+const path = require('path')
 const RunLogJSON = require(path.join(__dirname, 'build/contracts/RunLog.json'))
 
-var provider = new Web3.providers.HttpProvider('http://localhost:18545')
-var RunLog = contract(RunLogJSON)
-RunLog.setProvider(provider)
-var devnetAddress = '0x9CA9d2D5E04012C9Ed24C0e513C9bfAa4A2dD77f'
+const provider = new Web3.providers.HttpProvider('http://localhost:18545')
+const devnetAddress = '0x9CA9d2D5E04012C9Ed24C0e513C9bfAa4A2dD77f'
+const web3 = new Web3(provider)
+const networkId = Object.keys(RunLogJSON.networks)[0]
+const wc3 = new web3.eth.Contract(
+  RunLogJSON.abi,
+  RunLogJSON.networks[networkId].address
+)
 
-RunLog.deployed()
-  .then(function(instance) {
-    return instance.request({
-      from: devnetAddress,
-      gas: 200000
-    })
-  })
-  .then(console.log, console.log)
+wc3.methods.request().send(
+  {
+    from: devnetAddress,
+    gas: 200000
+  },
+  (error, transactionHash) => {
+    if (error) {
+      console.error('encountered error: ', error, transactionHash)
+    } else {
+      console.log('sent tx: ', transactionHash)
+    }
+  }
+)
