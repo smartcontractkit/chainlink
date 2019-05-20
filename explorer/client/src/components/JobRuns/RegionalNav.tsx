0231cc64eb4bfa42eb8c15d067fd1d2f9c4285c5
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-05-20 15:57:43 -0400

    Use smaller font for mobile explorer JobRuns#Show

--- explorer/client/src/components/JobRuns/RegionalNav.tsx
+++ explorer/client/src/components/JobRuns/RegionalNav.tsx
@@ -5,11 +5,28 @@ import {
   withStyles,
   WithStyles
 } from '@material-ui/core/styles'
+import { ThemeStyle } from '@material-ui/core/styles/createTypography'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Hidden from '@material-ui/core/Hidden'
 import moment from 'moment'
 import TimeAgo from '../../components/TimeAgo'
+
+type Variant = ThemeStyle | 'srOnly' | 'inherit'
+
+interface IJobRunProps {
+  jobRun?: IJobRun
+  variant: Variant
+}
+
+const JobRunId = ({ jobRun, variant }: IJobRunProps) => {
+  return (
+    <Typography variant={variant} color="secondary" gutterBottom>
+      {jobRun ? jobRun.runId : '...'}
+    </Typography>
+  )
+}
 
 const regionalNavStyles = ({ spacing }: Theme) =>
   createStyles({
@@ -28,9 +45,12 @@ const RegionalNav = withStyles(regionalNavStyles)(
       <Paper square className={classes.container}>
         <Grid container spacing={0}>
           <Grid item xs={12}>
-            <Typography variant="h3" color="secondary" gutterBottom>
-              {jobRun ? jobRun.runId : '...'}
-            </Typography>
+            <Hidden xsDown>
+              <JobRunId jobRun={jobRun} variant="h3" />
+            </Hidden>
+            <Hidden smUp>
+              <JobRunId jobRun={jobRun} variant="h5" />
+            </Hidden>
           </Grid>
           <Grid item xs={12}>
             <Typography variant="subtitle2" color="textSecondary">
