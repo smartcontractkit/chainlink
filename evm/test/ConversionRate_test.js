aac06fdb5856ec739884f291e7339f88dccfa366
Steve Ellis <email@steveell.is>
2019-05-20 15:39:10 -0400

    change file extension to .js
    - not using any TypeScript features, don't need the linting

--- evm/test/ConversionRate_test.js
+++ evm/test/ConversionRate_test.js
@@ -4,9 +4,12 @@ const personas = h.personas
 
 contract('ConverstionRate', () => {
   const SOURCE_PATH: string = 'ConversionRate.sol'
-  const jobId1 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000001'
-  const jobId2 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000002'
-  const jobId3 = '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000003'
+  const jobId1 =
+    '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000001'
+  const jobId2 =
+    '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000002'
+  const jobId3 =
+    '0x4c7b7ffb66b344fbaa64995af81e355a00000000000000000000000000000003'
   let link, rate, oc1, oc2, oc3, oracles
 
   beforeEach(async () => {
@@ -39,7 +42,12 @@ contract('ConverstionRate', () => {
 
     context('with one oracle', () => {
       beforeEach(async () => {
-        rate = await h.deploy(SOURCE_PATH, link.address, [oc1.address], [jobId1])
+        rate = await h.deploy(
+          SOURCE_PATH,
+          link.address,
+          [oc1.address],
+          [jobId1]
+        )
 
         await link.transfer(rate.address, h.toWei('1', 'ether'))
 
@@ -67,7 +75,12 @@ contract('ConverstionRate', () => {
         oc3 = await h.deploy('Oracle.sol', link.address)
         oracles = [oc1, oc2, oc3]
 
-        rate = await h.deploy(SOURCE_PATH, link.address, oracles.map(o => o.address), [jobId1, jobId2, jobId3])
+        rate = await h.deploy(
+          SOURCE_PATH,
+          link.address,
+          oracles.map(o => o.address),
+          [jobId1, jobId2, jobId3]
+        )
 
         await link.transfer(rate.address, h.toWei('100', 'ether'))
 
@@ -80,12 +93,12 @@ contract('ConverstionRate', () => {
         const responses = [101, 102, 103]
 
         for (let i = 0; i < oracles.length; i++) {
-            const oracle = oracles[i]
-            const log = requestTx.receipt.rawLogs[(i * 4) + 3]
-            assert.equal(oracle.address, log.address)
-            const request = h.decodeRunRequest(log)
+          const oracle = oracles[i]
+          const log = requestTx.receipt.rawLogs[i * 4 + 3]
+          assert.equal(oracle.address, log.address)
+          const request = h.decodeRunRequest(log)
 
-            await h.fulfillOracleRequest(oracle, request, responses[i])
+          await h.fulfillOracleRequest(oracle, request, responses[i])
         }
 
         const current = await rate.currentRate.call()
@@ -105,10 +118,10 @@ contract('ConverstionRate', () => {
         const request2 = await rate.requestRateUpdate()
         const response2 = 200
         for (let i = 0; i < oracles.length; i++) {
-             const log = request2.receipt.rawLogs[(i * 4) + 3]
-             const request = h.decodeRunRequest(log)
-             await h.fulfillOracleRequest(oracles[i], request, response2)
-         }
+          const log = request2.receipt.rawLogs[i * 4 + 3]
+          const request = h.decodeRunRequest(log)
+          await h.fulfillOracleRequest(oracles[i], request, response2)
+        }
         assertBigNum(response2, await rate.currentRate.call())
 
         for (let i = 0; i < oracles.length; i++) {
@@ -163,7 +176,12 @@ contract('ConverstionRate', () => {
 
     context('when it is called before an answer is fulfilled', () => {
       beforeEach(async () => {
-        rate = await h.deploy(SOURCE_PATH, link.address, [oc1.address], [jobId1])
+        rate = await h.deploy(
+          SOURCE_PATH,
+          link.address,
+          [oc1.address],
+          [jobId1]
+        )
         await link.transfer(rate.address, h.toWei('100', 'ether'))
 
         oc2 = await h.deploy('Oracle.sol', link.address)
