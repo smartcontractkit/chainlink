16666d33edf4f294e48c97fd11a4ae4f437464bf
NavyAdmiral <keccaksha3@gmail.com>
2019-05-23 16:30:24 -0400

    Explorer search match hex values whether it has a 0x prefix or not

--- explorer/src/__tests__/queries/Search.test.ts
+++ explorer/src/__tests__/queries/Search.test.ts
@@ -48,6 +48,20 @@ describe('search', () => {
     jrC.requester = 'fixtureCRequester'
     jrC.requestId = 'fixtureCRequestID'
     await db.manager.save(jrC)
+
+    const fixtureD = Object.assign({}, fixture, {
+      jobId: 'jobD',
+      runId: 'runD'
+    })
+    const jrD = fromString(JSON.stringify(fixtureD))
+    jrD.chainlinkNodeId = chainlinkNode.id
+    jrD.createdAt = new Date('2019-05-09T01:00:00.000Z')
+    jrD.txHash =
+      '0x0458b93fc1cc51807089ae2794ea80ce26abee69a4541bdf1181305290514839'
+    jrD.requester = '0x56F83bE0b26B1B4B641a2ecAd74b037e131989E2'
+    jrD.requestId =
+      '0xc4cb943023a30d9102406799150bae23665517ab4b230d41b54490baa3aad42c'
+    await db.manager.save(jrD)
   })
 
   it('returns no results when no query is supplied', async () => {
@@ -135,5 +149,31 @@ describe('search', () => {
     const txHash = 'fixtureBTxHash'
     const results = await search(db, { searchQuery: txHash })
     expect(results).toHaveLength(1)
+  })
+
+  it('finds matches for search tokens with or without 0x prefixes', async () => {
+    const txHash =
+      '0458b93fc1cc51807089ae2794ea80ce26abee69a4541bdf1181305290514839'
+    const requester = '56F83bE0b26B1B4B641a2ecAd74b037e131989E2'
+    const requestId =
+      'c4cb943023a30d9102406799150bae23665517ab4b230d41b54490baa3aad42c'
+    const resultsTxHash = await search(db, { searchQuery: txHash })
+    const resultsRequester = await search(db, { searchQuery: requester })
+    const resultsRequestId = await search(db, { searchQuery: requestId })
+    expect(resultsTxHash).toHaveLength(1)
+    expect(resultsRequester).toHaveLength(1)
+    expect(resultsRequestId).toHaveLength(1)
+    const resultsPrefixedTxHash = await search(db, {
+      searchQuery: '0x' + txHash
+    })
+    const resultsPrefixedRequester = await search(db, {
+      searchQuery: '0x' + requester
+    })
+    const resultsPrefixedRequestId = await search(db, {
+      searchQuery: '0x' + requestId
+    })
+    expect(resultsPrefixedTxHash).toHaveLength(1)
+    expect(resultsPrefixedRequester).toHaveLength(1)
+    expect(resultsPrefixedRequestId).toHaveLength(1)
   })
 })
