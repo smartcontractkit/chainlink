37d7b314c4a0ae42998c7ac8d333e6f69399df9e
NavyAdmiral <keccaksha3@gmail.com>
2019-05-27 18:02:33 -0400

    test

--- operator_ui/support/test-helpers/mountWithTheme.js
+++ operator_ui/support/test-helpers/mountWithTheme.js
@@ -2,25 +2,15 @@ import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import { mount } from 'enzyme'
 import { Provider } from 'react-redux'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from 'theme'
 import createStore from 'connectors/redux'
-import { SheetsRegistry } from 'jss'
-import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 
-const sheetsRegistry = new SheetsRegistry()
-const muiTheme = createMuiTheme(theme)
-const generateClassName = createGenerateClassName()
 export default (children, opts = {}) =>
   mount(
-    <StylesProvider
-      registry={sheetsRegistry}
-      generateClassName={generateClassName}
-    >
-      <MuiThemeProvider theme={muiTheme} sheetsManager={new Map()}>
-        <Provider store={createStore()}>
-          <MemoryRouter initialEntries={['/']}>{children}</MemoryRouter>
-        </Provider>
-      </MuiThemeProvider>
-    </StylesProvider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={createStore()}>
+        <MemoryRouter initialEntries={['/']}>{children}</MemoryRouter>
+      </Provider>
+    </MuiThemeProvider>
   )
