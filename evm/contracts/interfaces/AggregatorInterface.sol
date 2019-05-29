f4cb6633441d2ed5bc027cc9d056702b073ae778
Steve Ellis <email@steveell.is>
2019-05-29 14:23:50 -0400

    rename {CurrentAnswer, Aggregator}Interface

--- evm/contracts/interfaces/AggregatorInterface.sol
+++ evm/contracts/interfaces/AggregatorInterface.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.24;
 
-interface CurrentAnswerInterface {
+interface AggregatorInterface {
   function currentAnswer() external returns (int256);
   function updatedHeight() external returns (uint256);
 }
