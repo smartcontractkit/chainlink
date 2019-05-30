2abe05f3ad87435451a9c83d5c212652db5c7de3
Dimitri Roche <dimroc@gmail.com>
2019-05-30 11:21:46 -0400

    exclude jobspec internal fields from operator_ui definition

--- operator_ui/src/utils/jobSpecDefinition.js
+++ operator_ui/src/utils/jobSpecDefinition.js
@@ -1,4 +1,19 @@
 const DEFINITION_KEYS = ['initiators', 'tasks', 'startAt', 'endAt']
+const SCRUBBED_KEYS = ['ID', 'CreatedAt', 'DeletedAt', 'UpdatedAt']
+
+const scrub = payload => {
+  if (Array.isArray(payload)) {
+    return payload.map(p => scrub(p))
+  }
+  if (typeof payload !== 'object' || payload === null) {
+    return payload
+  }
+  const keepers = Object.keys(payload).filter(k => !SCRUBBED_KEYS.includes(k))
+  return keepers.reduce((obj, key) => ({ ...obj, [key]: payload[key] }), {})
+}
 
 export default jobSpec =>
-  DEFINITION_KEYS.reduce((obj, key) => ({ ...obj, [key]: jobSpec[key] }), {})
+  DEFINITION_KEYS.reduce(
+    (obj, key) => ({ ...obj, [key]: scrub(jobSpec[key]) }),
+    {}
+  )
