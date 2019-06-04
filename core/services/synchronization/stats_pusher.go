72921215cb657c8599605059456ce4bc8b0e495d
John Barker <jebarker@gmail.com>
2019-06-04 13:06:16 -0600

    Add PushNow into StatsPusher callback

--- core/services/synchronization/stats_pusher.go
+++ core/services/synchronization/stats_pusher.go
@@ -23,6 +23,7 @@ type StatsPusher struct {
 	cancel         context.CancelFunc
 	clock          utils.Afterer
 	backoffSleeper backoff.Backoff
+	waker          chan struct{}
 }
 
 const (
@@ -39,55 +40,64 @@ func NewStatsPusher(orm *orm.ORM, url *url.URL, accessKey, secret string, afters
 		clock = afters[0]
 	}
 
-	var wsClient WebSocketClient
-	wsClient = noopWebSocketClient{}
-	if url != nil {
-		wsClient = NewWebSocketClient(url, accessKey, secret)
-		orm.DB.Callback().Create().Register(createCallbackName, createSyncEvent)
-		orm.DB.Callback().Update().Register(updateCallbackName, createSyncEvent)
-	}
-	return &StatsPusher{
+	sp := &StatsPusher{
 		ORM:      orm,
-		WSClient: wsClient,
-		Period:   5 * time.Second,
+		WSClient: noopWebSocketClient{},
+		Period:   30 * time.Minute,
 		clock:    clock,
 		backoffSleeper: backoff.Backoff{
 			Min: 1 * time.Second,
 			Max: 5 * time.Minute,
 		},
+		waker: make(chan struct{}, 1),
 	}
+
+	if url != nil {
+		sp.WSClient = NewWebSocketClient(url, accessKey, secret)
+		orm.DB.Callback().Create().Register(createCallbackName, createSyncEventWithStatsPusher(sp))
+		orm.DB.Callback().Update().Register(updateCallbackName, createSyncEventWithStatsPusher(sp))
+	}
+	return sp
 }
 
 // Start starts the stats pusher
-func (eq *StatsPusher) Start() error {
-	err := eq.WSClient.Start()
+func (sp *StatsPusher) Start() error {
+	err := sp.WSClient.Start()
 	if err != nil {
 		return err
 	}
 	ctx, cancel := context.WithCancel(context.Background())
-	eq.cancel = cancel
-	go eq.eventLoop(ctx)
+	sp.cancel = cancel
+	go sp.eventLoop(ctx)
 	return nil
 }
 
 // Close shuts down the stats pusher
-func (eq *StatsPusher) Close() error {
-	if eq.cancel != nil {
-		eq.cancel()
+func (sp *StatsPusher) Close() error {
+	if sp.cancel != nil {
+		sp.cancel()
 	}
-	eq.ORM.DB.Callback().Create().Remove(createCallbackName)
-	eq.ORM.DB.Callback().Update().Remove(updateCallbackName)
-	return eq.WSClient.Close()
+	sp.ORM.DB.Callback().Create().Remove(createCallbackName)
+	sp.ORM.DB.Callback().Update().Remove(updateCallbackName)
+	return sp.WSClient.Close()
+}
+
+// PushNow wakes up the stats pusher, asking it to push all queued events immediately.
+func (sp *StatsPusher) PushNow() {
+	select {
+	case sp.waker <- struct{}{}:
+	default:
+	}
 }
 
 type response struct {
 	Status int `json:"status"`
 }
 
-func (eq *StatsPusher) syncEvent(event *models.SyncEvent) error {
-	eq.WSClient.Send([]byte(event.Body))
+func (sp *StatsPusher) syncEvent(event *models.SyncEvent) error {
+	sp.WSClient.Send([]byte(event.Body))
 
-	message, err := eq.WSClient.Receive()
+	message, err := sp.WSClient.Receive()
 	if err != nil {
 		return err
 	}
@@ -102,7 +112,7 @@ func (eq *StatsPusher) syncEvent(event *models.SyncEvent) error {
 		return errors.New("event not created")
 	}
 
-	err = eq.ORM.DB.Delete(event).Error
+	err = sp.ORM.DB.Delete(event).Error
 	if err != nil {
 		return err
 	}
@@ -110,69 +120,77 @@ func (eq *StatsPusher) syncEvent(event *models.SyncEvent) error {
 	return nil
 }
 
-func (eq *StatsPusher) eventLoop(parentCtx context.Context) {
+func (sp *StatsPusher) eventLoop(parentCtx context.Context) {
 	for {
-		err := eq.pollEvents(parentCtx)
+		err := sp.pollEvents(parentCtx)
 		if err == nil {
 			return
 		}
 
-		duration := eq.backoffSleeper.Duration()
+		duration := sp.backoffSleeper.Duration()
 		logger.Errorw("Error during event synchronization", "error", err, "sleep_duration", duration)
 
 		select {
 		case <-parentCtx.Done():
 			return
-		case <-eq.clock.After(duration):
+		case <-sp.clock.After(duration):
 			continue
 		}
 	}
 }
 
-func (eq *StatsPusher) pollEvents(parentCtx context.Context) error {
+func (sp *StatsPusher) pollEvents(parentCtx context.Context) error {
 	logger.Debugw("Polling for events to synchronize")
 
 	for {
 		select {
 		case <-parentCtx.Done():
 			return nil
-		case <-eq.clock.After(eq.Period):
-			err := eq.ORM.AllSyncEvents(func(event *models.SyncEvent) error {
+		case <-sp.waker:
+		case <-sp.clock.After(sp.Period):
+			err := sp.ORM.AllSyncEvents(func(event *models.SyncEvent) error {
 				logger.Debugw("StatsPusher got event", "event", event.ID)
-				return eq.syncEvent(event)
+				return sp.syncEvent(event)
 			})
 
 			if err != nil {
 				return err
 			}
 
-			eq.backoffSleeper.Reset()
+			sp.backoffSleeper.Reset()
 		}
 	}
 }
 
-func createSyncEvent(scope *gorm.Scope) {
-	if scope.HasError() {
-		return
-	}
-
-	if scope.TableName() == "job_runs" {
-		run, ok := scope.Value.(*models.JobRun)
-		if !ok {
+func createSyncEventWithStatsPusher(sp *StatsPusher) func(*gorm.Scope) {
+	return func(scope *gorm.Scope) {
+		if scope.HasError() {
 			return
 		}
 
-		presenter := SyncJobRunPresenter{run}
-		bodyBytes, err := json.Marshal(presenter)
-		if err != nil {
-			scope.Err(err)
-			return
-		}
+		if scope.TableName() == "job_runs" {
+			run, ok := scope.Value.(*models.JobRun)
+			if !ok {
+				return
+			}
 
-		event := models.SyncEvent{
-			Body: string(bodyBytes),
+			presenter := SyncJobRunPresenter{run}
+			bodyBytes, err := json.Marshal(presenter)
+			if err != nil {
+				scope.Err(err)
+				return
+			}
+
+			event := models.SyncEvent{
+				Body: string(bodyBytes),
+			}
+			err = scope.DB().Save(&event).Error
+			if err != nil {
+				scope.Err(err)
+				return
+			}
+
+			sp.PushNow()
 		}
-		err = scope.DB().Save(&event).Error
-		scope.Err(err)
 	}
 }
