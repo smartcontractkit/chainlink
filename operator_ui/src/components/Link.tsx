3f36e085a9eadc26f1680f8d2c6eda2c70a031f4
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-06-10 15:14:05 -0700

    Fix warning about invalid DOM nesting

--- operator_ui/src/components/Link.tsx
+++ operator_ui/src/components/Link.tsx
@@ -8,7 +8,12 @@ import {
 } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import { grey } from '@material-ui/core/colors'
+import { ThemeStyle } from '@material-ui/core/styles/createTypography'
+import { PropTypes } from '@material-ui/core'
 import classNames from 'classnames'
+
+type Variant = ThemeStyle | 'srOnly'
+type Color = PropTypes.Color | 'textPrimary' | 'textSecondary' | 'error'
 
 const styles = (_theme: Theme) =>
   createStyles({
@@ -24,15 +29,22 @@ const styles = (_theme: Theme) =>
 interface IProps extends WithStyles<typeof styles> {
   children: React.ReactNode
   to: string
+  variant?: Variant
+  color?: Color
   className?: string
 }
 
-const Link = ({ children, classes, className, to }: IProps) => (
-  <ReactStaticLink to={to} className={classNames(classes.link, className)}>
-    <Typography variant="body1" color="inherit" className={classes.linkContent}>
-      {children}
-    </Typography>
-  </ReactStaticLink>
-)
+const Link = ({ children, classes, className, to, variant, color }: IProps) => {
+  const v = variant || 'body1'
+  const c = color || 'inherit'
+
+  return (
+    <ReactStaticLink to={to} className={classNames(classes.link, className)}>
+      <Typography variant={v} color={c} className={classes.linkContent}>
+        {children}
+      </Typography>
+    </ReactStaticLink>
+  )
+}
 
 export default withStyles(styles)(Link)
