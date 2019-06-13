d1444d4e275b9ac5a90d54fdcf974b8919e53246
NavyAdmiral <keccaksha3@gmail.com>
2019-06-13 19:52:13 -0400

    Trim down the redundant tests for random adapter

--- core/adapters/random_test.go
+++ core/adapters/random_test.go
@@ -10,35 +10,13 @@ import (
 )
 
 func TestRandom_Perform(t *testing.T) {
-	tests := []struct {
-		name    string
-		errored bool
-	}{
-		{"resulting string parses to a big.Int 1", false},
-		{"resulting string parses to a big.Int 2", false},
-		{"resulting string parses to a big.Int 3", false},
-		{"resulting string parses to a big.Int 4", false},
-		{"resulting string parses to a big.Int 5", false},
-	}
-
-	for _, tt := range tests {
-		test := tt
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-			input := models.RunResult{}
-			adapter := adapters.Random{}
-			result := adapter.Perform(input, nil)
-
-			if test.errored {
-				assert.Error(t, result.GetError())
-			} else {
-				val, err := result.ResultString()
-				assert.NoError(t, err)
-				assert.NoError(t, result.GetError())
-				res := new(big.Int)
-				res, ok := res.SetString(val, 10)
-				assert.True(t, ok)
-			}
-		})
-	}
+	input := models.RunResult{}
+	adapter := adapters.Random{}
+	result := adapter.Perform(input, nil)
+	val, err := result.ResultString()
+	assert.NoError(t, err)
+	assert.NoError(t, result.GetError())
+	res := new(big.Int)
+	res, ok := res.SetString(val, 10)
+	assert.True(t, ok)
 }
