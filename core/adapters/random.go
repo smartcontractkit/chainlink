d385d62ad95f8530749f78f1e64cbee0c1fc450c
NavyAdmiral <keccaksha3@gmail.com>
2019-06-13 00:19:42 -0400

    Stop parametrizing range and start generating a random uint256

--- core/adapters/random.go
+++ core/adapters/random.go
@@ -2,55 +2,27 @@ package adapters
 
 import (
 	"crypto/rand"
-	"fmt"
 	"math/big"
-	"strconv"
 
 	"github.com/smartcontractkit/chainlink/core/store"
 	"github.com/smartcontractkit/chainlink/core/store/models"
-	"github.com/smartcontractkit/chainlink/core/utils"
 )
 
-// Bounds represents the start/end param for the Random adapter
-type Bounds int64
+// Random adapter type holds no fields
+type Random struct{}
 
-// UnmarshalJSON implements json.Unmarshaler for Random
-func (r *Bounds) UnmarshalJSON(input []byte) error {
-	input = utils.RemoveQuotes(input)
-	bound, err := strconv.ParseInt(string(input), 10, 64)
-	if err != nil {
-		return fmt.Errorf("cannot parse into float: %s", input)
-	}
-	*r = Bounds(bound)
-	return nil
-}
-
-// Random holds the range given by start and end parameters
-type Random struct {
-	Start Bounds `json:"start"`
-	End   Bounds `json:"end"`
-}
-
-// Perform returns a random integer in the inclusive range as
-// specified by Start and End parameters.
+// Perform returns a random uint256 number in 0 | 2**256-1 range
 func (ra *Random) Perform(input models.RunResult, _ *store.Store) models.RunResult {
-	start := int64(ra.Start)
-	end := int64(ra.End)
-	if start == 0 && end == 0 {
-		input.SetError(fmt.Errorf("Both start and end ranges must be specified as parameters"))
-		return input
-	}
-	if start >= end {
-		input.SetError(fmt.Errorf("End must be strictly greater than start"))
-		return input
-	}
-	diff := big.NewInt(end - start)
-	val, err := rand.Int(rand.Reader, diff)
+	var uint256Max big.Int
+	one := big.NewInt(1)
+	base := big.NewInt(2)
+	exp := big.NewInt(256)
+	uint256Max.Sub(base.Exp(base, exp, nil), one)
+	randVal, err := rand.Int(rand.Reader, &uint256Max)
 	if err != nil {
 		input.SetError(err)
 		return input
 	}
-	randNumInRange := val.Int64() + start
-	input.ApplyResult(strconv.FormatInt(randNumInRange, 10))
+	input.ApplyResult(randVal.String())
 	return input
 }
