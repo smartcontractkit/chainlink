f8a9b863113206f1bd91d99b0ff94e365ca731fe
NavyAdmiral <keccaksha3@gmail.com>
2019-06-17 14:16:24 -0400

    Rename To Capital Cased Interfaces

--- operator_ui/@types/operator_ui/index.d.ts
+++ operator_ui/@types/operator_ui/index.d.ts
@@ -1,7 +1,7 @@
 import * as dbTypes from "../db"
 import { status, adapterTypes } from "../constants"
 
-interface runResult {
+interface RunResult {
     data: { result: string | null }
     error: boolean | null
     jobRunId: string
@@ -9,38 +9,38 @@ interface runResult {
     status: status
 }
 
-export interface IBridgeType extends Omit<dbTypes.bridgeType, 'incomingTokenHash' | 'salt'> { }
+export interface IBridgeType extends Omit<dbTypes.BridgeType, 'incomingTokenHash' | 'salt'> { }
 
 //REVIEW what to do with this?
-export interface IExternalInitiator extends Omit<dbTypes.externalInitiator, 'salt' | 'hashedSecret' | 'accessKey'> { }
+export interface IExternalInitiator extends Omit<dbTypes.ExternalInitiator, 'salt' | 'hashedSecret' | 'accessKey'> { }
 
 export interface IInitiator extends dbTypes.Initiator { }
 
-export interface IJobRun extends Omit<dbTypes.jobRun, 'initiatorId' | 'overridesId' | 'jobSpecId'> {
+export interface IJobRun extends Omit<dbTypes.JobRun, 'initiatorId' | 'overridesId' | 'jobSpecId'> {
     initiator: dbTypes.Initiator
     jobId: string
-    overrides: runResult
-    result: runResult
+    overrides: RunResult
+    result: RunResult
     taskRuns: ITaskRuns
 }
 
-export interface IJobSpec extends dbTypes.jobSpec {
+export interface IJobSpec extends dbTypes.JobSpec {
     initiators: dbTypes.Initiator
     tasks: ITaskSpecs
     runs: ITaskRuns
 }
 
-export interface ITaskRun extends Omit<dbTypes.taskRun, 'taskSpecId' | 'jobRunId' | 'resultId'> {
-    result: runResult
+export interface ITaskRun extends Omit<dbTypes.TaskRun, 'taskSpecId' | 'jobRunId' | 'resultId'> {
+    result: RunResult
     task: ITaskSpec
     updatedAt: Date
     type: adapterTypes
 }
 
-export interface ITaskSpec extends Omit<dbTypes.taskSpec, 'jobSpecId'> { }
+export interface ITaskSpec extends Omit<dbTypes.TaskSpec, 'jobSpecId'> { }
 
 //REVIEW Not needed?
-export interface ITxAttempt extends dbTypes.txAttempt { }
+export interface ITxAttempt extends dbTypes.TxAttempt { }
 
 export interface ITransaction extends Omit<dbTypes.Tx, 'surrogateId' | 'signedRawTx'> {
     rawHex: string
