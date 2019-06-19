b38f31ea3b022da19640d16a6d5dd154f484d833
Alex Coventry <alx@mit.edu>
2019-06-19 19:43:55 -0400

    Complete automation of test

---
+++ integration/forks/log_in_to_chainlink_node.sh
@@ -0,0 +1,10 @@
+login() {
+    docker exec -it forks_chainlink_1 chainlink \
+           login -f /run/secrets/node_api
+}
+
+# Keep trying to log in until chainlink has started its RPC service
+until login
+do
+    sleep 0.1
+done
