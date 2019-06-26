f7635f8e0a3c26008c81a872775bb4a88827984b
NavyAdmiral <keccaksha3@gmail.com>
2019-06-25 22:25:33 -0400

    Reflect changes in reducers for updated global state

--- operator_ui/src/connectors/redux/reducers/jobs.js
+++ operator_ui/src/connectors/redux/reducers/jobs.js
@@ -1,3 +1,5 @@
+import { RECEIVE_DELETE_SUCCESS } from '../../../actions'
+
 const initialState = {
   items: {},
   currentPage: null,
@@ -36,6 +38,16 @@ export default (state = initialState, action = {}) => {
     case UPSERT_JOB: {
       return Object.assign({}, state, {
         items: Object.assign({}, state.items, action.data.specs)
+      })
+    }
+    case RECEIVE_DELETE_SUCCESS: {
+      return Object.assign({}, state, {
+        items: Object.assign(
+          {},
+          Object.keys(state.items)
+            .filter(i => i !== action.response)
+            .reduce((res, key) => ((res[key] = state.items[key]), res), {})
+        )
       })
     }
     default:
