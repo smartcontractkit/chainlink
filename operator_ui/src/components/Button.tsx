692ea839cc8146af30153883534fac645bd819d1
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-07-18 16:26:29 -0700

    Convert OperatorUI Button to Typescript
    - Replace runtime checks with type annotations

---
+++ operator_ui/src/components/Button.tsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles'
+import { PropTypes as MuiPropTypes } from '@material-ui/core'
+import MuiButton, {
+  ButtonProps as MuiButtonProps
+} from '@material-ui/core/Button'
+import { TouchRippleProps } from '@material-ui/core/ButtonBase/TouchRipple'
+import classNames from 'classnames'
+
+const styles = (theme: Theme) =>
+  createStyles({
+    default: {
+      borderColor: '#BECAD6',
+      '&:hover': {
+        backgroundColor: theme.palette.common.white,
+        borderColor: '#BECAD6',
+        boxShadow:
+          '0 2px 4px 0 rgba(0,123,255,0.06), 0 2px 2px 0 rgba(0,0,0,0.06)'
+      }
+    },
+    primary: {
+      boxShadow: '0 0',
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+      '&:hover': {
+        backgroundColor: theme.palette.primary.main,
+        boxShadow:
+          '0 2px 4px 0 rgba(0,123,255,0.19), 0 2px 2px 0 rgba(0,0,0,0.15)'
+      }
+    },
+    secondary: {
+      '&:hover': {
+        backgroundColor: theme.palette.common.white
+      }
+    },
+    danger: {
+      borderColor: theme.palette.error.main,
+      color: theme.palette.error.main,
+      '&:hover': {
+        backgroundColor: theme.palette.common.white,
+        borderColor: theme.palette.error.main,
+        boxShadow:
+          '0 2px 4px 0 rgba(0,123,255,0.06), 0 2px 2px 0 rgba(0,0,0,0.06)'
+      }
+    },
+    defaultRipple: {
+      color: '#818EA3'
+    }
+  })
+
+// Unfortunately @material-ui/core does not export the type so we have to redefine it.
+type MuiButtonVariant =
+  | 'text'
+  | 'flat'
+  | 'outlined'
+  | 'contained'
+  | 'raised'
+  | 'fab'
+  | 'extendedFab'
+  | undefined
+
+export type ButtonVariant =
+  | MuiButtonVariant
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'default'
+
+const muiProps = (variant: ButtonVariant, classes: any): MuiButtonProps => {
+  switch (variant) {
+    case 'primary':
+      return { variant: 'contained' }
+    case 'secondary':
+      return { variant: 'outlined', color: 'primary' }
+    case 'danger':
+      return { variant: 'outlined', color: 'primary' }
+    default: {
+      return {
+        variant: 'outlined',
+        color: 'secondary',
+        TouchRippleProps: {
+          classes: {
+            root: classes.defaultRipple
+          }
+        }
+      }
+    }
+  }
+}
+
+interface IProps extends WithStyles<typeof styles> {
+  children: React.ReactNode
+  component?: React.ReactNode
+  onClick?: React.MouseEventHandler<JSX.Element>
+  type?: string
+  disabled?: boolean
+  className?: string
+  variant?: ButtonVariant
+  // Ideally this would be typed as below. However the MuiButton type annotations
+  // don't allow an object to be passed through.
+  //
+  // href?:
+  //   | string
+  //   | {
+  //       pathname: string
+  //       state: { definition: object }
+  //     }
+  href?: any
+}
+
+const Button = ({
+  variant = 'default',
+  disabled,
+  type,
+  component,
+  href,
+  classes,
+  className,
+  children,
+  onClick
+}: IProps) => {
+  const curryProps = Object.assign(
+    { component, disabled, href, onClick, type },
+    muiProps(variant, classes)
+  )
+  const cn = classNames(classes[variant as keyof typeof classes], className)
+
+  return (
+    <MuiButton {...curryProps} className={cn}>
+      {children}
+    </MuiButton>
+  )
+}
+
+export default withStyles(styles)(Button)
