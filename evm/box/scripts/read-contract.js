d12d6d56ba5770e0028bf30f248562df38f96f56
Thomas Hodges <me@thodges.com>
2019-07-22 14:48:38 -0500

    Add scripts to our Truffle Box

---
+++ evm/box/scripts/read-contract.js
@@ -0,0 +1,12 @@
+let MyContract = artifacts.require('MyContract')
+
+/*
+  This script makes it easy to read the data variable
+  of the requesting contract.
+*/
+
+module.exports = async (callback) => {
+  let mc = await MyContract.deployed()
+  let data = await mc.data.call()
+  callback(data)
+}
