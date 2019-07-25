4f84f08da00431d0e1e7d71ff9b2433b438220cb
NavyAdmiral <keccaksha3@gmail.com>
2019-07-25 16:55:26 -0400

    Fix pg query

--- core/store/migrations/migration1560881855/migrate.go
+++ core/store/migrations/migration1560881855/migrate.go
@@ -15,25 +15,17 @@ func Migrate(tx *gorm.DB) error {
 	if dbutil.IsPostgres(tx) {
 		fillLinkEarned = `
 		INSERT INTO link_earned
-		SELECT * FROM
-		(
-		SELECT ROW_NUMBER() OVER (ORDER BY job_spec_id) AS id, job_spec_id, job_runs.id AS job_run_id, amount, finished_at		FROM job_runs
-		FROM
-		(
-		job_runs
-		INNER JOIN run_results ON job_runs.overrides_id  = run_results.id
-		) AS postgres_syntax
-		) AS another_postgres_syntax
-		WHERE amount IS NOT NULL`
+		SELECT ROW_NUMBER() OVER (ORDER BY job_spec_id) AS id, job_spec_id, jr.id AS job_run_id, amount, finished_at
+		FROM job_runs jr INNER JOIN run_results rr ON jr.overrides_id  = rr.id
+		WHERE amount IS NOT NULL
+		`
 	} else {
 		fillLinkEarned = `
 		INSERT INTO link_earned
-		SELECT * FROM
-		(
-		SELECT ROW_NUMBER() OVER (ORDER BY job_spec_id) AS id, job_spec_id, job_runs.id AS job_run_id, amount, finished_at		FROM job_runs
+		SELECT ROW_NUMBER() OVER (ORDER BY job_spec_id) AS id, job_spec_id, job_runs.id AS job_run_id, amount, finished_at FROM job_runs
 		INNER JOIN run_results ON job_runs.overrides_id  = run_results.id
 		WHERE amount IS NOT NULL
-		)`
+		`
 	}
 	if err := tx.Exec(fillLinkEarned).Error; err != nil {
 		return errors.Wrap(err, "failed to fill existing run rewards to link_earned table")
