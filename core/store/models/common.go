dfd3321707e351663db14fb32475893e26e427f2
Steve Ellis <email@steveell.is>
2019-08-02 17:14:38 -0400

    use strings when parsing db results when necessary

--- core/store/models/common.go
+++ core/store/models/common.go
@@ -140,12 +140,12 @@ func (j JSON) Value() (driver.Value, error) {
 
 // Scan reads the database value and returns an instance.
 func (j *JSON) Scan(value interface{}) error {
-	temp, ok := value.([]byte)
+	temp, ok := value.(string)
 	if !ok {
-		return fmt.Errorf("Unable to convert %v of %T to Time", value, value)
+		return fmt.Errorf("Unable to convert %v of %T to JSON", value, value)
 	}
 
-	*j = JSON{Result: gjson.Parse(string(temp))}
+	*j = JSON{Result: gjson.Parse(temp)}
 	return nil
 }
 
