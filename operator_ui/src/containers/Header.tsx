933a28212f732509d977bd0ca2a0921063a09b22
typescribe <53539231+typescribe@users.noreply.github.com>
2019-08-02 21:18:29 -0400

    Type use-react-hooks package
    * Fix type errors in Header.tsx from update
    * Update react types to 16.8 to re-use hook types

--- operator_ui/src/containers/Header.tsx
+++ operator_ui/src/containers/Header.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { useHooks, useState } from 'use-react-hooks'
+import AppBar from '@material-ui/core/AppBar'
+import MuiDrawer from '@material-ui/core/Drawer'
+import Grid from '@material-ui/core/Grid'
+import Hidden from '@material-ui/core/Hidden'
+import IconButton from '@material-ui/core/IconButton'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Portal from '@material-ui/core/Portal'
 import {
   createStyles,
   Theme,
   withStyles,
   WithStyles
 } from '@material-ui/core/styles'
-import BaseLink from '../components/BaseLink'
-import { connect } from 'react-redux'
-import { bindActionCreators, Dispatch } from 'redux'
-import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
 import Typography from '@material-ui/core/Typography'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import MuiDrawer from '@material-ui/core/Drawer'
-import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import Portal from '@material-ui/core/Portal'
-import ReactResizeDetector from 'react-resize-detector'
 import classNames from 'classnames'
+import React from 'react'
+import { connect } from 'react-redux'
+import ReactResizeDetector from 'react-resize-detector'
+import { bindActionCreators, Dispatch } from 'redux'
+import { useHooks, useState } from 'use-react-hooks'
+import { submitSignOut } from '../actions'
+import AvatarMenu from '../components/AvatarMenu'
+import BaseLink from '../components/BaseLink'
 import LoadingBar from '../components/LoadingBar'
 import MainLogo from '../components/Logos/Main'
-import AvatarMenu from '../components/AvatarMenu'
-import { submitSignOut } from '../actions'
 import fetchCountSelector from '../selectors/fetchCount'
 
 const SHARED_NAV_ITEMS = [
@@ -56,8 +56,8 @@ const drawerStyles = ({ palette, spacing }: Theme) =>
 interface IDrawerProps extends WithStyles<typeof drawerStyles> {
   authenticated: boolean
   drawerOpen: boolean
-  toggleDrawer: () => undefined
-  submitSignOut: () => undefined
+  toggleDrawer: () => void
+  submitSignOut: () => void
 }
 
 const Drawer = withStyles(drawerStyles)(
@@ -189,8 +189,8 @@ interface IProps extends WithStyles<typeof styles> {
   fetchCount: number
   authenticated: boolean
   drawerContainer: Element
-  submitSignOut: () => undefined
-  onResize: () => undefined
+  submitSignOut: () => void
+  onResize: () => void
   url?: string
 }
 
