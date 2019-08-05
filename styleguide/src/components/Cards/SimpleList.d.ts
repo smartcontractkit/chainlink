b51628ec6291f22391306aedb3e6164873149be3
typescribe <53539231+typescribe@users.noreply.github.com>
2019-08-05 16:17:22 -0400

    Remove build artifacts from source control

--- styleguide/src/components/Cards/SimpleList.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface IProps {
-    children: React.ReactNode;
-    title: string;
-}
-declare const SimpleList: ({ children, title }: IProps) => JSX.Element;
-export default SimpleList;
