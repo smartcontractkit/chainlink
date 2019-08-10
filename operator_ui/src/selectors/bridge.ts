a7b5e7272d90546ecd01e162292f8e9b7d413000
typescribe <53539231+typescribe@users.noreply.github.com>
2019-08-10 14:44:23 -0400

    Change IState -> AppState, Cleanup Imports

--- operator_ui/src/selectors/bridge.ts
+++ operator_ui/src/selectors/bridge.ts
@@ -1,6 +1,6 @@
+import { AppState } from 'connectors/redux/reducers'
 import build from 'redux-object'
-import { IState } from '../connectors/redux/reducers/index'
 
-export default ({ bridges }: Pick<IState, 'bridges'>, id: string) => {
+export default ({ bridges }: Pick<AppState, 'bridges'>, id: string) => {
   return build(bridges, 'items', id)
 }
