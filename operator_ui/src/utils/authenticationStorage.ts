f2886f91f3ad3ab50675b6e34a3d808c12f21b62
typescribe <53539231+typescribe@users.noreply.github.com>
2019-08-10 14:44:42 -0400

    Fix type error in authenticationStorage

--- operator_ui/src/utils/authenticationStorage.ts
+++ operator_ui/src/utils/authenticationStorage.ts
@@ -1,5 +1,6 @@
-import * as storage from 'utils/storage'
+import * as storage from 'utils/storage';
 
 export const get = () => storage.get('authentication')
 
-export const set = obj => storage.set('authentication', obj)
+// CHECK ME
+export const set = (obj: any) => storage.set('authentication', obj)
