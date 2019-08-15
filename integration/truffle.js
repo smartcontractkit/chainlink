6e11b5f188a5e405c904cba8c81453f7a50989b8
Ryan Hall <hall.ryan.r@gmail.com>
2019-08-15 19:58:57 -0400

    create truffle project in integrations folder

---
+++ integration/truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: '0.4.24'
+    }
+  },
+  networks: {
+    cldev: {
+      host: '127.0.0.1',
+      port: 18545,
+      network_id: '*',
+      gas: 4700000,
+      gasPrice: 5e9
+    }
+  }
+}
