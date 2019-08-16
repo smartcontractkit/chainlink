69c7442bd5695086bbec2441a97d4f5806fc7e0c
John Barker <dev@j16r.net>
2019-08-16 15:41:47 -0600

    Add migration for JobIDs as uuid

--- core/store/migrations/migration1565291711/migrate.go
+++ core/store/migrations/migration1565291711/migrate.go
@@ -1,4 +1,4 @@
-package migration1565210496
+package migration1565291711
 
 import (
 	"github.com/jinzhu/gorm"
@@ -10,8 +10,48 @@ import (
 func Migrate(tx *gorm.DB) error {
 	if dbutil.IsPostgres(tx) {
 		if err := tx.Exec(`
+ALTER TABLE job_runs ADD COLUMN "id_uuid" uuid;
+UPDATE job_runs
+SET
+	"id_uuid" = CAST("id" as uuid);
+ALTER TABLE job_runs DROP CONSTRAINT "job_runs_pkey" CASCADE;
 	`).Error; err != nil {
-			return errors.Wrap(err, "failed to update ids on job_runs")
+			return errors.Wrap(err, "failed to add id_uuid on job_runs")
+		}
+
+		if err := tx.Exec(`
+ALTER TABLE task_runs ADD COLUMN "job_run_id_uuid" uuid;
+UPDATE task_runs
+SET
+	"job_run_id_uuid" = CAST("job_run_id" as uuid);
+	`).Error; err != nil {
+			return errors.Wrap(err, "failed to add job_run_id_uuid on task_runs")
+		}
+
+		if err := tx.Exec(`
+ALTER TABLE job_runs DROP COLUMN "id";
+ALTER TABLE job_runs RENAME COLUMN "id_uuid" TO "id";
+ALTER TABLE job_runs ADD CONSTRAINT "job_run_pkey" PRIMARY KEY ("id");
+	`).Error; err != nil {
+			return errors.Wrap(err, "failed to rename id on job_runs")
+		}
+
+		if err := tx.Exec(`
+ALTER TABLE task_runs DROP COLUMN "job_run_id";
+ALTER TABLE task_runs RENAME COLUMN "job_run_id_uuid" TO "job_run_id";
+
+ALTER TABLE task_runs ADD CONSTRAINT "task_runs_job_run_id_fkey" FOREIGN KEY ("job_run_id") REFERENCES job_runs ("id") MATCH FULL;
+	`).Error; err != nil {
+			return errors.Wrap(err, "failed to update job_run_id id on task_runs")
+		}
+
+		if err := tx.Exec(`
+ALTER TABLE task_runs ADD COLUMN "id_uuid" uuid;
+UPDATE task_runs SET "id_uuid" = CAST("id" as uuid);
+ALTER TABLE task_runs DROP COLUMN "id";
+ALTER TABLE task_runs RENAME COLUMN "id_uuid" TO "id";
+	`).Error; err != nil {
+			return errors.Wrap(err, "failed to update id on task_runs")
 		}
 	}
 
