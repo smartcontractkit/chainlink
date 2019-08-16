adb152fe12fdca59ec9e4735d8fc4d846da4d14a
felder-cli <felderhof@smartcontract.com>
2019-08-16 11:49:05 +0200

    Use previously defined migration0

--- core/store/migrations/migration1565877314/migrate.go
+++ core/store/migrations/migration1565877314/migrate.go
@@ -3,19 +3,9 @@ package migration1565877314
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
+	"github.com/smartcontractkit/chainlink/core/store/migrations/migration0"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 )
-
-type ExternalInitiatorOld struct {
-	*gorm.Model
-	AccessKey    string
-	Salt         string
-	HashedSecret string
-}
-
-func (ExternalInitiatorOld) TableName() string {
-	return "external_initiators"
-}
 
 type ExternalInitiator struct {
 	*gorm.Model
@@ -27,7 +17,7 @@ type ExternalInitiator struct {
 }
 
 // newExternalInitiator creates a new row, setting the Name to the AccessKey
-func newExternalInitiator(arg ExternalInitiatorOld) ExternalInitiator {
+func newExternalInitiator(arg migration0.ExternalInitiator) ExternalInitiator {
 	url, _ := models.NewWebURL("https://unset.url")
 	return ExternalInitiator{
 		Model: arg.Model,
@@ -42,13 +32,13 @@ func newExternalInitiator(arg ExternalInitiatorOld) ExternalInitiator {
 
 // Migrate adds External Initiator Name and URL fields.
 func Migrate(tx *gorm.DB) error {
-	var exis []ExternalInitiatorOld
+	var exis []migration0.ExternalInitiator
 	if err := tx.Find(&exis).Error; err != nil {
 		return errors.Wrap(err, "could not load all External Intitiators")
 	}
 
 	// Make new table
-	if err := tx.DropTable(ExternalInitiatorOld{}).Error; err != nil {
+	if err := tx.DropTable(migration0.ExternalInitiator{}).Error; err != nil {
 		return errors.Wrap(err, "could not drop old External Intitiator table")
 	}
 	if err := tx.AutoMigrate(&ExternalInitiator{}).Error; err != nil {
