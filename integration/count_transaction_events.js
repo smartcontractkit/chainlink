f2f8d5a8620ef788892823c3b30a891f413014a6
Ryan Hall <hall.ryan.r@gmail.com>
2019-08-22 12:18:38 -0400

    convert to using migrations - all tests passing

---
+++ integration/count_transaction_events.js
@@ -0,0 +1,41 @@
+// truffle script
+
+const commandLineArgs = require('command-line-args')
+const { abort, port } = require('./common.js')
+
+// compand line options
+const optionDefinitions = [
+  { name: 'args', type: String, multiple: true, defaultOption: true },
+  { name: 'compile', type: Boolean },
+  { name: 'network', type: String }
+]
+
+const main = async () => {
+  // parse command line args
+  const options = commandLineArgs(optionDefinitions)
+  const [txID, fromAddress] = options.args.slice(2)
+
+  // find transaction
+  const transaction = await web3.eth
+    .getTransactionReceipt(txID)
+    .catch(abort('Error getting transaction receipt'))
+
+  // count events in transaction
+  let count = 0
+  for (let log of transaction.logs) {
+    if (log.address.toLowerCase() === fromAddress.toLowerCase()) {
+      count += 1
+    }
+  }
+  console.log(`events from ${fromAddress} in ${txID}: ${count}`)
+}
+
+// truffle exec won't capture errors automatically
+module.exports = async callback => {
+  try {
+    await main()
+    callback()
+  } catch (error) {
+    callback(error)
+  }
+}
