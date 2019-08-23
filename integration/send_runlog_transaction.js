6ecf5a5b87c332d98b1bd4c9d37ceb8a35f5f731
Ryan Hall <hall.ryan.r@gmail.com>
2019-08-22 21:49:51 -0400

    cleanup

--- integration/send_runlog_transaction.js
+++ integration/send_runlog_transaction.js
@@ -7,8 +7,6 @@ const RunLog = artifacts.require('RunLog')
 const LinkToken = artifacts.require('LinkToken')
 const { CHAINLINK_URL, ECHO_SERVER_URL } = process.env
 
-const runLog = await RunLog.deployed()
-const linkToken = await LinkToken.deployed()
 const sessionsUrl = url.resolve(CHAINLINK_URL, '/sessions')
 const specsUrl = url.resolve(CHAINLINK_URL, '/v2/specs')
 const credentials = { email: 'notreal@fakeemail.ch', password: 'twochains' }
@@ -16,10 +14,10 @@ const amount = web3.utils.toBN(web3.utils.toWei('1000'))
 
 const futureOffset = seconds => parseInt(new Date().getTime() / 1000) + seconds
 
-const generateJob = () => ({
+const generateJob = requesterAddress => ({
   _comment:
     'A runlog has a jobid baked into the contract so chainlink knows which job to run.',
-  initiators: [{ type: 'runlog', params: { requesters: [runLog.address] } }],
+  initiators: [{ type: 'runlog', params: { requesters: [requesterAddress] } }],
   tasks: [
     // 10 seconds to ensure the time has not elapsed by the time the run is triggered
     { type: 'Sleep', params: { until: futureOffset(10) } },
@@ -32,6 +30,9 @@ const generateJob = () => ({
 })
 
 const main = async () => {
+  const runLog = await RunLog.deployed()
+  const linkToken = await LinkToken.deployed()
+
   await linkToken
     .transfer(runLog.address, amount, {
       gas: 100000,
@@ -40,7 +41,7 @@ const main = async () => {
     .catch(abort('Error transferring link to RunLog'))
   console.log(`Transferred ${amount} to RunLog at: ${runLog.address}`)
 
-  const job = generateJob()
+  const job = generateJob(runLog.address)
 
   await request.post(sessionsUrl, { json: credentials })
   let Job = await request
