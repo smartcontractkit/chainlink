fb97ded00972fccc39fbd72435170f33d733d2d8
Ryan Hall <hall.ryan.r@gmail.com>
2019-08-24 13:25:51 -0400

    add vendor contracts

---
+++ evm/contracts/vendor/SignedSafeMath.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.4.24;
+
+library SignedSafeMath {
+
+  /**
+   * @dev Adds two int256s and makes sure the result doesn't overflow. Signed
+   * integers aren't supported by the SafeMath library, thus this method
+   * @param _a The first number to be added
+   * @param _a The second number to be added
+   */
+  function add(int256 _a, int256 _b)
+    internal
+    pure
+    returns (int256)
+  {
+    int256 c = _a + _b;
+    require((_b >= 0 && c >= _a) || (_b < 0 && c < _a), "SignedSafeMath: addition overflow");
+
+    return c;
+  }
+}
