6c9e87a40a9522dbde07eb23e4b92151f9b9d9d6
typescribe <53539231+typescribe@users.noreply.github.com>
2019-08-26 19:22:10 -0400

    Fix typo

--- operator_ui/src/selectors/jobRunsByJobId.ts
+++ operator_ui/src/selectors/jobRunsByJobId.ts
@@ -1,11 +1,11 @@
-import { AppState } from 'connectors/redux/reducers';
-import { IJobRun } from 'operator_ui';
-import build from 'redux-object';
+import { AppState } from 'connectors/redux/reducers'
+import { IJobRun } from 'operator_ui'
+import build from 'redux-object'
 
 export default (state: AppState, jobId: string, take: number) => {
   return build(state.jobRuns, 'items')
     .filter((r: IJobRun) => r.jobId === jobId)
-    .sort((a: IJobRun, b: IJobRun) => {\
+    .sort((a: IJobRun, b: IJobRun) => {
       const dateA = new Date(a.createdAt)
       const dateB = new Date(b.createdAt)
 
