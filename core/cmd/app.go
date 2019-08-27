236585a3f351a49619d426d757d07b78d3132057
Steve Ellis <email@steveell.is>
2019-08-27 13:44:24 +0200

    fix up CLI documentation

--- core/cmd/app.go
+++ core/cmd/app.go
@@ -27,11 +27,136 @@ func NewApp(client *Client) *cli.App {
 	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "local",
-			Usage: "Commands which are run locally",
+			Name:  "admin",
+			Usage: "Commands for remotely taking admin related actions",
 			Subcommands: []cli.Command{
 				{
-					Name:    "node",
+					Name:   "chpass",
+					Usage:  "Change your account password remotely",
+					Action: client.ChangePassword,
+				},
+				{
+					Name:   "info",
+					Usage:  "Display the Account's address with its ETH & LINK balances",
+					Action: client.DisplayAccountBalance,
+				},
+				{
+					Name:   "login",
+					Usage:  "Login to remote client by creating a session cookie",
+					Action: client.RemoteLogin,
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "file, f",
+							Usage: "text file holding the API email and password needed to create a session cookie",
+						},
+					},
+				},
+				{
+					Name:        "withdraw",
+					Usage:       "Withdraw to <address>, <amount> units of LINK from the configured Oracle Contract",
+					Description: "Only works if the Chainlink node is the owner of the contract being withdrawn from",
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "from",
+							Usage: "override the configured oracle address to withdraw from",
+						},
+					},
+					Action: client.Withdraw,
+				},
+			},
+		},
+
+		{
+			Name:  "bridges",
+			Usage: "Commands for Bridges communicating with External Adapters",
+			Subcommands: []cli.Command{
+				{
+					Name:   "create",
+					Usage:  "Create a new Bridge to an External Adapter",
+					Action: client.CreateBridge,
+				},
+				{
+					Name:   "destroy",
+					Usage:  "Destroys the Bridge for an External Adapter",
+					Action: client.RemoveBridge,
+				},
+				{
+					Name:   "list",
+					Usage:  "List all Bridges to External Adapters",
+					Action: client.IndexBridges,
+					Flags: []cli.Flag{
+						cli.IntFlag{
+							Name:  "page",
+							Usage: "page of results to display",
+						},
+					},
+				},
+				{
+					Name:   "show",
+					Usage:  "Show an Bridge's details",
+					Action: client.ShowBridge,
+				},
+			},
+		},
+
+		{
+			Name:  "config",
+			Usage: "Commands for the node's configuration",
+			Subcommands: []cli.Command{
+				{
+					Name:   "setgasprice",
+					Usage:  "Set the minimum gas price to use for outgoing transactions",
+					Action: client.SetMinimumGasPrice,
+					Flags: []cli.Flag{
+						cli.BoolFlag{
+							Name:  "gwei",
+							Usage: "Specify amount in gwei",
+						},
+					},
+				},
+			},
+		},
+
+		{
+			Name:  "jobs",
+			Usage: "Commands for managing Jobs",
+			Subcommands: []cli.Command{
+				{
+					Name:   "archive",
+					Usage:  "Archive a Job and all its associated Runs",
+					Action: client.ArchiveJobSpec,
+				},
+				{
+					Name:   "create",
+					Usage:  "Create Job from a Job Specification JSON",
+					Action: client.CreateJobSpec,
+				},
+				{
+					Name:   "list",
+					Usage:  "List all jobs",
+					Action: client.IndexJobSpecs,
+					Flags: []cli.Flag{
+						cli.IntFlag{
+							Name:  "page",
+							Usage: "page of results to display",
+						},
+					},
+				},
+				{
+					Name:   "show",
+					Usage:  "Show a specific Job's details",
+					Action: client.ShowJobSpec,
+				},
+			},
+		},
+
+		{
+			Name:        "node",
+			Usage:       "Commands for admin actions that must be run locally",
+			Description: "Commands can only be run from on the same machine as the Chainlink node.",
+			Subcommands: []cli.Command{
+				{
+					Name:    "start",
 					Aliases: []string{"n"},
 					Flags: []cli.Flag{
 						cli.StringFlag{
@@ -51,9 +176,10 @@ func NewApp(client *Client) *cli.App {
 					Action: client.RunNode,
 				},
 				{
-					Name:   "deleteuser",
-					Usage:  "Erase the *local node's* user and corresponding session to force recreation on next node launch. Does not work remotely over API.",
-					Action: client.DeleteUser,
+					Name:        "deleteuser",
+					Usage:       "Erase the *local node's* user and corresponding session to force recreation on next node launch.",
+					Description: "Does not work remotely over API.",
+					Action:      client.DeleteUser,
 				},
 				{
 					Name:    "import",
@@ -63,79 +189,10 @@ func NewApp(client *Client) *cli.App {
 				},
 			},
 		},
-		{
-			Name:  "account",
-			Usage: "Display the account related info for remote admin access",
-			Subcommands: []cli.Command{
-				{
-					Name:   "show",
-					Usage:  "Display the account address with its ETH & LINK balances",
-					Action: client.DisplayAccountBalance,
-				},
-				{
-					Name:   "login",
-					Usage:  "Login to remote client by creating a session cookie",
-					Action: client.RemoteLogin,
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "file, f",
-							Usage: "text file holding the API email and password needed to create a session cookie",
-						},
-					},
-				},
-				{
-					Name:   "chpass",
-					Usage:  "Change your password",
-					Action: client.ChangePassword,
-				},
-				{
-					Name:  "withdraw",
-					Usage: "Withdraw to <address>, <amount> units of LINK from the configured oracle",
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "from",
-							Usage: "override the configured oracle address to withdraw from",
-						},
-					},
-					Action: client.Withdraw,
-				},
-			},
-		},
-		{
-			Name:  "jobs",
-			Usage: "Job related commands",
-			Subcommands: []cli.Command{
-				{
-					Name:   "list",
-					Usage:  "List all jobs",
-					Action: client.IndexJobSpecs,
-					Flags: []cli.Flag{
-						cli.IntFlag{
-							Name:  "page",
-							Usage: "page of results to display",
-						},
-					},
-				},
-				{
-					Name:   "show",
-					Usage:  "Show a specific job's details",
-					Action: client.ShowJobSpec,
-				},
-				{
-					Name:   "create",
-					Usage:  "Create job spec from JSON",
-					Action: client.CreateJobSpec,
-				},
-				{
-					Name:   "archive",
-					Usage:  "Archive job and all associated runs",
-					Action: client.ArchiveJobSpec,
-				},
-			},
-		},
+
 		{
 			Name:  "runs",
-			Usage: "Run related commands",
+			Usage: "Commands for managing Runs",
 			Subcommands: []cli.Command{
 				{
 					Name:        "create",
@@ -145,14 +202,8 @@ func NewApp(client *Client) *cli.App {
 					Action:      client.CreateJobRun,
 				},
 				{
-					Name:    "show",
-					Aliases: []string{"sr"},
-					Usage:   "Show a run for a specific ID",
-					Action:  client.ShowJobRun,
-				},
-				{
 					Name:   "list",
-					Usage:  "List all runs",
+					Usage:  "List all Runs",
 					Action: client.IndexJobRuns,
 					Flags: []cli.Flag{
 						cli.IntFlag{
@@ -161,95 +212,22 @@ func NewApp(client *Client) *cli.App {
 						},
 						cli.StringFlag{
 							Name:  "jobid",
-							Usage: "filter all runs to match the given jobid",
-						},
-					},
-				},
-			},
-		},
-		{
-			Name:  "adapters",
-			Usage: "External adapter and bridge related commands",
-			Subcommands: []cli.Command{
-				{
-					Name:   "create",
-					Usage:  "Create a new bridge to an external adapter",
-					Action: client.CreateBridge,
-				},
-				{
-					Name:   "list",
-					Usage:  "List all bridges to external adapters",
-					Action: client.IndexBridges,
-					Flags: []cli.Flag{
-						cli.IntFlag{
-							Name:  "page",
-							Usage: "page of results to display",
+							Usage: "filter all Runs to match the given jobid",
 						},
 					},
 				},
 				{
-					Name:   "show",
-					Usage:  "Show an external adapter's bridge information",
-					Action: client.ShowBridge,
-				},
-				{
-					Name:   "destroy",
-					Usage:  "Destroys the bridge for an external adapter",
-					Action: client.RemoveBridge,
+					Name:    "show",
+					Aliases: []string{"sr"},
+					Usage:   "Show a Run for a specific ID",
+					Action:  client.ShowJobRun,
 				},
 			},
 		},
+
 		{
-			Name:    "initiators",
-			Aliases: []string{"exi"},
-			Usage:   "Commands for managing external initiators",
-			Subcommands: []cli.Command{
-				{
-					Name:   "create",
-					Usage:  "Create an authentication key for a user of external initiators",
-					Action: client.CreateExternalInitiator,
-				},
-				{
-					Name:   "delete",
-					Usage:  "Remove an authentication key",
-					Action: client.DeleteExternalInitiator,
-				},
-			},
-		},
-		{
-			Name:    "config",
-			Aliases: []string{"exi"},
-			Usage:   "Commands for the node's configuration",
-			Subcommands: []cli.Command{
-				{
-					Name:   "setgasprice",
-					Usage:  "Set the minimum gas price to use for outgoing transactions",
-					Action: client.SetMinimumGasPrice,
-					Flags: []cli.Flag{
-						cli.BoolFlag{
-							Name:  "gwei",
-							Usage: "Specify amount in gwei",
-						},
-					},
-				},
-			},
-		},
-		{
-			Name:    "agreements",
-			Aliases: []string{"agree"},
-			Usage:   "Commands for handling service agreements",
-			Subcommands: []cli.Command{
-				{
-					Name:   "create",
-					Usage:  "Creates a service agreement",
-					Action: client.CreateServiceAgreement,
-				},
-			},
-		},
-		{
-			Name:    "transactions",
-			Aliases: []string{"tx"},
-			Usage:   "Commands for handling Ethereum transactions",
+			Name:  "txs",
+			Usage: "Commands for handling Ethereum transactions",
 			Subcommands: []cli.Command{
 				{
 					Name:  "create",
@@ -264,7 +242,7 @@ func NewApp(client *Client) *cli.App {
 				},
 				{
 					Name:   "list",
-					Usage:  "List the transactions in descending order",
+					Usage:  "List the Ethereum Transactions in descending order",
 					Action: client.IndexTransactions,
 					Flags: []cli.Flag{
 						cli.IntFlag{
@@ -275,39 +253,68 @@ func NewApp(client *Client) *cli.App {
 				},
 				{
 					Name:   "show",
-					Usage:  "get information on a specific transaction",
+					Usage:  "get information on a specific Ethereum Transaction",
 					Action: client.ShowTransaction,
-				},
-			},
-		},
-		{
-			Name:    "attempts",
-			Aliases: []string{"txa"},
-			Usage:   "Commands for managing Ethereum Transaction Attempts",
-			Subcommands: []cli.Command{
-				{
-					Name:    "list",
-					Aliases: []string{"txas"},
-					Usage:   "List the transaction attempts in descending order",
-					Action:  client.IndexTxAttempts,
-					Flags: []cli.Flag{
-						cli.IntFlag{
-							Name:  "page",
-							Usage: "page of results to display",
-						},
-					},
 				},
 			},
 		},
 	}
 
 	if client.Config.Dev() {
-		createextrakey := cli.Command{
-			Name:   "createextrakey",
-			Usage:  "Create a key in the node's keystore alongside the existing key; to create an original key, just run the node",
-			Action: client.CreateExtraKey,
-		}
-		app.Commands = append(app.Commands, createextrakey)
+		app.Commands = append(app.Commands, cli.Command{
+			Name:    "agreements",
+			Aliases: []string{"agree"},
+			Usage:   "Commands for handling service agreements",
+			Subcommands: []cli.Command{
+				{
+					Name:   "create",
+					Usage:  "Creates a Service Agreement",
+					Action: client.CreateServiceAgreement,
+				},
+			},
+		},
+
+			cli.Command{
+				Name:  "attempts",
+				Usage: "Commands for managing Ethereum Transaction Attempts",
+				Subcommands: []cli.Command{
+					{
+						Name:    "list",
+						Aliases: []string{"txas"},
+						Usage:   "List the Transaction Attempts in descending order",
+						Action:  client.IndexTxAttempts,
+						Flags: []cli.Flag{
+							cli.IntFlag{
+								Name:  "page",
+								Usage: "page of results to display",
+							},
+						},
+					},
+				},
+			},
+
+			cli.Command{
+				Name:   "createextrakey",
+				Usage:  "Create a key in the node's keystore alongside the existing key; to create an original key, just run the node",
+				Action: client.CreateExtraKey,
+			},
+
+			cli.Command{
+				Name:  "initiators",
+				Usage: "Commands for managing External Initiators",
+				Subcommands: []cli.Command{
+					{
+						Name:   "create",
+						Usage:  "Create an authentication key for a user of External Initiators",
+						Action: client.CreateExternalInitiator,
+					},
+					{
+						Name:   "destroy",
+						Usage:  "Remove an authentication key",
+						Action: client.DeleteExternalInitiator,
+					},
+				},
+			})
 	}
 
 	return app
--- core/main_test.go
+++ core/main_test.go
@@ -38,41 +38,15 @@ func ExampleRun() {
 	// VERSION:
 	//    unset@unset
 	//
-	// Output:
-	// NAME:
-	//    core.test - CLI for Chainlink
-	//
-	// USAGE:
-	//    core.test [global options] command [command options] [arguments...]
-	//
-	// VERSION:
-	//    unset@unset
-	//
 	// COMMANDS:
-	//    local                      Commands which are run locally
-	//    login                      Login to remote client by creating a session cookie
-	//    account, a                 Display the account address with its ETH & LINK balances
-	//    jobs, js, specs, jobspecs  List all jobs
-	//    job, j                     Show a specific job's details
-	//    create, c                  Create job spec from JSON
-	//    archivejob                 Archive job and all associated runs
-	//    run, r                     Create a new run for a job
-	//    showrun, sr                Show a run for a specific ID
-	//    runs, lr                   List all runs
-	//    addbridge                  Create a new bridge to the node
-	//    bridges                    List all bridges added to the node
-	//    bridge                     Show a specific bridge
-	//    removebridge               Removes a specific bridge
-	//    initiators, exi            Tasks for managing external initiators
-	//    agree, createsa            Creates a service agreement
-	//    withdraw, w                Withdraw, to an authorized Ethereum <address>, <amount> units of LINK. Withdraws from the configured oracle contract by default, or from contract optionally specified by a third command-line argument --from-oracle-contract-address=<contract address>. Address inputs must be in EIP55-compliant capitalization.
-	//    sendether                  Send <amount> ETH from the node's ETH account to an <address>.
-	//    chpass                     Change your password
-	//    setgasprice                Set the minimum gas price to use for outgoing transactions
-	//    transactions, txs          List the transactions in descending order
-	//    transaction, tx            get information on a specific transaction
-	//    txattempts, txas           List the transaction attempts in descending order
-	//    help, h                    Shows a list of commands or help for one command
+	//    admin    Commands for remotely taking admin related actions
+	//    bridges  Commands for Bridges communicating with External Adapters
+	//    config   Commands for the node's configuration
+	//    jobs     Commands for managing Jobs
+	//    node     Commands for admin actions that must be run locally
+	//    runs     Commands for managing Runs
+	//    txs      Commands for handling Ethereum transactions
+	//    help, h  Shows a list of commands or help for one command
 	//
 	// GLOBAL OPTIONS:
 	//    --json, -j     json output as opposed to table
