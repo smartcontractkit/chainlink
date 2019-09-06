a168a242b7b7c7a5868263be310ce55b389cd404
Alex Coventry <alx@mit.edu>
2019-09-06 16:04:36 -0400

    Move VRF/threshold-signature code to v0.5/contracts/dev

--- evm/v0.5/contracts/dev/SchnorrSECP256K1.sol
+++ evm/v0.5/contracts/dev/SchnorrSECP256K1.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 ////////////////////////////////////////////////////////////////////////////////
 //       XXX: Do not use in production until this code has been audited.
