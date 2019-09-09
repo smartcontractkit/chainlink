5035bafff0267188d9896abf7d8e948a2b44a656
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-09 16:01:53 -0400

    rewrite connection test

--- explorer/src/__tests__/realtime.test.ts
+++ explorer/src/__tests__/realtime.test.ts
@@ -183,16 +183,15 @@ describe('realtime', () => {
     )
   })
 
-  it.only('rejects multiple connections from single node', async (done) => {
-    expect.assertions(5)
+  it.only('rejects multiple connections from single node', async done => {
+    expect.assertions(4)
 
     const ws1 = await newChainlinkNode(ENDPOINT, chainlinkNode.accessKey, secret)
-    expect(ws1.readyState).toBe(1) // connection open
     const ws2 = await newChainlinkNode(ENDPOINT, chainlinkNode.accessKey, secret)
 
     ws1.addEventListener('close', event => {
-      expect(ws2.readyState).toBe(1) // connection open
       expect(ws1.readyState).toBe(3) // connection closed
+      expect(ws2.readyState).toBe(1) // connection open
       expect(event.code).toBe(1000)
       expect(event.reason).toEqual('Duplicate connection opened')
       ws2.close()
