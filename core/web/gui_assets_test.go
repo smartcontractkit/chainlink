cf01b4ce36f2eb8d5cc0c583041da47c7a98aac6
felder-cl <felderhof@smartcontract.com>
2019-09-10 16:33:34 +0200

    Migrate to use http.StatusCode const.
    To improve clarity, continue migrating code to use the http packages
    constants for status codes.

--- core/web/gui_assets_test.go
+++ core/web/gui_assets_test.go
@@ -17,35 +17,35 @@ func TestGuiAssets_WildcardIndexHtml(t *testing.T) {
 
 	resp, err := client.Get(app.Server.URL + "/")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/not_found")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/jjob_specs/abc123")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/runs")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/rruns")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/runs/abc123")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/rruns/abc123")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 }
 
 func TestGuiAssets_WildcardRouteInfo(t *testing.T) {
@@ -57,19 +57,19 @@ func TestGuiAssets_WildcardRouteInfo(t *testing.T) {
 
 	resp, err := client.Get(app.Server.URL + "/job_specs/abc123/routeInfo.json")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/rrouteInfo.json")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/runs/routeInfo.json")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/job_specs/abc123/runs/rrouteInfo.json")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 }
 
 func TestGuiAssets_Exact(t *testing.T) {
@@ -81,9 +81,9 @@ func TestGuiAssets_Exact(t *testing.T) {
 
 	resp, err := client.Get(app.Server.URL + "/main.js")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 200)
+	cltest.AssertServerResponse(t, resp, http.StatusOK)
 
 	resp, err = client.Get(app.Server.URL + "/mmain.js")
 	require.NoError(t, err)
-	cltest.AssertServerResponse(t, resp, 404)
+	cltest.AssertServerResponse(t, resp, http.StatusNotFound)
 }
