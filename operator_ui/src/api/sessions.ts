79a7da8a30cacedc5d7a9968191de3fb7378736f
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-10 13:18:19 -0400

    Fix @typescript-eslint/camelcase for operator_ui

--- operator_ui/src/api/sessions.ts
+++ operator_ui/src/api/sessions.ts
@@ -1,6 +1,6 @@
 import * as jsonapi from 'api/transport/json'
 import * as models from 'core/store/models'
-import * as sessions_controller from 'core/web/sessions_controller'
+import * as sessionsController from 'core/web/sessions_controller'
 
 /**
  * Create creates a session ID for the given user credentials
@@ -9,25 +9,25 @@ import * as sessions_controller from 'core/web/sessions_controller'
 const CREATE_ENDPOINT = '/sessions'
 const create = jsonapi.createResource<
   models.SessionRequest,
-  sessions_controller.Session
+  sessionsController.Session
 >(CREATE_ENDPOINT)
 
 /**
  * Destroy erases the session ID for the sole API user.
  */
 const DESTROY_ENDPOINT = '/sessions'
-const destroy = jsonapi.deleteResource<undefined, sessions_controller.Session>(
+const destroy = jsonapi.deleteResource<undefined, sessionsController.Session>(
   DESTROY_ENDPOINT,
 )
 
 export function createSession(
   sessionRequest: models.SessionRequest,
-): Promise<jsonapi.ApiResponse<sessions_controller.Session>> {
+): Promise<jsonapi.ApiResponse<sessionsController.Session>> {
   return create(sessionRequest)
 }
 
 export function destroySession(): Promise<
-  jsonapi.ApiResponse<sessions_controller.Session>
+  jsonapi.ApiResponse<sessionsController.Session>
 > {
   return destroy()
 }
