8a927e4b66fa7b4b5ff6420cc6e4fb060fc51d49
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-10 13:33:18 -0400

    Fix @typescript-eslint/class-name-casing for operator_ui

--- operator_ui/@types/json-api-normalizer.d.ts
+++ operator_ui/@types/json-api-normalizer.d.ts
@@ -1,4 +1,5 @@
 /* eslint @typescript-eslint/no-empty-interface: 'off' */
+/* eslint @typescript-eslint/class-name-casing: 'off' */
 
 declare module 'json-api-normalizer' {
   export interface JsonApiResponse<
