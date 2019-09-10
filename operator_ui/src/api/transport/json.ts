81b8a0dfb10ed115d942e7a2dde72dbfa9838fb3
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-09 20:31:19 -0400

    Fix @typescript-eslint/no-unused-vars for operator_ui

--- operator_ui/src/api/transport/json.ts
+++ operator_ui/src/api/transport/json.ts
@@ -127,7 +127,7 @@ type ResponseType<TParams, T> = TParams extends PaginatedRequestParams
 
 function parseResponse<T>(response: Response): Promise<T> {
   if (response.status === 204) {
-    return new Promise((resolve, _reject) => resolve({} as T))
+    return new Promise(resolve => resolve({} as T))
   } else if (response.status >= 200 && response.status < 300) {
     return response.json()
   } else if (response.status === 400) {
