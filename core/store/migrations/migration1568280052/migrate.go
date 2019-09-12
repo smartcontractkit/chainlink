f39ac09bb20040137f590d488d1e539c0991b8ec
felder-cl <felderhof@smartcontract.com>
2019-09-12 11:44:24 +0200

    Fix External Initiator 'gorm' tag
    Make the External Initiator's 'name' column unique by correcting the tag
    used for the struct's field.  It should be ';' instead of ','.

---
+++ core/store/migrations/migration1568280052/migrate.go
@@ -0,0 +1,59 @@
+package migration1568280052
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1565877314"
+	"github.com/smartcontractkit/chainlink/core/store/models"
+)
+
+type ExternalInitiator struct {
+	*gorm.Model
+	Name           string        `gorm:"not null;unique"`
+	URL            models.WebURL `gorm:"not null"`
+	AccessKey      string        `gorm:"not null"`
+	Salt           string        `gorm:"not null"`
+	HashedSecret   string        `gorm:"not null"`
+	OutgoingSecret string        `gorm:"not null"`
+	OutgoingToken  string        `gorm:"not null"`
+}
+
+// newExternalInitiator returns a copy of the old struct with the fields untouched.
+func newExternalInitiator(arg migration1565877314.ExternalInitiator) ExternalInitiator {
+	return ExternalInitiator{
+		Model:          arg.Model,
+		Name:           arg.AccessKey,
+		URL:            arg.URL,
+		AccessKey:      arg.AccessKey,
+		Salt:           arg.Salt,
+		HashedSecret:   arg.HashedSecret,
+		OutgoingSecret: arg.OutgoingSecret,
+		OutgoingToken:  arg.OutgoingToken,
+	}
+}
+
+// Migrate adds External Initiator Name and URL fields.
+func Migrate(tx *gorm.DB) error {
+	var exis []migration1565877314.ExternalInitiator
+	if err := tx.Find(&exis).Error; err != nil {
+		return errors.Wrap(err, "could not load all External Intitiators")
+	}
+
+	// Make new table
+	if err := tx.DropTable(migration1565877314.ExternalInitiator{}).Error; err != nil {
+		return errors.Wrap(err, "could not drop old External Intitiator table")
+	}
+	if err := tx.AutoMigrate(&ExternalInitiator{}).Error; err != nil {
+		return errors.Wrap(err, "could not create new External Intitiator table")
+	}
+
+	// Copy
+	for _, old := range exis {
+		exi := newExternalInitiator(old)
+		if err := tx.Save(exi).Error; err != nil {
+			return errors.Wrap(err, "could not save migrated version of External Initiator")
+		}
+	}
+
+	return nil
+}
