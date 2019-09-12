6e7920e6e735782aae27585243f63a6efd997528
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-11 20:11:15 -0400

    Fix @typescript-eslint/no-unused-vars for evm/v0.5

--- evm/v0.5/test/support/helpers.ts
+++ evm/v0.5/test/support/helpers.ts
@@ -7,8 +7,6 @@ import TruffleContract from 'truffle-contract'
 import { linkToken } from './linkToken'
 import { assertBigNum } from './matchers'
 
-const HEX_BASE = 16
-
 // https://github.com/ethereum/web3.js/issues/1119#issuecomment-394217563
 web3.providers.HttpProvider.prototype.sendAsync =
   web3.providers.HttpProvider.prototype.send
