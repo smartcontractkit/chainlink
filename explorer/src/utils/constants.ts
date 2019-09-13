bbdbca3119670e44a5882592a4e08100ef4d463e
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-13 12:14:37 -0400

    make headers all lowercase

--- explorer/src/utils/constants.ts
+++ explorer/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const OPEN_CONNECTION: number = 1
 export const CLOSED_CONNECTION: number = 3
 export const NORMAL_CLOSE: number = 1000
-export const ACCESS_KEY_HEADER: string = 'X-Explore-Chainlink-AccessKey'
-export const SECRET_HEADER: string = 'X-Explore-Chainlink-Secret'
+export const ACCESS_KEY_HEADER: string = 'x-explore-chainlink-accesskey'
+export const SECRET_HEADER: string = 'x-explore-chainlink-secret'
