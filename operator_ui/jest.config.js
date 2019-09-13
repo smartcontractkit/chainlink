5d31fce0a60678a3d59e41230b5dd26b1ee5b467
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-12 21:51:20 -0400

    Remove unused dependencies for operator_ui

--- operator_ui/jest.config.js
+++ operator_ui/jest.config.js
@@ -7,9 +7,6 @@ module.exports = {
     '<rootDir>/node_modules/',
     '<rootDir>/__tests__/.eslintrc.js',
   ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)?$': './support/upwardBabelJestTransform.js',
-  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
--- operator_ui/support/upwardBabelJestTransform.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { createTransformer } = require('babel-jest')
-
-module.exports = createTransformer({ rootMode: 'upward' })
