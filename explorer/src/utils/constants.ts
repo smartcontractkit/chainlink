055eb9ed63a8312757b848883cab42d63c0e53db
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-13 15:58:08 -0400

    switch to WebSocket OPEN and CLOSED constants

--- explorer/src/utils/constants.ts
+++ explorer/src/utils/constants.ts
@@ -1,5 +1,3 @@
-export const OPEN_CONNECTION = 1
-export const CLOSED_CONNECTION = 3
 export const NORMAL_CLOSE = 1000
 export const ACCESS_KEY_HEADER = 'x-explore-chainlink-accesskey'
 export const SECRET_HEADER = 'x-explore-chainlink-secret'
