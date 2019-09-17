262fa2ca9584abf5a19b1c84b8d9cb0645b45f19
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-16 19:46:33 -0700

    Use TypeORM classes in migration

--- explorer/src/migration/1565028153000-AddAdmin.ts
+++ explorer/src/migration/1565028153000-AddAdmin.ts
@@ -1,19 +1,60 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
+import { MigrationInterface, QueryRunner, Table, TableColumn } from 'typeorm'
+
+const TABLE_NAME = 'admin'
+
+const idColumm = new TableColumn({
+  name: 'id',
+  type: 'character varying',
+  isPrimary: true,
+  isNullable: false,
+  default: 'uuid_generate_v4()',
+})
+
+const usernameColumn = new TableColumn({
+  name: 'username',
+  type: 'character varying',
+  isUnique: true,
+  isNullable: false,
+})
+
+const hashedPasswordColumn = new TableColumn({
+  name: 'hashedPassword',
+  type: 'character varying',
+  isNullable: false,
+})
+
+const createdAtColumn = new TableColumn({
+  name: 'createdAt',
+  type: 'timestamp without time zone',
+  isNullable: true,
+  default: 'now()',
+})
+
+const updatedAtColumn = new TableColumn({
+  name: 'updatedAt',
+  type: 'timestamp without time zone',
+  isNullable: true,
+  default: 'now()',
+})
 
 export class AddAdmin1565028153000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(`
-CREATE TABLE admin (
-  "id" character varying DEFAULT uuid_generate_v4() PRIMARY KEY,
-  "username" character varying UNIQUE NOT NULL,
-  "hashedPassword" character varying NOT NULL,
-  "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
-  "updatedAt" timestamp without time zone DEFAULT now() NOT NULL
-);
-    `)
+    const options = {
+      name: TABLE_NAME,
+      columns: [
+        idColumm,
+        usernameColumn,
+        hashedPasswordColumn,
+        createdAtColumn,
+        updatedAtColumn,
+      ],
+    }
+
+    await queryRunner.createTable(new Table(options))
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('DROP TABLE admin;')
+    const options = { name: TABLE_NAME }
+    await queryRunner.dropTable(new Table(options))
   }
 }
