a742f5a5da5dd53ad638cfc39375d50b546f3725
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-16 20:02:02 -0700

    Use password service with 1 round to create admin

--- explorer/src/support/admin.ts
+++ explorer/src/support/admin.ts
@@ -1,13 +1,13 @@
 import { Connection } from 'typeorm'
-import bcrypt from 'bcrypt'
 import { Admin } from '../entity/Admin'
+import { hash } from '../services/password'
 
 export async function createAdmin(
   db: Connection,
   username: string,
   password: string,
 ) {
-  const hashedPassword = await bcrypt.hash(password, 1)
+  const hashedPassword = await hash(password, 1)
   const admin = new Admin()
   admin.username = username
   admin.hashedPassword = hashedPassword
