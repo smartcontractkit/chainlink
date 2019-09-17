69cbdabedd5721af1b2bb7388bf3cd60208b3d63
typescribe <53539231+typescribe@users.noreply.github.com>
2019-09-17 18:35:32 -0400

    Add tests to tsconfig.json
    - Add tests to evm's tsconfig.json
    - Fix any tsc errors from adding tests to tsconfig

--- evm/@types/globals.d.ts
+++ evm/@types/globals.d.ts
@@ -1,2 +1,6 @@
 declare const web3: import('web3')
 declare const assert: Chai.Assert
+declare const contract: import('mocha').MochaGlobals['describe']
+declare const artifacts: {
+  require: (contract: string) => any
+}
