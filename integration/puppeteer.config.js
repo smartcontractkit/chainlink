d517154d93cc30ecc3cc2ce358695e80201a81f6
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-18 17:40:54 -0400

    add slowmo to puppeteer config

--- integration/puppeteer.config.js
+++ integration/puppeteer.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  devtools: false,
-  headless: process.env.HEADLESS !== 'false',
   args: ['--no-sandbox'],
   defaultViewport: {
     width: 1366,
     height: 768,
   },
+  devtools: false,
+  headless: process.env.HEADLESS !== 'false',
+  slowMo: process.env.SLOWMO || 10,
 }
