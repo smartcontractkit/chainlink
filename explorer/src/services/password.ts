e97421faf26f96b25f6460eae7b3597ce5d01703
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-18 10:11:08 -0700

    Use argon2id for explorer admin password hash

--- explorer/src/services/password.ts
+++ explorer/src/services/password.ts
@@ -1,7 +1,7 @@
 import argon2 from 'argon2'
 
 export function hash(plaintext: string): Promise<string> {
-  return argon2.hash(plaintext)
+  return argon2.hash(plaintext, { type: argon2.argon2id })
 }
 
 export function compare(plaintext: string, hash: string): Promise<boolean> {
