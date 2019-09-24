657d291323c5ce9afd7ceb595f94e9902f04bfd3
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-24 13:39:37 -0400

    add editBridge test

--- integration/cypress/integration/createBridge.spec.ts
+++ integration/cypress/integration/createBridge.spec.ts
@@ -1,25 +1,29 @@
+const bridgeProperties = {
+  name: 'create_test_bridge',
+  url: 'http://example.com',
+  minimumContractPayment: '123',
+  confirmations: '5',
+}
+
 context('End to end', function() {
   it('Creates a bridge', () => {
     cy.login()
-
     // Navigate to New Bridge page
     cy.clickLink('Bridges')
     cy.contains('h4', 'Bridges').should('exist')
     cy.clickLink('New Bridge')
     cy.contains('h5', 'New Bridge').should('exist')
     // Create Bridge
-    cy.get('form input[name=name]').type('create_test_bridge')
-    cy.get('form input[name=url]').type('http://example.com')
-    cy.get('form input[name=minimumContractPayment]').type('123')
-    cy.get('form input[name=confirmations]').type('5')
+    cy.get('form').fill(bridgeProperties)
     cy.clickButton('Create Bridge')
     // Check new bridge created successfuly
     cy.contains('p', 'Successfully created bridge')
+      .should('exist')
       .children('a')
       .click()
-    cy.contains('p', 'create_test_bridge').should('exist')
-    cy.contains('p', 'http://example.com').should('exist')
-    cy.contains('p', '123').should('exist')
-    cy.contains('p', '5').should('exist')
+    cy.contains('p', bridgeProperties.name).should('exist')
+    cy.contains('p', bridgeProperties.url).should('exist')
+    cy.contains('p', bridgeProperties.minimumContractPayment).should('exist')
+    cy.contains('p', bridgeProperties.confirmations).should('exist')
   })
 })
--- integration/cypress/integration/editBridge.spec.ts
+++ integration/cypress/integration/editBridge.spec.ts
@@ -1,25 +1,47 @@
+const bridgeProperties = {
+  name: 'edit_test_bridge',
+  url: 'http://example_1.com',
+  minimumContractPayment: '123',
+  confirmations: '5',
+}
+
+const newBridgeProperties = {
+  url: 'http://example_2.com',
+  minimumContractPayment: '321',
+  confirmations: '10',
+}
+
 context('End to end', function() {
   it('Edits a bridge', () => {
     cy.login()
-
     // Navigate to New Bridge page
     cy.clickLink('Bridges')
     cy.contains('h4', 'Bridges').should('exist')
     cy.clickLink('New Bridge')
     cy.contains('h5', 'New Bridge').should('exist')
     // Create Bridge
-    cy.get('form input[name=name]').type('create_test_bridge')
-    cy.get('form input[name=url]').type('http://example_1.com')
-    cy.get('form input[name=minimumContractPayment]').type('123')
-    cy.get('form input[name=confirmations]').type('5')
+    cy.get('form').fill(bridgeProperties)
     cy.clickButton('Create Bridge')
     // Check new bridge created successfuly
     cy.contains('p', 'Successfully created bridge')
+      .should('exist')
       .children('a')
       .click()
-    cy.contains('p', 'create_test_bridge').should('exist')
-    cy.contains('p', 'http://example_1.com').should('exist')
-    cy.contains('p', '123').should('exist')
-    cy.contains('p', '5').should('exist')
+    cy.contains('p', bridgeProperties.name).should('exist')
+    cy.contains('p', bridgeProperties.url).should('exist')
+    cy.contains('p', bridgeProperties.minimumContractPayment).should('exist')
+    cy.contains('p', bridgeProperties.confirmations).should('exist')
+    // Edit Bridge
+    cy.clickLink('Edit')
+    cy.get('form').fill(newBridgeProperties)
+    cy.clickButton('Save Bridge')
+    cy.contains('p', 'Successfully updated')
+      .should('exist')
+      .children('a')
+      .click()
+    cy.contains('p', bridgeProperties.name).should('exist')
+    cy.contains('p', newBridgeProperties.url).should('exist')
+    cy.contains('p', newBridgeProperties.minimumContractPayment).should('exist')
+    cy.contains('p', newBridgeProperties.confirmations).should('exist')
   })
 })
