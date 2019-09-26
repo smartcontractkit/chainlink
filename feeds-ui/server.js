fb8956e9ba0123e0d62ae49ce1af71483b3e551b
Bartosz Wojciechowski <zly.ojciec@gmail.com>
2019-09-26 10:26:38 +0200

    Added express server

---
+++ feeds-ui/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(port)
