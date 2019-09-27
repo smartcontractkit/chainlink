fa57daeb33e1664385a8fdac4306506c820f4289
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-09-27 16:46:05 -0400

    Export v2 test helpers from package

--- evm/src/index.ts
+++ evm/src/index.ts
@@ -1,3 +1,8 @@
-export * from './helpers'
-export * from './linkToken'
-export * from './matchers'
+import * as contract from './contract'
+import * as helpers from './helpersV2'
+import * as debug from './debug'
+import * as linkToken from './linkToken'
+import * as wallet from './wallet'
+import * as matchers from './matchersV2'
+
+export { contract, helpers, debug, linkToken, wallet, matchers }
