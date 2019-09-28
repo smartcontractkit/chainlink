074c4ef1a9336f32088fc61ab1636bcb338172b1
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-28 18:55:32 -0400

    improve comment on deleteCompletedJobRun.spec

--- integration/cypress/integration/deleteCompletedJobRun.spec.ts
+++ integration/cypress/integration/deleteCompletedJobRun.spec.ts
@@ -2,7 +2,9 @@ context('End to end', function() {
   it('Deletes a completed job', () => {
     cy.login()
 
-    // Create Job
+    // Can only delete job runs that are > 1 week old...
+    // This test only ensures that the UI for deleting a job run is available
+
     cy.clickLink('Configuration')
     cy.contains('h5', 'Configuration').should('exist')
     cy.clickButton('Delete Completed Jobs')
