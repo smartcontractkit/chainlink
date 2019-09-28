8e1b6f0346dcfa448536a6e40ca416e989474212
Ryan Hall <hall.ryan.r@gmail.com>
2019-09-28 19:43:33 -0400

    convert cypressJobServer to typescript

--- integration/cypress/support/cypressJobServer.ts
+++ integration/cypress/support/cypressJobServer.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+import net from 'net'
+import { exists } from 'fs'
 ;(async function() {
   const net = require('net')
 
@@ -7,7 +9,7 @@
   const response = customResponse || defaultResponse
   const port = process.env.CYPRESS_JOB_SERVER_PORT || 6692
 
-  const server = new net.Server(socket => {
+  const server = new net.Server((socket: net.Socket) => {
     socket.on('data', () => {
       socket.write(`HTTP/1.1 200 OK
 Content-Type: application/json
@@ -22,5 +24,13 @@ ${response}`)
   })
 
   const endpoint = await server.listen(port)
-  console.log(`Job Server listening on port ${endpoint.address().port}`)
+  const address = endpoint.address()
+  if (address && typeof address != 'string') {
+    console.log(`Job Server listening on port ${address.port}`)
+  } else {
+    console.error(
+      'Invalid server setup. Address should be of type net.AddressInfo',
+    )
+    process.exit(1)
+  }
 })()
