17cba2dad556222d9998daf202b0e3bed90ec16c
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-30 16:09:31 -0700

    Remove no-unused-expressions for CLI commands

--- explorer/src/bin/clnodes.ts
+++ explorer/src/bin/clnodes.ts
@@ -1,7 +1,6 @@
 import yargs from 'yargs'
 import { add, remove } from '../cli/clnodes'
 
-/* eslint-disable-next-line no-unused-expressions */
 yargs
   .usage('Usage: $0 <command> [options]')
   .command({
