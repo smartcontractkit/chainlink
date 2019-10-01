4df1c6710b23c5ab9d829d84312637536ffc4291
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-09-30 21:22:17 -0400

    Convert Chainlinked_test to ethers+ganache

--- evm/test/Chainlinked_test.ts
+++
@@ -1,8 +0,0 @@
-import { checkPublicABI } from '../src/helpers'
-const Chainlinked = artifacts.require('Chainlinked.sol')
-
-contract('Chainlinked', () => {
-  it('has a limited public interface', () => {
-    checkPublicABI(Chainlinked, [])
-  })
-})
