dfae85d25e075ab0fca52a196c4c2756fbd07896
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-10-16 11:59:53 -0700

    Remove explicit type annotations for admin login controller

--- explorer/src/controllers/admin/login.ts
+++ explorer/src/controllers/admin/login.ts
@@ -1,8 +1,8 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
 
 const router = Router()
 
-router.post('/login', async (req: Request, res: Response) => {
+router.post('/login', async (_req, res) => {
   return res.sendStatus(200)
 })
 
