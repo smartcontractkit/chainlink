6bf0a0c175d202b314ed74db261dd06c53d6db12
Ryan Hall <hall.ryan.r@gmail.com>
2019-10-16 15:50:17 -0400

    all tests passing

---
+++ integration/forks/scripts/search_chainlink_logs.sh
@@ -0,0 +1,13 @@
+check_count=0;
+TIMEOUT=20
+
+echo $1
+
+until ( docker-compose --compatibility logs chainlink | grep "$1" > /dev/null ) ; do
+  if [ $check_count -gt $TIMEOUT ]; then
+    echo "Timed out searching chainlink logs for $1";
+    exit 1 ;
+  fi;
+  check_count=$((check_count + 1))
+  sleep 1;
+done
---
+++ integration/forks/test.sh
@@ -0,0 +1,5 @@
+make initial_setup
+make run_chain_1
+make run_chain_2
+
+echo "test passes!"
