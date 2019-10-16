2d7cb49c320d3a6fdc5b53d0b618536c0d266193
Ryan Hall <hall.ryan.r@gmail.com>
2019-10-16 17:08:12 -0400

    cleanup

--- integration/forks/scripts/search_chainlink_logs.sh
+++ integration/forks/scripts/search_chainlink_logs.sh
@@ -1,9 +1,9 @@
 check_count=0;
-TIMEOUT=20
+TIMEOUT=30
 
 until ( docker-compose --compatibility logs chainlink | grep "$1" > /dev/null) ; do
   if [ $check_count -gt $TIMEOUT ]; then
-    echo "Timed out searching chainlink logs for $1";
+    echo "Timed out searching chainlink logs for \"$1\"";
     exit 1 ;
   fi;
   check_count=$((check_count + 1))
