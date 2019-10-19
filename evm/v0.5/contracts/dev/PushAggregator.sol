c964339a14e61104f32460ec4d4c1a2049e3fd81
Steve Ellis <email@steveell.is>
2019-10-19 21:45:16 +0900

    make solidity style consistent

--- evm/v0.5/contracts/dev/PushAggregator.sol
+++ evm/v0.5/contracts/dev/PushAggregator.sol
@@ -15,12 +15,16 @@ contract PushAggregator is Ownable {
   LinkTokenInterface private LINK;
   mapping(address => bool) private oracles;
 
-  constructor(address _link, uint256 _paymentAmount) public {
+  constructor(address _link, uint256 _paymentAmount)
+    public
+  {
     LINK = LinkTokenInterface(_link);
     updatePaymentAmount(_paymentAmount);
   }
 
-  function updateAnswer(int256 _answer) public {
+  function updateAnswer(int256 _answer)
+    public
+  {
     require(oracles[msg.sender], "Only updatable by designated oracles");
 
     currentAnswer = _answer;
@@ -28,14 +32,20 @@ contract PushAggregator is Ownable {
     require(LINK.transfer(msg.sender, paymentAmount), "LINK transfer failed");
   }
 
-  function addOracle(address _oracle) public onlyOwner {
+  function addOracle(address _oracle)
+    public
+    onlyOwner()
+  {
     require(!oracles[_oracle], "Address is already recorded as an oracle");
 
     oracles[_oracle] = true;
     oracleCount += 1;
   }
 
-  function removeOracle(address _oracle) public onlyOwner {
+  function removeOracle(address _oracle)
+    public
+    onlyOwner()
+  {
     require(oracles[_oracle], "Address is not an oracle");
     oracles[_oracle] = false;
     oracleCount -= 1;
