7a52e0089fc9b59a11b86d0c0263460390914771
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-10-21 18:29:55 -0400

    Swap out truffle compile for @0x/sol-compiler
    The swap out of the compiler allows us to create a much more efficient
    build process for developers. We gain the ability to do dockerized
    compilation to gain an order of magnitude of speedup for compiling
    solidity files; the ability to enable watch mode which leads to hot-
    reloadable solidity tests written in typescript; and the eventual
    removal of the heavyweight truffle dependency.
    
    Some bigger changes that were made along with the swap for
    compatibility and cleanup reasons:
    
    - Generating a index barrel file for generated contract factories via
    the evm/scripts/export-generated-contract-factories.ts script.
    
    This lets us export our contract factories that were generated from the
    typechain package. Allowing package consumers to access these generated
    factories, giving them typesafe interaction and deployment of our
    solidity contracts.
    
    - Moving typechain generated files from dist/generated to src/generated
    
    This is a change for future watch mode compilation and testing of
    contracts, the directory src/generated is currently ignored for now,
    incase we want to move it again before implementation starts.
    
    - Move compiled solidity artifacts from build/contracts to
    dist/artifacts
    
    This was done to aggregate our compilation outputs into dist/ since we
    now include javascript output, not just solidity output.

--- evm/src/index.ts
+++ evm/src/index.ts
@@ -4,5 +4,6 @@ import * as debug from './debug'
 import LinkToken from './LinkToken.json'
 import * as wallet from './wallet'
 import * as matchers from './matchersV2'
+import * as generated from './generated'
 
-export { contract, helpers, debug, LinkToken, wallet, matchers }
+export { contract, helpers, debug, LinkToken, wallet, matchers, generated }
