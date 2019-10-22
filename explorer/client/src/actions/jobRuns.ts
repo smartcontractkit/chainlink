c485e13857a1f651e99bb4ac7364b5310814b00d
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-10-22 11:00:19 -0700

    Infer dispatch type

--- explorer/client/src/actions/jobRuns.ts
+++ explorer/client/src/actions/jobRuns.ts
@@ -1,12 +1,14 @@
-import { Dispatch } from 'redux'
+import { Action, Dispatch } from 'redux'
+import { ThunkAction } from 'redux-thunk'
 import normalize from 'json-api-normalizer'
 import * as api from '../api'
 import { JobRunsAction } from '../reducers/jobRuns'
 import { Query } from '../reducers/search'
+import { State as AppState } from '../reducers'
 
-const getJobRuns = (query: Query, page: number, size: number) => {
-  return (dispatch: Dispatch<any>) => {
-    api.getJobRuns(query, page, size).then((r: JobRun[]) => {
+export function getJobRuns(query: Query, page: number, size: number) {
+  return (dispatch: Dispatch) => {
+    return api.getJobRuns(query, page, size).then((r: JobRun[]) => {
       const normalizedData = normalize(r, { endpoint: 'jobRuns' })
       const action: JobRunsAction = {
         type: 'UPSERT_JOB_RUNS',
@@ -18,9 +20,11 @@ const getJobRuns = (query: Query, page: number, size: number) => {
   }
 }
 
-const getJobRun = (jobRunId?: string) => {
-  return (dispatch: Dispatch<any>) => {
-    api.getJobRun(jobRunId).then((r: JobRun) => {
+export function getJobRun(
+  jobRunId?: string,
+): ThunkAction<Promise<void>, AppState, void, Action<string>> {
+  return (dispatch: Dispatch) => {
+    return api.getJobRun(jobRunId).then((r: JobRun) => {
       const normalizedData = normalize(r, { endpoint: 'jobRun' })
       const action: JobRunsAction = {
         type: 'UPSERT_JOB_RUN',
@@ -31,5 +35,3 @@ const getJobRun = (jobRunId?: string) => {
     })
   }
 }
-
-export { getJobRuns, getJobRun }
