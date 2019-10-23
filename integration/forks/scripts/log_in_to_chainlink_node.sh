8c186d52822326b00c9aca4489a0fddcc76693c6
Ryan Hall <hall.ryan.r@gmail.com>
2019-10-23 16:30:08 -0400

    remove secrets from forks/ directory

--- integration/forks/scripts/log_in_to_chainlink_node.sh
+++ integration/forks/scripts/log_in_to_chainlink_node.sh
@@ -1,6 +1,6 @@
 login() {
     docker exec -it forks_chainlink chainlink \
-           admin login -f /run/secrets/node_api_credentials
+           admin login -f /run/secrets/apicredentials
 }
 
 # Keep trying to log in until chainlink has started its RPC service
