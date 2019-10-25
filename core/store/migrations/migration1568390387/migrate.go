d3c6e3d2f9e26e753ef5dd04714a3e0d56a03876
Dimitri Roche <dimroc@gmail.com>
2019-10-25 09:12:18 -0400

    encumbrance migration has proper vchar length,just bytea:
    not an array of bytea. bytea is already an array.

--- core/store/migrations/migration1568390387/migrate.go
+++ core/store/migrations/migration1568390387/migrate.go
@@ -8,9 +8,9 @@ import (
 // Migrate amends the encumbrances table to include the aggregator contact details
 func Migrate(tx *gorm.DB) error {
 	if err := tx.Exec(
-		`ALTER TABLE encumbrances ADD COLUMN "aggregator"                VARCHAR(40);
-     ALTER TABLE encumbrances ADD COLUMN "agg_initiate_job_selector" BYTEA[4];
-     ALTER TABLE encumbrances ADD COLUMN "agg_fulfill_selector"      BYTEA[4];`,
+		`ALTER TABLE encumbrances ADD COLUMN "aggregator"                VARCHAR(42);
+     ALTER TABLE encumbrances ADD COLUMN "agg_initiate_job_selector" BYTEA;
+     ALTER TABLE encumbrances ADD COLUMN "agg_fulfill_selector"      BYTEA;`,
 	).Error; err != nil {
 		return errors.Wrap(err, "failed to automigrate encumbrances to include aggregator info")
 	}
