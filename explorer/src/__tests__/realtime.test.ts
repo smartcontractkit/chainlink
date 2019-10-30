a2227840ebd8ec6ca7320cf5854a830f2f52f4b9
Ryan Hall <hall.ryan.r@gmail.com>
2019-10-30 11:02:15 -0400

    add more tests surrounding error handling

--- explorer/src/__tests__/realtime.test.ts
+++ explorer/src/__tests__/realtime.test.ts
@@ -68,7 +68,7 @@ describe('realtime', () => {
 
   afterAll(done => stop(server, done))
 
-  describe('legacy message format', () => {
+  describe('when sending messages in legacy format', () => {
     it('can create a job run with valid JSON', async () => {
       expect.assertions(3)
 
@@ -190,125 +190,202 @@ describe('realtime', () => {
     })
   })
 
-  describe('jsonRPC message format', () => {
-    it('can create a job run with valid JSON', async () => {
-      expect.assertions(3)
+  describe('when sending messages in JSON-RPC format', () => {
+    describe('#upsertJobRun', () => {
+      it('can create a job run with valid JSON', async () => {
+        expect.assertions(3)
 
-      const ws = await authenticatedNode()
+        const ws = await authenticatedNode()
 
-      ws.send(JSON.stringify(createFixture))
+        ws.send(JSON.stringify(createFixture))
 
-      await new Promise(resolve => {
-        ws.on('message', (data: WebSocket.Data) => {
-          const response = JSON.parse(data as string)
-          expect(response.result).toEqual('success')
-          ws.close()
-          resolve()
-        })
-      })
-
-      const jobRunCount = await db.manager.count(JobRun)
-      expect(jobRunCount).toEqual(1)
-
-      const taskRunCount = await db.manager.count(TaskRun)
-      expect(taskRunCount).toEqual(1)
-    })
-
-    it('can create and update a job run and task runs', async () => {
-      expect.assertions(6)
-
-      const ws = await authenticatedNode()
-
-      ws.send(JSON.stringify(createFixture))
-
-      await new Promise(resolve => {
-        ws.on('message', (data: any) => {
-          const response = JSON.parse(data)
-
-          if (response.id === createFixture.id) {
-            expect(response.result).toEqual('success')
-            ws.send(JSON.stringify(updateFixture))
-          }
-
-          if (response.id === updateFixture.id) {
+        await new Promise(resolve => {
+          ws.on('message', (data: WebSocket.Data) => {
+            const response = JSON.parse(data as string)
             expect(response.result).toEqual('success')
             ws.close()
             resolve()
-          }
+          })
         })
+
+        const jobRunCount = await db.manager.count(JobRun)
+        expect(jobRunCount).toEqual(1)
+
+        const taskRunCount = await db.manager.count(TaskRun)
+        expect(taskRunCount).toEqual(1)
       })
 
-      const jobRunCount = await db.manager.count(JobRun)
-      expect(jobRunCount).toEqual(1)
+      it('can create and update a job run and task runs', async () => {
+        expect.assertions(6)
 
-      const taskRunCount = await db.manager.count(TaskRun)
-      expect(taskRunCount).toEqual(1)
+        const ws = await authenticatedNode()
 
-      const jr = await db.manager.findOne(JobRun)
-      expect(jr.status).toEqual('completed')
+        ws.send(JSON.stringify(createFixture))
 
-      const tr = jr.taskRuns[0]
-      expect(tr.status).toEqual('completed')
-    })
+        await new Promise(resolve => {
+          ws.on('message', (data: any) => {
+            const response = JSON.parse(data)
 
-    it('can create a task run with transactionHash and status', async () => {
-      expect.assertions(10)
+            if (response.id === createFixture.id) {
+              expect(response.result).toEqual('success')
+              ws.send(JSON.stringify(updateFixture))
+            }
 
-      const ws = await authenticatedNode()
+            if (response.id === updateFixture.id) {
+              expect(response.result).toEqual('success')
+              ws.close()
+              resolve()
+            }
+          })
+        })
 
-      const messageReceived = new Promise(resolve => {
-        ws.on('message', (data: any) => {
+        const jobRunCount = await db.manager.count(JobRun)
+        expect(jobRunCount).toEqual(1)
+
+        const taskRunCount = await db.manager.count(TaskRun)
+        expect(taskRunCount).toEqual(1)
+
+        const jr = await db.manager.findOne(JobRun)
+        expect(jr.status).toEqual('completed')
+
+        const tr = jr.taskRuns[0]
+        expect(tr.status).toEqual('completed')
+      })
+
+      it('can create a task run with transactionHash and status', async () => {
+        expect.assertions(10)
+
+        const ws = await authenticatedNode()
+
+        const messageReceived = new Promise(resolve => {
+          ws.on('message', (data: any) => {
+            const response = JSON.parse(data)
+            expect(response.result).toEqual('success')
+            resolve()
+          })
+        })
+
+        ws.send(JSON.stringify(ethtxFixture))
+        await messageReceived
+
+        const jobRunCount = await db.manager.count(JobRun)
+        expect(jobRunCount).toEqual(1)
+
+        const taskRunCount = await db.manager.count(TaskRun)
+        expect(taskRunCount).toEqual(4)
+
+        const jobRunRepository = getCustomRepository(JobRunRepository, db.name)
+        const jr = await jobRunRepository.getFirst()
+
+        expect(jr.status).toEqual('completed')
+
+        const tr = jr.taskRuns[3]
+        expect(tr.status).toEqual('completed')
+        expect(tr.transactionHash).toEqual(
+          '0x1111111111111111111111111111111111111111111111111111111111111111',
+        )
+        expect(tr.timestamp).toEqual(new Date('2018-01-08T18:12:01.103Z'))
+        expect(tr.blockHeight).toEqual('3735928559')
+        expect(tr.blockHash).toEqual('0xbadc0de5')
+        expect(tr.transactionStatus).toEqual('fulfilledRunLog')
+        ws.close()
+      })
+
+      it('rejects malformed json events with code -32602', async (done: any) => {
+        expect.assertions(2)
+
+        const ws = await authenticatedNode()
+
+        const invalidJSON = {
+          jsonrpc: '2.0',
+          method: 'upsertJobRun',
+          id: 1,
+          params: {
+            invalid: 'json',
+          },
+        }
+
+        ws.send(JSON.stringify(invalidJSON))
+
+        ws.on('message', async (data: any) => {
           const response = JSON.parse(data)
-          expect(response.result).toEqual('success')
-          resolve()
+          expect(response.error.code).toEqual(-32602)
+
+          const count = await db.manager.count(JobRun)
+          expect(count).toEqual(0)
+
+          ws.close()
+          done()
         })
       })
-
-      ws.send(JSON.stringify(ethtxFixture))
-      await messageReceived
-
-      const jobRunCount = await db.manager.count(JobRun)
-      expect(jobRunCount).toEqual(1)
-
-      const taskRunCount = await db.manager.count(TaskRun)
-      expect(taskRunCount).toEqual(4)
-
-      const jobRunRepository = getCustomRepository(JobRunRepository, db.name)
-      const jr = await jobRunRepository.getFirst()
-
-      expect(jr.status).toEqual('completed')
-
-      const tr = jr.taskRuns[3]
-      expect(tr.status).toEqual('completed')
-      expect(tr.transactionHash).toEqual(
-        '0x1111111111111111111111111111111111111111111111111111111111111111',
-      )
-      expect(tr.timestamp).toEqual(new Date('2018-01-08T18:12:01.103Z'))
-      expect(tr.blockHeight).toEqual('3735928559')
-      expect(tr.blockHash).toEqual('0xbadc0de5')
-      expect(tr.transactionStatus).toEqual('fulfilledRunLog')
-      ws.close()
     })
 
-    it('rejects malformed json events with code -32602', async (done: any) => {
+    it('rejects non-existing methods with code -32601', async (done: any) => {
       expect.assertions(2)
 
       const ws = await authenticatedNode()
 
-      const invalidJSON = {
+      const request = {
         jsonrpc: '2.0',
-        method: 'upsertJobRun',
+        method: 'doesNotExist',
         id: 1,
-        params: {
-          invalid: 'json',
-        },
       }
 
-      ws.send(JSON.stringify(invalidJSON))
+      ws.send(JSON.stringify(request))
 
       ws.on('message', async (data: any) => {
         const response = JSON.parse(data)
-        expect(response.error.code).toEqual(-32602)
+        expect(response.error.code).toEqual(-32601)
+
+        const count = await db.manager.count(JobRun)
+        expect(count).toEqual(0)
+
+        ws.close()
+        done()
+      })
+    })
+
+    // this test depends on the presence of "jsonrpc" in the message
+    // otherwise, the server will attempt to process the message as a
+    // legacy message and will respond with { status: 422 }.
+    // This test will be more appropriate once the legacy format is removed.
+    it('rejects malformed json with code -32700', async (done: any) => {
+      expect.assertions(2)
+
+      const ws = await authenticatedNode()
+
+      const request = 'jsonrpc invalid'
+
+      ws.send(request)
+
+      ws.on('message', async (data: any) => {
+        const response = JSON.parse(data)
+        expect(response.error.code).toEqual(-32700)
+
+        const count = await db.manager.count(JobRun)
+        expect(count).toEqual(0)
+
+        ws.close()
+        done()
+      })
+    })
+
+    it('rejects invalid rpc requests with code -32600', async (done: any) => {
+      expect.assertions(2)
+
+      const ws = await authenticatedNode()
+
+      const request = {
+        jsonrpc: '2.0',
+        function: 'foo',
+        id: 1,
+      }
+
+      ws.send(JSON.stringify(request))
+
+      ws.on('message', async (data: any) => {
+        const response = JSON.parse(data)
+        expect(response.error.code).toEqual(-32600)
 
         const count = await db.manager.count(JobRun)
         expect(count).toEqual(0)
