f82a5c146057e364ae047c6526b5c0f11d233e8f
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-10-30 16:53:56 -0400

    Deploy LinkToken contract only when needed
    LinkToken contract deploys are now conditional, being deployed when the
    LINK_TOKEN_ADDRESS environment variable has not been set yet.

--- integration-scripts/src/deployContracts.ts
+++ integration-scripts/src/deployContracts.ts
@@ -1,4 +1,3 @@
-import { LinkTokenFactory } from 'chainlink/dist/src/generated/LinkTokenFactory'
 import { OracleFactory } from './generated/OracleFactory'
 import {
   createProvider,
@@ -9,6 +8,7 @@ import {
 } from './common'
 import { EthLogFactory } from './generated/EthLogFactory'
 import { RunLogFactory } from './generated/RunLogFactory'
+import { deployLinkTokenContract } from './deployLinkTokenContract'
 
 async function main() {
   registerPromiseHandler()
@@ -25,18 +25,13 @@ async function deployContracts({ chainlinkNodeAddress }: Args) {
   const provider = createProvider()
   const signer = provider.getSigner(DEVNET_ADDRESS)
 
-  const linkToken = await deployContract({
-    Factory: LinkTokenFactory,
-    name: 'LinkToken',
-    signer,
-  })
+  const linkToken = await deployLinkTokenContract()
 
   const oracle = await deployContract(
     { Factory: OracleFactory, name: 'Oracle', signer },
     linkToken.address,
   )
   await oracle.setFulfillmentPermission(chainlinkNodeAddress, true)
-  console.log(`Deployed Oracle at: ${oracle.address}`)
 
   await deployContract({ Factory: EthLogFactory, name: 'EthLog', signer })
 
