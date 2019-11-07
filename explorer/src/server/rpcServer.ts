19a25af069e395375cd801d20c0f8283c13d0bb4
Ryan Hall <hall.ryan.r@gmail.com>
2019-11-07 10:33:33 -0500

    remove rpcServer call function type patch

--- explorer/src/server/rpcServer.ts
+++ explorer/src/server/rpcServer.ts
@@ -5,16 +5,4 @@ const serverOptions = {
   useContext: true, // permits passing extra data object to RPC methods as 'server context'
 }
 
-export const rpcServer = new jayson.Server(rpcMethods, serverOptions)
-
-// broken typing for server.call - should be able to accept 3 arguments
-// https://github.com/tedeh/jayson#server-context
-// https://github.com/tedeh/jayson/pull/152
-
-type callFunction = (
-  request: jayson.JSONRPCRequestLike | Array<jayson.JSONRPCRequestLike>,
-  context: object,
-  originalCallback?: jayson.JSONRPCCallbackType,
-) => void
-
-export const callRPCServer: callFunction = rpcServer.call.bind(rpcServer)
+export default new jayson.Server(rpcMethods, serverOptions)
