5045b8dbe316a6a1908461fdc121d74211d1c0c5
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-11-07 17:01:42 -0800

    Rename operators action to adminOperators

--- explorer/client/src/actions/adminOperators.ts
+++ explorer/client/src/actions/adminOperators.ts
@@ -2,8 +2,8 @@ import normalize from 'json-api-normalizer'
 import { request } from './helpers'
 import * as api from '../api/index'
 
-export const fetchOperators = request(
-  'OPERATORS',
+export const fetchAdminOperators = request(
+  'ADMIN_OPERATORS',
   api.v1.adminOperators.getOperators,
   json => normalize(json, { endpoint: 'currentPageOperators' }),
 )
