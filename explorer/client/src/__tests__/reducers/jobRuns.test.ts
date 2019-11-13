12f64cf4e9a3cf8f29ba28ff37cfbd8c96b8bef5
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-11-13 10:26:42 -0800

    Use mockPartial helper in jobRuns reducer tests

--- explorer/client/src/__tests__/reducers/jobRuns.test.ts
+++ explorer/client/src/__tests__/reducers/jobRuns.test.ts
@@ -3,10 +3,13 @@ import reducer, {
   AppState,
 } from '../../reducers'
 import {
+  JobRunsNormalizedData,
+  JobRunNormalizedData,
   FetchJobRunsSucceededAction,
   FetchJobRunSucceededAction,
 } from '../../reducers/actions'
 import { JobRun } from 'explorer/models'
+import { mockPartial } from '../support/mocks'
 
 const INITIAL_JOB_RUN = { id: 'replace-me' } as JobRun
 
@@ -20,25 +23,23 @@ const INITIAL_STATE: AppState = {
 describe('reducers/jobRuns', () => {
   describe('FETCH_JOB_RUNS_SUCCEEDED', () => {
     it('can replace items', () => {
-      const normalizedJobRuns = {
-        '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e': {
-          id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e',
-        },
-      }
-      const orderedJobRuns = [{ id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e' }]
-      const data = {
-        jobRuns: normalizedJobRuns,
-        meta: {
-          currentPageJobRuns: {
-            data: orderedJobRuns,
-            meta: {},
+      const data = mockPartial<JobRunsNormalizedData>({
+        jobRuns: {
+          '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e': {
+            id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e',
           },
         },
-      }
-      const action = {
+        meta: {
+          currentPageJobRuns: {
+            data: [{ id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e' }],
+            meta: { count: 100 },
+          },
+        },
+      })
+      const action: FetchJobRunsSucceededAction = {
         type: 'FETCH_JOB_RUNS_SUCCEEDED',
         data: data,
-      } as FetchJobRunsSucceededAction
+      }
       const state = reducer(INITIAL_STATE, action)
 
       expect(state.jobRuns).toEqual({
@@ -53,21 +54,20 @@ describe('reducers/jobRuns', () => {
 
   describe('FETCH_JOB_RUN_SUCCEEDED', () => {
     it('can replace items', () => {
-      const normalizedJobRuns = {
-        '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e': {
-          id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e',
+      const data = mockPartial<JobRunNormalizedData>({
+        jobRuns: {
+          '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e': {
+            id: '9b7d791a-9a1f-4c55-a6be-b4231cf9fd4e',
+          },
         },
-      }
-      const data = {
-        jobRuns: normalizedJobRuns,
         meta: {
           jobRun: { meta: {} },
         },
-      }
-      const action = {
+      })
+      const action: FetchJobRunSucceededAction = {
         type: 'FETCH_JOB_RUN_SUCCEEDED',
         data: data,
-      } as FetchJobRunSucceededAction
+      }
       const state = reducer(INITIAL_STATE, action)
 
       expect(state.jobRuns).toEqual({
