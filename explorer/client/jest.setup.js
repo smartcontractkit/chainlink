7b87de14971cac5f89e96c82aace44b93aa66f98
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-11-13 11:35:49 -0800

    Convert jest setup from js -> ts
    - Add basic typings for javascript-time-ago lib

--- explorer/client/jest.setup.js
+++
@@ -1,9 +0,0 @@
-const { configure } = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
-
-configure({ adapter: new Adapter() })
-
-const JavascriptTimeAgo = require('javascript-time-ago')
-const en = require('javascript-time-ago/locale/en')
-
-JavascriptTimeAgo.locale(en)
