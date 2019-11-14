125119ca4949a73952a418dc787192fcddb07ab6
Ryan Hall <hall.ryan.r@gmail.com>
2019-11-14 17:38:49 -0500

    make url optional on remote_client

--- core/cmd/remote_client.go
+++ core/cmd/remote_client.go
@@ -68,19 +68,28 @@ func (cli *Client) CreateServiceAgreement(c *clipkg.Context) error {
 
 // CreateExternalInitiator adds an external initiator
 func (cli *Client) CreateExternalInitiator(c *clipkg.Context) error {
-	if c.NArg() != 2 {
-		return cli.errorOut(errors.New("create expects two arguments: a name and a url"))
+	if c.NArg() != 1 && c.NArg() != 2 {
+		return cli.errorOut(errors.New("create expects 1 - 2 arguments: a name and a url (optional)"))
 	}
 
-	var request models.ExternalInitiatorRequest
-	request.Name = c.Args().Get(0)
-	if url, err := url.ParseRequestURI(c.Args().Get(1)); err == nil {
-		request.URL = models.WebURL(*url)
+	var requestData []byte
+	var err error
+
+	if c.NArg() == 2 {
+		var request models.ExternalInitiatorRequest
+		request.Name = c.Args().Get(0)
+		if url, err := url.ParseRequestURI(c.Args().Get(1)); err == nil {
+			request.URL = models.WebURL(*url)
+			requestData, err = json.Marshal(request)
+		} else {
+			return cli.errorOut(err)
+		}
 	} else {
-		return cli.errorOut(err)
+		var request models.ExternalInitiatorRequestWithoutURL
+		request.Name = c.Args().Get(0)
+		requestData, err = json.Marshal(request)
 	}
 
-	requestData, err := json.Marshal(request)
 	if err != nil {
 		return cli.errorOut(err)
 	}
