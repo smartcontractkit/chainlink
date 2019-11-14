938cf01d44c656054276394468c8391f184969bd
Steve Ellis <email@steveell.is>
2019-11-14 00:36:32 -0500

    prevent invalid runlog spec from being created

--- core/services/validators_test.go
+++ core/services/validators_test.go
@@ -54,6 +54,21 @@ func TestValidateJob(t *testing.T) {
 			cltest.MustReadFile(t, "testdata/task_only_job.json"),
 			models.NewJSONAPIErrorsWith("Must have at least one Initiator and one Task"),
 		},
+		{
+			"runlog and ethtx with an address",
+			cltest.MustReadFile(t, "testdata/runlog_ethtx_w_address_job.json"),
+			models.NewJSONAPIErrorsWith("Cannot set EthTx Task's address parameter with a RunLog Initiator"),
+		},
+		{
+			"runlog and ethtx with a function selector",
+			cltest.MustReadFile(t, "testdata/runlog_ethtx_w_funcselector_job.json"),
+			models.NewJSONAPIErrorsWith("Cannot set EthTx Task's function selector parameter with a RunLog Initiator"),
+		},
+		{
+			"runlog with two ethtx tasks",
+			cltest.MustReadFile(t, "testdata/runlog_2_ethlogs_job.json"),
+			models.NewJSONAPIErrorsWith("Cannot RunLog initiated jobs cannot have more than one EthTx Task"),
+		},
 	}
 
 	store, cleanup := cltest.NewStore(t)
