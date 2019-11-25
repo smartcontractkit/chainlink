ee737fa20b47b961490215898a5500fde2360c79
Steve Ellis <email@steveell.is>
2019-11-25 17:27:13 -0500

    update docs for the withdrawal interface

--- evm/v0.5/contracts/interfaces/WithdrawalInterface.sol
+++ evm/v0.5/contracts/interfaces/WithdrawalInterface.sol
@@ -2,14 +2,15 @@ pragma solidity ^0.5.0;
 
 interface WithdrawalInterface {
   /**
-   * @notice transfer LINK to another address
+   * @notice transfer LINK held by the contract belonging to msg.sender to
+   * another address
    * @param recipient is the address to send the LINK to
    * @param amount is the amount of LINK to send
    */
   function withdraw(address recipient, uint256 amount) external;
 
   /**
-   * @notice query the available amount of LINK to withdraw
+   * @notice query the available amount of LINK to withdraw by msg.sender
    */
   function withdrawable() external view returns (uint256);
 }
