f1aee5d7adf7d33cd52ae0662bf0555bf03f2346
Thomas Hodges <me@thodges.com>
2019-11-26 14:47:32 -0600

    PrepaidAggregator implements AggregatorInterface

---
+++ evm/v0.5/contracts/dev/AggregatorInterface.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.5.0;
+
+interface AggregatorInterface {
+  function currentAnswer() external view returns (int256);
+  function updatedTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+}
