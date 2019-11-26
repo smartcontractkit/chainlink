104364c3c324bfe6e0c7515a959a57c41e125dc9
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-11-26 13:31:11 -0800

    Extract common redux setup into separate package

--- operator_ui/src/createStore.ts
+++ operator_ui/src/createStore.ts
@@ -1,18 +1,12 @@
-import { createStore, applyMiddleware, Middleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { Middleware } from 'redux'
+import { createStore } from '@chainlink/redux'
 import thunkMiddleware from 'redux-thunk'
-import logger from 'redux-logger'
 import { createExplorerConnectionMiddleware } from './middleware'
 import reducer from './reducers'
 
-let middleware: Middleware[] = [
+const middleware: Middleware[] = [
   thunkMiddleware,
   createExplorerConnectionMiddleware(),
 ]
-if (process.env.LOG_REDUX === 'true') {
-  middleware = middleware.concat(logger)
-}
-const composeEnhancers = composeWithDevTools({})
 
-export default () =>
-  createStore(reducer, composeEnhancers(applyMiddleware(...middleware)))
+export default () => createStore(reducer, middleware)
