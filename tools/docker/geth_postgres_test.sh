a6ce7f5c2f445f858885a8562724b385f8138824
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-11-28 18:38:54 -0500

    Handle account funding for geth nodes

--- tools/docker/geth_postgres_test.sh
+++ tools/docker/geth_postgres_test.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 set -e
-set -x
 
+export GETH_MODE=TRUE
 all_files="-f docker-compose.yaml -f docker-compose.gethnet.yaml -f docker-compose.postgres.yaml -f docker-compose.integration.yaml"
-docker-compose $all_files build --parallel
-docker-compose $all_files up --exit-code-from integration
+base="docker-compose $all_files"
+dev="$base -f docker-compose.dev.yaml"
+
+case "$1" in
+  base)
+    echo $base
+    ;;
+  down)
+    $base down
+    ;;
+  down:clean)
+    $base down -v
+    ;;
+  up)
+    $base up
+    ;;
+  up:dev)
+    $dev up
+    ;;
+  build)
+    $base build --parallel
+    ;;
+  test)
+    $base up --exit-code-from integration
+    ;;
+  *)
+    ./geth_postgres_test.sh build
+    ./geth_postgres_test.sh test
+    ;;
+esac
