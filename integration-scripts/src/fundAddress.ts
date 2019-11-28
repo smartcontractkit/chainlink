712b28b860529084cf8406b40bc76c73a3a1c41a
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-11-28 18:38:54 -0500

    Handle account funding for geth nodes

--- integration-scripts/src/fundAddress.ts
+++ integration-scripts/src/fundAddress.ts
@@ -1,25 +1,29 @@
 import { utils } from 'ethers'
-import {
-  DEVNET_ADDRESS,
-  registerPromiseHandler,
-  createProvider,
-  getArgs,
-} from './common'
+import { registerPromiseHandler, createProvider, getArgs } from './common'
 
+/**
+ * This script is used to fund a chainlink node address given a developer account.
+ * This is used in our integration test to fund our devnet account from a geth developer account.
+ * It isnt used in our parity version of the integration test since the parity account is already funded for us.
+ */
 async function main() {
   registerPromiseHandler()
-  const args = getArgs(['CHAINLINK_NODE_ADDRESS'])
+  const args = getArgs(['CHAINLINK_NODE_ADDRESS', 'DEVELOPER_ACCOUNT'])
 
-  await fundAddress({ recipient: args.CHAINLINK_NODE_ADDRESS })
+  await fundAddress({
+    recipient: args.CHAINLINK_NODE_ADDRESS,
+    signerAddress: args.DEVELOPER_ACCOUNT,
+  })
 }
 main()
 
 interface Args {
   recipient: string
+  signerAddress: string
 }
-async function fundAddress({ recipient }: Args) {
+async function fundAddress({ recipient, signerAddress }: Args) {
   const provider = createProvider()
-  const signer = provider.getSigner(DEVNET_ADDRESS)
+  const signer = provider.getSigner(signerAddress)
 
   const tx = await signer.sendTransaction({
     to: recipient,
