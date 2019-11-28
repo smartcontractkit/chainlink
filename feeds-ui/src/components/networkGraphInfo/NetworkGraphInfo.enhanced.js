f4cb73b9a76faac4a2d5d44460db20c59b0f3b80
Bartosz Wojciechowski <32734780+hellobart@users.noreply.github.com>
2019-11-28 20:42:30 +0100

    Merge pull request #6 from smartcontractkit/169957214-Improve-aggregator-UX
    Improve aggregator UX

--- feeds-ui/src/components/networkGraphInfo/NetworkGraphInfo.enhanced.js
+++ feeds-ui/src/components/networkGraphInfo/NetworkGraphInfo.enhanced.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux'
 const mapStateToProps = state => ({
   currentAnswer: state.aggregation.currentAnswer,
   pendingAnswerId: state.aggregation.pendingAnswerId,
-  nextAnswerId: state.aggregation.nextAnswerId,
   oracleResponse: state.aggregation.oracleResponse,
   oracles: state.aggregation.oracles,
   requestTime: state.aggregation.requestTime,
