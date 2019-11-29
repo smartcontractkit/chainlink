103ca941c83e897950f5ecd88786bc1dc42d13c8
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-11-28 21:27:37 -0500

    Add help command to test scripts

--- tools/docker/geth_postgres_test.sh
+++ tools/docker/geth_postgres_test.sh
@@ -5,8 +5,25 @@ export GETH_MODE=TRUE
 all_files="-f docker-compose.yaml -f docker-compose.gethnet.yaml -f docker-compose.postgres.yaml -f docker-compose.integration.yaml"
 base="docker-compose $all_files"
 dev="$base -f docker-compose.dev.yaml"
+usage="geth_postgres_test -- run the integration test suite against geth as the ethereum node, and postgres as the backing database for chainlink
+
+Commands:
+    base        Echo the docker-compose command with all of its composed files,
+                useful for running docker-compose commands that aren't shortcutted here.
+                NOTE: You'll want to set the environment variable GETH_MODE=TRUE if you're using this command to run the integration tests. 
+                Otherwise, tests will expect that parity is running due to the lack of the set environment variable.
+    down        Brings down all services.
+    down:clean  Brings down all services and removes any volumes, good for clean slate testing.
+    up          Brings up all services.
+    up:dev      Brings up all services, and bind-mounts source files for quick changes without rebuilding a container. 
+                See docker-compose.dev.yaml for the list of bind-mounted folders per service.
+    build       Builds all images in parallel.
+    test        Runs the test suite, exiting on any failures."
 
 case "$1" in
+  help)
+    echo "$usage"
+    ;;
   base)
     echo $base
     ;;
--- tools/docker/parity_postgres_test.sh
+++ tools/docker/parity_postgres_test.sh
@@ -4,8 +4,23 @@ set -e
 all_files="-f docker-compose.yaml -f docker-compose.paritynet.yaml -f docker-compose.postgres.yaml -f docker-compose.integration.yaml"
 base="docker-compose $all_files"
 dev="$base -f docker-compose.dev.yaml"
+usage="parity_postgres_test -- run the integration test suite against parity as the ethereum node, and postgres as the backing database for chainlink\
+
+Commands:
+    base        Echo the docker-compose command with all of its composed files,
+                useful for running docker-compose commands that aren't shortcutted here
+    down        Brings down all services.
+    down:clean  Brings down all services and removes any volumes, good for clean slate testing.
+    up          Brings up all services.
+    up:dev      Brings up all services, and bind-mounts source files for quick changes without rebuilding a container.
+                See docker-compose.dev.yaml for the list of bind-mounted folders per service.
+    build       Builds all images in parallel.
+    test        Runs the test suite, exiting on any failures."
 
 case "$1" in
+  help)
+    echo "$usage"
+    ;;
   base)
     echo $base
     ;;
