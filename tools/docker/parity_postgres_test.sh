362e4c2ce51d2dcd935b8d50864e300dd3ba799d
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-11-28 20:17:00 -0500

    Add configurable parity test

--- tools/docker/parity_postgres_test.sh
+++ tools/docker/parity_postgres_test.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 set -e
-set -x
 
 all_files="-f docker-compose.yaml -f docker-compose.paritynet.yaml -f docker-compose.postgres.yaml -f docker-compose.integration.yaml"
-docker-compose $all_files build --parallel
-docker-compose $all_files up --exit-code-from integration
+base="docker-compose $all_files"
+dev="$base -f docker-compose.dev.yaml"
+
+case "$1" in
+  base)
+    echo $base
+    ;;
+  down)
+    $base down
+    ;;
+  down:clean)
+    $base down -v
+    ;;
+  up)
+    $base up
+    ;;
+  up:dev)
+    $dev up
+    ;;
+  build)
+    $base build --parallel
+    ;;
+  test)
+    $base up --exit-code-from integration
+    ;;
+  *)
+    ./parity_postgres_test.sh build
+    ./parity_postgres_test.sh test
+    ;;
+esac
