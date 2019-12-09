6cfd96415cc41afc2f22da0b96e73a4bbc9724c9
Ryan Hall <hall.ryan.r@gmail.com>
2019-12-09 18:18:52 -0500

    uncouple initiateServiceAgreement and helpersV2

--- integration-scripts/src/initiateServiceAgreement.ts
+++ integration-scripts/src/initiateServiceAgreement.ts
@@ -7,13 +7,24 @@ import {
   createProvider,
 } from './common'
 import { ethers } from 'ethers'
-import {
-  encodeServiceAgreement,
-  encodeOracleSignatures,
-  OracleSignatures,
-  ServiceAgreement,
-} from '../../evm/src/helpersV2'
 const { CoordinatorFactory } = chainlink
+
+interface ServiceAgreement {
+  payment: ethers.utils.BigNumberish // uint256
+  expiration: ethers.utils.BigNumberish // uint256
+  endAt: ethers.utils.BigNumberish // uint256
+  oracles: string[] // 0x hex representation of oracle addresses (uint160's)
+  requestDigest: string // 0x hex representation of bytes32
+  aggregator: string // 0x hex representation of aggregator address
+  aggInitiateJobSelector: string // 0x hex representation of aggregator.initiateAggregatorForJob function selector (uint32)
+  aggFulfillSelector: string // function selector for aggregator.fulfill
+}
+
+interface OracleSignatures {
+  vs: number[] // uint8[]
+  rs: string[] // bytes32[]
+  ss: string[] // bytes32[]
+}
 
 /**
  * This json definition may be missing types, it was generated from a fixture.
@@ -76,6 +87,33 @@ interface Args {
   normalizedRequest: string
   saJson: ServiceAgreementJson
   expectedAddress: string
+}
+
+const SERVICE_AGREEMENT_TYPES = [
+  'uint256',
+  'uint256',
+  'uint256',
+  'address[]',
+  'bytes32',
+  'address',
+  'bytes4',
+  'bytes4',
+]
+
+const ORACLE_SIGNATURES_TYPES = ['uint8[]', 'bytes32[]', 'bytes32[]']
+
+const encodeServiceAgreement = (serviceAgreement: ServiceAgreement) => {
+  return ethers.utils.defaultAbiCoder.encode(
+    SERVICE_AGREEMENT_TYPES,
+    Object.values(serviceAgreement),
+  )
+}
+
+const encodeOracleSignatures = (oracleSignatures: OracleSignatures) => {
+  return ethers.utils.defaultAbiCoder.encode(
+    ORACLE_SIGNATURES_TYPES,
+    Object.values(oracleSignatures),
+  )
 }
 
 async function initiateServiceAgreement({
