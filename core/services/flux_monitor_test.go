c47e3508657fd6eaf398c4f8153113c175e9e84c
Dimitri Roche <dimroc@gmail.com>
2019-12-14 16:38:21 -0500

    remove railway,test PollingDeviationChecker delay,parentctx

--- core/services/flux_monitor_test.go
+++ core/services/flux_monitor_test.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/shopspring/decimal"
@@ -126,9 +127,9 @@ func TestPollingDeviationChecker_Happy(t *testing.T) {
 	rm.On("Create", job.ID, &initr, &data, mock.Anything, mock.Anything).
 		Return(&run, nil)
 
-	checker, err := services.NewPollingDeviationChecker(context.Background(), initr, rm)
+	checker, err := services.NewPollingDeviationChecker(context.Background(), initr, rm, time.Second)
 	require.NoError(t, err)
-	assert.Equal(t, decimal.NewFromInt(0), checker.PreviousPrice())
+	assert.Equal(t, decimal.NewFromInt(0), checker.CurrentPrice())
 
 	ethClient := new(mocks.Client)
 	ethClient.On("GetAggregatorPrice", initr.InitiatorParams.Address, initr.InitiatorParams.Precision).
@@ -136,13 +137,13 @@ func TestPollingDeviationChecker_Happy(t *testing.T) {
 
 	require.NoError(t, checker.Initialize(ethClient))
 	ethClient.AssertExpectations(t)
-	assert.Equal(t, decimal.NewFromInt(100), checker.PreviousPrice())
+	assert.Equal(t, decimal.NewFromInt(100), checker.CurrentPrice())
 
 	assert.NoError(t, checker.Poll())
 
 	serverAssert()
 	rm.AssertExpectations(t)
-	assert.Equal(t, decimal.NewFromInt(102), checker.PreviousPrice())
+	assert.Equal(t, decimal.NewFromInt(102), checker.CurrentPrice())
 }
 
 func TestPollingDeviationChecker_InitializeError(t *testing.T) {
@@ -155,7 +156,52 @@ func TestPollingDeviationChecker_InitializeError(t *testing.T) {
 	ethClient.On("GetAggregatorPrice", initr.InitiatorParams.Address, initr.InitiatorParams.Precision).
 		Return(decimal.NewFromInt(0), errors.New("deliberate test error"))
 
-	checker, err := services.NewPollingDeviationChecker(context.Background(), initr, rm)
+	checker, err := services.NewPollingDeviationChecker(context.Background(), initr, rm, time.Second)
 	require.NoError(t, err)
 	require.Error(t, checker.Initialize(ethClient))
+}
+
+func TestPollingDeviationChecker_NoDeviationLoopsThenCancels(t *testing.T) {
+	// 1. Set up external adapter to mark when polled
+	priceResponse := `{"data":{"result": 100}}`
+	polled := make(chan struct{})
+	mockServer, serverAssert := cltest.NewHTTPMockServer(t, http.StatusOK, "POST", priceResponse, func(http.Header, string) {
+		polled <- struct{}{}
+	})
+	defer serverAssert()
+
+	// 2. Prepare initialization to 100, which matches external adapter, so no deviation
+	job := cltest.NewJobWithFluxMonitorInitiator()
+	initr := job.Initiators[0]
+	initr.ID = 1
+	initr.InitiatorParams.Feeds = models.Feeds([]string{mockServer.URL})
+
+	ethClient := new(mocks.Client)
+	ethClient.On("GetAggregatorPrice", initr.InitiatorParams.Address, initr.InitiatorParams.Precision).
+		Return(decimal.NewFromInt(100), nil)
+
+	// 3. Start() with no delay to speed up test and polling.
+	rm := new(mocks.RunManager)
+	parentCtx, cancel := context.WithCancel(context.Background())
+	checker, err := services.NewPollingDeviationChecker(parentCtx, initr, rm, 0)
+	require.NoError(t, err)
+	require.NoError(t, checker.Initialize(ethClient))
+
+	done := make(chan struct{})
+	go func() {
+		checker.Start() // Start() polling and delaying until cancel() from parent ctx.
+		done <- struct{}{}
+	}()
+
+	// 4. Check if Polled
+	cltest.CallbackOrTimeout(t, "start repeatedly polls external adapter", func() {
+		<-polled // launched at the beginning of Start before delay
+		<-polled // need two hits
+	})
+
+	// 5. Cancel parent context and ensure Start() stops.
+	cancel()
+	cltest.CallbackOrTimeout(t, "Start() unblocks and is done", func() {
+		<-done
+	})
 }
