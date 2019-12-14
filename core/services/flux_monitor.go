f5c29a3f1eda65b4db5500c3957d44c280e06e07
Dimitri Roche <dimroc@gmail.com>
2019-12-14 16:38:21 -0500

    remove railway,test PollingDeviationChecker delay,parentctx

--- core/services/flux_monitor.go
+++ core/services/flux_monitor.go
@@ -165,7 +165,7 @@ type DeviationCheckerFactory interface {
 type pollingDeviationCheckerFactory struct{}
 
 func (f pollingDeviationCheckerFactory) New(parentCtx context.Context, initr models.Initiator, runManager RunManager) (DeviationChecker, error) {
-	return NewPollingDeviationChecker(parentCtx, initr, runManager)
+	return NewPollingDeviationChecker(parentCtx, initr, runManager, 1*time.Second)
 }
 
 // DeviationChecker encapsulate methods needed to initialize and check prices
@@ -178,23 +178,24 @@ type DeviationChecker interface {
 
 // PollingDeviationChecker polls external price adapters via HTTP to check for price swings.
 type PollingDeviationChecker struct {
-	initr         models.Initiator
-	address       common.Address
-	requestData   models.JSON
-	threshold     float64
-	precision     int32
-	runManager    RunManager
-	previousPrice decimal.Decimal
-	fetcher       fetcher
-	ctx           context.Context
-	cancel        context.CancelFunc
+	initr        models.Initiator
+	address      common.Address
+	requestData  models.JSON
+	threshold    float64
+	precision    int32
+	runManager   RunManager
+	currentPrice decimal.Decimal
+	fetcher      fetcher
+	ctx          context.Context
+	cancel       context.CancelFunc
+	delay        time.Duration
 }
 
 // defaultHTTPTimeout is the timeout used by the price adapter fetcher for outgoing HTTP requests.
 const defaultHTTPTimeout = 5 * time.Second
 
 // NewPollingDeviationChecker returns a new instance of PollingDeviationChecker.
-func NewPollingDeviationChecker(parentCtx context.Context, initr models.Initiator, runManager RunManager) (*PollingDeviationChecker, error) {
+func NewPollingDeviationChecker(parentCtx context.Context, initr models.Initiator, runManager RunManager, delay time.Duration) (*PollingDeviationChecker, error) {
 	fetcher, err := newMedianFetcherFromURLs(
 		defaultHTTPTimeout,
 		initr.InitiatorParams.RequestData.String(),
@@ -206,16 +207,17 @@ func NewPollingDeviationChecker(parentCtx context.Context, initr models.Initiato
 
 	ctx, cancel := context.WithCancel(parentCtx)
 	return &PollingDeviationChecker{
-		initr:         initr,
-		address:       initr.InitiatorParams.Address,
-		requestData:   initr.InitiatorParams.RequestData,
-		threshold:     float64(initr.InitiatorParams.Threshold),
-		precision:     initr.InitiatorParams.Precision,
-		runManager:    runManager,
-		previousPrice: decimal.NewFromInt(0),
-		ctx:           ctx,
-		cancel:        cancel,
-		fetcher:       fetcher,
+		initr:        initr,
+		address:      initr.InitiatorParams.Address,
+		requestData:  initr.InitiatorParams.RequestData,
+		threshold:    float64(initr.InitiatorParams.Threshold),
+		precision:    initr.InitiatorParams.Precision,
+		runManager:   runManager,
+		currentPrice: decimal.NewFromInt(0),
+		ctx:          ctx,
+		cancel:       cancel,
+		fetcher:      fetcher,
+		delay:        delay,
 	}, nil
 }
 
@@ -223,7 +225,7 @@ func NewPollingDeviationChecker(parentCtx context.Context, initr models.Initiato
 // the deviation from.
 func (p *PollingDeviationChecker) Initialize(client eth.Client) error {
 	price, err := client.GetAggregatorPrice(p.address, p.precision)
-	p.previousPrice = price
+	p.currentPrice = price
 	return err
 }
 
@@ -235,7 +237,7 @@ func (p *PollingDeviationChecker) Start() {
 		select {
 		case <-p.ctx.Done():
 			return
-		case <-time.After(1 * time.Second):
+		case <-time.After(p.delay):
 		}
 	}
 }
@@ -245,101 +247,63 @@ func (p *PollingDeviationChecker) Stop() {
 	p.cancel()
 }
 
-// PreviousPrice returns the price used to check deviations against.
-func (p *PollingDeviationChecker) PreviousPrice() decimal.Decimal {
-	return p.previousPrice
+// CurrentPrice returns the price used to check deviations against.
+func (p *PollingDeviationChecker) CurrentPrice() decimal.Decimal {
+	return p.currentPrice
 }
 
-// Poll walks through the steps to check for a deviation, early exiting if any particular
-// step fails, or triggering a new job run.
-// Uses a railway paradigm: https://fsharpforfunandprofit.com/rop/
+// Poll walks through the steps to check for a deviation, early exiting if deviation
+// is not met, or triggering a new job run if deviation is met.
 func (p *PollingDeviationChecker) Poll() error {
-	_, err := railway(
-		newData(p.previousPrice),
-		railwayStep{"fetch current prices", p.fetchPrices},
-		railwayStep{"check if outside deviation", p.checkIfOutsideDeviation},
-		railwayStep{"create job run", p.createJobRun},
-		railwayStep{"update previous price", p.updatePreviousPrice}, // only reached if outside deviation
-	)
-	return err
-}
-
-// railway walks through a set of steps linearly, with each step having one of three
-// states: stop, continue, error.
-func railway(d *data, steps ...railwayStep) (*data, error) {
-	for _, step := range steps {
-		exit, err := step.operation(d)
-		if err != nil {
-			return d, errors.Wrapf(err, "on step %s", step.label)
-		}
-		if exit != nil {
-			logger.Infow(fmt.Sprintf("%s early exited: %s", step.label, exit.reason), "step", step.label)
-			return d, nil
-		}
+	nextPrice, err := p.fetchPrices()
+	if err != nil {
+		return err
 	}
-	return d, nil
+
+	deviated, err := p.checkIfOutsideDeviation(nextPrice)
+	if err != nil {
+		return err
+	}
+
+	if !deviated {
+		return nil // early exit since deviation criteria not met.
+	}
+
+	err = p.createJobRun(nextPrice)
+	if err != nil {
+		return err
+	}
+
+	p.currentPrice = nextPrice
+	return nil
 }
 
-type railwayStep struct {
-	label     string
-	operation func(*data) (*railwayExit, error)
-}
-
-type railwayExit struct {
-	reason string
-}
-
-func newRailwayExit(reason string) *railwayExit {
-	return &railwayExit{reason: reason}
-}
-
-func (p *PollingDeviationChecker) fetchPrices(d *data) (*railwayExit, error) {
+func (p *PollingDeviationChecker) fetchPrices() (decimal.Decimal, error) {
 	median, err := p.fetcher.Fetch()
-	d.MedianPrice = decimal.NewFromFloat(median)
-	return nil, errors.Wrap(err, "unable to fetch median price")
+	return decimal.NewFromFloat(median), errors.Wrap(err, "unable to fetch median price")
 }
 
-func (p *PollingDeviationChecker) checkIfOutsideDeviation(d *data) (*railwayExit, error) {
-	prevPrice := d.PreviousPrice
-	diff := prevPrice.Sub(d.MedianPrice).Abs()
-	perc := diff.Div(prevPrice).Mul(decimal.NewFromInt(100))
+func (p *PollingDeviationChecker) checkIfOutsideDeviation(nextPrice decimal.Decimal) (bool, error) {
+	curPrice := p.currentPrice
+	diff := curPrice.Sub(nextPrice).Abs()
+	perc := diff.Div(curPrice).Mul(decimal.NewFromInt(100))
+	if perc.LessThan(decimal.NewFromFloat(p.threshold)) {
+		logger.Debug(fmt.Sprintf("difference is %v%%, deviation threshold of %f%% not met", perc, p.threshold))
+		return false, nil
+	}
 	logger.Infow(
-		fmt.Sprintf("deviation of %v%% for threshold %f%% with %s", perc, p.threshold, d),
+		fmt.Sprintf("deviation of %v%% for threshold %f%% with previous price %v next price %v", perc, p.threshold, curPrice, nextPrice),
 		"threshold", p.threshold,
 		"deviation", perc,
 	)
-	if perc.LessThan(decimal.NewFromFloat(p.threshold)) {
-		reason := fmt.Sprintf("difference is %v%%, deviation threshold of %f%% not met", perc, p.threshold)
-		return newRailwayExit(reason), nil
-	}
-	return nil, nil
+	return true, nil
 }
 
-func (p *PollingDeviationChecker) createJobRun(d *data) (*railwayExit, error) {
-	runData, err := models.JSON{}.Add("result", fmt.Sprintf("%v", d.MedianPrice))
+func (p *PollingDeviationChecker) createJobRun(nextPrice decimal.Decimal) error {
+	runData, err := models.JSON{}.Add("result", fmt.Sprintf("%v", nextPrice))
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to start chainlink run")
+		return errors.Wrap(err, "unable to start chainlink run")
 	}
 	_, err = p.runManager.Create(p.initr.JobSpecID, &p.initr, &runData, nil, models.NewRunRequest())
-	return nil, err
-}
-
-func (p *PollingDeviationChecker) updatePreviousPrice(d *data) (*railwayExit, error) {
-	p.previousPrice = d.MedianPrice
-	return nil, nil
-}
-
-type data struct {
-	MedianPrice   decimal.Decimal
-	PreviousPrice decimal.Decimal
-}
-
-func newData(previousPrice decimal.Decimal) *data {
-	return &data{
-		PreviousPrice: previousPrice,
-	}
-}
-
-func (d *data) String() string {
-	return fmt.Sprintf("previous: %v, current median: %v", d.PreviousPrice, d.MedianPrice)
+	return err
 }
