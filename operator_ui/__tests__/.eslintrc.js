12125b38f6d6c2a1a79569762603f9da9ca79b78
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2019-12-16 18:59:55 -0500

    Prettier + eslint as workspace commands

--- operator_ui/__tests__/.eslintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ['@chainlink/eslint-config/jest'],
-}
--- tools/eslint-config/truffle.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@chainlink/eslint-config/mocha'],
-  globals: {
-    assert: 'readonly',
-    artifacts: 'readonly',
-    web3: 'readonly',
-    contract: 'readonly',
-  },
-}
