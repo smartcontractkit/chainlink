9e65a3938ce0ece5348733b4b46e00e07b87184f
Steve Ellis <email@steveell.is>
2019-12-19 11:30:28 -0500

    fix new round event ID report

--- evm/test/Aggregator.test.ts
+++ evm/test/Aggregator.test.ts
@@ -96,6 +96,15 @@ describe('Aggregator', () => {
         assertBigNum(ethers.constants.Zero, current)
       })
 
+      it('emits a new round log', async () => {
+        const requestTx = await rate.requestRateUpdate()
+        const receipt = await requestTx.wait()
+
+        const answerId = h.numToBytes32(1)
+        const newRoundLog = receipt.logs![receipt.logs!.length - 1]
+        assert.equal(answerId, newRoundLog.topics[1])
+      })
+
       it('trigger a request to the oracle and accepts a response', async () => {
         const requestTx = await rate.requestRateUpdate()
         const receipt = await requestTx.wait()
