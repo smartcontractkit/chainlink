9ab3ab0a71c591092a5f33423e2ca76853978888
Alex Coventry <alx@mit.edu>
2019-12-20 15:20:00 -0500

    Handle VRF proving keys
    Despite both being secp256k1 keys, VRF proving keys are handled completely
    differently from Ethereum keys, for the following reasons:
    
    - Preventing anyone from using the one type of key for the other purpose reduces
      the damage which could occur if the secret key leaks (e.g., through an error
      in the VRF.)
    
    - We need access to the secret key to construct VRF proofs, but go-ethereum's
      keystore doesn't provide public access to the secret key (it provides a public
      method for constructing signatures.)
    
    Abuse of one key in for the other purpose is prevented by prefixing the password
    with a string. The encryption/decryption framework from go-ethereum is used,
    though.

---
+++ core/store/models/vrf_key/private_key_test.go
@@ -0,0 +1,60 @@
+package vrf_key
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"regexp"
+	"testing"
+
+	"chainlink/core/services/vrf/generated/solidity_verifier_wrapper"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/eth"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var sk = 0xdeadbeefdeadbee
+var k = mustNewPrivateKey(big.NewInt(int64(sk)))
+var pkr = regexp.MustCompile(`PrivateKey\{k: <redacted>, PublicKey: 0x[0-9A-Fa-f]{128}\}`)
+
+func TestPrintingDoesNotLeakKey(t *testing.T) {
+	v := fmt.Sprintf("%v", k)
+	assert.Equal(t, v+"\n", fmt.Sprintln(k))
+	assert.Regexp(t, pkr, v)
+	assert.NotContains(t, v, fmt.Sprintf("%x", sk))
+	// Other verbs just give the corresponding encoding of .String()
+	assert.Equal(t, fmt.Sprintf("%x", k), hex.EncodeToString([]byte(v)))
+}
+
+func TestMarshaledProof(t *testing.T) {
+	proof, err := k.MarshaledProof(big.NewInt(1))
+	require.NoError(t, err)
+	// NB: For changes to the VRF solidity code to be reflected here, "go generate"
+	// must be run in core/services/vrf.
+	ethereumKey, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(ethereumKey)
+	genesisData := core.GenesisAlloc{auth.From: {Balance: big.NewInt(1000000000)}}
+	gasLimit := eth.DefaultConfig.Miner.GasCeil
+	backend := backends.NewSimulatedBackend(genesisData, gasLimit)
+	_, _, verifier, err := solidity_verifier_wrapper.DeployVRFTestHelper(auth, backend)
+	if err != nil {
+		panic(errors.Wrapf(err, "while initializing EVM contract wrapper"))
+	}
+	backend.Commit()
+	_, err = verifier.RandomValueFromVRFProof(nil, proof[:])
+	require.NoError(t, err)
+}
+
+func mustNewPrivateKey(rawKey *big.Int) *PrivateKey {
+	k, err := newPrivateKey(rawKey)
+	if err != nil {
+		panic(err)
+	}
+	return k
+}
---
+++ core/store/models/vrf_key/public_key.go
@@ -0,0 +1,81 @@
+package vrf_key
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/pkg/errors"
+	"go.dedis.ch/kyber/v3"
+
+	"chainlink/core/services/signatures/secp256k1"
+	"chainlink/core/utils"
+)
+
+const UncompressedPublicKeyLength = 64
+
+// Set sets k to the public key represented by l
+func (k *PublicKey) Set(l PublicKey) {
+	if copy(k[:], l[:]) != UncompressedPublicKeyLength {
+		panic(fmt.Errorf("failed to copy entire public key %x to %x", l, k))
+	}
+}
+
+// Point returns the secp256k1 point corresponding to k
+func (k *PublicKey) Point() (kyber.Point, error) {
+	return secp256k1.LongUnmarshal(k[:])
+}
+
+// NewPublicKey returns the PublicKey corresponding to rawKey
+func NewPublicKey(rawKey [64]byte) *PublicKey {
+	rv := PublicKey(rawKey)
+	return &rv
+}
+
+// NewPublicKeyFromHex returns the PublicKey encoded by 0x-hex string hex, or errors
+func NewPublicKeyFromHex(hex string) (*PublicKey, error) {
+	rawKey, err := hexutil.Decode(hex)
+	if err != nil {
+		return nil, err
+	}
+	if l := len(rawKey); l != UncompressedPublicKeyLength {
+		return nil, fmt.Errorf("wrong length for public key: %s of length %d", rawKey, l)
+	}
+	k := &PublicKey{}
+	if c := copy(k[:], rawKey[:]); c != UncompressedPublicKeyLength {
+		panic(fmt.Errorf("failed to copy entire key to return value"))
+	}
+	return k, err
+}
+
+// SetFromHex sets k to the public key represented by hex, which must represent
+// the uncompressed binary format
+func (k *PublicKey) SetFromHex(hex string) error {
+	nk, err := NewPublicKeyFromHex(hex)
+	if err != nil {
+		return err
+	}
+	k.Set(*nk)
+	return nil
+}
+
+// String returns k's binary uncompressed representation, as 0x-hex
+func (k *PublicKey) String() string {
+	return "0x" + hex.EncodeToString(k[:])
+}
+
+// Hash returns the solidity Keccak256 hash of k. Corresponds to hashOfKey on
+// VRFCoordinator.
+func (k *PublicKey) Hash() common.Hash {
+	rv, err := utils.Keccak256(k[:])
+	if err != nil {
+		panic(errors.Wrapf(err, "while computing hash of public key %s", k))
+	}
+	return common.BytesToHash(rv)
+}
+
+// Address returns the Ethereum address of k
+func (k *PublicKey) Address() common.Address {
+	return common.BytesToAddress(k.Hash().Bytes()[12:])
+}
