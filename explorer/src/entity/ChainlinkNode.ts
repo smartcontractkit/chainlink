c78a92360df94675b7a334e0f8d281f077536515
Ryan Hall <hall.ryan.r@gmail.com>
2019-12-20 17:15:26 -0500

    add jobCounts and uptime data to endpoint

--- explorer/src/entity/ChainlinkNode.ts
+++ explorer/src/entity/ChainlinkNode.ts
@@ -10,6 +10,7 @@ import { MinLength } from 'class-validator'
 import { JobRun } from './JobRun'
 import { sha256 } from 'js-sha256'
 import { randomBytes } from 'crypto'
+import { Session } from './Session'
 
 export interface ChainlinkNodePresenter {
   id: number
@@ -66,7 +67,7 @@ export class ChainlinkNode {
       onDelete: 'CASCADE',
     },
   )
-  jobRuns: Array<JobRun>
+  jobRuns: JobRun[]
 
   public present(): ChainlinkNodePresenter {
     return {
@@ -125,4 +126,88 @@ export function hashCredentials(
 
 export async function find(db: Connection, id: number): Promise<ChainlinkNode> {
   return db.getRepository(ChainlinkNode).findOne({ id })
+}
+
+export interface JobCountReport {
+  completed: number
+  errored: number
+  in_progress: number
+  total: number
+}
+
+export async function jobCountReport(
+  db: Connection,
+  node: ChainlinkNode | number,
+): Promise<JobCountReport> {
+  const id = node instanceof ChainlinkNode ? node.id : node
+
+  const initialReport: JobCountReport = {
+    completed: 0,
+    errored: 0,
+    in_progress: 0, // eslint-disable-line
+    total: 0,
+  }
+
+  const jobCountQueryResult = await db
+    .getRepository(JobRun)
+    .createQueryBuilder()
+    .select('COUNT(*), status')
+    .where({ chainlinkNodeId: id })
+    .groupBy('status')
+    .getRawMany()
+
+  const report = jobCountQueryResult.reduce((result, { count, status }) => {
+    result[status] = parseInt(count)
+    result.total = result.total + result[status]
+    return result
+  }, initialReport)
+
+  return report
+}
+
+// calculating uptime by diffing createdAt and finishedAt columns
+// using strategy described here: http://www.sqlines.com/postgresql/how-to/datediff
+// typeORM missing UNION function, so must do in two separate queries or write entire
+// query in raw SQL
+export async function uptime(db: Connection, node: ChainlinkNode | number) {
+  const id = node instanceof ChainlinkNode ? node.id : node
+  return (await historicUptime(db, id)) + (await currentUptime(db, id))
+}
+
+async function historicUptime(db: Connection, id: number): Promise<number> {
+  const { seconds } = await db
+    .createQueryBuilder()
+    .select(
+      `FLOOR(SUM(
+        (31536000 * DATE_PART('year', session.finishedAt - session.createdAt)) +
+        (86400 * DATE_PART('day', session.finishedAt - session.createdAt)) +
+        (3600 * DATE_PART('hour', session.finishedAt - session.createdAt)) +
+        (60 * DATE_PART('minute', session.finishedAt - session.createdAt)) +
+        (DATE_PART('second', session.finishedAt - session.createdAt))
+      )) as seconds`,
+    )
+    .from(Session, 'session')
+    .where({ chainlinkNodeId: id })
+    .andWhere('session.finishedAt is not null')
+    .getRawOne()
+  return parseInt(seconds) || 0
+}
+
+async function currentUptime(db: Connection, id: number): Promise<number> {
+  const { seconds } = await db
+    .createQueryBuilder()
+    .select(
+      `FLOOR(SUM(
+        (31536000 * DATE_PART('year', now() - session.createdAt)) +
+        (86400 * DATE_PART('day', now() - session.createdAt)) +
+        (3600 * DATE_PART('hour', now() - session.createdAt)) +
+        (60 * DATE_PART('minute', now() - session.createdAt)) +
+        (DATE_PART('second', now() - session.createdAt))
+      )) as seconds`,
+    )
+    .from(Session, 'session')
+    .where({ chainlinkNodeId: id })
+    .andWhere('session.finishedAt is null')
+    .getRawOne()
+  return parseInt(seconds) || 0
 }
