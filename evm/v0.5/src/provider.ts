f77208520cb9fe6f1f2a5820ca6b83d720855254
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-01-08 15:16:38 -0500

    Add sol-trace support to v0.5 tests

--- evm/v0.5/src/provider.ts
+++ evm/v0.5/src/provider.ts
@@ -1,9 +1,35 @@
 import { ethers } from 'ethers'
-import ganache from 'ganache-core'
+import {
+  SolCompilerArtifactAdapter,
+  Web3ProviderEngine,
+  RevertTraceSubprovider,
+} from '@0x/sol-trace'
+import {
+  FakeGasEstimateSubprovider,
+  GanacheSubprovider,
+} from '@0x/subproviders'
+import * as path from 'path'
 
 /**
  * Create a test provider which uses an in-memory, in-process chain
  */
 export function makeTestProvider(): ethers.providers.JsonRpcProvider {
-  return new ethers.providers.Web3Provider(ganache.provider() as any)
+  const defaultFromAddress = ''
+  const artifactAdapter = new SolCompilerArtifactAdapter(
+    path.resolve('dist/artifacts'),
+    path.resolve('contracts'),
+  )
+  const revertTraceSubprovider = new RevertTraceSubprovider(
+    artifactAdapter,
+    defaultFromAddress,
+    true,
+  )
+
+  const providerEngine = new Web3ProviderEngine()
+  providerEngine.addProvider(new FakeGasEstimateSubprovider(4 * 10 ** 6)) // Ganache does a poor job of estimating gas, so just crank it up for testing.
+  providerEngine.addProvider(revertTraceSubprovider)
+  providerEngine.addProvider(new GanacheSubprovider({}))
+  providerEngine.start()
+
+  return new ethers.providers.Web3Provider(providerEngine)
 }
