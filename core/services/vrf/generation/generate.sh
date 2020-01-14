665d9b5b8d9b2905079106d5993b8ce28464ba64
Alex Coventry <alx@mit.edu>
2020-01-14 17:05:22 -0500

    Appease shellcheck on golang wrapper generation scripts

--- core/services/vrf/generation/generate.sh
+++ core/services/vrf/generation/generate.sh
@@ -10,48 +10,48 @@
 set -e
 
 function cleanup() { # Release resources on script exit
-    rm -rf $TMP_DIR
-    docker rm -f $DOCKER_CONTAINER_NAME > /dev/null
+    rm -rf "$TMP_DIR"
+    docker rm -f "$DOCKER_CONTAINER_NAME" > /dev/null
 }
 trap cleanup EXIT
 
-CDIR=$(dirname $0)
+CDIR=$(dirname "$0")
 
-CLASS_NAME=$(basename $1 .json)
+CLASS_NAME=$(basename "$1" .json)
 
-TMP_DIR=$(mktemp -d /tmp/${CLASS_NAME}.XXXXXXXXX)
+TMP_DIR=$(mktemp -d "/tmp/${CLASS_NAME}.XXXXXXXXX")
 
-ABI_PATH=${TMP_DIR}/abi.json
-jq .compilerOutput.abi < $1 > $ABI_PATH
+ABI_PATH="${TMP_DIR}/abi.json"
+jq .compilerOutput.abi < "$1" > "$ABI_PATH"
 
 BIN_PATH=${TMP_DIR}/bin
-jq .compilerOutput.evm.bytecode.object < $1 | tr -d '"' > $BIN_PATH
+jq .compilerOutput.evm.bytecode.object < "$1" | tr -d '"' > "$BIN_PATH"
 
 GETH_VERSION=$(go list -json -m github.com/ethereum/go-ethereum | jq -r .Version)
-CONTAINER_NAME_PATH=${TMP_DIR}/container_name
-DOCKER_BIN_PATH=/data/$(basename $BIN_PATH)
-DOCKER_ABI_PATH=/data/$(basename $ABI_PATH)
-DOCKER_OUT_PATH=/data/$2.go
+CONTAINER_NAME_PATH="${TMP_DIR}/container_name"
+DOCKER_BIN_PATH=/data/$(basename "$BIN_PATH")
+DOCKER_ABI_PATH=/data/$(basename "$ABI_PATH")
+DOCKER_OUT_PATH="/data/$2.go"
 
 # Invoke abigen from the ethereum/go-client image to generate the golang wrapper
-docker run -v ${TMP_DIR}:/data --cidfile=$CONTAINER_NAME_PATH \
+docker run -v "${TMP_DIR}:/data" --cidfile="$CONTAINER_NAME_PATH" \
        \
-       ethereum/client-go:alltools-$GETH_VERSION \
+       "ethereum/client-go:alltools-$GETH_VERSION" \
        \
-       abigen -bin $DOCKER_BIN_PATH \
-              -abi $DOCKER_ABI_PATH \
-              -out $DOCKER_OUT_PATH \
-              -type $CLASS_NAME \
-              -pkg $2
+       abigen -bin "$DOCKER_BIN_PATH" \
+              -abi "$DOCKER_ABI_PATH" \
+              -out "$DOCKER_OUT_PATH" \
+              -type "$CLASS_NAME" \
+              -pkg "$2"
 
-DOCKER_CONTAINER_NAME=$(cat $CONTAINER_NAME_PATH)
-if [ "$(docker wait $DOCKER_CONTAINER_NAME)" != "0" ] ; then
+DOCKER_CONTAINER_NAME=$(cat "$CONTAINER_NAME_PATH")
+if [ "$(docker wait "$DOCKER_CONTAINER_NAME")" != "0" ] ; then
     echo "Failed to build $CLASS_NAME golang wrapper"
     exit 1
 fi
 
 TARGET_DIR="./generated/$2/"
 mkdir -p "$TARGET_DIR"
-docker cp $DOCKER_CONTAINER_NAME:$DOCKER_OUT_PATH "$TARGET_DIR"
+docker cp "$DOCKER_CONTAINER_NAME:$DOCKER_OUT_PATH" "$TARGET_DIR"
 
-$CDIR/record_versions.sh "$@"
+"$CDIR/record_versions.sh" "$@"
--- core/services/vrf/generation/record_versions.sh
+++ core/services/vrf/generation/record_versions.sh
@@ -14,20 +14,20 @@ set -e
 # with each contract path followed by the current hash of that contract. These
 # are checked in the golang test TestCheckContractHashesFromLastGoGenerate, to
 # ensure that the golang wrappers are current.
-CDIR=$(dirname $0)
-VERSION_DB_PATH=$CDIR/"generated-wrapper-dependency-versions-do-not-edit.txt"
-touch $VERSION_DB_PATH
+CDIR=$(dirname "$0")
+VERSION_DB_PATH="$CDIR/generated-wrapper-dependency-versions-do-not-edit.txt"
+touch "$VERSION_DB_PATH"
 
 function blow_away_version_record() {
-    (grep -v "$1": $VERSION_DB_PATH > $VERSION_DB_PATH.tmp) || true
-    mv $VERSION_DB_PATH.tmp $VERSION_DB_PATH
+    (grep -v "$1": "$VERSION_DB_PATH" > "$VERSION_DB_PATH.tmp") || true
+    mv "$VERSION_DB_PATH.tmp" "$VERSION_DB_PATH"
 }
 
 blow_away_version_record GETH_VERSION
 
 GETH_VERSION=$(go list -m github.com/ethereum/go-ethereum | awk '{print $2}')
 # go.mod geth version is of form v1.9.9. Strip leading v.
-echo GETH_VERSION: ${GETH_VERSION//v/} >> $VERSION_DB_PATH
+echo GETH_VERSION: "${GETH_VERSION//v/}" >> "$VERSION_DB_PATH"
 
-blow_away_version_record $2
-echo "$2: $1 $(md5sum $1 | cut -f 1 -d ' ')" >> $VERSION_DB_PATH
+blow_away_version_record "$2"
+echo "$2: $1 $(md5sum "$1" | cut -f 1 -d ' ')" >> "$VERSION_DB_PATH"
