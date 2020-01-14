6fdc157229e61f1a931f1becfde7b283819b8db8
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-01-14 08:59:18 -0800

    Use base ts-jest preset in styleguide package

--- styleguide/jest.config.js
+++ styleguide/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
+  preset: 'ts-jest',
   moduleDirectories: ['node_modules', '<rootDir>/src/'],
   testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/tmp/'],
 }
