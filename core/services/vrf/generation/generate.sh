15be8c87b1dc0b1dd91a99e4dc2cfa5dddccb241
Alex Coventry <alx@mit.edu>
2020-01-26 00:14:50 -0500

    Factor out abigen call in VRF go generate

--- core/services/vrf/generation/generate.sh
+++ core/services/vrf/generation/generate.sh
@@ -11,9 +11,6 @@ set -e
 
 function cleanup() { # Release resources on script exit
     rm -rf "$TMP_DIR"
-    if [ ! -z "$DOCKER_CONTAINER_NAME" ]; then
-        docker rm -f "$DOCKER_CONTAINER_NAME" > /dev/null
-    fi
 }
 trap cleanup EXIT
 
@@ -31,43 +28,12 @@ jq .compilerOutput.abi < "$1" > "$ABI_PATH"
 BIN_PATH="${TMP_DIR}/bin"
 jq .compilerOutput.evm.bytecode.object < "$1" | tr -d '"' > "$BIN_PATH"
 
-CONTAINER_NAME_PATH="${TMP_DIR}/container_name"
 OUT_PATH="${TMP_DIR}/$2.go"
 
-ABIGEN_ARGS=(
-    -bin "$BIN_PATH" \
-    -abi "$ABI_PATH" \
-    -out "$OUT_PATH" \
-    -type "$CLASS_NAME" \
-    -pkg "$2"
-)
+PKG_NAME="$2"
+"$CDIR"/abigen.sh "$BIN_PATH" "$ABI_PATH" "$OUT_PATH" "$CLASS_NAME" "$PKG_NAME"
 
-# Geth version from which native abigen was built, or empty string.
-NATIVE_ABIGEN_VERSION=v"$( (
-    abigen --version 2> /dev/null | \
-    grep -E -o '([0-9]+\.[0-9]+\.[0-9]+)'
-  ) || true
-)"
-
-# Generate golang wrapper
-if [ "$NATIVE_ABIGEN_VERSION" == "$GETH_VERSION" ]; then
-    abigen "${ABIGEN_ARGS[@]}"
-else
-    DOCKER_IMAGE="ethereum/client-go:alltools-$GETH_VERSION"
-    echo -n "Native abigen unavailable, broken, or wrong version (need version "
-    echo "$GETH_VERSION). Invoking abigen from $DOCKER_IMAGE docker image."
-    docker run -v "${TMP_DIR}:${TMP_DIR}" --cidfile="$CONTAINER_NAME_PATH" \
-           "$DOCKER_IMAGE" \
-           abigen "${ABIGEN_ARGS[@]}"
-    DOCKER_CONTAINER_NAME=$(cat "$CONTAINER_NAME_PATH")
-    if [ "$(docker wait "$DOCKER_CONTAINER_NAME")" != "0" ] ; then
-        echo "Failed to build $CLASS_NAME golang wrapper"
-        exit 1
-    fi
-    docker cp "$DOCKER_CONTAINER_NAME:${OUT_PATH}" "${OUT_PATH}"
-fi
-
-TARGET_DIR="./generated/$2/"
+TARGET_DIR="./generated/$PKG_NAME/"
 mkdir -p "$TARGET_DIR"
 cp "$OUT_PATH" "$TARGET_DIR"
 
