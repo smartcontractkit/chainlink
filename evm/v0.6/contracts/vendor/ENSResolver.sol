ee091f522c23f27a1f162e06fc49494cb129464b
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-01-27 17:27:20 -0500

    Mark ENSResolver contract as abstract

--- evm/v0.6/contracts/vendor/ENSResolver.sol
+++ evm/v0.6/contracts/vendor/ENSResolver.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.6.0;
 
-contract ENSResolver {
-  function addr(bytes32 node) public view returns (address);
+abstract contract ENSResolver {
+  function addr(bytes32 node) public virtual view returns (address);
 }
