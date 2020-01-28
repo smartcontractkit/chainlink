1f0f8f3bdb9477ccaecd53fa9941cff26ef1ae0d
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-01-28 10:52:35 -0500

    Respect recommended function modifier order

--- evm/v0.6/contracts/vendor/ENSResolver.sol
+++ evm/v0.6/contracts/vendor/ENSResolver.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.6.0;
 
 abstract contract ENSResolver {
-  function addr(bytes32 node) public virtual view returns (address);
+  function addr(bytes32 node) public view virtual returns (address);
 }
