2ba532cd7cbcab4eebebd77da6805ce6cf9ba813
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-02-05 22:51:44 -0500

    Use @chainlink/contracts across repo

--- evm-contracts/jest.config.js
+++ evm-contracts/jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
   testPathIgnorePatterns: ['/node_modules/', 'dist/'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
 }
