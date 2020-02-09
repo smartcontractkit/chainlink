18977be0f1194284192a4c6fb5bd12e11bddc70a
Alex Coventry <alx@mit.edu>
2020-02-09 14:02:20 -0500

    Better failure messages for vrf_hash_to_curve_cost_test.go

--- core/services/vrf/vrf_hash_to_curve_cost_test.go
+++ core/services/vrf/vrf_hash_to_curve_cost_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -42,10 +43,10 @@ func deployVRFContract(t *testing.T) (contract, common.Address) {
 	backend := backends.NewSimulatedBackend(genesisData, gasLimit)
 	parsed, err := abi.JSON(strings.NewReader(
 		solidity_verifier_wrapper.VRFTestHelperABI))
-	require.NoError(t, err)
+	require.NoError(t, err, "could not parse VRF ABI")
 	address, _, vRFContract, err := bind.DeployContract(auth, parsed,
 		common.FromHex(solidity_verifier_wrapper.VRFTestHelperBin), backend)
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to deploy VRF contract to simulated blockchain")
 	backend.Commit()
 	return contract{vRFContract, address, &parsed, backend}, crypto.PubkeyToAddress(
 		key.PublicKey)
@@ -55,13 +56,13 @@ func measureHashToCurveGasCost(t *testing.T, contract contract,
 	owner common.Address, input int64) (gasCost, numOrdinates uint64) {
 	rawData, err := contract.abi.Pack("hashToCurve_", pair(secp256k1.Coordinates(Generator)),
 		big.NewInt(input))
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to construct hashToCurve message for VRF contract")
 	callMsg := ethereum.CallMsg{From: owner, To: &contract.address, Data: rawData}
 	estimate, err := contract.backend.EstimateGas(context.TODO(), callMsg)
-	require.NoError(t, err)
+	require.NoError(t, err, "failed to estimate gas for on-chain hashToCurve calculation")
 	_, err = HashToCurve(Generator, big.NewInt(input),
 		func(*big.Int) { numOrdinates += 1 })
-	require.NoError(t, err)
+	require.NoError(t, err, "corresponding golang HashToCurve calculation failed")
 	return estimate, numOrdinates
 }
 
@@ -77,7 +78,10 @@ func TestMeasureHashToCurveGasCost(t *testing.T) {
 	numSamples := int64(numSamples())
 	for i := int64(0); i < numSamples; i += 1 {
 		gasCost, numOrdinates := measureHashToCurveGasCost(t, contract, owner, i)
-		require.Less(t, gasCost, HashToCurveGasCostBound(numOrdinates))
+		assert.Less(t, gasCost, HashToCurveGasCostBound(numOrdinates),
+			"on-chain hashToCurve gas cost exceeded estimate function")
 	}
-	require.Less(t, HashToCurveGasCostBound(128), uint64(2.017e6))
+	require.Less(t, HashToCurveGasCostBound(128), uint64(2.017e6),
+		"estimate for on-chain hashToCurve gas cost with 128 iterations is greater "+
+			"than stated in the VRF.sol documentation")
 }
