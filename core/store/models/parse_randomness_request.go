37f7131fd597cbc22e0979dda9811f8b1fc1d66f
Alex Coventry <alx@mit.edu>
2020-02-09 18:06:43 -0500

    Describe fields in JSON resulting from RandomnessRequest log

--- core/store/models/parse_randomness_request.go
+++ core/store/models/parse_randomness_request.go
@@ -25,11 +25,17 @@ func (parseRandomnessRequest) parseJSON(log eth.Log) (js JSON, err error) {
 		return JSON{}, errors.Wrap(err, "vrf seed out of bounds")
 	}
 	return js.MultiAdd([]KV{
+		// Address of log emitter
 		{"address", log.Address.String()},
+		// Signature of callback function on consuming contract
 		{"functionSelector", vrf.FulfillSelector()},
+		// Hash of the public key for the VRF to be used
 		{"keyHash", parsedLog.KeyHash.Hex()},
+		// Raw input seed for the VRF (includes requester, nonce, etc.)
 		{"seed", fullSeedString},
+		// The chainlink job corresponding to this VRF
 		{"jobID", parsedLog.JobID.Hex()},
+		// Address of consuming contract which initially made the request
 		{"sender", parsedLog.Sender.Hex()},
 	})
 }
