4b3e7673379e3d1dadf51b48bf2ef07782f3e5c4
Alex Coventry <alx@mit.edu>
2020-02-09 18:21:36 -0500

    Restore relevant tests from TopicFiltersForRunLog

--- core/store/models/topics_test.go
+++ core/store/models/topics_test.go
@@ -5,8 +5,28 @@ import (
 
 	"chainlink/core/store/models"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestJobSpecIDTopics(t *testing.T) {
+	t.Parallel()
+
+	jobID, err := models.NewIDFromString("4a1eb0e8df314cb894024a38991cff0f")
+	require.NoError(t, err)
+
+	topics := models.JobSpecIDTopics(jobID)
+	assert.Equal(t, 2, len(topics))
+
+	assert.Equal(
+		t,
+		[]common.Hash{
+			common.HexToHash("0x4a1eb0e8df314cb894024a38991cff0f00000000000000000000000000000000"),
+			common.HexToHash("0x3461316562306538646633313463623839343032346133383939316366663066"),
+		},
+		topics)
+}
 
 func TestRunLogTopic0original(t *testing.T) {
 	assert.Equal(t, "0x6d6db1f8fe19d95b1d0fa6a4bce7bb24fbf84597b35a33ff95521fac453c1529", models.RunLogTopic0original.Hex())
