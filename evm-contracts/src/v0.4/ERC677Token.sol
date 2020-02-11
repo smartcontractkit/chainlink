96f207d9668c86f7a7e4332281fe1af886fa3e6d
Steve Ellis <email@steveell.is>
2020-02-11 01:40:18 -0500

    add LINK token to repo

---
+++ evm-contracts/src/v0.4/ERC677Token.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.11;
+
+
+import "./interfaces/ERC677.sol";
+import "./interfaces/ERC677Receiver.sol";
+
+
+contract ERC677Token is ERC677 {
+
+  /**
+  * @dev transfer token to a contract address with additional data if the recipient is a contact.
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  * @param _data The extra data to be passed to the receiving contract.
+  */
+  function transferAndCall(address _to, uint _value, bytes _data)
+    public
+    returns (bool success)
+  {
+    super.transfer(_to, _value);
+    Transfer(msg.sender, _to, _value, _data);
+    if (isContract(_to)) {
+      contractFallback(_to, _value, _data);
+    }
+    return true;
+  }
+
+
+  // PRIVATE
+
+  function contractFallback(address _to, uint _value, bytes _data)
+    private
+  {
+    ERC677Receiver receiver = ERC677Receiver(_to);
+    receiver.onTokenTransfer(msg.sender, _value, _data);
+  }
+
+  function isContract(address _addr)
+    private
+    returns (bool hasCode)
+  {
+    uint length;
+    assembly { length := extcodesize(_addr) }
+    return length > 0;
+  }
+
+}
