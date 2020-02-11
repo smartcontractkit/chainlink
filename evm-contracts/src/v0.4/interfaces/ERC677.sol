e10bccfb8a92d0e72b2de0ea399e809815ae0665
Steve Ellis <email@steveell.is>
2020-02-11 01:40:18 -0500

    add LINK token to repo

---
+++ evm-contracts/src/v0.4/interfaces/ERC677.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.8;
+
+import "./linkERC20.sol";
+
+contract ERC677 is linkERC20 {
+  function transferAndCall(address to, uint value, bytes data) returns (bool success);
+
+  event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
