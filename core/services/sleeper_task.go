25d10fb3a8d1313d4fa5c34ee606d82c123daba0
Sam Davies <sam@samueldavies.net>
2020-02-19 11:41:09 +0000

    Make SleeperTask.Stop() synchronous
    - Fixes postgres "database still in use" test errors

--- core/services/sleeper_task.go
+++ core/services/sleeper_task.go
@@ -1,10 +1,21 @@
 package services
 
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	stopWaitTime = 5 * time.Second
+)
+
 // SleeperTask represents a task that waits in the background to process some work.
 type SleeperTask interface {
 	Start() error
 	Stop() error
-	WakeUp()
+	WakeUp() error
 }
 
 // Worker is a simple interface that represents some work to do repeatedly
@@ -13,9 +24,13 @@ type Worker interface {
 }
 
 type sleeperTask struct {
-	worker Worker
-	waker  chan struct{}
-	closer chan struct{}
+	worker  Worker
+	waker   chan struct{}
+	closer  chan struct{}
+	closed  chan struct{}
+	started bool
+	stopped bool
+	mutex   sync.Mutex
 }
 
 // NewSleeperTask takes a worker and returns a SleeperTask.
@@ -29,30 +44,72 @@ type sleeperTask struct {
 //
 func NewSleeperTask(worker Worker) SleeperTask {
 	return &sleeperTask{
-		worker: worker,
-		waker:  make(chan struct{}, 1),
-		closer: make(chan struct{}, 1),
+		worker:  worker,
+		waker:   make(chan struct{}, 1),
+		closer:  make(chan struct{}, 1),
+		closed:  make(chan struct{}, 1),
+		started: false,
+		stopped: false,
+		mutex:   sync.Mutex{},
 	}
 }
 
 // Start begins the SleeperTask
+// Calling start on a started task will return an error
 func (s *sleeperTask) Start() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if s.stopped {
+		return errors.New("cannot start a sleeper task that has been stopped")
+	} else if s.started {
+		return errors.New("sleeper task is already started")
+	}
+	s.started = true
 	go s.workerLoop()
 	return nil
 }
 
 // Stop stops the SleeperTask
+// It can only be called once
+// It will block until all tasks have completed, or the timeout is reached
+// Stopped tasks cannot be restarted
 func (s *sleeperTask) Stop() error {
+	s.mutex.Lock()
+	if s.stopped {
+		s.mutex.Unlock()
+		return errors.New("sleeper task is already stopped")
+	}
+	s.stopped = true
+	s.mutex.Unlock()
+
 	s.closer <- struct{}{}
-	return nil
+	// NOTE: Closing the channels will cause the rogue task to panic if it does eventually complete
+	defer close(s.waker)
+	defer close(s.closer)
+	defer close(s.closed)
+	select {
+	case <-s.closed:
+		return nil
+	case <-time.After(stopWaitTime):
+		return fmt.Errorf("task did not complete within %s", stopWaitTime.String())
+	}
 }
 
 // WakeUp wakes up the sleeper task, asking it to execute its Worker.
-func (s *sleeperTask) WakeUp() {
+// Idempotent, can be called multiple times but will only wake the worker once (until the worker finishes again)
+func (s *sleeperTask) WakeUp() error {
+	s.mutex.Lock()
+	if s.stopped {
+		s.mutex.Unlock()
+		return errors.New("cannot wake up stopped sleeper task")
+	}
+	s.mutex.Unlock()
+
 	select {
 	case s.waker <- struct{}{}:
 	default:
 	}
+	return nil
 }
 
 // workerLoop is the goroutine behind the sleeper task that waits for a signal
@@ -63,6 +120,7 @@ func (s *sleeperTask) workerLoop() {
 		case <-s.waker:
 			s.worker.Work()
 		case <-s.closer:
+			s.closed <- struct{}{}
 			return
 		}
 	}
