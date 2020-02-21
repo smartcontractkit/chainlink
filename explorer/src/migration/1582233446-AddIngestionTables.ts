a823af4abefa01d959be413fb6a1a44da5a98030
John Barker <dev@j16r.net>
2020-02-20 21:23:49 -0700

    Add head model and migration

---
+++ explorer/src/migration/1582233446-AddIngestionTables.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class AddIngestionTables1582233446 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+CREATE TABLE head (
+  "address" bytea PRIMARY KEY,
+  "topics" text NOT NULL,
+  "data" bytea NOT NULL,
+  "blockNumber" unsigned long NOT NULL,
+  "txHash" bytea NOT NULL,
+  "txIndex" bytea NOT NULL,
+  "blockHash" bytea NOT NULL,
+  "index" long NOT NULL,
+  "removed" bool NOT NULL DEFAULT FALSE,
+);
+    `)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('DROP TABLE head;')
+  }
+}
