259e155dd6da6e6db0fca479b721c326dee8c47c
Alex Coventry <alx@mit.edu>
2020-02-26 12:39:43 -0500

    Improved error message

--- core/internal/features_test.go
+++ core/internal/features_test.go
@@ -1064,7 +1064,7 @@ func TestIntegration_RandomnessRequest(t *testing.T) {
 	require.NoError(t, err, "problem parsing solidity proof")
 	proofValid, err := goProof.VerifyVRFProof()
 	require.NoError(t, err, "problem verifying solidity proof")
-	require.True(t, proofValid)
+	require.True(t, proofValid, "vrf proof was invalid: %s", goProof.String())
 
 	// Check that a log from a different address is rejected. (The node will only
 	// ever see this situation if the ethereum.FilterQuery for this job breaks,
