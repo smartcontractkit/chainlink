e6ae6bb15700e502cee3192e97b93862fccfd07f
Alex Coventry <alx@mit.edu>
2020-03-03 17:13:01 -0500

    Fix data races in memory-log utility
    I don't see how, but somehow these were causing logging to hang.

--- core/logger/memory_sink.go
+++ core/logger/memory_sink.go
@@ -11,10 +11,17 @@ import (
 
 // MemorySink implements zap.Sink by writing all messages to a buffer.
 type MemorySink struct {
-	*bytes.Buffer
+	m sync.Mutex
+	b bytes.Buffer
 }
 
 var _ zap.Sink = &MemorySink{}
+
+func (s *MemorySink) Write(p []byte) (n int, err error) {
+	s.m.Lock()
+	defer s.m.Unlock()
+	return s.b.Write(p)
+}
 
 // Close is a dummy method to satisfy the zap.Sink interface
 func (s *MemorySink) Close() error { return nil }
@@ -22,11 +29,18 @@ func (s *MemorySink) Close() error { return nil }
 // Sync is a dummy method to satisfy the zap.Sink interface
 func (s *MemorySink) Sync() error { return nil }
 
+// String returns the full log contents, as a string
+func (s *MemorySink) String() string {
+	s.m.Lock()
+	defer s.m.Unlock()
+	return s.b.String()
+}
+
 var testMemoryLog *MemorySink
 var createSinkOnce sync.Once
 
 func registerMemorySink() {
-	testMemoryLog = &MemorySink{new(bytes.Buffer)}
+	testMemoryLog = &MemorySink{m: sync.Mutex{}, b: bytes.Buffer{}}
 	if err := zap.RegisterSink("memory", prettyConsoleSink(testMemoryLog)); err != nil {
 		panic(err)
 	}
