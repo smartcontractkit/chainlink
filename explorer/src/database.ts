43162ee4a779b4a981b20e8c56d29f094655b5af
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-03-10 15:01:04 -0400

    Address PR change requests

--- explorer/src/database.ts
+++ explorer/src/database.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata'
-import { createConnection, Connection } from 'typeorm'
+import { Connection, createConnection } from 'typeorm'
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
-import { TypeOrmLogger } from './logging'
 import options from '../ormconfig.json'
+import { TypeOrmLogger } from './logging'
 
 const overridableKeys = ['host', 'port', 'username', 'password', 'database']
 
@@ -27,7 +27,7 @@ const loadOptions = (env?: string) => {
 // Loads the following ENV vars, giving them precedence.
 // i.e. TYPEORM_PORT will replace "port" in ormconfig.json.
 const mergeOptions = (): PostgresConnectionOptions => {
-  const envOptions: { [key: string]: any } = { }
+  const envOptions: Record<string, string> = {}
   for (const v of overridableKeys) {
     const envVar = process.env[`TYPEORM_${v.toUpperCase()}`]
     if (envVar) {
