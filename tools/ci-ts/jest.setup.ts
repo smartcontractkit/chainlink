11f8a0c54f8db90ff4c189430636f2c2f91827c1
Ryan Hall <hall.ryan.r@gmail.com>
2020-03-11 15:33:03 -0600

    add waitFor EXTERNAL_ADAPTER_URL to test setup

--- tools/ci-ts/jest.setup.ts
+++ tools/ci-ts/jest.setup.ts
@@ -1,7 +1,11 @@
 import { getArgs, waitForService } from './test-helpers/common'
 
-const { CHAINLINK_URL } = getArgs(['CHAINLINK_URL'])
+const { CHAINLINK_URL, EXTERNAL_ADAPTER_URL } = getArgs([
+  'CHAINLINK_URL',
+  'EXTERNAL_ADAPTER_URL',
+])
 
 beforeAll(async () => {
   await waitForService(CHAINLINK_URL)
+  await waitForService(EXTERNAL_ADAPTER_URL)
 })
