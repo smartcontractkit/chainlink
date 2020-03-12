64a4f60d16e19d98e75907737b20cfe99a6e599c
spooktheducks <spooktheducks@protonmail.com>
2020-03-12 11:31:03 -0500

    Add comment explaining how Geth translates Solidity field names to Go struct field names

--- core/eth/geth_copied.go
+++ core/eth/geth_copied.go
@@ -48,6 +48,13 @@ func parseTopics(out interface{}, fields abi.Arguments, topics []common.Hash) er
 			return errors.New("non-indexed field in topic reconstruction")
 		}
 
+		// If Go structs aren't kept correctly in sync with log fields defined in Solidity, this error will be returned.
+		// The name convention is to remove underscores, capitalize all characters following them, and capitalize the
+		// first letter of the field:
+		//
+		// round_id => RoundId
+		// roundId => RoundId
+		// _roundId => RoundId
 		_, exists := reflect.TypeOf(out).Elem().FieldByName(capitalise(arg.Name))
 		if !exists {
 			return errors.Errorf(`can't find matching struct field for log "%T", field "%v" (expected "%v")`, out, arg.Name, capitalise(arg.Name))
