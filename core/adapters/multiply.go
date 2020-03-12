55305124fd7d20e1fdf875f54fd0bfa6fe97b663
felder-cl <felder-cl@smartcontract.com>
2020-03-12 14:46:25 +0100

    adapters: Make TaskType inspectable
    Support logic on the TaskType of the adapter by introducing the method
    TaskType() to the Adapter interface.  This will be usefull for logging
    and metrics.

--- core/adapters/multiply.go
+++ core/adapters/multiply.go
@@ -1,10 +1,17 @@
 package adapters
 
 import (
+	"chainlink/core/store/models"
+
 	"github.com/shopspring/decimal"
 )
 
 // Multiply holds the a number to multiply the given value by.
 type Multiply struct {
 	Times *decimal.Decimal `json:"times,omitempty"`
+}
+
+// TaskType returns the type of Adapter.
+func (m *Multiply) TaskType() models.TaskType {
+	return TaskTypeMultiply
 }
