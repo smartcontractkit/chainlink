44024993ae2b69b13fcc3d05dc2d2d93e7f6502c
Bartosz Wojciechowski <zly.ojciec@gmail.com>
2020-03-16 15:07:00 +0100

    Fixed import order

--- feeds/src/state/ducks/index.ts
+++ feeds/src/state/ducks/index.ts
@@ -1,10 +1,10 @@
-export { default as aggregation } from './aggregation'
-export { default as networkGraph } from './networkGraph'
-export { default as listing } from './listing'
-
 import { State as AggregationState } from './aggregation/reducers'
 import { State as NetworkGraphState } from './aggregation/reducers'
 import { State as LisitingState } from './aggregation/reducers'
+
+export { default as aggregation } from './aggregation'
+export { default as networkGraph } from './networkGraph'
+export { default as listing } from './listing'
 
 export type Ducks = {
   aggregation: AggregationState
