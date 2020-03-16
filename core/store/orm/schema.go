701e403c1747974d5843720b6e9388c4c6176f19
Sam Davies <sam@samueldavies.net>
2020-03-16 18:33:00 -0300

    Ensure we always bump gas above geth's minimum required limit
    - Avoids transactions getting permanently stuck at a gas "ceiling"

--- core/store/orm/schema.go
+++ core/store/orm/schema.go
@@ -28,7 +28,9 @@ type ConfigSchema struct {
 	MinimumServiceDuration    time.Duration  `env:"MINIMUM_SERVICE_DURATION" default:"0s" `
 	EthGasBumpThreshold       uint64         `env:"ETH_GAS_BUMP_THRESHOLD" default:"12" `
 	EthGasBumpWei             big.Int        `env:"ETH_GAS_BUMP_WEI" default:"5000000000"`
+	EthGasBumpPercent         uint16         `env:"ETH_GAS_BUMP_PERCENT" default:"10"`
 	EthGasPriceDefault        big.Int        `env:"ETH_GAS_PRICE_DEFAULT" default:"20000000000"`
+	EthMaxGasPriceWei         uint64         `env:"ETH_MAX_GAS_PRICE_WEI" default:"500000000000"`
 	EthereumURL               string         `env:"ETH_URL" default:"ws://localhost:8546"`
 	JSONConsole               bool           `env:"JSON_CONSOLE" default:"false"`
 	LinkContractAddress       string         `env:"LINK_CONTRACT_ADDRESS" default:"0x514910771AF9Ca656af840dff83E8264EcF986CA"`
