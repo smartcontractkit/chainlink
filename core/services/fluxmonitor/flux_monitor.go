517cba3d117051f7d346b004b3ca785748557327
Alex Coventry <alx@mit.edu>
2020-04-13 23:47:17 -0400

    Add a test helper which forces a CL node to attempt early new round

--- core/services/fluxmonitor/flux_monitor.go
+++ core/services/fluxmonitor/flux_monitor.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"reflect"
 	"sync"
+	"testing"
 	"time"
 
 	"github.com/smartcontractkit/chainlink/core/logger"
@@ -21,6 +22,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
 	"github.com/tevino/abool"
 )
 
@@ -222,6 +224,19 @@ func (fm *ConcreteFluxMonitor) RemoveJob(id *models.ID) {
 		return
 	}
 	fm.chRemove <- *id
+}
+
+// XXXTestingOnlyCreateJob creates a job with a specific answer and round, for
+// testing nodes with malicious behavior
+func (fm *ConcreteFluxMonitor) XXXTestingOnlyCreateJob(t *testing.T,
+	jobSpecId *models.ID, polledAnswer decimal.Decimal,
+	nextRound *big.Int) error {
+	jobSpec, err := fm.store.ORM.FindJob(jobSpecId)
+	require.NoError(t, err, "could not find job spec with that ID")
+	checker, err := fm.checkerFactory.New(jobSpec.Initiators[0], fm.runManager,
+		fm.store.ORM, 100*time.Second)
+	require.NoError(t, err, "could not create deviation checker")
+	return checker.(*PollingDeviationChecker).createJobRun(polledAnswer, nextRound)
 }
 
 // DeviationCheckerFactory holds the New method needed to create a new instance
