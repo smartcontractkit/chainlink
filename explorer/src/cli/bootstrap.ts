3891feec39c6122761029b9fe72e9eb7319c5f92
John Barker <dev@j16r.net>
2020-04-15 18:55:05 -0600

    Use full cb type for bootstrap in place of any

--- explorer/src/cli/bootstrap.ts
+++ explorer/src/cli/bootstrap.ts
@@ -1,9 +1,10 @@
+import { Connection } from 'typeorm'
 import { openDbConnection } from '../database'
 
-export async function bootstrap(cb: any) {
+export async function bootstrap(cb: (db: Connection) => void) {
   const db = await openDbConnection()
   try {
-    await cb(db)
+    return cb(db)
   } catch (err) {
     console.error(err)
   } finally {
