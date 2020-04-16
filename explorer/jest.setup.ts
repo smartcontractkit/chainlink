231f8cef349ab98c5151a39b42661eb2d31f51cc
John Barker <dev@j16r.net>
2020-04-16 15:41:41 -0600

    Instead of global, close pool with getConnection.close()

--- explorer/jest.setup.ts
+++ explorer/jest.setup.ts
@@ -1,16 +1,16 @@
-import { Connection } from 'typeorm'
+import { getConnection } from 'typeorm'
 import { openDbConnection } from './src/database'
 import { clearDb } from './src/__tests__/testdatabase'
 
 process.env.NODE_ENV = 'test'
 
-let globalDBConnection: Connection
 beforeAll(async () => {
-  globalDBConnection = await openDbConnection()
+  await openDbConnection()
 })
 afterEach(() => clearDb())
 afterAll(async () => {
-  if (globalDBConnection) {
-    await globalDBConnection.close()
+  const db = getConnection()
+  if (db) {
+    await db.close()
   }
 })
