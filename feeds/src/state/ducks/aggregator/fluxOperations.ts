b74aaa80cbeecb98bdabccdb034b2eafd47b6bf5
hellobart <zly.ojciec@gmail.com>
2020-04-17 16:41:58 +0200

    - converted FluxOperations to Class - added flux operation test

--- feeds/src/state/ducks/aggregator/fluxOperations.ts
+++ feeds/src/state/ducks/aggregator/fluxOperations.ts
@@ -5,232 +5,249 @@ import FluxAggregatorAbi from '../../../contracts/FluxAggregatorAbi.json'
 import FluxAggregatorContract from '../../../contracts/FluxAggregatorContract'
 import * as actions from './actions'
 
-let contractInstance: any
+export default class FluxOperations {
+  static contractInstance: any
 
-function fetchOracleList() {
-  return async (dispatch: any, getState: any) => {
-    if (getState().aggregator.oracleList) {
-      return
-    }
-    try {
-      const payload = await contractInstance.oracles()
-      dispatch(actions.setOracleList(payload))
-    } catch {
-      console.error('Could not fetch oracle list ')
-    }
-  }
-}
-
-function fetchLatestCompletedAnswerId() {
-  return async (dispatch: any) => {
-    try {
-      const payload = await contractInstance.latestRound()
-      dispatch(actions.setLatestCompletedAnswerId(payload))
-      return payload
-    } catch {
-      console.error('Could not fetch latest completed answer id')
-    }
-  }
-}
-
-function fetchLatestAnswer() {
-  return async (dispatch: any) => {
-    try {
-      const payload = await contractInstance.latestAnswer()
-      dispatch(actions.setLatestAnswer(payload))
-    } catch {
-      console.error('Could not fetch latest answer')
-    }
-  }
-}
-
-function fetchLatestAnswerTimestamp() {
-  return async (dispatch: any) => {
-    try {
-      const payload = await contractInstance.latestTimestamp()
-      dispatch(actions.setLatestAnswerTimestamp(payload))
-      return payload
-    } catch {
-      console.error('Could not fetch latest answer timestamp')
-    }
-  }
-}
-
-const fetchOracleAnswersById = (request: any) => {
-  return async (dispatch: any, getState: any) => {
-    try {
-      const currentLogs = getState().aggregator.oracleAnswers
-      const logs = await contractInstance.submissionReceivedLogs(request)
-      const withTimestamp = await contractInstance.addBlockTimestampToLogs(logs)
-      const withGasAndTimeStamp = await contractInstance.addGasPriceToLogs(
-        withTimestamp,
-      )
-
-      const uniquePayload = _.uniqBy(
-        [...withGasAndTimeStamp, ...currentLogs],
-        l => l.sender,
-      )
-
-      dispatch(actions.setOracleAnswers(uniquePayload))
-    } catch {
-      console.error('Could not fetch oracle answers')
-    }
-  }
-}
-
-const fetchLatestRequestTimestamp = (request: any) => {
-  return async (dispatch: any) => {
-    try {
-      const logs = await contractInstance.newRoundLogs(request)
-      const startedAt = logs?.[logs.length - 1].startedAt
-      dispatch(actions.setLatestRequestTimestamp(startedAt))
-    } catch {
-      console.error('Could not fetch request time')
-    }
-  }
-}
-
-function fetchMinimumAnswers() {
-  return async (dispatch: any) => {
-    try {
-      const payload = await contractInstance.minimumAnswers()
-      dispatch(actions.setMinumumAnswers(payload))
-    } catch {
-      console.error('Could not fetch minimum answers')
-    }
-  }
-}
-
-function fetchAnswerHistory(fromBlock: number) {
-  return async (dispatch: any) => {
-    try {
-      const payload = await contractInstance.answerUpdatedLogs({ fromBlock })
-      const uniquePayload = _.uniqBy(payload, (e: any) => {
-        return e.answerId
-      })
-
-      dispatch(actions.setAnswerHistory(uniquePayload))
-    } catch {
-      console.error('Could not fetch answer history')
-    }
-  }
-}
-
-function initListeners() {
-  return async (dispatch: any, getState: any) => {
-    contractInstance.listenSubmissionReceivedEvent(async (responseLog: any) => {
-      const { minimumAnswers } = getState().aggregator
-      const oracleAnswers = getState().aggregator.oracleAnswers || []
-      const updatedAnswers = oracleAnswers.map((response: any) => {
-        return response.sender === responseLog.sender ? responseLog : response
-      })
-
-      dispatch(actions.setOracleAnswers(updatedAnswers))
-
-      const latestIdAnswers = _.filter(updatedAnswers, {
-        answerId: responseLog.answerId,
-      })
-
-      if (latestIdAnswers.length >= minimumAnswers) {
-        fetchLatestAnswer()(dispatch)
-        fetchLatestAnswerTimestamp()(dispatch)
+  static fetchOracleList() {
+    return async (dispatch: any, getState: any) => {
+      if (getState().aggregator.oracleList) {
+        return
       }
-    })
-
-    contractInstance.listenNewRoundEvent(async (responseLog: any) => {
-      await fetchLatestCompletedAnswerId()(dispatch)
-      dispatch(actions.setPendingAnswerId(responseLog.answerId))
-      dispatch(actions.setLatestRequestTimestamp(responseLog.startedAt))
-    })
+      try {
+        const payload = await FluxOperations.contractInstance.oracles()
+        dispatch(actions.setOracleList(payload))
+      } catch {
+        console.error('Could not fetch oracle list ')
+      }
+    }
   }
-}
 
-/**
- * Initialise aggregator contract and fill the store with all necessery data for a visualisation page.
- * @param config FeedsConfig
- */
-
-const initContract = (config: FeedConfig) => {
-  return async (dispatch: any, getState: any) => {
-    try {
-      contractInstance?.kill()
-    } catch {
-      console.error('Could not close the contract instance')
+  static fetchLatestCompletedAnswerId() {
+    return async (dispatch: any) => {
+      try {
+        const payload = await FluxOperations.contractInstance.latestRound()
+        dispatch(actions.setLatestCompletedAnswerId(payload))
+        return payload
+      } catch {
+        console.error('Could not fetch latest completed answer id')
+      }
     }
+  }
 
-    try {
-      ethers.utils.getAddress(config.contractAddress)
-    } catch (error) {
-      throw new Error('Wrong contract address')
+  static fetchLatestAnswer() {
+    return async (dispatch: any) => {
+      try {
+        const payload = await FluxOperations.contractInstance.latestAnswer()
+        dispatch(actions.setLatestAnswer(payload))
+      } catch {
+        console.error('Could not fetch latest answer')
+      }
     }
+  }
 
-    // dispatch(actions.setConfig(config))
-    dispatch(actions.setContractAddress(config.contractAddress))
+  static fetchLatestAnswerTimestamp() {
+    return async (dispatch: any) => {
+      try {
+        const payload = await FluxOperations.contractInstance.latestTimestamp()
+        dispatch(actions.setLatestAnswerTimestamp(payload))
+        return payload
+      } catch {
+        console.error('Could not fetch latest answer timestamp')
+      }
+    }
+  }
 
-    contractInstance = new FluxAggregatorContract(config, FluxAggregatorAbi)
+  static fetchOracleAnswersById(request: any) {
+    return async (dispatch: any, getState: any) => {
+      try {
+        const currentLogs = getState().aggregator.oracleAnswers
+        const logs = await FluxOperations.contractInstance.submissionReceivedLogs(
+          request,
+        )
+        const withTimestamp = await FluxOperations.contractInstance.addBlockTimestampToLogs(
+          logs,
+        )
+        const withGasAndTimeStamp = await FluxOperations.contractInstance.addGasPriceToLogs(
+          withTimestamp,
+        )
 
-    // Oracle addresses
-    await fetchOracleList()(dispatch, getState)
+        const uniquePayload = _.uniqBy(
+          [...withGasAndTimeStamp, ...currentLogs],
+          l => l.sender,
+        )
 
-    // Minimum oracle responses
-    fetchMinimumAnswers()(dispatch)
+        dispatch(actions.setOracleAnswers(uniquePayload))
+      } catch {
+        console.error('Could not fetch oracle answers')
+      }
+    }
+  }
 
-    // Set answer Id
-    const reportingAnswerId = await contractInstance.reportingRound()
-    dispatch(actions.setPendingAnswerId(reportingAnswerId))
+  static fetchLatestRequestTimestamp = (request: any) => {
+    return async (dispatch: any) => {
+      try {
+        const logs = await FluxOperations.contractInstance.newRoundLogs(request)
+        const startedAt = logs?.[logs.length - 1].startedAt
+        dispatch(actions.setLatestRequestTimestamp(startedAt))
+      } catch {
+        console.error('Could not fetch request time')
+      }
+    }
+  }
 
-    // Current answers
-    await fetchLatestAnswerTimestamp()(dispatch)
+  static fetchMinimumAnswers() {
+    return async (dispatch: any) => {
+      try {
+        const payload = await FluxOperations.contractInstance.minimumAnswers()
+        dispatch(actions.setMinumumAnswers(payload))
+      } catch {
+        console.error('Could not fetch minimum answers')
+      }
+    }
+  }
 
-    // Fetch previous answers
-    const currentBlockNumber = await contractInstance.provider.getBlockNumber()
-    const latestAnswerId = await contractInstance.latestRound()
-    const fromBlock = currentBlockNumber <= 6700 ? 0 : currentBlockNumber - 6700 // ~6700 blocks per day
+  static fetchAnswerHistory(fromBlock: number) {
+    return async (dispatch: any) => {
+      try {
+        const payload = await FluxOperations.contractInstance.answerUpdatedLogs(
+          {
+            fromBlock,
+          },
+        )
+        const uniquePayload = _.uniqBy(payload, (e: any) => {
+          return e.answerId
+        })
 
-    await fetchOracleAnswersById({
-      round: latestAnswerId,
-      fromBlock,
-    })(dispatch, getState)
+        dispatch(actions.setAnswerHistory(uniquePayload))
+      } catch {
+        console.error('Could not fetch answer history')
+      }
+    }
+  }
 
-    // Fetch latest answers
-    await fetchOracleAnswersById({
-      round: reportingAnswerId,
-      fromBlock,
-    })(dispatch, getState)
+  static initListeners() {
+    return async (dispatch: any, getState: any) => {
+      FluxOperations.contractInstance.listenSubmissionReceivedEvent(
+        async (responseLog: any) => {
+          const { minimumAnswers } = getState().aggregator
+          const oracleAnswers = getState().aggregator.oracleAnswers || []
+          const updatedAnswers = oracleAnswers.map((response: any) => {
+            return response.sender === responseLog.sender
+              ? responseLog
+              : response
+          })
 
-    /**
-     * Oracle Latest Request Time
-     * Used to calculate hearbeat countdown timer
-     */
-    if (config.heartbeat) {
-      fetchLatestRequestTimestamp({
+          dispatch(actions.setOracleAnswers(updatedAnswers))
+
+          const latestIdAnswers = _.filter(updatedAnswers, {
+            answerId: responseLog.answerId,
+          })
+
+          if (latestIdAnswers.length >= minimumAnswers) {
+            FluxOperations.fetchLatestAnswer()(dispatch)
+            FluxOperations.fetchLatestAnswerTimestamp()(dispatch)
+          }
+        },
+      )
+
+      FluxOperations.contractInstance.listenNewRoundEvent(
+        async (responseLog: any) => {
+          await FluxOperations.fetchLatestCompletedAnswerId()(dispatch)
+          dispatch(actions.setPendingAnswerId(responseLog.answerId))
+          dispatch(actions.setLatestRequestTimestamp(responseLog.startedAt))
+        },
+      )
+    }
+  }
+
+  /**
+   * Initialise aggregator contract and fill the store with all necessery data for a visualisation page.
+   * @param config FeedsConfig
+   */
+
+  static initContract(config: FeedConfig) {
+    return async (dispatch: any, getState: any) => {
+      try {
+        FluxOperations.contractInstance?.kill()
+      } catch {
+        console.error('Could not close the contract instance')
+      }
+
+      try {
+        ethers.utils.getAddress(config.contractAddress)
+      } catch (error) {
+        throw new Error('Wrong contract address')
+      }
+
+      dispatch(actions.setContractAddress(config.contractAddress))
+
+      FluxOperations.contractInstance = new FluxAggregatorContract(
+        config,
+        FluxAggregatorAbi,
+      )
+
+      // Oracle addresses
+      await FluxOperations.fetchOracleList()(dispatch, getState)
+
+      // Minimum oracle responses
+      FluxOperations.fetchMinimumAnswers()(dispatch)
+
+      // Set answer Id
+      const reportingAnswerId = await FluxOperations.contractInstance.reportingRound()
+      dispatch(actions.setPendingAnswerId(reportingAnswerId))
+
+      // Current answers
+      await FluxOperations.fetchLatestAnswerTimestamp()(dispatch)
+
+      // Fetch previous answers
+      const currentBlockNumber = await FluxOperations.contractInstance.provider.getBlockNumber()
+      const latestAnswerId = await FluxOperations.contractInstance.latestRound()
+      const fromBlock =
+        currentBlockNumber <= 6700 ? 0 : currentBlockNumber - 6700 // ~6700 blocks per day
+
+      await FluxOperations.fetchOracleAnswersById({
+        round: latestAnswerId,
+        fromBlock,
+      })(dispatch, getState)
+
+      // Fetch latest answers
+      await FluxOperations.fetchOracleAnswersById({
         round: reportingAnswerId,
         fromBlock,
-      })(dispatch)
+      })(dispatch, getState)
+
+      /**
+       * Oracle Latest Request Time
+       * Used to calculate hearbeat countdown timer
+       */
+      if (config.heartbeat) {
+        FluxOperations.fetchLatestRequestTimestamp({
+          round: reportingAnswerId,
+          fromBlock,
+        })(dispatch)
+      }
+
+      // Current answer
+      FluxOperations.fetchLatestAnswer()(dispatch)
+
+      // initalise listeners
+      FluxOperations.initListeners()(dispatch, getState)
+
+      if (config.history) {
+        FluxOperations.fetchAnswerHistory(fromBlock)(dispatch)
+      }
     }
+  }
 
-    // Current answer
-    fetchLatestAnswer()(dispatch)
-
-    // initalise listeners
-    initListeners()(dispatch, getState)
-
-    if (config.history) {
-      fetchAnswerHistory(fromBlock)(dispatch)
+  static fetchJobId(address: any) {
+    return async (_dispatch: any, getState: any) => {
+      const { oracleList } = getState().aggregator
+      try {
+        const index = oracleList.indexOf(address)
+        return FluxOperations.contractInstance.jobId(index)
+      } catch {
+        console.error('Could not fetch a job id')
+      }
     }
   }
 }
-
-const fetchJobId = (address: any) => {
-  return async (_dispatch: any, getState: any) => {
-    const { oracleList } = getState().aggregator
-    try {
-      const index = oracleList.indexOf(address)
-      return contractInstance.jobId(index)
-    } catch {
-      console.error('Could not fetch a job id')
-    }
-  }
-}
-
-export { initContract, fetchJobId }
