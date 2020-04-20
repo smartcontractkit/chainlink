829e1b62445a3405b292424c7e679bae18d42765
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-04-20 14:15:28 -0700

    Allow feeds.json & nodes.json to be overridden with query parameters

--- feeds/src/config.test.ts
+++ feeds/src/config.test.ts
@@ -1,4 +1,4 @@
-// import { partialAsFull } from '@chainlink/ts-helpers'
+import { partialAsFull } from '@chainlink/ts-helpers'
 import { Config } from './config'
 
 describe('config', () => {
@@ -22,45 +22,83 @@ describe('config', () => {
     expect(Config.gaId(processEnv)).toEqual('ga-id')
   })
 
-  it('returns feeds json from the process env', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-      REACT_APP_FEEDS_JSON: 'https://test.dev/feeds.json',
-    }
+  describe('feeds json', () => {
+    it('returns feeds json from the process env', () => {
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+        REACT_APP_FEEDS_JSON: 'https://test.dev/feeds.json',
+      }
 
-    expect(Config.feedsJson(processEnv)).toEqual('https://test.dev/feeds.json')
+      expect(Config.feedsJson(processEnv)).toEqual(
+        'https://test.dev/feeds.json',
+      )
+    })
+
+    it('returns a default feeds json when not provided', () => {
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+      }
+
+      expect(Config.feedsJson(processEnv)).toEqual(
+        'https://weiwatchers.com/feeds.json',
+      )
+    })
+
+    it('can override feeds json with a query parameter', () => {
+      const location = partialAsFull<Location>({
+        search: '?feeds-json=https%3A%2F%2Foverride.dev%2Ffeeds.json',
+      })
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+        REACT_APP_FEEDS_JSON: 'https://test.dev/feeds.json',
+      }
+
+      expect(Config.feedsJson(processEnv, location)).toEqual(
+        'https://override.dev/feeds.json',
+      )
+    })
   })
 
-  it('returns a default feeds json when not provided', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-    }
+  describe('nodes json', () => {
+    it('returns nodes json from the process env', () => {
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+        REACT_APP_NODES_JSON: 'https://test.dev/nodes.json',
+      }
 
-    expect(Config.feedsJson(processEnv)).toEqual(
-      'https://weiwatchers.com/feeds.json',
-    )
-  })
+      expect(Config.nodesJson(processEnv)).toEqual(
+        'https://test.dev/nodes.json',
+      )
+    })
 
-  it('returns nodes json from the process env', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-      REACT_APP_NODES_JSON: 'https://test.dev/nodes.json',
-    }
+    it('returns a default nodes json when not provided', () => {
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+      }
 
-    expect(Config.nodesJson(processEnv)).toEqual('https://test.dev/nodes.json')
-  })
+      expect(Config.nodesJson(processEnv)).toEqual(
+        'https://weiwatchers.com/nodes.json',
+      )
+    })
 
-  it('returns a default nodes json when not provided', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-    }
+    it('can override nodes json with a query parameter', () => {
+      const location = partialAsFull<Location>({
+        search: '?nodes-json=https%3A%2F%2Foverride.dev%2Fnodes.json',
+      })
+      const processEnv = {
+        NODE_ENV: 'test' as const,
+        PUBLIC_URL: 'https://test.dev',
+        REACT_APP_FEEDS_JSON: 'https://test.dev/nodes.json',
+      }
 
-    expect(Config.nodesJson(processEnv)).toEqual(
-      'https://weiwatchers.com/nodes.json',
-    )
+      expect(Config.nodesJson(processEnv, location)).toEqual(
+        'https://override.dev/nodes.json',
+      )
+    })
   })
 })
