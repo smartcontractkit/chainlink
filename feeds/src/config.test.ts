bdb69d49cdb2af730a2b729db70a8a5e67de3431
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-04-21 17:38:13 -0700

    Set shared process.env in beforeEach

--- feeds/src/config.test.ts
+++ feeds/src/config.test.ts
@@ -2,61 +2,39 @@ import { partialAsFull } from '@chainlink/ts-helpers'
 import { Config } from './config'
 
 describe('config', () => {
-  it('returns infura key from the process env', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-      REACT_APP_INFURA_KEY: 'infuraKey',
-    }
+  const originalEnv = { ...process.env }
+  beforeEach(() => {
+    process.env = originalEnv
+  })
 
-    expect(Config.infuraKey(processEnv)).toEqual('infuraKey')
+  it('returns infura key from the process env', () => {
+    process.env.REACT_APP_INFURA_KEY = 'infuraKey'
+    expect(Config.infuraKey()).toEqual('infuraKey')
   })
 
   it('returns the google analytics id from the process env', () => {
-    const processEnv = {
-      NODE_ENV: 'test' as const,
-      PUBLIC_URL: 'https://test.dev',
-      REACT_APP_GA_ID: 'ga-id',
-    }
-
-    expect(Config.gaId(processEnv)).toEqual('ga-id')
+    process.env.REACT_APP_GA_ID = 'ga-id'
+    expect(Config.gaId()).toEqual('ga-id')
   })
 
   describe('feeds json', () => {
     it('returns feeds json from the process env', () => {
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-        REACT_APP_FEEDS_JSON: 'https://test.dev/feeds.json',
-      }
-
-      expect(Config.feedsJson(processEnv)).toEqual(
-        'https://test.dev/feeds.json',
-      )
+      process.env.REACT_APP_FEEDS_JSON = 'https://test.dev/feeds.json'
+      expect(Config.feedsJson()).toEqual('https://test.dev/feeds.json')
     })
 
-    it('returns a default feeds json when not provided', () => {
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-      }
-
-      expect(Config.feedsJson(processEnv)).toEqual(
-        'https://weiwatchers.com/feeds.json',
-      )
+    it('returns a default feeds json when undefined', () => {
+      process.env.REACT_APP_FEEDS_JSON = undefined
+      expect(Config.feedsJson()).toEqual('https://weiwatchers.com/feeds.json')
     })
 
     it('can override feeds json with a query parameter', () => {
       const location = partialAsFull<Location>({
         search: '?feeds-json=https%3A%2F%2Foverride.dev%2Ffeeds.json',
       })
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-        REACT_APP_FEEDS_JSON: 'https://test.dev/feeds.json',
-      }
+      process.env.REACT_APP_FEEDS_JSON = 'https://test.dev/feeds.json'
 
-      expect(Config.feedsJson(processEnv, location)).toEqual(
+      expect(Config.feedsJson(process.env, location)).toEqual(
         'https://override.dev/feeds.json',
       )
     })
@@ -64,39 +42,22 @@ describe('config', () => {
 
   describe('nodes json', () => {
     it('returns nodes json from the process env', () => {
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-        REACT_APP_NODES_JSON: 'https://test.dev/nodes.json',
-      }
-
-      expect(Config.nodesJson(processEnv)).toEqual(
-        'https://test.dev/nodes.json',
-      )
+      process.env.REACT_APP_NODES_JSON = 'https://test.dev/nodes.json'
+      expect(Config.nodesJson()).toEqual('https://test.dev/nodes.json')
     })
 
-    it('returns a default nodes json when not provided', () => {
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-      }
-
-      expect(Config.nodesJson(processEnv)).toEqual(
-        'https://weiwatchers.com/nodes.json',
-      )
+    it('returns a default nodes json when undefined', () => {
+      process.env.REACT_APP_NODES_JSON = undefined
+      expect(Config.nodesJson()).toEqual('https://weiwatchers.com/nodes.json')
     })
 
     it('can override nodes json with a query parameter', () => {
       const location = partialAsFull<Location>({
         search: '?nodes-json=https%3A%2F%2Foverride.dev%2Fnodes.json',
       })
-      const processEnv = {
-        NODE_ENV: 'test' as const,
-        PUBLIC_URL: 'https://test.dev',
-        REACT_APP_FEEDS_JSON: 'https://test.dev/nodes.json',
-      }
+      process.env.REACT_APP_FEEDS_JSON = 'https://test.dev/nodes.json'
 
-      expect(Config.nodesJson(processEnv, location)).toEqual(
+      expect(Config.nodesJson(process.env, location)).toEqual(
         'https://override.dev/nodes.json',
       )
     })
