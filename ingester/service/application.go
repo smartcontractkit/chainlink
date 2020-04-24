072925ac8e37cf3477369bcd74e84faa25eead19
shannon <liyunlong518@gmail.com>
2020-04-24 13:09:59 +0800

    fix sec check error

--- ingester/service/application.go
+++ ingester/service/application.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 
-	"ingester/client"
-	"ingester/logger"
+	"github.com/smartcontractkit/chainlink/ingester/client"
+	"github.com/smartcontractkit/chainlink/ingester/logger"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
