2c1f7bce46f69faa054abd1948d9c2ca6d04554f
Steve Ellis <email@steveell.is>
2020-04-25 10:20:11 -0400

    rename requestRateUpdate to requestNewRound

--- evm-contracts/src/v0.6/dev/FluxAggregator.sol
+++ evm-contracts/src/v0.6/dev/FluxAggregator.sol
@@ -520,7 +520,7 @@ contract FluxAggregator is AggregatorInterface, Owned {
   /**
    * @notice allows non-oracles to request a new round
    */
-  function requestRateUpdate()
+  function requestNewRound()
     external
   {
     require(requesters[msg.sender].authorized, "not authorized requester");
