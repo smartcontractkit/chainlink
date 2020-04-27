931f76b38ac299486298d9e8aee1c2226fe85535
Jonathan Mukai-Heidt <johnny@whiskermanor.com>
2020-04-27 17:55:37 -0400

    Include total count in job runs search
    - Reduce round trips to the DB from 2 to 1

--- explorer/src/migration/1588016794171-SpeedUpJobRunSearch.ts
+++ explorer/src/migration/1588016794171-SpeedUpJobRunSearch.ts
@@ -1,18 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class SpeedUpJobRunSearch1588016794171 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
         CREATE INDEX "job_run_searchable_addresses" ON "job_run" USING GIN
           ((ARRAY["job_run"."runId","job_run"."jobId","job_run"."requestId","job_run"."requester", "job_run"."txHash"]));
       `)
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
         DROP INDEX "job_run_searchable_addresses";
       `)
-    }
-
+  }
 }
