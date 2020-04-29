45502d3916fd9a8fa68bfcb843db38ef3265254e
Sam Davies <sam@samueldavies.net>
2020-04-29 14:53:57 +0100

    Correctly handle empty string values in json conversion

--- core/store/migrations/migration1585908150/migrate.go
+++ core/store/migrations/migration1585908150/migrate.go
@@ -7,12 +7,21 @@ import (
 // Migrate changes all json columns to be jsonb
 func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
+		UPDATE initiators SET params = NULL WHERE params = '';
 		ALTER TABLE initiators ALTER COLUMN params TYPE jsonb USING params::jsonb;
+
+		UPDATE keys SET json = NULL WHERE json = '';
 		ALTER TABLE keys ALTER COLUMN json TYPE jsonb USING json::jsonb;
+
+		UPDATE run_requests SET request_params = '{}' WHERE request_params = '';
 		ALTER TABLE run_requests ALTER COLUMN request_params DROP DEFAULT;
 		ALTER TABLE run_requests ALTER COLUMN request_params TYPE jsonb USING request_params::jsonb;
 		ALTER TABLE run_requests ALTER COLUMN request_params SET DEFAULT '{}'::jsonb;
+
+		UPDATE run_results SET data = NULL WHERE data = '';
 		ALTER TABLE run_results ALTER COLUMN data TYPE jsonb USING data::jsonb;
+
+		UPDATE task_specs SET params = NULL WHERE params = '';
 		ALTER TABLE task_specs ALTER COLUMN params TYPE jsonb USING params::jsonb;
 	`).Error
 }
