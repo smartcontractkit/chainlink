63e1098f694d1d627215fdf2f84ed012a19daea0
Jonathan Mukai-Heidt <johnny@whiskermanor.com>
2020-04-30 09:39:15 -0400

    Stop using TYPEORM_* vars rather than JSON configs
    - NB: TYPEORM_NAME still sets ENV var for the database connection

--- explorer/src/database.ts
+++ explorer/src/database.ts
@@ -1,38 +1,13 @@
 import 'reflect-metadata'
 import { Connection, createConnection } from 'typeorm'
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import { TypeOrmLogger } from './logging'
-
-const overridableKeys = ['host', 'port', 'username', 'password', 'database']
 
 const getEnv = (): string => {
   return process.env.TYPEORM_NAME || process.env.NODE_ENV || 'development'
 }
 
-// Loads the following ENV vars, giving them precedence.
-// i.e. TYPEORM_PORT will replace "port" in ormconfig.json.
-const mergeOptions = (
-  loadedOptions: PostgresConnectionOptions,
-): PostgresConnectionOptions => {
-  const envOptions: Record<string, string> = {}
-  for (const v of overridableKeys) {
-    const envVar = process.env[`TYPEORM_${v.toUpperCase()}`]
-    if (envVar) {
-      envOptions[v] = envVar
-    }
-  }
-
-  const connectionOpts = {
-    ...loadedOptions,
-    ...envOptions,
-    logger: new TypeOrmLogger(),
-  } as PostgresConnectionOptions
-
-  return connectionOpts
-}
-
 export const openDbConnection = async (): Promise<Connection> => {
   const env = getEnv()
   const options = await import(`../ormconfig/${env}.json`)
-  return createConnection(mergeOptions(options))
+  return createConnection({ ...options, logger: new TypeOrmLogger() })
 }
