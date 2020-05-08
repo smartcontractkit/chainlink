cced8282c33d2ac5d87ccc94fd9260e3fc3ba17e
Steve Ellis <email@steveell.is>
2020-05-08 18:40:13 -0400

    break out historic aggregator interface

--- evm-contracts/src/v0.6/dev/AggregatorInterface.sol
+++ evm-contracts/src/v0.6/dev/AggregatorInterface.sol
@@ -1,19 +1,9 @@
 pragma solidity ^0.6.0;
 
-interface AggregatorInterface {
-  /*
-   * Historic Aggregator (a.k.a. Solidity v0.4 Aggregator, Aggregator version 2)
-   * functions
-   */
-  function latestAnswer() external view returns (int256);
-  function latestTimestamp() external view returns (uint256);
-  function latestRound() external view returns (uint256);
-  function getAnswer(uint256 roundId) external view returns (int256);
-  function getTimestamp(uint256 roundId) external view returns (uint256);
+import "./HistoricAggregatorInterface.sol";
 
-  /*
-   * FluxAggregator functions
-   */
+interface AggregatorInterface is HistoricAggregatorInterface {
+  function decimals() external view returns (uint8);
   function getRoundData(uint256 _roundId)
     external
     view
@@ -34,8 +24,4 @@ interface AggregatorInterface {
       uint64 updatedAt,
       uint256 answeredInRound
     );
-  function decimals() external view returns (uint8);
-
-  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
-  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
 }
