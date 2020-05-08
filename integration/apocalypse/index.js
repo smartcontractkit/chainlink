0ddc2d3000c37ebb5959f24b306896bac0e17073
spooktheducks <spooktheducks@protonmail.com>
2020-05-08 01:37:49 -0500

    Apocalypse test: environment online

--- integration/apocalypse/index.js
+++ integration/apocalypse/index.js
@@ -3,7 +3,43 @@ const ethers = require('ethers')
 const _ = require('lodash')
 const { contract } = require('@chainlink/test-helpers')
 
+// Contracts
 const oracleJson = require('../../evm-contracts/abi/v0.6/Oracle.json')
+const fluxAggregatorJson = require('../../evm-contracts/abi/v0.6/FluxAggregator.json')
+
+// RPC
+const RPC_GETH1  = 'http://localhost:8545'
+const RPC_GETH2  = 'http://localhost:18545'
+const RPC_PARITY = 'http://localhost:28545'
+const rpcProviders = [RPC_GETH1, RPC_GETH2]
+
+// Personas
+const accounts = require('./config/accounts.json')
+const carol = new ethers.Wallet(accounts.carol.privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH1))
+const loki = new ethers.Wallet(accounts.loki.privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH1))
+const lokiJr = new ethers.Wallet(accounts['loki-jr'].privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH2))
+const geth1 = new ethers.Wallet(accounts.geth1.privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH1))
+const geth2 = new ethers.Wallet(accounts.geth2.privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH2))
+const parity = new ethers.Wallet(accounts.parity.privkey.substring(2), new ethers.providers.JsonRpcProvider(RPC_GETH2))
+
+async function main() {
+    let linkToken = await deployLINK(carol, undefined)
+    console.log('LINK token:', linkToken.address)
+
+    let oracle = await deployOracle(carol, undefined, {
+        linkTokenAddress: linkToken.address,
+    })
+    console.log('Oracle:', oracle.address)
+
+    let fluxAggregator = await deployFluxAggregator(carol, undefined, {
+        linkTokenAddress: linkToken.address,
+        paymentAmount: '100',    // LINK-sats
+        timeout: 300,            // seconds
+    })
+    console.log('Flux Aggregator:', fluxAggregator.address)
+
+    mimicRegularTraffic(200)
+}
 
 async function deployContract({ Factory, name, signer }, ...deployArgs) {
     const contractFactory = new Factory(signer)
@@ -12,96 +48,87 @@ async function deployContract({ Factory, name, signer }, ...deployArgs) {
     return contract
 }
 
-async function deployLinkTokenContract(wallet, nonce) {
-    
+async function deployLINK(wallet, nonce) {
     const linkToken = await deployContract({
         Factory: contract.LinkTokenFactory,
         name: 'LinkToken',
         signer: wallet,
         nonce: nonce,
     })
-  
     return linkToken
 }
 
-async function txBombardment() {
-    let tx = {
-        to: '0x9ca9d2d5e04012c9ed24c0e513c9bfaa4a2dd77f',
-        value: ethers.utils.parseUnits('1', 'gwei'),
-        gasPrice: ethers.utils.parseUnits('100', 'gwei'),
-    }
-    
-    //let nonce = await wallets[0].provider.getTransactionCount('0xde554b6c292f5e5794a68dc560a537dd89d3b03e', 'pending')
-    //for (let i = 0; i < 333; i++) {
-    //    for (let wallet of wallets) {
-    //        tx.nonce = nonce
-    //        nonce++
-    //        wallet.sendTransaction(tx).then(tx => {
-    //            console.log(tx)
-    //        })
-    //    }
-    //}
+async function deployOracle(wallet, nonce, { linkTokenAddress }) {
+    let oracleFactory = new ethers.ContractFactory(oracleJson.compilerOutput.abi, oracleJson.compilerOutput.evm.bytecode, wallet)
+    let oracle = await oracleFactory.deploy(linkTokenAddress, { gasPrice: ethers.utils.parseUnits('50', 'gwei') })
+    await oracle.deployed()
+    return oracle
+}
 
-    let providers = [
-        new ethers.providers.JsonRpcProvider('http://localhost:8545'),
-        new ethers.providers.JsonRpcProvider('http://localhost:18545'),
-        //new ethers.providers.JsonRpcProvider('http://localhost:28545'),
-    ]
+async function deployFluxAggregator(wallet, nonce, { linkTokenAddress, paymentAmount, timeout }) {
+    let description = ethers.utils.formatBytes32String('xyzzy')
+    let fluxAggregatorFactory = new ethers.ContractFactory(fluxAggregatorJson.compilerOutput.abi, fluxAggregatorJson.compilerOutput.evm.bytecode.object, wallet)
+    let fluxAggregator = await fluxAggregatorFactory.deploy(linkTokenAddress, paymentAmount, timeout, 2, description, {
+        gasPrice: ethers.utils.parseUnits('50', 'gwei'),
+    })
+    await fluxAggregator.deployed()
+    return fluxAggregator
+}
 
-    let wallets = Array(100).fill(null).map((_, i) => ethers.Wallet.createRandom().connect(providers[i % providers.length]))
-    let nonces = Array(100).fill(null).map(_ => 0)
 
-    const moneybagsPrivateKey = 'a82180a8001e2681b9feac787afaf45f1d0bb7cb61eed53f879030cca1823459'
-    const moneybagsWallet = new ethers.Wallet(moneybagsPrivateKey, new ethers.providers.JsonRpcProvider('http://localhost:8545'))
-    let moneybagsNonce = await wallets[0].provider.getTransactionCount('0xde554b6c292f5e5794a68dc560a537dd89d3b03e', 'pending')
+async function mimicRegularTraffic(totalTxsPerSecond) {
+    const numAccounts = 200
 
-    let groups = _.chunk(wallets, 10)
-    for (let group of groups) {
-        console.log(`Funding ${group}...`)
-        let batch = group.map(async (wallet) => (await moneybagsWallet.sendTransaction({
-            to: wallet.address,
-            value: ethers.utils.parseUnits('1', 'ether'),
-            gasPrice: ethers.utils.parseUnits('20', 'gwei'),
-            nonce: moneybagsNonce++,
-        })).wait())
-        console.log(batch)
-
-        await Promise.all(batch)
-    }
-
-    console.log('Initiating tx tornado...')
-    for (let i = 0; i < 1000; i++) {
-        let wallet = wallets[i % providers.length]
-        tx.nonce = nonces[i % providers.length]
-        nonces[i % providers.length]++
-        //nonce++
-        wallet.sendTransaction(tx).then(tx => {
-            console.log(tx)
-        })
+    let accounts = await makeRandomAccounts(numAccounts, rpcProviders)
+    for (let account of accounts) {
+        sendRandomTransactions(account, accounts)
     }
 }
 
-async function main() {
-    const privateKey = 'a82180a8001e2681b9feac787afaf45f1d0bb7cb61eed53f879030cca1823459'
-    let wallet1 = new ethers.Wallet(privateKey, new ethers.providers.JsonRpcProvider('http://localhost:8545'))
-    let wallet2 = new ethers.Wallet(privateKey, new ethers.providers.JsonRpcProvider('http://localhost:18545'))
-    let wallet3 = new ethers.Wallet(privateKey, new ethers.providers.JsonRpcProvider('http://localhost:28545'))
-        
-    //console.log('Deploying LINK token...')
-    //let linkToken = await deployLinkTokenContract(wallet)
-    //await linkToken.deployed()
-    //console.log('LINK token:', linkToken.address)
-    //
-    //console.log('Deploying Oracle contract...')
-    //let oracleFactory = new ethers.ContractFactory(oracleJson.compilerOutput.abi, oracleJson.compilerOutput.evm.bytecode, wallet)
-    //let oracle = await oracleFactory.deploy(linkToken.address, { gasPrice: ethers.utils.parseUnits('50', 'gwei') })
-    //await oracle.deployed()
-    //console.log('Oracle:', oracle.address)
-    
-    console.log('Sending txs...')
-    await txBombardment([wallet1, wallet2, wallet3])
-    console.log('done')
+async function makeRandomAccounts(num, rpcProviders) {
+    let accounts = Array(num).fill(null).map((_, i) => ethers.Wallet.createRandom().connect(new ethers.providers.JsonRpcProvider(rpcProviders[i % rpcProviders.length])))
+    // Fund the accounts
+    let nonce = await carol.provider.getTransactionCount(carol.address, 'pending')
+    await Promise.all(
+        accounts.map(account => carol.sendTransaction({
+            to: account.address,
+            value: ethers.utils.parseUnits('5', 'ether'),
+            nonce: nonce++,
+        }))
+    )
+    return accounts
 }
+
+function sendRandomTransactions(account, accounts) {
+    function randomAccount() {
+        let i = Math.floor(Math.random() * Math.floor(accounts.length - 1))
+        return accounts[i]
+    }
+
+    async function send() {
+        let msBetweenTxs = 500
+        try {
+            // Re-read the config each time so that we can control the congestion dynamically
+            msBetweenTxs = getConfig().randomTraffic.msBetweenTxs
+
+            let tx = await account.sendTransaction({
+                to: randomAccount().address,
+                value: ethers.utils.parseUnits('1', 'wei'),
+                gasPrice: ethers.utils.parseUnits('20', 'gwei'),
+            })
+            // console.log(tx)
+        } catch (err) {
+            // console.log(err)
+        }
+        setTimeout(send, msBetweenTxs)
+    }
+    send()
+}
+
+function getConfig() {
+    return JSON.parse(fs.readFileSync('./config/config.json').toString())
+}
+
 
 main()
 
--- integration/apocalypse/scripts/build-images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd gethnet && \
-docker build -t smartcontract/gethnet:apocalypse . && \
-popd && \
-pushd paritynet && \
-docker build -t smartcontract/paritynet:apocalypse . && \
-popd
