a228b73ec5e5b0f1aae0a671e279a3935dca9b71
kaleofduty <59616916+kaleofduty@users.noreply.github.com>
2020-05-10 20:23:11 +0000

    Improve comments

--- evm-contracts/src/v0.6/dev/AggregatorProxy.sol
+++ evm-contracts/src/v0.6/dev/AggregatorProxy.sol
@@ -85,12 +85,10 @@ contract AggregatorProxy is AggregatorInterface, Owned {
   }
 
   /**
-   * @notice get all details about a round. Consumers are encouraged to check
+   * @notice get data about a round. Consumers are encouraged to check
    * that they're receiving fresh data by inspecting the updatedAt and
    * answeredInRound return values.
-   * @param _roundId the round ID to retrieve the details for. If _roundId
-   * has the special value UINT256_MAX (2**256-1), the contract will retrieve
-   * the latest round's details.
+   * @param _roundId the round ID to retrieve the round data for
    * @return roundId is the round ID for which details were retrieved
    * @return answer is the answer for the given round
    * @return startedAt is the timestamp when the round was started. This is 0
