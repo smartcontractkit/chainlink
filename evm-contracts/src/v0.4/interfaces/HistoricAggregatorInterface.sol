a5144e53d9d04e0af330db4d66cdc608c92d375f
Steve Ellis <email@steveell.is>
2020-05-18 19:51:43 -0400

    add HistoricInterface to all solidity versions

--- evm-contracts/src/v0.4/interfaces/HistoricAggregatorInterface.sol
+++ evm-contracts/src/v0.4/interfaces/HistoricAggregatorInterface.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0;
+pragma solidity >=0.4.24;
 
 interface HistoricAggregatorInterface {
   function latestAnswer() external returns (int256);
--- evm-contracts/src/v0.5/interfaces/HistoricAggregatorInterface.sol
+++ evm-contracts/src/v0.5/interfaces/HistoricAggregatorInterface.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0;
+pragma solidity >=0.5.0;
 
 interface HistoricAggregatorInterface {
   function latestAnswer() external returns (int256);
