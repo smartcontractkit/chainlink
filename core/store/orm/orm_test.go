a378cf666768c948e69eeffdaf2e2bd44c8b7e43
Steve Ellis <email@steveell.is>
2020-06-02 23:33:16 -0400

    fix go formatting

--- core/store/orm/orm_test.go
+++ core/store/orm/orm_test.go
@@ -83,10 +83,10 @@ func TestORM_ShowJobWithMultipleTasks(t *testing.T) {
 
 	job := cltest.NewJob()
 	job.Tasks = []models.TaskSpec{
-		models.TaskSpec{ Type: models.MustNewTaskType("task1") },
-		models.TaskSpec{ Type: models.MustNewTaskType("task2") },
-		models.TaskSpec{ Type: models.MustNewTaskType("task3") },
-		models.TaskSpec{ Type: models.MustNewTaskType("task4") },
+		models.TaskSpec{Type: models.MustNewTaskType("task1")},
+		models.TaskSpec{Type: models.MustNewTaskType("task2")},
+		models.TaskSpec{Type: models.MustNewTaskType("task3")},
+		models.TaskSpec{Type: models.MustNewTaskType("task4")},
 	}
 	assert.NoError(t, store.CreateJob(&job))
 
