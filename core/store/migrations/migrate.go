0356a1f4fc196fdbda6473d7f3154f0525ed0009
Sam <samsondav@protonmail.com>
2020-06-08 09:12:55 +0100

    Remove the unique index on txes.hash
    - Existing code creates duplicates in certain cases when races occur
    - It's almost impossible to fix this, the better solution is to allow
    the buggy behaviour to continue and focus on shipping the replacement
    transaction manager which will eliminate this class of problems

--- core/store/migrations/migrate.go
+++ core/store/migrations/migrate.go
@@ -54,6 +54,7 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1589470036"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1590226486"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1591141873"
+	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1591603775"
 
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
@@ -266,6 +267,10 @@ func init() {
 		{
 			ID:      "1591141873",
 			Migrate: migration1591141873.Migrate,
+		},
+		{
+			ID:      "1591603775",
+			Migrate: migration1591603775.Migrate,
 		},
 	}
 }
