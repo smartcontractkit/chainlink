1b0e92ef599e47186ca8ad9bf539199ab05d40b7
Sam <samsondav@protonmail.com>
2020-06-08 12:40:24 +0100

    Remove unique nonce index also

--- core/store/migrations/migration1591603775/migrate.go
+++ core/store/migrations/migration1591603775/migrate.go
@@ -5,9 +5,12 @@ import (
 )
 
 // Migrate changes the index on txes.hash to be non-unique again to allow existing buggy code to continue to function
+// It also drops the unique nonce index
 func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
 		DROP INDEX IF EXISTS idx_txes_hash;
 		CREATE INDEX idx_txes_hash ON txes (hash);
+		DROP INDEX IF EXISTS idx_txes_unique_nonces_per_account;
+		CREATE INDEX idx_txes_nonce ON txes(nonce);
     `).Error
 }
