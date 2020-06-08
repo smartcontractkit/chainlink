e46c7b8f62393d6feb2d5b232d9e813bd6f96f37
hellobart <zly.ojciec@gmail.com>
2020-06-08 17:38:24 +0200

    - update custom page - fixed dates on charts

--- feeds/src/pages/Custom.tsx
+++ feeds/src/pages/Custom.tsx
@@ -1,55 +1,52 @@
 import { DispatchBinding } from '@chainlink/ts-helpers'
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { aggregatorOperations } from 'state/ducks/aggregator'
-import { AggregatorVis } from 'components/aggregatorVis'
-import { AnswerHistory } from 'components/answerHistory'
-import { DeviationHistory } from 'components/deviationHistory'
-import { Header } from 'components/header'
-import { OracleTable } from 'components/oracleTable'
+import { RouteComponentProps } from 'react-router-dom'
+import { connect, MapDispatchToProps } from 'react-redux'
 import { FeedConfig } from 'config'
+import { Header } from 'components/header'
+import { Aggregator, FluxAggregator } from 'components/aggregator'
+import { aggregatorOperations } from '../state/ducks/aggregator'
+import { useLocation } from 'react-router-dom'
 import { parseQuery, uIntFrom } from 'utils'
 
-interface OwnProps {}
+interface OwnProps
+  extends RouteComponentProps<{ pair: string; network?: string }> {}
 
 interface DispatchProps {
-  initContract: DispatchBinding<typeof aggregatorOperations.initContract>
+  fetchOracleNodes: DispatchBinding<
+    typeof aggregatorOperations.fetchOracleNodes
+  >
 }
 
 interface Props extends OwnProps, DispatchProps {}
 
-const Page: React.FC<Props> = ({ initContract }) => {
+const Page: React.FC<Props> = ({ fetchOracleNodes }) => {
   const location = useLocation()
   const [config] = useState<FeedConfig>(
     parseConfig(parseQuery(location.search)),
   )
 
   useEffect(() => {
-    try {
-      initContract(config)
-    } catch (error) {
-      console.error('Could not initiate contract:', error)
-    }
-  }, [initContract, config])
+    fetchOracleNodes()
+  }, [fetchOracleNodes])
+
+  let content
+  if (config && config.contractVersion === 3) {
+    content = <FluxAggregator config={config} />
+  } else if (config) {
+    content = <Aggregator config={config} />
+  } else {
+    content = <>There was an error loading the page. Refresh to try again.</>
+  }
 
   return (
     <>
       <div className="page-container-full-width">
         <Header />
       </div>
-      <div className="page-wrapper network-page">
-        <AggregatorVis config={config} />
-        {config && config.history && <AnswerHistory config={config} />}
-        {config && config.history && <DeviationHistory config={config} />}
-        <OracleTable />
-      </div>
+      <div className="page-wrapper network-page">{content}</div>
     </>
   )
-}
-
-const mapDispatchToProps = {
-  initContract: aggregatorOperations.initContract,
 }
 
 /**
@@ -66,6 +63,10 @@ function parseConfig(config: Record<string, string>): FeedConfig {
     heartbeat: uIntFrom(config.heartbeat ?? 0) ?? false,
     historyDays: uIntFrom(config.historyDays ?? 1),
   }
+}
+
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, OwnProps> = {
+  fetchOracleNodes: aggregatorOperations.fetchOracleNodes,
 }
 
 export default connect(null, mapDispatchToProps)(Page)
