cdd8a0f3d5d35253b9f1f8a34a7a1f02425cb5ba
spooktheducks <spooktheducks@protonmail.com>
2020-06-18 00:16:07 -0500

    Update fixture paths

--- core/services/subscription_test.go
+++ core/services/subscription_test.go
@@ -416,7 +416,7 @@ func TestServices_NewInitiatorSubscription_RunLog_ReplayFromBlock(t *testing.T) 
 					{models.IDToTopic(initr.JobSpecID), models.IDToHexTopic(initr.JobSpecID)}},
 			}
 
-			receipt := cltest.TxReceiptFromFixture(t, "../eth/testdata/runlogReceipt.json")
+			receipt := cltest.TxReceiptFromFixture(t, "../services/eth/testdata/runlogReceipt.json")
 			log := receipt.Logs[3]
 			log.Topics[1] = models.IDToTopic(job.ID)
 
