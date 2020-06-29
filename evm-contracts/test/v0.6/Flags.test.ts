006c08db6552658e9679bfdce82510111dd70439
Steve Ellis <email@steveell.is>
2020-06-28 20:02:02 -0400

    guard flag reads with access control

--- evm-contracts/test/v0.6/Flags.test.ts
+++ evm-contracts/test/v0.6/Flags.test.ts
@@ -7,9 +7,11 @@ import {
 import { assert } from 'chai'
 //import { ethers } from 'ethers'
 import { FlagsFactory } from '../../ethers/v0.6/FlagsFactory'
+import { FlagsTestHelperFactory } from '../../ethers/v0.6/FlagsTestHelperFactory'
 
 const provider = setup.provider()
 const flagsFactory = new FlagsFactory()
+const consumerFactory = new FlagsTestHelperFactory()
 let personas: setup.Personas
 
 beforeAll(async () => {
@@ -18,15 +20,17 @@ beforeAll(async () => {
 
 describe('Flags', () => {
   let flags: contract.Instance<FlagsFactory>
-  let questionable: string
+  let consumer: contract.Instance<FlagsTestHelperFactory>
   const deployment = setup.snapshot(provider, async () => {
     flags = await flagsFactory.connect(personas.Nelly).deploy()
     await flags.connect(personas.Nelly).disableAccessCheck()
+    consumer = await consumerFactory
+      .connect(personas.Nelly)
+      .deploy(flags.address)
   })
 
   beforeEach(async () => {
     await deployment()
-    questionable = personas.Norbert.address
   })
 
   it('has a limited public interface', () => {
@@ -51,30 +55,34 @@ describe('Flags', () => {
   describe('#setFlagOn', () => {
     describe('when called by the owner', () => {
       it('updates the warning flag', async () => {
-        assert.equal(false, await flags.getFlag(questionable))
+        assert.equal(false, await flags.getFlag(consumer.address))
 
-        await flags.connect(personas.Nelly).setFlagOn(questionable)
+        await flags.connect(personas.Nelly).setFlagOn(consumer.address)
 
-        assert.equal(true, await flags.getFlag(questionable))
+        assert.equal(true, await flags.getFlag(consumer.address))
       })
 
       it('emits an event log', async () => {
-        const tx = await flags.connect(personas.Nelly).setFlagOn(questionable)
+        const tx = await flags
+          .connect(personas.Nelly)
+          .setFlagOn(consumer.address)
         const receipt = await tx.wait()
 
         const event = matchers.eventExists(
           receipt,
           flags.interface.events.FlagOn,
         )
-        assert.equal(questionable, h.eventArgs(event).subject)
+        assert.equal(consumer.address, h.eventArgs(event).subject)
       })
       describe('if a flag has already been raised', () => {
         beforeEach(async () => {
-          await flags.connect(personas.Nelly).setFlagOn(questionable)
+          await flags.connect(personas.Nelly).setFlagOn(consumer.address)
         })
 
         it('emits an event log', async () => {
-          const tx = await flags.connect(personas.Nelly).setFlagOn(questionable)
+          const tx = await flags
+            .connect(personas.Nelly)
+            .setFlagOn(consumer.address)
           const receipt = await tx.wait()
           assert.equal(0, receipt.events?.length)
         })
@@ -84,7 +92,7 @@ describe('Flags', () => {
     describe('when called by a non-owner', () => {
       it('updates the warning flag', async () => {
         await matchers.evmRevert(
-          flags.connect(personas.Neil).setFlagOn(questionable),
+          flags.connect(personas.Neil).setFlagOn(consumer.address),
           'Only callable by owner',
         )
       })
@@ -93,38 +101,40 @@ describe('Flags', () => {
 
   describe('#setFlagOff', () => {
     beforeEach(async () => {
-      await flags.connect(personas.Nelly).setFlagOn(questionable)
+      await flags.connect(personas.Nelly).setFlagOn(consumer.address)
     })
 
     describe('when called by the owner', () => {
       it('updates the warning flag', async () => {
-        assert.equal(true, await flags.getFlag(questionable))
+        assert.equal(true, await flags.getFlag(consumer.address))
 
-        await flags.connect(personas.Nelly).setFlagOff(questionable)
+        await flags.connect(personas.Nelly).setFlagOff(consumer.address)
 
-        assert.equal(false, await flags.getFlag(questionable))
+        assert.equal(false, await flags.getFlag(consumer.address))
       })
 
       it('emits an event log', async () => {
-        const tx = await flags.connect(personas.Nelly).setFlagOff(questionable)
+        const tx = await flags
+          .connect(personas.Nelly)
+          .setFlagOff(consumer.address)
         const receipt = await tx.wait()
 
         const event = matchers.eventExists(
           receipt,
           flags.interface.events.FlagOff,
         )
-        assert.equal(questionable, h.eventArgs(event).subject)
+        assert.equal(consumer.address, h.eventArgs(event).subject)
       })
 
       describe('if a flag has already been raised', () => {
         beforeEach(async () => {
-          await flags.connect(personas.Nelly).setFlagOff(questionable)
+          await flags.connect(personas.Nelly).setFlagOff(consumer.address)
         })
 
         it('emits an event log', async () => {
           const tx = await flags
             .connect(personas.Nelly)
-            .setFlagOff(questionable)
+            .setFlagOff(consumer.address)
           const receipt = await tx.wait()
           assert.equal(0, receipt.events?.length)
         })
@@ -134,9 +144,54 @@ describe('Flags', () => {
     describe('when called by a non-owner', () => {
       it('updates the warning flag', async () => {
         await matchers.evmRevert(
-          flags.connect(personas.Neil).setFlagOff(questionable),
+          flags.connect(personas.Neil).setFlagOff(consumer.address),
           'Only callable by owner',
         )
+      })
+    })
+  })
+
+  describe('#getFlag', () => {
+    describe('if the access control is turned on', () => {
+      beforeEach(async () => {
+        await flags.connect(personas.Nelly).enableAccessCheck()
+      })
+
+      it('reverts', async () => {
+        await matchers.evmRevert(
+          consumer.getFlag(consumer.address),
+          'No access',
+        )
+      })
+
+      describe('if access is granted to the address', () => {
+        beforeEach(async () => {
+          await flags.connect(personas.Nelly).addAccess(consumer.address)
+        })
+
+        it('does not revert', async () => {
+          await consumer.getFlag(consumer.address)
+        })
+      })
+    })
+
+    describe('if the access control is turned off', () => {
+      beforeEach(async () => {
+        await flags.connect(personas.Nelly).disableAccessCheck()
+      })
+
+      it('does not revert', async () => {
+        await consumer.getFlag(consumer.address)
+      })
+
+      describe('if access is granted to the address', () => {
+        beforeEach(async () => {
+          await flags.connect(personas.Nelly).addAccess(consumer.address)
+        })
+
+        it('does not revert', async () => {
+          await consumer.getFlag(consumer.address)
+        })
       })
     })
   })
