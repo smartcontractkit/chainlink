f68c7ed02ffd0881d8b7eaef0d84a11785eb426c
kaleofduty <59616916+kaleofduty@users.noreply.github.com>
2020-07-01 12:19:38 +0000

    Remove leftover comment

--- evm-contracts/src/v0.6/dev/SimpleWriteAccessController.sol
+++ evm-contracts/src/v0.6/dev/SimpleWriteAccessController.sol
@@ -103,7 +103,6 @@ contract SimpleWriteAccessController is AccessControllerInterface, Owned {
 
   /**
    * @dev reverts if the caller does not have access
-   * @dev WARNING: This modifier should only be used on view methods
    */
   modifier checkAccess() {
     require(hasAccess(msg.sender, msg.data), "No access");
