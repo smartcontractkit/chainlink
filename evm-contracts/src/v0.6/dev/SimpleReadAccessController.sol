5702bf27159c339d834669f36d8cab1711f6ef9d
Steve Ellis <email@steveell.is>
2020-06-30 21:16:47 -0400

    flip SimpleRead/Write inherritance

--- evm-contracts/src/v0.6/dev/SimpleReadAccessController.sol
+++ evm-contracts/src/v0.6/dev/SimpleReadAccessController.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.6.0;
 
-import "../Owned.sol";
-import "./AccessControllerInterface.sol";
+import "./SimpleWriteAccessController.sol";
 
 /**
  * @title SimpleReadAccessController
@@ -11,23 +10,10 @@ import "./AccessControllerInterface.sol";
  * does not weaken the access control while improving usability)
  * - accounts explicitly added to an access list
  * @dev SimpleReadAccessController is not suitable for access controlling writes
- * since it grants any externally owned account access!
+ * since it grants any externally owned account access! See
+ * SimpleWriteAccessController for that.
  */
-contract SimpleReadAccessController is AccessControllerInterface, Owned {
-
-  bool public checkEnabled;
-  mapping(address => bool) internal accessList;
-
-  event AddedAccess(address user);
-  event RemovedAccess(address user);
-  event CheckAccessEnabled();
-  event CheckAccessDisabled();
-
-  constructor()
-    public
-  {
-    checkEnabled = true;
-  }
+contract SimpleReadAccessController is SimpleWriteAccessController {
 
   /**
    * @notice Returns the access of an address
@@ -46,70 +32,4 @@ contract SimpleReadAccessController is AccessControllerInterface, Owned {
     return accessList[_user] || !checkEnabled || _user == tx.origin;
   }
 
-  /**
-   * @notice Adds an address to the access list
-   * @param _user The address to add
-   */
-  function addAccess(address _user)
-    external
-    onlyOwner()
-  {
-    if (!accessList[_user]) {
-      accessList[_user] = true;
-
-      emit AddedAccess(_user);
-    }
-  }
-
-  /**
-   * @notice Removes an address from the access list
-   * @param _user The address to remove
-   */
-  function removeAccess(address _user)
-    external
-    onlyOwner()
-  {
-    if (accessList[_user]) {
-      accessList[_user] = false;
-
-      emit RemovedAccess(_user);
-    }
-  }
-
-  /**
-   * @notice makes the access check enforced
-   */
-  function enableAccessCheck()
-    external
-    onlyOwner()
-  {
-    if (!checkEnabled) {
-      checkEnabled = true;
-
-      emit CheckAccessEnabled();
-    }
-  }
-
-  /**
-   * @notice makes the access check unenforced
-   */
-  function disableAccessCheck()
-    external
-    onlyOwner()
-  {
-    if (checkEnabled) {
-      checkEnabled = false;
-
-      emit CheckAccessDisabled();
-    }
-  }
-
-  /**
-   * @dev reverts if the caller does not have access
-   * @dev WARNING: This modifier should only be used on view methods
-   */
-  modifier checkAccess() {
-    require(hasAccess(msg.sender, msg.data), "No access");
-    _;
-  }
 }
--- evm-contracts/src/v0.6/dev/SimpleWriteAccessController.sol
+++ evm-contracts/src/v0.6/dev/SimpleWriteAccessController.sol
@@ -1,13 +1,30 @@
 pragma solidity ^0.6.0;
 
-import "./SimpleReadAccessController.sol";
+import "../Owned.sol";
+import "./AccessControllerInterface.sol";
 
 /**
  * @title SimpleWriteAccessController
- * @notice Allows the owner to set access for addresses. External accounts are
- * not granted special access.
+ * @notice Gives access to accounts explicitly added to an access list by the
+ * controller's owner.
+ * @dev does not make any special permissions for externally, see
+ * SimpleReadAccessController for that.
  */
-contract SimpleWriteAccessController is SimpleReadAccessController {
+contract SimpleWriteAccessController is AccessControllerInterface, Owned {
+
+  bool public checkEnabled;
+  mapping(address => bool) internal accessList;
+
+  event AddedAccess(address user);
+  event RemovedAccess(address user);
+  event CheckAccessEnabled();
+  event CheckAccessDisabled();
+
+  constructor()
+    public
+  {
+    checkEnabled = true;
+  }
 
   /**
    * @notice Returns the access of an address
@@ -19,10 +36,77 @@ contract SimpleWriteAccessController is SimpleReadAccessController {
   )
     public
     view
+    virtual
     override
     returns (bool)
   {
     return accessList[_user] || !checkEnabled;
   }
 
+  /**
+   * @notice Adds an address to the access list
+   * @param _user The address to add
+   */
+  function addAccess(address _user)
+    external
+    onlyOwner()
+  {
+    if (!accessList[_user]) {
+      accessList[_user] = true;
+
+      emit AddedAccess(_user);
+    }
+  }
+
+  /**
+   * @notice Removes an address from the access list
+   * @param _user The address to remove
+   */
+  function removeAccess(address _user)
+    external
+    onlyOwner()
+  {
+    if (accessList[_user]) {
+      accessList[_user] = false;
+
+      emit RemovedAccess(_user);
+    }
+  }
+
+  /**
+   * @notice makes the access check enforced
+   */
+  function enableAccessCheck()
+    external
+    onlyOwner()
+  {
+    if (!checkEnabled) {
+      checkEnabled = true;
+
+      emit CheckAccessEnabled();
+    }
+  }
+
+  /**
+   * @notice makes the access check unenforced
+   */
+  function disableAccessCheck()
+    external
+    onlyOwner()
+  {
+    if (checkEnabled) {
+      checkEnabled = false;
+
+      emit CheckAccessDisabled();
+    }
+  }
+
+  /**
+   * @dev reverts if the caller does not have access
+   * @dev WARNING: This modifier should only be used on view methods
+   */
+  modifier checkAccess() {
+    require(hasAccess(msg.sender, msg.data), "No access");
+    _;
+  }
 }
