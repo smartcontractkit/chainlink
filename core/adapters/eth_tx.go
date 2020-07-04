d6077fceaade35a3973041a625bb0095d1d1a2cb
Sam <samsondav@protonmail.com>
2020-07-04 09:05:07 +0100

    Create EthTxCommon interface

--- core/adapters/eth_tx.go
+++ core/adapters/eth_tx.go
@@ -5,14 +5,12 @@ import (
 	"fmt"
 
 	"github.com/smartcontractkit/chainlink/core/logger"
-	"github.com/smartcontractkit/chainlink/core/store"
 	strpkg "github.com/smartcontractkit/chainlink/core/store"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/utils"
 
-	"github.com/ethereum/go-ethereum/common"
+	gethCommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 	"gopkg.in/guregu/null.v3"
 )
@@ -26,10 +24,10 @@ const (
 // EthTx holds the Address to send the result to and the FunctionSelector
 // to execute.
 type EthTx struct {
-	ToAddress common.Address `json:"address"`
+	ToAddress gethCommon.Address `json:"address"`
 	// NOTE: FromAddress is deprecated and kept for backwards compatibility, new job specs should use fromAddresses
-	FromAddress      common.Address          `json:"fromAddress,omitempty"`
-	FromAddresses    []common.Address        `json:"fromAddresses,omitempty"`
+	FromAddress      gethCommon.Address      `json:"fromAddress,omitempty"`
+	FromAddresses    []gethCommon.Address    `json:"fromAddresses,omitempty"`
 	FunctionSelector models.FunctionSelector `json:"functionSelector"`
 	DataPrefix       hexutil.Bytes           `json:"dataPrefix"`
 	DataFormat       string                  `json:"format"`
@@ -40,6 +38,44 @@ type EthTx struct {
 
 	// MinRequiredOutgoingConfirmations only works with bulletprooftxmanager
 	MinRequiredOutgoingConfirmations uint64 `json:"minRequiredOutgoingConfirmations,omitempty"`
+}
+
+func (e *EthTx) GetToAddress() gethCommon.Address {
+	return e.ToAddress
+}
+
+func (e *EthTx) GetFromAddresses() []gethCommon.Address {
+	if len(e.FromAddresses) > 0 {
+		if e.FromAddress != utils.ZeroAddress {
+			logger.Warn("task spec for task run specified both fromAddress and fromAddresses." +
+				" fromAddress is deprecated, it will be ignored and fromAddresses used instead. " +
+				"Specifying both of these keys in a job spec may result in an error in future versions of Chainlink")
+		}
+		return e.FromAddresses
+	}
+
+	if e.FromAddress == utils.ZeroAddress {
+		return []gethCommon.Address{}
+	}
+	logger.Warnf(`DEPRECATION WARNING: task spec specified a fromAddress of %s. fromAddress has been deprecated and will be removed in a future version of Chainlink. Please use fromAddresses instead. You can pin a job to one address simply by using only one element, like so:
+{
+	"type": "EthTx",
+	"fromAddresses": ["%s"],
+} 
+`, e.FromAddress.Hex(), e.FromAddress.Hex())
+	return []gethCommon.Address{e.FromAddress}
+}
+
+func (e *EthTx) GetGasLimit() uint64 {
+	return e.GasLimit
+}
+
+func (e *EthTx) GetGasPrice() *utils.Big {
+	return e.GasPrice
+}
+
+func (e *EthTx) GetMinRequiredOutgoingConfirmations() uint64 {
+	return e.MinRequiredOutgoingConfirmations
 }
 
 // TaskType returns the type of Adapter.
@@ -59,139 +95,13 @@ func (e *EthTx) Perform(input models.RunInput, store *strpkg.Store) models.RunOu
 
 // TODO(sam): https://www.pivotaltracker.com/story/show/173280188
 func (e *EthTx) perform(input models.RunInput, store *strpkg.Store) models.RunOutput {
-	trtx, err := store.FindEthTaskRunTxByTaskRunID(input.TaskRunID().UUID())
-	if err != nil {
-		err = errors.Wrap(err, "FindEthTaskRunTxByTaskRunID failed")
-		logger.Error(err)
-		return models.NewRunOutputError(err)
-	}
-	if trtx != nil {
-		return e.checkForConfirmation(*trtx, input, store)
-	}
-	return e.insertEthTx(input, store)
-}
-
-func (e *EthTx) checkForConfirmation(trtx models.EthTaskRunTx, input models.RunInput, store *store.Store) models.RunOutput {
-	switch trtx.EthTx.State {
-	case models.EthTxConfirmed:
-		return e.checkEthTxForReceipt(trtx.EthTx.ID, input, store)
-	case models.EthTxFatalError:
-		return models.NewRunOutputError(trtx.EthTx.GetError())
-	default:
-		return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
-	}
-}
-
-func (e *EthTx) pickFromAddress(input models.RunInput, store *store.Store) (common.Address, error) {
-	if len(e.FromAddresses) > 0 {
-		if e.FromAddress != utils.ZeroAddress {
-			logger.Warnf("task spec for task run %s specified both fromAddress and fromAddresses."+
-				" fromAddress is deprecated, it will be ignored and fromAddresses used instead. "+
-				"Specifying both of these keys in a job spec may result in an error in future versions of Chainlink", input.TaskRunID())
+	return findOrInsertEthTx(e, input, store, func() ([]byte, error) {
+		txData, err := getTxData(e, input)
+		if err != nil {
+			return nil, err
 		}
-		return store.GetRoundRobinAddress(e.FromAddresses...)
-	}
-	if e.FromAddress == utils.ZeroAddress {
-		return store.GetRoundRobinAddress()
-	}
-	logger.Warnf(`DEPRECATION WARNING: task spec for task run %s specified a fromAddress of %s. fromAddress has been deprecated and will be removed in a future version of Chainlink. Please use fromAddresses instead. You can pin a job to one address simply by using only one element, like so:
-{
-	"type": "EthTx",
-	"fromAddresses": ["%s"],
-} 
-`, input.TaskRunID(), e.FromAddress.Hex(), e.FromAddress.Hex())
-	return e.FromAddress, nil
-}
-
-func (e *EthTx) insertEthTx(input models.RunInput, store *store.Store) models.RunOutput {
-	txData, err := getTxData(e, input)
-	if err != nil {
-		err = errors.Wrap(err, "insertEthTx failed while constructing EthTx data")
-		return models.NewRunOutputError(err)
-	}
-
-	taskRunID := input.TaskRunID()
-	toAddress := e.ToAddress
-	fromAddress, err := e.pickFromAddress(input, store)
-	if err != nil {
-		err = errors.Wrap(err, "insertEthTx failed to pickFromAddress")
-		logger.Error(err)
-		return models.NewRunOutputError(err)
-	}
-	encodedPayload := utils.ConcatBytes(e.FunctionSelector.Bytes(), e.DataPrefix, txData)
-
-	var gasLimit uint64
-	if e.GasLimit == 0 {
-		gasLimit = store.Config.EthGasLimitDefault()
-	} else {
-		gasLimit = e.GasLimit
-	}
-
-	if err := store.IdempotentInsertEthTaskRunTx(taskRunID, fromAddress, toAddress, encodedPayload, gasLimit); err != nil {
-		err = errors.Wrap(err, "insertEthTx failed")
-		logger.Error(err)
-		return models.NewRunOutputError(err)
-	}
-
-	store.NotifyNewEthTx.Trigger()
-
-	return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
-}
-
-func (e *EthTx) checkEthTxForReceipt(ethTxID int64, input models.RunInput, s *store.Store) models.RunOutput {
-	var minRequiredOutgoingConfirmations uint64
-	if e.MinRequiredOutgoingConfirmations == 0 {
-		minRequiredOutgoingConfirmations = s.Config.MinRequiredOutgoingConfirmations()
-	} else {
-		minRequiredOutgoingConfirmations = e.MinRequiredOutgoingConfirmations
-	}
-
-	hash, err := getConfirmedTxHash(ethTxID, s.GetRawDB(), minRequiredOutgoingConfirmations)
-
-	if err != nil {
-		logger.Error(err)
-		return models.NewRunOutputError(err)
-	}
-
-	if hash == nil {
-		return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
-	}
-
-	hexHash := (*hash).Hex()
-
-	output := input.Data()
-	output, err = output.MultiAdd(models.KV{
-		"result": hexHash,
-		// HACK: latestOutgoingTxHash is used for backwards compatibility with the stats pusher
-		"latestOutgoingTxHash": hexHash,
+		return utils.ConcatBytes(e.FunctionSelector.Bytes(), e.DataPrefix, txData), nil
 	})
-	if err != nil {
-		err = errors.Wrap(err, "checkEthTxForReceipt failed")
-		logger.Error(err)
-		return models.NewRunOutputError(err)
-	}
-	return models.NewRunOutputComplete(output)
-}
-
-func getConfirmedTxHash(ethTxID int64, db *gorm.DB, minRequiredOutgoingConfirmations uint64) (*common.Hash, error) {
-	receipt := models.EthReceipt{}
-	err := db.
-		Joins("INNER JOIN eth_tx_attempts ON eth_tx_attempts.hash = eth_receipts.tx_hash AND eth_tx_attempts.eth_tx_id = ?", ethTxID).
-		Joins("INNER JOIN eth_txes ON eth_txes.id = eth_tx_attempts.eth_tx_id AND eth_txes.state = 'confirmed'").
-		Where("eth_receipts.block_number <= (SELECT max(number) - ? FROM heads)", minRequiredOutgoingConfirmations).
-		First(&receipt).
-		Error
-
-	if err == nil {
-		return &receipt.TxHash, nil
-	}
-
-	if gorm.IsRecordNotFoundError(err) {
-		return nil, nil
-	}
-
-	return nil, errors.Wrap(err, "getConfirmedTxHash failed")
-
 }
 
 func (e *EthTx) legacyPerform(input models.RunInput, store *strpkg.Store) models.RunOutput {
@@ -218,7 +128,7 @@ func (e *EthTx) legacyPerform(input models.RunInput, store *strpkg.Store) models
 func getTxData(e *EthTx, input models.RunInput) ([]byte, error) {
 	result := input.Result()
 	if e.DataFormat == "" {
-		return common.HexToHash(result.Str).Bytes(), nil
+		return gethCommon.HexToHash(result.Str).Bytes(), nil
 	}
 
 	output, err := utils.EVMTranscodeJSONWithFormat(result, e.DataFormat)
@@ -237,7 +147,7 @@ func getTxData(e *EthTx, input models.RunInput) ([]byte, error) {
 }
 
 func createTxRunResult(
-	address common.Address,
+	address gethCommon.Address,
 	gasPrice *utils.Big,
 	gasLimit uint64,
 	data []byte,
@@ -294,7 +204,7 @@ func ensureTxRunResult(input models.RunInput, str *strpkg.Store) models.RunOutpu
 		return models.NewRunOutputError(errors.Wrapf(err, "while processing ethtx input %#+v", input))
 	}
 
-	hash := common.HexToHash(val)
+	hash := gethCommon.HexToHash(val)
 	receipt, state, err := str.TxManager.BumpGasUntilSafe(hash)
 	if err != nil {
 		// We failed to get one of the TxAttempt receipts, so we won't mark this
---
+++ core/adapters/eth_tx_common.go
@@ -0,0 +1,137 @@
+package adapters
+
+import (
+	"github.com/smartcontractkit/chainlink/core/logger"
+	strpkg "github.com/smartcontractkit/chainlink/core/store"
+	"github.com/smartcontractkit/chainlink/core/store/models"
+	"github.com/smartcontractkit/chainlink/core/utils"
+
+	gethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+)
+
+// EthTxCommon represents a common interface for all EthTx adapters
+type EthTxCommon interface {
+	GetToAddress() gethCommon.Address
+	GetFromAddresses() []gethCommon.Address
+	GetGasLimit() uint64
+	// GasPrice only needed for legacy tx manager
+	GetGasPrice() *utils.Big
+	// MinRequiredOutgoingConfirmations only works with bulletprooftxmanager
+	GetMinRequiredOutgoingConfirmations() uint64
+}
+
+func findOrInsertEthTx(e EthTxCommon, input models.RunInput, store *strpkg.Store, payloadCB func() ([]byte, error)) models.RunOutput {
+	trtx, err := store.FindEthTaskRunTxByTaskRunID(input.TaskRunID().UUID())
+	if err != nil {
+		err = errors.Wrap(err, "FindEthTaskRunTxByTaskRunID failed")
+		logger.Error(err)
+		return models.NewRunOutputError(err)
+	}
+	if trtx != nil {
+		return checkForConfirmation(e, *trtx, input, store)
+	}
+	return insertEthTx(e, input, store, payloadCB)
+}
+
+func checkForConfirmation(e EthTxCommon, trtx models.EthTaskRunTx, input models.RunInput, store *strpkg.Store) models.RunOutput {
+	switch trtx.EthTx.State {
+	case models.EthTxConfirmed:
+		return checkEthTxForReceipt(e, trtx.EthTx.ID, input, store)
+	case models.EthTxFatalError:
+		return models.NewRunOutputError(trtx.EthTx.GetError())
+	default:
+		return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
+	}
+}
+
+func insertEthTx(e EthTxCommon, input models.RunInput, store *strpkg.Store, payloadCB func() ([]byte, error)) models.RunOutput {
+	encodedPayload, err := payloadCB()
+	if err != nil {
+		err = errors.Wrap(err, "insertEthTx failed while constructing EthTx data")
+		return models.NewRunOutputError(err)
+	}
+
+	taskRunID := input.TaskRunID()
+	toAddress := e.GetToAddress()
+	fromAddress, err := store.GetRoundRobinAddress(e.GetFromAddresses()...)
+	if err != nil {
+		err = errors.Wrap(err, "insertEthTx failed to GetRoundRobinAddress")
+		logger.Error(err)
+		return models.NewRunOutputError(err)
+	}
+
+	var gasLimit uint64
+	if e.GetGasLimit() == 0 {
+		gasLimit = store.Config.EthGasLimitDefault()
+	} else {
+		gasLimit = e.GetGasLimit()
+	}
+
+	if err := store.IdempotentInsertEthTaskRunTx(taskRunID, fromAddress, toAddress, encodedPayload, gasLimit); err != nil {
+		err = errors.Wrap(err, "insertEthTx failed")
+		logger.Error(err)
+		return models.NewRunOutputError(err)
+	}
+
+	store.NotifyNewEthTx.Trigger()
+
+	return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
+}
+
+func checkEthTxForReceipt(e EthTxCommon, ethTxID int64, input models.RunInput, s *strpkg.Store) models.RunOutput {
+	var minRequiredOutgoingConfirmations uint64
+	if e.GetMinRequiredOutgoingConfirmations() == 0 {
+		minRequiredOutgoingConfirmations = s.Config.MinRequiredOutgoingConfirmations()
+	} else {
+		minRequiredOutgoingConfirmations = e.GetMinRequiredOutgoingConfirmations()
+	}
+
+	hash, err := getConfirmedTxHash(ethTxID, s.GetRawDB(), minRequiredOutgoingConfirmations)
+
+	if err != nil {
+		logger.Error(err)
+		return models.NewRunOutputError(err)
+	}
+
+	if hash == nil {
+		return models.NewRunOutputPendingOutgoingConfirmationsWithData(input.Data())
+	}
+
+	hexHash := (*hash).Hex()
+
+	output := input.Data()
+	output, err = output.MultiAdd(models.KV{
+		"result": hexHash,
+		// HACK: latestOutgoingTxHash is used for backwards compatibility with the stats pusher
+		"latestOutgoingTxHash": hexHash,
+	})
+	if err != nil {
+		err = errors.Wrap(err, "checkEthTxForReceipt failed")
+		logger.Error(err)
+		return models.NewRunOutputError(err)
+	}
+	return models.NewRunOutputComplete(output)
+}
+
+func getConfirmedTxHash(ethTxID int64, db *gorm.DB, minRequiredOutgoingConfirmations uint64) (*gethCommon.Hash, error) {
+	receipt := models.EthReceipt{}
+	err := db.
+		Joins("INNER JOIN eth_tx_attempts ON eth_tx_attempts.hash = eth_receipts.tx_hash AND eth_tx_attempts.eth_tx_id = ?", ethTxID).
+		Joins("INNER JOIN eth_txes ON eth_txes.id = eth_tx_attempts.eth_tx_id AND eth_txes.state = 'confirmed'").
+		Where("eth_receipts.block_number <= (SELECT max(number) - ? FROM heads)", minRequiredOutgoingConfirmations).
+		First(&receipt).
+		Error
+
+	if err == nil {
+		return &receipt.TxHash, nil
+	}
+
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, nil
+	}
+
+	return nil, errors.Wrap(err, "getConfirmedTxHash failed")
+
+}
