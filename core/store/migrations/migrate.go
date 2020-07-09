7a1087c212b9455f4383fcaeed6a059bc73c09c9
Sam <samsondav@protonmail.com>
2020-07-09 14:34:21 +0100

    Enable bulletproof tx manager for integration tests

--- core/store/migrations/migrate.go
+++ core/store/migrations/migrate.go
@@ -58,6 +58,7 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1591141873"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1591603775"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1592355365"
+	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1594306515"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1594393769"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1594642891"
 
@@ -296,6 +297,10 @@ func init() {
 		{
 			ID:      "1594642891",
 			Migrate: migration1594642891.Migrate,
+		},
+		{
+			ID:      "1594306515",
+			Migrate: migration1594306515.Migrate,
 		},
 	}
 }
