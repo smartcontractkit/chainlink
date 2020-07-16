3989b1e91336ef39ef2dc90f5bf9e18ffd921e21
Steve Ellis <email@steveell.is>
2020-07-16 13:15:28 -0400

    add documentation for DeviationFlaggingValidator

--- evm-contracts/src/v0.6/dev/FluxAggregator.sol
+++ evm-contracts/src/v0.6/dev/FluxAggregator.sol
@@ -12,7 +12,7 @@ import "../vendor/SafeMath.sol";
 
 /**
  * @title The Prepaid Aggregator contract
- * @notice Node handles aggregating data pushed in from off-chain, and unlocks
+ * @notice Handles aggregating data pushed in from off-chain, and unlocks
  * payment for oracles as they report. Oracles' submissions are gathered in
  * rounds, with each round aggregating the submissions for each oracle into a
  * single answer. The latest aggregated answer is exposed as well as historical
@@ -143,7 +143,6 @@ contract FluxAggregator is AggregatorV3Interface, Owned {
 
   /**
    * @notice set up the aggregator with initial configuration
-   * @dev Sets the LinkToken address and amount of LINK paid
    * @param _link The address of the LINK token
    * @param _paymentAmount The amount paid of LINK paid to each oracle per submission, in wei (units of 10⁻¹⁸ LINK)
    * @param _timeout is the number of seconds after the previous round that are
