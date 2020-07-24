b0b9094f94c92e6704da2cde6bc5bc905de955ea
spooktheducks <spooktheducks@protonmail.com>
2020-07-23 19:56:07 -0500

    Regenerate mocks

--- core/internal/mocks/client.go
+++ core/internal/mocks/client.go
@@ -168,27 +168,6 @@ func (_m *Client) FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]typ
 	return r0, r1
 }
 
-// GetBlockHeight provides a mock function with given fields:
-func (_m *Client) GetBlockHeight() (uint64, error) {
-	ret := _m.Called()
-
-	var r0 uint64
-	if rf, ok := ret.Get(0).(func() uint64); ok {
-		r0 = rf()
-	} else {
-		r0 = ret.Get(0).(uint64)
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func() error); ok {
-		r1 = rf()
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetERC20Balance provides a mock function with given fields: address, contractAddress
 func (_m *Client) GetERC20Balance(address common.Address, contractAddress common.Address) (*big.Int, error) {
 	ret := _m.Called(address, contractAddress)
