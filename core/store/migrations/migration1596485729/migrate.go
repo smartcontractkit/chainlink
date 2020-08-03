827ae4a49e132ce1e73247616c3221a1ee453330
Ryan Hall <hall.ryan.r@gmail.com>
2020-08-03 17:08:45 -0600

    add foreign key constraint to migration

--- core/store/migrations/migration1596485729/migrate.go
+++ core/store/migrations/migration1596485729/migrate.go
@@ -7,6 +7,6 @@ import (
 // Migrate adds a job_run_id column to flux_monitor_round_stats
 func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
-		ALTER TABLE flux_monitor_round_stats ADD COLUMN "job_run_id" uuid;
+		ALTER TABLE flux_monitor_round_stats ADD COLUMN "job_run_id" uuid REFERENCES job_runs(id) ON DELETE CASCADE NOT NULL;
 	`).Error
 }
