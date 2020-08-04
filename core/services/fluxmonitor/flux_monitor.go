65db7a2ac8fb71a17954787cf2b2cb2f131fb284
Ryan Hall <hall.ryan.r@gmail.com>
2020-08-04 14:08:28 -0600

    make flux monitor respond to NewRoundLogs when it's previous submission was reorged

--- core/services/fluxmonitor/flux_monitor.go
+++ core/services/fluxmonitor/flux_monitor.go
@@ -676,21 +676,21 @@ func (p *PollingDeviationChecker) respondToNewRoundLog(log contracts.LogNewRound
 		// This indicates either that:
 		//     - We tried to start a round at the same time as another node, and their transaction was mined first, or
 		//     - The chain experienced a shallow reorg that unstarted the current round.
-
-		logger.Debugw("Ignoring new round request: started round simultaneously with another node", p.loggerFieldsForNewRound(log)...)
-		return
-
-		// jobRun, err := p.store.FindJobRun(roundStats.JobRunID)
-		// if err != nil {
-		// 	logger.Errorw(fmt.Sprintf("error finding JobRun associated with FMRoundStat: %v", err), p.loggerFieldsForNewRound(log)...)
-		// 	return
-		// }
-		// if jobRun.Status != models.RunStatusCompleted && jobRun.Status != models.RunStatusErrored {
-		// 	// If our previous attempt is still pending, don't re-submit
-		// 	// If our previous attempt is already over (completed or errored), we should retry
-		// 	logger.Debugw("Ignoring new round request: started round simultaneously with another node", p.loggerFieldsForNewRound(log)...)
-		// 	return
-		// }
+		jobRun, err := p.store.FindJobRun(roundStats.JobRunID)
+		if err != nil {
+			logger.Errorw(fmt.Sprintf("error finding JobRun associated with FMRoundStat: %v", err), p.loggerFieldsForNewRound(log)...)
+			logger.Warn(`
+				The preceding error may occur for old FMRoundStat records that don't have an associated JobRun.
+				If so, that error can likely be ignored.
+			`) // This warning can be removed in a future release
+			return
+		}
+		if jobRun.Status != models.RunStatusCompleted && jobRun.Status != models.RunStatusErrored {
+			// If our previous attempt is still pending, return early and don't re-submit
+			// If our previous attempt is already over (completed or errored), we should retry
+			logger.Debugw("Ignoring new round request: started round simultaneously with another node", p.loggerFieldsForNewRound(log)...)
+			return
+		}
 	}
 
 	// Ignore rounds we started
