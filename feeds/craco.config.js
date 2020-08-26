51c315822207aed5d46ee180a7a0b51787cbf4a2
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-08-26 11:05:00 -0700

    Add craco & build compressed assets in FeedsUI

---
+++ feeds/craco.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  webpack: {
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new CompressionPlugin({
+        test: /\.js$|\.css$|\.html$|\.svg/,
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        threshold: 0,
+        minRatio: 0.8,
+      }),
+      new BrotliPlugin({
+        test: /\.(js|css|html|svg)$/,
+        asset: '[path].br[query]',
+        threshold: 0,
+        minRatio: 0.8,
+      }),
+    ],
+  },
+  eslint: {
+    enable: false
+  },
+}
