b3c2364758ea95504de5e00d8a1101d47f5f9ded
Danny Skubak <skubakdj@gmail.com>
2020-08-28 14:11:47 -0400

    remove feeds

--- feeds/src/components/aggregatorVis/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AggregatorVis } from './AggregatorVis'
-export { default as FluxAggregatorVis } from './FluxAggregatorVis'
--- feeds/src/utils/uIntFrom.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Parse a string into a finite, unsigned integer. Unsafe for integers greater than 32 bits of precision.
- *
- * @param str The string to parse into a finite unsigned integer.
- */
-export function uIntFrom(val: string | number): number {
-  return /^[+]?(\d+)$/.test(val.toString()) ? Number(val) : NaN
-}
