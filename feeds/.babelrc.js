b710df4c87716ef59c91fb25076bfecdd2faae77
Danny Skubak <skubakdj@gmail.com>
2020-08-28 14:11:47 -0400

    remove feeds

--- feeds/.babelrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['react-app', '@babel/preset-env', '@babel/preset-typescript'],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-class-properties',
-  ],
-}
