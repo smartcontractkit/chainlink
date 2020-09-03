66c8b8360db91c7a6ebe1c8f1b813913a1e37ca6
Danny Skubak <skubakdj@gmail.com>
2020-09-03 17:46:56 -0400

    have jest transform published packages in operator_ui

--- operator_ui/jest.config.js
+++ operator_ui/jest.config.js
@@ -13,6 +13,9 @@ module.exports = {
     '<rootDir>/node_modules/',
     '<rootDir>/__tests__/.eslintrc.js',
   ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@chainlink/json-api-client|@chainlink/local-storage|@chainlink/styleguide|@chainlink/redux)/)',
+  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
