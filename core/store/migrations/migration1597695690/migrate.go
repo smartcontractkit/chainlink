828eae06c05db13fc4b7e92dc8a8c756ef3d95bf
Sam <samsondav@protonmail.com>
2020-09-09 11:20:02 +0200

    Finalised schema

--- core/store/migrations/migration1597695690/migrate.go
+++ core/store/migrations/migration1597695690/migrate.go
@@ -6,134 +6,99 @@ import (
 
 // Migrate creates the offchain_reporting_job_specs table
 func Migrate(tx *gorm.DB) error {
-	// JID                *models.ID         `toml:"jobID"              gorm:"not null;column:job_id"`
-	// ContractAddress    common.Address     `toml:"contractAddress"    gorm:"not null"`
-	// P2PNodeID          string             `toml:"p2pNodeID"          gorm:"not null"`
-	// P2PBootstrapNodes  []P2PBootstrapNode `toml:"p2pBootstrapNodes"  gorm:"not null;type:jsonb"`
-	// KeyBundle          string             `toml:"keyBundle"          gorm:"not null"`
-	// MonitoringEndpoint string             `toml:"monitoringEndpoint" gorm:"not null"`
-	// NodeAddress        common.Address     `toml:"nodeAddress"        gorm:"not null"`
-	// ObservationTimeout time.Duration      `toml:"observationTimeout" gorm:"not null"`
-	// ObservationSource  pipeline.TaskDAG   `toml:"observationSource"  gorm:"-"`
-	// LogLevel           ocrtypes.LogLevel  `toml:"logLevel,omitempty"`
 	return tx.Exec(`
 	 	CREATE TABLE offchainreporting_key_bundles (
-	 		-- NOTE: Key bundle ID is intended to be set by software as keccak256 hash of {onchain sig pubkey, offchain sig pubkey, config decryption pubkey}
+	 		-- NOTE: Key bundle ID is intended to be set by software as sha256 hash of {onchain sig pubkey, offchain sig pubkey, config decryption pubkey}
 	 		id bytea NOT NULL PRIMARY KEY,
 	 		CONSTRAINT chk_id_length CHECK (octet_length(id) = 32),
 	 		encrypted_priv_key_bundle jsonb NOT NULL,
 	 		created_at timestamptz NOT NULL,
 	 	);
 
+		CREATE INDEX idx_offchainreporting_oracles_unique_key_bundles_created_at ON offchainreporting_key_bundles USING BRIN (created_at);
+
 	 	CREATE TABLE offchainreporting_oracles (
 	 		id BIGSERIAL PRIMARY KEY,
 	 		contract_address bytea NOT NULL,
 	 		CONSTRAINT chk_contract_address_length CHECK (octet_length(contract_address) = 20),
 	 		p2p_peer_id text NOT NULL REFERENCES encrypted_p2p_keys (peer_id),
-	 		p2p_bootstrap_peers jsonb NOT NULL, -- NOTE: Needs revisiting
+	 		p2p_bootstrap_peers jsonb NOT NULL,
 	 		key_bundle_id bytea NOT NULL REFERENCES offchainreporting_key_bundles (id),
 	 		monitoring_endpoint TEXT,
 	 		transmitter_address bytea NOT NULL REFERENCES keys (address),
 	 		observation_timeout interval NOT NULL,
-			-- TODO: Revisit
-	 		-- observation_source_id bigint NOT NULL REFERENCES observation_aggregators(id),
-	 		created_at timestamptz NOT NULL
+			data_fetch_pipeline_spec_id BIGINT NOT NULL REFERENCES pipeline_specs (id),
+	 		created_at timestamptz NOT NULL,
+			updated_at timestamptz NOT NULL
 	 	);
 
 		CREATE UNIQUE INDEX idx_offchainreporting_oracles_unique_key_bundles ON offchainreporting_oracles (key_bundle_id, contract_address);
 		CREATE UNIQUE INDEX idx_offchainreporting_oracles_unique_peer_ids ON offchainreporting_oracles (p2p_peer_id, contract_address);
+		CREATE INDEX idx_offchainreporting_oracles_data_fetch_pipeline_spec_id ON offchainreporting_oracles (data_fetch_pipeline_spec_id);
+		
+		CREATE INDEX idx_offchainreporting_oracles_created_at ON offchainreporting_oracles USING BRIN (created_at);
+		CREATE INDEX idx_offchainreporting_oracles_updated_at ON offchainreporting_oracles USING BRIN (updated_at);
 
-		CREATE TABLE offchainreporting_oracles_pipeline_specs (
-			offchainreporting_oracle_id BIGINT REFERENCES offchainreporting_oracles (id) NOT NULL,
-			pipeline_spec_id BIGINT REFERENCES pipeline_specs (id) NOT NULL,
-			PRIMARY KEY(offchainreporting_oracle_id, pipeline_spec_id)
+		CREATE TABLE pipeline_specs (
+			-- id is intended to be a sha256 hash of the json representation of the DOT dag
+			id BYTEA PRIMARY KEY,
+			CONSTRAINT chk_id_length CHECK (octet_length(id) = 32),
+			source_dot_dag TEXT NOT NULL,
+			created_at timestamptz NOT NULL
 		);
 
-		CREATE INDEX idx_offchainreporting_oracles_pipeline_specs_pipeline_spec_id offchainreporting_oracles_pipeline_specs (pipeline_spec_id);
+		CREATE INDEX idx_pipeline_specs_created_at ON pipeline_specs USING BRIN (created_at);
 
+		CREATE TABLE pipeline_task_specs (
+			id BIGSERIAL PRIMARY KEY,
+			pipeline_spec_id BIGINT NOT NULL REFERENCES pipeline_specs (id),
+
+			task_spec jsonb NOT NULL,
+
+			created_at timestamptz NOT NULL,
+		);
+
+		CREATE INDEX idx_pipeline_task_specs_created_at ON pipeline_task_specs USING BRIN (created_at);
+
+		CREATE TABLE pipeline_task_spec_edges (
+			predecessor_id BIGINT NOT NULL REFERENCES pipeline_task_specs (id),
+			successor_id BIGINT NOT NULL REFERENCES pipeline_task_specs (id),
+			PRIMARY KEY(successor_id, predecessor_id)
+		);
+
+		-- This index is a little confusing, but the result is to only allow one successor (child) for any single predecessor (parent)
+		CREATE UNIQUE INDEX idx_pipeline_task_run_edges_only_one_successor_per_predecessor pipeline_task_spec_edges (predecessor_id);
 
 		CREATE TABLE pipeline_runs (
 			id BIGSERIAL PRIMARY KEY,
-			pipeline_spec_id BIGINT NOT NULL REFERENCES pipeline_specs (id)
+			pipeline_spec_id BIGINT NOT NULL REFERENCES pipeline_specs (id),
+			created_at timestamptz NOT NULL
+			-- NOTE: Could denormalize here with finished_at/output/error of last task_run if that proves necessary for performance
 		);
+
+		CREATE INDEX idx_pipeline_runs_pipeline_spec_id ON pipeline_runs (pipeline_spec_id);
+		CREATE INDEX idx_pipeline_runs_created_at ON pipeline_runs USING BRIN (created_at);
 
 		CREATE TABLE pipeline_task_runs (
 			id BIGSERIAL PRIMARY KEY,
 			pipeline_run_id BIGINT NOT NULL REFERENCES pipeline_runs (id),
 			output JSONB,
-			error TEXT, -- TODO: Actually should probabl reference errors instead?
-			pipeline_task_id BIGINT NOT NULL REFERENCES pipeline_tasks (id),
-			-- TODO: These columns
-			-- started_at timestamptz,
-			-- finished_at timestamptz,
+			error TEXT, 
+			pipeline_task_spec_id BIGINT NOT NULL REFERENCES pipeline_task_specs (id),
+			created_at timestamptz NOT NULL,
+			finished_at timestamptz,
 			CONSTRAINT chk_pipeline_task_run_fsm CHECK (
-				error IS NULL AND output IS NULL
+				error IS NULL AND output IS NULL AND finished_at IS NULL
 				OR
-				error IS NULL AND output IS NOT NULL
+				error IS NULL AND output IS NOT NULL AND finished_at IS NOT NULL
 				OR
-				output IS NULL AND error IS NOT NULL
+				output IS NULL AND error IS NOT NULL AND finished_at IS NOT NULL
 			)
 		);
 
-		-- TODO: indexes for pipeline_task_runs
-
-		CREATE TABLE pipeline_task_run_edges (
-			parent_id BIGINT NOT NULL REFERENCES pipeline_task_runs (id),
-			child_id BIGINT NOT NULL REFERENCES pipeline_task_runs (id),
-			PRIMARY KEY(child_id, parent_id)
-		);
-
-		CREATE INDEX idx_pipeline_task_run_edges ON pipeline_task_run_edges (parent_id);
-
-		CREATE TABLE pipeline_tasks (
-			id BIGSERIAL PRIMARY KEY,
-
-			task jsonb NOT NULL,
-
-			created_at timestamptz NOT NULL,
-			updated_at timestamptz NOT NULL
-		);
-
-		-- XXX: Do we use task edges or represent it as a serialized DAG?
-		CREATE TABLE pipeline_task_edges (
-			parent_id BIGINT NOT NULL REFERENCES pipeline_tasks (id),
-			child_id BIGINT NOT NULL REFERENCES pipeline_tasks (id),
-			PRIMARY KEY(child_id, parent_id)
-		);
+		CREATE INDEX idx_pipeline_task_runs ON pipeline_task_tuns USING BRIN (created_at);
+		-- TODO: more indexes for pipeline_task_runs (dependent on queries in the pipeline runner)
 	`).Error
-
-	// 	CREATE TYPE observation_aggregator_type AS ENUM ('median');
-
-	// 	CREATE TABLE observation_aggregators (
-	// 		id BIGSERIAL PRIMARY KEY,
-	// 		type observation_aggregator_type NOT NULL,
-	// 		created_at timestamptz NOT NULL,
-	// 	);
-
-	// 	CREATE TYPE observation_source_type AS ENUM ('http', 'bridge');
-
-	// 	CREATE TABLE observation_sources (
-	// 		id BIGSERIAL PRIMARY KEY,
-	// 		name TEXT,
-	// 		type observation_source_type NOT NULL,
-	// 		params jsonb NOT NULL,
-	// 		created_at timestamptz NOT NULL,
-	// 		updated_at timestamptz NOT NULL,
-	// 	);
-
-	// 	CREATE UNIQUE INDEX idx_unique_observation_sources ON observation_sources (params);
-	// 	CREATE UNIQUE INDEX idx_unique_observation_source_names ON observation_sources (name);
-
-	// 	CREATE TABLE observation_aggregator_sources (
-	// 		observation_aggregator_id BIGINT NOT NULL REFERENCES observation_aggregators (id),
-	// 		observation_source_id BIGINT NOT NULL REFERENCES observation_sources (id),
-	// 		PRIMARY KEY(observation_aggregator_id, observation_source_id),
-	// 		created_at timestamptz NOT NULL
-	// 	);
-
-	// 	CREATE INDEX idx_observation_aggregator_sources_observation_source_id ON observation_aggregator_sources (observation_source_id);
-
-	// `).Error
 
 	// err = tx.Exec(`
 	//        CREATE TABLE offchain_reporting_persistent_states (
