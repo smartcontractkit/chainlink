2d97f8c0b1ec5413b911f526f21ca9135cd94709
Ryan Hall <hall.ryan.r@gmail.com>
2020-09-29 16:04:49 -0600

    refactor getABI() into commmon.go file

---
+++ core/services/eth/contracts/common.go
@@ -0,0 +1,15 @@
+package contracts
+
+import (
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+func getABI(json string) abi.ABI {
+	abi, err := abi.JSON(strings.NewReader(json))
+	if err != nil {
+		panic("could not parse OffchainAggregator ABI: " + err.Error())
+	}
+	return abi
+}
