b92570f9b82c0347f81853b0653a1a47193963cb
Sam <samsondav@protonmail.com>
2020-10-07 15:27:53 +0200

    Prevent duplicate use of EthConfirmer advisory lock

--- core/utils/db.go
+++ core/utils/db.go
@@ -111,6 +111,7 @@ func (p *PostgresEventListener) Events() <-chan string {
 const (
 	AdvisoryLockClassID_EthBroadcaster int32 = 0
 	AdvisoryLockClassID_JobSpawner     int32 = 1
+	AdvisoryLockClassID_EthConfirmer   int32 = 2
 )
 
 var (
