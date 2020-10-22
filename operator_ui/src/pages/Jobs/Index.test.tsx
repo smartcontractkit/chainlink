a43030b5ff282d16e284c5c15569730ad2cfa5a3
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2020-10-22 19:19:03 +0100

    Remove pagination from the "/jobs" page

--- operator_ui/src/pages/Jobs/Index.test.tsx
+++ operator_ui/src/pages/Jobs/Index.test.tsx
@@ -1,34 +1,11 @@
 /* eslint-env jest */
-import createStore from 'createStore'
 import { JobsIndex as Index } from 'pages/Jobs/Index'
-import { mount } from 'enzyme'
 import jsonApiJobSpecsFactory from 'factories/jsonApiJobSpecs'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
-import { Provider } from 'react-redux'
-import { MemoryRouter, Route } from 'react-router-dom'
-import clickNextPage from 'test-helpers/clickNextPage'
-import clickPreviousPage from 'test-helpers/clickPreviousPage'
 import syncFetch from 'test-helpers/syncFetch'
 import globPath from 'test-helpers/globPath'
-
-const mountIndex = (opts: { pageSize?: number } = {}) =>
-  mount(
-    <Provider store={createStore()}>
-      <MemoryRouter>
-        <Route
-          path="/"
-          exact
-          render={(props) => <Index {...props} pageSize={opts.pageSize} />}
-        />
-        <Route
-          path="/jobs/page/:pageNumber"
-          exact
-          render={(props) => <Index {...props} pageSize={opts.pageSize} />}
-        />
-      </MemoryRouter>
-    </Provider>,
-  )
+import { mountWithProviders } from 'test-helpers/mountWithTheme'
 
 describe('pages/Jobs/Index', () => {
   it('renders the list of jobs', async () => {
@@ -41,7 +18,7 @@ describe('pages/Jobs/Index', () => {
     ])
     global.fetch.getOnce(globPath('/v2/specs'), jobSpecsResponse)
 
-    const wrapper = mountIndex()
+    const wrapper = mountWithProviders(<Index />)
 
     await act(async () => {
       await syncFetch(wrapper)
@@ -49,45 +26,5 @@ describe('pages/Jobs/Index', () => {
     expect(wrapper.text()).toContain('c60b9927eeae43168ddbe92584937b1b')
     expect(wrapper.text()).toContain('web')
     expect(wrapper.text()).toContain('just now')
-  })
-
-  it('can page through the list of jobs', async () => {
-    const pageOneResponse = jsonApiJobSpecsFactory(
-      [{ id: 'ID-ON-FIRST-PAGE' }],
-      2,
-    )
-    global.fetch.getOnce(globPath('/v2/specs'), pageOneResponse)
-
-    const wrapper = mountIndex({ pageSize: 1 })
-
-    await act(async () => {
-      await syncFetch(wrapper)
-    })
-    expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
-    expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
-
-    const pageTwoResponse = jsonApiJobSpecsFactory(
-      [{ id: 'ID-ON-SECOND-PAGE' }],
-      2,
-    )
-    global.fetch.getOnce(globPath('/v2/specs'), pageTwoResponse)
-    clickNextPage(wrapper)
-
-    await act(async () => {
-      await syncFetch(wrapper)
-    })
-    wrapper.update()
-
-    expect(wrapper.text()).not.toContain('ID-ON-FIRST-PAGE')
-    expect(wrapper.text()).toContain('ID-ON-SECOND-PAGE')
-
-    global.fetch.getOnce(globPath('/v2/specs'), pageOneResponse)
-    clickPreviousPage(wrapper)
-
-    await act(async () => {
-      await syncFetch(wrapper)
-    })
-    expect(wrapper.text()).toContain('ID-ON-FIRST-PAGE')
-    expect(wrapper.text()).not.toContain('ID-ON-SECOND-PAGE')
   })
 })
