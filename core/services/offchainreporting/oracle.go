a487fe8029e59146485d9bdb93f62ba2b671f6e3
Sam <samsondav@protonmail.com>
2020-11-04 16:51:29 +0100

    Add P2PAccountIP and P2PAnnouncePort to override autodetected values
    When CL is running in some environments e.g. Docker, it announces an
    incorrect IP/port since these are autodetected from the local network
    adapter, which may be running inside a private network.
    
    Two new environment variables are introduced to allow node operators to
    override this manually:
    
    P2P_ANNOUNCE_IP
    P2P_ANNOUNCE_PORT
    
    In addition, OCR_LISTEN_PORT and OCR_LISTEN_IP have been renamed to
    P2P_XXX for consistency.

--- core/services/offchainreporting/oracle.go
+++ core/services/offchainreporting/oracle.go
@@ -117,12 +117,19 @@ func (d jobSpawnerDelegate) ServicesForSpec(spec job.Spec) ([]job.Service, error
 
 	ocrLogger := NewLogger(logger.Default, d.config.OCRTraceLogging())
 
+	listenPort := d.config.P2PListenPort()
+	if listenPort == 0 {
+		return nil, errors.New("failed to instantiate oracle or bootstrapper service, OCR_LISTEN_PORT is required and must be set to a non-zero value")
+	}
+
 	peer, err := ocrnetworking.NewPeer(ocrnetworking.PeerConfig{
-		PrivKey:    p2pkey.PrivKey,
-		ListenPort: d.config.OCRListenPort(),
-		ListenIP:   d.config.OCRListenIP(),
-		Logger:     ocrLogger,
-		Peerstore:  peerstore,
+		PrivKey:      p2pkey.PrivKey,
+		ListenIP:     d.config.P2PListenIP(),
+		ListenPort:   listenPort,
+		AnnounceIP:   d.config.P2PAnnounceIP(),
+		AnnouncePort: d.config.P2PAnnouncePort(),
+		Logger:       ocrLogger,
+		Peerstore:    peerstore,
 		EndpointConfig: ocrnetworking.EndpointConfig{
 			IncomingMessageBufferSize: d.config.OCRIncomingMessageBufferSize(),
 			OutgoingMessageBufferSize: d.config.OCROutgoingMessageBufferSize(),
