dc29798af1ad37b6bfe4d1465dd1f42841546102
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2020-11-09 10:03:56 +0000

    Added "Errors" and "Outputs" to the `pipeline_job_runs` table

--- core/store/migrations/migration1604674426/migrate.go
+++ core/store/migrations/migration1604674426/migrate.go
@@ -1,4 +1,4 @@
-package migration1604003825
+package migration1604674426
 
 import "github.com/jinzhu/gorm"
 
@@ -7,7 +7,7 @@ func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
 		ALTER TABLE pipeline_runs
 		ADD COLUMN errors jsonb,
-		ADD COLUMN result jsonb,
-		ADD CHECK ((result is null and errors is null and finished_at is null) or (result is not null and errors is not null and finished_at is not null));
+		ADD COLUMN outputs jsonb,
+		ADD CHECK ((outputs is null and errors is null and finished_at is null) or (outputs is not null and errors is not null and finished_at is not null));
     `).Error
 }
