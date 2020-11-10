bd2b0280eb03773f1c3b35fa0f97469a20cce515
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2020-11-10 11:21:20 +0000

    ♻ Separate OCR job runs controller from specs controller (#3603)

--- core/web/ocr_job_specs_controller.go
+++ core/web/ocr_job_specs_controller.go
@@ -4,12 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 	"github.com/smartcontractkit/chainlink/core/services"
 	"github.com/smartcontractkit/chainlink/core/services/chainlink"
 	"github.com/smartcontractkit/chainlink/core/services/offchainreporting"
-	"github.com/smartcontractkit/chainlink/core/services/pipeline"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/store/orm"
 )
@@ -113,86 +111,4 @@ func (ocrjsc *OCRJobSpecsController) Delete(c *gin.Context) {
 	}
 
 	jsonAPIResponseWithStatus(c, nil, "offChainReportingJobSpec", http.StatusNoContent)
-}
-
-// Run triggers a pipeline run for an OCR job.
-// Example:
-// "POST <application>/ocr/specs/:ID/runs"
-func (ocrjsc *OCRJobSpecsController) Run(c *gin.Context) {
-	jobSpec := models.JobSpecV2{}
-	err := jobSpec.SetID(c.Param("ID"))
-	if err != nil {
-		jsonAPIError(c, http.StatusUnprocessableEntity, err)
-		return
-	}
-
-	jobRunID, err := ocrjsc.App.RunJobV2(c, jobSpec.ID, nil)
-
-	if err != nil {
-		jsonAPIError(c, http.StatusInternalServerError, err)
-		return
-	}
-
-	jsonAPIResponse(c, models.OCRJobRun{ID: jobRunID}, "offChainReportingJobRun")
-}
-
-// Runs returns all pipeline runs for an OCR job.
-// Example:
-// "GET <application>/ocr/specs/:ID/runs"
-func (ocrjsc *OCRJobSpecsController) Runs(c *gin.Context) {
-	jobSpec := models.JobSpecV2{}
-	err := jobSpec.SetID(c.Param("ID"))
-	if err != nil {
-		jsonAPIError(c, http.StatusUnprocessableEntity, err)
-		return
-	}
-
-	var pipelineRuns []pipeline.Run
-	err = preloadPipelineRunDependencies(ocrjsc.App.GetStore().DB).
-		Joins("INNER JOIN jobs ON pipeline_runs.pipeline_spec_id = jobs.pipeline_spec_id").
-		Where("jobs.offchainreporting_oracle_spec_id IS NOT NULL").
-		Where("jobs.id = ?", jobSpec.ID).
-		Order("created_at ASC, id ASC").
-		Find(&pipelineRuns).Error
-
-	if err != nil {
-		jsonAPIError(c, http.StatusInternalServerError, err)
-		return
-	}
-
-	jsonAPIResponse(c, pipelineRuns, "offChainReportingJobRun")
-}
-
-// ShowRun returns a specified pipeline run.
-// Example:
-// "GET <application>/ocr/specs/:ID/runs/:runID"
-func (ocrjsc *OCRJobSpecsController) ShowRun(c *gin.Context) {
-	pipelineRun := pipeline.Run{}
-	err := pipelineRun.SetID(c.Param("runID"))
-	if err != nil {
-		jsonAPIError(c, http.StatusUnprocessableEntity, err)
-		return
-	}
-
-	err = preloadPipelineRunDependencies(ocrjsc.App.GetStore().DB).
-		Where("pipeline_runs.id = ?", pipelineRun.ID).
-		First(&pipelineRun).Error
-
-	if err != nil {
-		jsonAPIError(c, http.StatusInternalServerError, err)
-		return
-	}
-
-	jsonAPIResponse(c, pipelineRun, "offChainReportingJobRun")
-}
-
-func preloadPipelineRunDependencies(db *gorm.DB) *gorm.DB {
-	return db.
-		Preload("PipelineSpec").
-		Preload("PipelineTaskRuns", func(db *gorm.DB) *gorm.DB {
-			return db.
-				Where(`pipeline_task_runs.type != 'result'`).
-				Order("created_at ASC, id ASC")
-		}).
-		Preload("PipelineTaskRuns.PipelineTaskSpec")
 }
