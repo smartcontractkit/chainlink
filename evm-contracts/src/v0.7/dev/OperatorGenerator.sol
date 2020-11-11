52ef9e3470f3d446daf59d7e21fc55d15a6dd965
Alex Roan <alex.roan@hotmail.com>
2020-11-11 14:15:12 +0000

    Added comments

--- evm-contracts/src/v0.7/dev/OperatorGenerator.sol
+++ evm-contracts/src/v0.7/dev/OperatorGenerator.sol
@@ -1,17 +1,28 @@
-pragma solidity 0.7.0;
+pragma solidity ^0.7.0;
 
 import "./Operator.sol";
 
+/**
+ * @title Operator Generator
+ * @notice Generates Operator contracts for node operators
+ */
 contract OperatorGenerator {
 
     address public link;
 
     event OperatorCreated(address indexed operator, address indexed owner);
 
+    /**
+     * @param linkAddress address
+     */
     constructor(address linkAddress) public {
         link = linkAddress;
     }
 
+    /**
+     * @notice Create a new Operator contract with the msg.sender as owner
+     * @return operator address
+     */
     function createOperator() external returns (address operatorAddress){
         Operator operator = new Operator(link, msg.sender);
         operatorAddress = address(operator);
