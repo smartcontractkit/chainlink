3c1eeb8fd07b5c04c9ea1049f9288d5cf6f34534
Sam <samsondav@protonmail.com>
2020-11-17 12:54:37 +0100

    Randomise nonce in an attempt to avoid deadlocks

--- core/internal/cltest/cltest.go
+++ core/internal/cltest/cltest.go
@@ -1467,3 +1467,10 @@ func MustDefaultKey(t *testing.T, s *strpkg.Store) models.Key {
 	require.NoError(t, err)
 	return k
 }
+
+func RandomiseNonce(t *testing.T, s *strpkg.Store) {
+	t.Helper()
+	n := rand.Intn(32767) + 100
+	err := s.DB.Exec(`UPDATE keys SET next_nonce = ?`, n).Error
+	require.NoError(t, err)
+}
