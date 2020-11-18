94bb23f5026d574850c98848264db028e417274c
bwest981 <bwest@ip-10-6-1-1.us-west-2.compute.internal>
2020-11-17 19:46:43 -0500

    added docker-scripts folder which includes automation scripts for OCR testing

---
+++ tools/docker/docker-scripts/linker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+chainlink admin login -f apicredentials
+echo ""
+echo "🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠🧠"
+echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 NODE $3 🔥🔥🔥🔥🔥🔥🔥🔥🔥"
+echo "💪🏋️ 💪💪💪💪💪💪💪🏋️ 🏋️ 🏋️ 🏋️ 🏋️ 💪💪💪💪💪💪💪🏋 💪"
+
+chainlink keys p2p list
+chainlink keys ocr list
+chainlink jobs deletev2 $1
+chainlink jobs createocr $2
+
+
+
