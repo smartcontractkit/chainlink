224b8b47decbc8de196d5607887b75084dcaab68
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2020-11-20 15:26:20 +0000

    ✨ Add OCR runs page (#3670)

--- operator_ui/jest.config.js
+++ operator_ui/jest.config.js
@@ -21,7 +21,5 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    // handle declaration file having an enum value
-    'core/store/models': '<rootDir>/@types/core/store/models.d.ts',
   },
 }
---
+++ operator_ui/src/pages/Jobs/Runs.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { JobsShow } from 'pages/Jobs/Show'
+import { jsonApiOcrJobSpec } from 'factories/jsonApiOcrJobSpec'
+import { jsonApiOcrJobRuns } from 'factories/jsonApiOcrJobRuns'
+import { mountWithProviders } from 'test-helpers/mountWithTheme'
+import { syncFetch } from 'test-helpers/syncFetch'
+import globPath from 'test-helpers/globPath'
+
+const JOB_SPEC_ID = '1'
+
+describe('pages/Jobs/Runs', () => {
+  it('adds pagination info to the URL and renders job runs', async () => {
+    const runs = []
+    const RUNS_COUNT = 100
+
+    for (let runId = 100; runId >= 1; runId--) {
+      runs.push({ id: String(runId) })
+    }
+
+    global.fetch.getOnce(
+      globPath(`/v2/ocr/specs/${JOB_SPEC_ID}/runs?page=1&size=10`),
+      jsonApiOcrJobRuns(runs.slice(0, 10), RUNS_COUNT),
+    )
+    global.fetch.getOnce(
+      globPath(`/v2/ocr/specs/${JOB_SPEC_ID}`),
+      jsonApiOcrJobSpec({
+        id: JOB_SPEC_ID,
+      }),
+    )
+
+    const wrapper = mountWithProviders(
+      <Route path="/jobs/:jobSpecId" component={JobsShow} />,
+      {
+        initialEntries: [`/jobs/${JOB_SPEC_ID}/runs`],
+      },
+    )
+    await syncFetch(wrapper)
+
+    expect(wrapper.find('tr').length).toEqual(10)
+
+    const routerCopmonentProps: any = wrapper.find('Router').props()
+    expect(routerCopmonentProps?.history?.location?.search).toEqual(
+      '?page=1&size=10',
+    )
+  })
+})
