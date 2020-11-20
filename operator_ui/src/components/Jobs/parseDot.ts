570ff6c6a1b4a955b36169b5a6ea1226f5c87968
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2020-11-20 11:47:00 +0000

    Rename "node" to "id"

--- operator_ui/src/components/Jobs/parseDot.ts
+++ operator_ui/src/components/Jobs/parseDot.ts
@@ -15,11 +15,11 @@ export function parseDot(dot: string): Stratify[] {
   const digraph = graphlibDot.read(dot)
   const edges = digraph.edges()
 
-  return digraph.nodes().map((node: string) => ({
-    id: node,
+  return digraph.nodes().map((id: string) => ({
+    id,
     parentIds: edges
-      .filter((edge: Edge) => edge.w === node)
+      .filter((edge: Edge) => edge.w === id)
       .map((edge: Edge) => edge.v),
-    attributes: digraph.node(node),
+    attributes: digraph.node(id),
   }))
 }
