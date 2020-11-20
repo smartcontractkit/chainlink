11f8cc29a4f868ae24f1d335226b52d19391791b
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2020-11-20 12:02:25 +0000

    Splitting tests and making attributes optional

--- operator_ui/src/components/Jobs/parseDot.ts
+++ operator_ui/src/components/Jobs/parseDot.ts
@@ -3,7 +3,7 @@ import graphlibDot from 'graphlib-dot'
 export type Stratify = {
   id: string
   parentIds: string[]
-  attributes: { [key: string]: string }
+  attributes?: { [key: string]: string }
 }
 
 type Edge = {
@@ -15,11 +15,18 @@ export function parseDot(dot: string): Stratify[] {
   const digraph = graphlibDot.read(dot)
   const edges = digraph.edges()
 
-  return digraph.nodes().map((id: string) => ({
-    id,
-    parentIds: edges
-      .filter((edge: Edge) => edge.w === id)
-      .map((edge: Edge) => edge.v),
-    attributes: digraph.node(id),
-  }))
+  return digraph.nodes().map((id: string) => {
+    const nodeInformation: Stratify = {
+      id,
+      parentIds: edges
+        .filter((edge: Edge) => edge.w === id)
+        .map((edge: Edge) => edge.v),
+    }
+
+    if (Object.keys(digraph.node(id)).length > 0) {
+      nodeInformation.attributes = digraph.node(id)
+    }
+
+    return nodeInformation
+  })
 }
