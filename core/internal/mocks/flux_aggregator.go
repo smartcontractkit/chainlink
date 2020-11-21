6a8092f922069fe9ba92ea69a4604ad9a4490c90
Ryan Hall <hall.ryan.r@gmail.com>
2020-11-20 16:47:31 -0800

    make fluxMonitor use latest completed round when first setting timers

--- core/internal/mocks/flux_aggregator.go
+++ core/internal/mocks/flux_aggregator.go
@@ -125,6 +125,27 @@ func (_m *FluxAggregator) GetOracles() ([]common.Address, error) {
 	return r0, r1
 }
 
+// LatestRoundData provides a mock function with given fields:
+func (_m *FluxAggregator) LatestRoundData() (contracts.FluxAggregatorRoundData, error) {
+	ret := _m.Called()
+
+	var r0 contracts.FluxAggregatorRoundData
+	if rf, ok := ret.Get(0).(func() contracts.FluxAggregatorRoundData); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(contracts.FluxAggregatorRoundData)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // RoundState provides a mock function with given fields: oracle, roundID
 func (_m *FluxAggregator) RoundState(oracle common.Address, roundID uint32) (contracts.FluxAggregatorRoundState, error) {
 	ret := _m.Called(oracle, roundID)
