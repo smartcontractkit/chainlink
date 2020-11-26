8806f62371d5441a78d2a4747e7e96e457cf9e5a
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2020-11-26 12:09:28 +0000

    Remove redundant localization call which does not actually localize

--- operator_ui/src/pages/Jobs/Errors.tsx
+++ operator_ui/src/pages/Jobs/Errors.tsx
@@ -11,7 +11,7 @@ import {
 import { v2 } from 'api'
 import Button from 'components/Button'
 import Content from 'components/Content'
-import { localizedTimestamp, TimeAgo } from '@chainlink/styleguide'
+import { TimeAgo } from '@chainlink/styleguide'
 import { JobData } from './sharedTypes'
 
 export const JobsErrors: React.FC<{
@@ -84,16 +84,12 @@ export const JobsErrors: React.FC<{
                     </TableCell>
                     <TableCell>
                       <Typography variant="body1">
-                        <TimeAgo tooltip>
-                          {localizedTimestamp(jobSpecError.createdAt)}
-                        </TimeAgo>
+                        <TimeAgo tooltip>{jobSpecError.createdAt}</TimeAgo>
                       </Typography>
                     </TableCell>
                     <TableCell>
                       <Typography variant="body1">
-                        <TimeAgo tooltip>
-                          {localizedTimestamp(jobSpecError.updatedAt)}
-                        </TimeAgo>
+                        <TimeAgo tooltip>{jobSpecError.updatedAt}</TimeAgo>
                       </Typography>
                     </TableCell>
                     <TableCell>
