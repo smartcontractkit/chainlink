4f01781973d08ff4b2872f8085bfc442eb6653d0
Alex Roan <alex.roan@hotmail.com>
2020-12-01 18:10:28 +0000

    Added docblocks to OperatorProxy functions

--- evm-contracts/src/v0.7/dev/OperatorProxy.sol
+++ evm-contracts/src/v0.7/dev/OperatorProxy.sol
@@ -7,10 +7,21 @@ contract OperatorProxy is Owned {
 
   address internal immutable link;
 
+  /**
+   * @notice Deploy using the address of the LINK token
+   * @dev The msg.sender is set as the owner of this contract
+   * @param linkAddress Address of deployed LINK token
+   */
   constructor(address linkAddress) Owned(msg.sender) {
     link = linkAddress;
   }
 
+  /**
+   * @notice Forward a call on to another address, checking that the
+   * msg.sender is authorized to do so from the owner
+   * @param to Target address
+   * @param data Data to send to the target address
+   */
   function forward(address to, bytes calldata data) public
   {
     require(OperatorInterface(owner).isAuthorizedSender(msg.sender), "Not an authorized node");
