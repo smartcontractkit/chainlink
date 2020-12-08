fac25e06d1e95dbb172403feb96d341de7c9441e
Alex Roan <alex.roan@hotmail.com>
2020-12-08 18:57:46 +0000

    Store thresholds per aggregator

--- evm-contracts/src/v0.7/dev/StalenessFlaggingValidator.sol
+++ evm-contracts/src/v0.7/dev/StalenessFlaggingValidator.sol
@@ -10,7 +10,7 @@ contract StalenessFlaggingValidator is ConfirmedOwner {
   using SafeMathChainlink for uint256;
 
   FlagsInterface private s_flags;
-  uint256 private s_threshold;
+  mapping(address => uint256) s_thresholds;
 
   event FlagsAddressUpdated(
     address indexed previous,
@@ -21,12 +21,11 @@ contract StalenessFlaggingValidator is ConfirmedOwner {
     uint256 indexed current
   );
 
-  constructor(address flagsAddress, uint256 flaggingThreshold) 
+  constructor(address flagsAddress) 
     public 
     ConfirmedOwner(msg.sender)
   {
     setFlagsAddress(flagsAddress);
-    setThreshold(flaggingThreshold);
   }
 
   function update(address[] calldata aggregators) external {
@@ -51,11 +50,19 @@ contract StalenessFlaggingValidator is ConfirmedOwner {
     return false;
   }
 
-  function setThreshold(uint256 flaggingThreshold) public onlyOwner() {
-    uint256 previous = s_threshold;
-    if (previous != flaggingThreshold) {
-      s_threshold = flaggingThreshold;
-      emit FlaggingThresholdUpdated(previous, flaggingThreshold);
+  function setThresholds(address[] memory aggregators, uint256[] memory flaggingThresholds)
+    public 
+    onlyOwner()
+  {
+    require(aggregators.length == flaggingThresholds.length, "Different sized arrays");
+    for (uint256 i = 0; i < aggregators.length; i++) {
+      address aggregator = aggregators[i];
+      uint256 previousThreshold = s_thresholds[aggregator];
+      uint newThreshold = flaggingThresholds[i];
+      if (previousThreshold != newThreshold) {
+        s_thresholds[aggregator] = newThreshold;
+        emit FlaggingThresholdUpdated(previousThreshold, newThreshold);
+      }
     }
   }
 
@@ -74,8 +81,8 @@ contract StalenessFlaggingValidator is ConfirmedOwner {
     }
   }
 
-  function threshold() public view returns (uint256) {
-    return s_threshold;
+  function threshold(address aggregator) public view returns (uint256) {
+    return s_thresholds[aggregator];
   }
 
   function flags() public view returns (address) {
@@ -86,9 +93,12 @@ contract StalenessFlaggingValidator is ConfirmedOwner {
     private 
     returns (bool stale)
   {
+    if (s_thresholds[aggregator] == 0) {
+      return false;
+    }
     (,,,uint updatedAt,) = AggregatorV3Interface(aggregator).latestRoundData();
     uint256 diff = currentTimestamp.sub(updatedAt);
-    if (diff > s_threshold) {
+    if (diff > s_thresholds[aggregator]) {
       stale = true;
     }
   }
