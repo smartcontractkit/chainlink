40ef1754ff2370990e57947e39a5c32036f29821
John Barker <dev@j16r.net>
2020-12-09 15:49:50 -0700

    Use new vendored solidity library

--- evm-contracts/src/v0.7/vendor/CBORChainlink.sol
+++ evm-contracts/src/v0.7/vendor/CBORChainlink.sol
@@ -1,72 +1,89 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity >= 0.4.19 < 0.7.0;
 
-import { BufferChainlink } from "./BufferChainlink.sol";
+import "@ensdomains/buffer/contracts/Buffer.sol";
 
-library CBORChainlink {
-  using BufferChainlink for BufferChainlink.buffer;
+library CBOR {
+    using Buffer for Buffer.buffer;
 
-  uint8 private constant MAJOR_TYPE_INT = 0;
-  uint8 private constant MAJOR_TYPE_NEGATIVE_INT = 1;
-  uint8 private constant MAJOR_TYPE_BYTES = 2;
-  uint8 private constant MAJOR_TYPE_STRING = 3;
-  uint8 private constant MAJOR_TYPE_ARRAY = 4;
-  uint8 private constant MAJOR_TYPE_MAP = 5;
-  uint8 private constant MAJOR_TYPE_CONTENT_FREE = 7;
+    uint8 private constant MAJOR_TYPE_INT = 0;
+    uint8 private constant MAJOR_TYPE_NEGATIVE_INT = 1;
+    uint8 private constant MAJOR_TYPE_BYTES = 2;
+    uint8 private constant MAJOR_TYPE_STRING = 3;
+    uint8 private constant MAJOR_TYPE_ARRAY = 4;
+    uint8 private constant MAJOR_TYPE_MAP = 5;
+    uint8 private constant MAJOR_TYPE_TAG = 6;
+    uint8 private constant MAJOR_TYPE_CONTENT_FREE = 7;
 
-  function encodeType(BufferChainlink.buffer memory buf, uint8 major, uint value) private pure {
-    if(value <= 23) {
-      buf.appendUint8(uint8((major << 5) | value));
-    } else if(value <= 0xFF) {
-      buf.appendUint8(uint8((major << 5) | 24));
-      buf.appendInt(value, 1);
-    } else if(value <= 0xFFFF) {
-      buf.appendUint8(uint8((major << 5) | 25));
-      buf.appendInt(value, 2);
-    } else if(value <= 0xFFFFFFFF) {
-      buf.appendUint8(uint8((major << 5) | 26));
-      buf.appendInt(value, 4);
-    } else if(value <= 0xFFFFFFFFFFFFFFFF) {
-      buf.appendUint8(uint8((major << 5) | 27));
-      buf.appendInt(value, 8);
+    uint8 private constant TAG_TYPE_BIGNUM = 2;
+    uint8 private constant TAG_TYPE_NEGATIVE_BIGNUM = 3;
+
+    function encodeType(Buffer.buffer memory buf, uint8 major, uint value) private pure {
+        if(value <= 23) {
+            buf.appendUint8(uint8((major << 5) | value));
+        } else if(value <= 0xFF) {
+            buf.appendUint8(uint8((major << 5) | 24));
+            buf.appendInt(value, 1);
+        } else if(value <= 0xFFFF) {
+            buf.appendUint8(uint8((major << 5) | 25));
+            buf.appendInt(value, 2);
+        } else if(value <= 0xFFFFFFFF) {
+            buf.appendUint8(uint8((major << 5) | 26));
+            buf.appendInt(value, 4);
+        } else if(value <= 0xFFFFFFFFFFFFFFFF) {
+            buf.appendUint8(uint8((major << 5) | 27));
+            buf.appendInt(value, 8);
+        }
     }
-  }
 
-  function encodeIndefiniteLengthType(BufferChainlink.buffer memory buf, uint8 major) private pure {
-    buf.appendUint8(uint8((major << 5) | 31));
-  }
-
-  function encodeUInt(BufferChainlink.buffer memory buf, uint value) internal pure {
-    encodeType(buf, MAJOR_TYPE_INT, value);
-  }
-
-  function encodeInt(BufferChainlink.buffer memory buf, int value) internal pure {
-    if(value >= 0) {
-      encodeType(buf, MAJOR_TYPE_INT, uint(value));
-    } else {
-      encodeType(buf, MAJOR_TYPE_NEGATIVE_INT, uint(-1 - value));
+    function encodeIndefiniteLengthType(Buffer.buffer memory buf, uint8 major) private pure {
+        buf.appendUint8(uint8((major << 5) | 31));
     }
-  }
 
-  function encodeBytes(BufferChainlink.buffer memory buf, bytes memory value) internal pure {
-    encodeType(buf, MAJOR_TYPE_BYTES, value.length);
-    buf.append(value);
-  }
+    function encodeUInt(Buffer.buffer memory buf, uint value) internal pure {
+        encodeType(buf, MAJOR_TYPE_INT, value);
+    }
 
-  function encodeString(BufferChainlink.buffer memory buf, string memory value) internal pure {
-    encodeType(buf, MAJOR_TYPE_STRING, bytes(value).length);
-    buf.append(bytes(value));
-  }
+    function encodeInt(Buffer.buffer memory buf, int value) internal pure {
+        if(value < -0x10000000000000000) {
+            encodeSignedBigNum(buf, value);
+        } else if(value > 0xFFFFFFFFFFFFFFFF) {
+            encodeBigNum(buf, value);
+        } else if(value >= 0) {
+            encodeType(buf, MAJOR_TYPE_INT, uint(value));
+        } else {
+            encodeType(buf, MAJOR_TYPE_NEGATIVE_INT, uint(-1 - value));
+        }
+    }
 
-  function startArray(BufferChainlink.buffer memory buf) internal pure {
-    encodeIndefiniteLengthType(buf, MAJOR_TYPE_ARRAY);
-  }
+    function encodeBytes(Buffer.buffer memory buf, bytes memory value) internal pure {
+        encodeType(buf, MAJOR_TYPE_BYTES, value.length);
+        buf.append(value);
+    }
 
-  function startMap(BufferChainlink.buffer memory buf) internal pure {
-    encodeIndefiniteLengthType(buf, MAJOR_TYPE_MAP);
-  }
+    function encodeBigNum(Buffer.buffer memory buf, int value) internal pure {
+      buf.appendUint8(uint8((MAJOR_TYPE_TAG << 5) | TAG_TYPE_BIGNUM));
+      encodeBytes(buf, abi.encode(uint(value)));
+    }
 
-  function endSequence(BufferChainlink.buffer memory buf) internal pure {
-    encodeIndefiniteLengthType(buf, MAJOR_TYPE_CONTENT_FREE);
-  }
+    function encodeSignedBigNum(Buffer.buffer memory buf, int input) internal pure {
+      buf.appendUint8(uint8((MAJOR_TYPE_TAG << 5) | TAG_TYPE_NEGATIVE_BIGNUM));
+      encodeBytes(buf, abi.encode(uint(-1 - input)));
+    }
+
+    function encodeString(Buffer.buffer memory buf, string memory value) internal pure {
+        encodeType(buf, MAJOR_TYPE_STRING, bytes(value).length);
+        buf.append(bytes(value));
+    }
+
+    function startArray(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_ARRAY);
+    }
+
+    function startMap(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_MAP);
+    }
+
+    function endSequence(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_CONTENT_FREE);
+    }
 }
