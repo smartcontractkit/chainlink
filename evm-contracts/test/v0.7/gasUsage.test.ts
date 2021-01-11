ff3ecb1fe3c0ee44713d41259e97d0384de99b42
Alex Roan <alex.roan@hotmail.com>
2021-01-11 14:48:17 +0000

    Linting

--- evm-contracts/test/v0.7/gasUsage.test.ts
+++ evm-contracts/test/v0.7/gasUsage.test.ts
@@ -35,20 +35,21 @@ describe('Operator Gas Tests', () => {
 
   const deployment = setup.snapshot(provider, async () => {
     link = await linkTokenFactory.connect(roles.defaultAccount).deploy()
-    
+
     operator1 = await operatorFactory
       .connect(roles.defaultAccount)
       .deploy(link.address, roles.defaultAccount.address)
     await operator1.setAuthorizedSender(roles.oracleNode.address, true)
-    
+
     operator2 = await operatorFactory
       .connect(roles.defaultAccount)
       .deploy(link.address, roles.defaultAccount.address)
     await operator2.setAuthorizedSender(roles.oracleNode.address, true)
-    
-    oracle1 = await oracleFactory.connect(roles.defaultAccount).deploy(link.address)
-    await oracle1.setFulfillmentPermission(roles.oracleNode.address, true)
 
+    oracle1 = await oracleFactory
+      .connect(roles.defaultAccount)
+      .deploy(link.address)
+    await oracle1.setFulfillmentPermission(roles.oracleNode.address, true)
   })
 
   beforeEach(async () => {
@@ -95,14 +96,10 @@ describe('Operator Gas Tests', () => {
     it('uses acceptable gas', async () => {
       const tx1 = await oracle1
         .connect(roles.oracleNode)
-        .fulfillOracleRequest(
-          ...oracle.convertFufillParams(request1, response),
-        )
+        .fulfillOracleRequest(...oracle.convertFufillParams(request1, response))
       const tx2 = await operator1
         .connect(roles.oracleNode)
-        .fulfillOracleRequest(
-          ...oracle.convertFufillParams(request2, response),
-        )
+        .fulfillOracleRequest(...oracle.convertFufillParams(request2, response))
       const receipt1 = await tx1.wait()
       const receipt2 = await tx2.wait()
       // 38014 vs 40260
@@ -151,23 +148,24 @@ describe('Operator Gas Tests', () => {
     it('uses acceptable gas', async () => {
       const tx1 = await operator1
         .connect(roles.oracleNode)
-        .fulfillOracleRequest(
-          ...oracle.convertFufillParams(request1, response),
-        )
-      
+        .fulfillOracleRequest(...oracle.convertFufillParams(request1, response))
+
       const responseTypes = ['bytes32']
       const responseValues = [h.toBytes32String(response)]
       const tx2 = await operator2
         .connect(roles.oracleNode)
         .fulfillOracleRequest2(
-          ...oracle.convertFulfill2Params(request2, responseTypes, responseValues),
+          ...oracle.convertFulfill2Params(
+            request2,
+            responseTypes,
+            responseValues,
+          ),
         )
-      
+
       const receipt1 = await tx1.wait()
       const receipt2 = await tx2.wait()
       // 40260 vs 41423
       matchers.gasDiffLessThan(1200, receipt1, receipt2)
-
     })
   })
 })
