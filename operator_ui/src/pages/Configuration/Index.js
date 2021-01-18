31a3ef18a221fea55a66a798f659b875a8f0a6eb
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2021-01-18 18:55:03 +0000

    🔥 Remove @chainlink/styleguide dependency (#3872)

--- operator_ui/src/pages/Configuration/Index.js
+++ operator_ui/src/pages/Configuration/Index.js
@@ -1,4 +1,5 @@
-import { KeyValueList, PaddedCard } from '@chainlink/styleguide'
+import { PaddedCard } from 'components/PaddedCard'
+import { KeyValueList } from 'components/KeyValueList'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { fetchConfiguration } from 'actionCreators'
--- operator_ui/src/pages/Keys/P2PKeys.tsx
+++ operator_ui/src/pages/Keys/P2PKeys.tsx
@@ -15,7 +15,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Typography from '@material-ui/core/Typography'
-import { TimeAgo } from '@chainlink/styleguide'
+import { TimeAgo } from 'components/TimeAgo'
 import {
   createStyles,
   withStyles,
