fd9168ce51909fd662d6bcd2b9cf2c4d76d784fa
connorwstein <connor.stein@mail.mcgill.ca>
2021-01-21 11:43:45 -0500

    remove migrate comment

--- core/store/migrations/migration1611174407/migrate.go
+++ core/store/migrations/migration1611174407/migrate.go
@@ -2,8 +2,6 @@ package migration1611174407
 
 import "github.com/jinzhu/gorm"
 
-// Migrate makes the explicit the pre-existing
-// implicit assumption that lowercase external initiator names are unique
 func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
 		ALTER TABLE pipeline_task_runs 
