b5c7d77cbf929f99ccfa01ba991041fdcddb35af
Sam <samsondav@protonmail.com>
2021-02-09 18:11:18 -0600

    Add a comment

--- core/services/bulletprooftxmanager/eth_confirmer.go
+++ core/services/bulletprooftxmanager/eth_confirmer.go
@@ -73,6 +73,14 @@ func (ec *ethConfirmer) Disconnect() {
 	// pass
 }
 
+// OnNewLongestChain uses a mailbox with capacity 1 to deliver the latest head to the runLoop.
+// This is because it may take longer than the intervals between heads to process, but we don't want to interrupt the loop.
+// e.g.
+// - We’re still processing head 41
+// - Head 42 comes in
+// - Now heads 43, 44, 45 come in
+// - Now we finish head 41
+// - We move straight on to processing head 45
 func (ec *ethConfirmer) OnNewLongestChain(ctx context.Context, head models.Head) {
 	ec.mb.Deliver(head)
 }
