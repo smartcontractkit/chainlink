c0536f4237db0759a14c70a3b1f0fba84f7d926e
Alex Roan <alex.roan@hotmail.com>
2021-02-10 18:31:33 +0000

    Added comments

--- evm-contracts/src/v0.7/dev/CompoundPriceFlaggingValidator.sol
+++ evm-contracts/src/v0.7/dev/CompoundPriceFlaggingValidator.sol
@@ -8,11 +8,20 @@ import "../interfaces/AggregatorV3Interface.sol";
 import "../interfaces/UniswapAnchoredView.sol";
 import "./UpkeepCompatible.sol";
 
+/**
+ * @notice This validator compares the price of Chainlink aggregators against
+ * their equivalent Compound Open Oracle feeds. For each aggregator, a Compound
+ * feed is configured with its symbol, number of decimals, and deviation threshold.
+ * An aggregator address is flagged when its corresponding Compound feed price deviates
+ * by more than the configured threshold from the aggregator price.
+ */
 contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
   using SafeMathChainlink for uint256;
 
   struct CompoundAssetDetails {
+    // Used to call the Compound Open Oracle
     string symbol;
+    // Used to convert price to match aggregator decimals
     uint8 decimals;
     // 1        = 100%
     // 5        = 20%
@@ -45,6 +54,13 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     uint256 deviationThresholdDenominator
   );
   
+  /**
+   * @notice Create a new CompoundPriceFlaggingValidator
+   * @dev Use this contract to compare Chainlink aggregator prices
+   * against the Compound Open Oracle prices
+   * @param flagsAddress Address of the flag contract
+   * @param compoundOracleAddress Address of the Compound Open Oracle UniswapAnchoredView contract
+   */
   constructor(address flagsAddress, address compoundOracleAddress)
     ConfirmedOwner(msg.sender)
   {
@@ -52,6 +68,10 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     setCompoundOpenOracleAddress(compoundOracleAddress);
   }
 
+  /**
+   * @notice Set the address of the Compound Open Oracle UniswapAnchoredView contract
+   * @param oracleAddress Compound Open Oracle UniswapAnchoredView address
+   */
   function setCompoundOpenOracleAddress(address oracleAddress)
     public
     onlyOwner()
@@ -78,6 +98,17 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     }
   }
 
+  /**
+   * @notice Set the threshold details for comparing a Chainlink aggregator
+   * to a Compound Open Oracle feed.
+   * @param aggregator The Chainlink aggregator address
+   * @param compoundSymbol The symbol used by Compound for this feed
+   * @param compoundDecimals The number of decimals in the Compound feed
+   * @param compoundDeviationThresholdDenominator The threshold denominator use to determine
+   * the percentage with which the difference in prices must reside within. For example:
+   * If the prices are valid within a 5% threshold, and 1 / denominator, this denominator
+   * = 20, since 1/20 = 5.
+   */
   function setThreshold(
     address aggregator,
     string calldata compoundSymbol,
@@ -100,6 +131,13 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     );
   }
 
+  /**
+   * @notice Check the price deviation of an array of aggregators
+   * @dev If any of the aggregators provided have an equivalent Compound Oracle feed
+   * that with a price outside of the configured deviation, this function will return them.
+   * @param aggregators address[] memory
+   * @return address[] invalid feeds
+   */
   function check(address[] memory aggregators) public view returns (address[] memory) {
     address[] memory invalidAggregators = new address[](aggregators.length);
     uint256 invalidCount = 0;
@@ -119,22 +157,50 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     return invalidAggregators;
   }
 
+  /**
+   * @notice Check and raise flags for any aggregator that has an equivalent Compound
+   * Open Oracle feed with a price deviation exceeding the configured setting.
+   * @dev This contract must have write permissions on the Flags contract
+   * @param aggregators address[] memory
+   * @return address[] memory invalid aggregators
+   */
   function update(address[] memory aggregators) public returns (address[] memory){
     address[] memory invalidAggregators = check(aggregators);
     s_flags.raiseFlags(invalidAggregators);
     return invalidAggregators;
   }
 
+  /**
+   * @notice Check the price deviation of an array of aggregators
+   * @dev If any of the aggregators provided have an equivalent Compound Oracle feed
+   * that with a price outside of the configured deviation, this function will return them.
+   * @param data bytes encoded address array
+   * @return needsUpkeep bool indicating whether upkeep needs to be performed
+   * @return invalid aggregators - bytes encoded address array of invalid aggregator addresses
+   */
   function checkForUpkeep(bytes calldata data) external view override returns (bool, bytes memory) {
     address[] memory invalidAggregators = check(abi.decode(data, (address[])));
     bool needsUpkeep = (invalidAggregators.length > 0);
     return (needsUpkeep, abi.encode(invalidAggregators));
   }
 
+  /**
+   * @notice Check and raise flags for any aggregator that has an equivalent Compound
+   * Open Oracle feed with a price deviation exceeding the configured setting.
+   * @dev This contract must have write permissions on the Flags contract
+   * @param data bytes encoded address array
+   */
   function performUpkeep(bytes calldata data) external override {
     update(abi.decode(data, (address[])));
   }
 
+  /**
+   * @notice Get the threshold of an aggregator
+   * @param aggregator address
+   * @return string Compound Oracle Symbol
+   * @return uint8 Compound Oracle Decimals
+   * @return uint256 Deviation Threshold Denominator
+   */
   function threshold(address aggregator)
     public
     view
@@ -156,12 +222,25 @@ contract CompoundPriceFlaggingValidator is ConfirmedOwner, UpkeepCompatible {
     return address(s_flags);
   }
 
+  /**
+   * @notice Get the Compound Open Oracle address
+   * @return address
+   */
   function compoundOpenOracle() external view returns (address) {
     return address(s_compOpenOracle);
   }
 
+  /**
+   * @notice Check if an aggregator has an equivalent Compound Oracle feed
+   * that's price is deviated more than the threshold.
+   * @param aggregator address of the Chainlink aggregator
+   * @return invalid bool. True if the deviation exceeds threshold.
+   */
   function isInvalid(address aggregator) private view returns (bool invalid) {
     CompoundAssetDetails memory compDetails = s_thresholds[aggregator];
+    if (bytes(compDetails.symbol).length == 0) {
+      return false;
+    }
     // Get aggregator price & decimals
     AggregatorV3Interface priceFeed = AggregatorV3Interface(aggregator);
     (,int256 signedPrice,,,) = priceFeed.latestRoundData();
