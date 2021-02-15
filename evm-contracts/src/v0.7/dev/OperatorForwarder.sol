e10278b60ff9958a86b02f3d9e52789005a9a87e
Alex Roan <alex.roan@hotmail.com>
2021-02-15 14:20:04 +0000

    Use interface rather than full Operator

--- evm-contracts/src/v0.7/dev/OperatorForwarder.sol
+++ evm-contracts/src/v0.7/dev/OperatorForwarder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "./Operator.sol";
+import "../interfaces/OperatorInterface.sol";
 
 contract OperatorForwarder {
   address public immutable authorizedSender1;
@@ -9,7 +9,7 @@ contract OperatorForwarder {
   address public immutable authorizedSender3;
 
   constructor() {
-    address[] memory authorizedSenders = Operator(msg.sender).getAuthorizedSenders();
+    address[] memory authorizedSenders = OperatorInterface(msg.sender).getAuthorizedSenders();
     authorizedSender1 = authorizedSenders[0];
     authorizedSender2 = authorizedSenders[1];
     authorizedSender3 = authorizedSenders[2];
