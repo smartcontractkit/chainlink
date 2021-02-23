a3c5343acd653cb984c379a8a125ebfb3b211003
Alex Roan <alex.roan@hotmail.com>
2021-02-23 18:22:15 +0000

    Moved forward into forwarder
    Altered createForwarder to auto generate the salt depending on the number of forwarders in the list. Move forward function into the forwarder. Regenerated wrappers. Fixed Operator tests

--- evm-contracts/src/v0.7/dev/OperatorForwarder.sol
+++ evm-contracts/src/v0.7/dev/OperatorForwarder.sol
@@ -8,10 +8,34 @@ contract OperatorForwarder {
   address public immutable authorizedSender2;
   address public immutable authorizedSender3;
 
-  constructor() {
+  address public immutable linkAddr;
+
+  constructor(address link) {
+    linkAddr = link;
     address[] memory authorizedSenders = OperatorInterface(msg.sender).getAuthorizedSenders();
     authorizedSender1 = authorizedSenders[0];
     authorizedSender2 = (authorizedSenders.length > 1) ? authorizedSenders[1] : address(0);
     authorizedSender3 = (authorizedSenders.length > 2) ? authorizedSenders[2] : address(0);
+  }
+
+  function forward(
+    address to,
+    bytes calldata data
+  )
+    public
+    onlyAuthorizedSender()
+  {
+    require(to != linkAddr, "Cannot #forward to Link token");
+    (bool status,) = to.call(data);
+    require(status, "Forwarded call failed.");
+  }
+
+  modifier onlyAuthorizedSender() {
+    require(msg.sender == authorizedSender1
+      || msg.sender == authorizedSender2
+      || msg.sender == authorizedSender3,
+      "Not authorized to fulfill requests"
+    );
+    _;
   }
 }
