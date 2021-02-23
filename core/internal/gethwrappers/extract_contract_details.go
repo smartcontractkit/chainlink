22520aae38ad03526b540d3a02935545ade5070f
Alex Coventry <alx@mit.edu>
2021-02-22 19:05:29 -0500

    Replace all metadata hashes with a constant value
    It seems that if the `new` solidity keyword is used by a contract, solc will
    generate a binary which includes the entire binary of the contract `new` is
    deploying, including an extra metadata hash.
    
    This commit replaces all matches for the metadata hash in the binary.
    
    This is perhaps slightly fragile; it's conceivable that a metadata-hash match
    could happen accidentally. There's about 128 bits of difficulty in coming up
    with such a hash (16 fixed bytes, and ignoring the fixed distances required
    between the matches, and the fact that there are multiple candidates in a given
    binary..)

--- core/internal/gethwrappers/extract_contract_details.go
+++ core/internal/gethwrappers/extract_contract_details.go
@@ -54,32 +54,11 @@ func ExtractContractDetails(beltArtifactPath string) (*ContractDetails, error) {
 	if err := json.Unmarshal(beltArtifact, &sources); err != nil {
 		return nil, errors.Wrapf(err, "could not read source code from compiler artifact")
 	}
-
 	return &ContractDetails{
-		Binary:  binaryData.String(),
+		Binary:  binaryData,
 		ABI:     contractABI,
 		Sources: sources.Sources,
 	}, nil
-}
-
-// binaryContractSections represents an analysis of the contract binary produced
-// by solc. Constituent strings are the hex representations of those sections
-type binaryContractSections struct {
-	contractBinary string // The actual contract bytecode
-	// The contract binary is followed by a hash of the contract metadata Because
-	// the metadata can vary from compile to compile, it's necessary to replace
-	// this with a constant.
-	// https://solidity.readthedocs.io/en/v0.6.2/metadata.html#encoding-of-the-metadata-hash-in-the-bytecode
-	metadataHash string
-	// If any static data such as often-used strings need to be stored, they come
-	// after the metadata hash
-	// https://gitter.im/ethereum/solidity?at=5f10a247d60398014655861f
-	staticData string
-}
-
-// String returns the concatenated contract binary, as hex
-func (b *binaryContractSections) String() string {
-	return b.contractBinary + b.metadataHash + b.staticData
 }
 
 // extractSuffix returns the binary with the metadata replaced by a constant
@@ -88,47 +67,39 @@ func (b *binaryContractSections) String() string {
 //
 // Since the binary metadata suffix varies so much, it can't be included in a
 // reliable check that the golang wrapper is up-to-date, so we replace it here
-func replaceMetadata(beltArtifact []byte) (*binaryContractSections, error) {
+// https://solidity.readthedocs.io/en/v0.6.2/metadata.html#encoding-of-the-metadata-hash-in-the-bytecode
+//
+// Note that if the contract being wrapped uses the `new` keyword to deploy some
+// other contract, the binary for wrapped contract must contain the contract to
+// be deployed as well, and solc will *also* add metadata to that contract.
+// Thus, there can be multiple metadata hashes which need to be replaced.
+func replaceMetadata(beltArtifact []byte) (string, error) {
 	// We want the bytecode here, not the deployedByteCode. The latter does not
 	// include the initialization code.
 	rawBinary := gjson.Get(string(beltArtifact),
 		"compilerOutput.evm.bytecode.object").String()
 	if rawBinary == "" {
-		return nil, errors.Errorf(
+		return "", errors.Errorf(
 			"could not parse belt contract binary JSON as JSON")
 	}
 	if _, err := hexutil.Decode(rawBinary); err != nil {
-		return nil, errors.Errorf("contract binary from belt artifact is not JSON")
+		return "", errors.Errorf("contract binary from belt artifact is not JSON")
 	}
-	// Since regexp.Regexp.FindAll returns successive NON-OVERLAPPING matches, and
-	// we want the FINAL match, it is necessary to reverse the binary data and
-	// regexp, so that we can search from the front.
-	//
+	binLen := len(rawBinary)
+	binRawBin := []byte(rawBinary)
 	// See https://golang.org/pkg/regexp/#Overview for submatch structure, search
 	// for "matches and submatches are identified by byte index"
-	revMetadataBounds := reversedMetaData.FindAllStringSubmatchIndex(
-		utils.ReverseString(rawBinary), 1)
-	// We want the last submatch
-	revBounds := revMetadataBounds[0][len(revMetadataBounds[0])-2:]
-	metadataStart := len(rawBinary) - revBounds[1]
-	metadataEnd := len(rawBinary) - revBounds[0]
-	metadataHash := rawBinary[metadataStart:metadataEnd]
-	if !metadata.MatchString(metadataHash) {
-		panic(errors.Errorf("retrieved bad metadata: %s", metadataHash))
+	for _, metadataBounds := range metadata.FindAllSubmatchIndex(binRawBin, binLen) {
+		for i := range constantBinaryMetadataHash {
+			binRawBin[metadataBounds[0]+i] = constantBinaryMetadataHash[i]
+		}
 	}
-	rv := &binaryContractSections{
-		contractBinary: rawBinary[:metadataStart],
-		metadataHash:   constantBinaryMetadataHash,
-		staticData:     rawBinary[metadataEnd:],
-	}
-	if len(rv.String()) != len(rawBinary) {
-		panic("reconstruction with fixed metadata hash failed")
-	}
-	_, err := hex.DecodeString(rv.staticData)
+	rawBinary = string(binRawBin)
+	_, err := hex.DecodeString(rawBinary[2:])
 	if err != nil {
 		panic(errors.Wrapf(err, "can't fail, due to earlier hexutil.Decode check!"))
 	}
-	return rv, nil
+	return rawBinary, nil
 }
 
 // binarySuffixRegexp checks that the hex representation of the trailing bytes
@@ -145,22 +116,15 @@ func replaceMetadata(beltArtifact []byte) (*binaryContractSections, error) {
 var metadata = regexp.MustCompile("a264697066735822[[:xdigit:]]{68}" +
 	"64736f6c6343[[:xdigit:]]{6}0033")
 
-var reversedMetaData = regexp.MustCompile("(..)*(3300[[:xdigit:]]{6}" +
-	"3436c6f63746[[:xdigit:]]{68}228537660796462a)")
-
 // constantBinaryMetadataHash is an arbitrary constant stand-in for the
 // metadata suffix which the EVM expects (and in some cases, it seems, requires)
 // to find at the end of the binary object representing a contract under
 // deployment. See metadata docstring.
-var constantBinaryMetadataHash = "a264697066735822" +
-	strings.Repeat("beef", 68/4) + "64736f6c6343" + "decafe" + "0033"
+var constantBinaryMetadataHash = []byte("a264697066735822" +
+	strings.Repeat("beef", 68/4) + "64736f6c6343" + "decafe" + "0033")
 
 func init() {
-	if !metadata.MatchString(constantBinaryMetadataHash) {
+	if !metadata.Match(constantBinaryMetadataHash) {
 		panic("constantBinaryMetadataHash is invalid")
-	}
-	if !reversedMetaData.MatchString(utils.ReverseString(
-		constantBinaryMetadataHash)) {
-		panic("constantBinaryMetadataHash does not match reversedMetaData regex")
 	}
 }
