34d0e2af86a2dc2e01d94c3f3a578dd0ff567474
Ryan Hall <hall.ryan.r@gmail.com>
2021-02-26 12:19:22 -0600

    add keeper contract wrappers

--- core/services/keeper/models.go
+++ core/services/keeper/models.go
@@ -25,33 +25,6 @@ func (Registry) TableName() string {
 	return "keeper_registries"
 }
 
-// func (reg Registry) SyncFromContract(contract *keeper_registry_contract.KeeperRegistryContract) (Registry, error) {
-// 	config, err := contract.GetConfig(nil)
-// 	if err != nil {
-// 		return Registry{}, err
-// 	}
-// 	reg.CheckGas = config.CheckGasLimit
-// 	reg.BlockCountPerTurn = uint32(config.BlockCountPerTurn.Uint64())
-// 	keeperAddresses, err := contract.GetKeeperList(nil)
-// 	if err != nil {
-// 		return Registry{}, err
-// 	}
-// 	found := false
-// 	for idx, address := range keeperAddresses {
-// 		if address == reg.From {
-// 			reg.KeeperIndex = uint32(idx)
-// 			found = true
-// 		}
-// 	}
-// 	if !found {
-// 		return Registry{}, fmt.Errorf("unable to find %s in keeper list on registry %s", reg.From.Hex(), reg.Address.Hex())
-// 	}
-
-// 	reg.NumKeepers = uint32(len(keeperAddresses))
-
-// 	return reg, nil
-// }
-
 type UpkeepRegistration struct {
 	ID                  int32 `gorm:"primary_key"`
 	CheckData           []byte
