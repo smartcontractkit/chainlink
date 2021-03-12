311b5b6bddfffddb97f986feede84deb58ff9d64
connorwstein <connor.stein@mail.mcgill.ca>
2021-03-12 12:10:12 -0500

    stacktrace

--- core/gracefulpanic/recover.go
+++ core/gracefulpanic/recover.go
@@ -1,11 +1,14 @@
 package gracefulpanic
 
-import "github.com/smartcontractkit/chainlink/core/logger"
+import (
+	"github.com/smartcontractkit/chainlink/core/logger"
+	"runtime/debug"
+)
 
 func WrapRecover(fn func()) {
 	defer func() {
 		if err := recover(); err != nil {
-			logger.Default.Errorw("goroutine panicked", "panic", err)
+			logger.Default.Errorw("goroutine panicked", "panic", err, "stacktrace", string(debug.Stack()))
 		}
 	}()
 	fn()
