09c6ca0cabd2883af9feaf5048095a9678106951
PiotrTrzpil <piotr.trzpilgmail.com>
2021-03-12 11:58:28 +0100

    Add time measurements to head tracker prometheus errors

--- core/services/prom_reporter.go
+++ core/services/prom_reporter.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
@@ -87,6 +89,7 @@ func (pr *promReporter) Disconnect() {
 }
 
 func (pr *promReporter) OnNewLongestChain(ctx context.Context, head models.Head) {
+	start := time.Now()
 	err := multierr.Combine(
 		errors.Wrap(pr.reportPendingEthTxes(ctx), "reportPendingEthTxes failed"),
 		errors.Wrap(pr.reportMaxUnconfirmedBlocks(ctx, head), "reportMaxUnconfirmedBlocks failed"),
@@ -94,14 +97,24 @@ func (pr *promReporter) OnNewLongestChain(ctx context.Context, head models.Head)
 	)
 
 	if err != nil {
-		logger.Errorw("Error reporting prometheus metrics", "err", err)
+		deadline, _ := ctx.Deadline()
+		took := time.Now().Sub(start) * time.Millisecond
+		available := deadline.Sub(start) * time.Millisecond
+		logger.Errorw(fmt.Sprintf("Error reporting prometheus metrics. Took: %d ms, time available when starting: %d ms", took, available), "err", err)
 	}
 }
 
 func (pr *promReporter) reportPendingEthTxes(ctx context.Context) (err error) {
+	start := time.Now()
 	rows, err := pr.db.QueryContext(ctx, `SELECT count(*) FROM eth_txes WHERE state = 'unconfirmed'`)
 	if err != nil {
-		return errors.Wrap(err, "failed to query for unconfirmed eth_tx count")
+		deadline, _ := ctx.Deadline()
+		return errors.Wrap(err,
+			fmt.Sprintf("failed to query for unconfirmed eth_tx count. Query took: %d ms. Time left when starting: %d ms",
+				time.Now().Sub(start) * time.Millisecond,
+				deadline.Sub(start) * time.Millisecond,
+			),
+		)
 	}
 
 	defer func() {
@@ -119,12 +132,19 @@ func (pr *promReporter) reportPendingEthTxes(ctx context.Context) (err error) {
 }
 
 func (pr *promReporter) reportMaxUnconfirmedBlocks(ctx context.Context, head models.Head) (err error) {
+	start := time.Now()
 	rows, err := pr.db.QueryContext(ctx, `
 SELECT MIN(broadcast_before_block_num) FROM eth_tx_attempts
 JOIN eth_txes ON eth_txes.id = eth_tx_attempts.eth_tx_id
 AND eth_txes.state = 'unconfirmed'`)
 	if err != nil {
-		return errors.Wrap(err, "failed to query for min broadcast_before_block_num")
+		deadline, _ := ctx.Deadline()
+		return errors.Wrap(err,
+			fmt.Sprintf("failed to query for min broadcast_before_block_num. Query took: %d ms. Time left when starting: %d ms",
+				time.Now().Sub(start) * time.Millisecond,
+				deadline.Sub(start) * time.Millisecond,
+			),
+		)
 	}
 	defer func() {
 		err = multierr.Combine(err, rows.Close())
@@ -145,11 +165,18 @@ AND eth_txes.state = 'unconfirmed'`)
 }
 
 func (pr *promReporter) reportPipelineRunStats(ctx context.Context) (err error) {
+	start := time.Now()
 	rows, err := pr.db.QueryContext(ctx, `
 SELECT pipeline_run_id FROM pipeline_task_runs WHERE finished_at IS NULL
 `)
 	if err != nil {
-		return errors.Wrap(err, "failed to query for pipeline_run_id")
+		deadline, _ := ctx.Deadline()
+		return errors.Wrap(err,
+			fmt.Sprintf("failed to query for pipeline_run_id. Query took: %d ms. Time left when starting: %d ms",
+				time.Now().Sub(start) * time.Millisecond,
+				deadline.Sub(start) * time.Millisecond,
+			),
+		)
 	}
 	defer func() {
 		err = multierr.Combine(err, rows.Close())
