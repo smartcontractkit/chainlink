b9482daf8a03803d028b2bbba80468ae47f43b94
connorwstein <connor.stein@mail.mcgill.ca>
2021-03-31 16:32:58 -0400

    no lock

--- core/services/synchronization/explorer_client.go
+++ core/services/synchronization/explorer_client.go
@@ -138,9 +138,6 @@ func (ec *explorerClient) Start() error {
 // once buffer is full.
 // func (ec *explorerClient) Receive(durationParams ...time.Duration) ([]byte, error) {
 func (ec *explorerClient) Send(ctx context.Context, data []byte, messageTypes ...int) {
-	ec.boot.RLock()
-	defer ec.boot.RUnlock()
-
 	messageType := ExplorerTextMessage
 	if len(messageTypes) > 0 {
 		messageType = messageTypes[0]
