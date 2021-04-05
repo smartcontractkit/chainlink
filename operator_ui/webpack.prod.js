12d04606156916e4a76059c9c0ba7d7286fb5e05
James Kong <jkongie@gmail.com>
2021-04-05 19:50:26 +0800

    Compress gui assets using gzip during build

--- operator_ui/webpack.prod.js
+++ operator_ui/webpack.prod.js
@@ -1,22 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
+const CompressionPlugin = require('compression-webpack-plugin')
 const webpackBase = require('./webpack.config')
 
 module.exports = Object.assign(webpackBase, {
-  plugins: [
-    ...webpackBase.plugins,
-    // new CompressionPlugin({
-    //   filename: '[path][base].br',
-    //   algorithm: 'brotliCompress',
-    //   test: /\.js$/,
-    //   compressionOptions: {
-    //     params: {
-    //       [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-    //     },
-    //   },
-    //   threshold: 10240,
-    //   minRatio: 0.8,
-    //   deleteOriginalAssets: false,
-    // }),
-  ],
+  plugins: [...webpackBase.plugins, new CompressionPlugin({})],
 })
