a7cb7f1f58c0bdb70cf58836b8c9f37afa2c31de
James Kong <jkongie@gmail.com>
2021-04-06 14:45:57 +0800

    Refactor Eth Tx presenter

--- core/web/tx_attempts_controller.go
+++ core/web/tx_attempts_controller.go
@@ -2,7 +2,7 @@ package web
 
 import (
 	"github.com/smartcontractkit/chainlink/core/services/chainlink"
-	"github.com/smartcontractkit/chainlink/core/store/presenters"
+	"github.com/smartcontractkit/chainlink/core/web/presenters"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,9 +15,9 @@ type TxAttemptsController struct {
 // Index returns paginated transaction attempts
 func (tac *TxAttemptsController) Index(c *gin.Context, size, page, offset int) {
 	attempts, count, err := tac.App.GetStore().EthTxAttempts(offset, size)
-	ptxs := make([]presenters.EthTx, len(attempts))
+	ptxs := make([]presenters.EthTxResource, len(attempts))
 	for i, attempt := range attempts {
-		ptxs[i] = presenters.NewEthTxFromAttempt(attempt)
+		ptxs[i] = presenters.NewEthTxResourceFromAttempt(attempt)
 	}
 	paginatedResponse(c, "transactions", size, page, ptxs, count, err)
 }
