6226e64631c9b440aa6d7b629b9be4d4f2c6bb26
James Kong <jkongie@gmail.com>
2021-04-06 22:23:14 +0800

    Extract Eth Keys commands and add a delete eth keys test

--- core/cmd/remote_client_test.go
+++ core/cmd/remote_client_test.go
@@ -9,9 +9,7 @@ import (
 	"io/ioutil"
 	"math/big"
 	"os"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/smartcontractkit/chainlink/core/services/job"
@@ -31,7 +29,6 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/models/p2pkey"
 	"github.com/smartcontractkit/chainlink/core/store/presenters"
 	"github.com/smartcontractkit/chainlink/core/utils"
-	webpresenters "github.com/smartcontractkit/chainlink/core/web/presenters"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -154,25 +151,6 @@ func requireP2PKeyCount(t *testing.T, store *store.Store, length int) []p2pkey.E
 	require.NoError(t, err)
 	require.Len(t, keys, length)
 	return keys
-}
-
-func TestClient_ListETHKeys(t *testing.T) {
-	t.Parallel()
-
-	rpcClient, gethClient := newEthMocks(t)
-	app := startNewApplication(t,
-		withKey(),
-		withMocks(eth.NewClientWith(rpcClient, gethClient)),
-	)
-	client, r := app.NewClientAndRenderer()
-
-	gethClient.On("BalanceAt", mock.Anything, mock.Anything, mock.Anything).Maybe().Return(big.NewInt(42), nil)
-	rpcClient.On("Call", mock.Anything, "eth_call", mock.Anything, "latest").Return(nil)
-
-	assert.Nil(t, client.ListETHKeys(cltest.EmptyCLIContext()))
-	require.Equal(t, 1, len(r.Renders))
-	balances := *r.Renders[0].(*[]webpresenters.ETHKeyResource)
-	assert.Equal(t, app.Key.Address.Hex(), balances[0].Address)
 }
 
 func TestClient_IndexJobSpecs(t *testing.T) {
@@ -799,117 +777,6 @@ func TestClient_IndexTxAttempts(t *testing.T) {
 
 	renderedAttempts = *r.Renders[1].(*[]presenters.EthTx)
 	assert.Equal(t, 0, len(renderedAttempts))
-}
-
-func TestClient_CreateETHKey(t *testing.T) {
-	t.Parallel()
-
-	rpcClient, gethClient := newEthMocks(t)
-	app := startNewApplication(t,
-		withKey(),
-		withMocks(eth.NewClientWith(rpcClient, gethClient)),
-	)
-	client, _ := app.NewClientAndRenderer()
-
-	gethClient.On("BalanceAt", mock.Anything, mock.Anything, mock.Anything).Return(big.NewInt(42), nil)
-	rpcClient.On("Call", mock.Anything, "eth_call", mock.Anything, "latest").Return(nil)
-
-	assert.NoError(t, client.CreateETHKey(nilContext))
-}
-
-func TestClient_ImportExportETHKey(t *testing.T) {
-	t.Parallel()
-
-	defer deleteKeyExportFile(t)
-
-	rpcClient, gethClient := newEthMocks(t)
-	app := startNewApplication(t,
-		withMocks(eth.NewClientWith(rpcClient, gethClient)),
-	)
-	client, r := app.NewClientAndRenderer()
-
-	gethClient.On("BalanceAt", mock.Anything, mock.Anything, mock.Anything).Return(big.NewInt(42), nil)
-	rpcClient.On("Call", mock.Anything, "eth_call", mock.Anything, "latest").Return(nil)
-
-	set := flag.NewFlagSet("test", 0)
-	set.String("file", "internal/fixtures/apicredentials", "")
-	c := cli.NewContext(nil, set, nil)
-	err := client.RemoteLogin(c)
-	assert.NoError(t, err)
-
-	err = app.Store.KeyStore.Unlock(cltest.Password)
-	assert.NoError(t, err)
-
-	err = client.ListETHKeys(c)
-	assert.NoError(t, err)
-	require.Len(t, *r.Renders[0].(*[]webpresenters.ETHKeyResource), 0)
-
-	r.Renders = nil
-
-	set = flag.NewFlagSet("test", 0)
-	set.String("oldpassword", "../internal/fixtures/correct_password.txt", "")
-	set.Parse([]string{"../internal/fixtures/keys/testkey-0x69Ca211a68100E18B40683E96b55cD217AC95006.json"})
-	c = cli.NewContext(nil, set, nil)
-	err = client.ImportETHKey(c)
-	assert.NoError(t, err)
-
-	r.Renders = nil
-
-	set = flag.NewFlagSet("test", 0)
-	c = cli.NewContext(nil, set, nil)
-	err = client.ListETHKeys(c)
-	assert.NoError(t, err)
-	require.Len(t, *r.Renders[0].(*[]webpresenters.ETHKeyResource), 1)
-
-	ethkeys := *r.Renders[0].(*[]webpresenters.ETHKeyResource)
-	addr := common.HexToAddress("0x69Ca211a68100E18B40683E96b55cD217AC95006")
-	assert.Equal(t, addr.Hex(), ethkeys[0].Address)
-
-	testdir := filepath.Join(os.TempDir(), t.Name())
-	err = os.MkdirAll(testdir, 0700|os.ModeDir)
-	assert.NoError(t, err)
-	defer os.RemoveAll(testdir)
-
-	keyfilepath := filepath.Join(testdir, "key")
-	set = flag.NewFlagSet("test", 0)
-	set.String("oldpassword", "../internal/fixtures/correct_password.txt", "")
-	set.String("newpassword", "../internal/fixtures/incorrect_password.txt", "")
-	set.String("output", keyfilepath, "")
-	set.Parse([]string{addr.Hex()})
-	c = cli.NewContext(nil, set, nil)
-	err = client.ExportETHKey(c)
-	assert.NoError(t, err)
-
-	// Now, make sure that the keyfile can be imported with the `newpassword` and yields the correct address
-	keyJSON, err := ioutil.ReadFile(keyfilepath)
-	assert.NoError(t, err)
-	oldpassword, err := ioutil.ReadFile("../internal/fixtures/correct_password.txt")
-	assert.NoError(t, err)
-	newpassword, err := ioutil.ReadFile("../internal/fixtures/incorrect_password.txt")
-	assert.NoError(t, err)
-
-	keystoreDir := filepath.Join(os.TempDir(), t.Name(), "keystore")
-	err = os.MkdirAll(keystoreDir, 0700|os.ModeDir)
-	assert.NoError(t, err)
-
-	scryptParams := utils.GetScryptParams(app.Store.Config)
-	keystore := store.NewKeyStore(keystoreDir, scryptParams)
-	err = keystore.Unlock(string(oldpassword))
-	assert.NoError(t, err)
-	acct, err := keystore.Import(keyJSON, strings.TrimSpace(string(newpassword)))
-	assert.NoError(t, err)
-	assert.Equal(t, addr.Hex(), acct.Address.Hex())
-
-	// Export test invalid id
-	keyName := keyNameForTest(t)
-	set = flag.NewFlagSet("test Eth export invalid id", 0)
-	set.Parse([]string{"999"})
-	set.String("newpassword", "../internal/fixtures/apicredentials", "")
-	set.String("output", keyName, "")
-	c = cli.NewContext(nil, set, nil)
-	err = client.ExportETHKey(c)
-	require.Error(t, err, "Error exporting")
-	require.Error(t, utils.JustError(os.Stat(keyName)))
 }
 
 func TestClient_SetMinimumGasPrice(t *testing.T) {
