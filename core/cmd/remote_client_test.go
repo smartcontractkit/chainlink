69abca0a25485c65a433262b382c7a5ca7e7f9e4
James Kong <jkongie@gmail.com>
2021-04-06 22:37:51 +0800

    Extract p2p keys commands and add list p2p keys test

--- core/cmd/remote_client_test.go
+++ core/cmd/remote_client_test.go
@@ -26,7 +26,6 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/store/models/ocrkey"
-	"github.com/smartcontractkit/chainlink/core/store/models/p2pkey"
 	"github.com/smartcontractkit/chainlink/core/store/presenters"
 	"github.com/smartcontractkit/chainlink/core/utils"
 	"github.com/stretchr/testify/assert"
@@ -141,13 +140,6 @@ func deleteKeyExportFile(t *testing.T) {
 
 func requireOCRKeyCount(t *testing.T, store *store.Store, length int) []ocrkey.EncryptedKeyBundle {
 	keys, err := store.OCRKeyStore.FindEncryptedOCRKeyBundles()
-	require.NoError(t, err)
-	require.Len(t, keys, length)
-	return keys
-}
-
-func requireP2PKeyCount(t *testing.T, store *store.Store, length int) []p2pkey.EncryptedP2PKey {
-	keys, err := store.OCRKeyStore.FindEncryptedP2PKeys()
 	require.NoError(t, err)
 	require.Len(t, keys, length)
 	return keys
@@ -858,103 +850,6 @@ func TestClient_CancelJobRun(t *testing.T) {
 	require.Len(t, runs, 1)
 	assert.Equal(t, models.RunStatusCancelled, runs[0].GetStatus())
 	assert.NotNil(t, runs[0].FinishedAt)
-}
-
-func TestClient_P2P_CreateKey(t *testing.T) {
-	t.Parallel()
-
-	app := startNewApplication(t)
-	client, _ := app.NewClientAndRenderer()
-
-	app.Store.OCRKeyStore.Unlock(cltest.Password)
-
-	require.NoError(t, client.CreateP2PKey(nilContext))
-
-	keys, err := app.GetStore().OCRKeyStore.FindEncryptedP2PKeys()
-	require.NoError(t, err)
-
-	// Created + fixture key
-	require.Len(t, keys, 2)
-
-	for _, e := range keys {
-		_, err = e.Decrypt(cltest.Password)
-		require.NoError(t, err)
-	}
-}
-
-func TestClient_P2P_DeleteKey(t *testing.T) {
-	t.Parallel()
-
-	app := startNewApplication(t)
-	client, _ := app.NewClientAndRenderer()
-
-	app.Store.OCRKeyStore.Unlock(cltest.Password)
-
-	key, err := p2pkey.CreateKey()
-	require.NoError(t, err)
-	encKey, err := key.ToEncryptedP2PKey(cltest.Password, utils.FastScryptParams)
-	require.NoError(t, err)
-	err = app.Store.OCRKeyStore.UpsertEncryptedP2PKey(&encKey)
-	require.NoError(t, err)
-
-	requireP2PKeyCount(t, app.Store, 2) // Created  + fixture key
-
-	set := flag.NewFlagSet("test", 0)
-	set.Bool("yes", true, "")
-	strID := strconv.FormatInt(int64(encKey.ID), 10)
-	set.Parse([]string{strID})
-	c := cli.NewContext(nil, set, nil)
-	err = client.DeleteP2PKey(c)
-	require.NoError(t, err)
-
-	requireP2PKeyCount(t, app.Store, 1) // fixture key only
-}
-
-func TestClient_ImportExportP2PKeyBundle(t *testing.T) {
-	t.Parallel()
-
-	defer deleteKeyExportFile(t)
-
-	app := startNewApplication(t)
-	client, _ := app.NewClientAndRenderer()
-	store := app.GetStore()
-
-	store.OCRKeyStore.Unlock(cltest.Password)
-
-	keys := requireP2PKeyCount(t, store, 1)
-	key := keys[0]
-	keyName := keyNameForTest(t)
-
-	// Export test invalid id
-	set := flag.NewFlagSet("test P2P export", 0)
-	set.Parse([]string{"0"})
-	set.String("newpassword", "../internal/fixtures/apicredentials", "")
-	set.String("output", keyName, "")
-	c := cli.NewContext(nil, set, nil)
-	err := client.ExportP2PKey(c)
-	require.Error(t, err, "Error exporting")
-	require.Error(t, utils.JustError(os.Stat(keyName)))
-
-	// Export test
-	set = flag.NewFlagSet("test P2P export", 0)
-	set.Parse([]string{fmt.Sprint(key.ID)})
-	set.String("newpassword", "../internal/fixtures/apicredentials", "")
-	set.String("output", keyName, "")
-	c = cli.NewContext(nil, set, nil)
-
-	require.NoError(t, client.ExportP2PKey(c))
-	require.NoError(t, utils.JustError(os.Stat(keyName)))
-
-	require.NoError(t, store.OCRKeyStore.DeleteEncryptedP2PKey(&key))
-	requireP2PKeyCount(t, store, 0)
-
-	set = flag.NewFlagSet("test P2P import", 0)
-	set.Parse([]string{keyName})
-	set.String("oldpassword", "../internal/fixtures/apicredentials", "")
-	c = cli.NewContext(nil, set, nil)
-	require.NoError(t, client.ImportP2PKey(c))
-
-	requireP2PKeyCount(t, store, 1)
 }
 
 func TestClient_CreateOCRKeyBundle(t *testing.T) {
