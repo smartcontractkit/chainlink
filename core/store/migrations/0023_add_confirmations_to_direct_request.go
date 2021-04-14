4d94f75f463b000b574a4eec0d5640cadaef4fd7
Piotr Trzpil <PiotrTrzpil@users.noreply.github.com>
2021-04-14 16:22:30 +0200

    Add min required confirmations to new direct request logic (#4199)
    * Add min required confirmations to new direct request logic

---
+++ core/store/migrations/0023_add_confirmations_to_direct_request.go
@@ -0,0 +1,27 @@
+package migrations
+
+import (
+	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
+)
+
+const (
+	up23 = `
+ALTER TABLE direct_request_specs ADD COLUMN num_confirmations bigint DEFAULT NULL;
+`
+	down23 = `
+ALTER TABLE direct_request_specs DROP COLUMN num_confirmations;
+`
+)
+
+func init() {
+	Migrations = append(Migrations, &gormigrate.Migration{
+		ID: "0023_add_confirmations_to_direct_request",
+		Migrate: func(db *gorm.DB) error {
+			return db.Exec(up23).Error
+		},
+		Rollback: func(db *gorm.DB) error {
+			return db.Exec(down23).Error
+		},
+	})
+}
