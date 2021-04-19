4d28c80960ba92c5e1641f3b3ba9bb38cdedbe3d
James Kong <jkongie@gmail.com>
2021-04-20 00:00:23 +0800

    Fixes an issue where starting operator ui in dev mode would not load the page

--- operator_ui/webpack.prod.js
+++ operator_ui/webpack.prod.js
@@ -3,5 +3,9 @@ const CompressionPlugin = require('compression-webpack-plugin')
 const webpackBase = require('./webpack.config')
 
 module.exports = Object.assign(webpackBase, {
+  output: {
+    ...webpackBase.output,
+    publicPath: '/assets/', // JS files are served from `/assets` by web
+  },
   plugins: [...webpackBase.plugins, new CompressionPlugin({})],
 })
