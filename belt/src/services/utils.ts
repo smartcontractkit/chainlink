0836092679186cdcf4e062ff4d250c53dacab646
Alex Roan <alex.roan@hotmail.com>
2021-04-24 19:36:52 +0100

    Prettier linting

--- belt/src/services/utils.ts
+++ belt/src/services/utils.ts
@@ -7,11 +7,11 @@ import * as config from './config'
  * Get contract versions and their directories
  */
 export function getContractDirs(conf: config.App) {
-  let contractsMap = []
-  for(let dir in conf.compilerSettings.versions) {
+  const contractsMap = []
+  for (const dir in conf.compilerSettings.versions) {
     contractsMap.push({
-      dir: dir,
-      version: conf.compilerSettings.versions[dir]
+      dir,
+      version: conf.compilerSettings.versions[dir],
     })
   }
   return contractsMap
