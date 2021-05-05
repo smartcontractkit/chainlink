e675cc70df1c1940addb8e6531b2d716d971cf75
Alex Roan <alex.roan@hotmail.com>
2021-05-05 16:34:40 +0100

    Added reentrancy disclaimer for validate

--- evm-contracts/src/v0.8/dev/ValidatorProxy.sol
+++ evm-contracts/src/v0.8/dev/ValidatorProxy.sol
@@ -81,6 +81,9 @@ contract ValidatorProxy is AggregatorValidatorInterface, ConfirmedOwner {
    * @dev To guard against external `validate` calls reverting, we use raw calls here.
    * We favour `call` over try-catch to ensure that failures are avoided even if the validator address is incorrectly
    * set as a non-contract address.
+   * @dev If the `aggregator` and `validator` are the same contract, this could exhibit reentrancy behavior.
+   * However, since that contract would have to be explicitly written for reentrancy and that the `owner` would have
+   * to configure this contract to use that malicious contract, we refrain from using mutex or check here.
    * @param previousRoundId uint256
    * @param previousAnswer int256
    * @param currentRoundId uint256
