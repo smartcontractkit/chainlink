a002c0bdcfbefccdab2de7a25f187f12f6f64aa5
PiotrTrzpil <piotr.trzpil@gmail.com>
2021-05-07 12:21:07 +0200

    tweaks

--- core/services/log/eth_subscriber.go
+++ core/services/log/eth_subscriber.go
@@ -85,7 +85,6 @@ func (sub *ethSubscriber) backfillLogs(fromBlockOverride *models.Head, addresses
 		// request data limit.
 		// On matic its 5MB [https://github.com/maticnetwork/bor/blob/3de2110886522ab17e0b45f3c4a6722da72b7519/rpc/http.go#L35]
 		// On ethereum its 15MB [https://github.com/ethereum/go-ethereum/blob/master/rpc/websocket.go#L40]
-		//latest := b.Number()
 		batchSize := int64(sub.config.EthLogBackfillBatchSize())
 		for i := q.FromBlock.Int64(); i <= int64(currentHeight); i += batchSize {
 
