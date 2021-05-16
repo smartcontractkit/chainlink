47f4b6cab154cad53a66800b5ace0c300923f097
Sam <samsondav@protonmail.com>
2021-05-16 11:28:27 -0500

    Do not resend insufficient_eth attempts for confirmed/errored transactions
    Fixes a potential panic in rare cases and reduces unnecessary resends.

--- core/services/offchainreporting/transmitter.go
+++ core/services/offchainreporting/transmitter.go
@@ -35,6 +35,11 @@ func (t *transmitter) CreateEthTransaction(ctx context.Context, toAddress gethCo
 	}
 
 	value := 0
+	// NOTE: It is important to remember that eth_tx_attempts with state
+	// insufficient_eth can actually hang around long after the node has been
+	// refunded and started sending transactions again.
+	// This is because they are not ever deleted if attached to an eth_tx that
+	// is moved into confirmed/fatal_error state
 	res, err := t.db.ExecContext(ctx, `
 INSERT INTO eth_txes (from_address, to_address, encoded_payload, value, gas_limit, state, created_at)
 SELECT $1,$2,$3,$4,$5,'unstarted',NOW()
