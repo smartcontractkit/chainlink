d8aa90a7b92300b0dc29f365b176774e97afaadf
Blaž Hrastnik <blaz@mxxn.io>
2021-05-21 11:44:02 +0900

    7237: Replace OkToStart and OkToStop calls with StartOnce, StopOnce

--- core/services/offchainreporting/peer_wrapper.go
+++ core/services/offchainreporting/peer_wrapper.go
@@ -40,112 +40,112 @@ type (
 // It currently only supports one peerID/key
 // It should be fairly easy to modify it to support multiple peerIDs/keys using e.g. a map
 func NewSingletonPeerWrapper(keyStore *KeyStore, config *orm.Config, db *gorm.DB) *SingletonPeerWrapper {
-	return &SingletonPeerWrapper{keyStore, config, db, nil, "", nil, utils.StartStopOnce{}}
+	return &SingletonPeerWrapper{
+		keyStore: keyStore,
+		config:   config,
+		db:       db,
+	}
 }
 
 func (p *SingletonPeerWrapper) IsStarted() bool {
 	return p.State() == utils.StartStopOnce_Started
 }
 
-func (p *SingletonPeerWrapper) Start() (err error) {
-	return p.StartOnce("Singleton peer wrapper", p.start)
-}
+func (p *SingletonPeerWrapper) Start() error {
+	return p.StartOnce("SingletonPeerWrapper", func() (err error) {
+		p2pkeys := p.keyStore.DecryptedP2PKeys()
+		listenPort := p.config.P2PListenPort()
+		if listenPort == 0 {
+			return errors.New("failed to instantiate oracle or bootstrapper service. If FEATURE_OFFCHAIN_REPORTING is on, then P2P_LISTEN_PORT is required and must be set to a non-zero value")
+		}
 
-func (p *SingletonPeerWrapper) start() (err error) {
-	p2pkeys := p.keyStore.DecryptedP2PKeys()
-	listenPort := p.config.P2PListenPort()
-	if listenPort == 0 {
-		return errors.New("failed to instantiate oracle or bootstrapper service. If FEATURE_OFFCHAIN_REPORTING is on, then P2P_LISTEN_PORT is required and must be set to a non-zero value")
-	}
+		if len(p2pkeys) == 0 {
+			return nil
+		}
 
-	if len(p2pkeys) == 0 {
-		return nil
-	}
-
-	var key p2pkey.Key
-	var matched bool
-	checkedKeys := []string{}
-	configuredPeerID, err := p.config.P2PPeerID(nil)
-	if err != nil {
-		return errors.Wrap(err, "failed to start peer wrapper")
-	}
-	for _, k := range p2pkeys {
-		var peerID models.PeerID
-		peerID, err = k.GetPeerID()
+		var key p2pkey.Key
+		var matched bool
+		checkedKeys := []string{}
+		configuredPeerID, err := p.config.P2PPeerID(nil)
 		if err != nil {
-			return errors.Wrap(err, "unexpectedly failed to get peer ID from key")
+			return errors.Wrap(err, "failed to start peer wrapper")
 		}
-		if peerID == configuredPeerID {
-			key = k
-			matched = true
-			break
+		for _, k := range p2pkeys {
+			var peerID models.PeerID
+			peerID, err = k.GetPeerID()
+			if err != nil {
+				return errors.Wrap(err, "unexpectedly failed to get peer ID from key")
+			}
+			if peerID == configuredPeerID {
+				key = k
+				matched = true
+				break
+			}
+			checkedKeys = append(checkedKeys, peerID.String())
 		}
-		checkedKeys = append(checkedKeys, peerID.String())
-	}
-	keys := strings.Join(checkedKeys, ", ")
-	if !matched {
-		if configuredPeerID == "" {
-			return errors.Errorf("multiple p2p keys found but peer ID was not set. You must specify P2P_PEER_ID if you have more than one key. Keys available: %s", keys)
+		keys := strings.Join(checkedKeys, ", ")
+		if !matched {
+			if configuredPeerID == "" {
+				return errors.Errorf("multiple p2p keys found but peer ID was not set. You must specify P2P_PEER_ID if you have more than one key. Keys available: %s", keys)
+			}
+			return errors.Errorf("multiple p2p keys found but none matched the given P2P_PEER_ID of '%s'. Keys available: %s", configuredPeerID, keys)
 		}
-		return errors.Errorf("multiple p2p keys found but none matched the given P2P_PEER_ID of '%s'. Keys available: %s", configuredPeerID, keys)
-	}
 
-	p.PeerID, err = key.GetPeerID()
-	if err != nil {
-		return errors.Wrap(err, "could not get peer ID")
-	}
-	p.pstoreWrapper, err = NewPeerstoreWrapper(p.db, p.config.P2PPeerstoreWriteInterval(), p.PeerID)
-	if err != nil {
-		return errors.Wrap(err, "could not make new pstorewrapper")
-	}
+		p.PeerID, err = key.GetPeerID()
+		if err != nil {
+			return errors.Wrap(err, "could not get peer ID")
+		}
+		p.pstoreWrapper, err = NewPeerstoreWrapper(p.db, p.config.P2PPeerstoreWriteInterval(), p.PeerID)
+		if err != nil {
+			return errors.Wrap(err, "could not make new pstorewrapper")
+		}
 
-	// If the P2PAnnounceIP is set we must also set the P2PAnnouncePort
-	// Fallback to P2PListenPort if it wasn't made explicit
-	var announcePort uint16
-	if p.config.P2PAnnounceIP() != nil && p.config.P2PAnnouncePort() != 0 {
-		announcePort = p.config.P2PAnnouncePort()
-	} else if p.config.P2PAnnounceIP() != nil {
-		announcePort = listenPort
-	}
+		// If the P2PAnnounceIP is set we must also set the P2PAnnouncePort
+		// Fallback to P2PListenPort if it wasn't made explicit
+		var announcePort uint16
+		if p.config.P2PAnnounceIP() != nil && p.config.P2PAnnouncePort() != 0 {
+			announcePort = p.config.P2PAnnouncePort()
+		} else if p.config.P2PAnnounceIP() != nil {
+			announcePort = listenPort
+		}
 
-	peerLogger := NewLogger(logger.Default, p.config.OCRTraceLogging(), func(string) {})
+		peerLogger := NewLogger(logger.Default, p.config.OCRTraceLogging(), func(string) {})
 
-	p.Peer, err = ocrnetworking.NewPeer(ocrnetworking.PeerConfig{
-		PrivKey:      key.PrivKey,
-		ListenIP:     p.config.P2PListenIP(),
-		ListenPort:   listenPort,
-		AnnounceIP:   p.config.P2PAnnounceIP(),
-		AnnouncePort: announcePort,
-		Logger:       peerLogger,
-		Peerstore:    p.pstoreWrapper.Peerstore,
-		EndpointConfig: ocrnetworking.EndpointConfig{
-			IncomingMessageBufferSize: p.config.OCRIncomingMessageBufferSize(),
-			OutgoingMessageBufferSize: p.config.OCROutgoingMessageBufferSize(),
-			NewStreamTimeout:          p.config.OCRNewStreamTimeout(),
-			DHTLookupInterval:         p.config.OCRDHTLookupInterval(),
-			BootstrapCheckInterval:    p.config.OCRBootstrapCheckInterval(),
-		},
-		DHTAnnouncementCounterUserPrefix: p.config.P2PDHTAnnouncementCounterUserPrefix(),
+		p.Peer, err = ocrnetworking.NewPeer(ocrnetworking.PeerConfig{
+			PrivKey:      key.PrivKey,
+			ListenIP:     p.config.P2PListenIP(),
+			ListenPort:   listenPort,
+			AnnounceIP:   p.config.P2PAnnounceIP(),
+			AnnouncePort: announcePort,
+			Logger:       peerLogger,
+			Peerstore:    p.pstoreWrapper.Peerstore,
+			EndpointConfig: ocrnetworking.EndpointConfig{
+				IncomingMessageBufferSize: p.config.OCRIncomingMessageBufferSize(),
+				OutgoingMessageBufferSize: p.config.OCROutgoingMessageBufferSize(),
+				NewStreamTimeout:          p.config.OCRNewStreamTimeout(),
+				DHTLookupInterval:         p.config.OCRDHTLookupInterval(),
+				BootstrapCheckInterval:    p.config.OCRBootstrapCheckInterval(),
+			},
+			DHTAnnouncementCounterUserPrefix: p.config.P2PDHTAnnouncementCounterUserPrefix(),
+		})
+		if err != nil {
+			return errors.Wrap(err, "error calling NewPeer")
+		}
+		return p.pstoreWrapper.Start()
 	})
-	if err != nil {
-		return errors.Wrap(err, "error calling NewPeer")
-	}
-	return p.pstoreWrapper.Start()
 }
 
 // Close closes the peer and peerstore
-func (p *SingletonPeerWrapper) Close() (err error) {
-	return p.StopOnce("Singleton peer wrapper", p.close)
-}
+func (p *SingletonPeerWrapper) Close() error {
+	return p.StopOnce("SingletonPeerWrapper", func() (err error) {
+		if p.Peer != nil {
+			err = p.Peer.Close()
+		}
 
-func (p *SingletonPeerWrapper) close() (err error) {
-	if p.Peer != nil {
-		err = p.Peer.Close()
-	}
+		if p.pstoreWrapper != nil {
+			err = multierr.Combine(err, p.pstoreWrapper.Close())
+		}
 
-	if p.pstoreWrapper != nil {
-		err = multierr.Combine(err, p.pstoreWrapper.Close())
-	}
-
-	return err
+		return err
+	})
 }
