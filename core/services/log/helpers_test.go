8844243ff84dede94aff1d644f1d8f903b6270df
PiotrTrzpil <piotr.trzpil@gmail.com>
2021-05-28 13:10:01 +0100

    cleanup

--- core/services/log/helpers_test.go
+++ core/services/log/helpers_test.go
@@ -359,7 +359,7 @@ func newMockEthClient(chchRawLogs chan chan<- types.Log, blockHeight int64, expe
 
 	mockEth.sub.On("Unsubscribe").
 		Return().
-		Run(func(mock.Arguments) { atomic.AddInt32(&(mockEth.unsubscribeCalls), 1) }) //.
+		Run(func(mock.Arguments) { atomic.AddInt32(&(mockEth.unsubscribeCalls), 1) })
 
 	return mockEth
 }
