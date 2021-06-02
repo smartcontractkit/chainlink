1610856cb189bb580d2a3f743a0a1d654c6ae999
Alex Roan <alex.roan@hotmail.com>
2021-06-02 16:26:05 +0100

    Prettier

--- contracts/test/client/AggregatorV2V3Interface.test.ts
+++ contracts/test/client/AggregatorV2V3Interface.test.ts
@@ -24,26 +24,26 @@ describe("AggregatorV2V3Interface Solidity version tests", () => {
     consumerV6 = await v6Factory.deploy(mockAggregator.address);
     consumerV7 = await v7Factory.deploy(mockAggregator.address);
     consumerV8 = await v8Factory.deploy(mockAggregator.address);
-  })
+  });
 
-  it('should work using a Solidity 0.6.x consumer', async () => {
+  it("should work using a Solidity 0.6.x consumer", async () => {
     const answer = 123;
     mockAggregator.connect(owner).setLatestAnswer(answer);
     const response = await consumerV6.getLatestPrice();
     assert.equal(response.toNumber(), answer);
-  })
+  });
 
-  it('should work using a Solidity 0.7.x consumer', async () => {
+  it("should work using a Solidity 0.7.x consumer", async () => {
     const answer = 234;
     mockAggregator.connect(owner).setLatestAnswer(answer);
     const response = await consumerV7.getLatestPrice();
     assert.equal(response.toNumber(), answer);
-  })
+  });
 
-  it('should work using a Solidity 0.8.x consumer', async () => {
+  it("should work using a Solidity 0.8.x consumer", async () => {
     const answer = 345;
     mockAggregator.connect(owner).setLatestAnswer(answer);
     const response = await consumerV8.getLatestPrice();
     assert.equal(response.toNumber(), answer);
-  })
-})
+  });
+});
