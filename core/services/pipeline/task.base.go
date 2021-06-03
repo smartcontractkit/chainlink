2b3ddd06c7d35ab5296c0dff248efeb7154661e3
Blaž Hrastnik <blaz@mxxn.io>
2021-06-03 15:02:45 +0900

    pipeline: Drop OutputTask/SetOutputTask

--- core/services/pipeline/task.base.go
+++ core/services/pipeline/task.base.go
@@ -6,15 +6,14 @@ type BaseTask struct {
 	outputs []Task
 	inputs  []Task
 
-	id         int64
-	outputTask Task
-	dotID      string
-	Index      int32         `mapstructure:"index" json:"-" `
-	Timeout    time.Duration `mapstructure:"timeout"`
+	id      int64
+	dotID   string
+	Index   int32         `mapstructure:"index" json:"-" `
+	Timeout time.Duration `mapstructure:"timeout"`
 }
 
-func NewBaseTask(id int64, dotID string, t Task, index int32) BaseTask {
-	return BaseTask{dotID: dotID, outputTask: t, Index: index}
+func NewBaseTask(id int64, dotID string, outputs []Task, index int32) BaseTask {
+	return BaseTask{dotID: dotID, outputs: outputs, Index: index}
 }
 
 func (t *BaseTask) Base() *BaseTask {
@@ -33,20 +32,12 @@ func (t BaseTask) OutputIndex() int32 {
 	return t.Index
 }
 
-func (t BaseTask) OutputTask() Task {
-	return t.outputTask
-}
-
 func (t BaseTask) Outputs() []Task {
 	return t.outputs
 }
 
 func (t BaseTask) Inputs() []Task {
 	return t.inputs
-}
-
-func (t *BaseTask) SetOutputTask(outputTask Task) {
-	t.outputTask = outputTask
 }
 
 func (t BaseTask) TaskTimeout() (time.Duration, bool) {
