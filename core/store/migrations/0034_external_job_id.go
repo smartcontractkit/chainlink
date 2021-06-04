68d37208fcd61eda525a7893211194145d1e3508
connorwstein <connor.stein@mail.mcgill.ca>
2021-06-04 13:46:57 -0400

    bump migration

--- core/store/migrations/0034_external_job_id.go
+++ core/store/migrations/0034_external_job_id.go
@@ -3,7 +3,7 @@ package migrations
 import "gorm.io/gorm"
 
 const (
-	up33 = `
+	up34 = `
                CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
                ALTER TABLE direct_request_specs DROP COLUMN on_chain_job_spec_id;
                ALTER TABLE webhook_specs DROP COLUMN on_chain_job_spec_id;
@@ -12,7 +12,7 @@ const (
                ALTER TABLE jobs ADD CONSTRAINT non_zero_uuid_check CHECK (external_job_id <> '00000000-0000-0000-0000-000000000000');
                ALTER TABLE vrf_specs ADD CONSTRAINT vrf_specs_public_key_fkey FOREIGN KEY (public_key) REFERENCES encrypted_vrf_keys(public_key);
 	`
-	down33 = `
+	down34 = `
                ALTER TABLE direct_request_specs ADD COLUMN on_chain_job_spec_id bytea;
                ALTER TABLE webhook_specs ADD COLUMN on_chain_job_spec_id bytea;
                ALTER TABLE jobs DROP CONSTRAINT external_job_id_uniq;
@@ -22,12 +22,12 @@ const (
 
 func init() {
 	Migrations = append(Migrations, &Migration{
-		ID: "0033_external_job_id",
+		ID: "0034_external_job_id",
 		Migrate: func(db *gorm.DB) error {
-			return db.Exec(up33).Error
+			return db.Exec(up34).Error
 		},
 		Rollback: func(db *gorm.DB) error {
-			return db.Exec(down33).Error
+			return db.Exec(down34).Error
 		},
 	})
 }
