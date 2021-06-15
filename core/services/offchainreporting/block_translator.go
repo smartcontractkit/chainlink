7d754954c9d134ada9b2aa8b2dbaeb4162b482c0
Sam <samsondav@protonmail.com>
2021-06-15 15:22:03 +0100

    Set sensible minimums for arbitrum

--- core/services/offchainreporting/block_translator.go
+++ core/services/offchainreporting/block_translator.go
@@ -17,7 +17,7 @@ type BlockTranslator interface {
 // NewBlockTranslator returns the block translator for the given chain
 func NewBlockTranslator(chain *chains.Chain) BlockTranslator {
 	if chain.IsArbitrum() {
-		return newArbitrumBlockTranslator()
+		return newArbitrumBlockTranslator(chain)
 	} else if chain.IsOptimism() {
 		return newOptimismBlockTranslator()
 	}
@@ -32,13 +32,23 @@ func (*l1BlockTranslator) NumberToQueryRange(changedInBlock uint64) (fromBlock *
 func (*l1BlockTranslator) Start() {}
 func (*l1BlockTranslator) Close() {}
 
-type arbitrumBlockTranslator struct{}
-
-func newArbitrumBlockTranslator() *arbitrumBlockTranslator {
-	return &arbitrumBlockTranslator{}
+type arbitrumBlockTranslator struct {
+	min int64
 }
 
-func (*arbitrumBlockTranslator) NumberToQueryRange(changedInBlock uint64) (fromBlock *big.Int, toBlock *big.Int) {
+func newArbitrumBlockTranslator(chain *chains.Chain) *arbitrumBlockTranslator {
+	var min int64
+	if chain == chains.ArbitrumMainnet {
+		// sequencer contract deployed in https://etherscan.io/tx/0xd71d3c90adcce0fabc903fceed668561c92f5be5d8837295f9e46e2f6d99894e
+		min = 12525700
+	} else if chain == chains.ArbitrumRinkeby {
+		// sequencer inbox was deployed in https://rinkeby.etherscan.io/tx/0x01eb72f978399a61549d71fe723e3d9943d1314717e0df10c902b2b2256fc974
+		min = 8700589
+	}
+	return &arbitrumBlockTranslator{min}
+}
+
+func (a *arbitrumBlockTranslator) NumberToQueryRange(changedInBlock uint64) (fromBlock *big.Int, toBlock *big.Int) {
 	// TODO: OPTIMISE THIS
 	// TODO: Logic goes here that:
 	// 1. Subscribes to SequencerBatchDeliveredFromOrigin on https://github.com/OffchainLabs/arbitrum/blob/next/packages/arb-bridge-eth/contracts/bridge/SequencerInbox.sol#L138
@@ -46,7 +56,9 @@ func (*arbitrumBlockTranslator) NumberToQueryRange(changedInBlock uint64) (fromB
 	// 3. Includes some sort of database persistence?
 	// Currently we simply query the entire block range. This is correct, but very slow and suboptimal
 	// See: https://app.clubhouse.io/chainlinklabs/story/11270/optimise-blocktranslator-ocr-for-optimism-and-arbitrum
-	return big.NewInt(0), nil
+
+	// NOTE: Mainnet l2
+	return big.NewInt(a.min), nil
 }
 func (a *arbitrumBlockTranslator) Start() {}
 func (a *arbitrumBlockTranslator) Close() {}
--- core/services/offchainreporting/block_translator_test.go
+++ core/services/offchainreporting/block_translator_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Test_BlockTranslator(t *testing.T) {
-	t.Run("for non-arbitrum chains, returns the block changed argument", func(t *testing.T) {
+	t.Run("for L1 chains, returns the block changed argument", func(t *testing.T) {
 		chain := chains.ChainFromID(big.NewInt(1))
 
 		bt := offchainreporting.NewBlockTranslator(chain)
@@ -23,15 +23,32 @@ func Test_BlockTranslator(t *testing.T) {
 		assert.Equal(t, big.NewInt(42), to)
 	})
 
-	t.Run("for arbitrum chains, returns 0 and nil", func(t *testing.T) {
-		chain := chains.ChainFromID(big.NewInt(42161))
-
-		bt := offchainreporting.NewBlockTranslator(chain)
+	t.Run("for L2 chains, returns an initial block number and nil", func(t *testing.T) {
+		bt := offchainreporting.NewBlockTranslator(chains.ArbitrumMainnet)
 		bt.Start()
 		defer bt.Close()
-
 		from, to := bt.NumberToQueryRange(42)
+		assert.Equal(t, big.NewInt(12525700), from)
+		assert.Equal(t, (*big.Int)(nil), to)
 
+		bt = offchainreporting.NewBlockTranslator(chains.ArbitrumRinkeby)
+		bt.Start()
+		defer bt.Close()
+		from, to = bt.NumberToQueryRange(42)
+		assert.Equal(t, big.NewInt(8700589), from)
+		assert.Equal(t, (*big.Int)(nil), to)
+
+		bt = offchainreporting.NewBlockTranslator(chains.OptimismMainnet)
+		bt.Start()
+		defer bt.Close()
+		from, to = bt.NumberToQueryRange(42)
+		assert.Equal(t, big.NewInt(0), from)
+		assert.Equal(t, (*big.Int)(nil), to)
+
+		bt = offchainreporting.NewBlockTranslator(chains.OptimismKovan)
+		bt.Start()
+		defer bt.Close()
+		from, to = bt.NumberToQueryRange(42)
 		assert.Equal(t, big.NewInt(0), from)
 		assert.Equal(t, (*big.Int)(nil), to)
 	})
