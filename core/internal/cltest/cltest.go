f0960b9504a8cffa4d2891a3676410c84e06f7cc
James Kong <jkongie@gmail.com>
2021-06-16 22:41:51 +0800

    Use Golang CI Lint to replace golang/static CI checks
    Adds checks and code fixes for:
    
    * errcheck
    * shadow
    * vet
    * staticcheck

--- core/internal/cltest/cltest.go
+++ core/internal/cltest/cltest.go
@@ -306,7 +306,8 @@ func NewJobPipelineV2(t testing.TB, tc *TestConfig, db *gorm.DB) JobPipelineV2Te
 func NewPipelineORM(t testing.TB, config *TestConfig, db *gorm.DB) (pipeline.ORM, postgres.EventBroadcaster, func()) {
 	t.Helper()
 	eventBroadcaster := postgres.NewEventBroadcaster(config.DatabaseURL(), 0, 0)
-	eventBroadcaster.Start()
+	err := eventBroadcaster.Start()
+	require.NoError(t, err)
 	return pipeline.NewORM(db, config), eventBroadcaster, func() {
 		eventBroadcaster.Close()
 	}
@@ -315,7 +316,8 @@ func NewPipelineORM(t testing.TB, config *TestConfig, db *gorm.DB) (pipeline.ORM
 func NewEthBroadcaster(t testing.TB, store *strpkg.Store, keyStore bulletprooftxmanager.KeyStore, config *TestConfig, keys ...ethkey.Key) (*bulletprooftxmanager.EthBroadcaster, func()) {
 	t.Helper()
 	eventBroadcaster := postgres.NewEventBroadcaster(config.DatabaseURL(), 0, 0)
-	eventBroadcaster.Start()
+	err := eventBroadcaster.Start()
+	require.NoError(t, err)
 	return bulletprooftxmanager.NewEthBroadcaster(store.DB, store.EthClient, config, keyStore, &postgres.NullAdvisoryLocker{}, eventBroadcaster, keys), func() {
 		eventBroadcaster.Close()
 	}
@@ -482,7 +484,8 @@ func NewApplicationWithConfig(t testing.TB, tc *TestConfig, flagsAndDeps ...inte
 	ta.Server = server
 	ta.wsServer = tc.wsServer
 	return ta, func() {
-		ta.StopIfStarted()
+		err := ta.StopIfStarted()
+		require.NoError(t, err)
 	}
 }
 
@@ -532,9 +535,12 @@ func (ta *TestApplication) Start() error {
 	ta.Started = true
 	// TODO - RYAN - we should have a global keystore.Unlock() function
 	// https://app.clubhouse.io/chainlinklabs/story/7735/combine-keystores
-	ta.ChainlinkApplication.KeyStore.Eth().Unlock(Password)
+	err := ta.ChainlinkApplication.KeyStore.Eth().Unlock(Password)
+	if err != nil {
+		return err
+	}
 
-	err := ta.ChainlinkApplication.Start()
+	err = ta.ChainlinkApplication.Start()
 	return err
 }
 
@@ -573,7 +579,10 @@ func (ta *TestApplication) Stop() error {
 	// TODO: Here we double close, which is less than ideal.
 	// We would prefer to invoke a method on an interface that
 	// cleans up only in test.
-	ta.ChainlinkApplication.StopIfStarted()
+	err := ta.ChainlinkApplication.StopIfStarted()
+	if err != nil {
+		return err
+	}
 	cleanUpStore(ta.t, ta.Store)
 	if ta.Server != nil {
 		ta.Server.Close()
@@ -708,7 +717,8 @@ func cleanUpStore(t testing.TB, store *strpkg.Store) {
 			logger.Warn("unable to clear test store:", err)
 		}
 	}()
-	logger.Sync()
+	// Ignore sync errors for testing
+	_ = logger.Sync()
 	require.NoError(t, store.Close())
 }
 
@@ -726,7 +736,8 @@ func ParseJSONAPIErrors(t testing.TB, body io.Reader) *models.JSONAPIErrors {
 	b, err := ioutil.ReadAll(body)
 	require.NoError(t, err)
 	var respJSON models.JSONAPIErrors
-	json.Unmarshal(b, &respJSON)
+	err = json.Unmarshal(b, &respJSON)
+	require.NoError(t, err)
 	return &respJSON
 }
 
