31cedcded6c47d395a2bb4df4fa4b14fdab88a46
spooktheducks <spooktheducks@protonmail.com>
2021-06-21 18:08:13 -0500

    Additional math task tests

--- core/services/pipeline/task.mode_test.go
+++ core/services/pipeline/task.mode_test.go
@@ -98,7 +98,7 @@ func TestModeTask(t *testing.T) {
 				BaseTask:      pipeline.NewBaseTask(0, "mode", nil, nil, 0),
 				AllowedFaults: test.allowedFaults,
 			}
-			output := task.Run(context.Background(), pipeline.NewVarsFrom(nil), pipeline.JSONSerializable{}, test.inputs)
+			output := task.Run(context.Background(), pipeline.NewVarsFrom(nil), test.inputs)
 			if output.Error != nil {
 				require.Equal(t, test.wantErrorCause, errors.Cause(output.Error))
 				require.Nil(t, output.Value)
@@ -112,83 +112,87 @@ func TestModeTask(t *testing.T) {
 		})
 	}
 
-	// for _, test := range tests {
-	// 	test := test
-	// 	t.Run(test.name+" (VarExpr)", func(t *testing.T) {
-	// 		t.Parallel()
+	for _, test := range tests {
+		test := test
+		t.Run(test.name+" (VarExpr)", func(t *testing.T) {
+			t.Parallel()
 
-	// 		var inputs []interface{}
-	// 		for _, input := range test.inputs {
-	// 			if input.Error != nil {
-	// 				inputs = append(inputs, input.Error)
-	// 			} else {
-	// 				inputs = append(inputs, input.Value)
-	// 			}
-	// 		}
-	// 		vars := pipeline.NewVarsFrom(map[string]interface{}{
-	// 			"foo": map[string]interface{}{"bar": inputs},
-	// 		})
-	// 		task := pipeline.ModeTask{
-	// 			BaseTask:      pipeline.NewBaseTask(0, "task", nil, nil, 0),
-	// 			Values:        "$(foo.bar)",
-	// 			AllowedFaults: test.allowedFaults,
-	// 			Precision:     test.precision,
-	// 		}
-	// 		output := task.Run(context.Background(), vars, pipeline.JSONSerializable{}, nil)
-	// 		if output.Error != nil {
-	// 			require.Equal(t, test.want.Error, errors.Cause(output.Error))
-	// 			require.Nil(t, output.Value)
-	// 		} else {
-	// 			require.Equal(t, test.want.Value.(*decimal.Decimal).String(), output.Value.(decimal.Decimal).String())
-	// 			require.NoError(t, output.Error)
-	// 		}
-	// 	})
-	// }
+			var inputs []interface{}
+			for _, input := range test.inputs {
+				if input.Error != nil {
+					inputs = append(inputs, input.Error)
+				} else {
+					inputs = append(inputs, input.Value)
+				}
+			}
+			vars := pipeline.NewVarsFrom(map[string]interface{}{
+				"foo": map[string]interface{}{"bar": inputs},
+			})
+			task := pipeline.ModeTask{
+				BaseTask:      pipeline.NewBaseTask(0, "task", nil, nil, 0),
+				Values:        "$(foo.bar)",
+				AllowedFaults: test.allowedFaults,
+			}
+			output := task.Run(context.Background(), vars, nil)
+			if output.Error != nil {
+				require.Equal(t, test.wantErrorCause, errors.Cause(output.Error))
+				require.Nil(t, output.Value)
+			} else {
+				require.Equal(t, map[string]interface{}{
+					"results":     test.wantResults,
+					"occurrences": test.wantOccurrences,
+				}, output.Value)
+				require.NoError(t, output.Error)
+			}
+		})
+	}
 
-	// for _, test := range tests {
-	// 	test := test
-	// 	t.Run(test.name+" (JSONWithVarExprs)", func(t *testing.T) {
-	// 		t.Parallel()
+	for _, test := range tests {
+		test := test
+		t.Run(test.name+" (JSONWithVarExprs)", func(t *testing.T) {
+			t.Parallel()
 
-	// 		var inputs []interface{}
-	// 		for _, input := range test.inputs {
-	// 			if input.Error != nil {
-	// 				inputs = append(inputs, input.Error)
-	// 			} else {
-	// 				inputs = append(inputs, input.Value)
-	// 			}
-	// 		}
-	// 		var valuesParam string
-	// 		var vars pipeline.Vars
-	// 		switch len(inputs) {
-	// 		case 0:
-	// 			valuesParam = "[]"
-	// 			vars = pipeline.NewVarsFrom(nil)
-	// 		case 1:
-	// 			valuesParam = "[ $(foo) ]"
-	// 			vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0]})
-	// 		case 3:
-	// 			valuesParam = "[ $(foo), $(bar), $(chain) ]"
-	// 			vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0], "bar": inputs[1], "chain": inputs[2]})
-	// 		case 4:
-	// 			valuesParam = "[ $(foo), $(bar), $(chain), $(link) ]"
-	// 			vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0], "bar": inputs[1], "chain": inputs[2], "link": inputs[3]})
-	// 		}
+			var inputs []interface{}
+			for _, input := range test.inputs {
+				if input.Error != nil {
+					inputs = append(inputs, input.Error)
+				} else {
+					inputs = append(inputs, input.Value)
+				}
+			}
+			var valuesParam string
+			var vars pipeline.Vars
+			switch len(inputs) {
+			case 0:
+				valuesParam = "[]"
+				vars = pipeline.NewVarsFrom(nil)
+			case 1:
+				valuesParam = "[ $(foo) ]"
+				vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0]})
+			case 3:
+				valuesParam = "[ $(foo), $(bar), $(chain) ]"
+				vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0], "bar": inputs[1], "chain": inputs[2]})
+			case 4:
+				valuesParam = "[ $(foo), $(bar), $(chain), $(link) ]"
+				vars = pipeline.NewVarsFrom(map[string]interface{}{"foo": inputs[0], "bar": inputs[1], "chain": inputs[2], "link": inputs[3]})
+			}
 
-	// 		task := pipeline.ModeTask{
-	// 			BaseTask:      pipeline.NewBaseTask(0, "task", nil, nil, 0),
-	// 			Values:        valuesParam,
-	// 			AllowedFaults: test.allowedFaults,
-	// 			Precision:     test.precision,
-	// 		}
-	// 		output := task.Run(context.Background(), vars, pipeline.JSONSerializable{}, nil)
-	// 		if output.Error != nil {
-	// 			require.Equal(t, test.want.Error, errors.Cause(output.Error))
-	// 			require.Nil(t, output.Value)
-	// 		} else {
-	// 			require.Equal(t, test.want.Value.(*decimal.Decimal).String(), output.Value.(decimal.Decimal).String())
-	// 			require.NoError(t, output.Error)
-	// 		}
-	// 	})
-	// }
+			task := pipeline.ModeTask{
+				BaseTask:      pipeline.NewBaseTask(0, "task", nil, nil, 0),
+				Values:        valuesParam,
+				AllowedFaults: test.allowedFaults,
+			}
+			output := task.Run(context.Background(), vars, nil)
+			if output.Error != nil {
+				require.Equal(t, test.wantErrorCause, errors.Cause(output.Error))
+				require.Nil(t, output.Value)
+			} else {
+				require.Equal(t, map[string]interface{}{
+					"results":     test.wantResults,
+					"occurrences": test.wantOccurrences,
+				}, output.Value)
+				require.NoError(t, output.Error)
+			}
+		})
+	}
 }
