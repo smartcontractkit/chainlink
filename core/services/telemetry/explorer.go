d59175cd3ac52122810fe36cc066064094a11fcb
Danny Skubak <skubakdj@users.noreply.github.com>
2021-06-21 20:24:41 -0400

    Rename Agent to ExplorerAgent

--- core/services/telemetry/explorer.go
+++ core/services/telemetry/explorer.go
@@ -6,17 +6,17 @@ import (
 	"github.com/smartcontractkit/chainlink/core/services/synchronization"
 )
 
-type Agent struct {
+type ExplorerAgent struct {
 	explorerClient synchronization.ExplorerClient
 }
 
-// NewAgent returns a Agent which is just a thin wrapper over
+// NewExplorerAgent returns a Agent which is just a thin wrapper over
 // the explorerClient for now
-func NewAgent(explorerClient synchronization.ExplorerClient) *Agent {
-	return &Agent{explorerClient}
+func NewExplorerAgent(explorerClient synchronization.ExplorerClient) *ExplorerAgent {
+	return &ExplorerAgent{explorerClient}
 }
 
 // SendLog sends a telemetry log to the explorer
-func (t *Agent) SendLog(log []byte) {
+func (t *ExplorerAgent) SendLog(log []byte) {
 	t.explorerClient.Send(context.Background(), log, synchronization.ExplorerBinaryMessage)
 }
