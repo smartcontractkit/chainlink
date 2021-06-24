cdd36f95822479860fbd58d9201442943558f095
Blaž Hrastnik <blaz@mxxn.io>
2021-06-24 16:35:29 +0900

    Rename migration

--- core/store/migrations/0043_add_uuid_to_pipeline_task_runs.go
+++ core/store/migrations/0043_add_uuid_to_pipeline_task_runs.go
@@ -4,7 +4,7 @@ import (
 	"gorm.io/gorm"
 )
 
-const up40 = `
+const up43 = `
 	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 	ALTER TABLE pipeline_task_runs ADD COLUMN run_id uuid NOT NULL UNIQUE DEFAULT uuid_generate_v4 ();
 	ALTER TABLE pipeline_runs ADD COLUMN inputs jsonb;
@@ -21,7 +21,7 @@ const up40 = `
 
 // TODO: update the state machine constraint to include state
 
-const down40 = `
+const down43 = `
 	DROP CONSTRAINT IF EXISTS pipeline_task_runs_run_id_key;
 	ALTER TABLE pipeline_task_runs DROP COLUMN run_id;
 	ALTER TABLE pipeline_runs DROP COLUMN inputs;
@@ -30,12 +30,12 @@ const down40 = `
 
 func init() {
 	Migrations = append(Migrations, &Migration{
-		ID: "0040_add_run_id_to_pipeline_runs",
+		ID: "0043_add_uuid_to_pipeline_task_runs",
 		Migrate: func(db *gorm.DB) error {
-			return db.Exec(up40).Error
+			return db.Exec(up43).Error
 		},
 		Rollback: func(db *gorm.DB) error {
-			return db.Exec(down40).Error
+			return db.Exec(down43).Error
 		},
 	})
 }
