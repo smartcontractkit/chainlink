a608d5d86da4ee5327adda33c6b090b922f69287
Yos Riady <yos.riady@smartcontract.com>
2021-06-28 23:47:05 +0800

    Make contracts available on v0.6 and v0.8

--- contracts/src/v0.6/interfaces/FeedRegistryInterface.sol
+++ contracts/src/v0.6/interfaces/FeedRegistryInterface.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-pragma abicoder v2;
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 import "./AggregatorV2V3Interface.sol";
 
