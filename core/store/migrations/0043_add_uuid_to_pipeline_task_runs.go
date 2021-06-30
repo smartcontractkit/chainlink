0d410de70ec2ba9c4b18997517ae1cf79918c6e2
Blaž Hrastnik <blaz@mxxn.io>
2021-06-30 16:54:33 +0900

    pipeline: Update pipeline_runs state machine

--- core/store/migrations/0043_add_uuid_to_pipeline_task_runs.go
+++ core/store/migrations/0043_add_uuid_to_pipeline_task_runs.go
@@ -17,11 +17,23 @@ const up43 = `
 	);
 
 	ALTER TABLE pipeline_runs ADD COLUMN state pipeline_runs_state DEFAULT 'completed';
+
+	ALTER TABLE pipeline_runs DROP CONSTRAINT pipeline_runs_check;
+	ALTER TABLE pipeline_runs ADD CONSTRAINT pipeline_runs_check CHECK (
+		((state IN ('completed', 'errored')) AND (finished_at IS NOT NULL) AND (num_nulls(outputs, errors) = 0))
+			OR 
+		((state IN ('running', 'suspended')) AND num_nulls(finished_at, outputs, errors) = 3)
+	);
 `
 
 // TODO: update the state machine constraint to include state
 
 const down43 = `
+	ALTER TABLE pipeline_runs DROP CONSTRAINT pipeline_runs_check;
+	ALTER TABLE pipeline_runs ADD CONSTRAINT pipeline_runs_check CHECK (
+		(((outputs IS NULL) AND (errors IS NULL) AND (finished_at IS NULL))
+		OR ((outputs IS NOT NULL) AND (errors IS NOT NULL) AND (finished_at IS NOT NULL)))
+	)
 	DROP CONSTRAINT IF EXISTS pipeline_task_runs_run_id_key;
 	ALTER TABLE pipeline_task_runs DROP COLUMN run_id;
 	ALTER TABLE pipeline_runs DROP COLUMN inputs;
