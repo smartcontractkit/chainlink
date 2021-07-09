5bd31318d8a86928cf75c08490688f192dc4e083
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-07-09 16:47:28 +0200

    Confirmed Owner out of deve

--- contracts/src/v0.8/SimpleWriteAccessController.sol
+++ contracts/src/v0.8/SimpleWriteAccessController.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./dev/ConfirmedOwner.sol";
+import "./ConfirmedOwner.sol";
 import "./interfaces/AccessControllerInterface.sol";
 
 /**
