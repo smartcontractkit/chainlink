4088d05f32aa4b39e0b37fad359f16519329af14
Alex Roan <alex.roan@hotmail.com>
2021-07-09 15:47:22 +0100

    Moved Operator contracts out of dev

--- contracts/src/v0.7/dev/AggregatorProxy.sol
+++ contracts/src/v0.7/dev/AggregatorProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "./ConfirmedOwner.sol";
+import "../ConfirmedOwner.sol";
 import "../interfaces/AggregatorProxyInterface.sol";
 
 /**
--- contracts/src/v0.7/tests/ConfirmedOwnerTestHelper.sol
+++ contracts/src/v0.7/tests/ConfirmedOwnerTestHelper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "../dev/ConfirmedOwner.sol";
+import "../ConfirmedOwner.sol";
 
 contract ConfirmedOwnerTestHelper is ConfirmedOwner {
 
