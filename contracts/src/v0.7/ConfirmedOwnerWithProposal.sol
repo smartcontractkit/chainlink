6fd2704bb7c1e71c2d435459dc914b2f629238a6
Alex Roan <alex.roan@hotmail.com>
2021-07-09 15:47:22 +0100

    Moved Operator contracts out of dev

--- contracts/src/v0.7/ConfirmedOwnerWithProposal.sol
+++ contracts/src/v0.7/ConfirmedOwnerWithProposal.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "../interfaces/OwnableInterface.sol";
+import "./interfaces/OwnableInterface.sol";
 
 /**
  * @title The ConfirmedOwner contract
