66e38e428f95c5f04837e1cb2cb0297e19f257af
James Kong <jkongie@gmail.com>
2021-07-14 20:25:03 +0800

    Style the alert message on the dashboard

--- operator_ui/src/pages/Dashboards/Index.tsx
+++ operator_ui/src/pages/Dashboards/Index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import WarningIcon from '@material-ui/icons/Warning'
+import { withStyles, WithStyles, Theme } from '@material-ui/core/styles'
 import Activity from 'components/Dashboards/Activity'
 import TokenBalanceCard from 'components/Cards/TokenBalance'
 import RecentlyCreatedJobs from 'components/Jobs/RecentlyCreated'
@@ -16,15 +19,27 @@ import accountBalanceSelector from 'selectors/accountBalance'
 import dashboardJobRunsCountSelector from 'selectors/dashboardJobRunsCount'
 import recentJobRunsSelector from 'selectors/recentJobRuns'
 import recentlyCreatedJobsSelector from 'selectors/recentlyCreatedJobs'
+import { ISO_8601 } from 'moment'
+
+const styles = (theme: Theme) => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    backgroundColor: 'rgb(255, 213, 153)',
+  },
+})
 
 type Props = {
   recentJobRunsCount: number
   recentlyCreatedPageSize: number
+  classes: WithStyles<typeof styles>['classes']
 }
 
 export const Index = ({
   recentJobRunsCount = 2,
   recentlyCreatedPageSize = 2,
+  classes,
 }: Props) => {
   const dispatch = useDispatch()
   const accountBalance = useSelector(accountBalanceSelector)
@@ -51,7 +66,32 @@ export const Index = ({
   return (
     <Content>
       <Grid container>
-        {recentlyCreatedJobs && recentlyCreatedJobs.length > 0 ? <Grid item xs={12}> <Paper><h1>WARNING: Found legacy job_specs. The JSON style of job spec is now deprecated and support for jobs using this format will be REMOVED in an upcoming release. You should migrate all these jobs to V2 (TOML) format. For help doing this, please contact our node operator support team. To test your node to see how it would behave after support for these jobs is removed, you may set ENABLE_LEGACY_JOB_PIPELINE=false</h1></Paper> </Grid> : null}
+        {recentlyCreatedJobs && recentlyCreatedJobs.length > 0 && (
+          <Grid item xs={12}>
+            <Paper className={classes.root}>
+              <p
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  margin: 0,
+                  marginBottom: 8,
+                }}
+              >
+                <WarningIcon style={{ marginRight: 8, color: '#ff9800' }} />
+                <Typography variant="h5">Found legacy job specs</Typography>
+              </p>
+              <Typography>
+                The JSON style of job spec is now deprecated and support for
+                jobs using this format will be REMOVED in an upcoming release.
+                You should migrate all these jobs to V2 (TOML) format. For help
+                doing this, please contact our node operator support team. To
+                test your node to see how it would behave after support for
+                these jobs is removed, you may set
+                ENABLE_LEGACY_JOB_PIPELINE=false
+              </Typography>
+            </Paper>
+          </Grid>
+        )}
         <Grid item xs={9}>
           <Activity
             runs={recentJobRuns}
@@ -84,4 +124,4 @@ export const Index = ({
   )
 }
 
-export default Index
+export default withStyles(styles)(Index)
