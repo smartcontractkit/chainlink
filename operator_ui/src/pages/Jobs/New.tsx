38daf99036797bcff032eef11973eb631ef1de55
Sam <samsondav@protonmail.com>
2021-07-14 08:31:45 +0100

    Deprecation warnings for legacy (JSON) job specs
    Add changelog

--- operator_ui/src/pages/Jobs/New.tsx
+++ operator_ui/src/pages/Jobs/New.tsx
@@ -251,10 +251,18 @@ export const New = ({
                             value={format}
                             control={<Radio />}
                             label={format}
+                            disabled={format === 'json'}
+                            checked={format === 'toml'}
                           />
                         ))}
                       </RadioGroup>
                     </FormControl>
+                    <b>
+                      NOTE: Support for adding JSON jobs has been removed. These
+                      legacy job types will be disabled entirely in an upcoming
+                      release. For help migrating existing jobs to the TOML
+                      format, please contact node operator support.
+                    </b>
                   </Grid>
                   <Grid item xs={12}>
                     <FormLabel>Job Spec</FormLabel>
