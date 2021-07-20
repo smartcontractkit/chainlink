a3813c181cff7e9b57b440b83c92f25cfabc231c
Blaž Hrastnik <blaz@mxxn.io>
2021-07-20 14:55:09 +0900

    fix: pipeline: Logging and metrics were missing job name/ID
    [ch10705]

--- core/services/webhook/delegate.go
+++ core/services/webhook/delegate.go
@@ -74,6 +74,10 @@ func (d *Delegate) OnJobDeleted(spec job.Job) {
 }
 
 func (d *Delegate) ServicesForSpec(spec job.Job) ([]job.Service, error) {
+	// TODO: we need to fill these out manually, find a better fix
+	spec.PipelineSpec.JobName = spec.Name.ValueOrZero()
+	spec.PipelineSpec.JobID = spec.ID
+
 	service := &pseudoService{
 		spec:             spec,
 		webhookJobRunner: d.webhookJobRunner,
