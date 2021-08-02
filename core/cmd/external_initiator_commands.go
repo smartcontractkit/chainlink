6a9f8f407c0ff4a089468be6f4450d22710e5b32
Sam <samsondav@protonmail.com>
2021-08-02 15:12:08 +0100

    Fix JSONAPI rendering of external initiators

--- core/cmd/external_initiator_commands.go
+++ core/cmd/external_initiator_commands.go
@@ -1,27 +1,28 @@
 package cmd
 
 import (
-	"github.com/olekukonko/tablewriter"
 	"github.com/smartcontractkit/chainlink/core/web/presenters"
+	clipkg "github.com/urfave/cli"
 )
 
 type ExternalInitiatorPresenter struct {
+	JAID
 	presenters.ExternalInitiatorResource
 }
 
-func (eip ExternalInitiatorPresenter) RenderTable(rt RendererTable) error {
+func (eip *ExternalInitiatorPresenter) RenderTable(rt RendererTable) error {
 	table := rt.newTable([]string{"ID", "Name", "URL", "AccessKey", "OutgoingToken", "CreatedAt", "UpdatedAt"})
-	eip.render(table)
+	table.Append(eip.ToRow())
 	render("External Initiator:", table)
 	return nil
 }
 
-func (eip ExternalInitiatorPresenter) render(table *tablewriter.Table) {
+func (eip *ExternalInitiatorPresenter) ToRow() []string {
 	var urlS string
 	if eip.URL != nil {
 		urlS = eip.URL.String()
 	}
-	table.Append([]string{
+	return []string{
 		eip.ID,
 		eip.Name,
 		urlS,
@@ -29,16 +30,21 @@ func (eip ExternalInitiatorPresenter) render(table *tablewriter.Table) {
 		eip.OutgoingToken,
 		eip.CreatedAt.String(),
 		eip.UpdatedAt.String(),
-	})
+	}
 }
 
 type ExternalInitiatorPresenters []ExternalInitiatorPresenter
 
-func (eips ExternalInitiatorPresenters) RenderTable(rt RendererTable) error {
+func (eips *ExternalInitiatorPresenters) RenderTable(rt RendererTable) error {
 	table := rt.newTable([]string{"ID", "Name", "URL", "AccessKey", "OutgoingToken", "CreatedAt", "UpdatedAt"})
-	for _, eip := range eips {
-		eip.render(table)
+	for _, eip := range *eips {
+		table.Append(eip.ToRow())
 	}
 	render("External Initiators:", table)
 	return nil
+}
+
+// IndexExternalInitiators lists external initiators
+func (cli *Client) IndexExternalInitiators(c *clipkg.Context) (err error) {
+	return cli.getPage("/v2/external_initiators", c.Int("page"), &ExternalInitiatorPresenters{})
 }
