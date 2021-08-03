4219f5fa08067b8f0554800b3241854caa496de8
James Kong <james.kong@smartcontract.com>
2021-08-04 06:57:23 +0800

    Remove JPV1 jobs features from Operator UI (#4761)
    * Remove v1 jobs from displaying
    * Remove v1 jobs from show
    * Remove v1 jobs from new job
    * Remove jobs code from dashboard
    * Clean up action creators and api
    * Remove JobRuns
    * Clean up models
    * Clean up selectors and reducers
    * Rename types
    * Rename factories
    * Delete job JPV1 related integration tests
    
    Co-authored-by: Sam <samsondav@protonmail.com>

--- operator_ui/src/pages/Jobs/JobDefinition.test.tsx
+++ operator_ui/src/pages/Jobs/JobDefinition.test.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
 import { JobsShow } from 'pages/Jobs/Show'
-import jsonApiJobSpecFactory from 'factories/jsonApiJobSpec'
 import { Route } from 'react-router-dom'
 import { mountWithProviders } from 'test-helpers/mountWithTheme'
 import { syncFetch } from 'test-helpers/syncFetch'
 import globPath from 'test-helpers/globPath'
 
-const JOB_SPEC_ID = 'c60b9927eeae43168ddbe92584937b1b'
+import {
+  jsonApiJob,
+  fluxMonitorJobResource,
+} from 'support/factories/jsonApiJobs'
+
+const JOB_ID = '200'
 
 describe('pages/Jobs/Definition', () => {
   it('renders the job definition component', async () => {
-    global.fetch.getOnce(
-      globPath(`/v2/specs/${JOB_SPEC_ID}`),
-      jsonApiJobSpecFactory({
-        id: JOB_SPEC_ID,
+    // Mock the job fetch
+    const jobResponse = jsonApiJob(
+      fluxMonitorJobResource({
+        id: JOB_ID,
       }),
     )
+    global.fetch.getOnce(globPath(`/v2/jobs/${JOB_ID}`), jobResponse)
 
     const wrapper = mountWithProviders(
-      <Route path="/jobs/:jobSpecId" component={JobsShow} />,
+      <Route path="/jobs/:jobId" component={JobsShow} />,
       {
-        initialEntries: [`/jobs/${JOB_SPEC_ID}/definition`],
+        initialEntries: [`/jobs/${JOB_ID}/definition`],
       },
     )
 
     await syncFetch(wrapper)
-    expect(wrapper.find('PrettyJson').text()).toMatchSnapshot()
+
+    expect(wrapper.find('code').text()).toMatchSnapshot()
   })
 })
