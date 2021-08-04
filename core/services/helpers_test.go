6764ad060bd848248330fde3279f62297b2f5871
PiotrTrzpil <piotr.trzpil@gmail.com>
2021-08-04 16:07:47 +0200

    merge fixes

--- core/services/helpers_test.go
+++ core/services/helpers_test.go
@@ -5,14 +5,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func NewEthClientAndSubMock(t mock.TestingT) (*mocks.Client, *mocks.Subscription) {
-	mockSub := new(mocks.Subscription)
-	mockSub.Test(t)
-	mockEth := new(mocks.Client)
-	mockEth.Test(t)
-	return mockEth, mockSub
-}
-
 func NewEthClientMock(t mock.TestingT) *mocks.Client {
 	mockEth := new(mocks.Client)
 	mockEth.Test(t)
