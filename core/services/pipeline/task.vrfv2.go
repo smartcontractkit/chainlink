bd29cdd7e151160ad3a6fa4c427985a79fece129
Connor Stein <connor.stein@mail.mcgill.ca>
2021-08-10 09:57:18 -0400

    VRF v2: VRFCoordinatorV2 implementation (#4526)
    - New v0.8 Contracts for the V2 VRF: `VRFCoordinatorV2.sol`, `VRF.sol`,
    `VRFConsumerBaseV2.sol`, `VRFCoordinatorV2Interface.sol`
    - Related test contract `VRFConsumerV2.sol` and example contracts 
    `VRFSingleConsumerExample.sol` and `VRFConsumerExternalSubOwnerExampl.sol`
    - New vrfv2 and estimategas task types
    - The vrf delegate now detects which task type is present and returns a v1 or v2 vrf listener depending on that
    - Typescript test coverage for all aspects of VRFCoordinatorV2.sol except for the fulfillment, which is tested in integration_v2_test.go

--- core/services/pipeline/task.vrfv2.go
+++ core/services/pipeline/task.vrfv2.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/smartcontractkit/chainlink/core/services/keystore/keys/vrfkey"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
 	"github.com/smartcontractkit/chainlink/core/services/signatures/secp256k1"
@@ -18,7 +16,7 @@ import (
 	"go.uber.org/multierr"
 )
 
-type VRFTask struct {
+type VRFTaskV2 struct {
 	BaseTask           `mapstructure:",squash"`
 	PublicKey          string `json:"publicKey"`
 	RequestBlockHash   string `json:"requestBlockHash"`
@@ -28,17 +26,13 @@ type VRFTask struct {
 	keyStore VRFKeyStore
 }
 
-type VRFKeyStore interface {
-	GenerateProof(k secp256k1.PublicKey, seed *big.Int) (vrfkey.Proof, error)
+var _ Task = (*VRFTaskV2)(nil)
+
+func (t *VRFTaskV2) Type() TaskType {
+	return TaskTypeVRFV2
 }
 
-var _ Task = (*VRFTask)(nil)
-
-func (t *VRFTask) Type() TaskType {
-	return TaskTypeVRF
-}
-
-func (t *VRFTask) Run(_ context.Context, vars Vars, inputs []Result) (result Result) {
+func (t *VRFTaskV2) Run(_ context.Context, vars Vars, inputs []Result) (result Result) {
 	if len(inputs) != 1 {
 		return Result{Error: ErrWrongInputCardinality}
 	}
@@ -69,13 +63,29 @@ func (t *VRFTask) Run(_ context.Context, vars Vars, inputs []Result) (result Res
 	if !ok {
 		return Result{Error: errors.Wrapf(ErrBadInput, "invalid keyHash")}
 	}
-	requestPreSeed, ok := logValues["seed"].(*big.Int)
+	requestPreSeed, ok := logValues["preSeedAndRequestId"].(*big.Int)
 	if !ok {
-		return Result{Error: errors.Wrapf(ErrBadInput, "invalid preSeed")}
+		return Result{Error: errors.Wrapf(ErrBadInput, "invalid preSeedAndRequestId")}
 	}
-	requestJobID, ok := logValues["jobID"].([32]byte)
+	subID, ok := logValues["subId"].(uint64)
 	if !ok {
-		return Result{Error: errors.Wrapf(ErrBadInput, "invalid requestJobID")}
+		return Result{Error: errors.Wrapf(ErrBadInput, "invalid subId")}
+	}
+	//minReqConf, ok := logValues["minimumRequestConfirmations"].(uint16)
+	//if !ok {
+	//	return Result{Error: errors.Wrapf(ErrBadInput, "invalid minimumRequestConfirmations")}
+	//}
+	callbackGasLimit, ok := logValues["callbackGasLimit"].(uint32)
+	if !ok {
+		return Result{Error: errors.Wrapf(ErrBadInput, "invalid callbackGasLimit")}
+	}
+	numWords, ok := logValues["numWords"].(uint32)
+	if !ok {
+		return Result{Error: errors.Wrapf(ErrBadInput, "invalid numWords")}
+	}
+	sender, ok := logValues["sender"].(common.Address)
+	if !ok {
+		return Result{Error: errors.Wrapf(ErrBadInput, "invalid sender")}
 	}
 	var pk secp256k1.PublicKey
 	copy(pk[:], pubKey[:])
@@ -88,25 +98,26 @@ func (t *VRFTask) Run(_ context.Context, vars Vars, inputs []Result) (result Res
 	if err != nil {
 		return Result{Error: fmt.Errorf("unable to parse preseed %v", preSeed)}
 	}
-	if !bytes.Equal(topics[0][:], requestJobID[:]) && !bytes.Equal(topics[1][:], requestJobID[:]) {
-		return Result{Error: fmt.Errorf("request jobID %v doesn't match expected %v or %v", requestJobID[:], topics[0][:], topics[1][:])}
+	preSeedData := proof.PreSeedDataV2{
+		PreSeed:          preSeed,
+		BlockHash:        common.BytesToHash(requestBlockHash),
+		BlockNum:         uint64(requestBlockNumber),
+		SubId:            subID,
+		CallbackGasLimit: callbackGasLimit,
+		NumWords:         numWords,
+		Sender:           sender,
 	}
-	preSeedData := proof.PreSeedData{
-		PreSeed:   preSeed,
-		BlockHash: common.BytesToHash(requestBlockHash),
-		BlockNum:  uint64(requestBlockNumber),
-	}
-	finalSeed := proof.FinalSeed(preSeedData)
+	finalSeed := proof.FinalSeedV2(preSeedData)
 	p, err := t.keyStore.GenerateProof(pk, finalSeed)
 	if err != nil {
 		return Result{Error: err}
 	}
-	onChainProof, err := proof.GenerateProofResponseFromProof(p, preSeedData)
+	onChainProof, err := proof.GenerateProofResponseFromProofV2(p, preSeedData)
 	if err != nil {
 		return Result{Error: err}
 	}
-	var results = make(map[string]interface{})
-	results["onChainProof"] = hexutil.Encode(onChainProof[:])
-
-	return Result{Value: hexutil.Encode(onChainProof[:])}
+	results := make(map[string]interface{})
+	results["proof"] = hexutil.Encode(onChainProof[:])
+	results["requestID"] = hexutil.Encode([]byte(requestPreSeed.String()))
+	return Result{Value: results}
 }
