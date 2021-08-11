9ccd2adf0f6fde26c79a095534135ad66de74e93
Piotr Trzpil <PiotrTrzpil@users.noreply.github.com>
2021-08-11 14:33:42 +0200

    Handle Gas Estimator failure on start

--- core/services/gas/block_history_estimator_test.go
+++ core/services/gas/block_history_estimator_test.go
@@ -40,9 +40,9 @@ func TestBlockHistoryEstimator_Start(t *testing.T) {
 	config.On("BlockHistoryEstimatorBlockHistorySize").Return(historySize)
 
 	config.On("BlockHistoryEstimatorTransactionPercentile").Maybe().Return(percentile)
-	config.On("EthFinalityDepth").Return(ethFinalityDepth)
-	config.On("EthGasLimitMultiplier").Maybe().Return(float32(1))
-	config.On("EthMinGasPriceWei").Maybe().Return(minGasPrice)
+	config.On("EvmFinalityDepth").Return(ethFinalityDepth)
+	config.On("EvmGasLimitMultiplier").Maybe().Return(float32(1))
+	config.On("EvmMinGasPriceWei").Maybe().Return(minGasPrice)
 	config.On("ChainID").Maybe().Return(big.NewInt(0))
 
 	t.Run("loads initial state", func(t *testing.T) {
@@ -118,6 +118,26 @@ func TestBlockHistoryEstimator_Start(t *testing.T) {
 
 	t.Run("starts anyway if fetching first fetch fails, but errors on estimation", func(t *testing.T) {
 		ethClient := new(mocks.Client)
+
+		bhe := gas.NewBlockHistoryEstimator(ethClient, config)
+
+		h := &models.Head{Hash: utils.NewHash(), Number: 42}
+		ethClient.On("HeadByNumber", mock.Anything, (*big.Int)(nil)).Return(h, nil)
+		ethClient.On("BatchCallContext", mock.Anything, mock.Anything).Return(errors.New("something went wrong"))
+
+		err := bhe.Start()
+		require.NoError(t, err)
+
+		_, _, err = bhe.EstimateGas(make([]byte, 0), 100)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "has not finished the first gas estimation yet")
+
+		ethClient.AssertExpectations(t)
+		config.AssertExpectations(t)
+	})
+
+	t.Run("starts anyway if fetching first fetch fails, but errors on estimation", func(t *testing.T) {
+		ethClient := cltest.NewEthClientMock(t)
 
 		bhe := gas.NewBlockHistoryEstimator(ethClient, config)
 
