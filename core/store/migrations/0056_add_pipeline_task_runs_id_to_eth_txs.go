61fd51bc54b65eddeb31998b12e2167548d30b28
Blaž Hrastnik <blaz@mxxn.io>
2021-08-12 14:10:16 +0900

    ...

--- core/store/migrations/0056_add_pipeline_task_runs_id_to_eth_txs.go
+++ core/store/migrations/0056_add_pipeline_task_runs_id_to_eth_txs.go
@@ -4,22 +4,22 @@ import (
 	"gorm.io/gorm"
 )
 
-const up55 = `
+const up56 = `
 CREATE INDEX eth_txes_pipeline_task_run_id_idx ON eth_txes ((meta ->> 'PipelineTaskRunID')) WHERE state = 'confirmed';
 `
 
-const down55 = `
+const down56 = `
 DROP INDEX  eth_txes_pipeline_task_run_id_idx;
 `
 
 func init() {
 	Migrations = append(Migrations, &Migration{
-		ID: "0055_add_pipeline_task_runs_id_idx_to_eth_txs",
+		ID: "0056_add_pipeline_task_runs_id_idx_to_eth_txs",
 		Migrate: func(db *gorm.DB) error {
-			return db.Exec(up55).Error
+			return db.Exec(up56).Error
 		},
 		Rollback: func(db *gorm.DB) error {
-			return db.Exec(down55).Error
+			return db.Exec(down56).Error
 		},
 	})
 }
