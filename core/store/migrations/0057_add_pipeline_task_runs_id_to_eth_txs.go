abc5d952b7bafdfbe9b79b79fadb677cdf76f8b0
Blaž Hrastnik <blaz@mxxn.io>
2021-08-17 10:32:52 +0900

    ...

--- core/store/migrations/0057_add_pipeline_task_runs_id_to_eth_txs.go
+++ core/store/migrations/0057_add_pipeline_task_runs_id_to_eth_txs.go
@@ -4,12 +4,12 @@ import (
 	"gorm.io/gorm"
 )
 
-const up56 = `
+const up57 = `
 ALTER TABLE eth_txes ADD COLUMN pipeline_task_run_id uuid UNIQUE;
 ALTER TABLE eth_txes ADD COLUMN min_confirmations integer;
 `
 
-const down56 = `
+const down57 = `
 DROP INDEX eth_txes_pipeline_task_run_id_idx;
 ALTER TABLE eth_txes DROP COLUMN pipeline_task_run_id;
 ALTER TABLE eth_txes DROP COLUMN min_confirmations;
@@ -17,12 +17,12 @@ ALTER TABLE eth_txes DROP COLUMN min_confirmations;
 
 func init() {
 	Migrations = append(Migrations, &Migration{
-		ID: "0056_add_pipeline_task_runs_id_idx_to_eth_txs",
+		ID: "0057_add_pipeline_task_runs_id_idx_to_eth_txs",
 		Migrate: func(db *gorm.DB) error {
-			return db.Exec(up56).Error
+			return db.Exec(up57).Error
 		},
 		Rollback: func(db *gorm.DB) error {
-			return db.Exec(down56).Error
+			return db.Exec(down57).Error
 		},
 	})
 }
