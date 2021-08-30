11d23f6e878600e5db51dfd20f2c25048cfa1b54
Blaž Hrastnik <blaz@mxxn.io>
2021-08-30 12:44:05 +0900

    Migrate from old migration system to the new one

--- core/store/migrate/migrate.go
+++ core/store/migrate/migrate.go
@@ -1,11 +1,16 @@
 package migrate
 
 import (
+	"context"
 	"database/sql"
 	"embed"
+	"fmt"
+	"strconv"
+	"strings"
 
-	// Invoke init() functions within migrations pkg.
-	_ "github.com/smartcontractkit/chainlink/core/store/migrate/migrations"
+	"github.com/smartcontractkit/chainlink/core/services/postgres"
+	_ "github.com/smartcontractkit/chainlink/core/store/migrate/migrations" // Invoke init() functions within migrations pkg.
+	"github.com/smartcontractkit/sqlx"
 
 	"github.com/pressly/goose/v3"
 )
@@ -15,30 +20,61 @@ var embedMigrations embed.FS
 
 const MIGRATIONS_DIR string = "migrations"
 
-// migrate existing migrations ->
-// id version_id is_applied tstamp
-// 1  0          t          ...
-// 2  1          t          ...
-
 func init() {
 	goose.SetBaseFS(embedMigrations)
 	goose.SetSequential(true)
-	// SetTableName
-	// goose.SetVerbose(true) can be set to debug migrations
+	goose.SetTableName("goose_migrations")
+	// goose.SetVerbose(true) // can be set to debug migrations
+}
+
+// Ensure we migrated from v1 migrations to goose_migrations
+func ensureMigrated(db *sql.DB) {
+	var count int
+	err := db.QueryRow(`SELECT count(*) FROM migrations`).Scan(&count)
+	if err != nil {
+		// already migrated
+		return
+	}
+
+	// ensure a goose migrations table exists with it's initial v0
+	if _, err = goose.GetDBVersion(db); err != nil {
+		panic(err)
+	}
+
+	// insert records for existing migrations
+	sql := `INSERT INTO %s (version_id, is_applied) VALUES %s;`
+
+	valueStrings := []string{}
+	for i := 1; i <= count; i++ {
+		valueStrings = append(valueStrings, fmt.Sprintf("(%v, true)", strconv.FormatInt(int64(i), 10)))
+	}
+	sql = fmt.Sprintf(sql, goose.TableName(), strings.Join(valueStrings, ","))
+
+	err = postgres.SqlTransaction(context.Background(), db, func(tx *sqlx.Tx) error {
+		if _, err = db.Exec(sql); err != nil {
+			return err
+		}
+
+		_, err = db.Exec("DROP TABLE migrations;")
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func Migrate(db *sql.DB) error {
+	ensureMigrated(db)
 	return goose.Up(db, MIGRATIONS_DIR)
 }
 
 func Current(db *sql.DB) (int64, error) {
+	ensureMigrated(db)
 	return goose.EnsureDBVersion(db)
 }
 
 func Status(db *sql.DB) error {
+	ensureMigrated(db)
 	return goose.Status(db, MIGRATIONS_DIR)
 }
-
-// func MigrateDown(db *sql.DB) error {
-// 	return goose.Down(db, MIGRATIONS_DIR)
-// }
