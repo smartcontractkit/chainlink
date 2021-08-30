770c31b066522dca634cb9dac3bd77e58fe250bc
PiotrTrzpil <piotr.trzpil@gmail.com>
2021-08-30 18:14:43 +0200

    lint

--- core/services/webhook/external_initiator_manager.go
+++ core/services/webhook/external_initiator_manager.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/services/job"
 	"go.uber.org/multierr"
