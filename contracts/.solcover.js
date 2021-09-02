a0d21f552243986cbc8d28d900fcc7a03cb54552
Alex Roan <alex.roan@hotmail.com>
2021-09-02 11:04:22 +0100

    Solcover config

--- contracts/.solcover.js
+++ contracts/.solcover.js
@@ -2,9 +2,5 @@ module.exports = {
   mocha: {
     grep: "@skip-coverage", // Find everything with this tag
     invert: true               // Run the grep's inverse set.
-  },
-  skipFiles: [
-    "src/v0.4",
-    "src/v0.5"
-  ]
+  }
 }
