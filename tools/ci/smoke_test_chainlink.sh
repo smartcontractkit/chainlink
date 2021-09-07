f5e90f7b21c3bc59e40f78c621c4b5906b987f6d
Adam Hamrick <adam.hamrick@smartcontract.com>
2021-09-07 11:33:05 -0400

    Try polling

--- tools/ci/smoke_test_chainlink.sh
+++ tools/ci/smoke_test_chainlink.sh
@@ -12,19 +12,30 @@ export APPS_CHAINLINK_VERSION=latest.99415739365570301bb898f4b9c531e75b85a22d
 
 echo "IMAGE: $APPS_CHAINLINK_IMAGE | TAG: $APPS_CHAINLINK_VERSION"
 
-IMAGE_META="$( aws ecr describe-images --repository-name=chainlink --image-ids=imageTag=$APPS_CHAINLINK_VERSION 2> /dev/null )"
+# Need to poll and see if an image for this commit gets pushed to ECR
+# Obviously non-ideal, but having the build-and-push take place in a circle-ci step has introduced a host of issues
+# trying to get this run correctly.
+end_time=$((SECONDS+6))
+while [[ $SECONDS -lt $end_time ]]
+do 
+  IMAGE_META=$( aws ecr describe-images --repository-name=chainlink --image-ids=imageTag=$APPS_CHAINLINK_VERSION )
+  if [[ $? == 0 ]]; then
+    IMAGE_TAGS="$( jq '.imageDetails[0].imageTags[0]' -r < ${IMAGE_META})"
+    echo "$APPS_CHAINLINK_IMAGE:$APPS_CHAINLINK_VERSION found"
+  fi
+  sleep 3
+done
 
-if [[ $? == 0 ]]; then
-  IMAGE_TAGS="$( jq '.imageDetails[0].imageTags[0]' -r < ${IMAGE_META})"
-  echo "$1:$2 found"
-else
-  echo "$1:$2 not found"
-  echo "Not running any tests as no new image for this commit is available"
+if [[ $SECONDS -ge $end_time ]]; then
+  echo "$APPS_CHAINLINK_IMAGE:$APPS_CHAINLINK_VERSION not found after $SECONDS seconds, not running any tests"
   exit 0
 fi
 
-# if [[ $performance == 0 ]]; then
-#   ginkgo -r -p -keepGoing --trace --randomizeAllSpecs -skipPackage=./integration/suite/performance,./integration/suite/chaos ./integration/suite/...
-# else
-#   ginkgo -r -p -keepGoing --trace --randomizeAllSpecs ./integration/suite/performance ./integration/suite/chaos
-# fi
+
+if [[ $performance == 0 ]]; then
+  echo "Running smoke tests"
+  ginkgo -r -p -keepGoing --trace --randomizeAllSpecs -skipPackage=./integration/suite/performance,./integration/suite/chaos ./integration/suite/...
+else
+  echo "Running performance and chaos tests"
+  ginkgo -r -p -keepGoing --trace --randomizeAllSpecs ./integration/suite/performance ./integration/suite/chaos
+fi
