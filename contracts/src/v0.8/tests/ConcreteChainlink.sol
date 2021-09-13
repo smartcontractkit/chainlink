8ea045433957910856bdde296ee73d0156a05cfb
Alex Roan <alex.roan@hotmail.com>
2021-09-13 11:58:27 +0100

    Fix final ConcreteChainlink tests

--- contracts/src/v0.8/tests/ConcreteChainlink.sol
+++ contracts/src/v0.8/tests/ConcreteChainlink.sol
@@ -49,33 +49,9 @@ contract ConcreteChainlink {
 
   // Temporarily have method receive bytes32[] memory until experimental
   // string[] memory can be invoked from truffle tests.
-  function addStringArray(string memory _key, bytes32[] memory _values) public {
-    string[] memory strings = new string[](_values.length);
-    for (uint256 i = 0; i < _values.length; i++) {
-      strings[i] = bytes32ToString(_values[i]);
-    }
+  function addStringArray(string memory _key, string[] memory _values) public {
     Chainlink.Request memory r2 = req;
-    r2.addStringArray(_key, strings);
+    r2.addStringArray(_key, _values);
     req = r2;
-  }
-
-  function bytes32ToString(bytes32 x) private pure returns (string memory) {
-    bytes memory bytesString = new bytes(32);
-    uint charCount = 0;
-    for (uint j = 0; j < 32; j++) {
-      bytes1 char = bytes1(0);
-      unchecked {
-        bytes1(bytes32(uint(x) * 2 ** (8 * j)));
-      }
-      if (char != 0) {
-        bytesString[charCount] = char;
-        charCount++;
-      }
-    }
-    bytes memory bytesStringTrimmed = new bytes(charCount);
-    for (uint j = 0; j < charCount; j++) {
-      bytesStringTrimmed[j] = bytesString[j];
-    }
-    return string(bytesStringTrimmed);
   }
 }
