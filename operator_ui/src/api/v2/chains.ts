2ff9ce187bf5e1b16397478de8c63f667826114f
TJBigelow <tjbigelow@att.net>
2021-09-14 14:58:18 -0500

    Delete chain

--- operator_ui/src/api/v2/chains.ts
+++ operator_ui/src/api/v2/chains.ts
@@ -3,6 +3,7 @@ import { boundMethod } from 'autobind-decorator'
 import * as models from 'core/store/models'
 
 export const ENDPOINT = '/v2/chains/evm'
+const DESTROY_ENDPOINT = `${ENDPOINT}/:chainId`
 
 export class Chains {
   constructor(private api: jsonapi.Api) {}
@@ -19,10 +20,23 @@ export class Chains {
     return this.create(request)
   }
 
+  @boundMethod
+  public destroyChain(id: string): Promise<jsonapi.ApiResponse<null>> {
+    return this.destroy(undefined, { chainId: id })
+  }
+
   private index = this.api.fetchResource<{}, models.Chain[]>(ENDPOINT)
 
   private create = this.api.createResource<
     models.CreateChainRequest,
     models.Chain
   >(ENDPOINT)
+
+  private destroy = this.api.deleteResource<
+    undefined,
+    null,
+    {
+      chainId: string
+    }
+  >(DESTROY_ENDPOINT)
 }
