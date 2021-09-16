ef6aedd16481e0e55f9109138eeb10aa2d5d55fb
Sam <samsondav@protonmail.com>
2021-09-16 11:35:23 +0200

    Use chain-specific config for Keeper (#5014)

--- core/services/chainlink/application.go
+++ core/services/chainlink/application.go
@@ -206,7 +206,6 @@ func NewApplication(opts ApplicationOpts) (Application, error) {
 				jobORM,
 				pipelineRunner,
 				globalLogger,
-				cfg,
 				chainSet),
 			job.VRF: vrf.NewDelegate(
 				db,
