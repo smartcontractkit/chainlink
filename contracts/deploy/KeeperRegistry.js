c19084369dc3468d53fc2ff28ace83dbe7623d2c
behmaroman <begmaroman@gmail.com>
2021-09-20 11:25:58 +0300

    linter

--- contracts/deploy/KeeperRegistry.js
+++ contracts/deploy/KeeperRegistry.js
@@ -1,17 +1,17 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deployer, linkToken, linkEth, fastGas } = await getNamedAccounts()
-  const paymentPremiumPPB = 250000000
-  const blockCountPerTurn = 3
-  const maxCheckGas = 20000000
-  const stalenessSeconds = 43820
-  const gasCeilingMultiplier = 1
-  const fallbackGasPrice = 200
-  const fallbackLinkPrice = 200000000
+  const { deployer, linkToken, linkEth, fastGas } = await getNamedAccounts();
+  const paymentPremiumPPB = 250000000;
+  const blockCountPerTurn = 3;
+  const maxCheckGas = 20000000;
+  const stalenessSeconds = 43820;
+  const gasCeilingMultiplier = 1;
+  const fallbackGasPrice = 200;
+  const fallbackLinkPrice = 200000000;
 
-  await deployments.deploy('KeeperRegistry', {
+  await deployments.deploy("KeeperRegistry", {
     from: deployer,
     log: true,
-    contract: 'KeeperRegistry',
+    contract: "KeeperRegistry",
     args: [
       linkToken,
       linkEth,
@@ -23,6 +23,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       gasCeilingMultiplier,
       fallbackGasPrice,
       fallbackLinkPrice,
-    ]
-  })
-}
+    ],
+  });
+};
