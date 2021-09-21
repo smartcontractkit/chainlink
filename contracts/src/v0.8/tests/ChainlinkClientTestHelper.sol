4ac202d5cf016823d0866ffcbeff2bad679d06b0
Steve Ellis <email@steveell.is>
2021-09-21 12:05:19 -0400

    fix tests after merge

--- contracts/src/v0.8/tests/ChainlinkClientTestHelper.sol
+++ contracts/src/v0.8/tests/ChainlinkClientTestHelper.sol
@@ -68,15 +68,14 @@ contract ChainlinkClientTestHelper is ChainlinkClient {
 
   function publicRequestOracleData(
     bytes32 _id,
-    address _address,
     bytes memory _fulfillmentSignature,
     uint256 _wei
   )
     public
   {
-    Chainlink.Request memory req = buildChainlinkRequest(
-      _id, _address, bytes4(keccak256(_fulfillmentSignature)));
-    requestOracleData(req, _wei);
+    Chainlink.Request memory req = buildOperatorRequest(
+      _id, bytes4(keccak256(_fulfillmentSignature)));
+    sendOperatorRequest(req, _wei);
   }
 
   function publicRequestOracleDataFrom(
@@ -88,8 +87,8 @@ contract ChainlinkClientTestHelper is ChainlinkClient {
   )
     public
   {
-    Chainlink.Request memory run = buildChainlinkRequest(_id, _address, bytes4(keccak256(_fulfillmentSignature)));
-    requestOracleDataFrom(_oracle, run, _wei);
+    Chainlink.Request memory run = buildOperatorRequest(_id, bytes4(keccak256(_fulfillmentSignature)));
+    sendOperatorRequestTo(_oracle, run, _wei);
   }
 
   function publicCancelRequest(
