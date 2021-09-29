e2652af3fee2a7f666ebed115573475edd364fd3
Connor Stein <connor.stein@mail.mcgill.ca>
2021-09-29 02:41:01 -0400

    Scale up request script (#5029)

--- core/scripts/vrfv2/get-randomness/main.go
+++ core/scripts/vrfv2/get-randomness/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/vrf_consumer_v2"
+	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/vrf_coordinator_v2"
 )
 
 func panicErr(err error) {
@@ -19,6 +20,13 @@ func main() {
 	consumerAddress := "0x9E79d9A7F68D136ec4c1C0187B97c271CEa6008B"
 	ec, err := ethclient.Dial("ws://127.0.0.1:8546")
 	panicErr(err)
+	coordinator, err := vrf_coordinator_v2.NewVRFCoordinatorV2(common.HexToAddress("0x569620752AbB8a31AC2832b6680efAFcB399E37e"), ec)
+	panicErr(err)
+	sub, err := coordinator.GetSubscription(nil, 1)
+	fmt.Println(sub, err)
+	c, err := coordinator.GetConfig(nil)
+	fmt.Println(c, err)
+
 	consumer, err := vrf_consumer_v2.NewVRFConsumerV2(common.HexToAddress(consumerAddress), ec)
 	panicErr(err)
 	var rw []*big.Int
