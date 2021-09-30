604d8b80b1f87fe32ceffd3111d6ab843640815b
James Kong <jkongie@gmail.com>
2021-09-30 17:51:16 +0800

    Add a job show command and update the readme with link to job docs

--- core/cmd/remote_client_test.go
+++ core/cmd/remote_client_test.go
@@ -488,12 +488,12 @@ func TestClient_AutoLogin(t *testing.T) {
 	client.HTTP = cmd.NewAuthenticatedHTTPClient(app.Config, client.CookieAuthenticator, sr)
 
 	fs := flag.NewFlagSet("", flag.ExitOnError)
-	err := client.ListJobsV2(cli.NewContext(nil, fs, nil))
+	err := client.ListJobs(cli.NewContext(nil, fs, nil))
 	require.NoError(t, err)
 
 	// Expire the session and then try again
 	require.NoError(t, app.GetDB().Exec("delete from sessions;").Error)
-	err = client.ListJobsV2(cli.NewContext(nil, fs, nil))
+	err = client.ListJobs(cli.NewContext(nil, fs, nil))
 	require.NoError(t, err)
 }
 
@@ -514,7 +514,7 @@ func TestClient_AutoLogin_AuthFails(t *testing.T) {
 	client.HTTP = cmd.NewAuthenticatedHTTPClient(app.Config, client.CookieAuthenticator, sr)
 
 	fs := flag.NewFlagSet("", flag.ExitOnError)
-	err := client.ListJobsV2(cli.NewContext(nil, fs, nil))
+	err := client.ListJobs(cli.NewContext(nil, fs, nil))
 	require.Error(t, err)
 }
 
