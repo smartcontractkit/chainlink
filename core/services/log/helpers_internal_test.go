e7492d57594ddeb9c9069f6075c2db1075e3a16e
Jordan Krage <jmank88@gmail.com>
2021-10-05 18:15:52 -0500

    log broadcaster test races (#5177)
    * broadcaster test races
    
    * testPause/Resume: don't block if stopped

--- core/services/log/helpers_internal_test.go
+++ core/services/log/helpers_internal_test.go
@@ -2,18 +2,33 @@ package log
 
 import (
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/smartcontractkit/chainlink/core/logger"
+	"github.com/smartcontractkit/chainlink/core/services/eth"
 )
+
+// NewTestBroadcaster creates a broadcaster with Pause/Resume enabled.
+func NewTestBroadcaster(orm ORM, ethClient eth.Client, config Config, logger logger.Logger, highestSavedHead *eth.Head) *broadcaster {
+	b := NewBroadcaster(orm, ethClient, config, logger, highestSavedHead)
+	b.testPause, b.testResume = make(chan struct{}), make(chan struct{})
+	return b
+}
 
 func (b *broadcaster) ExportedAppendLogChannel(ch1, ch2 <-chan types.Log) chan types.Log {
 	return b.appendLogChannel(ch1, ch2)
 }
 
-func (b *broadcaster) ExportedGetPoolCount() func() int {
-	return func() int {
-		count := 0
-		for _, logs := range b.logPool.logsByBlockHash {
-			count += len(logs)
-		}
-		return count
+// Pause pauses the eventLoop until Resume is called.
+func (b *broadcaster) Pause() {
+	select {
+	case b.testPause <- struct{}{}:
+	case <-b.chStop:
+	}
+}
+
+// Resume resumes the eventLoop after calling Pause.
+func (b *broadcaster) Resume() {
+	select {
+	case b.testResume <- struct{}{}:
+	case <-b.chStop:
 	}
 }
