d78f7a3562aa13ba2411a0e8f6f16df78f3fa79a
Jordan Krage <jmank88@gmail.com>
2021-10-06 06:25:00 -0500

    fix eventually check

--- core/services/keeper/upkeep_executer_test.go
+++ core/services/keeper/upkeep_executer_test.go
@@ -206,7 +206,7 @@ func Test_UpkeepExecuter_PerformsUpkeep_Error(t *testing.T) {
 	head := newHead()
 	executer.OnNewLongestChain(context.TODO(), head)
 
-	g.Eventually(wasCalled.Load()).Should(gomega.Equal(true))
+	g.Eventually(wasCalled.Load).Should(gomega.Equal(true))
 	cltest.AssertCountStays(t, db, bulletprooftxmanager.EthTx{}, 0)
 	ethMock.AssertExpectations(t)
 }
