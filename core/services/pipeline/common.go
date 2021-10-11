826ba163fd1f0f906807b123ececbf4d7033cb28
Sam <samsondav@protonmail.com>
2021-10-11 13:53:20 +0200

    Fix creation of `merge` jobs (#5205)

--- core/services/pipeline/common.go
+++ core/services/pipeline/common.go
@@ -233,7 +233,7 @@ const (
 	TaskTypeETHABIEncode    TaskType = "ethabiencode"
 	TaskTypeETHABIDecode    TaskType = "ethabidecode"
 	TaskTypeETHABIDecodeLog TaskType = "ethabidecodelog"
-	TaskTypeMergeTask       TaskType = "merge"
+	TaskTypeMerge           TaskType = "merge"
 
 	// Testing only.
 	TaskTypePanic TaskType = "panic"
@@ -295,6 +295,8 @@ func UnmarshalTaskFromMap(taskType TaskType, taskMap interface{}, ID int, dotID 
 		task = &ETHABIDecodeLogTask{BaseTask: BaseTask{id: ID, dotID: dotID}}
 	case TaskTypeCBORParse:
 		task = &CBORParseTask{BaseTask: BaseTask{id: ID, dotID: dotID}}
+	case TaskTypeMerge:
+		task = &MergeTask{BaseTask: BaseTask{id: ID, dotID: dotID}}
 	default:
 		return nil, errors.Errorf(`unknown task type: "%v"`, taskType)
 	}
