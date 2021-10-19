aa92f2c671c2e309d7d3acfeda5db430ac23d019
Yos Riady <yos.riady@smartcontract.com>
2021-10-19 23:45:24 +0800

    Add test contracts, #forward test case

--- contracts/test/v0.8/dev/ArbitrumCrossDomainForwarder.test.ts
+++ contracts/test/v0.8/dev/ArbitrumCrossDomainForwarder.test.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'hardhat'
-import { assert } from 'chai'
+import { assert, expect } from 'chai'
 import { Contract, ContractFactory } from 'ethers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import {
@@ -9,31 +9,47 @@ import {
 } from '../../test-helpers/helpers'
 
 let owner: SignerWithAddress
-// let stranger: SignerWithAddress
+let stranger: SignerWithAddress
 let l1OwnerAddress: string
 let crossdomainMessenger: SignerWithAddress
 let forwarderFactory: ContractFactory
+let greeterFactory: ContractFactory
+// let multisendFactory: ContractFactory
 let forwarder: Contract
+let greeter: Contract
+// let multisend: Contract
 
 before(async () => {
   const accounts = await ethers.getSigners()
   owner = accounts[0]
-  // stranger = accounts[1]
+  stranger = accounts[1]
+
+  // Forwarder config
   l1OwnerAddress = owner.address
   crossdomainMessenger = await impersonateAs(
     toArbitrumL2AliasAddress(l1OwnerAddress),
   )
 
+  // Contract factories
   forwarderFactory = await ethers.getContractFactory(
     'src/v0.8/dev/ArbitrumCrossDomainForwarder.sol:ArbitrumCrossDomainForwarder',
     owner,
   )
+  greeterFactory = await ethers.getContractFactory(
+    'src/v0.8/tests/Greeter.sol:Greeter',
+    owner,
+  )
+  // multisendFactory = await ethers.getContractFactory(
+  //   'src/v0.8/tests/Multisend.sol:Multisend',
+  //   owner,
+  // )
 })
 
 describe('ArbitrumCrossDomainForwarder', () => {
   beforeEach(async () => {
     forwarder = await forwarderFactory.deploy(l1OwnerAddress)
-    forwarder = await forwarder.deployed()
+    greeter = await greeterFactory.deploy()
+    // multisend = await multisendFactory.deploy()
   })
 
   it('has a limited public interface [ @skip-coverage ]', async () => {
@@ -65,6 +81,14 @@ describe('ArbitrumCrossDomainForwarder', () => {
     it('should set the crossdomain messenger correctly', async () => {
       const response = await forwarder.crossDomainMessenger()
       assert.equal(response, crossdomainMessenger.address)
+    })
+  })
+
+  describe('#forward', () => {
+    it('should not be callable by unknown crossdomain messenger address', async () => {
+      await expect(
+        forwarder.connect(stranger).forward(greeter.address, '0x'),
+      ).to.be.revertedWith('Sender is not the L2 messenger')
     })
   })
 
