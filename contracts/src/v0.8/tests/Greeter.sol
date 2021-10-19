706f142e2c21047728945496fe9abca1db476edc
Yos Riady <yos.riady@smartcontract.com>
2021-10-20 00:24:53 +0800

    Complete #forward test case

--- contracts/src/v0.8/tests/Greeter.sol
+++ contracts/src/v0.8/tests/Greeter.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.8.0;
 
 import "../ConfirmedOwner.sol";
 
-contract Greeter is ConfirmedOwner(msg.sender) {
+contract Greeter is ConfirmedOwner {
   string public greeting;
+
+  constructor(address owner) ConfirmedOwner(owner) {}
 
   function setGreeting(string calldata _greeting) external onlyOwner {
     greeting = _greeting;
