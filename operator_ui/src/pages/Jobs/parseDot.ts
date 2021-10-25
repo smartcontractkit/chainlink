cc35d92af8e33cd488ece96547c7c3189dd90360
Vyzaldy Andrés Sanchez <vyzaldysanchez@gmail.com>
2021-10-25 23:39:05 +0200

    Fixes TOML parse on tasks with data attributes having newlines (#5289)
    * Fixes TOML parse on tasks with data attributes having newlines
    
    * Removes log from test

--- operator_ui/src/pages/Jobs/parseDot.ts
+++ operator_ui/src/pages/Jobs/parseDot.ts
@@ -12,7 +12,14 @@ type Edge = {
 }
 
 export function parseDot(dot: string): Stratify[] {
-  const digraph = graphlibDot.read(dot)
+  // We want to permit the use of angle brackets to make the
+  // specs more readable for multi-line task attributes. The backend
+  // dot parsing library supports angle brackets which do not contain
+  // valid HTML inside them, but the frontend dot library graphlibDot does not.
+  // Since the dot parsing on the frontend is merely used to display the graph nodes
+  // its fine to omit the angle bracket attributes.
+  const dotNoAngleBrackets = dot.replace(/\w+\s*=\s*<([^>]|[\r\n])*>/g, '')
+  const digraph = graphlibDot.read(dotNoAngleBrackets)
   const edges = digraph.edges()
 
   return digraph.nodes().map((id: string) => {
