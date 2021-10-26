08cc5de5765092b717c171daf68c1d6720615211
James Kong <james.kong@smartcontract.com>
2021-10-27 00:25:20 +0800

    Refactor bridges GQL queries to be wrapped in payloads (#5295)
    Refactor bridges GQL queries to be wrapped in payloads

---
+++ core/web/loader/getters.go
@@ -0,0 +1,45 @@
+package loader
+
+import (
+	"context"
+	"errors"
+
+	"github.com/graph-gophers/dataloader"
+	"github.com/smartcontractkit/chainlink/core/chains/evm/types"
+)
+
+// GetChainByID fetches the chain by it's id.
+func GetChainByID(ctx context.Context, id string) (*types.Chain, error) {
+	ldr := For(ctx)
+
+	thunk := ldr.ChainsByIDLoader.Load(ctx, dataloader.StringKey(id))
+	result, err := thunk()
+	if err != nil {
+		return nil, err
+	}
+
+	chain, ok := result.(types.Chain)
+	if !ok {
+		return nil, errors.New("invalid type")
+	}
+
+	return &chain, nil
+}
+
+// GetNodesByChainID fetches the nodes for a chain.
+func GetNodesByChainID(ctx context.Context, id string) ([]types.Node, error) {
+	ldr := For(ctx)
+
+	thunk := ldr.NodesByChainIDLoader.Load(ctx, dataloader.StringKey(id))
+	result, err := thunk()
+	if err != nil {
+		return nil, err
+	}
+
+	nodes, ok := result.([]types.Node)
+	if !ok {
+		return nil, errors.New("invalid type")
+	}
+
+	return nodes, nil
+}
