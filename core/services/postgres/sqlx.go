62afe2b78e50e71051f26882880c8611b8bcc3a0
Sam <samsondav@protonmail.com>
2021-11-01 14:10:43 +0000

    Handle errors in transactions properly

--- core/services/postgres/sqlx.go
+++ core/services/postgres/sqlx.go
@@ -3,7 +3,6 @@ package postgres
 import (
 	"context"
 	"database/sql"
-	"fmt"
 
 	"github.com/pkg/errors"
 
@@ -49,38 +48,8 @@ func SqlxTransaction(ctx context.Context, q Queryer, fc func(tx *sqlx.Tx) error,
 	case *sqlx.Tx:
 		// nested transaction: just use the outer transaction
 		err = fc(db)
-
 	case *sqlx.DB:
-		opts := &DefaultSqlTxOptions
-		if len(txOpts) > 0 {
-			opts = &txOpts[0]
-		}
-
-		var tx *sqlx.Tx
-		tx, err = db.BeginTxx(ctx, opts)
-		panicked := false
-
-		defer func() {
-			// Make sure to rollback when panic, Block error or Commit error
-			if panicked || err != nil {
-				if perr := tx.Rollback(); perr != nil {
-					panic(perr)
-				}
-			}
-		}()
-
-		_, err = tx.Exec(fmt.Sprintf(`SET LOCAL lock_timeout = %v; SET LOCAL idle_in_transaction_session_timeout = %v;`, LockTimeout.Milliseconds(), IdleInTxSessionTimeout.Milliseconds()))
-		if err != nil {
-			return errors.Wrap(err, "error setting transaction timeouts")
-		}
-
-		panicked = true
-		err = fc(tx)
-		panicked = false
-
-		if err == nil {
-			err = errors.WithStack(tx.Commit())
-		}
+		err = sqlxTransaction(ctx, db, fc, txOpts...)
 	default:
 		err = errors.Errorf("invalid db type")
 	}
