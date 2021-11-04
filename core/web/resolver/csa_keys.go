8fee773ee73eae46984fe5273216b557a506b9d2
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2021-11-04 00:24:55 -0400

    Fixes `imports` ordering (maybe?)

--- core/web/resolver/csa_keys.go
+++ core/web/resolver/csa_keys.go
@@ -1,10 +1,10 @@
 package resolver
 
 import (
+	"github.com/pkg/errors"
+
 	"github.com/smartcontractkit/chainlink/core/services/keystore"
 	"github.com/smartcontractkit/chainlink/core/services/keystore/keys/csakey"
-
-	"github.com/pkg/errors"
 )
 
 // CSAKeyResolver resolves the single CSA Key object
