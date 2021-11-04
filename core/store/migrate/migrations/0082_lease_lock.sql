e1c5d909eb73806e2a9feab6e9921ea97ea6465c
Sam <samsondav@protonmail.com>
2021-11-04 21:11:38 +0100

    Allow lease lock on unmigrated table (#5368)
    * Allow lease log on unmigrated table
    
    * Add Jordan to store codeowners

--- core/store/migrate/migrations/0082_lease_lock.sql
+++ core/store/migrate/migrations/0082_lease_lock.sql
@@ -1,10 +1,10 @@
 -- +goose Up
-CREATE TABLE lease_lock (
+CREATE TABLE IF NOT EXISTS lease_lock (
     client_id uuid NOT NULL,
     expires_at timestamptz NOT NULL
 );
 
-CREATE UNIQUE INDEX only_one_lease_lock ON lease_lock ((client_id IS NOT NULL));
+CREATE UNIQUE INDEX IF NOT EXISTS only_one_lease_lock ON lease_lock ((client_id IS NOT NULL));
 
 -- +goose Down
 DROP TABLE lease_lock;
