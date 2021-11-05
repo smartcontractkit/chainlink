5eeace19be582558186f5d80856fc4cbf785cba8
DeividasK (YubiKey 5 Nano) <deividas.karzinauskas@gmail.com>
2021-11-05 11:11:19 +0000

    Type return and remove redundant comments

--- operator_ui/support/test-utils.tsx
+++ operator_ui/support/test-utils.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
-import { render, RenderOptions } from '@testing-library/react'
+import { render, RenderOptions, RenderResult } from '@testing-library/react'
 
 import { MuiThemeProvider } from '@material-ui/core/styles'
 
@@ -9,7 +9,6 @@ import createStore from 'src/createStore'
 import Notifications from 'pages/Notifications'
 import { theme } from 'src/theme'
 
-// AllTheProviders wraps the ui with our providers
 const AllTheProviders: React.FC = ({ children }) => {
   return (
     <MuiThemeProvider theme={theme}>
@@ -18,14 +17,14 @@ const AllTheProviders: React.FC = ({ children }) => {
   )
 }
 
-// customRender wraps the render method with the providers.
-//
-// https://testing-library.com/docs/react-testing-library/setup/#custom-render
 const customRender = (
   ui: React.ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: AllTheProviders, ...options })
-
+): RenderResult =>
+  render(ui, {
+    wrapper: AllTheProviders,
+    ...options,
+  })
 interface RenderWithRouterProps {
   initialEntries?: string[]
 }
@@ -35,13 +34,11 @@ interface RenderWithRouterProps {
 //
 // Use this when you need a router in your tests for page components and any
 // component which uses react-router hooks.
-//
-// TODO - Could not figure out how to type the return.
-const renderWithRouter: any = (
+const renderWithRouter = (
   ui: React.ReactElement,
   { initialEntries }: RenderWithRouterProps = { initialEntries: ['/'] },
   options?: Omit<RenderOptions, 'wrapper'>,
-) => {
+): RenderResult => {
   return {
     ...customRender(
       <MemoryRouter initialEntries={initialEntries}>
