a6390c058dc92000ec6ebb420e1f46e19b348c54
Sam <samsondav@protonmail.com>
2021-11-08 16:03:11 +0000

    Remove panic recovers

--- core/gracefulpanic/recover.go
+++ core/gracefulpanic/recover.go
@@ -1,16 +1,6 @@
 package gracefulpanic
 
-import (
-	"runtime/debug"
-
-	"github.com/smartcontractkit/chainlink/core/logger"
-)
-
 func WrapRecover(fn func()) {
-	defer func() {
-		if err := recover(); err != nil {
-			logger.Default.Errorw("goroutine panicked", "panic", err, "stacktrace", string(debug.Stack()))
-		}
-	}()
+	// Hotfix: removed recover to ease debugging
 	fn()
 }
