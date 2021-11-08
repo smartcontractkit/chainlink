843e0d5daef53d5a6725e2a0a5d647a5f1405a1c
Yos Riady <yos.riady@smartcontract.com>
2021-11-08 17:38:38 +0800

    Add missing docstring

--- contracts/src/v0.8/dev/interfaces/CrossDomainOwnableInterface.sol
+++ contracts/src/v0.8/dev/interfaces/CrossDomainOwnableInterface.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @title CrossDomainOwnableInterface - A contract with helpers for cross-domain contract ownership
 interface CrossDomainOwnableInterface {
   function l1Owner() external returns (address);
 
