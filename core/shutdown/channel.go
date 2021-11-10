fc94364fa46bf0d7f36ab4407a124a4530539fa2
Sam <samsondav@protonmail.com>
2021-11-10 14:14:16 +0000

    Remove unused Panic
    Closes: #5384
    Approved by: samsondav

--- core/shutdown/channel.go
+++ core/shutdown/channel.go
@@ -7,21 +7,12 @@ import (
 	"syscall"
 )
 
-var HardPanic bool
-
-func init() {
-	if os.Getenv("ENABLE_HARD_PANIC") == "true" {
-		HardPanic = true
-	}
-}
-
 type signal struct {
 	ch       chan struct{}
 	stopOnce sync.Once
 }
 
 type Signal interface {
-	Panic()
 	Wait() <-chan struct{}
 }
 
@@ -42,13 +33,6 @@ func (p *signal) Stop() {
 	p.stopOnce.Do(func() {
 		close(p.ch)
 	})
-}
-
-func (p *signal) Panic() {
-	if HardPanic {
-		panic("panic")
-	}
-	p.Stop()
 }
 
 func (p *signal) Wait() <-chan struct{} {
