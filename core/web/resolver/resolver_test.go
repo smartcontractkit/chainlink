c4cb29c850603a15bef048d00d3805eb32b4e7ea
Makram Kamaleddine <makramkd@users.noreply.github.com>
2021-11-10 10:30:28 -0500

    Implement P2P key querying and mutation

--- core/web/resolver/resolver_test.go
+++ core/web/resolver/resolver_test.go
@@ -32,6 +32,7 @@ type mocks struct {
 	ocr       *keystoreMocks.OCR
 	csa       *keystoreMocks.CSA
 	keystore  *keystoreMocks.Master
+	p2p       *keystoreMocks.P2P
 }
 
 // gqlTestFramework is a framework wrapper containing the objects needed to run
@@ -75,6 +76,7 @@ func setupFramework(t *testing.T) *gqlTestFramework {
 		ocr:       &keystoreMocks.OCR{},
 		csa:       &keystoreMocks.CSA{},
 		keystore:  &keystoreMocks.Master{},
+		p2p:       &keystoreMocks.P2P{},
 	}
 
 	// Assert expectations for any mocks that we set up
@@ -89,6 +91,7 @@ func setupFramework(t *testing.T) *gqlTestFramework {
 			m.ocr,
 			m.csa,
 			m.keystore,
+			m.p2p,
 		)
 	})
 
