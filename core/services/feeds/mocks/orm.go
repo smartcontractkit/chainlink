a16ec7d762ad8b1ef9da25336012099a7485e073
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2021-11-11 18:30:24 -0400

    Fixes compiling issues from using sqlx

--- core/services/feeds/mocks/orm.go
+++ core/services/feeds/mocks/orm.go
@@ -218,6 +218,29 @@ func (_m *ORM) GetManager(id int64) (*feeds.FeedsManager, error) {
 	return r0, r1
 }
 
+// GetManagers provides a mock function with given fields: ids
+func (_m *ORM) GetManagers(ids []int64) ([]feeds.FeedsManager, error) {
+	ret := _m.Called(ids)
+
+	var r0 []feeds.FeedsManager
+	if rf, ok := ret.Get(0).(func([]int64) []feeds.FeedsManager); ok {
+		r0 = rf(ids)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]feeds.FeedsManager)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func([]int64) error); ok {
+		r1 = rf(ids)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // IsJobManaged provides a mock function with given fields: jobID, qopts
 func (_m *ORM) IsJobManaged(jobID int64, qopts ...postgres.QOpt) (bool, error) {
 	_va := make([]interface{}, len(qopts))
