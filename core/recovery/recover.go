c4a654c054fb192060c28900dd546d24ff1c39fb
Jordan Krage <jmank88@gmail.com>
2021-11-17 14:06:26 -0600

    restore WrapRecover

---
+++ core/recovery/recover.go
@@ -0,0 +1,16 @@
+package recovery
+
+import (
+	"runtime/debug"
+
+	"github.com/smartcontractkit/chainlink/core/logger"
+)
+
+func WrapRecover(lggr logger.Logger, fn func()) {
+	defer func() {
+		if err := recover(); err != nil {
+			lggr.Errorw("goroutine panicked", "panic", err, "stacktrace", string(debug.Stack()))
+		}
+	}()
+	fn()
+}
