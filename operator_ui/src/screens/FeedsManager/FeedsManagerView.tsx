6d3b6747283c34589589ca8bacaa8f1142c603bf
James Kong <james.kong@smartcontract.com>
2021-11-18 21:23:30 +0800

    Fix the typecheck caused by an API update (#5465)

--- operator_ui/src/screens/FeedsManager/FeedsManagerView.tsx
+++ operator_ui/src/screens/FeedsManager/FeedsManagerView.tsx
@@ -3,11 +3,10 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 
 import { FeedsManagerCard } from './FeedsManagerCard'
-import { FeedsManager } from 'types/generated/graphql'
 import { JobProposalsCard } from './JobProposalsCard'
 
 interface Props {
-  manager: FeedsManager
+  manager: any // This is a hack to fix the typecheck until we can merged the fix
 }
 
 export const FeedsManagerView: React.FC<Props> = ({ manager }) => {
