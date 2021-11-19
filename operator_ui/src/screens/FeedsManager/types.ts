039ea724c70029ac03cddc0a96666eeda01ddc56
James Kong <james.kong@smartcontract.com>
2021-11-20 02:36:19 +0800

    Co-locate GQL fragments in Operator UI for the feeds manager page (#5482)

--- operator_ui/src/screens/FeedsManager/types.ts
+++
@@ -1,9 +0,0 @@
-// FetchResults types the results of fetching all the feed managers
-export type FetchResults =
-  FetchFeedManagersWithProposals['feedsManagers']['results']
-// FeedsManager types a feedsManager result
-export type FeedsManager = FetchResults[number]
-// JobProposals types the job proposals field of a feeds manager
-export type JobProposals = FeedsManager['jobProposals']
-// JobProposals types a job proposal
-export type JobProposal = JobProposals[number]
