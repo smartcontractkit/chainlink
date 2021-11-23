636d11c83964058cd299372f2f6f39af17bcdd86
Makram Kamaleddine <makramkd@users.noreply.github.com>
2021-11-23 09:07:50 -0500

    Speed up get resp count query (#5497)
    * Cherry picked from #5451

--- core/services/vrf/delegate.go
+++ core/services/vrf/delegate.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
+
 	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/solidity_vrf_coordinator_interface"
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/services/eth"
@@ -117,7 +118,7 @@ func (d *Delegate) ServicesForSpec(jb job.Job) ([]job.Service, error) {
 				reqLogs:            utils.NewHighCapacityMailbox(),
 				chStop:             make(chan struct{}),
 				waitOnStop:         make(chan struct{}),
-				respCount:          GetStartingResponseCountsV2(d.db, lV2),
+				respCount:          GetStartingResponseCountsV2(d.db, lV2, chain.Client().ChainID().Uint64(), chain.Config().EvmFinalityDepth()),
 				blockNumberToReqID: pairing.New(),
 				reqAdded:           func() {},
 			}}, nil
@@ -143,7 +144,7 @@ func (d *Delegate) ServicesForSpec(jb job.Job) ([]job.Service, error) {
 				chStop:             make(chan struct{}),
 				waitOnStop:         make(chan struct{}),
 				newHead:            make(chan struct{}, 1),
-				respCount:          getStartingResponseCounts(d.db, lV1),
+				respCount:          GetStartingResponseCountsV1(d.db, lV1, chain.Client().ChainID().Uint64(), chain.Config().EvmFinalityDepth()),
 				blockNumberToReqID: pairing.New(),
 				reqAdded:           func() {},
 			}}, nil
@@ -152,24 +153,17 @@ func (d *Delegate) ServicesForSpec(jb job.Job) ([]job.Service, error) {
 	return nil, errors.New("invalid job spec expected a vrf task")
 }
 
-type scanStartingResponseCountsCallback func(b []byte, count uint64)
+func GetStartingResponseCountsV1(db *sqlx.DB, l logger.Logger, chainID uint64, evmFinalityDepth uint32) map[[32]byte]uint64 {
+	respCounts := map[[32]byte]uint64{}
 
-func scanStartingResponseCounts(db *sqlx.DB, l logger.Logger, cb scanStartingResponseCountsCallback) {
-	var counts []struct {
-		RequestID string
-		Count     int
-	}
-	// Allow any state, not just confirmed, on purpose.
-	// We assume once a ethtx is queued it will go through.
-	err := db.Select(&counts, `SELECT meta->'RequestID' AS request_id, count(meta->'RequestID') as count
-			FROM eth_txes
-			WHERE meta->'RequestID' IS NOT NULL
-			GROUP BY meta->'RequestID'`)
+	// Only check as far back as the evm finality depth for completed transactions.
+	counts, err := getRespCounts(db, chainID, evmFinalityDepth)
 	if err != nil {
 		// Continue with an empty map, do not block job on this.
-		l.Errorw("Unable to read previous fulfillments", "err", err)
-		return
+		l.Errorw("Unable to read previous confirmed fulfillments", "err", err)
+		return respCounts
 	}
+
 	for _, c := range counts {
 		// Remove the quotes from the json
 		req := strings.Replace(c.RequestID, `"`, ``, 2)
@@ -179,25 +173,80 @@ func scanStartingResponseCounts(db *sqlx.DB, l logger.Logger, cb scanStartingRes
 			l.Errorw("Unable to read fulfillment", "err", err, "reqID", c.RequestID)
 			continue
 		}
-		cb(b, uint64(c.Count))
-	}
-}
-
-func getStartingResponseCounts(db *sqlx.DB, l logger.Logger) map[[32]byte]uint64 {
-	respCounts := make(map[[32]byte]uint64)
-	scanStartingResponseCounts(db, l, func(b []byte, count uint64) {
 		var reqID [32]byte
 		copy(reqID[:], b)
-		respCounts[reqID] = count
-	})
+		respCounts[reqID] = uint64(c.Count)
+	}
+
 	return respCounts
 }
 
-func GetStartingResponseCountsV2(db *sqlx.DB, l logger.Logger) map[string]uint64 {
-	respCounts := make(map[string]uint64)
-	scanStartingResponseCounts(db, l, func(b []byte, count uint64) {
+func GetStartingResponseCountsV2(
+	db *sqlx.DB,
+	l logger.Logger,
+	chainID uint64,
+	evmFinalityDepth uint32,
+) map[string]uint64 {
+	respCounts := map[string]uint64{}
+
+	// Only check as far back as the evm finality depth for completed transactions.
+	counts, err := getRespCounts(db, chainID, evmFinalityDepth)
+	if err != nil {
+		// Continue with an empty map, do not block job on this.
+		l.Errorw("Unable to read previous confirmed fulfillments", "err", err)
+		return respCounts
+	}
+
+	for _, c := range counts {
+		// Remove the quotes from the json
+		req := strings.Replace(c.RequestID, `"`, ``, 2)
+		// Remove the 0x prefix
+		b, err := hex.DecodeString(req[2:])
+		if err != nil {
+			l.Errorw("Unable to read fulfillment", "err", err, "reqID", c.RequestID)
+			continue
+		}
 		bi := new(big.Int).SetBytes(b)
-		respCounts[bi.String()] = count
-	})
+		respCounts[bi.String()] = uint64(c.Count)
+	}
 	return respCounts
+}
+
+func getRespCounts(db *sqlx.DB, chainID uint64, evmFinalityDepth uint32) (
+	[]struct {
+		RequestID string
+		Count     int
+	},
+	error,
+) {
+	counts := []struct {
+		RequestID string
+		Count     int
+	}{}
+	// This query should use the idx_eth_txes_state_from_address_evm_chain_id
+	// index, since the quantity of unconfirmed/unstarted/in_progress transactions _should_ be small
+	// relative to the rest of the data.
+	unconfirmedQuery := `
+SELECT meta->'RequestID' AS request_id, count(meta->'RequestID') AS count
+FROM eth_txes et
+WHERE et.meta->'RequestID' IS NOT NULL
+AND et.state IN ('unconfirmed', 'unstarted', 'in_progress')
+GROUP BY meta->'RequestID'
+	`
+	// Fetch completed transactions only as far back as the given cutoffBlockNumber. This avoids
+	// a table scan of the eth_txes table, which could be large if it is unpruned.
+	confirmedQuery := `
+SELECT meta->'RequestID' AS request_id, count(meta->'RequestID') AS count
+FROM eth_txes et JOIN eth_tx_attempts eta on et.id = eta.eth_tx_id
+	join eth_receipts er on eta.hash = er.tx_hash
+WHERE et.meta->'RequestID' is not null
+AND er.block_number >= (SELECT number FROM heads WHERE evm_chain_id = $1 ORDER BY number DESC LIMIT 1) - $2
+GROUP BY meta->'RequestID'
+	`
+	query := unconfirmedQuery + "\nUNION ALL\n" + confirmedQuery
+	err := db.Select(&counts, query, chainID, evmFinalityDepth)
+	if err != nil {
+		return nil, err
+	}
+	return counts, nil
 }
--- core/services/vrf/delegate_test.go
+++ core/services/vrf/delegate_test.go
@@ -3,7 +3,6 @@ package vrf
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"math/big"
 	"testing"
 	"time"
@@ -25,7 +24,6 @@ import (
 	"github.com/smartcontractkit/chainlink/core/services/keystore/keys/vrfkey"
 	"github.com/smartcontractkit/chainlink/core/services/log"
 	log_mocks "github.com/smartcontractkit/chainlink/core/services/log/mocks"
-	"github.com/smartcontractkit/chainlink/core/services/pg/datatypes"
 	"github.com/smartcontractkit/chainlink/core/services/pipeline"
 	"github.com/smartcontractkit/chainlink/core/services/signatures/secp256k1"
 	"github.com/smartcontractkit/chainlink/core/testdata/testspecs"
@@ -39,7 +37,6 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/theodesp/go-heaps/pairing"
-	"gopkg.in/guregu/null.v4"
 )
 
 type vrfUniverse struct {
@@ -171,84 +168,6 @@ func setup(t *testing.T) (vrfUniverse, *listenerV1, job.Job) {
 		vuni.txm.AssertExpectations(t)
 	})
 	return vuni, listener, jb
-}
-
-func TestStartingCounts(t *testing.T) {
-	db := pgtest.NewSqlxDB(t)
-	lggr := logger.TestLogger(t)
-	counts := getStartingResponseCounts(db, lggr)
-	assert.Equal(t, 0, len(counts))
-	ks := keystore.New(db, utils.FastScryptParams, lggr)
-	err := ks.Unlock("p4SsW0rD1!@#_")
-	require.NoError(t, err)
-	k, err := ks.Eth().Create(big.NewInt(0))
-	require.NoError(t, err)
-	b := time.Now()
-	n1, n2, n3, n4 := int64(0), int64(1), int64(2), int64(3)
-	m1 := bulletprooftxmanager.EthTxMeta{
-		RequestID: utils.PadByteToHash(0x10),
-	}
-	md1, err := json.Marshal(&m1)
-	require.NoError(t, err)
-	md1_ := datatypes.JSON(md1)
-	m2 := bulletprooftxmanager.EthTxMeta{
-		RequestID: utils.PadByteToHash(0x11),
-	}
-	md2, err := json.Marshal(&m2)
-	md2_ := datatypes.JSON(md2)
-	require.NoError(t, err)
-	var txes = []bulletprooftxmanager.EthTx{
-		{
-			Nonce:          &n1,
-			FromAddress:    k.Address.Address(),
-			Error:          null.String{},
-			BroadcastAt:    &b,
-			CreatedAt:      b,
-			State:          bulletprooftxmanager.EthTxConfirmed,
-			Meta:           &datatypes.JSON{},
-			EncodedPayload: []byte{},
-		},
-		{
-			Nonce:          &n2,
-			FromAddress:    k.Address.Address(),
-			Error:          null.String{},
-			BroadcastAt:    &b,
-			CreatedAt:      b,
-			State:          bulletprooftxmanager.EthTxConfirmed,
-			Meta:           &md1_,
-			EncodedPayload: []byte{},
-		},
-		{
-			Nonce:          &n3,
-			FromAddress:    k.Address.Address(),
-			Error:          null.String{},
-			BroadcastAt:    &b,
-			CreatedAt:      b,
-			State:          bulletprooftxmanager.EthTxConfirmed,
-			Meta:           &md2_,
-			EncodedPayload: []byte{},
-		},
-		{
-			Nonce:          &n4,
-			FromAddress:    k.Address.Address(),
-			Error:          null.String{},
-			BroadcastAt:    &b,
-			CreatedAt:      b,
-			State:          bulletprooftxmanager.EthTxConfirmed,
-			Meta:           &md2_,
-			EncodedPayload: []byte{},
-		},
-	}
-	sql := `INSERT INTO eth_txes (nonce, from_address, to_address, encoded_payload, value, gas_limit, state, created_at, broadcast_at, meta, subject, evm_chain_id, min_confirmations, pipeline_task_run_id, simulate) 
-			VALUES (:nonce, :from_address, :to_address, :encoded_payload, :value, :gas_limit, :state, :created_at, :broadcast_at, :meta, :subject, :evm_chain_id, :min_confirmations, :pipeline_task_run_id, :simulate);`
-	for _, tx := range txes {
-		_, err = db.NamedExec(sql, &tx)
-		require.NoError(t, err)
-	}
-	counts = getStartingResponseCounts(db, logger.TestLogger(t))
-	assert.Equal(t, 2, len(counts))
-	assert.Equal(t, uint64(1), counts[utils.PadByteToHash(0x10)])
-	assert.Equal(t, uint64(2), counts[utils.PadByteToHash(0x11)])
 }
 
 func TestConfirmedLogExtraction(t *testing.T) {
