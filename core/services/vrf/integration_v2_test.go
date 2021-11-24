f22ca616055eeef0615439b584e8a6814a8c5cf8
Jordan Krage <jmank88@gmail.com>
2021-11-23 21:27:19 -0600

    core/services/vrf: use cltest.DefaultWaitTimeout in tests (#5510)

--- core/services/vrf/integration_v2_test.go
+++ core/services/vrf/integration_v2_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/smartcontractkit/chainlink/core/services/pg"
 	"math/big"
 	"strconv"
 	"strings"
@@ -42,6 +41,7 @@ import (
 	"github.com/smartcontractkit/chainlink/core/services/keystore"
 	"github.com/smartcontractkit/chainlink/core/services/keystore/keys/ethkey"
 	"github.com/smartcontractkit/chainlink/core/services/keystore/keys/vrfkey"
+	"github.com/smartcontractkit/chainlink/core/services/pg"
 	"github.com/smartcontractkit/chainlink/core/services/pg/datatypes"
 	"github.com/smartcontractkit/chainlink/core/services/pipeline"
 	"github.com/smartcontractkit/chainlink/core/services/signatures/secp256k1"
@@ -278,7 +278,7 @@ func createJobs(t *testing.T, keys []ethkey.KeyV2, app *cltest.TestApplication, 
 	gomega.NewWithT(t).Eventually(func() bool {
 		jbs := app.JobSpawner().ActiveJobs()
 		return len(jbs) == 2
-	}, 5*time.Second, 100*time.Millisecond).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 100*time.Millisecond).Should(gomega.BeTrue())
 	// Unfortunately the lb needs heads to be able to backfill logs to new subscribers.
 	// To avoid confirming
 	// TODO: it could just backfill immediately upon receiving a new subscriber? (though would
@@ -376,7 +376,7 @@ func TestIntegrationVRFV2_OffchainSimulation(t *testing.T) {
 		require.NoError(t, err)
 		t.Log("runs", len(runs))
 		return len(runs) == (2 + len(uni.vrfConsumers) - 1)
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 
 	// As we send new blocks, we should observe the fulfillments go through and the balance
 	// reduce.
@@ -410,7 +410,7 @@ func TestIntegrationVRFV2_OffchainSimulation(t *testing.T) {
 		t.Log("runs", len(runs))
 		uni.backend.Commit()
 		return len(runs) == (4 + len(uni.vrfConsumers) - 1)
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 	// One more time for the final tx
 	_, err = carolContract.TopUpSubscription(carol, assets.Ether(1))
 	require.NoError(t, err)
@@ -420,7 +420,7 @@ func TestIntegrationVRFV2_OffchainSimulation(t *testing.T) {
 		t.Log("runs", len(runs))
 		uni.backend.Commit()
 		return len(runs) == (5 + len(uni.vrfConsumers) - 1)
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 
 	// Send a huge topup and observe the high max gwei go through.
 	_, err = carolContract.TopUpSubscription(carol, assets.Ether(7))
@@ -431,7 +431,7 @@ func TestIntegrationVRFV2_OffchainSimulation(t *testing.T) {
 		t.Log("runs", len(runs))
 		uni.backend.Commit()
 		return len(runs) == (6 + len(uni.vrfConsumers) - 1)
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 }
 
 func configureSimChain(app *cltest.TestApplication, ks map[string]types.ChainCfg, defaultGasPrice *big.Int) {
@@ -657,7 +657,7 @@ func TestIntegrationVRFV2(t *testing.T) {
 		// keep blocks coming in for the lb to send the backfilled logs.
 		uni.backend.Commit()
 		return len(runs) == 1 && runs[0].State == pipeline.RunStatusCompleted
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 
 	// Wait for the request to be fulfilled on-chain.
 	var rf []*vrf_coordinator_v2.VRFCoordinatorV2RandomWordsFulfilled
@@ -669,7 +669,7 @@ func TestIntegrationVRFV2(t *testing.T) {
 			rf = append(rf, rfIterator.Event)
 		}
 		return len(rf) == 1
-	}, 10*time.Second, 500*time.Millisecond).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 500*time.Millisecond).Should(gomega.BeTrue())
 	assert.True(t, rf[0].Success, "expected callback to succeed")
 	fulfillReceipt, err := uni.backend.TransactionReceipt(context.Background(), rf[0].Raw.TxHash)
 	require.NoError(t, err)
@@ -812,7 +812,7 @@ func TestMaliciousConsumer(t *testing.T) {
 		t.Log("attempts", attempts)
 		uni.backend.Commit()
 		return len(attempts) == 1 && attempts[0].EthTx.State == bulletprooftxmanager.EthTxConfirmed
-	}, 10*time.Second, 1*time.Second).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 1*time.Second).Should(gomega.BeTrue())
 
 	// The fulfillment tx should succeed
 	ch, err := app.GetChainSet().Default()
@@ -1165,7 +1165,7 @@ func FindLatestRandomnessRequestedLog(t *testing.T,
 			rf = append(rf, rfIterator.Event)
 		}
 		return len(rf) >= 1
-	}, 5*time.Second, 500*time.Millisecond).Should(gomega.BeTrue())
+	}, cltest.DefaultWaitTimeout, 500*time.Millisecond).Should(gomega.BeTrue())
 	latest := len(rf) - 1
 	return rf[latest]
 }
