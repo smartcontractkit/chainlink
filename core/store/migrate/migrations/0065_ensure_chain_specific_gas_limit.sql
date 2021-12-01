8a1965ecfd1d0d51b6335574fed4e71c2817057b
Sam <samsondav@protonmail.com>
2021-12-01 09:12:33 -0500

    Avoid chk_chain_specific_gas_limit_not_zero constraint errors migrating on old data

--- core/store/migrate/migrations/0065_ensure_chain_specific_gas_limit.sql
+++ core/store/migrate/migrations/0065_ensure_chain_specific_gas_limit.sql
@@ -1,9 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 
--- NOT VALID is here to grandfather in old attempts which inadvertently saved
--- with a 0 value, but to enforce correctly writing data for future attempts
-ALTER TABLE eth_tx_attempts ADD CONSTRAINT chk_chain_specific_gas_limit_not_zero CHECK (chain_specific_gas_limit > 0) NOT VALID;
+-- Grandfather in old attempts which inadvertently saved with a 0 value, and
+-- enforce correctly writing data for future attempts
+UPDATE eth_tx_attempts SET chain_specific_gas_limit=1 WHERE chain_specific_gas_limit=0
+ALTER TABLE eth_tx_attempts ADD CONSTRAINT chk_chain_specific_gas_limit_not_zero CHECK (chain_specific_gas_limit > 0);
 
 -- +goose StatementEnd
 
