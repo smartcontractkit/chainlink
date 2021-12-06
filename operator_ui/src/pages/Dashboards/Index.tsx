52c415f2e02bdcd2a00c7560b8d2c902d0568890
James Kong <james.kong@smartcontract.com>
2021-12-07 03:43:04 +0800

    Update the Activity card on the dashboard to use GQL (#5612)
    Co-authored-by: Vyzaldy Sanchez <vyzaldysanchez@gmail.com>

--- operator_ui/src/pages/Dashboards/Index.tsx
+++ operator_ui/src/pages/Dashboards/Index.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Grid from '@material-ui/core/Grid'
-import Activity from 'components/Dashboards/Activity'
+
 import TokenBalanceCard from 'components/Cards/TokenBalance'
 import Footer from 'components/Footer'
 import Content from 'components/Content'
 import { fetchAccountBalance } from 'actionCreators'
 import accountBalanceSelector from 'selectors/accountBalance'
-import { v2 } from 'api'
-import { JobRunV2, Resource } from 'core/store/models'
 
+import { Activity } from 'src/screens/Dashboard/Activity'
 import { RecentJobs } from 'src/screens/Dashboard/RecentJobs'
 
 type Props = {
@@ -17,43 +16,22 @@ type Props = {
   recentlyCreatedPageSize: number
 }
 
-const fetchRuns = async (
-  size: number,
-): Promise<{ runs: Resource<JobRunV2>[]; count: number }> => {
-  const response = await v2.runs.getAllJobRuns({ page: 1, size })
-
-  return {
-    runs: response.data as unknown as Resource<JobRunV2>[],
-    count: response.meta.count,
-  }
-}
-
 export const Index = ({
   recentJobRunsCount = 2,
   recentlyCreatedPageSize,
 }: Props) => {
-  const [runs, setRuns] = useState<Resource<JobRunV2>[]>([])
-  const [count, setCount] = useState(0)
   const dispatch = useDispatch()
   const accountBalance = useSelector(accountBalanceSelector)
 
   React.useEffect(() => {
-    document.title = 'Dashboard'
-  }, [])
-
-  React.useEffect(() => {
     dispatch(fetchAccountBalance())
-    fetchRuns(recentJobRunsCount).then((data) => {
-      setRuns(data.runs)
-      setCount(data.count)
-    })
   }, [dispatch, recentlyCreatedPageSize, recentJobRunsCount])
 
   return (
     <Content>
       <Grid container>
         <Grid item xs={8}>
-          <Activity runs={runs} pageSize={recentJobRunsCount} count={count} />
+          <Activity />
         </Grid>
         <Grid item xs={4}>
           <Grid container>
