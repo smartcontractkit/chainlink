31db4578a701b72c61213ccbbb7bd12975c3fe46
connorwstein <connor.stein@mail.mcgill.ca>
2021-12-08 17:49:46 -0500

    Fix relay config marshalling

--- core/web/gqlscalar/map.go
+++ core/web/gqlscalar/map.go
@@ -21,5 +21,7 @@ func (m *Map) UnmarshalGraphQL(input interface{}) error {
 }
 
 func (m Map) MarshalJSON() ([]byte, error) {
-	return json.Marshal(m)
+	// Cast this so we don't have infinite recursion
+	// (don't want json.Marshal calling the MarshalJSON method on m)
+	return json.Marshal(map[string]interface{}(m))
 }
