547336118c08ffaaf19ccf4f983c8729a0591ccc
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2022-01-10 17:37:55 -0400

    Extracts validation functionality into helper function

--- core/web/transfer_controller.go
+++ core/web/transfer_controller.go
@@ -4,7 +4,11 @@ import (
 	"math/big"
 	"net/http"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
+
+	"github.com/smartcontractkit/chainlink/core/assets"
+	"github.com/smartcontractkit/chainlink/core/chains/evm"
 	"github.com/smartcontractkit/chainlink/core/services/chainlink"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/utils"
@@ -46,47 +50,9 @@ func (tc *TransfersController) Create(c *gin.Context) {
 	}
 
 	if !tr.AllowHigherAmounts {
-		var balance *big.Int
-
-		balanceMonitor := chain.BalanceMonitor()
-
-		if balanceMonitor != nil {
-			balance = balanceMonitor.GetEthBalance(tr.FromAddress).ToInt()
-		} else {
-			balance, err = chain.Client().BalanceAt(c, tr.FromAddress, nil)
-			if err != nil {
-				jsonAPIError(c, http.StatusInternalServerError, err)
-				return
-			}
-		}
-
-		zero := big.NewInt(0)
-
-		if balance == nil || balance.Cmp(zero) == 0 {
-			jsonAPIError(c, http.StatusUnprocessableEntity, errors.Errorf("balance is too low for this transaction to be executed: %v", balance))
-			return
-		}
-
-		var gasPrice *big.Int
-
-		gasLimit := chain.Config().EvmGasLimitTransfer()
-		estimator := chain.TxManager().GetGasEstimator()
-
-		gasPrice, gasLimit, err = estimator.GetLegacyGas(nil, gasLimit)
+		err = ValidateEthBalanceForTransfer(c, chain, tr.FromAddress, tr.Amount)
 		if err != nil {
-			jsonAPIError(c, http.StatusUnprocessableEntity, errors.Wrap(err, "failed to estimate gas"))
-			return
-		}
-
-		// Creating a `Big` struct to avoid having a mutation on `tr.Amount` and hence affecting the value stored in the DB
-		amountAsBig := utils.NewBig(tr.Amount.ToInt())
-		fee := new(big.Int).Mul(gasPrice, big.NewInt(int64(gasLimit)))
-
-		amountWithFees := new(big.Int).Add(amountAsBig.ToInt(), fee)
-
-		// ETH balance is less than the sent amount + fees
-		if balance.Cmp(amountWithFees) < 0 {
-			jsonAPIError(c, http.StatusUnprocessableEntity, errors.Errorf("balance is too low for this transaction to be executed: %v", balance))
+			jsonAPIError(c, http.StatusUnprocessableEntity, errors.Errorf("transaction failed: %v", err))
 			return
 		}
 	}
@@ -98,4 +64,51 @@ func (tc *TransfersController) Create(c *gin.Context) {
 	}
 
 	jsonAPIResponse(c, presenters.NewEthTxResource(etx), "eth_tx")
+}
+
+// ValidateEthBalanceForTransfer validates that the current balance can cover the transaction amount
+func ValidateEthBalanceForTransfer(c *gin.Context, chain evm.Chain, fromAddr common.Address, amount assets.Eth) error {
+	var err error
+	var balance *big.Int
+
+	balanceMonitor := chain.BalanceMonitor()
+
+	if balanceMonitor != nil {
+		balance = balanceMonitor.GetEthBalance(fromAddr).ToInt()
+	} else {
+		balance, err = chain.Client().BalanceAt(c, fromAddr, nil)
+		if err != nil {
+			// jsonAPIError(c, http.StatusInternalServerError, err)
+			return err
+		}
+	}
+
+	zero := big.NewInt(0)
+
+	if balance == nil || balance.Cmp(zero) == 0 {
+		// jsonAPIError(c, http.StatusUnprocessableEntity, errors.Errorf("balance is too low for this transaction to be executed: %v", balance))
+		return errors.Errorf("balance is too low for this transaction to be executed: %v", balance)
+	}
+
+	var gasPrice *big.Int
+
+	gasLimit := chain.Config().EvmGasLimitTransfer()
+	estimator := chain.TxManager().GetGasEstimator()
+	gasPrice, gasLimit, err = estimator.GetLegacyGas(nil, gasLimit)
+	if err != nil {
+		// jsonAPIError(c, http.StatusUnprocessableEntity, errors.Wrap(err, "failed to estimate gas"))
+		return errors.Wrap(err, "failed to estimate gas")
+	}
+
+	// Creating a `Big` struct to avoid having a mutation on `tr.Amount` and hence affecting the value stored in the DB
+	amountAsBig := utils.NewBig(amount.ToInt())
+	fee := new(big.Int).Mul(gasPrice, big.NewInt(int64(gasLimit)))
+	amountWithFees := new(big.Int).Add(amountAsBig.ToInt(), fee)
+	if balance.Cmp(amountWithFees) < 0 {
+		// ETH balance is less than the sent amount + fees
+		// jsonAPIError(c, http.StatusUnprocessableEntity, errors.Errorf("balance is too low for this transaction to be executed: %v", balance))
+		return errors.Errorf("balance is too low for this transaction to be executed: %v", balance)
+	}
+
+	return nil
 }
