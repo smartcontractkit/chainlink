362f586c6210afedc303794e0928609d4779627b
aalu1418 <aalu1418@gmail.com>
2022-01-10 08:42:59 -0500

    test clean up

--- core/services/relay/delegate_test.go
+++ core/services/relay/delegate_test.go
@@ -1,7 +1,6 @@
 package relay_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/pelletier/go-toml"
@@ -13,8 +12,8 @@ import (
 	keystoreMock "github.com/smartcontractkit/chainlink/core/services/keystore/mocks"
 	"github.com/smartcontractkit/chainlink/core/services/relay"
 	"github.com/smartcontractkit/sqlx"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 var sampleSolanaSpec = `type = "offchainreporting2"
@@ -47,18 +46,30 @@ func makeOCR2JobSpecFromToml(t *testing.T, jobSpecToml string) job.OffchainRepor
 }
 
 func TestNewOCR2Provider(t *testing.T) {
+	// setup keystore mock
 	solKey := new(keystoreMock.Solana)
 	solKey.On("Get", mock.AnythingOfType("string")).Return(solkey.Key{}, nil)
 
+	// setup solana key mock
 	keystore := new(keystoreMock.Master)
 	keystore.On("Solana").Return(solKey, nil)
 
 	d := relay.NewDelegate(&sqlx.DB{}, keystore, &chainsMock.ChainSet{}, logger.NewLogger())
 
-	spec := makeOCR2JobSpecFromToml(t, sampleSolanaSpec)
-	fmt.Printf("%+v\n", spec)
+	// struct for testing multiple specs
+	specs := []struct {
+		name string
+		spec string
+	}{
+		{"solana", sampleSolanaSpec},
+	}
 
-	_, err := d.NewOCR2Provider(uuid.UUID{}, &spec)
-	require.NoError(t, err)
+	for _, s := range specs {
+		t.Run(s.name, func(t *testing.T) {
+			spec := makeOCR2JobSpecFromToml(t, s.spec)
 
+			_, err := d.NewOCR2Provider(uuid.UUID{}, &spec)
+			require.NoError(t, err)
+		})
+	}
 }
