afa366877bb7bf11d7a9f30035f02eb9d4171b10
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-01-30 09:16:50 +0000

    capitalize HTTP to follow go's guidelines

--- pkg/monitoring/http.go
+++ pkg/monitoring/http.go
@@ -8,14 +8,14 @@ import (
 	"sync"
 )
 
-// HttpServer is the HTTP interface exposed by every monitoring.
+// HTTPServer is the HTTP interface exposed by every monitoring.
 // It's used to export metrics to prometheus, to query the node for configurations, etc.
-type HttpServer interface {
+type HTTPServer interface {
 	Handle(path string, handler http.Handler)
 	Run(ctx context.Context)
 }
 
-func NewHttpServer(baseCtx context.Context, addr string, log Logger) HttpServer {
+func NewHTTPServer(baseCtx context.Context, addr string, log Logger) HTTPServer {
 	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr:    addr,
@@ -45,17 +45,17 @@ func (h *httpServer) Run(ctx context.Context) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		h.log.Debugw("starting http server")
+		h.log.Debugw("starting HTTP server")
 		if err := h.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			h.log.Fatalw("failed to start http server", "address", h.addr, "error", err)
+			h.log.Fatalw("failed to start HTTP server", "address", h.addr, "error", err)
 		} else {
-			h.log.Infow("http server stopped")
+			h.log.Infow("HTTP server stopped")
 		}
 	}()
 	wg.Add(1)
 	defer wg.Done()
 	<-ctx.Done()
 	if err := h.server.Shutdown(ctx); err != nil && !errors.Is(err, context.Canceled) {
-		h.log.Errorw("failed to shut http server down", "error", err)
+		h.log.Errorw("failed to shut HTTP server down", "error", err)
 	}
 }
