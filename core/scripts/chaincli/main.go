5793061cb1746622cf1f76cd355dad3a85b42e79
behmaroman <begmaroman@gmail.com>
2022-02-03 17:56:52 +0200

    Minor structure changes

--- core/scripts/chaincli/main.go
+++ core/scripts/chaincli/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/smartcontractkit/chainlink/core/scripts/chaincli/command"
-	_ "github.com/smartcontractkit/chainlink/core/scripts/chaincli/command/keeper"
 )
 
 func main() {
