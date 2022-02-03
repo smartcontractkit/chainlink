4e33bd654dae2f15ff6630ff4a2ee7ae51a28fe1
behmaroman <begmaroman@gmail.com>
2022-02-03 10:57:09 +0200

    Added comment to exported function

--- core/scripts/chaincli/cliex/flags.go
+++ core/scripts/chaincli/cliex/flags.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/viper"
 )
 
+// BindStringFlag binds the given string flag.
 func BindStringFlag(cmd *cobra.Command, key, flag, short, usage string) {
 	cmd.Flags().StringP(flag, short, "", usage)
 	if err := viper.BindPFlag(key, cmd.Flags().Lookup(flag)); err != nil {
