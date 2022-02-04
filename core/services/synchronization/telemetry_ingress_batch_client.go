eb1d44668049abe27b6c85111030160bf4c55983
Danny Skubak <skubakdj@users.noreply.github.com>
2022-02-04 13:46:02 -0500

    Address feedback, first round

--- core/services/synchronization/telemetry_ingress_batch_client.go
+++ core/services/synchronization/telemetry_ingress_batch_client.go
@@ -1,13 +1,10 @@
 package synchronization
 
 import (
-	"context"
 	"errors"
 	"net/url"
 	"sync"
 	"time"
-
-	"go.uber.org/atomic"
 
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/services"
@@ -24,8 +21,6 @@ import (
 // send telemetry to the ingress server using wsrpc
 type TelemetryIngressBatchClient interface {
 	services.Service
-	Start() error
-	Close() error
 	Send(TelemPayload)
 }
 
@@ -110,6 +105,14 @@ func (tc *telemetryIngressBatchClient) Close() error {
 	})
 }
 
+// Connects to the telemetry ingress server
+//
+// Connection is handled in a goroutine because Dial will block
+// until it can establish a connection. This is important during startup because
+// we do not want to block other services from starting.
+//
+// Eventually when the ingress server does come back up, wsrpc will establish the connection
+// without any interaction on behalf of the node operator.
 func (tc *telemetryIngressBatchClient) connect(clientPrivKey []byte) {
 	tc.wgDone.Add(1)
 
@@ -189,113 +192,4 @@ func (tc *telemetryIngressBatchClient) findOrCreateWorker(payload TelemPayload) 
 	}
 
 	return worker
-}
-
-// telemetryIngressBatchWorker pushes telemetry in batches to the ingress server via wsrpc.
-// A worker is created per ContractID.
-type telemetryIngressBatchWorker struct {
-	telemMaxBatchSize uint
-	telemSendInterval time.Duration
-	telemClient       telemPb.TelemClient
-	wgDone            *sync.WaitGroup
-	chDone            chan struct{}
-	chTelemetry       chan TelemPayload
-	contractID        string
-	logging           bool
-	lggr              logger.Logger
-	dropMessageCount  atomic.Uint32
-}
-
-// NewTelemetryIngressBatchWorker returns a worker for a given contractID that can send
-// telemetry to the ingress server via WSRPC
-func NewTelemetryIngressBatchWorker(
-	telemMaxBatchSize uint,
-	telemSendInterval time.Duration,
-	telemClient telemPb.TelemClient,
-	wgDone *sync.WaitGroup,
-	chDone chan struct{},
-	chTelemetry chan TelemPayload,
-	contractID string,
-	globalLogger logger.Logger,
-	logging bool,
-) *telemetryIngressBatchWorker {
-	return &telemetryIngressBatchWorker{
-		telemSendInterval: telemSendInterval,
-		telemMaxBatchSize: telemMaxBatchSize,
-		telemClient:       telemClient,
-		wgDone:            wgDone,
-		chDone:            chDone,
-		chTelemetry:       chTelemetry,
-		contractID:        contractID,
-		logging:           logging,
-		lggr:              globalLogger.Named("TelemetryIngressBatchWorker"),
-	}
-}
-
-// Start sends batched telemetry to the ingress server on an interval
-func (tw *telemetryIngressBatchWorker) Start() {
-	tw.wgDone.Add(1)
-	sendTicker := time.NewTicker(tw.telemSendInterval)
-
-	go func() {
-		defer tw.wgDone.Done()
-
-		for {
-			select {
-			case <-sendTicker.C:
-				if len(tw.chTelemetry) == 0 {
-					continue
-				}
-
-				// Send batched telemetry to the ingress server, log any errors
-				telemBatchReq := tw.BuildTelemBatchReq()
-				_, err := tw.telemClient.TelemBatch(context.Background(), telemBatchReq)
-
-				if err != nil {
-					tw.lggr.Warnf("Could not send telemetry: %v", err)
-					continue
-				}
-				if tw.logging {
-					tw.lggr.Debugw("Successfully sent telemetry to ingress server", "contractID", telemBatchReq.ContractId, "telemetry", telemBatchReq.Telemetry)
-				}
-			case <-tw.chDone:
-				return
-			}
-		}
-	}()
-}
-
-// logBufferFullWithExpBackoff logs messages at
-// 1
-// 2
-// 4
-// 8
-// 16
-// 32
-// 64
-// 100
-// 200
-// 300
-// etc...
-func (tw *telemetryIngressBatchWorker) logBufferFullWithExpBackoff(payload TelemPayload) {
-	count := tw.dropMessageCount.Inc()
-	if count > 0 && (count%100 == 0 || count&(count-1) == 0) {
-		tw.lggr.Warnw("telemetry ingress client buffer full, dropping message", "telemetry", payload.Telemetry, "droppedCount", count)
-	}
-}
-
-// BuildTelemBatchReq reads telemetry off the worker channel and packages it into a batch request
-func (tw *telemetryIngressBatchWorker) BuildTelemBatchReq() *telemPb.TelemBatchRequest {
-	var telemBatch [][]byte
-
-	// Read telemetry off the channel up to the max batch size
-	for len(tw.chTelemetry) > 0 && len(telemBatch) < int(tw.telemMaxBatchSize) {
-		telemPayload := <-tw.chTelemetry
-		telemBatch = append(telemBatch, telemPayload.Telemetry)
-	}
-
-	return &telemPb.TelemBatchRequest{
-		ContractId: tw.contractID,
-		Telemetry:  telemBatch,
-	}
 }
