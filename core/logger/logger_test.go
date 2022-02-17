a9c75dcb907d1892cd7fef484a6f3d183261209c
Jordan Krage <jmank88@gmail.com>
2022-02-17 09:51:25 -0600

    core/logger: sanitize static Sha and Version for baseLoggerName

--- core/logger/logger_test.go
+++ core/logger/logger_test.go
@@ -17,3 +17,22 @@ func TestConfig(t *testing.T) {
 	assert.False(t, newTestConfig().Development)
 	assert.False(t, newProductionConfig("", false, true, false).Development)
 }
+
+func Test_verShaName(t *testing.T) {
+	for _, tt := range []struct {
+		ver, sha string
+		exp      string
+	}{
+		{"1.0", "1234567890", "1.0@1234567"},
+		{"1", "a", "1@a"},
+		{"", "", "unset@unset"},
+		{"1.0", "", "1.0@unset"},
+		{"", "1234567890", "unset@1234567"},
+	} {
+		t.Run(tt.ver+":"+tt.sha, func(t *testing.T) {
+			if got := verShaName(tt.ver, tt.sha); got != tt.exp {
+				t.Errorf("expected %q but got %q", tt.exp, got)
+			}
+		})
+	}
+}
