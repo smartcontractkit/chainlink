4abdb452da5f484ef6db94d0c696054d10f98139
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2022-02-22 17:38:55 -0400

    Logs which directory is being used if it fails

--- core/logger/zap_disk_logging.go
+++ core/logger/zap_disk_logging.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/smartcontractkit/chainlink/core/utils"
@@ -30,7 +31,7 @@ func newDiskPollConfig(interval time.Duration) zapDiskPollConfig {
 func newDiskCore(cfg ZapLoggerConfig) (zapcore.Core, error) {
 	availableSpace, err := cfg.diskStats.AvailableSpace(cfg.local.Dir)
 	if err != nil {
-		return nil, errors.Wrap(err, "error getting disk space available for logging")
+		return nil, errors.Wrap(err, fmt.Sprintf("error getting disk space available for logging for directory: %s", cfg.local.Dir))
 	}
 	if availableSpace < cfg.local.RequiredDiskSpace {
 		return nil, errors.Errorf(
