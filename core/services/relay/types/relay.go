4b71abe9be15880255e2c0ab8b835cb12871e75a
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-02-25 19:24:51 +0300

    Addressed issues

--- core/services/relay/types/relay.go
+++ core/services/relay/types/relay.go
@@ -26,6 +26,7 @@ type Relayer interface {
 // RelayerCtx is replacing Relayer interface
 type RelayerCtx interface {
 	services.ServiceCtx
+	// NewOCR2Provider creates new OCR2 provider
 	NewOCR2Provider(externalJobID uuid.UUID, spec interface{}) (OCR2ProviderCtx, error)
 }
 
