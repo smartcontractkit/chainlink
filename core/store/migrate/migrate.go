b05b572db418589feb827f6ce0cf6f0fe5fe3a06
chainchad <chad.bean@smartcontract.com>
2022-03-02 14:31:23 -0500

    Fix lint error: G202

--- core/store/migrate/migrate.go
+++ core/store/migrate/migrate.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"embed"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -68,7 +67,7 @@ func ensureMigrated(db *sql.DB, lggr logger.Logger) {
 		panic(err)
 	}
 	// insert records for existing migrations
-	sql := fmt.Sprintf(`INSERT INTO %s (version_id, is_applied) VALUES ($1, true);`, goose.TableName())
+
 	err = pg.SqlTransaction(context.Background(), db, lggr, func(tx *sqlx.Tx) error {
 		for _, name := range names {
 			var id int64
@@ -88,7 +87,7 @@ func ensureMigrated(db *sql.DB, lggr logger.Logger) {
 				}
 			}
 
-			if _, err = db.Exec(sql, id); err != nil {
+			if _, err = db.Exec(`INSERT INTO ? (version_id, is_applied) VALUES ($1, true)`, goose.TableName()); err != nil {
 				return err
 			}
 		}
