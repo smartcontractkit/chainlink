a94af8da5a82090b3bf5f2dbdc2ba47872ff3b4f
Sam <samsondav@protonmail.com>
2022-03-10 15:11:24 -0500

    Fix Deadline/Timeouts when using CombinedContext (#6194)
    * Fix Deadline/Timeouts when using CombinedContext
    
    * Ranem to WithCloseChan
    
    * Rename channels for clarity
    
    * Fix comment

--- core/utils/utils_test.go
+++ core/utils/utils_test.go
@@ -1,7 +1,6 @@
 package utils_test
 
 import (
-	"context"
 	"strings"
 	"sync"
 	"testing"
@@ -390,77 +389,6 @@ func TestEVMBytesToUint64(t *testing.T) {
 
 	// overflows without erroring
 	require.Equal(t, uint64(17), utils.EVMBytesToUint64([]byte{0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11}))
-}
-
-func TestCombinedContext(t *testing.T) {
-	t.Parallel()
-	t.Run("cancels when an inner context is canceled", func(t *testing.T) {
-		innerCtx, innerCancel := context.WithCancel(context.Background())
-		defer innerCancel()
-
-		chStop := make(chan struct{})
-
-		ctx, cancel := utils.CombinedContext(innerCtx, chStop, 1*time.Hour)
-		defer cancel()
-
-		innerCancel()
-
-		select {
-		case <-ctx.Done():
-		case <-time.After(5 * time.Second):
-			t.Fatal("context didn't cancel")
-		}
-	})
-
-	t.Run("cancels when a channel is closed", func(t *testing.T) {
-		innerCtx, innerCancel := context.WithCancel(context.Background())
-		defer innerCancel()
-
-		chStop := make(chan struct{})
-
-		ctx, cancel := utils.CombinedContext(innerCtx, chStop, 1*time.Hour)
-		defer cancel()
-
-		close(chStop)
-
-		select {
-		case <-ctx.Done():
-		case <-time.After(5 * time.Second):
-			t.Fatal("context didn't cancel")
-		}
-	})
-
-	t.Run("cancels when a duration elapses", func(t *testing.T) {
-		innerCtx, innerCancel := context.WithCancel(context.Background())
-		defer innerCancel()
-
-		chStop := make(chan struct{})
-
-		ctx, cancel := utils.CombinedContext(innerCtx, chStop, 1*time.Second)
-		defer cancel()
-
-		select {
-		case <-ctx.Done():
-		case <-time.After(5 * time.Second):
-			t.Fatal("context didn't cancel")
-		}
-	})
-
-	t.Run("doesn't cancel if none of its children cancel", func(t *testing.T) {
-		innerCtx, innerCancel := context.WithCancel(context.Background())
-		defer innerCancel()
-
-		chStop := make(chan struct{})
-
-		ctx, cancel := utils.CombinedContext(innerCtx, chStop, 1*time.Hour)
-		defer cancel()
-
-		select {
-		case <-ctx.Done():
-			t.Fatal("context canceled")
-		case <-time.After(5 * time.Second):
-		}
-	})
 }
 
 func Test_WithJitter(t *testing.T) {
