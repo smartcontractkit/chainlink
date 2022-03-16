a56960e23077b8f9ff7cd39f79be1724909fd9e6
behmaroman <begmaroman@gmail.com>
2022-03-16 11:52:27 +0100

    Minor updates

--- core/scripts/chaincli/command/keeper/upkeep.go
+++ core/scripts/chaincli/command/keeper/upkeep.go
@@ -2,18 +2,12 @@ package keeper
 
 import (
 	"log"
-	"regexp"
 	"strconv"
 
 	"github.com/spf13/cobra"
 
 	"github.com/smartcontractkit/chainlink/core/scripts/chaincli/config"
 	"github.com/smartcontractkit/chainlink/core/scripts/chaincli/handler"
-)
-
-var (
-	// rangeRegexp is the regexp for the block range string format, e.g. 10-100
-	rangeRegexp = regexp.MustCompile(`^([0-9]*)?-?([0-9]*)$`)
 )
 
 // upkeepEventsCmd represents the command to run the upkeep events counter command
@@ -42,10 +36,19 @@ var upkeepHistoryCmd = &cobra.Command{
 	Short: "Print checkUpkeep history",
 	Long:  `Print checkUpkeep status and keeper responsibility for a given upkeep in a set block range`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fromBlock, toBlock := getRange(cmd)
 		upkeepId, err := cmd.Flags().GetInt64("upkeep-id")
 		if err != nil {
-			log.Fatal("failed to get upkeep-id flag: ", err)
+			log.Fatal("failed to get 'upkeep-id' flag: ", err)
+		}
+
+		fromBlock, err := cmd.Flags().GetUint64("from")
+		if err != nil {
+			log.Fatal("failed to get 'from' flag: ", err)
+		}
+
+		toBlock, err := cmd.Flags().GetUint64("to")
+		if err != nil {
+			log.Fatal("failed to get 'to' flag: ", err)
 		}
 
 		cfg := config.New()
@@ -55,37 +58,8 @@ var upkeepHistoryCmd = &cobra.Command{
 	},
 }
 
-func getRange(cmd *cobra.Command) (uint64, uint64) {
-	rangeRaw, err := cmd.Flags().GetString("range")
-	if err != nil {
-		log.Fatal("failed to get range flag: ", err)
-	}
-
-	matches := rangeRegexp.FindStringSubmatch(rangeRaw)
-	if len(matches) != 3 {
-		log.Fatal("unexpected matches: ", matches)
-	}
-
-	var from, to int
-
-	if len(matches[1]) > 0 {
-		from, err = strconv.Atoi(matches[1])
-		if err != nil {
-			log.Fatal("failed to parse from block: ", err)
-		}
-	}
-
-	if len(matches[2]) > 0 {
-		to, err = strconv.Atoi(matches[2])
-		if err != nil {
-			log.Fatal("failed to parse to block: ", err)
-		}
-	}
-
-	return uint64(from), uint64(to)
-}
-
 func init() {
 	upkeepHistoryCmd.Flags().Int64("upkeep-id", 0, "upkeep ID")
-	upkeepHistoryCmd.Flags().String("range", "", "block range")
+	upkeepHistoryCmd.Flags().Uint64("from", 0, "from block")
+	upkeepHistoryCmd.Flags().Uint64("to", 0, "to block")
 }
