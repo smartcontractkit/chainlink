3ce08d9ca2aba70616533251509bec8f3953d68e
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-03-18 15:13:00 +0000

    Fix flakey test

--- core/cmd/eth_keys_commands_test.go
+++ core/cmd/eth_keys_commands_test.go
@@ -257,6 +257,7 @@ func TestClient_ImportExportETHKey_NoChains(t *testing.T) {
 
 	set := flag.NewFlagSet("test", 0)
 	set.String("file", "internal/fixtures/apicredentials", "")
+	set.Bool("bypass-version-check", true, "")
 	c := cli.NewContext(nil, set, nil)
 	err := client.RemoteLogin(c)
 	require.NoError(t, err)
@@ -350,6 +351,7 @@ func TestClient_ImportExportETHKey_WithChains(t *testing.T) {
 
 	set := flag.NewFlagSet("test", 0)
 	set.String("file", "internal/fixtures/apicredentials", "")
+	set.Bool("bypass-version-check", true, "")
 	c := cli.NewContext(nil, set, nil)
 	err := client.RemoteLogin(c)
 	require.NoError(t, err)
