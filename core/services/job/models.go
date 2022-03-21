adc834652ffb8613c8784958296e093cd2b0dc43
Nico Schlumprecht <7006025+Nic0S@users.noreply.github.com>
2022-03-21 12:41:25 -0400

    VRF V1: Retry requests in listener (#6243)
    Add retry logic similar to VRF V2. Requests are retried until
    they are either fulfilled, or until they expire (24 hours by
    default, configurable in the job spec).

--- core/services/job/models.go
+++ core/services/job/models.go
@@ -434,7 +434,7 @@ type VRFSpec struct {
 	PollPeriod               time.Duration        `toml:"pollPeriod"` // For v2 jobs
 	PollPeriodEnv            bool
 	RequestedConfsDelay      int64         `toml:"requestedConfsDelay"` // For v2 jobs. Optional, defaults to 0 if not provided.
-	RequestTimeout           time.Duration `toml:"requestTimeout"`      // For v2 jobs. Optional, defaults to 24hr if not provided.
+	RequestTimeout           time.Duration `toml:"requestTimeout"`      // Optional, defaults to 24hr if not provided.
 	CreatedAt                time.Time     `toml:"-"`
 	UpdatedAt                time.Time     `toml:"-"`
 }
