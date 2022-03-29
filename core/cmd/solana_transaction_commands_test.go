1e81bdbbb7c973fd673be40b0f7643381cf883a6
Aaron Lu <50029043+aalu1418@users.noreply.github.com>
2022-03-29 14:32:15 -0400

    solana nodes chains (#6231)
    * remove terra from chains/nodes + monitor, txm not touched
    
    * solana client + initial balance monitoring
    
    * simple solana txm
    
    * connecting things together
    
    * initial tests + connecting with relay code
    
    * pass solana chains client to relay
    
    * solana nodes/chains CLI subcommands and DB tables
    
    * fix cmd/ewb tests
    
    * point to current solana relay commit
    
    * tx manager unit test
    
    * update relay test
    
    * update sample specs
    
    * placate linter
    
    * fix migration number
    
    * start solana chains service
    
    * naming fixes
    
    * update solana-relay + small fixes
    
    * initial validation for node/endpoint
    
    * test cases for getClient + getOrCreate, clean up duplicate code
    
    * core/cmd: add txs solana create
    
    * document potential values for solana chainId
    
    * clean up solana/chain.go
    
    * fix up cli command - still not working: sends to tx manager, returns hash, but not onchain
    
    * implement gas fee checking for validating balance transfer
    
    * works! wasn't testing properly, added additional logging
    
    * remove nodeName from spec, update balance monitor to use BalancePollPeriod
    
    * address chains feedback + fix blockRate => BalancePollPeriod
    
    * fix relay delegate test case
    
    * fix: solana cli test
    
    * address + add additional comments + fix prom metric
    
    * replace fmt.Errorf => errors.Errorf
    
    * add solana integration tests
    
    * fix test transfer test case + cli responses
    
    * use uuid for solana test chainid
    
    * fix: remove terra from solana related comment
    
    * fix: migration ordering
    
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

---
+++ core/cmd/solana_transaction_commands_test.go
@@ -0,0 +1,112 @@
+//go:build integration
+
+package cmd_test
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/urfave/cli"
+
+	solanaClient "github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
+	solanadb "github.com/smartcontractkit/chainlink-solana/pkg/solana/db"
+	"github.com/smartcontractkit/chainlink/core/internal/testutils"
+	"github.com/smartcontractkit/chainlink/core/web/presenters"
+)
+
+func TestClient_SolanaSendSol(t *testing.T) {
+	chainID := "localnet"
+	url := solanaClient.SetupLocalSolNode(t)
+	app := solanaStartNewApplication(t)
+	from, err := app.GetKeyStore().Solana().Create()
+	require.NoError(t, err)
+	to, err := solana.NewRandomPrivateKey()
+	require.NoError(t, err)
+	solanaClient.FundTestAccounts(t, []solana.PublicKey{from.PublicKey()}, url)
+
+	chains := app.GetChains()
+	_, err = chains.Solana.Add(testutils.Context(t), chainID, solanadb.ChainCfg{})
+	require.NoError(t, err)
+	chain, err := chains.Solana.Chain(testutils.Context(t), chainID)
+	require.NoError(t, err)
+
+	_, err = chains.Solana.ORM().CreateNode(solanadb.NewNode{
+		Name:          t.Name(),
+		SolanaChainID: chainID,
+		SolanaURL:     url,
+	})
+	require.NoError(t, err)
+
+	reader, err := chain.Reader()
+	require.NoError(t, err)
+
+	require.Eventually(t, func() bool {
+		coin, err := reader.Balance(from.PublicKey())
+		if !assert.NoError(t, err) {
+			return false
+		}
+		return coin == 100*solana.LAMPORTS_PER_SOL
+	}, time.Minute, 5*time.Second)
+
+	client, r := app.NewClientAndRenderer()
+	cliapp := cli.NewApp()
+
+	for _, tt := range []struct {
+		amount string
+		expErr string
+	}{
+		{amount: "1000000000"},
+		{amount: "100000000000", expErr: "is too low for this transaction to be executed:"},
+		{amount: "0", expErr: "amount must be greater than zero"},
+		{amount: "asdf", expErr: "invalid amount:"},
+	} {
+		tt := tt
+		t.Run(tt.amount, func(t *testing.T) {
+			startBal, err := reader.Balance(from.PublicKey())
+			require.NoError(t, err)
+
+			set := flag.NewFlagSet("sendsolcoins", 0)
+			set.String("id", chainID, "")
+			set.Parse([]string{tt.amount, from.PublicKey().String(), to.PublicKey().String()})
+			c := cli.NewContext(cliapp, set, nil)
+			err = client.SolanaSendSol(c)
+			if tt.expErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.expErr)
+				return
+			}
+
+			// Check CLI output
+			require.Greater(t, len(r.Renders), 0)
+			renderer := r.Renders[len(r.Renders)-1]
+			renderedMsg := renderer.(*presenters.SolanaMsgResource)
+			fmt.Printf("%+v\n", renderedMsg)
+			require.NotEmpty(t, renderedMsg.ID)
+			assert.Equal(t, chainID, renderedMsg.ChainID)
+			assert.Equal(t, from.PublicKey().String(), renderedMsg.From)
+			assert.Equal(t, to.PublicKey().String(), renderedMsg.To)
+			assert.Equal(t, tt.amount, strconv.FormatUint(renderedMsg.Amount, 10))
+
+      time.Sleep(time.Second) // wait for tx execution
+
+			// Check balance
+			endBal, err := reader.Balance(from.PublicKey())
+			require.NoError(t, err)
+			if assert.NotEqual(t, 0, startBal) && assert.NotEqual(t, 0, endBal) {
+				diff := startBal - endBal
+				receiveBal, err := reader.Balance(to.PublicKey())
+				require.NoError(t, err)
+				assert.Equal(t, tt.amount, strconv.FormatUint(receiveBal, 10))
+				assert.Greater(t, diff, receiveBal)
+			}
+		})
+	}
+}
--- core/main_test.go
+++ core/main_test.go
@@ -447,8 +447,9 @@ func ExampleRun_txs() {
 	//    core.test txs command [command options] [arguments...]
 	//
 	// COMMANDS:
-	//    evm    Commands for handling EVM transactions
-	//    terra  Commands for handling Terra transactions
+	//    evm     Commands for handling EVM transactions
+	//    solana  Commands for handling Solana transactions
+	//    terra   Commands for handling Terra transactions
 	//
 	// OPTIONS:
 	//    --help, -h  show help
@@ -467,6 +468,22 @@ func ExampleRun_txs_evm() {
 	//    create  Send <amount> ETH (or wei) from node ETH account <fromAddress> to destination <toAddress>.
 	//    list    List the Ethereum Transactions in descending order
 	//    show    get information on a specific Ethereum Transaction
+	//
+	// OPTIONS:
+	//    --help, -h  show help
+}
+
+func ExampleRun_txs_solana() {
+	run("txs", "solana", "--help")
+	// Output:
+	// NAME:
+	//    core.test txs solana - Commands for handling Solana transactions
+	//
+	// USAGE:
+	//    core.test txs solana command [command options] [arguments...]
+	//
+	// COMMANDS:
+	//    create  Send <amount> lamports from node Solana account <fromAddress> to destination <toAddress>.
 	//
 	// OPTIONS:
 	//    --help, -h  show help
@@ -498,8 +515,9 @@ func ExampleRun_chains() {
 	//    core.test chains command [command options] [arguments...]
 	//
 	// COMMANDS:
-	//    evm    Commands for handling EVM chains
-	//    terra  Commands for handling Terra chains
+	//    evm     Commands for handling EVM chains
+	//    solana  Commands for handling Solana chains
+	//    terra   Commands for handling Terra chains
 	//
 	// OPTIONS:
 	//    --help, -h  show help
@@ -519,6 +537,25 @@ func ExampleRun_chains_evm() {
 	//    delete     Delete an EVM chain
 	//    list       List all EVM chains
 	//    configure  Configure an EVM chain
+	//
+	// OPTIONS:
+	//    --help, -h  show help
+}
+
+func ExampleRun_chains_solana() {
+	run("chains", "solana", "--help")
+	// Output:
+	// NAME:
+	//    core.test chains solana - Commands for handling Solana chains
+	//
+	// USAGE:
+	//    core.test chains solana command [command options] [arguments...]
+	//
+	// COMMANDS:
+	//    create     Create a new Solana chain
+	//    delete     Delete a Solana chain
+	//    list       List all Solana chains
+	//    configure  Configure a Solana chain
 	//
 	// OPTIONS:
 	//    --help, -h  show help
@@ -553,8 +590,9 @@ func ExampleRun_nodes() {
 	//    core.test nodes command [command options] [arguments...]
 	//
 	// COMMANDS:
-	//    evm    Commands for handling EVM node configuration
-	//    terra  Commands for handling Terra node configuration
+	//    evm     Commands for handling EVM node configuration
+	//    solana  Commands for handling Solana node configuration
+	//    terra   Commands for handling Terra node configuration
 	//
 	// OPTIONS:
 	//    --help, -h  show help
@@ -573,6 +611,24 @@ func ExampleRun_nodes_evm() {
 	//    create  Create a new EVM node
 	//    delete  Delete an EVM node
 	//    list    List all EVM nodes
+	//
+	// OPTIONS:
+	//    --help, -h  show help
+}
+
+func ExampleRun_nodes_solana() {
+	run("nodes", "solana", "--help")
+	// Output:
+	// NAME:
+	//    core.test nodes solana - Commands for handling Solana node configuration
+	//
+	// USAGE:
+	//    core.test nodes solana command [command options] [arguments...]
+	//
+	// COMMANDS:
+	//    create  Create a new Solana node
+	//    delete  Delete a Solana node
+	//    list    List all Solana nodes
 	//
 	// OPTIONS:
 	//    --help, -h  show help
