72dc42e217e50d5cdb137e7f1e75d2cd275b2639
Sam <samsondav@protonmail.com>
2022-03-30 16:06:31 -0400

    Handle duplicate sendonly/primary HTTP URL; EVM_NODES clobbers (#6335)

--- core/chains/evm/legacy_test.go
+++ core/chains/evm/legacy_test.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 
 	"github.com/smartcontractkit/chainlink/core/chains/evm"
 	evmtypes "github.com/smartcontractkit/chainlink/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
+	"github.com/smartcontractkit/chainlink/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/core/internal/testutils/pgtest"
 	"github.com/smartcontractkit/chainlink/core/logger"
 )
@@ -46,55 +48,77 @@ func (c legacyEthNodeConfig) EthereumNodes() string {
 func (c legacyEthNodeConfig) LogSQL() bool { return false }
 
 func Test_ClobberDBFromEnv(t *testing.T) {
-	db := pgtest.NewSqlxDB(t)
 	var fixtureChains int64 = 2
 	var fixtureNodes int64 = 1
 
-	cfg := legacyEthNodeConfig{
-		defaultChainID:        big.NewInt(42),
-		ethereumURL:           "ws://example.com/foo/ws",
-		ethereumHTTPURL:       cltest.MustParseURL(t, "http://example.com/foo"),
-		ethereumSecondaryURLs: []url.URL{*cltest.MustParseURL(t, "http://secondary1.example/foo"), *cltest.MustParseURL(t, "https://secondary2.example/bar")},
-	}
+	t.Run("inserts nodes from config", func(t *testing.T) {
+		db := pgtest.NewSqlxDB(t)
 
-	err := evm.ClobberDBFromEnv(db, cfg, logger.TestLogger(t))
-	require.NoError(t, err)
+		cfg := legacyEthNodeConfig{
+			defaultChainID:        big.NewInt(42),
+			ethereumURL:           "ws://example.com/foo/ws",
+			ethereumHTTPURL:       cltest.MustParseURL(t, "http://example.com/foo"),
+			ethereumSecondaryURLs: []url.URL{*cltest.MustParseURL(t, "http://secondary1.example/foo"), *cltest.MustParseURL(t, "https://secondary2.example/bar")},
+		}
 
-	cltest.AssertCount(t, db, "evm_chains", fixtureChains+1)
-	cltest.AssertCount(t, db, "evm_nodes", fixtureNodes+3)
+		err := evm.ClobberDBFromEnv(db, cfg, logger.TestLogger(t))
+		require.NoError(t, err)
 
-	var primaryNode evmtypes.Node
-	err = db.Get(&primaryNode, `SELECT * FROM evm_nodes WHERE evm_chain_id = 42 AND NOT send_only`)
-	require.NoError(t, err)
+		cltest.AssertCount(t, db, "evm_chains", fixtureChains+1)
+		cltest.AssertCount(t, db, "evm_nodes", fixtureNodes+3)
 
-	assert.Equal(t, "primary-0-42", primaryNode.Name)
-	assert.Equal(t, cfg.defaultChainID.String(), primaryNode.EVMChainID.String())
-	assert.True(t, primaryNode.WSURL.Valid)
-	assert.Equal(t, cfg.ethereumURL, primaryNode.WSURL.String)
-	assert.True(t, primaryNode.HTTPURL.Valid)
-	assert.Equal(t, cfg.ethereumHTTPURL.String(), primaryNode.HTTPURL.String)
-	assert.False(t, primaryNode.SendOnly)
+		var primaryNode evmtypes.Node
+		err = db.Get(&primaryNode, `SELECT * FROM evm_nodes WHERE evm_chain_id = 42 AND NOT send_only`)
+		require.NoError(t, err)
 
-	var sendonlyNodes []evmtypes.Node
-	err = db.Select(&sendonlyNodes, `SELECT * FROM evm_nodes WHERE evm_chain_id = 42 AND send_only ORDER BY http_url`)
-	require.NoError(t, err)
-	require.Len(t, sendonlyNodes, 2)
+		assert.Equal(t, "primary-0-42", primaryNode.Name)
+		assert.Equal(t, cfg.defaultChainID.String(), primaryNode.EVMChainID.String())
+		assert.True(t, primaryNode.WSURL.Valid)
+		assert.Equal(t, cfg.ethereumURL, primaryNode.WSURL.String)
+		assert.True(t, primaryNode.HTTPURL.Valid)
+		assert.Equal(t, cfg.ethereumHTTPURL.String(), primaryNode.HTTPURL.String)
+		assert.False(t, primaryNode.SendOnly)
 
-	assert.True(t, sendonlyNodes[0].SendOnly)
-	assert.Equal(t, "sendonly-0-42", sendonlyNodes[0].Name)
-	assert.False(t, sendonlyNodes[0].WSURL.Valid)
-	assert.True(t, sendonlyNodes[0].HTTPURL.Valid)
-	assert.Equal(t, "http://secondary1.example/foo", sendonlyNodes[0].HTTPURL.String)
+		var sendonlyNodes []evmtypes.Node
+		err = db.Select(&sendonlyNodes, `SELECT * FROM evm_nodes WHERE evm_chain_id = 42 AND send_only ORDER BY http_url`)
+		require.NoError(t, err)
+		require.Len(t, sendonlyNodes, 2)
 
-	assert.True(t, sendonlyNodes[1].SendOnly)
-	assert.Equal(t, "sendonly-1-42", sendonlyNodes[1].Name)
-	assert.False(t, sendonlyNodes[1].WSURL.Valid)
-	assert.True(t, sendonlyNodes[1].HTTPURL.Valid)
-	assert.Equal(t, "https://secondary2.example/bar", sendonlyNodes[1].HTTPURL.String)
+		assert.True(t, sendonlyNodes[0].SendOnly)
+		assert.Equal(t, "sendonly-0-42", sendonlyNodes[0].Name)
+		assert.False(t, sendonlyNodes[0].WSURL.Valid)
+		assert.True(t, sendonlyNodes[0].HTTPURL.Valid)
+		assert.Equal(t, "http://secondary1.example/foo", sendonlyNodes[0].HTTPURL.String)
+
+		assert.True(t, sendonlyNodes[1].SendOnly)
+		assert.Equal(t, "sendonly-1-42", sendonlyNodes[1].Name)
+		assert.False(t, sendonlyNodes[1].WSURL.Valid)
+		assert.True(t, sendonlyNodes[1].HTTPURL.Valid)
+		assert.Equal(t, "https://secondary2.example/bar", sendonlyNodes[1].HTTPURL.String)
+	})
+
+	t.Run("ignores sendonlys if they duplicate the primary URL", func(t *testing.T) {
+		db := pgtest.NewSqlxDB(t)
+		cfg := legacyEthNodeConfig{
+			defaultChainID:        big.NewInt(42),
+			ethereumURL:           "ws://example.com/foo/ws",
+			ethereumHTTPURL:       cltest.MustParseURL(t, "http://example.com/foo"),
+			ethereumSecondaryURLs: []url.URL{*cltest.MustParseURL(t, "http://example.com/foo"), *cltest.MustParseURL(t, "https://secondary2.example/bar")},
+		}
+
+		lggr, observedLogs := logger.TestLoggerObserved(t, zap.ErrorLevel)
+		err := evm.ClobberDBFromEnv(db, cfg, lggr)
+		require.NoError(t, err)
+
+		testutils.RequireLogMessage(t, observedLogs, "Got secondary URL http://example.com/foo which is already specified as a primary node HTTP URL. It does not make any sense to have both primary and sendonly nodes sharing the same URL and does not grant any extra redundancy. This sendonly RPC url will be ignored.")
+	})
 }
 
 func Test_SetupMultiplePrimaries(t *testing.T) {
 	db := pgtest.NewSqlxDB(t)
+
+	// Insert existing node which will be erased
+	pgtest.MustExec(t, db, `INSERT INTO evm_nodes (name, evm_chain_id, ws_url, http_url, send_only, created_at, updated_at) VALUES ('foo','0','ws://example.com',null,false,NOW(),NOW())`)
 
 	s := `
 [
@@ -165,20 +189,19 @@ func Test_SetupMultiplePrimaries(t *testing.T) {
 	err := evm.ClobberDBFromEnv(db, cfg, logger.TestLogger(t))
 	require.NoError(t, err)
 
-	cltest.AssertCount(t, db, "evm_nodes", 8) // 7 inserted plus 1 fixture node
+	cltest.AssertCount(t, db, "evm_nodes", 7)
 
 	var nodes []evmtypes.Node
 	err = db.Select(&nodes, `SELECT * FROM evm_nodes ORDER BY name ASC`)
 	require.NoError(t, err)
 
-	require.Len(t, nodes, 8)
+	require.Len(t, nodes, 7)
 
-	assert.Equal(t, "eth-test-ws-only-0", nodes[0].Name)
-	assert.Equal(t, "primary_0_1", nodes[1].Name)
-	assert.Equal(t, "primary_0_2", nodes[2].Name)
-	assert.Equal(t, "primary_1337_1", nodes[3].Name)
-	assert.Equal(t, "primary_42_1", nodes[4].Name)
-	assert.Equal(t, "sendonly_0_1", nodes[5].Name)
-	assert.Equal(t, "sendonly_1337_1", nodes[6].Name)
-	assert.Equal(t, "sendonly_43_1", nodes[7].Name)
+	assert.Equal(t, "primary_0_1", nodes[0].Name)
+	assert.Equal(t, "primary_0_2", nodes[1].Name)
+	assert.Equal(t, "primary_1337_1", nodes[2].Name)
+	assert.Equal(t, "primary_42_1", nodes[3].Name)
+	assert.Equal(t, "sendonly_0_1", nodes[4].Name)
+	assert.Equal(t, "sendonly_1337_1", nodes[5].Name)
+	assert.Equal(t, "sendonly_43_1", nodes[6].Name)
 }
