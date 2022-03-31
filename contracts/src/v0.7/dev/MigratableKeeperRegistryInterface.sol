c979226df1f23941cfc2535db4b53e170cd67805
Ryan Hall <hall.ryan.r@gmail.com>
2022-03-31 13:37:21 -0400

    wip

---
+++ contracts/src/v0.7/dev/MigratableKeeperRegistryInterface.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+pragma abicoder v2;
+
+interface MigratableKeeperRegistryInterface {
+  function migrateUpkeeps(uint256[] calldata upkeepIDs, address destination) external;
+
+  function importUpkeeps(bytes calldata encodedUpkeeps) external;
+}
