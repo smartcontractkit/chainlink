dde0775deeb788f26e0d34f6879bc34ef625a285
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-04-01 12:48:12 +0300

    Reverted one file staged by mistake

--- core/services/pipeline/task.http_test.go
+++ core/services/pipeline/task.http_test.go
@@ -167,7 +167,6 @@ func TestHTTPTask_Variables(t *testing.T) {
 			task.HelperSetDependencies(cfg, db, uuid.UUID{})
 
 			test.vars.Set("meta", test.meta)
-
 			result, runInfo := task.Run(context.Background(), logger.TestLogger(t), test.vars, test.inputs)
 			assert.False(t, runInfo.IsPending)
 			assert.False(t, runInfo.IsRetryable)
