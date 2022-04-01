10fd5d61b085a60acafca62463835326c1926df2
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-04-01 12:45:42 +0100

    upgrade keeper registrar to 0.8

---
+++ contracts/src/v0.8/dev/interfaces/KeeperRegistryInterfaceDev.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+/*
+ * This is a development version of KeeperRegistryInterface. Once it's audited and finalised
+ * it will be copied to KeeperRegistryInterface
+ */
+pragma solidity ^0.8.0;
+
+interface KeeperRegistryBaseInterface {
+  function registerUpkeep(
+    address target,
+    uint32 gasLimit,
+    address admin,
+    bytes calldata checkData
+  ) external returns (uint256 id);
+
+  function performUpkeep(uint256 id, bytes calldata performData) external returns (bool success);
+
+  function cancelUpkeep(uint256 id) external;
+
+  function addFunds(uint256 id, uint96 amount) external;
+
+  function setUpkeepGasLimit(uint256 id, uint32 gasLimit) external;
+
+  function getUpkeep(uint256 id)
+    external
+    view
+    returns (
+      address target,
+      uint32 executeGas,
+      bytes memory checkData,
+      uint96 balance,
+      address lastKeeper,
+      address admin,
+      uint64 maxValidBlocknumber,
+      uint96 amountSpent
+    );
+
+  function getUpkeepCount() external view returns (uint256);
+
+  function getCanceledUpkeepList() external view returns (uint256[] memory);
+
+  function getKeeperList() external view returns (address[] memory);
+
+  function getKeeperInfo(address query)
+    external
+    view
+    returns (
+      address payee,
+      bool active,
+      uint96 balance
+    );
+
+  function getConfig()
+    external
+    view
+    returns (
+      uint32 paymentPremiumPPB,
+      uint24 checkFrequencyBlocks,
+      uint32 checkGasLimit,
+      uint24 stalenessSeconds,
+      uint16 gasCeilingMultiplier,
+      uint256 fallbackGasPrice,
+      uint256 fallbackLinkPrice,
+      uint32 maxPerformGas,
+      uint96 minUpkeepSpend
+    );
+}
+
+/**
+ * @dev The view methods are not actually marked as view in the implementation
+ * but we want them to be easily queried off-chain. Solidity will not compile
+ * if we actually inherit from this interface, so we document it here.
+ */
+interface KeeperRegistryInterface is KeeperRegistryBaseInterface {
+  function checkUpkeep(uint256 upkeepId, address from)
+    external
+    view
+    returns (
+      bytes memory performData,
+      uint256 maxLinkPayment,
+      uint256 gasLimit,
+      int256 gasWei,
+      int256 linkEth
+    );
+}
+
+interface KeeperRegistryExecutableInterface is KeeperRegistryBaseInterface {
+  function checkUpkeep(uint256 upkeepId, address from)
+    external
+    returns (
+      bytes memory performData,
+      uint256 maxLinkPayment,
+      uint256 gasLimit,
+      uint256 adjustedGasWei,
+      uint256 linkEth
+    );
+}
