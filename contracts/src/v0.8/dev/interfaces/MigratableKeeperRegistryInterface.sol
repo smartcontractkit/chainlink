96a1e8af6203c2893bbd3930b257d0f358d5ccb4
Steve Ellis <email@steveell.is>
2022-04-03 14:05:27 -0400

    test upkeep transcoder
    - ethers doesn't work well with enumerables, so avoid using them in
    transacoder public API. More info here:
    https://github.com/ethers-io/ethers.js/discussions/2432

--- contracts/src/v0.8/dev/interfaces/MigratableKeeperRegistryInterface.sol
+++ contracts/src/v0.8/dev/interfaces/MigratableKeeperRegistryInterface.sol
@@ -9,5 +9,5 @@ interface MigratableKeeperRegistryInterface {
 
   function receiveUpkeeps(bytes calldata encodedUpkeeps) external;
 
- function upkeepTranscoderVersion() external returns (UpkeepTranscoderVersion version);
+ function upkeepTranscoderVersion() external returns (uint8 version);
 }
