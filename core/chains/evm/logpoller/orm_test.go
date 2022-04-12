d77503ece8218949dbe76893a2f662dbc60dfc99
connorwstein <connor.stein@mail.mcgill.ca>
2022-04-12 17:55:30 -0400

    Fix latest block query

--- core/chains/evm/logpoller/orm_test.go
+++ core/chains/evm/logpoller/orm_test.go
@@ -20,25 +20,48 @@ func TestORM(t *testing.T) {
 	lggr := logger.TestLogger(t)
 	require.NoError(t, utils.JustError(db.Exec(`SET CONSTRAINTS log_poller_blocks_evm_chain_id_fkey DEFERRED`)))
 	require.NoError(t, utils.JustError(db.Exec(`SET CONSTRAINTS logs_evm_chain_id_fkey DEFERRED`)))
-	o := NewORM(big.NewInt(137), db, lggr, pgtest.NewPGCfg(true))
+	o1 := NewORM(big.NewInt(137), db, lggr, pgtest.NewPGCfg(true))
+	o2 := NewORM(big.NewInt(138), db, lggr, pgtest.NewPGCfg(true))
 
 	// Insert and read back a block.
-	require.NoError(t, o.InsertBlock(common.HexToHash("0x1234"), 10))
-	b, err := o.SelectBlockByHash(common.HexToHash("0x1234"))
+	require.NoError(t, o1.InsertBlock(common.HexToHash("0x1234"), 10))
+	b, err := o1.SelectBlockByHash(common.HexToHash("0x1234"))
 	require.NoError(t, err)
 	assert.Equal(t, b.BlockNumber, int64(10))
 	assert.Equal(t, b.BlockHash.Bytes(), common.HexToHash("0x1234").Bytes())
 	assert.Equal(t, b.EvmChainId.String(), "137")
 
+	// Insert another block from a different chain
+	require.NoError(t, o2.InsertBlock(common.HexToHash("0x1234"), 11))
+	b2, err := o2.SelectBlockByHash(common.HexToHash("0x1234"))
+	require.NoError(t, err)
+	assert.Equal(t, b2.BlockNumber, int64(11))
+	assert.Equal(t, b2.BlockHash.Bytes(), common.HexToHash("0x1234").Bytes())
+	assert.Equal(t, b2.EvmChainId.String(), "138")
+
+	latest, err := o1.SelectLatestBlock()
+	require.NoError(t, err)
+	assert.Equal(t, int64(10), latest.BlockNumber)
+
+	latest, err = o2.SelectLatestBlock()
+	require.NoError(t, err)
+	assert.Equal(t, int64(11), latest.BlockNumber)
+
 	// Delete a block
-	require.NoError(t, o.DeleteRangeBlocks(10, 10))
-	_, err = o.SelectBlockByHash(common.HexToHash("0x1234"))
+	require.NoError(t, o1.DeleteRangeBlocks(10, 10))
+	_, err = o1.SelectBlockByHash(common.HexToHash("0x1234"))
 	require.Error(t, err)
-	t.Log(errors.Is(err, sql.ErrNoRows))
+	assert.True(t, errors.Is(err, sql.ErrNoRows))
+
+	// Delete block from another chain.
+	require.NoError(t, o2.DeleteRangeBlocks(11, 11))
+	_, err = o2.SelectBlockByHash(common.HexToHash("0x1234"))
+	require.Error(t, err)
+	assert.True(t, errors.Is(err, sql.ErrNoRows))
 
 	// Should be able to insert and read back a log.
 	topic := common.HexToHash("0x1599")
-	require.NoError(t, o.InsertLogs([]Log{
+	require.NoError(t, o1.InsertLogs([]Log{
 		{
 			EvmChainId:  utils.NewBigI(137),
 			LogIndex:    1,
@@ -51,12 +74,12 @@ func TestORM(t *testing.T) {
 			Data:        []byte("hello"),
 		},
 	}))
-	logs, err := o.selectLogsByBlockRange(10, 10)
+	logs, err := o1.selectLogsByBlockRange(10, 10)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(logs))
 	assert.Equal(t, []byte("hello"), logs[0].Data)
 
-	logs, err = o.SelectLogsByBlockRangeFilter(10, 10, common.HexToAddress("0x1234"), topic[:])
+	logs, err = o1.SelectLogsByBlockRangeFilter(10, 10, common.HexToAddress("0x1234"), topic[:])
 	require.NoError(t, err)
 	require.Equal(t, 1, len(logs))
 }
