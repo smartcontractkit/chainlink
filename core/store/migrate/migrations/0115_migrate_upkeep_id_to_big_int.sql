66e72e95b9771bd7d4f888584d7e03e077ec4bda
Ryan Hall <hall.ryan.r@gmail.com>
2022-04-18 15:50:00 -0400

    wip

---
+++ core/store/migrate/migrations/0115_migrate_upkeep_id_to_big_int.sql
@@ -0,0 +1,54 @@
+-- +goose Up
+-- +goose StatementBegin
+
+-- INSERT INTO keeper_registries (job_id, keeper_index, contract_address, from_address, check_gas, block_count_per_turn, num_keepers) VALUES(1, 1, decode('0000000000000000000000000000000000000000', 'hex'), decode('0000000000000000000000000000000000000000', 'hex'), 1, 1, 1);
+-- INSERT INTO upkeep_registrations (registry_id, execute_gas, check_data, upkeep_id, positioning_constant) VALUES(1, 1, decode('01', 'hex'), 1, 1);
+
+-- DROP INDEX idx_upkeep_registrations_unique_upkeep_ids_per_keeper;
+
+ALTER TABLE upkeep_registrations ALTER COLUMN upkeep_id TYPE numeric(78,0);
+
+
+CREATE OR REPLACE FUNCTION uint256_to_bit(NUMERIC)
+  RETURNS BIT VARYING AS $$
+DECLARE
+  num ALIAS FOR $1;
+  -- 1 + largest positive BIGINT --
+  max_bigint NUMERIC := '9223372036854775808' :: NUMERIC(19, 0);
+  result BIT VARYING;
+BEGIN
+  ASSERT num <= 115792089237316195423570985008687907853269984665640564039457584007913129639935, "value larger than max uint256";
+  WITH
+      chunks (exponent, chunk) AS (
+        SELECT
+          exponent,
+          floor((num / (max_bigint ^ exponent) :: NUMERIC(256, 20)) % max_bigint) :: BIGINT
+        FROM generate_series(0, 5) exponent
+    )
+  SELECT bit_or(chunk :: BIT(256) :: BIT VARYING << (63 * (exponent))) :: BIT VARYING
+  FROM chunks INTO result;
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION least_significant(bits BIT VARYING, num integer)
+  RETURNS BIT VARYING AS $$
+BEGIN
+  ASSERT length(bits) >= num, "slice is larger than input";
+  RETURN substring(bits from length(bits) - num + 1 for num);
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- CREATE UNIQUE INDEX idx_upkeep_registrations_unique_upkeep_ids_per_keeper ON upkeep_registrations(registry_id, upkeep_id);
+
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+ALTER TABLE upkeep_registrations ALTER COLUMN upkeep_id TYPE bigint;
+
+-- +goose StatementEnd
