86c8b6214f2f61a06a94a816a4d7a84979a96497
Malec <cmalec@users.noreply.github.com>
2022-04-20 09:58:59 -0400

    make all alter table migrations IF statements

--- core/store/migrate/migrations/0114_add_last_keeper_id_to_upkeep_table.sql
+++ core/store/migrate/migrations/0114_add_last_keeper_id_to_upkeep_table.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 ALTER TABLE upkeep_registrations
-    ADD COLUMN last_keeper_index integer DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS last_keeper_index integer DEFAULT NULL;
 ALTER TABLE upkeep_registrations
-    DROP COLUMN positioning_constant;
+    DROP COLUMN IF EXISTS positioning_constant;
 ALTER TABLE keeper_registries
-    ADD COLUMN keeper_index_map jsonb DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS keeper_index_map jsonb DEFAULT NULL;
 
 -- +goose Down
 ALTER TABLE upkeep_registrations
-    DROP COLUMN last_keeper_index;
+    DROP COLUMN IF EXISTS last_keeper_index;
 ALTER TABLE upkeep_registrations
-    ADD COLUMN positioning_constant integer;
+    ADD COLUMN IF NOT EXISTS positioning_constant integer;
 ALTER TABLE keeper_registries
     DROP COLUMN IF EXISTS keeper_index_map;
