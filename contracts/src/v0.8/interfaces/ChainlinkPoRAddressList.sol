02eb811bab33ac9c868abc55787798e82d853841
Steve Ellis <email@steveell.is>
2022-04-26 15:30:51 -0400

    Simplify interface naming
    Co-authored-by: Jonas Hals <contact@jonashals.me>

--- contracts/src/v0.8/interfaces/ChainlinkPoRAddressList.sol
+++ contracts/src/v0.8/interfaces/ChainlinkPoRAddressList.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-interface ChainlinkPoRAddressList {
+interface PoRAddressList {
   function getPoRAddressListLength() external view returns (uint256);
 
   function getPoRAddressList(uint256 startIndex, uint256 endIndex) external view returns (string[] memory);
