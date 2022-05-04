b0efb287cd95dc9dac61b96d9dfbad1a3034d9ca
Chris <104409744+vreff@users.noreply.github.com>
2022-05-04 10:13:23 -0700

    [Bugfix-24009] Update compiler version to ^0.8.4 for VRF contracts that use the custom error format

--- contracts/src/v0.8/VRFConsumerBaseV2.sol
+++ contracts/src/v0.8/VRFConsumerBaseV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 /** ****************************************************************************
  * @notice Interface for contracts using VRF randomness
