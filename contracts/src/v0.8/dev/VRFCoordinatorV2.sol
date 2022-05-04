8e8a996fd882c0861bdc9824c1ca27c857c87d03
Chris <104409744+vreff@users.noreply.github.com>
2022-05-04 10:13:23 -0700

    [Bugfix-24009] Update compiler version to ^0.8.4 for VRF contracts that use the custom error format

--- contracts/src/v0.8/dev/VRFCoordinatorV2.sol
+++ contracts/src/v0.8/dev/VRFCoordinatorV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "../interfaces/LinkTokenInterface.sol";
 import "../interfaces/BlockhashStoreInterface.sol";
