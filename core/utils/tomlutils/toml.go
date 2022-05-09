ef1ee7bf2330513e47e19208cf5169c2d689ad10
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2022-05-09 15:39:22 -0400

    Adds tests

--- core/utils/tomlutils/toml.go
+++ core/utils/tomlutils/toml.go
@@ -1,35 +1,35 @@
-package utils
+package tomlutils
 
 import (
 	"strconv"
 )
 
-// TomlFloat32 represents float32 values for TOML
-type TomlFloat32 float32
+// Float32 represents float32 values for TOML
+type Float32 float32
 
 // UnmarshalText parses the value as a proper float32
-func (t *TomlFloat32) UnmarshalText(text []byte) error {
+func (t *Float32) UnmarshalText(text []byte) error {
 	f32, err := strconv.ParseFloat(string(text), 32)
 	if err != nil {
 		return err
 	}
 
-	*t = TomlFloat32(f32)
+	*t = Float32(f32)
 
 	return nil
 }
 
-// TomlFloat64 represents float64 values for TOML
-type TomlFloat64 float64
+// Float64 represents float64 values for TOML
+type Float64 float64
 
 // UnmarshalText parses the value as a proper float64
-func (t *TomlFloat64) UnmarshalText(text []byte) error {
+func (t *Float64) UnmarshalText(text []byte) error {
 	f32, err := strconv.ParseFloat(string(text), 64)
 	if err != nil {
 		return err
 	}
 
-	*t = TomlFloat64(f32)
+	*t = Float64(f32)
 
 	return nil
 }
