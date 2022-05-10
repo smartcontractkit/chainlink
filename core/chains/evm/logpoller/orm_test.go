4441f2128b67da898eed3b85baab35f11809f294
Connor Stein <connor.stein@mail.mcgill.ca>
2022-05-10 15:35:49 -0400

    Support log querying by contents (#6574)

--- core/chains/evm/logpoller/orm_test.go
+++ core/chains/evm/logpoller/orm_test.go
@@ -15,14 +15,19 @@ import (
 	"github.com/smartcontractkit/chainlink/core/utils"
 )
 
-func TestORM(t *testing.T) {
+// Setup creates two orms representing logs from different chains.
+func setup(t *testing.T) (*ORM, *ORM) {
 	db := pgtest.NewSqlxDB(t)
 	lggr := logger.TestLogger(t)
 	require.NoError(t, utils.JustError(db.Exec(`SET CONSTRAINTS log_poller_blocks_evm_chain_id_fkey DEFERRED`)))
 	require.NoError(t, utils.JustError(db.Exec(`SET CONSTRAINTS logs_evm_chain_id_fkey DEFERRED`)))
 	o1 := NewORM(big.NewInt(137), db, lggr, pgtest.NewPGCfg(true))
 	o2 := NewORM(big.NewInt(138), db, lggr, pgtest.NewPGCfg(true))
+	return o1, o2
+}
 
+func TestORM(t *testing.T) {
+	o1, o2 := setup(t)
 	// Insert and read back a block.
 	require.NoError(t, o1.InsertBlock(common.HexToHash("0x1234"), 10))
 	b, err := o1.SelectBlockByHash(common.HexToHash("0x1234"))
@@ -187,4 +192,127 @@ func TestORM(t *testing.T) {
 	lgs, err = o1.LatestLogEventSigsAddrs(0 /* startBlock */, []common.Address{common.HexToAddress("0x1234"), common.HexToAddress("0x1235")}, []common.Hash{topic, topic2})
 	require.NoError(t, err)
 	require.Equal(t, 4, len(lgs))
+}
+
+func insertLogsTopicValueRange(t *testing.T, o *ORM, addr common.Address, blockNumber int, eventSig []byte, start, stop int) {
+	var lgs []Log
+	for i := start; i <= stop; i++ {
+		lgs = append(lgs, Log{
+			EvmChainId:  utils.NewBig(o.chainID),
+			LogIndex:    int64(i),
+			BlockHash:   common.HexToHash("0x1234"),
+			BlockNumber: int64(blockNumber),
+			EventSig:    eventSig[:],
+			Topics:      [][]byte{eventSig[:], EvmWord(uint64(i)).Bytes()},
+			Address:     addr,
+			TxHash:      common.HexToHash("0x1888"),
+			Data:        []byte("hello"),
+		})
+	}
+	require.NoError(t, o.InsertLogs(lgs))
+}
+
+func TestORM_IndexedLogs(t *testing.T) {
+	o1, _ := setup(t)
+	eventSig := common.HexToHash("0x1599")
+	addr := common.HexToAddress("0x1234")
+	bh := common.HexToHash("0x1234")
+	require.NoError(t, o1.InsertBlock(bh, 1))
+	insertLogsTopicValueRange(t, o1, addr, 1, eventSig.Bytes(), 1, 3)
+	insertLogsTopicValueRange(t, o1, addr, 2, eventSig.Bytes(), 4, 4) // unconfirmed
+
+	lgs, err := o1.SelectIndexedLogs(addr, eventSig[:], 1, []common.Hash{EvmWord(1)}, 0)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(lgs))
+	assert.Equal(t, EvmWord(1).Bytes(), lgs[0].GetTopics()[1].Bytes())
+
+	lgs, err = o1.SelectIndexedLogs(addr, eventSig[:], 1, []common.Hash{EvmWord(1), EvmWord(2)}, 0)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(lgs))
+
+	lgs, err = o1.SelectIndexLogsTopicGreaterThan(addr, eventSig[:], 1, EvmWord(2), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(lgs))
+
+	lgs, err = o1.SelectIndexLogsTopicRange(addr, eventSig[:], 1, EvmWord(3), EvmWord(3), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(lgs))
+	assert.Equal(t, EvmWord(3).Bytes(), lgs[0].GetTopics()[1].Bytes())
+
+	lgs, err = o1.SelectIndexLogsTopicRange(addr, eventSig[:], 1, EvmWord(1), EvmWord(3), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 3, len(lgs))
+
+	// Check confirmations work as expected.
+	require.NoError(t, o1.InsertBlock(bh, 2))
+	lgs, err = o1.SelectIndexLogsTopicRange(addr, eventSig[:], 1, EvmWord(4), EvmWord(4), 1)
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(lgs))
+	require.NoError(t, o1.InsertBlock(bh, 3))
+	lgs, err = o1.SelectIndexLogsTopicRange(addr, eventSig[:], 1, EvmWord(4), EvmWord(4), 1)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(lgs))
+}
+
+func TestORM_DataWords(t *testing.T) {
+	o1, _ := setup(t)
+	eventSig := common.HexToHash("0x1599")
+	addr := common.HexToAddress("0x1234")
+	bh := common.HexToHash("0x1234")
+	require.NoError(t, o1.InsertBlock(bh, 1))
+	require.NoError(t, o1.InsertLogs([]Log{
+		{
+			EvmChainId:  utils.NewBig(o1.chainID),
+			LogIndex:    int64(0),
+			BlockHash:   bh,
+			BlockNumber: int64(1),
+			EventSig:    eventSig[:],
+			Topics:      [][]byte{eventSig[:]},
+			Address:     addr,
+			TxHash:      common.HexToHash("0x1888"),
+			Data:        EvmWord(1).Bytes(),
+		},
+		{
+			// In block 2, unconfirmed to start
+			EvmChainId:  utils.NewBig(o1.chainID),
+			LogIndex:    int64(1),
+			BlockHash:   bh,
+			BlockNumber: int64(2),
+			EventSig:    eventSig[:],
+			Topics:      [][]byte{eventSig[:]},
+			Address:     addr,
+			TxHash:      common.HexToHash("0x1888"),
+			Data:        append(EvmWord(2).Bytes(), EvmWord(3).Bytes()...),
+		},
+	}))
+	// Outside range should fail.
+	lgs, err := o1.SelectDataWordRange(addr, eventSig[:], 0, EvmWord(2), EvmWord(2), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(lgs))
+
+	// Range including log should succeed
+	lgs, err = o1.SelectDataWordRange(addr, eventSig[:], 0, EvmWord(1), EvmWord(2), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(lgs))
+
+	// Range only covering log should succeed
+	lgs, err = o1.SelectDataWordRange(addr, eventSig[:], 0, EvmWord(1), EvmWord(1), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(lgs))
+
+	// Cannot query for unconfirmed second log.
+	lgs, err = o1.SelectDataWordRange(addr, eventSig[:], 1, EvmWord(3), EvmWord(3), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(lgs))
+	// Confirm it, then can query.
+	require.NoError(t, o1.InsertBlock(bh, 2))
+	lgs, err = o1.SelectDataWordRange(addr, eventSig[:], 1, EvmWord(3), EvmWord(3), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(lgs))
+	assert.Equal(t, lgs[0].Data, append(EvmWord(2).Bytes(), EvmWord(3).Bytes()...))
+
+	// Check greater than 1 yields both logs.
+	lgs, err = o1.SelectDataWordGreaterThan(addr, eventSig[:], 0, EvmWord(1), 0)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(lgs))
 }
