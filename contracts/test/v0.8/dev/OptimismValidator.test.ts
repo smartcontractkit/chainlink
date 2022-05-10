a28b5357aec6b4d38a8703b110808afe31048469
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2022-05-10 20:29:38 +0800

    address PR comments

--- contracts/test/v0.8/dev/OptimismValidator.test.ts
+++ contracts/test/v0.8/dev/OptimismValidator.test.ts
@@ -15,10 +15,7 @@ describe('OptimismValidator', () => {
   /** Fake L2 target */
   const L2_SEQ_STATUS_RECORDER_ADDRESS =
     '0x491B1dDA0A8fa069bbC1125133A975BF4e85a91b'
-  const L2_TOKEN_BRIDGE_ADDR = '0x4200000000000000000000000000000000000010'
-  const L2_ETH_TOKEN_ADDR = '0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000'
   let optimismValidator: Contract
-  let accessController: Contract
   let optimismUptimeFeedFactory: ContractFactory
   let mockOptimismL1CrossDomainMessenger: Contract
   let deployer: SignerWithAddress
@@ -31,12 +28,6 @@ describe('OptimismValidator', () => {
   })
 
   beforeEach(async () => {
-    const accessControllerFactory = await ethers.getContractFactory(
-      'src/v0.8/SimpleWriteAccessController.sol:SimpleWriteAccessController',
-      deployer,
-    )
-    accessController = await accessControllerFactory.deploy()
-
     // Required for building the calldata
     optimismUptimeFeedFactory = await ethers.getContractFactory(
       'src/v0.8/dev/OptimismSequencerUptimeFeed.sol:OptimismSequencerUptimeFeed',
@@ -60,9 +51,6 @@ describe('OptimismValidator', () => {
     optimismValidator = await optimismValidatorFactory.deploy(
       mockOptimismL1CrossDomainMessenger.address,
       L2_SEQ_STATUS_RECORDER_ADDRESS,
-      accessController.address,
-      L2_TOKEN_BRIDGE_ADDR,
-      L2_ETH_TOKEN_ADDR,
       GAS_LIMIT,
     )
     // Transfer some ETH to the OptimismValidator contract
@@ -116,30 +104,6 @@ describe('OptimismValidator', () => {
     })
   })
 
-  describe('#withdrawFundsFromL2', () => {
-    const amountToWithdraw = BigNumber.from(10).pow(18)
-
-    it('reverts if called by non owner', async () => {
-      const refundAddr = deployer.address
-      await expect(
-        optimismValidator
-          .connect(eoaValidator)
-          .withdrawFundsFromL2(amountToWithdraw, refundAddr),
-      ).to.be.revertedWith('Only callable by owner')
-    })
-
-    it('successfully withdraws funds from L2', async () => {
-      const refundAddr = deployer.address
-      await expect(
-        optimismValidator
-          .connect(deployer)
-          .withdrawFundsFromL2(amountToWithdraw, refundAddr),
-      )
-        .to.emit(optimismValidator, 'L2WithdrawalRequested')
-        .withArgs(amountToWithdraw, refundAddr)
-    })
-  })
-
   describe('#validate', () => {
     it('does not update the status when there is no status change', async () => {
       await optimismValidator.addAccess(eoaValidator.address)
@@ -159,7 +123,7 @@ describe('OptimismValidator', () => {
 
       const now = Math.ceil(Date.now() / 1000) + 1000
       await ethers.provider.send('evm_setNextBlockTimestamp', [now])
-      const arbitrumSequencerStatusRecorderCallData =
+      const sequencerStatusRecorderCallData =
         optimismUptimeFeedFactory.interface.encodeFunctionData('updateStatus', [
           true,
           now,
@@ -170,7 +134,7 @@ describe('OptimismValidator', () => {
         .withArgs(
           L2_SEQ_STATUS_RECORDER_ADDRESS,
           optimismValidator.address,
-          arbitrumSequencerStatusRecorderCallData,
+          sequencerStatusRecorderCallData,
           0,
           GAS_LIMIT,
         )
