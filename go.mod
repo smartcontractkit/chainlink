c4759cac049a76c8000a8313f647305e08c6565d
Jordan Krage <jmank88@gmail.com>
2022-05-16 17:47:34 -0500

    common utilities from core (#50)
    * pkg/utils: WithJitter, ContextFromChain, StartStopOnce
    
    * pkg/utils: Duration
    
    * pkg/types: add common core types

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.8.2
 	github.com/golang/protobuf v1.5.2
 	github.com/linkedin/goavro v2.1.0+incompatible
+	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.12.0
 	github.com/riferrei/srclient v0.4.1-0.20211229125508-8edc580da179
 	github.com/smartcontractkit/libocr v0.0.0-20211210213233-5443fb9db7f7
 	github.com/stretchr/testify v1.7.0
+	go.uber.org/atomic v1.7.0
 	go.uber.org/goleak v1.1.12
 	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.16.0
@@ -24,14 +26,12 @@ require (
 	github.com/linkedin/goavro/v2 v2.9.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/santhosh-tekuri/jsonschema/v5 v5.0.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
---
+++ pkg/types/types.go
@@ -0,0 +1,59 @@
+package types
+
+import (
+	"context"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/smartcontractkit/libocr/offchainreporting2/reportingplugin/median"
+	"github.com/smartcontractkit/libocr/offchainreporting2/types"
+)
+
+type Service interface {
+	Start(context.Context) error
+	Close() error
+	Ready() error
+	Healthy() error
+}
+
+// PluginArgs are the args required to create any OCR2 plugin components.
+// Its possible that the plugin config might actually be different
+// per relay type, so we pass the config directly through.
+type PluginArgs struct {
+	TransmitterID string
+	PluginConfig  []byte
+}
+
+type RelayArgs struct {
+	ExternalJobID uuid.UUID
+	JobID         int32
+	ContractID    string
+	RelayConfig   []byte
+}
+
+type Relayer interface {
+	Service
+	NewConfigProvider(rargs RelayArgs) (ConfigProvider, error)
+	NewMedianProvider(rargs RelayArgs, pargs PluginArgs) (MedianProvider, error)
+}
+
+// The bootstrap jobs only watch config.
+type ConfigProvider interface {
+	Service
+	OffchainConfigDigester() types.OffchainConfigDigester
+	ContractConfigTracker() types.ContractConfigTracker
+}
+
+// Plugin provides common components for any OCR2 plugin.
+// It watches config and is able to transmit.
+type Plugin interface {
+	ConfigProvider
+	ContractTransmitter() types.ContractTransmitter
+}
+
+// MedianProvider provides all components needed for a median OCR2 plugin.
+type MedianProvider interface {
+	Plugin
+	ReportCodec() median.ReportCodec
+	MedianContract() median.MedianContract
+}
