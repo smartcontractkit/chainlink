52bf9a6c90423537c292532072aa881cbb2f0563
FelixFan1992 <fankejin@gmail.com>
2022-05-20 17:56:48 -0400

    update tests

--- contracts/test/v0.8/CanaryUpkeep.test.ts
+++ contracts/test/v0.8/CanaryUpkeep.test.ts
@@ -1,42 +1,76 @@
 import { ethers } from 'hardhat'
-import {BigNumber} from "ethers";
+import {BigNumber, Signer} from "ethers";
 import moment from "moment";
-import {assert, expect} from "chai";
+import {assert} from "chai";
 import { CanaryUpkeep } from '../../typechain/CanaryUpkeep'
-import KeeperRegistryAbi from '../../abi/src/v0.8/KeeperRegistry.sol/KeeperRegistry.json'
+import { KeeperRegistry } from '../../typechain/KeeperRegistry';
 import { fastForward, reset } from '../test-helpers/helpers'
-import {deployMockContract, MockContract} from '@ethereum-waffle/mock-contract'
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers"
+import { getUsers, Personas } from '../test-helpers/setup'
 import {evmRevert} from "../test-helpers/matchers";
 
+let personas: Personas
 let canaryUpkeep: CanaryUpkeep
-let nelly: SignerWithAddress
-let nancy: SignerWithAddress
-let neo: SignerWithAddress
+let owner: Signer
+let nelly: Signer
+let nancy: Signer
+let ned: Signer
 let keeperAddresses: string[]
-let emptyKeeperAddresses: string[]
-let mockContract: MockContract
+let keeperRegistry: KeeperRegistry
+
+const paymentPremiumPPB = BigNumber.from(250000000)
+const flatFeeMicroLink = BigNumber.from(0)
+const blockCountPerTurn = BigNumber.from(3)
+const stalenessSeconds = BigNumber.from(43820)
+const gasCeilingMultiplier = BigNumber.from(1)
+const checkGasLimit = BigNumber.from(20000000)
+const fallbackGasPrice = BigNumber.from(200)
+const fallbackLinkPrice = BigNumber.from(200000000)
+const maxPerformGas = BigNumber.from(5000000)
+const minUpkeepSpend = BigNumber.from('1000000000000000000')
+const transcoder = ethers.constants.AddressZero
+const registrar = ethers.constants.AddressZero
+const config = {
+    paymentPremiumPPB,
+    flatFeeMicroLink,
+    blockCountPerTurn,
+    checkGasLimit,
+    stalenessSeconds,
+    gasCeilingMultiplier,
+    minUpkeepSpend,
+    maxPerformGas,
+    fallbackGasPrice,
+    fallbackLinkPrice,
+    transcoder,
+    registrar
+}
+
+before(async () => {
+    personas = (await getUsers()).personas
+    owner = personas.Default
+    nelly = personas.Nelly
+    nancy = personas.Nancy
+    ned = personas.Ned
+    keeperAddresses = [await nelly.getAddress(), await nancy.getAddress(), await ned.getAddress()]
+})
 
 describe.only('CanaryUpkeep', () => {
-    beforeEach(async () => {
-        const accounts = await ethers.getSigners()
-        const admin = accounts[1]
-        // initialize fake node operators
-        nelly = accounts[2]
-        nancy = accounts[3]
-        neo = accounts[4]
-        mockContract = await deployMockContract(admin as any, KeeperRegistryAbi);
 
-        // this object is the return type of getState function of KeeperRegistry. we still need to investigate how
-        // to create an object for the return type. for the purpose of this test, only the address array is needed.
-        // hence, the first two sub structs are omitted.
-        keeperAddresses = [nelly.address, nancy.address, neo.address]
-        const getStateReturn = [{}, {}, keeperAddresses];
-        await mockContract.mock.getState.returns(...getStateReturn)
+    beforeEach(async () => {
+        const keeperRegistryFactory = await ethers.getContractFactory(
+            'KeeperRegistry'
+        )
+        keeperRegistry = await keeperRegistryFactory.connect(owner).deploy(
+            ethers.constants.AddressZero,
+            ethers.constants.AddressZero,
+            ethers.constants.AddressZero,
+            config
+        )
+        await keeperRegistry.deployed()
+
         const canaryUpkeepFactory = await ethers.getContractFactory(
             'CanaryUpkeep'
         )
-        canaryUpkeep = await canaryUpkeepFactory.deploy(ethers.constants.AddressZero)
+        canaryUpkeep = await canaryUpkeepFactory.connect(owner).deploy(keeperRegistry.address)
         await canaryUpkeep.deployed()
     })
 
@@ -47,18 +81,19 @@ describe.only('CanaryUpkeep', () => {
     describe('checkUpkeep()', () => {
         it('returns true when sufficient time passes', async () => {
             await fastForward(moment.duration(6, 'minutes').asSeconds())
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
             const [ needsUpkeep ] = await canaryUpkeep.checkUpkeep('0x')
             assert.isTrue(needsUpkeep)
         })
 
         it('returns false when insufficient time passes', async () => {
             await fastForward(moment.duration(2, 'minutes').asSeconds())
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
             const [ needsUpkeep ] = await canaryUpkeep.checkUpkeep('0x')
             assert.isFalse(needsUpkeep)
         })
 
         it('returns false when keeper array is empty', async () => {
-            await mockContract.mock.getState.returns({}, {}, emptyKeeperAddresses)
             await fastForward(moment.duration(6, 'minutes').asSeconds())
             const [ needsUpkeep ] = await canaryUpkeep.checkUpkeep('0x')
             assert.isFalse(needsUpkeep)
@@ -67,39 +102,60 @@ describe.only('CanaryUpkeep', () => {
 
     describe('performUpkeep()', () => {
         it('enforces that transaction origin is the anticipated keeper', async () => {
-            const oldTimestamp = await canaryUpkeep.connect(keeperAddresses[0]).getTimestamp()
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
+
+            const oldTimestamp = await canaryUpkeep.connect(nelly).getTimestamp()
+            const oldKeeperIndex = await canaryUpkeep.connect(nelly).getKeeperIndex()
             await fastForward(moment.duration(6, 'minutes').asSeconds())
-            await canaryUpkeep.connect(keeperAddresses[0]).performUpkeep('0x')
+            await canaryUpkeep.connect(nelly).performUpkeep('0x')
+            const newKeeperIndex = await canaryUpkeep.connect(nelly).getKeeperIndex()
+            assert.equal(newKeeperIndex.toNumber() - oldKeeperIndex.toNumber(), 1, "keeper index needs to increment by 1 after performUpkeep")
 
-            const keeperIndex = await canaryUpkeep.connect(keeperAddresses[0]).getKeeperIndex()
-            assert.equal(keeperIndex, BigNumber.from(1), "keeper index needs to increment by 1 after performUpkeep")
-
-            const newTimestamp = await canaryUpkeep.connect(keeperAddresses[0]).getTimestamp()
-            const interval = await canaryUpkeep.connect(keeperAddresses[0]).getInterval()
+            const newTimestamp = await canaryUpkeep.connect(nelly).getTimestamp()
+            const interval = await canaryUpkeep.connect(nelly).getInterval()
             assert.isAtLeast(newTimestamp.toNumber() - oldTimestamp.toNumber(), interval.toNumber(), "timestamp needs to be updated after performUpkeep")
         })
 
+        it('enforces that keeper index will reset to zero after visiting the last keeper', async () => {
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
+
+            await fastForward(moment.duration(6, 'minutes').asSeconds())
+            await canaryUpkeep.connect(nelly).performUpkeep('0x')
+
+            await fastForward(moment.duration(6, 'minutes').asSeconds())
+            await canaryUpkeep.connect(nancy).performUpkeep('0x')
+
+            await fastForward(moment.duration(6, 'minutes').asSeconds())
+            await canaryUpkeep.connect(ned).performUpkeep('0x')
+
+            const keeperIndex = await canaryUpkeep.connect(ned).getKeeperIndex()
+            assert.equal(keeperIndex.toNumber(), keeperAddresses.length, "Keeper index is not updated properly")
+
+            await fastForward(moment.duration(6, 'minutes').asSeconds())
+            await canaryUpkeep.connect(nelly).performUpkeep('0x')
+            const newKeeperIndex = await canaryUpkeep.connect(ned).getKeeperIndex()
+            assert.equal(newKeeperIndex.toNumber(), 1, "Keeper index does not reset to zero after visiting the last keeper")
+        })
+
         it('reverts if the keeper array is empty', async () => {
-            await mockContract.mock.getState.returns({}, {}, emptyKeeperAddresses)
-            // await evmRevert(
-            //     canaryUpkeep.connect(keeperAddresses[0]).performUpkeep('0x'),
-            //     "no keeper nodes exists");
-            await expect(canaryUpkeep.connect(keeperAddresses[0]).performUpkeep('0x')).to.be.revertedWith(
-                "no keeper nodes exists"
-            )
+            await evmRevert(
+                canaryUpkeep.connect(nelly).performUpkeep('0x'),
+                "no keeper nodes exists");
         })
 
         it('reverts if not enough time has passed', async () => {
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
             await fastForward(moment.duration(3, 'minutes').asSeconds())
             await evmRevert(
-                canaryUpkeep.connect(keeperAddresses[0]).performUpkeep('0x'),
+                canaryUpkeep.connect(nelly).performUpkeep('0x'),
                 "Not enough time has passed after the previous upkeep");
         })
 
         it('reverts if an incorrect keeper tries to perform upkeep', async () => {
+            await keeperRegistry.setKeepers(keeperAddresses, keeperAddresses)
             await fastForward(moment.duration(6, 'minutes').asSeconds())
             await evmRevert(
-                canaryUpkeep.connect(keeperAddresses[1]).performUpkeep('0x'),
+                canaryUpkeep.connect(nancy).performUpkeep('0x'),
                 "transaction origin is not the anticipated keeper.");
         })
     })
