de8ee8d1a63b60ed74a50d5725996d49ae8d3dc5
cuckflong <johnathanclong13@gmail.com>
2022-05-25 15:52:39 +0800

    feat (vrf v2 mock): use func overloading

--- contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol
+++ contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol
@@ -83,6 +83,43 @@ contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
    *
    * @param _requestId the request to fulfill
    * @param _consumer the VRF randomness consumer to send the result to
+   */
+  function fulfillRandomWords(uint256 _requestId, address _consumer) external {
+    uint256 startGas = gasleft();
+    if (s_requests[_requestId].subId == 0) {
+      revert("nonexistent request");
+    }
+    Request memory req = s_requests[_requestId];
+
+    uint256[] memory words = new uint256[](req.numWords);
+
+    for (uint256 i = 0; i < req.numWords; i++) {
+      words[i] = uint256(keccak256(abi.encode(_requestId, i)));
+    }
+
+    VRFConsumerBaseV2 v;
+    bytes memory callReq = abi.encodeWithSelector(v.rawFulfillRandomWords.selector, _requestId, words);
+    (bool success, ) = _consumer.call{gas: req.callbackGasLimit}(callReq);
+
+    uint96 payment = uint96(BASE_FEE + ((startGas - gasleft()) * GAS_PRICE_LINK));
+    if (s_subscriptions[req.subId].balance < payment) {
+      revert InsufficientBalance();
+    }
+    s_subscriptions[req.subId].balance -= payment;
+    delete (s_requests[_requestId]);
+    emit RandomWordsFulfilled(_requestId, _requestId, payment, success);
+  }
+
+  /**
+   * @notice fulfillRandomWords fulfills the given request, sending the random words to the supplied
+   * @notice consumer.
+   *
+   * @dev This mock uses a simplified formula for calculating payment amount and gas usage, and does
+   * @dev not account for all edge cases handled in the real VRF coordinator. When making requests
+   * @dev against the real coordinator a small amount of additional LINK is required.
+   *
+   * @param _requestId the request to fulfill
+   * @param _consumer the VRF randomness consumer to send the result to
    * @param _words user provided random words
    */
   function fulfillRandomWords(
@@ -97,10 +134,7 @@ contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
     Request memory req = s_requests[_requestId];
 
     if (_words.length != req.numWords) {
-      _words = new uint256[](req.numWords);
-      for (uint256 i = 0; i < req.numWords; i++) {
-        _words[i] = uint256(keccak256(abi.encode(_requestId, i)));
-      }
+      revert("invalid words length");
     }
 
     VRFConsumerBaseV2 v;
