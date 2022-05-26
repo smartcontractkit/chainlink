bb9ef2948f4427c5cef6eb45f8c5758a42f31fe3
cuckflong <johnathanclong13@gmail.com>
2022-05-26 22:04:40 +0800

    feat (vrf v2 mock): use overloading

--- contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol
+++ contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol
@@ -85,28 +85,7 @@ contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
    * @param _consumer the VRF randomness consumer to send the result to
    */
   function fulfillRandomWords(uint256 _requestId, address _consumer) external {
-    uint256 startGas = gasleft();
-    if (s_requests[_requestId].subId == 0) {
-      revert("nonexistent request");
-    }
-    Request memory req = s_requests[_requestId];
-
-    uint256[] memory words = new uint256[](req.numWords);
-    for (uint256 i = 0; i < req.numWords; i++) {
-      words[i] = uint256(keccak256(abi.encode(_requestId, i)));
-    }
-
-    VRFConsumerBaseV2 v;
-    bytes memory callReq = abi.encodeWithSelector(v.rawFulfillRandomWords.selector, _requestId, words);
-    (bool success, ) = _consumer.call{gas: req.callbackGasLimit}(callReq);
-
-    uint96 payment = uint96(BASE_FEE + ((startGas - gasleft()) * GAS_PRICE_LINK));
-    if (s_subscriptions[req.subId].balance < payment) {
-      revert InsufficientBalance();
-    }
-    s_subscriptions[req.subId].balance -= payment;
-    delete (s_requests[_requestId]);
-    emit RandomWordsFulfilled(_requestId, _requestId, payment, success);
+    fulfillRandomWords(_requestId, _consumer, new uint256[](0));
   }
 
   /**
@@ -125,7 +104,7 @@ contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
     uint256 _requestId,
     address _consumer,
     uint256[] memory _words
-  ) external {
+  ) public {
     uint256 startGas = gasleft();
     if (s_requests[_requestId].subId == 0) {
       revert("nonexistent request");
@@ -133,7 +112,10 @@ contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
     Request memory req = s_requests[_requestId];
 
     if (_words.length != req.numWords) {
-      revert("invalid words length");
+      _words = new uint256[](req.numWords);
+      for (uint256 i = 0; i < req.numWords; i++) {
+        _words[i] = uint256(keccak256(abi.encode(_requestId, i)));
+      }
     }
 
     VRFConsumerBaseV2 v;
