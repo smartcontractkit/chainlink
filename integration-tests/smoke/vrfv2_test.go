07ec2341b8f83c24ae37b6bb06ce0bb3aca2fe8e
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-06-09 15:52:11 +0100

    fix comment

--- integration-tests/smoke/vrfv2_test.go
+++ integration-tests/smoke/vrfv2_test.go
@@ -149,7 +149,7 @@ var _ = Describe("VRFv2 suite @v2vrf", func() {
 		})
 	})
 
-	// This test is disabled until sc-43033 is fixed. To re-enable use replace PDescribe with Describe
+	// This test is disabled until sc-43033 is fixed. To re-enable replace PDescribe with Describe
 	PDescribe("with VRF job", func() {
 		It("randomness is fulfilled", func() {
 			words := uint32(10)
