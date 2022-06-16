20109d8e7e95877dd6f0ff9f68cdbf3857e66671
mesoic <mesoic@protonmail.com>
2022-06-16 16:39:26 -0500

    fix ChainConfigurationForm test

--- operator_ui/src/components/Form/ChainConfigurationForm.test.tsx
+++ operator_ui/src/components/Form/ChainConfigurationForm.test.tsx
@@ -22,6 +22,7 @@ describe('ChainConfigurationForm', () => {
       ocr1P2PPeerID: '',
       ocr1KeyBundleID: '',
       ocr2Enabled: false,
+      ocr2IsBootstrap: false,
       ocr2Multiaddr: '',
       ocr2P2PPeerID: '',
       ocr2KeyBundleID: '',
@@ -103,6 +104,60 @@ describe('ChainConfigurationForm', () => {
     )
 
     expect(await findByTestId('ocr1Multiaddr-helper-text')).toHaveTextContent(
+      'Required',
+    )
+  })
+
+  it('validates OCR2 input', async () => {
+    const handleSubmit = jest.fn()
+    const initialValues: FormValues = {
+      chainID: '',
+      chainType: '',
+      accountAddr: '',
+      adminAddr: '',
+      fluxMonitorEnabled: false,
+      ocr1Enabled: false,
+      ocr1IsBootstrap: false,
+      ocr1Multiaddr: '',
+      ocr1P2PPeerID: '',
+      ocr1KeyBundleID: '',
+      ocr2Enabled: false,
+      ocr2IsBootstrap: false,
+      ocr2Multiaddr: '',
+      ocr2P2PPeerID: '',
+      ocr2KeyBundleID: '',
+    }
+
+    render(
+      <ChainConfigurationForm
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        accounts={[]}
+        chainIDs={[]}
+        p2pKeys={[]}
+        ocrKeys={[]}
+        ocr2Keys={[]}
+        showSubmit
+      />,
+    )
+
+    userEvent.click(getByRole('checkbox', { name: 'OCR' }))
+    userEvent.click(getByRole('button', { name: /submit/i }))
+
+    expect(await findByTestId('ocr2P2PPeerID-helper-text')).toHaveTextContent(
+      'Required',
+    )
+    expect(await findByTestId('ocr2KeyBundleID-helper-text')).toHaveTextContent(
+      'Required',
+    )
+
+    userEvent.click(
+      getByRole('checkbox', {
+        name: 'Is this node running as a bootstrap peer?',
+      }),
+    )
+
+    expect(await findByTestId('ocr2Multiaddr-helper-text')).toHaveTextContent(
       'Required',
     )
   })
