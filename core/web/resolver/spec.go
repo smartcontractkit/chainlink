0d622c9c25c57c43368ec391f77b0d61f2f98a18
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-06-17 10:22:14 -0400

    Add p2pv2_bootstrappers field to OCR1 job spec (#6754)
    * peer_wrapper.go OCR1:  P2PV2_BOOTSTRAPPERS no longer required at startup
    
    * services/ocr/delegate.go:  Update validation of v1 & v2 bootstrap peers
    
    * Add p2pv2Bootstrappers field to OCR1 job spec
    
    - Add corresponding p2pv2_bootstrappers column to ocr_oracle_specs table
    - Update web / ui / presenter / resolver with new field
    - Update tests
    
    * Fix bug in develop branch which was causing several tests to fail with:
    
    ERROR: null value in column "p2pv2_bootstrappers" of relation "ocr_oracle_specs" violates not-null constraint (SQLSTATE 23502)

--- core/web/resolver/spec.go
+++ core/web/resolver/spec.go
@@ -467,6 +467,17 @@ func (r *OCRSpecResolver) P2PBootstrapPeers() *[]string {
 	return &peers
 }
 
+// P2PV2Bootstrappers resolves the OCR1 spec's p2pv2 bootstrappers
+func (r *OCRSpecResolver) P2PV2Bootstrappers() *[]string {
+	if len(r.spec.P2PV2Bootstrappers) == 0 {
+		return nil
+	}
+
+	peers := []string(r.spec.P2PV2Bootstrappers)
+
+	return &peers
+}
+
 // TransmitterAddress resolves the spec's transmitter address
 func (r *OCRSpecResolver) TransmitterAddress() *string {
 	if r.spec.TransmitterAddress == nil {
@@ -543,7 +554,7 @@ func (r *OCR2SpecResolver) MonitoringEndpoint() *string {
 	return &r.spec.MonitoringEndpoint.String
 }
 
-// P2PV2Bootstrappers resolves the spec's p2pv2 bootstrappers
+// P2PV2Bootstrappers resolves the OCR2 spec's p2pv2 bootstrappers
 func (r *OCR2SpecResolver) P2PV2Bootstrappers() *[]string {
 	if len(r.spec.P2PV2Bootstrappers) == 0 {
 		return nil
