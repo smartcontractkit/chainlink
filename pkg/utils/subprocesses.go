105fdc04eda7b9af8af9398a56370e9799d41459
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-06-22 13:42:48 +0100

    add description of failure modes for pkg/utils#Subprocesses

--- pkg/utils/subprocesses.go
+++ pkg/utils/subprocesses.go
@@ -2,7 +2,7 @@ package utils
 
 import "sync"
 
-// Subprocesses is an abstraction over the following pattern:
+// Subprocesses is an abstraction over the following pattern of sync.WaitGroup:
 //
 //   var wg sync.Subprocesses
 //   wg.Add(1)
@@ -17,6 +17,16 @@ import "sync"
 //  subs.Go(func() {
 //     // ...
 //  })
+//
+// Note that it's important to not call Subprocesses.Wait() when there are
+// no `Go()`ed functions in progress. This will panic.
+// There are two cases when this can happen:
+// 1. all the `Go()`ed functions started before the call to `Wait()` have already
+// returned, maybe because a system-wide error or an already cancelled context.
+// 2. Wait() gets called before any function is executed with `Go()`.
+//
+// Reusing a Subprocesses instance is discouraged.
+// See mode details here https://pkg.go.dev/sync#WaitGroup.Add)
 type Subprocesses struct {
 	wg sync.WaitGroup
 }
