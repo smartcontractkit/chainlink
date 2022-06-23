8c78b2be229d127c5a5fdc8a89b73fe4f2d62121
jinhoonbang <jin.bang@smartcontract.com>
2022-06-22 18:15:50 -0700

    scripts for deploying and setting up DKG contract

---
+++ core/scripts/ocr2vrf/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
+	"go.dedis.ch/kyber/v3"
+)
+
+// TODO: Remove below once translator implementation is available in ocr2vrf repo
+type trivialTranslation struct{ base kyber.Point }
+
+func NewTrivialTranslation(base kyber.Point) *trivialTranslation {
+	return &trivialTranslation{base}
+}
+
+func (t *trivialTranslation) TranslateKey(share kyber.Scalar) (kyber.Point, error) {
+	return t.base.Clone().Mul(share, nil), nil
+}
+
+func (t *trivialTranslation) VerifyTranslation(pk1, pk2 kyber.Point) error {
+	if pk1.Equal(pk2) {
+		return nil
+	}
+	return errors.Errorf("putative translated points are not equal")
+}
+
+func (t *trivialTranslation) Name() string {
+	return "trivial translator"
+}
+
+func (t *trivialTranslation) TargetGroup(
+	sourceGroup kyber.Group,
+) (targetGroup kyber.Group, err error) {
+	return sourceGroup, nil
+}
+
+type commonSetConfigArgs struct {
+	onchainPubKeys         string
+	offchainPubKeys        string
+	configPubKeys          string
+	peerIDs                string
+	transmitters           string
+	schedule               string
+	f                      uint
+	deltaProgress          time.Duration
+	deltaResend            time.Duration
+	deltaRound             time.Duration
+	deltaGrace             time.Duration
+	deltaStage             time.Duration
+	maxRounds              time.Duration
+	maxDurationQuery       time.Duration
+	maxDurationObservation time.Duration
+	maxDurationReport      time.Duration
+	maxDurationAccept      time.Duration
+	maxDurationTransmit    time.Duration
+}
+
+type dkgSetConfigArgs struct {
+	commonSetConfigArgs
+	dkgEncryptionPubKeys string
+	dkgSigningPubKeys    string
+	keyID                string
+}
+
+func main() {
+	e := helpers.SetupEnv()
+
+	switch os.Args[1] {
+
+	case "dkg-deploy":
+		deployDKG(e)
+
+	case "vrf-deploy":
+		cmd := flag.NewFlagSet("vrf-deploy", flag.ExitOnError)
+		dkgAddress := cmd.String("dkg-address", "", "dkg contract address")
+		keyID := cmd.String("key-id", "", "key ID")
+		helpers.ParseArgs(cmd, os.Args[2:], "dkg-address", "key-id")
+		deployVRF(e, *dkgAddress, *keyID)
+
+	case "dkg-add-client":
+		cmd := flag.NewFlagSet("dkg-add-client", flag.ExitOnError)
+		dkgAddress := cmd.String("dkg-address", "", "DKG contract address")
+		keyID := cmd.String("key-id", "", "key ID")
+		clientAddress := cmd.String("client-address", "", "client address")
+		helpers.ParseArgs(cmd, os.Args[2:], "dkg-address", "key-id", "client-address")
+		addClientToDKG(e, *dkgAddress, *keyID, *clientAddress)
+
+	case "dkg-remove-client":
+		cmd := flag.NewFlagSet("dkg-add-client", flag.ExitOnError)
+		dkgAddress := cmd.String("dkg-address", "", "DKG contract address")
+		keyID := cmd.String("key-id", "", "key ID")
+		clientAddress := cmd.String("client-address", "", "client address")
+		helpers.ParseArgs(cmd, os.Args[2:], "dkg-address", "key-id", "client-address")
+		removeClientFromDKG(e, *dkgAddress, *keyID, *clientAddress)
+
+	case "dkg-set-config":
+		cmd := flag.NewFlagSet("dkg-set-config", flag.ExitOnError)
+		dkgAddress := cmd.String("dkg-address", "", "DKG contract address")
+		keyID := cmd.String("key-id", "", "key ID")
+		onchainPubKeys := cmd.String("onchain-pub-keys", "", "comma-separated list of OCR on-chain pubkeys")
+		offchainPubKeys := cmd.String("offchain-pub-keys", "", "comma-separated list of OCR off-chain pubkeys")
+		configPubKeys := cmd.String("config-pub-keys", "", "comma-separated list of OCR config pubkeys")
+		peerIDs := cmd.String("peer-ids", "", "comma-separated list of peer IDs")
+		transmitters := cmd.String("transmitters", "", "comma-separated list transmitters")
+		dkgEncryptionPubKeys := cmd.String("dkg-encryption-pub-keys", "", "comma-separated list of DKG encryption pubkeys")
+		dkgSigningPubKeys := cmd.String("dkg-signing-pub-keys", "", "comma-separated list of DKG signing pubkeys")
+		schedule := cmd.String("schedule", "", "comma-separted list of transmission schedule")
+		f := cmd.Uint("f", 1, "number of faulty oracles")
+		deltaProgress := cmd.Duration("delta-progress", 30*time.Second, "duration of delta progress")
+		deltaResend := cmd.Duration("delta-resend", 10*time.Second, "duration of delta resend")
+		deltaRound := cmd.Duration("delta-round", 10*time.Second, "duration of delta round")
+		deltaGrace := cmd.Duration("delta-grace", 20*time.Second, "duration of delta grace")
+		deltaStage := cmd.Duration("delta-stage", 20*time.Second, "duration of delta grace")
+		maxRounds := cmd.Duration("max-rounds", 3*time.Second, "maximum number of rounds")
+		maxDurationQuery := cmd.Duration("max-duration-query", 5*time.Second, "maximum duration of query")
+		maxDurationObservation := cmd.Duration("max-duration-observation", 5*time.Second, "maximum duration of observation method")
+		maxDurationReport := cmd.Duration("max-duration-report", 5*time.Second, "maximum duration of report method")
+		maxDurationAccept := cmd.Duration("max-duration-accept", 5*time.Second, "maximum duration of shouldAcceptFinalizedReport method")
+		maxDurationTransmit := cmd.Duration("max-duration-transmit", 5*time.Second, "maximum duration of shouldTransmitAcceptedReport method")
+
+		helpers.ParseArgs(cmd,
+			os.Args[2:],
+			"dkg-address",
+			"key-id",
+			"onchain-pub-keys",
+			"offchain-pub-keys",
+			"config-pub-keys",
+			"peer-ids",
+			"transmitters",
+			"dkg-encryption-pub-keys",
+			"dkg-signing-pub-keys",
+			"schedule")
+
+		commands := dkgSetConfigArgs{
+			commonSetConfigArgs: commonSetConfigArgs{
+				onchainPubKeys:         *onchainPubKeys,
+				offchainPubKeys:        *offchainPubKeys,
+				configPubKeys:          *configPubKeys,
+				peerIDs:                *peerIDs,
+				transmitters:           *transmitters,
+				schedule:               *schedule,
+				f:                      *f,
+				deltaProgress:          *deltaProgress,
+				deltaResend:            *deltaResend,
+				deltaRound:             *deltaRound,
+				deltaGrace:             *deltaGrace,
+				deltaStage:             *deltaStage,
+				maxRounds:              *maxRounds,
+				maxDurationQuery:       *maxDurationQuery,
+				maxDurationObservation: *maxDurationObservation,
+				maxDurationReport:      *maxDurationReport,
+				maxDurationAccept:      *maxDurationAccept,
+				maxDurationTransmit:    *maxDurationTransmit,
+			},
+			dkgEncryptionPubKeys: *dkgEncryptionPubKeys,
+			dkgSigningPubKeys:    *dkgSigningPubKeys,
+			keyID:                *keyID,
+		}
+
+		setDKGConfig(e, *dkgAddress, commands)
+
+	default:
+		panic("unrecognized subcommand: " + os.Args[1])
+	}
+}
--- core/scripts/vrfv2/testnet/util.go
+++ core/scripts/vrfv2/testnet/util.go
@@ -5,15 +5,11 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
-	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/shopspring/decimal"
 	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/blockhash_store"
 	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/link_token_interface"
 	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/vrf_coordinator_v2"
@@ -22,80 +18,65 @@ import (
 	"github.com/smartcontractkit/chainlink/core/utils"
 )
 
-func confirmTXMined(context context.Context, client *ethclient.Client, transaction *types.Transaction, chainID int64, txInfo ...string) {
-	fmt.Println("Executing TX", helpers.ExplorerLink(chainID, transaction.Hash()), txInfo)
-	receipt, err := bind.WaitMined(context, client, transaction)
+func deployBHS(e helpers.Environment) (blockhashStoreAddress common.Address) {
+	_, tx, _, err := blockhash_store.DeployBlockhashStore(e.Owner, e.Ec)
 	helpers.PanicErr(err)
-	fmt.Println("TX", receipt.TxHash, "mined. \nBlock Number:", receipt.BlockNumber, "\nGas Used: ", receipt.GasUsed)
-}
-
-func confirmContractDeployed(context context.Context, client *ethclient.Client, transaction *types.Transaction, chainID int64) (address common.Address) {
-	fmt.Println("Executing contract deployment, TX:", helpers.ExplorerLink(chainID, transaction.Hash()))
-	contractAddress, err := bind.WaitDeployed(context, client, transaction)
-	helpers.PanicErr(err)
-	fmt.Println("Contract Address:", contractAddress.String())
-	return contractAddress
-}
-
-func deployBHS(e environment) (blockhashStoreAddress common.Address) {
-	_, tx, _, err := blockhash_store.DeployBlockhashStore(e.owner, e.ec)
-	helpers.PanicErr(err)
-	return confirmContractDeployed(context.Background(), e.ec, tx, e.chainID)
+	return helpers.ConfirmContractDeployed(context.Background(), e.Ec, tx, e.ChainID)
 }
 
 func deployCoordinator(
-	e environment,
+	e helpers.Environment,
 	linkAddress string,
 	bhsAddress string,
 	linkEthAddress string,
 ) (coordinatorAddress common.Address) {
 	_, tx, _, err := vrf_coordinator_v2.DeployVRFCoordinatorV2(
-		e.owner,
-		e.ec,
+		e.Owner,
+		e.Ec,
 		common.HexToAddress(linkAddress),
 		common.HexToAddress(bhsAddress),
 		common.HexToAddress(linkEthAddress))
 	helpers.PanicErr(err)
-	return confirmContractDeployed(context.Background(), e.ec, tx, e.chainID)
+	return helpers.ConfirmContractDeployed(context.Background(), e.Ec, tx, e.ChainID)
 }
 
-func eoaAddConsumerToSub(e environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, subID uint64, consumerAddress string) {
-	txadd, err := coordinator.AddConsumer(e.owner, subID, common.HexToAddress(consumerAddress))
+func eoaAddConsumerToSub(e helpers.Environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, subID uint64, consumerAddress string) {
+	txadd, err := coordinator.AddConsumer(e.Owner, subID, common.HexToAddress(consumerAddress))
 	helpers.PanicErr(err)
-	confirmTXMined(context.Background(), e.ec, txadd, e.chainID)
+	helpers.ConfirmTXMined(context.Background(), e.Ec, txadd, e.ChainID)
 }
 
-func eoaCreateSub(e environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2) {
-	tx, err := coordinator.CreateSubscription(e.owner)
+func eoaCreateSub(e helpers.Environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2) {
+	tx, err := coordinator.CreateSubscription(e.Owner)
 	helpers.PanicErr(err)
-	confirmTXMined(context.Background(), e.ec, tx, e.chainID)
+	helpers.ConfirmTXMined(context.Background(), e.Ec, tx, e.ChainID)
 }
 
-func eoaDeployConsumer(e environment, coordinatorAddress string, linkAddress string) (consumerAddress common.Address) {
+func eoaDeployConsumer(e helpers.Environment, coordinatorAddress string, linkAddress string) (consumerAddress common.Address) {
 	_, tx, _, err := vrf_external_sub_owner_example.DeployVRFExternalSubOwnerExample(
-		e.owner,
-		e.ec,
+		e.Owner,
+		e.Ec,
 		common.HexToAddress(coordinatorAddress),
 		common.HexToAddress(linkAddress))
 	helpers.PanicErr(err)
-	return confirmContractDeployed(context.Background(), e.ec, tx, e.chainID)
+	return helpers.ConfirmContractDeployed(context.Background(), e.Ec, tx, e.ChainID)
 }
 
-func eoaFundSubscription(e environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, linkAddress string, amount *big.Int, subID uint64) {
-	linkToken, err := link_token_interface.NewLinkToken(common.HexToAddress(linkAddress), e.ec)
+func eoaFundSubscription(e helpers.Environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, linkAddress string, amount *big.Int, subID uint64) {
+	linkToken, err := link_token_interface.NewLinkToken(common.HexToAddress(linkAddress), e.Ec)
 	helpers.PanicErr(err)
-	bal, err := linkToken.BalanceOf(nil, e.owner.From)
+	bal, err := linkToken.BalanceOf(nil, e.Owner.From)
 	helpers.PanicErr(err)
-	fmt.Println("Initial account balance:", bal, e.owner.From.String(), "Funding amount:", amount.String())
+	fmt.Println("Initial account balance:", bal, e.Owner.From.String(), "Funding amount:", amount.String())
 	b, err := utils.GenericEncode([]string{"uint64"}, subID)
 	helpers.PanicErr(err)
-	e.owner.GasLimit = 500000
-	tx, err := linkToken.TransferAndCall(e.owner, coordinator.Address(), amount, b)
+	e.Owner.GasLimit = 500000
+	tx, err := linkToken.TransferAndCall(e.Owner, coordinator.Address(), amount, b)
 	helpers.PanicErr(err)
-	confirmTXMined(context.Background(), e.ec, tx, e.chainID, fmt.Sprintf("sub ID: %d", subID))
+	helpers.ConfirmTXMined(context.Background(), e.Ec, tx, e.ChainID, fmt.Sprintf("sub ID: %d", subID))
 }
 
-func printCoordinatorConfig(e environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2) {
+func printCoordinatorConfig(e helpers.Environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2) {
 	cfg, err := coordinator.GetConfig(nil)
 	helpers.PanicErr(err)
 
@@ -107,7 +88,7 @@ func printCoordinatorConfig(e environment, coordinator vrf_coordinator_v2.VRFCoo
 }
 
 func setCoordinatorConfig(
-	e environment,
+	e helpers.Environment,
 	coordinator vrf_coordinator_v2.VRFCoordinatorV2,
 	minConfs uint16,
 	maxGasLimit uint32,
@@ -117,7 +98,7 @@ func setCoordinatorConfig(
 	feeConfig vrf_coordinator_v2.VRFCoordinatorV2FeeConfig,
 ) {
 	tx, err := coordinator.SetConfig(
-		e.owner,
+		e.Owner,
 		minConfs,               // minRequestConfirmations
 		maxGasLimit,            // max gas limit
 		stalenessSeconds,       // stalenessSeconds
@@ -126,53 +107,26 @@ func setCoordinatorConfig(
 		feeConfig,
 	)
 	helpers.PanicErr(err)
-	confirmTXMined(context.Background(), e.ec, tx, e.chainID)
+	helpers.ConfirmTXMined(context.Background(), e.Ec, tx, e.ChainID)
 }
 
-func registerCoordinatorProvingKey(e environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, uncompressed string, oracleAddress string) {
+func registerCoordinatorProvingKey(e helpers.Environment, coordinator vrf_coordinator_v2.VRFCoordinatorV2, uncompressed string, oracleAddress string) {
 	pubBytes, err := hex.DecodeString(uncompressed)
 	helpers.PanicErr(err)
 	pk, err := crypto.UnmarshalPubkey(pubBytes)
 	helpers.PanicErr(err)
-	tx, err := coordinator.RegisterProvingKey(e.owner,
+	tx, err := coordinator.RegisterProvingKey(e.Owner,
 		common.HexToAddress(oracleAddress),
 		[2]*big.Int{pk.X, pk.Y})
 	helpers.PanicErr(err)
-	confirmTXMined(
+	helpers.ConfirmTXMined(
 		context.Background(),
-		e.ec,
+		e.Ec,
 		tx,
-		e.chainID,
+		e.ChainID,
 		fmt.Sprintf("Uncompressed public key: %s,", uncompressed),
 		fmt.Sprintf("Oracle address: %s,", oracleAddress),
 	)
-}
-
-func parseIntSlice(arg string) (ret []*big.Int) {
-	parts := strings.Split(arg, ",")
-	ret = []*big.Int{}
-	for _, part := range parts {
-		ret = append(ret, decimal.RequireFromString(part).BigInt())
-	}
-	return ret
-}
-
-func parseAddressSlice(arg string) (ret []common.Address) {
-	parts := strings.Split(arg, ",")
-	ret = []common.Address{}
-	for _, part := range parts {
-		ret = append(ret, common.HexToAddress(part))
-	}
-	return
-}
-
-func parseHashSlice(arg string) (ret []common.Hash) {
-	parts := strings.Split(arg, ",")
-	ret = []common.Hash{}
-	for _, part := range parts {
-		ret = append(ret, common.HexToHash(part))
-	}
-	return
 }
 
 // decreasingBlockRange creates a continugous block range starting with
