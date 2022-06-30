fe1034ba929be5f048d320d4baba7bdf2f7cd3b2
Makram <makramkd@users.noreply.github.com>
2022-06-30 10:31:15 -0400

    feat: DKG encryption keystore support (#6825)

--- core/web/router.go
+++ core/web/router.go
@@ -321,6 +321,7 @@ func v2Routes(app chainlink.Application, r *gin.RouterGroup) {
 			{"terra", NewTerraKeysController(app)},
 			{"starknet", NewStarkNetKeysController(app)},
 			{"dkgsign", NewDKGSignKeysController(app)},
+			{"dkgencrypt", NewDKGEncryptKeysController(app)},
 		} {
 			authv2.GET("/keys/"+keys.path, keys.kc.Index)
 			authv2.POST("/keys/"+keys.path, keys.kc.Create)
