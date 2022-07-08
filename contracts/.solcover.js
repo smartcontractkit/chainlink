b80a06100cab41ae277332f7a308072c2010113f
Steve Ellis <email@steveell.is>
2022-07-08 13:18:01 -0400

    do not count coverage on vendored code

--- contracts/.solcover.js
+++ contracts/.solcover.js
@@ -3,9 +3,12 @@ module.exports = {
     'v0.4/',
     'v0.5/',
     'v0.6/tests',
+    'v0.6/vendor',
     'v0.7/tests',
+    'v0.7/vendor',
     'v0.8/mocks',
     'v0.8/tests',
+    'v0.8/vendor',
   ],
   mocha: {
     grep: '@skip-coverage', // Find everything with this tag
