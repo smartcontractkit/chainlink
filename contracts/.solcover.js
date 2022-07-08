4d1730d3d8a07a917cf646f5ec4485a10ad78227
Steve Ellis <email@steveell.is>
2022-07-08 14:49:13 -0400

    ignore coverage on when tested in Golang

--- contracts/.solcover.js
+++ contracts/.solcover.js
@@ -4,11 +4,13 @@ module.exports = {
     'v0.5/',
     'v0.6/tests',
     'v0.6/vendor',
+    'v0.6/VRF*.sol' // tested in Golang
     'v0.7/tests',
     'v0.7/vendor',
     'v0.8/mocks',
     'v0.8/tests',
     'v0.8/vendor',
+    'v0.8/VRF*.sol' // tested in Golang
   ],
   mocha: {
     grep: '@skip-coverage', // Find everything with this tag
