db1535f3322b76b96e9498504c1e3e10f946be4b
Chris <104409744+vreff@users.noreply.github.com>
2022-07-15 07:54:22 -0400

    Add report serializer, leave deserializer unimplemented.

---
+++ core/services/ocr2/plugins/ocr2vrf/report_serializer.go
@@ -0,0 +1,47 @@
+package ocr2vrf
+
+import (
+	"github.com/pkg/errors"
+	"go.dedis.ch/kyber/v3"
+
+	"github.com/smartcontractkit/ocr2vrf/ocr2vrf"
+	types "github.com/smartcontractkit/ocr2vrf/types"
+)
+
+type ReportSerializer struct {
+	G kyber.Group
+}
+
+// Return the serialized byte representation of the report, as
+// expected by the onchain machinery.
+func (serializer *ReportSerializer) SerializeReport(r types.AbstractReport) ([]byte, error) {
+
+	s := ocr2vrf.ReportSerializer{
+		G: serializer.G,
+	}
+	packed, err := s.SerializeReport(r)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "serialize report")
+	}
+
+	return packed, nil
+}
+
+func (serializer *ReportSerializer) DeserializeReport(reportBytes []byte) (types.AbstractReport, error) {
+	panic("not implemented.")
+}
+
+// Return the longest possible report which can be passed onchain
+func (serializer *ReportSerializer) MaxReportLength() uint {
+	return 150_000 // TODO: change this.
+}
+
+// Return the predicted length of the output from SerializeReport
+func (serializer *ReportSerializer) ReportLength(a types.AbstractReport) uint {
+	s, err := serializer.SerializeReport(a)
+	if err != nil {
+		return 0
+	}
+	return uint(len(s))
+}
---
+++ core/services/ocr2/plugins/ocr2vrf/report_serializer_test.go
@@ -0,0 +1,72 @@
+package ocr2vrf_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/ocr2vrf/altbn_128"
+	"github.com/smartcontractkit/ocr2vrf/types"
+
+	"github.com/smartcontractkit/chainlink/core/services/ocr2/plugins/ocr2vrf"
+)
+
+func Test_Serialize_Deserialize(t *testing.T) {
+	altbn128Suite := &altbn_128.PairingSuite{}
+	reportSerializer := ocr2vrf.ReportSerializer{
+		G: altbn128Suite.G1(),
+	}
+
+	unserializedReport := types.AbstractReport{
+		JulesPerFeeCoin:   big.NewInt(10),
+		RecentBlockHeight: 100,
+		RecentBlockHash:   common.HexToHash("0x002"),
+		Outputs: []types.AbstractVRFOutput{{
+			BlockHeight:       10,
+			ConfirmationDelay: 20,
+			Callbacks: []types.AbstractCostedCallbackRequest{{
+				RequestID:      1,
+				NumWords:       2,
+				Requester:      common.HexToAddress("0x03"),
+				Arguments:      []byte{4},
+				SubscriptionID: 5,
+				GasAllowance:   big.NewInt(6),
+				Price:          big.NewInt(7),
+			}},
+		}},
+	}
+	_, err := reportSerializer.SerializeReport(unserializedReport)
+	require.NoError(t, err)
+}
+
+func Test_Serialize_Length(t *testing.T) {
+	altbn128Suite := &altbn_128.PairingSuite{}
+	reportSerializer := ocr2vrf.ReportSerializer{
+		G: altbn128Suite.G1(),
+	}
+
+	unserializedReport := types.AbstractReport{
+		JulesPerFeeCoin:   big.NewInt(10),
+		RecentBlockHeight: 100,
+		RecentBlockHash:   common.HexToHash("0x002"),
+		Outputs: []types.AbstractVRFOutput{{
+			BlockHeight:       10,
+			ConfirmationDelay: 20,
+			Callbacks: []types.AbstractCostedCallbackRequest{{
+				RequestID:      1,
+				NumWords:       2,
+				Requester:      common.HexToAddress("0x03"),
+				Arguments:      []byte{4},
+				SubscriptionID: 5,
+				GasAllowance:   big.NewInt(6),
+				Price:          big.NewInt(7),
+			}},
+		}},
+	}
+	r, err := reportSerializer.SerializeReport(unserializedReport)
+	require.NoError(t, err)
+
+	require.Equal(t, uint(len(r)), reportSerializer.ReportLength(unserializedReport))
+}
