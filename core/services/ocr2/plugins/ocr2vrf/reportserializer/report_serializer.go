641c709e18bb235e3eb4c7256392e9ea76e8a651
Chris <104409744+vreff@users.noreply.github.com>
2022-07-18 15:41:36 -0400

    Add type checking and consume the updated ocr2vrf repo, adjust methods comments and make private struct

--- core/services/ocr2/plugins/ocr2vrf/reportserializer/report_serializer.go
+++ core/services/ocr2/plugins/ocr2vrf/reportserializer/report_serializer.go
@@ -8,18 +8,25 @@ import (
 	types "github.com/smartcontractkit/ocr2vrf/types"
 )
 
-type ReportSerializer struct {
-	G kyber.Group
+type reportSerializer struct {
+	e ocr2vrf.EthereumReportSerializer
 }
 
-// Return the serialized byte representation of the report, as
-// expected by the onchain machinery.
-func (serializer *ReportSerializer) SerializeReport(r types.AbstractReport) ([]byte, error) {
+var _ types.ReportSerializer = (*reportSerializer)(nil)
 
-	s := ocr2vrf.EthereumReportSerializer{
-		G: serializer.G,
+// NewReportSerializer provides a serialization component for sending byte-encoded reports on-chain.
+func NewReportSerializer(encryptionGroup kyber.Group) types.ReportSerializer {
+	return &reportSerializer{
+		e: ocr2vrf.EthereumReportSerializer{
+			G: encryptionGroup,
+		},
 	}
-	packed, err := s.SerializeReport(r)
+}
+
+// SerializeReport serializes an abstract report into abi-encoded bytes.
+func (serializer *reportSerializer) SerializeReport(r types.AbstractReport) ([]byte, error) {
+
+	packed, err := serializer.e.SerializeReport(r)
 
 	if err != nil {
 		return nil, errors.Wrap(err, "serialize report")
@@ -28,17 +35,25 @@ func (serializer *ReportSerializer) SerializeReport(r types.AbstractReport) ([]b
 	return packed, nil
 }
 
-func (serializer *ReportSerializer) DeserializeReport(reportBytes []byte) (types.AbstractReport, error) {
+// DeserializeReport deserializes a serialized byte array into a report.
+func (serializer *reportSerializer) DeserializeReport(reportBytes []byte) (types.AbstractReport, error) {
+	// Leaving unimplemented, as serialization here is not two-way. The object that is abi-encoded on-chain is a BeaconReport, not an AbstractReport.
+	// So, the AbstractReport is first converted to a BeaconReport before the encoding. Converting an AbstractReport to a BeaconReport requires
+	// the removal of some fields, so when it is converted back to a BeaconReport and then deserialized, those fields are missing.
+	// Consequently, either the returned object from this function will be an abstract report
+	// that has had some fields removed/zeroed, or the return type will be changed to a BeaconReport, which cannot be re-serialized.
+	//
+	// Also, the need for off-chain deserialization is not currently clear.
 	panic("implement me")
 }
 
-// Return the longest possible report which can be passed onchain
-func (serializer *ReportSerializer) MaxReportLength() uint {
+// MaxReportLength gives the max length of a report to be transmitted on-chain.
+func (serializer *reportSerializer) MaxReportLength() uint {
 	return 150_000 // TODO: change this.
 }
 
-// Return the predicted length of the output from SerializeReport
-func (serializer *ReportSerializer) ReportLength(a types.AbstractReport) uint {
+// ReportLength provides the expected report length of a report.
+func (serializer *reportSerializer) ReportLength(a types.AbstractReport) uint {
 	s, err := serializer.SerializeReport(a)
 	if err != nil {
 		return 0
--- core/services/ocr2/plugins/ocr2vrf/reportserializer/report_serializer_test.go
+++ core/services/ocr2/plugins/ocr2vrf/reportserializer/report_serializer_test.go
@@ -15,9 +15,7 @@ import (
 
 func Test_Serialize_Deserialize(t *testing.T) {
 	altbn128Suite := &altbn_128.PairingSuite{}
-	reportSerializer := reportserializer.ReportSerializer{
-		G: altbn128Suite.G1(),
-	}
+	reportSerializer := reportserializer.NewReportSerializer(altbn128Suite.G1())
 
 	unserializedReport := types.AbstractReport{
 		JulesPerFeeCoin:   big.NewInt(10),
@@ -39,36 +37,6 @@ func Test_Serialize_Deserialize(t *testing.T) {
 	}
 	r, err := reportSerializer.SerializeReport(unserializedReport)
 	require.NoError(t, err)
-	require.True(t, len(r) > 0)
-	// TODO: Add deserialization after this point to verify.
-}
-
-func Test_Serialize_Length(t *testing.T) {
-	altbn128Suite := &altbn_128.PairingSuite{}
-	reportSerializer := reportserializer.ReportSerializer{
-		G: altbn128Suite.G1(),
-	}
-
-	unserializedReport := types.AbstractReport{
-		JulesPerFeeCoin:   big.NewInt(10),
-		RecentBlockHeight: 100,
-		RecentBlockHash:   common.HexToHash("0x002"),
-		Outputs: []types.AbstractVRFOutput{{
-			BlockHeight:       10,
-			ConfirmationDelay: 20,
-			Callbacks: []types.AbstractCostedCallbackRequest{{
-				RequestID:      1,
-				NumWords:       2,
-				Requester:      common.HexToAddress("0x03"),
-				Arguments:      []byte{4},
-				SubscriptionID: 5,
-				GasAllowance:   big.NewInt(6),
-				Price:          big.NewInt(7),
-			}},
-		}},
-	}
-	r, err := reportSerializer.SerializeReport(unserializedReport)
-	require.NoError(t, err)
-
 	require.Equal(t, uint(len(r)), reportSerializer.ReportLength(unserializedReport))
+	// TODO: Add deserialization after this point to verify.
 }
