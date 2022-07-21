1af4865b9e3fda5dbd3786dceedefe3a06d80a3f
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-07-21 10:46:49 +0100

    fix minor issues

--- contracts/test/v0.8/dev/KeeperRegistryCheckUpkeepGasUsageWrapper.test.ts
+++ contracts/test/v0.8/dev/KeeperRegistryCheckUpkeepGasUsageWrapper.test.ts
@@ -7,7 +7,7 @@ import {
   deployMockContract,
   MockContract,
 } from '@ethereum-waffle/mock-contract'
-import { abi as registryAbi } from '../../../artifacts/src/v0.8/dev/KeeperRegistryDev.sol/KeeperRegistryDev.json'
+import { abi as registryAbi } from '../../../artifacts/src/v0.8/KeeperRegistry.sol/KeeperRegistry.json'
 
 let personas: Personas
 let owner: Signer
