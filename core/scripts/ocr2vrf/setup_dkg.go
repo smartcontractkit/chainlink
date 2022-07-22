73ff648fd8be9c0ae04d169a98169f1ff9307f18
Makram <makramkd@users.noreply.github.com>
2022-07-22 18:22:00 -0400

    feat: automate ocr2vrf job creation (#7018)
    Co-authored-by: Chris C <104409744+vreff@users.noreply.github.com>

--- core/scripts/ocr2vrf/setup_dkg.go
+++ core/scripts/ocr2vrf/setup_dkg.go
@@ -1,30 +1,25 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"math/big"
 	"os"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/urfave/cli"
 
 	"github.com/smartcontractkit/chainlink/core/cmd"
-	"github.com/smartcontractkit/chainlink/core/config"
-	"github.com/smartcontractkit/chainlink/core/logger"
 	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
 )
 
 func setupDKGNodes(e helpers.Environment) {
-	client := newDKGSetupClient()
+	client := newSetupClient()
 	app := cmd.NewApp(client)
 
 	cmd := flag.NewFlagSet("dkg-setup", flag.ExitOnError)
 	keyID := cmd.String("key-id", "aee00d81f822f882b6fe28489822f59ebb21ea95c0ae21d9f67c0239461148fc", "key ID")
-	apiFile := cmd.String("api", "../../../tools/secrets/apicredentials", "api file")
+	apiFile := cmd.String("api", "../../../tools/secrets/apicredentials", "api credentials file")
 	passwordFile := cmd.String("password", "../../../tools/secrets/password.txt", "password file")
 	databasePrefix := cmd.String("database-prefix", "postgres://postgres:postgres@localhost:5432/dkg-test", "database prefix")
 	databaseSuffixes := cmd.String("database-suffixes", "sslmode=disable", "database parameters to be added")
@@ -33,7 +28,7 @@ func setupDKGNodes(e helpers.Environment) {
 	helpers.ParseArgs(cmd, os.Args[2:])
 
 	if *nodeCount < 6 {
-		fmt.Println("Node count too low for DKG job.")
+		fmt.Println("Node count too low for DKG job, need at least 6.")
 		os.Exit(1)
 	}
 
@@ -60,6 +55,7 @@ func setupDKGNodes(e helpers.Environment) {
 		flagSet.String("api", *apiFile, "api file")
 		flagSet.String("password", *passwordFile, "password file")
 		flagSet.String("bootstrapPort", fmt.Sprintf("%d", 8000), "port of bootstrap")
+		flagSet.String("job-type", string(jobTypeDKG), "the job type")
 		flagSet.String("keyID", *keyID, "")
 		flagSet.String("contractID", dkgAddress, "the contract address of the DKG")
 		flagSet.Int64("chainID", e.ChainID, "the chain ID")
@@ -78,7 +74,7 @@ func setupDKGNodes(e helpers.Environment) {
 		resetDatabase(client, context, i, *databasePrefix, *databaseSuffixes)
 
 		// Setup DKG node.
-		payload := setupDKGNodeFromClient(client, context)
+		payload := setupOCR2VRFNodeFromClient(client, context)
 
 		// Append arguments for dkg-set-config command.
 		onChainPublicKeys = append(onChainPublicKeys, payload.OnChainPublicKey)
@@ -91,7 +87,7 @@ func setupDKGNodes(e helpers.Environment) {
 	}
 
 	// Fund transmitters with funding amount.
-	fundNodes(e, transmitters, *fundingAmount)
+	helpers.FundNodes(e, transmitters, big.NewInt(*fundingAmount))
 
 	// Construct and print dkg-set-config command.
 	fmt.Println("Generated setConfig Command:")
@@ -109,68 +105,4 @@ func setupDKGNodes(e helpers.Environment) {
 	)
 
 	fmt.Println(command)
-}
-
-func fundNodes(e helpers.Environment, transmitters []string, fundingAmount int64) {
-	fmt.Println("Funding Nodes:")
-
-	block, err := e.Ec.BlockNumber(context.Background())
-	helpers.PanicErr(err)
-
-	nonce, err := e.Ec.NonceAt(context.Background(), e.Owner.From, big.NewInt(int64(block)))
-	helpers.PanicErr(err)
-
-	for i := 1; i < len(transmitters); i++ {
-		tx := types.NewTransaction(
-			nonce+uint64(i),
-			common.HexToAddress(transmitters[i]),
-			big.NewInt(fundingAmount),
-			uint64(21000),
-			e.Owner.GasPrice,
-			nil,
-		)
-		signedTx, err := e.Owner.Signer(e.Owner.From, tx)
-		helpers.PanicErr(err)
-		err = e.Ec.SendTransaction(context.Background(), signedTx)
-		helpers.PanicErr(err)
-
-		fmt.Printf("Sending to %s: %s\n", transmitters[i], helpers.ExplorerLink(e.ChainID, signedTx.Hash()))
-	}
-}
-
-func setupDKGNodeFromClient(client *cmd.Client, context *cli.Context) *cmd.SetupDKGNodePayload {
-	payload, err := client.ConfigureDKGNode(context)
-	helpers.PanicErr(err)
-
-	return payload
-}
-
-func configureEnvironmentVariables() {
-	helpers.PanicErr(os.Setenv("FEATURE_OFFCHAIN_REPORTING2", "true"))
-	helpers.PanicErr(os.Setenv("SKIP_DATABASE_PASSWORD_COMPLEXITY_CHECK", "true"))
-}
-
-func resetDatabase(client *cmd.Client, context *cli.Context, index int, databasePrefix string, databaseSuffixes string) {
-	helpers.PanicErr(os.Setenv("DATABASE_URL", fmt.Sprintf("%s-%d?%s", databasePrefix, index, databaseSuffixes)))
-	helpers.PanicErr(client.ResetDatabase(context))
-}
-
-func newDKGSetupClient() *cmd.Client {
-	lggr, closeLggr := logger.NewLogger()
-	cfg := config.NewGeneralConfig(lggr)
-
-	prompter := cmd.NewTerminalPrompter()
-	return &cmd.Client{
-		Renderer:                       cmd.RendererTable{Writer: os.Stdout},
-		Config:                         cfg,
-		Logger:                         lggr,
-		CloseLogger:                    closeLggr,
-		AppFactory:                     cmd.ChainlinkAppFactory{},
-		KeyStoreAuthenticator:          cmd.TerminalKeyStoreAuthenticator{Prompter: prompter},
-		FallbackAPIInitializer:         cmd.NewPromptingAPIInitializer(prompter, lggr),
-		Runner:                         cmd.ChainlinkRunner{},
-		PromptingSessionRequestBuilder: cmd.NewPromptingSessionRequestBuilder(prompter),
-		ChangePasswordPrompter:         cmd.NewChangePasswordPrompter(),
-		PasswordPrompter:               cmd.NewPasswordPrompter(),
-	}
 }
