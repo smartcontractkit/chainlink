17eca634639436501ee54cfb26578959e4590aec
Adam Hamrick <adam.hamrick@smartcontract.com>
2022-07-28 13:35:12 -0400

    Arbitrum Test Support (#7098)
    * Rinkeby and Goerli

--- integration-tests/contracts/contract_models.go
+++ integration-tests/contracts/contract_models.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/smartcontractkit/chainlink-testing-framework/contracts/ethereum"
-	"github.com/smartcontractkit/chainlink/integration-tests/client"
 	ocrConfigHelper "github.com/smartcontractkit/libocr/offchainreporting/confighelper"
 	ocrConfigHelper2 "github.com/smartcontractkit/libocr/offchainreporting2/confighelper"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/contracts/ethereum"
+	"github.com/smartcontractkit/chainlink/integration-tests/client"
 )
 
 type FluxAggregatorOptions struct {
