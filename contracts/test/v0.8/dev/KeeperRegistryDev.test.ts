b39ac6f837c7ce09065ea75bf35dc3349c02161a
FelixFan1992 <fankejin@gmail.com>
2022-08-01 15:40:54 -0400

    fix tests

--- contracts/test/v0.8/dev/KeeperRegistryDev.test.ts
+++ contracts/test/v0.8/dev/KeeperRegistryDev.test.ts
@@ -767,7 +767,7 @@ describe('KeeperRegistryDev', () => {
             registry
               .connect(zeroAddress)
               .callStatic.checkUpkeep(id, await keeper1.getAddress()),
-            'OnlyActiveUpkeep()',
+            'OnlyNonPausedUpkeep()',
           )
         })
 
@@ -1105,7 +1105,7 @@ describe('KeeperRegistryDev', () => {
 
         await evmRevert(
           registry.connect(keeper1).performUpkeep(id, '0x'),
-          'OnlyActiveUpkeep()',
+          'OnlyNonPausedUpkeep()',
         )
       })
 
