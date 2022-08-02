f966868ec72538dfe8d859c934db705684e04b2d
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2022-08-02 16:05:38 -0400

    Fixes error notation

--- core/logger/pyroscope.go
+++ core/logger/pyroscope.go
@@ -7,8 +7,8 @@ import (
 	"github.com/pyroscope-io/client/pyroscope"
 )
 
-// PyroscopeServerAddrMissingErr error to be triggered if the server address is missing
-var PyroscopeServerAddrMissingErr = errors.New("pyroscope server address is missing")
+// ErrPyroscopeServerAddrMissing error to be triggered if the server address is missing
+var ErrPyroscopeServerAddrMissing = errors.New("pyroscope server address is missing")
 
 // PyroscopeConfig represents the expected configuration for Pyroscope to properly work
 type PyroscopeConfig interface {
@@ -22,22 +22,21 @@ type PyroscopeConfig interface {
 // StartPyroscope starts continuous profiling of the Chainlink Node
 func StartPyroscope(cfg PyroscopeConfig) (*pyroscope.Profiler, error) {
 	if cfg.PyroscopeServerAddress() == "" {
-		return nil, PyroscopeServerAddrMissingErr
+		return nil, ErrPyroscopeServerAddrMissing
 	}
 
 	runtime.SetBlockProfileRate(cfg.AutoPprofBlockProfileRate())
 	runtime.SetMutexProfileFraction(cfg.AutoPprofMutexProfileFraction())
 
 	return pyroscope.Start(pyroscope.Config{
-		// Maybe configurable to identify the specific NOP
+		// Maybe configurable to identify the specific NOP - TBD
 		ApplicationName: "chainlink-node",
 
-		// TBD
 		ServerAddress: cfg.PyroscopeServerAddress(),
 		AuthToken:     cfg.PyroscopeAuthToken(),
 
-		// may end up disabling logging
-		Logger: pyroscope.StandardLogger,
+		// We disable logging the profiling info, it will be in the Pyroscope instance anyways...
+		Logger: nil,
 
 		ProfileTypes: []pyroscope.ProfileType{
 			// these profile types are enabled by default:
