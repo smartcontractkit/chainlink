e6a4e56b79a292d64f1f512708fc510e02895fd3
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2022-08-03 16:40:38 -0400

    Loosens up geth error matching (#7105)
    Adds supporting comment
    
    Adds test case for longer error msg
    
    Restore `$` on most geth errors
    
    Updates explanation message
    
    refactor; support 'nonce too high' and 'execution reverted' variants
    
    core/chains/evm/config: remove Arbitrum validation requiring FixedPrice estimator
    
    update changelog
    
    support insufficient funds
    
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/chains/evm/client/errors.go
+++ core/chains/evm/client/errors.go
@@ -118,14 +118,15 @@ var erigon = ClientErrors{
 
 // Arbitrum
 // https://github.com/OffchainLabs/arbitrum/blob/cac30586bc10ecc1ae73e93de517c90984677fdb/packages/arb-evm/evm/result.go#L158
-var arbitrumFatal = regexp.MustCompile(`(: |^)(invalid message format|forbidden sender address|execution reverted: error code)$`)
+// nitro: https://github.com/OffchainLabs/go-ethereum/blob/master/core/state_transition.go
+var arbitrumFatal = regexp.MustCompile(`(: |^)(invalid message format|forbidden sender address|execution reverted(: error code)?)$|(: |^)nonce too high(:|$)`)
 var arbitrum = ClientErrors{
 	// TODO: Arbitrum returns this in case of low or high nonce. Update this when Arbitrum fix it
 	// https://app.shortcut.com/chainlinklabs/story/16801/add-full-support-for-incorrect-nonce-on-arbitrum
-	NonceTooLow: regexp.MustCompile(`(: |^)invalid transaction nonce$`),
+	NonceTooLow: regexp.MustCompile(`(: |^)invalid transaction nonce$|(: |^)nonce too low(:|$)`),
 	// TODO: Is it terminally or replacement?
 	TerminallyUnderpriced: regexp.MustCompile(`(: |^)gas price too low$`),
-	InsufficientEth:       regexp.MustCompile(`(: |^)not enough funds for gas`),
+	InsufficientEth:       regexp.MustCompile(`(: |^)(not enough funds for gas|insufficient funds for gas \* price \+ value)`),
 	Fatal:                 arbitrumFatal,
 }
 
