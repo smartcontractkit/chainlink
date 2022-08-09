66f7a375bb97b4af4163afea6f83f6682d1dd1eb
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-08-09 09:11:48 +0300

    Fixing linter issues

--- core/services/vrf/delegate_test.go
+++ core/services/vrf/delegate_test.go
@@ -253,9 +253,9 @@ func TestDelegate_ReorgAttackProtection(t *testing.T) {
 	listener.HandleLog(log.NewLogBroadcast(types.Log{
 		// Data has all the NON-indexed parameters
 		Data: bytes.Join([][]byte{pk.MustHash().Bytes(), // key hash
-			preSeed,                 // preSeed
-			utils.NewHash().Bytes(), // sender
-			utils.NewHash().Bytes(), // fee
+			preSeed,                  // preSeed
+			utils.NewHash().Bytes(),  // sender
+			utils.NewHash().Bytes(),  // fee
 			reqID.Bytes()}, []byte{}, // requestID
 		),
 		// JobID is indexed, thats why it lives in the Topics.
@@ -302,7 +302,7 @@ func TestDelegate_ValidLog(t *testing.T) {
 					common.BigToHash(big.NewInt(42)).Bytes(), // seed
 					utils.NewHash().Bytes(),                  // sender
 					utils.NewHash().Bytes(),                  // fee
-					reqID1.Bytes()}, // requestID
+					reqID1.Bytes()},                          // requestID
 					[]byte{}),
 				// JobID is indexed, thats why it lives in the Topics.
 				Topics: []common.Hash{
@@ -324,7 +324,7 @@ func TestDelegate_ValidLog(t *testing.T) {
 					common.BigToHash(big.NewInt(42)).Bytes(), // seed
 					utils.NewHash().Bytes(),                  // sender
 					utils.NewHash().Bytes(),                  // fee
-					reqID2.Bytes()}, // requestID
+					reqID2.Bytes()},                          // requestID
 					[]byte{}),
 				Topics: []common.Hash{
 					VRFRandomnessRequestLogTopic(),
@@ -427,7 +427,7 @@ func TestDelegate_InvalidLog(t *testing.T) {
 	listener.HandleLog(log.NewLogBroadcast(types.Log{
 		// Data has all the NON-indexed parameters
 		Data: append(append(append(append(
-			utils.NewHash().Bytes(), // key hash
+			utils.NewHash().Bytes(),                      // key hash
 			common.BigToHash(big.NewInt(42)).Bytes()...), // seed
 			utils.NewHash().Bytes()...), // sender
 			utils.NewHash().Bytes()...), // fee
@@ -495,7 +495,7 @@ func TestFulfilledCheck(t *testing.T) {
 				common.BigToHash(big.NewInt(42)).Bytes(), // seed
 				utils.NewHash().Bytes(),                  // sender
 				utils.NewHash().Bytes(),                  // fee
-				utils.NewHash().Bytes()}, // requestID
+				utils.NewHash().Bytes()},                 // requestID
 				[]byte{}),
 			// JobID is indexed, that's why it lives in the Topics.
 			Topics: []common.Hash{
