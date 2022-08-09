30240649f781a15daff2f4124eec113c168b3d80
FelixFan1992 <fankejin@gmail.com>
2022-08-09 14:39:21 -0400

    add comments

--- contracts/src/v0.8/dev/UpkeepTranscoderDev.sol
+++ contracts/src/v0.8/dev/UpkeepTranscoderDev.sol
@@ -14,6 +14,7 @@ contract UpkeepTranscoderDev is UpkeepTranscoderInterface, TypeAndVersionInterfa
 
   /**
    * @notice versions:
+   * - UpkeepTranscoder 1.3.0: placeholder to be replaced with logic to convert Upkeep V1 to V2
    * - UpkeepTranscoder 1.0.0: placeholder to allow new formats in the future
    */
   string public constant override typeAndVersion = "UpkeepTranscoder 1.3.0";
