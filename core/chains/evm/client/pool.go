deb53fac1ddc3e38300013f8ad9d9bc5fd5bf2b9
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-08-22 16:49:45 -0700

    Remove Call() from Client interface
    The only 2 functions using this were GetLinkBalance() and GetERC20Balance().
    Now they will use CallContext() like everything else in our evmclient interface.
    
    Previously there was no timeout or other means of cancelling a LINK or ERC20 balance
    request, even if a slow rpc server were to hang for several minutes.

--- core/chains/evm/client/pool.go
+++ core/chains/evm/client/pool.go
@@ -305,10 +305,11 @@ func (p *Pool) SendTransaction(ctx context.Context, tx *types.Transaction) error
 			p.wg.Add(1)
 			go func(n SendOnlyNode, txCp types.Transaction) {
 				defer p.wg.Done()
-				timeoutCtx, cancel := DefaultQueryCtx()
+
+				chCtx, cancel := utils.ContextFromChan(p.chStop)
+				sendCtx, _ := WithDefaultTimeout(chCtx)
 				defer cancel()
-				sendCtx, cancel2 := utils.WithCloseChan(timeoutCtx, p.chStop)
-				defer cancel2()
+
 				err := NewSendError(n.SendTransaction(sendCtx, &txCp))
 				p.logger.Debugw("Sendonly node sent transaction", "name", n.String(), "tx", tx, "err", err)
 				if err == nil || err.IsNonceTooLowError() || err.IsTransactionAlreadyMined() || err.IsTransactionAlreadyInMempool() {
