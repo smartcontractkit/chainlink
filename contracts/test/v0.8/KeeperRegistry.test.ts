41cd188dfd907a849ca4035ddf19942efbdac369
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-08-23 10:30:47 +0100

    small format fix

--- contracts/test/v0.8/KeeperRegistry.test.ts
+++ contracts/test/v0.8/KeeperRegistry.test.ts
@@ -966,11 +966,9 @@ describe('KeeperRegistry', () => {
       it('uses the fallback gas price if the feed price is non-sensical [ @skip-coverage ]', async () => {
         const normalAmount = await getPerformPaymentAmount()
         const roundId = 99
-
         const currentBlockNum = await ethers.provider.getBlockNumber()
         const currentBlock = await ethers.provider.getBlock(currentBlockNum)
         const updatedAt = currentBlock.timestamp
-
         const startedAt = 946684799
         await gasPriceFeed
           .connect(owner)
@@ -1000,11 +998,9 @@ describe('KeeperRegistry', () => {
       it('uses the fallback link price if the feed price is non-sensical [ @skip-coverage ]', async () => {
         const normalAmount = await getPerformPaymentAmount()
         const roundId = 99
-
         const currentBlockNum = await ethers.provider.getBlockNumber()
         const currentBlock = await ethers.provider.getBlock(currentBlockNum)
         const updatedAt = currentBlock.timestamp
-
         const startedAt = 946684799
         await linkEthFeed
           .connect(owner)
