4c8e51b622672b8ad62c5b32b4dcf89b87c06097
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-08-23 09:47:41 +0100

    format

--- contracts/test/v0.8/KeeperRegistry.test.ts
+++ contracts/test/v0.8/KeeperRegistry.test.ts
@@ -966,7 +966,7 @@ describe('KeeperRegistry', () => {
       it('uses the fallback gas price if the feed price is non-sensical [ @skip-coverage ]', async () => {
         const normalAmount = await getPerformPaymentAmount()
         const roundId = 99
-        
+
         const currentBlockNum = await ethers.provider.getBlockNumber()
         const currentBlock = await ethers.provider.getBlock(currentBlockNum)
         const currentTimestamp = currentBlock.timestamp
@@ -1001,7 +1001,7 @@ describe('KeeperRegistry', () => {
       it('uses the fallback link price if the feed price is non-sensical [ @skip-coverage ]', async () => {
         const normalAmount = await getPerformPaymentAmount()
         const roundId = 99
-        
+
         const currentBlockNum = await ethers.provider.getBlockNumber()
         const currentBlock = await ethers.provider.getBlock(currentBlockNum)
         const currentTimestamp = currentBlock.timestamp
