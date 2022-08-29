9438121c49e9ce286d5c50ecb5903ecd4f73088e
Jordan Krage <jmank88@gmail.com>
2022-08-29 09:32:14 -0500

    core/chains/evm/logpoller: fix data race (#7316)

--- core/chains/evm/logpoller/log_poller.go
+++ core/chains/evm/logpoller/log_poller.go
@@ -60,7 +60,7 @@ type logPoller struct {
 	finalityDepth     int64         // finality depth is taken to mean that block (head - finality) is finalized
 	backfillBatchSize int64         // batch size to use when backfilling finalized logs
 
-	filterMu  sync.Mutex
+	filterMu  sync.RWMutex
 	addresses map[common.Address]struct{}
 	eventSigs map[common.Hash]struct{}
 
@@ -506,6 +506,8 @@ func (lp *logPoller) findBlockAfterLCA(ctx context.Context, current *types.Heade
 }
 
 func (lp *logPoller) assertInFilter(eventSigs []common.Hash, addresses []common.Address) error {
+	lp.filterMu.RLock()
+	defer lp.filterMu.RUnlock()
 	for _, eventSig := range eventSigs {
 		if _, ok := lp.eventSigs[eventSig]; !ok {
 			return errors.Errorf("eventSig %x not registered", eventSig)
