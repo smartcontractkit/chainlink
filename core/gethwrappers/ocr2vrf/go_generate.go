5e97895d45a8eed0e5f467dfbb2d327bf1500e55
Makram <makramkd@users.noreply.github.com>
2022-09-16 00:42:39 +0300

    Chore/bump ocr2vrf to latest (#7441)
    * chore: bump ocr2vrf version
    
    * chore: exclude labstack/echo
    
    This is not required to build chainlink but is still in the dependency
    tree as a dependency of sentry-go.
    
    * chore: exclude influxdata/influxdb
    
    * chore: bump to latest ocr2vrf
    
    * chore: generate gethwrappers
    
    * chore: bump integration-tests ocr2vrf version
    
    Co-authored-by: jinhoonbang <jin.bang@smartcontract.com>

--- core/gethwrappers/ocr2vrf/go_generate.go
+++ core/gethwrappers/ocr2vrf/go_generate.go
@@ -3,6 +3,6 @@
 package gethwrappers
 
 // OCR2VRF
-//go:generate go run ../generation/generate/wrap.go ../../../../contracts/solc/v0.8.15/DKG.abi ../../../../contracts/solc/v0.8.15/DKG.bin DKG dkg
-//go:generate go run ../generation/generate/wrap.go ../../../../contracts/solc/v0.8.15/VRFBeaconCoordinator.abi ../../../../contracts/solc/v0.8.15/VRFBeaconCoordinator.bin VRFBeaconCoordinator vrf_beacon_coordinator
-//go:generate go run ../generation/generate/wrap.go ../../../../contracts/solc/v0.8.15/BeaconVRFConsumer.abi ../../../../contracts/solc/v0.8.15/BeaconVRFConsumer.bin BeaconVRFConsumer vrf_beacon_consumer
+//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.15/DKG.abi ../../../contracts/solc/v0.8.15/DKG.bin DKG dkg
+//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.15/VRFBeaconCoordinator.abi ../../../contracts/solc/v0.8.15/VRFBeaconCoordinator.bin VRFBeaconCoordinator vrf_beacon_coordinator
+//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.15/BeaconVRFConsumer.abi ../../../contracts/solc/v0.8.15/BeaconVRFConsumer.bin BeaconVRFConsumer vrf_beacon_consumer
