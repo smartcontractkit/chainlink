c0e03ce9e4643699cf636dda085652433cf2becf
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2022-09-19 15:17:49 +0100

    prettier

--- contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol
+++ contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol
@@ -3,4 +3,4 @@
  * @notice This is a deprecated interface. Please use AutomationCompatibleInterface directly.
  */
 pragma solidity ^0.8.0;
-import {AutomationCompatibleInterface as KeeperCompatibleInterface} from  "./AutomationCompatibleInterface.sol";
+import {AutomationCompatibleInterface as KeeperCompatibleInterface} from "./AutomationCompatibleInterface.sol";
