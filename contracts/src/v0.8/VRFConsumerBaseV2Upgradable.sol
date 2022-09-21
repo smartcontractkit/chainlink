391a02177fe207e1425beda2d59ddd2d289b6ec9
Makram Kamaleddine <makramkd@users.noreply.github.com>
2022-09-21 16:55:38 +0300

    chore: formatting

--- contracts/src/v0.8/VRFConsumerBaseV2Upgradable.sol
+++ contracts/src/v0.8/VRFConsumerBaseV2Upgradable.sol
@@ -111,7 +111,7 @@ abstract contract VRFConsumerBaseV2Upgradeable is Initializable {
    * @param _vrfCoordinator the VRFCoordinatorV2 address.
    * @dev See https://docs.chain.link/docs/vrf/v2/supported-networks/ for coordinator
    * @dev addresses on your preferred network.
-  */
+   */
   function __VRFConsumerBaseV2_init(address _vrfCoordinator) public initializer {
     vrfCoordinator = _vrfCoordinator;
   }
--- contracts/src/v0.8/tests/VRFV2TransparentUpgradeableProxy.sol
+++ contracts/src/v0.8/tests/VRFV2TransparentUpgradeableProxy.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.so
 contract VRFV2TransparentUpgradeableProxy is TransparentUpgradeableProxy {
   // Nothing special here, this is just to generate the gethwrapper for tests.
   constructor(
-        address _logic,
-        address admin_,
-        bytes memory _data
-    ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {}
+    address _logic,
+    address admin_,
+    bytes memory _data
+  ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {}
 }
