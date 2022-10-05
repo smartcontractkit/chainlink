717d41e908009b0bad7570572b07fd4f7e950d2e
Makram Kamaleddine <makramkd@users.noreply.github.com>
2022-10-05 21:47:13 +0300

    feature: add consumer-read-randomness command
    To read the randomness given to a callback.
    
    Also, add optimism goerli settings to the helpers so we print out
    the explorer address for it.

--- core/scripts/ocr2vrf/main.go
+++ core/scripts/ocr2vrf/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/shopspring/decimal"
 
 	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
 )
@@ -289,6 +290,13 @@ func main() {
 			uint32(*callbackGasLimit),
 			nil, // test consumer doesn't use any args
 		)
+	case "consumer-read-randomness":
+		cmd := flag.NewFlagSet("consumer-read-randomness", flag.ExitOnError)
+		consumerAddress := cmd.String("consumer-address", "", "VRF coordinator consumer address")
+		requestID := cmd.String("request-id", "", "VRF request ID")
+		numWords := cmd.Int("num-words", 1, "number of words to fetch")
+		helpers.ParseArgs(cmd, os.Args[2:], "consumer-address")
+		readRandomness(e, *consumerAddress, decimal.RequireFromString(*requestID).BigInt(), *numWords)
 
 	case "dkg-setup":
 		setupDKGNodes(e)
