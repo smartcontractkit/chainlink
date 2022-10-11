4153cea6f6af5e9220c78f979d7c8f4c20d3310d
Chris <104409744+vreff@users.noreply.github.com>
2022-10-11 15:42:12 -0400

    fix merge conflicts

--- core/services/relay/evm/evm.go
+++ core/services/relay/evm/evm.go
@@ -22,7 +22,9 @@ import (
 	"github.com/smartcontractkit/chainlink/core/chains/evm"
 	txm "github.com/smartcontractkit/chainlink/core/chains/evm/txmgr"
 	"github.com/smartcontractkit/chainlink/core/logger"
+	"github.com/smartcontractkit/chainlink/core/services/job"
 	"github.com/smartcontractkit/chainlink/core/services/ocrcommon"
+	"github.com/smartcontractkit/chainlink/core/services/pipeline"
 	"github.com/smartcontractkit/chainlink/core/utils"
 )
 
@@ -182,6 +184,45 @@ func newContractTransmitter(lggr logger.Logger, rargs relaytypes.RelayArgs, tran
 		configWatcher.chain.Client(),
 		configWatcher.contractABI,
 		ocrcommon.NewTransmitter(configWatcher.chain.TxManager(), fromAddresses, gasLimit, effectiveTransmitterAddress, strategy, txm.TransmitCheckerSpec{}),
+		configWatcher.chain.LogPoller(),
+		lggr,
+	)
+}
+
+func newPipelineContractTransmitter(lggr logger.Logger, rargs relaytypes.RelayArgs, transmitterID string, configWatcher *configWatcher, spec job.Job, pr pipeline.Runner) (*ContractTransmitter, error) {
+	var relayConfig RelayConfig
+	if err := json.Unmarshal(rargs.RelayConfig, &relayConfig); err != nil {
+		return nil, err
+	}
+
+	effectiveTransmitterAddress := common.HexToAddress(relayConfig.EffectiveTransmitterAddress.String)
+	transmitterAddress := common.HexToAddress(transmitterID)
+	strategy := txm.NewQueueingTxStrategy(rargs.ExternalJobID, configWatcher.chain.Config().OCRDefaultTransactionQueueDepth())
+
+	var checker txm.TransmitCheckerSpec
+	if configWatcher.chain.Config().OCRSimulateTransactions() {
+		checker.CheckerType = txm.TransmitCheckerTypeSimulate
+	}
+
+	gasLimit := configWatcher.chain.Config().EvmGasLimitDefault()
+	if configWatcher.chain.Config().EvmGasLimitOCRJobType() != nil {
+		gasLimit = *configWatcher.chain.Config().EvmGasLimitOCRJobType()
+	}
+	return NewOCRContractTransmitter(
+		configWatcher.contractAddress,
+		configWatcher.chain.Client(),
+		configWatcher.contractABI,
+		ocrcommon.NewPipelineTransmitter(
+			lggr,
+			transmitterAddress,
+			gasLimit,
+			effectiveTransmitterAddress,
+			strategy,
+			txm.TransmitCheckerSpec{},
+			pr,
+			spec,
+			configWatcher.chain.ID().String(),
+		),
 		configWatcher.chain.LogPoller(),
 		lggr,
 	)
