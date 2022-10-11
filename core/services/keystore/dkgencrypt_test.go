5e678dd46ad8119bda3a481b4c6d02cdc86958cf
Jordan Krage <jmank88@gmail.com>
2022-10-11 18:08:50 -0500

    golangci-lint: fix some linter issues (#7636)

--- core/services/keystore/dkgencrypt_test.go
+++ core/services/keystore/dkgencrypt_test.go
@@ -26,7 +26,7 @@ func Test_DKGEncryptKeyStore_E2E(t *testing.T) {
 		_, err := db.Exec("DELETE FROM encrypted_key_rings")
 		require.NoError(t, err)
 		keyStore.ResetXXXTestOnly()
-		keyStore.Unlock(cltest.Password)
+		require.NoError(t, keyStore.Unlock(cltest.Password))
 	}
 
 	t.Run("initializes with an empty state", func(t *testing.T) {
