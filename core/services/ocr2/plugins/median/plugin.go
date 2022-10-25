8011aa7edc2f6262d5fbc51acd1c91db40e78962
connorwstein <connor.stein@mail.mcgill.ca>
2022-10-25 18:05:42 -0400

    Simplify

--- core/services/ocr2/plugins/median/plugin.go
+++ core/services/ocr2/plugins/median/plugin.go
@@ -9,7 +9,6 @@ import (
 	libocr2 "github.com/smartcontractkit/libocr/offchainreporting2"
 	"github.com/smartcontractkit/libocr/offchainreporting2/reportingplugin/median"
 
-	"github.com/smartcontractkit/chainlink/core/chains/evm"
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/services/job"
 	"github.com/smartcontractkit/chainlink/core/services/ocr2/plugins/median/config"
@@ -19,13 +18,11 @@ import (
 
 // NewMedian parses the arguments and returns a new Median struct.
 func NewMedianServices(jb job.Job,
-	chainSet evm.ChainSet,
 	ocr2Provider types.MedianProvider,
 	pipelineRunner pipeline.Runner,
 	runResults chan pipeline.Run,
 	lggr logger.Logger,
 	ocrLogger commontypes.Logger,
-	new bool,
 	argsNoPlugin libocr2.OracleArgs,
 ) ([]job.ServiceCtx, error) {
 	var pluginConfig config.PluginConfig
@@ -37,11 +34,6 @@ func NewMedianServices(jb job.Job,
 	if err != nil {
 		return nil, err
 	}
-	//chainID := big.NewInt(0).SetUint64(pluginConfig.ChainID)
-	//chain, err := chainSet.Get(chainID)
-	//if err != nil {
-	//	return nil, err
-	//}
 	juelsPerFeeCoinPipelineSpec := pipeline.Spec{
 		ID:           jb.ID,
 		DotDagSource: pluginConfig.JuelsPerFeeCoinPipeline,
@@ -64,15 +56,10 @@ func NewMedianServices(jb job.Job,
 	if err != nil {
 		return nil, err
 	}
-	if new {
-		// If this is a brand-new job, then we make use of the start blocks. If not then we're rebooting and log poller will pick up where we left off.
-		// Median doesn't need specific fromBlocks before starting the plugin.
-		return []job.ServiceCtx{ocr2Provider, ocrcommon.NewResultRunSaver(
-			runResults,
-			pipelineRunner,
-			make(chan struct{}),
-			lggr),
-			ocrcommon.NewBackfilledOracle(lggr, []ocrcommon.BackfilledPoller{}, job.NewServiceAdapter(oracle))}, nil
-	}
-	return []job.ServiceCtx{}, nil
+	return []job.ServiceCtx{ocr2Provider, ocrcommon.NewResultRunSaver(
+		runResults,
+		pipelineRunner,
+		make(chan struct{}),
+		lggr),
+		job.NewServiceAdapter(oracle)}, nil
 }
