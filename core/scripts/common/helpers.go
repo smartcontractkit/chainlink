f572c79fe7f68e2a9a63e6f7196360ea093e210a
jinhoonbang <jin.bang@smartcontract.com>
2022-10-27 07:31:41 -0700

    address comment

--- core/scripts/common/helpers.go
+++ core/scripts/common/helpers.go
@@ -282,7 +282,7 @@ func FundNodes(e Environment, transmitters []string, fundingAmount *big.Int) {
 	for i := 0; i < len(transmitters); i++ {
 		// Special case for Arbitrum since gas estimation there is different.
 		var gasLimit uint64
-		if e.ChainID == ArbitrumGoerliChainID {
+		if IsArbitrumChainID(e.ChainID) {
 			to := common.HexToAddress(transmitters[i])
 			estimated, err := e.Ec.EstimateGas(context.Background(), ethereum.CallMsg{
 				From:  e.Owner.From,
