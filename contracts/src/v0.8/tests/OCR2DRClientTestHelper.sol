36f1d8e73a2d13da47dd045845c8a0d51f1b2ac2
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-10-27 09:54:25 +0300

    Handling user callback errors

--- contracts/src/v0.8/tests/OCR2DRClientTestHelper.sol
+++ contracts/src/v0.8/tests/OCR2DRClientTestHelper.sol
@@ -10,6 +10,9 @@ contract OCR2DRClientTestHelper is OCR2DRClient {
 
   event FulfillRequestInvoked(bytes32 requestId, bytes response, bytes err);
 
+  bool private s_revertFulfillRequest;
+  bool private s_doInvalidOperation;
+
   constructor(address oracle) OCR2DRClient(oracle) {}
 
   function sendSimpleRequestWithJavaScript(string memory sourceCode, uint256 subscriptionId) public returns (bytes32) {
@@ -23,6 +26,22 @@ contract OCR2DRClientTestHelper is OCR2DRClient {
     bytes memory response,
     bytes memory err
   ) internal override {
+    if (s_revertFulfillRequest) {
+      revert("asked to revert");
+    }
+    if (s_doInvalidOperation) {
+      uint256 x = 1;
+      uint256 y = 0;
+      x = x / y;
+    }
     emit FulfillRequestInvoked(requestId, response, err);
+  }
+
+  function setRevertFulfillRequest(bool on) external {
+    s_revertFulfillRequest = on;
+  }
+
+  function setDoInvalidOperation(bool on) external {
+    s_doInvalidOperation = on;
   }
 }
