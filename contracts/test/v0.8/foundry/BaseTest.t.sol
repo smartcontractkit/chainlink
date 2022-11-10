ef689cc156a8c1a0fdbcf346bfa531f84ec59eef
Rens Rooimans <github@rensrooimans.nl>
2022-11-10 17:52:35 +0100

    chore: improve wording on setting prank

--- contracts/test/v0.8/foundry/BaseTest.t.sol
+++ contracts/test/v0.8/foundry/BaseTest.t.sol
@@ -6,7 +6,7 @@ contract BaseTest is Test {
     address internal constant OWNER = 0x00007e64E1fB0C487F25dd6D3601ff6aF8d32e4e;
 
     function setUp() public virtual {
-        // Set the sender to OWNER permanently
+        // Set msg.sender to OWNER until changePrank or stopPrank is called
         changePrank(OWNER);
     }
 }
