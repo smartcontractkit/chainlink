9ac73cd31eb6da5c4151ef57e0a884ef5e6311cf
Rens Rooimans <github@rensrooimans.nl>
2022-11-10 18:14:55 +0100

    fix: set OCR2DR solidity version to 0.8.13

--- contracts/src/v0.8/dev/ocr2dr/OCR2DROracle.sol
+++ contracts/src/v0.8/dev/ocr2dr/OCR2DROracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity 0.8.13;
 
 import "../../interfaces/TypeAndVersionInterface.sol";
 import "../interfaces/OCR2DRClientInterface.sol";
