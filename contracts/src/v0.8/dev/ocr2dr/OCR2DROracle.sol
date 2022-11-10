941f4a0c0d8e0f03ad7a875a38d37ee5cf6b6757
Rens Rooimans <github@rensrooimans.nl>
2022-11-10 18:30:31 +0100

    chore: bump version HH supported sol version

--- contracts/src/v0.8/dev/ocr2dr/OCR2DROracle.sol
+++ contracts/src/v0.8/dev/ocr2dr/OCR2DROracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 
 import "../../interfaces/TypeAndVersionInterface.sol";
 import "../interfaces/OCR2DRClientInterface.sol";
