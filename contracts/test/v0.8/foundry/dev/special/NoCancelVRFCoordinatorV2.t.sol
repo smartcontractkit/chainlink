3fb1d7d4ba93e705819fb7035c62a8a0404a16fc
Makram Kamaleddine <makramkd@users.noreply.github.com>
2022-11-15 14:51:27 -0500

    fix: expectEmit comments

--- contracts/test/v0.8/foundry/dev/special/NoCancelVRFCoordinatorV2.t.sol
+++ contracts/test/v0.8/foundry/dev/special/NoCancelVRFCoordinatorV2.t.sol
@@ -45,7 +45,7 @@ contract NoCancelVRFCoordinatorV2_cancelSubscription is NoCancelVRFCoordinatorV2
     // contract owner are different.
     vm.stopPrank();
     vm.prank(subscriptionOwner);
-    vm.expectEmit(true /* checkTopic1 */, false /* checkTopic2 */, false /* checkTopic3 */, true /* checkData */);
+    vm.expectEmit(true /* subId */, false /* checkTopic2 */, false /* checkTopic3 */, true /* owner */);
     emit SubscriptionCreated(1, subscriptionOwner);
     s_coordinator.createSubscription();
 
@@ -58,7 +58,7 @@ contract NoCancelVRFCoordinatorV2_cancelSubscription is NoCancelVRFCoordinatorV2
 
     // transfer and call some link to the newly created subscription
     uint256 ownerLinkBalanceBefore = LINK.balanceOf(OWNER);
-    vm.expectEmit(true /* checkTopic1 */, false /* checkTopic2 */, false /* checkTopic3 */, true /* checkData */);
+    vm.expectEmit(true /* subId */, false /* checkTopic2 */, false /* checkTopic3 */, true /* oldBalance,newBalance */);
     emit SubscriptionFunded(1, 0, 1e18);
     vm.prank(OWNER);
     bool success = LINK.transferAndCall(address(s_coordinator), 1e18 /* 1 LINK */, abi.encode(uint64(1) /* sub id */));
@@ -68,7 +68,7 @@ contract NoCancelVRFCoordinatorV2_cancelSubscription is NoCancelVRFCoordinatorV2
     assertEq(1e18 /* expected balance */, balance /* actual balance */); // balance is 1 LINK
 
     // cancel subscription and assert OWNER link balance updated
-    vm.expectEmit(true /* checkTopic1 */, false /* checkTopic2 */, false /* checkTopic3 */, true /* checkData */);
+    vm.expectEmit(true /* subId */, false /* checkTopic2 */, false /* checkTopic3 */, true /* to,amount */);
     emit SubscriptionCanceled(1, OWNER, 1e18);
     vm.prank(OWNER);
     s_coordinator.ownerCancelSubscription(1);
