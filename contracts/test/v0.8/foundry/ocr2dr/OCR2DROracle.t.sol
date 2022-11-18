3782125019c392270b6c9470d634e4914a96e130
Justin Kaseman <justinkaseman@live.com>
2022-11-18 14:53:50 -0800

    Regen geth wrappers & fix foundry test

--- contracts/test/v0.8/foundry/ocr2dr/OCR2DROracle.t.sol
+++ contracts/test/v0.8/foundry/ocr2dr/OCR2DROracle.t.sol
@@ -4,83 +4,83 @@ import {BaseTest} from "../BaseTest.t.sol";
 import {OCR2DROracle} from "../../../../src/v0.8/dev/ocr2dr/OCR2DROracle.sol";
 
 contract OCR2DROracleSetup is BaseTest {
-    bytes constant DATA = abi.encode("bytes");
+  bytes constant DATA = abi.encode("bytes");
 
-    OCR2DROracle s_oracle;
+  OCR2DROracle s_oracle;
 
-    function setUp() public virtual override {
-        BaseTest.setUp();
+  function setUp() public virtual override {
+    BaseTest.setUp();
 
-        s_oracle = new OCR2DROracle();
-    }
+    s_oracle = new OCR2DROracle();
+  }
 }
 
 contract OCR2DROracle_typeAndVersion is OCR2DROracleSetup {
-    function testTypeAndVersionSuccess() public {
-       assertEq(s_oracle.typeAndVersion(), "OCR2DROracle 0.0.0");
-    }
+  function testTypeAndVersionSuccess() public {
+    assertEq(s_oracle.typeAndVersion(), "OCR2DROracle 0.0.0");
+  }
 }
 
 contract OCR2DROracle_setDONPublicKey is OCR2DROracleSetup {
-    function testSetDONPublicKey_gas() public {
-        s_oracle.setDONPublicKey(DATA);
-    }
+  function testSetDONPublicKey_gas() public {
+    s_oracle.setDONPublicKey(DATA);
+  }
 
-    function testSetDONPublicKeySuccess() public {
-        bytes memory donPublicKey = abi.encode("newKey");
+  function testSetDONPublicKeySuccess() public {
+    bytes memory donPublicKey = abi.encode("newKey");
 
-        // Verify the existing key is different from the new key
-        bytes memory existingDonPublicKey = s_oracle.getDONPublicKey();
-        bytes memory expectedExistingDonPublicKey;
-        assertEq(existingDonPublicKey, expectedExistingDonPublicKey);
-        // If they have different lengths they are not the same
-        assertFalse(donPublicKey.length == expectedExistingDonPublicKey.length);
+    // Verify the existing key is different from the new key
+    bytes memory existingDonPublicKey = s_oracle.getDONPublicKey();
+    bytes memory expectedExistingDonPublicKey;
+    assertEq(existingDonPublicKey, expectedExistingDonPublicKey);
+    // If they have different lengths they are not the same
+    assertFalse(donPublicKey.length == expectedExistingDonPublicKey.length);
 
-        s_oracle.setDONPublicKey(donPublicKey);
-        bytes memory newDonPublicKey = s_oracle.getDONPublicKey();
-        assertEq(newDonPublicKey, donPublicKey);
-    }
+    s_oracle.setDONPublicKey(donPublicKey);
+    bytes memory newDonPublicKey = s_oracle.getDONPublicKey();
+    assertEq(newDonPublicKey, donPublicKey);
+  }
 
-    // Reverts
+  // Reverts
 
-    function testEmptyPublicKeyReverts() public {
-        bytes memory donPublicKey;
+  function testEmptyPublicKeyReverts() public {
+    bytes memory donPublicKey;
 
-        vm.expectRevert(OCR2DROracle.EmptyPublicKey.selector);
-        s_oracle.setDONPublicKey(donPublicKey);
-    }
+    vm.expectRevert(OCR2DROracle.EmptyPublicKey.selector);
+    s_oracle.setDONPublicKey(donPublicKey);
+  }
 
-    function testOnlyOwnerReverts() public {
-        vm.stopPrank();
-        vm.expectRevert("Only callable by owner");
+  function testOnlyOwnerReverts() public {
+    vm.stopPrank();
+    vm.expectRevert("Only callable by owner");
 
-        bytes memory donPublicKey;
-        s_oracle.setDONPublicKey(donPublicKey);
-    }
+    bytes memory donPublicKey;
+    s_oracle.setDONPublicKey(donPublicKey);
+  }
 }
 
 contract OCR2DROracle_sendRequest is OCR2DROracleSetup {
-    event OracleRequest(bytes32 requestId, bytes data);
+  event OracleRequest(bytes32 requestId, bytes data);
 
-    function testSendRequest_gas() public {
-        s_oracle.sendRequest(0, DATA);
-    }
+  function testSendRequest_gas() public {
+    s_oracle.sendRequest(0, DATA, 0);
+  }
 
-    function testSendRequestFuzzSuccess(uint256 subscriptionId, bytes calldata data) public {
-        vm.assume(data.length != 0);
+  function testSendRequestFuzzSuccess(uint64 subscriptionId, bytes calldata data) public {
+    vm.assume(data.length != 0);
 
-        vm.expectEmit(false, false, false, false);
-        emit OracleRequest(0, data);
+    vm.expectEmit(false, false, false, false);
+    emit OracleRequest(0, data);
 
-        s_oracle.sendRequest(subscriptionId, data);
-    }
+    s_oracle.sendRequest(subscriptionId, data, 0);
+  }
 
-    // Reverts
+  // Reverts
 
-    function testEmptyRequestDataReverts() public {
-        bytes memory emptyData;
+  function testEmptyRequestDataReverts() public {
+    bytes memory emptyData;
 
-        vm.expectRevert(OCR2DROracle.EmptyRequestData.selector);
-        s_oracle.sendRequest(0, emptyData);
-    }
+    vm.expectRevert(OCR2DROracle.EmptyRequestData.selector);
+    s_oracle.sendRequest(0, emptyData, 0);
+  }
 }
