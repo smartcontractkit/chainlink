5d0da0e1b58939dfa1bf24a9cca6ef0ccebdad67
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-11-21 15:00:19 +0300

    Added missing migration

---
+++ core/store/migrate/migrations/0152_ocr2dr_requests_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+
+CREATE TABLE ocr2dr_requests(
+    id BIGSERIAL PRIMARY KEY,
+    request_id bytea CHECK (octet_length(request_id) = 32) NOT NULL UNIQUE,
+    oracle_address bytea CHECK (octet_length(oracle_address) = 20) NOT NULL,
+    run_id bigint, --  NOT NULL REFERENCES public.pipeline_runs(id) ON DELETE CASCADE DEFERRABLE
+    received_at timestamp with time zone NOT NULL,
+    request_tx_hash bytea CHECK (octet_length(oracle_address) = 32) NOT NULL,
+    state INTEGER,
+    result_ready_at timestamp with time zone,
+    result bytea,
+    error_type INTEGER,
+    error bytea,
+    transmitted_result bytea,
+    transmitted_error bytea
+);
+
+-- +goose Down
+
+DROP TABLE ocr2dr_requests;
