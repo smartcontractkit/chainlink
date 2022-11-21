8526c5251bea3f2547f85467c87f0584c921a59c
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2022-11-21 16:55:53 +0300

    Set primary key

--- core/store/migrate/migrations/0152_ocr2dr_requests_table.sql
+++ core/store/migrate/migrations/0152_ocr2dr_requests_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 
 CREATE TABLE ocr2dr_requests(
-    request_id bytea CHECK (octet_length(request_id) = 32) NOT NULL UNIQUE,
+    request_id bytea CHECK (octet_length(request_id) = 32) PRIMARY KEY,
     contract_address bytea CHECK (octet_length(contract_address) = 20) NOT NULL,
     run_id bigint, --  NOT NULL REFERENCES public.pipeline_runs(id) ON DELETE CASCADE DEFERRABLE
     received_at timestamp with time zone NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE ocr2dr_requests(
     transmitted_error bytea
 );
 
-CREATE INDEX idx_ocr2dr_requests ON ocr2dr_requests (request_id, contract_address);
+CREATE INDEX idx_ocr2dr_requests ON ocr2dr_requests (contract_address);
 
 -- +goose Down
 
