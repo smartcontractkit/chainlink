853ed15fda31efdbf2916af629d9640b73884410
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-11-22 18:20:19 -0800

    Update core/services/job/orm.go
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/services/job/orm.go
+++ core/services/job/orm.go
@@ -261,12 +261,8 @@ func (o *orm) CreateJob(jb *Job, qopts ...pg.QOpt) error {
 						if err != nil {
 							return errors.Wrapf(err, "db read error while validating contract_id")
 						}
-						chainID, err := EVMChainIDForJobSpec(&spec)
-						if err != nil {
-							return errors.Wrapf(err, "internal error while validating contract_id")
-						}
 						return errors.Errorf("Job ID %v already exists for chain ID %v with contract address %v",
-							jb.ID, chainID, jb.OCROracleSpec.ContractAddress)
+							jb.ID, newChainID, jb.OCROracleSpec.ContractAddress)
 					}
 				case relay.Solana:
 					_, err := o.keyStore.Solana().Get(jb.OCR2OracleSpec.TransmitterID.String)
