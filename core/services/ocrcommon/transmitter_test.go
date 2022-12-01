e0685595905f0c2e2cd9264e39f84de260a03954
Chris C <104409744+vreff@users.noreply.github.com>
2022-12-01 13:17:54 -0500

    Fix the transaction forwarder integration to use the Eth Keystore when selecting next the forwarder via round-robin. (#8009)

--- core/services/ocrcommon/transmitter_test.go
+++ core/services/ocrcommon/transmitter_test.go
@@ -1,6 +1,7 @@
 package ocrcommon_test
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -26,13 +27,24 @@ func Test_DefaultTransmitter_CreateEthTransaction(t *testing.T) {
 	_, fromAddress := cltest.MustInsertRandomKey(t, ethKeyStore, 0)
 
 	gasLimit := uint32(1000)
+	chainID := big.NewInt(0)
 	effectiveTransmitterAddress := fromAddress
 	toAddress := testutils.NewAddress()
 	payload := []byte{1, 2, 3}
 	txm := txmmocks.NewTxManager(t)
 	strategy := txmmocks.NewTxStrategy(t)
 
-	transmitter := ocrcommon.NewTransmitter(txm, []common.Address{fromAddress}, gasLimit, effectiveTransmitterAddress, strategy, txmgr.TransmitCheckerSpec{})
+	transmitter, err := ocrcommon.NewTransmitter(
+		txm,
+		[]common.Address{fromAddress},
+		gasLimit,
+		effectiveTransmitterAddress,
+		strategy,
+		txmgr.TransmitCheckerSpec{},
+		chainID,
+		ethKeyStore,
+	)
+	require.NoError(t, err)
 
 	txm.On("CreateEthTransaction", txmgr.NewTx{
 		FromAddress:    fromAddress,
@@ -43,4 +55,114 @@ func Test_DefaultTransmitter_CreateEthTransaction(t *testing.T) {
 		Strategy:       strategy,
 	}, mock.Anything).Return(txmgr.EthTx{}, nil).Once()
 	require.NoError(t, transmitter.CreateEthTransaction(testutils.Context(t), toAddress, payload))
+}
+
+func Test_DefaultTransmitter_Forwarding_Enabled_CreateEthTransaction(t *testing.T) {
+	t.Parallel()
+
+	db := pgtest.NewSqlxDB(t)
+	cfg := configtest.NewTestGeneralConfig(t)
+	ethKeyStore := cltest.NewKeyStore(t, db, cfg).Eth()
+
+	_, fromAddress := cltest.MustInsertRandomKey(t, ethKeyStore, 0)
+	_, fromAddress2 := cltest.MustInsertRandomKey(t, ethKeyStore, 0)
+
+	gasLimit := uint32(1000)
+	chainID := big.NewInt(0)
+	effectiveTransmitterAddress := common.Address{}
+	toAddress := testutils.NewAddress()
+	payload := []byte{1, 2, 3}
+	txm := txmmocks.NewTxManager(t)
+	strategy := txmmocks.NewTxStrategy(t)
+
+	transmitter, err := ocrcommon.NewTransmitter(
+		txm,
+		[]common.Address{fromAddress, fromAddress2},
+		gasLimit,
+		effectiveTransmitterAddress,
+		strategy,
+		txmgr.TransmitCheckerSpec{},
+		chainID,
+		ethKeyStore,
+	)
+	require.NoError(t, err)
+
+	txm.On("CreateEthTransaction", txmgr.NewTx{
+		FromAddress:    fromAddress,
+		ToAddress:      toAddress,
+		EncodedPayload: payload,
+		GasLimit:       gasLimit,
+		Meta:           nil,
+		Strategy:       strategy,
+	}, mock.Anything).Return(txmgr.EthTx{}, nil).Once()
+	txm.On("CreateEthTransaction", txmgr.NewTx{
+		FromAddress:    fromAddress2,
+		ToAddress:      toAddress,
+		EncodedPayload: payload,
+		GasLimit:       gasLimit,
+		Meta:           nil,
+		Strategy:       strategy,
+	}, mock.Anything).Return(txmgr.EthTx{}, nil).Once()
+	require.NoError(t, transmitter.CreateEthTransaction(testutils.Context(t), toAddress, payload))
+	require.NoError(t, transmitter.CreateEthTransaction(testutils.Context(t), toAddress, payload))
+}
+
+func Test_DefaultTransmitter_Forwarding_Enabled_CreateEthTransaction_Round_Robin_Error(t *testing.T) {
+	t.Parallel()
+
+	db := pgtest.NewSqlxDB(t)
+	cfg := configtest.NewTestGeneralConfig(t)
+	ethKeyStore := cltest.NewKeyStore(t, db, cfg).Eth()
+
+	fromAddress := common.Address{}
+
+	gasLimit := uint32(1000)
+	chainID := big.NewInt(0)
+	effectiveTransmitterAddress := common.Address{}
+	toAddress := testutils.NewAddress()
+	payload := []byte{1, 2, 3}
+	txm := txmmocks.NewTxManager(t)
+	strategy := txmmocks.NewTxStrategy(t)
+
+	transmitter, err := ocrcommon.NewTransmitter(
+		txm,
+		[]common.Address{fromAddress},
+		gasLimit,
+		effectiveTransmitterAddress,
+		strategy,
+		txmgr.TransmitCheckerSpec{},
+		chainID,
+		ethKeyStore,
+	)
+	require.NoError(t, err)
+	require.Error(t, transmitter.CreateEthTransaction(testutils.Context(t), toAddress, payload))
+}
+
+func Test_DefaultTransmitter_Forwarding_Enabled_CreateEthTransaction_No_Keystore_Error(t *testing.T) {
+	t.Parallel()
+
+	db := pgtest.NewSqlxDB(t)
+	cfg := configtest.NewTestGeneralConfig(t)
+	ethKeyStore := cltest.NewKeyStore(t, db, cfg).Eth()
+
+	_, fromAddress := cltest.MustInsertRandomKey(t, ethKeyStore, 0)
+	_, fromAddress2 := cltest.MustInsertRandomKey(t, ethKeyStore, 0)
+
+	gasLimit := uint32(1000)
+	chainID := big.NewInt(0)
+	effectiveTransmitterAddress := common.Address{}
+	txm := txmmocks.NewTxManager(t)
+	strategy := txmmocks.NewTxStrategy(t)
+
+	_, err := ocrcommon.NewTransmitter(
+		txm,
+		[]common.Address{fromAddress, fromAddress2},
+		gasLimit,
+		effectiveTransmitterAddress,
+		strategy,
+		txmgr.TransmitCheckerSpec{},
+		chainID,
+		nil,
+	)
+	require.Error(t, err)
 }
