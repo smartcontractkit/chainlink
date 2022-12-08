009857cb9a8a2fed736a616714a2a083e85c1fc2
Michael Street <5597260+MStreet3@users.noreply.github.com>
2022-12-08 14:09:02 -0500

    feeds: add job proposal counts gauge vector

--- core/services/feeds/mocks/orm.go
+++ core/services/feeds/mocks/orm.go
@@ -79,6 +79,29 @@ func (_m *ORM) CountJobProposals() (int64, error) {
 	return r0, r1
 }
 
+// CountJobProposalsByStatus provides a mock function with given fields:
+func (_m *ORM) CountJobProposalsByStatus() (*feeds.JobProposalCounts, error) {
+	ret := _m.Called()
+
+	var r0 *feeds.JobProposalCounts
+	if rf, ok := ret.Get(0).(func() *feeds.JobProposalCounts); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*feeds.JobProposalCounts)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CountManagers provides a mock function with given fields:
 func (_m *ORM) CountManagers() (int64, error) {
 	ret := _m.Called()
