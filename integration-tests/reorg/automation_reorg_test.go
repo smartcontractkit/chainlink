50f35f9770a2c529079899abbcf3d14499a579f3
anirudhwarrier <anirudhwarrier@users.noreply.github.com>
2022-12-14 15:29:38 +0530

    remove ginkgo from reorg test

--- integration-tests/reorg/automation_reorg_test.go
+++ integration-tests/reorg/automation_reorg_test.go
@@ -3,11 +3,14 @@ package reorg
 //revive:disable:dot-imports
 import (
 	"context"
+	"fmt"
+	"github.com/onsi/gomega"
 	"github.com/smartcontractkit/chainlink-env/pkg/cdk8s/blockscout"
 	"github.com/smartcontractkit/chainlink-env/pkg/helm/reorg"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils"
+	"github.com/stretchr/testify/require"
 	"math/big"
-	"strconv"
+	"testing"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -20,9 +23,6 @@ import (
 	"github.com/smartcontractkit/chainlink/integration-tests/actions"
 	"github.com/smartcontractkit/chainlink/integration-tests/client"
 	"github.com/smartcontractkit/chainlink/integration-tests/contracts"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
 var (
@@ -95,149 +95,117 @@ HistoryDepth = 400`
 const (
 	defaultUpkeepGasLimit = uint32(2500000)
 	defaultLinkFunds      = int64(9e18)
+	numberOfUpkeeps       = 2
+	automationReorgBlocks = 10
 )
 
-var _ = Describe("Automation reorg test @reorg-automation", func() {
-	numberOfUpkeeps := 2
-	reorgBlocks := 10
-	var (
-		testScenarios = []TableEntry{
-			Entry("Must survive 50 block reorg for 1m @reorg-automation-50-block",
-				reorg.New(defaultReorgEthereumSettings),
-				chainlink.New(0, defaultAutomationSettings),
-			),
-		}
+func TestAutomationReorg(t *testing.T) {
+	network := networks.SimulatedEVMNonDev1
 
-		testEnvironment *environment.Environment
-		chainlinkNodes  []*client.Chainlink
-		chainClient     blockchain.EVMClient
-		registry        contracts.KeeperRegistry
-		registrar       contracts.KeeperRegistrar
-		consumers       []contracts.KeeperConsumer
-		upkeepIDs       []*big.Int
-	)
+	testEnvironment := environment.
+		New(&environment.Config{
+			NamespacePrefix: fmt.Sprintf("reorg-automation-%d", automationReorgBlocks),
+			TTL:             time.Hour * 1}).
+		AddHelm(reorg.New(defaultReorgEthereumSettings)).
+		AddHelm(chainlink.New(0, defaultAutomationSettings)).
+		AddChart(blockscout.New(&blockscout.Props{
+			Name:    "geth-blockscout",
+			WsURL:   activeEVMNetwork.URL,
+			HttpURL: activeEVMNetwork.HTTPURLs[0]}))
+	err := testEnvironment.Run()
+	require.NoError(t, err, "Error setting up test environment")
 
-	AfterEach(func() {
-		err := actions.TeardownSuite(testEnvironment, utils.ProjectRoot, chainlinkNodes, nil, chainClient)
-		Expect(err).ShouldNot(HaveOccurred(), "Environment teardown shouldn't fail")
+	chainClient, err := blockchain.NewEVMClient(network, testEnvironment)
+	require.NoError(t, err, "Error connecting to blockchain")
+	contractDeployer, err := contracts.NewContractDeployer(chainClient)
+	require.NoError(t, err, "Error building contract deployer")
+	chainlinkNodes, err := client.ConnectChainlinkNodes(testEnvironment)
+	require.NoError(t, err, "Error connecting to Chainlink nodes")
+	chainClient.ParallelTransactions(true)
+
+	// Register cleanup for any test
+	t.Cleanup(func() {
+		err := actions.TeardownSuite(t, testEnvironment, utils.ProjectRoot, chainlinkNodes, nil, chainClient)
+		require.NoError(t, err, "Error tearing down environment")
 	})
 
-	DescribeTable("Automation reorg on different EVM networks", func(
-		networkChart environment.ConnectedChart,
-		clChart environment.ConnectedChart,
-	) {
-		By("Deploying the environment")
-		testEnvironment = environment.
-			New(&environment.Config{
-				NamespacePrefix: "reorg-automation",
-				TTL:             time.Hour * 1}).
-			AddHelm(networkChart).
-			AddChart(blockscout.New(&blockscout.Props{
-				Name:    "geth-blockscout",
-				WsURL:   activeEVMNetwork.URL,
-				HttpURL: activeEVMNetwork.HTTPURLs[0]}))
-		err := testEnvironment.Run()
-		Expect(err).ShouldNot(HaveOccurred())
+	txCost, err := chainClient.EstimateCostForChainlinkOperations(1000)
+	require.NoError(t, err, "Error estimating cost for Chainlink Operations")
+	err = actions.FundChainlinkNodes(chainlinkNodes, chainClient, txCost)
+	require.NoError(t, err, "Error funding Chainlink nodes")
 
-		// wait for bloclchain nodes to be online
-		time.Sleep(90 * time.Second)
-		err = testEnvironment.AddHelm(clChart).Run()
+	linkToken, err := contractDeployer.DeployLinkTokenContract()
+	require.NoError(t, err, "Error deploying LINK token")
 
-		By("Connecting to launched resources")
-		chainClient, err = blockchain.NewEVMClient(activeEVMNetwork, testEnvironment)
-		Expect(err).ShouldNot(HaveOccurred(), "Connecting to blockchain nodes shouldn't fail")
-		contractDeployer, err := contracts.NewContractDeployer(chainClient)
-		Expect(err).ShouldNot(HaveOccurred(), "Deploying contracts shouldn't fail")
-
-		chainlinkNodes, err = client.ConnectChainlinkNodes(testEnvironment)
-		Expect(err).ShouldNot(HaveOccurred(), "Connecting to chainlink nodes shouldn't fail")
-
-		chainClient.ParallelTransactions(true)
-
-		linkToken, err := contractDeployer.DeployLinkTokenContract()
-		Expect(err).ShouldNot(HaveOccurred(), "Deploying Link Token Contract shouldn't fail")
-
-		By("Funding Chainlink nodes")
-		//txCost, err := chainClient.EstimateCostForChainlinkOperations(10000)
-		//Expect(err).ShouldNot(HaveOccurred(), "Estimating cost for Chainlink Operations shouldn't fail")
-		err = actions.FundChainlinkNodes(chainlinkNodes, chainClient, big.NewFloat(10))
-		Expect(err).ShouldNot(HaveOccurred())
-
-		By("Deploy Registry and Registrar")
-		registry, registrar = actions.DeployAutoOCRRegistryAndRegistrar(
-			ethereum.RegistryVersion_2_0,
-			defaultOCRRegistryConfig,
-			numberOfUpkeeps,
-			linkToken,
-			contractDeployer,
-			chainClient,
-		)
-
-		By("Create OCR Automation Jobs")
-		actions.CreateOCRKeeperJobs(chainlinkNodes, registry.Address(), activeEVMNetwork.ChainID, 0)
-		nodesWithoutBootstrap := chainlinkNodes[1:]
-		ocrConfig := actions.BuildAutoOCR2ConfigVars(nodesWithoutBootstrap, defaultOCRRegistryConfig, registrar.Address(), 60*time.Second)
-		err = registry.SetConfig(defaultOCRRegistryConfig, ocrConfig)
-		Expect(err).ShouldNot(HaveOccurred(), "Registry config should be be set successfully")
-		Expect(chainClient.WaitForEvents()).ShouldNot(HaveOccurred(), "Waiting for config to be set")
-
-		By("Deploy Consumers")
-		consumers, upkeepIDs = actions.DeployConsumers(
-			registry,
-			registrar,
-			linkToken,
-			contractDeployer,
-			chainClient,
-			numberOfUpkeeps,
-			big.NewInt(defaultLinkFunds),
-			defaultUpkeepGasLimit,
-		)
-
-		By("watches all the registered upkeeps perform and then cancels them from the registry")
-		Eventually(func(g Gomega) {
-			// Check if the upkeeps are performing multiple times by analysing their counters and checking they are greater than 10
-			for i := 0; i < len(upkeepIDs); i++ {
-				counter, err := consumers[i].Counter(context.Background())
-				g.Expect(err).ShouldNot(HaveOccurred(), "Failed to retrieve consumer counter for upkeep at index "+strconv.Itoa(i))
-				expect := 5
-				g.Expect(counter.Int64()).Should(BeNumerically(">=", int64(expect)),
-					"Expected consumer counter to be greater than %d, but got %d", expect, counter.Int64())
-				log.Info().Int64("Upkeep counter", counter.Int64()).Msg("Number of upkeeps performed")
-			}
-		}, "10m", "1s").Should(Succeed()) // ~6m for cluster setup, ~2m for performing each upkeep 5 times, ~2m buffer
-
-		By("creating reorg for 10 blocks", func() {
-			rc, err := NewReorgController(
-				&ReorgConfig{
-					FromPodLabel:            reorg.TXNodesAppLabel,
-					ToPodLabel:              reorg.MinerNodesAppLabel,
-					Network:                 chainClient,
-					Env:                     testEnvironment,
-					BlockConsensusThreshold: 3,
-					Timeout:                 1800 * time.Second,
-				},
-			)
-
-			Expect(err).ShouldNot(HaveOccurred())
-			rc.ReOrg(reorgBlocks)
-			rc.WaitReorgStarted()
-
-			Eventually(func(g Gomega) {
-				// Check if the upkeeps are performing multiple times by analysing their counters and checking they are greater than 10
-				for i := 0; i < len(upkeepIDs); i++ {
-					counter, err := consumers[i].Counter(context.Background())
-					g.Expect(err).ShouldNot(HaveOccurred(), "Failed to retrieve consumer counter for upkeep at index "+strconv.Itoa(i))
-					expect := 10
-					g.Expect(counter.Int64()).Should(BeNumerically(">=", int64(expect)),
-						"Expected consumer counter to be greater than %d, but got %d", expect, counter.Int64())
-					log.Info().Int64("Upkeep counter", counter.Int64()).Msg("Number of upkeeps performed")
-				}
-			}, "5m", "1s").Should(Succeed())
-
-			err = rc.WaitDepthReached()
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-	},
-		testScenarios,
+	registry, registrar := actions.DeployAutoOCRRegistryAndRegistrar(
+		t,
+		ethereum.RegistryVersion_2_0,
+		defaultOCRRegistryConfig,
+		numberOfUpkeeps,
+		linkToken,
+		contractDeployer,
+		chainClient,
 	)
-})
+
+	actions.CreateOCRKeeperJobs(t, chainlinkNodes, registry.Address(), network.ChainID, 0)
+	nodesWithoutBootstrap := chainlinkNodes[1:]
+	ocrConfig := actions.BuildAutoOCR2ConfigVars(t, nodesWithoutBootstrap, defaultOCRRegistryConfig, registrar.Address(), 5*time.Second)
+	err = registry.SetConfig(defaultOCRRegistryConfig, ocrConfig)
+	require.NoError(t, err, "Registry config should be be set successfully")
+	require.NoError(t, chainClient.WaitForEvents(), "Waiting for config to be set")
+
+	consumers, upkeepIDs := actions.DeployConsumers(
+		t,
+		registry,
+		registrar,
+		linkToken,
+		contractDeployer,
+		chainClient,
+		numberOfUpkeeps,
+		big.NewInt(defaultLinkFunds),
+		defaultUpkeepGasLimit,
+	)
+
+	log.Info().Msg("Waiting for all upkeeps to be performed")
+
+	gom := gomega.NewGomegaWithT(t)
+	gom.Eventually(func(g gomega.Gomega) {
+		// Check if the upkeeps are performing multiple times by analyzing their counters and checking they are greater than 10
+		for i := 0; i < len(upkeepIDs); i++ {
+			counter, err := consumers[i].Counter(context.Background())
+			require.NoError(t, err, "Failed to retrieve consumer counter for upkeep at index %d", i)
+			expect := 5
+			log.Info().Int64("Upkeeps Performed", counter.Int64()).Int("Upkeep ID", i).Msg("Number of upkeeps performed")
+			g.Expect(counter.Int64()).Should(gomega.BeNumerically(">=", int64(expect)),
+				"Expected consumer counter to be greater than %d, but got %d", expect, counter.Int64())
+		}
+	}, "10m", "1s").Should(gomega.Succeed()) // ~1m for cluster setup, ~2m for performing each upkeep 5 times, ~2m buffer
+
+	rc, err := NewReorgController(
+		&ReorgConfig{
+			FromPodLabel:            reorg.TXNodesAppLabel,
+			ToPodLabel:              reorg.MinerNodesAppLabel,
+			Network:                 chainClient,
+			Env:                     testEnvironment,
+			BlockConsensusThreshold: 3,
+			Timeout:                 1800 * time.Second,
+		},
+	)
+
+	require.NoError(t, err, "Error getting reorg controller")
+	rc.ReOrg(automationReorgBlocks)
+	rc.WaitReorgStarted()
+
+	gom.Eventually(func(g gomega.Gomega) {
+		// Check if the upkeeps are performing multiple times by analyzing their counters and checking they are greater than 10
+		for i := 0; i < len(upkeepIDs); i++ {
+			counter, err := consumers[i].Counter(context.Background())
+			require.NoError(t, err, "Failed to retrieve consumer counter for upkeep at index %d", i)
+			expect := 10
+			log.Info().Int64("Upkeeps Performed", counter.Int64()).Int("Upkeep ID", i).Msg("Number of upkeeps performed")
+			g.Expect(counter.Int64()).Should(gomega.BeNumerically(">=", int64(expect)),
+				"Expected consumer counter to be greater than %d, but got %d", expect, counter.Int64())
+		}
+	}, "5m", "1s").Should(gomega.Succeed()) // ~1m for cluster setup, ~2m for performing each upkeep 5 times, ~2m buffer
+
+}
