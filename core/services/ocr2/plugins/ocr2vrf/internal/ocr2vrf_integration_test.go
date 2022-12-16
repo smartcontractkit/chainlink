16885e4562f1db084a5eb9faa8ab882f4e1ec5f6
Chris <104409744+vreff@users.noreply.github.com>
2022-12-16 09:08:59 -0700

    Use integration build tag instead

--- core/services/ocr2/plugins/ocr2vrf/internal/ocr2vrf_integration_test.go
+++ core/services/ocr2/plugins/ocr2vrf/internal/ocr2vrf_integration_test.go
@@ -1,3 +1,5 @@
+//go:build integration
+
 package internal_test
 
 import (
@@ -8,7 +10,6 @@ import (
 	"fmt"
 	"math/big"
 	"net"
-	"os"
 	"testing"
 	"time"
 
@@ -56,6 +57,9 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/utils"
 )
+
+// Note: these are using the "integration" build tag.
+// To run, use: go test -tags integration
 
 type ocr2vrfUniverse struct {
 	owner   *bind.TransactOpts
@@ -300,16 +304,10 @@ func setupNodeOCR2(
 }
 
 func TestIntegration_OCR2VRF_ForwarderFlow(t *testing.T) {
-	if os.Getenv("CI") == "" && os.Getenv("VRF_LOCAL_TESTING") == "" {
-		t.Skip("Skipping test locally.")
-	}
 	runOCR2VRFTest(t, true)
 }
 
 func TestIntegration_OCR2VRF(t *testing.T) {
-	if os.Getenv("CI") == "" && os.Getenv("VRF_LOCAL_TESTING") == "" {
-		t.Skip("Skipping test locally.")
-	}
 	runOCR2VRFTest(t, false)
 }
 
