01dc1abbe7369a5abcd61ea653ebef0141bf3f97
Sam Davies <samsondav@protonmail.com>
2023-01-10 12:13:07 -0500

    Implement WSRPC connection for Mercury

--- core/services/relay/evm/mercury/transmitter.go
+++ core/services/relay/evm/mercury/transmitter.go
@@ -1,32 +1,27 @@
 package mercury
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/pkg/errors"
 	"github.com/smartcontractkit/libocr/offchainreporting2/chains/evmutil"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2/types"
 
 	"github.com/smartcontractkit/chainlink/core/logger"
+	"github.com/smartcontractkit/chainlink/core/services"
+	"github.com/smartcontractkit/chainlink/core/services/relay/evm/mercury/wsrpc"
+	pb "github.com/smartcontractkit/chainlink/core/services/relay/evm/mercury/wsrpc/report"
 )
 
-var _ ocrtypes.ContractTransmitter = &MercuryTransmitter{}
+var _ ocrtypes.ContractTransmitter = &mercuryTransmitter{}
+var _ services.ServiceCtx = &mercuryTransmitter{}
 
-type HTTPClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
-type MercuryTransmitter struct {
-	lggr       logger.Logger
-	httpClient HTTPClient
+type mercuryTransmitter struct {
+	lggr      logger.Logger
+	rpcClient wsrpc.Client
 
 	fromAccount common.Address
 
@@ -54,17 +49,23 @@ func getPayloadTypes() abi.Arguments {
 	})
 }
 
-func NewTransmitter(lggr logger.Logger, httpClient HTTPClient, fromAccount common.Address, reportURL, username, password string) *MercuryTransmitter {
-	return &MercuryTransmitter{lggr.Named("Mercury"), httpClient, fromAccount, reportURL, username, password}
+func NewTransmitter(lggr logger.Logger, rpcClient wsrpc.Client, fromAccount common.Address, reportURL, username, password string) *mercuryTransmitter {
+	return &mercuryTransmitter{lggr.Named("Mercury"), rpcClient, fromAccount, reportURL, username, password}
 }
 
-type MercuryReport struct {
-	Payload     hexutil.Bytes
-	FromAccount common.Address
+func (mt *mercuryTransmitter) Start(ctx context.Context) error {
+	return mt.rpcClient.Start(ctx)
 }
+
+func (mt *mercuryTransmitter) Close() error {
+	return mt.rpcClient.Close()
+}
+
+func (mt *mercuryTransmitter) Healthy() error { return nil } //TODO
+func (mt *mercuryTransmitter) Ready() error   { return nil } //TODO
 
 // Transmit sends the report to the on-chain smart contract's Transmit method.
-func (mt *MercuryTransmitter) Transmit(ctx context.Context, reportCtx ocrtypes.ReportContext, report ocrtypes.Report, signatures []ocrtypes.AttributedOnchainSignature) error {
+func (mt *mercuryTransmitter) Transmit(ctx context.Context, reportCtx ocrtypes.ReportContext, report ocrtypes.Report, signatures []ocrtypes.AttributedOnchainSignature) error {
 	var rs [][32]byte
 	var ss [][32]byte
 	var vs [32]byte
@@ -84,57 +85,36 @@ func (mt *MercuryTransmitter) Transmit(ctx context.Context, reportCtx ocrtypes.R
 		return errors.Wrap(err, "abi.Pack failed")
 	}
 
-	mr := MercuryReport{
+	rr := &pb.ReportRequest{
 		Payload:     payload,
-		FromAccount: mt.fromAccount,
-	}
-	mt.lggr.Infow("Transmitting report", "mercuryReport", mr, "report", report, "reportCtx", reportCtx, "signatures", signatures)
-
-	b, err := json.Marshal(mr)
-	if err != nil {
-		return errors.Wrap(err, "failed to marshal mercury report JSON")
+		FromAccount: mt.fromAccount.Bytes(),
 	}
 
-	req, err := http.NewRequest(http.MethodPost, mt.reportURL, bytes.NewReader(b))
-	if err != nil {
-		return errors.Wrap(err, "failed to instantiate mercury server http request")
-	}
+	mt.lggr.Debugw("Transmitting report", "reportRequest", rr, "report", report, "reportCtx", reportCtx, "signatures", signatures)
 
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(mt.username, mt.password)
-	req = req.WithContext(ctx)
-
-	res, err := mt.httpClient.Do(req)
+	res, err := mt.rpcClient.Transmit(ctx, rr)
 	if err != nil {
 		return errors.Wrap(err, "failed to POST to mercury server")
 	}
-	defer res.Body.Close()
 
-	// It's only used for logging, keep it short
-	safeLimitResp := http.MaxBytesReader(nil, res.Body, 1024)
-	respBody, err := io.ReadAll(safeLimitResp)
-	if err != nil {
-		mt.lggr.Errorw("Failed to read response body", "err", err)
-	}
-
-	if res.StatusCode >= 200 && res.StatusCode < 300 {
-		mt.lggr.Infow("Transmit report success", "responseStatus", res.Status, "reponseBody", string(respBody), "reportCtx", reportCtx)
+	if res.Error == "" {
+		mt.lggr.Debugw("Transmit report success", "response", res, "reportCtx", reportCtx)
 	} else {
-		mt.lggr.Errorw("Transmit report failed", "responseStatus", res.Status, "reponseBody", string(respBody), "reportCtx", reportCtx)
+		mt.lggr.Errorw("Transmit report failed", "response", res, "reportCtx", reportCtx)
 
 	}
 
 	return nil
 }
 
-func (mt *MercuryTransmitter) FromAccount() ocrtypes.Account {
+func (mt *mercuryTransmitter) FromAccount() ocrtypes.Account {
 	return ocrtypes.Account(mt.fromAccount.Hex())
 }
 
 // LatestConfigDigestAndEpoch retrieves the latest config digest and epoch from the OCR2 contract.
 // It is plugin independent, in particular avoids use of the plugin specific generated evm wrappers
 // by using the evm client Call directly for functions/events that are part of OCR2Abstract.
-func (mt *MercuryTransmitter) LatestConfigDigestAndEpoch(ctx context.Context) (cd ocrtypes.ConfigDigest, epoch uint32, err error) {
+func (mt *mercuryTransmitter) LatestConfigDigestAndEpoch(ctx context.Context) (cd ocrtypes.ConfigDigest, epoch uint32, err error) {
 	// ConfigDigest and epoch are not stored on the contract in mercury mode
 	// TODO: Do we need to support retrieving it from the server? Does it matter?
 	// https://app.shortcut.com/chainlinklabs/story/57500/return-the-actual-latest-transmission-details
