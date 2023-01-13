9a795fb0d7664ec3917050f910ca914ec43b2ad9
Martin Kersner <martin@bisonai.com>
2023-01-13 16:24:29 +0900

    fix: Add missing break to for loop in `deregisterProvingKey`

--- contracts/src/v0.8/VRFCoordinatorV2.sol
+++ contracts/src/v0.8/VRFCoordinatorV2.sol
@@ -205,6 +205,7 @@ contract VRFCoordinatorV2 is
         // Copy last element and overwrite kh to be deleted with it
         s_provingKeyHashes[i] = last;
         s_provingKeyHashes.pop();
+        break;
       }
     }
     emit ProvingKeyDeregistered(kh, oracle);
