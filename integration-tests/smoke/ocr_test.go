7b4a5cb9bff4561b075773d733d8a70612ce5d6f
Sergey Kudasov <f4hrenh9it@gmail.com>
2023-01-18 20:26:44 +0300

    fix chaos suites, make them run in parallel for groups (pods/network) (#8259)
    * fix ocr chaos suite
    
    * fix ocr chaos tests suite
    
    * fix go mod
    
    * fix go mod
    
    * fix go mod
    
    * update go mod/sum
    
    * exclude broken cdk8s generated cases, make cases parallel
    
    * split parallel groups
    
    * fix job names
    
    * remove cleanup
    
    * turn off on-push CI run
    
    * move base config to a separate package
    
    Co-authored-by: Tate <tate.exon@smartcontract.com>

--- integration-tests/smoke/ocr_test.go
+++ integration-tests/smoke/ocr_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/smartcontractkit/chainlink-testing-framework/blockchain"
 	ctfClient "github.com/smartcontractkit/chainlink-testing-framework/client"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils"
+	"github.com/smartcontractkit/chainlink/integration-tests/config"
 	"github.com/stretchr/testify/require"
 
 	networks "github.com/smartcontractkit/chainlink/integration-tests"
@@ -79,14 +80,6 @@ func setupOCRTest(t *testing.T) (testEnvironment *environment.Environment, testN
 			WsURLs:      testNetwork.URLs,
 		})
 	}
-	baseTOML := `[OCR]
-Enabled = true
-
-[P2P]
-[P2P.V1]
-Enabled = true
-ListenIP = '0.0.0.0'
-ListenPort = 6690`
 	testEnvironment = environment.New(&environment.Config{
 		NamespacePrefix: fmt.Sprintf("smoke-ocr-%s", strings.ReplaceAll(strings.ToLower(testNetwork.Name), " ", "-")),
 	}).
@@ -94,7 +87,7 @@ ListenPort = 6690`
 		AddHelm(mockserver.New(nil)).
 		AddHelm(evmConfig).
 		AddHelm(chainlink.New(0, map[string]interface{}{
-			"toml":     client.AddNetworksConfig(baseTOML, testNetwork),
+			"toml":     client.AddNetworksConfig(config.BaseOCRP2PV1Config, testNetwork),
 			"replicas": 6,
 		}))
 	err := testEnvironment.Run()
