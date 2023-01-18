31e2b7a50848545afa0294584ee612b040e69ab1
Chris C <104409744+vreff@users.noreply.github.com>
2023-01-18 14:55:05 -0500

    OCR2VRF - stub juelsperfeecoin (#8263)

--- core/services/ocr2/plugins/ocr2vrf/juelsfeecoin/link_eth_price_provider_test.go
+++ core/services/ocr2/plugins/ocr2vrf/juelsfeecoin/link_eth_price_provider_test.go
@@ -38,4 +38,26 @@ func Test_JuelsPerFeeCoin(t *testing.T) {
 		assert.Nil(t, price)
 		assert.Equal(t, "get aggregator latest answer: network failure", err.Error())
 	})
+
+	t.Run("returns juels per fee coin", func(t *testing.T) {
+		latestRoundData := aggregator_v3_interface.LatestRoundData{Answer: big.NewInt(10000)}
+		mockAggregator.On("LatestRoundData", mock.Anything).Return(latestRoundData, nil)
+		p := linkEthPriceProvider{aggregator: mockAggregator, stubbed: true}
+		price, err := p.JuelsPerFeeCoin()
+
+		require.NoError(t, err)
+		assert.Equal(t, int64(0), price.Int64())
+		mockAggregator.AssertNotCalled(t, "LatestRoundData")
+	})
+
+	t.Run("returns error when contract call fails", func(t *testing.T) {
+		latestRoundData := aggregator_v3_interface.LatestRoundData{}
+		mockAggregator.On("LatestRoundData", mock.Anything).Return(latestRoundData, errors.New("network failure"))
+		p := linkEthPriceProvider{aggregator: mockAggregator, stubbed: true}
+		price, err := p.JuelsPerFeeCoin()
+
+		require.NoError(t, err)
+		assert.Zero(t, price.Int64())
+		mockAggregator.AssertNotCalled(t, "LatestRoundData")
+	})
 }
