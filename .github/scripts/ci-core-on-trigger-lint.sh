bc7781fa7637dce9001e6a30d634c49b36acc417
chainchad <96362174+chainchad@users.noreply.github.com>
2023-01-19 08:50:14 -0500

    Fix output name

--- .github/scripts/ci-core-on-trigger-lint.sh
+++ .github/scripts/ci-core-on-trigger-lint.sh
@@ -8,7 +8,9 @@
 # 3. on pushes to these branches: staging, trying, rollup
 ##
 
-echo ${{ github.event_name }}
+# TODO: make this use getopts and pass in the github expressions as args or convert to a zx/typescript script
+# then call it and have it echo output "false" or "true" and >> send to GITHUB_OUTPUT from the yaml file
+
 
 on_trigger=false
 if [[ "${{ github.event_name }}" = "schedule" ]]; then
