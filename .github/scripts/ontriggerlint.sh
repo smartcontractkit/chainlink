5223084ba2d91f81beb073f3d2a8545e85fc5433
chainchad <96362174+chainchad@users.noreply.github.com>
2023-01-20 11:32:03 -0500

    Make ontriggerlint script consider file path changes

--- .github/scripts/ontriggerlint.sh
+++ .github/scripts/ontriggerlint.sh
@@ -10,12 +10,15 @@ set -euo pipefail
 # Trigger golangci-lint job steps when event is one of:
 #   1. on a schedule (GITHUB_EVENT_NAME)
 #   2. on PR's where the target branch (GITHUB_BASE_REF) is not prefixed with 'release/*'
-#   3. on pushes to these branches (GITHUB_REF): staging, trying, rollup
+#      and where the relevant source code (SRC_CHANGED) has changed
+#   3. on pushes to these branches (GITHUB_REF): staging, trying, rollup and where the
+#      relevant source code (SRC_CHANGED) has changed
 #
 # env vars:
 # GITHUB_EVENT_NAME GitHub's event name, ex: schedule|pull_request|push (GitHub context: github.event_name)
 # GITHUB_BASE_REF   GitHub's base ref - target branch of pull request (GitHub context: github.base_ref)
 # GITHUB_REF        GitHub's ref - branch or tag that triggered run (GitHub context: github.ref)
+# SRC_CHANGED       Specific source paths that we want to trigger on were modified. One of: true|false
 ##
 
 if [[ -z "${GITHUB_REF:-}" ]]; then GITHUB_REF=""; fi
@@ -27,10 +30,10 @@ ON_TRIGGER=false
 if [[ "${GITHUB_EVENT_NAME:-}" = "schedule" ]]; then
     # Trigger on scheduled runs
     ON_TRIGGER=true
-elif [[ "${GITHUB_EVENT_NAME:-}" = "pull_request" && "${GITHUB_BASE_REF:-}" != release/* ]]; then
+elif [[ "${SRC_CHANGED:-}" = "true" && "${GITHUB_EVENT_NAME:-}" = "pull_request" && "${GITHUB_BASE_REF:-}" != release/* ]]; then
     # Trigger if it's from a pull request targetting any branch EXCEPT the release branch
     ON_TRIGGER=true
-elif [[ "${GITHUB_EVENT_NAME:-}" = "push" && "${ABBREV_GITHUB_REF}" =~ ^(staging|trying|rollup)$ ]]; then
+elif [[ "${SRC_CHANGED:-}" = "true" && "${GITHUB_EVENT_NAME:-}" = "push" && "${ABBREV_GITHUB_REF}" =~ ^(staging|trying|rollup)$ ]]; then
     # Trigger if it's a push to specific branches
     ON_TRIGGER=true
 fi
--- .github/scripts/ontriggerlint_test.sh
+++ .github/scripts/ontriggerlint_test.sh
@@ -11,59 +11,87 @@ _compare_result() {
     local ACTUAL_RESULT=$3
 
     if [[ "${EXPECTED_RESULT:-}" != "${ACTUAL_RESULT:-}" ]]; then
-        echo "Fail: ${TEST_NAME} expected ${EXPECTED_RESULT} got ${ACTUAL_RESULT}"
+        echo "Fail: ${TEST_NAME} EXPECTED ${EXPECTED_RESULT} GOT ${ACTUAL_RESULT}"
         exit 1
     else
-         echo "Pass: ${TEST_NAME} expected ${EXPECTED_RESULT} got ${ACTUAL_RESULT}"
+        echo "Pass: ${TEST_NAME} EXPECTED ${EXPECTED_RESULT} GOT ${ACTUAL_RESULT}"
     fi
 }
 
 test_schedule() {
-    local TEST_NAME="Trigger on schedule"
+    local TEST_NAME="Trigger on schedule when source changed"
     local EXPECTED_RESULT="on_trigger=true"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=schedule ./ontriggerlint.sh)
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=schedule ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+    local TEST_NAME="Trigger on schedule when source unchanged"
+    local EXPECTED_RESULT="on_trigger=true"
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=false GITHUB_EVENT_NAME=schedule ./ontriggerlint.sh)
     _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
 }
 
 test_pull_request() {
-    local TEST_NAME="Trigger on pull_request for non release branches"
+    local TEST_NAME="Trigger on pull_request for non release branches when source changed"
     local EXPECTED_RESULT="on_trigger=true"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=pull_request GITHUB_BASE_REF=develop ./ontriggerlint.sh)
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=pull_request GITHUB_BASE_REF=develop ./ontriggerlint.sh)
     _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
 
-    local TEST_NAME="No trigger on pull_request for release branches"
+    local TEST_NAME="No trigger on pull_request for non release branches when source unchanged"
     local EXPECTED_RESULT="on_trigger=false"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=pull_request GITHUB_BASE_REF=release/1.2.3 ./ontriggerlint.sh)
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=false GITHUB_EVENT_NAME=pull_request GITHUB_BASE_REF=develop ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+    local TEST_NAME="No trigger on pull_request for release branches when source changed"
+    local EXPECTED_RESULT="on_trigger=false"
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=pull_request GITHUB_BASE_REF=release/1.2.3 ./ontriggerlint.sh)
     _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
 }
 
 test_push() {
-    local TEST_NAME="Trigger on push to the staging branch"
+    local TEST_NAME="Trigger on push to the staging branch when source changed"
     local EXPECTED_RESULT="on_trigger=true"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=push GITHUB_REF=staging ./ontriggerlint.sh)
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=push GITHUB_REF=staging ./ontriggerlint.sh)
     _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
 
-    local TEST_NAME="Trigger on push to the trying branch"
-    local EXPECTED_RESULT="on_trigger=true"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=push GITHUB_REF=staging ./ontriggerlint.sh)
-    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
-
-    local TEST_NAME="Trigger on push to the rollup branch"
-    local EXPECTED_RESULT="on_trigger=true"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=push GITHUB_REF=rollup ./ontriggerlint.sh)
-    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
-
-    local TEST_NAME="No trigger on push to the develop branch"
+    local TEST_NAME="No trigger on push to the staging branch when source unchanged"
     local EXPECTED_RESULT="on_trigger=false"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=push GITHUB_REF=develop ./ontriggerlint.sh)
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=false GITHUB_EVENT_NAME=push GITHUB_REF=staging ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+    local TEST_NAME="Trigger on push to the trying branch when source changed"
+    local EXPECTED_RESULT="on_trigger=true"
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=push GITHUB_REF=staging ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+    local TEST_NAME="Trigger on push to the rollup branch when source changed"
+    local EXPECTED_RESULT="on_trigger=true"
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=push GITHUB_REF=rollup ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+    local TEST_NAME="No trigger on push to the develop branch when source changed"
+    local EXPECTED_RESULT="on_trigger=false"
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=push GITHUB_REF=develop ./ontriggerlint.sh)
     _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
 }
 
 test_misc() {
-    local TEST_NAME="No trigger on invalid event name"
+    local TEST_NAME="No trigger on invalid event name when source changed"
     local EXPECTED_RESULT="on_trigger=false"
-    ACTUAL_RESULT=$(GITHUB_EVENT_NAME=invalid_event_name ./ontriggerlint.sh)
-    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"    
+    local ACTUAL_RESULT
+    ACTUAL_RESULT=$(SRC_CHANGED=true GITHUB_EVENT_NAME=invalid_event_name ./ontriggerlint.sh)
+    _compare_result "${TEST_NAME}" "${EXPECTED_RESULT}" "${ACTUAL_RESULT}"
+
+
 }
 
 test_schedule
