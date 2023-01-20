796493a05e7c283982c0fa0307ed1a5742d0023c
Sam <samsondav@protonmail.com>
2023-01-20 08:37:52 -0500

    Update core/chains/evm/txmgr/eth_broadcaster.go
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/chains/evm/txmgr/eth_broadcaster.go
+++ core/chains/evm/txmgr/eth_broadcaster.go
@@ -327,9 +327,7 @@ func (eb *EthBroadcaster) SyncNonce(ctx context.Context, k ethkey.State) {
 						eb.logger.Warnw("Failed to sync with on-chain nonce", "address", k.Address, "attempt", attempt, "err", err)
 					}
 					continue
-				} else {
-					return
-				}
+				return
 			}
 		}
 	}
