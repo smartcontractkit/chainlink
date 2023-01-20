f1563ecb6bf08ea61f0198dc62cc83c8005d7e1e
Danny Skubak <skubakdj@users.noreply.github.com>
2023-01-20 17:09:55 -0500

    Add telemetry types

--- core/services/telemetry/common.go
+++ core/services/telemetry/common.go
@@ -1,9 +1,10 @@
 package telemetry
 
 import (
+	"github.com/smartcontractkit/chainlink/core/services/synchronization"
 	ocrtypes "github.com/smartcontractkit/libocr/commontypes"
 )
 
 type MonitoringEndpointGenerator interface {
-	GenMonitoringEndpoint(contractID string) ocrtypes.MonitoringEndpoint
+	GenMonitoringEndpoint(contractID string, telemType synchronization.TelemetryType) ocrtypes.MonitoringEndpoint
 }
