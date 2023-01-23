73e07b090e3dafc92f3b0f55d01397c81c6e15d2
Pedro Grilo <pedro.grilo@smartcontract.com>
2023-01-23 19:10:26 +0000

    Log telemetry to a file

--- core/services/telemetry/noop.go
+++ core/services/telemetry/noop.go
@@ -1,6 +1,8 @@
 package telemetry
 
 import (
+	"os"
+
 	ocrtypes "github.com/smartcontractkit/libocr/commontypes"
 )
 
@@ -11,6 +13,17 @@ type NoopAgent struct {
 
 // SendLog sends a telemetry log to the explorer
 func (t *NoopAgent) SendLog(log []byte) {
+	if os.Getenv("TELEMETRY_INGRESS_LOG_FILE") != "" {
+		f, err := os.Create("telemetry.log")
+		if err != nil {
+			return
+		}
+		defer f.Close()
+		_, err = f.Write(log)
+		if err != nil {
+			return
+		}
+	}
 }
 
 // GenMonitoringEndpoint creates a monitoring endpoint for telemetry
