d660bc37748c68f039a0d32da7d62690b9d5c914
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2023-01-27 20:55:50 +0200

    VRF-297: PR comments

--- integration-tests/chaos/ocr2vrf_chaos_test.go
+++ integration-tests/chaos/ocr2vrf_chaos_test.go
@@ -136,11 +136,11 @@ func TestOCR2VRFChaos(t *testing.T) {
 			require.NoError(t, err)
 
 			chainClient, err := blockchain.NewEVMClient(testNetwork, testEnvironment)
-			require.NoError(t, err)
+			require.NoError(t, err, "Error connecting to blockchain")
 			contractDeployer, err := contracts.NewContractDeployer(chainClient)
-			require.NoError(t, err)
+			require.NoError(t, err, "Error building contract deployer")
 			chainlinkNodes, err := client.ConnectChainlinkNodes(testEnvironment)
-			require.NoError(t, err)
+			require.NoError(t, err, "Error connecting to Chainlink nodes")
 			nodeAddresses, err := actions.ChainlinkNodeAddresses(chainlinkNodes)
 			require.NoError(t, err, "Retreiving on-chain wallet addresses for chainlink nodes shouldn't fail")
 
@@ -152,10 +152,10 @@ func TestOCR2VRFChaos(t *testing.T) {
 			chainClient.ParallelTransactions(true)
 
 			linkToken, err := contractDeployer.DeployLinkTokenContract()
-			require.NoError(t, err)
+			require.NoError(t, err, "Error deploying LINK token")
 
 			mockETHLinkFeed, err := contractDeployer.DeployMockETHLINKFeed(ocr2vrf_constants.LinkEthFeedResponse)
-			require.NoError(t, err)
+			require.NoError(t, err, "Error deploying Mock ETH/LINK Feed")
 
 			_, _, vrfBeaconContract, consumerContract := ocr2vrf_actions.SetupOCR2VRFUniverse(
 				t,
@@ -186,11 +186,11 @@ func TestOCR2VRFChaos(t *testing.T) {
 			}
 
 			id, err := testEnvironment.Chaos.Run(testCase.chaosFunc(testEnvironment.Cfg.Namespace, testCase.chaosProps))
-			require.NoError(t, err)
+			require.NoError(t, err, "Error running Chaos Experiment")
 			log.Info().Msg("Chaos Applied")
 
 			err = testEnvironment.Chaos.WaitForAllRecovered(id)
-			require.NoError(t, err)
+			require.NoError(t, err, "Error waiting for Chaos Experiment to end")
 			log.Info().Msg("Chaos Recovered")
 
 			//Request and Redeem Randomness again to see that after Chaos Experiment whole process is still working
@@ -206,7 +206,7 @@ func TestOCR2VRFChaos(t *testing.T) {
 
 			for i := uint16(0); i < ocr2vrf_constants.NumberOfRandomWordsToRequest; i++ {
 				randomness, err := consumerContract.GetRandomnessByRequestId(nil, requestID, big.NewInt(int64(i)))
-				require.NoError(t, err)
+				require.NoError(t, err, "Error getting Randomness result from Consumer Contract")
 				log.Info().Interface("Random Number", randomness).Interface("Randomness Number Index", i).Msg("Randomness retrieved from Consumer contract")
 				require.NotEqual(t, 0, randomness.Uint64(), "Randomness retrieved from Consumer contract give an answer other than 0")
 			}
