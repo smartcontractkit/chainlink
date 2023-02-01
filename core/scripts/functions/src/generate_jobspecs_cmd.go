9fe2478e234a8a096ab60648e21bf0630aa7d82e
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2023-02-01 12:45:41 -0800

    [Functions/scripts] Optionally read keys from file instead of fetching them from the nodes (#8362)
    This will be used to generate config for an external DON

--- core/scripts/functions/src/generate_jobspecs_cmd.go
+++ core/scripts/functions/src/generate_jobspecs_cmd.go
@@ -44,7 +44,7 @@ func (g *generateJobSpecs) Run(args []string) {
 	helpers.PanicErr(err)
 
 	bootHost := nodes[0].url.Host
-	lines = replacePlaceholders(lines, *chainID, *p2pPort, *contractAddress, bootHost, bootstrapNode, bootstrapNode, *truncateHostname)
+	lines = replacePlaceholders(lines, *chainID, *p2pPort, *contractAddress, bootHost, &bootstrapNode, &bootstrapNode, *truncateHostname)
 	outputPath := filepath.Join(artefactsDir, bootHost+".toml")
 	err = writeLines(lines, outputPath)
 	helpers.PanicErr(err)
@@ -53,7 +53,7 @@ func (g *generateJobSpecs) Run(args []string) {
 	lines, err = readLines(filepath.Join(templatesDir, oracleSpecTemplate))
 	helpers.PanicErr(err)
 	for i := 1; i < len(nodes); i++ {
-		oracleLines := replacePlaceholders(lines, *chainID, *p2pPort, *contractAddress, bootHost, bootstrapNode, nca[i], *truncateHostname)
+		oracleLines := replacePlaceholders(lines, *chainID, *p2pPort, *contractAddress, bootHost, &bootstrapNode, &nca[i], *truncateHostname)
 		outputPath := filepath.Join(artefactsDir, nodes[i].url.Host+".toml")
 		err = writeLines(oracleLines, outputPath)
 		helpers.PanicErr(err)
@@ -61,18 +61,18 @@ func (g *generateJobSpecs) Run(args []string) {
 	}
 }
 
-func replacePlaceholders(lines []string, chainID, p2pPort int64, contractAddress, bootHost string, boot *nodeConfig, node *nodeConfig, truncateHostname bool) (output []string) {
+func replacePlaceholders(lines []string, chainID, p2pPort int64, contractAddress, bootHost string, boot *NodeKeys, node *NodeKeys, truncateHostname bool) (output []string) {
 	chainIDStr := strconv.FormatInt(chainID, 10)
 	if truncateHostname {
 		bootHost = bootHost[:strings.IndexByte(bootHost, '.')]
 	}
-	bootstrapper := fmt.Sprintf("%s@%s:%d", boot.p2pPeerID, bootHost, p2pPort)
+	bootstrapper := fmt.Sprintf("%s@%s:%d", boot.P2PPeerID, bootHost, p2pPort)
 	ts := time.Now().UTC().Format("2006-01-02T15:04")
 	for _, l := range lines {
 		l = strings.Replace(l, "{{chain_id}}", chainIDStr, 1)
 		l = strings.Replace(l, "{{oracle_contract_address}}", contractAddress, 1)
-		l = strings.Replace(l, "{{node_eth_address}}", node.ethAddress, 1)
-		l = strings.Replace(l, "{{ocr2_key_bundle_id}}", node.ocr2BundleID, 1)
+		l = strings.Replace(l, "{{node_eth_address}}", node.EthAddress, 1)
+		l = strings.Replace(l, "{{ocr2_key_bundle_id}}", node.OCR2BundleID, 1)
 		l = strings.Replace(l, "{{p2p_bootstrapper}}", bootstrapper, 1)
 		l = strings.Replace(l, "{{timestamp}}", ts, 1)
 		output = append(output, l)
