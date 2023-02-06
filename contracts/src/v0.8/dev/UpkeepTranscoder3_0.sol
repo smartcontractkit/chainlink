666c28d0ea2a236b231e6793a20648e18be49241
Ryan Hall <hall.ryan.r@gmail.com>
2023-02-06 15:17:22 -0500

    lock pragma version for UpkeepTranscoder3_0

--- contracts/src/v0.8/dev/UpkeepTranscoder3_0.sol
+++ contracts/src/v0.8/dev/UpkeepTranscoder3_0.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 
 import "./../interfaces/UpkeepTranscoderInterface.sol";
 import "./../interfaces/TypeAndVersionInterface.sol";
