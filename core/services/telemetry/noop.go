a832bfbd5dbc4bb62f62b4277c8897a5fb4ea6eb
Danny Skubak <skubakdj@gmail.com>
2023-02-15 14:03:33 -0500

    Add new fields to telemetry ingress protobuf (#8318)
    Co-authored-by: Danny Skubak <skubakdj@users.noreply.github.com>

--- core/services/telemetry/noop.go
+++ core/services/telemetry/noop.go
@@ -2,6 +2,8 @@ package telemetry
 
 import (
 	ocrtypes "github.com/smartcontractkit/libocr/commontypes"
+
+	"github.com/smartcontractkit/chainlink/core/services/synchronization"
 )
 
 var _ MonitoringEndpointGenerator = &NoopAgent{}
@@ -14,6 +16,6 @@ func (t *NoopAgent) SendLog(log []byte) {
 }
 
 // GenMonitoringEndpoint creates a monitoring endpoint for telemetry
-func (t *NoopAgent) GenMonitoringEndpoint(contractID string) ocrtypes.MonitoringEndpoint {
+func (t *NoopAgent) GenMonitoringEndpoint(contractID string, telemType synchronization.TelemetryType) ocrtypes.MonitoringEndpoint {
 	return t
 }
