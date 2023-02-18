e978a83c783651d9ce099aa9f4b4d109adb7d578
krehermann <keith.rehermann@smartcontract.com>
2023-02-17 17:17:12 -0700

    fix linter

--- core/utils/deferable_writer_closer.go
+++ core/utils/deferable_writer_closer.go
@@ -8,20 +8,19 @@ import (
 // DeferableWriterCloser is to be used in leiu of defer'ing
 // Close on an io.WriterClose (// For more background see https://www.joeshaw.org/dont-defer-close-on-writable-files/)
 // Callers should *both*
-// explicity call Close and check for errors when done with the underlying writerclose
+// explicitly call Close and check for errors when done with the underlying writerclose
 // *and* defer the Close() to handle returns before the explicit close
 //
 // For example rather than
-
 // f, err := os.Create(...)
 // if err != nil {...}
 // defer f.Close()
 // f.Write(...)
 // ...
 // return
-
+//
 // do
-
+//
 // f, err := os.Create(...)
 // if err != nil {...}
 // wc := NewDeferableWriterCloser(f)
@@ -31,7 +30,6 @@ import (
 // err = wc.Close()
 // if err != nil {...}
 // return
-
 type DeferableWriterCloser struct {
 	mu sync.Mutex
 	io.WriteCloser
