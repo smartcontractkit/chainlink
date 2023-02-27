cef2fb4a8fda0cd2694666375ebf906f2c45c7d4
Sam Hassan <es.hassan187@gmail.com>
2023-02-27 20:16:21 +0100

    Add auth option for metric endpoint

--- core/cmd/client.go
+++ core/cmd/client.go
@@ -51,10 +51,10 @@ import (
 
 var prometheus *ginprom.Prometheus
 
-func init() {
+func initPrometheus(cfg config.GeneralConfig) {
 	// ensure metrics are registered once per instance to avoid registering
 	// metrics multiple times (panic)
-	prometheus = ginprom.New(ginprom.Namespace("service"))
+	prometheus = ginprom.New(ginprom.Namespace("service"), ginprom.Token(cfg.PrometheusAuthToken()))
 }
 
 var (
@@ -98,7 +98,8 @@ type ChainlinkAppFactory struct{}
 
 // NewApplication returns a new instance of the node with the given config.
 func (n ChainlinkAppFactory) NewApplication(ctx context.Context, cfg config.GeneralConfig, appLggr logger.Logger, db *sqlx.DB) (app chainlink.Application, err error) {
-
+	initPrometheus(cfg)
+	appLggr.Critical("Auth token:", cfg.PrometheusAuthToken())
 	keyStore := keystore.New(db, utils.GetScryptParams(cfg), appLggr, cfg)
 
 	// Set up the versioning ORM
