67e898ea19849b0e63255328282e4508a6dfaa53
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2023-03-07 14:35:21 +0100

    Update contract bindings

--- integration-tests/contracts/ethereum/mercury/verifier_proxy/VerifierProxy.go
+++ integration-tests/contracts/ethereum/mercury/verifier_proxy/VerifierProxy.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // VerifierProxyMetaData contains all meta data concerning the VerifierProxy contract.
@@ -156,11 +157,11 @@ func NewVerifierProxyFilterer(address common.Address, filterer bind.ContractFilt
 
 // bindVerifierProxy binds a generic wrapper to an already deployed contract.
 func bindVerifierProxy(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(VerifierProxyABI))
+	parsed, err := VerifierProxyMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
