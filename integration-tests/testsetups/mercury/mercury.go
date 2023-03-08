6d24db9a41fbf4b58a796ce9dc70fae017efc3f0
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2023-03-09 00:23:28 +0100

    Refactoring 2

--- integration-tests/testsetups/mercury/mercury.go
+++ integration-tests/testsetups/mercury/mercury.go
@@ -2,9 +2,7 @@ package mercury
 
 import (
 	"crypto/ed25519"
-	"crypto/rand"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"math"
 	"math/big"
@@ -38,31 +36,13 @@ import (
 	"github.com/smartcontractkit/chainlink/integration-tests/client"
 	"github.com/smartcontractkit/chainlink/integration-tests/config"
 	"github.com/smartcontractkit/chainlink/integration-tests/contracts"
+	mercuryserversetup "github.com/smartcontractkit/chainlink/integration-tests/testsetups/mercury/mercuryserver"
 	"github.com/smartcontractkit/libocr/offchainreporting2/confighelper"
 	"github.com/smartcontractkit/libocr/offchainreporting2/types"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/guregu/null.v4"
 )
-
-type csaKey struct {
-	NodeName    string `json:"nodeName"`
-	NodeAddress string `json:"nodeAddress"`
-	PublicKey   string `json:"publicKey"`
-}
-
-type oracle struct {
-	Id                    string   `json:"id"`
-	Website               string   `json:"website"`
-	Name                  string   `json:"name"`
-	Status                string   `json:"status"`
-	NodeAddress           []string `json:"nodeAddress"`
-	OracleAddress         string   `json:"oracleAddress"`
-	CsaKeys               []csaKey `json:"csaKeys"`
-	Ocr2ConfigPublicKey   []string `json:"ocr2ConfigPublicKey"`
-	Ocr2OffchainPublicKey []string `json:"ocr2OffchainPublicKey"`
-	Ocr2OnchainPublicKey  []string `json:"ocr2OnchainPublicKey"`
-}
 
 func ValidateReport(r map[string]interface{}) error {
 	feedIdInterface, ok := r["feedId"]
@@ -106,98 +86,6 @@ func ValidateReport(r map[string]interface{}) error {
 	log.Trace().Uint32("Timestamp", observationsTimestamp).Msg("Observation timestamp")
 
 	return nil
-}
-
-// Build config with nodes for Mercury server
-func BuildRpcNodesJsonConf(t *testing.T, chainlinkNodes []*client.Chainlink) ([]byte, error) {
-	var msRpcNodesConf []*oracle
-	for i, chainlinkNode := range chainlinkNodes {
-		nodeName := fmt.Sprint(i)
-		nodeAddress, err := chainlinkNode.PrimaryEthAddress()
-		require.NoError(t, err)
-		csaKeys, _, err := chainlinkNode.ReadCSAKeys()
-		require.NoError(t, err)
-		csaPubKey := csaKeys.Data[0].Attributes.PublicKey
-		ocr2Keys, resp, err := chainlinkNode.ReadOCR2Keys()
-		_ = ocr2Keys
-		_ = resp
-		require.NoError(t, err)
-		var ocr2Config client.OCR2KeyAttributes
-		for _, key := range ocr2Keys.Data {
-			if key.Attributes.ChainType == string(chaintype.EVM) {
-				ocr2Config = key.Attributes
-				break
-			}
-		}
-		ocr2ConfigPublicKey := strings.TrimPrefix(ocr2Config.ConfigPublicKey, "ocr2cfg_evm_")
-		ocr2OffchainPublicKey := strings.TrimPrefix(ocr2Config.OffChainPublicKey, "ocr2off_evm_")
-		ocr2OnchainPublicKey := strings.TrimPrefix(ocr2Config.OnChainPublicKey, "ocr2on_evm_")
-
-		node := &oracle{
-			Id:            fmt.Sprint(i),
-			Name:          nodeName,
-			Status:        "active",
-			NodeAddress:   []string{nodeAddress},
-			OracleAddress: "0x0000000000000000000000000000000000000000",
-			CsaKeys: []csaKey{
-				{
-					NodeName:    nodeName,
-					NodeAddress: nodeAddress,
-					PublicKey:   csaPubKey,
-				},
-			},
-			Ocr2ConfigPublicKey:   []string{ocr2ConfigPublicKey},
-			Ocr2OffchainPublicKey: []string{ocr2OffchainPublicKey},
-			Ocr2OnchainPublicKey:  []string{ocr2OnchainPublicKey},
-		}
-		msRpcNodesConf = append(msRpcNodesConf, node)
-	}
-	return json.Marshal(msRpcNodesConf)
-}
-
-func setupMercuryServer(
-	t *testing.T,
-	testEnv *environment.Environment,
-	dbSettings map[string]interface{},
-	serverSettings map[string]interface{},
-) ed25519.PublicKey {
-	chainlinkNodes, err := client.ConnectChainlinkNodes(testEnv)
-	require.NoError(t, err, "Error connecting to Chainlink nodes")
-
-	rpcNodesJsonConf, _ := BuildRpcNodesJsonConf(t, chainlinkNodes)
-	log.Info().Msgf("RPC nodes conf for mercury server: %s", rpcNodesJsonConf)
-
-	// Generate keys for Mercury RPC server
-	// rpcPrivKey, rpcPubKey, err := generateEd25519Keys()
-	rpcPubKey, rpcPrivKey, err := ed25519.GenerateKey(rand.Reader)
-	require.NoError(t, err)
-
-	settings := map[string]interface{}{
-		"image": map[string]interface{}{
-			"repository": os.Getenv("MERCURY_SERVER_IMAGE"),
-			"tag":        os.Getenv("MERCURY_SERVER_TAG"),
-		},
-		"postgresql": map[string]interface{}{
-			"enabled": true,
-		},
-		"qa": map[string]interface{}{
-			"rpcPrivateKey": hex.EncodeToString(rpcPrivKey),
-			"enabled":       true,
-		},
-		"rpcNodesConf": string(rpcNodesJsonConf),
-		"prometheus":   "true",
-	}
-
-	if dbSettings != nil {
-		settings["db"] = dbSettings
-	}
-	if serverSettings != nil {
-		settings["resources"] = serverSettings
-	}
-
-	testEnv.AddHelm(mercury_server.New(settings)).Run()
-
-	return rpcPubKey
 }
 
 func SetupMercuryEnv(t *testing.T, dbSettings map[string]interface{}, serverResources map[string]interface{}) (
@@ -245,7 +133,7 @@ func SetupMercuryEnv(t *testing.T, dbSettings map[string]interface{}, serverReso
 	err := testEnvironment.Run()
 	require.NoError(t, err, "Error running test environment")
 
-	msRpcPubKey := setupMercuryServer(t, testEnvironment, dbSettings, serverResources)
+	msRpcPubKey := mercuryserversetup.SetupMercuryServer(t, testEnvironment, dbSettings, serverResources)
 
 	chainlinkNodes, err := client.ConnectChainlinkNodes(testEnvironment)
 	require.NoError(t, err, "Error connecting to Chainlink nodes")
@@ -275,31 +163,6 @@ func SetupMercuryEnv(t *testing.T, dbSettings map[string]interface{}, serverReso
 
 	return testEnvironment, isExistingTestEnv, testNetwork, chainlinkNodes,
 		mercuryServerRemoteUrl, evmClient, mockserverClient, mercuryServerClient, msRpcPubKey
-}
-
-func SetupMercuryContracts(t *testing.T, evmClient blockchain.EVMClient, mercuryRemoteUrl string, feedId [32]byte, ocrConfig contracts.MercuryOCRConfig) (contracts.Verifier, contracts.VerifierProxy, contracts.ReadAccessController, contracts.Exchanger) {
-	contractDeployer, err := contracts.NewContractDeployer(evmClient)
-	require.NoError(t, err, "Deploying contracts shouldn't fail")
-
-	accessController, err := contractDeployer.DeployReadAccessController()
-	require.NoError(t, err, "Error deploying ReadAccessController contract")
-
-	// verifierProxy, err := contractDeployer.DeployVerifierProxy(accessController.Address())
-	// Use zero address for access controller disables access control
-	verifierProxy, err := contractDeployer.DeployVerifierProxy("0x0")
-	require.NoError(t, err, "Error deploying VerifierProxy contract")
-
-	verifier, err := contractDeployer.DeployVerifier(verifierProxy.Address())
-	require.NoError(t, err, "Error deploying Verifier contract")
-
-	latestConfigDetails, err := verifier.LatestConfigDetails(feedId)
-	require.NoError(t, err, "Error getting Verifier.LatestConfigDetails()")
-	log.Info().Msgf("Latest config digest: %x", latestConfigDetails.ConfigDigest)
-	log.Info().Msgf("Latest config details: %v", latestConfigDetails)
-
-	verifierProxy.InitializeVerifier(latestConfigDetails.ConfigDigest, verifier.Address())
-
-	return verifier, verifierProxy, accessController, nil
 }
 
 func SetupMercuryNodeJobs(
