009bdbd6312b9b464629d0a05e5ae37492a9850b
Sam Hassan <es.hassan187@gmail.com>
2023-03-16 12:34:13 +0100

    name testlogger

--- core/logger/test_logger.go
+++ core/logger/test_logger.go
@@ -42,5 +42,5 @@ func testLogger(tb testing.TB, core zapcore.Core) SugaredLogger {
 		level:         a,
 		SugaredLogger: zaptest.NewLogger(tb, opts...).Sugar(),
 	}
-	return Sugared(l)
+	return Sugared(l.Named("TestLogger"))
 }
