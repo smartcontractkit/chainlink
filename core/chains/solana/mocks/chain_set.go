afc0c882c31bd8f30672463095f68d4ec3a3f5bd
Essam A. Hassan <es.hassan187@gmail.com>
2023-03-20 14:36:15 +0100

    Remove references for healthy calls in relayer ifaces (#8755)

--- core/chains/solana/mocks/chain_set.go
+++ core/chains/solana/mocks/chain_set.go
@@ -70,20 +70,6 @@ func (_m *ChainSet) HealthReport() map[string]error {
 	return r0
 }
 
-// Healthy provides a mock function with given fields:
-func (_m *ChainSet) Healthy() error {
-	ret := _m.Called()
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func() error); ok {
-		r0 = rf()
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
 // Name provides a mock function with given fields:
 func (_m *ChainSet) Name() string {
 	ret := _m.Called()
