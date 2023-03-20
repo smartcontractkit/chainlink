0e55337563f4075a580f95592ed8e59c1c345205
Aaron Lu <50029043+aalu1418@users.noreply.github.com>
2023-03-20 14:33:33 -0600

    BCI856: extract gas estimator factory logic from core txm logic (#8658)

--- core/chains/evm/evm_txm.go
+++ core/chains/evm/evm_txm.go
@@ -7,6 +7,8 @@ import (
 
 	evmclient "github.com/smartcontractkit/chainlink/core/chains/evm/client"
 	evmconfig "github.com/smartcontractkit/chainlink/core/chains/evm/config"
+	"github.com/smartcontractkit/chainlink/core/chains/evm/forwarders"
+	"github.com/smartcontractkit/chainlink/core/chains/evm/gas"
 	"github.com/smartcontractkit/chainlink/core/chains/evm/logpoller"
 	"github.com/smartcontractkit/chainlink/core/chains/evm/txmgr"
 	"github.com/smartcontractkit/chainlink/core/logger"
@@ -25,8 +27,28 @@ func newEvmTxm(
 	if !cfg.EVMRPCEnabled() {
 		txm = &txmgr.NullTxManager{ErrMsg: fmt.Sprintf("Ethereum is disabled for chain %d", chainID)}
 	} else if opts.GenTxManager == nil {
+		lggr = lggr.Named("Txm")
+		lggr.Infow("Initializing EVM transaction manager",
+			"gasBumpTxDepth", cfg.EvmGasBumpTxDepth(),
+			"maxInFlightTransactions", cfg.EvmMaxInFlightTransactions(),
+			"maxQueuedTransactions", cfg.EvmMaxQueuedTransactions(),
+			"nonceAutoSync", cfg.EvmNonceAutoSync(),
+			"gasLimitDefault", cfg.EvmGasLimitDefault(),
+		)
+
+		// build estimator from factory
+		estimator := gas.NewEstimator(lggr, client, cfg)
+
+		// build forwarder manager for evm txm
+		var fwdMgr *forwarders.FwdMgr
+		if cfg.EvmUseForwarders() {
+			fwdMgr = forwarders.NewFwdMgr(db, client, logPoller, lggr, cfg)
+		} else {
+			lggr.Info("EvmForwarderManager: Disabled")
+		}
+
 		checker := &txmgr.CheckerFactory{Client: client}
-		txm = txmgr.NewTxm(db, client, cfg, opts.KeyStore, opts.EventBroadcaster, lggr, checker, logPoller)
+		txm = txmgr.NewTxm(db, client, cfg, opts.KeyStore, opts.EventBroadcaster, lggr, checker, estimator, fwdMgr)
 	} else {
 		txm = opts.GenTxManager(chainID)
 	}
