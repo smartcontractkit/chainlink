421f2d9cdd10021b791b670ecb7f102879905876
Austin Born <austinborn212@gmail.com>
2023-03-20 11:31:37 -0700

    Update Verifier contract versions to 0.8.16

--- contracts/src/v0.8/VerifierProxy.sol
+++ contracts/src/v0.8/VerifierProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.6;
+pragma solidity 0.8.16;
 
 import {ConfirmedOwner} from "./ConfirmedOwner.sol";
 import {IVerifierProxy} from "./interfaces/IVerifierProxy.sol";
--- contracts/src/v0.8/interfaces/IVerifierProxy.sol
+++ contracts/src/v0.8/interfaces/IVerifierProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.6;
+pragma solidity 0.8.16;
 
 interface IVerifierProxy {
     /**
