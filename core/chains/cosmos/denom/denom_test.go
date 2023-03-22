61c76a335427ee23877dc7b79c0dcb17d87cfe30
cfal <cfal@users.noreply.github.com>
2023-03-22 10:57:10 +0900

    Add Cosmos chain (#8567)
    * readd terra files
    
    * unrevert unrelated files
    
    * replace terra-money/core wasmtypes dependency
    
    * rename to cosmos
    
    * remove terra.go references
    
    * dont include libwasvm
    
    * luna to atom
    
    * add new Name and HealthReport functions to cosmos Chain
    
    * remove install_terrad script
    
    * remove balance monitor
    
    * add Name and HealthReport function to txm
    
    * fix wasmtypes change, set valid cosmos addresses in txm tests
    
    * update cosmos key test, remove old cosmos key, keep marshal/unmarshal testing
    
    * update CODEOWNERS for core/chains/cosmos
    
    * cosmosd references to wasmd
    
    * update testnet names
    
    * cosmos_transfer_controller: remove error switch statement to satisfy lint
    
    * rename to chainlink-cosmos repo
    
    * regenerate docs
    
    * change cosmos addresses to wasm prefix
    
    * copy libwasmvm libs
    
    * cleanup README, remove LIBWASMVM_ARCH blurb
    
    * remove cosmos FCDGasPriceEstimator
    
    * add cosmos migration
    
    * refer to interfaces in ChainSet
    
    * remove go-keychain replace directive, chainlink-terra points to 99design
    
    * remove legacy config
    
    * update mocks
    
    * adapters.Config -> config.Config
    
    * cosmos/types: remove EnabledChains
    
    * cosmos/denom: rename convert functions
    
    * cosmos/cosmostxm: update comment
    
    * migrations: remove cosmos_chains, cosmos_nodes from cosmos migration
    
    * cosmos/chain_set: remove NewChain
    
    * cosmostest: remove MustInsertChain
    
    * cosmos/orm: remove NewORM
    
    * cmd/client: don't EnsureChains for cosmos
    
    * cosmostxm: remove MustInsertChain and EnsureChain invocations in tests, use cosmostest.RandomChainID
    
    * internal/cltest: remove cosmos EnsureChains invocation
    
    * migrations: update cosmos migration counter
    
    * services/chainlink: remove COSMOS_ENABLED and COSMOS_NODES from legacy.env
    
    * solana/monitor: fix comment referring to luna
    
    * web/solana_transfer_controller: fix comment referring to luna
    
    * cosmos/chain: remove dial client TODO
    
    * cosmos/types: embed ChainsORM and NodesORM in ORM
    
    * cosmos/chain: remove cfgImmutable and UpdateConfig function
    
    * cosmos/chain: use crypto/rand to generate node index
    
    * cosmos/config: remove Config.Update implementation
    
    * use UUID for random cosmos chain ID
    
    * keystore/ocr2key: remove cosmos from key migration
    
    * core/web: remove cosmos chain and node controllers
    
    * Revert "core/web: remove cosmos chain and node controllers"
    
    This reverts commit 11edb627ed5a28b25481649332e091eb4dbed073.
    
    * don't skip tests, switch to wasmd build tag
    
    * add TODO in related integration tests
    
    * add comment on meaning of FCD
    
    * fix build after legacy cleanup
    
    * cosmos/chain: inline Healthy into HealthReport
    
    * presenters/cosmos_chain: set JAID for CosmosNodeResource from node name
    
    * update comments
    
    * rename chain interfaces
    
    * fix legacy change conflicts for CosmosChainClient and CosmosChainResource
    
    * core/scripts/go.mod: remove unused go-keychain replace directive, run make gomodtidy
    
    ---------
    
    Co-authored-by: Blaž Hrastnik <blaz@mxxn.io>

---
+++ core/chains/cosmos/denom/denom_test.go
@@ -0,0 +1,29 @@
+package denom
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCoinToAtom(t *testing.T) {
+	tests := []struct {
+		coin types.Coin
+		exp  string
+	}{
+		{types.NewInt64Coin("uatom", 1), "0.000001000000000000atom"},
+		{types.NewInt64Coin("uatom", 0), "0.000000000000000000atom"},
+		{types.NewInt64Coin("atom", 1), "1.000000000000000000atom"},
+		{types.NewInt64Coin("uatom", 1000000), "1.000000000000000000atom"},
+		{types.NewInt64Coin("matom", 1000000), "1000.000000000000000000atom"},
+		{types.NewInt64Coin("matom", 123456789), "123456.789000000000000000atom"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.coin.String(), func(t *testing.T) {
+			got, err := CoinToAtom(tt.coin)
+			require.NoError(t, err)
+			require.Equal(t, tt.exp, got.String())
+		})
+	}
+}
