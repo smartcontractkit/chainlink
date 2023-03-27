c2b8c4ba176951bacc48083ff18eeae5fd63c408
skudasov <f4hrenh9it@gmail.com>
2023-03-27 14:29:06 +0200

    retrigger

--- integration-tests/load/mercury_test.go
+++ integration-tests/load/mercury_test.go
@@ -65,6 +65,7 @@ var (
 )
 
 func TestMercuryHTTPLoad(t *testing.T) {
+
 	feeds := mercuryactions.GenFeedIds(9)
 	testEnv, _, err := mercury.SetupMultiFeedSingleVerifierEnv(t.Name(), "load", feeds, resources)
 	require.NoError(t, err)
