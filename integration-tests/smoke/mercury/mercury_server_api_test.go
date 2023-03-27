a189cf61b9dcdc872cd505af90084d68e59d3e7c
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2023-03-27 15:56:01 +0200

    skip /admin tests
    https://github.com/smartcontractkit/mercury-server/pull/79

--- integration-tests/smoke/mercury/mercury_server_api_test.go
+++ integration-tests/smoke/mercury/mercury_server_api_test.go
@@ -96,6 +96,7 @@ func TestMercuryServerAPI(t *testing.T) {
 	})
 
 	t.Run("GET /admin/user as admin role", func(t *testing.T) {
+		t.Skip()
 		c := client.NewMercuryServerClient(msUrl, admin.Id, admin.Key)
 		users, resp, err := c.GetUsers()
 		require.NoError(t, err)
@@ -104,6 +105,7 @@ func TestMercuryServerAPI(t *testing.T) {
 	})
 
 	t.Run("GET /admin/user as user role", func(t *testing.T) {
+		t.Skip()
 		c := client.NewMercuryServerClient(msUrl, user.Id, user.Key)
 		users, resp, err := c.GetUsers()
 		require.NoError(t, err)
