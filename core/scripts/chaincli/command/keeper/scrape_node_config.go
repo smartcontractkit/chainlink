7330c35319ba1b3e69f5d04fdea861cb4314ba65
FelixFan1992 <fankejin@gmail.com>
2023-03-29 12:08:36 -0400

    fixes

--- core/scripts/chaincli/command/keeper/scrape_node_config.go
+++ core/scripts/chaincli/command/keeper/scrape_node_config.go
@@ -10,8 +10,8 @@ import (
 // scrapeNodes represents the command to run the service
 var scrapeNodes = &cobra.Command{
 	Use:   "scrape-node-config",
-	Short: "ScrapeNodes automation OCR2 node configs",
-	Long:  `This command scrape OCR2 node configs`,
+	Short: "Scrape OCR2 node configs",
+	Long:  `This command scrape OCR2 node configs. Users need to provide node URLs, emails, passwords, node URL etc as env vars.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := config.New()
 		hdlr := handler.NewBaseHandler(cfg)
