c6437decafc4672852419d305a381335bd3d0948
Sam Hassan <es.hassan187@gmail.com>
2023-03-30 21:15:13 +0200

    implement insecure configs

--- core/services/feeds/mocks/config.go
+++ core/services/feeds/mocks/config.go
@@ -262,6 +262,20 @@ func (_m *Config) OCR2TraceLogging() bool {
 	return r0
 }
 
+// OCRDevelopmentMode provides a mock function with given fields:
+func (_m *Config) OCRDevelopmentMode() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 type mockConstructorTestingTNewConfig interface {
 	mock.TestingT
 	Cleanup(func())
