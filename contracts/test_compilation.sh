eb9bbcb47f04ba16449cd8a7ffcc0e52276443fb
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2023-04-05 17:05:37 -0400

    Number log files by iteration

--- contracts/test_compilation.sh
+++ contracts/test_compilation.sh
@@ -8,7 +8,7 @@ while true; do
   rm -rf ~/.cache/hardhat-nodejs/
   pnpm i >/dev/null 2>&1
   pnpm link ~/src/cl/hardhat/packages/hardhat-core >/dev/null 2>&1
-  pnpm compile >logs.log 2>&1
+  pnpm compile >logs-iter-$count.log 2>&1
   exit_status=$?
   if [ $exit_status -ne 0 ]; then
     echo "Last command returned a non-zero exit status: $exit_status"
