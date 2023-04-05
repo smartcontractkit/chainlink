65e6f58b85519a421d007dc6b1739acb0ba341b5
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2023-04-05 17:06:52 -0400

    Add file diff support

--- contracts/test_compilation.sh
+++ contracts/test_compilation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+prev_output_file=$(mktemp)
+next_output_file=$(mktemp)
+
 count=0
 while true; do
   ((count++))
@@ -14,4 +17,24 @@ while true; do
     echo "Last command returned a non-zero exit status: $exit_status"
     break
   fi
+done
+
+function cleanup {
+  rm -f "$prev_output_file" "$next_output_file"
+}
+
+trap cleanup EXIT
+
+function collect_and_diff {
+  find ~/.cache/hardhat-nodejs/ -type f -exec sh -c 'file="{}"; size=$(stat -c "%s" "$file"); perm=$(stat -c "%A" "$file"); name=$(basename "$file"); timestamp=$(stat -c "%y" "$file"); printf "%-12s\t%-12s\t%-40s\t%-32s\n" "$size" "$perm" "$name" "$timestamp"' \; >"$next_output_file"
+  diff --color=always --unified=0 --suppress-common-lines "$prev_output_file" "$next_output_file" | tail -n +3
+  mv "$next_output_file" "$prev_output_file"
+}
+
+# Run once initially to show the current state
+collect_and_diff
+
+# Watch for events in the directory
+while inotifywait -q -r -e modify,create,delete ~/.cache/hardhat-nodejs/; do
+  collect_and_diff
 done
