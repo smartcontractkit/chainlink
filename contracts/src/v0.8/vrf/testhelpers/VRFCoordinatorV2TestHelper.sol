692022c9e6279795413bb876a24e4768325fa65a
jinhoonbang <jin.bang@smartcontract.com>
2023-04-07 15:56:18 -0700

    remove VRFCoordinatorV2 dependency from VRFCoordinatorV2TestHelper.sol

--- contracts/src/v0.8/vrf/testhelpers/VRFCoordinatorV2TestHelper.sol
+++ contracts/src/v0.8/vrf/testhelpers/VRFCoordinatorV2TestHelper.sol
@@ -1,20 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "../../interfaces/AggregatorV3Interface.sol";
 import "../VRFCoordinatorV2.sol";
 
-contract VRFCoordinatorV2TestHelper is VRFCoordinatorV2 {
+contract VRFCoordinatorV2TestHelper {
   uint96 s_paymentAmount;
 
+  AggregatorV3Interface public immutable LINK_ETH_FEED;
+
+  struct Config {
+    uint16 minimumRequestConfirmations;
+    uint32 maxGasLimit;
+    // Reentrancy protection.
+    bool reentrancyLock;
+    // stalenessSeconds is how long before we consider the feed price to be stale
+    // and fallback to fallbackWeiPerUnitLink.
+    uint32 stalenessSeconds;
+    // Gas to cover oracle payment after we calculate the payment.
+    // We make it configurable in case those operations are repriced.
+    uint32 gasAfterPaymentCalculation;
+  }
+  int256 private s_fallbackWeiPerUnitLink;
+  Config private s_config;
+
   constructor(
-    address link,
-    address blockhashStore,
     address linkEthFeed
   )
     // solhint-disable-next-line no-empty-blocks
-    VRFCoordinatorV2(link, blockhashStore, linkEthFeed)
   {
-    /* empty */
+    LINK_ETH_FEED = AggregatorV3Interface(linkEthFeed);
   }
 
   function calculatePaymentAmountTest(
@@ -22,12 +37,36 @@ contract VRFCoordinatorV2TestHelper is VRFCoordinatorV2 {
     uint32 fulfillmentFlatFeeLinkPPM,
     uint256 weiPerUnitGas
   ) external {
-    s_paymentAmount = calculatePaymentAmount(
-      gasleft(),
-      gasAfterPaymentCalculation,
-      fulfillmentFlatFeeLinkPPM,
-      weiPerUnitGas
-    );
+    uint256 startGas = gasleft();
+    int256 weiPerUnitLink;
+    weiPerUnitLink = getFeedData();
+    if (weiPerUnitLink <= 0) {
+      revert InvalidLinkWeiPrice(weiPerUnitLink);
+    }
+    // (1e18 juels/link) (wei/gas * gas) / (wei/link) = juels
+    uint256 paymentNoFee = (1e18 * weiPerUnitGas * (gasAfterPaymentCalculation + startGas - gasleft())) /
+      uint256(weiPerUnitLink);
+    uint256 fee = 1e12 * uint256(fulfillmentFlatFeeLinkPPM);
+    if (paymentNoFee > (1e27 - fee)) {
+      revert PaymentTooLarge(); // Payment + fee cannot be more than all of the link in existence.
+    }
+    return;
+  }
+
+  error InvalidLinkWeiPrice(int256 linkWei);
+  error PaymentTooLarge();
+
+  function getFeedData() private view returns (int256) {
+    uint32 stalenessSeconds = s_config.stalenessSeconds;
+    bool staleFallback = stalenessSeconds > 0;
+    uint256 timestamp;
+    int256 weiPerUnitLink;
+    (, weiPerUnitLink, , timestamp, ) = LINK_ETH_FEED.latestRoundData();
+    // solhint-disable-next-line not-rely-on-time
+    if (staleFallback && stalenessSeconds < block.timestamp - timestamp) {
+      weiPerUnitLink = s_fallbackWeiPerUnitLink;
+    }
+    return weiPerUnitLink;
   }
 
   function getPaymentAmount() public view returns (uint96) {
