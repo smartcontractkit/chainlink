7dafdd2e041ac2a6dcd85b327b5d75c61f006326
Prashant Yadav <34992934+prashantkumar1982@users.noreply.github.com>
2023-04-11 07:37:29 -0700

    Update core/chains/evm/txmgr/models.go
    as suggested by Jordan
    
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/chains/evm/txmgr/models.go
+++ core/chains/evm/txmgr/models.go
@@ -28,24 +28,26 @@ import (
 )
 
 // Type aliases for EVM
-type EvmConfirmer = EthConfirmer[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmBroadcaster = EthBroadcaster[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmResender = EthResender[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmTxStorageService = txmgrtypes.TxStorageService[*evmtypes.Address, big.Int, *evmtypes.TxHash, *evmtypes.BlockHash, NewTx[*evmtypes.Address], *evmtypes.Receipt, EthTx[*evmtypes.Address, *evmtypes.TxHash], EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash], int64, int64]
-type EvmKeyStore = txmgrtypes.KeyStore[*evmtypes.Address, *big.Int, int64]
-type EvmTxAttemptBuilder = txmgrtypes.TxAttemptBuilder[*evmtypes.Head, gas.EvmFee, *evmtypes.Address, *evmtypes.TxHash, EthTx[*evmtypes.Address, *evmtypes.TxHash], EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash]]
-type EvmNonceSyncer = NonceSyncer[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmTransmitCheckerFactory = TransmitCheckerFactory[*evmtypes.Address, *evmtypes.TxHash]
-type EvmTxm = Txm[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmTxManager = TxManager[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type NullEvmTxManager = NullTxManager[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmFwdMgr = txmgrtypes.ForwarderManager[*evmtypes.Address]
-type EvmNewTx = NewTx[*evmtypes.Address]
-type EvmTx = EthTx[*evmtypes.Address, *evmtypes.TxHash]
-type EvmTxAttempt = EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash]
-type EvmPriorAttempt = txmgrtypes.PriorAttempt[gas.EvmFee, *evmtypes.TxHash]
-type EvmReceipt = txmgrtypes.Receipt[*evmtypes.Receipt, *evmtypes.TxHash, *evmtypes.BlockHash]
-type EvmReceiptPlus = txmgrtypes.ReceiptPlus[*evmtypes.Receipt]
+type (
+ EvmConfirmer = EthConfirmer[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmBroadcaster = EthBroadcaster[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmResender = EthResender[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmTxStorageService = txmgrtypes.TxStorageService[*evmtypes.Address, big.Int, *evmtypes.TxHash, *evmtypes.BlockHash, NewTx[*evmtypes.Address], *evmtypes.Receipt, EthTx[*evmtypes.Address, *evmtypes.TxHash], EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash], int64, int64]
+ EvmKeyStore = txmgrtypes.KeyStore[*evmtypes.Address, *big.Int, int64]
+ EvmTxAttemptBuilder = txmgrtypes.TxAttemptBuilder[*evmtypes.Head, gas.EvmFee, *evmtypes.Address, *evmtypes.TxHash, EthTx[*evmtypes.Address, *evmtypes.TxHash], EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash]]
+ EvmNonceSyncer = NonceSyncer[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmTransmitCheckerFactory = TransmitCheckerFactory[*evmtypes.Address, *evmtypes.TxHash]
+ EvmTxm = Txm[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmTxManager = TxManager[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ NullEvmTxManager = NullTxManager[*evmtypes.Address, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmFwdMgr = txmgrtypes.ForwarderManager[*evmtypes.Address]
+ EvmNewTx = NewTx[*evmtypes.Address]
+ EvmTx = EthTx[*evmtypes.Address, *evmtypes.TxHash]
+ EvmTxAttempt = EthTxAttempt[*evmtypes.Address, *evmtypes.TxHash]
+ EvmPriorAttempt = txmgrtypes.PriorAttempt[gas.EvmFee, *evmtypes.TxHash]
+ EvmReceipt = txmgrtypes.Receipt[*evmtypes.Receipt, *evmtypes.TxHash, *evmtypes.BlockHash]
+ EvmReceiptPlus = txmgrtypes.ReceiptPlus[*evmtypes.Receipt]
+)
 
 // EthTxMeta contains fields of the transaction metadata
 // Not all fields are guaranteed to be present
