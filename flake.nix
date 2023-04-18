edaa1429fa99888852827b3696a57a00e19ae910
André Vitor de Lima Matos <andre.matos@smartcontract.com>
2023-04-18 09:24:59 -0300

    flake: refactor and update with dapp's solc static versions

--- flake.nix
+++ flake.nix
@@ -2,17 +2,32 @@
   description = "Chainlink development shell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dapp.url = "github:dapphub/dapptools";
+    foundry.url = "github:shazow/foundry.nix";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ ]; };
-      in
-      rec {
-        devShell = pkgs.callPackage ./shell.nix { };
-        formatter = pkgs.nixpkgs-fmt;
-      });
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    dapp,
+    foundry,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      inherit (pkgs) lib;
+    in {
+      devShell = pkgs.callPackage ./shell.nix {
+        foundry-bin = foundry.defaultPackage.${system};
+        dapp =
+          dapp.packages.${system}
+          // {
+            solc-static-versions = with lib; filterAttrs (n: v: hasPrefix "solc" n) dapp.packages.${system};
+          };
+      };
+
+      formatter = pkgs.alejandra;
+    });
 }
--- shell.nix
+++ shell.nix
@@ -1,50 +1,66 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
-let
-  go = go_1_19;
-  postgresql = postgresql_14;
-  nodejs = nodejs-16_x;
-  nodePackages = pkgs.nodePackages.override { inherit nodejs; };
-in
-mkShell {
-  nativeBuildInputs = [
-    go
+{
+  pkgs ? import <nixpkgs> {},
+  dapp ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.dapp.locked;
+  in
+    import (fetchTarball {
+      url = "https://github.com/dapphub/dapptools/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    }) {inherit (pkgs) system;},
+  foundry-bin ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.foundry.locked;
+  in
+    import (fetchTarball {
+        url = "https://github.com/shazow/foundry.nix/archive/${lock.rev}.tar.gz";
+        sha256 = lock.narHash;
+      }
+      + "/foundry-bin") {inherit pkgs;},
+}: let
+  go = pkgs.go_1_20;
+  postgresql = pkgs.postgresql_14;
+  nodejs = pkgs.nodejs-16_x;
+  nodePackages = pkgs.nodePackages.override {inherit nodejs;};
 
-    postgresql
-    python3
-    python3Packages.pip
-    curl
-    nodejs
-    nodePackages.pnpm
-    # TODO: compiler / gcc for secp compilation
-    nodePackages.ganache
-    # py3: web3 slither-analyzer crytic-compile
-    # echidna
-    go-ethereum # geth
-    # parity # openethereum
-    go-mockery
-
-    # tooling
-    gotools
-    gopls
-    delve
-    golangci-lint
-    github-cli
-
-    # gofuzz
-  ] ++ lib.optionals stdenv.isLinux [
-    # some dependencies needed for node-gyp on pnpm install
-    pkg-config
-    libudev-zero
-    libusb1
+  solcs = with dapp.solc-static-versions; [
+    solc_0_6_6
+    solc_0_7_6
+    solc_0_8_6
+    solc_0_8_15
+    solc_0_8_16
   ];
-  LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-  GOROOT = "${go}/share/go";
+in
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs;
+      [
+        go
+        postgresql
+        curl
+        cacert
+        which
+        git
 
-  PGDATA = "db";
-  CL_DATABASE_URL = "postgresql://chainlink:chainlink@localhost:5432/chainlink_test?sslmode=disable";
-  shellHook = ''
-    export GOPATH=$HOME/go
-    export PATH=$GOPATH/bin:$PATH
-  '';
-}
+        nodejs
+        nodePackages.pnpm
+        # TODO: compiler / gcc for secp compilation
+        nodePackages.ganache
+        go-ethereum # geth
+
+        # tooling
+        gotools
+        gopls
+        delve
+        golangci-lint
+        go-mockery
+
+        github-cli
+        kubernetes
+
+        # gofuzz
+        foundry-bin
+        lcov
+      ]
+      ++ solcs;
+
+    # env vars
+    CL_DATABASE_URL = "postgresql://chainlink:chainlink@localhost:5432/chainlink_test?sslmode=disable";
+  }
