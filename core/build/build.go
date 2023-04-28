f16661b65a5b2359948c243b9aca8b3e05388be8
Essam A. Hassan <es.hassan187@gmail.com>
2023-04-28 19:08:22 +0200

    Update core/build/build.go
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/build/build.go
+++ core/build/build.go
@@ -3,12 +3,10 @@ package build
 // The build module utilizes build tags to determine the environment that this binary was built to target
 // the currently supported build modes are dev, test. Setting both tags is not allowed and will result to compilation errors.
 
-type Build string
-
 const (
-	Prod Build = "prod"
-	Dev  Build = "dev"
-	Test Build = "test"
+	Prod = "prod"
+	Dev  = "dev"
+	Test = "test"
 )
 
 func DevelopmentBuild() bool {
