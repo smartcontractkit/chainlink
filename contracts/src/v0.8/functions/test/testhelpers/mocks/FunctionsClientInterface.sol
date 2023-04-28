ee4694a96d6222fbe185f18c6e722e1b60c85cb2
Justin Kaseman <justinkaseman@live.com>
2023-04-28 13:05:22 -0700

    1: add new folder with contracts

---
+++ contracts/src/v0.8/functions/test/testhelpers/mocks/FunctionsClientInterface.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+/**
+ * @title Chainlink Functions client interface.
+ */
+interface FunctionsClientInterface {
+  /**
+   * @notice Returns the DON's secp256k1 public key used to encrypt secrets
+   * @dev All Oracles nodes have the corresponding private key
+   * needed to decrypt the secrets encrypted with the public key
+   * @return publicKey DON's public key
+   */
+  function getDONPublicKey() external view returns (bytes memory);
+
+  /**
+   * @notice Chainlink Functions response handler called by the designated transmitter node in an OCR round.
+   * @param requestId The requestId returned by FunctionsClient.sendRequest().
+   * @param response Aggregated response from the user code.
+   * @param err Aggregated error either from the user code or from the execution pipeline.
+   * Either response or error parameter will be set, but never both.
+   */
+  function handleOracleFulfillment(
+    bytes32 requestId,
+    bytes memory response,
+    bytes memory err
+  ) external;
+}
