4c7550a0d1107f757bb7890e7c63411b8df0d1de
Andres M. Julia <andres.julia@smartcontract.com>
2023-05-05 14:21:34 -0300

    Mercury OCR2 metrics

--- core/services/ocr2/plugins/mercury/plugin.go
+++ core/services/ocr2/plugins/mercury/plugin.go
@@ -2,16 +2,17 @@ package mercury
 
 import (
 	"encoding/json"
+	"math/big"
 
 	"github.com/pkg/errors"
 	libocr2 "github.com/smartcontractkit/libocr/offchainreporting2"
 
 	relaymercury "github.com/smartcontractkit/chainlink-relay/pkg/reportingplugins/mercury"
 	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
-
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/job"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/mercury/config"
+	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/promwrapper"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocrcommon"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pipeline"
 	mercury "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury"
@@ -50,13 +51,18 @@ func NewServices(
 		lggr,
 		runResults,
 	)
-	argsNoPlugin.ReportingPluginFactory = relaymercury.NewFactory(
+	wrappedPluginFactory := relaymercury.NewFactory(
 		ds,
 		lggr,
 		ocr2Provider.OnchainConfigCodec(),
 		ocr2Provider.ReportCodec(),
 		ocr2Provider.ContractTransmitter(),
 	)
+	chain, err := jb.OCR2OracleSpec.RelayConfig.EVMChainID()
+	if err != nil {
+		return nil, errors.Wrap(err, "get chainset")
+	}
+	argsNoPlugin.ReportingPluginFactory = promwrapper.NewPromFactory(wrappedPluginFactory, "Mercury", string(jb.OCR2OracleSpec.Relay), big.NewInt(chain))
 	oracle, err := libocr2.NewOracle(argsNoPlugin)
 	if err != nil {
 		return nil, errors.WithStack(err)
