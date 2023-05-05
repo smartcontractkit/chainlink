b929f6c6c0555344bf9ad9de97cbed6c5899a11d
Ryan Hall <RyanRHall@users.noreply.github.com>
2023-05-05 14:18:06 -0400

    skip sol coverage for frozen contracts (#9143)
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

--- contracts/test/v0.8/automation/KeeperRegistry2_0.test.ts
+++ contracts/test/v0.8/automation/KeeperRegistry2_0.test.ts
@@ -36,7 +36,7 @@ const BYTECODE = KeeperRegistryFactory.bytecode
 const BYTECODE_CHECKSUM =
   '0x60660453a335cdcd42b5aa64e58a8c04517e8a8645d2618b51a7552df6e2973b'
 
-describe('KeeperRegistry2_0 - Frozen', () => {
+describe('KeeperRegistry2_0 - Frozen [ @skip-coverage ]', () => {
   it('has not changed', () => {
     assert.equal(ethers.utils.id(BYTECODE), BYTECODE_CHECKSUM)
   })
