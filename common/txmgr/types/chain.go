1300adbbae1cf1794ba168200c679d47415a19a5
aalu1418 <50029043+aalu1418@users.noreply.github.com>
2023-05-09 14:43:04 -0600

    add comment regarding Int64 method on sequence

--- common/txmgr/types/chain.go
+++ common/txmgr/types/chain.go
@@ -6,7 +6,7 @@ import "fmt"
 // It should be convertible to a string
 type Sequence interface {
 	fmt.Stringer
-	Int64() int64
+	Int64() int64 // needed for numeric sequence confirmation - to be removed with confirmation logic generalization: https://smartcontract-it.atlassian.net/browse/BCI-860
 }
 
 // ID represents the base type, for any chain's ID.
