7fd2c65b115c056150ecef779f7494e95fd17832
Yong Kang <chiayongkang@hotmail.com>
2023-05-08 23:21:32 -0700

    fixed tests

--- core/chains/evm/headtracker/head_tracker_test.go
+++ core/chains/evm/headtracker/head_tracker_test.go
@@ -556,7 +556,7 @@ func TestHeadTracker_SwitchesToLongestChainWithHeadSamplingDisabled(t *testing.T
 
 	ethClient := evmtest.NewEthClientMockWithDefaultChain(t)
 
-	checker := txmmocks.NewHeadTrackable[*evmtypes.Head](t)
+	checker := txmmocks.NewHeadTrackable[*evmtypes.Head, gethCommon.Hash](t)
 	orm := headtracker.NewORM(db, logger, config, cltest.FixtureChainID)
 	evmcfg := evmtest.NewChainScopedConfig(t, config)
 	ht := createHeadTrackerWithChecker(t, ethClient, evmcfg, orm, checker)
