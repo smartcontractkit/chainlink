2b9c97e5ee72b3a1e7436f9f1ac27d498ca5785d
Andres M. Julia <andres.julia@smartcontract.com>
2023-05-10 14:46:29 -0300

    renaming metric namespaces

--- core/services/relay/evm/mercury/wsrpc/metrics.go
+++ core/services/relay/evm/mercury/wsrpc/metrics.go
@@ -14,18 +14,18 @@ const (
 
 var (
 	aliveMetric = prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "mercury_client_exporter",
+		Namespace: "mercury",
 		Name:      "wsrpc_connection_alive",
 		Help:      "Total time spent connected to the WSRPC server",
 	})
 	requestsStatusMetric = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "mercury_client_exporter",
+		Namespace: "mercury",
 		Name:      "wsrpc_requests_status_count",
 		Help:      "Number of request status made to the WSRPC server",
 	}, []string{"status"})
 
 	requestLatencyMetric = prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "mercury_client_exporter",
+		Namespace: "mercury",
 		Name:      "wsrpc_request_latency",
 		Help:      "Latency of requests made to the WSRPC server",
 	})
