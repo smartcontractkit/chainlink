4f9ee246e4a65c940983622659b472f3f7901286
Yong Kang <chiayongkang@hotmail.com>
2023-05-10 14:08:55 -0700

    changed HASH to BLOCK_HASH

--- common/headtracker/types/heads.go
+++ common/headtracker/types/heads.go
@@ -5,11 +5,11 @@ import (
 )
 
 // Heads is a collection of heads. All methods are thread-safe.
-type Heads[H types.Head[HASH], HASH types.Hashable] interface {
+type Heads[H types.Head[BLOCK_HASH], BLOCK_HASH types.Hashable] interface {
 	// LatestHead returns the block header with the highest number that has been seen, or nil.
 	LatestHead() H
 	// HeadByHash returns a head for the specified hash, or nil.
-	HeadByHash(hash HASH) H
+	HeadByHash(hash BLOCK_HASH) H
 	// AddHeads adds newHeads to the collection, eliminates duplicates,
 	// sorts by head number, fixes parents and cuts off old heads (historyDepth).
 	AddHeads(historyDepth uint, newHeads ...H)
