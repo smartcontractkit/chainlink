5632fd2d960187365fb2fb7e664b62b1f96c3763
Yong Kang <chiayongkang@hotmail.com>
2023-05-19 12:18:10 -0700

    update client interface

--- common/headtracker/types/client.go
+++ common/headtracker/types/client.go
@@ -11,8 +11,8 @@ import (
 type Client[H commontypes.Head[BLOCK_HASH], S Subscription, ID txmgrtypes.ID, BLOCK_HASH commontypes.Hashable] interface {
 	HeadByNumber(ctx context.Context, number *big.Int) (head H, err error)
 	HeadByHash(ctx context.Context, hash BLOCK_HASH) (head H, err error)
-	ChainID() (id ID, err error)
-
+	// ConfiguredChainID returns the chain ID that the node is configured to connect to
+	ConfiguredChainID() (id ID)
 	// SubscribeNewHead is the method in which the client receives new Head.
 	// It can be implemented differently for each chain i.e websocket, polling, etc
 	SubscribeNewHead(ctx context.Context, ch chan<- H) (S, error)
