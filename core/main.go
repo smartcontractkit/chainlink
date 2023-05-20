a1815b376ad214a65a84a81162a0ca0fb03afbdf
jmank88 <jmank88@gmail.com>
2023-05-20 07:12:36 -0500

    ignore error

--- core/main.go
+++ core/main.go
@@ -28,10 +28,7 @@ func init() {
 func Main() (code int) {
 	recovery.ReportPanics(func() {
 		app := cmd.NewApp(newProductionClient())
-		if err := app.Run(os.Args); err != nil {
-			fmt.Fprintf(os.Stderr, "Error running app: %v\n", err)
-			code = 1
-		}
+		app.Run(os.Args)
 	})
 	return
 }
