72eb5b22bef8cb722d9a046ebefc35d890442618
Yong Kang <chiayongkang@hotmail.com>
2023-05-22 16:09:57 -0700

    Refactored functions in Head to fit HeadListener

--- core/chains/evm/headtracker/head_listener.go
+++ core/chains/evm/headtracker/head_listener.go
@@ -33,7 +33,7 @@ var (
 )
 
 type headListener[
-	H commontypes.Head[BLOCK_HASH],
+	H htrktypes.Head[BLOCK_HASH, ID],
 	S commontypes.Subscription,
 	ID txmgrtypes.ID,
 	BLOCK_HASH commontypes.Hashable,
@@ -141,8 +141,10 @@ func (hl *headListener[H, S, ID, BLOCK_HASH, CLIENT]) receiveHeaders(ctx context
 				hl.logger.Error("got nil block header")
 				continue
 			}
-			if blockHeader.EVMChainID == nil || !utils.NewBig(chainId).Equal(blockHeader.EVMChainID) {
-				hl.logger.Panicf("head listener for %s received block header for %s", chainId, blockHeader.EVMChainID)
+
+			// Compare the chain ID of the block header to the chain ID of the client
+			if blockHeader.IsChainIdNil() || !blockHeader.IsSameChain(chainId) {
+				hl.logger.Panicf("head listener for %s received block header for %s", chainId, blockHeader.ChainId())
 			}
 			promNumHeadsReceived.WithLabelValues(chainId.String()).Inc()
 
