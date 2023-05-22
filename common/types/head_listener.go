47c11daa0be96696798845d89bc3ff6046f7faf9
Yong Kang <chiayongkang@hotmail.com>
2023-05-22 10:51:39 -0700

    moved HeadListner to common/types

--- common/types/head_listener.go
+++ common/types/head_listener.go
@@ -2,18 +2,15 @@ package types
 
 import "context"
 
-// HeadListener is a chain agnostic interface that manages connection of Client that receives heads from the blockchain node
+// HeadListener manages evmclient.Client connection that receives heads from the eth node
 type HeadListener[H Head[BLOCK_HASH], BLOCK_HASH Hashable] interface {
 	// ListenForNewHeads kicks off the listen loop (not thread safe)
 	// done() must be executed upon leaving ListenForNewHeads()
 	ListenForNewHeads(handleNewHead NewHeadHandler[H, BLOCK_HASH], done func())
-
 	// ReceivingHeads returns true if the listener is receiving heads (thread safe)
 	ReceivingHeads() bool
-
 	// Connected returns true if the listener is connected (thread safe)
 	Connected() bool
-
 	// HealthReport returns report of errors within HeadListener
 	HealthReport() map[string]error
 }
