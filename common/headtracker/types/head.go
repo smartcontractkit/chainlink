70858b7fbc6fe9834f293bd2dd856cb5cdc172e2
Yong Kang <chiayongkang@hotmail.com>
2023-05-22 16:35:09 -0700

    nit changes

--- common/headtracker/types/head.go
+++ common/headtracker/types/head.go
@@ -7,13 +7,12 @@ import (
 
 type Head[BLOCK_HASH types.Hashable, CHAIN_ID txmgrtypes.ID] interface {
 	types.Head[BLOCK_HASH]
-
+	// Equals returns true if the two heads are equal
+	Equals(Head[BLOCK_HASH, CHAIN_ID]) bool
 	// ChainID returns the chain ID that the head is for
 	ChainId() CHAIN_ID
-
-	// Check if ChainId is nil
+	// Check if the chain ID of the Head is nil
 	IsChainIdNil() bool
-
 	// Check if the two heads are of the same chain
 	IsSameChain(CHAIN_ID) bool
 }
