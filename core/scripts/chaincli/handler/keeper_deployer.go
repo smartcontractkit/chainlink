592f3d0f4ec8b27d08164172e3438028491e7ed8
Ryan Hall <hall.ryan.r@gmail.com>
2023-05-23 16:59:22 -0400

    update chaincli to support registry 2.1

--- core/scripts/chaincli/handler/keeper_deployer.go
+++ core/scripts/chaincli/handler/keeper_deployer.go
@@ -20,6 +20,7 @@ import (
 	"github.com/smartcontractkit/chainlink/core/scripts/chaincli/config"
 
 	"github.com/smartcontractkit/chainlink/v2/core/cmd"
+	iregistry21 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/i_keeper_registry_master_wrapper_2_1"
 	registry11 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/keeper_registry_wrapper1_1"
 	registry12 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/keeper_registry_wrapper1_2"
 	registry20 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/keeper_registry_wrapper2_0"
@@ -34,7 +35,7 @@ type canceller interface {
 
 // upkeepDeployer contains functions needed to deploy an upkeep
 type upkeepDeployer interface {
-	RegisterUpkeep(opts *bind.TransactOpts, target common.Address, gasLimit uint32, admin common.Address, checkData []byte) (*types.Transaction, error)
+	RegisterUpkeep(opts *bind.TransactOpts, target common.Address, gasLimit uint32, admin common.Address, checkData []byte, offchainConfig []byte) (*types.Transaction, error)
 	AddFunds(opts *bind.TransactOpts, id *big.Int, amount *big.Int) (*types.Transaction, error)
 }
 
@@ -53,6 +54,10 @@ func (d *v11KeeperDeployer) SetKeepers(opts *bind.TransactOpts, _ []cmd.HTTPClie
 	return d.KeeperRegistryInterface.SetKeepers(opts, keepers, payees)
 }
 
+func (d *v11KeeperDeployer) RegisterUpkeep(opts *bind.TransactOpts, target common.Address, gasLimit uint32, admin common.Address, checkData []byte, offchainConfig []byte) (*types.Transaction, error) {
+	return d.KeeperRegistryInterface.RegisterUpkeep(opts, target, gasLimit, admin, checkData)
+}
+
 type v12KeeperDeployer struct {
 	registry12.KeeperRegistryInterface
 }
@@ -61,13 +66,13 @@ func (d *v12KeeperDeployer) SetKeepers(opts *bind.TransactOpts, _ []cmd.HTTPClie
 	return d.KeeperRegistryInterface.SetKeepers(opts, keepers, payees)
 }
 
+func (d *v12KeeperDeployer) RegisterUpkeep(opts *bind.TransactOpts, target common.Address, gasLimit uint32, admin common.Address, checkData []byte, offchainConfig []byte) (*types.Transaction, error) {
+	return d.KeeperRegistryInterface.RegisterUpkeep(opts, target, gasLimit, admin, checkData)
+}
+
 type v20KeeperDeployer struct {
 	registry20.KeeperRegistryInterface
 	cfg *config.Config
-}
-
-func (d *v20KeeperDeployer) RegisterUpkeep(opts *bind.TransactOpts, target common.Address, gasLimit uint32, admin common.Address, checkData []byte) (*types.Transaction, error) {
-	return d.KeeperRegistryInterface.RegisterUpkeep(opts, target, gasLimit, admin, checkData, nil)
 }
 
 func (d *v20KeeperDeployer) SetKeepers(opts *bind.TransactOpts, cls []cmd.HTTPClient, keepers []common.Address, _ []common.Address) (*types.Transaction, error) {
@@ -195,4 +200,135 @@ func (d *v20KeeperDeployer) SetKeepers(opts *bind.TransactOpts, cls []cmd.HTTPCl
 	}
 
 	return d.KeeperRegistryInterface.SetConfig(opts, signers, transmitters, f, onchainConfig, offchainConfigVersion, offchainConfig)
+}
+
+type v21KeeperDeployer struct {
+	iregistry21.IKeeperRegistryMasterInterface
+	cfg *config.Config
+}
+
+func (d *v21KeeperDeployer) SetKeepers(opts *bind.TransactOpts, cls []cmd.HTTPClient, keepers []common.Address, _ []common.Address) (*types.Transaction, error) {
+	S := make([]int, len(cls))
+	oracleIdentities := make([]ocr2config.OracleIdentityExtra, len(cls))
+	sharedSecretEncryptionPublicKeys := make([]ocr2types.ConfigEncryptionPublicKey, len(cls))
+	var wg sync.WaitGroup
+	for i, cl := range cls {
+		wg.Add(1)
+		go func(i int, cl cmd.HTTPClient) {
+			defer wg.Done()
+
+			ocr2Config, err := getNodeOCR2Config(cl)
+			if err != nil {
+				panic(err)
+			}
+
+			p2pKeyID, err := getP2PKeyID(cl)
+			if err != nil {
+				panic(err)
+			}
+
+			offchainPkBytes, err := hex.DecodeString(strings.TrimPrefix(ocr2Config.OffChainPublicKey, "ocr2off_evm_"))
+			if err != nil {
+				panic(fmt.Errorf("failed to decode %s: %v", ocr2Config.OffChainPublicKey, err))
+			}
+
+			offchainPkBytesFixed := [ed25519.PublicKeySize]byte{}
+			n := copy(offchainPkBytesFixed[:], offchainPkBytes)
+			if n != ed25519.PublicKeySize {
+				panic(fmt.Errorf("wrong num elements copied"))
+			}
+
+			configPkBytes, err := hex.DecodeString(strings.TrimPrefix(ocr2Config.ConfigPublicKey, "ocr2cfg_evm_"))
+			if err != nil {
+				panic(fmt.Errorf("failed to decode %s: %v", ocr2Config.ConfigPublicKey, err))
+			}
+
+			configPkBytesFixed := [ed25519.PublicKeySize]byte{}
+			n = copy(configPkBytesFixed[:], configPkBytes)
+			if n != ed25519.PublicKeySize {
+				panic(fmt.Errorf("wrong num elements copied"))
+			}
+
+			onchainPkBytes, err := hex.DecodeString(strings.TrimPrefix(ocr2Config.OnchainPublicKey, "ocr2on_evm_"))
+			if err != nil {
+				panic(fmt.Errorf("failed to decode %s: %v", ocr2Config.OnchainPublicKey, err))
+			}
+
+			sharedSecretEncryptionPublicKeys[i] = configPkBytesFixed
+			oracleIdentities[i] = ocr2config.OracleIdentityExtra{
+				OracleIdentity: ocr2config.OracleIdentity{
+					OnchainPublicKey:  onchainPkBytes,
+					OffchainPublicKey: offchainPkBytesFixed,
+					PeerID:            p2pKeyID,
+					TransmitAccount:   ocr2types.Account(keepers[i].String()),
+				},
+				ConfigEncryptionPublicKey: configPkBytesFixed,
+			}
+			S[i] = 1
+		}(i, cl)
+	}
+	wg.Wait()
+
+	signerOnchainPublicKeys, transmitterAccounts, f, _, offchainConfigVersion, offchainConfig, err := ocr2config.ContractSetConfigArgsForTests(
+		5*time.Second,         // deltaProgress time.Duration,
+		10*time.Second,        // deltaResend time.Duration,
+		2500*time.Millisecond, // deltaRound time.Duration,
+		40*time.Millisecond,   // deltaGrace time.Duration,
+		30*time.Second,        // deltaStage time.Duration,
+		50,                    // rMax uint8,
+		S,                     // s []int,
+		oracleIdentities,      // oracles []OracleIdentityExtra,
+		ocr2keepers.OffchainConfig{
+			PerformLockoutWindow: 100 * 3 * 1000, // ~100 block lockout (on mumbai)
+			MinConfirmations:     1,
+		}.Encode(), // reportingPluginConfig []byte,
+		20*time.Millisecond,   // maxDurationQuery time.Duration,
+		1600*time.Millisecond, // maxDurationObservation time.Duration,
+		800*time.Millisecond,  // maxDurationReport time.Duration,
+		20*time.Millisecond,   // maxDurationShouldAcceptFinalizedReport time.Duration,
+		20*time.Millisecond,   // maxDurationShouldTransmitAcceptedReport time.Duration,
+		1,                     // f int,
+		nil,                   // onchainConfig []byte,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var signers []common.Address
+	for _, signer := range signerOnchainPublicKeys {
+		if len(signer) != 20 {
+			return nil, fmt.Errorf("OnChainPublicKey has wrong length for address")
+		}
+		signers = append(signers, common.BytesToAddress(signer))
+	}
+
+	var transmitters []common.Address
+	for _, transmitter := range transmitterAccounts {
+		if !common.IsHexAddress(string(transmitter)) {
+			return nil, fmt.Errorf("TransmitAccount is not a valid Ethereum address")
+		}
+		transmitters = append(transmitters, common.HexToAddress(string(transmitter)))
+	}
+
+	configType := abi.MustNewType("tuple(uint32 paymentPremiumPPB,uint32 flatFeeMicroLink,uint32 checkGasLimit,uint24 stalenessSeconds,uint16 gasCeilingMultiplier,uint96 minUpkeepSpend,uint32 maxPerformGas,uint32 maxCheckDataSize,uint32 maxPerformDataSize,uint256 fallbackGasPrice,uint256 fallbackLinkPrice,address transcoder,address registrar)")
+	onchainConfig, err := abi.Encode(map[string]interface{}{
+		"paymentPremiumPPB":    d.cfg.PaymentPremiumPBB,
+		"flatFeeMicroLink":     d.cfg.FlatFeeMicroLink,
+		"checkGasLimit":        d.cfg.CheckGasLimit,
+		"stalenessSeconds":     d.cfg.StalenessSeconds,
+		"gasCeilingMultiplier": d.cfg.GasCeilingMultiplier,
+		"minUpkeepSpend":       d.cfg.MinUpkeepSpend,
+		"maxPerformGas":        d.cfg.MaxPerformGas,
+		"maxCheckDataSize":     d.cfg.MaxCheckDataSize,
+		"maxPerformDataSize":   d.cfg.MaxPerformDataSize,
+		"fallbackGasPrice":     big.NewInt(d.cfg.FallbackGasPrice),
+		"fallbackLinkPrice":    big.NewInt(d.cfg.FallbackLinkPrice),
+		"transcoder":           common.HexToAddress(d.cfg.Transcoder),
+		"registrar":            common.HexToAddress(d.cfg.Registrar),
+	}, configType)
+	if err != nil {
+		return nil, err
+	}
+
+	return d.IKeeperRegistryMasterInterface.SetConfig(opts, signers, transmitters, f, onchainConfig, offchainConfigVersion, offchainConfig)
 }
