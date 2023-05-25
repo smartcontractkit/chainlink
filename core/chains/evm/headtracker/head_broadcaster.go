7f958e65f614fb15b7dfd64a3467c82385ec4f32
Yong Kang <chiayongkang@hotmail.com>
2023-05-25 15:49:38 -0700

    lint fixes

--- core/chains/evm/headtracker/head_broadcaster.go
+++ core/chains/evm/headtracker/head_broadcaster.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+
 	commontypes "github.com/smartcontractkit/chainlink/v2/common/types"
 	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
