982df8261cba77f337d366cb65c5dcfd04d0cfb1
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-05-26 17:23:14 -0500

    updated comment

--- core/services/keystore/keys/ocr2key/key_bundle.go
+++ core/services/keystore/keys/ocr2key/key_bundle.go
@@ -27,7 +27,7 @@ type KeyBundle interface {
 	Unmarshal(b []byte) (err error)
 	Raw() Raw
 	OnChainPublicKey() string
-	// Decrypts ciphertext using the encryptionKey from the OCR2 OffchainKeyring
+	// Decrypts ciphertext using the encryptionKey from an OCR2 OffchainKeyring
 	NaclBoxOpenAnonymous(ciphertext []byte) (plaintext []byte, err error)
 }
 
