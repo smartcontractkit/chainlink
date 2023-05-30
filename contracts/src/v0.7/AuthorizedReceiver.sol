1dd748bcf0627b24d7a69e44b5682498fe4ababf
ilija <pavlovicilija42@gmail.com>
2023-05-30 16:13:44 +0200

    shorten authorizedReceiver senders length revert msg to use less gas

--- contracts/src/v0.7/AuthorizedReceiver.sol
+++ contracts/src/v0.7/AuthorizedReceiver.sol
@@ -14,7 +14,7 @@ abstract contract AuthorizedReceiver is AuthorizedReceiverInterface {
    * @param senders The addresses of the authorized Chainlink node
    */
   function setAuthorizedSenders(address[] calldata senders) external override validateAuthorizedSenderSetter {
-    require(senders.length > 0, "Must have at least 1 authorized sender");
+    require(senders.length > 0, "Must have at least 1 auth sender");
     // Set previous authorized senders to false
     uint256 authorizedSendersLength = s_authorizedSenderList.length;
     for (uint256 i = 0; i < authorizedSendersLength; i++) {
