a43ba9d751356d3e2f79e6fbe8fc7c1378752d51
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2023-05-30 08:21:07 +0300

    Refactoring

--- core/services/ocr2/plugins/s4/messages.go
+++ core/services/ocr2/plugins/s4/messages.go
@@ -1,22 +1,35 @@
 package s4
 
 import (
+	"github.com/smartcontractkit/chainlink/v2/core/services/s4"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"google.golang.org/protobuf/proto"
 )
 
-func MarshalRows(rows []*Row) ([]byte, error) {
-	rr := &Rows{
-		Rows: rows,
+func MarshalQuery(versions []*VersionRow) ([]byte, error) {
+	rr := &Query{
+		Versions: versions,
 	}
 	return proto.Marshal(rr)
 }
 
-func MarshalQuery(versions []*VersionRow) ([]byte, error) {
-	rr := &Query{
-		Versions: versions,
+func UnmarshalQuery(data []byte) ([]*VersionRow, error) {
+	query := &Query{}
+	if err := proto.Unmarshal(data, query); err != nil {
+		return nil, err
+	}
+	if query.Versions == nil {
+		query.Versions = make([]*VersionRow, 0)
+	}
+	return query.Versions, nil
+}
+
+func MarshalRows(rows []*Row) ([]byte, error) {
+	rr := &Rows{
+		Rows: rows,
 	}
 	return proto.Marshal(rr)
 }
@@ -32,25 +45,29 @@ func UnmarshalRows(data []byte) ([]*Row, error) {
 	return rows.Rows, nil
 }
 
-func UnmarshalQuery(data []byte) ([]*VersionRow, error) {
-	query := &Query{}
-	if err := proto.Unmarshal(data, query); err != nil {
-		return nil, err
-	}
-	if query.Versions == nil {
-		query.Versions = make([]*VersionRow, 0)
-	}
-	return query.Versions, nil
-}
-
-func MarshalAddress(address *utils.Big) string {
-	return address.Hex()
-}
-
 func UnmarshalAddress(address string) (*utils.Big, error) {
 	decoded, err := hexutil.DecodeBig(address)
 	if err != nil {
 		return nil, err
 	}
 	return utils.NewBig(decoded), nil
+}
+
+func (row *Row) VerifySignature() error {
+	address := common.HexToAddress(row.Address)
+	e := &s4.Envelope{
+		Address:    address.Bytes(),
+		SlotID:     uint(row.Slotid),
+		Payload:    row.Payload,
+		Version:    row.Version,
+		Expiration: row.Expiration,
+	}
+	signer, err := e.GetSignerAddress(row.Signature)
+	if err != nil {
+		return err
+	}
+	if signer != address {
+		return s4.ErrWrongSignature
+	}
+	return nil
 }
