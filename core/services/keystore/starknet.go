32b7c948566fa1e6f33a7e458e493bc48a6d7875
krehermann <keith.rehermann@smartcontract.com>
2023-05-31 12:11:50 -0600

    reorganize code

--- core/services/keystore/starknet.go
+++ core/services/keystore/starknet.go
@@ -13,6 +13,7 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/starkkey"
 )
 
+//go:generate mockery --name StarkNet --output ./mocks/ --case=underscore --filename starknet.go
 type StarkNet interface {
 	Get(id string) (starkkey.Key, error)
 	GetAll() ([]starkkey.Key, error)
@@ -153,13 +154,59 @@ func (ks *starknet) getByID(id string) (starkkey.Key, error) {
 	return key, nil
 }
 
+// StarkNetKeystoreAdapter is a starknet-specific adaption layer to translate between the generic Loop Keystore (bytes) and
+// the type specific caigo Keystore (big.Int)
+// The loop.Keystore must be produce a byte representation that can be parsed by the Decode func implementation
+// Users of the interface are responsible to ensure this compatibility.
+type StarkNetKeystoreAdapter interface {
+	caigo.Keystore
+	// Loopp must return a LOOPp Keystore implementation whose Sign func
+	// is compatible with the [Decode] func implementation
+	Loopp() loop.Keystore
+	// Decode translates from the raw signature of the LOOPp Keystore to that of the Caigo Keystore
+	Decode(ctx context.Context, rawSignature []byte) (*big.Int, *big.Int, error)
+}
+
+// keystoreAdapter implements [KeystoreAdapter]
+type keystoreAdapter struct {
+	looppKs loop.Keystore
+}
+
+// NewStarkNetKeystoreAdapter instantiates the KeystoreAdapter interface
+// Callers are responsible for ensuring that the given LOOPp Keystore encodes
+// signatures that can be parsed by the Decode function
+func NewStarkNetKeystoreAdapter(lk loop.Keystore) StarkNetKeystoreAdapter {
+	return &keystoreAdapter{looppKs: lk}
+}
+
+// Sign implements the caigo Keystore Sign func.
+func (ca *keystoreAdapter) Sign(ctx context.Context, senderAddress string, hash *big.Int) (*big.Int, *big.Int, error) {
+	raw, err := ca.looppKs.Sign(ctx, senderAddress, hash.Bytes())
+	if err != nil {
+		return nil, nil, fmt.Errorf("error computing loopp keystore signature: %w", err)
+	}
+	return ca.Decode(ctx, raw)
+}
+
+func (ca *keystoreAdapter) Decode(ctx context.Context, rawSignature []byte) (x *big.Int, y *big.Int, err error) {
+	starknetSig, serr := adapters.SignatureFromBytes(rawSignature)
+	if serr != nil {
+		return nil, nil, fmt.Errorf("error creating starknet signature from raw signature: %w", serr)
+	}
+	return starknetSig.Ints()
+}
+
+func (ca *keystoreAdapter) Loopp() loop.Keystore {
+	return ca.looppKs
+}
+
 // StarknetLooppSigner implements [loop.Keystore] interface and the requirements
 // of signature d/encoding of the [KeystoreAdapter]
 type StarknetLooppSigner struct {
 	StarkNet
 }
 
-func NewLooppKeystore(ks StarkNet) *StarknetLooppSigner {
+func NewStarkNetLooppKeystore(ks StarkNet) *StarknetLooppSigner {
 	return &StarknetLooppSigner{StarkNet: ks}
 }
 
