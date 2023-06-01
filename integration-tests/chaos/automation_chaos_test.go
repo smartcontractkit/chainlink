ed7c04f0085f10736159a7e8ca98a2c2a704996c
Anirudh Warrier <anirudhwarrier@users.noreply.github.com>
2023-06-01 14:53:47 +0530

    [AUTO-2640] add automation node upgrade test (#9263)

--- integration-tests/chaos/automation_chaos_test.go
+++ integration-tests/chaos/automation_chaos_test.go
@@ -36,8 +36,8 @@ Enabled = true
 [P2P]
 [P2P.V2]
 Enabled = true
-AnnounceAddresses = ["0.0.0.0:6690"]
-ListenAddresses = ["0.0.0.0:6690"]`
+AnnounceAddresses = ["0.0.0.0:8090"]
+ListenAddresses = ["0.0.0.0:8090"]`
 	defaultAutomationSettings = map[string]interface{}{
 		"toml":     client.AddNetworksConfig(baseTOML, networks.SelectedNetwork),
 		"replicas": "6",
