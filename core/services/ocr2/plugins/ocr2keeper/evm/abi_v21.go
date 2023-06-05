66b55ad1c071c90d08c158505a2e69f9cbbcd547
amirylm <amirylm.dev@gmail.com>
2023-06-05 12:52:47 +0300

    update log filter upon log trigger config change
    the update function is yet to be called (will be in future PR)
    including minimal v2.1 integration for
    getUpkeepType and packer for trigger config

---
+++ core/services/ocr2/plugins/ocr2keeper/evm/abi_v21.go
@@ -0,0 +1,40 @@
+package evm
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/i_keeper_registry_master_wrapper_2_1"
+)
+
+type evmRegistryPackerV21 struct {
+	abi abi.ABI
+}
+
+func NewEvmRegistryPackerV2_1(abi abi.ABI) *evmRegistryPackerV21 {
+	return &evmRegistryPackerV21{abi: abi}
+}
+
+// TODO: implement other methods as needed
+
+// UnpackLogTriggerConfig unpacks the log trigger config from the given raw data
+func (rp *evmRegistryPackerV21) UnpackLogTriggerConfig(raw string) (i_keeper_registry_master_wrapper_2_1.KeeperRegistryBase21LogTriggerConfig, error) {
+	var cfg i_keeper_registry_master_wrapper_2_1.KeeperRegistryBase21LogTriggerConfig
+	b, err := hexutil.Decode(raw)
+	if err != nil {
+		return cfg, err
+	}
+
+	out, err := rp.abi.Methods["getLogTriggerConfig"].Outputs.UnpackValues(b)
+	if err != nil {
+		return cfg, fmt.Errorf("%w: unpack getUpkeep return: %s", err, raw)
+	}
+
+	converted, ok := abi.ConvertType(out[0], new(i_keeper_registry_master_wrapper_2_1.KeeperRegistryBase21LogTriggerConfig)).(*i_keeper_registry_master_wrapper_2_1.KeeperRegistryBase21LogTriggerConfig)
+	if !ok {
+		return cfg, fmt.Errorf("failed to convert type")
+	}
+	return *converted, nil
+}
