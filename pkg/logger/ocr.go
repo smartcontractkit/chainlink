acb07f2c0f79af089bab939f7543fbbb089883f8
Jordan Krage <jmank88@gmail.com>
2023-06-13 08:10:43 -0500

    pkg/logger: rm trace; fix ocr wrapper (#128)

--- pkg/logger/ocr.go
+++ pkg/logger/ocr.go
@@ -8,43 +8,48 @@ var _ ocrtypes.Logger = &ocrWrapper{}
 
 type ocrWrapper struct {
 	l         Logger
+	trace     bool
 	saveError func(string)
 }
 
 // NewOCRWrapper returns a new [ocrtypes.Logger] backed by the given Logger.
-func NewOCRWrapper(l Logger, saveError func(string)) ocrtypes.Logger {
+// Note: trace logs are written at debug level, regardless of any build tags.
+func NewOCRWrapper(l Logger, trace bool, saveError func(string)) ocrtypes.Logger {
 	return &ocrWrapper{
 		// Skip an extra level since we are passed along to another wrapper.
 		l:         Helper(l, 2),
+		trace:     trace,
 		saveError: saveError,
 	}
 }
-func (ol *ocrWrapper) Trace(msg string, fields ocrtypes.LogFields) {
-	Tracew(ol.l, msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Trace(msg string, fields ocrtypes.LogFields) {
+	if o.trace {
+		o.l.Debugw(msg, toKeysAndValues(fields)...)
+	}
 }
 
-func (ol *ocrWrapper) Debug(msg string, fields ocrtypes.LogFields) {
-	ol.l.Debugw(msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Debug(msg string, fields ocrtypes.LogFields) {
+	o.l.Debugw(msg, toKeysAndValues(fields)...)
 }
 
-func (ol *ocrWrapper) Info(msg string, fields ocrtypes.LogFields) {
-	ol.l.Infow(msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Info(msg string, fields ocrtypes.LogFields) {
+	o.l.Infow(msg, toKeysAndValues(fields)...)
 }
 
-func (ol *ocrWrapper) Warn(msg string, fields ocrtypes.LogFields) {
-	ol.l.Warnw(msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Warn(msg string, fields ocrtypes.LogFields) {
+	o.l.Warnw(msg, toKeysAndValues(fields)...)
 }
 
 // Note that the structured fields may contain dynamic data (timestamps etc.)
 // So when saving the error, we only save the top level string, details
 // are included in the log.
-func (ol *ocrWrapper) Error(msg string, fields ocrtypes.LogFields) {
-	ol.saveError(msg)
-	ol.l.Errorw(msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Error(msg string, fields ocrtypes.LogFields) {
+	o.saveError(msg)
+	o.l.Errorw(msg, toKeysAndValues(fields)...)
 }
 
-func (ol *ocrWrapper) Critical(msg string, fields ocrtypes.LogFields) {
-	Criticalw(ol.l, msg, toKeysAndValues(fields)...)
+func (o *ocrWrapper) Critical(msg string, fields ocrtypes.LogFields) {
+	Criticalw(o.l, msg, toKeysAndValues(fields)...)
 }
 
 func toKeysAndValues(fields ocrtypes.LogFields) []interface{} {
--- pkg/logger/trace_noop.go
+++
@@ -1,9 +0,0 @@
-//go:build !trace
-
-package logger
-
-// Tracew is a no-op.
-func Tracew(l Logger, msg string, keysAndValues ...interface{}) {}
-
-// Tracef is a no-op.
-func Tracef(l Logger, format string, values ...interface{}) {}
