4d22f2849eb9ccff631fceee9256d8042d6dbe52
Michael Fletcher <michael.fletcher@smartcontract.com>
2023-06-14 18:14:17 +0100

    Update mentions of "NOP" to "Payee" + Optimizations

--- contracts/src/v0.8/llo-feeds/RewardBank.sol
+++ contracts/src/v0.8/llo-feeds/RewardBank.sol
@@ -11,27 +11,27 @@ import {IERC165} from "../shared/vendor/IERC165.sol";
 /*
  * @title FeeManager
  * @author Michael Fletcher
- * @notice This contract will be used to reward NOPs for generating reports. Any reports verified by end users will have the fee split amongst the NOPs that are part of the DON that produced the report.
+ * @notice This contract will be used to reward NOPs for generating reports and any additional Orchestrators such as Chainlink Labs. Any reports verified by end users will have the fee split amongst the Payees and Orchestrators that are part of the DON that produced the report.
  */
 contract RewardBank is IRewardBank, ConfirmedOwner, TypeAndVersionInterface{
     using SafeERC20 for IERC20;
 
-    // @dev The mapping of total fees collected for a particular pot: totalNOPFees[configDigest][asset]
-    mapping(bytes32 => mapping(address => uint256)) private totalNOPFees;
+    // @dev The mapping of total fees collected for a particular pot: totalPayeeFees[configDigest][asset]
+    mapping(bytes32 => mapping(address => uint256)) private totalPayeeFees;
 
-    // @dev The mapping of NOP configs for a particular feed config: NOPs[configDigest][NOP]
-    mapping(bytes32 => NOPAndWeight[]) private NOPsAndWeights;
+    // @dev The mapping of Payee configs for a particular feed config: Payees[configDigest][Payee]
+    mapping(bytes32 => PayeeAndWeight[]) private PayeesAndWeights;
 
     // @dev The address of the verifier proxy
     address private immutable i_verifierProxyAddr;
 
-    // The total weight of all NOPs in a DON
+    // The total weight of all Payees
     uint8 private constant TOTAL_WEIGHT = 100;
 
     // @notice Thrown whenever a zero address is passed
     error ZeroAddress();
 
-    // @notice Thrown whenever the NOP weights are invalid
+    // @notice Thrown whenever the Payee weights are invalid
     error InvalidWeights();
 
     // @notice Thrown whenever their are no fees to claim
@@ -69,7 +69,7 @@ contract RewardBank is IRewardBank, ConfirmedOwner, TypeAndVersionInterface{
         //update the total fees collected for this pot
         unchecked {
             // the total amount for any ERC20 token cannot exceed 2^256 - 1
-            totalNOPFees[configDigest][fee.assetAddress] += fee.amount;
+            totalPayeeFees[configDigest][fee.assetAddress] += fee.amount;
         }
 
         //transfer the fee to the contract
@@ -77,32 +77,32 @@ contract RewardBank is IRewardBank, ConfirmedOwner, TypeAndVersionInterface{
     }
 
     // @inheritdoc IRewardBank
-    function payNOPs(bytes32 configDigest, address assetAddress) external override {
+    function payPayees(bytes32 configDigest, address assetAddress) external override {
         //get the total fees collected for this pot
-        uint256 totalFees = totalNOPFees[configDigest][assetAddress];
+        uint256 totalFees = totalPayeeFees[configDigest][assetAddress];
 
         //ensure there are fees to pay
         if (totalFees == 0) revert ZeroFees();
 
         //variables used within the loop
-        uint256 totalNOPs = NOPsAndWeights[configDigest].length;
-        uint256 NOPShare;
-        NOPAndWeight storage NOP;
+        uint256 totalPayees = PayeesAndWeights[configDigest].length;
+        uint256 PayeeShare;
+        PayeeAndWeight memory Payee;
 
         //Dust will be lost setting this to 0, but the value of that dust is significantly less than the gas would be if we were to account for the dust.
         //Setting this here will prevent us needing to use the nonReentrancy modifier
-        totalNOPFees[configDigest][assetAddress] = 0;
+        totalPayeeFees[configDigest][assetAddress] = 0;
 
-        //loop all the NOPs and pay them their share of the fees
-        for (uint256 i; i < totalNOPs;) {
-            //get the NOP for address and weight
-            NOP = NOPsAndWeights[configDigest][i];
+        //loop all the Payees and pay them their share of the fees
+        for (uint256 i; i < totalPayees;) {
+            //get the Payee for address and weight
+            Payee = PayeesAndWeights[configDigest][i];
 
-            //calculate the NOP share of the fees.
-            NOPShare = (totalFees * NOP.weight) / TOTAL_WEIGHT;
+            //calculate the Payee share of the fees.
+            PayeeShare = (totalFees * Payee.weight) / TOTAL_WEIGHT;
 
-            //transfer the NOP share of the fees to the NOP
-            IERC20(assetAddress).safeTransfer(NOP.NOPAddress, NOPShare);
+            //transfer the Payee share of the fees to the Payee
+            IERC20(assetAddress).safeTransfer(Payee.PayeeAddress, PayeeShare);
 
             unchecked {
                 // there will never be enough ops in a config for i to overflow
@@ -113,28 +113,28 @@ contract RewardBank is IRewardBank, ConfirmedOwner, TypeAndVersionInterface{
 
 
     // @inheritdoc IRewardBank
-    function setNOPs(bytes32 configDigest, NOPAndWeight[] calldata NOPAndWeights) external override onlyAdminOrProxy {
-        if (NOPAndWeights.length == 0) revert ZeroAddress();
+    function setPayees(bytes32 configDigest, PayeeAndWeight[] calldata PayeeAndWeights) external override onlyAdminOrProxy {
+        if (PayeeAndWeights.length == 0) revert ZeroAddress();
 
-        //loop all the NOPs and validate the weight and address
+        //loop all the Payees and validate the weight and address
         uint256 totalWeight;
-        for (uint256 i; i < NOPAndWeights.length;) {
+        for (uint256 i; i < PayeeAndWeights.length;) {
             unchecked {
                 //keep track of the cumulative weight, this cannot overflow as the passed in weight is 16 bits
-                totalWeight += NOPAndWeights[i].weight;
+                totalWeight += PayeeAndWeights[i].weight;
 
                 // there will never be enough ops in a config for i to overflow
                 ++i;
             }
 
-            //ensure the NOP address is not zero
-            if (NOPAndWeights[i].NOPAddress == address(0)) revert ZeroAddress();
+            //ensure the Payee address is not zero
+            if (PayeeAndWeights[i].PayeeAddress == address(0)) revert ZeroAddress();
 
-            //ensure NOP weight is not 0
-            if (NOPAndWeights[i].weight == 0) revert InvalidWeights();
+            //ensure Payee weight is not 0
+            if (PayeeAndWeights[i].weight == 0) revert InvalidWeights();
 
             //copy to contract storage
-            NOPsAndWeights[configDigest][i] = NOPAndWeight(NOPAndWeights[i].NOPAddress, NOPAndWeights[i].weight);
+            PayeesAndWeights[configDigest][i] = PayeeAndWeight(PayeeAndWeights[i].PayeeAddress, PayeeAndWeights[i].weight);
         }
 
         // If total weight is not met, the fees will either be under or over distributed
--- contracts/src/v0.8/llo-feeds/interfaces/IRewardBank.sol
+++ contracts/src/v0.8/llo-feeds/interfaces/IRewardBank.sol
@@ -13,18 +13,18 @@ interface IRewardBank is IERC165 {
     function onFeePaid(bytes32 configDigest, Asset calldata fee) external;
 
     /**
-    * @notice Distributes all the rewards in the specified pot to the NOPs
+    * @notice Distributes all the rewards in the specified pot to the Payees
     * @param configDigest config digest of the report being verified
     * @param assetAddress address of the assetAddress to distribute
     */
-    function payNOPs(bytes32 configDigest, address assetAddress) external;
+    function payPayees(bytes32 configDigest, address assetAddress) external;
 
     /**
-    * @notice Set the NOPs and weights for a specific feed config
-    * @param configDigest config digest to set NOPs and weights for
-    * @param NOPAndWeights array of each NOP and associated weight
+    * @notice Set the Payees and weights for a specific feed config
+    * @param configDigest config digest to set Payees and weights for
+    * @param PayeeAndWeights array of each Payee and associated weight
     */
-    function setNOPs(bytes32 configDigest, NOPAndWeight[] calldata NOPAndWeights) external;
+    function setPayees(bytes32 configDigest, PayeeAndWeight[] calldata PayeeAndWeights) external;
 
     // @notice The asset struct to hold the address of the asset and the amount
     struct Asset {
@@ -32,9 +32,9 @@ interface IRewardBank is IERC165 {
         uint256 amount;
     }
 
-    // @notice Struct to hold the address of a NOP and its weight to determine what percentage of the pot it receives
-    struct NOPAndWeight {
-        address NOPAddress;
+    // @notice Struct to hold the address of a Payee and its weight to determine what percentage of the pot it receives
+    struct PayeeAndWeight {
+        address PayeeAddress;
         uint8 weight;
     }
 }
