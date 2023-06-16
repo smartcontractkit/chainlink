a95408cead568b1248da1074aab0fb67dde8e181
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2023-06-15 17:17:49 -0700

    [Functions] Allowlist support

--- core/services/gateway/handler_factory.go
+++ core/services/gateway/handler_factory.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/config"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/handlers"
@@ -16,19 +17,20 @@ const (
 )
 
 type handlerFactory struct {
-	lggr logger.Logger
+	chains evm.ChainSet
+	lggr   logger.Logger
 }
 
 var _ HandlerFactory = (*handlerFactory)(nil)
 
-func NewHandlerFactory(lggr logger.Logger) HandlerFactory {
-	return &handlerFactory{lggr}
+func NewHandlerFactory(chains evm.ChainSet, lggr logger.Logger) HandlerFactory {
+	return &handlerFactory{chains, lggr}
 }
 
 func (hf *handlerFactory) NewHandler(handlerType HandlerType, handlerConfig json.RawMessage, donConfig *config.DONConfig, don handlers.DON) (handlers.Handler, error) {
 	switch handlerType {
 	case FunctionsHandlerType:
-		return functions.NewFunctionsHandler(handlerConfig, donConfig, don, hf.lggr)
+		return functions.NewFunctionsHandler(handlerConfig, donConfig, don, hf.chains, hf.lggr)
 	case DummyHandlerType:
 		return handlers.NewDummyHandler(donConfig, don, hf.lggr)
 	default:
