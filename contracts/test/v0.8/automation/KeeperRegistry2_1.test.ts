158da77059e9565d8f850cf0df55d1ac69a0dc68
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-06-19 11:51:34 +0100

    regen wrappers

--- contracts/test/v0.8/automation/KeeperRegistry2_1.test.ts
+++ contracts/test/v0.8/automation/KeeperRegistry2_1.test.ts
@@ -1795,6 +1795,101 @@ describe('KeeperRegistry2_1', () => {
           })
         },
       )
+
+      describeMaybe(
+        'Gas benchmarking log upkeeps [ @skip-coverage ]',
+        function () {
+          const fs = [1, 10]
+          fs.forEach(function (newF) {
+            it(
+              'When f=' +
+                newF +
+                ' calculates gas overhead appropriately within a margin',
+              async () => {
+                // Perform the upkeep once to remove non-zero storage slots and have predictable gas measurement
+                let tx = await getTransmitTx(registry, keeper1, [logUpkeepId])
+                await tx.wait()
+                const performData = '0x'
+                await mock.setCanPerform(true)
+                await mock.setPerformGasToBurn(executeGas)
+                await registry
+                  .connect(owner)
+                  .setConfig(
+                    signerAddresses,
+                    keeperAddresses,
+                    newF,
+                    encodeConfig(config),
+                    offchainVersion,
+                    offchainBytes,
+                  )
+                tx = await getTransmitTx(registry, keeper1, [upkeepId], {
+                  numSigners: newF + 1,
+                  performData,
+                })
+                const receipt = await tx.wait()
+                const upkeepPerformedLogs = parseUpkeepPerformedLogs(receipt)
+                // exactly 1 Upkeep Performed should be emitted
+                assert.equal(upkeepPerformedLogs.length, 1)
+                const upkeepPerformedLog = upkeepPerformedLogs[0]
+
+                const upkeepGasUsed = upkeepPerformedLog.args.gasUsed
+                const chargedGasOverhead = upkeepPerformedLog.args.gasOverhead
+                const actualGasOverhead = receipt.gasUsed.sub(upkeepGasUsed)
+
+                assert.isTrue(upkeepGasUsed.gt(BigNumber.from('0')))
+                assert.isTrue(chargedGasOverhead.gt(BigNumber.from('0')))
+
+                console.log(
+                  'Gas Benchmarking - sig verification ( f =',
+                  newF,
+                  '): calculated overhead: ',
+                  chargedGasOverhead.toString(),
+                  ' actual overhead: ',
+                  actualGasOverhead.toString(),
+                  ' margin over gasUsed: ',
+                  chargedGasOverhead.sub(actualGasOverhead).toString(),
+                )
+
+                // Overhead should not get capped
+                const gasOverheadCap = registryLogOverhead
+                  .add(
+                    registryPerSignerGasOverhead.mul(BigNumber.from(newF + 1)),
+                  )
+                  .add(
+                    BigNumber.from(
+                      registryPerPerformByteGasOverhead.toNumber() *
+                        performData.length,
+                    ),
+                  )
+                const gasCapMinusOverhead =
+                  gasOverheadCap.sub(chargedGasOverhead)
+                assert.isTrue(
+                  gasCapMinusOverhead.gt(BigNumber.from(0)),
+                  'Gas overhead got capped. Verify gas overhead variables in test match those in the registry. To not have the overheads capped increase REGISTRY_GAS_OVERHEAD by atleast ' +
+                    gasCapMinusOverhead.toString(),
+                )
+                // total gas charged should be greater than tx gas but within gasCalculationMargin
+                assert.isTrue(
+                  chargedGasOverhead.gt(actualGasOverhead),
+                  'Gas overhead calculated is too low, increase account gas variables (ACCOUNTING_FIXED_GAS_OVERHEAD/ACCOUNTING_PER_SIGNER_GAS_OVERHEAD) by atleast ' +
+                    actualGasOverhead.sub(chargedGasOverhead).toString(),
+                )
+
+                assert.isTrue(
+                  chargedGasOverhead
+                    .sub(actualGasOverhead)
+                    .lt(BigNumber.from(gasCalculationMargin)),
+                ),
+                  'Gas overhead calculated is too high, decrease account gas variables (ACCOUNTING_FIXED_GAS_OVERHEAD/ACCOUNTING_PER_SIGNER_GAS_OVERHEAD)  by atleast ' +
+                    chargedGasOverhead
+                      .sub(chargedGasOverhead)
+                      .sub(BigNumber.from(gasCalculationMargin))
+                      .toString()
+              },
+            )
+          })
+        },
+      )
     })
 
     describeMaybe('When condiitonal upkeeps are batched', function () {
