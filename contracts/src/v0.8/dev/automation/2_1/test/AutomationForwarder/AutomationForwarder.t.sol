7b2f7d7728d3d41efacc20a6b0e8666f0cfea73a
JonWong203 <thejwongg@gmail.com>
2023-06-20 13:44:54 -0400

    foundry refactor

--- contracts/src/v0.8/dev/automation/2_1/test/AutomationForwarder/AutomationForwarder.t.sol
+++ contracts/src/v0.8/dev/automation/2_1/test/AutomationForwarder/AutomationForwarder.t.sol
@@ -2,25 +2,28 @@
 pragma solidity 0.8.6;
 
 import 'forge-std/Test.sol';
-import '../AutomationForwarder.sol';
-import {IAutomationRegistryConsumer} from "../interfaces/IAutomationRegistryConsumer.sol";
-import '../mocks/MockCustomerTarget.sol';
-import '../KeeperRegistryBase2_1.sol';
-import '../mocks/MockKeeperRegistry2_1.sol';
+import {AutomationForwarder} from '../../AutomationForwarder.sol';
+import {IAutomationRegistryConsumer} from "../../interfaces/IAutomationRegistryConsumer.sol";
+import {MockCustomerTarget} from '../../mocks/MockCustomerTarget.sol';
+import {KeeperRegistryBase2_1} from '../../KeeperRegistryBase2_1.sol';
+import {MockKeeperRegistry2_1} from '../../mocks/MockKeeperRegistry2_1.sol';
 
-contract AutomationForwarderTest is Test {
+contract AutomationForwarderSetUp is Test {
 
-    AutomationForwarder public forwarder;
-    IAutomationRegistryConsumer public default_registry;
-    MockCustomerTarget public default_target;
+    AutomationForwarder internal forwarder;
+    IAutomationRegistryConsumer internal default_registry;
+    MockCustomerTarget internal default_target;
+    address internal OWNER;
+    address internal constant STRANGER = address(999);
 
     uint256 constant GAS = 5_000;
 
      function setUp() public {
         default_registry = IAutomationRegistryConsumer(new MockKeeperRegistry2_1());
         default_target = new MockCustomerTarget();
-        vm.prank(address(default_registry));
+        vm.startPrank(address(default_registry));
         forwarder = new AutomationForwarder(1, address(default_target));
+        OWNER = address(default_registry);
     }
 
     function getSelector(string memory _func) public pure returns (bytes memory) {
@@ -28,50 +31,43 @@ contract AutomationForwarderTest is Test {
         bytes memory selectorBytes = abi.encodePacked(selector);
         return selectorBytes;
     }
-  
 
-    function test_forward() public {
+}
+
+contract AutomationForwarderTest_forward is AutomationForwarderSetUp {
+    function testBasicSuccess() public {
         bytes memory selector = getSelector('performUpkeep()');
-        vm.prank(address(default_registry));
         bool val = forwarder.forward(GAS, selector);
         assertEq(val, true);
     }
 
-    function test_forward_WrongFunctionSelector() public {
+    function testWrongFunctionSelectorSuccess() public {
         bytes memory selector = getSelector('performUpkeep(int num)');
-        vm.prank(address(default_registry));
         bool val = forwarder.forward(GAS, selector);
         assertFalse(val);
     }
 
-    function test_forward_NotFromRegistry() public {
+    function testNotFromRegistryNotAuthorizedReverts() public {
         bytes memory selector = getSelector('performUpkeep()');
+        changePrank(STRANGER);
         vm.expectRevert(AutomationForwarder.NotAuthorized.selector);
         bool val = forwarder.forward(GAS, selector);
     }
+}
 
-    function test_updateRegistry() public {
+contract AutomationForwarderTest_updateRegistry is AutomationForwarderSetUp {
+    function testBasicSuccess() public {
         address newRegistry = address(1);
-        vm.prank(address(default_registry));
         forwarder.updateRegistry(address(newRegistry));
         IAutomationRegistryConsumer newReg = forwarder.getRegistry();
         assertEq(address(newReg), newRegistry);
     }
 
-    function test_updateRegistry_NotFromRegistry() public {
+    function testNotFromRegistryNotAuthorizedReverts() public {
         address newRegistry = address(1);
+        changePrank(STRANGER);
         vm.expectRevert(AutomationForwarder.NotAuthorized.selector);
         forwarder.updateRegistry(address(newRegistry));
     }
-
-    function test_getters() public {
-        bytes memory reg = abi.encodePacked(address(forwarder.getRegistry()));
-        assertEq(reg, abi.encodePacked(default_registry), 'getRegistry()');
-        bytes memory targ = abi.encodePacked(address(forwarder.getTarget()));
-        assertEq(targ, abi.encodePacked(default_target), 'getTarget()');
-        uint256 id = forwarder.getUpkeepID();
-        assertEq(id, 1, 'getUpkeepId()');
-    }
-
 
 }
