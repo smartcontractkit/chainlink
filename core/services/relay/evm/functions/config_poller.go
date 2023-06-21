d41d0ae541b72290544d2b50224907b1e8ed0b76
Morgan Kuphal <87319522+KuphJr@users.noreply.github.com>
2023-06-21 17:01:37 -0500

    Custom configPoller to support multiple Functions plugins (#9648)
    * custom configPoller to support multiple Functions plugins
    
    * removed log
    
    * Fixed tests
    
    * fixed lint errors
    
    * rolled back unneeded lint changes
    
    * addressed feedback
    
    * fixed error
    
    * use imported ConfigPoller interface
    
    ---------
    
    Co-authored-by: Bolek <1416262+bolekk@users.noreply.github.com>

--- core/services/relay/evm/functions/config_poller.go
+++ core/services/relay/evm/functions/config_poller.go
@@ -1,20 +1,40 @@
-package evm
+package functions
 
 import (
 	"context"
 	"database/sql"
+	"encoding/binary"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
 	"github.com/smartcontractkit/libocr/gethwrappers2/ocr2aggregator"
-	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
+	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2/types"
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pg"
-	evmRelayTypes "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
+	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
 )
+
+var _ types.ConfigPoller = &configPoller{}
+
+type FunctionsPluginType int
+
+const (
+	FunctionsPlugin FunctionsPluginType = iota
+	ThresholdPlugin
+	S4Plugin
+)
+
+type configPoller struct {
+	lggr               logger.Logger
+	filterName         string
+	destChainLogPoller logpoller.LogPoller
+	addr               common.Address
+	pluginType         FunctionsPluginType
+}
 
 // ConfigSet Common to all OCR2 evm based contracts: https://github.com/smartcontractkit/libocr/blob/master/contract2/dev/OCR2Abstract.sol
 var ConfigSet common.Hash
@@ -42,7 +62,7 @@ func unpackLogData(d []byte) (*ocr2aggregator.OCR2AggregatorConfigSet, error) {
 	return unpacked, nil
 }
 
-func configFromLog(logData []byte) (ocrtypes.ContractConfig, error) {
+func configFromLog(logData []byte, pluginType FunctionsPluginType) (ocrtypes.ContractConfig, error) {
 	unpacked, err := unpackLogData(logData)
 	if err != nil {
 		return ocrtypes.ContractConfig{}, err
@@ -58,6 +78,18 @@ func configFromLog(logData []byte) (ocrtypes.ContractConfig, error) {
 		signers = append(signers, addr[:])
 	}
 
+	// Replace the first two bytes of the config digest with the plugin type to avoid duplicate config digests between Functions plugins
+	switch pluginType {
+	case FunctionsPlugin:
+		// FunctionsPluginType should already have the correct prefix, so this is a no-op
+	case ThresholdPlugin:
+		binary.BigEndian.PutUint16(unpacked.ConfigDigest[:2], uint16(ThresholdDigestPrefix))
+	case S4Plugin:
+		binary.BigEndian.PutUint16(unpacked.ConfigDigest[:2], uint16(S4DigestPrefix))
+	default:
+		return ocrtypes.ContractConfig{}, errors.New("unknown plugin type")
+	}
+
 	return ocrtypes.ContractConfig{
 		ConfigDigest:          unpacked.ConfigDigest,
 		ConfigCount:           unpacked.ConfigCount,
@@ -70,18 +102,11 @@ func configFromLog(logData []byte) (ocrtypes.ContractConfig, error) {
 	}, nil
 }
 
-type configPoller struct {
-	lggr               logger.Logger
-	filterName         string
-	destChainLogPoller logpoller.LogPoller
-	addr               common.Address
-}
-
 func configPollerFilterName(addr common.Address) string {
 	return logpoller.FilterName("OCR2ConfigPoller", addr.String())
 }
 
-func NewConfigPoller(lggr logger.Logger, destChainPoller logpoller.LogPoller, addr common.Address) (evmRelayTypes.ConfigPoller, error) {
+func NewFunctionsConfigPoller(pluginType FunctionsPluginType, destChainPoller logpoller.LogPoller, addr common.Address, lggr logger.Logger) (types.ConfigPoller, error) {
 	err := destChainPoller.RegisterFilter(logpoller.Filter{Name: configPollerFilterName(addr), EventSigs: []common.Hash{ConfigSet}, Addresses: []common.Address{addr}})
 	if err != nil {
 		return nil, err
@@ -92,53 +117,47 @@ func NewConfigPoller(lggr logger.Logger, destChainPoller logpoller.LogPoller, ad
 		filterName:         configPollerFilterName(addr),
 		destChainLogPoller: destChainPoller,
 		addr:               addr,
+		pluginType:         pluginType,
 	}
 
 	return cp, nil
 }
 
-// Start noop method
 func (cp *configPoller) Start() {}
 
-// Close noop method
 func (cp *configPoller) Close() error {
 	return nil
 }
 
-// Notify noop method
 func (cp *configPoller) Notify() <-chan struct{} {
 	return nil
 }
 
-// Replay abstracts the logpoller.LogPoller Replay() implementation
 func (cp *configPoller) Replay(ctx context.Context, fromBlock int64) error {
 	return cp.destChainLogPoller.Replay(ctx, fromBlock)
 }
 
-// LatestConfigDetails returns the latest config details from the logs
 func (cp *configPoller) LatestConfigDetails(ctx context.Context) (changedInBlock uint64, configDigest ocrtypes.ConfigDigest, err error) {
 	latest, err := cp.destChainLogPoller.LatestLogByEventSigWithConfs(ConfigSet, cp.addr, 1, pg.WithParentCtx(ctx))
 	if err != nil {
-		// If contract is not configured, we will not have the log.
 		if errors.Is(err, sql.ErrNoRows) {
 			return 0, ocrtypes.ConfigDigest{}, nil
 		}
 		return 0, ocrtypes.ConfigDigest{}, err
 	}
-	latestConfigSet, err := configFromLog(latest.Data)
+	latestConfigSet, err := configFromLog(latest.Data, cp.pluginType)
 	if err != nil {
 		return 0, ocrtypes.ConfigDigest{}, err
 	}
 	return uint64(latest.BlockNumber), latestConfigSet.ConfigDigest, nil
 }
 
-// LatestConfig returns the latest config from the logs on a certain block
 func (cp *configPoller) LatestConfig(ctx context.Context, changedInBlock uint64) (ocrtypes.ContractConfig, error) {
 	lgs, err := cp.destChainLogPoller.Logs(int64(changedInBlock), int64(changedInBlock), ConfigSet, cp.addr, pg.WithParentCtx(ctx))
 	if err != nil {
 		return ocrtypes.ContractConfig{}, err
 	}
-	latestConfigSet, err := configFromLog(lgs[len(lgs)-1].Data)
+	latestConfigSet, err := configFromLog(lgs[len(lgs)-1].Data, cp.pluginType)
 	if err != nil {
 		return ocrtypes.ContractConfig{}, err
 	}
@@ -146,7 +165,6 @@ func (cp *configPoller) LatestConfig(ctx context.Context, changedInBlock uint64)
 	return latestConfigSet, nil
 }
 
-// LatestBlockHeight returns the latest block height from the logs
 func (cp *configPoller) LatestBlockHeight(ctx context.Context) (blockHeight uint64, err error) {
 	latest, err := cp.destChainLogPoller.LatestBlock(pg.WithParentCtx(ctx))
 	if err != nil {
