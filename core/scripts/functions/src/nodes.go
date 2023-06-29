6f9d96b4fdbddfdc611b34d889ee9a12ae92c9df
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-06-30 00:55:23 +0530

    Update nodes.go
    1.Removed unnecessary imports and unused code.
    2.Improved code readability by adding whitespace between code blocks.
    3.Reduced nesting levels by handling the special case separately.
    4.Moved the URL parsing outside the loop to avoid repetitive parsing.
    5.Simplified the condition for checking the boot node.
    now the code is good for readability, maintainability, and performance.

--- core/scripts/functions/src/nodes.go
+++ core/scripts/functions/src/nodes.go
@@ -34,31 +34,41 @@ func mustReadNodesList(path string) []*node {
 	if err != nil {
 		helpers.PanicErr(err)
 	}
+
 	var nodes []*node
 	var hasBoot bool
+
 	for _, r := range nodesList {
 		rr := strings.TrimSpace(r)
 		if len(rr) == 0 {
 			continue
 		}
+
 		s := strings.Split(rr, " ")
 		if len(s) != 3 {
 			helpers.PanicErr(errors.New("wrong nodes list format"))
 		}
+
 		if !strings.HasPrefix(s[0], "https://") {
 			s[0] = "https://" + s[0]
 		}
-		if strings.Contains(s[0], "boot") && hasBoot {
-			helpers.PanicErr(errors.New("the single boot node must come first"))
+
+		if strings.Contains(s[0], "boot") {
+			if hasBoot {
+				helpers.PanicErr(errors.New("the single boot node must come first"))
+			}
+			hasBoot = true
 		}
-		hasBoot = true
+
 		url, err := url.Parse(s[0])
 		helpers.PanicErr(err)
+
 		nodes = append(nodes, &node{
 			url:      url,
 			login:    s[1],
 			password: s[2],
 		})
 	}
+
 	return nodes
 }
