cae962b43c72c0b8d8f1507286fa28dd7fce670e
Ryan Hall <hall.ryan.r@gmail.com>
2023-07-12 15:35:25 -0400

    typo

--- contracts/src/v0.8/automation/UpkeepFormat.sol
+++ contracts/src/v0.8/automation/UpkeepFormat.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 /**
  * @dev this struct is only maintained for backwards compatibility with MigratableKeeperRegistryInterface
- * it should deprecated in the future in favor of MigratableKeeperRegistryInterfaceV2
+ * it should be deprecated in the future in favor of MigratableKeeperRegistryInterfaceV2
  */
 enum UpkeepFormat {
   V1,
