49e00100d001fbcdc029990c37020ca7513ec2f2
Bolek <1416262+bolekk@users.noreply.github.com>
2023-07-12 05:55:05 -0700

    [Functions] Basic handler (#9709)

--- core/services/gateway/handlers/functions/handler.functions.go
+++ core/services/gateway/handlers/functions/handler.functions.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 
@@ -13,26 +14,48 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/api"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/config"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/handlers"
+	hc "github.com/smartcontractkit/chainlink/v2/core/services/gateway/handlers/common"
+	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
 
 type FunctionsHandlerConfig struct {
 	OnchainAllowlistChainID string `json:"onchainAllowlistChainId"`
 	// Not specifying OnchainAllowlist config disables allowlist checks
 	OnchainAllowlist *OnchainAllowlistConfig `json:"onchainAllowlist"`
+	// Not specifying RateLimiter config disables rate limiting
+	UserRateLimiter           *hc.RateLimiterConfig `json:"userRateLimiter"`
+	NodeRateLimiter           *hc.RateLimiterConfig `json:"nodeRateLimiter"`
+	MaxPendingRequestsPerUser int                   `json:"maxPendingRequestsPerUser"`
+	MaxPendingRequestsGlobal  int                   `json:"maxPendingRequestsGlobal"`
+	RequestTimeoutMillis      int64                 `json:"requestTimeoutMillis"`
 }
 
 type functionsHandler struct {
-	handlerConfig *FunctionsHandlerConfig
-	donConfig     *config.DONConfig
-	don           handlers.DON
-	allowlist     OnchainAllowlist
-	lggr          logger.Logger
+	utils.StartStopOnce
+
+	handlerConfig   FunctionsHandlerConfig
+	donConfig       *config.DONConfig
+	don             handlers.DON
+	pendingRequests hc.RequestCache[PendingSecretsRequest]
+	allowlist       OnchainAllowlist
+	userRateLimiter *hc.RateLimiter
+	nodeRateLimiter *hc.RateLimiter
+	chStop          utils.StopChan
+	lggr            logger.Logger
+}
+
+type PendingSecretsRequest struct {
+	request    *api.Message
+	responses  map[string]*api.Message
+	successful []*api.Message
+	errors     []*api.Message
 }
 
 var _ handlers.Handler = (*functionsHandler)(nil)
 
-func NewFunctionsHandler(handlerConfig json.RawMessage, donConfig *config.DONConfig, don handlers.DON, chains evm.ChainSet, lggr logger.Logger) (handlers.Handler, error) {
-	cfg, err := ParseConfig(handlerConfig)
+func NewFunctionsHandlerFromConfig(handlerConfig json.RawMessage, donConfig *config.DONConfig, don handlers.DON, chains evm.ChainSet, lggr logger.Logger) (handlers.Handler, error) {
+	var cfg FunctionsHandlerConfig
+	err := json.Unmarshal(handlerConfig, &cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -51,21 +74,43 @@ func NewFunctionsHandler(handlerConfig json.RawMessage, donConfig *config.DONCon
 			return nil, err
 		}
 	}
-	return &functionsHandler{
-		handlerConfig: cfg,
-		donConfig:     donConfig,
-		don:           don,
-		allowlist:     allowlist,
-		lggr:          lggr,
-	}, nil
+	var userRateLimiter, nodeRateLimiter *hc.RateLimiter
+	if cfg.UserRateLimiter != nil {
+		userRateLimiter, err = hc.NewRateLimiter(*cfg.UserRateLimiter)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if cfg.NodeRateLimiter != nil {
+		nodeRateLimiter, err = hc.NewRateLimiter(*cfg.NodeRateLimiter)
+		if err != nil {
+			return nil, err
+		}
+	}
+	pendingRequestsCache := hc.NewRequestCache[PendingSecretsRequest](time.Millisecond * time.Duration(cfg.RequestTimeoutMillis))
+	return NewFunctionsHandler(cfg, donConfig, don, pendingRequestsCache, allowlist, userRateLimiter, nodeRateLimiter, lggr), nil
 }
 
-func ParseConfig(handlerConfig json.RawMessage) (*FunctionsHandlerConfig, error) {
-	var cfg FunctionsHandlerConfig
-	if err := json.Unmarshal(handlerConfig, &cfg); err != nil {
-		return nil, err
+func NewFunctionsHandler(
+	cfg FunctionsHandlerConfig,
+	donConfig *config.DONConfig,
+	don handlers.DON,
+	pendingRequestsCache hc.RequestCache[PendingSecretsRequest],
+	allowlist OnchainAllowlist,
+	userRateLimiter *hc.RateLimiter,
+	nodeRateLimiter *hc.RateLimiter,
+	lggr logger.Logger) handlers.Handler {
+	return &functionsHandler{
+		handlerConfig:   cfg,
+		donConfig:       donConfig,
+		don:             don,
+		pendingRequests: pendingRequestsCache,
+		allowlist:       allowlist,
+		userRateLimiter: userRateLimiter,
+		nodeRateLimiter: nodeRateLimiter,
+		chStop:          make(utils.StopChan),
+		lggr:            lggr,
 	}
-	return &cfg, nil
 }
 
 func (h *functionsHandler) HandleUserMessage(ctx context.Context, msg *api.Message, callbackCh chan<- handlers.UserCallbackPayload) error {
@@ -74,24 +119,113 @@ func (h *functionsHandler) HandleUserMessage(ctx context.Context, msg *api.Messa
 		h.lggr.Debugw("received a message from a non-allowlisted address", "sender", msg.Body.Sender)
 		return errors.New("sender not allowlisted")
 	}
-	h.lggr.Debugw("received a valid message", "sender", msg.Body.Sender)
+	if h.userRateLimiter != nil && !h.userRateLimiter.Allow(msg.Body.Sender) {
+		h.lggr.Debug("rate-limited", "sender", msg.Body.Sender)
+		return errors.New("rate-limited")
+	}
+	switch msg.Body.Method {
+	case MethodSecretsSet, MethodSecretsList:
+		return h.handleSecretsRequest(ctx, msg, callbackCh)
+	default:
+		h.lggr.Debug("unsupported method", "method", msg.Body.Method)
+		return errors.New("unsupported method")
+	}
+}
+
+func (h *functionsHandler) handleSecretsRequest(ctx context.Context, msg *api.Message, callbackCh chan<- handlers.UserCallbackPayload) error {
+	h.lggr.Debugw("handleSecretsRequest: processing message", "sender", msg.Body.Sender)
+	err := h.pendingRequests.NewRequest(msg, callbackCh, &PendingSecretsRequest{request: msg, responses: make(map[string]*api.Message)})
+	if err != nil {
+		h.lggr.Warnw("handleSecretsRequest: error adding new request", "sender", msg.Body.Sender, "err", err)
+		return err
+	}
+	// Send to all nodes.
+	for _, member := range h.donConfig.Members {
+		err := h.don.SendToNode(ctx, member.Address, msg)
+		if err != nil {
+			h.lggr.Debugw("handleSecretsRequest: failed to send to a node", "node", member.Address)
+		}
+	}
 	return nil
 }
 
 func (h *functionsHandler) HandleNodeMessage(ctx context.Context, msg *api.Message, nodeAddr string) error {
-	return nil
+	if h.nodeRateLimiter != nil && !h.nodeRateLimiter.Allow(nodeAddr) {
+		h.lggr.Debug("rate-limited", "sender", nodeAddr)
+		return errors.New("rate-limited")
+	}
+	switch msg.Body.Method {
+	case MethodSecretsSet, MethodSecretsList:
+		return h.pendingRequests.ProcessResponse(msg, h.processSecretsResponse)
+	default:
+		h.lggr.Debug("unsupported method", "method", msg.Body.Method)
+		return errors.New("unsupported method")
+	}
 }
 
-func (h *functionsHandler) Start(ctx context.Context) (err error) {
-	if h.allowlist != nil {
-		err = h.allowlist.Start(ctx)
+// Conforms to ResponseProcessor[*PendingSecretsRequest]
+func (h *functionsHandler) processSecretsResponse(response *api.Message, responseData *PendingSecretsRequest) (*handlers.UserCallbackPayload, *PendingSecretsRequest, error) {
+	if _, exists := responseData.responses[response.Body.Sender]; exists {
+		return nil, nil, errors.New("duplicate response")
 	}
-	return
+	responseData.responses[response.Body.Sender] = response
+	if response.Body.Method != responseData.request.Body.Method {
+		return nil, responseData, errors.New("invalid method")
+	}
+	var responsePayload SecretsResponseBase
+	err := json.Unmarshal(response.Body.Payload, &responsePayload)
+	if err != nil {
+		responseData.errors = append(responseData.errors, response)
+		return nil, responseData, err
+	}
+	// user response is ready with either F+1 successes or N-F failures
+	if responsePayload.Success {
+		responseData.successful = append(responseData.successful, response)
+		if len(responseData.successful) >= h.donConfig.F+1 {
+			// return success to the user
+			callbackPayload, err := newSecretsResponse(responseData.request, true, responseData.successful)
+			return callbackPayload, responseData, err
+		}
+	} else {
+		responseData.errors = append(responseData.errors, response)
+		if len(responseData.errors) >= len(h.donConfig.Members)-h.donConfig.F {
+			// return error to the user
+			callbackPayload, err := newSecretsResponse(responseData.request, false, responseData.errors)
+			return callbackPayload, responseData, err
+		}
+	}
+	// not ready to be processed yet
+	return nil, responseData, nil
 }
 
-func (h *functionsHandler) Close() (err error) {
-	if h.allowlist != nil {
-		err = h.allowlist.Close()
+func newSecretsResponse(request *api.Message, success bool, responses []*api.Message) (*handlers.UserCallbackPayload, error) {
+	payload := CombinedSecretsResponse{SecretsResponseBase: SecretsResponseBase{Success: success}, NodeResponses: responses}
+	payloadJson, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
 	}
-	return
+	userResponse := *request
+	userResponse.Body.Receiver = request.Body.Sender
+	userResponse.Body.Payload = payloadJson
+	return &handlers.UserCallbackPayload{Msg: &userResponse, ErrCode: api.NoError, ErrMsg: ""}, nil
+}
+
+func (h *functionsHandler) Start(ctx context.Context) error {
+	return h.StartOnce("FunctionsHandler", func() error {
+		h.lggr.Info("starting FunctionsHandler")
+		if h.allowlist != nil {
+			return h.allowlist.Start(ctx)
+		}
+		return nil
+	})
+}
+
+func (h *functionsHandler) Close() error {
+	return h.StopOnce("FunctionsHandler", func() (err error) {
+		close(h.chStop)
+		if h.allowlist != nil {
+			return h.allowlist.Close()
+		}
+		return nil
+	})
 }
