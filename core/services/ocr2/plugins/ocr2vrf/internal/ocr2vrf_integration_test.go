87f4b867a23747394b4cd1322fe7cf0453847380
ilija <pavlovicilija42@gmail.com>
2023-07-14 09:52:33 -0600

    FIx ocr2vrf integ test and job creation tests to fit validation changes

--- core/services/ocr2/plugins/ocr2vrf/internal/ocr2vrf_integration_test.go
+++ core/services/ocr2/plugins/ocr2vrf/internal/ocr2vrf_integration_test.go
@@ -330,9 +330,9 @@ func TestIntegration_OCR2VRF_ForwarderFlow(t *testing.T) {
 }
 
 func TestIntegration_OCR2VRF(t *testing.T) {
-	if os.Getenv("CI") == "" && os.Getenv("VRF_LOCAL_TESTING") == "" {
-		t.Skip("Skipping test locally.")
-	}
+	//if os.Getenv("CI") == "" && os.Getenv("VRF_LOCAL_TESTING") == "" {
+	//	t.Skip("Skipping test locally.")
+	//}
 	runOCR2VRFTest(t, false)
 }
 
@@ -475,7 +475,6 @@ contractConfigTrackerPollInterval = "15s"
 [relayConfig]
 chainID              	= 1337
 fromBlock               = %d
-sendingKeys             = [%s]
 
 [pluginConfig]
 dkgEncryptionPublicKey 	= "%s"
@@ -490,7 +489,6 @@ linkEthFeedAddress     	= "%s"
 			transmitters[i],
 			useForwarders,
 			blockBeforeConfig.Number().Int64(),
-			sendingKeysString,
 			dkgEncrypters[i].PublicKeyString(),
 			dkgSigners[i].PublicKeyString(),
 			hex.EncodeToString(keyID[:]),
