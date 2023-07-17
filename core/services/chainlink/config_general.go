cb-defi-notifs/smartcontractkit-chainlink-ad0a3279

feature/2268-multiple-secrets-files: adding non-redacted merge validation test

core/services/chainlink/config_general.go
@@ -64,6 +64,35 @@ type GeneralConfigOpts struct {
 	SkipEnv bool
 }
 
+func (opts *GeneralConfigOpts) Setup(configFiles []string, secretsFiles []string) error {
+	configs := []string{}
+	for _, fileName := range configFiles {
+		b, err := os.ReadFile(fileName)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read config file: %s", fileName)
+		}
+		configs = append(configs, string(b))
+	}
+
+	if configTOML := v2.EnvConfig.Get(); configTOML != "" {
+		configs = append(configs, configTOML)
+	}
+
+	opts.ConfigStrings = configs
+
+	secrets := []string{}
+	for _, fileName := range secretsFiles {
+		b, err := os.ReadFile(fileName)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read secrets file: %s", fileName)
+		}
+		secrets = append(secrets, string(b))
+	}
+
+	opts.SecretsStrings = secrets
+	return nil
+}
+
 // parseConfig sets Config from the given TOML string, overriding any existing duplicate Config fields.
 func (o *GeneralConfigOpts) parseConfig(config string) error {
 	var c Config
@@ -95,21 +124,10 @@ func (o *GeneralConfigOpts) parseSecrets(secrets string) error {
 
 // New returns a coreconfig.GeneralConfig for the given options.
 func (o GeneralConfigOpts) New() (GeneralConfig, error) {
-	for _, c := range o.ConfigStrings {
-		err := o.parseConfig(c)
-		if err != nil {
-			return nil, err
-		}
+	err := o.parse()
+	if err != nil {
+		return nil, err
 	}
-
-	for _, s := range o.SecretsStrings {
-		err := o.parseSecrets(s)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	o.Secrets.setDefaults()
 
 	input, err := o.Config.TOMLString()
 	if err != nil {
@@ -150,6 +168,25 @@ func (o GeneralConfigOpts) New() (GeneralConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+func (o *GeneralConfigOpts) parse() (err error) {
+	for _, c := range o.ConfigStrings {
+		err := o.parseConfig(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, s := range o.SecretsStrings {
+		err := o.parseSecrets(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	o.Secrets.setDefaults()
+	return
 }
 
 func (g *generalConfig) EVMConfigs() evmcfg.EVMConfigs {

core/services/chainlink/config_general_test.go
@@ -12,7 +12,61 @@ import (
 	"github.com/stretchr/testify/require"
 
 	v2 "github.com/smartcontractkit/chainlink/v2/core/config/v2"
+	"github.com/smartcontractkit/chainlink/v2/core/store/models"
+	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
+
+type TestSecrets struct {
+	TestConfigSecrets
+}
+
+type TestConfigSecrets struct {
+	Database   TestDatabaseSecrets          `toml:",omitempty"`
+	Explorer   TestExplorerSecrets          `toml:",omitempty"`
+	Password   TestPasswordsSecrets         `toml:",omitempty"`
+	Pyroscope  TestPyroscopeSecrets         `toml:",omitempty"`
+	Prometheus TestPrometheusSecrets        `toml:",omitempty"`
+	Mercury    TestMercurySecrets           `toml:",omitempty"`
+	Threshold  TestThresholdKeyShareSecrets `toml:",omitempty"`
+}
+
+type TestDatabaseSecrets struct {
+	URL                  models.URL
+	BackupURL            models.URL
+	AllowSimplePasswords *bool
+}
+
+type TestExplorerSecrets struct {
+	AccessKey string
+	Secret    string
+}
+
+type TestPasswordsSecrets struct {
+	Keystore string
+	VRF      string
+}
+
+type TestPyroscopeSecrets struct {
+	AuthToken string
+}
+
+type TestPrometheusSecrets struct {
+	AuthToken string
+}
+
+type TestMercurySecrets struct {
+	Credentials map[string]TestMercuryCredentials
+}
+
+type TestMercuryCredentials struct {
+	URL      models.URL
+	Username string
+	Password string
+}
+
+type TestThresholdKeyShareSecrets struct {
+	ThresholdKeyShare string
+}
 
 func TestTOMLGeneralConfig_Defaults(t *testing.T) {
 	config, err := GeneralConfigOpts{}.New()
@@ -122,4 +176,141 @@ func TestConfig_LogSQL(t *testing.T) {
 
 	config.SetLogSQL(false)
 	assert.Equal(t, config.Database().LogSQL(), false)
+}
+
+func TestConfig_SecretsMerging(t *testing.T) {
+	setInFile := "set in config file"
+	allow := false
+	dbURL := "postgres://172.17.0.1:5432/primary"
+	backupDbURL := "postgres://172.17.0.1:5433/replica"
+	testConfigFileContents := Config{
+		Core: v2.Core{
+			RootDir: &setInFile,
+			P2P: v2.P2P{
+				V2: v2.P2PV2{
+					AnnounceAddresses: &[]string{setInFile},
+					ListenAddresses:   &[]string{setInFile},
+				},
+			},
+		},
+	}
+	testSecretsFileContentsComplete := TestSecrets{
+		TestConfigSecrets: TestConfigSecrets{
+			Database: TestDatabaseSecrets{
+				URL:                  *models.MustParseURL(dbURL),
+				BackupURL:            *models.MustParseURL(backupDbURL),
+				AllowSimplePasswords: &allow,
+			},
+			Explorer: TestExplorerSecrets{
+				AccessKey: "EXPLORER_ACCESS_KEY",
+				Secret:    "EXPLORER_TOKEN",
+			},
+			Password: TestPasswordsSecrets{
+				Keystore: "mysecretpassword",
+				VRF:      "mysecretvrfpassword",
+			},
+			Pyroscope: TestPyroscopeSecrets{
+				AuthToken: "PYROSCOPE_TOKEN",
+			},
+			Prometheus: TestPrometheusSecrets{
+				AuthToken: "PROM_TOKEN",
+			},
+			Mercury: TestMercurySecrets{
+				Credentials: map[string]TestMercuryCredentials{
+					"key1": {
+						URL:      *models.MustParseURL("https://mercury.stage.link"),
+						Username: "user",
+						Password: "user_pass",
+					},
+					"key2": {
+						URL:      *models.MustParseURL("https://mercury.stage.link"),
+						Username: "user",
+						Password: "user_pass",
+					},
+				},
+			},
+			Threshold: TestThresholdKeyShareSecrets{
+				ThresholdKeyShare: "THRESHOLD_SECRET",
+			},
+		},
+	}
+
+	additionalMercurySecrets := TestMercurySecrets{
+		Credentials: map[string]TestMercuryCredentials{
+			"key3": {
+				URL:      *models.MustParseURL("https://mercury.stage.link"),
+				Username: "user",
+				Password: "user_pass",
+			},
+			"key4": {
+				URL:      *models.MustParseURL("https://mercury.stage.link"),
+				Username: "user",
+				Password: "user_pass",
+			},
+		},
+	}
+	t.Run("verify secrets merging", func(t *testing.T) {
+		opts := new(GeneralConfigOpts)
+		configFiles := []string{utils.MakeTestFile(t, testConfigFileContents, "test.toml")}
+		secretsFiles := []string{
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Database: testSecretsFileContentsComplete.Database}}, "test_secrets1.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Explorer: testSecretsFileContentsComplete.Explorer}}, "test_secrets2.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Password: testSecretsFileContentsComplete.Password}}, "test_secrets3.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Pyroscope: testSecretsFileContentsComplete.Pyroscope}}, "test_secrets4.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Prometheus: testSecretsFileContentsComplete.Prometheus}}, "test_secrets5.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Mercury: testSecretsFileContentsComplete.Mercury}}, "test_secrets6.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Mercury: additionalMercurySecrets}}, "test_secrets6a.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Threshold: testSecretsFileContentsComplete.Threshold}}, "test_secrets7.toml"),
+		}
+		err := opts.Setup(configFiles, secretsFiles)
+		require.NoErrorf(t, err, "error: %s", err)
+
+		err = opts.parse()
+		require.NoErrorf(t, err, "error: %s", err)
+
+		require.NoErrorf(t, err, "error testing: %s, %s", configFiles, secretsFiles)
+
+		assert.Equal(t, dbURL, opts.Secrets.Database.URL.URL().String())
+		assert.Equal(t, backupDbURL, opts.Secrets.Database.BackupURL.URL().String())
+		assert.Equal(t, testSecretsFileContentsComplete.Explorer.AccessKey, opts.Secrets.Explorer.AccessKey.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Explorer.Secret, opts.Secrets.Explorer.Secret.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Password.Keystore, opts.Secrets.Password.Keystore.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Password.VRF, opts.Secrets.Password.VRF.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Pyroscope.AuthToken, opts.Secrets.Pyroscope.AuthToken.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Prometheus.AuthToken, opts.Secrets.Prometheus.AuthToken.XXXTestingOnlyString())
+		assert.Equal(t, *merge(testSecretsFileContentsComplete.Mercury, additionalMercurySecrets), convertMercurySecrets(opts.Secrets.Mercury))
+		assert.Equal(t, testSecretsFileContentsComplete.Threshold.ThresholdKeyShare, opts.Secrets.Threshold.ThresholdKeyShare.XXXTestingOnlyString())
+	})
+}
+
+func convertMercurySecrets(mercurySecrets v2.MercurySecrets) TestMercurySecrets {
+	testSecrets := TestMercurySecrets{
+		Credentials: make(map[string]TestMercuryCredentials),
+	}
+
+	for key, credentials := range mercurySecrets.Credentials {
+		testCredentials := TestMercuryCredentials{
+			URL:      (models.URL)(*credentials.URL),
+			Username: (string)(*credentials.Username),
+			Password: (string)(*credentials.Password),
+		}
+
+		testSecrets.Credentials[key] = testCredentials
+	}
+
+	return testSecrets
+}
+
+func merge(map1 TestMercurySecrets, map2 TestMercurySecrets) *TestMercurySecrets {
+	combinedMap := make(map[string]TestMercuryCredentials)
+
+	for key, value := range map1.Credentials {
+		combinedMap[key] = value
+	}
+
+	for key, value := range map2.Credentials {
+		combinedMap[key] = value
+	}
+
+	return &TestMercurySecrets{Credentials: combinedMap}
 }

smartcontractkit/chainlink

core/services/chainlink/config_general.go
@@ -23,6 +23,7 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/chains/starknet"
 	"github.com/smartcontractkit/chainlink/v2/core/config"
 	coreconfig "github.com/smartcontractkit/chainlink/v2/core/config"
+	"github.com/smartcontractkit/chainlink/v2/core/config/env"
 	"github.com/smartcontractkit/chainlink/v2/core/config/parse"
 	v2 "github.com/smartcontractkit/chainlink/v2/core/config/toml"
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/p2pkey"
@@ -65,6 +66,35 @@ type GeneralConfigOpts struct {
 	SkipEnv bool
 }
 
+func (opts *GeneralConfigOpts) Setup(configFiles []string, secretsFiles []string) error {
+	configs := []string{}
+	for _, fileName := range configFiles {
+		b, err := os.ReadFile(fileName)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read config file: %s", fileName)
+		}
+		configs = append(configs, string(b))
+	}
+
+	if configTOML := env.Config.Get(); configTOML != "" {
+		configs = append(configs, configTOML)
+	}
+
+	opts.ConfigStrings = configs
+
+	secrets := []string{}
+	for _, fileName := range secretsFiles {
+		b, err := os.ReadFile(fileName)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read secrets file: %s", fileName)
+		}
+		secrets = append(secrets, string(b))
+	}
+
+	opts.SecretsStrings = secrets
+	return nil
+}
+
 // parseConfig sets Config from the given TOML string, overriding any existing duplicate Config fields.
 func (o *GeneralConfigOpts) parseConfig(config string) error {
 	var c Config
@@ -96,21 +126,10 @@ func (o *GeneralConfigOpts) parseSecrets(secrets string) error {
 
 // New returns a coreconfig.GeneralConfig for the given options.
 func (o GeneralConfigOpts) New() (GeneralConfig, error) {
-	for _, c := range o.ConfigStrings {
-		err := o.parseConfig(c)
-		if err != nil {
-			return nil, err
-		}
+	err := o.parse()
+	if err != nil {
+		return nil, err
 	}
-
-	for _, s := range o.SecretsStrings {
-		err := o.parseSecrets(s)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	o.Secrets.setDefaults()
 
 	input, err := o.Config.TOMLString()
 	if err != nil {
@@ -151,6 +170,25 @@ func (o GeneralConfigOpts) New() (GeneralConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+func (o *GeneralConfigOpts) parse() (err error) {
+	for _, c := range o.ConfigStrings {
+		err := o.parseConfig(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, s := range o.SecretsStrings {
+		err := o.parseSecrets(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	o.Secrets.setDefaults()
+	return
 }
 
 func (g *generalConfig) EVMConfigs() evmcfg.EVMConfigs {

core/services/chainlink/config_general_test.go
@@ -12,7 +12,62 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink/v2/core/config/env"
+	"github.com/smartcontractkit/chainlink/v2/core/config/toml"
+	"github.com/smartcontractkit/chainlink/v2/core/store/models"
+	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
+
+type TestSecrets struct {
+	TestConfigSecrets
+}
+
+type TestConfigSecrets struct {
+	Database   TestDatabaseSecrets          `toml:",omitempty"`
+	Explorer   TestExplorerSecrets          `toml:",omitempty"`
+	Password   TestPasswordsSecrets         `toml:",omitempty"`
+	Pyroscope  TestPyroscopeSecrets         `toml:",omitempty"`
+	Prometheus TestPrometheusSecrets        `toml:",omitempty"`
+	Mercury    TestMercurySecrets           `toml:",omitempty"`
+	Threshold  TestThresholdKeyShareSecrets `toml:",omitempty"`
+}
+
+type TestDatabaseSecrets struct {
+	URL                  models.URL
+	BackupURL            models.URL
+	AllowSimplePasswords *bool
+}
+
+type TestExplorerSecrets struct {
+	AccessKey string
+	Secret    string
+}
+
+type TestPasswordsSecrets struct {
+	Keystore string
+	VRF      string
+}
+
+type TestPyroscopeSecrets struct {
+	AuthToken string
+}
+
+type TestPrometheusSecrets struct {
+	AuthToken string
+}
+
+type TestMercurySecrets struct {
+	Credentials map[string]TestMercuryCredentials
+}
+
+type TestMercuryCredentials struct {
+	URL      models.URL
+	Username string
+	Password string
+}
+
+type TestThresholdKeyShareSecrets struct {
+	ThresholdKeyShare string
+}
 
 func TestTOMLGeneralConfig_Defaults(t *testing.T) {
 	config, err := GeneralConfigOpts{}.New()
@@ -122,4 +177,141 @@ func TestConfig_LogSQL(t *testing.T) {
 
 	config.SetLogSQL(false)
 	assert.Equal(t, config.Database().LogSQL(), false)
+}
+
+func TestConfig_SecretsMerging(t *testing.T) {
+	setInFile := "set in config file"
+	allow := false
+	dbURL := "postgres://172.17.0.1:5432/primary"
+	backupDbURL := "postgres://172.17.0.1:5433/replica"
+	testConfigFileContents := Config{
+		Core: toml.Core{
+			RootDir: &setInFile,
+			P2P: toml.P2P{
+				V2: toml.P2PV2{
+					AnnounceAddresses: &[]string{setInFile},
+					ListenAddresses:   &[]string{setInFile},
+				},
+			},
+		},
+	}
+	testSecretsFileContentsComplete := TestSecrets{
+		TestConfigSecrets: TestConfigSecrets{
+			Database: TestDatabaseSecrets{
+				URL:                  *models.MustParseURL(dbURL),
+				BackupURL:            *models.MustParseURL(backupDbURL),
+				AllowSimplePasswords: &allow,
+			},
+			Explorer: TestExplorerSecrets{
+				AccessKey: "EXPLORER_ACCESS_KEY",
+				Secret:    "EXPLORER_TOKEN",
+			},
+			Password: TestPasswordsSecrets{
+				Keystore: "mysecretpassword",
+				VRF:      "mysecretvrfpassword",
+			},
+			Pyroscope: TestPyroscopeSecrets{
+				AuthToken: "PYROSCOPE_TOKEN",
+			},
+			Prometheus: TestPrometheusSecrets{
+				AuthToken: "PROM_TOKEN",
+			},
+			Mercury: TestMercurySecrets{
+				Credentials: map[string]TestMercuryCredentials{
+					"key1": {
+						URL:      *models.MustParseURL("https://mercury.stage.link"),
+						Username: "user",
+						Password: "user_pass",
+					},
+					"key2": {
+						URL:      *models.MustParseURL("https://mercury.stage.link"),
+						Username: "user",
+						Password: "user_pass",
+					},
+				},
+			},
+			Threshold: TestThresholdKeyShareSecrets{
+				ThresholdKeyShare: "THRESHOLD_SECRET",
+			},
+		},
+	}
+
+	additionalMercurySecrets := TestMercurySecrets{
+		Credentials: map[string]TestMercuryCredentials{
+			"key3": {
+				URL:      *models.MustParseURL("https://mercury.stage.link"),
+				Username: "user",
+				Password: "user_pass",
+			},
+			"key4": {
+				URL:      *models.MustParseURL("https://mercury.stage.link"),
+				Username: "user",
+				Password: "user_pass",
+			},
+		},
+	}
+	t.Run("verify secrets merging", func(t *testing.T) {
+		opts := new(GeneralConfigOpts)
+		configFiles := []string{utils.MakeTestFile(t, testConfigFileContents, "test.toml")}
+		secretsFiles := []string{
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Database: testSecretsFileContentsComplete.Database}}, "test_secrets1.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Explorer: testSecretsFileContentsComplete.Explorer}}, "test_secrets2.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Password: testSecretsFileContentsComplete.Password}}, "test_secrets3.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Pyroscope: testSecretsFileContentsComplete.Pyroscope}}, "test_secrets4.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Prometheus: testSecretsFileContentsComplete.Prometheus}}, "test_secrets5.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Mercury: testSecretsFileContentsComplete.Mercury}}, "test_secrets6.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Mercury: additionalMercurySecrets}}, "test_secrets6a.toml"),
+			utils.MakeTestFile(t, TestSecrets{TestConfigSecrets: TestConfigSecrets{Threshold: testSecretsFileContentsComplete.Threshold}}, "test_secrets7.toml"),
+		}
+		err := opts.Setup(configFiles, secretsFiles)
+		require.NoErrorf(t, err, "error: %s", err)
+
+		err = opts.parse()
+		require.NoErrorf(t, err, "error: %s", err)
+
+		require.NoErrorf(t, err, "error testing: %s, %s", configFiles, secretsFiles)
+
+		assert.Equal(t, dbURL, opts.Secrets.Database.URL.URL().String())
+		assert.Equal(t, backupDbURL, opts.Secrets.Database.BackupURL.URL().String())
+		assert.Equal(t, testSecretsFileContentsComplete.Explorer.AccessKey, opts.Secrets.Explorer.AccessKey.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Explorer.Secret, opts.Secrets.Explorer.Secret.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Password.Keystore, opts.Secrets.Password.Keystore.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Password.VRF, opts.Secrets.Password.VRF.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Pyroscope.AuthToken, opts.Secrets.Pyroscope.AuthToken.XXXTestingOnlyString())
+		assert.Equal(t, testSecretsFileContentsComplete.Prometheus.AuthToken, opts.Secrets.Prometheus.AuthToken.XXXTestingOnlyString())
+		assert.Equal(t, *merge(testSecretsFileContentsComplete.Mercury, additionalMercurySecrets), convertMercurySecrets(opts.Secrets.Mercury))
+		assert.Equal(t, testSecretsFileContentsComplete.Threshold.ThresholdKeyShare, opts.Secrets.Threshold.ThresholdKeyShare.XXXTestingOnlyString())
+	})
+}
+
+func convertMercurySecrets(mercurySecrets toml.MercurySecrets) TestMercurySecrets {
+	testSecrets := TestMercurySecrets{
+		Credentials: make(map[string]TestMercuryCredentials),
+	}
+
+	for key, credentials := range mercurySecrets.Credentials {
+		testCredentials := TestMercuryCredentials{
+			URL:      (models.URL)(*credentials.URL),
+			Username: (string)(*credentials.Username),
+			Password: (string)(*credentials.Password),
+		}
+
+		testSecrets.Credentials[key] = testCredentials
+	}
+
+	return testSecrets
+}
+
+func merge(map1 TestMercurySecrets, map2 TestMercurySecrets) *TestMercurySecrets {
+	combinedMap := make(map[string]TestMercuryCredentials)
+
+	for key, value := range map1.Credentials {
+		combinedMap[key] = value
+	}
+
+	for key, value := range map2.Credentials {
+		combinedMap[key] = value
+	}
+
+	return &TestMercurySecrets{Credentials: combinedMap}
 }