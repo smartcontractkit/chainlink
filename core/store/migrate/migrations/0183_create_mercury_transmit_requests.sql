ac0607415560032651f321416adf8f845a13fa34
Martin Liu <martin.liu@smartcontract.com>
2023-07-18 09:17:05 +1000

    Set payload_hash as the PK

--- core/store/migrate/migrations/0183_create_mercury_transmit_requests.sql
+++ core/store/migrate/migrations/0183_create_mercury_transmit_requests.sql
@@ -1,15 +1,14 @@
 -- +goose Up
 
 CREATE TABLE mercury_transmit_requests (
+  payload_hash BYTEA PRIMARY KEY,
   payload BYTEA NOT NULL,
-  payload_hash BYTEA NOT NULL,
   config_digest BYTEA NOT NULL,
   epoch INT NOT NULL,
   round INT NOT NULL,
   extra_hash BYTEA NOT NULL
 );
 
-CREATE UNIQUE INDEX idx_mercury_transmission_requests_payload_hash ON mercury_transmit_requests (payload_hash);
 CREATE INDEX idx_mercury_transmission_requests_epoch_round ON mercury_transmit_requests (epoch DESC, round DESC);
 
 -- +goose Down
