47685a3fc1cb5607bbeea8291c2a0a9398a7eecf
Bolek <1416262+bolekk@users.noreply.github.com>
2023-07-17 22:28:09 -0700

    [Gateway] Handshake improvements (#9822)
    1. Structured challenge
    2. Use GatewayID instead of URL in the header/challenge
    3. Drop requirement for strictly increasing timestamps
    4. Better test coverage

--- core/services/gateway/connector/connector_test.go
+++ core/services/gateway/connector/connector_test.go
@@ -13,12 +13,15 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/connector"
 	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/connector/mocks"
+	"github.com/smartcontractkit/chainlink/v2/core/services/gateway/network"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
 
 const defaultConfig = `
 NodeAddress = "0x68902d681c28119f9b2531473a417088bf008e59"
 DonId = "example_don"
+AuthMinChallengeLen = 10
+AuthTimestampToleranceSec = 5
 
 [[Gateways]]
 Id = "example_gateway"
@@ -36,10 +39,10 @@ func parseTOMLConfig(t *testing.T, tomlConfig string) *connector.ConnectorConfig
 	return &cfg
 }
 
-func newTestConnector(t *testing.T, config *connector.ConnectorConfig) (connector.GatewayConnector, *mocks.Signer, *mocks.GatewayConnectorHandler) {
+func newTestConnector(t *testing.T, config *connector.ConnectorConfig, now time.Time) (connector.GatewayConnector, *mocks.Signer, *mocks.GatewayConnectorHandler) {
 	signer := mocks.NewSigner(t)
 	handler := mocks.NewGatewayConnectorHandler(t)
-	clock := utils.NewFixedClock(time.Now())
+	clock := utils.NewFixedClock(now)
 	connector, err := connector.NewGatewayConnector(config, signer, handler, clock, logger.TestLogger(t))
 	require.NoError(t, err)
 	return connector, signer, handler
@@ -57,7 +60,7 @@ Id = "example_gateway"
 URL = "ws://localhost:8081/node"
 `)
 
-	newTestConnector(t, tomlConfig)
+	newTestConnector(t, tomlConfig, time.Now())
 }
 
 func TestGatewayConnector_NewGatewayConnector_InvalidConfig(t *testing.T) {
@@ -78,10 +81,22 @@ DonId = "example_don"
 
 [[Gateways]]
 Id = "example_gateway"
-URL = "ws://localhost:8081/node"
+URL = "ws://localhost:8081/a"
 
 [[Gateways]]
 Id = "example_gateway"
+URL = "ws://localhost:8081/b"
+`,
+		"duplicate gateway URL": `
+NodeAddress = "0x68902d681c28119f9b2531473a417088bf008e59"
+DonId = "example_don"
+
+[[Gateways]]
+Id = "gateway_A"
+URL = "ws://localhost:8081/node"
+
+[[Gateways]]
+Id = "gateway_B"
 URL = "ws://localhost:8081/node"
 `,
 	}
@@ -101,11 +116,67 @@ URL = "ws://localhost:8081/node"
 func TestGatewayConnector_NewAuthHeader_SignerError(t *testing.T) {
 	t.Parallel()
 
-	connector, signer, _ := newTestConnector(t, parseTOMLConfig(t, defaultConfig))
+	connector, signer, _ := newTestConnector(t, parseTOMLConfig(t, defaultConfig), time.Now())
 	signer.On("Sign", mock.Anything).Return(nil, errors.New("cannot sign"))
 
 	url, err := url.Parse("ws://localhost:8081/node")
 	require.NoError(t, err)
 	_, err = connector.NewAuthHeader(url)
 	require.Error(t, err)
+}
+
+func TestGatewayConnector_NewAuthHeader_Success(t *testing.T) {
+	t.Parallel()
+
+	testSignature := make([]byte, network.HandshakeSignatureLen)
+	testSignature[1] = 0xfa
+	connector, signer, _ := newTestConnector(t, parseTOMLConfig(t, defaultConfig), time.Now())
+	signer.On("Sign", mock.Anything).Return(testSignature, nil)
+	url, err := url.Parse("ws://localhost:8081/node")
+	require.NoError(t, err)
+
+	header, err := connector.NewAuthHeader(url)
+	require.NoError(t, err)
+	require.Equal(t, testSignature, header[len(header)-65:])
+}
+
+func TestGatewayConnector_ChallengeResponse(t *testing.T) {
+	t.Parallel()
+
+	testSignature := make([]byte, network.HandshakeSignatureLen)
+	testSignature[1] = 0xfa
+	now := time.Now()
+	connector, signer, _ := newTestConnector(t, parseTOMLConfig(t, defaultConfig), now)
+	signer.On("Sign", mock.Anything).Return(testSignature, nil)
+	url, err := url.Parse("ws://localhost:8081/node")
+	require.NoError(t, err)
+
+	challenge := network.ChallengeElems{
+		Timestamp:      uint32(now.Unix()),
+		GatewayId:      "example_gateway",
+		ChallengeBytes: []byte("1234567890"),
+	}
+
+	// valid
+	signature, err := connector.ChallengeResponse(url, network.PackChallenge(&challenge))
+	require.NoError(t, err)
+	require.Equal(t, testSignature, signature)
+
+	// invalid timestamp
+	badChallenge := challenge
+	badChallenge.Timestamp += 100
+	_, err = connector.ChallengeResponse(url, network.PackChallenge(&badChallenge))
+	require.Equal(t, network.ErrAuthInvalidTimestamp, err)
+
+	// too short
+	badChallenge = challenge
+	badChallenge.ChallengeBytes = []byte("aabb")
+	_, err = connector.ChallengeResponse(url, network.PackChallenge(&badChallenge))
+	require.Equal(t, network.ErrChallengeTooShort, err)
+
+	// invalid GatewayId
+	badChallenge = challenge
+	badChallenge.GatewayId = "wrong"
+	_, err = connector.ChallengeResponse(url, network.PackChallenge(&badChallenge))
+	require.Equal(t, network.ErrAuthInvalidGateway, err)
 }
