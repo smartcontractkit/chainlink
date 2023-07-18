b99253802b69bc14df9676b2fda9de96a1e4defa
Chris <104409744+vreff@users.noreply.github.com>
2023-07-18 12:08:33 +0200

    Make trustedblockhash store extend blockhashstore so that no changes to the coordinator are required.

--- contracts/src/v0.8/dev/vrf/TrustedBlockhashStore.sol
+++ contracts/src/v0.8/dev/vrf/TrustedBlockhashStore.sol
@@ -3,23 +3,13 @@ pragma solidity 0.8.6;
 
 import "../../ChainSpecificUtil.sol";
 import "../../ConfirmedOwner.sol";
+import "./BlockhashStore.sol";
 
-/**
- * @title BlockhashStore
- * @notice This contract provides a way to access blockhashes older than
- *   the 256 block limit imposed by the BLOCKHASH opcode.
- *   You may assume that any blockhash stored by the contract is correct.
- *   Note that the contract depends on the format of serialized Ethereum
- *   blocks. If a future hardfork of Ethereum changes that format, the
- *   logic in this contract may become incorrect and an updated version
- *   would have to be deployed.
- */
-contract TrustedBlockhashStore is ConfirmedOwner {
+contract TrustedBlockhashStore is ConfirmedOwner, BlockhashStore {
   error NotInWhitelist();
   error InvalidTrustedBlockhashes();
   error InvalidRecentBlockhash();
 
-  mapping(uint256 => bytes32) public s_blockhashes;
   mapping(address => bool) public s_whitelistStatus;
   address[] public s_whitelist;
 
@@ -43,17 +33,6 @@ contract TrustedBlockhashStore is ConfirmedOwner {
     for (uint256 i = 0; i < whitelist.length; i++) {
       s_whitelistStatus[whitelist[i]] = true;
     }
-  }
-
-  /**
-   * @notice stores blockhash of a given block, assuming it is available through BLOCKHASH
-   * @param n the number of the block whose blockhash should be stored
-   */
-
-  function store(uint256 n) public {
-    bytes32 h = ChainSpecificUtil.getBlockhash(uint64(n));
-    require(h != 0x0, "blockhash(n) failed");
-    s_blockhashes[n] = h;
   }
 
   /**
@@ -83,15 +62,5 @@ contract TrustedBlockhashStore is ConfirmedOwner {
     for (uint256 i = 0; i < blockNums.length; i++) {
       s_blockhashes[blockNums[i]] = blockhashes[i];
     }
-  }
-
-  /**
-   * @notice gets a blockhash from the store. If no hash is known, this function reverts.
-   * @param n the number of the block whose blockhash should be returned
-   */
-  function getBlockhash(uint256 n) external view returns (bytes32) {
-    bytes32 h = s_blockhashes[n];
-    require(h != 0x0, "blockhash not found in store");
-    return h;
   }
 }
