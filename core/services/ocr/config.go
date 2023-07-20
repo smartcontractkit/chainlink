ec54870e6e00f15bb58dd65599fff90079a46e22
simsonraj <simsonraj.easvarasakthi@smartcontract.com>
2023-07-20 17:53:05 +0530

    Revert "Base(Mainnet) + Optimism hotfix"
    This reverts commit a13e8487eff0935b3eacf78bc020288842028c96.

--- core/services/ocr/config.go
+++ core/services/ocr/config.go
@@ -4,7 +4,6 @@ import (
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting/types"
 
 	evmconfig "github.com/smartcontractkit/chainlink/v2/core/chains/evm/config"
-	"github.com/smartcontractkit/chainlink/v2/core/config"
 	"github.com/smartcontractkit/chainlink/v2/core/services/job"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pg"
 )
@@ -19,7 +18,7 @@ func toLocalConfig(cfg ValidationConfig, evmOcrConfig evmconfig.OCR, insecureCfg
 	lc := ocrtypes.LocalConfig{
 		BlockchainTimeout:                      concreteSpec.BlockchainTimeout.Duration(),
 		ContractConfigConfirmations:            concreteSpec.ContractConfigConfirmations,
-		SkipContractConfigConfirmations:        cfg.ChainType().IsL2() || cfg.ChainType() == config.ChainOptimismBedrock, //hotfix to allow optimism & Base to skip contract confirmation checks in OCR, TODO: fix this after redfining L2 chains & finality
+		SkipContractConfigConfirmations:        cfg.ChainType().IsL2(),
 		ContractConfigTrackerPollInterval:      concreteSpec.ContractConfigTrackerPollInterval.Duration(),
 		ContractConfigTrackerSubscribeInterval: concreteSpec.ContractConfigTrackerSubscribeInterval.Duration(),
 		ContractTransmitterTransmitTimeout:     concreteSpec.ContractTransmitterTransmitTimeout.Duration(),
