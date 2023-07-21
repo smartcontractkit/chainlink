8c230d845eea409e396e05bd3acde313e8a30100
ilija <pavlovicilija42@gmail.com>
2023-07-21 17:46:02 +0200

    Improve delete-chain script by reducing db calls

--- core/cmd/shell_local.go
+++ core/cmd/shell_local.go
@@ -943,19 +943,20 @@ func (s *Shell) CleanupChainTables(c *cli.Context) error {
 
 	defer db.Close()
 
+	tablesToDeleteFromQuery := "SELECT table_name FROM information_schema.columns WHERE \"column_name\"=$1;"
 	// Delete rows from each table based on the chain_id.
-	if strings.EqualFold("EVM", c.String("chaintype")) {
-		tables, err := getTablesContainingColumn(db, "evm_chain_id")
-		if err != nil {
-			return s.errorOut(errors.Wrap(err, "Failed to get tables"))
+	if strings.EqualFold("EVM", c.String("chainType")) {
+		var tables []string
+		if err = db.Select(&tables, tablesToDeleteFromQuery, "evm_chain_id"); err != nil {
+			return err
 		}
 		for _, tableName := range tables {
-			query := fmt.Sprintf("DELETE FROM %s WHERE evm_chain_id = $1", tableName)
-			_, err := db.Exec(query, c.Int("chain-id"))
+			query := fmt.Sprintf("DELETE FROM %s WHERE \"evm_chain_id\"=$1;", tableName)
+			_, err = db.Exec(query, c.Int("id"))
 			if err != nil {
 				fmt.Printf("Error deleting rows from %s: %v\n", tableName, err)
 			} else {
-				fmt.Printf("Rows with chain_id %d deleted from %s.\n", c.Int("chain-id"), tableName)
+				fmt.Printf("Rows with chain_id %d deleted from %s.\n", c.Int("id"), tableName)
 			}
 		}
 	} else {
@@ -1099,44 +1100,4 @@ func insertFixtures(dbURL url.URL, pathToFixtures string) (err error) {
 	}
 	_, err = db.Exec(string(fixturesSQL))
 	return err
-}
-
-func getTablesContainingColumn(db *sqlx.DB, columnName string) ([]string, error) {
-	rows, err := db.Query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var tables []string
-	for rows.Next() {
-		var tableName string
-		if err = rows.Scan(&tableName); err != nil {
-			return nil, err
-		}
-		tables = append(tables, tableName)
-	}
-
-	var tablesWithColumn []string
-	for _, table := range tables {
-		has, err := hasColumn(db, table, columnName)
-		if err != nil {
-			return nil, err
-		}
-		if has {
-			tablesWithColumn = append(tablesWithColumn, table)
-		}
-	}
-	return tablesWithColumn, nil
-}
-
-func hasColumn(db *sqlx.DB, table, columnName string) (bool, error) {
-	query := fmt.Sprintf("SELECT column_name FROM information_schema.columns WHERE table_name='%s' AND column_name='%s'", table, columnName)
-	rows, err := db.Query(query)
-	if err != nil {
-		return false, err
-	}
-	defer rows.Close()
-
-	return rows.Next(), nil
 }
