948b96df5149b9af4277821c02310159881a90c4
Justin Kaseman <justinkaseman@live.com>
2023-07-22 00:30:22 -0400

    [Functions] (feat): Terms of Service Access Control (#9855)
    * Reduce contract sizes
    
    * (feat): Add ToS allow list
    
    * Amend interface function docs

--- contracts/src/v0.8/functions/dev/1_0_0/interfaces/IFunctionsRouter.sol
+++ contracts/src/v0.8/functions/dev/1_0_0/interfaces/IFunctionsRouter.sol
@@ -8,6 +8,13 @@ import {IRouterBase} from "./IRouterBase.sol";
  */
 interface IFunctionsRouter is IRouterBase {
   /**
+   * @notice The identifier of the route to retrieve the address of the access control contract
+   * The access control contract controls which accounts can manage subscriptions
+   * @return id - bytes32 id that can be passed to the "getContractById" of the Router
+   */
+  function getAllowListId() external pure returns (bytes32);
+
+  /**
    * @notice The fee that will be paid to the Router owner for operating the network
    * @return fee Cost in Juels (1e18) of LINK
    */
