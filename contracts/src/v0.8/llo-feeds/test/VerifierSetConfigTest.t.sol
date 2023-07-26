8338ba16fa6b76dd7f6754a4286d9581f050dcf5
Michael Fletcher <michael.fletcher@smartcontract.com>
2023-07-26 19:51:45 +0100

    Added storing of latest digest

--- contracts/src/v0.8/llo-feeds/test/VerifierSetConfigTest.t.sol
+++ contracts/src/v0.8/llo-feeds/test/VerifierSetConfigTest.t.sol
@@ -7,6 +7,7 @@ import {VerifierProxy} from "../VerifierProxy.sol";
 
 contract VerifierSetConfigTest is BaseTest {
   function setUp() public virtual override {
+    persistConfig = true;
     BaseTest.setUp();
   }
 
@@ -107,6 +108,67 @@ contract VerifierSetConfigTest is BaseTest {
 
   function test_correctlyUpdatesTheConfig() public {
     Signer[] memory signers = _getSigners(MAX_ORACLES);
+    bytes memory onchainConfigBytes = bytes("onchain config");
+    bytes memory offchainConfigBytes = bytes("offchain config");
+
+    s_verifierProxy.initializeVerifier(address(s_verifier));
+    s_verifier.setConfig(
+      FEED_ID,
+      _getSignerAddresses(signers),
+      s_offchaintransmitters,
+      FAULT_TOLERANCE,
+      onchainConfigBytes,
+      VERIFIER_VERSION,
+      offchainConfigBytes
+    );
+
+    bytes32 expectedConfigDigest = _configDigestFromConfigData(
+      FEED_ID,
+      address(s_verifier),
+      1,
+      _getSignerAddresses(signers),
+      s_offchaintransmitters,
+      FAULT_TOLERANCE,
+      onchainConfigBytes,
+      VERIFIER_VERSION,
+      offchainConfigBytes
+    );
+
+    //change to an EOA to request the config
+    changePrank(tx.origin);
+    Verifier.ActiveConfig memory latestConfig = s_verifier.latestConfig(FEED_ID);
+    changePrank(msg.sender);
+
+    //check the latest config matches
+    assertEq(latestConfig.previousConfigBlockNumber, 0);
+    assertEq(latestConfig.configDigest, expectedConfigDigest);
+    assertEq(latestConfig.configCount, 1);
+    for (uint256 i; i < signers.length; i++) {
+      assertEq(latestConfig.signers[i], _getSignerAddresses(signers)[i]);
+    }
+    for (uint256 i; i < s_offchaintransmitters.length; i++) {
+      assertEq(latestConfig.transmitters[i], s_offchaintransmitters[i]);
+    }
+    assertEq(latestConfig.f, FAULT_TOLERANCE);
+    assertEq(latestConfig.onchainConfig, onchainConfigBytes);
+    assertEq(latestConfig.offchainConfigVersion, VERIFIER_VERSION);
+    assertEq(latestConfig.offchainConfig, offchainConfigBytes);
+
+    (uint32 configCount, uint32 blockNumber, bytes32 configDigest) = s_verifier.latestConfigDetails(FEED_ID);
+    assertEq(configCount, 1);
+    assertEq(blockNumber, block.number);
+    assertEq(configDigest, expectedConfigDigest);
+
+    (bool scanLogs, bytes32 configDigestTwo, uint32 epoch) = s_verifier.latestConfigDigestAndEpoch(FEED_ID);
+    assertEq(scanLogs, false);
+    assertEq(configDigestTwo, expectedConfigDigest);
+    assertEq(epoch, 0);
+  }
+
+  function test_setConfigUpdatesLatest() public {
+    Signer[] memory signers = _getSigners(MAX_ORACLES);
+    bytes memory onchainConfigBytes = bytes("onchain config");
+    bytes memory offchainConfigBytes = bytes("offchain config");
 
     s_verifierProxy.initializeVerifier(address(s_verifier));
     s_verifier.setConfig(
@@ -119,27 +181,47 @@ contract VerifierSetConfigTest is BaseTest {
       bytes("")
     );
 
-    bytes32 expectedConfigDigest = _configDigestFromConfigData(
+    s_verifier.setConfig(
       FEED_ID,
-      address(s_verifier),
-      1,
       _getSignerAddresses(signers),
       s_offchaintransmitters,
       FAULT_TOLERANCE,
-      bytes(""),
+      onchainConfigBytes,
       VERIFIER_VERSION,
-      bytes("")
+      offchainConfigBytes
     );
 
-    (uint32 configCount, uint32 blockNumber, bytes32 configDigest) = s_verifier.latestConfigDetails(FEED_ID);
-    assertEq(configCount, 1);
-    assertEq(blockNumber, block.number);
-    assertEq(configDigest, expectedConfigDigest);
+    bytes32 expectedConfigDigest = _configDigestFromConfigData(
+      FEED_ID,
+      address(s_verifier),
+      2,
+      _getSignerAddresses(signers),
+      s_offchaintransmitters,
+      FAULT_TOLERANCE,
+      onchainConfigBytes,
+      VERIFIER_VERSION,
+      offchainConfigBytes
+    );
 
-    (bool scanLogs, bytes32 configDigestTwo, uint32 epoch) = s_verifier.latestConfigDigestAndEpoch(FEED_ID);
-    assertEq(scanLogs, false);
-    assertEq(configDigestTwo, expectedConfigDigest);
-    assertEq(epoch, 0);
+    //change to an EOA to request the config
+    changePrank(tx.origin);
+    Verifier.ActiveConfig memory latestConfig = s_verifier.latestConfig(FEED_ID);
+    changePrank(msg.sender);
+
+    //check the latest config matches
+    assertGt(latestConfig.previousConfigBlockNumber, 0);
+    assertEq(latestConfig.configDigest, expectedConfigDigest);
+    assertEq(latestConfig.configCount, 2);
+    for (uint256 i; i < signers.length; i++) {
+      assertEq(latestConfig.signers[i], _getSignerAddresses(signers)[i]);
+    }
+    for (uint256 i; i < s_offchaintransmitters.length; i++) {
+      assertEq(latestConfig.transmitters[i], s_offchaintransmitters[i]);
+    }
+    assertEq(latestConfig.f, FAULT_TOLERANCE);
+    assertEq(latestConfig.onchainConfig, onchainConfigBytes);
+    assertEq(latestConfig.offchainConfigVersion, VERIFIER_VERSION);
+    assertEq(latestConfig.offchainConfig, offchainConfigBytes);
   }
 }
 
@@ -226,5 +308,44 @@ contract VerifierSetConfigWhenThereAreMultipleDigestsTest is BaseTestWithMultipl
     assertEq(configCount, s_numConfigsSet + 1);
     assertEq(blockNumber, block.number);
     assertEq(configDigest, expectedConfigDigest);
+  }
+}
+
+contract VerifierProxyTestSetConfigWithoutPersist is BaseTest {
+  function setUp() public override {
+    BaseTest.setUp();
+  }
+
+  function test_setConfigDoesNotPersist() public {
+    Signer[] memory signers = _getSigners(MAX_ORACLES);
+    bytes memory onchainConfigBytes = bytes("onchain config");
+    bytes memory offchainConfigBytes = bytes("offchain config");
+
+    s_verifierProxy.initializeVerifier(address(s_verifier));
+    s_verifier.setConfig(
+      FEED_ID,
+      _getSignerAddresses(signers),
+      s_offchaintransmitters,
+      FAULT_TOLERANCE,
+      onchainConfigBytes,
+      VERIFIER_VERSION,
+      offchainConfigBytes
+    );
+
+    //change to an EOA to request the config
+    changePrank(tx.origin);
+    Verifier.ActiveConfig memory latestConfig = s_verifier.latestConfig(FEED_ID);
+    changePrank(msg.sender);
+
+    //check the latest config is null
+    assertEq(latestConfig.previousConfigBlockNumber, 0);
+    assertEq(latestConfig.configDigest, bytes32(""));
+    assertEq(latestConfig.configCount, 0);
+    assertEq(latestConfig.signers.length, 0);
+    assertEq(latestConfig.transmitters.length, 0);
+    assertEq(latestConfig.f, 0);
+    assertEq(latestConfig.onchainConfig, bytes(""));
+    assertEq(latestConfig.offchainConfigVersion, 0);
+    assertEq(latestConfig.offchainConfig, bytes(""));
   }
 }
