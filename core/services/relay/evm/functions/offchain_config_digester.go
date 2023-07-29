7e2bf6c7107ae8b50bc8928ad0629eb52586cc07
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2023-07-29 11:05:19 -0700

    [Functions] Don't use a static EVM Digester in Functions Digester
    Contract address will soon be dynamic so we can't use a single base digester. Let's create one on each call.

--- core/services/relay/evm/functions/offchain_config_digester.go
+++ core/services/relay/evm/functions/offchain_config_digester.go
@@ -5,12 +5,13 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/smartcontractkit/libocr/offchainreporting2plus/chains/evmutil"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/libocr/offchainreporting2/chains/evmutil"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 )
 
 var (
-	_                     types.OffchainConfigDigester = FunctionsOffchainConfigDigester{}
+	_                     types.OffchainConfigDigester = functionsOffchainConfigDigester{}
 	FunctionsDigestPrefix                              = types.ConfigDigestPrefixEVM
 	// In order to support multiple OCR plugins with a single jobspec & OCR2Base contract, each plugin must have a unique config digest.
 	// This is accomplished by overriding the single config digest from the contract with a unique prefix for each plugin via this custom offchain digester & config poller.
@@ -18,19 +19,33 @@ var (
 	S4DigestPrefix        = types.ConfigDigestPrefix(8)
 )
 
-type FunctionsOffchainConfigDigester struct {
-	PluginType   FunctionsPluginType
-	BaseDigester evmutil.EVMOffchainConfigDigester
+type functionsOffchainConfigDigester struct {
+	pluginType      FunctionsPluginType
+	chainID         uint64
+	contractAddress common.Address
 }
 
-func (d FunctionsOffchainConfigDigester) ConfigDigest(cc types.ContractConfig) (types.ConfigDigest, error) {
-	configDigest, err := d.BaseDigester.ConfigDigest(cc)
+func NewFunctionsOffchainConfigDigester(pluginType FunctionsPluginType, chainID uint64, contractAddress common.Address) *functionsOffchainConfigDigester {
+	return &functionsOffchainConfigDigester{
+		pluginType:      pluginType,
+		chainID:         chainID,
+		contractAddress: contractAddress,
+	}
+}
+
+func (d functionsOffchainConfigDigester) ConfigDigest(cc types.ContractConfig) (types.ConfigDigest, error) {
+	baseDigester := evmutil.EVMOffchainConfigDigester{
+		ChainID:         d.chainID,
+		ContractAddress: d.contractAddress,
+	}
+
+	configDigest, err := baseDigester.ConfigDigest(cc)
 	if err != nil {
 		return types.ConfigDigest{}, err
 	}
 
 	var prefix types.ConfigDigestPrefix
-	switch d.PluginType {
+	switch d.pluginType {
 	case FunctionsPlugin:
 		prefix = FunctionsDigestPrefix
 	case ThresholdPlugin:
@@ -46,8 +61,8 @@ func (d FunctionsOffchainConfigDigester) ConfigDigest(cc types.ContractConfig) (
 	return configDigest, nil
 }
 
-func (d FunctionsOffchainConfigDigester) ConfigDigestPrefix() (types.ConfigDigestPrefix, error) {
-	switch d.PluginType {
+func (d functionsOffchainConfigDigester) ConfigDigestPrefix() (types.ConfigDigestPrefix, error) {
+	switch d.pluginType {
 	case FunctionsPlugin:
 		return FunctionsDigestPrefix, nil
 	case ThresholdPlugin:
@@ -55,6 +70,6 @@ func (d FunctionsOffchainConfigDigester) ConfigDigestPrefix() (types.ConfigDiges
 	case S4Plugin:
 		return S4DigestPrefix, nil
 	default:
-		return 0, fmt.Errorf("unknown plugin type: %v", d.PluginType)
+		return 0, fmt.Errorf("unknown plugin type: %v", d.pluginType)
 	}
 }
