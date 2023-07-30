e55151f3e95f8cc86457d64ddac15abc13e44e8d
Ryan Nelson <130587585+ryannelsonsmartcontract@users.noreply.github.com>
2023-07-30 14:21:22 -0400

    Functions Gas Golf (#9960)
    * Removed unnecessary _nonReentrant call from oracleWithdraw
    
    * Changed access from internal to external for ownerWithdraw
    
    * More reentrancy guard removal
    
    * Set FunctionsClient and Routable routers to immutable
    
    * Keep subscription methods protected against reentrancy
    
    * (test): Add reentrancy unit tests
    
    * Remove reentrancy lock because of CEI pattern's protection (-8500 gas)
    
    * Amend line from merging develop
    
    * Regenerate geth wrappers
    
    * (test): Lower test's sub funding to prevent flakes from gasprice fluctuations
    
    * (test): Pin tx.gasprice for reentrancy test
    
    ---------
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

--- contracts/src/v0.8/functions/dev/1_0_0/FunctionsClient.sol
+++ contracts/src/v0.8/functions/dev/1_0_0/FunctionsClient.sol
@@ -10,7 +10,7 @@ import {IFunctionsClient} from "./interfaces/IFunctionsClient.sol";
  * @notice Contract writers can inherit this contract in order to create Chainlink Functions requests
  */
 abstract contract FunctionsClient is IFunctionsClient {
-  IFunctionsRouter internal s_router;
+  IFunctionsRouter internal immutable s_router;
 
   event RequestSent(bytes32 indexed id);
   event RequestFulfilled(bytes32 indexed id);
@@ -18,7 +18,7 @@ abstract contract FunctionsClient is IFunctionsClient {
   error OnlyRouterCanFufill();
 
   constructor(address router) {
-    setRouter(router);
+    s_router = IFunctionsRouter(router);
   }
 
   /**
@@ -73,14 +73,6 @@ abstract contract FunctionsClient is IFunctionsClient {
     bytes memory err
   ) external override onlyRouter {
     fulfillRequest(requestId, response, err);
-  }
-
-  /**
-   * @notice Sets the stored router address
-   * @param router The address of Functions router contract
-   */
-  function setRouter(address router) internal {
-    s_router = IFunctionsRouter(router);
   }
 
   /**
