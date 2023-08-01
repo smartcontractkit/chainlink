11d9e80dcef5d12ce131ae7cc3e05bf73cfa527f
anirudhwarrier <anirudhwarrier@users.noreply.github.com>
2023-08-01 15:15:18 +0400

    remove mockserver from automation test setup

--- integration-tests/smoke/automation_test.go
+++ integration-tests/smoke/automation_test.go
@@ -18,8 +18,6 @@ import (
 	"github.com/smartcontractkit/chainlink-env/logging"
 	"github.com/smartcontractkit/chainlink-env/pkg/helm/chainlink"
 	eth "github.com/smartcontractkit/chainlink-env/pkg/helm/ethereum"
-	"github.com/smartcontractkit/chainlink-env/pkg/helm/mockserver"
-	mockservercfg "github.com/smartcontractkit/chainlink-env/pkg/helm/mockserver-cfg"
 	"github.com/smartcontractkit/chainlink-testing-framework/blockchain"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils"
 
@@ -816,8 +814,6 @@ func setupAutomationTest(
 		NamespacePrefix: fmt.Sprintf("smoke-automation-%s-%s", testName, strings.ReplaceAll(strings.ToLower(network.Name), " ", "-")),
 		Test:            t,
 	}).
-		AddHelm(mockservercfg.New(nil)).
-		AddHelm(mockserver.New(nil)).
 		AddHelm(evmConfig).
 		AddHelmCharts(cd)
 	err = testEnvironment.Run()
