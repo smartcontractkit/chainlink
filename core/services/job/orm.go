9a8d23b03714984c97026b0010fed92149cbb444
george-dorin <george.dorin@smartcontract.com>
2023-08-02 15:31:11 +0300

    Fix lint

--- core/services/job/orm.go
+++ core/services/job/orm.go
@@ -683,7 +683,7 @@ func (o *orm) FindJobs(offset, limit int) (jobs []Job, count int, err error) {
 			return err
 		}
 		for i := range jobs {
-			multierr.Combine(err, o.LoadEnvConfigVars(&jobs[i]))
+			err = multierr.Combine(err, o.LoadEnvConfigVars(&jobs[i]))
 		}
 		return nil
 	})
