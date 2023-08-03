07c33fde40f680f082682d64935028326e513a5f
Fergal Gribben <fergal.gribben@smartcontract.com>
2023-08-03 03:22:51 +0100

    Fix imports

--- core/services/relay/evm/mercury_provider.go
+++ core/services/relay/evm/mercury_provider.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"errors"
 
-	relaymercury "github.com/smartcontractkit/chainlink-relay/pkg/reportingplugins/mercury"
-	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 	"golang.org/x/exp/maps"
+
+	relaymercury "github.com/smartcontractkit/chainlink-relay/pkg/reportingplugins/mercury"
+	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
 
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services"
--- core/services/relay/evm/ocr2keeper.go
+++ core/services/relay/evm/ocr2keeper.go
@@ -9,12 +9,13 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
-	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
 	"github.com/smartcontractkit/libocr/gethwrappers2/ocr2aggregator"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/chains/evmutil"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
 	"github.com/smartcontractkit/ocr2keepers/pkg/v3/plugin"
 	"github.com/smartcontractkit/sqlx"
+
+	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
 
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 
