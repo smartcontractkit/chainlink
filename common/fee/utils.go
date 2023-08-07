b065dd9e08159a8acecbeeee91e40a226cea3913
Yong Kang <chiayongkang@hotmail.com>
2023-08-07 11:52:19 -0700

    changed error formatting type

--- common/fee/utils.go
+++ common/fee/utils.go
@@ -12,7 +12,7 @@ func ApplyMultiplier(feeLimit uint32, multiplier float32) (uint32, error) {
 	result := decimal.NewFromBigInt(big.NewInt(0).SetUint64(uint64(feeLimit)), 0).Mul(decimal.NewFromFloat32(multiplier)).IntPart()
 
 	if result > math.MaxUint32 {
-		return 0, errors.Errorf("integer overflow when applying multiplier to fee limit: %d", feeLimit)
+		return 0, errors.Errorf("integer overflow when applying multiplier of %f to fee limit of %d", multiplier, feeLimit)
 	}
 	return uint32(result), nil
 }
