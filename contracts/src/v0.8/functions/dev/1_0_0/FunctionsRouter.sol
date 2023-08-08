a4bd203119d8bbb50b9707e2a78300b4627611a9
Justin Kaseman <justinkaseman@live.com>
2023-08-08 11:32:20 -0400

    (fix): callback gasUsed should not count gasForCallExactCheck (#10104)
    * (fix): callback gasUsed should not count gasForCallExactCheck
    
    * (test): Add FaultyClient test
    
    * rename testhelper to FunctionsClientWithEmptyCallback

--- contracts/src/v0.8/functions/dev/1_0_0/FunctionsRouter.sol
+++ contracts/src/v0.8/functions/dev/1_0_0/FunctionsRouter.sol
@@ -427,8 +427,9 @@ contract FunctionsRouter is IFunctionsRouter, FunctionsSubscriptions, Pausable, 
       }
       // call and report whether we succeeded
       // call(gas,addr,value,argsOffset,argsLength,retOffset,retLength)
+      let gasBeforeCall := gas()
       success := call(callbackGasLimit, client, 0, add(encodedCallback, 0x20), mload(encodedCallback), 0, 0)
-      gasUsed := sub(g, gas())
+      gasUsed := sub(gasBeforeCall, gas())
 
       // limit our copy to MAX_CALLBACK_RETURN_BYTES bytes
       let toCopy := returndatasize()
