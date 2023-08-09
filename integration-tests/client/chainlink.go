f6cbf1efd6ca34cc4551ec998b0d24a4bed5a724
Damjan Smickovski <smickovskid@outlook.com>
2023-08-09 10:48:09 +0200

    Removed newline

--- integration-tests/client/chainlink.go
+++ integration-tests/client/chainlink.go
@@ -24,8 +24,7 @@ const (
 
 var (
 	// OneLINK representation of a single LINK token
-	OneLINK = big.NewFloat(1e18)
-
+	OneLINK           = big.NewFloat(1e18)
 	mapKeyTypeToChain = map[string]string{
 		"evm":      "eTHKeys",
 		"solana":   "encryptedSolanaKeys",
