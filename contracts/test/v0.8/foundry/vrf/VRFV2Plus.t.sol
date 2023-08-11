d48fa61a426bee563bccc892d5093076ee66f46a
Chris <104409744+vreff@users.noreply.github.com>
2023-08-11 13:34:47 -0400

    prettier

--- contracts/test/v0.8/foundry/vrf/VRFV2Plus.t.sol
+++ contracts/test/v0.8/foundry/vrf/VRFV2Plus.t.sol
@@ -256,10 +256,7 @@ contract VRFV2Plus is BaseTest {
     VmSafe.Log[] memory entries = vm.getRecordedLogs();
     assertEq(entries[0].topics[1], bytes32(uint256(requestId)));
     assertEq(entries[0].topics[2], bytes32(uint256(subId)));
-    (uint256 loggedOutputSeed, , bool loggedSuccess) = abi.decode(
-      entries[0].data,
-      (uint256, uint256, bool)
-    );
+    (uint256 loggedOutputSeed, , bool loggedSuccess) = abi.decode(entries[0].data, (uint256, uint256, bool));
     assertEq(loggedOutputSeed, outputSeed);
     assertEq(loggedSuccess, true);
 
@@ -376,10 +373,7 @@ contract VRFV2Plus is BaseTest {
     VmSafe.Log[] memory entries = vm.getRecordedLogs();
     assertEq(entries[0].topics[1], bytes32(uint256(requestId)));
     assertEq(entries[0].topics[2], bytes32(uint256(subId)));
-    (uint256 loggedOutputSeed, , bool loggedSuccess) = abi.decode(
-      entries[0].data,
-      (uint256, uint256, bool)
-    );
+    (uint256 loggedOutputSeed, , bool loggedSuccess) = abi.decode(entries[0].data, (uint256, uint256, bool));
     assertEq(loggedOutputSeed, outputSeed);
     assertEq(loggedSuccess, true);
 
