6641fe103766d894a0cf0dacfbc4c09d14075bcc
skudasov <f4hrenh9it@gmail.com>
2023-08-12 16:14:29 +0200

    update wasp

--- integration-tests/docker/test_env/test_env.go
+++ integration-tests/docker/test_env/test_env.go
@@ -102,7 +102,9 @@ func (te *CLClusterTestEnv) StartClNodes(nodeConfig *chainlink.Config, count int
 			}
 			n := NewClNode([]string{te.Network.Name}, nodeConfig,
 				WithNodeContainerName(nodeContainerName),
-				WithDbContainerName(dbContainerName))
+				WithDbContainerName(dbContainerName),
+				WithLogWatch(te.LogWatch),
+			)
 			err := n.StartContainer()
 			if err != nil {
 				mu.Lock()
