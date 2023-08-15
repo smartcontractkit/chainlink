e41cc410671e4dd8acadc048cdaf68f2135cf807
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-08-15 14:40:54 +0100

    cleanup

--- core/services/ocr2/plugins/ocr2keeper/evm21/payload_builder.go
+++ core/services/ocr2/plugins/ocr2keeper/evm21/payload_builder.go
@@ -11,7 +11,6 @@ import (
 
 type payloadBuilder struct {
 	lggr logger.Logger
-	//cl   client.Client
 }
 
 var _ ocr2keepers.PayloadBuilder = &payloadBuilder{}
@@ -19,18 +18,11 @@ var _ ocr2keepers.PayloadBuilder = &payloadBuilder{}
 func NewPayloadBuilder(lggr logger.Logger) *payloadBuilder {
 	return &payloadBuilder{
 		lggr: lggr,
-		//cl:   cl,
 	}
 }
 
 func (b *payloadBuilder) BuildPayloads(ctx context.Context, proposals ...ocr2keepers.CoordinatedBlockProposal) ([]ocr2keepers.UpkeepPayload, error) {
-
 	payloads := make([]ocr2keepers.UpkeepPayload, len(proposals))
-	//block, err := b.latestBlock(ctx)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to get latest block: %w", err)
-	//}
-
 	for i, p := range proposals {
 		b.lggr.Debugf("building payload for coordinated block proposal %+v", p)
 		var checkData []byte
@@ -38,11 +30,9 @@ func (b *payloadBuilder) BuildPayloads(ctx context.Context, proposals ...ocr2kee
 		case ocr2keepers.LogTrigger:
 			checkData = []byte{} // TODO: call recoverer
 		case ocr2keepers.ConditionTrigger:
-			checkData = []byte{} // CheckData derived on chain for conditionals
 			// Trigger.BlockNumber and Trigger.BlockHash are already coordinated
+			checkData = []byte{} // CheckData derived on chain for conditionals
 			// TODO: check for upkeepID being active upkeep here
-			//p.Trigger.BlockNumber = block.Number
-			//p.Trigger.BlockHash = block.Hash
 		default:
 		}
 		payload, err := core.NewUpkeepPayload(p.UpkeepID.BigInt(), p.Trigger, checkData)
@@ -56,19 +46,3 @@ func (b *payloadBuilder) BuildPayloads(ctx context.Context, proposals ...ocr2kee
 
 	return payloads, nil
 }
-
-/*
-func (b *payloadBuilder) latestBlock(ctx context.Context) (ocr2keepers.BlockKey, error) {
-	latest, err := b.cl.LatestBlockHeight(ctx)
-	if err != nil {
-		return ocr2keepers.BlockKey{}, err
-	}
-	block, err := b.cl.BlockByNumber(ctx, latest)
-	if err != nil {
-		return ocr2keepers.BlockKey{}, err
-	}
-	return ocr2keepers.BlockKey{
-		Number: ocr2keepers.BlockNumber(latest.Uint64()),
-		Hash:   block.Hash(),
-	}, nil
-}*/
