666bdd1cb791e460575d4fc43cd5b133feec4343
gheorghestrimtu <studentcuza@gmail.com>
2023-08-15 12:32:43 +0300

    make go-solidity-wrappers

--- contracts/src/v0.8/mocks/FunctionsBillingRegistryEventsMock.sol
+++ contracts/src/v0.8/mocks/FunctionsBillingRegistryEventsMock.sol
@@ -5,50 +5,38 @@ pragma solidity ^0.8.6;
 
 contract FunctionsBillingRegistryEventsMock {
     struct FunctionsBillingRegistryCommitment {uint64 subscriptionId;address client;uint32 gasLimit;uint256 gasPrice;address don;uint96 donFee;uint96 registryFee;uint96 estimatedCost;uint256 timestamp; }
-    event Paused(address account);
-    event SubscriptionConsumerAdded(uint64 indexed subscriptionId,address consumer);
-    event SubscriptionCreated(uint64 indexed subscriptionId,address owner);
-    event SubscriptionOwnerTransferRequested(uint64 indexed subscriptionId,address from,address to);
-    event AuthorizedSendersChanged(address[] senders,address changedBy);
-    event SubscriptionConsumerRemoved(uint64 indexed subscriptionId,address consumer);
+    event BillingEnd(bytes32 indexed requestId,uint64 subscriptionId,uint96 signerPayment,uint96 transmitterPayment,uint96 totalCost,bool success);
     event BillingStart(bytes32 indexed requestId,FunctionsBillingRegistryCommitment commitment);
-    event ConfigSet(uint32 maxGasLimit,uint32 stalenessSeconds,uint256 gasAfterPaymentCalculation,int256 fallbackWeiPerUnitLink,uint32 gasOverhead);
+    event FundsRecovered(address to,uint256 amount);
     event OwnershipTransferRequested(address indexed from,address indexed to);
+    event Paused(address account);
     event RequestTimedOut(bytes32 indexed requestId);
     event SubscriptionCanceled(uint64 indexed subscriptionId,address to,uint256 amount);
+    event SubscriptionConsumerRemoved(uint64 indexed subscriptionId,address consumer);
+    event SubscriptionFunded(uint64 indexed subscriptionId,uint256 oldBalance,uint256 newBalance);
     event SubscriptionOwnerTransferred(uint64 indexed subscriptionId,address from,address to);
-    event BillingEnd(bytes32 indexed requestId,uint64 subscriptionId,uint96 signerPayment,uint96 transmitterPayment,uint96 totalCost,bool success);
-    event FundsRecovered(address to,uint256 amount);
+    event AuthorizedSendersChanged(address[] senders,address changedBy);
+    event ConfigSet(uint32 maxGasLimit,uint32 stalenessSeconds,uint256 gasAfterPaymentCalculation,int256 fallbackWeiPerUnitLink,uint32 gasOverhead);
     event Initialized(uint8 version);
     event OwnershipTransferred(address indexed from,address indexed to);
-    event SubscriptionFunded(uint64 indexed subscriptionId,uint256 oldBalance,uint256 newBalance);
+    event SubscriptionCreated(uint64 indexed subscriptionId,address owner);
     event Unpaused(address account);
-    function emitPaused(address account) public {
-        emit Paused(account);
-    }
-    function emitSubscriptionConsumerAdded(uint64 subscriptionId,address consumer) public {
-        emit SubscriptionConsumerAdded(subscriptionId,consumer);
-    }
-    function emitSubscriptionCreated(uint64 subscriptionId,address owner) public {
-        emit SubscriptionCreated(subscriptionId,owner);
-    }
-    function emitSubscriptionOwnerTransferRequested(uint64 subscriptionId,address from,address to) public {
-        emit SubscriptionOwnerTransferRequested(subscriptionId,from,to);
-    }
-    function emitAuthorizedSendersChanged(address[] memory senders,address changedBy) public {
-        emit AuthorizedSendersChanged(senders,changedBy);
-    }
-    function emitSubscriptionConsumerRemoved(uint64 subscriptionId,address consumer) public {
-        emit SubscriptionConsumerRemoved(subscriptionId,consumer);
+    event SubscriptionConsumerAdded(uint64 indexed subscriptionId,address consumer);
+    event SubscriptionOwnerTransferRequested(uint64 indexed subscriptionId,address from,address to);
+    function emitBillingEnd(bytes32 requestId,uint64 subscriptionId,uint96 signerPayment,uint96 transmitterPayment,uint96 totalCost,bool success) public {
+        emit BillingEnd(requestId,subscriptionId,signerPayment,transmitterPayment,totalCost,success);
     }
     function emitBillingStart(bytes32 requestId,FunctionsBillingRegistryCommitment memory commitment) public {
         emit BillingStart(requestId,commitment);
     }
-    function emitConfigSet(uint32 maxGasLimit,uint32 stalenessSeconds,uint256 gasAfterPaymentCalculation,int256 fallbackWeiPerUnitLink,uint32 gasOverhead) public {
-        emit ConfigSet(maxGasLimit,stalenessSeconds,gasAfterPaymentCalculation,fallbackWeiPerUnitLink,gasOverhead);
+    function emitFundsRecovered(address to,uint256 amount) public {
+        emit FundsRecovered(to,amount);
     }
     function emitOwnershipTransferRequested(address from,address to) public {
         emit OwnershipTransferRequested(from,to);
+    }
+    function emitPaused(address account) public {
+        emit Paused(account);
     }
     function emitRequestTimedOut(bytes32 requestId) public {
         emit RequestTimedOut(requestId);
@@ -56,14 +44,20 @@ contract FunctionsBillingRegistryEventsMock {
     function emitSubscriptionCanceled(uint64 subscriptionId,address to,uint256 amount) public {
         emit SubscriptionCanceled(subscriptionId,to,amount);
     }
+    function emitSubscriptionConsumerRemoved(uint64 subscriptionId,address consumer) public {
+        emit SubscriptionConsumerRemoved(subscriptionId,consumer);
+    }
+    function emitSubscriptionFunded(uint64 subscriptionId,uint256 oldBalance,uint256 newBalance) public {
+        emit SubscriptionFunded(subscriptionId,oldBalance,newBalance);
+    }
     function emitSubscriptionOwnerTransferred(uint64 subscriptionId,address from,address to) public {
         emit SubscriptionOwnerTransferred(subscriptionId,from,to);
     }
-    function emitBillingEnd(bytes32 requestId,uint64 subscriptionId,uint96 signerPayment,uint96 transmitterPayment,uint96 totalCost,bool success) public {
-        emit BillingEnd(requestId,subscriptionId,signerPayment,transmitterPayment,totalCost,success);
+    function emitAuthorizedSendersChanged(address[] memory senders,address changedBy) public {
+        emit AuthorizedSendersChanged(senders,changedBy);
     }
-    function emitFundsRecovered(address to,uint256 amount) public {
-        emit FundsRecovered(to,amount);
+    function emitConfigSet(uint32 maxGasLimit,uint32 stalenessSeconds,uint256 gasAfterPaymentCalculation,int256 fallbackWeiPerUnitLink,uint32 gasOverhead) public {
+        emit ConfigSet(maxGasLimit,stalenessSeconds,gasAfterPaymentCalculation,fallbackWeiPerUnitLink,gasOverhead);
     }
     function emitInitialized(uint8 version) public {
         emit Initialized(version);
@@ -71,10 +65,16 @@ contract FunctionsBillingRegistryEventsMock {
     function emitOwnershipTransferred(address from,address to) public {
         emit OwnershipTransferred(from,to);
     }
-    function emitSubscriptionFunded(uint64 subscriptionId,uint256 oldBalance,uint256 newBalance) public {
-        emit SubscriptionFunded(subscriptionId,oldBalance,newBalance);
+    function emitSubscriptionCreated(uint64 subscriptionId,address owner) public {
+        emit SubscriptionCreated(subscriptionId,owner);
     }
     function emitUnpaused(address account) public {
         emit Unpaused(account);
+    }
+    function emitSubscriptionConsumerAdded(uint64 subscriptionId,address consumer) public {
+        emit SubscriptionConsumerAdded(subscriptionId,consumer);
+    }
+    function emitSubscriptionOwnerTransferRequested(uint64 subscriptionId,address from,address to) public {
+        emit SubscriptionOwnerTransferRequested(subscriptionId,from,to);
     }
 }
--- contracts/src/v0.8/mocks/FunctionsOracleEventsMock.sol
+++ contracts/src/v0.8/mocks/FunctionsOracleEventsMock.sol
@@ -4,29 +4,20 @@
 pragma solidity ^0.8.6;
 
 contract FunctionsOracleEventsMock {
-    event AuthorizedSendersDeactive(address account);
-    event Initialized(uint8 version);
-    event InvalidRequestID(bytes32 indexed requestId);
     event OracleResponse(bytes32 indexed requestId);
     event UserCallbackRawError(bytes32 indexed requestId,bytes lowLevelData);
     event AuthorizedSendersActive(address account);
-    event AuthorizedSendersChanged(address[] senders,address changedBy);
+    event AuthorizedSendersDeactive(address account);
     event ConfigSet(uint32 previousConfigBlockNumber,bytes32 configDigest,uint64 configCount,address[] signers,address[] transmitters,uint8 f,bytes onchainConfig,uint64 offchainConfigVersion,bytes offchainConfig);
-    event UserCallbackError(bytes32 indexed requestId,string reason);
+    event Initialized(uint8 version);
+    event OracleRequest(bytes32 indexed requestId,address requestingContract,address requestInitiator,uint64 subscriptionId,address subscriptionOwner,bytes data);
+    event AuthorizedSendersChanged(address[] senders,address changedBy);
     event ResponseTransmitted(bytes32 indexed requestId,address transmitter);
+    event UserCallbackError(bytes32 indexed requestId,string reason);
+    event InvalidRequestID(bytes32 indexed requestId);
+    event OwnershipTransferRequested(address indexed from,address indexed to);
     event OwnershipTransferred(address indexed from,address indexed to);
     event Transmitted(bytes32 configDigest,uint32 epoch);
-    event OracleRequest(bytes32 indexed requestId,address requestingContract,address requestInitiator,uint64 subscriptionId,address subscriptionOwner,bytes data);
-    event OwnershipTransferRequested(address indexed from,address indexed to);
-    function emitAuthorizedSendersDeactive(address account) public {
-        emit AuthorizedSendersDeactive(account);
-    }
-    function emitInitialized(uint8 version) public {
-        emit Initialized(version);
-    }
-    function emitInvalidRequestID(bytes32 requestId) public {
-        emit InvalidRequestID(requestId);
-    }
     function emitOracleResponse(bytes32 requestId) public {
         emit OracleResponse(requestId);
     }
@@ -36,28 +27,37 @@ contract FunctionsOracleEventsMock {
     function emitAuthorizedSendersActive(address account) public {
         emit AuthorizedSendersActive(account);
     }
-    function emitAuthorizedSendersChanged(address[] memory senders,address changedBy) public {
-        emit AuthorizedSendersChanged(senders,changedBy);
+    function emitAuthorizedSendersDeactive(address account) public {
+        emit AuthorizedSendersDeactive(account);
     }
     function emitConfigSet(uint32 previousConfigBlockNumber,bytes32 configDigest,uint64 configCount,address[] memory signers,address[] memory transmitters,uint8 f,bytes memory onchainConfig,uint64 offchainConfigVersion,bytes memory offchainConfig) public {
         emit ConfigSet(previousConfigBlockNumber,configDigest,configCount,signers,transmitters,f,onchainConfig,offchainConfigVersion,offchainConfig);
     }
-    function emitUserCallbackError(bytes32 requestId,string memory reason) public {
-        emit UserCallbackError(requestId,reason);
+    function emitInitialized(uint8 version) public {
+        emit Initialized(version);
+    }
+    function emitOracleRequest(bytes32 requestId,address requestingContract,address requestInitiator,uint64 subscriptionId,address subscriptionOwner,bytes memory data) public {
+        emit OracleRequest(requestId,requestingContract,requestInitiator,subscriptionId,subscriptionOwner,data);
+    }
+    function emitAuthorizedSendersChanged(address[] memory senders,address changedBy) public {
+        emit AuthorizedSendersChanged(senders,changedBy);
     }
     function emitResponseTransmitted(bytes32 requestId,address transmitter) public {
         emit ResponseTransmitted(requestId,transmitter);
+    }
+    function emitUserCallbackError(bytes32 requestId,string memory reason) public {
+        emit UserCallbackError(requestId,reason);
+    }
+    function emitInvalidRequestID(bytes32 requestId) public {
+        emit InvalidRequestID(requestId);
+    }
+    function emitOwnershipTransferRequested(address from,address to) public {
+        emit OwnershipTransferRequested(from,to);
     }
     function emitOwnershipTransferred(address from,address to) public {
         emit OwnershipTransferred(from,to);
     }
     function emitTransmitted(bytes32 configDigest,uint32 epoch) public {
         emit Transmitted(configDigest,epoch);
-    }
-    function emitOracleRequest(bytes32 requestId,address requestingContract,address requestInitiator,uint64 subscriptionId,address subscriptionOwner,bytes memory data) public {
-        emit OracleRequest(requestId,requestingContract,requestInitiator,subscriptionId,subscriptionOwner,data);
-    }
-    function emitOwnershipTransferRequested(address from,address to) public {
-        emit OwnershipTransferRequested(from,to);
     }
 }
