73d8d7b132a1656c78525c1f9b09b59d46849478
Mateusz Sekara <mateusz.sekara@smartcontract.com>
2023-08-16 07:56:20 +0200

    [CCIP-958] Passing qopts all the way down when starting jobs  (#10166)
    * Unification in LogPoller interface - RegisterFilter and UnregisterFilter accepts pg.QOpt like rest of the LogPoller's interface function
    
    * Adding pg.QOpts to the Delegate interface
    
    * Propagating qopts in Spawner
    
    * Missing qopts in LogPoller

--- core/services/fluxmonitorv2/delegate.go
+++ core/services/fluxmonitorv2/delegate.go
@@ -59,7 +59,7 @@ func (d *Delegate) BeforeJobDeleted(spec job.Job)                {}
 func (d *Delegate) OnDeleteJob(spec job.Job, q pg.Queryer) error { return nil }
 
 // ServicesForSpec returns the flux monitor service for the job spec
-func (d *Delegate) ServicesForSpec(jb job.Job) (services []job.ServiceCtx, err error) {
+func (d *Delegate) ServicesForSpec(jb job.Job, qopts ...pg.QOpt) (services []job.ServiceCtx, err error) {
 	if jb.FluxMonitorSpec == nil {
 		return nil, errors.Errorf("Delegate expects a *job.FluxMonitorSpec to be present, got %v", jb)
 	}
