718442cd473004783cec92080ad893fda88c5866
gheorghestrimtu <studentcuza@gmail.com>
2023-08-22 15:40:24 +0300

    integration-tests contract wrapper

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -16,6 +16,7 @@ import (
 	ocrConfigHelper "github.com/smartcontractkit/libocr/offchainreporting/confighelper"
 
 	eth_contracts "github.com/smartcontractkit/chainlink/integration-tests/contracts/ethereum"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/functions/generated/functions_v1_events_mock"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/automation_consumer_benchmark"
 	automationForwarderLogic "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/automation_forwarder_logic"
 	registrar21 "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generated/automation_registrar_wrapper2_1"
@@ -100,6 +101,7 @@ type ContractDeployer interface {
 	DeployFunctionsOracleEventsMock() (FunctionsOracleEventsMock, error)
 	DeployFunctionsBillingRegistryEventsMock() (FunctionsBillingRegistryEventsMock, error)
 	DeployStakingEventsMock() (StakingEventsMock, error)
+	DeployFunctionsV1EventsMock() (FunctionsV1EventsMock, error)
 	DeployOffchainAggregatorEventsMock() (OffchainAggregatorEventsMock, error)
 	DeployMockAggregatorProxy(aggregatorAddr string) (MockAggregatorProxy, error)
 	DeployOffchainAggregatorV2(linkAddr string, offchainOptions OffchainOptions) (OffchainAggregatorV2, error)
@@ -323,6 +325,23 @@ func (e *EthereumContractDeployer) DeployStakingEventsMock() (StakingEventsMock,
 	return &EthereumStakingEventsMock{
 		client:     e.client,
 		eventsMock: instance.(*eth_contracts.StakingEventsMock),
+		address:    address,
+	}, nil
+}
+
+func (e *EthereumContractDeployer) DeployFunctionsV1EventsMock() (FunctionsV1EventsMock, error) {
+	address, _, instance, err := e.client.DeployContract("FunctionsV1EventsMock", func(
+		auth *bind.TransactOpts,
+		backend bind.ContractBackend,
+	) (common.Address, *types.Transaction, interface{}, error) {
+		return functions_v1_events_mock.DeployFunctionsV1EventsMock(auth, backend)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &EthereumFunctionsV1EventsMock{
+		client:     e.client,
+		eventsMock: instance.(*functions_v1_events_mock.FunctionsV1EventsMock),
 		address:    address,
 	}, nil
 }
