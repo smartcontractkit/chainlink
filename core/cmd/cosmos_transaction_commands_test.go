e1c53ab46423d6c1a40617ecff89dce2f1dbc49b
Calvin <78729586+calvwang9@users.noreply.github.com>
2023-08-25 01:31:35 +1000

    Bci 1738/cosmos cleanup (#10238)
    * add native token field to cosmos config
    
    * remove fcdurl
    
    * change gas token to native token
    
    * rename to fee token
    
    * add bech32 prefix to cosmos config
    
    * bump chainlink-cosmos to feature branch
    
    * fix type error
    
    * update chain-cosmos.toml
    
    * fix fee estimator test
    
    * fix types and gas price estimator input
    
    * make config-docs
    
    * update chainlink-cosmos version
    
    * fix cosmos config tests
    
    * update tomls and types
    
    * fix tests
    
    * cleanup CosmosSendAtom
    
    * fix docs and scripts tests
    
    * remove atom references
    
    * use ValidateDenom
    
    * fix cosmos config in relayer test
    
    * sort cosmos config fields alphabetically
    
    * undo accidental test changes
    
    * bump chainlink-cosmos version
    
    * fix cosmos config order
    
    ---------
    
    Co-authored-by: krehermann <keith.rehermann@smartcontract.com>

--- core/cmd/cosmos_transaction_commands_test.go
+++ core/cmd/cosmos_transaction_commands_test.go
@@ -32,11 +32,13 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/cosmoskey"
 )
 
+var nativeToken = "cosm"
+
 func TestMain(m *testing.M) {
 
 	params.InitCosmosSdk(
 		/* bech32Prefix= */ "wasm",
-		/* token= */ "atom",
+		/* token= */ nativeToken,
 	)
 
 	code := m.Run()
@@ -46,10 +48,10 @@ func TestMain(m *testing.M) {
 func TestShell_SendCosmosCoins(t *testing.T) {
 	// TODO(BCI-978): cleanup once SetupLocalCosmosNode is updated
 	chainID := cosmostest.RandomChainID()
-	accounts, _, url := cosmosclient.SetupLocalCosmosNode(t, chainID, "uatom")
-	require.Greater(t, len(accounts), 1)
 	cosmosChain := coscfg.Chain{}
 	cosmosChain.SetDefaults()
+	accounts, _, url := cosmosclient.SetupLocalCosmosNode(t, chainID, *cosmosChain.FeeToken)
+	require.Greater(t, len(accounts), 1)
 	nodes := cosmos.CosmosNodes{
 		&coscfg.Node{
 			Name:          ptr("random"),
@@ -69,7 +71,7 @@ func TestShell_SendCosmosCoins(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Eventually(t, func() bool {
-		coin, err := reader.Balance(from.Address, "uatom")
+		coin, err := reader.Balance(from.Address, *cosmosChain.FeeToken)
 		if !assert.NoError(t, err) {
 			return false
 		}
@@ -95,17 +97,17 @@ func TestShell_SendCosmosCoins(t *testing.T) {
 	} {
 		tt := tt
 		t.Run(tt.amount, func(t *testing.T) {
-			startBal, err := reader.Balance(from.Address, "uatom")
+			startBal, err := reader.Balance(from.Address, *cosmosChain.FeeToken)
 			require.NoError(t, err)
 
 			set := flag.NewFlagSet("sendcosmoscoins", 0)
-			cltest.FlagSetApplyFromAction(client.CosmosSendAtom, set, "cosmos")
+			cltest.FlagSetApplyFromAction(client.CosmosSendNativeToken, set, "cosmos")
 
 			require.NoError(t, set.Set("id", chainID))
-			require.NoError(t, set.Parse([]string{tt.amount, from.Address.String(), to.Address.String()}))
+			require.NoError(t, set.Parse([]string{nativeToken, tt.amount, from.Address.String(), to.Address.String()}))
 
 			c := cli.NewContext(cliapp, set, nil)
-			err = client.CosmosSendAtom(c)
+			err = client.CosmosSendNativeToken(c)
 			if tt.expErr == "" {
 				require.NoError(t, err)
 			} else {
@@ -154,11 +156,11 @@ func TestShell_SendCosmosCoins(t *testing.T) {
 			}
 
 			// Check balance
-			endBal, err := reader.Balance(from.Address, "uatom")
+			endBal, err := reader.Balance(from.Address, *cosmosChain.FeeToken)
 			require.NoError(t, err)
 			if assert.NotNil(t, startBal) && assert.NotNil(t, endBal) {
 				diff := startBal.Sub(*endBal).Amount
-				sent, err := denom.ConvertDecCoinToDenom(sdk.NewDecCoinFromDec("atom", sdk.MustNewDecFromStr(tt.amount)), "uatom")
+				sent, err := denom.ConvertDecCoinToDenom(sdk.NewDecCoinFromDec(nativeToken, sdk.MustNewDecFromStr(tt.amount)), *cosmosChain.FeeToken)
 				require.NoError(t, err)
 				if assert.True(t, diff.IsInt64()) && assert.True(t, sent.Amount.IsInt64()) {
 					require.Greater(t, diff.Int64(), sent.Amount.Int64())
