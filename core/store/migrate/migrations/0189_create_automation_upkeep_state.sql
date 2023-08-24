a18b7703934d9ea611186aebdc0b2a9c17fb6197
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-08-24 10:26:31 -0500

    remove default

--- core/store/migrate/migrations/0189_create_automation_upkeep_state.sql
+++ core/store/migrate/migrations/0189_create_automation_upkeep_state.sql
@@ -7,7 +7,7 @@ CREATE TABLE evm_upkeep_states (
   upkeep_id NUMERIC(78) NOT NULL, -- upkeep id is an evm word (uint256) which has a max size of precision 78
   completion_state SMALLINT NOT NULL,
   ineligibility_reason SMALLINT NOT NULL,
-  block_number BIGINT DEFAULT 0 NOT NULL,
+  block_number BIGINT NOT NULL,
   inserted_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
   CONSTRAINT work_id_len_chk CHECK (
     length(work_id) > 0 AND length(work_id) < 255
