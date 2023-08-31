e90cd8edc1742343b98a2ca84e2fe5b6d5f448e4
jmank88 <jmank88@gmail.com>
2023-08-31 06:55:16 -0500

    loopclosure: loop variable tt captured by func literal (govet)

--- core/services/chainlink/relayer_chain_interoperators_test.go
+++ core/services/chainlink/relayer_chain_interoperators_test.go
@@ -320,6 +320,7 @@ func TestCoreRelayerChainInteroperators(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			cr, err := chainlink.NewCoreRelayerChainInteroperators(tt.initFuncs...)
