7697a8a807c42bce0390397deb7914b6f868e767
krehermann <keith.rehermann@smartcontract.com>
2023-08-31 13:23:26 -0600

    remove chainset interface (#160)

--- pkg/types/relayer.go
+++ pkg/types/relayer.go
@@ -46,20 +46,6 @@ type Relayer interface {
 }
 
 // Deprecated
-type ChainSet[I any, C ChainService] interface {
-	Service
-
-	Chain(ctx context.Context, id I) (C, error)
-
-	ChainStatus(ctx context.Context, id string) (ChainStatus, error)
-	ChainStatuses(ctx context.Context, offset, limit int) (chains []ChainStatus, count int, err error)
-
-	NodeStatuses(ctx context.Context, offset, limit int, chainIDs ...string) (nodes []NodeStatus, count int, err error)
-
-	SendTx(ctx context.Context, chainID, from, to string, amount *big.Int, balanceCheck bool) error
-}
-
-// Deprecated
 type ChainService interface {
 	Service
 
