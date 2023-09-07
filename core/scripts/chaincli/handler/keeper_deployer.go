16c33dad4bf95142e41f2eadd9e41bf9ea953f69
Chris <104409744+vreff@users.noreply.github.com>
2023-09-07 10:15:12 -0400

    [Automation] Fixes to chaincli for Automation v2.1

--- core/scripts/chaincli/handler/keeper_deployer.go
+++ core/scripts/chaincli/handler/keeper_deployer.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	ocr2config "github.com/smartcontractkit/libocr/offchainreporting2plus/confighelper"
+	ocr3confighelper "github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3confighelper"
 	ocr2types "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 	"github.com/umbracle/ethgo/abi"
 
@@ -297,11 +298,13 @@ func (d *v21KeeperDeployer) SetKeepers(opts *bind.TransactOpts, cls []cmd.HTTPCl
 		panic(err)
 	}
 
-	signerOnchainPublicKeys, transmitterAccounts, f, _, offchainConfigVersion, offchainConfig, err := ocr2config.ContractSetConfigArgsForTests(
+	signerOnchainPublicKeys, transmitterAccounts, f, _, offchainConfigVersion, offchainConfig, err := ocr3confighelper.ContractSetConfigArgsForTests(
 		5*time.Second,         // deltaProgress time.Duration,
 		10*time.Second,        // deltaResend time.Duration,
+		400*time.Millisecond,  // deltaInitial time.Duration,
 		2500*time.Millisecond, // deltaRound time.Duration,
 		40*time.Millisecond,   // deltaGrace time.Duration,
+		300*time.Millisecond,  // deltaCertifiedCommitRequest time.Duration,
 		30*time.Second,        // deltaStage time.Duration,
 		50,                    // rMax uint8,
 		S,                     // s []int,
@@ -309,7 +312,6 @@ func (d *v21KeeperDeployer) SetKeepers(opts *bind.TransactOpts, cls []cmd.HTTPCl
 		offC,                  // reportingPluginConfig []byte,
 		20*time.Millisecond,   // maxDurationQuery time.Duration,
 		1600*time.Millisecond, // maxDurationObservation time.Duration,
-		800*time.Millisecond,  // maxDurationReport time.Duration,
 		20*time.Millisecond,   // maxDurationShouldAcceptFinalizedReport time.Duration,
 		20*time.Millisecond,   // maxDurationShouldTransmitAcceptedReport time.Duration,
 		1,                     // f int,
