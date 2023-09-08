2ee4471f2b101e3580f7104f88bbf71aca5eb856
amirylm <amirylm.dev@gmail.com>
2023-09-08 17:17:33 +0300

    remove limits

--- core/utils/thread_control.go
+++ core/utils/thread_control.go
@@ -16,8 +16,8 @@ var (
 // ThreadControl is a helper for managing a group of goroutines.
 type ThreadControl interface {
 	// Go starts a goroutine and tracks the lifetime of the goroutine.
-	Go(fn func(context.Context)) error
-	// Close cancels the context and waits for all tracked goroutines to exit.
+	Go(fn func(context.Context))
+	// Close cancels the goroutines and waits for all of them to exit.
 	Close()
 }
 
@@ -39,19 +39,14 @@ type threadControl struct {
 	stop StopChan
 }
 
-func (tc *threadControl) Go(fn func(context.Context)) error {
-	if err := tc.add(); err != nil {
-		return err
-	}
-
+func (tc *threadControl) Go(fn func(context.Context)) {
+	tc.add()
 	go func() {
 		defer tc.done()
 		ctx, cancel := tc.stop.NewCtx()
 		defer cancel()
 		fn(ctx)
 	}()
-
-	return nil
 }
 
 func (tc *threadControl) Close() {
@@ -59,13 +54,9 @@ func (tc *threadControl) Close() {
 	tc.threadsWG.Wait()
 }
 
-func (tc *threadControl) add() error {
-	if tc.running.Add(1) > tc.limit {
-		tc.running.Add(-1)
-		return ErrThreadLimitReached
-	}
+func (tc *threadControl) add() {
+	tc.running.Add(1)
 	tc.threadsWG.Add(1)
-	return nil
 }
 
 func (tc *threadControl) done() {
--- core/utils/thread_control_test.go
+++ core/utils/thread_control_test.go
@@ -15,29 +15,13 @@ func TestThreadControl_Close(t *testing.T) {
 	finished := atomic.Int32{}
 
 	for i := 0; i < n; i++ {
-		require.NoError(t, tc.Go(func(ctx context.Context) {
+		tc.Go(func(ctx context.Context) {
 			<-ctx.Done()
 			finished.Add(1)
-		}))
+		})
 	}
 
 	tc.Close()
 
 	require.Equal(t, int32(n), finished.Load())
-}
-func TestThreadControl_ThreadsLimitExceeded(t *testing.T) {
-	tc := NewThreadControl(1)
-
-	finished := atomic.Int32{}
-
-	fn := func(ctx context.Context) {
-		<-ctx.Done()
-		finished.Add(1)
-	}
-	require.NoError(t, tc.Go(fn))
-	require.Error(t, tc.Go(fn))
-
-	tc.Close()
-
-	require.Equal(t, int32(1), finished.Load())
 }
