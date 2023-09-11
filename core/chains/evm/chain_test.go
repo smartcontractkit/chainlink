6c2c10d8cce42a67d45babe3046d1fe6392b7eeb
krehermann <keith.rehermann@smartcontract.com>
2023-09-11 12:38:19 -0600

    BCF-2605 clean up Configs (#10551)
    * unify chain type
    
    * rm Configs from single chain; delete generic Configs struct and correpsonding iface
    
    * delete unneeded configs code
    
    * more dead code cleanup
    
    * add tests
    
    * rm dead code
    
    * simplify ChainID type

--- core/chains/evm/chain_test.go
+++ core/chains/evm/chain_test.go
@@ -5,13 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/mocks"
 	configtest "github.com/smartcontractkit/chainlink/v2/core/internal/testutils/configtest/v2"
-	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
-	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
 
 func TestLegacyChains(t *testing.T) {
@@ -27,29 +24,4 @@ func TestLegacyChains(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, c, got)
 
-	require.NotPanics(t, func() {
-		l = evm.NewLegacyChains(m, nil)
-		assert.NotNil(t, l.ChainNodeConfigs())
-	})
-}
-
-func TestRelayConfigInit(t *testing.T) {
-	appCfg := configtest.NewGeneralConfig(t, nil)
-	rCfg := evm.RelayerConfig{
-		AppConfig: appCfg,
-	}
-
-	evmCfg := rCfg.EVMConfigs()
-	assert.NotNil(t, evmCfg)
-
-	// test lazy init is done only once
-	// note this kind of swapping should never happen in prod
-	appCfg2 := configtest.NewGeneralConfig(t, func(c *chainlink.Config, s *chainlink.Secrets) {
-		c.EVM[0].ChainID = utils.NewBig(big.NewInt(27))
-	})
-	rCfg.AppConfig = appCfg2
-
-	newEvmCfg := rCfg.EVMConfigs()
-	assert.NotNil(t, newEvmCfg)
-	assert.Equal(t, evmCfg, newEvmCfg)
 }
