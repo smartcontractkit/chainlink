0d70d85249e5ae85d77b62b073fdc3ce7411ebbe
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2023-09-12 07:49:11 +0300

    Functions: fixing S4 ORM

--- core/store/migrate/migrations/0193_functions_alter_version_type.sql
+++ core/store/migrate/migrations/0193_functions_alter_version_type.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 
-ALTER TABLE "s4".functions ALTER COLUMN version TYPE BIGINT;
+ALTER TABLE "s4".shared ALTER COLUMN version TYPE BIGINT;
 
 -- +goose Down
 
-ALTER TABLE "s4".functions ALTER COLUMN version TYPE INT USING version::integer;
+ALTER TABLE "s4".shared ALTER COLUMN version TYPE INT USING version::integer;
