c1de4413d7132896d83f709c33efbf21c59883e6
Mateusz Sekara <mateusz.sekara@smartcontract.com>
2023-09-18 07:45:53 +0200

    Backporting LogPoller's function used in CCIP (#10645)
    * Backporting LogPoller's function used in CCIP
    
    * Missing tests added as well

--- core/chains/evm/logpoller/orm.go
+++ core/chains/evm/logpoller/orm.go
@@ -400,6 +400,31 @@ func (o *ORM) SelectIndexLogsTopicGreaterThan(address common.Address, eventSig c
 	return logs, nil
 }
 
+func (o *ORM) SelectUntilBlockHashDataWordGreaterThan(address common.Address, eventSig common.Hash, wordIndex int, wordValueMin common.Hash, untilBlockHash common.Hash, qopts ...pg.QOpt) ([]Log, error) {
+	var logs []Log
+	q := o.q.WithOpts(qopts...)
+	err := q.Transaction(func(tx pg.Queryer) error {
+		// We want to mimic the behaviour of the ETH RPC which errors if blockhash not found.
+		var block LogPollerBlock
+		if err := tx.Get(&block,
+			`SELECT * FROM evm.log_poller_blocks 
+					WHERE evm_chain_id = $1 AND block_hash = $2`, utils.NewBig(o.chainID), untilBlockHash); err != nil {
+			return err
+		}
+		return q.Select(&logs,
+			`SELECT * FROM evm.logs 
+			WHERE evm_chain_id = $1
+			AND address = $2 AND event_sig = $3
+			AND substring(data from 32*$4+1 for 32) >= $5
+			AND block_number <= $6 
+			ORDER BY (block_number, log_index)`, utils.NewBig(o.chainID), address, eventSig.Bytes(), wordIndex, wordValueMin.Bytes(), block.BlockNumber)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return logs, nil
+}
+
 func (o *ORM) SelectIndexLogsTopicRange(address common.Address, eventSig common.Hash, topicIndex int, topicValueMin, topicValueMax common.Hash, confs int, qopts ...pg.QOpt) ([]Log, error) {
 	if err := validateTopicIndex(topicIndex); err != nil {
 		return nil, err
