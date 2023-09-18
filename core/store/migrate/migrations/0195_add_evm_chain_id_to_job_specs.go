39433ea11856e8ac06be330e39cf8ca215c32b3f
ilija <pavlovicilija42@gmail.com>
2023-09-18 17:43:11 +0200

    Update 0195 migration to only add/remove evmChainID not nil constraints

--- core/store/migrate/migrations/0195_add_evm_chain_id_to_job_specs.go
+++ core/store/migrate/migrations/0195_add_evm_chain_id_to_job_specs.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/pressly/goose/v3"
-
-	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
 )
 
 func init() {
@@ -15,15 +13,6 @@ func init() {
 }
 
 const (
-	setMissingEvmChainIDsInSpecs = `
-	UPDATE direct_request_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE flux_monitor_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE ocr_oracle_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE keeper_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE vrf_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE blockhash_store_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	UPDATE block_header_feeder_specs SET evm_chain_id = '%d' WHERE evm_chain_id IS NULL;
-	`
 	addNullConstraintsToSpecs = `
 	ALTER TABLE direct_request_specs ALTER COLUMN evm_chain_id SET NOT NULL;
 	ALTER TABLE flux_monitor_specs ALTER COLUMN evm_chain_id SET NOT NULL;
@@ -47,21 +36,7 @@ const (
 
 // nolint
 func Up195(ctx context.Context, tx *sql.Tx) error {
-	var opts chainlink.GeneralConfigOpts
-	cfg, err := opts.New()
-	if err != nil {
-		return err
-	}
-
-	if cfg.EVMEnabled() {
-		chainID := cfg.EVMConfigs()[0].ChainID
-		_, err = tx.ExecContext(ctx, setMissingEvmChainIDsInSpecs, chainID)
-		if err != nil {
-			return errors.Wrap(err, "failed to set missing evm chain ids")
-		}
-	}
-
-	_, err = tx.ExecContext(ctx, addNullConstraintsToSpecs)
+	_, err := tx.ExecContext(ctx, addNullConstraintsToSpecs)
 	return errors.Wrap(err, "failed to add null constraints")
 }
 
