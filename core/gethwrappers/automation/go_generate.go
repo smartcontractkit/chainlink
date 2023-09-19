81a8c134d950677645df1b65390a7f01ed76f4e0
Chris <104409744+vreff@users.noreply.github.com>
2023-09-19 10:26:37 -0400

    Remove unnecessary code

--- core/gethwrappers/automation/go_generate.go
+++ core/gethwrappers/automation/go_generate.go
@@ -5,4 +5,3 @@ package gethwrappers
 // Automation
 //go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.16/ComposerCompatibleInterfaceV1.abi ../../../contracts/solc/v0.8.16/ComposerCompatibleInterfaceV1.bin ComposerCompatibleInterfaceV1 composer_compatible_interface
 //go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.16/MercuryRegistryComposer.abi ../../../contracts/solc/v0.8.16/MercuryRegistryComposer.bin MercuryRegistryComposer mercury_registry_composer
-//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.16/StreamsLookupUpkeep.abi ../../../contracts/solc/v0.8.16/StreamsLookupUpkeep.bin StreamsLookupUpkeep streams_lookup_upkeep
