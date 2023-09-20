9f640d57854029b6b15f5cac66b99b1a896eb631
Austin Born <austinborn212@gmail.com>
2023-09-20 09:27:44 -0700

    Update llo versions and organization (#10727)
    * Update data streams contract versions and file structure
    
    * Update wrappers
    
    * Update VerifierProxy and FeeManager versions

--- contracts/src/v0.8/llo-feeds/test/reward-manager/BaseRewardManager.t.sol
+++ contracts/src/v0.8/llo-feeds/test/reward-manager/BaseRewardManager.t.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.16;
 
 import {Test} from "forge-std/Test.sol";
 import {ERC20Mock} from "../../../vendor/openzeppelin-solidity/v4.8.0/contracts/mocks/ERC20Mock.sol";
-import {RewardManager} from "../../dev/RewardManager.sol";
+import {RewardManager} from "../../RewardManager.sol";
 import {Common} from "../../../libraries/Common.sol";
-import {IRewardManager} from "../../dev/interfaces/IRewardManager.sol";
+import {IRewardManager} from "../../interfaces/IRewardManager.sol";
 
 /**
  * @title BaseRewardManagerTest
