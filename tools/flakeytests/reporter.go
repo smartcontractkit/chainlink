7d586fc6f5ef600942bd4eb9744b60d3ddc9411a
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2023-09-20 17:46:55 +0100

    [fix] Handle panics in the flakeytest runner correctly
    - Previously we would run tests multiple times using the -count=N flag
    on the test command. This worked most of the time, but if a test
    panic'd, the test would not get rerun, which meant that we would
    incorrectly report those tests as flakey (due to the presence of only 1
    log indicating an error, rather than once per rerun).

--- tools/flakeytests/reporter.go
+++ tools/flakeytests/reporter.go
@@ -45,12 +45,12 @@ type LokiReporter struct {
 	ctx     Context
 }
 
-func (l *LokiReporter) createRequest(flakeyTests map[string][]string) (pushRequest, error) {
+func (l *LokiReporter) createRequest(flakeyTests map[string]map[string]struct{}) (pushRequest, error) {
 	vs := [][]string{}
 	now := l.now()
 	nows := fmt.Sprintf("%d", now.UnixNano())
 	for pkg, tests := range flakeyTests {
-		for _, t := range tests {
+		for t := range tests {
 			d, err := json.Marshal(flakeyTest{
 				Package:    pkg,
 				TestName:   t,
@@ -117,7 +117,7 @@ func (l *LokiReporter) makeRequest(pushReq pushRequest) error {
 	return err
 }
 
-func (l *LokiReporter) Report(flakeyTests map[string][]string) error {
+func (l *LokiReporter) Report(flakeyTests map[string]map[string]struct{}) error {
 	pushReq, err := l.createRequest(flakeyTests)
 	if err != nil {
 		return err
