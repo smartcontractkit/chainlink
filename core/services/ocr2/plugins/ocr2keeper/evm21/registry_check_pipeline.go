351630dd969f68b4882728731ec526147c5a9910
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-09-20 19:25:56 +0100

    proper extract var

--- core/services/ocr2/plugins/ocr2keeper/evm21/registry_check_pipeline.go
+++ core/services/ocr2/plugins/ocr2keeper/evm21/registry_check_pipeline.go
@@ -244,9 +244,9 @@ func (r *EvmRegistry) checkUpkeeps(ctx context.Context, payloads []ocr2keepers.U
 		if req.Error != nil {
 			latestBlock := r.bs.latestBlock.Load()
 			checkBlock, _, _ := r.getBlockAndUpkeepId(payloads[index].UpkeepID, payloads[index].Trigger)
-			errString := req.Error.Error()
 			// Exploratory: remove reliance on primitive way of checking errors
-			if (strings.Contains(errString, "header not found") || strings.Contains(errString, "missing trie node")) && int64(latestBlock.Number)-checkBlock.Int64() > checkBlockTooOldRange {
+			blockNotFound := (strings.Contains(req.Error.Error(), "header not found") || strings.Contains(req.Error.Error(), "missing trie node"))
+			if blockNotFound && int64(latestBlock.Number)-checkBlock.Int64() > checkBlockTooOldRange {
 				// Check block not found in RPC and it is too old, non-retryable error
 				r.lggr.Warnf("header not found error encountered in check result for upkeepId %s, check block %d, latest block %d: %s", results[index].UpkeepID.String(), checkBlock.Int64(), int64(latestBlock.Number), req.Error)
 				results[index].Retryable = false
