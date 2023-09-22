ba1a042fb1b25e400a47f6d8225e2087ef3df506
Rens Rooimans <github@rensrooimans.nl>
2023-09-22 21:15:18 +0200

    add real attestation as return & rm 0x prefix (#157)

--- core/services/ocr2/plugins/ccip/tokendata/usdc/usdc_blackbox_test.go
+++ core/services/ocr2/plugins/ccip/tokendata/usdc/usdc_blackbox_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -38,10 +39,10 @@ type attestationResponse struct {
 func TestUSDCReader_ReadTokenData(t *testing.T) {
 	response := attestationResponse{
 		Status:      "complete",
-		Attestation: "720502893578a89a8a87982982ef781c18b193",
+		Attestation: "0x9049623e91719ef2aa63c55f357be2529b0e7122ae552c18aff8db58b4633c4d3920ff03d3a6d1ddf11f06bf64d7fd60d45447ac81f527ba628877dc5ca759651b08ffae25a6d3b1411749765244f0a1c131cbfe04430d687a2e12fd9d2e6dc08e118ad95d94ad832332cf3c4f7a4f3da0baa803b7be024b02db81951c0f0714de1b",
 	}
 
-	attestationBytes, err := hex.DecodeString(response.Attestation)
+	attestationBytes, err := hex.DecodeString(strings.TrimPrefix(response.Attestation, "0x"))
 	require.NoError(t, err)
 
 	responseBytes, err := json.Marshal(response)
