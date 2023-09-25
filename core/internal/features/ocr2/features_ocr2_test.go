84d0aa804388f0633af331ad061a74dde2ac8796
Sam Davies <samsondav@protonmail.com>
2023-09-25 10:49:56 -0400

    More PR comments

--- core/internal/features/ocr2/features_ocr2_test.go
+++ core/internal/features/ocr2/features_ocr2_test.go
@@ -28,10 +28,10 @@ import (
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/maps"
 
+	"github.com/smartcontractkit/libocr/bigbigendian"
 	testoffchainaggregator2 "github.com/smartcontractkit/libocr/gethwrappers2/testocr2aggregator"
 	confighelper2 "github.com/smartcontractkit/libocr/offchainreporting2plus/confighelper"
 	ocrtypes2 "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
-	"github.com/smartcontractkit/libocr/bigbigendian"
 
 	"github.com/smartcontractkit/chainlink/v2/core/assets"
 	"github.com/smartcontractkit/chainlink/v2/core/bridges"
@@ -252,7 +252,7 @@ func TestIntegration_OCR2(t *testing.T) {
 	maxAnswer.Exp(big.NewInt(2), big.NewInt(191), nil)
 	maxAnswer.Sub(maxAnswer, big.NewInt(1))
 
-	onchainConfig := generateDefaultOCR2OnchainConfig(minAnswer, maxAnswer)
+	onchainConfig := generateDefaultOCR2OnchainConfig(t, minAnswer, maxAnswer)
 	lggr.Debugw("Setting Config on Oracle Contract",
 		"signers", signers,
 		"transmitters", transmitters,
@@ -527,7 +527,7 @@ func TestIntegration_OCR2_ForwarderFlow(t *testing.T) {
 	maxAnswer.Exp(big.NewInt(2), big.NewInt(191), nil)
 	maxAnswer.Sub(maxAnswer, big.NewInt(1))
 
-	onchainConfig := generateDefaultOCR2OnchainConfig(minAnswer, maxAnswer)
+	onchainConfig := generateDefaultOCR2OnchainConfig(t, minAnswer, maxAnswer)
 
 	lggr.Debugw("Setting Config on Oracle Contract",
 		"signers", signers,
@@ -738,24 +738,24 @@ juelsPerFeeCoinSource = """
 	assert.Equal(t, digestAndEpoch.Epoch, epoch)
 }
 
-func generateDefaultOCR2OnchainConfig(minValue *big.Int, maxValue *big.Int) []byte {
+func generateDefaultOCR2OnchainConfig(t *testing.T, minValue *big.Int, maxValue *big.Int) []byte {
 	serializedConfig := make([]byte, 0)
 
 	s1, err := bigbigendian.SerializeSigned(1, big.NewInt(1)) //version
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	serializedConfig = append(serializedConfig, s1...)
 
 	s2, err := bigbigendian.SerializeSigned(24, minValue) //min
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	serializedConfig = append(serializedConfig, s2...)
 
 	s3, err := bigbigendian.SerializeSigned(24, maxValue) //max
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	serializedConfig = append(serializedConfig, s3...)
 
