6ae09dc36d0d40ccffd4026520915a1cf4fa5abe
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2023-09-29 14:03:48 -0700

    updates to bump chainlink-env and new chainlink charts (#10831)
    * updates to bump chainlink-env and new chainlink charts
    
    * more fixes
    
    * update to chaos tests
    
    * update replica data type

--- integration-tests/chaos/automation_chaos_test.go
+++ integration-tests/chaos/automation_chaos_test.go
@@ -42,7 +42,7 @@ AnnounceAddresses = ["0.0.0.0:6690"]
 ListenAddresses = ["0.0.0.0:6690"]`
 
 	defaultAutomationSettings = map[string]interface{}{
-		"replicas": "6",
+		"replicas": 6,
 		"toml":     client.AddNetworksConfig(baseTOML, networks.SelectedNetwork),
 		"db": map[string]interface{}{
 			"stateful": true,
@@ -194,11 +194,11 @@ func TestAutomationChaos(t *testing.T) {
 				return
 			}
 
-			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=", 1, 2, ChaosGroupMinority)
+			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=node-", 1, 2, ChaosGroupMinority)
 			require.NoError(t, err)
-			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=", 3, 5, ChaosGroupMajority)
+			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=node-", 3, 5, ChaosGroupMajority)
 			require.NoError(t, err)
-			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=", 2, 5, ChaosGroupMajorityPlus)
+			err = testEnvironment.Client.LabelChaosGroup(testEnvironment.Cfg.Namespace, "instance=node-", 2, 5, ChaosGroupMajorityPlus)
 			require.NoError(t, err)
 
 			chainClient, err := blockchain.NewEVMClient(network, testEnvironment, l)
