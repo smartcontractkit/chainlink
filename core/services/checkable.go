198528a25501810e547b5e744b30ae6cba19ec0a
Jordan Krage <jmank88@gmail.com>
2023-10-04 09:43:21 -0500

    core/services: flag duplicate service names (#10843)

--- core/services/checkable.go
+++ core/services/checkable.go
@@ -1,5 +1,10 @@
 package services
 
+import (
+	"errors"
+	"testing"
+)
+
 // Checkable should be implemented by any type requiring health checks.
 // From the k8s docs:
 // > ready means it’s initialized and healthy means that it can accept traffic in kubernetes
@@ -9,7 +14,26 @@ type Checkable interface {
 	Ready() error
 	// HealthReport returns a full health report of the callee including it's dependencies.
 	// key is the dep name, value is nil if healthy, or error message otherwise.
+	// See CopyHealth.
 	HealthReport() map[string]error
 	// Name returns the fully qualified name of the component. Usually the logger name.
 	Name() string
+}
+
+// CopyHealth copies health statuses from src to dest.
+// If duplicate names are encountered, the errors are joined, unless testing in which case a panic is thrown.
+func CopyHealth(dest, src map[string]error) {
+	for name, err := range src {
+		errOrig, ok := dest[name]
+		if ok {
+			if testing.Testing() {
+				panic("service names must be unique: duplicate name: " + name)
+			}
+			if errOrig != nil {
+				dest[name] = errors.Join(errOrig, err)
+				continue
+			}
+		}
+		dest[name] = err
+	}
 }
