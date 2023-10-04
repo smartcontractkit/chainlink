80d0c428101566218fb8d9db901602655fc8b5b5
Sri Kidambi <kidambisrinivas@users.noreply.github.com>
2023-10-04 13:41:21 +0300

    Add VRFV2PlusWrapper as consumer to new migrated coordinator

--- contracts/src/v0.8/dev/vrf/VRFV2PlusWrapper.sol
+++ contracts/src/v0.8/dev/vrf/VRFV2PlusWrapper.sol
@@ -569,6 +569,7 @@ contract VRFV2PlusWrapper is ConfirmedOwner, TypeAndVersionInterface, VRFConsume
 
   function migrate(address newCoordinator) external onlyOwner {
     IMigration(address(s_vrfCoordinator)).migrate(SUBSCRIPTION_ID, newCoordinator);
+    s_vrfCoordinator.addConsumer(SUBSCRIPTION_ID, address(this));
   }
 }
 
