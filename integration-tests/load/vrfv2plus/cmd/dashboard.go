05d7b5a2c4afe49250818d48d4733cdcf835583c
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2023-10-05 19:51:24 +0300

    VRF-599: add load tests; test refactoring to have configurable setup;… (#10834)
    * VRF-599: add load tests; test refactoring to have configurable setup; adding GH Action workflow
    
    * VRF-599: PR comments
    
    * VRF-599: PR comments

--- integration-tests/load/vrfv2plus/cmd/dashboard.go
+++ integration-tests/load/vrfv2plus/cmd/dashboard.go
@@ -26,14 +26,14 @@ func main() {
 					),
 					timeseries.WithPrometheusTarget(
 						`
-					last_over_time({type="vrfv2_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
+					last_over_time({type="vrfv2plus_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
 					| json
 					| unwrap RequestCount [$__interval]) by (node_id, go_test_name, gen_name)
 					`, prometheus.Legend("{{go_test_name}} requests"),
 					),
 					timeseries.WithPrometheusTarget(
 						`
-					last_over_time({type="vrfv2_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
+					last_over_time({type="vrfv2plus_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
 					| json
 					| unwrap FulfilmentCount [$__interval]) by (node_id, go_test_name, gen_name)
 					`, prometheus.Legend("{{go_test_name}} fulfillments"),
@@ -49,21 +49,21 @@ func main() {
 					),
 					timeseries.WithPrometheusTarget(
 						`
-					last_over_time({type="vrfv2_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
+					last_over_time({type="vrfv2plus_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
 					| json
 					| unwrap AverageFulfillmentInMillions [$__interval]) by (node_id, go_test_name, gen_name) / 1e6
 					`, prometheus.Legend("{{go_test_name}} avg"),
 					),
 					timeseries.WithPrometheusTarget(
 						`
-					last_over_time({type="vrfv2_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
+					last_over_time({type="vrfv2plus_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
 					| json
 					| unwrap SlowestFulfillment [$__interval]) by (node_id, go_test_name, gen_name)
 					`, prometheus.Legend("{{go_test_name}} slowest"),
 					),
 					timeseries.WithPrometheusTarget(
 						`
-					last_over_time({type="vrfv2_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
+					last_over_time({type="vrfv2plus_contracts_load_summary", go_test_name=~"${go_test_name:pipe}", branch=~"${branch:pipe}", commit=~"${commit:pipe}", gen_name=~"${gen_name:pipe}"}
 					| json
 					| unwrap FastestFulfillment [$__interval]) by (node_id, go_test_name, gen_name)
 					`, prometheus.Legend("{{go_test_name}} fastest"),
@@ -92,7 +92,7 @@ func main() {
 	//export GRAFANA_TOKEN=...
 	//export DATA_SOURCE_NAME=Loki
 	//export DASHBOARD_FOLDER=LoadTests
-	//export DASHBOARD_NAME=WaspVRFv2
+	//export DASHBOARD_NAME=Waspvrfv2plus
 	if _, err := d.Deploy(); err != nil {
 		panic(err)
 	}
