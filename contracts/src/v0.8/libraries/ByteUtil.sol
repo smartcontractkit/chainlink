1da4afa6946081a2e23c188355091dd205b97961
Rens Rooimans <github@rensrooimans.nl>
2023-10-05 15:01:32 +0200

    clean /libraries

--- contracts/src/v0.8/libraries/ByteUtil.sol
+++ contracts/src/v0.8/libraries/ByteUtil.sol
@@ -16,10 +16,11 @@ library ByteUtil {
    * @param offset Position to start reading from.
    * @return result The uint256 read from the byte array.
    */
-  function readUint256(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
+  function _readUint256(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
     //bounds check
     if (offset + 32 > data.length) revert MalformedData();
 
+    // solhint-disable-next-line no-inline-assembly
     assembly {
       //load 32 byte word accounting for 32 bit length and offset
       result := mload(add(add(data, 32), offset))
@@ -32,10 +33,11 @@ library ByteUtil {
    * @param offset Position to start reading from.
    * @return result The uint192 read from the byte array.
    */
-  function readUint192(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
+  function _readUint192(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
     //bounds check
     if (offset + 24 > data.length) revert MalformedData();
 
+    // solhint-disable-next-line no-inline-assembly
     assembly {
       //load 32 byte word accounting for 32 bit length and offset
       result := mload(add(add(data, 32), offset))
@@ -50,10 +52,11 @@ library ByteUtil {
    * @param offset Position to start reading from.
    * @return result The uint32 read from the byte array.
    */
-  function readUint32(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
+  function _readUint32(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
     //bounds check
     if (offset + 4 > data.length) revert MalformedData();
 
+    // solhint-disable-next-line no-inline-assembly
     assembly {
       //load 32 byte word accounting for 32 bit length and offset
       result := mload(add(add(data, 32), offset))
@@ -68,10 +71,11 @@ library ByteUtil {
    * @param offset Position to start reading from.
    * @return result The uint32 read from the byte array.
    */
-  function readAddress(bytes memory data, uint256 offset) internal pure returns (address result) {
+  function _readAddress(bytes memory data, uint256 offset) internal pure returns (address result) {
     //bounds check
     if (offset + 20 > data.length) revert MalformedData();
 
+    // solhint-disable-next-line no-inline-assembly
     assembly {
       //load 32 byte word accounting for 32 bit length and offset
       let word := mload(add(add(data, 32), offset))
