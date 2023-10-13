6d1a1c85791d524911752e486a27595f7d5af5fa
Tate <tate.exon@smartcontract.com>
2023-10-13 09:47:12 -0600

    [TT-537] Use ipv4 localhost 127.0.0.1 to avoid intermittent ipv6 port mislanignment

--- integration-tests/docker/test_env/cl_node.go
+++ integration-tests/docker/test_env/cl_node.go
@@ -286,7 +286,7 @@ func (n *ClNode) StartContainer() error {
 			return err
 		}
 	}
-	clEndpoint, err := container.Endpoint(context.Background(), "http")
+	clEndpoint, err := test_env.GetEndpoint(context.Background(), container, "http")
 	if err != nil {
 		return err
 	}
