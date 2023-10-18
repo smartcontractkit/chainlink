bd1a6cd6a6926d602534c412bad771f28aecce71
davidcauchi <david.cauchi@smartcontract.com>
2023-10-18 10:52:10 +0200

    Add contract deployer

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -157,6 +157,8 @@ func NewContractDeployer(bcClient blockchain.EVMClient, logger zerolog.Logger) (
 		return &ScrollContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	case *blockchain.PolygonZkEvmClient:
 		return &PolygonZkEvmContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
+	case *blockchain.FantomClient:
+		return &FantomContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	}
 	return nil, errors.New("unknown blockchain client implementation for contract deployer, register blockchain client in NewContractDeployer")
 }
@@ -213,6 +215,10 @@ type ScrollContractDeployer struct {
 }
 
 type PolygonZkEvmContractDeployer struct {
+	*EthereumContractDeployer
+}
+
+type FantomContractDeployer struct {
 	*EthereumContractDeployer
 }
 
