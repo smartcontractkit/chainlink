34b10508bbcdc6e8afe0977549829ef2175bcb2a
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2023-10-24 17:27:53 +0100

    Prettier fix

--- contracts/src/v0.8/automation/dev/ComposerCrossChain/CCIPDeps/Client.sol
+++ contracts/src/v0.8/automation/dev/ComposerCrossChain/CCIPDeps/Client.sol
@@ -1,37 +1,37 @@
 pragma solidity ^0.8.0;
 
 library Client {
-    struct EVMTokenAmount {
-        address token; // token address on the local chain.
-        uint256 amount; // Amount of tokens.
-    }
+  struct EVMTokenAmount {
+    address token; // token address on the local chain.
+    uint256 amount; // Amount of tokens.
+  }
 
-    struct Any2EVMMessage {
-        bytes32 messageId; // MessageId corresponding to ccipSend on source.
-        uint64 sourceChainSelector; // Source chain selector.
-        bytes sender; // abi.decode(sender) if coming from an EVM chain.
-        bytes data; // payload sent in original message.
-        EVMTokenAmount[] destTokenAmounts; // Tokens and their amounts in their destination chain representation.
-    }
+  struct Any2EVMMessage {
+    bytes32 messageId; // MessageId corresponding to ccipSend on source.
+    uint64 sourceChainSelector; // Source chain selector.
+    bytes sender; // abi.decode(sender) if coming from an EVM chain.
+    bytes data; // payload sent in original message.
+    EVMTokenAmount[] destTokenAmounts; // Tokens and their amounts in their destination chain representation.
+  }
 
-    // If extraArgs is empty bytes, the default is 200k gas limit.
-    struct EVM2AnyMessage {
-        bytes receiver; // abi.encode(receiver address) for dest EVM chains
-        bytes data; // Data payload
-        EVMTokenAmount[] tokenAmounts; // Token transfers
-        address feeToken; // Address of feeToken. address(0) means you will send msg.value.
-        bytes extraArgs; // Populate this with _argsToBytes(EVMExtraArgsV1)
-    }
+  // If extraArgs is empty bytes, the default is 200k gas limit.
+  struct EVM2AnyMessage {
+    bytes receiver; // abi.encode(receiver address) for dest EVM chains
+    bytes data; // Data payload
+    EVMTokenAmount[] tokenAmounts; // Token transfers
+    address feeToken; // Address of feeToken. address(0) means you will send msg.value.
+    bytes extraArgs; // Populate this with _argsToBytes(EVMExtraArgsV1)
+  }
 
-    // bytes4(keccak256("CCIP EVMExtraArgsV1"));
-    bytes4 public constant EVM_EXTRA_ARGS_V1_TAG = 0x97a657c9;
+  // bytes4(keccak256("CCIP EVMExtraArgsV1"));
+  bytes4 public constant EVM_EXTRA_ARGS_V1_TAG = 0x97a657c9;
 
-    struct EVMExtraArgsV1 {
-        uint256 gasLimit;
-        bool strict;
-    }
+  struct EVMExtraArgsV1 {
+    uint256 gasLimit;
+    bool strict;
+  }
 
-    function _argsToBytes(EVMExtraArgsV1 memory extraArgs) internal pure returns (bytes memory bts) {
-        return abi.encodeWithSelector(EVM_EXTRA_ARGS_V1_TAG, extraArgs);
-    }
+  function _argsToBytes(EVMExtraArgsV1 memory extraArgs) internal pure returns (bytes memory bts) {
+    return abi.encodeWithSelector(EVM_EXTRA_ARGS_V1_TAG, extraArgs);
+  }
 }
