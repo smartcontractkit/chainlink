58af4e01f6ce446be2c6bc7e47f0499c10e713f7
george-dorin <george.dorin@smartcontract.com>
2023-10-27 19:38:58 +0300

    - Update enhancedEAmercury proto file - Fix tests

--- core/services/ocrcommon/telemetry.go
+++ core/services/ocrcommon/telemetry.go
@@ -287,9 +287,10 @@ func (e *EnhancedTelemetryService[T]) collectMercuryEnhancedTelemetry(d Enhanced
 	var bh string
 	var bt uint64
 	// v1+v2+v3 fields
-	var bp int64
+	bp := big.NewInt(0)
 	//v1+v3 fields
-	var bid, ask int64
+	bid := big.NewInt(0)
+	ask := big.NewInt(0)
 	// v2+v3 fields
 	var mfts, lp, np int64
 
@@ -306,13 +307,13 @@ func (e *EnhancedTelemetryService[T]) collectMercuryEnhancedTelemetry(d Enhanced
 			bt = obs.CurrentBlockTimestamp.Val
 		}
 		if obs.BenchmarkPrice.Err == nil && obs.BenchmarkPrice.Val != nil {
-			bp = obs.BenchmarkPrice.Val.Int64()
+			bp = obs.BenchmarkPrice.Val
 		}
 		if obs.Bid.Err == nil && obs.Bid.Val != nil {
-			bid = obs.Bid.Val.Int64()
+			bid = obs.Bid.Val
 		}
 		if obs.Ask.Err == nil && obs.Ask.Val != nil {
-			ask = obs.Ask.Val.Int64()
+			ask = obs.Ask.Val
 		}
 	case d.V2Observation != nil:
 		obs := *d.V2Observation
@@ -326,7 +327,7 @@ func (e *EnhancedTelemetryService[T]) collectMercuryEnhancedTelemetry(d Enhanced
 			np = obs.NativePrice.Val.Int64()
 		}
 		if obs.BenchmarkPrice.Err == nil && obs.BenchmarkPrice.Val != nil {
-			bp = obs.BenchmarkPrice.Val.Int64()
+			bp = obs.BenchmarkPrice.Val
 		}
 	case d.V3Observation != nil:
 		obs := *d.V3Observation
@@ -340,13 +341,13 @@ func (e *EnhancedTelemetryService[T]) collectMercuryEnhancedTelemetry(d Enhanced
 			np = obs.NativePrice.Val.Int64()
 		}
 		if obs.BenchmarkPrice.Err == nil && obs.BenchmarkPrice.Val != nil {
-			bp = obs.BenchmarkPrice.Val.Int64()
+			bp = obs.BenchmarkPrice.Val
 		}
 		if obs.Bid.Err == nil && obs.Bid.Val != nil {
-			bid = obs.Bid.Val.Int64()
+			bid = obs.Bid.Val
 		}
 		if obs.Ask.Err == nil && obs.Ask.Val != nil {
-			ask = obs.Ask.Val.Int64()
+			ask = obs.Ask.Val
 		}
 	}
 
@@ -367,37 +368,41 @@ func (e *EnhancedTelemetryService[T]) collectMercuryEnhancedTelemetry(d Enhanced
 		}
 
 		assetSymbol := e.getAssetSymbolFromRequestData(bridgeTask.RequestData)
-		benchmarkPrice, bidPrice, askPrice := e.getPricesFromResults(trr, d.TaskRunResults)
+
+		benchmarkPrice, bidPrice, askPrice := e.getPricesFromResults(trr, d.TaskRunResults, d.FeedVersion)
 
 		t := &telem.EnhancedEAMercury{
-			DataSource:                    eaTelem.DataSource,
-			DpBenchmarkPrice:              benchmarkPrice,
-			DpBid:                         bidPrice,
-			DpAsk:                         askPrice,
-			CurrentBlockNumber:            bn,
-			CurrentBlockHash:              bh,
-			CurrentBlockTimestamp:         bt,
-			FetchMaxFinalizedTimestamp:    d.FetchMaxFinalizedTimestamp,
-			MaxFinalizedTimestamp:         mfts,
-			BridgeTaskRunStartedTimestamp: trr.CreatedAt.UnixMilli(),
-			BridgeTaskRunEndedTimestamp:   trr.FinishedAt.Time.UnixMilli(),
-			ProviderRequestedTimestamp:    eaTelem.ProviderRequestedTimestamp,
-			ProviderReceivedTimestamp:     eaTelem.ProviderReceivedTimestamp,
-			ProviderDataStreamEstablished: eaTelem.ProviderDataStreamEstablished,
-			ProviderIndicatedTime:         eaTelem.ProviderIndicatedTime,
-			Feed:                          e.job.OCR2OracleSpec.FeedID.Hex(),
-			ObservationBenchmarkPrice:     bp,
-			ObservationBid:                bid,
-			ObservationAsk:                ask,
-			IsLinkFeed:                    d.IsLinkFeed,
-			LinkPrice:                     lp,
-			IsNativeFeed:                  d.IsNativeFeed,
-			NativePrice:                   np,
-			ConfigDigest:                  d.RepTimestamp.ConfigDigest.Hex(),
-			Round:                         int64(d.RepTimestamp.Round),
-			Epoch:                         int64(d.RepTimestamp.Epoch),
-			AssetSymbol:                   assetSymbol,
-			Version:                       uint32(d.FeedVersion),
+			DataSource:                      eaTelem.DataSource,
+			DpBenchmarkPrice:                benchmarkPrice,
+			DpBid:                           bidPrice,
+			DpAsk:                           askPrice,
+			CurrentBlockNumber:              bn,
+			CurrentBlockHash:                bh,
+			CurrentBlockTimestamp:           bt,
+			FetchMaxFinalizedTimestamp:      d.FetchMaxFinalizedTimestamp,
+			MaxFinalizedTimestamp:           mfts,
+			BridgeTaskRunStartedTimestamp:   trr.CreatedAt.UnixMilli(),
+			BridgeTaskRunEndedTimestamp:     trr.FinishedAt.Time.UnixMilli(),
+			ProviderRequestedTimestamp:      eaTelem.ProviderRequestedTimestamp,
+			ProviderReceivedTimestamp:       eaTelem.ProviderReceivedTimestamp,
+			ProviderDataStreamEstablished:   eaTelem.ProviderDataStreamEstablished,
+			ProviderIndicatedTime:           eaTelem.ProviderIndicatedTime,
+			Feed:                            e.job.OCR2OracleSpec.FeedID.Hex(),
+			ObservationBenchmarkPrice:       bp.Int64(),
+			ObservationBid:                  bid.Int64(),
+			ObservationAsk:                  ask.Int64(),
+			ObservationBenchmarkPriceString: stringOrEmpty(bp),
+			ObservationBidString:            stringOrEmpty(bid),
+			ObservationAskString:            stringOrEmpty(ask),
+			IsLinkFeed:                      d.IsLinkFeed,
+			LinkPrice:                       lp,
+			IsNativeFeed:                    d.IsNativeFeed,
+			NativePrice:                     np,
+			ConfigDigest:                    d.RepTimestamp.ConfigDigest.Hex(),
+			Round:                           int64(d.RepTimestamp.Round),
+			Epoch:                           int64(d.RepTimestamp.Epoch),
+			AssetSymbol:                     assetSymbol,
+			Version:                         uint32(d.FeedVersion),
 		}
 
 		bytes, err := proto.Marshal(t)
@@ -439,7 +444,7 @@ func ShouldCollectEnhancedTelemetryMercury(jb job.Job) bool {
 
 // getPricesFromResults parses the pipeline.TaskRunResults for pipeline.TaskTypeJSONParse and gets the benchmarkPrice,
 // bid and ask. This functions expects the pipeline.TaskRunResults to be correctly ordered
-func (e *EnhancedTelemetryService[T]) getPricesFromResults(startTask pipeline.TaskRunResult, allTasks pipeline.TaskRunResults) (float64, float64, float64) {
+func (e *EnhancedTelemetryService[T]) getPricesFromResults(startTask pipeline.TaskRunResult, allTasks pipeline.TaskRunResults, mercuryVersion mercuryutils.FeedVersion) (float64, float64, float64) {
 	var benchmarkPrice, askPrice, bidPrice float64
 	var err error
 	// We rely on task results to be sorted in the correct order
@@ -459,7 +464,17 @@ func (e *EnhancedTelemetryService[T]) getPricesFromResults(startTask pipeline.Ta
 		}
 	}
 
+	// mercury version 2 only supports benchmarkPrice
+	if mercuryVersion == 2 {
+		return benchmarkPrice, 0, 0
+	}
+
 	bidTask := allTasks.GetNextTaskOf(*benchmarkPriceTask)
+	if bidTask == nil {
+		e.lggr.Warnf("cannot parse enhanced EA telemetry bid price, task is nil, job %d, id %s", e.job.ID)
+		return benchmarkPrice, 0, 0
+	}
+
 	if bidTask != nil && bidTask.Task.Type() == pipeline.TaskTypeJSONParse {
 		if bidTask.Result.Error != nil {
 			e.lggr.Warnw(fmt.Sprintf("got error for enhanced EA telemetry bid price, job %d, id %s: %s", e.job.ID, bidTask.Task.DotID(), bidTask.Result.Error), "err", bidTask.Result.Error)
@@ -472,6 +487,10 @@ func (e *EnhancedTelemetryService[T]) getPricesFromResults(startTask pipeline.Ta
 	}
 
 	askTask := allTasks.GetNextTaskOf(*bidTask)
+	if askTask == nil {
+		e.lggr.Warnf("cannot parse enhanced EA telemetry ask price, task is nil, job %d, id %s", e.job.ID)
+		return benchmarkPrice, bidPrice, 0
+	}
 	if askTask != nil && askTask.Task.Type() == pipeline.TaskTypeJSONParse {
 		if bidTask.Result.Error != nil {
 			e.lggr.Warnw(fmt.Sprintf("got error for enhanced EA telemetry ask price, job %d, id %s: %s", e.job.ID, askTask.Task.DotID(), askTask.Result.Error), "err", askTask.Result.Error)
@@ -484,25 +503,6 @@ func (e *EnhancedTelemetryService[T]) getPricesFromResults(startTask pipeline.Ta
 	}
 
 	return benchmarkPrice, bidPrice, askPrice
-}
-
-// getFinalValues runs a parse on the pipeline.TaskRunResults and returns the values
-func (e *EnhancedTelemetryService[T]) getFinalValues(obs relaymercuryv1.Observation) (*big.Int, *big.Int, *big.Int, int64, []byte, uint64) {
-	benchmarkPrice := big.NewInt(0)
-	bid := big.NewInt(0)
-	ask := big.NewInt(0)
-
-	if obs.BenchmarkPrice.Val != nil {
-		benchmarkPrice = obs.BenchmarkPrice.Val
-	}
-	if obs.Bid.Val != nil {
-		bid = obs.Bid.Val
-	}
-	if obs.Ask.Val != nil {
-		ask = obs.Ask.Val
-	}
-
-	return benchmarkPrice, bid, ask, obs.CurrentBlockNum.Val, obs.CurrentBlockHash.Val, obs.CurrentBlockTimestamp.Val
 }
 
 // MaybeEnqueueEnhancedTelem sends data to the telemetry channel for processing
@@ -528,4 +528,11 @@ func getResultFloat64(task *pipeline.TaskRunResult) (float64, error) {
 	}
 	resultFloat64, _ := result.Float64()
 	return resultFloat64, nil
+}
+
+func stringOrEmpty(n *big.Int) string {
+	if n.Cmp(big.NewInt(0)) == 0 {
+		return ""
+	}
+	return n.String()
 }
