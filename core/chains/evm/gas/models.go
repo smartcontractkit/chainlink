c61edab16c5b2ef2a01994cfe5b7d390496e6603
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2023-10-30 14:12:27 -0400

    L2Suggested backwards compatibility

--- core/chains/evm/gas/models.go
+++ core/chains/evm/gas/models.go
@@ -79,7 +79,7 @@ func NewEstimator(lggr logger.Logger, ethClient evmclient.Client, cfg Config, ge
 		return NewWrappedEvmEstimator(NewBlockHistoryEstimator(lggr, ethClient, cfg, geCfg, bh, *ethClient.ConfiguredChainID()), df, l1Oracle)
 	case "FixedPrice":
 		return NewWrappedEvmEstimator(NewFixedPriceEstimator(geCfg, bh, lggr), df, l1Oracle)
-	case "Optimism2", "SuggestedPrice":
+	case "Optimism2", "L2Suggested", "SuggestedPrice":
 		return NewWrappedEvmEstimator(NewSuggestedPriceEstimator(lggr, ethClient), df, l1Oracle)
 	default:
 		lggr.Warnf("GasEstimator: unrecognised mode '%s', falling back to FixedPriceEstimator", s)
