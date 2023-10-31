3c27c61cd3b6cd3971e1c4fdea880a595b22d36a
ilija <pavlovicilija42@gmail.com>
2023-10-31 15:58:33 +0100

    Add chain reader config validation

--- core/services/relay/evm/chain_reader.go
+++ core/services/relay/evm/chain_reader.go
@@ -3,7 +3,10 @@ package evm
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/pkg/errors"
 	relaytypes "github.com/smartcontractkit/chainlink-relay/pkg/types"
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm"
@@ -24,16 +27,118 @@ func newChainReader(lggr logger.Logger, chain evm.Chain, ropts *types.RelayOpts)
 		return nil, relaytypes.ErrorChainReaderUnsupported{}
 	}
 
-	if err = validateChainReaderConfig(*relayConfig.ChainReader); err != nil {
-		return nil, fmt.Errorf("Invalid ChainReader configuration: %w", err)
+	if err = ValidateChainReaderConfig(*relayConfig.ChainReader); err != nil {
+		return nil, fmt.Errorf("invalid ChainReader configuration: %w", err)
 	}
 
 	return NewChainReaderService(lggr, chain.LogPoller())
 }
 
-func validateChainReaderConfig(cfg types.ChainReaderConfig) error {
-	// Validate config (check ABI from job spec against imported gethwrappers, etc.)
+func ValidateChainReaderConfig(cfg types.ChainReaderConfig) error {
+	for contractName, chainContractReader := range cfg.ChainContractReaders {
+		abi, err := abi.JSON(strings.NewReader(chainContractReader.ContractABI))
+		if err != nil {
+			return err
+		}
+
+		for chainReadingDefinitionName, chainReaderDefinition := range chainContractReader.ChainReaderDefinitions {
+			switch chainReaderDefinition.ReadType {
+			case types.Method:
+				err = validateMethods(abi, chainReaderDefinition)
+			case types.Event:
+				err = validateEvents(abi, chainReaderDefinition)
+			default:
+				return fmt.Errorf("invalid chain reader defintion read type: %d", chainReaderDefinition.ReadType)
+			}
+			if err != nil {
+				return errors.Wrap(err, fmt.Sprintf("invalid chain reader config for contract: %q chain reading definition: %q", contractName, chainReadingDefinitionName))
+			}
+		}
+	}
+
 	return nil
+}
+
+func validateEvents(contractABI abi.ABI, chainReaderDefinition types.ChainReaderDefinition) error {
+	event, methodExists := contractABI.Events[chainReaderDefinition.ChainSpecificName]
+	if !methodExists {
+		return fmt.Errorf("method: %s doesn't exist", chainReaderDefinition.ChainSpecificName)
+	}
+
+	if !areChainReaderArgumentsValid(event.Inputs, chainReaderDefinition.ReturnValues) {
+		var abiEventInputsNames []string
+		for _, input := range event.Inputs {
+			abiEventInputsNames = append(abiEventInputsNames, input.Name)
+		}
+		return fmt.Errorf("return values: [%s] don't match abi event inputs: [%s]", strings.Join(chainReaderDefinition.ReturnValues, ","), strings.Join(abiEventInputsNames, ","))
+	}
+
+	var abiEventIndexedInputs []abi.Argument
+	for _, eventInput := range event.Inputs {
+		if eventInput.Indexed {
+			abiEventIndexedInputs = append(abiEventIndexedInputs, eventInput)
+		}
+	}
+
+	var chainReaderEventParams []string
+	for chainReaderEventParam := range chainReaderDefinition.Params {
+		chainReaderEventParams = append(chainReaderEventParams, chainReaderEventParam)
+	}
+
+	if !areChainReaderArgumentsValid(abiEventIndexedInputs, chainReaderEventParams) {
+		var abiEventIndexedInputsNames []string
+		for _, abiEventIndexedInput := range abiEventIndexedInputs {
+			abiEventIndexedInputsNames = append(abiEventIndexedInputsNames, abiEventIndexedInput.Name)
+		}
+		return fmt.Errorf("params: [%s] don't match abi event indexed inputs: [%s]", strings.Join(chainReaderEventParams, ","), strings.Join(abiEventIndexedInputsNames, ","))
+	}
+	return nil
+}
+
+func validateMethods(abi abi.ABI, chainReaderDefinition types.ChainReaderDefinition) error {
+	method, methodExists := abi.Methods[chainReaderDefinition.ChainSpecificName]
+	if !methodExists {
+		return fmt.Errorf("method: %q doesn't exist", chainReaderDefinition.ChainSpecificName)
+	}
+
+	var methodNames []string
+	for methodName := range chainReaderDefinition.Params {
+		methodNames = append(methodNames, methodName)
+	}
+
+	if !areChainReaderArgumentsValid(method.Inputs, methodNames) {
+		var abiMethodInputs []string
+		for _, input := range method.Inputs {
+			abiMethodInputs = append(abiMethodInputs, input.Name)
+		}
+		return fmt.Errorf("params: [%s] don't match abi method inputs: [%s]", strings.Join(methodNames, ","), strings.Join(abiMethodInputs, ","))
+	}
+
+	if !areChainReaderArgumentsValid(method.Outputs, chainReaderDefinition.ReturnValues) {
+		var abiMethodOutputs []string
+		for _, input := range method.Outputs {
+			abiMethodOutputs = append(abiMethodOutputs, input.Name)
+		}
+		return fmt.Errorf("return values: [%s] don't match abi method outputs: [%s]", strings.Join(chainReaderDefinition.ReturnValues, ","), strings.Join(abiMethodOutputs, ","))
+	}
+
+	return nil
+}
+
+func areChainReaderArgumentsValid(contractArgs []abi.Argument, chainReaderArgs []string) bool {
+	for _, chArgName := range chainReaderArgs {
+		found := false
+		for _, contractArg := range contractArgs {
+			if chArgName == contractArg.Name {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
 }
 
 func (cr *chainReader) initialize() error {
