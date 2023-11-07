c491de7962a072a0a78ca06b453b481247dc7a8b
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2023-11-07 12:11:36 +0100

    Adds WeMix chain config (#10793)
    * Adds WeMix testnet config
    
    * Extending NoNewHeads threshold to 3s
    
    * Adds WeMix contract loader
    
    * Downgrading to go 1.20.5 bullseye
    
    * Revert "Downgrading to go 1.20.5 bullseye"
    
    This reverts commit 7eab839819456513dcf7f83ca8f08c97bdeb3835.
    
    * Update testing branch
    
    * Excludes fee delegation transactions for Wemix
    
    * Update testing branch
    
    * Linting
    
    * Fix typo
    
    * Update CTF
    
    ---------
    
    Co-authored-by: davidcauchi <david.cauchi@smartcontract.com>

--- core/config/chaintype.go
+++ core/config/chaintype.go
@@ -15,18 +15,18 @@ const (
 	ChainOptimismBedrock ChainType = "optimismBedrock"
 	ChainXDai            ChainType = "xdai"
 	ChainCelo            ChainType = "celo"
+	ChainWeMix           ChainType = "wemix"
 	ChainKroma           ChainType = "kroma"
 )
 
 var ErrInvalidChainType = fmt.Errorf("must be one of %s or omitted", strings.Join([]string{
 	string(ChainArbitrum), string(ChainMetis), string(ChainXDai), string(ChainOptimismBedrock), string(ChainCelo),
-	string(ChainKroma),
-}, ", "))
+	string(ChainKroma), string(ChainWeMix)}, ", "))
 
 // IsValid returns true if the ChainType value is known or empty.
 func (c ChainType) IsValid() bool {
 	switch c {
-	case "", ChainArbitrum, ChainMetis, ChainOptimismBedrock, ChainXDai, ChainCelo, ChainKroma:
+	case "", ChainArbitrum, ChainMetis, ChainOptimismBedrock, ChainXDai, ChainCelo, ChainKroma, ChainWeMix:
 		return true
 	}
 	return false
