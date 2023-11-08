66b9de48a746c5cd472ffb79baaed296166ba87b
skudasov <f4hrenh9it@gmail.com>
2023-11-08 17:21:25 +0100

    common dashboard

--- charts/chainlink-cluster/dashboard/dashboard.go
+++ charts/chainlink-cluster/dashboard/dashboard.go
@@ -9,6 +9,7 @@ import (
 	"github.com/K-Phoen/grabana/dashboard"
 	"github.com/K-Phoen/grabana/logs"
 	"github.com/K-Phoen/grabana/row"
+	"github.com/K-Phoen/grabana/stat"
 	"github.com/K-Phoen/grabana/target/prometheus"
 	"github.com/K-Phoen/grabana/timeseries"
 	"github.com/K-Phoen/grabana/timeseries/axis"
@@ -51,8 +52,8 @@ func NewCLClusterDashboard(name, ldsn, pdsn, dbf, grafanaURL, grafanaToken strin
 	return db, nil
 }
 
-// nodeLogsRowOption returns a row option for a node's logs with name and instance selector
-func (m *CLClusterDashboard) nodeLogsRowOption(name, instanceSelector string) row.Option {
+// logsRowOption returns a row option for a node's logs with name and instance selector
+func (m *CLClusterDashboard) logsRowOption(name, instanceSelector string) row.Option {
 	return row.WithLogs(
 		name,
 		logs.DataSource(m.LokiDataSourceName),
@@ -62,6 +63,51 @@ func (m *CLClusterDashboard) nodeLogsRowOption(name, instanceSelector string) ro
 		logs.WithLokiTarget(fmt.Sprintf(`
 			{namespace="${namespace}", app="app", instance="%s", container="node"}
 		`, instanceSelector)),
+	)
+}
+
+// timeseriesRowOption returns a row option for a timeseries with name, axis unit, query and legend template
+func (m *CLClusterDashboard) timeseriesRowOption(name, axisUnit, query, legendTemplate string) row.Option {
+	var tsq timeseries.Option
+	if legendTemplate != "" {
+		tsq = timeseries.WithPrometheusTarget(
+			query,
+			prometheus.Legend(legendTemplate),
+		)
+	} else {
+		tsq = timeseries.WithPrometheusTarget(query)
+	}
+	var au timeseries.Option
+	if axisUnit != "" {
+		au = timeseries.Axis(
+			axis.Unit(axisUnit),
+		)
+	} else {
+		au = timeseries.Axis()
+	}
+	return row.WithTimeSeries(
+		name,
+		timeseries.Span(6),
+		timeseries.Height("300px"),
+		timeseries.DataSource(m.PrometheusDataSourceName),
+		au,
+		tsq,
+	)
+}
+
+// statRowOption returns a row option for a stat with name, prometheus target and legend template
+func (m *CLClusterDashboard) statRowOption(name, target, legend string) row.Option {
+	return row.WithStat(
+		name,
+		stat.Transparent(),
+		stat.DataSource(m.PrometheusDataSourceName),
+		stat.Text(stat.TextValueAndName),
+		stat.Orientation(stat.OrientationVertical),
+		stat.TitleFontSize(12),
+		stat.ValueFontSize(20),
+		stat.Span(12),
+		stat.Height("100px"),
+		stat.WithPrometheusTarget(target, prometheus.Legend(legend)),
 	)
 }
 
@@ -84,6 +130,11 @@ func (m *CLClusterDashboard) generate() error {
 		),
 		dashboard.Row(
 			"Cluster health",
+			m.statRowOption(
+				"App Version",
+				`version{namespace="${namespace}"}`,
+				"{{pod}} - {{version}}",
+			),
 			row.WithTimeSeries(
 				"Restarts",
 				timeseries.Span(12),
@@ -94,79 +145,220 @@ func (m *CLClusterDashboard) generate() error {
 					prometheus.Legend("{{pod}}"),
 				),
 			),
+			row.WithTimeSeries(
+				"Service Components Health",
+				timeseries.Span(12),
+				timeseries.Height("200px"),
+				timeseries.DataSource(m.PrometheusDataSourceName),
+				timeseries.WithPrometheusTarget(
+					`health{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - {{service_id}}"),
+				),
+			),
+			row.WithTimeSeries(
+				"Log Counters",
+				timeseries.Span(12),
+				timeseries.Height("200px"),
+				timeseries.DataSource(m.PrometheusDataSourceName),
+				timeseries.WithPrometheusTarget(
+					`log_panic_count{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - panic"),
+				),
+				timeseries.WithPrometheusTarget(
+					`log_fatal_count{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - fatal"),
+				),
+				timeseries.WithPrometheusTarget(
+					`log_critical_count{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - critical"),
+				),
+				timeseries.WithPrometheusTarget(
+					`log_error_count{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - error"),
+				),
+			),
+			row.WithTimeSeries(
+				"ETH Balance",
+				timeseries.Span(12),
+				timeseries.Height("200px"),
+				timeseries.DataSource(m.PrometheusDataSourceName),
+				timeseries.WithPrometheusTarget(
+					`eth_balance{namespace="${namespace}"}`,
+					prometheus.Legend("{{pod}} - {{account}}"),
+				),
+			),
 		),
 		// logs
 		dashboard.Row(
 			"Logs",
 			row.Collapse(),
-			m.nodeLogsRowOption("Node 1", "node-1"),
-			m.nodeLogsRowOption("Node 2", "node-2"),
-			m.nodeLogsRowOption("Node 3", "node-3"),
-			m.nodeLogsRowOption("Node 4", "node-4"),
+			m.logsRowOption("Node 1", "node-1"),
+			m.logsRowOption("Node 2", "node-2"),
+			m.logsRowOption("Node 3", "node-3"),
+			m.logsRowOption("Node 4", "node-4"),
 		),
-		// FIXME: these metrics are not exposed by the node for some reason
 		// DON report metrics
 		dashboard.Row("DON Report metrics",
 			row.Collapse(),
-			row.WithTimeSeries(
-				"Plugin Query() time (95th)",
-				timeseries.Span(4),
-				timeseries.Height("300px"),
-				timeseries.DataSource(m.PrometheusDataSourceName),
-				timeseries.Axis(
-					axis.Unit("Sec"),
-				),
-				timeseries.WithPrometheusTarget(
-					`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_query_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
-				),
+			m.timeseriesRowOption(
+				"Plugin Query() count",
+				"Count",
+				`sum(rate(ocr2_reporting_plugin_query_count{namespace="${namespace}", app="app"}[$__rate_interval])) by (service)`,
+				"",
 			),
-			row.WithTimeSeries(
+			m.timeseriesRowOption(
 				"Plugin Observation() time (95th)",
-				timeseries.Span(4),
-				timeseries.Height("300px"),
-				timeseries.DataSource(m.PrometheusDataSourceName),
-				timeseries.Axis(
-					axis.Unit("Sec"),
-				),
-				timeseries.WithPrometheusTarget(
-					`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_observation_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
-				),
+				"Sec",
+				`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_observation_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
+				"",
 			),
-			row.WithTimeSeries(
-				"Plugin ShouldAcceptFinalizedReport() time (95th)",
-				timeseries.Span(4),
-				timeseries.Height("300px"),
-				timeseries.DataSource(m.PrometheusDataSourceName),
-				timeseries.Axis(
-					axis.Unit("Sec"),
-				),
-				timeseries.WithPrometheusTarget(
-					`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_should_accept_finalized_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
-				),
+			m.timeseriesRowOption(
+				"Plugin ShouldAcceptReport() time (95th)",
+				"Sec",
+				`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_should_accept_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
+				"",
 			),
-			row.WithTimeSeries(
+			m.timeseriesRowOption(
 				"Plugin Report() time (95th)",
-				timeseries.Span(6),
-				timeseries.Height("300px"),
-				timeseries.DataSource(m.PrometheusDataSourceName),
-				timeseries.Axis(
-					axis.Unit("Sec"),
-				),
-				timeseries.WithPrometheusTarget(
-					`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
-				),
+				"Sec",
+				`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
+				"",
 			),
-			row.WithTimeSeries(
-				"Plugin ShouldTransmitAcceptedReport() time (95th)",
-				timeseries.Span(6),
-				timeseries.Height("300px"),
-				timeseries.DataSource(m.PrometheusDataSourceName),
-				timeseries.Axis(
-					axis.Unit("Sec"),
-				),
-				timeseries.WithPrometheusTarget(
-					`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_should_transmit_accepted_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
-				),
+			m.timeseriesRowOption(
+				"Plugin ShouldTransmitReport() time (95th)",
+				"Sec",
+				`histogram_quantile(0.95, sum(rate(ocr2_reporting_plugin_should_transmit_report_time_bucket{namespace="${namespace}", app="app"}[$__rate_interval])) by (le, service)) / 1e9`,
+				"",
+			),
+		),
+		dashboard.Row(
+			"DB Connection Metrics (App)",
+			row.Collapse(),
+			m.timeseriesRowOption(
+				"DB Connections MAX",
+				"Conn",
+				`db_conns_max{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"DB Connections Open",
+				"Conn",
+				`db_conns_open{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"DB Connections Used",
+				"Conn",
+				`db_conns_used{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"DB Connections Wait",
+				"Conn",
+				`db_conns_wait{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"DB Wait time",
+				"Sec",
+				`db_wait_time_seconds{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+		),
+		dashboard.Row(
+			"EVM Pool RPC Node Metrics (App)",
+			row.Collapse(),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Calls Success",
+				"",
+				`evm_pool_rpc_node_calls_success{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Calls Total",
+				"",
+				`evm_pool_rpc_node_calls_total{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Dials Success",
+				"",
+				`evm_pool_rpc_node_dials_success{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Dials Total",
+				"",
+				`evm_pool_rpc_node_dials_total{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Highest Seen Block",
+				"",
+				`evm_pool_rpc_node_highest_seen_block{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Total Transitions to Alive",
+				"",
+				`evm_pool_rpc_node_num_transitions_to_alive{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Polls Success",
+				"",
+				`evm_pool_rpc_node_polls_success{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Polls Total",
+				"",
+				`evm_pool_rpc_node_polls_total{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node States",
+				"",
+				`evm_pool_rpc_node_states{namespace="${namespace}"}`,
+				"{{pod}} - {{evmChainID}} - {{state}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Verifies Total",
+				"",
+				`evm_pool_rpc_node_verifies{namespace="${namespace}"}`,
+				"{{pod}} - {{evmChainID}}",
+			),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Verifies Success",
+				"",
+				`evm_pool_rpc_node_verifies_success{namespace="${namespace}"}`,
+				"{{pod}} - {{evmChainID}}",
+			),
+		),
+		dashboard.Row(
+			"EVM Pool RPC Node Latencies (App)",
+			row.Collapse(),
+			m.timeseriesRowOption(
+				"EVM Pool RPC Node Calls Latency 0.95 quantile",
+				"ms",
+				`histogram_quantile(0.95, sum(rate(evm_pool_rpc_node_rpc_call_time_bucket{namespace="${namespace}"}[$__rate_interval])) by (le, rpcCallName)) / 1e6`,
+				"{{pod}}",
+			),
+		),
+		dashboard.Row(
+			"Pipeline Tasks Metrics (App)",
+			row.Collapse(),
+			m.timeseriesRowOption(
+				"Pipeline Runs Queued",
+				"",
+				`pipeline_runs_queued{namespace="${namespace}"}`,
+				"{{pod}}",
+			),
+			m.timeseriesRowOption(
+				"Pipeline Runs Tasks Queued",
+				"",
+				`pipeline_task_runs_queued{namespace="${namespace}"}`,
+				"{{pod}}",
 			),
 		),
 	}
