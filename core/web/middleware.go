2e93b55e17c8e02befe26e36e32e76f4bd54cf7d
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2023-11-07 20:52:43 -0500

    Use build tags to pull in embed operator_ui or not

--- core/web/middleware.go
+++ core/web/middleware.go
@@ -16,17 +16,6 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
 
-// Go's new embed feature doesn't allow us to embed things outside of the current module.
-// To get around this, we need to make sure that the assets we want to embed are available
-// inside this module. To achieve this, we direct webpack to output all of the compiled assets
-// in this module's folder under the "assets" directory.
-
-//go:embed "assets"
-var uiEmbedFs embed.FS
-
-// assetFs is the singleton file system instance that is used to serve the static
-// assets for the operator UI.
-var assetFs = NewEmbedFileSystem(uiEmbedFs, "assets")
 
 const (
 	acceptEncodingHeader  = "Accept-Encoding"
---
+++ core/web/middleware_assets_dev.go
@@ -0,0 +1,19 @@
+// +build dev
+
+package web
+
+import (
+	"embed"
+)
+
+// Go's new embed feature doesn't allow us to embed things outside of the current module.
+// To get around this, we need to make sure that the assets we want to embed are available
+// inside this module. To achieve this, we direct webpack to output all of the compiled assets
+// in this module's folder under the "assets" directory.
+
+//go:embed "mock_assets"
+var uiEmbedFs embed.FS
+
+// assetFs is the singleton file system instance that is used to serve the static
+// assets for the operator UI.
+var assetFs = NewEmbedFileSystem(uiEmbedFs, "assets")
