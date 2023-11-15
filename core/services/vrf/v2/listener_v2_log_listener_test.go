b5b6d4c49ffdf2c86841eafed0560b4f24fae86f
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2023-11-15 15:52:22 +0000

    Test VRF-LogPoller functionality- getUnfulfilled

--- core/services/vrf/v2/listener_v2_log_listener_test.go
+++ core/services/vrf/v2/listener_v2_log_listener_test.go
@@ -167,7 +167,13 @@ func SetupTH(t *testing.T,
 	return th
 }
 
-/* Tests for initializeLastProcessedBlock: BEGIN */
+/* Tests for initializeLastProcessedBlock: BEGIN
+ * TestInitProcessedBlock_NoVRFReqs
+ * TestInitProcessedBlock_NoUnfulfilledVRFReqs
+ * TestInitProcessedBlock_OneUnfulfilledVRFReq
+ * TestInitProcessedBlock_SomeUnfulfilledVRFReqs
+ * TestInitProcessedBlock_UnfulfilledNFulfilledVRFReqs
+ */
 
 func TestInitProcessedBlock_NoVRFReqs(t *testing.T) {
 	t.Parallel()
@@ -425,7 +431,13 @@ func TestInitProcessedBlock_UnfulfilledNFulfilledVRFReqs(t *testing.T) {
 
 /* Tests for initializeLastProcessedBlock: END */
 
-/* Tests for updateLastProcessedBlock: BEGIN */
+/* Tests for updateLastProcessedBlock: BEGIN
+ * TestUpdateLastProcessedBlock_NoVRFReqs
+ * TestUpdateLastProcessedBlock_NoUnfulfilledVRFReqs
+ * TestUpdateLastProcessedBlock_OneUnfulfilledVRFReq
+ * TestUpdateLastProcessedBlock_SomeUnfulfilledVRFReqs
+ * TestUpdateLastProcessedBlock_UnfulfilledNFulfilledVRFReqs
+ */
 
 func TestUpdateLastProcessedBlock_NoVRFReqs(t *testing.T) {
 	t.Parallel()
@@ -676,6 +688,15 @@ func TestUpdateLastProcessedBlock_UnfulfilledNFulfilledVRFReqs(t *testing.T) {
 	// does not internally call LogPoller.Replay
 	require.NoError(t, th.LogPoller.Replay(th.Ctx, 4))
 
+	lh, _ := th.LogPoller.LatestLogByEventSigWithConfs(VRFEmitterABI.Events["RandomWordsFulfilled"].ID,
+		th.Listener.coordinator.Address(),
+		logpoller.Finalized,
+	)
+	for i, topic := range lh.Topics {
+		fmt.Printf("LogTopic[%d]: %v\n", i, common.Bytes2Hex(topic))
+	}
+	fmt.Printf("LogData: %v\n", common.Bytes2Hex(lh.Data))
+
 	// updateLastProcessedBlock must return the VRF req at block (7) instead of
 	// finalizedBlockNumber (16) after currLastProcessedBlock (4) passed below,
 	// as block 7 contains the earliest unfulfilled VRF request. VRF request
@@ -686,3 +707,281 @@ func TestUpdateLastProcessedBlock_UnfulfilledNFulfilledVRFReqs(t *testing.T) {
 }
 
 /* Tests for updateLastProcessedBlock: END */
+
+/* Tests for getUnfulfilled: BEGIN
+ * TestGetUnfulfilled_NoVRFReqs
+ * TestGetUnfulfilled_NoUnfulfilledVRFReqs
+ * TestGetUnfulfilled_OneUnfulfilledVRFReq
+ * TestGetUnfulfilled_SomeUnfulfilledVRFReqs
+ * TestGetUnfulfilled_UnfulfilledNFulfilledVRFReqs
+ */
+
+func SetupGetUnfulfilledTH(t *testing.T) (*listenerV2, *utils.Big) {
+	chainID := utils.NewBig(big.NewInt(12345))
+	lggr := logger.TestLogger(t)
+	j, err := vrfcommon.ValidatedVRFSpec(testspecs.GenerateVRFSpec(testspecs.VRFSpecParams{
+		RequestedConfsDelay: 10,
+		EVMChainID:          chainID.String(),
+	}).Toml())
+	require.NoError(t, err)
+	chain := evmmocks.NewChain(t)
+
+	// Construct CoordinatorV2_X object for VRF listener
+	owner := testutils.MustNewSimTransactor(t)
+	ethDB := rawdb.NewMemoryDatabase()
+	ec := backends.NewSimulatedBackendWithDatabase(ethDB, map[common.Address]core.GenesisAccount{
+		owner.From: {
+			Balance: big.NewInt(0).Mul(big.NewInt(10), big.NewInt(1e18)),
+		},
+	}, 10e6)
+	_, _, vrfLogEmitter, err := vrf_log_emitter.DeployVRFLogEmitter(owner, ec)
+	require.NoError(t, err)
+	ec.Commit()
+	coordinatorV2, err := vrf_coordinator_v2.NewVRFCoordinatorV2(vrfLogEmitter.Address(), ec)
+	require.Nil(t, err)
+	coordinator := NewCoordinatorV2(coordinatorV2)
+
+	listener := &listenerV2{
+		respCount:   map[string]uint64{},
+		job:         j,
+		chain:       chain,
+		l:           logger.Sugared(lggr),
+		coordinator: coordinator,
+	}
+	return listener, chainID
+}
+
+func TestGetUnfulfilled_NoVRFReqs(t *testing.T) {
+	t.Parallel()
+
+	listener, chainID := SetupGetUnfulfilledTH(t)
+
+	logs := []logpoller.Log{}
+	for i := 0; i < 10; i++ {
+		logs = append(logs, logpoller.Log{
+			EvmChainId:     chainID,
+			LogIndex:       0,
+			BlockHash:      common.BigToHash(big.NewInt(int64(i))),
+			BlockNumber:    int64(i),
+			BlockTimestamp: time.Now(),
+			Topics: [][]byte{
+				[]byte("0x7299721c72d5436afdfb18da500ab7e08ac04c812d7204e1bddd322f67d8be4c"),
+			},
+			EventSig:  EmitterABI.Events["Log1"].ID,
+			Address:   common.Address{},
+			TxHash:    common.BigToHash(big.NewInt(int64(i))),
+			Data:      nil,
+			CreatedAt: time.Now(),
+		})
+	}
+
+	unfulfilled, _, fulfilled := listener.getUnfulfilled(logs, listener.l)
+	require.Empty(t, unfulfilled)
+	require.Empty(t, fulfilled)
+}
+
+func TestGetUnfulfilled_NoUnfulfilledVRFReqs(t *testing.T) {
+	t.Parallel()
+
+	listener, chainID := SetupGetUnfulfilledTH(t)
+
+	logs := []logpoller.Log{}
+	for i := 0; i < 10; i++ {
+		eventSig := EmitterABI.Events["Log1"].ID
+		topics := [][]byte{
+			common.FromHex("0x46692c0e59ca9cd1ad8f984a9d11715ec83424398b7eed4e05c8ce84662415a8"),
+		}
+		if i%2 == 0 {
+			eventSig = VRFEmitterABI.Events["RandomWordsRequested"].ID
+			topics = [][]byte{
+				common.FromHex("0x63373d1c4696214b898952999c9aaec57dac1ee2723cec59bea6888f489a9772"),
+				common.FromHex("0xc0a6c424ac7157ae408398df7e5f4552091a69125d5dfcb7b8c2659029395bdf"),
+				common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000001"),
+				common.FromHex("0x0000000000000000000000005ee3b50502b5c4c9184dcb281471a0614d4b2ef9"),
+			}
+		}
+		logs = append(logs, logpoller.Log{
+			EvmChainId:     chainID,
+			LogIndex:       0,
+			BlockHash:      common.BigToHash(big.NewInt(int64(2 * i))),
+			BlockNumber:    int64(2 * i),
+			BlockTimestamp: time.Now(),
+			Topics:         topics,
+			EventSig:       eventSig,
+			Address:        common.Address{},
+			TxHash:         common.BigToHash(big.NewInt(int64(2 * i))),
+			Data:           common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i) + "000000000000000000000000000000000000000000000000000000000000006a000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000027100000000000000000000000000000000000000000000000000000000000000002"),
+			CreatedAt:      time.Now(),
+		})
+		if i%2 == 0 {
+			logs = append(logs, logpoller.Log{
+				EvmChainId:     chainID,
+				LogIndex:       0,
+				BlockHash:      common.BigToHash(big.NewInt(int64(2*i + 1))),
+				BlockNumber:    int64(2*i + 1),
+				BlockTimestamp: time.Now(),
+				Topics: [][]byte{
+					common.FromHex("0x7dffc5ae5ee4e2e4df1651cf6ad329a73cebdb728f37ea0187b9b17e036756e4"),
+					common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i)),
+				},
+				EventSig:  VRFEmitterABI.Events["RandomWordsFulfilled"].ID,
+				Address:   common.Address{},
+				TxHash:    common.BigToHash(big.NewInt(int64(2*i + 1))),
+				Data:      common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000069000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000001"),
+				CreatedAt: time.Now(),
+			})
+		}
+	}
+
+	unfulfilled, _, fulfilled := listener.getUnfulfilled(logs, listener.l)
+	require.Empty(t, unfulfilled)
+	require.Len(t, fulfilled, 5)
+}
+
+func TestGetUnfulfilled_OneUnfulfilledVRFReq(t *testing.T) {
+	t.Parallel()
+
+	listener, chainID := SetupGetUnfulfilledTH(t)
+
+	logs := []logpoller.Log{}
+	for i := 0; i < 10; i++ {
+		eventSig := EmitterABI.Events["Log1"].ID
+		topics := [][]byte{
+			common.FromHex("0x46692c0e59ca9cd1ad8f984a9d11715ec83424398b7eed4e05c8ce84662415a8"),
+		}
+		if i == 4 {
+			eventSig = VRFEmitterABI.Events["RandomWordsRequested"].ID
+			topics = [][]byte{
+				common.FromHex("0x63373d1c4696214b898952999c9aaec57dac1ee2723cec59bea6888f489a9772"),
+				common.FromHex("0xc0a6c424ac7157ae408398df7e5f4552091a69125d5dfcb7b8c2659029395bdf"),
+				common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000001"),
+				common.FromHex("0x0000000000000000000000005ee3b50502b5c4c9184dcb281471a0614d4b2ef9"),
+			}
+		}
+		logs = append(logs, logpoller.Log{
+			EvmChainId:     chainID,
+			LogIndex:       0,
+			BlockHash:      common.BigToHash(big.NewInt(int64(2 * i))),
+			BlockNumber:    int64(2 * i),
+			BlockTimestamp: time.Now(),
+			Topics:         topics,
+			EventSig:       eventSig,
+			Address:        common.Address{},
+			TxHash:         common.BigToHash(big.NewInt(int64(2 * i))),
+			Data:           common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i) + "000000000000000000000000000000000000000000000000000000000000006a000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000027100000000000000000000000000000000000000000000000000000000000000002"),
+			CreatedAt:      time.Now(),
+		})
+	}
+
+	unfulfilled, _, fulfilled := listener.getUnfulfilled(logs, listener.l)
+	require.Equal(t, unfulfilled[0].RequestID().Int64(), big.NewInt(4).Int64())
+	require.Len(t, unfulfilled, 1)
+	require.Empty(t, fulfilled)
+}
+
+func TestGetUnfulfilled_SomeUnfulfilledVRFReq(t *testing.T) {
+	t.Parallel()
+
+	listener, chainID := SetupGetUnfulfilledTH(t)
+
+	logs := []logpoller.Log{}
+	for i := 0; i < 10; i++ {
+		eventSig := EmitterABI.Events["Log1"].ID
+		topics := [][]byte{
+			common.FromHex("0x46692c0e59ca9cd1ad8f984a9d11715ec83424398b7eed4e05c8ce84662415a8"),
+		}
+		if i%2 == 0 {
+			eventSig = VRFEmitterABI.Events["RandomWordsRequested"].ID
+			topics = [][]byte{
+				common.FromHex("0x63373d1c4696214b898952999c9aaec57dac1ee2723cec59bea6888f489a9772"),
+				common.FromHex("0xc0a6c424ac7157ae408398df7e5f4552091a69125d5dfcb7b8c2659029395bdf"),
+				common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000001"),
+				common.FromHex("0x0000000000000000000000005ee3b50502b5c4c9184dcb281471a0614d4b2ef9"),
+			}
+		}
+		logs = append(logs, logpoller.Log{
+			EvmChainId:     chainID,
+			LogIndex:       0,
+			BlockHash:      common.BigToHash(big.NewInt(int64(2 * i))),
+			BlockNumber:    int64(2 * i),
+			BlockTimestamp: time.Now(),
+			Topics:         topics,
+			EventSig:       eventSig,
+			Address:        common.Address{},
+			TxHash:         common.BigToHash(big.NewInt(int64(2 * i))),
+			Data:           common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i) + "000000000000000000000000000000000000000000000000000000000000006a000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000027100000000000000000000000000000000000000000000000000000000000000002"),
+			CreatedAt:      time.Now(),
+		})
+	}
+
+	unfulfilled, _, fulfilled := listener.getUnfulfilled(logs, listener.l)
+	require.Len(t, unfulfilled, 5)
+	require.Len(t, fulfilled, 0)
+	require.Equal(t, unfulfilled[0].RequestID().Int64(), big.NewInt(0).Int64())
+	require.Equal(t, unfulfilled[1].RequestID().Int64(), big.NewInt(2).Int64())
+	require.Equal(t, unfulfilled[2].RequestID().Int64(), big.NewInt(4).Int64())
+	require.Equal(t, unfulfilled[3].RequestID().Int64(), big.NewInt(6).Int64())
+	require.Equal(t, unfulfilled[4].RequestID().Int64(), big.NewInt(8).Int64())
+}
+
+func TestGetUnfulfilled_UnfulfilledNFulfilledVRFReqs(t *testing.T) {
+	t.Parallel()
+
+	listener, chainID := SetupGetUnfulfilledTH(t)
+
+	logs := []logpoller.Log{}
+	for i := 0; i < 10; i++ {
+		eventSig := EmitterABI.Events["Log1"].ID
+		topics := [][]byte{
+			common.FromHex("0x46692c0e59ca9cd1ad8f984a9d11715ec83424398b7eed4e05c8ce84662415a8"),
+		}
+		if i%2 == 0 {
+			eventSig = VRFEmitterABI.Events["RandomWordsRequested"].ID
+			topics = [][]byte{
+				common.FromHex("0x63373d1c4696214b898952999c9aaec57dac1ee2723cec59bea6888f489a9772"),
+				common.FromHex("0xc0a6c424ac7157ae408398df7e5f4552091a69125d5dfcb7b8c2659029395bdf"),
+				common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000001"),
+				common.FromHex("0x0000000000000000000000005ee3b50502b5c4c9184dcb281471a0614d4b2ef9"),
+			}
+		}
+		logs = append(logs, logpoller.Log{
+			EvmChainId:     chainID,
+			LogIndex:       0,
+			BlockHash:      common.BigToHash(big.NewInt(int64(2 * i))),
+			BlockNumber:    int64(2 * i),
+			BlockTimestamp: time.Now(),
+			Topics:         topics,
+			EventSig:       eventSig,
+			Address:        common.Address{},
+			TxHash:         common.BigToHash(big.NewInt(int64(2 * i))),
+			Data:           common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i) + "000000000000000000000000000000000000000000000000000000000000006a000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000027100000000000000000000000000000000000000000000000000000000000000002"),
+			CreatedAt:      time.Now(),
+		})
+		if i%2 == 0 && i < 6 {
+			logs = append(logs, logpoller.Log{
+				EvmChainId:     chainID,
+				LogIndex:       0,
+				BlockHash:      common.BigToHash(big.NewInt(int64(2*i + 1))),
+				BlockNumber:    int64(2*i + 1),
+				BlockTimestamp: time.Now(),
+				Topics: [][]byte{
+					common.FromHex("0x7dffc5ae5ee4e2e4df1651cf6ad329a73cebdb728f37ea0187b9b17e036756e4"),
+					common.FromHex("0x000000000000000000000000000000000000000000000000000000000000000" + fmt.Sprintf("%d", i)),
+				},
+				EventSig:  VRFEmitterABI.Events["RandomWordsFulfilled"].ID,
+				Address:   common.Address{},
+				TxHash:    common.BigToHash(big.NewInt(int64(2*i + 1))),
+				Data:      common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000069000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000001"),
+				CreatedAt: time.Now(),
+			})
+		}
+	}
+
+	unfulfilled, _, fulfilled := listener.getUnfulfilled(logs, listener.l)
+	require.Len(t, unfulfilled, 2)
+	require.Len(t, fulfilled, 3)
+	require.Equal(t, unfulfilled[0].RequestID().Int64(), big.NewInt(6).Int64())
+	require.Equal(t, unfulfilled[1].RequestID().Int64(), big.NewInt(8).Int64())
+}
+
+/* Tests for getUnfulfilled: END */
