1d67b7fc38245a89ee70c1cf7fe22e376e77a266
Jordan Krage <jmank88@gmail.com>
2023-11-20 05:35:30 -0600

    core/utils: make StopChan an alias to common

--- core/utils/utils.go
+++ core/utils/utils.go
@@ -417,50 +417,11 @@ func ContextFromChanWithTimeout(chStop chan struct{}, timeout time.Duration) (co
 }
 
 // A StopChan signals when some work should stop.
-type StopChan chan struct{}
-
-// NewCtx returns a background [context.Context] that is cancelled when StopChan is closed.
-func (s StopChan) NewCtx() (context.Context, context.CancelFunc) {
-	return StopRChan((<-chan struct{})(s)).NewCtx()
-}
-
-// Ctx cancels a [context.Context] when StopChan is closed.
-func (s StopChan) Ctx(ctx context.Context) (context.Context, context.CancelFunc) {
-	return StopRChan((<-chan struct{})(s)).Ctx(ctx)
-}
-
-// CtxCancel cancels a [context.Context] when StopChan is closed.
-// Returns ctx and cancel unmodified, for convenience.
-func (s StopChan) CtxCancel(ctx context.Context, cancel context.CancelFunc) (context.Context, context.CancelFunc) {
-	return StopRChan((<-chan struct{})(s)).CtxCancel(ctx, cancel)
-}
+type StopChan = services.StopChan
 
 // A StopRChan signals when some work should stop.
 // This version is receive-only.
-type StopRChan <-chan struct{}
-
-// NewCtx returns a background [context.Context] that is cancelled when StopChan is closed.
-func (s StopRChan) NewCtx() (context.Context, context.CancelFunc) {
-	return s.Ctx(context.Background())
-}
-
-// Ctx cancels a [context.Context] when StopChan is closed.
-func (s StopRChan) Ctx(ctx context.Context) (context.Context, context.CancelFunc) {
-	return s.CtxCancel(context.WithCancel(ctx))
-}
-
-// CtxCancel cancels a [context.Context] when StopChan is closed.
-// Returns ctx and cancel unmodified, for convenience.
-func (s StopRChan) CtxCancel(ctx context.Context, cancel context.CancelFunc) (context.Context, context.CancelFunc) {
-	go func() {
-		select {
-		case <-s:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
-	return ctx, cancel
-}
+type StopRChan = services.StopRChan
 
 // DependentAwaiter contains Dependent funcs
 type DependentAwaiter interface {
