68b702e9741b0f5b8cc2896eb1cc6d580756d680
davidcauchi <david.cauchi@smartcontract.com>
2023-11-20 11:00:58 +0100

    Enable Kava

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -181,6 +181,8 @@ func NewContractDeployer(bcClient blockchain.EVMClient, logger zerolog.Logger) (
 		return &KromaContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	case *blockchain.WeMixClient:
 		return &WeMixContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
+	case *blockchain.KavaClient:
+		return &KavaContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	}
 	return nil, errors.New("unknown blockchain client implementation for contract deployer, register blockchain client in NewContractDeployer")
 }
@@ -253,6 +255,10 @@ type KromaContractDeployer struct {
 }
 
 type WeMixContractDeployer struct {
+	*EthereumContractDeployer
+}
+
+type KavaContractDeployer struct {
 	*EthereumContractDeployer
 }
 
