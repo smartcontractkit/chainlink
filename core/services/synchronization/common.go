3541a99e2248d102b7a70ea01c7aaffa2eff78d2
Vlad Sirenko <821251+sirenko@users.noreply.github.com>
2023-11-21 09:15:42 -0800

    Add CCIP telemetry type

--- core/services/synchronization/common.go
+++ core/services/synchronization/common.go
@@ -15,6 +15,7 @@ const (
 	EnhancedEAMercury TelemetryType = "enhanced-ea-mercury"
 	OCR               TelemetryType = "ocr"
 	OCR2Automation    TelemetryType = "ocr2-automation"
+	OCR2CCIP          TelemetryType = "ocr2-ccip"
 	OCR2Functions     TelemetryType = "ocr2-functions"
 	OCR2Threshold     TelemetryType = "ocr2-threshold"
 	OCR2S4            TelemetryType = "ocr2-s4"
