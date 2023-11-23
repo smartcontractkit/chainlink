7cbdbf38f18d83103b6a524067504b21c54ba319
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2023-11-23 03:12:14 +0200

    fixing lint issues

--- integration-tests/contracts/contract_loader.go
+++ integration-tests/contracts/contract_loader.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rs/zerolog"
+
 	"github.com/smartcontractkit/chainlink-testing-framework/blockchain"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/functions/generated/functions_coordinator"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/functions/generated/functions_load_test_client"
