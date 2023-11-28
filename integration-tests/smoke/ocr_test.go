e100a56ec92c313c7553e698ddaecf6a70c68dd2
Adam Hamrick <adam.hamrick@smartcontract.com>
2023-11-28 17:38:51 -0500

    Use new compiled tests

--- integration-tests/smoke/ocr_test.go
+++ integration-tests/smoke/ocr_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink-testing-framework/logging"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils/testcontext"
+
 	"github.com/smartcontractkit/chainlink/integration-tests/actions"
 	"github.com/smartcontractkit/chainlink/integration-tests/docker/test_env"
 )
