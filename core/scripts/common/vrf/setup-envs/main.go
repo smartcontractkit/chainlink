a6e0ecf4c6908d0bf8d2385372fae50064e19293
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2023-12-01 17:07:36 +0000

    Update main.go

--- core/scripts/common/vrf/setup-envs/main.go
+++ core/scripts/common/vrf/setup-envs/main.go
@@ -509,7 +509,7 @@ func createETHKeysIfNeeded(client *clcmd.Shell, app *cli.App, output *bytes.Buff
 			if *maxGasPriceGwei > 0 {
 				helpers.PanicErr(flagSet.Set("max-gas-price-gwei", fmt.Sprintf("%d", *maxGasPriceGwei)))
 			}
-			err := flagSet.Parse([]string{"-evm-chain-id", os.Getenv("ETH_CHAIN_ID")})
+			err := flagSet.Parse([]string{"evm-chain-id", os.Getenv("ETH_CHAIN_ID")})
 			helpers.PanicErr(err)
 			err = client.CreateETHKey(cli.NewContext(app, flagSet, nil))
 			helpers.PanicErr(err)
