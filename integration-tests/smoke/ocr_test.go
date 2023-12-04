58a307eb87895c567a586ec8ff7e37434ad42075
Bartek Tofel <bartek.tofel@smartcontract.com>
2023-12-04 18:52:33 -0300

    run also ocr1 and 2 vrf tests iwth multiple clients

--- integration-tests/smoke/ocr_test.go
+++ integration-tests/smoke/ocr_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	ctf_test_env "github.com/smartcontractkit/chainlink-testing-framework/docker/test_env"
 	"github.com/smartcontractkit/chainlink-testing-framework/logging"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils/testcontext"
 	"github.com/smartcontractkit/chainlink/integration-tests/actions"
@@ -14,49 +15,108 @@ import (
 
 func TestOCRBasic(t *testing.T) {
 	t.Parallel()
-	l := logging.GetTestLogger(t)
+	chainConfig := ctf_test_env.EthereumChainConfig{
+		SecondsPerSlot: 8,
+		SlotsPerEpoch:  4,
+	}
 
-	env, err := test_env.NewCLTestEnvBuilder().
-		WithTestInstance(t).
-		WithGeth().
-		WithMockAdapter().
-		WithCLNodes(6).
-		WithFunding(big.NewFloat(.01)).
-		WithStandardCleanup().
-		Build()
-	require.NoError(t, err)
+	networks := map[string]ctf_test_env.EthereumNetwork{
+		"geth": func() ctf_test_env.EthereumNetwork {
+			ethBuilder := ctf_test_env.NewEthereumNetworkBuilder()
+			cfg, err := ethBuilder.
+				WithConsensusType(ctf_test_env.ConsensusType_PoS).
+				WithConsensusLayer(ctf_test_env.ConsensusLayer_Prysm).
+				WithExecutionLayer(ctf_test_env.ExecutionLayer_Geth).
+				WithEthereumChainConfig(chainConfig).
+				Build()
+			require.NoError(t, err, "Error building ethereum network config")
+			return cfg
+		}(),
+		"besu": func() ctf_test_env.EthereumNetwork {
+			ethBuilder := ctf_test_env.NewEthereumNetworkBuilder()
+			cfg, err := ethBuilder.
+				WithConsensusType(ctf_test_env.ConsensusType_PoS).
+				WithConsensusLayer(ctf_test_env.ConsensusLayer_Prysm).
+				WithExecutionLayer(ctf_test_env.ExecutionLayer_Besu).
+				WithEthereumChainConfig(chainConfig).
+				Build()
+			require.NoError(t, err, "Error building ethereum network config")
+			return cfg
+		}(),
+		"erigon": func() ctf_test_env.EthereumNetwork {
+			ethBuilder := ctf_test_env.NewEthereumNetworkBuilder()
+			cfg, err := ethBuilder.
+				WithConsensusType(ctf_test_env.ConsensusType_PoS).
+				WithConsensusLayer(ctf_test_env.ConsensusLayer_Prysm).
+				WithExecutionLayer(ctf_test_env.ExecutionLayer_Erigon).
+				WithEthereumChainConfig(chainConfig).
+				Build()
+			require.NoError(t, err, "Error building ethereum network config")
+			return cfg
+		}(),
+		"nethermind": func() ctf_test_env.EthereumNetwork {
+			ethBuilder := ctf_test_env.NewEthereumNetworkBuilder()
+			cfg, err := ethBuilder.
+				WithConsensusType(ctf_test_env.ConsensusType_PoS).
+				WithConsensusLayer(ctf_test_env.ConsensusLayer_Prysm).
+				WithExecutionLayer(ctf_test_env.ExecutionLayer_Nethermind).
+				WithEthereumChainConfig(chainConfig).
+				Build()
+			require.NoError(t, err, "Error building ethereum network config")
+			return cfg
+		}(),
+	}
 
-	env.ParallelTransactions(true)
+	for k, v := range networks {
+		name := k
+		network := v
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			l := logging.GetTestLogger(t)
 
-	nodeClients := env.ClCluster.NodeAPIs()
-	bootstrapNode, workerNodes := nodeClients[0], nodeClients[1:]
+			env, err := test_env.NewCLTestEnvBuilder().
+				WithTestInstance(t).
+				WithPrivateEthereumNetwork(network).
+				WithMockAdapter().
+				WithCLNodes(6).
+				WithFunding(big.NewFloat(.01)).
+				WithStandardCleanup().
+				Build()
+			require.NoError(t, err)
 
-	linkTokenContract, err := env.ContractDeployer.DeployLinkTokenContract()
-	require.NoError(t, err, "Deploying Link Token Contract shouldn't fail")
+			env.ParallelTransactions(true)
 
-	ocrInstances, err := actions.DeployOCRContractsLocal(1, linkTokenContract, env.ContractDeployer, workerNodes, env.EVMClient)
-	require.NoError(t, err)
-	err = env.EVMClient.WaitForEvents()
-	require.NoError(t, err, "Error waiting for events")
+			nodeClients := env.ClCluster.NodeAPIs()
+			bootstrapNode, workerNodes := nodeClients[0], nodeClients[1:]
 
-	err = actions.CreateOCRJobsLocal(ocrInstances, bootstrapNode, workerNodes, 5, env.MockAdapter, env.EVMClient.GetChainID())
-	require.NoError(t, err)
+			linkTokenContract, err := env.ContractDeployer.DeployLinkTokenContract()
+			require.NoError(t, err, "Deploying Link Token Contract shouldn't fail")
 
-	err = actions.StartNewRound(1, ocrInstances, env.EVMClient, l)
-	require.NoError(t, err)
+			ocrInstances, err := actions.DeployOCRContractsLocal(1, linkTokenContract, env.ContractDeployer, workerNodes, env.EVMClient)
+			require.NoError(t, err)
+			err = env.EVMClient.WaitForEvents()
+			require.NoError(t, err, "Error waiting for events")
 
-	answer, err := ocrInstances[0].GetLatestAnswer(testcontext.Get(t))
-	require.NoError(t, err, "Getting latest answer from OCR contract shouldn't fail")
-	require.Equal(t, int64(5), answer.Int64(), "Expected latest answer from OCR contract to be 5 but got %d", answer.Int64())
+			err = actions.CreateOCRJobsLocal(ocrInstances, bootstrapNode, workerNodes, 5, env.MockAdapter, env.EVMClient.GetChainID())
+			require.NoError(t, err)
 
-	err = actions.SetAllAdapterResponsesToTheSameValueLocal(10, ocrInstances, workerNodes, env.MockAdapter)
-	require.NoError(t, err)
-	err = actions.StartNewRound(2, ocrInstances, env.EVMClient, l)
-	require.NoError(t, err)
+			err = actions.StartNewRound(1, ocrInstances, env.EVMClient, l)
+			require.NoError(t, err)
 
-	answer, err = ocrInstances[0].GetLatestAnswer(testcontext.Get(t))
-	require.NoError(t, err, "Error getting latest OCR answer")
-	require.Equal(t, int64(10), answer.Int64(), "Expected latest answer from OCR contract to be 10 but got %d", answer.Int64())
+			answer, err := ocrInstances[0].GetLatestAnswer(testcontext.Get(t))
+			require.NoError(t, err, "Getting latest answer from OCR contract shouldn't fail")
+			require.Equal(t, int64(5), answer.Int64(), "Expected latest answer from OCR contract to be 5 but got %d", answer.Int64())
+
+			err = actions.SetAllAdapterResponsesToTheSameValueLocal(10, ocrInstances, workerNodes, env.MockAdapter)
+			require.NoError(t, err)
+			err = actions.StartNewRound(2, ocrInstances, env.EVMClient, l)
+			require.NoError(t, err)
+
+			answer, err = ocrInstances[0].GetLatestAnswer(testcontext.Get(t))
+			require.NoError(t, err, "Error getting latest OCR answer")
+			require.Equal(t, int64(10), answer.Int64(), "Expected latest answer from OCR contract to be 10 but got %d", answer.Int64())
+		})
+	}
 }
 
 func TestOCRJobReplacement(t *testing.T) {
