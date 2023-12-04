67861738903ddfd7982c8bb9067ee1088874f713
Chris De Leon <christopher.deleon@smartcontract.com>
2023-12-04 20:31:12 +0000

    use named parameters in mappings for scroll contracts

--- contracts/src/v0.8/l2ep/dev/scroll/ScrollSequencerUptimeFeed.sol
+++ contracts/src/v0.8/l2ep/dev/scroll/ScrollSequencerUptimeFeed.sol
@@ -57,7 +57,7 @@ contract ScrollSequencerUptimeFeed is
   address private s_l1Sender;
   /// @dev s_latestRoundId == 0 means this contract is uninitialized.
   FeedState private s_feedState = FeedState({latestRoundId: 0, latestStatus: false, startedAt: 0, updatedAt: 0});
-  mapping(uint80 => Round) private s_rounds;
+  mapping(uint80 roundId => Round round) private s_rounds;
 
   // solhint-disable-next-line chainlink-solidity/prefix-immutable-variables-with-i
   IL2ScrollMessenger private immutable s_l2CrossDomainMessenger;
