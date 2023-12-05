8f65343deeac390ee2b6c501488fddc2a33f950e
Dimitris Grigoriou <dimitris.grigoriou@smartcontract.com>
2023-12-05 14:15:09 +0200

    Extract mathutil (#11446)
    * Extract mathutil
    
    * Fix dependencies

--- core/chains/evm/logpoller/log_poller.go
+++ core/chains/evm/logpoller/log_poller.go
@@ -22,12 +22,12 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
+	"github.com/smartcontractkit/chainlink-common/pkg/utils/mathutil"
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
 	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pg"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
-	"github.com/smartcontractkit/chainlink/v2/core/utils/mathutil"
 )
 
 //go:generate mockery --quiet --name LogPoller --output ./mocks/ --case=underscore --structname LogPoller --filename log_poller.go
