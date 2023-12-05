ba1755d5b9d3761753712dc677399e309004cb0c
Finley Decker <finleydecker@gmail.com>
2023-12-04 16:00:15 -1000

    removed chaintype

--- common/config/chaintype.go
+++ common/config/chaintype.go
@@ -18,17 +18,16 @@ const (
 	ChainWeMix           ChainType = "wemix"
 	ChainKroma           ChainType = "kroma"
 	ChainZkSync          ChainType = "zksync"
-	ChainKava			 ChainType = "kava"
 )
 
 var ErrInvalidChainType = fmt.Errorf("must be one of %s or omitted", strings.Join([]string{
 	string(ChainArbitrum), string(ChainMetis), string(ChainXDai), string(ChainOptimismBedrock), string(ChainCelo),
-	string(ChainKroma), string(ChainWeMix), string(ChainZkSync), string(ChainKava)}, ", "))
+	string(ChainKroma), string(ChainWeMix), string(ChainZkSync)}, ", "))
 
 // IsValid returns true if the ChainType value is known or empty.
 func (c ChainType) IsValid() bool {
 	switch c {
-	case "", ChainArbitrum, ChainMetis, ChainOptimismBedrock, ChainXDai, ChainCelo, ChainKroma, ChainWeMix, ChainZkSync, ChainKava:
+	case "", ChainArbitrum, ChainMetis, ChainOptimismBedrock, ChainXDai, ChainCelo, ChainKroma, ChainWeMix, ChainZkSync:
 		return true
 	}
 	return false
