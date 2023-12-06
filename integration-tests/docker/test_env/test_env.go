c1f9479873c446aeacea434094db6daaac3bdc8d
Jason McInerney <jason.mcinerney@smartcontract.com>
2023-12-05 16:02:52 -0800

    Update CTF functions

--- integration-tests/docker/test_env/test_env.go
+++ integration-tests/docker/test_env/test_env.go
@@ -151,15 +151,17 @@ func (te *CLClusterTestEnv) StartMockAdapter() error {
 	return te.MockAdapter.StartContainer()
 }
 
-func (te *CLClusterTestEnv) StartClCluster(nodeConfig *chainlink.Config, count int, secretsConfig string) error {
+func (te *CLClusterTestEnv) StartClCluster(nodeConfig *chainlink.Config, count int, secretsConfig string, opts ...ClNodeOption) error {
 	if te.Cfg != nil && te.Cfg.ClCluster != nil {
 		te.ClCluster = te.Cfg.ClCluster
 	} else {
+		opts = append(opts, WithSecrets(secretsConfig))
 		te.ClCluster = &ClCluster{}
 		for i := 0; i < count; i++ {
-			ocrNode := NewClNode([]string{te.Network.Name}, os.Getenv("CHAINLINK_IMAGE"), os.Getenv("CHAINLINK_VERSION"), nodeConfig,
-				WithSecrets(secretsConfig),
-			)
+			ocrNode, err := NewClNode([]string{te.Network.Name}, os.Getenv("CHAINLINK_IMAGE"), os.Getenv("CHAINLINK_VERSION"), nodeConfig, opts...)
+			if err != nil {
+				return err
+			}
 			te.ClCluster.Nodes = append(te.ClCluster.Nodes, ocrNode)
 		}
 	}
