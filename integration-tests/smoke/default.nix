83b9f594d2f0e9a89393fd492b252557c89a41c8
Damjan Smickovski <smickovskid@outlook.com>
2023-12-07 08:55:55 +0100

    Changed script name

--- integration-tests/smoke/default.nix
+++ integration-tests/smoke/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 let
-  scriptName = "ethereum-smoke-tests";
+  scriptName = "test-run-smoke";
   go = pkgs.go_1_21;
   scriptBuildInputs = with pkgs; [ go go-ethereum ]; # add geth to handle missing underlying dependencies
   testDir = toString ./.;
