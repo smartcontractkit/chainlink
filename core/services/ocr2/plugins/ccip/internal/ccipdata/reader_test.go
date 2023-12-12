a8f6b3696a3ed71e96ab93baa2da04845b31f8b4
dimitris <dimitrios.kouveris@smartcontract.com>
2023-12-12 18:55:04 +0400

    offchain - remove unused lp reader + minor fixes (#371)

--- core/services/ocr2/plugins/ccip/internal/ccipdata/reader_test.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/reader_test.go
@@ -10,34 +10,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
-	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
-
-func TestLogPollerClient_loadDependency(t *testing.T) {
-	c := &LogPollerReader{}
-
-	someAddr := utils.RandomAddress()
-
-	onRamp, err := c.loadOnRamp(someAddr)
-	assert.NoError(t, err)
-	onRamp2, err := c.loadOnRamp(someAddr)
-	assert.NoError(t, err)
-	// the objects should have the same pointer
-	// since the second time the dependency should've been loaded from cache instead of initializing a new instance.
-	assert.True(t, onRamp == onRamp2)
-
-	offRamp, err := c.loadOffRamp(someAddr)
-	assert.NoError(t, err)
-	offRamp2, err := c.loadOffRamp(someAddr)
-	assert.NoError(t, err)
-	assert.True(t, offRamp == offRamp2)
-
-	priceReg, err := c.loadPriceRegistry(someAddr)
-	assert.NoError(t, err)
-	priceReg2, err := c.loadPriceRegistry(someAddr)
-	assert.NoError(t, err)
-	assert.True(t, priceReg == priceReg2)
-}
 
 func Test_parseLogs(t *testing.T) {
 	// generate 100 logs
