4e0b4143213f1a6c9d9866bf2de2a7813af54642
stackman27 <sishir.giri@smartcontract.com>
2023-12-13 10:16:13 -0800

    added fees

--- integration-tests/soak/ocr_test.go
+++ integration-tests/soak/ocr_test.go
@@ -14,35 +14,35 @@ import (
 func TestOCRSoak(t *testing.T) {
 	l := logging.GetTestLogger(t)
 	// Use this variable to pass in any custom EVM specific TOML values to your Chainlink nodes
-	customNetworkTOML := `
-	ChainID = '1029'
-ChainType = 'bttc'
-FinalityDepth = 500 
-# blocks are generated every 2-4s 
-LogPollInterval = '2s'
-
-[GasEstimator]
-PriceMin = '9000000 gwei' 
-PriceMax = '30000000 gwei' 
-
-[GasEstimator.BlockHistory]
-# how many blocks we want to keep in memory to calculate gas price
-# # Average block time of 2s
-BlockHistorySize = 24
-
-[Transactions]
-ResendAfterThreshold = '30s'
-
-[HeadTracker]
-# re-org for bttc is really high so we want to check for the block where reorg happens
-HistoryDepth = 500
-
-[NodePool]
-SyncThreshold = 10
-
-[OCR]
-ContractConfirmations = 1
-`
+	customNetworkTOML := `ChainID = '1029'
+	ChainType = 'bttc'
+	FinalityDepth = 500 
+	# blocks are generated every 2-4s 
+	LogPollInterval = '2s
+	
+	[GasEstimator] 
+	Mode = 'BlockHistory'
+	EIP1559DynamicFees = false
+	PriceMax = '115792089237316195423570985008687907853269984665.640564039457584007913129639935 tether'
+	PriceMin = '0.009 ether'
+	
+	[GasEstimator.BlockHistory]
+	# how many blocks we want to keep in memory to calculate gas price
+	# # Average block time of 2s
+	BlockHistorySize = 24
+	
+	[Transactions]
+	ResendAfterThreshold = '30s'
+	
+	[HeadTracker]
+	# re-org for bttc is really high so we want to check for the block where reorg happens
+	HistoryDepth = 500
+	
+	[NodePool]
+	SyncThreshold = 10
+	
+	[OCR]
+	ContractConfirmations = 1`
 	// Uncomment below for debugging TOML issues on the node
 	// network := networks.MustGetSelectedNetworksFromEnv()[0]
 	// fmt.Println("Using Chainlink TOML\n---------------------")
