5b351abd60ee66a454344afc5dd3428ac493d0d9
Lei <lei.shi@smartcontract.com>
2023-12-13 12:07:31 -0800

    Auto 8087 modular streams (#11489)
    * make streams lookup modular
    
    * polish
    
    * address comment to use pointer instead of array/map
    
    * rebase
    
    * get rid of slice
    
    * keep using array and index
    
    * remove duplicate logging
    
    * address err re-declarion lint issues

--- core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/mercury/streams/streams_test.go
+++ core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/mercury/streams/streams_test.go
@@ -127,6 +127,7 @@ func TestStreams_CheckCallback(t *testing.T) {
 	tests := []struct {
 		name       string
 		lookup     *mercury.StreamsLookup
+		input      []ocr2keepers.CheckResult
 		values     [][]byte
 		statusCode int
 
@@ -153,6 +154,9 @@ func TestStreams_CheckCallback(t *testing.T) {
 				},
 				UpkeepId: upkeepId,
 				Block:    bn,
+			},
+			input: []ocr2keepers.CheckResult{
+				{},
 			},
 			values:       values,
 			statusCode:   http.StatusOK,
@@ -186,6 +190,9 @@ func TestStreams_CheckCallback(t *testing.T) {
 				UpkeepId: upkeepId,
 				Block:    bn,
 			},
+			input: []ocr2keepers.CheckResult{
+				{},
+			},
 			values:       values,
 			statusCode:   http.StatusOK,
 			callbackResp: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -216,6 +223,9 @@ func TestStreams_CheckCallback(t *testing.T) {
 				},
 				UpkeepId: upkeepId,
 				Block:    bn,
+			},
+			input: []ocr2keepers.CheckResult{
+				{},
 			},
 			values:       values,
 			statusCode:   http.StatusOK,
@@ -256,10 +266,10 @@ func TestStreams_CheckCallback(t *testing.T) {
 				}).Once()
 			s.client = client
 
-			state, retryable, _, err := s.checkCallback(testutils.Context(t), tt.values, tt.lookup)
-			tt.wantErr(t, err, fmt.Sprintf("Error asserion failed: %v", tt.name))
-			assert.Equal(t, tt.state, state)
-			assert.Equal(t, tt.retryable, retryable)
+			err = s.CheckCallback(testutils.Context(t), tt.values, tt.lookup, tt.input, 0)
+			tt.wantErr(t, err, fmt.Sprintf("Error assertion failed: %v", tt.name))
+			assert.Equal(t, uint8(tt.state), tt.input[0].PipelineExecutionState)
+			assert.Equal(t, tt.retryable, tt.input[0].Retryable)
 		})
 	}
 }
@@ -435,7 +445,7 @@ func TestStreams_AllowedToUseMercury(t *testing.T) {
 				BlockNumber: big.NewInt(10),
 			}
 
-			state, reason, retryable, allowed, err := s.allowedToUseMercury(opts, upkeepId)
+			state, reason, retryable, allowed, err := s.AllowedToUseMercury(opts, upkeepId)
 			assert.Equal(t, tt.err, err)
 			assert.Equal(t, tt.allowed, allowed)
 			assert.Equal(t, tt.state, state)
