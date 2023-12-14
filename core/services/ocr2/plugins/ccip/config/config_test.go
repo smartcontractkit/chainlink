c5a5fea6dfa4a43d71cca8b71677e61ec696d30a
Will Winder <wwinder.unh@gmail.com>
2023-12-14 09:58:04 -0500

    ocr2: add CCIP spec validation (#372)

--- core/services/ocr2/plugins/ccip/config/config_test.go
+++ core/services/ocr2/plugins/ccip/config/config_test.go
@@ -2,8 +2,11 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink/v2/core/utils"
 	"github.com/stretchr/testify/require"
 )
 
@@ -11,7 +14,7 @@ func TestCommitConfig(t *testing.T) {
 	exampleConfig := CommitPluginJobSpecConfig{
 		SourceStartBlock:       222,
 		DestStartBlock:         333,
-		OffRamp:                "0x123",
+		OffRamp:                common.HexToAddress("0x123"),
 		TokenPricesUSDPipeline: `merge [type=merge left="{}" right="{\"0xC79b96044906550A5652BCf20a6EA02f139B9Ae5\":\"1000000000000000000\"}"];`,
 	}
 
@@ -37,4 +40,74 @@ func TestExecutionConfig(t *testing.T) {
 	require.NoError(t, json.Unmarshal(bts, &parsedConfig))
 
 	require.Equal(t, exampleConfig, parsedConfig)
+}
+
+func TestUSDCValidate(t *testing.T) {
+	testcases := []struct {
+		config USDCConfig
+		err    string
+	}{
+		{
+			config: USDCConfig{},
+			err:    "AttestationAPI is required",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI: "api",
+			},
+			err: "SourceTokenAddress is required",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI:     "api",
+				SourceTokenAddress: utils.ZeroAddress,
+			},
+			err: "SourceTokenAddress is required",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI:     "api",
+				SourceTokenAddress: utils.RandomAddress(),
+			},
+			err: "SourceMessageTransmitterAddress is required",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI:                  "api",
+				SourceTokenAddress:              utils.RandomAddress(),
+				SourceMessageTransmitterAddress: utils.ZeroAddress,
+			},
+			err: "SourceMessageTransmitterAddress is required",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI:                  "api",
+				SourceTokenAddress:              utils.RandomAddress(),
+				SourceMessageTransmitterAddress: utils.RandomAddress(),
+			},
+			err: "",
+		},
+		{
+			config: USDCConfig{
+				AttestationAPI:                  "api",
+				SourceTokenAddress:              utils.RandomAddress(),
+				SourceMessageTransmitterAddress: utils.RandomAddress(),
+				AttestationAPITimeoutSeconds:    -1,
+			},
+			err: "AttestationAPITimeoutSeconds must be non-negative",
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(fmt.Sprintf("error = %s", tc.err), func(t *testing.T) {
+			t.Parallel()
+			err := tc.config.ValidateUSDCConfig()
+			if tc.err != "" {
+				require.ErrorContains(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
