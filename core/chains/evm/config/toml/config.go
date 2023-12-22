8ffd8214dc917a77797b9ac8ccee00332983df21
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2023-12-22 11:49:18 -0500

    remove unused import

--- core/chains/evm/config/toml/config.go
+++ core/chains/evm/config/toml/config.go
@@ -23,7 +23,6 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils/big"
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/ethkey"
-	configutils "github.com/smartcontractkit/chainlink/v2/core/utils/config"
 )
 
 type HasEVMConfigs interface {
