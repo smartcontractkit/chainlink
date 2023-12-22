a9907140b7800a7f82b4c1b46b506d246216d8fa
Bartek Tofel <bartek.tofel@smartcontract.com>
2023-12-22 10:16:06 -0300

    fail config test on purpose

--- integration-tests/smoke/config_test.go
+++ integration-tests/smoke/config_test.go
@@ -1,22 +1,19 @@
 package smoke
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/smartcontractkit/chainlink-testing-framework/logging"
-
 	tc "github.com/smartcontractkit/chainlink/integration-tests/testconfig"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConfig(t *testing.T) {
 	t.Parallel()
-	l := logging.GetTestLogger(t)
 
-	config, err := tc.GetConfig(t.Name(), tc.Smoke, tc.OCR)
+	_, err := tc.GetConfig(t.Name(), tc.Smoke, tc.OCR)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	l.Info().Msg(fmt.Sprintf("%+v", config))
+	require.True(t, false, "err on purpose")
 }
