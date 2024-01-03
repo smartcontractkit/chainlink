96fd7d35dfa50ffb7d7693b536d16684a6c5e713
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-01-03 12:18:25 -0300

    fix loki compile issue

--- integration-tests/testconfig/loki.go
+++ integration-tests/testconfig/loki.go
@@ -7,9 +7,9 @@ import (
 // TODO until we add TOML config support to WASP
 func LokiConfigFromToml(config *TestConfig) *wasp.LokiConfig {
 	lokiConfig := wasp.NewEnvLokiConfig()
-	lokiConfig.BasicAuth = *config.Logging.Loki.LokiBasicAuth
-	lokiConfig.TenantID = *config.Logging.Loki.LokiTenantId
-	lokiConfig.URL = *config.Logging.Loki.LokiUrl
+	lokiConfig.BasicAuth = *config.Logging.Loki.BasicAuth
+	lokiConfig.TenantID = *config.Logging.Loki.TenantId
+	lokiConfig.URL = *config.Logging.Loki.Url
 
 	return lokiConfig
 }
