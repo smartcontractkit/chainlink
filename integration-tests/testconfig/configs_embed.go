6e850fa8a2dd7d769d09e71495c9fed492045a12
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-01-10 20:58:13 -0300

    fix log poller path, adjust client compatibility test to not copy tomls

--- integration-tests/testconfig/configs_embed.go
+++ integration-tests/testconfig/configs_embed.go
@@ -9,7 +9,7 @@ import "embed"
 //go:embed automation/automation.toml
 //go:embed functions/functions.toml
 //go:embed keeper/keeper.toml
-//go:embed logpoller/log_poller.toml
+//go:embed log_poller/log_poller.toml
 //go:embed node/node.toml
 //go:embed ocr/ocr.toml
 //go:embed vrfv2/vrfv2.toml
