d2b385fea089987397b206ffd81654a786fb0bf0
lei shi <lei.shi@smartcontract.com>
2024-01-25 12:46:34 -0800

    upgrade to new version

--- contracts/src/v0.8/automation/AutomationForwarderLogic.sol
+++ contracts/src/v0.8/automation/AutomationForwarderLogic.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
 import {IAutomationRegistryConsumer} from "./interfaces/IAutomationRegistryConsumer.sol";
 import {ITypeAndVersion} from "../shared/interfaces/ITypeAndVersion.sol";
