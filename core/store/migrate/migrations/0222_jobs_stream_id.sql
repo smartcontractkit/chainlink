603d7015b09bd052b5895a0d0161e9811486815c
Sam Davies <samsondav@protonmail.com>
2024-01-26 10:59:22 -0500

    Reduce size of index

--- core/store/migrate/migrations/0222_jobs_stream_id.sql
+++ core/store/migrate/migrations/0222_jobs_stream_id.sql
@@ -1,5 +1,6 @@
 -- +goose Up
-ALTER TABLE jobs ADD COLUMN stream_id BIGINT UNIQUE;
+ALTER TABLE jobs ADD COLUMN stream_id BIGINT;
+CREATE UNIQUE INDEX idx_jobs_unique_stream_id ON jobs(stream_id) WHERE stream_id IS NOT NULL;
 
 -- +goose Down
 ALTER TABLE jobs DROP COLUMN stream_id;
