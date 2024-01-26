73b43f569cbad5d7ddf3675e5aeaaaeda1843983
Chunkai Yang <matYang@users.noreply.github.com>
2024-01-26 05:48:41 -0500

    bump version to 1.4.0 (#468)

--- contracts/src/v0.8/ccip/test/onRamp/EVM2EVMOnRamp.t.sol
+++ contracts/src/v0.8/ccip/test/onRamp/EVM2EVMOnRamp.t.sol
@@ -65,7 +65,7 @@ contract EVM2EVMOnRamp_constructor is EVM2EVMOnRampSetup {
     assertEq(s_sourceTokens, s_onRamp.getSupportedTokens(DEST_CHAIN_ID));
 
     // Initial values
-    assertEq("EVM2EVMOnRamp 1.3.0-dev", s_onRamp.typeAndVersion());
+    assertEq("EVM2EVMOnRamp 1.4.0-dev", s_onRamp.typeAndVersion());
     assertEq(OWNER, s_onRamp.owner());
     assertEq(1, s_onRamp.getExpectedNextSequenceNumber());
   }
