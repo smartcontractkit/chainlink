b953c0675914522196337454446102e3630f9e5f
skudasov <f4hrenh9it@gmail.com>
2024-02-04 19:44:23 +0100

    tested script + build/deploy separation

--- charts/chainlink-cluster/setup.sh
+++ charts/chainlink-cluster/setup.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-cat << EOF >> ~/.aws/config
+set -e
+user_home="$HOME"
+file_path="$user_home/.aws/config"
+image=""
+
+if grep -q "staging-crib" "$file_path"; then
+  echo "Staging AWS config is already applied, role is 'staging-crib'"
+else
+  cat <<EOF >> "$file_path"
 [profile staging-crib]
 region=us-west-2
 sso_start_url=https://smartcontract.awsapps.com/start
@@ -8,10 +16,22 @@ sso_region=us-west-2
 sso_account_id=323150190480
 sso_role_name=CRIB-ECR-Power
 EOF
+  echo "~/.aws/config modified, added 'staging-crib"
+fi
 
+# Login through SSO
 aws sso login --profile staging-crib
+# Update kubeconfig and switch context
 export AWS_PROFILE=staging-crib
 aws eks update-kubeconfig --name main-stage-cluster --alias main-stage-cluster-crib --profile staging-crib
+
+# Check if the Docker daemon is running
+if docker info > /dev/null 2>&1; then
+  echo "Docker daemon is running, authorizing registry"
+else
+  echo "Docker daemon is not running, exiting"
+  exit 1
+fi
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 323150190480.dkr.ecr.us-west-2.amazonaws.com
-export DEVSPACE_IMAGE="323150190480.dkr.ecr.us-west-2.amazonaws.com/chainlink-devspace"
 devspace use namespace $1
+export DEVSPACE_IMAGE="323150190480.dkr.ecr.us-west-2.amazonaws.com/chainlink-devspace"
