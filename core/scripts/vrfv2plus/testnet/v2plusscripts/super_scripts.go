04605be2405c027f876404b1506f29b21f940fbf
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2024-02-06 22:45:47 +0530

    Minor changes (#11943)

--- core/scripts/vrfv2plus/testnet/v2plusscripts/super_scripts.go
+++ core/scripts/vrfv2plus/testnet/v2plusscripts/super_scripts.go
@@ -715,11 +715,10 @@ func VRFV2PlusDeployUniverse(e helpers.Environment,
 
 	formattedVrfV2PlusPrimaryJobSpec := fmt.Sprintf(
 		jobs.VRFV2PlusJobFormatted,
-		contractAddresses.CoordinatorAddress,                                                        //coordinatorAddress
-		contractAddresses.BatchCoordinatorAddress,                                                   //batchCoordinatorAddress
-		coordinatorJobSpecConfig.BatchFulfillmentEnabled,                                            //batchFulfillmentEnabled
-		coordinatorJobSpecConfig.BatchFulfillmentGasMultiplier,                                      //batchFulfillmentGasMultiplier
-		strings.Join(util.MapToAddressArr(nodesMap[model.VRFPrimaryNodeName].SendingKeys), "\",\""), //fromAddresses
+		contractAddresses.CoordinatorAddress,                   //coordinatorAddress
+		contractAddresses.BatchCoordinatorAddress,              //batchCoordinatorAddress
+		coordinatorJobSpecConfig.BatchFulfillmentEnabled,       //batchFulfillmentEnabled
+		coordinatorJobSpecConfig.BatchFulfillmentGasMultiplier, //batchFulfillmentGasMultiplier
 		compressedPkHex,            //publicKey
 		coordinatorConfig.MinConfs, //minIncomingConfirmations
 		e.ChainID,                  //evmChainID
