09347f75b6d8f582b5815988350a8fab45948fa4
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2024-02-07 06:48:04 +0200

    VRF-329: fixing lint issues

--- integration-tests/actions/vrf/vrfv2plus/vrfv2plus_models.go
+++ integration-tests/actions/vrf/vrfv2plus/vrfv2plus_models.go
@@ -4,22 +4,6 @@ import (
 	"github.com/smartcontractkit/chainlink/integration-tests/contracts"
 )
 
-//type VRFV2PlusEncodedProvingKey [2]*big.Int
-//
-//// VRFV2PlusKeyData defines a jobs into and proving key info
-//type VRFV2PlusKeyData struct {
-//	VRFKey            *client.VRFKey
-//	EncodedProvingKey VRFV2PlusEncodedProvingKey
-//	KeyHash           [32]byte
-//}
-//
-//type VRFV2PlusData struct {
-//	VRFV2PlusKeyData
-//	VRFJob            *client.Job
-//	PrimaryEthAddress string
-//	ChainID           *big.Int
-//}
-
 type VRFV2PlusWrapperContracts struct {
 	VRFV2PlusWrapper  contracts.VRFV2PlusWrapper
 	LoadTestConsumers []contracts.VRFv2PlusWrapperLoadTestConsumer
