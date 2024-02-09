06dd74468aed6388cccb64ac2729949e60e3d37a
Sam Davies <samsondav@protonmail.com>
2024-02-09 16:16:29 -0500

    Fix lint

--- core/services/keystore/keys/ocr2key/evm_keyring_test.go
+++ core/services/keystore/keys/ocr2key/evm_keyring_test.go
@@ -9,9 +9,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/libocr/offchainreporting2/types"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 )
 
 func TestEVMKeyring_SignVerify(t *testing.T) {
