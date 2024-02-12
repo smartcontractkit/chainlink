e230dda6e11ab8196521f7a952d44e9a51747443
Sam Davies <samsondav@protonmail.com>
2024-02-12 09:38:04 -0500

    Revert "Run prettier on new contracts"
    This reverts commit 739ed422616242b565ea1b046027b33bdc14d6cc.

--- contracts/src/v0.8/llo-feeds/dev/ChannelConfigStore.sol
+++ contracts/src/v0.8/llo-feeds/dev/ChannelConfigStore.sol
@@ -6,99 +6,102 @@ import {IChannelConfigStore} from "./interfaces/IChannelConfigStore.sol";
 import {TypeAndVersionInterface} from "../../interfaces/TypeAndVersionInterface.sol";
 
 contract ChannelConfigStore is ConfirmedOwner, IChannelConfigStore, TypeAndVersionInterface {
-  mapping(uint32 => ChannelDefinition) private s_channelDefinitions;
 
-  // mapping(bytes32 => ChannelConfiguration) private s_channelProductionConfigurations;
-  // mapping(bytes32 => ChannelConfiguration) private s_channelStagingConfigurations;
+    mapping(uint32 => ChannelDefinition) private s_channelDefinitions;
 
-  event NewChannelDefinition(uint32 channelId, ChannelDefinition channelDefinition);
-  event ChannelDefinitionRemoved(uint32 channelId);
-  // event NewProductionConfig(ChannelConfiguration channelConfig);
-  // event NewStagingConfig(ChannelConfiguration channelConfig);
-  event PromoteStagingConfig(uint32 channelId);
+    // mapping(bytes32 => ChannelConfiguration) private s_channelProductionConfigurations;
+    // mapping(bytes32 => ChannelConfiguration) private s_channelStagingConfigurations;
 
-  error OnlyCallableByEOA();
-  error StagingConfigAlreadyPromoted();
-  error EmptyStreamIDs();
-  error ZeroReportFormat();
-  error ZeroChainSelector();
-  error ChannelDefinitionNotFound();
+    event NewChannelDefinition(uint32 channelId, ChannelDefinition channelDefinition);
+    event ChannelDefinitionRemoved(uint32 channelId);
+    // event NewProductionConfig(ChannelConfiguration channelConfig);
+    // event NewStagingConfig(ChannelConfiguration channelConfig);
+    event PromoteStagingConfig(uint32 channelId);
 
-  constructor() ConfirmedOwner(msg.sender) {}
+    error OnlyCallableByEOA();
+    error StagingConfigAlreadyPromoted();
+    error EmptyStreamIDs();
+    error ZeroReportFormat();
+    error ZeroChainSelector();
+    error ChannelDefinitionNotFound();
 
-  // function setStagingConfig(bytes32 configDigest, ChannelConfiguration calldata channelConfig) external onlyOwner {
-  //     s_channelStagingConfigurations[channelId] = channelConfig;
+    constructor() ConfirmedOwner(msg.sender) {}
 
-  //     emit NewStagingConfig(channelConfig);
-  // }
+    // function setStagingConfig(bytes32 configDigest, ChannelConfiguration calldata channelConfig) external onlyOwner {
+    //     s_channelStagingConfigurations[channelId] = channelConfig;
 
-  //// this will trigger the following:
-  //// - offchain ShouldRetireCache will start returning true for the old (production)
-  ////   protocol instance
-  //// - once the old production instance retires it will generate a handover
-  ////   retirement report
-  //// - the staging instance will become the new production instance once
-  ////   any honest oracle that is on both instances forward the retirement
-  ////   report from the old instance to the new instace via the
-  ////   PredecessorRetirementReportCache
-  ////
-  //// Note: the promotion flow only works if the previous production instance
-  //// is working correctly & generating reports. If that's not the case, the
-  //// owner is expected to "setProductionConfig" directly instead. This will
-  //// cause "gaps" to be created, but that seems unavoidable in such a scenario.
-  // function promoteStagingConfig(bytes32 configDigest) external onlyOwner {
-  //     ChannelConfiguration memory stagingConfig = s_channelStagingConfigurations[channelId];
+    //     emit NewStagingConfig(channelConfig);
+    // }
 
-  //     if(stagingConfig.channelConfigId.length == 0) {
-  //         revert StagingConfigAlreadyPromoted();
-  //     }
+    //// this will trigger the following:
+    //// - offchain ShouldRetireCache will start returning true for the old (production)
+    ////   protocol instance
+    //// - once the old production instance retires it will generate a handover
+    ////   retirement report
+    //// - the staging instance will become the new production instance once
+    ////   any honest oracle that is on both instances forward the retirement
+    ////   report from the old instance to the new instace via the
+    ////   PredecessorRetirementReportCache
+    ////
+    //// Note: the promotion flow only works if the previous production instance
+    //// is working correctly & generating reports. If that's not the case, the
+    //// owner is expected to "setProductionConfig" directly instead. This will
+    //// cause "gaps" to be created, but that seems unavoidable in such a scenario.
+    // function promoteStagingConfig(bytes32 configDigest) external onlyOwner {
+    //     ChannelConfiguration memory stagingConfig = s_channelStagingConfigurations[channelId];
 
-  //     s_channelProductionConfigurations[channelId] = s_channelStagingConfigurations[channelId];
+    //     if(stagingConfig.channelConfigId.length == 0) {
+    //         revert StagingConfigAlreadyPromoted();
+    //     }
 
-  //     emit PromoteStagingConfig(channelId);
-  // }
+    //     s_channelProductionConfigurations[channelId] = s_channelStagingConfigurations[channelId];
 
-  function addChannel(uint32 channelId, ChannelDefinition calldata channelDefinition) external onlyOwner {
-    if (channelDefinition.streamIDs.length == 0) {
-      revert EmptyStreamIDs();
+    //     emit PromoteStagingConfig(channelId);
+    // }
+
+    function addChannel(uint32 channelId, ChannelDefinition calldata channelDefinition) external onlyOwner {
+
+        if(channelDefinition.streamIDs.length == 0) {
+            revert EmptyStreamIDs();
+        }
+
+        if(channelDefinition.chainSelector == 0) {
+            revert ZeroChainSelector();
+        }
+
+        if(channelDefinition.reportFormat == 0) {
+            revert ZeroReportFormat();
+        }
+
+        s_channelDefinitions[channelId] = channelDefinition;
+
+        emit NewChannelDefinition(channelId, channelDefinition);
     }
 
-    if (channelDefinition.chainSelector == 0) {
-      revert ZeroChainSelector();
+    function removeChannel(uint32 channelId) external onlyOwner {
+        if(s_channelDefinitions[channelId].streamIDs.length == 0) {
+            revert ChannelDefinitionNotFound();
+        }
+
+        delete s_channelDefinitions[channelId];
+
+        emit ChannelDefinitionRemoved(channelId);
     }
 
-    if (channelDefinition.reportFormat == 0) {
-      revert ZeroReportFormat();
+    function getChannelDefinitions(uint32 channelId) external view returns (ChannelDefinition memory) {
+        if(msg.sender != tx.origin) {
+            revert OnlyCallableByEOA();
+        }
+
+        return s_channelDefinitions[channelId];
     }
 
-    s_channelDefinitions[channelId] = channelDefinition;
-
-    emit NewChannelDefinition(channelId, channelDefinition);
-  }
-
-  function removeChannel(uint32 channelId) external onlyOwner {
-    if (s_channelDefinitions[channelId].streamIDs.length == 0) {
-      revert ChannelDefinitionNotFound();
+    function typeAndVersion() external override pure returns (string memory) {
+        return "ChannelConfigStore 0.0.0";
     }
 
-    delete s_channelDefinitions[channelId];
-
-    emit ChannelDefinitionRemoved(channelId);
-  }
-
-  function getChannelDefinitions(uint32 channelId) external view returns (ChannelDefinition memory) {
-    if (msg.sender != tx.origin) {
-      revert OnlyCallableByEOA();
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
+        return interfaceId == type(IChannelConfigStore).interfaceId;
     }
-
-    return s_channelDefinitions[channelId];
-  }
-
-  function typeAndVersion() external pure override returns (string memory) {
-    return "ChannelConfigStore 0.0.0";
-  }
-
-  function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
-    return interfaceId == type(IChannelConfigStore).interfaceId;
-  }
 }
+
--- contracts/test/v0.8/automation/AutomationRegistry2_2.test.ts
+++ contracts/test/v0.8/automation/AutomationRegistry2_2.test.ts
@@ -958,13 +958,9 @@ describe('AutomationRegistry2_2', () => {
       .transfer(await admin.getAddress(), toWei('1000'))
     let tx = await registry
       .connect(owner)
-      ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-        mock.address,
-        performGas,
-        await admin.getAddress(),
-        randomBytes,
-        '0x',
-      )
+      [
+        'registerUpkeep(address,uint32,address,bytes,bytes)'
+      ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
     upkeepId = await getUpkeepID(tx)
 
     autoFunderUpkeep = await upkeepAutoFunderFactory
@@ -972,27 +968,17 @@ describe('AutomationRegistry2_2', () => {
       .deploy(linkToken.address, registry.address)
     tx = await registry
       .connect(owner)
-      ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-        autoFunderUpkeep.address,
-        performGas,
-        autoFunderUpkeep.address,
-        randomBytes,
-        '0x',
-      )
+      [
+        'registerUpkeep(address,uint32,address,bytes,bytes)'
+      ](autoFunderUpkeep.address, performGas, autoFunderUpkeep.address, randomBytes, '0x')
     afUpkeepId = await getUpkeepID(tx)
 
     ltUpkeep = await deployMockContract(owner, ILogAutomationactory.abi)
     tx = await registry
       .connect(owner)
-      ['registerUpkeep(address,uint32,address,uint8,bytes,bytes,bytes)'](
-        ltUpkeep.address,
-        performGas,
-        await admin.getAddress(),
-        Trigger.LOG,
-        '0x',
-        logTriggerConfig,
-        emptyBytes,
-      )
+      [
+        'registerUpkeep(address,uint32,address,uint8,bytes,bytes,bytes)'
+      ](ltUpkeep.address, performGas, await admin.getAddress(), Trigger.LOG, '0x', logTriggerConfig, emptyBytes)
     logUpkeepId = await getUpkeepID(tx)
 
     await autoFunderUpkeep.setUpkeepId(afUpkeepId)
@@ -1003,13 +989,9 @@ describe('AutomationRegistry2_2', () => {
 
     tx = await registry
       .connect(owner)
-      ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-        streamsLookupUpkeep.address,
-        performGas,
-        await admin.getAddress(),
-        randomBytes,
-        '0x',
-      )
+      [
+        'registerUpkeep(address,uint32,address,bytes,bytes)'
+      ](streamsLookupUpkeep.address, performGas, await admin.getAddress(), randomBytes, '0x')
     streamsLookupUpkeepId = await getUpkeepID(tx)
   }
 
@@ -1027,13 +1009,9 @@ describe('AutomationRegistry2_2', () => {
       await mock.setPerformGasToBurn(BigNumber.from('0'))
       const tx = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          randomBytes,
-          '0x',
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
       const condUpkeepId = await getUpkeepID(tx)
       passingConditionalUpkeepIds.push(condUpkeepId)
 
@@ -1046,15 +1024,9 @@ describe('AutomationRegistry2_2', () => {
       await mock.setPerformGasToBurn(BigNumber.from('0'))
       const tx = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,uint8,bytes,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          Trigger.LOG,
-          '0x',
-          logTriggerConfig,
-          emptyBytes,
-        )
+        [
+          'registerUpkeep(address,uint32,address,uint8,bytes,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), Trigger.LOG, '0x', logTriggerConfig, emptyBytes)
       const logUpkeepId = await getUpkeepID(tx)
       passingLogUpkeepIds.push(logUpkeepId)
 
@@ -1067,13 +1039,9 @@ describe('AutomationRegistry2_2', () => {
       await mock.setPerformGasToBurn(BigNumber.from('0'))
       const tx = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          randomBytes,
-          '0x',
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
       const failingUpkeepId = await getUpkeepID(tx)
       failingUpkeepIds.push(failingUpkeepId)
     }
@@ -1555,13 +1523,9 @@ describe('AutomationRegistry2_2', () => {
 
         let tx = await arbRegistry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            randomBytes,
-            '0x',
-          )
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
         const testUpkeepId = await getUpkeepID(tx)
         await arbRegistry.connect(owner).addFunds(testUpkeepId, toWei('100'))
 
@@ -2516,13 +2480,9 @@ describe('AutomationRegistry2_2', () => {
         const mock = await upkeepMockFactory.deploy()
         const tx = await registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            randomBytes,
-            '0x',
-          )
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
         const testUpkeepId = await getUpkeepID(tx)
         upkeepIds.push(testUpkeepId)
 
@@ -2557,13 +2517,9 @@ describe('AutomationRegistry2_2', () => {
         const mock = await upkeepMockFactory.deploy()
         const tx = await arbRegistry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            randomBytes,
-            '0x',
-          )
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
         const testUpkeepId = await getUpkeepID(tx)
         upkeepIds.push(testUpkeepId)
 
@@ -2619,13 +2575,9 @@ describe('AutomationRegistry2_2', () => {
       // add funds to upkeep 1 and perform and withdraw some payment
       const tx = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          emptyBytes,
-          emptyBytes,
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), emptyBytes, emptyBytes)
 
       const id1 = await getUpkeepID(tx)
       await registry.connect(admin).addFunds(id1, toWei('5'))
@@ -2647,13 +2599,9 @@ describe('AutomationRegistry2_2', () => {
       // add funds to upkeep 2 and perform and withdraw some payment
       const tx2 = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          emptyBytes,
-          emptyBytes,
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), emptyBytes, emptyBytes)
       const id2 = await getUpkeepID(tx2)
       await registry.connect(admin).addFunds(id2, toWei('5'))
 
@@ -2731,23 +2679,15 @@ describe('AutomationRegistry2_2', () => {
     it('uses maxPerformData size in checkUpkeep but actual performDataSize in transmit', async () => {
       const tx1 = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          randomBytes,
-          '0x',
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
       const upkeepID1 = await getUpkeepID(tx1)
       const tx2 = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          randomBytes,
-          '0x',
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
       const upkeepID2 = await getUpkeepID(tx2)
       await mock.setCanCheck(true)
       await mock.setCanPerform(true)
@@ -2810,13 +2750,9 @@ describe('AutomationRegistry2_2', () => {
     beforeEach(async () => {
       const tx = await registry
         .connect(owner)
-        ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-          mock.address,
-          performGas,
-          await admin.getAddress(),
-          randomBytes,
-          '0x',
-        )
+        [
+          'registerUpkeep(address,uint32,address,bytes,bytes)'
+        ](mock.address, performGas, await admin.getAddress(), randomBytes, '0x')
       upkeepId2 = await getUpkeepID(tx)
 
       await registry.connect(admin).addFunds(upkeepId, toWei('100'))
@@ -3957,13 +3893,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), emptyBytes, '0x'),
         'RegistryPaused()',
       )
     })
@@ -3972,13 +3904,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            zeroAddress,
-            performGas,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](zeroAddress, performGas, await admin.getAddress(), emptyBytes, '0x'),
         'NotAContract()',
       )
     })
@@ -3987,13 +3915,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(keeper1)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), emptyBytes, '0x'),
         'OnlyCallableByOwnerOrRegistrar()',
       )
     })
@@ -4002,13 +3926,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            2299,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, 2299, await admin.getAddress(), emptyBytes, '0x'),
         'GasLimitOutsideRange()',
       )
     })
@@ -4017,13 +3937,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            5000001,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, 5000001, await admin.getAddress(), emptyBytes, '0x'),
         'GasLimitOutsideRange()',
       )
     })
@@ -4036,13 +3952,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            longBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), longBytes, '0x'),
         'CheckDataExceedsLimit()',
       )
     })
@@ -4057,13 +3969,9 @@ describe('AutomationRegistry2_2', () => {
           const checkData = checkDatas[kdx]
           const tx = await registry
             .connect(owner)
-            ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-              mock.address,
-              performGas,
-              await admin.getAddress(),
-              checkData,
-              '0x',
-            )
+            [
+              'registerUpkeep(address,uint32,address,bytes,bytes)'
+            ](mock.address, performGas, await admin.getAddress(), checkData, '0x')
 
           //confirm the upkeep details and verify emitted events
           const testUpkeepId = await getUpkeepID(tx)
@@ -4725,13 +4633,9 @@ describe('AutomationRegistry2_2', () => {
       await evmRevert(
         registry
           .connect(owner)
-          ['registerUpkeep(address,uint32,address,bytes,bytes)'](
-            mock.address,
-            performGas,
-            await admin.getAddress(),
-            emptyBytes,
-            '0x',
-          ),
+          [
+            'registerUpkeep(address,uint32,address,bytes,bytes)'
+          ](mock.address, performGas, await admin.getAddress(), emptyBytes, '0x'),
         'RegistryPaused()',
       )
     })
