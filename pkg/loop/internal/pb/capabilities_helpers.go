331721b9cb15622e877a792886c3b84422577747
Cedric <cedric.cordenier@smartcontract.com>
2024-02-13 14:52:29 +0000

    [KS-15] GRPC support for the capability API (#334)
    * [KS-15] Add CapabilitiesRegistry proto implementation.
    
    - Move capabilities from core into chainlink-common.
    - Add a nil value to the values package
    - Implement the capabilities registry GRPC implementation, including
    support for calling the execution and metadata APIs of all of the
    capability types.
    
    * More methods for CapabilityRegistry
    
    * Standardise proto casing
    
    * More tests
    
    * Linting
    
    * Just add the capability hooks
    
    * Alias capability type constants

---
+++ pkg/loop/internal/pb/capabilities_helpers.go
@@ -0,0 +1,9 @@
+package pb
+
+const (
+	CapabilityTypeUnknown   = CapabilityType_CAPABILITY_TYPE_UNKNOWN
+	CapabilityTypeTrigger   = CapabilityType_CAPABILITY_TYPE_TRIGGER
+	CapabilityTypeAction    = CapabilityType_CAPABILITY_TYPE_ACTION
+	CapabilityTypeConsensus = CapabilityType_CAPABILITY_TYPE_CONSENSUS
+	CapabilityTypeTarget    = CapabilityType_CAPABILITY_TYPE_TARGET
+)
--- pkg/loop/internal/pb/generate.go
+++ pkg/loop/internal/pb/generate.go
@@ -7,4 +7,5 @@
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative chain_reader.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative median_datasource.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative codec.proto
+//go:generate protoc --go_out=../../../ --go_opt=paths=source_relative --go-grpc_out=../../../ --go-grpc_opt=paths=source_relative --proto_path=../../../ loop/internal/pb/capabilities.proto values/pb/values.proto
 package pb
