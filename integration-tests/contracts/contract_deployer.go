22736d7f466e276c069fcbff515894275bcae80a
stackman27 <sishir.giri@smartcontract.com>
2024-02-18 11:28:21 -0800

    mantle sep

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -193,7 +193,7 @@ func NewContractDeployer(bcClient blockchain.EVMClient, logger zerolog.Logger) (
 		return &KromaContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	case *blockchain.WeMixClient:
 		return &WeMixContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
-	case *blockchain.MantleGoerliClient:
+	case *blockchain.MantleSepoliaClient:
 		return &MantleContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	}
 	return nil, errors.New("unknown blockchain client implementation for contract deployer, register blockchain client in NewContractDeployer")
