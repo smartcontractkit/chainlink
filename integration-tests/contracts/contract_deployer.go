81743750f257c9234e32bd82e9f57ed59a89c629
David Cauchi <13139524+davidcauchi@users.noreply.github.com>
2024-02-19 17:59:29 +0100

    [ship-812] enable gnosis soak (#12081)
    * Rename xdai config to gnosis
    
    * Add Gnosis Chiado toml config
    
    * Update config docs
    
    * Enable Gnosis
    
    * Enable EIP1559
    
    * Update CTF to latest
    
    * make config-docs
    
    * Remove legacy config
    
    * Removed cl address
    
    * Rebase develop
    
    * make config-docs

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -193,6 +193,8 @@ func NewContractDeployer(bcClient blockchain.EVMClient, logger zerolog.Logger) (
 		return &KromaContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	case *blockchain.WeMixClient:
 		return &WeMixContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
+	case *blockchain.GnosisClient:
+		return &GnosisContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	}
 	return nil, errors.New("unknown blockchain client implementation for contract deployer, register blockchain client in NewContractDeployer")
 }
@@ -265,6 +267,10 @@ type KromaContractDeployer struct {
 }
 
 type WeMixContractDeployer struct {
+	*EthereumContractDeployer
+}
+
+type GnosisContractDeployer struct {
 	*EthereumContractDeployer
 }
 
