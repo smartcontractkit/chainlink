067b85b5d6be486cf12fba256998c28f9c56da9a
dimitris <dimitrios.kouveris@smartcontract.com>
2024-02-20 18:00:56 +0100

    offchain - include job id in usdc LP filter (#525)
    ## Motivation
    
    Filters should be unique per lane.
    E.g. having two lanes `a@1.x eth->opt` and `b@1.y eth->opt`
    Deleting lane `a` should not affect filters of `b`.
    
    ## Solution
    
    Include jobID in the filter name making filters on each lane unique.

--- core/services/ocr2/plugins/ccip/internal/ccipdata/usdc_reader_internal_test.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/usdc_reader_internal_test.go
@@ -3,6 +3,7 @@ package ccipdata
 import (
 	"context"
 	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -28,7 +29,7 @@ func TestLogPollerClient_GetLastUSDCMessagePriorToLogIndexInTx(t *testing.T) {
 
 	t.Run("multiple found", func(t *testing.T) {
 		lp := lpmocks.NewLogPoller(t)
-		u := NewUSDCReader(lggr, utils.RandomAddress(), lp)
+		u, _ := NewUSDCReader(lggr, "job_123", utils.RandomAddress(), lp, false)
 		lp.On("IndexedLogsByTxHash",
 			u.usdcMessageSent,
 			u.transmitterAddress,
@@ -49,7 +50,7 @@ func TestLogPollerClient_GetLastUSDCMessagePriorToLogIndexInTx(t *testing.T) {
 
 	t.Run("none found", func(t *testing.T) {
 		lp := lpmocks.NewLogPoller(t)
-		u := NewUSDCReader(lggr, utils.RandomAddress(), lp)
+		u, _ := NewUSDCReader(lggr, "job_123", utils.RandomAddress(), lp, false)
 		lp.On("IndexedLogsByTxHash",
 			u.usdcMessageSent,
 			u.transmitterAddress,
@@ -75,4 +76,41 @@ func TestParse(t *testing.T) {
 	expectedPostParse := "0x0000000000000001000000020000000000048d71000000000000000000000000eb08f243e5d3fcff26a9e38ae5520a669f4019d000000000000000000000000023a04d5935ed8bc8e3eb78db3541f0abfb001c6e0000000000000000000000006cb3ed9b441eb674b58495c8b3324b59faff5243000000000000000000000000000000005425890298aed601595a70ab815c96711a31bc65000000000000000000000000ab4f961939bfe6a93567cc57c59eed7084ce2131000000000000000000000000000000000000000000000000000000000000271000000000000000000000000035e08285cfed1ef159236728f843286c55fc0861"
 
 	require.Equal(t, expectedPostParse, hexutil.Encode(parsedBody))
+}
+
+func TestFilters(t *testing.T) {
+	t.Run("filters of different jobs should be distinct", func(t *testing.T) {
+		lp := lpmocks.NewLogPoller(t)
+		lggr := logger.TestLogger(t)
+
+		jobID1 := "job-1"
+		jobID2 := "job-2"
+		transmitter := utils.RandomAddress()
+
+		var registeredFilters []string   // filters registered by job-1
+		var deregisteredFilters []string // should not exist in filters deregistered by job-2
+
+		// job-1 registers filters
+		lp.On("RegisterFilter", mock.Anything).Run(func(args mock.Arguments) {
+			f := args.Get(0).(logpoller.Filter)
+			registeredFilters = append(registeredFilters, f.Name)
+		}).Return(nil)
+		_, err := NewUSDCReader(lggr, jobID1, transmitter, lp, true)
+		assert.NoError(t, err)
+
+		// job-2 de-registers filters
+		lp.On("UnregisterFilter", mock.Anything).Run(func(args mock.Arguments) {
+			name := args.Get(0).(string)
+			deregisteredFilters = append(deregisteredFilters, name)
+		}).Return(nil)
+		err = CloseUSDCReader(lggr, jobID2, transmitter, lp)
+		assert.NoError(t, err)
+
+		assert.NotEmpty(t, deregisteredFilters)
+		assert.NotEmpty(t, registeredFilters)
+		for _, f := range registeredFilters {
+			assert.False(t, slices.Contains(deregisteredFilters, f))
+		}
+	})
+
 }
