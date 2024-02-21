777e5aa5d7698603f5b1dc8765dca0929ca6764a
amirylm <amirylm.dev@gmail.com>
2024-02-21 15:13:03 +0200

    handling empty performData case for err handler

--- contracts/src/v0.8/automation/testhelpers/LogTriggeredStreamsLookup.sol
+++ contracts/src/v0.8/automation/testhelpers/LogTriggeredStreamsLookup.sol
@@ -26,6 +26,7 @@ contract LogTriggeredStreamsLookup is ILogAutomation, StreamsLookupCompatibleInt
     bytes verified
   );
   event LimitOrderExecuted(uint256 indexed orderId, uint256 indexed amount, address indexed exchange); // keccak(LimitOrderExecuted(uint256,uint256,address)) => 0xd1ffe9e45581c11d7d9f2ed5f75217cd4be9f8b7eee6af0f6d03f46de53956cd
+  event IgnoringErrorHandlerData();
 
   ArbSys internal constant ARB_SYS = ArbSys(0x0000000000000000000000000000000000000064);
   IVerifierProxy internal constant VERIFIER = IVerifierProxy(0x09DFf56A4fF44e0f4436260A04F5CFa65636A481);
@@ -96,6 +97,9 @@ contract LogTriggeredStreamsLookup is ILogAutomation, StreamsLookupCompatibleInt
   }
 
   function performUpkeep(bytes calldata performData) external override {
+    if (performData.length == 0) {
+      emit IgnoringErrorHandlerData();
+    }
     (bytes[] memory values, bytes memory extraData) = abi.decode(performData, (bytes[], bytes));
     (uint256 orderId, uint256 amount, address exchange, bytes32 logTopic0) = abi.decode(
       extraData,
