6d5d9b9c73f459f759a3f6b2975c41c61d775737
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-02-22 19:14:31 +0100

    Minor fix

--- core/services/ocr2/plugins/llo/onchain_channel_definition_cache_integration_test.go
+++ core/services/ocr2/plugins/llo/onchain_channel_definition_cache_integration_test.go
@@ -77,7 +77,7 @@ func Test_ChannelDefinitionCache_Integration(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("with zero fromblock", func(t *testing.T) {
-		lp := logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000)
+		lp := logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000, 0)
 		servicetest.Run(t, lp)
 		cdc := llo.NewChannelDefinitionCache(lggr, orm, lp, configStoreAddress, 0)
 
@@ -142,7 +142,7 @@ func Test_ChannelDefinitionCache_Integration(t *testing.T) {
 	t.Run("loads from ORM", func(t *testing.T) {
 		// Override logpoller to always return no logs
 		lp := &mockLogPoller{
-			LogPoller: logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000),
+			LogPoller: logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000, 0),
 			LatestBlockFn: func(qopts ...pg.QOpt) (int64, error) {
 				return 0, nil
 			},
@@ -176,7 +176,7 @@ func Test_ChannelDefinitionCache_Integration(t *testing.T) {
 	pgtest.MustExec(t, db, `DELETE FROM channel_definitions`)
 
 	t.Run("with non-zero fromBlock", func(t *testing.T) {
-		lp := logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000)
+		lp := logpoller.NewLogPoller(logpoller.NewORM(testutils.SimulatedChainID, db, lggr, pgtest.NewQConfig(true)), ethClient, lggr, 100*time.Millisecond, false, 1, 3, 2, 1000, 0)
 		servicetest.Run(t, lp)
 		cdc := llo.NewChannelDefinitionCache(lggr, orm, lp, configStoreAddress, channel2Block.Number().Int64()+1)
 
