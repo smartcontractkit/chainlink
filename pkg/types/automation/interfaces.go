91a8eb31313083eccba379ea1eaa518219391afb
ferglor <fergal.gribben@smartcontract.com>
2024-03-04 14:21:08 +0000

    Revert "Add a SetConfig function to the LogEventProvider interface (#367)" (#385)
    This reverts commit c9d7b3cf9d07d031b629c3d8083d9fb6afd55e14.

--- pkg/types/automation/interfaces.go
+++ pkg/types/automation/interfaces.go
@@ -48,15 +48,8 @@ type Encoder interface {
 
 type LogEventProvider interface {
 	GetLatestPayloads(context.Context) ([]UpkeepPayload, error)
-	SetConfig(LogEventProviderConfig)
 	Start(context.Context) error
 	Close() error
-}
-
-type LogEventProviderConfig struct {
-	NumLogUpkeeps    uint32
-	FastExecLogsHigh uint32
-	FastExecLogsLow  uint32
 }
 
 type RecoverableProvider interface {
