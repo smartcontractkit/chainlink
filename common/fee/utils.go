16969bb8287bb6971c01e78eadfdd9490e516e56
amit-momin <amit.momin@smartcontract.com>
2024-03-05 21:45:00 -0600

    Fixed linting and tests

--- common/fee/utils.go
+++ common/fee/utils.go
@@ -11,7 +11,7 @@ import (
 func ApplyMultiplier(feeLimit uint64, multiplier float32) (uint64, error) {
 	result := decimal.NewFromBigInt(big.NewInt(0).SetUint64(feeLimit), 0).Mul(decimal.NewFromFloat32(multiplier))
 
-	if result.GreaterThan(decimal.NewFromBigInt(big.NewInt(0).SetUint64(math.MaxUint64), 0)){
+	if result.GreaterThan(decimal.NewFromBigInt(big.NewInt(0).SetUint64(math.MaxUint64), 0)) {
 		return 0, fmt.Errorf("integer overflow when applying multiplier of %f to fee limit of %d", multiplier, feeLimit)
 	}
 	return result.BigInt().Uint64(), nil
