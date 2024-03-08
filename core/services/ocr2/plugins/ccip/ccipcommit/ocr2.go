34f74d3d30f4b1ff57c07e691b7140a8924c5b79
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-03-08 10:47:18 +0100

    Removing checks from Query because of the latency requirements for that OCR phase (#595)
    `Query` has not been used so far, so its timeout is set to very low
    values - usually 100 millis. That makes querying blockchain not doable
    in that OCR2 phase

--- core/services/ocr2/plugins/ccip/ccipcommit/ocr2.go
+++ core/services/ocr2/plugins/ccip/ccipcommit/ocr2.go
@@ -92,12 +92,7 @@ type CommitReportingPlugin struct {
 }
 
 // Query is not used by the CCIP Commit plugin.
-func (r *CommitReportingPlugin) Query(ctx context.Context, _ types.ReportTimestamp) (types.Query, error) {
-	if healthy, err := r.chainHealthcheck.IsHealthy(ctx, false); err != nil {
-		return nil, err
-	} else if !healthy {
-		return nil, ccip.ErrChainIsNotHealthy
-	}
+func (r *CommitReportingPlugin) Query(context.Context, types.ReportTimestamp) (types.Query, error) {
 	return types.Query{}, nil
 }
 
