d3ee868c7d6b88d0389df0966b16ad2653a05aaa
vyzaldysanchez <vyzaldysanchez@gmail.com>
2024-03-10 19:59:01 -0400

    Fixes migration

--- core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
+++ core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
@@ -1,30 +1,30 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE job_pipeline_spec (
+CREATE TABLE public.job_pipeline_specs (
     job_id INT NOT NULL,
     pipeline_spec_id INT NOT NULL,
     is_primary BOOLEAN NOT NULL DEFAULT FALSE, -- Cannot use `primary` as column name in Postgres since it is a reserved keyword.
     CONSTRAINT pk_job_pipeline_spec PRIMARY KEY (job_id, pipeline_spec_id),
-    CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES job(id),
-    CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES pipeline_spec(id)
+    CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES public.jobs(id),
+    CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES public.pipeline_specs(id)
 );
 
-INSERT INTO job_pipeline_spec (job_id, pipeline_spec_id, is_primary)
+INSERT INTO public.job_pipeline_specs (job_id, pipeline_spec_id, is_primary)
 SELECT id, pipeline_spec_id, TRUE
-FROM job;
+FROM public.jobs;
 
-ALTER TABLE job DROP COLUMN pipeline_spec_id; -- Do we use CASCADE here? Does it have any relationship with other tables?
+ALTER TABLE public.jobs DROP COLUMN pipeline_spec_id; -- Do we use CASCADE here? Does it have any relationship with other tables?
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE job ADD COLUMN pipeline_spec_id INT;
+ALTER TABLE public.jobs ADD COLUMN pipeline_spec_id INT;
 
-UPDATE job
+UPDATE public.jobs
 SET pipeline_spec_id = jps.pipeline_spec_id
-FROM job_pipeline_spec jps
-WHERE jps.job_id = job.id
+FROM public.job_pipeline_specs jps
+WHERE jps.job_id = public.jobs.id
   AND jps.is_primary = TRUE;
 
-DROP TABLE IF EXISTS job_pipeline_spec; -- Do we use CASCADE here?
+DROP TABLE IF EXISTS public.job_pipeline_specs; -- Do we use CASCADE here?
 -- +goose StatementEnd
