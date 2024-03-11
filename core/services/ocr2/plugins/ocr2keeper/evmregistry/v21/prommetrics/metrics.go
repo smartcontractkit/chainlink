760601e627ad42895b16397f47bd75a78ef1bd76
Oliver Townsend <oliver.townsend@smartcontract.com>
2024-03-11 16:07:06 -0700

    Add error tracking metrics to streams lookup

--- core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/prommetrics/metrics.go
+++ core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/prommetrics/metrics.go
@@ -5,8 +5,24 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-// AutomationNamespace is the namespace for all Automation related metrics
+// Namespaces
 const AutomationLogTriggerNamespace = "automation_log_trigger"
+const AutomationStreamsNamespace = "automation_streams"
+
+// Streams steps
+const StreamsLookupStepDoMercuryRequest = "do_mercury_request"
+const StreamsLookupStepCheckErrorHandler = "check_error_handler"
+const StreamsLookupStepCheckCallback = "check_callback"
+
+// Streams error labels
+const StreamsLookupErrorReasonNotReverted = "error_reason_not_target_check_reverted"
+const StreamsLookupErrorDecodeRequestFailed = "decode_request_failed"
+const StreamsLookupErrorCredentialsNotConfigured = "credentials_not_configured"
+const StreamsLookupErrorDoMercuryRequest = "do_mercury_request"
+const StreamsLookupErrorCodeNotNil = "err_code_not_nil"
+const StreamsLookupErrorCheckCallback = "check_callback"
+const StreamsLookupErrorPackUserCheckErrorHandler = "pack_user_check_error_handler"
+const StreamsLookupErrorPackExecuteCallback = "pack_execute_callback"
 
 // Metric labels
 const (
@@ -17,6 +33,7 @@ const (
 
 // Automation metrics
 var (
+	// Log Trigger metrics
 	AutomationLogBufferFlow = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: AutomationLogTriggerNamespace,
 		Name:      "num_logs_in_log_buffer",
@@ -43,5 +60,21 @@ var (
 		Namespace: AutomationLogTriggerNamespace,
 		Name:      "log_provider_latest_block",
 		Help:      "The latest block number the log provider has seen",
+	})
+
+	// Streams metrics
+	AutomationStreamsLookupStep = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: AutomationStreamsNamespace,
+		Name:      "streams_lookup_step_count",
+		Help:      "How many times individual steps of the streams lookup process run",
+	}, []string{
+		"step",
+	})
+	AutomationStreamsLookupError = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: AutomationStreamsNamespace,
+		Name:      "streams_lookup_error_count",
+		Help:      "Errors occured during a streams lookup attempt",
+	}, []string{
+		"error",
 	})
 )
