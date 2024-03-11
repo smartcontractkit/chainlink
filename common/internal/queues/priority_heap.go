5b541cee4789a191bdfb533d956670d54ef6ae0b
James Walker <jim.walker@smartcontract.com>
2024-03-11 12:44:41 -0400

    address some comments

--- common/internal/queues/priority_heap.go
+++ common/internal/queues/priority_heap.go
@@ -1,8 +1,6 @@
 package queues
 
 import (
-	"sync"
-
 	feetypes "github.com/smartcontractkit/chainlink/v2/common/fee/types"
 	txmgrtypes "github.com/smartcontractkit/chainlink/v2/common/txmgr/types"
 	"github.com/smartcontractkit/chainlink/v2/common/types"
@@ -17,7 +15,6 @@ type priorityHeap[
 	SEQ types.Sequence,
 	FEE feetypes.Fee,
 ] struct {
-	sync.RWMutex
 	txs       []*txmgrtypes.Tx[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]
 	idToIndex map[int64]int
 }
@@ -44,32 +41,18 @@ func NewPriorityHeap[
 
 // Close clears the queue
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Close() {
-	pq.Lock()
-	defer pq.Unlock()
-
-	clear(pq.txs)
-	clear(pq.idToIndex)
 	pq.txs = nil
 	pq.idToIndex = nil
 }
 
 // FindIndexByID returns the index of the transaction with the given ID
-func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) FindIndexByID(id int64) int {
-	pq.RLock()
-	defer pq.RUnlock()
-
+func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) FindIndexByID(id int64) (int, bool) {
 	i, ok := pq.idToIndex[id]
-	if !ok {
-		return -1
-	}
-	return i
+	return i, ok
 }
 
 // Peek returns the next transaction to be processed
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Peek() *txmgrtypes.Tx[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE] {
-	pq.RLock()
-	defer pq.RUnlock()
-
 	if len(pq.txs) == 0 {
 		return nil
 	}
@@ -78,9 +61,6 @@ func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Peek()
 
 // Cap returns the capacity of the queue
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Cap() int {
-	pq.RLock()
-	defer pq.RUnlock()
-
 	if pq.txs == nil {
 		return 0
 	}
@@ -89,50 +69,28 @@ func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Cap() 
 
 // Len, Less, Swap, Push, and Pop methods implement the heap interface
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Len() int {
-	pq.RLock()
-	defer pq.RUnlock()
-
 	if pq.txs == nil {
 		return 0
 	}
 	return len(pq.txs)
 }
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Less(i, j int) bool {
-	pq.RLock()
-	defer pq.RUnlock()
 	// We want Pop to give us the oldest, not newest, transaction based on creation time
 	return pq.txs[i].CreatedAt.Before(pq.txs[j].CreatedAt)
 }
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Swap(i, j int) {
-	pq.Lock()
-	defer pq.Unlock()
-
 	pq.txs[i], pq.txs[j] = pq.txs[j], pq.txs[i]
-	pq.idToIndex[pq.txs[i].ID] = j
-	pq.idToIndex[pq.txs[j].ID] = i
+	pq.idToIndex[pq.txs[i].ID] = i
+	pq.idToIndex[pq.txs[j].ID] = j
 }
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Push(tx any) {
-	pq.Lock()
-	defer pq.Unlock()
-
-	if len(pq.txs) == cap(pq.txs) {
-		// make room if cap is reached
-		delete(pq.idToIndex, pq.txs[0].ID)
-		copy(pq.txs, pq.txs[1:])
-		pq.txs = pq.txs[:len(pq.txs)-1]
-	}
-
 	pq.txs = append(pq.txs, tx.(*txmgrtypes.Tx[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]))
-	pq.idToIndex[tx.(*txmgrtypes.Tx[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]).ID] = len(pq.txs)
+	pq.idToIndex[tx.(*txmgrtypes.Tx[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]).ID] = len(pq.txs) - 1
 }
 func (pq *priorityHeap[CHAIN_ID, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) Pop() any {
-	pq.Lock()
-	defer pq.Unlock()
-
 	old := pq.txs
 	n := len(old)
 	tx := old[n-1]
-	old[n-1] = nil // avoid memory leak
 	pq.txs = old[0 : n-1]
 	delete(pq.idToIndex, tx.ID)
 	return tx
