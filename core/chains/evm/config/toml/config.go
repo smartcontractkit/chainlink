3119ecdc5154454d996bbeb56d90c7510878f49b
Dimitris <dimitris.grigoriou@smartcontract.com>
2024-03-11 14:00:12 +0200

    Remove chains from evm config

--- core/chains/evm/config/toml/config.go
+++ core/chains/evm/config/toml/config.go
@@ -1,6 +1,7 @@
 package toml
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"slices"
@@ -17,12 +18,13 @@ import (
 	commontypes "github.com/smartcontractkit/chainlink-common/pkg/types"
 
 	"github.com/smartcontractkit/chainlink/v2/common/config"
-	"github.com/smartcontractkit/chainlink/v2/core/chains"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/assets"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils/big"
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/ethkey"
 )
+
+var ErrNotFound = errors.New("not found")
 
 type HasEVMConfigs interface {
 	EVMConfigs() EVMConfigs
@@ -145,7 +147,7 @@ func (cs EVMConfigs) Node(name string) (types.Node, error) {
 			}
 		}
 	}
-	return types.Node{}, fmt.Errorf("node %s: %w", name, chains.ErrNotFound)
+	return types.Node{}, fmt.Errorf("node %s: %w", name, ErrNotFound)
 }
 
 func (cs EVMConfigs) NodeStatus(name string) (commontypes.NodeStatus, error) {
@@ -156,7 +158,7 @@ func (cs EVMConfigs) NodeStatus(name string) (commontypes.NodeStatus, error) {
 			}
 		}
 	}
-	return commontypes.NodeStatus{}, fmt.Errorf("node %s: %w", name, chains.ErrNotFound)
+	return commontypes.NodeStatus{}, fmt.Errorf("node %s: %w", name, ErrNotFound)
 }
 
 func legacyNode(n *Node, chainID *big.Big) (v2 types.Node) {
@@ -205,7 +207,7 @@ func (cs EVMConfigs) Nodes(chainID string) (ns []types.Node, err error) {
 	}
 	nodes := cs.nodes(chainID)
 	if nodes == nil {
-		err = fmt.Errorf("no nodes: chain %q: %w", chainID, chains.ErrNotFound)
+		err = fmt.Errorf("no nodes: chain %q: %w", chainID, ErrNotFound)
 		return
 	}
 	for _, n := range nodes {
