44a8646e0906664a46c08e7bd976cedf07922b5d
vyzaldysanchez <vyzaldysanchez@gmail.com>
2024-03-12 10:51:19 -0400

    Removes primary key constraint

--- core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
+++ core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
@@ -4,7 +4,6 @@ CREATE TABLE public.job_pipeline_specs (
     job_id INT NOT NULL,
     pipeline_spec_id INT NOT NULL,
     is_primary BOOLEAN NOT NULL DEFAULT FALSE, -- Cannot use `primary` as column name in Postgres since it is a reserved keyword.
-    CONSTRAINT pk_job_pipeline_spec PRIMARY KEY (job_id, pipeline_spec_id),
     CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES public.jobs(id),
     CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES public.pipeline_specs(id)
 );
