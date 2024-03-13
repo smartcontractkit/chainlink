1b2007003ba0379e18ac334d92c33684a466a522
Oliver Townsend <oliver.townsend@smartcontract.com>
2024-03-13 12:51:47 -0700

    Test 3 - 40 batch size, 6 reader threads

--- core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/logprovider/provider.go
+++ core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/logprovider/provider.go
@@ -43,10 +43,10 @@ var (
 	readJobQueueSize = 64
 	readLogsTimeout  = 10 * time.Second
 
-	readMaxBatchSize = 32
+	readMaxBatchSize = 40
 	// reorgBuffer is the number of blocks to add as a buffer to the block range when reading logs.
 	reorgBuffer   = int64(32)
-	readerThreads = 8
+	readerThreads = 6
 )
 
 // LogTriggerConfig is an alias for log trigger config.
