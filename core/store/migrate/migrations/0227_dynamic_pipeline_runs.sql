0ecaab22d980c10b55d2968c2739211a46f9fdf1
vyzaldysanchez <vyzaldysanchez@gmail.com>
2024-03-13 09:16:30 -0400

    Fixes migration

--- core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
+++ core/store/migrate/migrations/0227_dynamic_pipeline_runs.sql
@@ -9,7 +9,7 @@ CREATE TABLE public.job_pipeline_specs (
     CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES public.pipeline_specs(id)
 );
 
-CREATE UNIQUE INDEX idx_unique_primary_per_job ON job_pipeline_spec(job_id) WHERE is_primary;
+CREATE UNIQUE INDEX idx_unique_primary_per_job ON public.job_pipeline_spec(job_id) WHERE is_primary;
 
 -- The moment this runs, we only have one job+pipeline_spec combination per job, complying with the unique index.
 INSERT INTO public.job_pipeline_specs (job_id, pipeline_spec_id, is_primary)
