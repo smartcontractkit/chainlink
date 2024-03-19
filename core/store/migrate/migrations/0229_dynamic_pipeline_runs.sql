672dec6e6657a897d167a973567ed2d3ef98b1d4
vyzaldysanchez <vyzaldysanchez@gmail.com>
2024-03-19 00:24:28 -0400

    Fixes migration

--- core/store/migrate/migrations/0229_dynamic_pipeline_runs.sql
+++ core/store/migrate/migrations/0229_dynamic_pipeline_runs.sql
@@ -1,48 +1,48 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE public.job_pipeline_specs (
+CREATE TABLE job_pipeline_specs (
     job_id INT NOT NULL,
     pipeline_spec_id INT NOT NULL,
     is_primary BOOLEAN NOT NULL DEFAULT FALSE, -- Cannot use `primary` as column name in Postgres since it is a reserved keyword.
     CONSTRAINT pk_job_pipeline_spec PRIMARY KEY (job_id, pipeline_spec_id),
-    CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES public.jobs(id),
-    CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES public.pipeline_specs(id)
+    CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES jobs(id),
+    CONSTRAINT fk_pipeline_spec FOREIGN KEY (pipeline_spec_id) REFERENCES pipeline_specs(id)
 );
 
-CREATE UNIQUE INDEX idx_unique_primary_per_job ON public.job_pipeline_spec(job_id) WHERE is_primary;
+CREATE UNIQUE INDEX idx_unique_primary_per_job ON job_pipeline_specs(job_id) WHERE is_primary;
 
 -- The moment this runs, we only have one job+pipeline_spec combination per job, complying with the unique index.
-INSERT INTO public.job_pipeline_specs (job_id, pipeline_spec_id, is_primary)
+INSERT INTO job_pipeline_specs (job_id, pipeline_spec_id, is_primary)
 SELECT id, pipeline_spec_id, TRUE
-FROM public.jobs;
+FROM jobs;
 
-ALTER TABLE public.jobs DROP COLUMN pipeline_spec_id; -- Do we use CASCADE here? Does it have any relationship with other tables?
+ALTER TABLE jobs DROP COLUMN pipeline_spec_id; -- Do we use CASCADE here? Does it have any relationship with other tables?
 
-ALTER TABLE public.pipeline_runs ADD COLUMN pruning_key INT;
+ALTER TABLE pipeline_runs ADD COLUMN pruning_key INT;
 
-UPDATE public.pipeline_runs
+UPDATE pipeline_runs
 SET pruning_key = pjps.job_id
-FROM public.job_pipeline_specs pjps
-WHERE pjps.pipeline_spec_id = public.pipeline_runs.pipeline_spec_id;
+FROM job_pipeline_specs pjps
+WHERE pjps.pipeline_spec_id = pipeline_runs.pipeline_spec_id;
 
-ALTER TABLE public.pipeline_runs ALTER COLUMN pruning_key SET NOT NULL;
+ALTER TABLE pipeline_runs ALTER COLUMN pruning_key SET NOT NULL;
 
-ALTER TABLE public.pipeline_runs ADD CONSTRAINT fk_pipeline_runs_job FOREIGN KEY (pruning_key) REFERENCES public.jobs(id);
+ALTER TABLE pipeline_runs ADD CONSTRAINT fk_pipeline_runs_job FOREIGN KEY (pruning_key) REFERENCES jobs(id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE public.jobs ADD COLUMN pipeline_spec_id INT;
+ALTER TABLE jobs ADD COLUMN pipeline_spec_id INT;
 
-UPDATE public.jobs
+UPDATE jobs
 SET pipeline_spec_id = jps.pipeline_spec_id
-FROM public.job_pipeline_specs jps
-WHERE jps.job_id = public.jobs.id
+FROM job_pipeline_specs jps
+WHERE jps.job_id = jobs.id
   AND jps.is_primary = TRUE;
 
-ALTER TABLE public.pipeline_runs DROP COLUMN pruning_key;
+ALTER TABLE pipeline_runs DROP COLUMN pruning_key;
 
-DROP INDEX IF EXISTS public.idx_unique_primary_per_job;
+DROP INDEX IF EXISTS idx_unique_primary_per_job;
 
-DROP TABLE IF EXISTS public.job_pipeline_specs; -- Do we use CASCADE here?
+DROP TABLE IF EXISTS job_pipeline_specs; -- Do we use CASCADE here?
 -- +goose StatementEnd
