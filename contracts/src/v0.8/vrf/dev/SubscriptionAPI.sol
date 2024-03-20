352f03a92efd4d6d7a3455862d7db18a5a9f23c7
jinhoonbang <jin.bang@smartcontract.com>
2024-03-20 12:46:16 -0700

    set solc version of VRF V2 Plus related contracts to 0.8.19 (#12479)
    * set solc version of VRF V2 Plus related contracts to 0.8.19
    
    * bump solc version in hardhat config for v2.5 coordinator
    
    * fix failing tests due to gas change
    
    * auto detect solc version for vrf contracts
    
    * upgrade mercury registry
    
    * add missing file
    
    * make generate

--- contracts/src/v0.8/vrf/dev/SubscriptionAPI.sol
+++ contracts/src/v0.8/vrf/dev/SubscriptionAPI.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import {EnumerableSet} from "../../vendor/openzeppelin-solidity/v4.7.3/contracts/utils/structs/EnumerableSet.sol";
 import {LinkTokenInterface} from "../../shared/interfaces/LinkTokenInterface.sol";
