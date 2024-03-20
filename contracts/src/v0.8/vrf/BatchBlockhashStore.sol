93762ccbd868b9e227abf3220afb9ad22ba41b92
jinhoonbang <jin.bang@smartcontract.com>
2024-03-20 12:46:16 -0700

    set solc version of VRF V2 Plus related contracts to 0.8.19 (#12479)
    * set solc version of VRF V2 Plus related contracts to 0.8.19
    
    * bump solc version in hardhat config for v2.5 coordinator
    
    * fix failing tests due to gas change
    
    * auto detect solc version for vrf contracts
    
    * upgrade mercury registry
    
    * add missing file
    
    * make generate

--- contracts/src/v0.8/vrf/BatchBlockhashStore.sol
+++ contracts/src/v0.8/vrf/BatchBlockhashStore.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // solhint-disable-next-line one-contract-per-file
-pragma solidity 0.8.6;
+pragma solidity 0.8.19;
 
 import {ChainSpecificUtil} from "../ChainSpecificUtil.sol";
 
