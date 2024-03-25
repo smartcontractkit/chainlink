ce8a3c7ad6ac97d4dcdf4d1fd45efe9b0947195a
amit-momin <amit.momin@smartcontract.com>
2024-03-25 16:34:07 -0500

    Removed custom zk counter method and simplified error handling

--- core/chains/evm/client/tx_simulator.go
+++ core/chains/evm/client/tx_simulator.go
@@ -2,17 +2,13 @@ package client
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink/v2/common/config"
-
-	commonclient "github.com/smartcontractkit/chainlink/v2/common/client"
 )
 
 const ErrOutOfCounters = "not enough counters to continue the execution"
@@ -23,20 +19,24 @@ type simulatorClient interface {
 
 // ZK chains can return an out-of-counters error
 // This method allows a caller to determine if a tx would fail due to OOC error by simulating the transaction
-// Used as an entry point for custom simulation across different chains
+// It will also return service unavailable or timeout errors for callers to react to retryable errors
+// Used as an entry point in case custom simulation is required across different chains
 func SimulateTransaction(ctx context.Context, client simulatorClient, lggr logger.SugaredLogger, chainType config.ChainType, msg ethereum.CallMsg) error {
-	var err error
-	switch chainType {
-	case config.ChainZkEvm:
-		err = simulateTransactionZkEvm(ctx, client, lggr, msg)
-	default:
-		err = simulateTransactionDefault(ctx, client, msg)
+	err := simulateTransactionDefault(ctx, client, msg)
+	sendErr := NewSendError(err)
+	if sendErr == nil {
+		return nil
 	}
-	// ClassifySendError will not have the proper fields for logging within the method due to the empty Transaction passed
-	code := ClassifySendError(err, lggr, &types.Transaction{}, msg.From, chainType.IsL2())
-	// Only return error if ZK OOC error is identified
-	if code == commonclient.OutOfCounters {
-		return errors.New(ErrOutOfCounters)
+	// Only return select errors to the caller
+	// Other errors are irrelevant to tx simulation to determin OOC error
+	if sendErr.IsOutOfCounters() {
+		return fmt.Errorf("%s: %w", ErrOutOfCounters, err)
+	}
+	if sendErr.IsServiceUnavailable() {
+		return fmt.Errorf("service not available when performing tx simulation: %w", err)
+	}
+	if sendErr.IsTimeout() {
+		return fmt.Errorf("timeout experienced when performing tx simulation: %w", err)
 	}
 	return nil
 }
@@ -44,67 +44,7 @@ func SimulateTransaction(ctx context.Context, client simulatorClient, lggr logge
 // eth_estimateGas returns out-of-counters (OOC) error if the transaction would result in an overflow
 func simulateTransactionDefault(ctx context.Context, client simulatorClient, msg ethereum.CallMsg) error {
 	var result hexutil.Big
-	errCall := client.CallContext(ctx, &result, "eth_estimateGas", toCallArg(msg), "pending")
-	jsonErr, _ := ExtractRPCError(errCall)
-	if jsonErr != nil && len(jsonErr.Message) > 0 {
-		return errors.New(jsonErr.Message)
-	}
-	return nil
-}
-
-type zkEvmEstimateCountResponse struct {
-	CountersUsed struct {
-		GasUsed              string
-		UsedKeccakHashes     string
-		UsedPoseidonHashes   string
-		UsedPoseidonPaddings string
-		UsedMemAligns        string
-		UsedArithmetics      string
-		UsedBinaries         string
-		UsedSteps            string
-		UsedSHA256Hashes     string
-	}
-	CountersLimit struct {
-		MaxGasUsed          string
-		MaxKeccakHashes     string
-		MaxPoseidonHashes   string
-		MaxPoseidonPaddings string
-		MaxMemAligns        string
-		MaxArithmetics      string
-		MaxBinaries         string
-		MaxSteps            string
-		MaxSHA256Hashes     string
-	}
-	OocError string
-}
-
-// zkEVM implemented a custom zkevm_estimateCounters method to detect if a transaction would result in an out-of-counters (OOC) error
-func simulateTransactionZkEvm(ctx context.Context, client simulatorClient, lggr logger.SugaredLogger, msg ethereum.CallMsg) error {
-	var result zkEvmEstimateCountResponse
-	err := client.CallContext(ctx, &result, "zkevm_estimateCounters", toCallArg(msg), "pending")
-	if err != nil {
-		return fmt.Errorf("failed to simulate tx: %w", err)
-	}
-	if detectZkEvmCounterOverflow(result) && len(result.OocError) > 0 {
-		lggr.Debugw("zkevm_estimateCounters returned", "result", result)
-		return errors.New(result.OocError)
-	}
-	return nil
-}
-
-// Helper method for zkEvm to determine if response indicates an overflow
-func detectZkEvmCounterOverflow(result zkEvmEstimateCountResponse) bool {
-	if result.CountersUsed.UsedKeccakHashes > result.CountersLimit.MaxKeccakHashes ||
-		result.CountersUsed.UsedPoseidonHashes > result.CountersLimit.MaxPoseidonHashes ||
-		result.CountersUsed.UsedPoseidonPaddings > result.CountersLimit.MaxPoseidonPaddings ||
-		result.CountersUsed.UsedMemAligns > result.CountersLimit.MaxMemAligns ||
-		result.CountersUsed.UsedArithmetics > result.CountersLimit.MaxArithmetics ||
-		result.CountersUsed.UsedBinaries > result.CountersLimit.MaxBinaries ||
-		result.CountersUsed.UsedSteps > result.CountersLimit.MaxSteps ||
-		result.CountersUsed.UsedSHA256Hashes > result.CountersLimit.MaxSHA256Hashes {
-		return true
-	}
-	return false
+	return client.CallContext(ctx, &result, "eth_estimateGas", toCallArg(msg), "pending")
 }
 
 func toCallArg(msg ethereum.CallMsg) interface{} {
--- core/chains/evm/client/tx_simulator_test.go
+++ core/chains/evm/client/tx_simulator_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 
-	"github.com/smartcontractkit/chainlink/v2/common/config"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
 	"github.com/smartcontractkit/chainlink/v2/core/internal/cltest"
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
@@ -95,7 +94,7 @@ func TestSimulateTx_Default(t *testing.T) {
 			case "eth_estimateGas":
 				resp.Error.Code = -32000
 				resp.Result = `"0x100"`
-				resp.Error.Message = "txpool is full"
+				resp.Error.Message = "something went wrong"
 			}
 			return
 		}).WSURL().String()
@@ -111,118 +110,5 @@ func TestSimulateTx_Default(t *testing.T) {
 		}
 		err = client.SimulateTransaction(ctx, ethClient, logger.TestSugared(t), "", msg)
 		require.NoError(t, err)
-	})
-}
-
-func TestSimulateTx_ZkEvm(t *testing.T) {
-	t.Parallel()
-
-	fromAddress := testutils.NewAddress()
-	toAddress := testutils.NewAddress()
-	ctx := testutils.Context(t)
-
-	t.Run("returns without error if simulation passes", func(t *testing.T) {
-		wsURL := testutils.NewWSServer(t, &cltest.FixtureChainID, func(method string, params gjson.Result) (resp testutils.JSONRPCResponse) {
-			switch method {
-			case "eth_subscribe":
-				resp.Result = `"0x00"`
-				resp.Notify = headResult
-				return
-			case "eth_unsubscribe":
-				resp.Result = "true"
-				return
-			case "zkevm_estimateCounters":
-				resp.Result = `{
-					"countersUsed": {
-						"gasUsed": "0x5360",
-						"usedKeccakHashes": "0x7",
-						"usedPoseidonHashes": "0x2bb",
-						"usedPoseidonPaddings": "0x4",
-						"usedMemAligns": "0x0",
-						"usedArithmetics": "0x263",
-						"usedBinaries": "0x40c",
-						"usedSteps": "0x3288",
-						"usedSHA256Hashes": "0x0"
-					},
-					"countersLimit": {
-						"maxGasUsed": "0x1c9c380",
-						"maxKeccakHashes": "0x861",
-						"maxPoseidonHashes": "0x3d9c5",
-						"maxPoseidonPaddings": "0x21017",
-						"maxMemAligns": "0x39c29",
-						"maxArithmetics": "0x39c29",
-						"maxBinaries": "0x73852",
-						"maxSteps": "0x73846a",
-						"maxSHA256Hashes": "0x63c"
-					}
-				}`
-			}
-			return
-		}).WSURL().String()
-
-		ethClient := mustNewChainClient(t, wsURL)
-		err := ethClient.Dial(ctx)
-		require.NoError(t, err)
-
-		msg := ethereum.CallMsg{
-			From: fromAddress,
-			To:   &toAddress,
-			Data: []byte("0x00"),
-		}
-		err = client.SimulateTransaction(ctx, ethClient, logger.TestSugared(t), config.ChainZkEvm, msg)
-		require.NoError(t, err)
-	})
-
-	t.Run("returns error if simulation returns zk out-of-counters error", func(t *testing.T) {
-		wsURL := testutils.NewWSServer(t, &cltest.FixtureChainID, func(method string, params gjson.Result) (resp testutils.JSONRPCResponse) {
-			switch method {
-			case "eth_subscribe":
-				resp.Result = `"0x00"`
-				resp.Notify = headResult
-				return
-			case "eth_unsubscribe":
-				resp.Result = "true"
-				return
-			case "zkevm_estimateCounters":
-				resp.Result = `{
-					"countersUsed": {
-						"gasUsed": "0x12f3bd",
-						"usedKeccakHashes": "0x8d3",
-						"usedPoseidonHashes": "0x222",
-						"usedPoseidonPaddings": "0x16",
-						"usedMemAligns": "0x1a69",
-						"usedArithmetics": "0x2619",
-						"usedBinaries": "0x2d738",
-						"usedSteps": "0x72e223",
-						"usedSHA256Hashes": "0x0"
-					},
-					"countersLimit": {
-						"maxGasUsed": "0x1c9c380",
-						"maxKeccakHashes": "0x861",
-						"maxPoseidonHashes": "0x3d9c5",
-						"maxPoseidonPaddings": "0x21017",
-						"maxMemAligns": "0x39c29",
-						"maxArithmetics": "0x39c29",
-						"maxBinaries": "0x73852",
-						"maxSteps": "0x73846a",
-						"maxSHA256Hashes": "0x63c"
-					},
-					"oocError": "not enough keccak counters to continue the execution"
-				}`
-			}
-			return
-		}).WSURL().String()
-
-		ethClient := mustNewChainClient(t, wsURL)
-		err := ethClient.Dial(ctx)
-		require.NoError(t, err)
-
-		msg := ethereum.CallMsg{
-			From: fromAddress,
-			To:   &toAddress,
-			Data: []byte("0x00"),
-		}
-		err = client.SimulateTransaction(ctx, ethClient, logger.TestSugared(t), config.ChainZkEvm, msg)
-		require.Error(t, err, client.ErrOutOfCounters)
 	})
 }
