1b07ecfded30cd96118ed23d8f87a312bcf78578
anirudhwarrier <12178754+anirudhwarrier@users.noreply.github.com>
2024-04-04 16:07:35 +0400

    update testconfig to read more config

--- integration-tests/testconfig/automation/config.go
+++ integration-tests/testconfig/automation/config.go
@@ -3,12 +3,14 @@ package automation
 import (
 	"errors"
 	"math/big"
+	"time"
 )
 
 type Config struct {
-	General     *General     `toml:"General"`
-	Load        []Load       `toml:"Load"`
-	DataStreams *DataStreams `toml:"DataStreams"`
+	General          *General          `toml:"General"`
+	Load             []Load            `toml:"Load"`
+	DataStreams      *DataStreams      `toml:"DataStreams"`
+	AutomationConfig *AutomationConfig `toml:"AutomationConfig"`
 }
 
 func (c *Config) Validate() error {
@@ -26,6 +28,12 @@ func (c *Config) Validate() error {
 	}
 	if c.DataStreams != nil {
 		if err := c.DataStreams.Validate(); err != nil {
+			return err
+		}
+	}
+
+	if c.AutomationConfig != nil {
+		if err := c.AutomationConfig.Validate(); err != nil {
 			return err
 		}
 	}
@@ -137,6 +145,200 @@ func (c *DataStreams) Validate() error {
 	} else {
 		c.Enabled = new(bool)
 		*c.Enabled = false
+	}
+	return nil
+}
+
+type AutomationConfig struct {
+	PluginConfig     *PluginConfig     `toml:"PluginConfig"`
+	PublicConfig     *PublicConfig     `toml:"PublicConfig"`
+	RegistrySettings *RegistrySettings `toml:"RegistrySettings"`
+	UseLogBufferV1   *bool             `toml:"use_log_buffer_v1"`
+}
+
+func (c *AutomationConfig) Validate() error {
+	if err := c.PluginConfig.Validate(); err != nil {
+		return err
+	}
+	if err := c.PublicConfig.Validate(); err != nil {
+		return err
+	}
+	if err := c.RegistrySettings.Validate(); err != nil {
+		return err
+	}
+	if c.UseLogBufferV1 == nil {
+		return errors.New("use_log_buffer_v1 must be set")
+	}
+	return nil
+}
+
+type PluginConfig struct {
+	PerformLockoutWindow *int64             `toml:"perform_lockout_window"`
+	TargetProbability    *string            `toml:"target_probability"`
+	TargetInRounds       *int               `toml:"target_in_rounds"`
+	MinConfirmations     *int               `toml:"min_confirmations"`
+	GasLimitPerReport    *uint32            `toml:"gas_limit_per_report"`
+	GasOverheadPerUpkeep *uint32            `toml:"gas_overhead_per_upkeep"`
+	MaxUpkeepBatchSize   *int               `toml:"max_upkeep_batch_size"`
+	LogProviderConfig    *LogProviderConfig `toml:"LogProviderConfig"`
+}
+
+type LogProviderConfig struct {
+	BlockRate *uint32 `toml:"block_rate"`
+	LogLimit  *uint32 `toml:"log_limit"`
+}
+
+func (c *PluginConfig) Validate() error {
+	if err := c.LogProviderConfig.Validate(); err != nil {
+		return err
+	}
+	if *c.PerformLockoutWindow < 0 || c.PerformLockoutWindow == nil {
+		return errors.New("perform_lockout_window must be set to a non-negative integer")
+	}
+	if *c.TargetProbability == "" || c.TargetProbability == nil {
+		return errors.New("target_probability must be set")
+	}
+	if *c.TargetInRounds < 1 || c.TargetInRounds == nil {
+		return errors.New("target_in_rounds must be set to a positive integer")
+	}
+	if *c.MinConfirmations < 0 || c.MinConfirmations == nil {
+		return errors.New("min_confirmations must be set to a non-negative integer")
+	}
+	if *c.GasLimitPerReport < 1 || c.GasLimitPerReport == nil {
+		return errors.New("gas_limit_per_report must be set to a positive integer")
+	}
+	if *c.GasOverheadPerUpkeep < 1 || c.GasOverheadPerUpkeep == nil {
+		return errors.New("gas_overhead_per_upkeep must be set to a positive integer")
+	}
+	if *c.MaxUpkeepBatchSize < 1 || c.MaxUpkeepBatchSize == nil {
+		return errors.New("max_upkeep_batch_size must be set to a positive integer")
+	}
+	return nil
+
+}
+
+func (c *LogProviderConfig) Validate() error {
+	if *c.BlockRate < 1 || c.BlockRate == nil {
+		return errors.New("block_rate must be set to a positive integer")
+	}
+	if *c.LogLimit < 1 || c.LogLimit == nil {
+		return errors.New("log_limit must be set to a positive integer")
+	}
+	return nil
+
+}
+
+type PublicConfig struct {
+	DeltaProgress                           *time.Duration `toml:"delta_progress"`
+	DeltaResend                             *time.Duration `toml:"delta_resend"`
+	DeltaInitial                            *time.Duration `toml:"delta_initial"`
+	DeltaRound                              *time.Duration `toml:"delta_round"`
+	DeltaGrace                              *time.Duration `toml:"delta_grace"`
+	DeltaCertifiedCommitRequest             *time.Duration `toml:"delta_certified_commit_request"`
+	DeltaStage                              *time.Duration `toml:"delta_stage"`
+	RMax                                    *uint64        `toml:"r_max"`
+	F                                       *int           `toml:"f"`
+	MaxDurationQuery                        *time.Duration `toml:"max_duration_query"`
+	MaxDurationObservation                  *time.Duration `toml:"max_duration_observation"`
+	MaxDurationShouldAcceptAttestedReport   *time.Duration `toml:"max_duration_should_accept_attested_report"`
+	MaxDurationShouldTransmitAcceptedReport *time.Duration `toml:"max_duration_should_transmit_accepted_report"`
+}
+
+func (c *PublicConfig) Validate() error {
+	if *c.DeltaProgress < 0 || c.DeltaProgress == nil {
+		return errors.New("delta_progress must be set to a non-negative duration")
+	}
+	if *c.DeltaResend < 0 || c.DeltaResend == nil {
+		return errors.New("delta_resend must be set to a non-negative duration")
+	}
+	if *c.DeltaInitial < 0 || c.DeltaInitial == nil {
+		return errors.New("delta_initial must be set to a non-negative duration")
+	}
+	if *c.DeltaRound < 0 || c.DeltaRound == nil {
+		return errors.New("delta_round must be set to a non-negative duration")
+	}
+	if *c.DeltaGrace < 0 || c.DeltaGrace == nil {
+		return errors.New("delta_grace must be set to a non-negative duration")
+	}
+	if *c.DeltaCertifiedCommitRequest < 0 || c.DeltaCertifiedCommitRequest == nil {
+		return errors.New("delta_certified_commit_request must be set to a non-negative duration")
+	}
+	if *c.DeltaStage < 0 || c.DeltaStage == nil {
+		return errors.New("delta_stage must be set to a non-negative duration")
+	}
+	if *c.RMax < 1 || c.RMax == nil {
+		return errors.New("r_max must be set to a positive integer")
+	}
+	if *c.F < 1 || c.F == nil {
+		return errors.New("f must be set to a positive integer")
+	}
+	if *c.MaxDurationQuery < 0 || c.MaxDurationQuery == nil {
+		return errors.New("max_duration_query must be set to a non-negative duration")
+	}
+	if *c.MaxDurationObservation < 0 || c.MaxDurationObservation == nil {
+		return errors.New("max_duration_observation must be set to a non-negative duration")
+	}
+	if *c.MaxDurationShouldAcceptAttestedReport < 0 || c.MaxDurationShouldAcceptAttestedReport == nil {
+		return errors.New("max_duration_should_accept_attested_report must be set to a non-negative duration")
+	}
+	if *c.MaxDurationShouldTransmitAcceptedReport < 0 || c.MaxDurationShouldTransmitAcceptedReport == nil {
+		return errors.New("max_duration_should_transmit_accepted_report must be set to a non-negative duration")
+	}
+	return nil
+
+}
+
+type RegistrySettings struct {
+	PaymentPremiumPPB    *uint32  `toml:"payment_premium_ppb"`
+	FlatFeeMicroLINK     *uint32  `toml:"flat_fee_micro_link"`
+	CheckGasLimit        *uint32  `toml:"check_gas_limit"`
+	StalenessSeconds     *big.Int `toml:"staleness_seconds"`
+	GasCeilingMultiplier *uint16  `toml:"gas_ceiling_multiplier"`
+	MaxPerformGas        *uint32  `toml:"max_perform_gas"`
+	MinUpkeepSpend       *big.Int `toml:"min_upkeep_spend"`
+	FallbackGasPrice     *big.Int `toml:"fallback_gas_price"`
+	FallbackLinkPrice    *big.Int `toml:"fallback_link_price"`
+	MaxCheckDataSize     *uint32  `toml:"max_check_data_size"`
+	MaxPerformDataSize   *uint32  `toml:"max_perform_data_size"`
+	MaxRevertDataSize    *uint32  `toml:"max_revert_data_size"`
+}
+
+func (c *RegistrySettings) Validate() error {
+	if c.PaymentPremiumPPB == nil {
+		return errors.New("payment_premium_ppb must be set to a non-negative integer")
+	}
+	if c.FlatFeeMicroLINK == nil {
+		return errors.New("flat_fee_micro_link must be set to a non-negative integer")
+	}
+	if *c.CheckGasLimit < 1 || c.CheckGasLimit == nil {
+		return errors.New("check_gas_limit must be set to a positive integer")
+	}
+	if c.StalenessSeconds == nil || c.StalenessSeconds.Cmp(big.NewInt(0)) < 0 {
+		return errors.New("staleness_seconds must be set to a non-negative integer")
+	}
+	if c.GasCeilingMultiplier == nil {
+		return errors.New("gas_ceiling_multiplier must be set to a non-negative integer")
+	}
+	if *c.MaxPerformGas < 1 || c.MaxPerformGas == nil {
+		return errors.New("max_perform_gas must be set to a positive integer")
+	}
+	if c.MinUpkeepSpend == nil || c.MinUpkeepSpend.Cmp(big.NewInt(0)) < 0 {
+		return errors.New("min_upkeep_spend must be set to a non-negative integer")
+	}
+	if c.FallbackGasPrice == nil || c.FallbackGasPrice.Cmp(big.NewInt(0)) < 0 {
+		return errors.New("fallback_gas_price must be set to a non-negative integer")
+	}
+	if c.FallbackLinkPrice == nil || c.FallbackLinkPrice.Cmp(big.NewInt(0)) < 0 {
+		return errors.New("fallback_link_price must be set to a non-negative integer")
+	}
+	if *c.MaxCheckDataSize < 1 || c.MaxCheckDataSize == nil {
+		return errors.New("max_check_data_size must be set to a positive integer")
+	}
+	if *c.MaxPerformDataSize < 1 || c.MaxPerformDataSize == nil {
+		return errors.New("max_perform_data_size must be set to a positive integer")
+	}
+	if *c.MaxRevertDataSize < 1 || c.MaxRevertDataSize == nil {
+		return errors.New("max_revert_data_size must be set to a positive integer")
 	}
 	return nil
 }
