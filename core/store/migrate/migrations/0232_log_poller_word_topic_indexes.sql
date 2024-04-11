9f60f8091297194cddce4eb7b5128af2f1b72f1d
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-04-11 14:16:04 +0200

    Adding evm_chain_id, address and event_sig to data and topic indexes

---
+++ core/store/migrate/migrations/0232_log_poller_word_topic_indexes.sql
@@ -0,0 +1,53 @@
+-- +goose Up
+
+drop index if exists evm.evm_logs_idx_data_word_one;
+drop index if exists evm.evm_logs_idx_data_word_two;
+drop index if exists evm.evm_logs_idx_data_word_three;
+drop index if exists evm.evm_logs_idx_data_word_four;
+drop index if exists evm.evm_logs_idx_topic_two;
+drop index if exists evm.evm_logs_idx_topic_three;
+drop index if exists evm.evm_logs_idx_topic_four;
+
+create index evm_logs_idx_data_word_three
+    on evm.logs (evm_chain_id, address, event_sig, "substring"(data, 65, 32));
+
+create index evm_logs_idx_data_word_five
+    on evm.logs (evm_chain_id, address, event_sig, "substring"(data, 129, 32));
+
+create index evm_logs_idx_topic_two
+    on evm.logs (evm_chain_id, address, event_sig, (topics[2]));
+
+create index evm_logs_idx_topic_three
+    on evm.logs (evm_chain_id, address, event_sig, (topics[3]));
+
+create index evm_logs_idx_topic_four
+    on evm.logs (evm_chain_id, address, event_sig, (topics[4]));
+
+-- +goose Down
+
+drop index if exists evm.evm_logs_idx_data_word_three;
+drop index if exists evm.evm_logs_idx_data_word_five;
+drop index if exists evm.evm_logs_idx_topic_two;
+drop index if exists evm.evm_logs_idx_topic_three;
+drop index if exists evm.evm_logs_idx_topic_four;
+
+create index evm_logs_idx_data_word_one
+    on evm.logs ("substring"(data, 1, 32));
+
+create index evm_logs_idx_data_word_two
+    on evm.logs ("substring"(data, 33, 32));
+
+create index evm_logs_idx_data_word_three
+    on evm.logs ("substring"(data, 65, 32));
+
+create index evm_logs_idx_data_word_four
+    on evm.logs ("substring"(data, 97, 32));
+
+create index evm_logs_idx_topic_two
+    on evm.logs ((topics[2]));
+
+create index evm_logs_idx_topic_three
+    on evm.logs ((topics[3]));
+
+create index evm_logs_idx_topic_four
+    on evm.logs ((topics[4]));
