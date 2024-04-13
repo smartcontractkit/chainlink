33be896cdfbbd948354af1574ac91280a625404e
Jordan Krage <jmank88@gmail.com>
2024-04-13 09:46:47 -0500

    core/services/keystore: switch to sqlutil.DataStore

--- core/chains/evm/log/helpers_test.go
+++ core/chains/evm/log/helpers_test.go
@@ -94,7 +94,7 @@ func newBroadcasterHelperWithEthClient(t *testing.T, ethClient evmclient.Client,
 	db := pgtest.NewSqlxDB(t)
 	orm := log.NewORM(db, cltest.FixtureChainID)
 	lb := log.NewTestBroadcaster(orm, ethClient, config.EVM(), lggr, highestSeenHead, mailMon)
-	kst := cltest.NewKeyStore(t, db, globalConfig.Database())
+	kst := cltest.NewKeyStore(t, db)
 
 	cc := evmtest.NewChainRelayExtenders(t, evmtest.TestChainOpts{
 		Client:         ethClient,
