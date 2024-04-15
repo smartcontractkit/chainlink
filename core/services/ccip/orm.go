35d720eb539a16ae2bc9de9a029fb7bc4bfe5256
Matt Yang <matt.yang@smartcontract.com>
2024-04-15 02:35:53 -0400

    POC orm

--- core/services/ccip/orm.go
+++ core/services/ccip/orm.go
@@ -3,130 +3,176 @@ package ccip
 import (
 	"fmt"
 	"math/big"
+	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/lib/pq"
-	"github.com/pkg/errors"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/ccip"
 
 	"github.com/jmoiron/sqlx"
 
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/functions/generated/functions_router"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/pg"
 )
 
-//go:generate mockery --quiet --name ORM --output ./mocks/ --case=underscore
+type GasPrice struct {
+	SourceChainSelector uint64
+	GasPrice            *big.Int
+	CreatedAt           time.Time
+}
 
+type TokenPrice struct {
+	TokenAddr  ccip.Address
+	TokenPrice *big.Int
+	CreatedAt  time.Time
+}
+
+//go:generate mockery --quiet --name ORM --output ./mocks/ --case=underscore
 type ORM interface {
-	GetSubscriptions(offset, limit uint, qopts ...pg.QOpt) ([]CachedSubscription, error)
-	UpsertSubscription(subscription CachedSubscription, qopts ...pg.QOpt) error
+	GetGasPricesByDestChain(destChainSelector uint64, qopts ...pg.QOpt) ([]GasPrice, error)
+	GetTokenPricesByDestChain(destChainSelector uint64, qopts ...pg.QOpt) ([]TokenPrice, error)
+
+	InsertGasPricesForDestChain(destChainSelector uint64, jobId int32, gasPrices []GasPrice, qopts ...pg.QOpt) error
+	InsertTokenPricesForDestChain(destChainSelector uint64, jobId int32, tokenPrices []TokenPrice, qopts ...pg.QOpt) error
+
+	ClearGasPricesByDestChain(destChainSelector uint64, to time.Time, qopts ...pg.QOpt) error
+	ClearTokenPricesByDestChain(destChainSelector uint64, to time.Time, qopts ...pg.QOpt) error
 }
 
 type orm struct {
-	q                     pg.Q
-	routerContractAddress common.Address
+	q    pg.Q
+	lggr logger.Logger
 }
 
 var _ ORM = (*orm)(nil)
-var (
-	ErrInvalidParameters = errors.New("invalid parameters provided to create a subscription cache ORM")
-)
 
 const (
-	tableName = "functions_subscriptions"
+	gasTableName   = "ccip.observed_gas_prices"
+	tokenTableName = "ccip.observed_token_prices"
 )
 
-type cachedSubscriptionRow struct {
-	SubscriptionID        uint64
-	Owner                 common.Address
-	Balance               int64
-	BlockedBalance        int64
-	ProposedOwner         common.Address
-	Consumers             pq.ByteaArray
-	Flags                 []uint8
-	RouterContractAddress common.Address
-}
-
-func NewORM(db *sqlx.DB, lggr logger.Logger, cfg pg.QConfig, routerContractAddress common.Address) (ORM, error) {
-	if db == nil || cfg == nil || lggr == nil || routerContractAddress == (common.Address{}) {
-		return nil, ErrInvalidParameters
+func NewORM(db *sqlx.DB, lggr logger.Logger, cfg pg.QConfig) (ORM, error) {
+	if db == nil || lggr == nil || cfg == nil {
+		return nil, fmt.Errorf("params to CCIP NewORM cannot be nil")
 	}
 
+	namedLogger := lggr.Named("CCIP_ORM")
+
 	return &orm{
-		q:                     pg.NewQ(db, lggr, cfg),
-		routerContractAddress: routerContractAddress,
+		q:    pg.NewQ(db, namedLogger, cfg),
+		lggr: namedLogger,
 	}, nil
 }
 
-func (o *orm) GetSubscriptions(offset, limit uint, qopts ...pg.QOpt) ([]CachedSubscription, error) {
-	var cacheSubscriptions []CachedSubscription
-	var cacheSubscriptionRows []cachedSubscriptionRow
+func (o *orm) GetGasPricesByDestChain(destChainSelector uint64, qopts ...pg.QOpt) ([]GasPrice, error) {
+	var gasPrices []GasPrice
 	stmt := fmt.Sprintf(`
-		SELECT subscription_id, owner, balance, blocked_balance, proposed_owner, consumers, flags, router_contract_address
+		SELECT DISTINCT ON (source_chain_selector)
+		source_chain_selector, gas_price, created_at
 		FROM %s
-		WHERE router_contract_address = $1
-		ORDER BY subscription_id ASC
-		OFFSET $2
-		LIMIT $3;
-	`, tableName)
-	err := o.q.WithOpts(qopts...).Select(&cacheSubscriptionRows, stmt, o.routerContractAddress, offset, limit)
+		WHERE chain_selector = $1
+		ORDER BY source_chain_selector, created_at DESC;
+	`, gasTableName)
+	err := o.q.WithOpts(qopts...).Select(&gasPrices, stmt, destChainSelector)
 	if err != nil {
-		return cacheSubscriptions, err
+		return nil, err
 	}
 
-	for _, cs := range cacheSubscriptionRows {
-		cacheSubscriptions = append(cacheSubscriptions, cs.encode())
-	}
-
-	return cacheSubscriptions, nil
+	return gasPrices, nil
 }
 
-// UpsertSubscription will update if a subscription exists or create if it does not.
-// In case a subscription gets deleted we will update it with an owner address equal to 0x0.
-func (o *orm) UpsertSubscription(subscription CachedSubscription, qopts ...pg.QOpt) error {
+func (o *orm) GetTokenPricesByDestChain(destChainSelector uint64, qopts ...pg.QOpt) ([]TokenPrice, error) {
+	var tokenPrices []TokenPrice
 	stmt := fmt.Sprintf(`
-		INSERT INTO %s (subscription_id, owner, balance, blocked_balance, proposed_owner, consumers, flags, router_contract_address)
-		VALUES ($1,$2,$3,$4,$5,$6,$7,$8) ON CONFLICT (subscription_id, router_contract_address) DO UPDATE
-		SET owner=$2, balance=$3, blocked_balance=$4, proposed_owner=$5, consumers=$6, flags=$7, router_contract_address=$8;`, tableName)
+		SELECT DISTINCT ON (token_addr)
+		token_addr, token_price, created_at
+		FROM %s
+		WHERE chain_selector = $1
+		ORDER BY token_addr, created_at DESC;
 
-	if subscription.Balance == nil {
-		subscription.Balance = big.NewInt(0)
+	`, tokenTableName)
+	err := o.q.WithOpts(qopts...).Select(&tokenPrices, stmt, destChainSelector)
+	if err != nil {
+		return nil, err
 	}
 
-	if subscription.BlockedBalance == nil {
-		subscription.BlockedBalance = big.NewInt(0)
+	return tokenPrices, nil
+}
+
+func (o *orm) InsertGasPricesForDestChain(destChainSelector uint64, jobId int32, gasPrices []GasPrice, qopts ...pg.QOpt) error {
+	if len(gasPrices) == 0 {
+		return nil
 	}
+
+	sqlStr := ""
+	var values []interface{}
+	for _, price := range gasPrices {
+		sqlStr += "($1,$2,$3,$4,NOW()),"
+		values = append(values, destChainSelector, jobId, price.SourceChainSelector, price.GasPrice)
+	}
+	// Trim the last comma
+	sqlStr = sqlStr[0 : len(sqlStr)-1]
+
+	stmt := fmt.Sprintf(`
+		INSERT INTO %s (chain_selector, job_id, source_chain_selector, gas_price, created_at)
+		VALUES %s;`,
+		gasTableName, sqlStr)
 
 	_, err := o.q.WithOpts(qopts...).Exec(
 		stmt,
-		subscription.SubscriptionID,
-		subscription.Owner,
-		subscription.Balance.Int64(),
-		subscription.BlockedBalance.Int64(),
-		subscription.ProposedOwner,
-		subscription.Consumers,
-		subscription.Flags[:],
-		o.routerContractAddress,
+		values...,
 	)
-
+	if err != nil {
+		o.lggr.Errorf("Error inserting gas prices for job %d: %v", jobId, err)
+	}
 	return err
 }
 
-func (cs *cachedSubscriptionRow) encode() CachedSubscription {
-	consumers := make([]common.Address, 0)
-	for _, csc := range cs.Consumers {
-		consumers = append(consumers, common.BytesToAddress(csc))
+func (o *orm) InsertTokenPricesForDestChain(destChainSelector uint64, jobId int32, tokenPrices []TokenPrice, qopts ...pg.QOpt) error {
+	if len(tokenPrices) == 0 {
+		return nil
 	}
 
-	return CachedSubscription{
-		SubscriptionID: cs.SubscriptionID,
-		IFunctionsSubscriptionsSubscription: functions_router.IFunctionsSubscriptionsSubscription{
-			Balance:        big.NewInt(cs.Balance),
-			Owner:          cs.Owner,
-			BlockedBalance: big.NewInt(cs.BlockedBalance),
-			ProposedOwner:  cs.ProposedOwner,
-			Consumers:      consumers,
-			Flags:          [32]byte(cs.Flags),
-		},
+	sqlStr := ""
+	var values []interface{}
+	for _, price := range tokenPrices {
+		sqlStr += "($1,$2,$3,$4,NOW()),"
+		values = append(values, destChainSelector, jobId, price.TokenAddr, price.TokenPrice)
 	}
+	// Trim the last comma
+	sqlStr = sqlStr[0 : len(sqlStr)-1]
+
+	stmt := fmt.Sprintf(`
+		INSERT INTO %s (chain_selector, job_id, token_addr, token_price, created_at)
+		VALUES %s;`,
+		tokenTableName, sqlStr)
+
+	_, err := o.q.WithOpts(qopts...).Exec(
+		stmt,
+		values...,
+	)
+	if err != nil {
+		o.lggr.Errorf("Error inserting token prices for job %d: %v", jobId, err)
+	}
+	return err
+}
+
+func (o *orm) ClearGasPricesByDestChain(destChainSelector uint64, to time.Time, qopts ...pg.QOpt) error {
+	stmt := fmt.Sprintf(`DELETE FROM %s WHERE chain_selector = $1 AND created_at < $2`, gasTableName)
+
+	_, err := o.q.WithOpts(qopts...).Exec(
+		stmt,
+		destChainSelector,
+		to,
+	)
+	return err
+}
+
+func (o *orm) ClearTokenPricesByDestChain(destChainSelector uint64, to time.Time, qopts ...pg.QOpt) error {
+	stmt := fmt.Sprintf(`DELETE FROM %s WHERE chain_selector = $1 AND created_at < $2`, tokenTableName)
+
+	_, err := o.q.WithOpts(qopts...).Exec(
+		stmt,
+		destChainSelector,
+		to,
+	)
+	return err
 }
--- core/store/migrate/migrations/0232_ccip_prices_cache.sql
+++ core/store/migrate/migrations/0232_ccip_prices_cache.sql
@@ -1,29 +1,31 @@
 -- +goose Up
-
+-- +goose StatementBegin
 CREATE SCHEMA ccip;
 
 CREATE TABLE ccip.observed_gas_prices(
     chain_selector NUMERIC(20,0) NOT NULL,
     job_id INTEGER NOT NULL,
     source_chain_selector NUMERIC(20,0) NOT NULL,
-    value NUMERIC(78,0) NOT NULL,
+    gas_price NUMERIC(78,0) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
 
 CREATE TABLE ccip.observed_token_prices(
      chain_selector NUMERIC(20,0) NOT NULL,
      job_id INTEGER NOT NULL,
-     token_addr BYTEA CHECK (octet_length(token_addr) = 20) NOT NULL,
-     value NUMERIC(78,0) NOT NULL,
+     token_addr BYTEA NOT NULL,
+     token_price NUMERIC(78,0) NOT NULL,
      created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
 
-CREATE INDEX idx_ccip_gas_prices_chain_value_timestamp ON ccip.observed_gas_prices (chain_selector, source_chain_selector, created_at DESC);
-CREATE INDEX idx_ccip_token_prices_token_value_timestamp ON ccip.observed_token_prices (chain_selector, token_addr, created_at DESC);
+CREATE INDEX idx_ccip_gas_prices_chain_gas_price_timestamp ON ccip.observed_gas_prices (chain_selector, source_chain_selector, created_at DESC);
+CREATE INDEX idx_ccip_token_prices_token_price_timestamp ON ccip.observed_token_prices (chain_selector, token_addr, created_at DESC);
+
+-- +goose StatementEnd
 
 
 -- +goose Down
-
+-- +goose StatementBegin
 DROP INDEX IF EXISTS idx_ccip_token_prices_token_value;
 DROP INDEX IF EXISTS idx_ccip_gas_prices_chain_value;
 
@@ -31,3 +33,4 @@ DROP TABLE "ccip".observed_token_prices;
 DROP TABLE "ccip".observed_gas_prices;
 
 DROP SCHEMA "ccip";
+-- +goose StatementEnd
