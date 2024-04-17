34c201c2974ca161cb9a17939cd891ba09d2f464
Clement Erena <clement.erena@smartcontract.com>
2024-04-17 16:58:27 +0200

    feat(dashboard): core_components cleanup code

--- dashboard-lib/core-node-components/component.go
+++ dashboard-lib/core-node-components/component.go
@@ -22,11 +22,11 @@ func vars(p Props) []dashboard.Option {
 		dashboard.VariableAsInterval(
 			"interval",
 			interval.Values([]string{"30s", "1m", "5m", "15m", "30m", "1h", "6h", "12h"}),
+			interval.Default("15m"),
 		),
 		dashboard.VariableAsQuery(
 			"env",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
 			query.Request("label_values(up, env)"),
 			query.Sort(query.NumericalAsc),
@@ -34,49 +34,43 @@ func vars(p Props) []dashboard.Option {
 		dashboard.VariableAsQuery(
 			"cluster",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\"}, cluster)"),
+			query.Request(`label_values(up{env="$env"}, cluster)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
 			"blockchain",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\", cluster=\"$cluster\"}, blockchain)"),
+			query.Request(`label_values(up{env="$env", cluster="$cluster"}, blockchain)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
 			"product",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\", cluster=\"$cluster\", blockchain=\"$blockchain\"}, product)"),
+			query.Request(`label_values(up{env="$env", cluster="$cluster", blockchain="$blockchain"}, product)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
 			"network_type",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\", cluster=\"$cluster\", blockchain=\"$blockchain\", product=\"$product\"}, network_type)"),
+			query.Request(`label_values(up{env="$env", cluster="$cluster", blockchain="$blockchain", product="$product"}, network_type)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
 			"component",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\", cluster=\"$cluster\", blockchain=\"$blockchain\", network_type=\"$network_type\"}, component)"),
+			query.Request(`label_values(up{env="$env", cluster="$cluster", blockchain="$blockchain", network_type="$network_type"}, component)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
 			"service",
 			query.DataSource(p.PrometheusDataSource),
-			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(up{env=\"$env\", cluster=\"$cluster\", blockchain=\"$blockchain\", network_type=\"$network_type\", component=\"$component\"}, service)"),
+			query.Request(`label_values(up{env="$env", cluster="$cluster", blockchain="$blockchain", network_type="$network_type", component="$component"}, service)`),
 			query.Sort(query.NumericalAsc),
 		),
 		dashboard.VariableAsQuery(
@@ -84,7 +78,7 @@ func vars(p Props) []dashboard.Option {
 			query.DataSource(p.PrometheusDataSource),
 			query.Multiple(),
 			query.IncludeAll(),
-			query.Request("label_values(health{env=\"$env\", cluster=\"$cluster\", blockchain=\"$blockchain\", network_type=\"$network_type\", component=\"$component\", service=\"$service\"}, service_id)"),
+			query.Request(`label_values(health{env="$env", cluster="$cluster", blockchain="$blockchain", network_type="$network_type", component="$component", service="$service"}, service_id)`),
 			query.Sort(query.NumericalAsc),
 		),
 	}
@@ -94,7 +88,6 @@ func generalInfoRow(p Props) []dashboard.Option {
 	return []dashboard.Option{
 		dashboard.Row(
 			"General CL Cluster Info",
-			// row.Collapse(),
 			row.WithTable(
 				"List Nodes",
 				table.Span(12),
@@ -121,7 +114,8 @@ func generalInfoRow(p Props) []dashboard.Option {
 				),
 				timeseries.WithPrometheusTarget(
 					`up{`+p.PlatformOpts.LabelQuery+`}`,
-					prometheus.Legend(""),
+					//prometheus.Legend(""),
+					prometheus.Legend("Team: {{team}} env: {{env}} cluster: {{cluster}} namespace: {{namespace}} job: {{job}} blockchain: {{blockchain}} product: {{product}} networkType: {{network_type}} component: {{component}} service: {{service}}"),
 				),
 			),
 			row.WithTimeSeries(
@@ -154,12 +148,17 @@ func generalInfoRow(p Props) []dashboard.Option {
 				stat.Text(stat.TextValueAndName),
 				stat.Orientation(stat.OrientationVertical),
 				stat.SparkLine(),
-				stat.TitleFontSize(4),
+				stat.TitleFontSize(8),
 				stat.ValueFontSize(12),
 				stat.WithPrometheusTarget(
 					`avg(avg_over_time(health{`+p.PlatformOpts.LabelQuery+`service_id=~"${service_id}"}[$interval])) by (service_id, version, service, cluster, env)`,
 					prometheus.Legend("{{service_id}}"),
 				),
+				stat.AbsoluteThresholds([]stat.ThresholdStep{
+					{Color: "#FF0000", Value: float64Ptr(0.0)},
+					{Color: "#FF0000", Value: float64Ptr(0.8)},
+					{Color: "#008000", Value: float64Ptr(1.0)},
+				}),
 			),
 		),
 	}
@@ -169,4 +168,8 @@ func New(p Props) []dashboard.Option {
 	opts := vars(p)
 	opts = append(opts, generalInfoRow(p)...)
 	return opts
+}
+
+func float64Ptr(input float64) *float64 {
+	return &input
 }
--- dashboard-lib/core-node-components/platform.go
+++ dashboard-lib/core-node-components/platform.go
@@ -1,7 +1,5 @@
 package core_node_components
 
-import "fmt"
-
 type PlatformOpts struct {
 	// Platform is infrastructure deployment platform: docker or k8s
 	Platform     string
@@ -12,18 +10,9 @@ type PlatformOpts struct {
 }
 
 // PlatformPanelOpts generate different queries for "docker" and "k8s" deployment platforms
-func PlatformPanelOpts(platform string) PlatformOpts {
+func PlatformPanelOpts() PlatformOpts {
 	po := PlatformOpts{
 		LabelFilters: map[string]string{
-			"instance": `=~"${instance}"`,
-			"commit":   `=~"${commit:pipe}"`,
-		},
-	}
-	switch platform {
-	case "kubernetes":
-		po.LabelFilters = map[string]string{
-			// "namespace":  `=~"${namespace}"`,
-			// "pod":        `=~"${pod}"`,
 			"env":          `=~"${env}"`,
 			"cluster":      `=~"${cluster}"`,
 			"blockchain":   `=~"${blockchain}"`,
@@ -31,19 +20,7 @@ func PlatformPanelOpts(platform string) PlatformOpts {
 			"network_type": `=~"${network_type}"`,
 			"component":    `=~"${component}"`,
 			"service":      `=~"${service}"`,
-		}
-		po.LabelFilter = "job"
-		po.LegendString = "pod"
-		break
-	case "docker":
-		po.LabelFilters = map[string]string{
-			"instance": `=~"${instance}"`,
-		}
-		po.LabelFilter = "instance"
-		po.LegendString = "instance"
-		break
-	default:
-		panic(fmt.Sprintf("failed to generate Platform dependent queries, unknown platform: %s", platform))
+		},
 	}
 	for key, value := range po.LabelFilters {
 		po.LabelQuery += key + value + ", "
