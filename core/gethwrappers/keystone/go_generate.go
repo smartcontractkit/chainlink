db5bf1e7f1c63372e59f7c6a24c3093e1e94665a
DeividasK <deividas.karzinauskas@gmail.com>
2024-04-18 14:43:01 +0300

    Update compile script to work with Foundry

--- core/gethwrappers/keystone/go_generate.go
+++ core/gethwrappers/keystone/go_generate.go
@@ -4,5 +4,5 @@ package gethwrappers
 
 // Keystone
 
-//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.19/KeystoneForwarder/KeystoneForwarder.abi ../../../contracts/solc/v0.8.19/KeystoneForwarder/KeystoneForwarder.bin KeystoneForwarder forwarder
-//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.19/OCR3Capability/OCR3Capability.abi ../../../contracts/solc/v0.8.19/OCR3Capability/OCR3Capability.bin OCR3Capability ocr3_capability
+//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.19/keystone/KeystoneForwarder.sol/KeystoneForwarder.abi.json ../../../contracts/solc/v0.8.19/keystone/KeystoneForwarder.sol/KeystoneForwarder.bin KeystoneForwarder forwarder
+//go:generate go run ../generation/generate/wrap.go ../../../contracts/solc/v0.8.19/keystone/OCR3Capability.sol/OCR3Capability.abi.json ../../../contracts/solc/v0.8.19/keystone/OCR3Capability.sol/OCR3Capability.bin OCR3Capability ocr3_capability
