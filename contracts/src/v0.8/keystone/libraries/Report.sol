90eda66e29f7188a506be372aa2a24ac750f992d
DeividasK <deividas.karzinauskas@gmail.com>
2024-04-18 13:02:01 +0300

    Comments in the Report library

--- contracts/src/v0.8/keystone/libraries/Report.sol
+++ contracts/src/v0.8/keystone/libraries/Report.sol
@@ -5,17 +5,22 @@ pragma solidity ^0.8.19;
 /// @notice A library for handling Keystone reports. Used by KeystoneForwarder
 /// and recipient (end-user) contracts.
 library Report {
+    /// @notice This error is returned when the report is malformed. We expect
+    /// Keystone reports to be at least 64 bytes long (the length of the metadata).
+    /// @param report the data that was received
+    error InvalidReport(bytes report);
+
     /// @notice Extracts the Keystone metadata from the report.
-    /// @param rawReport The raw report data without the function selector.
-    function getMetadata(bytes memory rawReport)
-        public
-        pure
-        returns (bytes32 workflowId, bytes32 workflowExecutionId)
-    {
-        assembly {
+    /// @param report The raw report data without the function selector.
+    function getMetadata(bytes memory report) public pure returns (bytes32 workflowId, bytes32 workflowExecutionId) {
+        if (report.length < 64) {
+            revert InvalidReport(report);
+        }
+
+        assembly ("memory-safe") {
             // skip first 32 bytes, contains length of the report
-            workflowId := mload(add(rawReport, 32))
-            workflowExecutionId := mload(add(rawReport, 64))
+            workflowId := mload(add(report, 32))
+            workflowExecutionId := mload(add(report, 64))
         }
 
         return (workflowId, workflowExecutionId);
