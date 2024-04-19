90c20f57ebf1b57b5d43eee5a414065bd4339804
lei shi <lei.shi@smartcontract.com>
2024-04-19 14:26:17 -0700

    add getters

--- contracts/src/v0.8/automation/dev/test/AutomationRegistry2_3.t.sol
+++ contracts/src/v0.8/automation/dev/test/AutomationRegistry2_3.t.sol
@@ -703,6 +703,23 @@ contract SetConfig is SetUp {
     );
   }
 
+  function testSetConfigOnTransmittersAndPayees() public {
+    AutomationRegistryBase2_3.TransmitterPayeeInfo[] memory transmitterPayeeInfos = registry
+      .getTransmittersWithPayees();
+    assertEq(transmitterPayeeInfos.length, TRANSMITTERS.length);
+
+    for (uint256 i = 0; i < transmitterPayeeInfos.length; i++) {
+      address transmitterAddress = transmitterPayeeInfos[i].transmitterAddress;
+      address payeeAddress = transmitterPayeeInfos[i].payeeAddress;
+
+      address expectedTransmitter = TRANSMITTERS[i];
+      address expectedPayee = PAYEES[i];
+
+      assertEq(transmitterAddress, expectedTransmitter);
+      assertEq(payeeAddress, expectedPayee);
+    }
+  }
+
   function testSetConfigWithNewTransmittersSuccess() public {
     registry = deployRegistry(AutoBase.PayoutMode.OFF_CHAIN);
 
