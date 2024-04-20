ffd96ef61a0766899a63da6625571cb4b2465df8
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2024-04-20 10:27:50 +0700

    add functions to add node operator

---
+++ contracts/src/v0.8/keystone/test/CapabilityRegistry_AddCapabilityTest.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {BaseTest} from "./BaseTest.t.sol";
+import {CapabilityRegistry} from "../CapabilityRegistry.sol";
+
+contract CapabilityRegistry_AddCapabilityTest is BaseTest {
+    function test_AddCapability() public {
+        s_capabilityRegistry.addCapability(CapabilityRegistry.Capability("data-streams-reports", "1.0.0"));
+
+        bytes32 capabilityId = s_capabilityRegistry.getCapabilityID(bytes32("data-streams-reports"), bytes32("1.0.0"));
+        CapabilityRegistry.Capability memory capability = s_capabilityRegistry.getCapability(capabilityId);
+
+        assertEq(capability.capabilityType, "data-streams-reports");
+        assertEq(capability.version, "1.0.0");
+    }
+}
---
+++ contracts/src/v0.8/keystone/test/CapabilityRegistry_AddNodeOperatorTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {BaseTest} from "./BaseTest.t.sol";
+import {CapabilityRegistry} from "../CapabilityRegistry.sol";
+
+contract CapabilityRegistry_AddNodeOperatorTest is BaseTest {
+    event NodeOperatorAdded(uint256 nodeOperatorId, address indexed admin, string name);
+
+    function test_RevertWhen_CalledByNonAdmin() public {
+        changePrank(STRANGER);
+        vm.expectRevert("Only callable by owner");
+        s_capabilityRegistry.addNodeOperator(NODE_OPERATOR_ONE_ADMIN, NODE_OPERATOR_ONE_NAME);
+    }
+
+    function test_RevertWhen_NodeOperatorAdminAddressZero() public {
+        changePrank(ADMIN);
+        vm.expectRevert(CapabilityRegistry.InvalidNodeOperatorAdmin.selector);
+        s_capabilityRegistry.addNodeOperator(address(0), NODE_OPERATOR_ONE_NAME);
+    }
+
+    function test_AddNodeOperator() public {
+        changePrank(ADMIN);
+
+        vm.expectEmit(true, true, true, true, address(s_capabilityRegistry));
+        emit NodeOperatorAdded(0, NODE_OPERATOR_ONE_ADMIN, NODE_OPERATOR_ONE_NAME);
+        s_capabilityRegistry.addNodeOperator(NODE_OPERATOR_ONE_ADMIN, NODE_OPERATOR_ONE_NAME);
+
+        CapabilityRegistry.NodeOperator memory nodeOperator = s_capabilityRegistry.getNodeOperator(0);
+
+        assertEq(nodeOperator.id, 0);
+        assertEq(nodeOperator.admin, NODE_OPERATOR_ONE_ADMIN);
+        assertEq(nodeOperator.name, NODE_OPERATOR_ONE_NAME);
+    }
+}
