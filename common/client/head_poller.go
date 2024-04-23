21268f998a64624c58e01bccea6d25d6230c2c99
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2024-04-23 14:47:17 -0400

    Update poller

--- common/client/head_poller.go
+++ common/client/head_poller.go
@@ -1,11 +1,9 @@
 package client
 
 import (
-	"context"
 	"sync"
 	"time"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
 
 	"github.com/smartcontractkit/chainlink/v2/common/types"
@@ -20,27 +18,20 @@ type HeadPoller[
 	services.StateMachine
 	pollingInterval time.Duration
 	pollingFunc     func() (HEAD, error)
-	logger          logger.Logger
-
-	channel chan<- HEAD
-	errCh   chan error
+	channel         chan<- HEAD
+	errCh           chan error
 
 	stopCh chan struct{}
 	wg     sync.WaitGroup
 }
 
-// TODO: write an error to the Err()<- chan if the parent context is canceled/closed?
-// TODO: Do we want to add ctx to the NewHeadPoller constructor?
-// TODO: Should we start the polling loop right away or wait for Subsribe?
-
 // NewHeadPoller creates a new HeadPoller instance
 func NewHeadPoller[
 	HEAD Head,
-](pollingInterval time.Duration, pollingFunc func() (HEAD, error), channel chan<- HEAD, logger logger.Logger) HeadPoller[HEAD] {
+](pollingInterval time.Duration, pollingFunc func() (HEAD, error), channel chan<- HEAD) HeadPoller[HEAD] {
 	return HeadPoller[HEAD]{
 		pollingInterval: pollingInterval,
 		pollingFunc:     pollingFunc,
-		logger:          logger,
 		channel:         channel,
 		errCh:           make(chan error),
 		stopCh:          make(chan struct{}),
@@ -49,10 +40,10 @@ func NewHeadPoller[
 
 var _ types.Subscription = &HeadPoller[Head]{}
 
-func (p *HeadPoller[HEAD]) Start(ctx context.Context) error {
+func (p *HeadPoller[HEAD]) Start() error {
 	return p.StartOnce("HeadPoller", func() error {
 		p.wg.Add(1)
-		go p.pollingLoop(ctx)
+		go p.pollingLoop()
 		return nil
 	})
 }
@@ -71,7 +62,7 @@ func (p *HeadPoller[HEAD]) Err() <-chan error {
 	return p.errCh
 }
 
-func (p *HeadPoller[HEAD]) pollingLoop(ctx context.Context) {
+func (p *HeadPoller[HEAD]) pollingLoop() {
 	defer p.wg.Done()
 
 	ticker := time.NewTicker(p.pollingInterval)
@@ -79,29 +70,23 @@ func (p *HeadPoller[HEAD]) pollingLoop(ctx context.Context) {
 
 	for {
 		select {
-		case <-ctx.Done():
-			p.errCh <- ctx.Err()
-			return
 		case <-p.stopCh:
 			return
 		case <-ticker.C:
 			result, err := p.pollingFunc()
 			if err != nil {
-				p.logger.Error("error occurred when calling polling function:", err)
-				p.errCh <- err
-				continue
-			}
-
-			// TODO: If channel is full, should we drop the message?
-			// TODO: Or maybe stop polling until the channel has room?
-		sendResult:
-			for {
 				select {
-				case p.channel <- result:
-					break sendResult
+				case p.errCh <- err:
+					continue
 				case <-p.stopCh:
 					return
 				}
+			}
+
+			select {
+			case p.channel <- result:
+			case <-p.stopCh:
+				return
 			}
 		}
 	}
--- common/client/head_poller_test.go
+++ common/client/head_poller_test.go
@@ -1,15 +1,13 @@
 package client
 
 import (
-	"context"
+	"github.com/pkg/errors"
 	"math/big"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 )
 
 func Test_Poller(t *testing.T) {
@@ -28,10 +26,9 @@ func Test_Poller(t *testing.T) {
 	channel := make(chan Head, 1)
 
 	// Create poller and subscribe to receive data
-	poller := NewHeadPoller[Head](time.Millisecond, pollFunc, channel, logger.Test(t))
-	ctx := context.Background()
+	poller := NewHeadPoller[Head](time.Millisecond, pollFunc, channel)
 
-	require.NoError(t, poller.Start(ctx))
+	require.NoError(t, poller.Start())
 	defer poller.Unsubscribe()
 
 	// Create goroutine to receive updates from the poller
@@ -49,5 +46,33 @@ func Test_Poller(t *testing.T) {
 	require.Equal(t, pollMax, pollCount)
 }
 
-// TODO: Test error in pollingFunc
-// TODO: Test context cancellation
+func Test_Poller_Error(t *testing.T) {
+	// Mock polling function that returns an error every time it's called
+	pollFunc := func() (Head, error) {
+		return nil, errors.New("polling error")
+	}
+
+	// data channel to receive updates from the poller
+	channel := make(chan Head, 1)
+
+	// Create poller and subscribe to receive data
+	poller := NewHeadPoller[Head](time.Millisecond, pollFunc, channel)
+
+	require.NoError(t, poller.Start())
+	defer poller.Unsubscribe()
+
+	// Create goroutine to receive updates from the poller
+	pollCount := 0
+	pollMax := 50
+	go func() {
+		for ; pollCount < pollMax; pollCount++ {
+			err := <-poller.Err()
+			require.Error(t, err)
+			require.Equal(t, "polling error", err.Error())
+		}
+	}()
+
+	// Wait for a short duration to allow for some polling iterations
+	time.Sleep(100 * time.Millisecond)
+	require.Equal(t, pollMax, pollCount)
+}
