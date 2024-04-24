e01bdb84def67831ddc8bacb6adc3072628e3b1d
Dimitris <dimitris.grigoriou@smartcontract.com>
2024-04-24 13:25:28 +0300

    Remove unused context util

--- core/chains/evm/internal/testutils/timeout.go
+++ core/chains/evm/internal/testutils/timeout.go
@@ -1,45 +1,9 @@
 package testutils
 
 import (
-	"context"
 	"testing"
 	"time"
 )
-
-// DefaultWaitTimeout is the default wait timeout. If you have a *testing.T, use WaitTimeout instead.
-const DefaultWaitTimeout = 30 * time.Second
-
-// TestInterval is just a sensible poll interval that gives fast tests without
-// risk of spamming
-const TestInterval = 100 * time.Millisecond
-
-// Context returns a context with the test's deadline, if available.
-func Context(tb testing.TB) context.Context {
-	ctx := context.Background()
-	var cancel func()
-	switch t := tb.(type) {
-	case *testing.T:
-		if d, ok := t.Deadline(); ok {
-			ctx, cancel = context.WithDeadline(ctx, d)
-		}
-	}
-	if cancel == nil {
-		ctx, cancel = context.WithCancel(ctx)
-	}
-	tb.Cleanup(cancel)
-	return ctx
-}
-
-// WaitTimeout returns a timeout based on the test's Deadline, if available.
-// Especially important to use in parallel tests, as their individual execution
-// can get paused for arbitrary amounts of time.
-func WaitTimeout(t *testing.T) time.Duration {
-	if d, ok := t.Deadline(); ok {
-		// 10% buffer for cleanup and scheduling delay
-		return time.Until(d) * 9 / 10
-	}
-	return DefaultWaitTimeout
-}
 
 type Awaiter chan struct{}
 
