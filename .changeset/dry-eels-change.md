eb2acdb04e2d4384d8ce649bedd77717cfd0ede1
Adam Hamrick <adam.hamrick@smartcontract.com>
2024-04-25 13:03:40 -0400

    Improved Transaction Revert Reason Script (#752)
    ## Motivation
    
    More flexibility in debugging revert reasons.
    
    ## Solution
    
    Convert this script to use command line flags instead of env vars and
    constant values. This avoids accidental commits to change the scripts
    constant values, and enables RPC URL input on the command line.

---
+++ .changeset/dry-eels-change.md
@@ -0,0 +1,5 @@
+---
+"ccip": patch
+---
+
+Converts script for decoding transaction revert reasons to a CLI method
--- core/scripts/ccip/ccip-revert-reason/main.go
+++ core/scripts/ccip/ccip-revert-reason/main.go
@@ -1,71 +1,71 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/joho/godotenv"
 
 	"github.com/smartcontractkit/chainlink/core/scripts/ccip/revert-reason/handler"
 	"github.com/smartcontractkit/chainlink/core/scripts/ccip/secrets"
 )
 
-// How to use
-// Set either an error code string OR set the chainId, txHash and txRequester.
-// Setting an error code allows the script to run offline and doesn't require any RPC
-// endpoint. Using the chainId, txHash and txRequester requires an RPC endpoint, and if
-// the tx is old, the node needs to run in archive mode.
-//
-// Set the variable(s) and run main.go. The script will try to match the error code to the
-// ABIs of various CCIP contracts. If it finds a match, it will check if it's a CCIP wrapped error
-// like ExecutionError and TokenRateLimitError, and if so, it will decode the inner error.
-//
-// To configure an RPC endpoint, set the RPC_<chain_id> environment variable to the RPC endpoint.
-// e.g. RPC_420=https://rpc.<chain_id>.com
-const (
-	ErrorCodeString = "0x4e487b710000000000000000000000000000000000000000000000000000000000000032"
+var (
+	errorCodeString = flag.String("errorCode", "", "Error code string (e.g. 0x08c379a0)")
 
-	// The following inputs are only used if ERROR_CODE_STRING is empty
-	// Need a node URL
-	// NOTE: this node needs to run in archive mode if the tx is old
-	ChainId     = uint64(420)
-	TxHash      = "0x97be8559164442595aba46b5f849c23257905b78e72ee43d9b998b28eee78b84"
-	TxRequester = "0xe88ff73814fb891bb0e149f5578796fa41f20242"
-	EnvFileName = ".env"
+	chainId     = flag.Uint64("chainId", 0, "Chain ID for the transaction (e.g. 420)")
+	txHash      = flag.String("txHash", "", "Transaction hash (e.g. 0x97be8559164442595aba46b5f849c23257905b78e72ee43d9b998b28eee78b84)")
+	txRequester = flag.String("txRequester", "", "Transaction requester address (e.g. 0xe88ff73814fb891bb0e149f5578796fa41f20242)")
+	rpcURL      = flag.String("rpcURL", "", "RPC URL for the chain (can also be set in env var RPC_<chain_id>)")
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("Usage: go run . [flags]")
+		fmt.Println("You must provide either an error code string or the transaction details and an RPC URL to decode the error")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *errorCodeString == "" && (*chainId == 0 || *txHash == "" || *txRequester == "") {
+		flag.Usage()
+		return
+	}
+
 	errorString, err := getErrorString()
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error getting error string: %v\n", err)
+		return
 	}
 	decodedError, err := handler.DecodeErrorStringFromABI(errorString)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error decoding error string: %v\n", err)
+		return
 	}
 
 	fmt.Println(decodedError)
 }
 
 func getErrorString() (string, error) {
-	errorCodeString := ErrorCodeString
-
-	if errorCodeString == "" {
-		// Try to load env vars from .env file
-		err := godotenv.Load(EnvFileName)
-		if err != nil {
-			fmt.Println("No .env file found, using env vars from shell")
-		}
-
-		ec, err := ethclient.Dial(secrets.GetRPC(ChainId))
-		if err != nil {
-			return "", err
-		}
-		errorCodeString, err = handler.GetErrorForTx(ec, TxHash, TxRequester)
-		if err != nil {
-			return "", err
-		}
+	if *errorCodeString != "" {
+		return *errorCodeString, nil
 	}
 
-	return errorCodeString, nil
+	if *rpcURL == "" {
+		fmt.Printf("RPC URL not provided, looking for RPC_%d env var\n", *chainId)
+		envRPC := secrets.GetRPC(*chainId)
+		rpcURL = &envRPC
+	}
+
+	ec, err := ethclient.Dial(*rpcURL)
+	if err != nil {
+		return "", err
+	}
+	errString, err := handler.GetErrorForTx(ec, *txHash, *txRequester)
+	if err != nil {
+		return "", err
+	}
+
+	return errString, nil
 }
