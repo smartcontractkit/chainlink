43d1b718c9f6a6bf79d50b4c1f5dad8bc5b66b23
Sam <samsondav@protonmail.com>
2024-04-26 10:10:40 -0400

    Configurable Mercury transmitter parameters (#12680)
    * Configurable Mercury transmitter parameters
    
    * Changeset
    
    * Remove commented code
    
    * add tag
    
    * Rename

--- core/config/mercury_config.go
+++ core/config/mercury_config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"time"
 
+	commonconfig "github.com/smartcontractkit/chainlink-common/pkg/config"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 )
 
@@ -16,8 +17,14 @@ type MercuryTLS interface {
 	CertFile() string
 }
 
+type MercuryTransmitter interface {
+	TransmitQueueMaxSize() uint32
+	TransmitTimeout() commonconfig.Duration
+}
+
 type Mercury interface {
 	Credentials(credName string) *types.MercuryCredentials
 	Cache() MercuryCache
 	TLS() MercuryTLS
+	Transmitter() MercuryTransmitter
 }
