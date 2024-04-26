f55d8be495a83c97ac5439672563400e12ec2ee7
Sam <samsondav@protonmail.com>
2024-04-26 10:10:40 -0400

    Configurable Mercury transmitter parameters (#12680)
    * Configurable Mercury transmitter parameters
    
    * Changeset
    
    * Remove commented code
    
    * add tag
    
    * Rename

--- core/services/chainlink/config_mercury.go
+++ core/services/chainlink/config_mercury.go
@@ -3,6 +3,7 @@ package chainlink
 import (
 	"time"
 
+	commonconfig "github.com/smartcontractkit/chainlink-common/pkg/config"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 
 	"github.com/smartcontractkit/chainlink/v2/core/config"
@@ -25,12 +26,28 @@ func (m *mercuryCacheConfig) LatestReportDeadline() time.Duration {
 	return m.c.LatestReportDeadline.Duration()
 }
 
+var _ config.MercuryTLS = (*mercuryTLSConfig)(nil)
+
 type mercuryTLSConfig struct {
 	c toml.MercuryTLS
 }
 
 func (m *mercuryTLSConfig) CertFile() string {
 	return *m.c.CertFile
+}
+
+var _ config.MercuryTransmitter = (*mercuryTransmitterConfig)(nil)
+
+type mercuryTransmitterConfig struct {
+	c toml.MercuryTransmitter
+}
+
+func (m *mercuryTransmitterConfig) TransmitQueueMaxSize() uint32 {
+	return *m.c.TransmitQueueMaxSize
+}
+
+func (m *mercuryTransmitterConfig) TransmitTimeout() commonconfig.Duration {
+	return *m.c.TransmitTimeout
 }
 
 type mercuryConfig struct {
@@ -59,4 +76,8 @@ func (m *mercuryConfig) Cache() config.MercuryCache {
 
 func (m *mercuryConfig) TLS() config.MercuryTLS {
 	return &mercuryTLSConfig{c: m.c.TLS}
+}
+
+func (m *mercuryConfig) Transmitter() config.MercuryTransmitter {
+	return &mercuryTransmitterConfig{c: m.c.Transmitter}
 }
