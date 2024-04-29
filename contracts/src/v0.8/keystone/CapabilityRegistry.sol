0a37c0ed5346df509b545c88278c026cb2adf375
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2024-04-29 08:32:38 +0300

    CapabilityRegistry: add capability version (#12996)
    * Add capability response type and configuration contract
    
    * MUST NOT override existing capability version
    
    * MUST match configuration contract interface
    
    * Add wrappers
    
    * Include a tag to core changeset
    
    * Update ICapabilityConfiguration
    
    * Regen wrappers

--- contracts/src/v0.8/keystone/CapabilityRegistry.sol
+++ contracts/src/v0.8/keystone/CapabilityRegistry.sol
@@ -3,14 +3,30 @@ pragma solidity ^0.8.0;
 
 import {TypeAndVersionInterface} from "../interfaces/TypeAndVersionInterface.sol";
 import {OwnerIsCreator} from "../shared/access/OwnerIsCreator.sol";
+import {IERC165} from "../vendor/openzeppelin-solidity/v4.8.3/contracts/interfaces/IERC165.sol";
+import {EnumerableSet} from "../vendor/openzeppelin-solidity/v4.8.3/contracts/utils/structs/EnumerableSet.sol";
+import {ICapabilityConfiguration} from "./interfaces/ICapabilityConfiguration.sol";
 
 contract CapabilityRegistry is OwnerIsCreator, TypeAndVersionInterface {
+  // Add the library methods
+  using EnumerableSet for EnumerableSet.Bytes32Set;
+
   struct NodeOperator {
     /// @notice The address of the admin that can manage a node
     /// operator
     address admin;
     /// @notice Human readable name of a Node Operator managing the node
     string name;
+  }
+
+  // CapabilityResponseType indicates whether remote response requires
+  // aggregation or is an already aggregated report. There are multiple
+  // possible ways to aggregate.
+  enum CapabilityResponseType {
+    // No additional aggregation is needed on the remote response.
+    REPORT,
+    // A number of identical observations need to be aggregated.
+    OBSERVATION_IDENTICAL
   }
 
   struct Capability {
@@ -21,11 +37,38 @@ contract CapabilityRegistry is OwnerIsCreator, TypeAndVersionInterface {
     // Semver, e.g., "1.2.3"
     // bytes32(string); must be valid Semver + max 32 characters.
     bytes32 version;
+    // responseType indicates whether remote response requires
+    // aggregation or is an OCR report. There are multiple possible
+    // ways to aggregate.
+    CapabilityResponseType responseType;
+    // An address to the capability configuration contract. Having this defined
+    // on a capability enforces consistent configuration across DON instances
+    // serving the same capability. Configuration contract MUST implement
+    // CapabilityConfigurationContractInterface.
+    //
+    // The main use cases are:
+    // 1) Sharing capability configuration across DON instances
+    // 2) Inspect and modify on-chain configuration without off-chain
+    // capability code.
+    //
+    // It is not recommended to store configuration which requires knowledge of
+    // the DON membership.
+    address configurationContract;
   }
 
   /// @notice This error is thrown when trying to set a node operator's
   /// admin address to the zero address
   error InvalidNodeOperatorAdmin();
+
+  /// @notice This error is thrown when trying add a capability that already
+  /// exists.
+  error CapabilityAlreadyExists();
+
+  /// @notice This error is thrown when trying to add a capability with a
+  /// configuration contract that does not implement the required interface.
+  /// @param proposedConfigurationContract The address of the proposed
+  /// configuration contract.
+  error InvalidCapabilityConfigurationContractInterface(address proposedConfigurationContract);
 
   /// @notice This event is emitted when a new node operator is added
   /// @param nodeOperatorId The ID of the newly added node operator
@@ -43,6 +86,7 @@ contract CapabilityRegistry is OwnerIsCreator, TypeAndVersionInterface {
   event CapabilityAdded(bytes32 indexed capabilityId);
 
   mapping(bytes32 => Capability) private s_capabilities;
+  EnumerableSet.Bytes32Set private s_capabilityIds;
 
   /// @notice Mapping of node operators
   mapping(uint256 nodeOperatorId => NodeOperator) private s_nodeOperators;
@@ -87,7 +131,21 @@ contract CapabilityRegistry is OwnerIsCreator, TypeAndVersionInterface {
 
   function addCapability(Capability calldata capability) external onlyOwner {
     bytes32 capabilityId = getCapabilityID(capability.capabilityType, capability.version);
+
+    if (s_capabilityIds.contains(capabilityId)) revert CapabilityAlreadyExists();
+
+    if (capability.configurationContract != address(0)) {
+      if (
+        capability.configurationContract.code.length == 0 ||
+        !IERC165(capability.configurationContract).supportsInterface(
+          ICapabilityConfiguration.getCapabilityConfiguration.selector
+        )
+      ) revert InvalidCapabilityConfigurationContractInterface(capability.configurationContract);
+    }
+
+    s_capabilityIds.add(capabilityId);
     s_capabilities[capabilityId] = capability;
+
     emit CapabilityAdded(capabilityId);
   }
 
