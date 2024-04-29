8ff1ca55b928393f11d65016eda44558f3053832
Iva Brajer <iva.brajer@smartcontract.com>
2024-04-29 12:34:28 +0200

    Change VRFV2Plus contract locations in Hardhat configs and tests

--- contracts/test/v0.8/vrf/VRFCoordinatorV2.test.ts
+++ contracts/test/v0.8/vrf/VRFCoordinatorV2.test.ts
@@ -42,7 +42,7 @@ describe('VRFCoordinatorV2', () => {
     )
     linkToken = await ltFactory.deploy()
     const bhFactory = await ethers.getContractFactory(
-      'src/v0.8/vrf/dev/BlockhashStore.sol:BlockhashStore',
+      'src/v0.8/vrf/BlockhashStore.sol:BlockhashStore',
       accounts[0],
     )
     blockHashStore = await bhFactory.deploy()
