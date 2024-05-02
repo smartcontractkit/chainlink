644f5f271d9ed47e999e1d9aa4b99e5de0fd8b89
Austin Born <austinborn212@gmail.com>
2024-05-02 05:47:42 -0700

    Operator Forwarder Production Readiness (#12983)
    * move oepratorforwarder out of dev/ and add more test cases
    
    * Update test locations
    
    * Update gas snapshot
    
    * Update solc compile
    
    * Changeset
    
    * Prettier linting + hardhat test updates
    
    * Interface naming and test updates
    
    * Update Deployer.sol => Deployer.t.sol
    
    * Rework OperatorForwarder tests
    
    * Update gas snapshot
    
    * prettier
    
    * Fix Hardhat tests
    
    * More style maintenance
    
    * Update constant variables in Deployer.t.sol
    
    * Update gas snapshot

--- contracts/src/v0.8/operatorforwarder/test/testhelpers/ChainlinkClientHelper.sol
+++ contracts/src/v0.8/operatorforwarder/test/testhelpers/ChainlinkClientHelper.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ChainlinkClient} from "../../../../ChainlinkClient.sol";
+import {ChainlinkClient} from "../../../ChainlinkClient.sol";
 
 contract ChainlinkClientHelper is ChainlinkClient {
-  bytes4 public constant FULFILSELECTOR = this.fulfill.selector;
+  bytes4 public constant FULFILL_SELECTOR = this.fulfill.selector;
 
   constructor(address link) {
     _setChainlinkToken(link);
   }
 
   function sendRequest(address op, uint256 payment) external returns (bytes32) {
-    return _sendChainlinkRequestTo(op, _buildOperatorRequest(bytes32(hex"10"), FULFILSELECTOR), payment);
+    return _sendChainlinkRequestTo(op, _buildOperatorRequest(bytes32(hex"10"), FULFILL_SELECTOR), payment);
   }
 
   function cancelRequest(bytes32 requestId, uint256 payment, uint256 expiration) external {
