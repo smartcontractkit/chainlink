3151bd3b31ef0b653bc3e07b6483e60e365dad04
Rens Rooimans <github@rensrooimans.nl>
2024-05-09 12:55:59 +0200

    use safetransfer (#826)
    Use safetransfer and remove an unused import

--- contracts/src/v0.8/ccip/pools/LockReleaseTokenPoolAndProxy.sol
+++ contracts/src/v0.8/ccip/pools/LockReleaseTokenPoolAndProxy.sol
@@ -7,7 +7,6 @@ import {ITypeAndVersion} from "../../shared/interfaces/ITypeAndVersion.sol";
 import {Pool} from "../libraries/Pool.sol";
 import {RateLimiter} from "../libraries/RateLimiter.sol";
 import {LegacyPoolWrapper} from "./LegacyPoolWrapper.sol";
-import {TokenPool} from "./TokenPool.sol";
 
 import {IERC20} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
