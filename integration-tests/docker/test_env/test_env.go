1e6dade504639a88a8e1045ca0fd53cf348243b0
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-05-13 09:20:10 +0200

    [TT-922, TT-943] Migrate automation and log poller tests to Seth (#12688)
    * WIP#1
    
    * fix load test
    
    * lints
    
    * lints again
    
    * uncomment tests
    
    * fix logtrigger mercury test
    
    * set privilege before starting consumer
    
    * WIP#2
    
    * WIP#2 - with high concurrency upkeep registration fails
    
    * more or less working
    
    * WIP#4
    
    * small fixes
    
    * fix lints
    
    * use latest Seth
    
    * Migrated keeper benchmark, kind of
    
    * migrate keeper smoke tests
    
    * bunch of optimisation
    
    * migrate automation chaos test
    
    * migrate log poller tests to Seth
    
    * use anysynced key with log poller
    
    * fix some of failing smoke tests
    
    * fix go mod
    
    * use correct Seth version
    
    * better automation some test structure
    
    * better automation some test structure
    
    * go mod
    
    * fix issues with missing LINK funds
    
    * fix keeper test
    
    * fix automation test
    
    * clean up, comments, migrate keeper benchmark test
    
    * mend
    
    * use latest Seth
    
    * cleanups, comments, etc
    
    * clean ups, automation tests + keeper tests now always test multiple contracts
    
    * use newer action version
    
    * debug results when upkeeps fails
    
    * run automation tests with post-failure debug
    
    * run automation tests with post-failure debug
    
    * better staleness report
    
    * more debug for basic automation tests
    
    * more assertions
    
    * try with sleep
    
    * increase performance check timeout to 3m
    
    * separate config from consumer start
    
    * more debug for performance chcek
    
    * try with just 1 contract
    
    * one more clean up
    
    * run keeper tests in CI to test them
    
    * solve automation load test registration failures
    
    * subscribe to headers in benchmark test to observe upkeeps
    
    * centralised ephemeral keys on testnet check, fixed keeper smoke tests, added root key buffer check
    
    * do not fail, when on testnet and ephemeral keys are !=0, set them 0 instead
    
    * don't fail in test config, display warning if eph addresses are set and set them to 0
    
    * fix possible nil pointer, remove block range timeout, mutex for error appending
    
    * update seth version
    
    * use buffered channel to read subscription headers from EVM node
    
    * benchmark test increase RR mem and set DEBUG log level
    
    * use CTF's concurrent executor to deploy contracts in parallel
    
    * change architecture so that we have only 1 subscription and then we fan out the header to all goroutines; use SafeEVMHeader
    
    * benchmark test increase geth disk and gaslimit
    
    * decrease log level, latest seth and ctf
    
    * update CTF dependency
    
    * use concurrent executor in automation load test
    
    * remove local replace
    
    * add header subscription reconnect handling
    
    * print return funds error
    
    * limit automation load test deployment concurrency
    
    * latest Seth + fix gun
    
    * do not run keeper smoke tests on each commit anymore
    
    * add one more error message
    
    * use tagged CTF
    
    * use latest Seth
    
    * use latest Seth
    
    * fix go.mod
    
    * forget about smart reconnect, just do it in a loop
    
    * use latest Seth
    
    * check if context has error before executing any of contract calling wrapper functions
    
    * prettify code + latest Seth
    
    * update Seth
    
    * optional retrying of read-only contract calls; used only by keeper benchmark test
    
    * update Seth, estimate gas for funds sending
    
    * fix new automation test, update Seth to v1.0.7
    
    * increase timeout
    
    * fix max price smoke test
    
    * fix CreateOCRKeeperJobs for v2.0
    
    * remove resolved TODO
    
    * code review changes
    
    * code review changes
    
    * code review changes vol 2
    
    * use CTF release version
    
    * bump mod version
    
    ---------
    
    Co-authored-by: anirudhwarrier <12178754+anirudhwarrier@users.noreply.github.com>

--- integration-tests/docker/test_env/test_env.go
+++ integration-tests/docker/test_env/test_env.go
@@ -393,7 +393,7 @@ func (te *CLClusterTestEnv) returnFunds() error {
 	}
 
 	for _, sethClient := range te.sethClients {
-		if err := actions_seth.ReturnFunds(te.l, sethClient, contracts.ChainlinkClientToChainlinkNodeWithKeysAndAddress(te.ClCluster.NodeAPIs())); err != nil {
+		if err := actions_seth.ReturnFundsFromNodes(te.l, sethClient, contracts.ChainlinkClientToChainlinkNodeWithKeysAndAddress(te.ClCluster.NodeAPIs())); err != nil {
 			te.l.Error().Err(err).Msg("Error returning funds from node")
 		}
 	}
@@ -403,6 +403,10 @@ func (te *CLClusterTestEnv) returnFunds() error {
 }
 
 func (te *CLClusterTestEnv) GetEVMClient(chainId int64) (blockchain.EVMClient, error) {
+	if len(te.sethClients) > 0 {
+		return nil, fmt.Errorf("Environment is using Seth clients, not EVM clients")
+	}
+
 	if evmClient, ok := te.evmClients[chainId]; ok {
 		return evmClient, nil
 	}
@@ -411,6 +415,9 @@ func (te *CLClusterTestEnv) GetEVMClient(chainId int64) (blockchain.EVMClient, e
 }
 
 func (te *CLClusterTestEnv) GetSethClient(chainId int64) (*seth.Client, error) {
+	if len(te.evmClients) > 0 {
+		return nil, fmt.Errorf("Environment is using EVMClients, not Seth clients")
+	}
 	if sethClient, ok := te.sethClients[chainId]; ok {
 		return sethClient, nil
 	}
