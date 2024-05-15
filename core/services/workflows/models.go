16b9230749888a6f26d35dfcc62685931158d9b2
krehermann <keith.rehermann@smartcontract.com>
2024-05-15 16:54:56 -0600

    Decouple static workflow graph validation from execution state (#13201)
    * Decouple static workflow graph validation from execution state
    
    * linter; revert executable to step; fix preexisiting shadowing in engine
    
    * address feedback

--- core/services/workflows/models.go
+++ core/services/workflows/models.go
@@ -16,10 +16,10 @@ type stepRequest struct {
 	state   store.WorkflowExecution
 }
 
-// stepDefinition is the parsed representation of a step in a workflow.
+// StepDefinition is the parsed representation of a step in a workflow.
 //
 // Within the workflow spec, they are called "Capability Properties".
-type stepDefinition struct {
+type StepDefinition struct {
 	ID     string         `json:"id" jsonschema:"required"`
 	Ref    string         `json:"ref,omitempty" jsonschema:"pattern=^[a-z0-9_]+$"`
 	Inputs map[string]any `json:"inputs,omitempty"`
@@ -28,16 +28,16 @@ type stepDefinition struct {
 	CapabilityType capabilities.CapabilityType `json:"-"`
 }
 
-// workflowSpec is the parsed representation of a workflow.
-type workflowSpec struct {
-	Triggers  []stepDefinition `json:"triggers" jsonschema:"required"`
-	Actions   []stepDefinition `json:"actions,omitempty"`
-	Consensus []stepDefinition `json:"consensus" jsonschema:"required"`
-	Targets   []stepDefinition `json:"targets" jsonschema:"required"`
+// WorkflowSpec is the parsed representation of a workflow.
+type WorkflowSpec struct {
+	Triggers  []StepDefinition `json:"triggers" jsonschema:"required"`
+	Actions   []StepDefinition `json:"actions,omitempty"`
+	Consensus []StepDefinition `json:"consensus" jsonschema:"required"`
+	Targets   []StepDefinition `json:"targets" jsonschema:"required"`
 }
 
-func (w *workflowSpec) steps() []stepDefinition {
-	s := []stepDefinition{}
+func (w *WorkflowSpec) Steps() []StepDefinition {
+	s := []StepDefinition{}
 	s = append(s, w.Actions...)
 	s = append(s, w.Consensus...)
 	s = append(s, w.Targets...)
@@ -55,7 +55,7 @@ type workflow struct {
 
 	triggers []*triggerCapability
 
-	spec *workflowSpec
+	spec *WorkflowSpec
 }
 
 func (w *workflow) walkDo(start string, do func(s *step) error) error {
@@ -106,17 +106,39 @@ func (w *workflow) dependents(start string) ([]*step, error) {
 	return steps, nil
 }
 
-// step wraps a stepDefinition with additional context for dependencies and execution
+// step wraps a Vertex with additional context for execution that is mutated by the engine
 type step struct {
-	stepDefinition
-	dependencies      []string
+	Vertex
 	capability        capabilities.CallbackCapability
 	config            *values.Map
 	executionStrategy executionStrategy
 }
 
+type Vertex struct {
+	StepDefinition
+	dependencies []string
+}
+
+// DependencyGraph is an intermediate representation of a workflow wherein all the graph
+// vertices are represented and validated. It is a static representation of the workflow dependencies.
+type DependencyGraph struct {
+	ID string
+	graph.Graph[string, *Vertex]
+
+	Triggers []*StepDefinition
+
+	Spec *WorkflowSpec
+}
+
+// VID is an identifier for a Vertex that can be used to uniquely identify it in a graph.
+// it represents the notion `hash` in the graph package AddVertex method.
+// we refrain from naming it `hash` to avoid confusion with the hash function.
+func (v *Vertex) VID() string {
+	return v.Ref
+}
+
 type triggerCapability struct {
-	stepDefinition
+	StepDefinition
 	trigger capabilities.TriggerCapability
 	config  *values.Map
 }
@@ -126,6 +148,14 @@ const (
 )
 
 func Parse(yamlWorkflow string) (*workflow, error) {
+	wf2, err := ParseDepedencyGraph(yamlWorkflow)
+	if err != nil {
+		return nil, err
+	}
+	return createWorkflow(wf2)
+}
+
+func ParseDepedencyGraph(yamlWorkflow string) (*DependencyGraph, error) {
 	spec, err := ParseWorkflowSpecYaml(yamlWorkflow)
 	if err != nil {
 		return nil, err
@@ -138,23 +168,23 @@ func Parse(yamlWorkflow string) (*workflow, error) {
 	// Note: all triggers are represented by a single step called
 	// `trigger`. This is because for workflows with multiple triggers
 	// only one trigger will have started the workflow.
-	stepHash := func(s *step) string {
-		return s.Ref
+	stepHash := func(s *Vertex) string {
+		return s.VID()
 	}
 	g := graph.New(
 		stepHash,
 		graph.PreventCycles(),
 		graph.Directed(),
 	)
-	err = g.AddVertex(&step{
-		stepDefinition: stepDefinition{Ref: keywordTrigger},
+	err = g.AddVertex(&Vertex{
+		StepDefinition: StepDefinition{Ref: keywordTrigger},
 	})
 	if err != nil {
 		return nil, err
 	}
 
 	// Next, let's populate the other entries in the graph.
-	for _, s := range spec.steps() {
+	for _, s := range spec.Steps() {
 		// TODO: The workflow format spec doesn't always require a `Ref`
 		// to be provided (triggers and targets don't have a `Ref` for example).
 		// To handle this, we default the `Ref` to the type, but ideally we
@@ -163,7 +193,7 @@ func Parse(yamlWorkflow string) (*workflow, error) {
 			s.Ref = s.ID
 		}
 
-		innerErr := g.AddVertex(&step{stepDefinition: s})
+		innerErr := g.AddVertex(&Vertex{StepDefinition: s})
 		if innerErr != nil {
 			return nil, fmt.Errorf("cannot add vertex %s: %w", s.Ref, innerErr)
 		}
@@ -200,16 +230,72 @@ func Parse(yamlWorkflow string) (*workflow, error) {
 		}
 	}
 
-	triggerSteps := []*triggerCapability{}
+	triggerSteps := []*StepDefinition{}
 	for _, t := range spec.Triggers {
-		triggerSteps = append(triggerSteps, &triggerCapability{
-			stepDefinition: t,
-		})
+		tt := t
+		triggerSteps = append(triggerSteps, &tt)
 	}
-	wf := &workflow{
-		spec:     &spec,
+	wf := &DependencyGraph{
+		Spec:     &spec,
 		Graph:    g,
-		triggers: triggerSteps,
+		Triggers: triggerSteps,
 	}
 	return wf, err
+}
+
+// createWorkflow converts a StaticWorkflow to an executable workflow
+// by adding metadata to the vertices that is owned by the workflow runtime.
+func createWorkflow(wf2 *DependencyGraph) (*workflow, error) {
+	out := &workflow{
+		id:       wf2.ID,
+		triggers: []*triggerCapability{},
+		spec:     wf2.Spec,
+	}
+
+	for _, t := range wf2.Triggers {
+		out.triggers = append(out.triggers, &triggerCapability{
+			StepDefinition: *t,
+		})
+	}
+
+	stepHash := func(s *step) string {
+		// must use the same hash function as the DependencyGraph.
+		// this ensures that the intermediate representation (DependencyGraph) and the workflow
+		// representation label vertices with the same identifier, which in turn allows us to
+		// to copy the edges from the intermediate representation to the executable representation.
+		return s.Vertex.VID()
+	}
+	g := graph.New(
+		stepHash,
+		graph.PreventCycles(),
+		graph.Directed(),
+	)
+	adjMap, err := wf2.Graph.AdjacencyMap()
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert intermediate representation to adjacency map: %w", err)
+	}
+
+	// copy the all the vertices from the intermediate graph to the executable workflow graph
+	for vertexRef := range adjMap {
+		v, innerErr := wf2.Graph.Vertex(vertexRef)
+		if innerErr != nil {
+			return nil, fmt.Errorf("failed to retrieve vertex for %s: %w", vertexRef, innerErr)
+		}
+		innerErr = g.AddVertex(&step{Vertex: *v})
+		if innerErr != nil {
+			return nil, fmt.Errorf("failed to add vertex to executable workflow %s: %w", vertexRef, innerErr)
+		}
+	}
+	// now we can add all the edges. this works because we are using vertex hash function is the same in both graphs.
+	// see comment on `stepHash` function.
+	for vertexRef, edgeRefs := range adjMap {
+		for edgeRef := range edgeRefs {
+			innerErr := g.AddEdge(vertexRef, edgeRef)
+			if innerErr != nil {
+				return nil, fmt.Errorf("failed to add edge from '%s' to '%s': %w", vertexRef, edgeRef, innerErr)
+			}
+		}
+	}
+	out.Graph = g
+	return out, nil
 }
