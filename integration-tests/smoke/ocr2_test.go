37f1f93f4b1601ce78c54a9778a0d2e881967773
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-05-20 13:35:34 +0200

    [TT-1179] scan cl logs to check plugin activation (#13247)
    * add support for median plugin in OCR2 tests
    
    * add support for median plugin in OCR2 tests
    
    * scan CL node logs to check whether plugins were activated
    
    * use tagged CTF version

--- integration-tests/smoke/ocr2_test.go
+++ integration-tests/smoke/ocr2_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -13,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-testing-framework/logging"
+	"github.com/smartcontractkit/chainlink-testing-framework/logstream"
 	"github.com/smartcontractkit/chainlink-testing-framework/networks"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils/testcontext"
 	"github.com/smartcontractkit/chainlink/v2/core/config/env"
@@ -56,9 +58,9 @@ func TestOCRv2Basic(t *testing.T) {
 			t.Parallel()
 			l := logging.GetTestLogger(t)
 
-			env, aggregatorContracts, sethClient := prepareORCv2SmokeTestEnv(t, test, l, 5)
+			testEnv, aggregatorContracts, sethClient := prepareORCv2SmokeTestEnv(t, test, l, 5)
 
-			err := env.MockAdapter.SetAdapterBasedIntValuePath("ocr2", []string{http.MethodGet, http.MethodPost}, 10)
+			err := testEnv.MockAdapter.SetAdapterBasedIntValuePath("ocr2", []string{http.MethodGet, http.MethodPost}, 10)
 			require.NoError(t, err)
 			err = actions_seth.WatchNewOCRRound(l, sethClient, 2, contracts.V2OffChainAgrregatorToOffChainAggregatorWithRounds(aggregatorContracts), time.Minute*5)
 			require.NoError(t, err)
@@ -145,7 +147,9 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 	privateNetwork, err := actions.EthereumNetworkConfigFromConfig(l, &config)
 	require.NoError(t, err, "Error building ethereum network config")
 
-	env, err := test_env.NewCLTestEnvBuilder().
+	clNodeCount := 6
+
+	testEnv, err := test_env.NewCLTestEnvBuilder().
 		WithTestInstance(t).
 		WithTestConfig(&config).
 		WithPrivateEthereumNetwork(privateNetwork.EthereumNetworkConfig).
@@ -156,7 +160,7 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 			node.WithTracing(),
 		)).
 		WithCLNodeOptions(test_env.WithNodeEnvVars(testData.env)).
-		WithCLNodes(6).
+		WithCLNodes(clNodeCount).
 		WithFunding(big.NewFloat(.1)).
 		WithStandardCleanup().
 		WithSeth().
@@ -164,10 +168,10 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 	require.NoError(t, err)
 
 	selectedNetwork := networks.MustGetSelectedNetworkConfig(config.Network)[0]
-	sethClient, err := env.GetSethClient(selectedNetwork.ChainID)
+	sethClient, err := testEnv.GetSethClient(selectedNetwork.ChainID)
 	require.NoError(t, err, "Error getting seth client")
 
-	nodeClients := env.ClCluster.NodeAPIs()
+	nodeClients := testEnv.ClCluster.NodeAPIs()
 	bootstrapNode, workerNodes := nodeClients[0], nodeClients[1:]
 
 	linkContract, err := contracts.DeployLinkTokenContract(l, sethClient)
@@ -190,7 +194,7 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 	aggregatorContracts, err := actions_seth.DeployOCRv2Contracts(l, sethClient, 1, common.HexToAddress(linkContract.Address()), transmitters, ocrOffchainOptions)
 	require.NoError(t, err, "Error deploying OCRv2 aggregator contracts")
 
-	err = actions.CreateOCRv2JobsLocal(aggregatorContracts, bootstrapNode, workerNodes, env.MockAdapter, "ocr2", 5, uint64(sethClient.ChainID), false, testData.chainReaderAndCodec)
+	err = actions.CreateOCRv2JobsLocal(aggregatorContracts, bootstrapNode, workerNodes, testEnv.MockAdapter, "ocr2", 5, uint64(sethClient.ChainID), false, testData.chainReaderAndCodec)
 	require.NoError(t, err, "Error creating OCRv2 jobs")
 
 	ocrv2Config, err := actions.BuildMedianOCR2ConfigLocal(workerNodes, ocrOffchainOptions)
@@ -198,6 +202,8 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 
 	err = actions_seth.ConfigureOCRv2AggregatorContracts(ocrv2Config, aggregatorContracts)
 	require.NoError(t, err, "Error configuring OCRv2 aggregator contracts")
+
+	assertCorrectNodeConfiguration(t, l, clNodeCount, testData, testEnv)
 
 	err = actions_seth.WatchNewOCRRound(l, sethClient, 1, contracts.V2OffChainAgrregatorToOffChainAggregatorWithRounds(aggregatorContracts), time.Minute*5)
 	require.NoError(t, err, "Error watching for new OCR2 round")
@@ -208,5 +214,37 @@ func prepareORCv2SmokeTestEnv(t *testing.T, testData ocr2test, l zerolog.Logger,
 		roundData.Answer.Int64(),
 	)
 
-	return env, aggregatorContracts, sethClient
+	return testEnv, aggregatorContracts, sethClient
+}
+
+func assertCorrectNodeConfiguration(t *testing.T, l zerolog.Logger, totalNodeCount int, testData ocr2test, testEnv *test_env.CLClusterTestEnv) {
+	expectedNodesWithConfiguration := totalNodeCount - 1 // minus bootstrap node
+	expectedPatterns := []string{}
+
+	if testData.env[string(env.MedianPlugin.Cmd)] != "" {
+		expectedPatterns = append(expectedPatterns, "Registered loopp.*OCR2.*Median.*")
+	}
+
+	if testData.chainReaderAndCodec {
+		expectedPatterns = append(expectedPatterns, "relayConfig\\.chainReader")
+	} else {
+		expectedPatterns = append(expectedPatterns, "ChainReader missing from RelayConfig; falling back to internal MedianContract")
+	}
+
+	// make sure that nodes are correctly configured by scanning the logs
+	for _, pattern := range expectedPatterns {
+		l.Info().Msgf("Checking for pattern: '%s' in CL node logs", pattern)
+		var correctlyConfiguredNodes []string
+		for i := 1; i < len(testEnv.ClCluster.Nodes); i++ {
+			logProcessor, processFn, err := logstream.GetRegexMatchingProcessor(testEnv.LogStream, pattern)
+			require.NoError(t, err, "Error getting regex matching processor")
+
+			count, err := logProcessor.ProcessContainerLogs(testEnv.ClCluster.Nodes[i].ContainerName, processFn)
+			require.NoError(t, err, "Error processing container logs")
+			if *count >= 1 {
+				correctlyConfiguredNodes = append(correctlyConfiguredNodes, testEnv.ClCluster.Nodes[i].ContainerName)
+			}
+		}
+		require.Equal(t, expectedNodesWithConfiguration, len(correctlyConfiguredNodes), "expected correct plugin config to be applied to %d cl-nodes, but only following ones had it: %s; regexp used: %s", expectedNodesWithConfiguration, strings.Join(correctlyConfiguredNodes, ", "), string(pattern))
+	}
 }
