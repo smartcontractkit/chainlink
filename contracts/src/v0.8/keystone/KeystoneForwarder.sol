7a356ceece990853ddcb88a5b6c86521235a0c31
DeividasK <deividas.karzinauskas@gmail.com>
2024-05-23 16:36:35 +0300

    Add onlyOwner to setConfig

--- contracts/src/v0.8/keystone/KeystoneForwarder.sol
+++ contracts/src/v0.8/keystone/KeystoneForwarder.sol
@@ -106,7 +106,7 @@ contract KeystoneForwarder is IForwarder, ConfirmedOwner, TypeAndVersionInterfac
   uint256 internal constant REPORT_METADATA_LENGTH = 164;
   uint256 internal constant SIGNATURE_LENGTH = 65;
 
-  function setConfig(uint32 donId, uint8 f, address[] calldata signers) external nonReentrant {
+  function setConfig(uint32 donId, uint8 f, address[] calldata signers) external onlyOwner {
     if (f == 0) revert FaultToleranceMustBePositive();
     if (signers.length > MAX_ORACLES) revert ExcessSigners(signers.length, MAX_ORACLES);
     if (signers.length <= 3 * f) revert InsufficientSigners(signers.length, 3 * f + 1);
