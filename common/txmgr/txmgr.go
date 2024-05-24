3e1c1f53c510ae40ba856fb00a53ea2a66a55d39
amit-momin <amit.momin@smartcontract.com>
2024-05-24 16:18:52 -0500

    Adjusted to align as close as possible with the ChainWriter interface

--- common/txmgr/txmgr.go
+++ common/txmgr/txmgr.go
@@ -65,7 +65,7 @@ type TxManager[
 	FindEarliestUnconfirmedBroadcastTime(ctx context.Context) (nullv4.Time, error)
 	FindEarliestUnconfirmedTxAttemptBlock(ctx context.Context) (nullv4.Int, error)
 	CountTransactionsByState(ctx context.Context, state txmgrtypes.TxState) (count uint32, err error)
-	TxStatusByIdempotencyKey(ctx context.Context, idempotency string) (txmgrtypes.TxState, client.TxError, error)
+	GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (state txmgrtypes.TxState, err error)
 }
 
 type reset struct {
@@ -632,14 +632,16 @@ func (b *Txm[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) CountTrans
 	return b.txStore.CountTransactionsByState(ctx, state, b.chainID)
 }
 
-func (b *Txm[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) TxStatusByIdempotencyKey(ctx context.Context, idempotencyKey string) (state txmgrtypes.TxState, txErr client.TxError, err error) {
-	tx, _ := b.txStore.FindTxWithIdempotencyKey(ctx, idempotencyKey, b.chainID)
-	// this check is more complete since a no-rows error returns nil err
-	if tx == nil {
-		return "", txErr, fmt.Errorf("failed to find transaction with IdempotencyKey: %s", idempotencyKey)
+func (b *Txm[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (status txmgrtypes.TxState, err error) {
+	tx, err := b.txStore.FindTxWithIdempotencyKey(ctx, transactionID.String(), b.chainID)
+	if err != nil {
+		return status, fmt.Errorf("failed to find transaction with IdempotencyKey %s: %w", transactionID.String(), err)
 	}
-	txErr = b.newTxError(tx.GetError())
-	return tx.State, txErr, nil
+	// This check is required since a no-rows error returns nil err
+	if tx == nil {
+		return status, fmt.Errorf("failed to find transaction with IdempotencyKey %s", transactionID.String())
+	}
+	return tx.State, tx.GetError()
 }
 
 type NullTxManager[
@@ -725,8 +727,8 @@ func (n *NullTxManager[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]) Cou
 	return count, errors.New(n.ErrMsg)
 }
 
-func (n *NullTxManager[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]) TxStatusByIdempotencyKey(ctx context.Context, idempotencyKey string) (state txmgrtypes.TxState, txErr client.TxError, err error) {
-	return "", txErr, nil
+func (n *NullTxManager[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, SEQ, FEE]) GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (state txmgrtypes.TxState, err error) {
+	return
 }
 
 func (b *Txm[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) pruneQueueAndCreateTxn(
