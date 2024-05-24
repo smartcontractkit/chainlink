ffafc4651b5165cd2504d33c4e327ce990d78362
Nick Corin <nickcorin@gmail.com>
2024-05-24 14:43:21 +0200

    evm: Pseudo-implement the submit transaction method on chainwriter

--- core/services/relay/evm/chain_writer.go
+++ core/services/relay/evm/chain_writer.go
@@ -6,10 +6,15 @@ import (
 	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/uuid"
 	commonservices "github.com/smartcontractkit/chainlink-common/pkg/services"
+	"github.com/smartcontractkit/chainlink/v2/common/txmgr"
+	txmgrtypes "github.com/smartcontractkit/chainlink/v2/common/txmgr/types"
 	evmclient "github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/config"
+	evmtxmgr "github.com/smartcontractkit/chainlink/v2/core/chains/evm/txmgr"
+	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services"
 
@@ -36,7 +41,39 @@ type chainWriter struct {
 	config config.ChainWriter
 }
 
-func (w *chainWriter) SubmitSignedTransaction(ctx context.Context, payload []byte, signature map[string]any, transactionID uuid.UUID, toAddress string, meta *types.TxMeta, value big.Int) error {
+func (w *chainWriter) SubmitSignedTransaction(ctx context.Context, payload []byte, signatures map[string]any, transactionID uuid.UUID, toAddress string, meta *types.TxMeta, value big.Int) error {
+	forwarderABI := evmtypes.MustGetABI(w.config.ABI())
+
+	// TODO(nickcorin):
+	// Check the required format for the signatures when packing the ABI. The original type was [][]byte, however we'll need strict type assertions
+	// translating any -> []byte.
+	//
+	// Also, figure out what to use for the method name.
+	calldata, err := forwarderABI.Pack("", common.HexToAddress(toAddress), payload, signatures)
+	if err != nil {
+		return fmt.Errorf("pack forwarder abi: %w", err)
+	}
+
+	// TODO(nickcorin): Change this to be config driven.
+	sendStrategy := txmgr.SendEveryStrategy{}
+
+	var checker evmtxmgr.TransmitCheckerSpec
+	if w.config.Checker() != "" {
+		checker.CheckerType = txmgrtypes.TransmitCheckerType(w.config.Checker())
+	}
+
+	req := evmtxmgr.TxRequest{
+		FromAddress:    w.config.FromAddress().Address(),
+		ToAddress:      w.config.ForwarderAddress().Address(),
+		EncodedPayload: calldata,
+		FeeLimit:       w.config.GasLimit(),
+		Meta:           nil, // TODO(nickcorin): Add this in once parsed.
+		Strategy:       sendStrategy,
+		Checker:        checker,
+	}
+
+	// TODO(nickcorin): Send the request to the TXM.
+
 	return fmt.Errorf("not implemented")
 }
 
