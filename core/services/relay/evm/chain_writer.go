8d7680c675c0093d1bddac4a8ad2c10e0eef57a9
Nick Corin <nickcorin@gmail.com>
2024-05-24 14:59:53 +0200

    evm: Remove the chain writer interface to reference chainlink-common

--- core/services/relay/evm/chain_writer.go
+++ core/services/relay/evm/chain_writer.go
@@ -12,50 +12,13 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/config"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types"
 )
-
-type ChainWriter interface {
-	// SubmitSignedTransaction packs and broadcasts a transaction to the underlying chain.
-	//
-	// The `transactionID` will be used by the underlying TXM as an idempotency key, and unique reference to track transaction attempts.
-	SubmitSignedTransaction(ctx context.Context, payload []byte, signature map[string]any, transactionID uuid.UUID, toAddress string, meta *TxMeta, value big.Int) (int64, error)
-
-	// GetTransactionStatus returns the current status of a transaction in the underlying chain's TXM.
-	GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (TransactionStatus, error)
-
-	// GetFeeComponents retrieves the associated gas costs for executing a transaction.
-	GetFeeComponents(ctx context.Context) (ChainFeeComponents, error)
-}
-
-// TxMeta contains metadata fields for a transaction.
-//
-// Eventually this will replace, or be replaced by (via a move), the `TxMeta` in core:
-// https://github.com/smartcontractkit/chainlink/blob/dfc399da715f16af1fcf6441ea5fc47b71800fa1/common/txmgr/types/tx.go#L121
-type TxMeta = map[string]string
-
-// TransactionStatus are the status we expect every TXM to support and that can be returned by StatusForUUID.
-type TransactionStatus int
-
-const (
-	Unknown TransactionStatus = iota
-	Unconfirmed
-	Finalized
-	Failed
-	Fatal
-)
-
-// ChainFeeComponents contains the different cost components of executing a transaction.
-type ChainFeeComponents struct {
-	// The cost of executing transaction in the chain's EVM (or the L2 environment).
-	ExecutionFee big.Int
-
-	// The cost associated with an L2 posting a transaction's data to the L1.
-	DataAvailabilityFee big.Int
-}
 
 type ChainWriterService interface {
 	services.ServiceCtx
-	ChainWriter
+	types.ChainWriter
 }
 
 // Compile-time assertion that chainWriter implements the ChainWriterService interface.
@@ -73,16 +36,16 @@ type chainWriter struct {
 	config config.ChainWriter
 }
 
-func (w *chainWriter) SubmitSignedTransaction(ctx context.Context, payload []byte, signature map[string]any, transactionID uuid.UUID, toAddress string, meta *TxMeta, value big.Int) (int64, error) {
+func (w *chainWriter) SubmitSignedTransaction(ctx context.Context, payload []byte, signature map[string]any, transactionID uuid.UUID, toAddress string, meta *types.TxMeta, value big.Int) (int64, error) {
 	return 0, fmt.Errorf("not implemented")
 }
 
-func (w *chainWriter) GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (TransactionStatus, error) {
-	return Unknown, fmt.Errorf("not implemented")
+func (w *chainWriter) GetTransactionStatus(ctx context.Context, transactionID uuid.UUID) (types.TransactionStatus, error) {
+	return types.Unknown, fmt.Errorf("not implemented")
 }
 
-func (w *chainWriter) GetFeeComponents(ctx context.Context) (ChainFeeComponents, error) {
-	return ChainFeeComponents{}, fmt.Errorf("not implemented")
+func (w *chainWriter) GetFeeComponents(ctx context.Context) (*types.ChainFeeComponents, error) {
+	return nil, fmt.Errorf("not implemented")
 }
 
 func (w *chainWriter) Close() error {
