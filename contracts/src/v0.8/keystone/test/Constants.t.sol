810c72607e8e3bd57c1e0f7b2de160a4c77943b8
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2024-05-27 17:29:25 +0700

    CapabilityRegistry updates (#13183)
    * encode instead of encode packed for capability id
    
    * add config count to DON struct
    
    * address PR feedback
    
    * Update gethwrappers
    
    * update node signer type
    
    * address naming suggestions
    
    * implement remove DONs in capability registry
    
    * address PR feedback
    
    * add check
    
    * regen wrappers
    
    * address PR feedback
    
    * implement modify DON
    
    * add changeset
    
    * address PR feedback
    
    * add test for capability ID generation
    
    * update ICapabilityConfiguration interface
    
    * add getters for entities
    
    * address PR feedback
    
    * remove unused increment
    
    * address PR feedback
    
    * Update gethwrappers
    
    * address PR feedback
    
    ---------
    
    Co-authored-by: app-token-issuer-infra-releng[bot] <120227048+app-token-issuer-infra-releng[bot]@users.noreply.github.com>

--- contracts/src/v0.8/keystone/test/Constants.t.sol
+++ contracts/src/v0.8/keystone/test/Constants.t.sol
@@ -6,10 +6,10 @@ contract Constants {
   address internal STRANGER = address(2);
   address internal NODE_OPERATOR_ONE_ADMIN = address(3);
   string internal NODE_OPERATOR_ONE_NAME = "node-operator-one";
-  address internal NODE_OPERATOR_ONE_SIGNER_ADDRESS = address(3333);
+  bytes32 internal NODE_OPERATOR_ONE_SIGNER_ADDRESS = bytes32(abi.encodePacked(address(3333)));
   address internal NODE_OPERATOR_TWO_ADMIN = address(4);
   string internal NODE_OPERATOR_TWO_NAME = "node-operator-two";
-  address internal NODE_OPERATOR_TWO_SIGNER_ADDRESS = address(4444);
+  bytes32 internal NODE_OPERATOR_TWO_SIGNER_ADDRESS = bytes32(abi.encodePacked(address(4444)));
 
   bytes32 internal P2P_ID = hex"e42415859707d90ed4dc534ad730f187a17b0c368e1beec2e9b995587c4b0a05";
   bytes32 internal P2P_ID_TWO = hex"f53415859707d90ed4dc534ad730f187a17b0c368e1beec2e9b995587c4b0a05";
