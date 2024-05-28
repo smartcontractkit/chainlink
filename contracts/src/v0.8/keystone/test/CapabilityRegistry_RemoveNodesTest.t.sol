3c2ba5ad359b7969421f2be9889fe49bc1f0ef59
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2024-05-28 16:06:02 +0800

    add extra test for removing ndoes

--- contracts/src/v0.8/keystone/test/CapabilityRegistry_RemoveNodesTest.t.sol
+++ contracts/src/v0.8/keystone/test/CapabilityRegistry_RemoveNodesTest.t.sol
@@ -82,6 +82,43 @@ contract CapabilityRegistry_RemoveNodesTest is BaseTest {
     s_capabilityRegistry.removeNodes(nodes);
   }
 
+  function test_CanRemoveWhenDONDeleted() public {
+    changePrank(ADMIN);
+
+    bytes32[] memory nodes = new bytes32[](1);
+    nodes[0] = P2P_ID;
+
+    CapabilityRegistry.CapabilityConfiguration[]
+      memory capabilityConfigs = new CapabilityRegistry.CapabilityConfiguration[](1);
+    capabilityConfigs[0] = CapabilityRegistry.CapabilityConfiguration({
+      capabilityId: s_basicHashedCapabilityId,
+      config: BASIC_CAPABILITY_CONFIG
+    });
+
+    // Add DON
+    s_capabilityRegistry.addDON(nodes, capabilityConfigs, true);
+
+    // Try remove nodes
+    bytes32[] memory removedNodes = new bytes32[](1);
+    removedNodes[0] = P2P_ID;
+    vm.expectRevert(abi.encodeWithSelector(CapabilityRegistry.NodePartOfDON.selector, P2P_ID, DON_ID));
+    s_capabilityRegistry.removeNodes(removedNodes);
+
+    // Remove DON
+    uint32[] memory donIds = new uint32[](1);
+    donIds[0] = DON_ID;
+    s_capabilityRegistry.removeDONs(donIds);
+
+    // Remove node
+    s_capabilityRegistry.removeNodes(removedNodes);
+    (CapabilityRegistry.NodeInfo memory node, uint32 configCount) = s_capabilityRegistry.getNode(P2P_ID);
+    assertEq(node.nodeOperatorId, 0);
+    assertEq(node.p2pId, bytes32(""));
+    assertEq(node.signer, bytes32(""));
+    assertEq(node.hashedCapabilityIds.length, 0);
+    assertEq(configCount, 0);
+  }
+
   function test_CanRemoveWhenNodeNoLongerPartOfDON() public {
     changePrank(ADMIN);
     CapabilityRegistry.NodeInfo[] memory newNodes = new CapabilityRegistry.NodeInfo[](1);
