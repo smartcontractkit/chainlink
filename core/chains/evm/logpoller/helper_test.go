b443b6cd0d2fce22c3455e811fb81514364108b0
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2024-05-28 13:55:15 -0700

    Address PR review comments
    - Consolidate go-ethereum imports
    - Remove extra geth-wrapper changes

--- core/chains/evm/logpoller/helper_test.go
+++ core/chains/evm/logpoller/helper_test.go
@@ -8,15 +8,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient/simulated"
 	pkgerrors "github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient/simulated"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 
