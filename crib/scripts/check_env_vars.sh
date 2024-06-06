19add5e016734c0a27b0fd67be8fdc8b37b94786
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-06-06 10:30:50 -0400

    Keystone CRIB Scripts (#13190)
    * Keystone CRIB Scripts
    
    * fixup! Keystone CRIB Scripts
    
    * Make keystone provisioning 1 click
    
    * Address self-feedback
    
    * Add env var check for keystone
    
    * Update documentation
    
    * Migrate gitignore to be relative to keystone
    
    * fixup! Keystone CRIB Scripts
    
    * fixup! fixup! Keystone CRIB Scripts
    
    * Update node rendering template
    
    * fixup! Update node rendering template

--- crib/scripts/check_env_vars.sh
+++ crib/scripts/check_env_vars.sh
@@ -3,23 +3,40 @@
 # List of required environment variables
 required_vars=(
   "DEVSPACE_IMAGE"
+  "DEVSPACE_PROFILE"
   "DEVSPACE_INGRESS_CIDRS"
   "DEVSPACE_INGRESS_BASE_DOMAIN"
   "DEVSPACE_INGRESS_CERT_ARN"
   "DEVSPACE_K8S_POD_WAIT_TIMEOUT"
   "CHAINLINK_CLUSTER_HELM_CHART_URI"
   "NS_TTL"
-  )
+)
 
-missing_vars=0  # Counter for missing variables
+missing_vars=0 # Counter for missing variables
 
 # Check each variable
 for var in "${required_vars[@]}"; do
-  if [ -z "${!var}" ]; then  # If variable is unset or empty
+  if [ -z "${!var}" ]; then # If variable is unset or empty
     echo "Error: Environment variable $var is not set."
     missing_vars=$((missing_vars + 1))
   fi
 done
+
+# Check for keystone specific profiles
+if [[ "${DEVSPACE_PROFILE}" == "keystone" ]]; then
+  keystone_vars=(
+    "KEYSTONE_ETH_WS_URL"
+    "KEYSTONE_ETH_HTTP_URL"
+    "KEYSTONE_ACCOUNT_KEY"
+  )
+
+  for var in "${keystone_vars[@]}"; do
+    if [ -z "${!var}" ]; then # If variable is unset or empty
+      echo "Error: Environment variable $var is not set."
+      missing_vars=$((missing_vars + 1))
+    fi
+  done
+fi
 
 # Exit with an error if any variables were missing
 if [ $missing_vars -ne 0 ]; then
