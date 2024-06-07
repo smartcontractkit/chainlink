b92b11611e5690e2bde028ec1ec27fda6fb3e75f
Blaž Hrastnik <blaz@mxxn.io>
2024-06-07 13:44:43 +0900

    add events to router, further changes

--- contracts/src/v0.8/keystone/KeystoneRouter.sol
+++ contracts/src/v0.8/keystone/KeystoneRouter.sol
@@ -3,20 +3,23 @@ pragma solidity ^0.8.19;
 
 import {IRouter} from "./interfaces/IRouter.sol";
 import {IReceiver} from "./interfaces/IReceiver.sol";
-import {ConfirmedOwner} from "../shared/access/ConfirmedOwner.sol";
-import {TypeAndVersionInterface} from "../interfaces/TypeAndVersionInterface.sol";
+import {OwnerIsCreator} from "../shared/access/OwnerIsCreator.sol";
+import {ITypeAndVersion} from "../shared/interfaces/ITypeAndVersion.sol";
 
-contract KeystoneRouter is IRouter, ConfirmedOwner, TypeAndVersionInterface {
+contract KeystoneRouter is IRouter, OwnerIsCreator, ITypeAndVersion {
   error Unauthorized();
 
   /// @notice This error is thrown whenever a message has already been processed.
   /// @param messageId The ID of the message that was already processed
   error AlreadyProcessed(bytes32 messageId);
 
+  event ForwarderAdded(address indexed forwarder);
+  event ForwarderRemoved(address indexed forwarder);
+
   mapping(address forwarder => bool) internal s_forwarders;
   mapping(bytes32 reportId => DeliveryStatus status) internal s_reports;
 
-  constructor() ConfirmedOwner(msg.sender) {}
+  string public constant override typeAndVersion = "KeystoneRouter 1.0.0";
 
   struct DeliveryStatus {
     address transmitter;
@@ -24,13 +27,13 @@ contract KeystoneRouter is IRouter, ConfirmedOwner, TypeAndVersionInterface {
   }
 
   function addForwarder(address forwarder) external onlyOwner {
-    // TODO: events
     s_forwarders[forwarder] = true;
+    emit ForwarderAdded(forwarder);
   }
 
   function removeForwarder(address forwarder) external onlyOwner {
-    // TODO: events
     s_forwarders[forwarder] = false;
+    emit ForwarderRemoved(forwarder);
   }
 
   function route(
@@ -54,18 +57,13 @@ contract KeystoneRouter is IRouter, ConfirmedOwner, TypeAndVersionInterface {
     return success;
   }
 
-  // get transmitter of a given report or 0x0 if it wasn't transmitted yet
+  // @notice Get transmitter of a given report or 0x0 if it wasn't transmitted yet
   function getTransmitter(bytes32 id) external view returns (address) {
     return s_reports[id].transmitter;
   }
 
-  // get delivery status of a given report
+  // @notice Get delivery status of a given report
   function getDeliveryStatus(bytes32 id) external view returns (bool) {
     return s_reports[id].transmitter != address(0);
-  }
-
-  /// @inheritdoc TypeAndVersionInterface
-  function typeAndVersion() external pure override returns (string memory) {
-    return "KeystoneRouter 1.0.0";
   }
 }
