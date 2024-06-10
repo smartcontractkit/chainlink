54c98d798cc0f318fe13d979cca4aaf0a3cc2556
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-06-10 15:07:03 +0200

    use go script to get latest images, another fix of input decoding

---
+++ integration-tests/scripts/latest_images/go.mod
@@ -0,0 +1,8 @@
+module github.com/smartcontractkit/chainlink/integration-tests/latest-images
+
+go 1.21.7
+
+require (
+	github.com/itchyny/gojq v0.12.16 // indirect
+	github.com/itchyny/timefmt-go v0.1.6 // indirect
+)
---
+++ integration-tests/scripts/latest_images/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/itchyny/gojq"
+)
+
+func getLatestImages(repositoryName, grepString string, count int, ignoredTags string) (string, error) {
+	// Run the AWS CLI command to get the image details
+	//TODO remove profile
+	cmd := exec.Command("aws", "ecr", "describe-images", "--profile", "sdlc-admin", "--repository-name", repositoryName, "--region", os.Getenv("QA_AWS_REGION"), "--output", "json", "--query", "imageDetails[?imageTags!=`null` && imageTags!=`[]`]")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to describe images: %w", err)
+	}
+
+	// Parse the JSON output using gojq
+	var imageDetails []interface{}
+	if err := json.Unmarshal(output, &imageDetails); err != nil {
+		return "", fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	query, err := gojq.Parse(".[] | .imageTags[0]")
+	if err != nil {
+		return "", fmt.Errorf("failed to parse gojq query: %w", err)
+	}
+
+	var tags []string
+	iter := query.Run(imageDetails)
+	for {
+		tag, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if tagStr, ok := tag.(string); ok {
+			tags = append(tags, tagStr)
+		} else if err, ok := tag.(error); ok {
+			return "", fmt.Errorf("failed to run gojq query: %w", err)
+		}
+	}
+
+	// Sort tags in reverse order
+	sort.Sort(sort.Reverse(sort.StringSlice(tags)))
+
+	// Filter tags based on the regex
+	re, err := regexp.Compile(grepString)
+	if err != nil {
+		return "", fmt.Errorf("failed to compile regex: %w", err)
+	}
+
+	var filteredTags []string
+	for _, tag := range tags {
+		if re.MatchString(tag) {
+			filteredTags = append(filteredTags, tag)
+		}
+		if len(filteredTags) == count {
+			break
+		}
+	}
+
+	if len(filteredTags) < count {
+		return "", fmt.Errorf("failed to get %d latest tags for %s", count, repositoryName)
+	}
+
+	// Parse ignored tags
+	ignoredTagsArray := strings.Split(ignoredTags, ",")
+
+	var imagesArr []string
+	for _, tag := range filteredTags {
+		ignore := false
+		for _, ignoredTag := range ignoredTagsArray {
+			if tag == ignoredTag {
+				ignore = true
+				break
+			}
+		}
+		if !ignore {
+			imagesArr = append(imagesArr, fmt.Sprintf("%s:%s", repositoryName, tag))
+		}
+	}
+
+	images := strings.Join(imagesArr, ",")
+	return images, nil
+}
+
+func main() {
+	// Read command line arguments
+	if len(os.Args) < 4 {
+		log.Fatalf("Usage: %s <repository_name> <grep_string> <count> <ignored_tags>", os.Args[0])
+	}
+
+	repositoryName := os.Args[1]
+	grepString := os.Args[2]
+	count, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatalf("Error: count must be an integer")
+	}
+
+	var ignoredTags string
+	if len(os.Args) == 5 {
+		ignoredTags = os.Args[4]
+	}
+
+	images, err := getLatestImages(repositoryName, grepString, count, ignoredTags)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	fmt.Println(images)
+}
