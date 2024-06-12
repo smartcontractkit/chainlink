8b9ae5d61cd31c73bfbd49c83d15cea267b37d84
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2024-06-12 13:37:46 +0100

    Drop --gen-go-grpc options as they aren't needed

--- core/capabilities/remote/types/types.go
+++ core/capabilities/remote/types/types.go
@@ -1,4 +1,4 @@
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative remote_messages.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative messages.proto
 package types
 
 import (
