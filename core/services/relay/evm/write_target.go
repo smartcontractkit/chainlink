be779f4cceb3ecd061a6ab66f8386d719abce057
Silas Lenihan <32529249+silaslenihan@users.noreply.github.com>
2024-06-13 14:02:46 -0400

    Update core/services/relay/evm/write_target.go
    Co-authored-by: Jordan Krage <jmank88@gmail.com>

--- core/services/relay/evm/write_target.go
+++ core/services/relay/evm/write_target.go
@@ -70,7 +70,8 @@ func NewWriteTarget(ctx context.Context, relayer *Relayer, chain legacyevm.Chain
 		},
 	}
 
-	cw, err := NewChainWriterService(lggr.Named("ChainWriter"), chain.Client(), chain.TxManager(), nil, chainWriterConfig)
+	chainWriterConfig.MaxGasPrice = chain.Config().EVM().GasEstimator().PriceMax()
+	cw, err := NewChainWriterService(lggr.Named("ChainWriter"), chain.Client(), chain.TxManager(), chain.GasEstimator(), chainWriterConfig)
 	if err != nil {
 		return nil, err
 	}
