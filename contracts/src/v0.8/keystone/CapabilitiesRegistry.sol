394ec88e04e0556a782296fca1f010f397f8bd50
DeividasK <deividas.karzinauskas@gmail.com>
2024-06-17 20:17:53 +0300

    --wip-- [skip CI]

--- contracts/src/v0.8/keystone/CapabilitiesRegistry.sol
+++ contracts/src/v0.8/keystone/CapabilitiesRegistry.sol
@@ -128,8 +128,6 @@ contract CapabilitiesRegistry is OwnerIsCreator, TypeAndVersionInterface {
     ///
     /// Ex. id = "data-streams-reports:chain:ethereum@1.0.0"
     ///     labelledName = "data-streams-reports:chain:ethereum"
-    ///
-    /// validation regex: ^[a-z0-9_\-:]{1,32}$
     string labelledName;
     /// @notice Semver, e.g., "1.2.3"
     /// @dev must be valid Semver + max 32 characters.
@@ -154,6 +152,43 @@ contract CapabilitiesRegistry is OwnerIsCreator, TypeAndVersionInterface {
     // It is not recommended to store configuration which requires knowledge of
     // the DON membership.
     address configurationContract;
+  }
+
+  struct CapabilityInfo {
+    /// @notice A hashed ID created by the `getHashedCapabilityId` function.
+    bytes32 hashedId;
+    /// @notice The partially qualified ID for the capability.
+    /// @dev Given the following capability ID: {name}:{label1_key}_{label1_value}:{label2_key}_{label2_value}@{version}
+    // Then we denote the `labelledName` as the `{name}:{label1_key}_{label1_value}:{label2_key}_{label2_value}` portion of the ID.
+    ///
+    /// Ex. id = "data-streams-reports:chain:ethereum@1.0.0"
+    ///     labelledName = "data-streams-reports:chain:ethereum"
+    string labelledName;
+    /// @notice Semver, e.g., "1.2.3"
+    /// @dev must be valid Semver + max 32 characters.
+    string version;
+    /// @notice CapabilityType indicates the type of capability which determines
+    /// where the capability can be used in a Workflow Spec.
+    CapabilityType capabilityType;
+    /// @notice CapabilityResponseType indicates whether remote response requires
+    // aggregation or is an already aggregated report. There are multiple
+    // possible ways to aggregate.
+    CapabilityResponseType responseType;
+    /// @notice An address to the capability configuration contract. Having this defined
+    // on a capability enforces consistent configuration across DON instances
+    // serving the same capability. Configuration contract MUST implement
+    // CapabilityConfigurationContractInterface.
+    //
+    /// @dev The main use cases are:
+    // 1) Sharing capability configuration across DON instances
+    // 2) Inspect and modify on-chain configuration without off-chain
+    // capability code.
+    //
+    // It is not recommended to store configuration which requires knowledge of
+    // the DON membership.
+    address configurationContract;
+    /// @notice True if the capability is deprecated
+    bool isDeprecated;
   }
 
   /// @notice CapabilityConfiguration is a struct that holds the capability configuration
@@ -693,44 +728,32 @@ contract CapabilitiesRegistry is OwnerIsCreator, TypeAndVersionInterface {
 
   /// @notice Returns a Capability by its hashed ID.
   /// @dev Use `getHashedCapabilityId` to get the hashed ID.
-  function getCapability(bytes32 hashedId) external view returns (Capability memory) {
-    return s_capabilities[hashedId];
+  function getCapability(bytes32 hashedId) public view returns (CapabilityInfo memory) {
+    return (
+      CapabilityInfo({
+        hashedId: hashedId,
+        labelledName: s_capabilities[hashedId].labelledName,
+        version: s_capabilities[hashedId].version,
+        capabilityType: s_capabilities[hashedId].capabilityType,
+        responseType: s_capabilities[hashedId].responseType,
+        configurationContract: s_capabilities[hashedId].configurationContract,
+        isDeprecated: s_deprecatedHashedCapabilityIds.contains(hashedId)
+      })
+    );
   }
 
   /// @notice Returns all capabilities. This operation will copy capabilities
   /// to memory, which can be quite expensive. This is designed to mostly be
   /// used by view accessors that are queried without any gas fees.
-  /// @return hashedCapabilityIds bytes32[] List of hashed capability Ids
-  /// @return capabilities Capability[] List of capabilities
-  function getCapabilities()
-    external
-    view
-    returns (bytes32[] memory hashedCapabilityIds, Capability[] memory capabilities)
-  {
-    hashedCapabilityIds = s_hashedCapabilityIds.values();
-
-    uint256 numSupportedCapabilities = hashedCapabilityIds.length - s_deprecatedHashedCapabilityIds.length();
-
-    // Solidity does not support dynamic arrays in memory, so we create a
-    // fixed-size array and copy the capabilities into it.
-    capabilities = new Capability[](numSupportedCapabilities);
-    bytes32[] memory supportedHashedCapabilityIds = new bytes32[](numSupportedCapabilities);
-
-    // We need to keep track of the new index because we are skipping
-    // deprecated capabilities.
-    uint256 newIndex;
+  /// @return CapabilityInfo[] List of capabilities
+  function getCapabilities() external view returns (CapabilityInfo[] memory) {
+    bytes32[] memory hashedCapabilityIds = s_hashedCapabilityIds.values();
+    CapabilityInfo[] memory capabilitiesInfo = new CapabilityInfo[](hashedCapabilityIds.length);
 
     for (uint256 i; i < hashedCapabilityIds.length; ++i) {
-      bytes32 hashedCapabilityId = hashedCapabilityIds[i];
-
-      if (!s_deprecatedHashedCapabilityIds.contains(hashedCapabilityId)) {
-        capabilities[newIndex] = s_capabilities[hashedCapabilityId];
-        supportedHashedCapabilityIds[newIndex] = hashedCapabilityId;
-        ++newIndex;
-      }
+      capabilitiesInfo[i] = getCapability(hashedCapabilityIds[i]);
     }
-
-    return (supportedHashedCapabilityIds, capabilities);
+    return capabilitiesInfo;
   }
 
   /// @notice This functions returns a capability id that has been hashed to fit into a bytes32 for cheaper access
