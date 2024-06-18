f7425d92d7a1098aa53a838ddc2261691d30b2d2
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2024-06-18 09:57:03 -0400

    Setup SendOnly nodes

--- core/chains/evm/client/evm_client.go
+++ core/chains/evm/client/evm_client.go
@@ -10,7 +10,6 @@ import (
 	evmconfig "github.com/smartcontractkit/chainlink/v2/core/chains/evm/config"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/config/chaintype"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/config/toml"
-	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
 )
 
 func NewEvmClient(cfg evmconfig.NodePool, chainCfg commonclient.ChainConfig, clientErrors evmconfig.ClientErrors, lggr logger.Logger, chainID *big.Int, nodes []*toml.Node, chainType chaintype.ChainType) Client {
@@ -18,16 +17,19 @@ func NewEvmClient(cfg evmconfig.NodePool, chainCfg commonclient.ChainConfig, cli
 	var primaries []commonclient.Node[*big.Int, ChainClientRPC]
 	var sendonlys []commonclient.SendOnlyNode[*big.Int, ChainClientRPC]
 	for i, node := range nodes {
-		rpc := NewRPCClient(cfg, lggr, empty, (*url.URL)(node.HTTPURL), *node.Name, int32(i), chainID,
-			commonclient.Secondary)
-		newNode := commonclient.NewNode[*big.Int, *evmtypes.Head, ChainClientRPC](cfg, chainCfg,
-			lggr, (url.URL)(*node.WSURL), (*url.URL)(node.HTTPURL), *node.Name, int32(i), chainID, *node.Order,
-			rpc, "EVM")
-
 		if node.SendOnly != nil && *node.SendOnly {
-			sendonlys = append(sendonlys, newNode)
+			rpc := NewRPCClient(cfg, lggr, empty, (*url.URL)(node.HTTPURL), *node.Name, int32(i), chainID,
+				commonclient.Secondary)
+			sendonly := commonclient.NewSendOnlyNode(lggr, (url.URL)(*node.HTTPURL),
+				*node.Name, chainID, rpc)
+			sendonlys = append(sendonlys, sendonly)
 		} else {
-			primaries = append(primaries, newNode)
+			rpc := NewRPCClient(cfg, lggr, (url.URL)(*node.WSURL), (*url.URL)(node.HTTPURL), *node.Name, int32(i),
+				chainID, commonclient.Primary)
+			primaryNode := commonclient.NewNode(cfg, chainCfg,
+				lggr, (url.URL)(*node.WSURL), (*url.URL)(node.HTTPURL), *node.Name, int32(i), chainID, *node.Order,
+				rpc, "EVM")
+			primaries = append(primaries, primaryNode)
 		}
 	}
 
