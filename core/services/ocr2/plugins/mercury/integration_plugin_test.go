09b09730db741c2b73977dce0c83888b4c72f414
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2024-06-20 13:47:38 +0300

    SkipFlakey: TestIntegration_MercuryV1 + TestIntegration_MercuryV2_Plugin (#13582)
    * SkipFlakey: TestIntegration_MercuryV2_Plugin
    
    * SkipFlakey: TestIntegration_MercuryV1_Plugin
    
    * Mark the right test 🙈

--- core/services/ocr2/plugins/mercury/integration_plugin_test.go
+++ core/services/ocr2/plugins/mercury/integration_plugin_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/smartcontractkit/chainlink/v2/core/config/env"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 )
 
 func TestIntegration_MercuryV1_Plugin(t *testing.T) {
@@ -15,8 +16,8 @@ func TestIntegration_MercuryV1_Plugin(t *testing.T) {
 }
 
 func TestIntegration_MercuryV2_Plugin(t *testing.T) {
+	testutils.SkipFlakey(t, "https://smartcontract-it.atlassian.net/browse/MERC-5697")
 	t.Setenv(string(env.MercuryPlugin.Cmd), "chainlink-mercury")
-
 	integration_MercuryV2(t)
 }
 
