d2f2a6326355d4e4b72e1c1dbfb77063061d056a
poke1994 <139136266+poke1994@users.noreply.github.com>
2024-06-21 17:34:02 +0100

    Update CCIPAutoWatchlist.sol

--- contracts/src/v0.8/automation/upkeeps/CCIPAutoWatchlist.sol
+++ contracts/src/v0.8/automation/upkeeps/CCIPAutoWatchlist.sol
@@ -6,89 +6,67 @@ import "../../shared/access/ConfirmedOwner.sol";
 import {AutomationCompatibleInterface} from "../interfaces/AutomationCompatibleInterface.sol";
 import {AccessControl} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts/access/AccessControl.sol";
 import {LinkAvailableBalanceMonitor} from "../upkeeps/LinkAvailableBalanceMonitor.sol";
+import "../interfaces/ILogAutomation.sol";
 
+contract CCIPOnRampAutoWatchlist is ILogAutomation, ConfirmedOwner {
+  event setWatchlistOnMonitor(uint64 indexed _dstChainSelector, address indexed _onRamp);
 
-struct Log {
-    uint256 index; // Index of the log in the block
-    uint256 timestamp; // Timestamp of the block containing the log
-    bytes32 txHash; // Hash of the transaction containing the log
-    uint256 blockNumber; // Number of the block containing the log
-    bytes32 blockHash; // Hash of the block containing the log
-    address source; // Address of the contract that emitted the log
-    bytes32[] topics; // Indexed topics of the log
-    bytes data; // Data of the log
-}
+  LinkAvailableBalanceMonitor public linkAvailableBalanceMonitorAddress;
+  address public routerAddress;
 
-interface ILogAutomation {
-    function checkLog(
-        Log calldata log,
-        bytes memory checkData
-    ) external returns (bool upkeepNeeded, bytes memory performData);
+  // keccak256 signature for OnRampSet event set as constant
+  bytes32 private constant EVENT_SIGNATURE = 0x1f7d0ec248b80e5c0dde0ee531c4fc8fdb6ce9a2b3d90f560c74acd6a7202f23;
 
-    function performUpkeep(bytes calldata performData) external;
-}
+  constructor(address contractaddress, address _routerAddress) ConfirmedOwner(msg.sender) {
+    linkAvailableBalanceMonitorAddress = LinkAvailableBalanceMonitor(contractaddress);
+    routerAddress = _routerAddress;
+  }
 
- contract CCIPOnRampAutoWatchlist is ILogAutomation,ConfirmedOwner {
+  // Option to change the Balance Monitor Address
+  function setBalanceMonitorAddress(address _newBalMonAddress) external onlyOwner {
+    linkAvailableBalanceMonitorAddress = LinkAvailableBalanceMonitor(_newBalMonAddress);
+  }
 
+  // Option to change the Router Address
+  // Changing Router Address would also require setting up new Upkeep as Upkeep is linked with the Router and cannot be changed
+  function setRouterAddress(address _newRouterAddress) external onlyOwner {
+    routerAddress = _newRouterAddress;
+  }
 
-    event setWatchlistOnMonitor(uint64 indexed _dstChainSelector,address indexed _onRamp);
+  function updateWatchList(address _targetAddress, uint64 _dstChainSelector) internal {
+    linkAvailableBalanceMonitorAddress.addToWatchListOrDecommission(_targetAddress, _dstChainSelector);
+  }
 
-    LinkAvailableBalanceMonitor public LinkAvailableBalanceMonitorAddress;
-    address public routerAddress;
-
-
-    constructor(address contractaddress,address _routerAddress) ConfirmedOwner(msg.sender){
-        LinkAvailableBalanceMonitorAddress = LinkAvailableBalanceMonitor(contractaddress);
-        routerAddress = _routerAddress;
-    }
-
-    // Option to change the Balance Monitor Address
-    function setBalanceMonitorAddress(address _newBalMonAddress) external onlyOwner{
-        LinkAvailableBalanceMonitorAddress = LinkAvailableBalanceMonitor(_newBalMonAddress);
-    }
-
-    // Option to change the Router Address
-    // Changing Router Address would also require setting up new Upkeep as Upkeep is linked with the Router and cannot be changed
-    function setRouterAddress(address _newRouterAddress) external onlyOwner{
-        routerAddress = _newRouterAddress;
-    }
-
-    function updateWatchList(address _targetAddress, uint64 _dstChainSelector) internal{
-        LinkAvailableBalanceMonitorAddress.addToWatchListOrDecommission(_targetAddress, _dstChainSelector);
-    }
-
-    function checkLog(
+  function checkLog(
     Log calldata log,
     bytes memory checkData
-) external view override returns (bool upkeepNeeded, bytes memory performData) {
-    
+  ) external view override returns (bool upkeepNeeded, bytes memory performData) {
     // Ensure Router address is set
     require(routerAddress != address(0), "Router address not set");
 
     // Define the event signature for OnRampSet(uint64,address)
-    bytes32 eventSignature = keccak256(abi.encodePacked("OnRampSet(uint64,address)"));
+    //bytes32 eventSignature = keccak256(abi.encodePacked("OnRampSet(uint64,address)"));
 
     // Check if the log source matches router contract and topics contain the event signature
-    if (log.source == routerAddress && log.topics.length > 0 && log.topics[0] == eventSignature) {
-        // Extract the indexed parameter from the log
-        uint64 destChainSelector = uint64(uint256(log.topics[1])); // cast to uint64
-        address onRamp = address(uint160(uint256(bytes32(log.data)))); // extract from log.data
+    if (log.source == routerAddress && log.topics.length > 0 && log.topics[0] == EVENT_SIGNATURE) {
+      // Extract the indexed parameter from the log
+      uint64 destChainSelector = uint64(uint256(log.topics[1])); // cast to uint64
+      address onRamp = address(uint160(uint256(bytes32(log.data)))); // extract from log.data
 
-        // Determine if upkeep is needed based on the emitted log
-        // For simplicity, always return true to trigger performUpkeep
-        return (true, abi.encode(destChainSelector, onRamp));
+      // No sanity checks necessary as the would on to relay information to LinkAvailableBalanceMonitor as it is
+      // Checking is enabled in LinkAvailableBalanceMonitor contract
+      return (true, abi.encode(destChainSelector, onRamp));
     }
 
     // If the event signature doesn't match or log source is not Router contract, no upkeep is needed
     return (false, "");
-}
+  }
 
-
-    function performUpkeep(bytes memory performData) external override{
-        // Decode the data received from checkLog
-        (uint64 destChainSelector, address onRamp) = abi.decode(performData, (uint64, address));
-        // Perform the necessary upkeep actions based on the decoded data
-        updateWatchList(onRamp,destChainSelector);
-        emit setWatchlistOnMonitor(destChainSelector,onRamp);
-    }
+  function performUpkeep(bytes memory performData) external override {
+    // Decode the data received from checkLog
+    (uint64 destChainSelector, address onRamp) = abi.decode(performData, (uint64, address));
+    // Perform the necessary upkeep actions based on the decoded data
+    updateWatchList(onRamp, destChainSelector);
+    emit setWatchlistOnMonitor(destChainSelector, onRamp);
+  }
 }
