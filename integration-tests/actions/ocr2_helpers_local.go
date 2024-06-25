2e3c27709412961090986b366ef9d74a2b3b597a
AnieeG <anindita.ghosh@smartcontract.com>
2024-06-25 11:01:26 -0700

    fixing ocr2 tests

--- integration-tests/actions/ocr2_helpers_local.go
+++ integration-tests/actions/ocr2_helpers_local.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/pkg/codec"
 	"github.com/smartcontractkit/chainlink-testing-framework/docker/test_env"
+
 	evmtypes "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
 
 	"github.com/smartcontractkit/chainlink/v2/core/services/job"
@@ -37,8 +38,8 @@ func CreateOCRv2JobsLocal(
 	workerChainlinkNodes []*client.ChainlinkClient,
 	mockAdapter *test_env.Killgrave,
 	mockAdapterPath string, // Path on the mock server for the Chainlink nodes to query
-	mockAdapterValue int, // Value to get from the mock server when querying the path
-	chainId uint64, // EVM chain ID
+	mockAdapterValue int,   // Value to get from the mock server when querying the path
+	chainId uint64,         // EVM chain ID
 	forwardingAllowed bool,
 	enableChainReaderAndCodec bool,
 ) error {
@@ -132,6 +133,9 @@ func CreateOCRv2JobsLocal(
 				ocrSpec.OCR2OracleSpec.RelayConfig["chainReader"] = evmtypes.ChainReaderConfig{
 					Contracts: map[string]evmtypes.ChainContractReader{
 						"median": {
+							ContractPollingFilter: evmtypes.ContractPollingFilter{
+								GenericEventNames: []string{"LatestRoundRequested"},
+							},
 							ContractABI: `[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"requester","type":"address"},{"indexed":false,"internalType":"bytes32","name":"configDigest","type":"bytes32"},{"indexed":false,"internalType":"uint32","name":"epoch","type":"uint32"},{"indexed":false,"internalType":"uint8","name":"round","type":"uint8"}],"name":"RoundRequested","type":"event"},{"inputs":[],"name":"latestTransmissionDetails","outputs":[{"internalType":"bytes32","name":"configDigest","type":"bytes32"},{"internalType":"uint32","name":"epoch","type":"uint32"},{"internalType":"uint8","name":"round","type":"uint8"},{"internalType":"int192","name":"latestAnswer_","type":"int192"},{"internalType":"uint64","name":"latestTimestamp_","type":"uint64"}],"stateMutability":"view","type":"function"}]`,
 							Configs: map[string]*evmtypes.ChainReaderDefinition{
 								"LatestTransmissionDetails": {
@@ -194,7 +198,7 @@ func BuildMedianOCR2ConfigLocal(workerNodes []*client.ChainlinkClient, ocrOffcha
 			AlphaAcceptInfinite: false,
 			AlphaAcceptPPB:      1,
 			DeltaC:              time.Minute * 30,
-		}.Encode(), // reportingPluginConfig []byte,
+		}.Encode(),    // reportingPluginConfig []byte,
 		5*time.Second, // maxDurationQuery time.Duration,
 		5*time.Second, // maxDurationObservation time.Duration,
 		5*time.Second, // maxDurationReport time.Duration,
