c6c0ad6c0a66323939ac97429d149aa0d86900a6
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-06-26 14:39:29 +0200

    embed more configs

--- integration-tests/testconfig/configs_embed.go
+++ integration-tests/testconfig/configs_embed.go
@@ -7,12 +7,17 @@ import "embed"
 
 //go:embed default.toml
 //go:embed automation/automation.toml
+//go:embed forwarder_ocr/forwarder_ocr.toml
+//go:embed forwarder_ocr2/forwarder_ocr2.toml
 //go:embed functions/functions.toml
 //go:embed keeper/keeper.toml
 //go:embed log_poller/log_poller.toml
 //go:embed node/node.toml
 //go:embed ocr/ocr.toml
+//go:embed ocr2/ocr2.toml
+//go:embed vrf/vrf.toml
 //go:embed vrfv2/vrfv2.toml
+//go:embed ocrvrfv2/ocrvrfv2.toml
 //go:embed vrfv2plus/vrfv2plus.toml
 var embeddedConfigsFs embed.FS
 
