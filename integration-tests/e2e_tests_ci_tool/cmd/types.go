2d0ae2b76da0ad636735433d4560e7af9bac3af2
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-07-01 13:11:43 +0200

    Add a test workflow with overrides

--- integration-tests/e2e_tests_ci_tool/cmd/types.go
+++ integration-tests/e2e_tests_ci_tool/cmd/types.go
@@ -7,24 +7,17 @@ type Test struct {
 
 // CITestConf defines the configuration for running a test in a CI environment, specifying details like test ID, path, type, runner settings, command, and associated workflows.
 type CITestConf struct {
-	ID                    string             `yaml:"id" json:"id"`
-	IDSanitized           string             `json:"idSanitized"`
-	Path                  string             `yaml:"path" json:"path"`
-	TestType              string             `yaml:"test-type" json:"testType"`
-	RunsOn                string             `yaml:"runs-on" json:"runsOn"`
-	TestCmd               string             `yaml:"test-cmd" json:"testCmd"`
-	TestConfigOverride    TestConfigOverride `yaml:"test-config-override" json:"testConfigOverride"`
-	RemoteRunnerTestSuite string             `yaml:"remote-runner-test-suite" json:"remoteRunnerTestSuite"`
-	RemoteRunnerMemory    string             `yaml:"remote-runner-memory" json:"remoteRunnerMemory"`
-	PyroscopeEnv          string             `yaml:"pyroscope-env" json:"pyroscopeEnv"`
-	Workflows             []string           `yaml:"workflows" json:"workflows"`
-}
-
-type TestConfigOverride struct {
-	ChainlinkUpgradeImage   string `yaml:"chainlink-upgrade-image" json:"chainlinkUpgradeImage"`
-	ChainlinkUpgradeVersion string `yaml:"chainlink-upgrade-version" json:"chainlinkUpgradeVersion"`
-	ChainlinkImage          string `yaml:"chainlink-image" json:"chainlinkImage"`
-	ChainlinkVersion        string `yaml:"chainlink-version" json:"chainlinkVersion"`
+	ID                    string            `yaml:"id" json:"id"`
+	IDSanitized           string            `json:"idSanitized"`
+	Path                  string            `yaml:"path" json:"path"`
+	TestType              string            `yaml:"test-type" json:"testType"`
+	RunsOn                string            `yaml:"runs-on" json:"runsOn"`
+	TestCmd               string            `yaml:"test-cmd" json:"testCmd"`
+	TestConfigOverrides   map[string]string `yaml:"test-config-overrides" json:"testConfigOverrides"`
+	RemoteRunnerTestSuite string            `yaml:"remote-runner-test-suite" json:"remoteRunnerTestSuite"`
+	RemoteRunnerMemory    string            `yaml:"remote-runner-memory" json:"remoteRunnerMemory"`
+	PyroscopeEnv          string            `yaml:"pyroscope-env" json:"pyroscopeEnv"`
+	Workflows             []string          `yaml:"workflows" json:"workflows"`
 }
 
 type Config struct {
