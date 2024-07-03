1708ce28764bf238e855ff9cb74d86ad78d1ac34
Fergal Gribben <fergal.gribben@smartcontract.com>
2024-07-03 15:39:39 +0100

    Update tests

--- core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/logprovider/buffer_v1_test.go
+++ core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/logprovider/buffer_v1_test.go
@@ -23,11 +23,11 @@ func TestLogEventBufferV1(t *testing.T) {
 		logpoller.Log{BlockNumber: 2, TxHash: common.HexToHash("0x2"), LogIndex: 0},
 		logpoller.Log{BlockNumber: 2, TxHash: common.HexToHash("0x1"), LogIndex: 2},
 	)
-	results, remaining := buf.Dequeue(int64(1), 10, 1, 2, false)
+	results, remaining := buf.Dequeue(int64(1), 10, 1, 2, true)
 	require.Equal(t, 2, len(results))
 	require.Equal(t, 2, remaining)
 	require.True(t, results[0].ID.Cmp(results[1].ID) != 0)
-	results, remaining = buf.Dequeue(int64(1), 10, 1, 2, false)
+	results, remaining = buf.Dequeue(int64(1), 10, 1, 2, true)
 	require.Equal(t, 0, len(results))
 	require.Equal(t, 0, remaining)
 }
@@ -217,7 +217,7 @@ func TestLogEventBufferV1_Dequeue(t *testing.T) {
 			}
 			start, end := getBlockWindow(tc.args.block, tc.args.blockRate)
 
-			results, remaining := buf.Dequeue(start, end, tc.args.upkeepLimit, tc.args.maxResults, false)
+			results, remaining := buf.Dequeue(start, end, tc.args.upkeepLimit, tc.args.maxResults, true)
 			require.Equal(t, len(tc.results), len(results))
 			require.Equal(t, tc.remaining, remaining)
 		})
