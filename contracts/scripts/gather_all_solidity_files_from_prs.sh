9d0c673820e2bca76129c3f2975004b684350c1d
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-07-12 12:57:19 +0200

    try a PR-gethering version

---
+++ contracts/scripts/gather_all_solidity_files_from_prs.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+if [ "$#" -lt 4 ]; then
+    echo "Copies all Solidity files from PRs in a comma-separated list of PR URLs to a target directory."
+    echo "Usage: $0 <repository base URL> <comma-separated list of PR URLs> <source_dir> <target directory>"
+    exit 1
+fi
+
+repo_url="$1"
+pr_urls="$2"
+source_dir="$3"
+target_dir="$4"
+
+IFS=',' read -r -a url_array <<< "$pr_urls"
+pr_numbers=""
+repo_name=$(echo "$repo_url" | cut -d '/' -f 4-)
+
+for url in "${url_array[@]}"
+do
+  if [[ $url == *"${repo_name}/pull/"* ]]; then
+    pr_number=$(echo "$url" | grep -o -E '/pull/[0-9]+' | grep -o -E '[0-9]+$')
+    pr_numbers+="$pr_number,"
+  else
+    >&2 echo "Error: PR URL $url does not belong to the $repo_url repository."
+    exit 1
+  fi
+done
+pr_numbers=${pr_numbers%,}
+
+>&2 echo "Will process PR numbers: $pr_numbers"
+>&2 echo
+
+IFS=',' read -r -a pr_array <<< "$pr_numbers"
+pr_merge_info=""
+for pr_number in "${pr_array[@]}"
+do
+  merge_commit_sha=$(gh pr view $pr_number --json mergeCommit -q '.mergeCommit.oid')
+  merge_date=$(gh pr view $pr_number --json mergedAt -q '.mergedAt')
+  pr_merge_info+="$pr_number,$merge_commit_sha,$merge_date"$'\n'
+done
+echo "$pr_merge_info" > pr_merge_info.txt
+
+sorted_prs=$(sort -t, -k3 pr_merge_info.txt)
+echo "$sorted_prs" > sorted_prs.txt
+
+full_sorted_prs_list=$(cut -d',' -f1,3 sorted_prs.txt | tr '\n' ';' | sed 's/^;//')
+IFS=';' read -r -a sorted_pr_array <<< "$full_sorted_prs_list"
+
+>&2 echo "Sorted PRs from oldest to newest based on merge date:"
+for pr_info in "${sorted_pr_array[@]}"
+  do
+    pr_number=$(echo "$pr_info" | cut -d',' -f1)
+    date=$(echo "$pr_info" | cut -d',' -f2)
+    echo "PR $pr_number merged at $date"
+done
+
+sorted_prs_list=$(cut -d',' -f1,2 sorted_prs.txt | tr '\n' ';' | sed 's/^;//')
+
+IFS=';' read -r -a pr_array <<< "$sorted_prs_list"
+mkdir -p "$target_dir"
+for pr_info in "${pr_array[@]}"
+do
+  pr_number=$(echo $pr_info | cut -d',' -f1)
+  merge_commit_sha=$(echo $pr_info | cut -d',' -f2)
+  >&2 echo
+  >&2 echo "Processing PR $pr_number with merge commit SHA $merge_commit_sha"
+
+  # Create directory for this PR
+  pr_dir="$target_dir/$pr_number"
+  mkdir -p "$pr_dir"
+
+  # Fetch all files from src directory
+  src_files=$(git ls-tree -r "$merge_commit_sha" --name-only | grep "^contracts/${source_dir}")
+  echo $src_files
+
+  for file in $src_files
+  do
+    mkdir -p "$pr_dir/$(dirname "$file")"
+    git show "$merge_commit_sha:$file" > "$pr_dir/$file"
+  done
+
+  # Get list of modified Solidity files and save to PR_number.txt
+  modified_files=$(git diff-tree --no-commit-id --name-only --diff-filter=AM -r "$merge_commit_sha" | grep '\.sol$')
+  for file in $modified_files
+  do
+    echo "$file" >> "$target_dir/$pr_number.txt"
+  done
+done
+
+rm pr_merge_info.txt
+rm sorted_prs.txt
+
+>&2 echo
+>&2 echo "Done processing PRs"
--- contracts/scripts/generate_slither_report.sh
+++ contracts/scripts/generate_slither_report.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -lt 4 ]; then
+if [ "$#" -lt 5 ]; then
 echo "Generates Markdown Slither reports and saves them to a target directory."
-echo "Usage: $0 <https://github.com/ORG/REPO/blob/COMMIT/> <config-file> <root-directory-with–contracts> <where-to-save-reports> [comma-separated list of contracts]"
+echo "Usage: $0 <https://github.com/ORG/REPO/blob/COMMIT/> <config-file> <root-directory-with–contracts> <comma-separated list of contracts> <where-to-save-reports>"
 exit 1
 fi
 
 REPO_URL=$1
 CONFIG_FILE=$2
 SOURCE_DIR=$3
-TARGET_DIR=$4
-FILES=${5// /}  # Remove any spaces from the list of files
+FILES=${4// /}  # Remove any spaces from the list of files
+TARGET_DIR=$5
 
 run_slither() {
     local FILE=$1
@@ -18,17 +18,6 @@ run_slither() {
 
     SLITHER_OUTPUT_FILE="$TARGET_DIR/$(basename "${FILE%.sol}")-slither-report.md"
     slither --config-file "$CONFIG_FILE" "$FILE" --checklist --markdown-root "$REPO_URL"  > "$SLITHER_OUTPUT_FILE"
-}
-
-flatten_contracts_in_directory() {
-    local SOURCE_DIR=$1
-    local TARGET_DIR=$2
-
-    mkdir -p "$TARGET_DIR"
-
-    for ITEM in $(find "$SOURCE_DIR" -type f -name '*.sol'); do
-        run_slither "$ITEM" "$TARGET_DIR"
-    done
 }
 
 process_files() {
@@ -39,19 +28,10 @@ process_files() {
     mkdir -p "$TARGET_DIR"
 
     for FILE in "${FILES[@]}"; do
-        MATCH=$(find "$SOURCE_DIR" -type f -name "$(basename "$FILE")")
-        if [ -n "$MATCH" ]; then
-            run_slither "$MATCH" "$TARGET_DIR"
-        else
-            echo "File $FILE does not exist within the source directory."
-        fi
+      run_slither "$SOURCE_DIR/$FILE" "$TARGET_DIR"
     done
 }
 
-if [ -z "$FILES" ]; then
-  flatten_contracts_in_directory "$SOURCE_DIR" "$TARGET_DIR"
-else
-  process_files "$SOURCE_DIR" "$TARGET_DIR" "$FILES"
-fi
+process_files "$SOURCE_DIR" "$TARGET_DIR" "${FILES[@]}"
 
 echo "UML diagrams and Slither reports saved in $TARGET_DIR folder"
