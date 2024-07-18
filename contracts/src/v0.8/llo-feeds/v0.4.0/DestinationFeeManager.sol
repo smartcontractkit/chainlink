3fb91836df72349973c120dcf64f6e846b595b69
David Przybilla <dav.alejandro@gmail.com>
2024-07-18 11:21:30 +0900

    temporal fix for compile error

--- contracts/src/v0.8/llo-feeds/v0.4.0/DestinationFeeManager.sol
+++ contracts/src/v0.8/llo-feeds/v0.4.0/DestinationFeeManager.sol
@@ -225,9 +225,9 @@ contract DestinationFeeManager is IDestinationFeeManager, ConfirmedOwner, TypeAn
     uint256 numberOfNativeFees;
 
     uint256 feesAndRewardsIndex;
-    bytes32 emptyBytes = bytes32(0);
+    //bytes32 emptyBytes = bytes32(0);
     for (uint256 i; i < payloads.length; ++i) {
-      if (poolIds[i] == emptyBytes) revert InvalidAddress();
+      if (poolIds[i] == bytes32(0)) revert InvalidAddress();
 
       (Common.Asset memory fee, Common.Asset memory reward, uint256 appliedDiscount) = _calculateFee(
         payloads[i],
