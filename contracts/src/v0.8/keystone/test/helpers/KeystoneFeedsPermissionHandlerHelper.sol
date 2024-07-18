e74de4fe2f5e9a3db8f3fffda89a776b361e2b05
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2024-07-18 15:31:25 +0530

    feat: add tests for KeystoneFeedsPermissionHandler

---
+++ contracts/src/v0.8/keystone/test/helpers/KeystoneFeedsPermissionHandlerHelper.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../../KeystoneFeedsPermissionHandler.sol";
+
+contract KeystoneFeedsPermissionHandlerHelper is KeystoneFeedsPermissionHandler {
+    function validateReportPermission(
+        address workflowOwner,
+        bytes10 workflowName,
+        bytes2 reportName
+    ) external view {
+        _validateReportPermission(msg.sender, workflowOwner, workflowName, reportName);
+    }
+
+    function createReportId(Permission memory permission) external pure returns (bytes32) {
+        return _createReportId(permission.forwarder, permission.workflowOwner, permission.workflowName, permission.reportName);
+    }
+
+    function getAllowedReports(bytes32 reportId) external view returns (bool) {
+        return s_allowedReports[reportId];
+    }
+}
