7e2761928a2e23576da61a4e4cf080d22b8f10e4
FelixFan1992 <fankejin@gmail.com>
2024-07-24 09:02:07 -0400

    update mock gas bound caller

--- contracts/src/v0.8/tests/MockGasBoundCaller.sol
+++ contracts/src/v0.8/tests/MockGasBoundCaller.sol
@@ -2,21 +2,18 @@
 pragma solidity 0.8.19;
 
 contract MockGasBoundCaller {
-  //  bool public revertCall;
-  //
-  //  function setRevertCall(bool _revertCall) external {
-  //    revertCall = _revertCall;
-  //  }
+  error TransactionFailed(address target);
 
   function gasBoundCall(address target, uint256 gasAmount, bytes memory data) external payable {
-    //    uint256 gasUsed = gasleft();
     bool success;
     assembly {
       success := call(gasAmount, target, 0, add(data, 0x20), mload(data), 0, 0)
     }
-    //    gasUsed = gasUsed - gasleft();
 
-    //    require(!revertCall, "Not enough gas for pubdata");
+    // gas bound caller will propagate the revert
+    if (!success) {
+      revert TransactionFailed(target);
+    }
 
     uint256 pubdataGas = 500000;
     bytes memory returnData = abi.encode(address(0), pubdataGas);
@@ -32,20 +29,4 @@ contract MockGasBoundCaller {
       return(sub(returnData, 0x40), paddedReturndataLen)
     }
   }
-
-  //  function gasBoundCall(address, uint256, bytes memory) external payable {
-  //    uint256 pubdataGas = 500000;
-  //    bytes memory returnData = abi.encode(address(0), uint256(500000));
-  //
-  //    uint256 paddedReturndataLen = returnData.length + 96;
-  //    if (paddedReturndataLen % 32 != 0) {
-  //      paddedReturndataLen += 32 - (paddedReturndataLen % 32);
-  //    }
-  //
-  //    assembly {
-  //      mstore(sub(returnData, 0x40), 0x40)
-  //      mstore(sub(returnData, 0x20), pubdataGas)
-  //      return(sub(returnData, 0x40), paddedReturndataLen)
-  //    }
-  //  }
 }
