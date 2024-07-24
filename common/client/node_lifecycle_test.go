ba7cdaffeeddbd0ca1c8f5fb3cfc285d5a9568f8
Dmytro Haidashenko <34754799+dhaidashenko@users.noreply.github.com>
2024-07-24 17:22:32 +0200

    Update common/client/node_lifecycle_test.go
    Co-authored-by: amit-momin <108959691+amit-momin@users.noreply.github.com>

--- common/client/node_lifecycle_test.go
+++ common/client/node_lifecycle_test.go
@@ -555,7 +555,7 @@ func TestUnit_NodeLifecycle_aliveLoop(t *testing.T) {
 			return node.State() == nodeStateUnreachable
 		})
 	})
-	t.Run("when no new heads received for threshold but we are the last live node, forcibly stays alive", func(t *testing.T) {
+	t.Run("when no new finalized heads received for threshold but we are the last live node, forcibly stays alive", func(t *testing.T) {
 		t.Parallel()
 		rpc := newMockNodeClient[types.ID, Head](t)
 		rpc.On("GetInterceptedChainInfo").Return(ChainInfo{}, ChainInfo{}).Once()
