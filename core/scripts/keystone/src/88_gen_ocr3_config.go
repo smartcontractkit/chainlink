121f8a2c629f39a08d244fe8caa4884a4a1447dd
Blaž Hrastnik <blaz@mxxn.io>
2024-07-24 18:37:58 +0900

    keystone: scripts: Also fetch the aptos account and OCR2 bundle id

--- core/scripts/keystone/src/88_gen_ocr3_config.go
+++ core/scripts/keystone/src/88_gen_ocr3_config.go
@@ -47,6 +47,8 @@ type OracleConfigSource struct {
 
 type NodeKeys struct {
 	EthAddress            string
+	AptosAccount          string
+	AptosBundleID         string
 	P2PPeerID             string // p2p_<key>
 	OCR2BundleID          string // used only in job spec
 	OCR2OnchainPublicKey  string // ocr2on_evm_<key>
