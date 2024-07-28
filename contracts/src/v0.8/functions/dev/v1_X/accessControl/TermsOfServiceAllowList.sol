ec0f31c9bb53fb3446b6cb0e117347e6ff3b8fda
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2024-07-27 20:04:57 -0500

    prettier

--- contracts/src/v0.8/functions/dev/v1_X/accessControl/TermsOfServiceAllowList.sol
+++ contracts/src/v0.8/functions/dev/v1_X/accessControl/TermsOfServiceAllowList.sol
@@ -210,9 +210,7 @@ contract TermsOfServiceAllowList is ITermsOfServiceAllowList, IAccessController,
     }
     IAccessController previousToSContract = IAccessController(s_previousToSContract);
     for (uint256 i = 0; i < previousSendersToAdd.length; ++i) {
-      if (
-        previousToSContract.hasAccess(previousSendersToAdd[i], "")
-      ) {
+      if (previousToSContract.hasAccess(previousSendersToAdd[i], "")) {
         if (!s_blockedSenders.contains(previousSendersToAdd[i])) {
           s_allowedSenders.add(previousSendersToAdd[i]);
         }
