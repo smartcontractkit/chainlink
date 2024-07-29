3461ba85a9d4c5b1b38f31108da2af966b2561b8
Blaž Hrastnik <blaz@mxxn.io>
2024-07-30 02:30:49 +0900

    keystore: aptos: Fix keystone multi-chain signature validation
    When using multi-chain, we're actually attempting to validate against the
    EVM key that's backing the OCR3Capability. Instead we should be using
    the public key that's part of the signature.

--- core/services/keystore/keys/ocr2key/aptos_keyring.go
+++ core/services/keystore/keys/ocr2key/aptos_keyring.go
@@ -91,6 +91,9 @@ func (akr *aptosKeyring) verifyBlob(pubkey ocrtypes.OnchainPublicKey, b, sig []b
 	if len(pubkey) != ed25519.PublicKeySize {
 		return false
 	}
+	// we ignore the passed in publicKey since that will be the EVM key if using multi-chain config
+	// instead we use the embedded publicKey that's part of the ed25519 signature
+	pubkey = sig[:32]
 	return ed25519consensus.Verify(ed25519.PublicKey(pubkey), b, sig[32:])
 }
 
