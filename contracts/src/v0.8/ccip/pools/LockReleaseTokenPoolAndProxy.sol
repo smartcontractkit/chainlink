9e74eee9d415b386db33bdf2dd44facc82cd3551
Rens Rooimans <github@rensrooimans.nl>
2024-07-31 10:23:21 +0200

    Port ccip onchain to chainlink (#13941)
    * init port
    
    * install dependencies and create snapshots
    
    * add to foundry ci
    
    * fix readme
    
    * fix ci & add changeset
    
    * fix fuzz exclusion
    
    * update license & solhint
    
    * add ccip solidity CI
    
    * fix coverage pruning
    
    * Update LICENSE
    
    * Update LICENSE
    
    * fix job name

---
+++ contracts/src/v0.8/ccip/pools/LockReleaseTokenPoolAndProxy.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.24;
+
+import {ILiquidityContainer} from "../../liquiditymanager/interfaces/ILiquidityContainer.sol";
+import {ITypeAndVersion} from "../../shared/interfaces/ITypeAndVersion.sol";
+
+import {Pool} from "../libraries/Pool.sol";
+import {RateLimiter} from "../libraries/RateLimiter.sol";
+import {LegacyPoolWrapper} from "./LegacyPoolWrapper.sol";
+
+import {IERC20} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/// @notice Token pool used for tokens on their native chain. This uses a lock and release mechanism.
+/// Because of lock/unlock requiring liquidity, this pool contract also has function to add and remove
+/// liquidity. This allows for proper bookkeeping for both user and liquidity provider balances.
+/// @dev One token per LockReleaseTokenPool.
+contract LockReleaseTokenPoolAndProxy is LegacyPoolWrapper, ILiquidityContainer, ITypeAndVersion {
+  using SafeERC20 for IERC20;
+
+  error InsufficientLiquidity();
+  error LiquidityNotAccepted();
+  error Unauthorized(address caller);
+
+  string public constant override typeAndVersion = "LockReleaseTokenPoolAndProxy 1.5.0-dev";
+
+  /// @dev Whether or not the pool accepts liquidity.
+  /// External liquidity is not required when there is one canonical token deployed to a chain,
+  /// and CCIP is facilitating mint/burn on all the other chains, in which case the invariant
+  /// balanceOf(pool) on home chain == sum(totalSupply(mint/burn "wrapped" token) on all remote chains) should always hold
+  bool internal immutable i_acceptLiquidity;
+  /// @notice The address of the rebalancer.
+  address internal s_rebalancer;
+  /// @notice The address of the rate limiter admin.
+  /// @dev Can be address(0) if none is configured.
+  address internal s_rateLimitAdmin;
+
+  constructor(
+    IERC20 token,
+    address[] memory allowlist,
+    address rmnProxy,
+    bool acceptLiquidity,
+    address router
+  ) LegacyPoolWrapper(token, allowlist, rmnProxy, router) {
+    i_acceptLiquidity = acceptLiquidity;
+  }
+
+  /// @notice Locks the token in the pool
+  /// @dev The _validateLockOrBurn check is an essential security check
+  function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn)
+    external
+    virtual
+    override
+    returns (Pool.LockOrBurnOutV1 memory)
+  {
+    _validateLockOrBurn(lockOrBurnIn);
+
+    if (_hasLegacyPool()) {
+      _lockOrBurnLegacy(lockOrBurnIn);
+    }
+
+    emit Locked(msg.sender, lockOrBurnIn.amount);
+
+    return Pool.LockOrBurnOutV1({destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector), destPoolData: ""});
+  }
+
+  /// @notice Release tokens from the pool to the recipient
+  /// @dev The _validateReleaseOrMint check is an essential security check
+  function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
+    external
+    virtual
+    override
+    returns (Pool.ReleaseOrMintOutV1 memory)
+  {
+    _validateReleaseOrMint(releaseOrMintIn);
+
+    if (!_hasLegacyPool()) {
+      // Release to the offRamp, which forwards it to the recipient
+      getToken().safeTransfer(msg.sender, releaseOrMintIn.amount);
+    } else {
+      _releaseOrMintLegacy(releaseOrMintIn);
+    }
+
+    emit Released(msg.sender, releaseOrMintIn.receiver, releaseOrMintIn.amount);
+
+    return Pool.ReleaseOrMintOutV1({destinationAmount: releaseOrMintIn.amount});
+  }
+
+  // @inheritdoc IERC165
+  function supportsInterface(bytes4 interfaceId) public pure virtual override returns (bool) {
+    return interfaceId == type(ILiquidityContainer).interfaceId || super.supportsInterface(interfaceId);
+  }
+
+  /// @notice Gets LiquidityManager, can be address(0) if none is configured.
+  /// @return The current liquidity manager.
+  function getRebalancer() external view returns (address) {
+    return s_rebalancer;
+  }
+
+  /// @notice Sets the LiquidityManager address.
+  /// @dev Only callable by the owner.
+  function setRebalancer(address rebalancer) external onlyOwner {
+    s_rebalancer = rebalancer;
+  }
+
+  /// @notice Sets the rate limiter admin address.
+  /// @dev Only callable by the owner.
+  /// @param rateLimitAdmin The new rate limiter admin address.
+  function setRateLimitAdmin(address rateLimitAdmin) external onlyOwner {
+    s_rateLimitAdmin = rateLimitAdmin;
+  }
+
+  /// @notice Gets the rate limiter admin address.
+  function getRateLimitAdmin() external view returns (address) {
+    return s_rateLimitAdmin;
+  }
+
+  /// @notice Checks if the pool can accept liquidity.
+  /// @return true if the pool can accept liquidity, false otherwise.
+  function canAcceptLiquidity() external view returns (bool) {
+    return i_acceptLiquidity;
+  }
+
+  /// @notice Adds liquidity to the pool. The tokens should be approved first.
+  /// @param amount The amount of liquidity to provide.
+  function provideLiquidity(uint256 amount) external {
+    if (!i_acceptLiquidity) revert LiquidityNotAccepted();
+    if (s_rebalancer != msg.sender) revert Unauthorized(msg.sender);
+
+    i_token.safeTransferFrom(msg.sender, address(this), amount);
+    emit LiquidityAdded(msg.sender, amount);
+  }
+
+  /// @notice Removed liquidity to the pool. The tokens will be sent to msg.sender.
+  /// @param amount The amount of liquidity to remove.
+  function withdrawLiquidity(uint256 amount) external {
+    if (s_rebalancer != msg.sender) revert Unauthorized(msg.sender);
+
+    if (i_token.balanceOf(address(this)) < amount) revert InsufficientLiquidity();
+    i_token.safeTransfer(msg.sender, amount);
+    emit LiquidityRemoved(msg.sender, amount);
+  }
+
+  /// @notice Sets the rate limiter admin address.
+  /// @dev Only callable by the owner or the rate limiter admin. NOTE: overwrites the normal
+  /// onlyAdmin check in the base implementation to also allow the rate limiter admin.
+  /// @param remoteChainSelector The remote chain selector for which the rate limits apply.
+  /// @param outboundConfig The new outbound rate limiter config.
+  /// @param inboundConfig The new inbound rate limiter config.
+  function setChainRateLimiterConfig(
+    uint64 remoteChainSelector,
+    RateLimiter.Config memory outboundConfig,
+    RateLimiter.Config memory inboundConfig
+  ) external override {
+    if (msg.sender != s_rateLimitAdmin && msg.sender != owner()) revert Unauthorized(msg.sender);
+
+    _setRateLimitConfig(remoteChainSelector, outboundConfig, inboundConfig);
+  }
+}
