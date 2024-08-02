82d0fcb1cf1a61a7be1067dd4352673247521d7b
defistar <lakshmi.kanth@smartcontract.com>
2024-08-02 15:18:34 +0530

    fix: CCIP-2465 prettier write for new library contracts

--- contracts/src/v0.8/shared/enumerable/EnumerableMapBytes32.sol
+++ contracts/src/v0.8/shared/enumerable/EnumerableMapBytes32.sol
@@ -39,57 +39,57 @@ import {EnumerableSet} from "../../vendor/openzeppelin-solidity/v4.8.3/contracts
  * ====
  */
 library EnumerableMapBytes32 {
-    using EnumerableSet for EnumerableSet.Bytes32Set;
+  using EnumerableSet for EnumerableSet.Bytes32Set;
 
-    error NonexistentKeyError();
+  error NonexistentKeyError();
 
-    struct Bytes32ToBytesMap {
-        EnumerableSet.Bytes32Set _keys;
-        mapping(bytes32 => bytes) _values;
-    }
+  struct Bytes32ToBytesMap {
+    EnumerableSet.Bytes32Set _keys;
+    mapping(bytes32 => bytes) _values;
+  }
 
-    /**
-     * @dev Adds a key-value pair to a map, or updates the value for an existing
+  /**
+   * @dev Adds a key-value pair to a map, or updates the value for an existing
    * key. O(1).
    *
    * Returns true if the key was added to the map, that is if it was not
    * already present.
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function set(Bytes32ToBytesMap storage map, bytes32 key, bytes memory value) internal returns (bool) {
-        map._values[key] = value;
-        return map._keys.add(key);
-    }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function set(Bytes32ToBytesMap storage map, bytes32 key, bytes memory value) internal returns (bool) {
+    map._values[key] = value;
+    return map._keys.add(key);
+  }
 
-    /**
-     * @dev Removes a key-value pair from a map. O(1).
+  /**
+   * @dev Removes a key-value pair from a map. O(1).
    *
    * Returns true if the key was removed from the map, that is if it was present.
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function remove(Bytes32ToBytesMap storage map, bytes32 key) internal returns (bool) {
-        delete map._values[key];
-        return map._keys.remove(key);
-    }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function remove(Bytes32ToBytesMap storage map, bytes32 key) internal returns (bool) {
+    delete map._values[key];
+    return map._keys.remove(key);
+  }
 
-    /**
-     * @dev Returns true if the key is in the map. O(1).
+  /**
+   * @dev Returns true if the key is in the map. O(1).
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function contains(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bool) {
-        return map._keys.contains(key);
-    }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function contains(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bool) {
+    return map._keys.contains(key);
+  }
 
-    /**
-     * @dev Returns the number of key-value pairs in the map. O(1).
+  /**
+   * @dev Returns the number of key-value pairs in the map. O(1).
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function length(Bytes32ToBytesMap storage map) internal view returns (uint256) {
-        return map._keys.length();
-    }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function length(Bytes32ToBytesMap storage map) internal view returns (uint256) {
+    return map._keys.length();
+  }
 
-    /**
-     * @dev Returns the key-value pair stored at position `index` in the map. O(1).
+  /**
+   * @dev Returns the key-value pair stored at position `index` in the map. O(1).
    *
    * Note that there are no guarantees on the ordering of entries inside the
    * array, and it may change when more entries are added or removed.
@@ -98,39 +98,39 @@ library EnumerableMapBytes32 {
    *
    * - `index` must be strictly less than {length}.
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function at(Bytes32ToBytesMap storage map, uint256 index) internal view returns (bytes32, bytes memory) {
-        bytes32 key = map._keys.at(index);
-        return (key, map._values[key]);
-    }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function at(Bytes32ToBytesMap storage map, uint256 index) internal view returns (bytes32, bytes memory) {
+    bytes32 key = map._keys.at(index);
+    return (key, map._values[key]);
+  }
 
-    /**
-     * @dev Tries to returns the value associated with `key`. O(1).
+  /**
+   * @dev Tries to returns the value associated with `key`. O(1).
    * Does not revert if `key` is not in the map.
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function tryGet(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bool, bytes memory) {
-        bytes memory value = map._values[key];
-        if (value.length == 0) {
-            return (contains(map, key), bytes(""));
-        } else {
-            return (true, value);
-        }
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function tryGet(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bool, bytes memory) {
+    bytes memory value = map._values[key];
+    if (value.length == 0) {
+      return (contains(map, key), bytes(""));
+    } else {
+      return (true, value);
     }
+  }
 
-    /**
-     * @dev Returns the value associated with `key`. O(1).
+  /**
+   * @dev Returns the value associated with `key`. O(1).
    *
    * Requirements:
    *
    * - `key` must be in the map.
    */
-    // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
-    function get(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bytes memory) {
-        bytes memory value = map._values[key];
-        if (value.length == 0 && !contains(map, key)) {
-            revert NonexistentKeyError();
-        }
-        return value;
+  // solhint-disable-next-line chainlink-solidity/prefix-internal-functions-with-underscore
+  function get(Bytes32ToBytesMap storage map, bytes32 key) internal view returns (bytes memory) {
+    bytes memory value = map._values[key];
+    if (value.length == 0 && !contains(map, key)) {
+      revert NonexistentKeyError();
     }
+    return value;
+  }
 }
