be447e81822d52a9751b1737bcaf95aaf2c3a76f
Joe Huang <joe.huang@smartcontract.com>
2024-08-02 00:57:26 -0500

    update comments

--- common/txmgr/confirmer.go
+++ common/txmgr/confirmer.go
@@ -339,8 +339,10 @@ func (ec *Confirmer[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) pro
 		// There might be some dependency here with BCI-3573 and BCI-3730. Since we haven't enabled finality tag,
 		// LatestAndFinalizedBlock() is returning 0 from calculateLatestFinalized() for the first $(finalityDepth+finalityOffset) number of blocks
 		// and this becomes an issue for integration test (TestIntegration_AsyncEthTx)
-		// Temporarily made the block number to be the current head, however we removed the minConfirmation offset when fetching pending
-		// task/blocks and this can be optimistic, so not sure if we want to merge this PR first among the three
+		//
+		// Therefore, line 349, temporarily made the block number to be the current head when finality tag disabled ,
+		// however we removed the minConfirmation offset when fetching pending task/blocks and this can be optimistic,
+		// wonder if we want to merge this PR the last among the three of them in BCI-3572
 
 		// TODO update this after BCI-3573 and BCI-3730 is merged
 		var latestFinalizedHeadNum int64
