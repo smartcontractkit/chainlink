07919f4b4ef08a1e0a91cb42c4f3c46e4a162193
Makram <makramkd@users.noreply.github.com>
2024-08-06 18:02:27 +0300

    core/capabilities/ccip: use OCR offchain config (#1264)
    ## Motivation
    
    We want to define and use the appropriate OCR offchain config for each
    plugin.
    
    ## Solution
    
    Requires https://github.com/smartcontractkit/chainlink-ccip/pull/36/

--- core/capabilities/ccip/ocrimpls/config_tracker.go
+++ core/capabilities/ccip/ocrimpls/config_tracker.go
@@ -6,6 +6,7 @@ import (
 	cctypes "github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/types"
 
 	gethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3confighelper"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 )
 
@@ -24,16 +25,7 @@ func (c *configTracker) LatestBlockHeight(ctx context.Context) (blockHeight uint
 
 // LatestConfig implements types.ContractConfigTracker.
 func (c *configTracker) LatestConfig(ctx context.Context, changedInBlock uint64) (types.ContractConfig, error) {
-	return types.ContractConfig{
-		ConfigDigest:          c.cfg.ConfigDigest,
-		ConfigCount:           c.cfg.ConfigCount,
-		Signers:               toOnchainPublicKeys(c.cfg.Config.Signers),
-		Transmitters:          toOCRAccounts(c.cfg.Config.Transmitters),
-		F:                     c.cfg.Config.F,
-		OnchainConfig:         []byte{},
-		OffchainConfigVersion: c.cfg.Config.OffchainConfigVersion,
-		OffchainConfig:        c.cfg.Config.OffchainConfig,
-	}, nil
+	return c.contractConfig(), nil
 }
 
 // LatestConfigDetails implements types.ContractConfigTracker.
@@ -44,6 +36,25 @@ func (c *configTracker) LatestConfigDetails(ctx context.Context) (changedInBlock
 // Notify implements types.ContractConfigTracker.
 func (c *configTracker) Notify() <-chan struct{} {
 	return nil
+}
+
+func (c *configTracker) contractConfig() types.ContractConfig {
+	return types.ContractConfig{
+		ConfigDigest:          c.cfg.ConfigDigest,
+		ConfigCount:           c.cfg.ConfigCount,
+		Signers:               toOnchainPublicKeys(c.cfg.Config.Signers),
+		Transmitters:          toOCRAccounts(c.cfg.Config.Transmitters),
+		F:                     c.cfg.Config.F,
+		OnchainConfig:         []byte{},
+		OffchainConfigVersion: c.cfg.Config.OffchainConfigVersion,
+		OffchainConfig:        c.cfg.Config.OffchainConfig,
+	}
+}
+
+// PublicConfig returns the OCR configuration as a PublicConfig so that we can
+// access ReportingPluginConfig and other fields prior to launching the plugins.
+func (c *configTracker) PublicConfig() (ocr3confighelper.PublicConfig, error) {
+	return ocr3confighelper.PublicConfigFromContractConfig(false, c.contractConfig())
 }
 
 func toOnchainPublicKeys(signers [][]byte) []types.OnchainPublicKey {
