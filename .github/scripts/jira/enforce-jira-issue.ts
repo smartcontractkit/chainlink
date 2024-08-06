2026b034ae3e112dbc953012c2cd4f01a8b4ed58
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-08-06 16:19:10 -0400

    Create issue enforcement option

---
+++ .github/scripts/jira/enforce-jira-issue.ts
@@ -0,0 +1,78 @@
+import * as core from "@actions/core";
+import jira from "jira.js";
+import { createJiraClient, parseIssueNumberFrom } from "./lib";
+
+async function doesIssueExist(
+  client: jira.Version3Client,
+  issueNumber: string,
+  dryRun: boolean
+) {
+  const payload = {
+    issueIdOrKey: issueNumber,
+  };
+
+  if (dryRun) {
+    core.info("Dry run enabled, skipping JIRA issue enforcement");
+    return true;
+  }
+
+  try {
+    /**
+     * The issue is identified by its ID or key, however, if the identifier doesn't match an issue, a case-insensitive search and check for moved issues is performed.
+     * If a matching issue is found its details are returned, a 302 or other redirect is not returned. The issue key returned in the response is the key of the issue found.
+     */
+    const issue = await client.issues.getIssue(payload);
+    core.debug(
+      `JIRA issue id:${issue.id} key: ${issue.key} found while querying for ${issueNumber}`
+    );
+    if (issue.key !== issueNumber) {
+      core.error(
+        `JIRA issue key ${issueNumber} not found, but found issue key ${issue.key} instead`
+      );
+      return false;
+    }
+
+    return true;
+  } catch (e) {
+    core.debug(e);
+    core.setFailed(`JIRA issue ${issueNumber} not found`);
+    return false;
+  }
+}
+
+async function main() {
+  const prTitle = process.env.PR_TITLE;
+  const commitMessage = process.env.COMMIT_MESSAGE;
+  const branchName = process.env.BRANCH_NAME;
+  const dryRun = !!process.env.DRY_RUN;
+  const client = createJiraClient();
+
+  // Checks for the Jira issue number and exit if it can't find it
+  const issueNumber = parseIssueNumberFrom(prTitle, commitMessage, branchName);
+  if (!issueNumber) {
+    const msg =
+      "No JIRA issue number found in: PR title, commit message, or branch name. Please include the issue ID in one of these.";
+
+    core.setFailed(msg);
+    return;
+  }
+
+  const exists = await doesIssueExist(client, issueNumber, dryRun);
+  if (!exists) {
+    core.setFailed(`JIRA issue ${issueNumber} not found`);
+    return;
+  }
+}
+
+async function run() {
+  try {
+    await main();
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+    core.setFailed(error);
+  }
+}
+
+run();
