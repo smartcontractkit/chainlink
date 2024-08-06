88e54cddcc193d1389249d25b38ea475749b74a3
Aleksandr Bukata <aleksandr.bukata@smartcontract.com>
2024-08-06 19:02:18 +0100

    review

--- core/services/headreporter/head_reporter_test.go
+++ core/services/headreporter/head_reporter_test.go
@@ -1,85 +1,37 @@
-package headreporter_test
+package headreporter
 
 import (
 	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/headtracker"
-
-	"github.com/jmoiron/sqlx"
-
-	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/gas"
-	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
-	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/txmgr"
-	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
-	ubig "github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils/big"
-	"github.com/smartcontractkit/chainlink/v2/core/chains/legacyevm"
-	"github.com/smartcontractkit/chainlink/v2/core/internal/cltest"
-	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/configtest"
-	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/evmtest"
-
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
-	"github.com/smartcontractkit/chainlink/v2/core/internal/mocks"
+	evmtypes "github.com/smartcontractkit/chainlink/v2/core/chains/evm/types"
+	ubig "github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils/big"
+
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/pgtest"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
-	"github.com/smartcontractkit/chainlink/v2/core/services/headreporter"
 )
 
-func newHead() evmtypes.Head {
+func NewHead() evmtypes.Head {
 	return evmtypes.Head{Number: 42, EVMChainID: ubig.NewI(0)}
-}
-
-func newLegacyChainContainer(t *testing.T, db *sqlx.DB) legacyevm.LegacyChainContainer {
-	config, dbConfig, evmConfig := txmgr.MakeTestConfigs(t)
-	keyStore := cltest.NewKeyStore(t, db).Eth()
-	ethClient := evmtest.NewEthClientMockWithDefaultChain(t)
-	estimator, err := gas.NewEstimator(logger.TestLogger(t), ethClient, config, evmConfig.GasEstimator())
-	require.NoError(t, err)
-	lggr := logger.TestLogger(t)
-	lpOpts := logpoller.Opts{
-		PollPeriod:               100 * time.Millisecond,
-		FinalityDepth:            2,
-		BackfillBatchSize:        3,
-		RpcBatchSize:             2,
-		KeepFinalizedBlocksDepth: 1000,
-	}
-	ht := headtracker.NewSimulatedHeadTracker(ethClient, lpOpts.UseFinalityTag, lpOpts.FinalityDepth)
-	lp := logpoller.NewLogPoller(logpoller.NewORM(testutils.FixtureChainID, db, lggr), ethClient, lggr, ht, lpOpts)
-
-	txm, err := txmgr.NewTxm(
-		db,
-		evmConfig,
-		evmConfig.GasEstimator(),
-		evmConfig.Transactions(),
-		nil,
-		dbConfig,
-		dbConfig.Listener(),
-		ethClient,
-		lggr,
-		lp,
-		keyStore,
-		estimator)
-	require.NoError(t, err)
-
-	cfg := configtest.NewGeneralConfig(t, nil)
-	return cltest.NewLegacyChainsWithMockChainAndTxManager(t, ethClient, cfg, txm)
 }
 
 func Test_HeadReporterService(t *testing.T) {
 	t.Run("report everything", func(t *testing.T) {
 		db := pgtest.NewSqlxDB(t)
 
-		headReporter := mocks.NewHeadReporter(t)
-		service := headreporter.NewHeadReporterServiceWithReporters(db, newLegacyChainContainer(t, db), logger.TestLogger(t), []headreporter.HeadReporter{headReporter}, time.Second)
+		headReporter := NewMockHeadReporter(t)
+		service := NewHeadReporterService(db, logger.TestLogger(t), headReporter)
+		service.reportPeriod = time.Second
 		err := service.Start(testutils.Context(t))
 		require.NoError(t, err)
 
 		var reportCalls atomic.Int32
-		head := newHead()
+		head := NewHead()
 		headReporter.On("ReportNewHead", mock.Anything, &head).Run(func(args mock.Arguments) {
 			reportCalls.Add(1)
 		}).Return(nil)
--- core/services/telemetry/common.go
+++ core/services/telemetry/common.go
@@ -6,7 +6,6 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/synchronization"
 )
 
-//go:generate mockery --quiet --name MonitoringEndpointGenerator --output ./mocks --case=underscore
 type MonitoringEndpointGenerator interface {
 	GenMonitoringEndpoint(network string, chainID string, contractID string, telemType synchronization.TelemetryType) ocrtypes.MonitoringEndpoint
 }
