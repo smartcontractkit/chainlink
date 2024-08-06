99d5305cac172f3c3d7d1a04fe90a1db20539d58
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-08-06 17:47:02 +0200

    remove everything, but the deleted contract

--- contracts/src/v0.8/shared/interfaces/deeper/AccessControllerInterfaceNew.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// solhint-disable-next-line interface-starts-with-i
-interface AccessControllerInterface {
-  function hasAccess(address user, bytes calldata data) external view returns (bool);
-}
