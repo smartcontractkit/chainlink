1b604a696298d1e6b9093342f96c271ff618af9b
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-08-07 17:39:20 -0400

    TEST: Trigger solidity jira

--- contracts/src/v0.8/Flags.sol
+++ contracts/src/v0.8/Flags.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {SimpleReadAccessController} from "./shared/access/SimpleReadAccessController.sol";
 import {AccessControllerInterface} from "./shared/interfaces/AccessControllerInterface.sol";
+// 
 import {FlagsInterface} from "./interfaces/FlagsInterface.sol";
 
 /**
