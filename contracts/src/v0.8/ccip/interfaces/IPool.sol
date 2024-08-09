169695725f200a39881a1c1ef5c0ade6ff794981
Rens Rooimans <github@rensrooimans.nl>
2024-08-09 11:02:46 +0200

    undo transferFrom, assert balance in offRamp (#1273)
    ## Motivation
    It is significantly cheaper to assert balances than do the transferFrom
    
    ## Solution
    Undo recent approve changes, add balance assertions
    
    
    had to undo some of the multiOfframp calldata changes to make it fit
    
    ---------
    
    Co-authored-by: app-token-issuer-infra-releng[bot] <120227048+app-token-issuer-infra-releng[bot]@users.noreply.github.com>

--- contracts/src/v0.8/ccip/interfaces/IPool.sol
+++ contracts/src/v0.8/ccip/interfaces/IPool.sol
@@ -19,6 +19,8 @@ interface IPoolV1 is IERC165 {
   /// @param releaseOrMintIn All data required to release or mint tokens.
   /// @return releaseOrMintOut The amount of tokens released or minted on the local chain, denominated
   /// in the local token's decimals.
+  /// @dev The offramp asserts that the balanceOf of the receiver has been incremented by exactly the number
+  /// of tokens that is returned in ReleaseOrMintOutV1.destinationAmount. If the amounts do not match, the tx reverts.
   function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
     external
     returns (Pool.ReleaseOrMintOutV1 memory);
