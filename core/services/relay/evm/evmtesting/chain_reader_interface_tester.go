6f8ed239168dd9bbca3660ea7ec54aea027f0eca
Farber98 <juan.farber@smartcontract.com>
2024-08-14 14:01:53 -0300

    move start outside getChainReader

--- core/services/relay/evm/evmtesting/chain_reader_interface_tester.go
+++ core/services/relay/evm/evmtesting/chain_reader_interface_tester.go
@@ -231,7 +231,6 @@ func (it *EVMChainReaderInterfaceTester[T]) GetChainReader(t T) clcommontypes.Co
 
 	cr, err := evm.NewChainReaderService(ctx, lggr, lp, ht, it.client, conf)
 	require.NoError(t, err)
-	require.NoError(t, cr.Start(ctx))
 	it.cr = cr
 	return cr
 }
