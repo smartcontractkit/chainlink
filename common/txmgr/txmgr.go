73d12a25abc5de1a5b3d90c66a8d0b5a20f9d625
Silas Lenihan <sjl@lenihan.net>
2024-08-16 17:45:35 -0400

    update comment

--- common/txmgr/txmgr.go
+++ common/txmgr/txmgr.go
@@ -654,7 +654,7 @@ func (b *Txm[CHAIN_ID, HEAD, ADDR, TX_HASH, BLOCK_HASH, R, SEQ, FEE]) GetTransac
 	}
 	switch tx.State {
 	case TxUnconfirmed, TxConfirmedMissingReceipt:
-		// Return unconfirmed for ConfirmedMissingReceipt since a receipt is required to determine if it is finalized
+		// Return pending for ConfirmedMissingReceipt since a receipt is required to determine if it is finalized
 		return commontypes.Pending, nil
 	case TxConfirmed:
 		// Return unconfirmed for confirmed transactions because they are not yet finalized
