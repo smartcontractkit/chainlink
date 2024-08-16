078a976363284a61a09728687f533107f4c562d9
Rens Rooimans <github@rensrooimans.nl>
2024-08-16 17:30:52 +0200

    add getPreviousPool (#1312)
    ## Motivation
    getPreviousPool was missing 
    
    ## Solution
    
    Add getPreviousPool

--- contracts/src/v0.8/ccip/pools/LegacyPoolWrapper.sol
+++ contracts/src/v0.8/ccip/pools/LegacyPoolWrapper.sol
@@ -55,6 +55,11 @@ abstract contract LegacyPoolWrapper is TokenPool {
     emit LegacyPoolChanged(oldPrevPool, prevPool);
   }
 
+  /// @notice Returns the address of the previous pool.
+  function getPreviousPool() external view returns (address) {
+    return address(s_previousPool);
+  }
+
   function _hasLegacyPool() internal view returns (bool) {
     return address(s_previousPool) != address(0);
   }
