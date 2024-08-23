2ff3892e44de1aa26f717df4af2cd50e4322bcd4
jlaveracll <joaquin.lavera@smartcontract.com>
2024-08-23 19:57:52 -0300

    intial commit

---
+++ contracts/src/v0.8/l2ep/dev/zksync/ZKSyncValidator.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AggregatorValidatorInterface} from "../../../shared/interfaces/AggregatorValidatorInterface.sol";
+import {TypeAndVersionInterface} from "../../../interfaces/TypeAndVersionInterface.sol";
+import {ZKSyncSequencerUptimeFeedInterface} from "./../interfaces/ZKSyncSequencerUptimeFeedInterface.sol";
+
+import {SimpleWriteAccessController} from "../../../shared/access/SimpleWriteAccessController.sol";
+
+import {IBridgehub} from "@zksync/contracts/l1-contracts/contracts/bridgehub/IBridgehub.sol";
+import {L2Message} from "@zksync/contracts/l1-contracts/contracts/common/Messaging.sol";
+
+/**
+ * @title ZKSyncValidator - makes cross chain call to update the Sequencer Uptime Feed on L2
+ */
+contract ZKSyncValidator is TypeAndVersionInterface, AggregatorValidatorInterface, SimpleWriteAccessController {
+  int256 private constant ANSWER_SEQ_OFFLINE = 1;
+  uint32 private s_gasLimit;
+
+  // solhint-disable-next-line chainlink-solidity/prefix-immutable-variables-with-i
+  address public immutable L1_CROSS_DOMAIN_MESSENGER_ADDRESS;
+  // solhint-disable-next-line chainlink-solidity/prefix-immutable-variables-with-i
+  address public immutable L2_UPTIME_FEED_ADDR;
+
+  /**
+   * @notice emitted when gas cost to spend on L2 is updated
+   * @param gasLimit updated gas cost
+   */
+  event GasLimitUpdated(uint32 gasLimit);
+
+  /**
+   * @param l1CrossDomainMessengerAddress address the Bridgehub contract address
+   * @param l2UptimeFeedAddr the address of the OptimismSequencerUptimeFeed contract address
+   * @param gasLimit the gasLimit to use for sending a message from L1 to L2
+   */
+  constructor(address l1CrossDomainMessengerAddress, address l2UptimeFeedAddr, uint32 gasLimit) {
+    // solhint-disable-next-line gas-custom-errors
+    require(l1CrossDomainMessengerAddress != address(0), "Invalid xDomain Messenger address");
+    // solhint-disable-next-line gas-custom-errors
+    require(l2UptimeFeedAddr != address(0), "Invalid OptimismSequencerUptimeFeed contract address");
+    L1_CROSS_DOMAIN_MESSENGER_ADDRESS = l1CrossDomainMessengerAddress;
+    L2_UPTIME_FEED_ADDR = l2UptimeFeedAddr;
+    s_gasLimit = gasLimit;
+  }
+
+  /**
+   * @notice versions:
+   *
+   * - ZKSyncValidator 0.1.0: initial release
+   * - ZKSyncValidator 1.0.0: change target of L2 sequencer status update
+   *   - now calls `updateStatus` on an L2 OptimismSequencerUptimeFeed contract instead of
+   *     directly calling the Flags contract
+   *
+   * @inheritdoc TypeAndVersionInterface
+   */
+  function typeAndVersion() external pure virtual override returns (string memory) {
+    return "ZKSyncValidator 1.0.0";
+  }
+
+  /**
+   * @notice sets the new gas cost to spend when sending cross chain message
+   * @param gasLimit the updated gas cost
+   */
+  function setGasLimit(uint32 gasLimit) external onlyOwner {
+    s_gasLimit = gasLimit;
+    emit GasLimitUpdated(gasLimit);
+  }
+
+  /**
+   * @notice fetches the gas cost of sending a cross chain message
+   */
+  function getGasLimit() external view returns (uint32) {
+    return s_gasLimit;
+  }
+
+  /**
+   * @notice validate method sends an xDomain L2 tx to update Uptime Feed contract on L2.
+   * @dev A message is sent using the Bridgehub. This method is accessed controlled.
+   * @param currentAnswer new aggregator answer - value of 1 considers the sequencer offline.
+   */
+  function validate(
+    uint256 /* previousRoundId */,
+    int256 /* previousAnswer */,
+    uint256 /* currentRoundId */,
+    int256 currentAnswer
+  ) external override checkAccess returns (bool) {
+    // Encode the OptimismSequencerUptimeFeed call
+    bytes4 selector = ZKSyncSequencerUptimeFeedInterface.updateStatus.selector;
+    bool status = currentAnswer == ANSWER_SEQ_OFFLINE;
+    uint64 timestamp = uint64(block.timestamp);
+    // Encode `status` and `timestamp`
+    bytes memory message = abi.encodeWithSelector(selector, status, timestamp);
+    // Make the xDomain call
+    L2Message memory _message = L2Message({
+      txNumberInBatch: 0,
+      sender: 0,
+      data: message,
+
+      // target: L2_UPTIME_FEED_ADDR,
+      // gasLimit: s_gasLimit,
+      // data: message
+    });
+    IBridgehub(L1_CROSS_DOMAIN_MESSENGER_ADDRESS).proveL2MessageInclusion(
+      _chainId, 
+      _batchNumber, 
+      _index, 
+      _message, 
+      _proof
+    );
+    // .sendMessage(
+    //   L2_UPTIME_FEED_ADDR, // target
+    //   message,
+    //   s_gasLimit
+    // );
+    // // return success
+    return true;
+  }
+}
