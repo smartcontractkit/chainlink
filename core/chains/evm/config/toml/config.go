4ce5d5fbe9cba6c31bdaee2029bae90ef6a348f4
AnieeG <anindita.ghosh@smartcontract.com>
2024-08-22 23:02:11 -0700

    deployment package inclusion

--- core/chains/evm/config/toml/config.go
+++ core/chains/evm/config/toml/config.go
@@ -549,12 +549,12 @@ type GasEstimator struct {
 	PriceMax     *assets.Wei
 	PriceMin     *assets.Wei
 
-	LimitDefault       *uint64
-	LimitMax           *uint64
-	LimitMultiplier    *decimal.Decimal
-	LimitTransfer      *uint64
-	LimitJobType       GasLimitJobType `toml:",omitempty"`
-	EstimateGasLimit   *bool
+	LimitDefault     *uint64
+	LimitMax         *uint64
+	LimitMultiplier  *decimal.Decimal
+	LimitTransfer    *uint64
+	LimitJobType     GasLimitJobType `toml:",omitempty"`
+	EstimateGasLimit *bool
 
 	BumpMin       *assets.Wei
 	BumpPercent   *uint16
