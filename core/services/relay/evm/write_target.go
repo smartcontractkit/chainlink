0272eecca2eee2a4442e0d95e2896d63a09304ff
Farber98 <juan.farber@smartcontract.com>
2024-08-24 11:18:18 -0300

    fix conflict

--- core/services/relay/evm/write_target.go
+++ core/services/relay/evm/write_target.go
@@ -52,11 +52,6 @@ func NewWriteTarget(ctx context.Context, relayer *Relayer, chain legacyevm.Chain
 		return nil, err
 	}
 
-	err = cr.Start(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	chainWriterConfig := relayevmtypes.ChainWriterConfig{
 		Contracts: map[string]*relayevmtypes.ContractConfig{
 			"forwarder": {
