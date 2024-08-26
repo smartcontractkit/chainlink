c00ac968e651fd7b09f473d20f0fe4755ba57367
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2024-08-26 14:19:43 -0700

    Ccip-3076 deployment package (#14209)
    * deployment package inclusion
    
    * go mod
    
    * Add README
    
    * Add changeset
    
    * Fix changeset
    
    * change confirm function
    
    * ignore linter for now
    
    * core generate
    
    * add todo ticket
    
    * fix test
    
    ---------
    
    Co-authored-by: Lukasz <120112546+lukaszcl@users.noreply.github.com>
    Co-authored-by: connorwstein <connor.stein@mail.mcgill.ca>

---
+++ integration-tests/deployment/memory/environment.go
@@ -0,0 +1,139 @@
+package memory
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hashicorp/consul/sdk/freeport"
+	chainsel "github.com/smartcontractkit/chain-selectors"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/smartcontractkit/chainlink/integration-tests/deployment"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+)
+
+const (
+	Memory = "memory"
+)
+
+type MemoryEnvironmentConfig struct {
+	Chains         int
+	Nodes          int
+	Bootstraps     int
+	RegistryConfig RegistryConfig
+}
+
+// Needed for environment variables on the node which point to prexisitng addresses.
+// i.e. CapReg.
+func NewMemoryChains(t *testing.T, numChains int) map[uint64]deployment.Chain {
+	mchains := GenerateChains(t, numChains)
+	chains := make(map[uint64]deployment.Chain)
+	for cid, chain := range mchains {
+		sel, err := chainsel.SelectorFromChainId(cid)
+		require.NoError(t, err)
+		chains[sel] = deployment.Chain{
+			Selector:    sel,
+			Client:      chain.Backend,
+			DeployerKey: chain.DeployerKey,
+			Confirm: func(tx common.Hash) (uint64, error) {
+				for {
+					chain.Backend.Commit()
+					receipt, err := chain.Backend.TransactionReceipt(context.Background(), tx)
+					if err != nil {
+						t.Log("failed to get receipt", err)
+						continue
+					}
+					if receipt.Status == 0 {
+						t.Logf("Status (reverted) %d for txhash %s\n", receipt.Status, tx.String())
+					}
+					return receipt.BlockNumber.Uint64(), nil
+				}
+			},
+		}
+	}
+	return chains
+}
+
+func NewNodes(t *testing.T, logLevel zapcore.Level, chains map[uint64]deployment.Chain, numNodes, numBootstraps int, registryConfig RegistryConfig) map[string]Node {
+	mchains := make(map[uint64]EVMChain)
+	for _, chain := range chains {
+		evmChainID, err := chainsel.ChainIdFromSelector(chain.Selector)
+		if err != nil {
+			t.Fatal(err)
+		}
+		mchains[evmChainID] = EVMChain{
+			Backend:     chain.Client.(*backends.SimulatedBackend),
+			DeployerKey: chain.DeployerKey,
+		}
+	}
+	nodesByPeerID := make(map[string]Node)
+	ports := freeport.GetN(t, numNodes)
+	var existingNumBootstraps int
+	for i := 0; i < numNodes; i++ {
+		bootstrap := false
+		if existingNumBootstraps < numBootstraps {
+			bootstrap = true
+			existingNumBootstraps++
+		}
+		node := NewNode(t, ports[i], mchains, logLevel, bootstrap, registryConfig)
+		nodesByPeerID[node.Keys.PeerID.String()] = *node
+		// Note in real env, this ID is allocated by JD.
+	}
+	return nodesByPeerID
+}
+
+func NewMemoryEnvironmentFromChainsNodes(t *testing.T,
+	lggr logger.Logger,
+	chains map[uint64]deployment.Chain,
+	nodes map[string]Node) deployment.Environment {
+	var nodeIDs []string
+	for id := range nodes {
+		nodeIDs = append(nodeIDs, id)
+	}
+	return deployment.Environment{
+		Name:     Memory,
+		Offchain: NewMemoryJobClient(nodes),
+		// Note these have the p2p_ prefix.
+		NodeIDs: nodeIDs,
+		Chains:  chains,
+		Logger:  lggr,
+	}
+}
+
+//func NewMemoryEnvironmentExistingChains(t *testing.T, lggr logger.Logger,
+//	chains map[uint64]deployment.Chain, config MemoryEnvironmentConfig) deployment.Environment {
+//	nodes := NewNodes(t, chains, config.Nodes, config.Bootstraps, config.RegistryConfig)
+//	var nodeIDs []string
+//	for id := range nodes {
+//		nodeIDs = append(nodeIDs, id)
+//	}
+//	return deployment.Environment{
+//		Name:     Memory,
+//		Offchain: NewMemoryJobClient(nodes),
+//		// Note these have the p2p_ prefix.
+//		NodeIDs: nodeIDs,
+//		Chains:  chains,
+//		Logger:  lggr,
+//	}
+//}
+
+// To be used by tests and any kind of deployment logic.
+func NewMemoryEnvironment(t *testing.T, lggr logger.Logger, logLevel zapcore.Level, config MemoryEnvironmentConfig) deployment.Environment {
+	chains := NewMemoryChains(t, config.Chains)
+	nodes := NewNodes(t, logLevel, chains, config.Nodes, config.Bootstraps, config.RegistryConfig)
+	var nodeIDs []string
+	for id := range nodes {
+		nodeIDs = append(nodeIDs, id)
+	}
+	return deployment.Environment{
+		Name:     Memory,
+		Offchain: NewMemoryJobClient(nodes),
+		NodeIDs:  nodeIDs,
+		Chains:   chains,
+		Logger:   lggr,
+	}
+}
