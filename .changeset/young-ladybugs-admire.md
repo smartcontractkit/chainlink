64f1203699ee767179118d716445800b2f3a44b6
asoliman <abdelrahman.soliman@smartcontract.com>
2024-08-27 21:24:04 +0400

    Remove accidental package.json merge

--- .changeset/young-ladybugs-admire.md
+++ .changeset/young-ladybugs-admire.md
@@ -1,5 +1,5 @@
 ---
-"ccip": minor
+"chainlink": minor
 ---
 
 #updated move latest capabilities code from ccip repo to chainlink repo [CCIP-2946]
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -18,16 +18,14 @@ const COMPILER_SETTINGS = {
 
 // prune forge style tests from hardhat paths
 subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
-  async (_, __, runSuper) => {
-    const paths = await runSuper()
-    const noTests = paths.filter((p: string) => !p.endsWith('.t.sol'))
-    const noCCIPTests = noTests.filter(
-      (p: string) => !p.includes('/v0.8/ccip/test'),
-    )
-    return noCCIPTests.filter(
-      (p: string) => !p.includes('src/v0.8/vendor/forge-std'),
-    )
-  },
+    async (_, __, runSuper) => {
+      const paths = await runSuper()
+      const noTests = paths.filter((p: string) => !p.endsWith('.t.sol'))
+      const noCCIP = noTests.filter((p: string) => !p.includes('/v0.8/ccip'))
+      return noCCIP.filter(
+          (p: string) => !p.includes('src/v0.8/vendor/forge-std'),
+      )
+    },
 )
 
 /**
@@ -41,8 +39,8 @@ let config = {
   paths: {
     artifacts: './artifacts',
     cache: './cache',
-    sources: './src/v0.8',
-    tests: './test/v0.8',
+    sources: './src',
+    tests: './test',
   },
   typechain: {
     outDir: './typechain',
@@ -54,7 +52,7 @@ let config = {
     },
     hardhat: {
       allowUnlimitedContractSize: Boolean(
-        process.env.ALLOW_UNLIMITED_CONTRACT_SIZE,
+          process.env.ALLOW_UNLIMITED_CONTRACT_SIZE,
       ),
       hardfork: 'merge',
     },
