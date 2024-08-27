3e256b12565e2919f51f5e95ad50f35b4fadbfa2
asoliman <abdelrahman.soliman@smartcontract.com>
2024-08-27 15:58:19 +0400

    Fixing dependencies

--- core/services/ocr2/plugins/ccip/internal/ccipdata/factory/onramp.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/factory/onramp.go
@@ -5,9 +5,9 @@ import (
 
 	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccip"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	ccipconfig "github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/config"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipcalc"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata"
