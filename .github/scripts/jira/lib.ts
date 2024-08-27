ca7b95841843e8e315d0be3c35b4911310cbc175
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-08-27 14:58:19 -0700

    RE 2877 Jira<->Changeset traceability for solidity changes (#14141)
    * Migrate contract changeset handling to solidity-jira
    
    * Update solidity-jira to handle jira tracability as a whole
    
    * Fix changesets output typo
    
    * Make file paths absolute
    
    * Prevent merge commits from being added by auto commit action
    
    * Add issue number to tail of document rather than head
    
    * Tighten up file pattern for committing
    
    * Add GATI so workflows run on auto commits
    
    * Modify workflow to generate jira traceabillity
    
    * Add checkout
    
    * Fix typo
    
    * Add better naming for job
    
    * Add more logging
    
    * Append to step summary from within script
    
    * Extract functions and use labels over individual issues
    
    * Fix comments handling
    
    * Use plain artifacts URL for action
    
    * Add test for jira issues in the middle of comments
    
    * Formatting
    
    * Actually write to step summary
    
    * Make jira host public
    
    * Handle csv output rather than JSON
    
    * Fix typo in changeset reference
    
    * Comment out broken step
    
    * Notify that auto commits are made via bot
    
    * Pin planetscale/ghcommit-action
    
    * Rename
    
    * Add pull-requests write perm for comments
    
    * Add always() to gha metrics
    
    * Use env var for head_ref
    
    * Formatting
    
    * Use JIRA_HOST rather than hard coded URL

--- .github/scripts/jira/lib.ts
+++ .github/scripts/jira/lib.ts
@@ -1,6 +1,51 @@
+import { readFile } from "fs/promises";
+import * as core from "@actions/core";
+import * as jira from "jira.js";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { join } from "path";
 
-import * as core from '@actions/core'
-import * as jira from 'jira.js'
+export function generateJiraIssuesLink(baseUrl: string, label: string) {
+  // https://smartcontract-it.atlassian.net/issues/?jql=labels%20%3D%20%22review-artifacts-automation-base%3A8d818ea265ff08887e61ace4f83364a3ee149ef0-head%3A3c45b71f3610de28f429cef0163936eaa448e63c%22
+  const jqlQuery = `labels = "${label}"`;
+  const fullUrl = new URL(baseUrl);
+  fullUrl.searchParams.set("jql", jqlQuery);
+
+  const urlStr = fullUrl.toString();
+  core.info(`Jira issues link: ${urlStr}`);
+  return urlStr;
+}
+
+export function generateIssueLabel(
+  product: string,
+  baseRef: string,
+  headRef: string
+) {
+  return `review-artifacts-${product}-base:${baseRef}-head:${headRef}`;
+}
+
+export async function getGitTopLevel(): Promise<string> {
+  const execPromise = promisify(exec);
+  try {
+    const { stdout, stderr } = await execPromise(
+      "git rev-parse --show-toplevel"
+    );
+
+    if (stderr) {
+      const msg = `Error in command output: ${stderr}`;
+      core.error(msg);
+      throw Error(msg);
+    }
+
+    const topLevelDir = stdout.trim();
+    core.info(`Top-level directory: ${topLevelDir}`);
+    return topLevelDir;
+  } catch (error) {
+    const msg = `Error executing command: ${(error as any).message}`;
+    core.error(msg);
+    throw Error(msg);
+  }
+}
 
 /**
  * Given a list of strings, this function will return the first JIRA issue number it finds.
@@ -24,6 +69,24 @@ export function parseIssueNumberFrom(
   return parsed[0];
 }
 
+export async function extractJiraIssueNumbersFrom(filePaths: string[]) {
+  const issueNumbers: string[] = [];
+  const gitTopLevel = await getGitTopLevel();
+
+  for (const path of filePaths) {
+    const fullPath = join(gitTopLevel, path);
+    core.info(`Reading file: ${fullPath}`);
+    const content = await readFile(fullPath, "utf-8");
+    const issueNumber = parseIssueNumberFrom(content);
+    core.info(`Extracted issue number: ${issueNumber}`);
+    if (issueNumber) {
+      issueNumbers.push(issueNumber);
+    }
+  }
+
+  return issueNumbers;
+}
+
 /**
  * Converts an array of tags to an array of labels.
  *
@@ -39,7 +102,7 @@ export function tagsToLabels(tags: string[]) {
   }));
 }
 
-export function createJiraClient() {
+export function getJiraEnvVars() {
   const jiraHost = process.env.JIRA_HOST;
   const jiraUserName = process.env.JIRA_USERNAME;
   const jiraApiToken = process.env.JIRA_API_TOKEN;
@@ -51,6 +114,11 @@ export function createJiraClient() {
     process.exit(1);
   }
 
+  return { jiraHost, jiraUserName, jiraApiToken };
+}
+
+export function createJiraClient() {
+  const { jiraHost, jiraUserName, jiraApiToken } = getJiraEnvVars();
   return new jira.Version3Client({
     host: jiraHost,
     authentication: {
