8572a7ad10dc024621d87d0b4555fa081c4d1da0
Anirudh Warrier <12178754+anirudhwarrier@users.noreply.github.com>
2024-08-28 21:02:13 +0400

    bump anvil version used in load tests (#1386)
    ## Motivation
    
    During load tests, when anvil node restarts, block history is lost.
    Identified this to be a bug in the pinned version of anvil.
    
    
    ## Solution
    
    Bump anvil version to latest nightly.

--- integration-tests/ccip-tests/testsetups/test_env.go
+++ integration-tests/ccip-tests/testsetups/test_env.go
@@ -490,8 +490,7 @@ func DeployEnvironments(
 							"fullnameOverride": actions.NetworkName(network.Name),
 							"image": map[string]interface{}{
 								"repository": "ghcr.io/foundry-rs/foundry",
-								"tag":        "nightly-5ac78a9cd4b94dc53d1fe5e0f42372b28b5a7559",
-								//	"tag":        "nightly-ea2eff95b5c17edd3ffbdfc6daab5ce5cc80afc0",
+								"tag":        "nightly-2442e7a5fc165d7d0b022aa8b9f09dcdf675157b",
 							},
 							"anvil": map[string]interface{}{
 								"chainId":                   fmt.Sprintf("%d", network.ChainID),
