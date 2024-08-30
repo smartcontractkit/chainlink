53ef01c66a5c87fbff011b9f2d6cbe82c623684d
Blaž Hrastnik <blaz@mxxn.io>
2024-08-30 14:46:27 +0900

    keystone: OCR3Capability to store all pubkeys for multi-chain bundles

--- contracts/src/v0.8/keystone/ocr/OCR2Base.sol
+++ contracts/src/v0.8/keystone/ocr/OCR2Base.sol
@@ -30,33 +30,30 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
   }
   ConfigInfo internal s_configInfo;
 
-  // Used for s_oracles[a].role, where a is an address, to track the purpose
-  // of the address, or to indicate that the address is unset.
-  enum Role {
-    // No oracle role has been set for address a
-    Unset,
-    // Signing address for the s_oracles[a].index'th oracle. I.e., report
-    // signatures from this oracle should ecrecover back to address a.
-    Signer,
-    // Transmission address for the s_oracles[a].index'th oracle. I.e., if a
-    // report is received by OCR2Aggregator.transmit in which msg.sender is
-    // a, it is attributed to the s_oracles[a].index'th oracle.
-    Transmitter
-  }
-
-  struct Oracle {
-    uint8 index; // Index of oracle in s_signers/s_transmitters
-    Role role; // Role of the address which mapped to this struct
-  }
-
-  mapping(address signerOrTransmitter => Oracle) internal s_oracles;
+  /// @notice triggers a new run of the offchain reporting protocol
+  /// @param previousConfigBlockNumber block in which the previous config was set, to simplify historic analysis
+  /// @param configDigest configDigest of this configuration
+  /// @param configCount ordinal number of this config setting among all config settings over the life of this contract
+  /// @param signers ith element is address ith oracle uses to sign a report
+  /// @param transmitters ith element is address ith oracle uses to transmit a report via the transmit method
+  /// @param f maximum number of faulty/dishonest oracles the protocol can tolerate while still working correctly
+  /// @param onchainConfig serialized configuration used by the contract (and possibly oracles)
+  /// @param offchainConfigVersion version of the serialization format used for "offchainConfig" parameter
+  /// @param offchainConfig serialized configuration used by the oracles exclusively and only passed through the contract
+  event ConfigSet(
+    uint32 previousConfigBlockNumber,
+    bytes32 configDigest,
+    uint64 configCount,
+    bytes[] signers,
+    address[] transmitters,
+    uint8 f,
+    bytes onchainConfig,
+    uint64 offchainConfigVersion,
+    bytes offchainConfig
+  );
 
   // s_signers contains the signing address of each oracle
-  address[] internal s_signers;
-
-  // s_transmitters contains the transmission address of each oracle,
-  // i.e. the address the oracle actually sends transactions to the contract from
-  address[] internal s_transmitters;
+  bytes[] internal s_signers;
 
   /*
    * Config logic
@@ -70,14 +67,13 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
   ) {
     if (numSigners > MAX_NUM_ORACLES) revert InvalidConfig("too many signers");
     if (f == 0) revert InvalidConfig("f must be positive");
-    if (numSigners != numTransmitters) revert InvalidConfig("oracle addresses out of registration");
     if (numSigners <= 3 * f) revert InvalidConfig("faulty-oracle f too high");
     _;
   }
 
   // solhint-disable-next-line gas-struct-packing
   struct SetConfigArgs {
-    address[] signers;
+    bytes[] signers;
     address[] transmitters;
     uint8 f;
     bytes onchainConfig;
@@ -96,6 +92,17 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
     return (true, bytes32(0), uint32(0));
   }
 
+  // signer = [ 1 byte type | 2 byte len | n byte value ]...
+
+  function setConfig(
+    address[] memory _signers,
+    address[] memory _transmitters,
+    uint8 _f,
+    bytes memory _onchainConfig,
+    uint64 _offchainConfigVersion,
+    bytes memory _offchainConfig
+  ) external override checkConfigValid(_signers.length, _transmitters.length, _f) onlyOwner {}
+
   /**
    * @notice sets offchain reporting protocol configuration incl. participating oracles
    * @param _signers addresses with which oracles sign the reports
@@ -106,14 +113,14 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
    * @param _offchainConfig encoded off-chain oracle configuration
    */
   function setConfig(
-    address[] memory _signers,
-    address[] memory _transmitters,
+    bytes[] calldata _signers,
+    address[] calldata _transmitters, // TODO: remove, use fake static addrs to satisfy offchain
     uint8 _f,
     bytes memory _onchainConfig,
     uint64 _offchainConfigVersion,
     bytes memory _offchainConfig
-  ) external override checkConfigValid(_signers.length, _transmitters.length, _f) onlyOwner {
-    SetConfigArgs memory args = SetConfigArgs({
+  ) external checkConfigValid(_signers.length, _transmitters.length, _f) onlyOwner {
+    SetConfigArgs memory args = SetConfigArgs({ // TODO: is this saving gas? unused fields
       signers: _signers,
       transmitters: _transmitters,
       f: _f,
@@ -123,27 +130,33 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
     });
 
     while (s_signers.length != 0) {
-      // remove any old signer/transmitter addresses
-      uint256 lastIdx = s_signers.length - 1;
-      address signer = s_signers[lastIdx];
-      address transmitter = s_transmitters[lastIdx];
-      delete s_oracles[signer];
-      delete s_oracles[transmitter];
+      // remove any old signer addresses
       s_signers.pop();
-      s_transmitters.pop();
     }
 
     // Bounded by MAX_NUM_ORACLES in OCR2Abstract.sol
     for (uint256 i = 0; i < args.signers.length; i++) {
-      if (args.signers[i] == address(0)) revert InvalidConfig("signer must not be empty");
       if (args.transmitters[i] == address(0)) revert InvalidConfig("transmitter must not be empty");
-      // add new signer/transmitter addresses
-      if (s_oracles[args.signers[i]].role != Role.Unset) revert InvalidConfig("repeated signer address");
-      s_oracles[args.signers[i]] = Oracle(uint8(i), Role.Signer);
-      if (s_oracles[args.transmitters[i]].role != Role.Unset) revert InvalidConfig("repeated transmitter address");
-      s_oracles[args.transmitters[i]] = Oracle(uint8(i), Role.Transmitter);
+      // add new signers
+      bytes calldata publicKeys = _signers[i];
+      uint16 offset = 32; // skip the array length
+      uint16 len = uint16(publicKeys.length);
+
+      // chainlink/core/capabilities/ccip/ocrimpls/config_tracker.go TODO: libocr better signer validation for key subtypes checkIdentityListsHaveNoDuplicates
+      // after PublicConfigFromContractConfig call
+
+      // parse encoded public keys to validate uniqueness
+      // TODO: could we just trust libocr to ensure uniqueness?
+      while (offset < len) {
+        uint8 keyType = uint8(publicKeys[offset]);
+        uint16 keyLen = uint16(uint8(publicKeys[offset + 1])) + (uint16(uint8(publicKeys[offset + 2])) << 8);
+        bytes calldata publicKey = publicKeys[offset + 3:offset + 3 + keyLen];
+        offset += 3 + keyLen;
+        // TODO: uniq the signers() list
+        // if (s_oracles[args.signers[i]].role != Role.Unset) revert InvalidConfig("repeated signer address");
+      }
+      // TODO: remove transmitters altogether?
       s_signers.push(args.signers[i]);
-      s_transmitters.push(args.transmitters[i]);
     }
     s_configInfo.f = args.f;
     uint32 previousConfigBlockNumber = s_latestConfigBlockNumber;
@@ -181,7 +194,7 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
     uint256 _chainId,
     address _contractAddress,
     uint64 _configCount,
-    address[] memory _signers,
+    bytes[] memory _signers,
     address[] memory _transmitters,
     uint8 _f,
     bytes memory _onchainConfig,
@@ -221,13 +234,5 @@ abstract contract OCR2Base is ConfirmedOwner, OCR2Abstract {
     returns (uint32 configCount, uint32 blockNumber, bytes32 configDigest)
   {
     return (s_configCount, s_latestConfigBlockNumber, s_configInfo.latestConfigDigest);
-  }
-
-  /**
-   * @return list of addresses permitted to transmit reports to this contract
-   * @dev The list will match the order used to specify the transmitter during setConfig
-   */
-  function transmitters() external view returns (address[] memory) {
-    return s_transmitters;
   }
 }
