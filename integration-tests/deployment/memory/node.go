85b33fd9acbd342d25bd84804d08451ab2590b97
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2024-09-02 04:14:05 -0700

    ccip 3076/ccip - port deployment tooling  (#14298)
    * porting ccip tooling into chainlink
    
    * make deployment test run
    
    * add missing changes
    
    * fix build error
    
    * make private
    
    * more updates
    
    * changes
    
    * make lint happy
    
    * make lint happy
    
    * Use tests.WaitTimeout for CCIPReader e2e tests (#1382)
    
    We've noticed that this tests have failed a few times on the CI.
    
    I've run the tests with `go test ./... -count 100 -failfast` and it
    never failed locally.
    I assume it runs slow in CI that's why we have some failing cases, I am
    replacing `10s` with `tests.WaitTimeout(t)`.
    
    * core/capabilities/ccip: fix oracle startup logic (#1348)
    
    Ticket: https://smartcontract-it.atlassian.net/browse/CCIP-3078
    
    We can't start bootstrap oracles and plugin oracles on the same
    chainlink node. The main reason for this is that we have a singleton
    `SingletonPeerWrapper` object which manages all libocr peers for the
    application as a whole. Since this singleton only works with a single
    peer ID, it creates a single OCR peer, and all streams are created using
    this OCR peer. Since streams must have unique config digests for the
    same peer object, running a bootstrap oracle and a plugin oracle for the
    same config digest will fail because the stream will not be created.
    
    In order to remedy this we match what will be the case in production,
    which is:
    
    * Separate bootstrap node, with a peer ID that is either part of the OCR
    committee or not (most likely the latter, in order to avoid exporting /
    importing the P2P key from one node's DB to another). This bootstrap
    node will have to have a DNS name and be publicly accessible over the
    internet in order to be accessed by all nodes in the committee, at least
    initially, to facilitate peer discovery.
    * Plugin node that is more locked down, i.e no public DNS name required.
    
    To enable this, in this PR we:
    
    * Tweak the `launcher` component to launch bootstrap nodes only or
    plugin nodes only, and not both. This does NOT rely on the on-chain
    bootstrap P2P IDs that are set in the OCR config. This field will be
    removed in subsequent PRs.
    * Tweak the `OracleCreator` interface to support the above operation
    
    Follow ups:
    * Remove the `bootstrapP2PIds` field from the OCR config in
    CCIPConfig.sol
    
    * Add changeset
    
    ---------
    
    Co-authored-by: dimitris <dimitrios.kouveris@smartcontract.com>
    Co-authored-by: Makram <makramkd@users.noreply.github.com>
    Co-authored-by: asoliman <abdelrahman.soliman@smartcontract.com>
    Co-authored-by: Abdelrahman Soliman (Boda) <2677789+asoliman92@users.noreply.github.com>

--- integration-tests/deployment/memory/node.go
+++ integration-tests/deployment/memory/node.go
@@ -13,14 +13,15 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	gethtypes "github.com/ethereum/go-ethereum/core/types"
 	chainsel "github.com/smartcontractkit/chain-selectors"
-
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
+
+	coretypes "github.com/smartcontractkit/chainlink-common/pkg/types/core/mocks"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/config"
 	"github.com/smartcontractkit/chainlink-common/pkg/loop"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/mailbox"
-	evmcapabilities "github.com/smartcontractkit/chainlink/v2/core/capabilities"
+
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/assets"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
 	v2toml "github.com/smartcontractkit/chainlink/v2/core/chains/evm/config/toml"
@@ -168,7 +169,7 @@ func NewNode(
 		Logger:               lggr,
 		LoopRegistry:         plugins.NewLoopRegistry(lggr.Named("LoopRegistry"), cfg.Tracing()),
 		GRPCOpts:             loop.GRPCOpts{},
-		CapabilitiesRegistry: evmcapabilities.NewRegistry(lggr),
+		CapabilitiesRegistry: coretypes.NewCapabilitiesRegistry(t),
 	}
 	initOps := []chainlink.CoreRelayerChainInitFunc{chainlink.InitEVM(context.Background(), relayerFactory, evmOpts)}
 	rci, err := chainlink.NewCoreRelayerChainInteroperators(initOps...)
