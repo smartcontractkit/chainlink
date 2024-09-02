0b969fb21773a791bd20d5811969c4c359bf4c7f
DeividasK <deividas.karzinauskas@gmail.com>
2024-09-02 19:46:28 +0300

    Add comment explaining delegate position

--- core/services/chainlink/application.go
+++ core/services/chainlink/application.go
@@ -477,6 +477,7 @@ func NewApplication(opts ApplicationOpts) (Application, error) {
 		return nil, fmt.Errorf("P2P stack required for OCR or OCR2")
 	}
 
+	// If peer wrapper is initialized, Oracle Factory dependency will be available to standard capabilities
 	delegates[job.StandardCapabilities] = standardcapabilities.NewDelegate(
 		globalLogger,
 		opts.DS, jobORM,
