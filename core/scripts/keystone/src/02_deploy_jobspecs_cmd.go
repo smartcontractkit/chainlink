58324c099da5bf0c134c3347ba071b51a6abdf59
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-09-03 18:10:20 -0700

    Add node api wrapper for ergonomic cmd usage

--- core/scripts/keystone/src/02_deploy_jobspecs_cmd.go
+++ core/scripts/keystone/src/02_deploy_jobspecs_cmd.go
@@ -1,21 +1,14 @@
 package src
 
 import (
-	"bytes"
 	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"reflect"
-	"runtime"
-	"strings"
-
-	"github.com/urfave/cli"
 
 	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
-	"github.com/smartcontractkit/chainlink/v2/core/cmd"
 )
-
+// Could be useful https://github.com/smartcontractkit/chainlink/blob/4d5fc1943bd6a60b49cbc3d263c0aa47dc3cecb7/core/scripts/chaincli/handler/scrape_node_config.go#L102
 type deployJobSpecs struct{}
 
 func NewDeployJobSpecsCommand() *deployJobSpecs {
@@ -79,87 +72,25 @@ func (g *deployJobSpecs) Run(args []string) {
 	}
 
 	for i, n := range nodes {
-		output := &bytes.Buffer{}
-		client, app := newApp(n, output)
-		fmt.Println("Logging in:", n.url)
-		loginFs := flag.NewFlagSet("test", flag.ContinueOnError)
-		loginFs.Bool("bypass-version-check", true, "")
-		loginCtx := cli.NewContext(app, loginFs, nil)
-		err := client.RemoteLogin(loginCtx)
-		helpers.PanicErr(err)
-		output.Reset()
-
+		api := newNodeAPI(n)
 		if !*onlyReplay {
 			specToDeploy := flattenedSpecs[i].spec.ToString()
 			specFragment := flattenedSpecs[i].spec[0:1]
 			fmt.Printf("Deploying jobspec: %s\n... \n", specFragment)
-			fs := flag.NewFlagSet("test", flag.ExitOnError)
-			err = fs.Parse([]string{specToDeploy})
 
-			helpers.PanicErr(err)
-			err = client.CreateJob(cli.NewContext(app, fs, nil))
+			_, err := api.withArg(specToDeploy).exec(api.methods.CreateJob)
 			if err != nil {
 				fmt.Println("Failed to deploy job spec:", specFragment, "Error:", err)
 			}
-			output.Reset()
 		}
-
-		replayFs := flag.NewFlagSet("test", flag.ExitOnError)
-		flagSetApplyFromAction(client.ReplayFromBlock, replayFs, "")
-		err = replayFs.Set("block-number", fmt.Sprint(deployedContracts.SetConfigTxBlock))
-		helpers.PanicErr(err)
-		err = replayFs.Set("evm-chain-id", fmt.Sprint(*chainID))
-		helpers.PanicErr(err)
 
 		fmt.Printf("Replaying from block: %d\n", deployedContracts.SetConfigTxBlock)
 		fmt.Printf("EVM Chain ID: %d\n\n", *chainID)
-		replayCtx := cli.NewContext(app, replayFs, nil)
-		err = client.ReplayFromBlock(replayCtx)
-		helpers.PanicErr(err)
+		api.withFlags(api.methods.ReplayFromBlock, func(fs *flag.FlagSet) {
+			err = fs.Set("block-number", fmt.Sprint(deployedContracts.SetConfigTxBlock))
+			helpers.PanicErr(err)
+			err = fs.Set("evm-chain-id", fmt.Sprint(*chainID))
+			helpers.PanicErr(err)
+		}).mustExec()
 	}
-}
-
-// flagSetApplyFromAction applies the flags from action to the flagSet.
-//
-// `parentCommand` will filter the app commands and only applies the flags if the command/subcommand has a parent with that name, if left empty no filtering is done
-//
-// Taken from: https://github.com/smartcontractkit/chainlink/blob/develop/core/cmd/shell_test.go#L590
-func flagSetApplyFromAction(action interface{}, flagSet *flag.FlagSet, parentCommand string) {
-	cliApp := cmd.Shell{}
-	app := cmd.NewApp(&cliApp)
-
-	foundName := parentCommand == ""
-	actionFuncName := getFuncName(action)
-
-	for _, command := range app.Commands {
-		flags := recursiveFindFlagsWithName(actionFuncName, command, parentCommand, foundName)
-
-		for _, flag := range flags {
-			flag.Apply(flagSet)
-		}
-	}
-}
-
-func recursiveFindFlagsWithName(actionFuncName string, command cli.Command, parent string, foundName bool) []cli.Flag {
-	if command.Action != nil {
-		if actionFuncName == getFuncName(command.Action) && foundName {
-			return command.Flags
-		}
-	}
-
-	for _, subcommand := range command.Subcommands {
-		if !foundName {
-			foundName = strings.EqualFold(subcommand.Name, parent)
-		}
-
-		found := recursiveFindFlagsWithName(actionFuncName, subcommand, parent, foundName)
-		if found != nil {
-			return found
-		}
-	}
-	return nil
-}
-
-func getFuncName(i interface{}) string {
-	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
