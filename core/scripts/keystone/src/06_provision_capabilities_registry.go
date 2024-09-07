1e991db5d12eb5602badc436681a025501eafdf3
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-09-07 13:29:01 -0700

    WIP: Add capabilities registry provisioner script

---
+++ core/scripts/keystone/src/06_provision_capabilities_registry.go
@@ -0,0 +1,124 @@
+package src
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
+	kcr "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/keystone/generated/capabilities_registry"
+)
+
+type provisionCR struct{}
+
+func NewProvisionCapabilitesRegistryCommand() *provisionCR {
+	return &provisionCR{}
+}
+
+func (c *provisionCR) Name() string {
+	return "provision-capabilities-registry"
+}
+
+func (c *provisionCR) Run(args []string) {
+	ctx := context.Background()
+
+	fs := flag.NewFlagSet(c.Name(), flag.ExitOnError)
+	// create flags for all of the env vars then set the env vars to normalize the interface
+	// this is a bit of a hack but it's the easiest way to make this work
+	ethUrl := fs.String("ethurl", "", "URL of the Ethereum node")
+	chainID := fs.Int64("chainid", 11155111, "chain ID of the Ethereum network to deploy to")
+	accountKey := fs.String("accountkey", "", "private key of the account to deploy from")
+	publicKeys := fs.String("publickeys", "", "Custom public keys json location")
+	nodeList := fs.String("nodes", "", "Custom node list location")
+	artefactsDir := fs.String("artefacts", "", "Custom artefacts directory location")
+
+	err := fs.Parse(args)
+	if err != nil ||
+		*ethUrl == "" || ethUrl == nil ||
+		*chainID == 0 || chainID == nil ||
+		*accountKey == "" || accountKey == nil {
+		fs.Usage()
+		os.Exit(1)
+	}
+
+	if *artefactsDir == "" {
+		*artefactsDir = defaultArtefactsDir
+	}
+	if *publicKeys == "" {
+		*publicKeys = defaultPublicKeys
+	}
+	if *nodeList == "" {
+		*nodeList = defaultNodeList
+	}
+
+	os.Setenv("ETH_URL", *ethUrl)
+	os.Setenv("ETH_CHAIN_ID", fmt.Sprintf("%d", *chainID))
+	os.Setenv("ACCOUNT_KEY", *accountKey)
+
+	env := helpers.SetupEnv(false)
+
+	reg := getOrDeployCapabilitiesRegistry(ctx, *artefactsDir, env)
+
+	// For now, trigger, target, and workflow DONs are the same node sets, and same don instance
+	//
+	// CHECKME: I *think* we can make different DON instances across the same nodes if we provision mulitiple OCR3 jobs, each other a different OCR2 key bundle so we get distinct signers yet the same PeerIDs, which makes sense since the nodes are the same
+	workflowDON := loadDON(
+		*publicKeys,
+		*chainID,
+		*nodeList,
+	)
+	crProvisioner := NewCapabilityRegistryProvisioner(reg)
+	// We're using the default capability set for now
+	capSet := NewCapabilitySet()
+	crProvisioner.AddCapabilities(ctx, capSet)
+
+	nodeOperator := NewNodeOperator(env.Owner.From, "MY_NODE_OPERATOR", workflowDON)
+	crProvisioner.AddNodeOperator(ctx, nodeOperator)
+
+	// Note that both of these calls are simplified versions of the actual calls
+	//
+	// See the method documentation for more details
+	crProvisioner.AddNodes(ctx, nodeOperator, capSet)
+	crProvisioner.AddDON(ctx, nodeOperator, capSet, true, true)
+}
+
+func loadDON(publicKeys string, chainID int64, nodeList string) []peer {
+	nca := downloadNodePubKeys(nodeList, chainID, publicKeys)
+	workflowDON := []peer{}
+	for _, n := range nca {
+
+		p := peer{
+			PeerID: n.P2PPeerID,
+			Signer: n.OCR2OnchainPublicKey,
+		}
+		workflowDON = append(workflowDON, p)
+	}
+	return workflowDON
+}
+
+func getOrDeployCapabilitiesRegistry(ctx context.Context, artefactsDir string, env helpers.Environment) *kcr.CapabilitiesRegistry {
+	contracts, err := LoadDeployedContracts(artefactsDir)
+	if err != nil {
+		panic(err)
+	}
+
+	if contracts.CapabilityRegsitry.String() == "0x" {
+		_, tx, capabilitiesRegistry, innerErr := kcr.DeployCapabilitiesRegistry(env.Owner, env.Ec)
+		if innerErr != nil {
+			panic(innerErr)
+		}
+
+		helpers.ConfirmContractDeployed(ctx, env.Ec, tx, env.ChainID)
+		contracts.CapabilityRegsitry = capabilitiesRegistry.Address()
+		WriteDeployedContracts(contracts, artefactsDir)
+		return capabilitiesRegistry
+	} else {
+		capabilitiesRegistry, innerErr := kcr.NewCapabilitiesRegistry(contracts.CapabilityRegsitry, env.Ec)
+		if innerErr != nil {
+			panic(innerErr)
+		}
+
+		return capabilitiesRegistry
+	}
+}
---
+++ core/scripts/keystone/src/88_capabilities_registry.go
@@ -0,0 +1,464 @@
+package src
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"strings"
+
+	"encoding/hex"
+
+	ragetypes "github.com/smartcontractkit/libocr/ragep2p/types"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
+
+	capabilitiespb "github.com/smartcontractkit/chainlink-common/pkg/capabilities/pb"
+	"github.com/smartcontractkit/chainlink-common/pkg/values"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	gethCommon "github.com/ethereum/go-ethereum/common"
+	gethTypes "github.com/ethereum/go-ethereum/core/types"
+
+	kcr "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/keystone/generated/capabilities_registry"
+)
+
+type CapabilityRegistryProvisioner struct {
+	reg *kcr.CapabilitiesRegistry
+	env helpers.Environment
+}
+
+func NewCapabilityRegistryProvisioner(reg *kcr.CapabilitiesRegistry) *CapabilityRegistryProvisioner {
+	return &CapabilityRegistryProvisioner{reg: reg}
+}
+
+// AddCapabilities takes a capability set and provisions it in the registry.
+func (c *CapabilityRegistryProvisioner) AddCapabilities(ctx context.Context, capSet CapabilitySet) {
+	tx, err := c.reg.AddCapabilities(c.env.Owner, capSet.Capabilities())
+	if err != nil {
+		log.Printf("failed to call AddCapabilities: %s", err)
+	}
+
+	helpers.ConfirmTXMined(ctx, c.env.Ec, tx, c.env.ChainID)
+}
+
+// AddNodeOperator takes a node operator and provisions it in the registry.
+//
+// A node operator is a group of nodes that are all controlled by the same entity. The admin address is the
+// address that controls the node operator.
+//
+// The name is a human-readable name for the node operator.
+//
+// The node operator is then added to the registry, and the registry will issue an ID for the node operator.
+// The ID is then used when adding nodes to the registry such that the registry knows which nodes belong to which
+// node operator.
+func (c *CapabilityRegistryProvisioner) AddNodeOperator(ctx context.Context, nop NodeOperator) {
+	nop.BindToRegistry(c.reg)
+	tx, err := c.reg.AddNodeOperators(c.env.Owner, []kcr.CapabilitiesRegistryNodeOperator{
+		{
+			Admin: nop.Admin,
+			Name:  nop.Name,
+		},
+	})
+	if err != nil {
+		log.Printf("failed to AddNodeOperators: %s", err)
+	}
+
+	receipt := helpers.ConfirmTXMined(ctx, c.env.Ec, tx, c.env.ChainID)
+	nop.SetCapabilityRegistryIssuedID(receipt)
+}
+
+// AddNodes takes a node operators nodes, along with a capability set, then configures the registry such that
+// each node is assigned the same capability set. The registry will then know that each node supports each of the
+// capabilities in the set.
+//
+// This is a simplified version of the actual implementation, which is more flexible. The actual implementation
+// allows for the ability to add different capability sets to different nodes, _and_ lets you add nodes from different
+// node operators to the same capability set. This is not yet implemented here.
+//
+// Note that the registry must already have the capability set added via `AddCapabilities`, you cannot
+// add capabilites that the registry is not yet aware of.
+//
+// Note that in terms of the provisioning process, this is not the last step. A capability is only active once
+// there is a DON servicing yet. This is done via `AddDON`.
+func (c *CapabilityRegistryProvisioner) AddNodes(ctx context.Context, nop NodeOperator, capSet CapabilitySet) {
+	params := []kcr.CapabilitiesRegistryNodeParams{}
+	for _, peer := range nop.DON {
+		node, innerErr := peerToNode(nop.id, peer)
+		if innerErr != nil {
+			panic(innerErr)
+		}
+
+		// Technically we could be more flexible here,
+		// where we can have different capset assignment for each node
+		node.HashedCapabilityIds = capSet.CapabilityIDs()
+
+		params = append(params, node)
+	}
+
+	tx, err := c.reg.AddNodes(c.env.Owner, params)
+	if err != nil {
+		log.Printf("failed to AddNodes: %s", err)
+	}
+	helpers.ConfirmTXMined(ctx, c.env.Ec, tx, c.env.ChainID)
+}
+
+// AddDON takes a node operator, a capability set, and provisions a DON with the given capabilities.
+//
+// A DON is a group of nodes that all support the same capability set. This set can be a subset of the
+// capabilities that the nodes support. In other words, each node within the node set can support
+// a different, possibly overlapping, set of capabilities, but a DON is a subgroup of those nodes that all support
+// the same set of capabilities.
+//
+// A node can belong to multiple DONs, but it must belong to one and only one workflow DON.
+//
+// A DON can be a capability DON or a workflow DON, or both.
+//
+// When you want to add solely a workflow DON, you should set `acceptsWorkflows` to true and
+// `isPublic` to false.
+// This means that the DON can service workflow requests and will not service external capability requests.
+//
+// If you want to add solely a capability DON, you should set `acceptsWorkflows` to false and `isPublic` to true. This means that the DON
+// will service external capability requests and reject workflow requests.
+//
+// If you want to add a DON that services both capabilities and workflows, you should set both `acceptsWorkflows` and `isPublic` to true.
+//
+// Another important distinction is that DON can comprise of nodes from different node operators, but for now, we're keeping it simple and restricting it to a single node operator. We also hard code F to 1.
+func (c *CapabilityRegistryProvisioner) AddDON(ctx context.Context, nop NodeOperator, capSet CapabilitySet, isPublic bool, acceptsWorkflows bool) {
+	configs := capSet.Configs(c.reg)
+
+	// Note: Technically we could be more flexible here,
+	//			 where we can have multiple DONs with different capability configurations
+	// and have a non-hardcoded number for F 
+	tx, err := c.reg.AddDON(c.env.Owner, nop.MustGetPeerIDs(), configs, isPublic, acceptsWorkflows, 1)
+	if err != nil {
+		log.Printf("failed to AddDON: %s", err)
+	}
+	helpers.ConfirmTXMined(ctx, c.env.Ec, tx, c.env.ChainID)
+}
+
+/*
+ *
+ * Capabilities
+ *
+ *
+ */
+const ( // Taken from https://github.com/smartcontractkit/chainlink/blob/29117850e9be1be1993dbf8f21cf13cbb6af9d24/core/capabilities/integration_tests/keystone_contracts_setup.go#L43
+	CapabilityTypeTrigger   = 0
+	CapabilityTypeAction    = 1
+	CapabilityTypeConsensus = 2
+	CapabilityTypeTarget    = 3
+)
+
+type CapabillityProvisioner interface {
+	Config() kcr.CapabilitiesRegistryCapabilityConfiguration
+	Capability() kcr.CapabilitiesRegistryCapability
+	BindToRegistry(reg *kcr.CapabilitiesRegistry)
+	GetHashedCID() [32]byte
+}
+
+type baseCapability struct {
+	registry   *kcr.CapabilitiesRegistry
+	capability kcr.CapabilitiesRegistryCapability
+}
+
+func (b *baseCapability) BindToRegistry(reg *kcr.CapabilitiesRegistry) {
+	b.registry = reg
+}
+
+func (b *baseCapability) GetHashedCID() [32]byte {
+	if b.registry == nil {
+		panic(fmt.Errorf("registry not bound to capability, cannot get hashed capability ID"))
+	}
+
+	return mustHashCapabilityID(b.registry, b.capability)
+}
+
+func (b *baseCapability) config(config *capabilitiespb.CapabilityConfig) kcr.CapabilitiesRegistryCapabilityConfiguration {
+	configBytes, err := proto.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+
+	return kcr.CapabilitiesRegistryCapabilityConfiguration{
+		Config:       configBytes,
+		CapabilityId: b.GetHashedCID(),
+	}
+}
+
+func (b *baseCapability) Capability() kcr.CapabilitiesRegistryCapability {
+	return b.capability
+}
+
+type ConsensusCapability struct {
+	baseCapability
+}
+var _ CapabillityProvisioner = &ConsensusCapability{}
+
+func (c *ConsensusCapability) Config() kcr.CapabilitiesRegistryCapabilityConfiguration {
+	// Note that this is hard-coded for now, we'll want to support more flexible configurations in the future
+	// for configuring consensus once it has more configuration options
+	config := &capabilitiespb.CapabilityConfig{
+		DefaultConfig: values.Proto(values.EmptyMap()).GetMapValue(),
+	}
+
+	return c.config(config)
+}
+
+// NewOCR3V1ConsensusCapability returns a new ConsensusCapability for OCR3 
+func NewOCR3V1ConsensusCapability() *ConsensusCapability {
+	return &ConsensusCapability{
+		baseCapability{
+			capability: kcr.CapabilitiesRegistryCapability{
+				LabelledName:   "offchain_reporting",
+				Version:        "1.0.0",
+				CapabilityType: CapabilityTypeConsensus,
+			},
+		},
+	}
+}
+
+type TargetCapability struct {
+	baseCapability
+}
+var _ CapabillityProvisioner = &TargetCapability{}
+
+func (t *TargetCapability) Config() kcr.CapabilitiesRegistryCapabilityConfiguration {
+	// Note that this is hard-coded for now, we'll want to support more flexible configurations in the future
+	// for configuring the target. This configuration is also specific to the write target
+	config := &capabilitiespb.CapabilityConfig{
+		DefaultConfig: values.Proto(values.EmptyMap()).GetMapValue(),
+		RemoteConfig: &capabilitiespb.CapabilityConfig_RemoteTargetConfig{
+			RemoteTargetConfig: &capabilitiespb.RemoteTargetConfig{
+				RequestHashExcludedAttributes: []string{"signed_report.Signatures"},
+			},
+		},
+	}
+
+	return t.config(config)
+}
+
+func NewEthereumSepoliaWriteTargetV1Capability() *TargetCapability {
+	return &TargetCapability{
+		baseCapability{
+			capability: kcr.CapabilitiesRegistryCapability{
+				LabelledName:   "write_ethereum-testnet_sepolia",
+				Version:        "1.0.0",
+				CapabilityType: CapabilityTypeTarget,
+			},
+		},
+	}
+}
+
+type TriggerCapability struct {
+	baseCapability
+}
+
+var _ CapabillityProvisioner = &TriggerCapability{}
+
+func (t *TriggerCapability) Config() kcr.CapabilitiesRegistryCapabilityConfiguration {
+	// Note that this is hard-coded for now, we'll want to support more flexible configurations in the future
+	// for configuring the trigger. This configuration is also possibly specific to the streams trigger.
+	config := &capabilitiespb.CapabilityConfig{
+		DefaultConfig: values.Proto(values.EmptyMap()).GetMapValue(),
+		RemoteConfig: &capabilitiespb.CapabilityConfig_RemoteTriggerConfig{
+			RemoteTriggerConfig: &capabilitiespb.RemoteTriggerConfig{
+				RegistrationRefresh: durationpb.New(20 * time.Second),
+				RegistrationExpiry:  durationpb.New(60 * time.Second),
+				// We've hardcoded F + 1 here
+				MinResponsesToAggregate: uint32(1) + 1,
+			},
+		},
+	}
+
+	return t.config(config)
+}
+
+func NewStreamsTriggerV1Capability() *TriggerCapability {
+	return &TriggerCapability{
+		baseCapability{
+			capability: kcr.CapabilitiesRegistryCapability{
+				LabelledName:   "streams-trigger",
+				Version:        "1.0.0",
+				CapabilityType: CapabilityTypeTrigger,
+			},
+		},
+	}
+}
+
+func mustHashCapabilityID(reg *kcr.CapabilitiesRegistry, capability kcr.CapabilitiesRegistryCapability) [32]byte {
+	hashedCapabilityID, err := reg.GetHashedCapabilityId(&bind.CallOpts{}, capability.LabelledName, capability.Version)
+	if err != nil {
+		panic(err)
+	}
+	return hashedCapabilityID
+}
+
+/*
+ *
+ * Capabililty Sets
+ *
+ *
+ */
+type CapabilitySet []CapabillityProvisioner
+
+func NewCapabilitySet(capabilities ...CapabillityProvisioner) CapabilitySet {
+	if len(capabilities) == 0 {
+		log.Println("No capabilities provided, using default capabillity set")
+		return []CapabillityProvisioner{
+			NewStreamsTriggerV1Capability(),
+			NewEthereumSepoliaWriteTargetV1Capability(),
+			NewOCR3V1ConsensusCapability(),
+		}
+	}
+
+	return capabilities
+}
+
+func (c *CapabilitySet) Capabilities() []kcr.CapabilitiesRegistryCapability {
+	var definitions []kcr.CapabilitiesRegistryCapability
+	for _, cap := range *c {
+		definitions = append(definitions, cap.Capability())
+	}
+
+	return definitions
+}
+
+func (c *CapabilitySet) CapabilityIDs() [][32]byte {
+	var ids [][32]byte
+	for _, cap := range *c {
+		ids = append(ids, cap.GetHashedCID())
+	}
+
+	return ids
+}
+
+func (c *CapabilitySet) Configs(reg *kcr.CapabilitiesRegistry) []kcr.CapabilitiesRegistryCapabilityConfiguration {
+	var configs []kcr.CapabilitiesRegistryCapabilityConfiguration
+	for _, cap := range *c {
+		cap.BindToRegistry(reg)
+		configs = append(configs, cap.Config())
+	}
+
+	return configs
+}
+
+/*
+ *
+ * Node Operator
+ *
+ *
+ */
+
+type NodeOperator struct {
+	Admin gethCommon.Address
+	Name  string
+	// This is a really simplified mapping
+	// We dont handle multichain, multi don, etc
+	// Take a look at https://github.com/smartcontractkit/chainlink/pull/14334/files#diff-9cd09c4e7efeae20108eea3eeeb1119bb923eecce51e98d9066ea0cee19af09c for a more complete version
+	// but we'll integrate with them in the future
+	DON []peer
+
+	reg *kcr.CapabilitiesRegistry
+	// This ID is generated by the registry when the NodeOperator is added
+	id uint32
+}
+
+func NewNodeOperator(admin gethCommon.Address, name string, don []peer) NodeOperator {
+	return NodeOperator{
+		Admin: admin,
+		Name:  name,
+		DON:   don,
+	}
+}
+
+func (n *NodeOperator) BindToRegistry(reg *kcr.CapabilitiesRegistry) {
+	n.reg = reg
+}
+
+func (n *NodeOperator) SetCapabilityRegistryIssuedID(receipt *gethTypes.Receipt) uint32 {
+	if n.reg == nil {
+		panic(fmt.Errorf("registry not bound to node operator, cannot set ID"))
+	}
+	// We'll need more complex handling for multiple node operators
+	// since we'll need to handle log ordering
+	recLog, err := n.reg.ParseNodeOperatorAdded(*receipt.Logs[0])
+	if err != nil {
+		panic(err)
+	}
+
+	n.id = recLog.NodeOperatorId
+	return n.id
+}
+
+func (n *NodeOperator) MustGetPeerIDs() [][32]byte {
+	ps, err := peers(n.DON)
+	if err != nil {
+		panic(err)
+	}
+
+	return ps
+}
+
+func peerIDToB(peerID string) ([32]byte, error) {
+	var peerIDB ragetypes.PeerID
+	err := peerIDB.UnmarshalText([]byte(peerID))
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	return peerIDB, nil
+}
+
+func peers(ps []peer) ([][32]byte, error) {
+	out := [][32]byte{}
+	for _, p := range ps {
+		b, err := peerIDToB(p.PeerID)
+		if err != nil {
+			return nil, err
+		}
+
+		out = append(out, b)
+	}
+
+	return out, nil
+}
+
+func peerToNode(nopID uint32, p peer) (kcr.CapabilitiesRegistryNodeParams, error) {
+	peerIDB, err := peerIDToB(p.PeerID)
+	if err != nil {
+		return kcr.CapabilitiesRegistryNodeParams{}, fmt.Errorf("failed to convert peerID: %w", err)
+	}
+
+	sig := strings.TrimPrefix(p.Signer, "0x")
+	signerB, err := hex.DecodeString(sig)
+	if err != nil {
+		return kcr.CapabilitiesRegistryNodeParams{}, fmt.Errorf("failed to convert signer: %w", err)
+	}
+
+	var sigb [32]byte
+	copy(sigb[:], signerB)
+
+	return kcr.CapabilitiesRegistryNodeParams{
+		NodeOperatorId: nopID,
+		P2pId:          peerIDB,
+		Signer:         sigb,
+	}, nil
+}
+
+
+// newCapabilityConfig returns a new capability config with the default config set as empty.
+// Override the empty default config with functional options.
+func newCapabilityConfig(opts ...func(*values.Map)) *capabilitiespb.CapabilityConfig {
+	dc := values.EmptyMap()
+	for _, opt := range opts {
+		opt(dc)
+	}
+
+	return &capabilitiespb.CapabilityConfig{
+		DefaultConfig: values.ProtoMap(dc),
+	}
+}
