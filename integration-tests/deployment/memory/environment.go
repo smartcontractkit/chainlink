3e12c672955a9af98fee020c7de38eac9da314c6
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-09-12 07:11:18 +0200

    [TT-1546] change the parameter of chains' Confirm() function (#14388)
    * pass *types.Transaction instead of common.Hash to chain's Confirm() function
    
    * fix lints

--- integration-tests/deployment/memory/environment.go
+++ integration-tests/deployment/memory/environment.go
@@ -2,14 +2,16 @@ package memory
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/hashicorp/consul/sdk/freeport"
-	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
+
+	chainsel "github.com/smartcontractkit/chain-selectors"
 
 	"github.com/smartcontractkit/chainlink/integration-tests/deployment"
 
@@ -39,16 +41,19 @@ func NewMemoryChains(t *testing.T, numChains int) map[uint64]deployment.Chain {
 			Selector:    sel,
 			Client:      chain.Backend,
 			DeployerKey: chain.DeployerKey,
-			Confirm: func(tx common.Hash) (uint64, error) {
+			Confirm: func(tx *types.Transaction) (uint64, error) {
+				if tx == nil {
+					return 0, fmt.Errorf("tx was nil, nothing to confirm")
+				}
 				for {
 					chain.Backend.Commit()
-					receipt, err := chain.Backend.TransactionReceipt(context.Background(), tx)
+					receipt, err := chain.Backend.TransactionReceipt(context.Background(), tx.Hash())
 					if err != nil {
 						t.Log("failed to get receipt", err)
 						continue
 					}
 					if receipt.Status == 0 {
-						t.Logf("Status (reverted) %d for txhash %s\n", receipt.Status, tx.String())
+						t.Logf("Status (reverted) %d for txhash %s\n", receipt.Status, tx.Hash().Hex())
 					}
 					return receipt.BlockNumber.Uint64(), nil
 				}
