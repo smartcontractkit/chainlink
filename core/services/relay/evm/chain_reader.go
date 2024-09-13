baf36ec4df51bb9218ca89988e9aa3ecb81af121
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2024-09-13 13:09:06 -0500

    added unimplemented contract reader (#14413)
    * added unimplemented contract reader
    
    * fix write targets test
    
    * update common

--- core/services/relay/evm/chain_reader.go
+++ core/services/relay/evm/chain_reader.go
@@ -34,6 +34,7 @@ type ChainReaderService interface {
 }
 
 type chainReader struct {
+	commontypes.UnimplementedContractReader
 	lggr     logger.Logger
 	ht       logpoller.HeadTracker
 	lp       logpoller.LogPoller
