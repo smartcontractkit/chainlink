40b94b53c4ff8a845f5b49acd33e0bd35b055379
Austin Born <austinborn212@gmail.com>
2024-09-16 10:39:02 -0700

    Remove changes for Mercury v2 schema

--- core/services/relay/evm/mercury/v2/data_source.go
+++ core/services/relay/evm/mercury/v2/data_source.go
@@ -2,7 +2,6 @@ package v2
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"math/big"
 	"sync"
@@ -23,7 +22,6 @@ import (
 	mercurytypes "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury/types"
 	mercuryutils "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury/utils"
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury/v2/reportcodec"
-	relayTypes "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
 
@@ -63,13 +61,6 @@ func NewDataSource(orm types.DataSourceORM, pr pipeline.Runner, jb job.Job, spec
 
 func (ds *datasource) Observe(ctx context.Context, repts ocrtypes.ReportTimestamp, fetchMaxFinalizedTimestamp bool) (obs v2types.Observation, pipelineExecutionErr error) {
 	var wg sync.WaitGroup
-	var relayConfig relayTypes.RelayConfig
-	err := json.Unmarshal(ds.jb.OCR2OracleSpec.RelayConfig.Bytes(), &relayConfig)
-	if err != nil {
-		pipelineExecutionErr = fmt.Errorf("failed to deserialize relay config: %w", err)
-		return
-	}
-	triggerCapabilityEnabled := relayConfig.EnableTriggerCapability
 	ctx, cancel := context.WithCancel(ctx)
 
 	if fetchMaxFinalizedTimestamp {
@@ -119,7 +110,7 @@ func (ds *datasource) Observe(ctx context.Context, repts ocrtypes.ReportTimestam
 	var isLink, isNative bool
 	if ds.feedID == ds.linkFeedID {
 		isLink = true
-	} else if !triggerCapabilityEnabled {
+	} else {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -137,7 +128,7 @@ func (ds *datasource) Observe(ctx context.Context, repts ocrtypes.ReportTimestam
 
 	if ds.feedID == ds.nativeFeedID {
 		isNative = true
-	} else if !triggerCapabilityEnabled {
+	} else {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
--- core/services/relay/evm/mercury/v2/data_source_test.go
+++ core/services/relay/evm/mercury/v2/data_source_test.go
@@ -274,31 +274,7 @@ func Test_Datasource(t *testing.T) {
 				assert.EqualError(t, obs.NativePrice.Err, "some error fetching native price")
 			})
 
-			t.Run("when EnableTriggerCapability=true skips fetching link and native prices", func(t *testing.T) {
-				t.Cleanup(func() {
-					fetcher.linkPriceErr = nil
-					fetcher.nativePriceErr = nil
-				})
-
-				fetcher.linkPriceErr = errors.New("some error fetching link price")
-				fetcher.nativePriceErr = errors.New("some error fetching native price")
-
-				ds.jb.OCR2OracleSpec.RelayConfig = map[string]interface{}{
-					"EnableTriggerCapability": true,
-				}
-
-				obs, err := ds.Observe(ctx, repts, false)
-				assert.NoError(t, err)
-				assert.Nil(t, obs.LinkPrice.Err)
-				assert.Nil(t, obs.NativePrice.Err)
-				assert.Equal(t, big.NewInt(122), obs.BenchmarkPrice.Val)
-			})
-
 			t.Run("when succeeds to fetch linkPrice or nativePrice but got nil (new feed)", func(t *testing.T) {
-				ds.jb.OCR2OracleSpec.RelayConfig = map[string]interface{}{
-					"EnableTriggerCapability": false,
-				}
-
 				obs, err := ds.Observe(ctx, repts, false)
 				assert.NoError(t, err)
 
