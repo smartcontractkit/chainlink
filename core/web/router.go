f0d25ce13dc1c89ad22248a329e36c76e71816d6
Joe Huang <joe.huang@smartcontract.com>
2024-09-18 10:55:05 -0500

    enable heap monitoring

--- core/web/router.go
+++ core/web/router.go
@@ -189,9 +189,8 @@ func metricRoutes(r *gin.RouterGroup, includeHeap bool) {
 	pprofGroup.GET("/allocs", ginHandlerFromHTTP(pprof.Handler("allocs").ServeHTTP))
 	pprofGroup.GET("/block", ginHandlerFromHTTP(pprof.Handler("block").ServeHTTP))
 	pprofGroup.GET("/goroutine", ginHandlerFromHTTP(pprof.Handler("goroutine").ServeHTTP))
-	if includeHeap {
-		pprofGroup.GET("/heap", ginHandlerFromHTTP(pprof.Handler("heap").ServeHTTP))
-	}
+	pprofGroup.GET("/heap", ginHandlerFromHTTP(pprof.Handler("heap").ServeHTTP))
+
 	pprofGroup.GET("/mutex", ginHandlerFromHTTP(pprof.Handler("mutex").ServeHTTP))
 	pprofGroup.GET("/threadcreate", ginHandlerFromHTTP(pprof.Handler("threadcreate").ServeHTTP))
 }
