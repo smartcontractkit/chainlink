7d3d8db9dec77d84df122f4d9113562709ca1edf
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-09-20 15:17:37 -0700

    Add external registry and capability p2p config gen

--- core/scripts/keystone/src/03_gen_crib_cluster_overrides_cmd_test.go
+++ core/scripts/keystone/src/03_gen_crib_cluster_overrides_cmd_test.go
@@ -11,9 +11,10 @@ func TestGenerateCribConfig(t *testing.T) {
 	chainID := int64(1337)
 	templatesDir := "../templates"
 	forwarderAddress := "0x1234567890abcdef"
+	externalRegistryAddress := "0xabcdef1234567890"
 	publicKeysPath := "./testdata/PublicKeys.json"
 
-	lines := generateCribConfig(defaultNodeList, publicKeysPath, &chainID, templatesDir, forwarderAddress)
+	lines := generateCribConfig(defaultNodeList, publicKeysPath, &chainID, templatesDir, forwarderAddress, externalRegistryAddress)
 
 	snaps.MatchSnapshot(t, strings.Join(lines, "\n"))
 }
