f6616e4f952f2ebfa77423afff4516fa805b1f0b
Ryan Tinianov <tinianov@live.com>
2024-09-23 13:40:52 -0400

    brotli the WASM so it's smaller for the sdk to pass around

--- core/services/job/wasm_file_spec_factory.go
+++ core/services/job/wasm_file_spec_factory.go
@@ -1,10 +1,13 @@
 package job
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"os"
+	"strings"
 
+	"github.com/andybalholm/brotli"
 	"github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk"
 	"github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/host"
 
@@ -26,7 +29,23 @@ func (w WasmFileSpecFactory) Spec(ctx context.Context, lggr logger.Logger, rawSp
 }
 
 func (w WasmFileSpecFactory) RawSpec(_ context.Context, wf string) ([]byte, error) {
-	return os.ReadFile(wf)
+	isRawWasm := strings.HasSuffix(wf, ".wasm")
+	read, err := os.ReadFile(wf)
+	if err != nil || !isRawWasm {
+		return read, err
+	}
+
+	var b bytes.Buffer
+	bwr := brotli.NewWriter(&b)
+	if _, err = bwr.Write(read); err != nil {
+		return nil, err
+	}
+
+	if err = bwr.Close(); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
 }
 
 var _ SDKWorkflowSpecFactory = (*WasmFileSpecFactory)(nil)
