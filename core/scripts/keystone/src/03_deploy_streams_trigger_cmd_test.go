7dfa9d6c79adcec28ce129a30c555dbbf0b4ab51
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-09-23 17:52:12 -0700

    HACK: Add keystone workflow deployment to streams trigger cmd

--- core/scripts/keystone/src/03_deploy_streams_trigger_cmd_test.go
+++ core/scripts/keystone/src/03_deploy_streams_trigger_cmd_test.go
@@ -1,6 +1,7 @@
 package src
 
 import (
+	"fmt"
 	"net/url"
 	"testing"
 
@@ -9,45 +10,62 @@ import (
 
 var (
 	chainID         = int64(123456)
-	feedID          = [32]byte{0: 1}
+	feedID          = fmt.Sprintf("%x", [32]byte{0: 1})
 	feedName        = "BTC/USD"
-	verifierAddress = [20]byte{0: 7}
+	verifierAddress = fmt.Sprintf("0x%x", [20]byte{0: 7})
 )
 
 func TestCreateMercuryV3Job(t *testing.T) {
 	ocrKeyBundleID := "ocr_key_bundle_id"
 	nodeCSAKey := "node_csa_key"
 	bridgeName := "bridge_name"
-	linkFeedID := [32]byte{0: 2}
-	nativeFeedID := [32]byte{0: 3}
+	linkFeedID := fmt.Sprintf("%x", [32]byte{0: 2})
+	nativeFeedID := fmt.Sprintf("%x", [32]byte{0: 3})
 	u, err := url.Parse("https://crib-henry-keystone-node1.main.stage.cldev.sh")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_, output := createMercuryV3Job(
-		ocrKeyBundleID,
-		u.Hostname(),
-		verifierAddress,
-		bridgeName,
-		nodeCSAKey,
-		feedName,
-		feedID,
-		linkFeedID,
-		nativeFeedID,
-		chainID,
-	)
+	jobConfigData := MercuryV3JobSpecData{
+		BootstrapHost:   u.Hostname(),
+		VerifierAddress: verifierAddress,
+		OCRKeyBundleID:  ocrKeyBundleID,
+		NodeCSAKey:      nodeCSAKey,
+		Bridge:          bridgeName,
+		FeedName:        feedName,
+		FeedID:          feedID,
+		LinkFeedID:      linkFeedID,
+		NativeFeedID:    nativeFeedID,
+		ChainID:         chainID,
+	}
+	_, output := createMercuryV3Job(jobConfigData)
 
 	snaps.MatchSnapshot(t, output)
 }
 
 func TestCreateMercuryBootstrapJob(t *testing.T) {
-	_, output := createMercuryV3BootstrapJob(
-		verifierAddress,
-		feedName,
-		feedID,
-		chainID,
-	)
+	jobConfigData := MercuryV3BootstrapJobSpecData{
+		FeedName:            feedName,
+		FeedID:          feedID,
+		ChainID:         chainID,
+		VerifierAddress: verifierAddress,
+	}
+
+	_, output := createMercuryV3BootstrapJob(jobConfigData)
 
 	snaps.MatchSnapshot(t, output)
 }
+func TestCreateKeystoneWorkflowJob(t *testing.T) {
+	workflowConfig := WorkflowJobSpecConfig{
+		JobSpecName:          "keystone_workflow",
+		WorkflowOwnerAddress: "0x1234567890abcdef1234567890abcdef12345678",
+		FeedIDs:              []string{"feed1", "feed2", "feed3"},
+		TargetID:             "target_id",
+		TargetAddress:        "0xabcdefabcdefabcdefabcdefabcdefabcdef",
+	}
+
+	output := createKeystoneWorkflowJob(workflowConfig)
+
+	snaps.MatchSnapshot(t, output)
+}
+
