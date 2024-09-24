7dc9d49994da72807ff5b00807b0902b0aa3bb85
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-09-24 10:11:06 +0200

    Post review fixes

--- contracts/src/v0.8/ccip/test/helpers/USDCReaderTester.sol
+++ contracts/src/v0.8/ccip/test/helpers/USDCReaderTester.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.24;
 contract USDCReaderTester {
   event MessageSent(bytes);
 
+  // emitMessageSent reflects the logic from Circle's MessageTransmitter emitting MeseageSent(bytes) events
+  // https://github.com/circlefin/evm-cctp-contracts/blob/377c9bd813fb86a42d900ae4003599d82aef635a/src/MessageTransmitter.sol#L41
+  // https://github.com/circlefin/evm-cctp-contracts/blob/377c9bd813fb86a42d900ae4003599d82aef635a/src/MessageTransmitter.sol#L365
   function emitMessageSent(
     uint32 version,
     uint32 sourceDomain,
@@ -19,6 +22,22 @@ contract USDCReaderTester {
     emit MessageSent(_message);
   }
 
+  /**
+   * @notice Returns formatted (packed) message with provided fields
+   * It's a copy paste of the Message._formatMessage() call in MessageTransmitter.sol
+   * https://github.com/circlefin/evm-cctp-contracts/blob/377c9bd813fb86a42d900ae4003599d82aef635a/src/messages/Message.sol#L54C1-L65C9
+   * Check the chainlink-ccip repo for the offchain implementation of matching this format
+   * @param _msgVersion the version of the message format
+   * @param _msgSourceDomain Domain of home chain
+   * @param _msgDestinationDomain Domain of destination chain
+   * @param _msgNonce Destination-specific nonce
+   * @param _msgSender Address of sender on source chain as bytes32
+   * @param _msgRecipient Address of recipient on destination chain as bytes32
+   * @param _msgDestinationCaller Address of caller on destination chain as bytes32
+   * @param _msgRawBody Raw bytes of message body
+   * @return Formatted message
+   *
+   */
   function _formatMessage(
     uint32 _msgVersion,
     uint32 _msgSourceDomain,
