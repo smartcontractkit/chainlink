cc11371774fe3b091259887100431cfc14e4471d
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-09-25 14:40:06 -0700

    Use nodeapi for deleting ocr3 jobs

--- core/scripts/keystone/src/04_delete_ocr3_jobs_cmd.go
+++ core/scripts/keystone/src/04_delete_ocr3_jobs_cmd.go
@@ -1,13 +1,10 @@
 package src
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/urfave/cli"
 
 	helpers "github.com/smartcontractkit/chainlink/core/scripts/common"
 )
@@ -68,37 +65,19 @@ func (g *deleteJobs) Run(args []string) {
 	nodes := downloadNodeAPICredentials(*nodeList)
 
 	for _, node := range nodes {
-		output := &bytes.Buffer{}
-		client, app := newApp(node, output)
-
-		fmt.Println("Logging in:", node.url)
-		loginFs := flag.NewFlagSet("test", flag.ContinueOnError)
-		loginFs.Bool("bypass-version-check", true, "")
-		loginCtx := cli.NewContext(app, loginFs, nil)
-		err := client.RemoteLogin(loginCtx)
-		helpers.PanicErr(err)
-		output.Reset()
-
-		fileFs := flag.NewFlagSet("test", flag.ExitOnError)
-		err = client.ListJobs(cli.NewContext(app, fileFs, nil))
-		helpers.PanicErr(err)
+		api := newNodeAPI(node)
+		jobsb := api.mustExec(api.methods.ListJobs)
 
 		var parsed []JobSpec
-		err = json.Unmarshal(output.Bytes(), &parsed)
+		err = json.Unmarshal(jobsb, &parsed)
 		helpers.PanicErr(err)
 
 		for _, jobSpec := range parsed {
 			if jobSpec.BootstrapSpec.ContractID == deployedContracts.OCRContract.String() ||
 				jobSpec.OffChainReporting2OracleSpec.ContractID == deployedContracts.OCRContract.String() {
 				fmt.Println("Deleting OCR3 job ID:", jobSpec.Id, "name:", jobSpec.Name)
-				set := flag.NewFlagSet("test", flag.ExitOnError)
-				err = set.Parse([]string{jobSpec.Id})
-				helpers.PanicErr(err)
-				err = client.DeleteJob(cli.NewContext(app, set, nil))
-				helpers.PanicErr(err)
+				api.withArg(jobSpec.Id).mustExec(api.methods.DeleteJob)
 			}
 		}
-
-		output.Reset()
 	}
 }
