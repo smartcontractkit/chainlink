dde4e78b4fd3ec1cb854b4846c539ca345e82ac8
DeividasK <deividas.karzinauskas@gmail.com>
2024-09-25 18:37:59 +0300

    Remove forge from mock contract

--- contracts/src/v0.8/keystone/test/mocks/MaliciousRevertingReceiver.sol
+++ contracts/src/v0.8/keystone/test/mocks/MaliciousRevertingReceiver.sol
@@ -3,11 +3,10 @@ pragma solidity 0.8.24;
 
 import {IERC165} from "../../../vendor/openzeppelin-solidity/v4.8.3/contracts/interfaces/IERC165.sol";
 import {IReceiver} from "../../interfaces/IReceiver.sol";
-import {Test} from "forge-std/Test.sol";
 
 /// A malicious receiver that uses max allowed for ERC165 checks and consumes all gas in `onReport()`
 /// Causes parent Forwarder contract to revert if it doesn't handle gas tracking accurately
-contract MaliciousRevertingReceiver is IReceiver, IERC165, Test {
+contract MaliciousRevertingReceiver is IReceiver, IERC165 {
   function onReport(bytes calldata, bytes calldata) external view override {
     // consumes about 63/64 of all gas available
     uint256 targetGasRemaining = 200;
