e23f0d134fb01aa9fa89ac08fb97ee55998c0dad
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2024-09-30 17:29:30 +0100

    Minor change

--- core/capabilities/testutils/backend.go
+++ core/capabilities/testutils/backend.go
@@ -56,9 +56,9 @@ func NewEVMBackendTH(t *testing.T) *EVMBackendTH {
 		contractsOwner.From: {Balance: assets.Ether(100000).ToInt()},
 	}
 	chainID := testutils.SimulatedChainID
-	gasLimit := uint32(ethconfig.Defaults.Miner.GasCeil)
+	gasLimit := uint32(ethconfig.Defaults.Miner.GasCeil) //nolint:gosec
 	backend := cltest.NewSimulatedBackend(t, genesisData, gasLimit)
-	blockTime := time.UnixMilli(int64(backend.Blockchain().CurrentHeader().Time))
+	blockTime := time.UnixMilli(int64(backend.Blockchain().CurrentHeader().Time)) //nolint:gosec
 	err = backend.AdjustTime(time.Since(blockTime) - 24*time.Hour)
 	require.NoError(t, err)
 	backend.Commit()
