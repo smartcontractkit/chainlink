8542f5babbb9c436e819cace13d3517e477ff0bd
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-10-01 18:34:05 +0200

    Merging USDC chain reader config

--- core/capabilities/ccip/oraclecreator/plugin.go
+++ core/capabilities/ccip/oraclecreator/plugin.go
@@ -383,10 +383,30 @@ func getChainReaderConfig(
 		chainReaderConfig = evmconfig.MergeReaderConfigs(chainReaderConfig, evmconfig.FeedReaderConfig)
 	}
 
+	if isUSDCEnabled(chainID, destChainID, ofc) {
+		chainReaderConfig = evmconfig.MergeReaderConfigs(chainReaderConfig, evmconfig.USDCReaderConfig)
+	}
+
 	if chainID == homeChainID {
 		chainReaderConfig = evmconfig.MergeReaderConfigs(chainReaderConfig, evmconfig.HomeChainReaderConfigRaw)
 	}
 	return chainReaderConfig
+}
+
+func isUSDCEnabled(chainID uint64, destChainID uint64, ofc offChainConfig) bool {
+	if chainID == destChainID {
+		return false
+	}
+
+	if ofc.execEmpty() {
+		return false
+	}
+
+	if len(ofc.exec().TokenDataObservers) == 0 {
+		return false
+	}
+
+	return ofc.exec().IsUSDCEnabled()
 }
 
 func createChainReader(
