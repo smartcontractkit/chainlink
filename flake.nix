19a0ef2e4640961f8eacca00f42fb71834443169
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-10-02 18:39:24 -0700

    Use goreleaser pro in nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     foundry.url = "github:shazow/foundry.nix/monthly";
     flake-utils.url = "github:numtide/flake-utils";
     foundry.inputs.flake-utils.follows = "flake-utils";
+    nur.url = "github:nix-community/NUR";
+    goreleaser-nur.url = "github:goreleaser/nur";
   };
 
   outputs = inputs @ {
@@ -13,13 +15,27 @@
     nixpkgs,
     flake-utils,
     foundry,
+    nur,
+    goreleaser-nur,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [foundry.overlay];
-      };
+
+        pkgs = import nixpkgs { inherit system; 
+          config = { allowUnfree = true; }; 
+          overlays = [
+            (final: prev: {
+              nur = import nur
+                {
+                  pkgs = prev;
+                  repoOverrides = {
+                    goreleaser = import goreleaser-nur { pkgs = prev; };
+                  };
+                };
+            })
+            foundry.overlay 
+          ];
+         };
     in rec {
       devShell = pkgs.callPackage ./shell.nix {
         inherit pkgs;
