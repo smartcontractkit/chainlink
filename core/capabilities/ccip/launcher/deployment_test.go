eb02cbbf3942e70600958fff351190d7ca7c4dfd
asoliman <abdelrahman.soliman@smartcontract.com>
2024-10-04 16:21:45 +0400

    Rename bluegreen to deployment and use active candidate naming

--- core/capabilities/ccip/launcher/deployment_test.go
+++ core/capabilities/ccip/launcher/deployment_test.go
@@ -27,7 +27,7 @@ func Test_ccipDeployment_Close(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "no errors, blue only",
+			name: "no errors, active only",
 			args: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -45,7 +45,7 @@ func Test_ccipDeployment_Close(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "no errors, blue and green",
+			name: "no errors, active and candidate",
 			args: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: mocktypes.NewCCIPOracle(t),
@@ -67,7 +67,7 @@ func Test_ccipDeployment_Close(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "error on commit blue",
+			name: "error on commit active",
 			args: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -88,19 +88,19 @@ func Test_ccipDeployment_Close(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &ccipDeployment{
-				commit: blueGreenDeployment{
-					blue: tt.args.commitBlue,
+				commit: activeCandidateDeployment{
+					active: tt.args.commitBlue,
 				},
-				exec: blueGreenDeployment{
-					blue: tt.args.execBlue,
+				exec: activeCandidateDeployment{
+					active: tt.args.execBlue,
 				},
 			}
 			if tt.args.commitGreen != nil {
-				c.commit.green = tt.args.commitGreen
+				c.commit.candidate = tt.args.commitGreen
 			}
 
 			if tt.args.execGreen != nil {
-				c.exec.green = tt.args.execGreen
+				c.exec.candidate = tt.args.execGreen
 			}
 
 			tt.expect(t, tt.args)
@@ -144,7 +144,7 @@ func Test_ccipDeployment_StartBlue(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "error on commit blue",
+			name: "error on commit active",
 			args: args{
 				commitBlue: mocktypes.NewCCIPOracle(t),
 				execBlue:   mocktypes.NewCCIPOracle(t),
@@ -160,7 +160,7 @@ func Test_ccipDeployment_StartBlue(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "error on exec blue",
+			name: "error on exec active",
 			args: args{
 				commitBlue: mocktypes.NewCCIPOracle(t),
 				execBlue:   mocktypes.NewCCIPOracle(t),
@@ -179,17 +179,17 @@ func Test_ccipDeployment_StartBlue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &ccipDeployment{
-				commit: blueGreenDeployment{
-					blue: tt.args.commitBlue,
+				commit: activeCandidateDeployment{
+					active: tt.args.commitBlue,
 				},
-				exec: blueGreenDeployment{
-					blue: tt.args.execBlue,
+				exec: activeCandidateDeployment{
+					active: tt.args.execBlue,
 				},
 			}
 
 			tt.expect(t, tt.args)
 			defer tt.asserts(t, tt.args)
-			err := c.StartBlue()
+			err := c.StartActive()
 			if tt.wantErr {
 				require.Error(t, err)
 			} else {
@@ -228,7 +228,7 @@ func Test_ccipDeployment_CloseBlue(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "error on commit blue",
+			name: "error on commit active",
 			args: args{
 				commitBlue: mocktypes.NewCCIPOracle(t),
 				execBlue:   mocktypes.NewCCIPOracle(t),
@@ -244,7 +244,7 @@ func Test_ccipDeployment_CloseBlue(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "error on exec blue",
+			name: "error on exec active",
 			args: args{
 				commitBlue: mocktypes.NewCCIPOracle(t),
 				execBlue:   mocktypes.NewCCIPOracle(t),
@@ -263,17 +263,17 @@ func Test_ccipDeployment_CloseBlue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &ccipDeployment{
-				commit: blueGreenDeployment{
-					blue: tt.args.commitBlue,
+				commit: activeCandidateDeployment{
+					active: tt.args.commitBlue,
 				},
-				exec: blueGreenDeployment{
-					blue: tt.args.execBlue,
+				exec: activeCandidateDeployment{
+					active: tt.args.execBlue,
 				},
 			}
 
 			tt.expect(t, tt.args)
 			defer tt.asserts(t, tt.args)
-			err := c.CloseBlue()
+			err := c.CloseActive()
 			if tt.wantErr {
 				require.Error(t, err)
 			} else {
@@ -284,7 +284,7 @@ func Test_ccipDeployment_CloseBlue(t *testing.T) {
 }
 
 func Test_ccipDeployment_HandleBlueGreen_PrevDeploymentNil(t *testing.T) {
-	require.Error(t, (&ccipDeployment{}).HandleBlueGreen(nil))
+	require.Error(t, (&ccipDeployment{}).TransitionDeployment(nil))
 }
 
 func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
@@ -303,7 +303,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 		wantErr              bool
 	}{
 		{
-			name: "promotion blue to green",
+			name: "promotion active to candidate",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: mocktypes.NewCCIPOracle(t),
@@ -327,7 +327,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "new green deployment",
+			name: "new candidate deployment",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -351,7 +351,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "error on commit green start",
+			name: "error on commit candidate start",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -375,7 +375,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "error on exec green start",
+			name: "error on exec candidate start",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -399,7 +399,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "invalid blue-green deployment transition commit: both prev and future deployment have green",
+			name: "invalid active-candidate deployment transition commit: both prev and future deployment have candidate",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: mocktypes.NewCCIPOracle(t),
@@ -417,7 +417,7 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "invalid blue-green deployment transition exec: both prev and future exec deployment have green",
+			name: "invalid active-candidate deployment transition exec: both prev and future exec deployment have candidate",
 			argsPrevDeployment: args{
 				commitBlue:  mocktypes.NewCCIPOracle(t),
 				commitGreen: nil,
@@ -442,38 +442,38 @@ func Test_ccipDeployment_HandleBlueGreen(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			futDeployment := &ccipDeployment{
-				commit: blueGreenDeployment{
-					blue: tt.argsFutureDeployment.commitBlue,
+				commit: activeCandidateDeployment{
+					active: tt.argsFutureDeployment.commitBlue,
 				},
-				exec: blueGreenDeployment{
-					blue: tt.argsFutureDeployment.execBlue,
+				exec: activeCandidateDeployment{
+					active: tt.argsFutureDeployment.execBlue,
 				},
 			}
 			if tt.argsFutureDeployment.commitGreen != nil {
-				futDeployment.commit.green = tt.argsFutureDeployment.commitGreen
+				futDeployment.commit.candidate = tt.argsFutureDeployment.commitGreen
 			}
 			if tt.argsFutureDeployment.execGreen != nil {
-				futDeployment.exec.green = tt.argsFutureDeployment.execGreen
+				futDeployment.exec.candidate = tt.argsFutureDeployment.execGreen
 			}
 
 			prevDeployment := &ccipDeployment{
-				commit: blueGreenDeployment{
-					blue: tt.argsPrevDeployment.commitBlue,
+				commit: activeCandidateDeployment{
+					active: tt.argsPrevDeployment.commitBlue,
 				},
-				exec: blueGreenDeployment{
-					blue: tt.argsPrevDeployment.execBlue,
+				exec: activeCandidateDeployment{
+					active: tt.argsPrevDeployment.execBlue,
 				},
 			}
 			if tt.argsPrevDeployment.commitGreen != nil {
-				prevDeployment.commit.green = tt.argsPrevDeployment.commitGreen
+				prevDeployment.commit.candidate = tt.argsPrevDeployment.commitGreen
 			}
 			if tt.argsPrevDeployment.execGreen != nil {
-				prevDeployment.exec.green = tt.argsPrevDeployment.execGreen
+				prevDeployment.exec.candidate = tt.argsPrevDeployment.execGreen
 			}
 
 			tt.expect(t, tt.argsFutureDeployment, tt.argsPrevDeployment)
 			defer tt.asserts(t, tt.argsFutureDeployment, tt.argsPrevDeployment)
-			err := futDeployment.HandleBlueGreen(prevDeployment)
+			err := futDeployment.TransitionDeployment(prevDeployment)
 			if tt.wantErr {
 				require.Error(t, err)
 			} else {
@@ -495,31 +495,31 @@ func Test_isNewGreenInstance(t *testing.T) {
 		want bool
 	}{
 		{
-			"prev deployment only blue",
+			"prev deployment only active",
 			args{
 				pluginType: cctypes.PluginTypeCCIPCommit,
 				ocrConfigs: []ccipreaderpkg.OCR3ConfigWithMeta{
 					{}, {},
 				},
 				prevDeployment: ccipDeployment{
-					commit: blueGreenDeployment{
-						blue: mocktypes.NewCCIPOracle(t),
+					commit: activeCandidateDeployment{
+						active: mocktypes.NewCCIPOracle(t),
 					},
 				},
 			},
 			true,
 		},
 		{
-			"green -> blue promotion",
+			"candidate -> active promotion",
 			args{
 				pluginType: cctypes.PluginTypeCCIPCommit,
 				ocrConfigs: []ccipreaderpkg.OCR3ConfigWithMeta{
 					{},
 				},
 				prevDeployment: ccipDeployment{
-					commit: blueGreenDeployment{
-						blue:  mocktypes.NewCCIPOracle(t),
-						green: mocktypes.NewCCIPOracle(t),
+					commit: activeCandidateDeployment{
+						active:    mocktypes.NewCCIPOracle(t),
+						candidate: mocktypes.NewCCIPOracle(t),
 					},
 				},
 			},
@@ -528,7 +528,7 @@ func Test_isNewGreenInstance(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := isNewGreenInstance(tt.args.pluginType, tt.args.ocrConfigs, tt.args.prevDeployment)
+			got := isNewCandidateInstance(tt.args.pluginType, tt.args.ocrConfigs, tt.args.prevDeployment)
 			require.Equal(t, tt.want, got)
 		})
 	}
@@ -546,31 +546,31 @@ func Test_isPromotion(t *testing.T) {
 		want bool
 	}{
 		{
-			"prev deployment only blue",
+			"prev deployment only active",
 			args{
 				pluginType: cctypes.PluginTypeCCIPCommit,
 				ocrConfigs: []ccipreaderpkg.OCR3ConfigWithMeta{
 					{}, {},
 				},
 				prevDeployment: ccipDeployment{
-					commit: blueGreenDeployment{
-						blue: mocktypes.NewCCIPOracle(t),
+					commit: activeCandidateDeployment{
+						active: mocktypes.NewCCIPOracle(t),
 					},
 				},
 			},
 			false,
 		},
 		{
-			"green -> blue promotion",
+			"candidate -> active promotion",
 			args{
 				pluginType: cctypes.PluginTypeCCIPCommit,
 				ocrConfigs: []ccipreaderpkg.OCR3ConfigWithMeta{
 					{},
 				},
 				prevDeployment: ccipDeployment{
-					commit: blueGreenDeployment{
-						blue:  mocktypes.NewCCIPOracle(t),
-						green: mocktypes.NewCCIPOracle(t),
+					commit: activeCandidateDeployment{
+						active:    mocktypes.NewCCIPOracle(t),
+						candidate: mocktypes.NewCCIPOracle(t),
 					},
 				},
 			},
@@ -588,8 +588,8 @@ func Test_isPromotion(t *testing.T) {
 
 func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 	type fields struct {
-		commit blueGreenDeployment
-		exec   blueGreenDeployment
+		commit activeCandidateDeployment
+		exec   activeCandidateDeployment
 	}
 	type args struct {
 		pluginType cctypes.PluginType
@@ -601,11 +601,11 @@ func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 		want   bool
 	}{
 		{
-			"commit green present",
+			"commit candidate present",
 			fields{
-				commit: blueGreenDeployment{
-					blue:  mocktypes.NewCCIPOracle(t),
-					green: mocktypes.NewCCIPOracle(t),
+				commit: activeCandidateDeployment{
+					active:    mocktypes.NewCCIPOracle(t),
+					candidate: mocktypes.NewCCIPOracle(t),
 				},
 			},
 			args{
@@ -614,10 +614,10 @@ func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 			true,
 		},
 		{
-			"commit green not present",
+			"commit candidate not present",
 			fields{
-				commit: blueGreenDeployment{
-					blue: mocktypes.NewCCIPOracle(t),
+				commit: activeCandidateDeployment{
+					active: mocktypes.NewCCIPOracle(t),
 				},
 			},
 			args{
@@ -626,11 +626,11 @@ func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 			false,
 		},
 		{
-			"exec green present",
+			"exec candidate present",
 			fields{
-				exec: blueGreenDeployment{
-					blue:  mocktypes.NewCCIPOracle(t),
-					green: mocktypes.NewCCIPOracle(t),
+				exec: activeCandidateDeployment{
+					active:    mocktypes.NewCCIPOracle(t),
+					candidate: mocktypes.NewCCIPOracle(t),
 				},
 			},
 			args{
@@ -639,10 +639,10 @@ func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 			true,
 		},
 		{
-			"exec green not present",
+			"exec candidate not present",
 			fields{
-				exec: blueGreenDeployment{
-					blue: mocktypes.NewCCIPOracle(t),
+				exec: activeCandidateDeployment{
+					active: mocktypes.NewCCIPOracle(t),
 				},
 			},
 			args{
@@ -662,19 +662,19 @@ func Test_ccipDeployment_HasGreenInstance(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &ccipDeployment{}
-			if tt.fields.commit.blue != nil {
-				c.commit.blue = tt.fields.commit.blue
+			if tt.fields.commit.active != nil {
+				c.commit.active = tt.fields.commit.active
 			}
-			if tt.fields.commit.green != nil {
-				c.commit.green = tt.fields.commit.green
+			if tt.fields.commit.candidate != nil {
+				c.commit.candidate = tt.fields.commit.candidate
 			}
-			if tt.fields.exec.blue != nil {
-				c.exec.blue = tt.fields.exec.blue
+			if tt.fields.exec.active != nil {
+				c.exec.active = tt.fields.exec.active
 			}
-			if tt.fields.exec.green != nil {
-				c.exec.green = tt.fields.exec.green
+			if tt.fields.exec.candidate != nil {
+				c.exec.candidate = tt.fields.exec.candidate
 			}
-			got := c.HasGreenInstance(tt.args.pluginType)
+			got := c.HasCandidateInstance(tt.args.pluginType)
 			require.Equal(t, tt.want, got)
 		})
 	}
