2983b0469d9bfe982aa772579ae05f8259f1433a
Ivaylo Novakov <ivaylo.novakov@smartcontract.com>
2024-10-11 21:33:12 +0200

    Remove unnecessary complexity.

--- integration-tests/deployment/llo/changeset/deploy_test.go
+++ integration-tests/deployment/llo/changeset/deploy_test.go
@@ -6,16 +6,12 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink/integration-tests/deployment/llo"
-	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
 
 func TestDeployChannelConfigStoreChangeSet(t *testing.T) {
-	lggr := logger.TestLogger(t)
-	tenv := llo.NewMemoryEnvironment(t, lggr)
-	e := tenv.Env
-
+	e := newMemoryEnv(t)
 	c := llo.DeployLLOContractConfig{
-		ChainsToDeploy: []uint64{llo.TestChain.Selector},
+		ChainsToDeploy: []uint64{TestChain.Selector},
 	}
 	out, err := DeployChannelConfigStoreChangeSet(e, c)
 	require.NoError(t, err)
@@ -25,7 +21,7 @@ func TestDeployChannelConfigStoreChangeSet(t *testing.T) {
 	require.Len(t, ab, 1)
 
 	for sel, addrMap := range ab {
-		require.Equal(t, llo.TestChain.Selector, sel)
+		require.Equal(t, TestChain.Selector, sel)
 		for _, tv := range addrMap {
 			require.Equal(t, tv.Type, llo.ChannelConfigStore)
 		}
