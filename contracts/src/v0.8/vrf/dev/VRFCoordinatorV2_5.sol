327178e446ff5080230e5beab9cf84de7598f501
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-10-11 13:02:11 +0530

    Update VRFCoordinatorV2_5.sol

--- contracts/src/v0.8/vrf/dev/VRFCoordinatorV2_5.sol
+++ contracts/src/v0.8/vrf/dev/VRFCoordinatorV2_5.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {BlockhashStoreInterface} from "../interfaces/BlockhashStoreInterface.sol";
 import {VRF} from "../../vrf/VRF.sol";
