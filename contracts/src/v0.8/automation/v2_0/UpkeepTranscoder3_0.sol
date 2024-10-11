3775cef63751db2784ecfce1914d5031e15ddbfa
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-10-11 13:17:26 +0530

    Update UpkeepTranscoder3_0.sol

--- contracts/src/v0.8/automation/v2_0/UpkeepTranscoder3_0.sol
+++ contracts/src/v0.8/automation/v2_0/UpkeepTranscoder3_0.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.6;
+pragma solidity ^0.8.6;
 
 import "../../automation/interfaces/UpkeepTranscoderInterface.sol";
 import "../../interfaces/TypeAndVersionInterface.sol";
