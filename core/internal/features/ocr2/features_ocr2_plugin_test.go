bd0ed98fc8021ad8738ff73a8f6f1a56472eb329
Rens Rooimans <github@rensrooimans.nl>
2024-10-14 16:15:57 +0200

    turn off broken CI components

--- core/internal/features/ocr2/features_ocr2_plugin_test.go
+++ core/internal/features/ocr2/features_ocr2_plugin_test.go
@@ -10,5 +10,6 @@ import (
 
 func TestIntegration_OCR2_plugins(t *testing.T) {
 	t.Setenv(string(env.MedianPlugin.Cmd), "chainlink-feeds")
+	testutils.SkipFlakey(t, "https://smartcontract-it.atlassian.net/browse/BCF-3417")
 	testIntegration_OCR2(t)
 }
