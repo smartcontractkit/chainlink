cc2a90de191870e9ccbb58618926774b784802ed
ChrisAmora <27789416+ChrisAmora@users.noreply.github.com>
2024-10-15 15:50:11 -0300

    feat: consume fm protos from chainlink-protos on integration tests

--- integration-tests/deployment/memory/job_client.go
+++ integration-tests/deployment/memory/job_client.go
@@ -9,9 +9,9 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"google.golang.org/grpc"
 
-	csav1 "github.com/smartcontractkit/chainlink/integration-tests/deployment/jd/csa/v1"
-	jobv1 "github.com/smartcontractkit/chainlink/integration-tests/deployment/jd/job/v1"
-	nodev1 "github.com/smartcontractkit/chainlink/integration-tests/deployment/jd/node/v1"
+	csav1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/csa"
+	jobv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/job"
+	nodev1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/node"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/validate"
 )
 
