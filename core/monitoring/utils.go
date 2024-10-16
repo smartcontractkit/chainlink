208ec3ea22511087b59bf99400fee0f70cbf6921
patrickhuie19 <patrick.huie@smartcontract.com>
2024-10-15 20:47:10 -0400

    cleanup

--- core/monitoring/utils.go
+++ core/monitoring/utils.go
@@ -3,7 +3,7 @@ package monitoring
 import "go.opentelemetry.io/otel/attribute"
 
 func KvMapToOtelAttributes(kvmap map[string]string) []attribute.KeyValue {
-	otelKVs := make([]attribute.KeyValue, len(kvmap))
+	otelKVs := make([]attribute.KeyValue, 0, len(kvmap))
 	for k, v := range kvmap {
 		otelKVs = append(otelKVs, attribute.String(k, v))
 	}
