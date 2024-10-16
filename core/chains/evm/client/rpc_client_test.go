04d7506ca6fb8d5040b5985d10b42663e51e0fe7
Jordan Krage <jmank88@gmail.com>
2024-10-16 06:51:10 -0500

    core/chains/evm/client: fix race with atomic.Pointer

--- core/chains/evm/client/rpc_client_test.go
+++ core/chains/evm/client/rpc_client_test.go
@@ -8,6 +8,7 @@ import (
 	"math/big"
 	"net/url"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -415,7 +416,7 @@ func TestRPCClient_LatestFinalizedBlock(t *testing.T) {
 	lggr := logger.Test(t)
 
 	type rpcServer struct {
-		Head *evmtypes.Head
+		Head atomic.Pointer[evmtypes.Head]
 		URL  *url.URL
 	}
 	createRPCServer := func() *rpcServer {
@@ -423,7 +424,7 @@ func TestRPCClient_LatestFinalizedBlock(t *testing.T) {
 		server.URL = testutils.NewWSServer(t, chainId, func(method string, params gjson.Result) (resp testutils.JSONRPCResponse) {
 			assert.Equal(t, "eth_getBlockByNumber", method)
 			if assert.True(t, params.IsArray()) && assert.Equal(t, "finalized", params.Array()[0].String()) {
-				head := server.Head
+				head := server.Head.Load()
 				jsonHead, err := json.Marshal(head)
 				if err != nil {
 					panic(fmt.Errorf("failed to marshal head: %w", err))
@@ -441,7 +442,7 @@ func TestRPCClient_LatestFinalizedBlock(t *testing.T) {
 	rpc := client.NewRPCClient(nodePoolCfg, lggr, server.URL, nil, "rpc", 1, chainId, commonclient.Primary, commonclient.QueryTimeout, commonclient.QueryTimeout, "")
 	require.NoError(t, rpc.Dial(ctx))
 	defer rpc.Close()
-	server.Head = &evmtypes.Head{Number: 128}
+	server.Head.Store(&evmtypes.Head{Number: 128})
 	// updates chain info
 	_, err := rpc.LatestFinalizedBlock(ctx)
 	require.NoError(t, err)
@@ -454,7 +455,7 @@ func TestRPCClient_LatestFinalizedBlock(t *testing.T) {
 	assert.Equal(t, int64(128), latest.FinalizedBlockNumber)
 
 	// lower block number does not update highestUserObservations
-	server.Head = &evmtypes.Head{Number: 127}
+	server.Head.Store(&evmtypes.Head{Number: 127})
 	_, err = rpc.LatestFinalizedBlock(ctx)
 	require.NoError(t, err)
 	latest, highestUserObservations = rpc.GetInterceptedChainInfo()
@@ -466,7 +467,7 @@ func TestRPCClient_LatestFinalizedBlock(t *testing.T) {
 	assert.Equal(t, int64(127), latest.FinalizedBlockNumber)
 
 	// health check flg prevents change in highestUserObservations
-	server.Head = &evmtypes.Head{Number: 256}
+	server.Head.Store(&evmtypes.Head{Number: 256})
 	_, err = rpc.LatestFinalizedBlock(commonclient.CtxAddHealthCheckFlag(ctx))
 	require.NoError(t, err)
 	latest, highestUserObservations = rpc.GetInterceptedChainInfo()
