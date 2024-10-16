a5d6dd73820739c3e5da2b1a94e45b59cf47363d
Makram <makramkd@users.noreply.github.com>
2024-10-16 17:17:48 +0400

    Mk/rmn home cfgs (#14802)
    * first pass
    
    * fix compile
    
    * rmn remote config setting

--- integration-tests/deployment/ccip/rmn_test.go
+++ integration-tests/deployment/ccip/rmn_test.go
@@ -1,11 +1,16 @@
 package ccipdeployment
 
 import (
+	"math/big"
 	"sort"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/smartcontractkit/chainlink-testing-framework/lib/utils/testcontext"
+	"github.com/smartcontractkit/chainlink/integration-tests/deployment"
 	jobv1 "github.com/smartcontractkit/chainlink/integration-tests/deployment/jd/job/v1"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_home"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_remote"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/stretchr/testify/require"
 
@@ -16,17 +21,125 @@ func TestRMN(t *testing.T) {
 	t.Skip("Local only")
 
 	envWithRMN, rmnCluster := NewLocalDevEnvironmentWithRMN(t, logger.TestLogger(t))
+	var (
+		rmnHomeNodes     []rmn_home.RMNHomeNode
+		rmnRemoteSigners []rmn_remote.RMNRemoteSigner
+		nodeIndex        uint64
+	)
 	for rmnNode, rmn := range rmnCluster.Nodes {
 		t.Log(rmnNode, rmn.Proxy.PeerID, rmn.RMN.OffchainPublicKey, rmn.RMN.EVMOnchainPublicKey)
+		var offchainPublicKey [32]byte
+		copy(offchainPublicKey[:], rmn.RMN.OffchainPublicKey)
+		rmnHomeNodes = append(rmnHomeNodes, rmn_home.RMNHomeNode{
+			PeerId:            rmn.Proxy.PeerID,
+			OffchainPublicKey: offchainPublicKey,
+		})
+		rmnRemoteSigners = append(rmnRemoteSigners, rmn_remote.RMNRemoteSigner{
+			OnchainPublicKey: rmn.RMN.EVMOnchainPublicKey,
+			NodeIndex:        nodeIndex,
+		})
+		nodeIndex++
 	}
 	pprint(t, "envWithRmn: ", envWithRMN)
+
+	var rmnHomeSourceChains []rmn_home.RMNHomeSourceChain
+	for _, chain := range envWithRMN.Env.Chains {
+		rmnHomeSourceChains = append(rmnHomeSourceChains, rmn_home.RMNHomeSourceChain{
+			ChainSelector:       chain.Selector,
+			MinObservers:        1,
+			ObserverNodesBitmap: createObserverNodesBitmap(len(rmnHomeNodes)),
+		})
+	}
 
 	onChainState, err := LoadOnchainState(envWithRMN.Env, envWithRMN.Ab)
 	require.NoError(t, err)
 	pprint(t, "onChainState", onChainState)
 
-	// Use peerIDs to set RMN config.
-	// Add a lane, send a message.
+	homeChain, ok := envWithRMN.Env.Chains[envWithRMN.HomeChainSel]
+	require.True(t, ok)
+
+	homeChainState, ok := onChainState.Chains[envWithRMN.HomeChainSel]
+	require.True(t, ok)
+
+	allDigests, err := homeChainState.RMNHome.GetConfigDigests(&bind.CallOpts{
+		Context: testcontext.Get(t),
+	})
+	require.NoError(t, err)
+
+	t.Logf("RMNHome candidateDigest before setting new candidate: %x, activeDigest: %x",
+		allDigests.CandidateConfigDigest[:], allDigests.ActiveConfigDigest[:])
+
+	staticConfig := rmn_home.RMNHomeStaticConfig{
+		Nodes:          rmnHomeNodes,
+		OffchainConfig: []byte{},
+	}
+	dynamicConfig := rmn_home.RMNHomeDynamicConfig{
+		SourceChains:   rmnHomeSourceChains,
+		OffchainConfig: []byte{},
+	}
+	t.Logf("Setting RMNHome candidate with staticConfig: %+v, dynamicConfig: %+v, current candidateDigest: %x",
+		staticConfig, dynamicConfig, allDigests.CandidateConfigDigest[:])
+	tx, err := homeChainState.RMNHome.SetCandidate(homeChain.DeployerKey, staticConfig, dynamicConfig, allDigests.CandidateConfigDigest)
+	require.NoError(t, err)
+
+	// TODO: whats the first return value?
+	_, err = deployment.ConfirmIfNoError(homeChain, tx, err)
+	require.NoError(t, err)
+
+	// get the new candidate digest
+	candidateDigest, err := homeChainState.RMNHome.GetCandidateDigest(&bind.CallOpts{
+		Context: testcontext.Get(t),
+	})
+	require.NoError(t, err)
+
+	t.Logf("RMNHome candidateDigest after setting new candidate: %x", candidateDigest[:])
+
+	// promote the candidate to active
+	t.Logf("Promoting RMNHome candidate with candidateDigest: %x", candidateDigest[:])
+	tx, err = homeChainState.RMNHome.PromoteCandidateAndRevokeActive(
+		homeChain.DeployerKey, candidateDigest, allDigests.ActiveConfigDigest)
+	require.NoError(t, err)
+
+	_, err = deployment.ConfirmIfNoError(homeChain, tx, err)
+	require.NoError(t, err)
+
+	// check the active digest is the same as the candidate digest
+	activeDigest, err := homeChainState.RMNHome.GetActiveDigest(&bind.CallOpts{
+		Context: testcontext.Get(t),
+	})
+	require.NoError(t, err)
+	require.Equalf(t, candidateDigest, activeDigest,
+		"active digest should be the same as the previously candidate digest after promotion, previous candidate: %x, active: %x",
+		candidateDigest[:], activeDigest[:])
+
+	// Set RMN remote config appropriately
+	for _, chain := range envWithRMN.Env.Chains {
+		chState, ok := onChainState.Chains[chain.Selector]
+		require.True(t, ok)
+		rmnRemoteConfig := rmn_remote.RMNRemoteConfig{
+			RmnHomeContractConfigDigest: activeDigest,
+			Signers:                     rmnRemoteSigners,
+			MinSigners:                  1,
+		}
+		t.Logf("Setting RMNRemote config with RMNHome active digest: %x, cfg: %+v", activeDigest[:], rmnRemoteConfig)
+		tx2, err2 := chState.RMNRemote.SetConfig(chain.DeployerKey, rmnRemoteConfig)
+		require.NoError(t, err2)
+		_, err2 = deployment.ConfirmIfNoError(chain, tx2, err2)
+		require.NoError(t, err2)
+
+		// confirm the config is set correctly
+		config, err2 := chState.RMNRemote.GetVersionedConfig(&bind.CallOpts{
+			Context: testcontext.Get(t),
+		})
+		require.NoError(t, err2)
+		require.Equalf(t,
+			activeDigest,
+			config.Config.RmnHomeContractConfigDigest,
+			"RMNRemote config digest should be the same as the active digest of RMNHome after setting, RMNHome active: %x, RMNRemote config: %x",
+			activeDigest[:], config.Config.RmnHomeContractConfigDigest[:])
+
+		t.Logf("RMNRemote config digest after setting: %x", config.Config.RmnHomeContractConfigDigest[:])
+	}
 
 	jobSpecs, err := NewCCIPJobSpecs(envWithRMN.Env.NodeIDs, envWithRMN.Env.Offchain)
 	require.NoError(t, err)
@@ -82,4 +195,15 @@ func TestRMN(t *testing.T) {
 
 func pprint(t *testing.T, msg string, v interface{}) {
 	t.Logf("%s %#v", msg, v)
+}
+
+func createObserverNodesBitmap(numNodes int) *big.Int {
+	// for now, all nodes support all chains, so the bitmap is all 1s.
+	// first, initialize a big.Int with all bits set to 0.
+	// then, set the first numNodes bits to 1.
+	bitmap := new(big.Int)
+	for i := 0; i < numNodes; i++ {
+		bitmap.SetBit(bitmap, i, 1)
+	}
+	return bitmap
 }
