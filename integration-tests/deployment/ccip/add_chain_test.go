6e0b3450c1d897544a8e748f7fc10a865e9f2ca3
Will Winder <wwinder.unh@gmail.com>
2024-10-16 16:52:52 -0400

    Switch types to chainlink-ccip

--- integration-tests/deployment/ccip/add_chain_test.go
+++ integration-tests/deployment/ccip/add_chain_test.go
@@ -8,9 +8,8 @@ import (
 
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_home"
 
+	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-ccip/pluginconfig"
-
-	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
