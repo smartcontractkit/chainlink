3650e5d8c05ad1a9e20162f74537ff428fd9f8e3
patrickhuie19 <patrick.huie@smartcontract.com>
2024-10-15 21:11:43 -0400

    manually adding edits from closed PR #14744

---
+++ core/monitoring/monitoring_test.go
@@ -0,0 +1,15 @@
+package monitoring
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// tests CustomMessageAgent does not share state across new instances created by `With`
+func Test_CustomMessageAgent(t *testing.T) {
+	cma := NewCustomMessageLabeler()
+	cma1 := cma.With("key1", "value1")
+	cma2 := cma1.With("key2", "value2")
+
+	assert.NotEqual(t, cma1.labels, cma2.labels)
+}
