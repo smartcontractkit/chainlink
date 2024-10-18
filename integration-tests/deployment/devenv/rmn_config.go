7afa18646cda618219f6abb728bd57e0d9bb141c
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-10-18 18:34:46 +0300

    0.0.0.0

--- integration-tests/deployment/devenv/rmn_config.go
+++ integration-tests/deployment/devenv/rmn_config.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	DefaultAFNPasphrase       = "my-not-so-secret-passphrase"
-	DefaultRageProxy          = "127.0.0.1:8081"
-	DefaultProxyListenAddress = "127.0.0.1:8080"
+	DefaultRageProxy          = "0.0.0.0:8081"
+	DefaultProxyListenAddress = "0.0.0.0:8080"
 	DefaultDiscovererDbPath   = "/app/rageproxy-discoverer-db.json"
 )
 
