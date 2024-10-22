1932205253fb1e35b6736a70b9425c81df3de372
Finley Decker <finley.decker@smartcontract.com>
2024-10-22 12:31:25 -1000

    change 0x7e tx to switch statement

--- core/chains/evm/gas/chain_specific.go
+++ core/chains/evm/gas/chain_specific.go
@@ -19,7 +19,12 @@ func chainSpecificIsUsable(tx evmtypes.Transaction, baseFee *assets.Wei, chainTy
 			return false
 		}
 	}
-	if chainType == chaintype.ChainOptimismBedrock || chainType == chaintype.ChainKroma || chainType == chaintype.ChainScroll || chainType == chaintype.ChainZircuit || chainType == chaintype.ChainCelo {
+	switch chainType {
+	case chaintype.ChainOptimismBedrock, 
+		chaintype.ChainKroma, 
+		chaintype.ChainScroll, 
+		chaintype.ChainZircuit, 
+		chaintype.ChainCelo:
 		// This is a special deposit transaction type introduced in Bedrock upgrade.
 		// This is a system transaction that it will occur at least one time per block.
 		// We should discard this type before even processing it to avoid flooding the
