511bbb707fa78dc05b1fd0e9ea1dc13df4b4efeb
Joe Huang <joe.huang@smartcontract.com>
2024-10-22 15:59:55 -0500

    update comment

--- core/chains/evm/gas/rollups/l1_oracle.go
+++ core/chains/evm/gas/rollups/l1_oracle.go
@@ -62,7 +62,7 @@ func NewL1GasOracle(lggr logger.Logger, ethClient l1OracleClient, chainType chai
 	var l1Oracle L1Oracle
 	var err error
 	if daOracle != nil {
-		// TODO implementation to use the clientsByChainID should update the check accordingly, potentially return errors instead of logging, JIRA https://smartcontract-it.atlassian.net/browse/CCIP-3551
+		// TODO(CCIP-3551) the actual usage of the clientsByChainID should update the check accordingly, potentially return errors instead of logging
 		if clientsByChainID != nil {
 			if _, exist := clientsByChainID[daOracle.L1ChainID()]; !exist {
 				lggr.Debugf("eth client for chainID %v should exist in clientsByChainID map", daOracle.L1ChainID())
