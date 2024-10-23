8a6f6b30f8694739c549fca26a62397e1636cf81
Kyle Montag <kyle.montag@smartcontract.com>
2024-10-23 18:20:44 -0400

    port implementation from old OCR repo

--- contracts/src/v0.8/feeds/test/PrimaryAggregator.t.sol
+++ contracts/src/v0.8/feeds/test/PrimaryAggregator.t.sol
@@ -3,13 +3,22 @@ pragma solidity 0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {PrimaryAggregator} from "../PrimaryAggregator.sol";
+import {LinkTokenInterface} from "../../shared/interfaces/LinkTokenInterface.sol";
+import {AccessControllerInterface} from "../../shared/interfaces/AccessControllerInterface.sol";
 
 contract PrimaryAggregatorTest is Test {
+  address constant LINK_TOKEN_ADDRESS = address(1);
+  address constant BILLING_ACCESS_CONTROLLER_ADDRESS = address(100);
+  address constant REQUESTER_ACCESS_CONTROLLER_ADDRESS = address(101);
+
   PrimaryAggregator aggregator;
 
   function setUp() public virtual {
-    // deploy the PrimaryAggregator contract here
-    aggregator = new PrimaryAggregator(18);
+    LinkTokenInterface _link = LinkTokenInterface(LINK_TOKEN_ADDRESS);
+    AccessControllerInterface _billingAccessController = AccessControllerInterface(BILLING_ACCESS_CONTROLLER_ADDRESS);
+    AccessControllerInterface _requesterAccessController = AccessControllerInterface(REQUESTER_ACCESS_CONTROLLER_ADDRESS);
+
+    aggregator = new PrimaryAggregator(_link, 0, 100, _billingAccessController, _requesterAccessController, 18, "TEST");
   }
 
   function test_decimalsIs18() public {
