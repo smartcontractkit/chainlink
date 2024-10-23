270cc3c603e44272acb0bc186cc83638b448366b
Finley Decker <finley.decker@smartcontract.com>
2024-10-22 20:55:02 -1000

    ignore 0x7e for celo

--- core/chains/evm/gas/chain_specific.go
+++ core/chains/evm/gas/chain_specific.go
@@ -19,12 +19,7 @@ func chainSpecificIsUsable(tx evmtypes.Transaction, baseFee *assets.Wei, chainTy
 			return false
 		}
 	}
-	switch chainType {
-	case chaintype.ChainOptimismBedrock,
-		chaintype.ChainKroma,
-		chaintype.ChainScroll,
-		chaintype.ChainZircuit,
-		chaintype.ChainCelo:
+	if chainType == chaintype.ChainOptimismBedrock || chainType == chaintype.ChainKroma || chainType == chaintype.ChainScroll || chainType == chaintype.ChainZircuit {
 		// This is a special deposit transaction type introduced in Bedrock upgrade.
 		// This is a system transaction that it will occur at least one time per block.
 		// We should discard this type before even processing it to avoid flooding the
@@ -33,12 +28,10 @@ func chainSpecificIsUsable(tx evmtypes.Transaction, baseFee *assets.Wei, chainTy
 		if tx.Type == 0x7e {
 			return false
 		}
-	default:
-		return true
 	}
 	if chainType == chaintype.ChainCelo {
 		// Celo specific transaction types that utilize the feeCurrency field.
-		if tx.Type == 0x7c || tx.Type == 0x7b {
+		if tx.Type == 0x7c || tx.Type == 0x7b || tx.Type == 0x7e {
 			return false
 		}
 		// Celo has not yet fully migrated to the 0x7c type for special feeCurrency transactions
