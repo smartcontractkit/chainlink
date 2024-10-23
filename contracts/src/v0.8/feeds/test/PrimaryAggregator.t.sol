bf8a17f6bd523e32ff2a47d3fcb4dd82d4d7fc8c
Kyle Montag <kyle.montag@smartcontract.com>
2024-10-23 18:23:52 -0400

    more test setup

--- contracts/src/v0.8/feeds/test/PrimaryAggregator.t.sol
+++ contracts/src/v0.8/feeds/test/PrimaryAggregator.t.sol
@@ -11,6 +11,9 @@ contract PrimaryAggregatorTest is Test {
   address constant BILLING_ACCESS_CONTROLLER_ADDRESS = address(100);
   address constant REQUESTER_ACCESS_CONTROLLER_ADDRESS = address(101);
 
+  int192 constant MIN_ANSWER = 0;
+  int192 constant MAX_ANSWER = 100;
+
   PrimaryAggregator aggregator;
 
   function setUp() public virtual {
@@ -18,7 +21,15 @@ contract PrimaryAggregatorTest is Test {
     AccessControllerInterface _billingAccessController = AccessControllerInterface(BILLING_ACCESS_CONTROLLER_ADDRESS);
     AccessControllerInterface _requesterAccessController = AccessControllerInterface(REQUESTER_ACCESS_CONTROLLER_ADDRESS);
 
-    aggregator = new PrimaryAggregator(_link, 0, 100, _billingAccessController, _requesterAccessController, 18, "TEST");
+    aggregator = new PrimaryAggregator(
+      _link,
+      MIN_ANSWER,
+      MAX_ANSWER,
+      _billingAccessController,
+      _requesterAccessController,
+      18,
+      "TEST"
+    );
   }
 
   function test_decimalsIs18() public {
