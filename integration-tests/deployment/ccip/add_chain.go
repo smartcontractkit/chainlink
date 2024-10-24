600bc33fd66e76c613292a42cd08b4a1271b15fd
Austin <107539019+0xAustinWang@users.noreply.github.com>
2024-10-25 00:03:30 +1100

    Active / Candidate test (#14838)
    * add mcms batch accumulator for running integ tests
    
    * add functions for proposing and promoting candidates in MCMS
    
    * add some more execution for mcms, breaking at timelock reverting on underlying transaction
    
    * format calls
    
    * more fixes
    
    * fixes
    
    * clean up
    
    * fix lint
    
    * review comments
    
    * review comments
    
    * fix lint
    
    * review comments
    
    * fix tests
    
    * more review comments
    
    * add sens requests
    
    * review comments
    
    * send a dummy req
    
    * add initial test for bluegreen migration
    
    * add ability to update number of nodes
    
    * working test, remove a node from candidate configuration, see working transmit
    
    * add active/candidate test without offchain confirmation
    
    * remove integration test binary
    
    * move code around
    
    * check for 0x0000 instead of nil from post-promotion candidate config
    
    * partial bluegreen
    
    * refactor to make plugin types generic
    
    * deploy job specs before transmitting message, pass tests
    
    * fix golang lint ci issues
    
    * passing changeset test
    
    * working test, no mcms
    
    * wip
    
    * passing test with candidate in mcms
    
    * passing test, skipping send txn after setting candidate
    
    * fix bug in transition code for active candidate
    
    * update changes that were made to deployment and launcher.go
    
    * get test passing, commit
    
    * update for linting in launcher.go
    
    * clean up, move transferownership into ownership file
    
    * lint
    
    * fixes from comments in the pr
    
    * go mod tidy
    
    * fix address type
    
    * moving imports around
    
    * fix issues from merging
    
    * fix goimport
    
    * fix go imports
    
    ---------
    
    Co-authored-by: AnieeG <anindita.ghosh@smartcontract.com>

--- integration-tests/deployment/ccip/add_chain.go
+++ integration-tests/deployment/ccip/add_chain.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/types"
 
 	"github.com/smartcontractkit/ccip-owner-contracts/pkg/proposal/mcms"
 	"github.com/smartcontractkit/ccip-owner-contracts/pkg/proposal/timelock"
 
 	"github.com/smartcontractkit/chainlink/integration-tests/deployment"
-	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/types"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/fee_quoter"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/onramp"
 )
@@ -26,7 +25,6 @@ func NewChainInboundProposal(
 ) (*timelock.MCMSWithTimelockProposal, error) {
 	// Generate proposal which enables new destination (from test router) on all source chains.
 	var batches []timelock.BatchChainOperation
-	var chains []uint64
 	for _, source := range sources {
 		enableOnRampDest, err := state.Chains[source].OnRamp.ApplyDestChainConfigUpdates(deployment.SimTransactOpts(), []onramp.OnRampDestChainConfigArgs{
 			{
@@ -64,47 +62,33 @@ func NewChainInboundProposal(
 				},
 			},
 		})
-		chains = append(chains, source)
 	}
 
 	addChainOp, err := ApplyChainConfigUpdatesOp(e, state, homeChainSel, []uint64{newChainSel})
 	if err != nil {
 		return nil, err
 	}
-	timelockAddresses, metaDataPerChain, err := BuildProposalMetadata(state, append(chains, homeChainSel))
-	if err != nil {
-		return nil, err
-	}
+
 	batches = append(batches, timelock.BatchChainOperation{
 		ChainIdentifier: mcms.ChainIdentifier(homeChainSel),
 		Batch: []mcms.Operation{
 			addChainOp,
 		},
 	})
-	return timelock.NewMCMSWithTimelockProposal(
-		"1",
-		2004259681, // TODO: should be parameterized and based on current block timestamp.
-		[]mcms.Signature{},
-		false,
-		metaDataPerChain,
-		timelockAddresses,
-		"blah", // TODO
-		batches,
-		timelock.Schedule,
-		"0s", // TODO: Should be parameterized.
-	)
+
+	return BuildProposalFromBatches(state, batches, "proposal to set new chains", 0)
 }
 
-// AddDonAndSetCandidateForCommitProposal adds new DON for destination to home chain
+// AddDonAndSetCandidateProposal adds new DON for destination to home chain
 // and sets the commit plugin config as candidateConfig for the don.
-func AddDonAndSetCandidateForCommitProposal(
+func AddDonAndSetCandidateProposal(
 	state CCIPOnChainState,
 	e deployment.Environment,
 	nodes deployment.Nodes,
 	ocrSecrets deployment.OCRSecrets,
 	homeChainSel, feedChainSel, newChainSel uint64,
 	tokenConfig TokenConfig,
-	rmnHomeAddress common.Address,
+	pluginType types.PluginType,
 ) (*timelock.MCMSWithTimelockProposal, error) {
 	newDONArgs, err := BuildOCR3ConfigForCCIPHome(
 		e.Logger,
@@ -114,7 +98,7 @@ func AddDonAndSetCandidateForCommitProposal(
 		feedChainSel,
 		tokenConfig.GetTokenInfo(e.Logger, state.Chains[newChainSel].LinkToken, state.Chains[newChainSel].Weth9),
 		nodes.NonBootstraps(),
-		rmnHomeAddress,
+		state.Chains[homeChainSel].RMNHome.Address(),
 	)
 	if err != nil {
 		return nil, err
@@ -123,12 +107,12 @@ func AddDonAndSetCandidateForCommitProposal(
 	if err != nil {
 		return nil, err
 	}
-	commitConfig, ok := newDONArgs[types.PluginTypeCCIPCommit]
+	commitConfig, ok := newDONArgs[pluginType]
 	if !ok {
 		return nil, fmt.Errorf("missing commit plugin in ocr3Configs")
 	}
 	donID := latestDon.Id + 1
-	addDonOp, err := SetCandidateCommitPluginWithAddDonOps(
+	addDonOp, err := NewDonWithCandidateOp(
 		donID, commitConfig,
 		state.Chains[homeChainSel].CapabilityRegistry,
 		nodes.NonBootstraps(),
@@ -136,23 +120,9 @@ func AddDonAndSetCandidateForCommitProposal(
 	if err != nil {
 		return nil, err
 	}
-	timelockAddresses, metaDataPerChain, err := BuildProposalMetadata(state, []uint64{homeChainSel})
-	if err != nil {
-		return nil, err
-	}
-	return timelock.NewMCMSWithTimelockProposal(
-		"1",
-		2004259681, // TODO: should be parameterized and based on current block timestamp.
-		[]mcms.Signature{},
-		false,
-		metaDataPerChain,
-		timelockAddresses,
-		"SetCandidate for commit And AddDon for new chain",
-		[]timelock.BatchChainOperation{{
-			ChainIdentifier: mcms.ChainIdentifier(homeChainSel),
-			Batch:           []mcms.Operation{addDonOp},
-		}},
-		timelock.Schedule,
-		"0s", // TODO: Should be parameterized.
-	)
+
+	return BuildProposalFromBatches(state, []timelock.BatchChainOperation{{
+		ChainIdentifier: mcms.ChainIdentifier(homeChainSel),
+		Batch:           []mcms.Operation{addDonOp},
+	}}, "setCandidate for commit and AddDon on new Chain", 0)
 }
