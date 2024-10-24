eb03ec3abd51b45f19fe227d84231a880d00fe37
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2024-10-23 19:32:11 -0700

    Enable add chain test  (#14904)
    * Add FeeQuoter to SourceConfig
    
    Add missing function from DestConfig
    
    * cl-ccip bump
    
    * Debugging
    
    * Fix OnRamp.GetDestChainConfig
    
    All destChain configs have same Router address which is the same as the chainSel.
    
    We just need to call with a different destChainSelector
    
    * Cleaning
    
    * Clean contract configs
    
    * Clean contract configs
    
    * Add Gas Price test in initial_deploy_test.go
    
    * Bump chainlink-ccip
    
    * Bump chainlink-ccip
    
    * cleaning cmoments
    
    * linting
    
    * skip failing test for now
    
    * unskip
    
    * Add FeeQuoter to SourceConfig
    
    Add missing function from DestConfig
    
    Debugging
    
    Fix OnRamp.GetDestChainConfig
    
    All destChain configs have same Router address which is the same as the chainSel.
    
    We just need to call with a different destChainSelector
    
    Cleaning
    
    Clean contract configs
    
    Clean contract configs
    
    Add Gas Price test in initial_deploy_test.go
    
    Bump chainlink-ccip
    
    Bump chainlink-ccip
    
    cleaning cmoments
    
    linting
    
    skip failing test for now
    
    unskip
    
    * Bump chainlink-ccip
    
    * Add weth feed
    
    * Remove deprecated thing.
    
    * Add assertions for weth
    
    * Remove replacement.
    
    * linting
    
    * gomodtidy
    
    * Make TestAddChainInbound test Bidirectional and rename to TestAddChain
    
    * updates
    
    * changes
    
    * more changes
    
    * updates
    
    * go mod update
    
    * more fix
    
    * fix lint
    
    * fix lint
    
    * go mod
    
    ---------
    
    Co-authored-by: asoliman <abdelrahman.soliman@smartcontract.com>
    Co-authored-by: Will Winder <wwinder.unh@gmail.com>

--- integration-tests/deployment/ccip/add_chain.go
+++ integration-tests/deployment/ccip/add_chain.go
@@ -9,15 +9,8 @@ import (
 	"github.com/smartcontractkit/ccip-owner-contracts/pkg/proposal/mcms"
 	"github.com/smartcontractkit/ccip-owner-contracts/pkg/proposal/timelock"
 
-	chainsel "github.com/smartcontractkit/chain-selectors"
-
-	"github.com/smartcontractkit/chainlink-ccip/chainconfig"
-
-	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
-
 	"github.com/smartcontractkit/chainlink/integration-tests/deployment"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/types"
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/ccip_home"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/fee_quoter"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/onramp"
 )
@@ -35,7 +28,6 @@ func NewChainInboundProposal(
 	var batches []timelock.BatchChainOperation
 	var chains []uint64
 	for _, source := range sources {
-		chain, _ := chainsel.ChainBySelector(source)
 		enableOnRampDest, err := state.Chains[source].OnRamp.ApplyDestChainConfigUpdates(deployment.SimTransactOpts(), []onramp.OnRampDestChainConfigArgs{
 			{
 				DestChainSelector: newChainSel,
@@ -56,33 +48,13 @@ func NewChainInboundProposal(
 		if err != nil {
 			return nil, err
 		}
-		initialPrices, err := state.Chains[source].FeeQuoter.UpdatePrices(
-			deployment.SimTransactOpts(),
-			fee_quoter.InternalPriceUpdates{
-				TokenPriceUpdates: []fee_quoter.InternalTokenPriceUpdate{},
-				GasPriceUpdates: []fee_quoter.InternalGasPriceUpdate{
-					{
-						DestChainSelector: newChainSel,
-						// TODO: parameterize
-						UsdPerUnitGas: big.NewInt(2e12),
-					},
-				}})
-		if err != nil {
-			return nil, err
-		}
 		batches = append(batches, timelock.BatchChainOperation{
-			ChainIdentifier: mcms.ChainIdentifier(chain.Selector),
+			ChainIdentifier: mcms.ChainIdentifier(source),
 			Batch: []mcms.Operation{
 				{
 					// Enable the source in on ramp
 					To:    state.Chains[source].OnRamp.Address(),
 					Data:  enableOnRampDest.Data(),
-					Value: big.NewInt(0),
-				},
-				{
-					// Set initial dest prices to unblock testing.
-					To:    state.Chains[source].FeeQuoter.Address(),
-					Data:  initialPrices.Data(),
 					Value: big.NewInt(0),
 				},
 				{
@@ -95,42 +67,18 @@ func NewChainInboundProposal(
 		chains = append(chains, source)
 	}
 
-	homeChain, _ := chainsel.ChainBySelector(homeChainSel)
-	nodes, err := deployment.NodeInfo(e.NodeIDs, e.Offchain)
+	addChainOp, err := ApplyChainConfigUpdatesOp(e, state, homeChainSel, []uint64{newChainSel})
 	if err != nil {
 		return nil, err
 	}
-	encodedExtraChainConfig, err := chainconfig.EncodeChainConfig(chainconfig.ChainConfig{
-		GasPriceDeviationPPB:    ccipocr3.NewBigIntFromInt64(1000),
-		DAGasPriceDeviationPPB:  ccipocr3.NewBigIntFromInt64(0),
-		OptimisticConfirmations: 1,
-	})
-	if err != nil {
-		return nil, err
-	}
-	chainConfig := SetupConfigInfo(newChainSel, nodes.NonBootstraps().PeerIDs(),
-		nodes.DefaultF(), encodedExtraChainConfig)
-	addChain, err := state.Chains[homeChainSel].CCIPHome.ApplyChainConfigUpdates(
-		deployment.SimTransactOpts(), nil, []ccip_home.CCIPHomeChainConfigArgs{
-			chainConfig,
-		})
-	if err != nil {
-		return nil, err
-	}
-
 	timelockAddresses, metaDataPerChain, err := BuildProposalMetadata(state, append(chains, homeChainSel))
 	if err != nil {
 		return nil, err
 	}
 	batches = append(batches, timelock.BatchChainOperation{
-		ChainIdentifier: mcms.ChainIdentifier(homeChain.Selector),
+		ChainIdentifier: mcms.ChainIdentifier(homeChainSel),
 		Batch: []mcms.Operation{
-			{
-				// Add the chain first, don needs it to be there.
-				To:    state.Chains[homeChainSel].CCIPHome.Address(),
-				Data:  addChain.Data(),
-				Value: big.NewInt(0),
-			},
+			addChainOp,
 		},
 	})
 	return timelock.NewMCMSWithTimelockProposal(
