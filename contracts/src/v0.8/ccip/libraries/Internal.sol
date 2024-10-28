3e96d4c3c9e07617eb4a2f9f1c14df53771e6049
Rens Rooimans <github@rensrooimans.nl>
2024-10-28 13:24:51 +0100

    fix comments

--- contracts/src/v0.8/ccip/libraries/Internal.sol
+++ contracts/src/v0.8/ccip/libraries/Internal.sol
@@ -227,8 +227,8 @@ library Internal {
     // The source pool EVM address encoded to bytes. This value is trusted as it is obtained through the onRamp. It can be
     // relied upon by the destination pool to validate the source pool.
     bytes sourcePoolAddress;
-    address destTokenAddress; // ───╮ Address of destination token
-    uint32 destGasAmount; //────────╯ The amount of gas available for the releaseOrMint and transfer calls on the offRamp.
+    address destTokenAddress; // ─╮ Address of destination token
+    uint32 destGasAmount; //──────╯ The amount of gas available for the releaseOrMint and transfer calls on the offRamp.
     // Optional pool data to be transferred to the destination chain. Be default this is capped at
     // CCIP_LOCK_OR_BURN_V1_RET_BYTES bytes. If more data is required, the TokenTransferFeeConfig.destBytesOverhead
     // has to be set for the specific token.
