779a06016720ac683aa588047ed84d13991160a3
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-10-29 14:38:27 -0700

    Refactor provisioning flags and improve argument validation in keystone script

--- core/scripts/keystone/src/01_provision_keystone.go
+++ core/scripts/keystone/src/01_provision_keystone.go
@@ -21,43 +21,48 @@ func (g *provisionKeystone) Name() string {
 
 func (g *provisionKeystone) Run(args []string) {
 	fs := flag.NewFlagSet(g.Name(), flag.ExitOnError)
-	preprovison := fs.Bool("preprovision", false, "Preprovision crib")
-	// create flags for all of the env vars then set the env vars to normalize the interface
-	// this is a bit of a hack but it's the easiest way to make this work
-	ethUrl := fs.String("ethurl", "", "URL of the Ethereum node")
-	chainID := fs.Int64("chainid", 1337, "chain ID of the Ethereum network to deploy to")
-	accountKey := fs.String("accountkey", "", "private key of the account to deploy from")
+
+	// common flags
+	artefactsDir := fs.String("artefacts", defaultArtefactsDir, "Custom artefacts directory location")
 	nodeSetsPath := fs.String("nodesets", defaultNodeSetsPath, "Custom node sets location")
+	chainID := fs.Int64("chainid", 1337, "chain ID of the Ethereum network to deploy to")
+
+	// preprovisioning flags
+	preprovison := fs.Bool("preprovision", false, "Preprovision crib")
+
+	// provisioning flags
+	ethUrl := fs.String("ethurl", "", "URL of the Ethereum node")
+	accountKey := fs.String("accountkey", "", "private key of the account to deploy from")
 	nodeSetSize := fs.Int("nodesetsize", 5, "number of nodes in a nodeset")
-	replaceResources := fs.Bool("replacejob", false, "replace jobs if they already exist")
+	replaceResources := fs.Bool("replaceresources", false, "replace jobs if they already exist")
 	ocrConfigFile := fs.String("ocrfile", "ocr_config.json", "path to OCR config file")
 	p2pPort := fs.Int64("p2pport", 6690, "p2p port")
 	capabilitiesP2PPort := fs.Int64("capabilitiesp2pport", 6691, "p2p port for capabilities")
 	templatesDir := fs.String("templates", defaultTemplatesDir, "Custom templates location")
-	artefactsDir := fs.String("artefacts", defaultArtefactsDir, "Custom artefacts directory location")
 	preprovisionConfigName := fs.String("preprovisionconfig", "crib-preprovision.yaml", "Name of the preprovision config file, stored in the artefacts directory")
 	postprovisionConfigName := fs.String("postprovisionconfig", "crib-postprovision.yaml", "Name of the postprovision config file, stored in the artefacts directory")
 
 	err := fs.Parse(args)
 
-	if err != nil ||
-		*ethUrl == "" || ethUrl == nil ||
-		*accountKey == "" || accountKey == nil {
+	if err != nil || (!*preprovison && (*ethUrl == "" || *accountKey == "")) {
 		fs.Usage()
 		os.Exit(1)
 	}
 
-	os.Setenv("ETH_URL", *ethUrl)
-	os.Setenv("ETH_CHAIN_ID", fmt.Sprintf("%d", *chainID))
-	os.Setenv("ACCOUNT_KEY", *accountKey)
-	os.Setenv("INSECURE_SKIP_VERIFY", "true")
-	env := helpers.SetupEnv(false)
 	nodeSets := downloadNodeSets(*chainID, *nodeSetsPath, *nodeSetSize)
 	if *preprovison {
 		fmt.Printf("Preprovisioning crib with %d nodes\n", *nodeSetSize)
 		writePreprovisionConfig(*nodeSetSize, filepath.Join(*artefactsDir, *preprovisionConfigName))
 		return
 	}
+
+	// Kinda hacky but it prevents us from refactoring the setupenv function which
+	// is used in many other places
+	os.Setenv("ETH_URL", *ethUrl)
+	os.Setenv("ETH_CHAIN_ID", fmt.Sprintf("%d", *chainID))
+	os.Setenv("ACCOUNT_KEY", *accountKey)
+	os.Setenv("INSECURE_SKIP_VERIFY", "true")
+	env := helpers.SetupEnv(false)
 
 	reg := provisionCapabillitiesRegistry(
 		env,
