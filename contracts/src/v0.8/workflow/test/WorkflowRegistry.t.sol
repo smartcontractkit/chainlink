18a52dce369de9514112246b69a1413e951d8777
Iva Brajer <iva.brajer@smartcontract.com>
2024-10-29 12:01:35 +0100

    Forge fmt and forge coverage with via-ir flag

--- contracts/src/v0.8/workflow/test/WorkflowRegistry.t.sol
+++ contracts/src/v0.8/workflow/test/WorkflowRegistry.t.sol
@@ -46,13 +46,7 @@ contract WorkflowRegistryTest is Test {
     // authorized user registers workflow
     vm.prank(workflowOwner);
     registry.registerWorkflow(
-      workflowName,
-      workflowID,
-      donID,
-      initialStatus,
-      testBinaryURL,
-      testConfigURL,
-      testSecretsURL
+      workflowName, workflowID, donID, initialStatus, testBinaryURL, testConfigURL, testSecretsURL
     );
   }
 
@@ -325,11 +319,8 @@ contract WorkflowRegistryTest is Test {
     _allowAccessAndRegisterWorkflow(authorizedUser, workflowName2, workflowID2, WorkflowRegistry.WorkflowStatus.PAUSED);
 
     // Retrieve the list of workflows for the owner
-    WorkflowRegistry.WorkflowMetadata[] memory workflows = registry.getWorkflowMetadataListByOwner(
-      authorizedUser,
-      0,
-      10
-    );
+    WorkflowRegistry.WorkflowMetadata[] memory workflows =
+      registry.getWorkflowMetadataListByOwner(authorizedUser, 0, 10);
 
     // Verify the workflows are retrieved correctly
     assertEq(workflows.length, 2);
