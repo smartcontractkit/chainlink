11a0b632147426cdff7516f7e23ea7d7f421b27f
Ryan Tinianov <tinianov@live.com>
2024-10-30 14:30:48 -0400

    Decode the bytes instead and add an 0x prefix

--- core/capabilities/triggers/logevent/trigger.go
+++ core/capabilities/triggers/logevent/trigger.go
@@ -2,6 +2,7 @@ package logevent
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"strconv"
@@ -192,7 +193,7 @@ func createTriggerResponse(log types.Sequence, version string) capabilities.Trig
 		Cursor: log.Cursor,
 		Data:   dataAsMap,
 		Head: logeventcap.Head{
-			Hash:      string(log.Hash),
+			Hash:      "0x" + hex.EncodeToString(log.Hash),
 			Height:    log.Height,
 			Timestamp: log.Timestamp,
 		},
