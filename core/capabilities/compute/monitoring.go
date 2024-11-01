d74dd7e4f97f473e681610d21d69db1d5f038b9e
vyzaldysanchez <vyzaldysanchez@gmail.com>
2024-11-01 16:29:17 -0400

    Adds beholder logging

---
+++ core/capabilities/compute/monitoring.go
@@ -0,0 +1,10 @@
+package compute
+
+// Observability keys
+const (
+	wIDKey       = "workflowID"
+	eIDKey       = "workflowExecutionID"
+	wnKey        = "workflowName"
+	woIDKey      = "workflowOwner"
+	timestampKey = "computeTimestamp"
+)
