0372c3b334439913dce934b23d4ffca4facd4331
Joe Huang <joe.huang@smartcontract.com>
2024-11-05 11:57:07 -0600

    fix make

--- core/capabilities/ccip/configs/evm/chain_writer_test.go
+++ core/capabilities/ccip/configs/evm/chain_writer_test.go
@@ -80,7 +80,7 @@ func TestChainWriterConfigRaw(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			config, err := evm.ChainWriterConfigRaw(tt.fromAddress, tt.maxGasPrice, tt.commitGasLimit, tt.execBatchGasLimit)
+			config, err := evm.ChainWriterConfigRaw(tt.fromAddress, tt.commitGasLimit, tt.execBatchGasLimit)
 			if tt.expectedError != "" {
 				assert.EqualError(t, err, tt.expectedError)
 			} else {
