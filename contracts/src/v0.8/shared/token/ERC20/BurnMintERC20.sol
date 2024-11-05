fb77e5b0f1617aae256f8ce4f249277e2afa48b7
Josh <josh.weintraub@smartcontract.com>
2024-11-05 12:29:46 -0500

    adjust compiler version

--- contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol
+++ contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
 import {IGetCCIPAdmin} from "../../../ccip/interfaces/IGetCCIPAdmin.sol";
 import {IOwnable} from "../../../shared/interfaces/IOwnable.sol";
