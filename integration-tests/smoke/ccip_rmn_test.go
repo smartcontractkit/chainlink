8deb4f3dd8f14af1cfd0da190ed7a8c0c4d5bb3f
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-11-06 11:41:35 +0200

    rmn test changes

--- integration-tests/smoke/ccip_rmn_test.go
+++ integration-tests/smoke/ccip_rmn_test.go
@@ -3,60 +3,120 @@ package smoke
 import (
 	"math/big"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/stretchr/testify/require"
-
 	jobv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/job"
 	"github.com/smartcontractkit/chainlink-testing-framework/lib/utils/testcontext"
-
 	"github.com/smartcontractkit/chainlink/deployment"
 	ccipdeployment "github.com/smartcontractkit/chainlink/deployment/ccip"
-	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/testsetups"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_home"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_remote"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/testsetups"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
 
 func TestRMN(t *testing.T) {
-	t.Skip("Local only")
-
+	//t.Skip("Local only")
 	require.NoError(t, os.Setenv("ENABLE_RMN", "true"))
 
-	// In this test setup every RMN node is both observer and signer.
-	const homeF = 2
-	const remoteF = 2
-	const numRmnNodes = 2*homeF + 1
+	type homeChainConfig struct {
+		f map[uint64]int
+	}
 
+	type remoteChainConfig struct {
+		selector uint64
+		f        int
+	}
+
+	type rmnNode struct {
+		id             int
+		isSigner       bool
+		observedChains []uint64
+	}
+
+	type messageToSend struct {
+		fromChain         uint64
+		toChain           uint64
+		count             int
+		expectedDelivered bool
+	}
+
+	type rmnTestCase struct {
+		name               string
+		waitTimeout        time.Duration
+		waitForExec        bool
+		homeChainConfig    homeChainConfig
+		remoteChainsConfig []remoteChainConfig
+		rmnNodes           []rmnNode
+		messagesToSend     []messageToSend
+	}
+
+	const numRmnNodes = 5
 	envWithRMN, rmnCluster := testsetups.NewLocalDevEnvironmentWithRMN(t, logger.TestLogger(t), numRmnNodes)
 	t.Logf("envWithRmn: %#v", envWithRMN)
+
+	remoteChainSelectors := make([]uint64, 0, len(envWithRMN.Env.Chains)-1)
+	for _, chain := range envWithRMN.Env.Chains {
+		remoteChainSelectors = append(remoteChainSelectors, chain.Selector)
+	}
+	require.Greater(t, len(remoteChainSelectors), 0, "There should be at least one remote chain")
+
+	tc := rmnTestCase{
+		name:        "one message from one chain to another",
+		waitTimeout: 10 * time.Minute,
+		waitForExec: true,
+		homeChainConfig: homeChainConfig{
+			f: map[uint64]int{
+				remoteChainSelectors[0]: 1,
+				remoteChainSelectors[1]: 1,
+			},
+		},
+		remoteChainsConfig: []remoteChainConfig{
+			{selector: remoteChainSelectors[0], f: 1},
+			{selector: remoteChainSelectors[1], f: 1},
+		},
+		rmnNodes: []rmnNode{
+			{id: 0, isSigner: true, observedChains: remoteChainSelectors},
+			{id: 1, isSigner: true, observedChains: remoteChainSelectors},
+			{id: 2, isSigner: true, observedChains: remoteChainSelectors},
+		},
+	}
 
 	var (
 		rmnHomeNodes     []rmn_home.RMNHomeNode
 		rmnRemoteSigners []rmn_remote.RMNRemoteSigner
-		nodeIndex        uint64
 	)
-	for rmnNode, rmn := range rmnCluster.Nodes {
-		t.Log(rmnNode, rmn.Proxy.PeerID, rmn.RMN.OffchainPublicKey, rmn.RMN.EVMOnchainPublicKey)
+
+	for _, rmnNodeInfo := range tc.rmnNodes {
+		rmn := rmnCluster.Nodes["rmn_"+strconv.Itoa(rmnNodeInfo.id)]
+
+		t.Log(rmnNodeInfo.id, rmn.Proxy.PeerID, rmn.RMN.OffchainPublicKey, rmn.RMN.EVMOnchainPublicKey)
+
 		var offchainPublicKey [32]byte
 		copy(offchainPublicKey[:], rmn.RMN.OffchainPublicKey)
+
 		rmnHomeNodes = append(rmnHomeNodes, rmn_home.RMNHomeNode{
 			PeerId:            rmn.Proxy.PeerID,
 			OffchainPublicKey: offchainPublicKey,
 		})
+
 		rmnRemoteSigners = append(rmnRemoteSigners, rmn_remote.RMNRemoteSigner{
 			OnchainPublicKey: rmn.RMN.EVMOnchainPublicKey,
-			NodeIndex:        nodeIndex,
+			NodeIndex:        uint64(rmnNodeInfo.id),
 		})
-		nodeIndex++
 	}
 
 	var rmnHomeSourceChains []rmn_home.RMNHomeSourceChain
-	for _, chain := range envWithRMN.Env.Chains {
+	for remoteChainSel, remoteF := range tc.homeChainConfig.f {
+		// configure remote chain details on the home contract
 		rmnHomeSourceChains = append(rmnHomeSourceChains, rmn_home.RMNHomeSourceChain{
-			ChainSelector:       chain.Selector,
-			F:                   homeF,
+			ChainSelector:       remoteChainSel,
+			F:                   uint64(remoteF),
 			ObserverNodesBitmap: createObserverNodesBitmap(len(rmnHomeNodes)),
 		})
 	}
@@ -120,14 +180,17 @@ func TestRMN(t *testing.T) {
 		candidateDigest[:], activeDigest[:])
 
 	// Set RMN remote config appropriately
-	for _, chain := range envWithRMN.Env.Chains {
-		chState, ok := onChainState.Chains[chain.Selector]
+	for _, remoteCfg := range tc.remoteChainsConfig {
+		chState, ok := onChainState.Chains[remoteCfg.selector]
 		require.True(t, ok)
 		rmnRemoteConfig := rmn_remote.RMNRemoteConfig{
 			RmnHomeContractConfigDigest: activeDigest,
 			Signers:                     rmnRemoteSigners,
-			F:                           remoteF,
+			F:                           uint64(remoteCfg.f),
 		}
+
+		chain := envWithRMN.Env.Chains[remoteCfg.selector]
+
 		t.Logf("Setting RMNRemote config with RMNHome active digest: %x, cfg: %+v", activeDigest[:], rmnRemoteConfig)
 		tx2, err2 := chState.RMNRemote.SetConfig(chain.DeployerKey, rmnRemoteConfig)
 		require.NoError(t, err2)
