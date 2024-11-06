4ea4acbcdb320ffac5c64171b391a3816b7927dd
Michael Street <5597260+MStreet3@users.noreply.github.com>
2024-11-05 21:24:36 -0500

    fixup migration

--- core/store/migrate/migrations/0259_add_workflow_artifacts.sql
+++ core/store/migrate/migrations/0259_add_workflow_artifacts.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 -- Create the workflow_artifacts table
-CREATE TABLE workflow_artifacts (
+CREATE TABLE workflow_secrets (
     id SERIAL PRIMARY KEY,
     secrets_url TEXT,
     secrets_url_hash TEXT UNIQUE,
@@ -9,26 +9,24 @@ CREATE TABLE workflow_artifacts (
 );
 
 -- Create an index on the secrets_url_hash column
-CREATE INDEX idx_secrets_url_hash ON workflow_artifacts(secrets_url_hash);
+CREATE INDEX idx_secrets_url_hash ON workflow_secrets(secrets_url_hash);
 
 -- Alter the workflow_specs table
 ALTER TABLE workflow_specs
 ADD COLUMN binary_url TEXT,
-ADD COLUMN artifact_id INT UNIQUE REFERENCES workflow_artifacts(id) ON DELETE CASCADE;
+ADD COLUMN config_url TEXT,
+ADD COLUMN secrets_id INT UNIQUE REFERENCES workflow_secrets(id) ON DELETE CASCADE;
 
 -- Alter the config column type
 ALTER TABLE workflow_specs
 ALTER COLUMN config TYPE TEXT;
-
--- Set the default value for the config column
-ALTER TABLE workflow_specs
-ALTER COLUMN config SET DEFAULT '';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE workflow_specs
-DROP COLUMN IF EXISTS artifact_id,
+DROP COLUMN IF EXISTS secrets_id,
+DROP COLUMN IF EXISTS config_url,
 DROP COLUMN IF EXISTS binary_url;
 
 -- Change the config column back to character varying(255)
@@ -39,5 +37,5 @@ ALTER COLUMN config TYPE CHARACTER VARYING(255);
 DROP INDEX IF EXISTS idx_secrets_url_hash;
 
 -- Drop the workflow_artifacts table
-DROP TABLE IF EXISTS workflow_artifacts;
+DROP TABLE IF EXISTS workflow_secrets;
 -- +goose StatementEnd
