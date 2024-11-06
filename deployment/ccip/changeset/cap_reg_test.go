3ace0f2357fcd14c859064503da756d65feb15cc
AnieeG <anindita.ghosh@smartcontract.com>
2024-11-06 15:18:09 -0800

    add a test

--- deployment/ccip/changeset/cap_reg_test.go
+++ deployment/ccip/changeset/cap_reg_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
 
 	"github.com/smartcontractkit/chainlink/deployment"
 	ccdeploy "github.com/smartcontractkit/chainlink/deployment/ccip"
+	"github.com/smartcontractkit/chainlink/deployment/common/view/v1_0"
 	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
@@ -20,7 +22,7 @@ func TestDeployCapReg(t *testing.T) {
 		Chains:     2,
 		Nodes:      4,
 	})
-	homeChainSel := e.Chains[0].Selector
+	homeChainSel := e.AllChainSelectors()[0]
 	nodes, err := deployment.NodeInfo(e.NodeIDs, e.Offchain)
 	require.NoError(t, err)
 	p2pIds := nodes.NonBootstraps().PeerIDs()
@@ -41,4 +43,22 @@ func TestDeployCapReg(t *testing.T) {
 	require.NotNil(t, state.Chains[homeChainSel].CapabilityRegistry)
 	require.NotNil(t, state.Chains[homeChainSel].CCIPHome)
 	require.NotNil(t, state.Chains[homeChainSel].RMNHome)
+	snap, err := state.View([]uint64{homeChainSel})
+	require.NoError(t, err)
+	chainid, err := chainsel.ChainIdFromSelector(homeChainSel)
+	require.NoError(t, err)
+	chainName, err := chainsel.NameFromChainId(chainid)
+	require.NoError(t, err)
+	_, ok := snap[chainName]
+	require.True(t, ok)
+	capRegSnap, ok := snap[chainName].CapabilityRegistry[state.Chains[homeChainSel].CapabilityRegistry.Address().String()]
+	require.True(t, ok)
+	require.NotNil(t, capRegSnap)
+	require.Equal(t, capRegSnap.Nops, []v1_0.NopView{
+		{
+			Admin: e.Chains[homeChainSel].DeployerKey.From,
+			Name:  "NodeOperator",
+		},
+	})
+	require.Len(t, capRegSnap.Nodes, len(p2pIds))
 }
