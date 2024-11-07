c77b3b5b635f7c6194b424906cbe006a01966c41
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2024-11-07 12:57:20 -0500

    lint

--- common/client/transaction_sender.go
+++ common/client/transaction_sender.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	"errors"
-	"fmt"
 	"math"
 	"slices"
 	"sync"
@@ -215,7 +214,7 @@ func aggregateTxResults[RESULT any](resultsByCode sendTxResults[RESULT]) (result
 		return r[0], nil
 	}
 
-	criticalErr = fmt.Errorf("expected at least one response on SendTransaction")
+	criticalErr = errors.New("expected at least one response on SendTransaction")
 	return result, criticalErr
 }
 
