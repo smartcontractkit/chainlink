cb348aa73e0ff0a95aa5841e46a3cdd581366dee
Geert G <117188496+cll-gg@users.noreply.github.com>
2024-11-08 13:37:53 +0000

    Potential test fix

--- core/cmd/shell_local_test.go
+++ core/cmd/shell_local_test.go
@@ -122,7 +122,7 @@ func TestShell_RunNodeWithPasswords(t *testing.T) {
 				Config:                 cfg,
 				FallbackAPIInitializer: apiPrompt,
 				Runner:                 cltest.EmptyRunner{},
-				AppFactory:             cltest.InstanceAppFactory{App: app},
+				AppFactory:             cmd.ChainlinkAppFactory{},
 				Logger:                 lggr,
 			}
 
