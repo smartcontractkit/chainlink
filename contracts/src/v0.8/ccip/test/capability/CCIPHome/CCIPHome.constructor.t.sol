7817c7780db22d4869b297fcd3be61d6aab7d919
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2024-11-08 20:35:17 +0530

    move contracts inside sub folder

---
+++ contracts/src/v0.8/ccip/test/capability/CCIPHome/CCIPHome.constructor.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.24;
+
+import {ICapabilityConfiguration} from "../../../../keystone/interfaces/ICapabilityConfiguration.sol";
+
+import {IERC165} from "../../../../vendor/openzeppelin-solidity/v5.0.2/contracts/interfaces/IERC165.sol";
+import {CCIPHome} from "../../../capability/CCIPHome.sol";
+import {CCIPHomeTestSetup} from "./CCIPHomeTestSetup.t.sol";
+
+contract CCIPHome_constructor is CCIPHomeTestSetup {
+  function test_constructor_success() public {
+    CCIPHome ccipHome = new CCIPHome(CAPABILITIES_REGISTRY);
+
+    assertEq(address(ccipHome.getCapabilityRegistry()), CAPABILITIES_REGISTRY);
+  }
+
+  function test_supportsInterface_success() public view {
+    assertTrue(s_ccipHome.supportsInterface(type(IERC165).interfaceId));
+    assertTrue(s_ccipHome.supportsInterface(type(ICapabilityConfiguration).interfaceId));
+  }
+
+  function test_getCapabilityConfiguration_success() public view {
+    bytes memory config = s_ccipHome.getCapabilityConfiguration(DEFAULT_DON_ID);
+    assertEq(config.length, 0);
+  }
+
+  function test_constructor_CapabilitiesRegistryAddressZero_reverts() public {
+    vm.expectRevert(CCIPHome.ZeroAddressNotAllowed.selector);
+    new CCIPHome(address(0));
+  }
+}
