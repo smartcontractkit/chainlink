322b74c85b982c904c64cfbcc4386167c67c2687
Rens Rooimans <github@rensrooimans.nl>
2024-11-13 12:50:34 +0100

    fix min pragma

--- contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol
+++ contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import {IGetCCIPAdmin} from "../../../ccip/interfaces/IGetCCIPAdmin.sol";
 import {IBurnMintERC20} from "../../../shared/token/ERC20/IBurnMintERC20.sol";
