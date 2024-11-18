a5c2f47606f524de66cea2e9cf8df19851c631dc
connorwstein <connor.stein@mail.mcgill.ca>
2024-11-18 12:09:26 -0500

    Fix mcms test imports

--- deployment/common/changeset/internal/mcms_test.go
+++ deployment/common/changeset/internal/mcms_test.go
@@ -1,7 +1,8 @@
-package internal
+package internal_test
 
 import (
 	"encoding/json"
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -9,7 +10,9 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink/deployment"
-	common2 "github.com/smartcontractkit/chainlink/deployment/common"
+	"github.com/smartcontractkit/chainlink/deployment/common/changeset"
+	"github.com/smartcontractkit/chainlink/deployment/common/changeset/internal"
+	"github.com/smartcontractkit/chainlink/deployment/common/types"
 	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
@@ -20,8 +23,8 @@ func TestDeployMCMSWithConfig(t *testing.T) {
 		chainsel.TEST_90000001.EvmChainID,
 	})
 	ab := deployment.NewMemoryAddressBook()
-	_, err := DeployMCMSWithConfig(ProposerManyChainMultisig,
-		lggr, chains[chainsel.TEST_90000001.Selector], ab, common2.SingleGroupMCMS(t))
+	_, err := internal.DeployMCMSWithConfig(types.ProposerManyChainMultisig,
+		lggr, chains[chainsel.TEST_90000001.Selector], ab, changeset.SingleGroupMCMS(t))
 	require.NoError(t, err)
 }
 
@@ -31,21 +34,22 @@ func TestDeployMCMSWithTimelockContracts(t *testing.T) {
 		chainsel.TEST_90000001.EvmChainID,
 	})
 	ab := deployment.NewMemoryAddressBook()
-	_, err := DeployMCMSWithTimelockContracts(lggr,
+	_, err := internal.DeployMCMSWithTimelockContracts(lggr,
 		chains[chainsel.TEST_90000001.Selector],
-		ab, MCMSConfig{
-			Canceller: common2.SingleGroupMCMS(t),
-			Bypasser:  common2.SingleGroupMCMS(t),
-			Proposer:  common2.SingleGroupMCMS(t),
-			Executors: []common.Address{
+		ab, types.MCMSWithTimelockConfig{
+			Canceller: changeset.SingleGroupMCMS(t),
+			Bypasser:  changeset.SingleGroupMCMS(t),
+			Proposer:  changeset.SingleGroupMCMS(t),
+			TimelockExecutors: []common.Address{
 				chains[chainsel.TEST_90000001.Selector].DeployerKey.From,
 			},
+			TimelockMinDelay: big.NewInt(0),
 		})
 	require.NoError(t, err)
 	addresses, err := ab.AddressesForChain(chainsel.TEST_90000001.Selector)
 	require.NoError(t, err)
 	require.Len(t, addresses, 4)
-	mcmsState, err := common2.LoadMCMSWithTimelockState(chains[chainsel.TEST_90000001.Selector], addresses)
+	mcmsState, err := changeset.LoadMCMSWithTimelockState(chains[chainsel.TEST_90000001.Selector], addresses)
 	require.NoError(t, err)
 	v, err := mcmsState.GenerateMCMSWithTimelockView()
 	b, err := json.MarshalIndent(v, "", "  ")
