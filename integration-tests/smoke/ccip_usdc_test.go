92d88d9de40fb02ff8e978ea5d11c757f75bdbe6
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-11-18 13:31:38 +0100

    Post review fixes

--- integration-tests/smoke/ccip_usdc_test.go
+++ integration-tests/smoke/ccip_usdc_test.go
@@ -1,8 +1,6 @@
 package smoke
 
 import (
-	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/actions"
-	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/testsetups"
 	"golang.org/x/exp/maps"
 	"math/big"
 	"net/http"
@@ -25,6 +23,8 @@ import (
 	"github.com/smartcontractkit/chainlink/deployment"
 	ccdeploy "github.com/smartcontractkit/chainlink/deployment/ccip"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
+	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/actions"
+	"github.com/smartcontractkit/chainlink/integration-tests/ccip-tests/testsetups"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/router"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
