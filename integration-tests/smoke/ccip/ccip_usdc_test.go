53a2687f8bcc05dc7ac0a00e7db31cd07ba91c7a
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-11-22 14:55:21 +0100

    Different naming and topology

--- integration-tests/smoke/ccip/ccip_usdc_test.go
+++ integration-tests/smoke/ccip/ccip_usdc_test.go
@@ -1,8 +1,6 @@
 package smoke
 
 import (
-	"github.com/smartcontractkit/chainlink/integration-tests/testsetups"
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/onramp"
 	"math/big"
 	"testing"
 	"time"
@@ -18,7 +16,9 @@ import (
 
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
+	"github.com/smartcontractkit/chainlink/integration-tests/testsetups"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/onramp"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/router"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/shared/generated/burn_mint_erc677"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
