2270338b8579ed2b9ca084b16aea5bb2f442aadb
0xAustinWang <austiwang+eth@gmail.com>
2024-11-26 00:13:49 +0900

    make test runnable

--- integration-tests/load/ccip/ccip_test.go
+++ integration-tests/load/ccip/ccip_test.go
@@ -32,7 +32,7 @@ var (
 // Parse all events from the simulated chains, send to Loki
 // step 4: teardown
 // Stop the chains, cleanup the environment
-func LoadTestCCIP(t *testing.T) {
+func TestCCIP_Load(t *testing.T) {
 	ctx := tests.Context(t)
 	l := logging.GetTestLogger(t)
 
