5b829d3bd78a66626e744d4a7068ed230fcbd5a3
Bartek Tofel <tofel.b@gmail.com>
2024-11-25 11:02:07 +0100

    fail core tests on purpose

--- core/chains/evm/logpoller/log_poller_test.go
+++ core/chains/evm/logpoller/log_poller_test.go
@@ -411,6 +411,9 @@ func TestLogPoller_BackupPollAndSaveLogsWithPollerNotWorking(t *testing.T) {
 	}
 	th := SetupTH(t, lpOpts)
 
+	//TODO on purpose
+	panic("on purpose")
+
 	// Emit some logs in blocks
 	for i := 0; i < emittedLogs; i++ {
 		if i == 30 {
