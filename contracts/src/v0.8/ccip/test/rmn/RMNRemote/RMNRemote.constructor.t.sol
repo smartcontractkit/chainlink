98991ec2fe4ee12ba715196df0865f0fa3c49629
Rens Rooimans <github@rensrooimans.nl>
2024-11-26 15:59:20 +0100

    add legacy fallback to RMNRemote

--- contracts/src/v0.8/ccip/test/rmn/RMNRemote/RMNRemote.constructor.t.sol
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote/RMNRemote.constructor.t.sol
@@ -5,12 +5,12 @@ import {RMNRemote} from "../../../rmn/RMNRemote.sol";
 import {RMNRemoteSetup} from "./RMNRemoteSetup.t.sol";
 
 contract RMNRemote_constructor is RMNRemoteSetup {
-  function test_constructor_success() public view {
+  function test_constructor() public view {
     assertEq(s_rmnRemote.getLocalChainSelector(), 1);
   }
 
-  function test_constructor_zeroChainSelector_reverts() public {
+  function test_constructor_RevertWhen_ZeroValueNotAllowed() public {
     vm.expectRevert(RMNRemote.ZeroValueNotAllowed.selector);
-    new RMNRemote(0);
+    new RMNRemote(0, s_legacyRMN);
   }
 }
---
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote/RMNRemote.isBlessed.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.24;
+
+import {IRMN} from "../../../interfaces/IRMN.sol";
+
+import {Ownable2Step} from "../../../../shared/access/Ownable2Step.sol";
+import {RMNRemote} from "../../../rmn/RMNRemote.sol";
+import {RMNRemoteSetup} from "./RMNRemoteSetup.t.sol";
+
+contract RMNRemote_isBlessed is RMNRemoteSetup {
+  function test_isBlessed() public {
+    IRMN.TaggedRoot memory taggedRoot = IRMN.TaggedRoot({root: keccak256("root"), commitStore: makeAddr("commitStore")});
+
+    vm.mockCall(
+      address(s_legacyRMN), abi.encodeWithSelector(s_legacyRMN.isBlessed.selector, taggedRoot), abi.encode(true)
+    );
+
+    assertTrue(s_rmnRemote.isBlessed(taggedRoot));
+
+    vm.mockCall(
+      address(s_legacyRMN), abi.encodeWithSelector(s_legacyRMN.isBlessed.selector, taggedRoot), abi.encode(false)
+    );
+
+    assertFalse(s_rmnRemote.isBlessed(taggedRoot));
+  }
+}
