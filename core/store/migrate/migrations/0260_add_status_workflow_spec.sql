4af21d5cf6269694055ff1ffab34f547492d2d96
Justin Kaseman <justinkaseman@live.com>
2024-11-27 09:30:36 -0800

    Revert "feat(workflows): adds orm methods for managing specs (#15356)"
    This reverts commit 1168674bdeaba1f0793e92b4600aaf8f1ff99cb5.

--- core/store/migrate/migrations/0260_add_status_workflow_spec.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- Add a `status` column to the `workflow_specs` table.
-ALTER TABLE workflow_specs
-ADD COLUMN status TEXT DEFAULT '' NOT NULL;
-
--- +goose Down
--- Remove the `status` column from the `workflow_specs` table.
-ALTER TABLE workflow_specs
-DROP COLUMN status;
