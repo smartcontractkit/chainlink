53760a5288243eaf794714f3ba85809570ac1402
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-12-06 13:54:48 +0100

    pass test

--- core/web/resolver/failtest/fail_test.go
+++ core/web/resolver/failtest/fail_test.go
@@ -2,6 +2,10 @@ package failtest
 
 import "testing"
 
-func TestImmediateFail(t *testing.T) {
-	t.Fatalf("This test fails immediately.")
+// func TestImmediateFail(t *testing.T) {
+// 	t.Fatalf("This test fails immediately.")
+// }
+
+func TestPassing(t *testing.T) {
+	t.Log("This test passes.")
 }
