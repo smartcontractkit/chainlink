3eb40fe12e22553f4f7ace358c82b7b84a74878f
connorwstein <connor.stein@mail.mcgill.ca>
2024-12-06 17:29:59 -0500

    Skip USDC

--- integration-tests/smoke/ccip/ccip_usdc_test.go
+++ integration-tests/smoke/ccip/ccip_usdc_test.go
@@ -29,6 +29,7 @@ import (
 * 	chainB (USDC)
  */
 func TestUSDCTokenTransfer(t *testing.T) {
+	t.Skip("need to deflake and optimize")
 	lggr := logger.TestLogger(t)
 	ctx := tests.Context(t)
 	config := &changeset.TestConfigs{
