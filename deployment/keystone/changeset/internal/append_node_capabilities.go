5ebf7e5ee726e2886e28bb3d84f3fc5774fb8f77
krehermann <16602512+krehermann@users.noreply.github.com>
2024-12-07 13:17:29 -0600

    support append capabilities

--- deployment/keystone/changeset/internal/append_node_capabilities.go
+++ deployment/keystone/changeset/internal/append_node_capabilities.go
@@ -33,15 +33,6 @@ func AppendNodeCapabilitiesImpl(lggr logger.Logger, req *AppendNodeCapabilitiesR
 	if err := req.Validate(); err != nil {
 		return nil, fmt.Errorf("failed to validate request: %w", err)
 	}
-	// collect all the capabilities and add them to the registry
-	var capabilities []kcr.CapabilitiesRegistryCapability
-	for _, cap := range req.P2pToCapabilities {
-		capabilities = append(capabilities, cap...)
-	}
-	op, err := kslib.AddCapabilities(lggr, req.ContractSet, req.Chain, capabilities, req.UseMCMS)
-	if err != nil {
-		return nil, fmt.Errorf("failed to add capabilities: %w", err)
-	}
 
 	// for each node, merge the new capabilities with the existing ones and update the node
 	updatesByPeer := make(map[p2pkey.PeerID]NodeUpdate)
@@ -51,6 +42,16 @@ func AppendNodeCapabilitiesImpl(lggr logger.Logger, req *AppendNodeCapabilitiesR
 			return nil, fmt.Errorf("failed to append capabilities for p2p %s: %w", p2pID, err)
 		}
 		updatesByPeer[p2pID] = NodeUpdate{Capabilities: caps[p2pID]}
+	}
+
+	// collect all the capabilities and add them to the registry
+	var capabilities []kcr.CapabilitiesRegistryCapability
+	for _, cap := range req.P2pToCapabilities {
+		capabilities = append(capabilities, cap...)
+	}
+	op, err := kslib.AddCapabilities(lggr, req.ContractSet, req.Chain, capabilities, req.UseMCMS)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add capabilities: %w", err)
 	}
 
 	updateNodesReq := &UpdateNodesRequest{
