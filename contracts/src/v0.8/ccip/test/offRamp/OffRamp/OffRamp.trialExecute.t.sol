ab7ffa190076926112d9854933f964d2f3e50d42
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2024-12-09 21:10:30 +0530

    reorder import

--- contracts/src/v0.8/ccip/test/offRamp/OffRamp/OffRamp.trialExecute.t.sol
+++ contracts/src/v0.8/ccip/test/offRamp/OffRamp/OffRamp.trialExecute.t.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.24;
 
-import {CallWithExactGas} from "../../../../shared/call/CallWithExactGas.sol";
-import {IERC20} from "../../../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {Internal} from "../../../libraries/Internal.sol";
 import {RateLimiter} from "../../../libraries/RateLimiter.sol";
 import {MultiOCR3Base} from "../../../ocr/MultiOCR3Base.sol";
 import {OffRamp} from "../../../offRamp/OffRamp.sol";
 import {OffRampSetup} from "./OffRampSetup.t.sol";
+
+import {CallWithExactGas} from "../../../../shared/call/CallWithExactGas.sol";
+
+import {IERC20} from "../../../../vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract OffRamp_trialExecute is OffRampSetup {
   function setUp() public virtual override {
