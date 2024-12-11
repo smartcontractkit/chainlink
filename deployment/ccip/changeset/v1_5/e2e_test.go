c4ab875e1deba3b7fc85777949562bcc7d2e8e6e
AnieeG <anindita.ghosh@smartcontract.com>
2024-12-11 13:16:42 -0800

    change

---
+++ deployment/ccip/changeset/v1_5/e2e_test.go
@@ -0,0 +1,11 @@
+package v1_5
+
+import (
+	"testing"
+
+	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
+)
+
+func TestE2ELegacy(t *testing.T) {
+	e := changeset.NewMemoryEnvironment()
+}
