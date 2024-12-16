2275efdb785960f4a46303645bc47821b988097b
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2024-12-16 15:06:04 +0100

    fix

--- core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/transmit/event_provider_test.go
+++ core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/transmit/event_provider_test.go
@@ -24,8 +24,6 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ocr2keeper/evmregistry/v21/core"
 )
 
-// test
-
 func TestTransmitEventProvider_Sanity(t *testing.T) {
 	ctx := testutils.Context(t)
 
