a305379d0747c9f92ededf71507cad198fd1166f
Rens Rooimans <github@rensrooimans.nl>
2024-12-17 11:26:35 +0100

    fix test

--- contracts/src/v0.8/ccip/test/rmn/RMNRemote/RMNRemote.verifywithConfigSet.t.sol
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote/RMNRemote.verifywithConfigSet.t.sol
@@ -8,6 +8,7 @@ import {RMNRemoteSetup} from "./RMNRemoteSetup.t.sol";
 contract RMNRemote_verify_withConfigSet is RMNRemoteSetup {
   function setUp() public override {
     super.setUp();
+
     RMNRemote.Config memory config =
       RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, f: 3});
     s_rmnRemote.setConfig(config);
@@ -19,37 +20,32 @@ contract RMNRemote_verify_withConfigSet is RMNRemoteSetup {
   }
 
   function test_verify_RevertWhen_InvalidSignature() public {
-    IRMNRemote.Signature memory sig = s_signatures[s_signatures.length - 1];
-    sig.r = _randomBytes32();
-    s_signatures.pop();
-    s_signatures.push(sig);
+    s_signatures[s_signatures.length - 1].r = 0x0;
 
     vm.expectRevert(RMNRemote.InvalidSignature.selector);
+
     s_rmnRemote.verify(OFF_RAMP_ADDRESS, s_merkleRoots, s_signatures);
   }
 
-  function test_RevertWhen_verify_OutOfOrderSignatures_not_sorted() public {
+  function test_verify_RevertWhen_OutOfOrderSignatures_not_sorted() public {
     IRMNRemote.Signature memory sig1 = s_signatures[s_signatures.length - 1];
-    s_signatures.pop();
-    IRMNRemote.Signature memory sig2 = s_signatures[s_signatures.length - 1];
-    s_signatures.pop();
-    s_signatures.push(sig1);
-    s_signatures.push(sig2);
+    IRMNRemote.Signature memory sig2 = s_signatures[s_signatures.length - 2];
+
+    s_signatures[s_signatures.length - 1] = sig2;
+    s_signatures[s_signatures.length - 2] = sig1;
 
     vm.expectRevert(RMNRemote.OutOfOrderSignatures.selector);
     s_rmnRemote.verify(OFF_RAMP_ADDRESS, s_merkleRoots, s_signatures);
   }
 
-  function test_RevertWhen_verify_OutOfOrderSignatures_duplicateSignature() public {
-    IRMNRemote.Signature memory sig = s_signatures[s_signatures.length - 2];
-    s_signatures.pop();
-    s_signatures.push(sig);
+  function test_verify_RevertWhen_OutOfOrderSignatures_duplicateSignature() public {
+    s_signatures[s_signatures.length - 1] = s_signatures[s_signatures.length - 2];
 
     vm.expectRevert(RMNRemote.OutOfOrderSignatures.selector);
     s_rmnRemote.verify(OFF_RAMP_ADDRESS, s_merkleRoots, s_signatures);
   }
 
-  function test_RevertWhen_verify_UnexpectedSigner() public {
+  function test_verify_RevertWhen_UnexpectedSigner() public {
     _setupSigners(4); // create new signers that aren't configured on RMNRemote
     _generatePayloadAndSigs(2, 4);
 
@@ -57,7 +53,7 @@ contract RMNRemote_verify_withConfigSet is RMNRemoteSetup {
     s_rmnRemote.verify(OFF_RAMP_ADDRESS, s_merkleRoots, s_signatures);
   }
 
-  function test_RevertWhen_verify_ThresholdNotMet() public {
+  function test_verify_RevertWhen_ThresholdNotMet() public {
     RMNRemote.Config memory config =
       RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, f: 2}); // 3 = f+1 sigs required
     s_rmnRemote.setConfig(config);
