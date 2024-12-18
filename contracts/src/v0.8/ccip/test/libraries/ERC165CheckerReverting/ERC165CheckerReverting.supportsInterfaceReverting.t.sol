e87c26b3dd6d959a37e297eb8c75ac9c85d3797e
Josh <josh.weintraub@smartcontract.com>
2024-12-18 17:52:50 -0500

    fix snapshot issues from duplicate tests

--- contracts/src/v0.8/ccip/test/libraries/ERC165CheckerReverting/ERC165CheckerReverting.supportsInterfaceReverting.t.sol
+++ contracts/src/v0.8/ccip/test/libraries/ERC165CheckerReverting/ERC165CheckerReverting.supportsInterfaceReverting.t.sol
@@ -13,46 +13,7 @@ contract ERC165CheckerReverting_supportsInterfaceReverting is ERC165CheckerRever
     assertTrue(s_receiver._supportsInterfaceReverting(type(IAny2EVMMessageReceiver).interfaceId));
   }
 
-  function test__getSupportedInterfacesReverting() public view {
-    bytes4[] memory interfaceIds = new bytes4[](1);
-
-    interfaceIds[0] = type(IAny2EVMMessageReceiver).interfaceId;
-
-    bool[] memory supportedIds = s_receiver._getSupportedInterfacesReverting(interfaceIds);
-    assertTrue(supportedIds[0]);
-    assertEq(interfaceIds.length, supportedIds.length);
-  }
-
-  function test__supportsAllInterfacesReverting() public view {
-    bytes4[] memory interfaceIds = new bytes4[](1);
-
-    interfaceIds[0] = type(IAny2EVMMessageReceiver).interfaceId;
-
-    assertTrue(s_receiver._supportsAllInterfacesReverting(interfaceIds));
-  }
-
-  function test__supportsAllInterfacesReverting_notAllSupported() public view {
-    bytes4[] memory interfaceIds = new bytes4[](2);
-
-    interfaceIds[0] = type(IAny2EVMMessageReceiver).interfaceId;
-    interfaceIds[1] = EXAMPLE_INTERFACE_ID;
-
-    assertFalse(s_receiver._supportsAllInterfacesReverting(interfaceIds));
-  }
-
   // Reverts
-
-  function test__supportsAllInterfacesReverting_notSupportsERC165() public view {
-    bytes4[] memory interfaceIds = new bytes4[](2);
-
-    interfaceIds[0] = type(IAny2EVMMessageReceiver).interfaceId;
-    interfaceIds[1] = EXAMPLE_INTERFACE_ID;
-
-    // An address that does not support ERC165
-    address randomAddress = address(0xdead);
-
-    assertFalse(randomAddress._supportsAllInterfacesReverting(interfaceIds));
-  }
 
   function test__supportsInterfaceReverting_RevertWhen_NotEnoughGasForSupportsInterface() public {
     vm.expectRevert(NOT_ENOUGH_GAS_SIG);
