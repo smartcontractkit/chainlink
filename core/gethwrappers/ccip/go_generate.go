d4809c7c0350a8979c476466fa0e6f6b9b884e73
Rens Rooimans <github@rensrooimans.nl>
2024-12-24 11:39:19 +0100

    remove ccip version of mock_v3_agg

--- core/gethwrappers/ccip/go_generate.go
+++ core/gethwrappers/ccip/go_generate.go
@@ -1,38 +1,38 @@
 package ccip
 
-//go:generate go run ../generation/generate/wrap_short.go ccip Router router
-//go:generate go run ../generation/generate/wrap_short.go ccip CCIPHome ccip_home
-//go:generate go run ../generation/generate/wrap_short.go ccip OnRamp onramp
-//go:generate go run ../generation/generate/wrap_short.go ccip OffRamp offramp
-//go:generate go run ../generation/generate/wrap_short.go ccip FeeQuoter fee_quoter
-//go:generate go run ../generation/generate/wrap_short.go ccip NonceManager nonce_manager
-//go:generate go run ../generation/generate/wrap_short.go ccip MultiAggregateRateLimiter multi_aggregate_rate_limiter
-//go:generate go run ../generation/generate/wrap_short.go ccip TokenAdminRegistry token_admin_registry
-//go:generate go run ../generation/generate/wrap_short.go ccip RegistryModuleOwnerCustom registry_module_owner_custom
-//go:generate go run ../generation/generate/wrap_short.go ccip RMNProxy rmn_proxy_contract
-//go:generate go run ../generation/generate/wrap_short.go ccip RMNRemote rmn_remote
-//go:generate go run ../generation/generate/wrap_short.go ccip RMNHome rmn_home
+//go:generate go run ../generation/wrap.go ccip Router router
+//go:generate go run ../generation/wrap.go ccip CCIPHome ccip_home
+//go:generate go run ../generation/wrap.go ccip OnRamp onramp
+//go:generate go run ../generation/wrap.go ccip OffRamp offramp
+//go:generate go run ../generation/wrap.go ccip FeeQuoter fee_quoter
+//go:generate go run ../generation/wrap.go ccip NonceManager nonce_manager
+//go:generate go run ../generation/wrap.go ccip MultiAggregateRateLimiter multi_aggregate_rate_limiter
+//go:generate go run ../generation/wrap.go ccip TokenAdminRegistry token_admin_registry
+//go:generate go run ../generation/wrap.go ccip RegistryModuleOwnerCustom registry_module_owner_custom
+//go:generate go run ../generation/wrap.go ccip RMNProxy rmn_proxy_contract
+//go:generate go run ../generation/wrap.go ccip RMNRemote rmn_remote
+//go:generate go run ../generation/wrap.go ccip RMNHome rmn_home
 
 // Pools
-//go:generate go run ../generation/generate/wrap_short.go ccip BurnMintTokenPool burn_mint_token_pool
-//go:generate go run ../generation/generate/wrap_short.go ccip BurnFromMintTokenPool burn_from_mint_token_pool
-//go:generate go run ../generation/generate/wrap_short.go ccip BurnWithFromMintTokenPool burn_with_from_mint_token_pool
-//go:generate go run ../generation/generate/wrap_short.go ccip LockReleaseTokenPool lock_release_token_pool
-//go:generate go run ../generation/generate/wrap_short.go ccip TokenPool token_pool
-//go:generate go run ../generation/generate/wrap_short.go ccip USDCTokenPool usdc_token_pool
+//go:generate go run ../generation/wrap.go ccip BurnMintTokenPool burn_mint_token_pool
+//go:generate go run ../generation/wrap.go ccip BurnFromMintTokenPool burn_from_mint_token_pool
+//go:generate go run ../generation/wrap.go ccip BurnWithFromMintTokenPool burn_with_from_mint_token_pool
+//go:generate go run ../generation/wrap.go ccip LockReleaseTokenPool lock_release_token_pool
+//go:generate go run ../generation/wrap.go ccip TokenPool token_pool
+//go:generate go run ../generation/wrap.go ccip USDCTokenPool usdc_token_pool
 
 // Helpers
-//go:generate go run ../generation/generate/wrap_short.go ccip MaybeRevertMessageReceiver maybe_revert_message_receiver
-//go:generate go run ../generation/generate/wrap_short.go ccip PingPongDemo ping_pong_demo
-//go:generate go run ../generation/generate/wrap_short.go ccip MessageHasher message_hasher
-//go:generate go run ../generation/generate/wrap_short.go ccip MultiOCR3Helper multi_ocr3_helper
-//go:generate go run ../generation/generate/wrap_short.go ccip USDCReaderTester usdc_reader_tester
-//go:generate go run ../generation/generate/wrap_short.go ccip ReportCodec report_codec
-//go:generate go run ../generation/generate/wrap_short.go ccip EtherSenderReceiver ether_sender_receiver
-//go:generate go run ../generation/generate/wrap_short.go ccip WETH9 weth9
-//go:generate go run ../generation/generate/wrap_short.go ccip MockE2EUSDCTokenMessenger mock_usdc_token_messenger
-//go:generate go run ../generation/generate/wrap_short.go ccip MockE2EUSDCTransmitter mock_usdc_token_transmitter
-//go:generate go run ../generation/generate/wrap_short.go ccip CCIPReaderTester ccip_reader_tester
+//go:generate go run ../generation/wrap.go ccip MaybeRevertMessageReceiver maybe_revert_message_receiver
+//go:generate go run ../generation/wrap.go ccip PingPongDemo ping_pong_demo
+//go:generate go run ../generation/wrap.go ccip MessageHasher message_hasher
+//go:generate go run ../generation/wrap.go ccip MultiOCR3Helper multi_ocr3_helper
+//go:generate go run ../generation/wrap.go ccip USDCReaderTester usdc_reader_tester
+//go:generate go run ../generation/wrap.go ccip ReportCodec report_codec
+//go:generate go run ../generation/wrap.go ccip EtherSenderReceiver ether_sender_receiver
+//go:generate go run ../generation/wrap.go ccip WETH9 weth9
+//go:generate go run ../generation/wrap.go ccip MockE2EUSDCTokenMessenger mock_usdc_token_messenger
+//go:generate go run ../generation/wrap.go ccip MockE2EUSDCTransmitter mock_usdc_token_transmitter
+//go:generate go run ../generation/wrap.go ccip CCIPReaderTester ccip_reader_tester
 
 // EncodingUtils
-//go:generate go run ../generation/generate/wrap_short.go ccip EncodingUtils ccip_encoding_utils
+//go:generate go run ../generation/wrap.go ccip EncodingUtils ccip_encoding_utils
--- core/gethwrappers/shared/go_generate.go
+++ core/gethwrappers/shared/go_generate.go
@@ -2,11 +2,11 @@
 // golang packages, using abigen.
 package gethwrappers
 
-//go:generate go run ../generation/generate/wrap_short.go shared BurnMintERC677 burn_mint_erc677
-//go:generate go run ../generation/generate/wrap_short.go shared LinkToken link_token
-//go:generate go run ../generation/generate/wrap_short.go shared BurnMintERC20 burn_mint_erc20
-//go:generate go run ../generation/generate/wrap_short.go shared WERC20Mock werc20_mock
+//go:generate go run ../generation/wrap.go shared BurnMintERC677 burn_mint_erc677
+//go:generate go run ../generation/wrap.go shared LinkToken link_token
+//go:generate go run ../generation/wrap.go shared BurnMintERC20 burn_mint_erc20
+//go:generate go run ../generation/wrap.go shared WERC20Mock werc20_mock
 
-//go:generate go run ../generation/generate/wrap_short.go vendor ERC20 erc20
-//go:generate go run ../generation/generate/wrap_short.go vendor Multicall3 multicall3
-//go:generate go run ../generation/generate/wrap_short.go tests MockV3Aggregator mock_v3_aggregator_contract
+//go:generate go run ../generation/wrap.go vendor ERC20 erc20
+//go:generate go run ../generation/wrap.go vendor Multicall3 multicall3
+//go:generate go run ../generation/wrap.go tests MockV3Aggregator mock_v3_aggregator_contract
