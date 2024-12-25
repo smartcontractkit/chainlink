a8164fd24c12b59b11f37f1287ffa6dbc63a0a3c
Rens Rooimans <github@rensrooimans.nl>
2024-12-25 23:36:05 +0100

    move chainlink client to operatorforwarder

--- contracts/src/v0.8/operatorforwarder/ChainlinkClient.sol
+++ contracts/src/v0.8/operatorforwarder/ChainlinkClient.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import {Chainlink} from "./Chainlink.sol";
-import {ENSInterface} from "./interfaces/ENSInterface.sol";
-import {LinkTokenInterface} from "./shared/interfaces/LinkTokenInterface.sol";
-import {ChainlinkRequestInterface} from "./interfaces/ChainlinkRequestInterface.sol";
-import {OperatorInterface} from "./interfaces/OperatorInterface.sol";
-import {PointerInterface} from "./interfaces/PointerInterface.sol";
-import {ENSResolver as ENSResolver_Chainlink} from "./vendor/ENSResolver.sol";
+import {ENSInterface} from "../interfaces/ENSInterface.sol";
+import {LinkTokenInterface} from "../shared/interfaces/LinkTokenInterface.sol";
+import {ChainlinkRequestInterface} from "../interfaces/ChainlinkRequestInterface.sol";
+import {OperatorInterface} from "../interfaces/OperatorInterface.sol";
+import {PointerInterface} from "../interfaces/PointerInterface.sol";
+import {ENSResolver as ENSResolver_Chainlink} from "../vendor/ENSResolver.sol";
 
 /**
  * @title The ChainlinkClient contract
