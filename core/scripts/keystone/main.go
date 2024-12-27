5670c75a47f8e36d8659fa586f132fa417cd1e53
Gabriel Paradiso <gabriel.paradiso@smartcontract.com>
2024-12-27 12:04:03 +0100

    chore: script to generate local ocr3 config

--- core/scripts/keystone/main.go
+++ core/scripts/keystone/main.go
@@ -22,6 +22,7 @@ func main() {
 		src.NewDeployAndInitializeCapabilitiesRegistryCommand(),
 		src.NewDeployWorkflowsCommand(),
 		src.NewDeleteWorkflowsCommand(),
+		src.NewGenerateLocalOCR3ConfigCommand(),
 	}
 
 	commandsList := func(commands []command) string {
