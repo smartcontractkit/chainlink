e51bd46bc4604c2dc428e49e454d531c0149c9a8
Joe Huang <joe.huang@smartcontract.com>
2024-12-31 07:14:36 -0600

    add test and update commit price processing logic

--- core/capabilities/ccip/ccipsolana/commitcodec.go
+++ core/capabilities/ccip/ccipsolana/commitcodec.go
@@ -3,13 +3,14 @@ package ccipsolana
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
+	"math/big"
 
 	agbinary "github.com/gagliardetto/binary"
 	"github.com/gagliardetto/solana-go"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_router"
+	"github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 )
 
@@ -25,46 +26,38 @@ func NewCommitPluginCodecV1() *CommitPluginCodecV1 {
 func (c *CommitPluginCodecV1) Encode(ctx context.Context, report cciptypes.CommitPluginReport) ([]byte, error) {
 	var buf bytes.Buffer
 	encoder := agbinary.NewBorshEncoder(&buf)
+	mr := ccip_router.MerkleRoot{}
 
-	mr := ccip_router.MerkleRoot{
-		SourceChainSelector: uint64(report.MerkleRoots[0].ChainSel),
-		OnRampAddress:       report.MerkleRoots[0].OnRampAddress,
-		MinSeqNr:            uint64(report.MerkleRoots[0].SeqNumsRange.Start()),
-		MaxSeqNr:            uint64(report.MerkleRoots[0].SeqNumsRange.End()),
-		MerkleRoot:          report.MerkleRoots[0].MerkleRoot,
+	if len(report.MerkleRoots) != 0 {
+		mr = ccip_router.MerkleRoot{
+			SourceChainSelector: uint64(report.MerkleRoots[0].ChainSel),
+			OnRampAddress:       report.MerkleRoots[0].OnRampAddress,
+			MinSeqNr:            uint64(report.MerkleRoots[0].SeqNumsRange.Start()),
+			MaxSeqNr:            uint64(report.MerkleRoots[0].SeqNumsRange.End()),
+			MerkleRoot:          report.MerkleRoots[0].MerkleRoot,
+		}
 	}
 
 	tpu := make([]ccip_router.TokenPriceUpdate, 0, len(report.PriceUpdates.TokenPriceUpdates))
 	for _, update := range report.PriceUpdates.TokenPriceUpdates {
-		b, err := update.Price.MarshalJSON()
+		token, err := solana.PublicKeyFromBase58(string(update.TokenID))
 		if err != nil {
-			return nil, fmt.Errorf("error marshaling token price: %w", err)
+			return nil, fmt.Errorf("invalid token address: %s, %v", update.TokenID, err)
 		}
-
-		if len(b) > 28 {
-			return nil, errors.New("token price is too large")
+		if update.Price.IsEmpty() {
+			return nil, fmt.Errorf("empty price for token: %s", update.TokenID)
 		}
-
 		tpu = append(tpu, ccip_router.TokenPriceUpdate{
-			SourceToken: solana.MPK(string(update.TokenID)),
-			UsdPerToken: [28]uint8(b),
+			SourceToken: token,
+			UsdPerToken: common.To28BytesBE(update.Price.Int.Uint64()),
 		})
 	}
 
 	gpu := make([]ccip_router.GasPriceUpdate, 0, len(report.PriceUpdates.GasPriceUpdates))
 	for _, update := range report.PriceUpdates.GasPriceUpdates {
-		b, err := update.GasPrice.MarshalJSON()
-		if err != nil {
-			return nil, fmt.Errorf("error marshaling gas price update: %w", err)
-		}
-
-		if len(b) > 28 {
-			return nil, errors.New("error marshaling gas price update: gas price is too large")
-		}
-
 		gpu = append(gpu, ccip_router.GasPriceUpdate{
 			DestChainSelector: uint64(update.ChainSel),
-			UsdPerUnitGas:     [28]uint8(b),
+			UsdPerUnitGas:     common.To28BytesBE(update.GasPrice.Int.Uint64()),
 		})
 	}
 
@@ -106,26 +99,16 @@ func (c *CommitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (cciptyp
 
 	tokenPriceUpdates := make([]cciptypes.TokenPrice, 0, len(commitReport.PriceUpdates.TokenPriceUpdates))
 	for _, update := range commitReport.PriceUpdates.TokenPriceUpdates {
-		price := cciptypes.BigInt{}
-		err = price.UnmarshalJSON(update.UsdPerToken[:])
-		if err != nil {
-			return cciptypes.CommitPluginReport{}, err
-		}
 		tokenPriceUpdates = append(tokenPriceUpdates, cciptypes.TokenPrice{
 			TokenID: cciptypes.UnknownEncodedAddress(update.SourceToken.String()),
-			Price:   price,
+			Price:   priceHelper(update.UsdPerToken[:]),
 		})
 	}
 
 	gasPriceUpdates := make([]cciptypes.GasPriceChain, 0, len(commitReport.PriceUpdates.GasPriceUpdates))
 	for _, update := range commitReport.PriceUpdates.GasPriceUpdates {
-		price := cciptypes.BigInt{}
-		err = price.UnmarshalJSON(update.UsdPerUnitGas[:])
-		if err != nil {
-			return cciptypes.CommitPluginReport{}, err
-		}
 		gasPriceUpdates = append(gasPriceUpdates, cciptypes.GasPriceChain{
-			GasPrice: price,
+			GasPrice: priceHelper(update.UsdPerUnitGas[:]),
 			ChainSel: cciptypes.ChainSelector(update.DestChainSelector),
 		})
 	}
@@ -137,6 +120,18 @@ func (c *CommitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (cciptyp
 			GasPriceUpdates:   gasPriceUpdates,
 		},
 	}, nil
+}
+
+func priceHelper(input []byte) cciptypes.BigInt {
+	var tokenPrice cciptypes.BigInt
+	price := new(big.Int).SetBytes(input)
+	if price.Int64() == 0 {
+		tokenPrice = cciptypes.NewBigInt(big.NewInt(0))
+	} else {
+		tokenPrice = cciptypes.NewBigInt(price)
+	}
+
+	return tokenPrice
 }
 
 // Ensure CommitPluginCodec implements the CommitPluginCodec interface
---
+++ core/capabilities/ccip/ccipsolana/commitcodec_test.go
@@ -0,0 +1,129 @@
+package ccipsolana
+
+import (
+	"math/big"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	solanacommon "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
+	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
+)
+
+var randomCommitReport = func() cciptypes.CommitPluginReport {
+	pubkey := solanacommon.MakeRandom32ByteArray()
+	return cciptypes.CommitPluginReport{
+		MerkleRoots: []cciptypes.MerkleRootChain{
+			{
+				OnRampAddress: pubkey[:],
+				ChainSel:      cciptypes.ChainSelector(rand.Uint64()),
+				SeqNumsRange: cciptypes.NewSeqNumRange(
+					cciptypes.SeqNum(rand.Uint64()),
+					cciptypes.SeqNum(rand.Uint64()),
+				),
+				MerkleRoot: utils.RandomBytes32(),
+			},
+		},
+		PriceUpdates: cciptypes.PriceUpdates{
+			TokenPriceUpdates: []cciptypes.TokenPrice{
+				{
+					TokenID: "C8WSPj3yyus1YN3yNB6YA5zStYtbjQWtpmKadmvyUXq8",
+					Price:   cciptypes.NewBigInt(big.NewInt(64)),
+				},
+			},
+			GasPriceUpdates: []cciptypes.GasPriceChain{
+				{GasPrice: cciptypes.NewBigInt(big.NewInt(64)), ChainSel: cciptypes.ChainSelector(rand.Uint64())},
+				{GasPrice: cciptypes.NewBigInt(big.NewInt(64)), ChainSel: cciptypes.ChainSelector(rand.Uint64())},
+				{GasPrice: cciptypes.NewBigInt(big.NewInt(64)), ChainSel: cciptypes.ChainSelector(rand.Uint64())},
+			},
+		},
+	}
+}
+
+func TestCommitPluginCodecV1(t *testing.T) {
+	testCases := []struct {
+		name   string
+		report func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport
+		expErr bool
+	}{
+		{
+			name: "base report",
+			report: func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport {
+				return report
+			},
+		},
+		{
+			name: "empty token address",
+			report: func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport {
+				report.PriceUpdates.TokenPriceUpdates[0].TokenID = ""
+				return report
+			},
+			expErr: true,
+		},
+		{
+			name: "empty merkle root",
+			report: func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport {
+				report.MerkleRoots[0].MerkleRoot = cciptypes.Bytes32{}
+				return report
+			},
+		},
+		{
+			name: "zero token price",
+			report: func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport {
+				report.PriceUpdates.TokenPriceUpdates[0].Price = cciptypes.NewBigInt(big.NewInt(0))
+				return report
+			},
+		},
+		{
+			name: "zero gas price",
+			report: func(report cciptypes.CommitPluginReport) cciptypes.CommitPluginReport {
+				report.PriceUpdates.GasPriceUpdates[0].GasPrice = cciptypes.NewBigInt(big.NewInt(0))
+				return report
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			report := tc.report(randomCommitReport())
+			commitCodec := NewCommitPluginCodecV1()
+			ctx := testutils.Context(t)
+			encodedReport, err := commitCodec.Encode(ctx, report)
+			if tc.expErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			decodedReport, err := commitCodec.Decode(ctx, encodedReport)
+			require.NoError(t, err)
+			require.Equal(t, report, decodedReport)
+		})
+	}
+}
+
+func BenchmarkCommitPluginCodecV1_Encode(b *testing.B) {
+	commitCodec := NewCommitPluginCodecV1()
+	ctx := testutils.Context(b)
+
+	rep := randomCommitReport()
+	for i := 0; i < b.N; i++ {
+		_, err := commitCodec.Encode(ctx, rep)
+		require.NoError(b, err)
+	}
+}
+
+func BenchmarkCommitPluginCodecV1_Decode(b *testing.B) {
+	commitCodec := NewCommitPluginCodecV1()
+	ctx := testutils.Context(b)
+	encodedReport, err := commitCodec.Encode(ctx, randomCommitReport())
+	require.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		_, err := commitCodec.Decode(ctx, encodedReport)
+		require.NoError(b, err)
+	}
+}
