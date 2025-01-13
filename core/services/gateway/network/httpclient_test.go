83a5ff0df18bc36da13fcf4295984066650e5729
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2025-01-13 15:57:22 +0000

    [CAPPL-438] SSRF follow-ups
    - Block redirects
    - Check various encoding formats

--- core/services/gateway/network/httpclient_test.go
+++ core/services/gateway/network/httpclient_test.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -9,7 +10,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/doyensec/safeurl"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
@@ -20,11 +20,6 @@ func TestHTTPClient_Send(t *testing.T) {
 
 	// Setup the test environment
 	lggr := logger.Test(t)
-	config := HTTPClientConfig{
-		MaxResponseBytes: 1024,
-		DefaultTimeout:   5 * time.Second,
-	}
-
 	// Define test cases
 	tests := []struct {
 		name          string
@@ -118,6 +113,29 @@ func TestHTTPClient_Send(t *testing.T) {
 			expectedError: &http.MaxBytesError{},
 			expectedResp:  nil,
 		},
+		{
+			name: "redirects are blocked",
+			setupServer: func() *httptest.Server {
+				count := 0
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					count++
+					if count <= 1 {
+						http.Redirect(w, r, "/", http.StatusMovedPermanently)
+					} else {
+						w.WriteHeader(http.StatusOK)
+					}
+					count++
+				}))
+			},
+			request: HTTPRequest{
+				Method:  "GET",
+				URL:     "/",
+				Headers: map[string]string{},
+				Body:    nil,
+				Timeout: 2 * time.Second,
+			},
+			expectedError: errors.New("redirects are not allowed"),
+		},
 	}
 
 	// Execute test cases
@@ -133,18 +151,15 @@ func TestHTTPClient_Send(t *testing.T) {
 			portInt, err := strconv.ParseInt(port, 10, 32)
 			require.NoError(t, err)
 
-			safeConfig := safeurl.
-				GetConfigBuilder().
-				SetTimeout(config.DefaultTimeout).
-				SetAllowedIPs(hostname).
-				SetAllowedPorts(int(portInt)).
-				Build()
-
-			client := &httpClient{
-				config: config,
-				client: safeurl.Client(safeConfig),
-				lggr:   lggr,
+			config := HTTPClientConfig{
+				MaxResponseBytes: 1024,
+				DefaultTimeout:   5 * time.Second,
+				allowedIPs:       []string{hostname},
+				AllowedPorts:     []int{int(portInt)},
 			}
+
+			client, err := NewHTTPClient(config, lggr)
+			require.NoError(t, err)
 
 			tt.request.URL = server.URL + tt.request.URL
 
@@ -174,6 +189,7 @@ func TestHTTPClient_BlocksUnallowed(t *testing.T) {
 	config := HTTPClientConfig{
 		MaxResponseBytes: 1024,
 		DefaultTimeout:   5 * time.Second,
+		BlockedIPs:       []string{"177.0.0.1"},
 	}
 
 	client, err := NewHTTPClient(config, lggr)
@@ -182,81 +198,194 @@ func TestHTTPClient_BlocksUnallowed(t *testing.T) {
 	// Define test cases
 	tests := []struct {
 		name          string
-		request       HTTPRequest
+		url           string
 		expectedError string
 	}{
 		{
-			name: "blocked port",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "http://127.0.0.1:8080",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "blocked port",
+			url:           "http://127.0.0.1:8080",
 			expectedError: "port: 8080 not found in allowlist",
 		},
 		{
-			name: "blocked scheme",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "file://127.0.0.1",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "blocked scheme",
+			url:           "file://127.0.0.1",
 			expectedError: "scheme: file not found in allowlist",
 		},
 		{
-			name: "explicitly blocked IP",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "http://169.254.0.1",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "explicitly blocked IP",
+			url:           "http://169.254.0.1",
 			expectedError: "ip: 169.254.0.1 not found in allowlist",
 		},
 		{
-			name: "explicitly blocked IP - internal network",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "http://169.254.0.1/endpoint",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "explicitly blocked IP - internal network",
+			url:           "http://169.254.0.1/endpoint",
 			expectedError: "ip: 169.254.0.1 not found in allowlist",
 		},
 		{
-			name: "explicitly blocked IP - localhost",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "http://127.0.0.1/endpoint",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "explicitly blocked IP - loopback",
+			url:           "http://127.0.0.1/endpoint",
 			expectedError: "ip: 127.0.0.1 not found in allowlist",
 		},
 		{
-			name: "explicitly blocked IP - current network",
-			request: HTTPRequest{
-				Method:  "GET",
-				URL:     "http://0.0.0.0/endpoint",
-				Headers: map[string]string{},
-				Body:    nil,
-				Timeout: 2 * time.Second,
-			},
+			name:          "explicitly blocked IP - loopback without scheme",
+			url:           "127.0.0.1",
+			expectedError: "host:  is not valid",
+		},
+		{
+			name:          "explicitly blocked IP - loopback",
+			url:           "https://⑫7.0.0.1/",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback shortened",
+			url:           "https://127.1",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback shortened",
+			url:           "https://127.0.1",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback hex no separators",
+			url:           "https://0x17F000001/",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback hex encoded with separators",
+			url:           `https://0x7F.0x00.0x00.0x01`,
+			expectedError: "127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback octal encoded",
+			url:           `https://\\0177.0000.0000.0001`,
+			expectedError: "invalid character",
+		},
+		{
+			name: "explicitly blocked IP - loopback octal encoded",
+			url:  `https://0177.0000.0000.0001`,
+			// 0177.0000.0000.0001 is 127.0.0.1 octal encoded
+			// however Go interprets this as `177.0.0.1`
+			// in the test setup we add that URL to the list of blocked IPs
+			// if the test confirms that 177.0.0.1 is blocked, then that
+			// means we cannot access 127.0.0.1 via its octal encoding.
+			expectedError: "177.0.0.1 not found in allowlist",
+		},
+		{
+			name: "explicitly blocked IP - loopback binary encoded",
+			url:  `https://01111111.00000000.00000000.00000001`,
+			// fails with dial error
+			expectedError: "no such host",
+		},
+		{
+			name: "explicitly blocked IP - loopback - dword",
+			url:  "https://\\2130706433/",
+			// this fails with a parsing error
+			expectedError: "invalid character",
+		},
+		{
+			name: "explicitly blocked IP - loopback - dword with overflow",
+			url:  "https://\\45080379393/",
+			// this fails with a parsing error
+			expectedError: "invalid character",
+		},
+		{
+			name:          "explicitly blocked IP - loopback - dword no escape",
+			url:           "https://2130706433/",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback - dword with overflow no escape",
+			url:           "https://45080379393/",
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback - ipv6",
+			url:           `https://[::1]`,
+			expectedError: "ipv6 blocked",
+		},
+		{
+			name:          "explicitly blocked IP - loopback ipv6 mapped ipv4",
+			url:           `https://[::FFF:7F00:0001]`,
+			expectedError: "ipv6 blocked",
+		},
+		{
+			name:          "explicitly blocked IP - loopback ipv6 mapped ipv4",
+			url:           `https://[::FFFF:127.0.0.1]`,
+			expectedError: "ip: 127.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - loopback long-form",
+			url:           `https://[0000:0000:0000:0000:0000:0000:0000:0001]`,
+			expectedError: "ipv6 blocked",
+		},
+		{
+			name:          "explicitly blocked IP - current network",
+			url:           "http://0.0.0.0/endpoint",
 			expectedError: "ip: 0.0.0.0 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - octal",
+			url:           "http://0000.0000.0000.0001",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - hex",
+			url:           "http://0x00.0x00.0x00.0x01",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - hex no separators",
+			url:           "http://0x00000001",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - binary",
+			url:           "http://00000000.00000000.00000000.00000001",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - shortened",
+			url:           "http://1",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - shortened",
+			url:           "http://0.1",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - current network - shortened",
+			url:           "http://0.0.1",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - dword",
+			url:           "http://42949672961",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - ipv6 mapped",
+			url:           "http://[::FFFF:0000:0001]",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
+		},
+		{
+			name:          "explicitly blocked IP - ipv6 mapped",
+			url:           "http://[::FFFF:0.0.0.1]",
+			expectedError: "ip: 0.0.0.1 not found in allowlist",
 		},
 	}
 
 	// Execute test cases
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := client.Send(context.Background(), tt.request)
+			_, err := client.Send(context.Background(), HTTPRequest{
+				Method:  "GET",
+				URL:     tt.url,
+				Headers: map[string]string{},
+				Body:    nil,
+				Timeout: 10 * time.Millisecond,
+			})
 			require.Error(t, err)
 			require.ErrorContains(t, err, tt.expectedError)
 		})
