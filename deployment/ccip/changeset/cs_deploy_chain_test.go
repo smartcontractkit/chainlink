e4fa6fb87acb01097399839358afdb0f228b6ed7
Terry Tata <terry.tata@smartcontract.com>
2025-01-13 13:27:41 -0500

    parallel

--- deployment/ccip/changeset/cs_deploy_chain_test.go
+++ deployment/ccip/changeset/cs_deploy_chain_test.go
@@ -137,6 +137,7 @@ func TestDeployCCIPContracts(t *testing.T) {
 }
 
 func TestHomeChainChangesetSolana(t *testing.T) {
+	t.Parallel()
 	e := NewMemoryEnvironment(t, WithSolChains(1))
 	evmSelectors := e.Env.AllChainSelectors()
 	homeChainSel := evmSelectors[0]
