9ab4a67362cc61f661a76d7b8fb73b56f87674c8
Oliver Townsend <oliver.townsend@smartcontract.com>
2025-01-13 22:39:13 -0800

    Add chaintype.ChainZircuit to chaintypes with rollup support in L1 oracle

--- core/chains/evm/gas/rollups/l1_oracle.go
+++ core/chains/evm/gas/rollups/l1_oracle.go
@@ -50,7 +50,15 @@ const (
 	PollPeriod = 6 * time.Second
 )
 
-var supportedChainTypes = []chaintype.ChainType{chaintype.ChainArbitrum, chaintype.ChainOptimismBedrock, chaintype.ChainKroma, chaintype.ChainScroll, chaintype.ChainZkSync, chaintype.ChainMantle}
+var supportedChainTypes = []chaintype.ChainType{
+	chaintype.ChainArbitrum,
+	chaintype.ChainOptimismBedrock,
+	chaintype.ChainKroma,
+	chaintype.ChainScroll,
+	chaintype.ChainZkSync,
+	chaintype.ChainMantle,
+	chaintype.ChainZircuit,
+}
 
 func IsRollupWithL1Support(chainType chaintype.ChainType) bool {
 	return slices.Contains(supportedChainTypes, chainType)
