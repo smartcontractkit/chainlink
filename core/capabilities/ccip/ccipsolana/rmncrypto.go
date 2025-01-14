de04fc4b8c0bd07eb54a5cd938d0eb9a3fb85dad
Joe Huang <joe.huang@smartcontract.com>
2025-01-14 13:22:14 -0600

    fix lint

--- core/capabilities/ccip/ccipsolana/rmncrypto.go
+++ core/capabilities/ccip/ccipsolana/rmncrypto.go
@@ -5,13 +5,10 @@ import (
 	"errors"
 
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
-	"github.com/smartcontractkit/chainlink/v2/core/logger"
 )
 
 // SolanaRMNCrypto is the RMNCrypto implementation for Solana chains.
-type SolanaRMNCrypto struct {
-	lggr logger.Logger
-}
+type SolanaRMNCrypto struct{}
 
 func (r *SolanaRMNCrypto) VerifyReportSignatures(
 	_ context.Context,
