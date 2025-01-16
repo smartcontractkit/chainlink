a0fa3366a899c820c34031442a3b98e73c29f1ba
Joe Huang <joe.huang@smartcontract.com>
2025-01-16 14:25:02 -0600

    update

--- core/capabilities/ccip/ccipevm/extraargscodec.go
+++ core/capabilities/ccip/ccipevm/extraargscodec.go
@@ -1,10 +1,6 @@
 package ccipevm
 
 import (
-	"fmt"
-
-	chain_selectors "github.com/smartcontractkit/chain-selectors"
-
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 )
 
@@ -17,23 +13,6 @@ func NewExtraArgsCodec() ExtraArgsCodec {
 }
 
 func (ExtraArgsCodec) DecodeExtraData(extraArgs cciptypes.Bytes, sourceChainSelector cciptypes.ChainSelector) (map[string]any, error) {
-	family, err := chain_selectors.GetSelectorFamily(uint64(sourceChainSelector))
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode extra data, %w", err)
-	}
-
-	extraArgsMap := make(map[string]any)
-	switch family {
-	case chain_selectors.FamilyEVM:
-		v2, err1 := decodeExtraArgsV1V2(extraArgs)
-		if err1 != nil {
-			return nil, err1
-		}
-
-		extraArgsMap[evmExtraArgsKey] = v2
-	case chain_selectors.FamilySolana:
-		// TODO add svm args type decoding logic once on-chain work is finished
-	}
-
-	return extraArgsMap, nil
+	// Not implemented and not return error
+	return nil, nil
 }
