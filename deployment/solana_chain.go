97e753660aae36ad7dd6b72e84e97403b11b1d74
jlaveracll <joaquin.lavera@smartcontract.com>
2025-01-20 11:37:09 -0300

    fix ci checks

--- deployment/solana_chain.go
+++ deployment/solana_chain.go
@@ -85,7 +85,7 @@ func (c SolChain) DeployProgram(logger logger.Logger, programName string) (strin
 		logger.Infow("Deploying program with existing keypair",
 			"programFile", programFile,
 			"programKeyPair", programKeyPair)
-		cmd = exec.Command("solana", append(baseArgs, "--program-id", programKeyPair)...)
+		cmd = exec.Command("solana", append(baseArgs, "--program-id", programKeyPair)...) // #nosec G204
 	} else {
 		// Keypairs wont be created for devenvs
 		logger.Infow("Deploying new program",
