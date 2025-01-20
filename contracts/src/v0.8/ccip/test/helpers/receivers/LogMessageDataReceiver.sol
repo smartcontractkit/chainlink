f282823b9f257952d2d0414d7e1f4c7df46f4635
rishabh570 <rishabhrawat570@gmail.com>
2025-01-20 17:47:18 +0530

    updates LogMessageDataReceiver contract

--- contracts/src/v0.8/ccip/test/helpers/receivers/LogMessageDataReceiver.sol
+++ contracts/src/v0.8/ccip/test/helpers/receivers/LogMessageDataReceiver.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-import {IAny2EVMMessageReceiver} from "../../../interfaces/IAny2EVMMessageReceiver.sol";
 import {ITypeAndVersion} from "../../../../shared/interfaces/ITypeAndVersion.sol";
+import {IAny2EVMMessageReceiver} from "../../../interfaces/IAny2EVMMessageReceiver.sol";
 
 import {Client} from "../../../libraries/Client.sol";
 
@@ -12,6 +12,8 @@ import {IERC165} from "../../../../vendor/openzeppelin-solidity/v5.0.2/contracts
 contract LogMessageDataReceiver is IAny2EVMMessageReceiver, ITypeAndVersion, IERC165 {
   event MessageReceived(bytes data);
 
+  string public constant override typeAndVersion = "LogMessageDataReceiver 1.0.0";
+
   /// @notice IERC165 supports an interfaceId
   /// @param interfaceId The interfaceId to check
   /// @return true if the interfaceId is supported
@@ -19,10 +21,6 @@ contract LogMessageDataReceiver is IAny2EVMMessageReceiver, ITypeAndVersion, IER
     bytes4 interfaceId
   ) public pure override returns (bool) {
     return interfaceId == type(IAny2EVMMessageReceiver).interfaceId || interfaceId == type(IERC165).interfaceId;
-  }
-
-    function typeAndVersion() external pure override returns (string memory) {
-    return "LogMessageDataReceiver 1.0.0";
   }
 
   /// @dev Logs the data of the message received
