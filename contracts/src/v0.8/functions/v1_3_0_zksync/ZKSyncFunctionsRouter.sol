7268d46baa83606f3c0da7d1ecfd26866aa8385f
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-01-20 15:09:53 +0100

    Adds exact gas bound call implementation for zkSync
    This commit adds a safe implementation for the gas bound call
    implementation for zksync. It also adds a custom Functions router
    contract for zkSync.

---
+++ contracts/src/v0.8/functions/v1_3_0_zksync/ZKSyncFunctionsRouter.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {FunctionsRouter} from "../v1_0_0/FunctionsRouter.sol";
+import {CallWithExactGasZKSync} from "../../shared/call/CallWithExactGasZKSync.sol";
+
+/**
+ * @title FunctionsRouterZkSync
+ * @notice Specialized version of FunctionsRouter for zkSync that uses
+ *         CallWithExactGasZKSync to control callback gas usage.
+ */
+contract ZKSyncFunctionsRouter is FunctionsRouter {
+  constructor(address linkToken, Config memory config) FunctionsRouter(linkToken, config) {}
+
+  /**
+   * @dev Override the internal callback function to use CallWithExactGasZKSync
+   *      for controlling and measuring gas usage on zkSync.
+   */
+  function _callback(
+    bytes32 requestId,
+    bytes memory response,
+    bytes memory err,
+    uint32 callbackGasLimit,
+    address client
+  ) private override returns (CallbackResult memory) {
+    // 1. Check if client code exists
+    bool destinationNoLongerExists;
+    assembly {
+      destinationNoLongerExists := iszero(extcodesize(client))
+    }
+    if (destinationNoLongerExists) {
+      // If there's no code at `client`, skip the callback
+      return CallbackResult({success: false, gasUsed: 0, returnData: new bytes(0)});
+    }
+
+    // 2. Encode callback to the client
+    bytes memory encodedCallback = abi.encodeWithSelector(
+      this.getConfig().handleOracleFulfillmentSelector,
+      requestId,
+      response,
+      err
+    );
+
+    // 3. Use our library to enforce an exact gas call
+    (bool success, uint256 gasUsed, bytes memory returnData) = CallWithExactGasZKSync._callWithExactGas(
+      client,
+      callbackGasLimit,
+      encodedCallback,
+      MAX_CALLBACK_RETURN_BYTES
+    );
+    return CallbackResult({success: success, gasUsed: gasUsed, returnData: returnData});
+  }
+}
---
+++ contracts/src/v0.8/shared/call/CallWithExactGasZKSync.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {EfficientCall} from "@zksync/contracts/system-contracts/contracts/libraries/EfficientCall.sol";
+import {ISystemContext} from "@zksync/contracts/gas-bound-caller/contracts/ISystemContext.sol";
+
+ISystemContext constant SYSTEM_CONTEXT_CONTRACT = ISystemContext(address(0x800b));
+
+/**
+ * @title CallWithExactGasZKSync
+ * @notice Library that attempts to call a target contract with exactly `gasAmount` gas on zkSync
+ *         and measures how much gas was actually used.
+ * Implementation based on the GasBoundCaller contract, https://github.com/matter-labs/era-contracts/blob/main/gas-bound-caller/contracts/GasBoundCaller.sol
+ */
+library CallWithExactGasZKSync {
+  /// @notice We assume that no more than `CALL_ENTRY_OVERHEAD` ergs are used for the O(1) operations at the start
+  /// of execution of the contract, such as abi decoding the parameters, jumping to the correct function, etc.
+  uint256 constant CALL_ENTRY_OVERHEAD = 800;
+  /// @notice We assume that no more than `CALL_RETURN_OVERHEAD` ergs are used for the O(1) operations at the end of the execution,
+  /// as such relaying the return.
+  uint256 constant CALL_RETURN_OVERHEAD = 400;
+
+  /// @notice The function that implements limiting of the total gas expenditure of the call.
+  /// @dev On Era, the gas for pubdata is charged at the end of the execution of the entire transaction, meaning
+  /// that if a subcall is not trusted, it can consume lots of pubdata in the process. This function ensures that
+  /// no more than  `_maxTotalGas` will be allowed to be spent by the call. To be sure, this function uses some margin
+  /// (`CALL_ENTRY_OVERHEAD` + `CALL_RETURN_OVERHEAD`) to ensure that the call will not exceed the limit, so it may
+  /// actually spend a bit less than `_maxTotalGas` in the end.
+  /// @dev The entire `gas` passed to this function could be used, regardless
+  /// of the `_maxTotalGas` parameter. In other words, `max(gas(), _maxTotalGas)` is the maximum amount of gas that can be spent by this function.
+  /// @dev The function relays the `returndata` returned by the callee. In case the `callee` reverts, it reverts with the same error.
+  /// @param _to The address of the contract to call.
+  /// @param _maxtotalgas the maximum amount of gas that can be spent by the call.
+  /// @param _data The calldata for the call.
+  /// @param _maxReturnBytes the maximum amount of bytes that can be returned by the call.
+  function _callWithExactGas(
+    address _to,
+    uint256 _maxTotalGas,
+    bytes calldata _data,
+    uint16 _maxReturnBytes
+  ) external payable {
+    // At the start of the execution we deduce how much gas be spent on things that will be
+    // paid for later on by the transaction.
+    // The `expectedForCompute` variable is an upper bound of how much this contract can spend on compute and
+    // MUST be higher or equal to the `gas` passed into the call.
+    uint256 expectedForCompute = gasleft() + CALL_ENTRY_OVERHEAD;
+
+    // We expect that the `_maxTotalGas` at least includes the `gas` required for the call.
+    // This require is more of a safety protection for the users that call this function with incorrect parameters.
+    //
+    // Ultimately, the entire `gas` sent to this call can be spent on compute regardless of the `_maxTotalGas` parameter.
+    require(_maxTotalGas >= gasleft(), "Gas limit is too low");
+
+    // This is the amount of gas that can be spent *exclusively* on pubdata in addition to the `gas` provided to this function.
+    uint256 pubdataAllowance = _maxTotalGas > expectedForCompute ? _maxTotalGas - expectedForCompute : 0;
+
+    uint256 pubdataPublishedBefore = SYSTEM_CONTEXT_CONTRACT.getCurrentPubdataSpent();
+
+    // We never permit system contract calls.
+    // If the call fails, the `EfficientCall.call` will propagate the revert.
+    // Since the revert is propagated, the pubdata published wouldn't change and so no
+    // other checks are needed.
+    bytes memory returnData = EfficientCall.call({
+      _gas: gasleft(),
+      _address: _to,
+      _value: msg.value,
+      _data: _data,
+      _isSystem: false
+    });
+
+    // We will calculate the length of the returndata to be used at the end of the function.
+    // We need additional `96` bytes to encode the offset `0x40` for the entire pubdata,
+    // the gas spent on pubdata as well as the length of the original returndata.
+    // Note, that it has to be padded to the 32 bytes to adhere to proper ABI encoding.
+    uint256 paddedReturndataLen = returnData.length + 96;
+    if (paddedReturndataLen % 32 != 0) {
+      paddedReturndataLen += 32 - (paddedReturndataLen % 32);
+    }
+
+    // limit our paddedReturndataLen to maxReturnBytes bytes
+    if (paddedReturndataLen > _maxReturnBytes) {
+      paddedReturndataLen := _maxReturnBytes;
+    }
+
+    uint256 pubdataPublishedAfter = SYSTEM_CONTEXT_CONTRACT.getCurrentPubdataSpent();
+
+    // It is possible that pubdataPublishedAfter < pubdataPublishedBefore if the call, e.g. removes
+    // some of the previously created state diffs
+    uint256 pubdataSpent = pubdataPublishedAfter > pubdataPublishedBefore
+      ? pubdataPublishedAfter - pubdataPublishedBefore
+      : 0;
+
+    uint256 pubdataGasRate = SYSTEM_CONTEXT_CONTRACT.gasPerPubdataByte();
+
+    // In case there is an overflow here, the `_maxTotalGas` wouldn't be able to cover it anyway, so
+    // we don't mind the contract panicking here in case of it.
+    uint256 pubdataGas = pubdataGasRate * pubdataSpent;
+
+    if (pubdataGas != 0) {
+      // Here we double check that the additional cost is not higher than the maximum allowed.
+      // Note, that the `gasleft()` can be spent on pubdata too.
+      require(pubdataAllowance + gasleft() >= pubdataGas + CALL_RETURN_OVERHEAD, "Not enough gas for pubdata");
+    }
+
+    assembly {
+      // This place does interfere with the memory layout, however, it is done right before
+      // the `return` statement, so it is safe to do.
+      // We need to transform `bytes memory returnData` into (bytes memory returndata, gasSpentOnPubdata)
+      // `bytes memory returnData` is encoded as `length` + `data`.
+      // We need to prepend it with 0x40 and `pubdataGas`.
+      //
+      // It is assumed that the position of returndata is >= 0x40, since 0x40 is the free memory pointer location.
+      mstore(sub(returnData, 0x40), 0x40)
+      mstore(sub(returnData, 0x20), pubdataGas)
+      return(sub(returnData, 0x40), paddedReturndataLen)
+    }
+  }
+}
