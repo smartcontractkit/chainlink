597e56ed32407ea772a0364df9e22ed25ed6e5d8
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-01-20 16:48:30 +0100

    Adds solidity version 0.8.20

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -72,6 +72,10 @@ let config = {
         settings: COMPILER_SETTINGS,
       },
       {
+        version: '0.8.20',
+        settings: COMPILER_SETTINGS,
+      },
+      {
         version: '0.8.24',
         settings: {
           ...COMPILER_SETTINGS,
