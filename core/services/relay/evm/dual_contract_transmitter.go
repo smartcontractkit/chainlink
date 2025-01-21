140aa03deae1d1d0b57bb60678433b5b950019c8
george-dorin <george.dorin@smartcontract.com>
2025-01-21 13:43:23 +0200

    Implement feedback

--- core/services/relay/evm/dual_contract_transmitter.go
+++ core/services/relay/evm/dual_contract_transmitter.go
@@ -183,20 +183,21 @@ func (oc *dualContractTransmitter) lockTransmitters(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	if err = rmPrimary.TryLock(keystore.TXMv1); err != nil {
-		return err
-	}
-	oc.lggr.Debugf("Key %s has been locked for TXMv1", primaryAddress.String())
 
 	secondaryAddress, err := oc.transmitter.SecondaryFromAddress(ctx)
 	if err != nil {
-		return multierr.Append(err, rmPrimary.Unlock(keystore.TXMv1))
+		return err
 	}
 
 	rmSecondary, err := oc.ks.GetResourceMutex(ctx, secondaryAddress)
 	if err != nil {
-		return multierr.Append(err, rmPrimary.Unlock(keystore.TXMv1))
+		return err
 	}
+
+	if err = rmPrimary.TryLock(keystore.TXMv1); err != nil {
+		return err
+	}
+	oc.lggr.Debugf("Key %s has been locked for TXMv1", primaryAddress.String())
 
 	err = rmSecondary.TryLock(keystore.TXMv2)
 	if err != nil {
@@ -207,15 +208,23 @@ func (oc *dualContractTransmitter) lockTransmitters(ctx context.Context) error {
 }
 
 func (oc *dualContractTransmitter) unlockTransmitters(ctx context.Context) error {
+	return multierr.Append(oc.unlockPrimary(ctx), oc.unlockSecondary(ctx))
+}
+
+func (oc *dualContractTransmitter) unlockPrimary(ctx context.Context) error {
 	primaryAddress := oc.transmitter.FromAddress(ctx)
 	rmPrimary, err := oc.ks.GetResourceMutex(ctx, primaryAddress)
 	if err != nil {
 		return err
 	}
-	if err = rmPrimary.Unlock(keystore.TXMv1); err != nil {
+	err = rmPrimary.Unlock(keystore.TXMv1)
+	if err != nil {
 		return err
 	}
 	oc.lggr.Debugf("Key %s has been unlocked for TXMv1", primaryAddress.String())
+	return nil
+}
+func (oc *dualContractTransmitter) unlockSecondary(ctx context.Context) error {
 	secondaryAddress, err := oc.transmitter.SecondaryFromAddress(ctx)
 	if err != nil {
 		return err
