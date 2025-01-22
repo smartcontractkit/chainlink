2e3f50a4aee2102eeece79c19f6cf7c9b9215220
0xAustinWang <austiwang+eth@gmail.com>
2025-01-22 09:20:44 +0800

    changes

--- integration-tests/load/ccip/helpers.go
+++ integration-tests/load/ccip/helpers.go
@@ -10,12 +10,13 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	chainselectors "github.com/smartcontractkit/chain-selectors"
 
+	"math/big"
+
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-testing-framework/wasp"
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/offramp"
-	"math/big"
 )
 
 var wgroup sync.WaitGroup
