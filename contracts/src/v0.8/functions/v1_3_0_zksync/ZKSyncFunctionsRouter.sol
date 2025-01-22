c08cda377cd73ea622a76c982255db21473d1b15
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-01-22 19:33:32 +0100

    Address feedback

--- contracts/src/v0.8/functions/v1_3_0_zksync/ZKSyncFunctionsRouter.sol
+++ contracts/src/v0.8/functions/v1_3_0_zksync/ZKSyncFunctionsRouter.sol
@@ -4,18 +4,16 @@ pragma solidity ^0.8.19;
 import {FunctionsRouter} from "../v1_0_0/FunctionsRouter.sol";
 import {CallWithExactGasZKSync} from "../../shared/call/CallWithExactGasZKSync.sol";
 
-/**
- * @title FunctionsRouterZkSync
- * @notice Specialized version of FunctionsRouter for zkSync that uses
- *         CallWithExactGasZKSync to control callback gas usage.
- */
+///
+/// @title FunctionsRouterZkSync
+/// @notice Specialized version of FunctionsRouter for zkSync that uses
+///         CallWithExactGasZKSync to control callback gas usage.
+///
 contract ZKSyncFunctionsRouter is FunctionsRouter {
   constructor(address linkToken, Config memory config) FunctionsRouter(linkToken, config) {}
 
-  /**
-   * @dev Override the internal callback function to use CallWithExactGasZKSync
-   *      for controlling and measuring gas usage on zkSync.
-   */
+  /// @dev Override the internal callback function to use CallWithExactGasZKSync
+  /// for controlling and measuring gas usage on zkSync.
   function _callback(
     bytes32 requestId,
     bytes memory response,
@@ -23,29 +21,20 @@ contract ZKSyncFunctionsRouter is FunctionsRouter {
     uint32 callbackGasLimit,
     address client
   ) internal override returns (CallbackResult memory) {
-    // 1. Check if client code exists
-    bool destinationNoLongerExists;
-    assembly {
-      destinationNoLongerExists := iszero(extcodesize(client))
-    }
-    if (destinationNoLongerExists) {
+    if (client.code.length == 0)
       // If there's no code at `client`, skip the callback
       return CallbackResult({success: false, gasUsed: 0, returnData: new bytes(0)});
     }
 
-    // 2. Encode callback to the client
-    bytes memory encodedCallback = abi.encodeWithSelector(
-      this.getConfig().handleOracleFulfillmentSelector,
-      requestId,
-      response,
-      err
-    );
-
-    // 3. Use our library to enforce an exact gas call
     (bool success, uint256 gasUsed, bytes memory returnData) = CallWithExactGasZKSync._callWithExactGasSafeReturnData(
       client,
       callbackGasLimit,
-      encodedCallback,
+      abi.encodeWithSelector(
+        this.getConfig().handleOracleFulfillmentSelector,
+        requestId,
+        response,
+        err
+      ),
       MAX_CALLBACK_RETURN_BYTES
     );
     return CallbackResult({success: success, gasUsed: gasUsed, returnData: returnData});
