7e048da1c7b952850cee86628dc1410eeb8a39fa
Mateusz Sekara <mateusz.sekara@gmail.com>
2025-01-23 16:07:54 +0100

    Fixing imports

--- core/capabilities/ccip/ccip_integration_tests/rmn/rmn_home_test.go
+++ core/capabilities/ccip/ccip_integration_tests/rmn/rmn_home_test.go
@@ -2,25 +2,25 @@ package rmn
 
 import (
 	"bytes"
-	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
-	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
-	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"math/big"
 	"slices"
 	"testing"
 	"time"
 
-	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/ccip_integration_tests/integrationhelpers"
-
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 
+	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	readerpkg "github.com/smartcontractkit/chainlink-ccip/pkg/reader"
+	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types"
+
+	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/ccip_integration_tests/integrationhelpers"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_home"
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
-
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/rmn_home"
 )
 
 func TestRMNHomeReader_GetRMNNodesInfo(t *testing.T) {
