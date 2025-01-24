04e729d53a9879b49bc2fe3c385d564026fb7a13
Makram Kamaleddine <makramkd@users.noreply.github.com>
2025-01-24 13:41:56 +0200

    hopefully not flakey

--- integration-tests/smoke/ccip/ccip_reorg_test.go
+++ integration-tests/smoke/ccip/ccip_reorg_test.go
@@ -11,31 +11,41 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	chainsel "github.com/smartcontractkit/chain-selectors"
 	ctf_client "github.com/smartcontractkit/chainlink-testing-framework/lib/client"
+	"github.com/smartcontractkit/chainlink-testing-framework/lib/logging"
 	"github.com/smartcontractkit/chainlink/deployment"
 	ccipcs "github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/testhelpers"
+	"github.com/smartcontractkit/chainlink/deployment/environment/nodeclient"
 	testsetups "github.com/smartcontractkit/chainlink/integration-tests/testsetups/ccip"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/router"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
 )
 
-func Test_CCIPReorgBelowFinality(t *testing.T) {
+func Test_CCIPReorg_BelowFinality_OnSource(t *testing.T) {
 }
 
-func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
-	t.Skip("Flakey")
-	// This test sends a ccip message and re-orgs the chain
-	// after the message block has been finalized.
-	// The result should be that the plugin does not process
-	// messages from the re-orged chain anymore.
-	// However, it should gracefully process messages from non-reorged chains.
+func Test_CCIPReorg_BelowFinality_OnDest(t *testing.T) {
+
+}
+
+func Test_CCIPReorg_GreaterThanFinality_OnDest(t *testing.T) {
+}
+
+// This test sends a ccip message and re-orgs the chain
+// after the message block has been finalized.
+// The result should be that the plugin does not process
+// messages from the re-orged chain anymore.
+// However, it should gracefully process messages from non-reorged chains.
+func Test_CCIPReorg_GreaterThanFinality_OnSource(t *testing.T) {
 	require.Equal(
 		t,
 		os.Getenv(testhelpers.ENVTESTTYPE),
 		string(testhelpers.Docker),
 		"Reorg tests are only supported in docker environments",
 	)
+
+	l := logging.GetTestLogger(t)
 
 	// This test sends a ccip message and re-orgs the chain
 	// prior to the message block being finalized.
@@ -52,6 +62,11 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 				Reason: "We are expecting a re-org beyond finality",
 				Level:  zapcore.DPanicLevel,
 			},
+			{
+				Msg:    "Failed to poll and save logs due to finality violation, retrying later",
+				Reason: "We are expecting a re-org beyond finality",
+				Level:  zapcore.DPanicLevel,
+			},
 		}),
 	)
 
@@ -63,7 +78,7 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 		nonBootstrapP2PIDs = append(nonBootstrapP2PIDs, strings.TrimPrefix(n.PeerID.String(), "p2p_"))
 	}
 
-	t.Log("nonBootstrapP2PIDs:", nonBootstrapP2PIDs)
+	l.Info().Msgf("nonBootstrapP2PIDs: %s", nonBootstrapP2PIDs)
 
 	state, err := ccipcs.LoadOnchainState(e.Env)
 	require.NoError(t, err)
@@ -73,9 +88,8 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 	sourceChainSelector := allChains[0]
 	sourceChain, ok := chainsel.ChainBySelector(sourceChainSelector)
 	require.True(t, ok)
-	headTrackerService := fmt.Sprintf("EVM.%d.HeadTracker", sourceChain.EvmChainID)
 	logPollerService := fmt.Sprintf("EVM.%d.LogPoller", sourceChain.EvmChainID)
-	t.Log("head tracker service name:", headTrackerService, "log poller service name:", logPollerService)
+	l.Info().Msgf("log poller service name: %s", logPollerService)
 	destChainSelector := allChains[1]
 
 	dockerEnv, ok := tEnv.(*testsetups.DeployedLocalDevEnvironment)
@@ -95,6 +109,7 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 	testhelpers.AddLaneWithDefaultPricesAndFeeQuoterConfig(t, &e, state, sourceChainSelector, destChainSelector, false)
 
 	// wait for log poller filters to get registered.
+	l.Info().Msg("waiting for log poller filters to get registered")
 	time.Sleep(30 * time.Second)
 	msgSentEvent := testhelpers.TestSendRequest(t, e.Env, state, sourceChainSelector, destChainSelector, false, router.ClientEVM2AnyMessage{
 		Receiver:     common.LeftPadBytes(state.Chains[destChainSelector].Receiver.Address().Bytes(), 32),
@@ -103,29 +118,17 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 		FeeToken:     common.HexToAddress("0x0"),
 		ExtraArgs:    nil,
 	})
-	t.Log("msgSentEvent:", msgSentEvent)
-	time.Sleep(10 * time.Second)
-	// expectedSeqNum := map[testhelpers.SourceDestPair]uint64{
-	// 	{
-	// 		SourceChainSelector: sourceChainSelector,
-	// 		DestChainSelector:   destChainSelector,
-	// 	}: msgSentEvent.SequenceNumber,
-	// }
-	// expectedSeqNumExec := map[testhelpers.SourceDestPair][]uint64{
-	// 	{
-	// 		SourceChainSelector: sourceChainSelector,
-	// 		DestChainSelector:   destChainSelector,
-	// 	}: {msgSentEvent.SequenceNumber},
-	// }
+	l.Info().Msgf("sent CCIP message, msgSentEvent: %+v", msgSentEvent)
 
 	// Run reorg above finality depth
-	t.Log("starting blockchain reorg on Simulated Geth chain")
 	const reorgDepth = 50
+	l.Info().Int("reorgDepth", reorgDepth).Msgf("starting blockchain reorg on Simulated Geth chain")
 	err = sourceClient.GethSetHead(reorgDepth)
 	require.NoError(t, err, "error starting blockchain reorg on Simulated Geth chain")
 
 	nodeAPIs := dockerEnv.GetCLClusterTestEnv().ClCluster.NodeAPIs()
-	t.Logf("waiting for %d non-bootstrap nodes to report finality violation on the logpoller", len(nodeAPIs)-1)
+	nonBootstrapCount := len(nodeAPIs) - 1
+	l.Info().Msgf("waiting for %d non-bootstrap nodes to report finality violation on the logpoller", nonBootstrapCount)
 	require.Eventually(t, func() bool {
 		violatedResponses := make(map[string]struct{})
 		for _, node := range nodeAPIs {
@@ -133,36 +136,49 @@ func Test_CCIPReorgGreaterThanFinalityOnSource(t *testing.T) {
 			p2pKeys, err := node.MustReadP2PKeys()
 			require.NoError(t, err)
 
-			t.Log("p2pKeys:", p2pKeys)
+			l.Debug().Msgf("got p2pKeys from node API: %+v", p2pKeys)
 
 			require.GreaterOrEqual(t, len(p2pKeys.Data), 1)
 			if !slices.Contains(nonBootstrapP2PIDs, p2pKeys.Data[0].Attributes.PeerID) {
-				t.Log("skipping bootstrap node w/ p2p id", p2pKeys.Data[0].Attributes.PeerID)
+				l.Info().Msgf("skipping bootstrap node w/ p2p id %s", p2pKeys.Data[0].Attributes.PeerID)
 				continue
 			}
 
 			resp, _, err := node.Health()
 			require.NoError(t, err)
-			var violated bool
 			for _, d := range resp.Data {
 				if d.Attributes.Name == logPollerService &&
 					d.Attributes.Output == "finality violated" &&
 					d.Attributes.Status == "failing" {
 					violatedResponses[p2pKeys.Data[0].Attributes.PeerID] = struct{}{}
-					violated = true
 					break
 				}
 			}
 
-			if violated {
-				t.Log("node", p2pKeys.Data[0].Attributes.PeerID, "reported finality violation")
+			if _, ok := violatedResponses[p2pKeys.Data[0].Attributes.PeerID]; ok {
+				l.Info().Msgf("node %s reported finality violation", p2pKeys.Data[0].Attributes.PeerID)
 			} else {
-				t.Log("node", p2pKeys.Data[0].Attributes.PeerID, "did not report finality violation, full response: %+v", resp)
+				l.Info().Msgf("node %s did not report finality violation, log poller response: %+v",
+					p2pKeys.Data[0].Attributes.PeerID,
+					getLogPollerHealth(logPollerService, resp.Data),
+				)
 			}
 		}
 
-		t.Logf("%d nodes reported finality violation", len(violatedResponses))
-		return len(violatedResponses) == (len(nodeAPIs) - 1)
+		l.Info().Msgf("%d nodes reported finality violation", len(violatedResponses))
+		return len(violatedResponses) == nonBootstrapCount
 	}, 3*time.Minute, 5*time.Second, "not all the nodes report finality violation")
-	t.Log("All nodes reported finality violation")
+	l.Info().Msg("All nodes reported finality violation")
+
+	// TODO: assert that the plugin does not commit the message?
+}
+
+func getLogPollerHealth(logPollerService string, healthResponses []nodeclient.HealthResponseDetail) nodeclient.HealthCheck {
+	for _, d := range healthResponses {
+		if d.Attributes.Name == logPollerService {
+			return d.Attributes
+		}
+	}
+
+	return nodeclient.HealthCheck{}
 }
