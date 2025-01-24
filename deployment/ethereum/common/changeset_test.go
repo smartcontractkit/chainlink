aff4cd8c0a19e283ff7a752a21012d2482b0aa9b
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2025-01-24 16:49:04 +0100

    add tests. add confirmation in eth ops

---
+++ deployment/ethereum/common/changeset_test.go
@@ -0,0 +1,40 @@
+package deployment_common
+
+import (
+	"encoding/json"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
+	"github.com/test-go/testify/require"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestLinkChangeset(t *testing.T) {
+
+	lggr := logger.TestLogger(t)
+	e := memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, memory.MemoryEnvironmentConfig{
+		Chains: 1,
+	})
+	chain1 := e.AllChainSelectors()[0]
+
+	changesetInput := ChangesetLinkInput{
+		MintAmount: big.NewInt(1000000000000000000),
+		Amount:     big.NewInt(1000000000000),
+		To:         common.HexToAddress("0x1"),
+		chainId:    chain1,
+	}
+	ret, err := LinkExampleChangeset(e, changesetInput)
+
+	jsonReports, err := json.MarshalIndent(ret.Reports, "", "  ")
+	if err != nil {
+		t.Errorf("Failed to marshal reports to JSON: %v", err)
+	}
+	t.Log(string(jsonReports))
+
+	require.NoError(t, err)
+	require.NotNil(t, ret)
+
+}
--- deployment/ethereum/common/operations_link.go
+++ deployment/ethereum/common/operations_link.go
@@ -8,7 +8,7 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/shared/generated/link_token"
 )
 
-var newLinkToken = deployment_ethereum.NewContractCtorFn(link_token.NewLinkToken)
+// var newLinkToken = deployment_ethereum.NewContractCtorFn(link_token.NewLinkToken)
 
 // Deployment Op
 var DeployLinkOp = deployment_ethereum.NewEthDeployOperationFromBindingNoParams(link_token.DeployLinkToken, "v1")
@@ -27,4 +27,35 @@ func (i TransferLinkInput) Address() common.Address {
 	return i.contractAddress
 }
 
-var TransferLinkOp = deployment_ethereum.NewEthOperationFromBinding[TransferLinkInput](newLinkToken, "v1", "transfer")
+var TransferLinkOp = deployment_ethereum.NewEthOperationFromBinding[TransferLinkInput](link_token.LinkTokenMetaData, "v1", "transfer")
+
+// Mint Op
+type MintLinkInput struct {
+	contractAddress common.Address
+	To              common.Address
+	Amount          *big.Int
+}
+
+func (i MintLinkInput) GetOrderedParams() []any {
+	return []any{i.To, i.Amount}
+}
+func (i MintLinkInput) Address() common.Address {
+	return i.contractAddress
+}
+
+var MintLinkOp = deployment_ethereum.NewEthOperationFromBinding[MintLinkInput](link_token.LinkTokenMetaData, "v1", "mint")
+
+// Grant Mint Role Op
+type GrantLinkInput struct {
+	contractAddress common.Address
+	To              common.Address
+}
+
+func (i GrantLinkInput) GetOrderedParams() []any {
+	return []any{i.To}
+}
+func (i GrantLinkInput) Address() common.Address {
+	return i.contractAddress
+}
+
+var GrantMintLinkOp = deployment_ethereum.NewEthOperationFromBinding[GrantLinkInput](link_token.LinkTokenMetaData, "v1", "grantMintRole")
