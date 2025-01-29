6e6441dafdd44e4b4aceb1ce5f887a3bbffd1324
Joe Huang <joe.huang@smartcontract.com>
2025-01-29 16:49:31 -0600

    refactor

--- core/capabilities/ccip/ccipevm/helpers_test.go
+++ core/capabilities/ccip/ccipevm/helpers_test.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/gagliardetto/solana-go"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/message_hasher"
@@ -73,58 +72,5 @@ func Test_decodeExtraArgs(t *testing.T) {
 		ooe, exist := m["allowOutOfOrderExecution"]
 		require.True(t, exist)
 		require.Equal(t, true, ooe)
-	})
-
-	t.Run("decode dest exec data into map", func(t *testing.T) {
-		destGasAmount := uint32(10000)
-		encoded, err := abiEncodeUint32(destGasAmount)
-		require.NoError(t, err)
-		m, err := DecodeDestExecDataToMap(encoded)
-		require.NoError(t, err)
-		require.Len(t, m, 1)
-
-		decoded, exist := m[evmDestExecDataKey]
-		require.True(t, exist)
-		require.Equal(t, destGasAmount, decoded)
-	})
-
-	t.Run("decode extra args into map svm", func(t *testing.T) {
-		key, err := solana.NewRandomPrivateKey()
-		require.NoError(t, err)
-		cu := uint32(10000)
-		bitmap := uint64(4)
-		ooe := false
-		tokenReceiver := [32]byte(key.PublicKey().Bytes())
-		accounts := [][32]byte{[32]byte(key.PublicKey().Bytes())}
-		decoded, err := d.contract.DecodeSVMExtraArgsV1(nil, cu, bitmap, ooe, tokenReceiver, accounts)
-		if err != nil {
-			return
-		}
-		encoded, err := d.contract.EncodeSVMExtraArgsV1(nil, decoded)
-		require.NoError(t, err)
-
-		m, err := DecodeExtraArgsToMap(encoded)
-		require.NoError(t, err)
-		require.Len(t, m, 5)
-
-		cuDecoded, exist := m["computeUnits"]
-		require.True(t, exist)
-		require.Equal(t, cuDecoded, cu)
-
-		bitmapDecoded, exist := m["accountIsWritableBitmap"]
-		require.True(t, exist)
-		require.Equal(t, bitmapDecoded, bitmap)
-
-		ooeDecoded, exist := m["allowOutOfOrderExecution"]
-		require.True(t, exist)
-		require.Equal(t, ooeDecoded, ooe)
-
-		tokenReceiverDecoded, exist := m["tokenReceiver"]
-		require.True(t, exist)
-		require.Equal(t, tokenReceiverDecoded, tokenReceiver)
-
-		accountsDecoded, exist := m["accounts"]
-		require.True(t, exist)
-		require.Equal(t, accountsDecoded, accounts)
 	})
 }
