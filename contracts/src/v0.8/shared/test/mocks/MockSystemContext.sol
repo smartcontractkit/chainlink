ef922be5a14a842655fdba99c90689693f2641b4
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-01-30 19:28:14 +0200

    Downgrade to solidity 0.8.19

--- contracts/src/v0.8/shared/test/mocks/MockSystemContext.sol
+++ contracts/src/v0.8/shared/test/mocks/MockSystemContext.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import {ISystemContext} from "../../../vendor/@matter-labs/era-contracts/gas-bound-caller/contracts/ISystemContext.sol";
 
