1be2a06d95d2f718795f64feeb9131a847af04c4
AnieeG <anindita.ghosh@smartcontract.com>
2025-01-31 11:18:04 -0800

    add a test for static link

--- deployment/ccip/changeset/cs_deploy_chain.go
+++ deployment/ccip/changeset/cs_deploy_chain.go
@@ -247,9 +247,6 @@ func deployChainContractsForChains(
 				return fmt.Errorf("fee tokens not valid for chain %d, staticLinkExists: %t, linkExists: %t, weth9Exists: %t", chainSel, staticLinkExists, linkExists, weth9Exists)
 			}
 			chain := e.Chains[chainSel]
-			if existingState.Chains[chainSel].LinkToken == nil || existingState.Chains[chainSel].Weth9 == nil {
-				return fmt.Errorf("fee tokens not found for chain %d", chainSel)
-			}
 			deployFn = func() error { return deployChainContractsEVM(e, chain, ab, rmnHome, contractParams) }
 
 		case chainsel.FamilySolana:
