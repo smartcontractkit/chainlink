b6ad6a37dc3068c6882af58c6a3e388f20211e56
Alexander Khozya <alexander.khozya@smartcontract.com>
2025-02-05 17:50:47 +0000

    TEST

--- integration-tests/smoke/ccip/ccip_cs_rmn_curse_uncurse_test.go
+++ integration-tests/smoke/ccip/ccip_cs_rmn_curse_uncurse_test.go
@@ -113,7 +113,7 @@ func TestRMNCurse(t *testing.T) {
 func TestRMNCurseIdempotent(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name+"_CURSE_IDEMPOTENT_NO_MCMS", func(t *testing.T) {
-			runRmnCurseIdempotentTest(t, tc)
+			runRmnCurseIdempotentTest(t, tc) ///zzzz``111
 		})
 	}
 }
