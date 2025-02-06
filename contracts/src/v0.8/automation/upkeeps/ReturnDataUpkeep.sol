d085f73b3c9d9b133f75d015ff1b1a60b74c2606
FelixFan1992 <fankejin@gmail.com>
2025-02-06 17:23:11 -0500

    update

--- contracts/src/v0.8/automation/upkeeps/ReturnDataUpkeep.sol
+++ contracts/src/v0.8/automation/upkeeps/ReturnDataUpkeep.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.24;
 
 contract ReturnDataUpkeep {
-    uint256 public testRange;
-    uint256 public interval;
+    uint256 public testRange = 1000000;
+    uint256 public interval = 100;
     uint256 public lastTimestamp;
     uint256 public previousPerformBlock;
     uint256 public initialTimestamp;
-    uint256 public length;
-    uint256 public size;
+    uint256 public length = 300;
+    uint256 public size = 999999;
     mapping(uint256 => bytes) public data;
 
     function setConfig(uint256 _testRange, uint256 _interval, uint256 _length) external {
@@ -36,6 +36,9 @@ contract ReturnDataUpkeep {
         for (uint256 i = 0; i < length; i++) {
             data[i] = new bytes(length);
         }
+
+        lastTimestamp = block.timestamp;
+        previousPerformBlock = block.number;
 
         return new bytes(size);
     }
--- contracts/src/v0.8/automation/upkeeps/ReturnDataUpkeepCompatible.sol
+++ contracts/src/v0.8/automation/upkeeps/ReturnDataUpkeepCompatible.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.24;
 
 contract ReturnDataUpkeepCompatible {
-    uint256 public testRange;
-    uint256 public interval;
+    uint256 public testRange = 1000000;
+    uint256 public interval = 100;
     uint256 public lastTimestamp;
     uint256 public previousPerformBlock;
     uint256 public initialTimestamp;
-    uint256 public length;
-    uint256 public size;
+    uint256 public length = 300;
+    uint256 public size = 999999;
     mapping(uint256 => bytes) public data;
 
     function setConfig(uint256 _testRange, uint256 _interval, uint256 _length) external {
@@ -36,6 +36,9 @@ contract ReturnDataUpkeepCompatible {
         for (uint256 i = 0; i < length; i++) {
             data[i] = new bytes(length);
         }
+
+        lastTimestamp = block.timestamp;
+        previousPerformBlock = block.number;
 
         uint256 s = size;
         bytes memory result = new bytes(size);
