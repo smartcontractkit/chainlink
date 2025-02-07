4ea5e26cab48d3aa9551afa9dbcefe1e9a886a53
krehermann <16602512+krehermann@users.noreply.github.com>
2025-02-07 12:46:21 -0700

    forbid wf don update; tests

--- deployment/keystone/changeset/update_don_test.go
+++ deployment/keystone/changeset/update_don_test.go
@@ -9,6 +9,7 @@ import (
 
 	commonchangeset "github.com/smartcontractkit/chainlink/deployment/common/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/common/proposalutils"
+	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 	"github.com/smartcontractkit/chainlink/deployment/keystone/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/keystone/changeset/internal"
 
@@ -38,106 +39,115 @@ func TestUpdateDon(t *testing.T) {
 	capBCfgB, err := proto.Marshal(capBCfg)
 	require.NoError(t, err)
 
-	t.Run("no mcms", func(t *testing.T) {
+	// run the same tests for both mcms and non-mcms
+	var mcmsConfigs = []*changeset.MCMSConfig{nil, {MinDuration: 0}}
+	for _, mcmsConfig := range mcmsConfigs {
+		prefix := "no mcms"
+		if mcmsConfig != nil {
+			prefix = "with mcms"
+		}
 		te := test.SetupTestEnv(t, test.TestConfig{
 			WFDonConfig:     test.DonConfig{N: 4},
 			AssetDonConfig:  test.DonConfig{N: 4},
 			WriterDonConfig: test.DonConfig{N: 4},
 			NumChains:       1,
+			UseMCMS:         mcmsConfig != nil,
 		})
 
-		// contract set is already deployed with capabilities
-		// we have to keep track of the existing capabilities to add to the new ones
-		var p2pIDs []p2pkey.PeerID
-		newCapabilities := make(map[p2pkey.PeerID][]kcr.CapabilitiesRegistryCapability)
-		for id := range te.WFNodes {
-			k, err := p2pkey.MakePeerID(id)
-			require.NoError(t, err)
-			p2pIDs = append(p2pIDs, k)
-			newCapabilities[k] = caps
-		}
-
-		t.Run("succeeds if update sets new and existing capabilities", func(t *testing.T) {
-			cfg := changeset.UpdateDonRequest{
-				RegistryChainSel: te.RegistrySelector,
-				P2PIDs:           p2pIDs,
-				CapabilityConfigs: []changeset.CapabilityConfig{
-					{
-						Capability: capA, Config: capACfgB,
+		t.Run(prefix, func(t *testing.T) {
+			type testCase struct {
+				name            string
+				nodeSetToUpdate map[string]memory.Node
+				checkErr        func(t *testing.T, useMCMS bool, err error)
+				mcmsConfig      *changeset.MCMSConfig
+			}
+			var cases = []testCase{
+				{
+					name:            "forbid wf update",
+					nodeSetToUpdate: te.WFNodes,
+					checkErr: func(t *testing.T, useMCMS bool, err error) {
+						if useMCMS {
+							assert.ErrorContains(t, err, "sadfasdds")
+						}
+						assert.ErrorContains(t, err, "refusing to update workflow don")
 					},
-					{
-						Capability: capB, Config: capBCfgB,
-					},
+					mcmsConfig: mcmsConfig,
+				},
+				{
+					name:            "writer don update ok",
+					nodeSetToUpdate: te.CWNodes,
+					mcmsConfig:      mcmsConfig,
 				},
 			}
+			for _, tc := range cases {
+				t.Run(tc.name, func(t *testing.T) {
 
-			csOut, err := changeset.UpdateDon(te.Env, &cfg)
-			require.NoError(t, err)
-			require.Empty(t, csOut.Proposals)
-			require.Nil(t, csOut.AddressBook)
+					// contract set is already deployed with capabilities
+					// we have to keep track of the existing capabilities to add to the new ones
+					var p2pIDs []p2pkey.PeerID
+					newCapabilities := make(map[p2pkey.PeerID][]kcr.CapabilitiesRegistryCapability)
+					for id := range tc.nodeSetToUpdate {
+						k, err := p2pkey.MakePeerID(id)
+						require.NoError(t, err)
+						p2pIDs = append(p2pIDs, k)
+						newCapabilities[k] = caps
+					}
 
-			assertDonContainsCapabilities(t, te.ContractSets()[te.RegistrySelector].CapabilitiesRegistry, caps, p2pIDs)
+					cfg := changeset.UpdateDonRequest{
+						RegistryChainSel: te.RegistrySelector,
+						P2PIDs:           p2pIDs,
+						CapabilityConfigs: []changeset.CapabilityConfig{
+							{
+								Capability: capA, Config: capACfgB,
+							},
+							{
+								Capability: capB, Config: capBCfgB,
+							},
+						},
+						MCMSConfig: tc.mcmsConfig,
+					}
+
+					csOut, err := changeset.UpdateDon(te.Env, &cfg)
+					if err != nil && tc.checkErr == nil {
+						t.Errorf("non nil err from UpdateDon %v but no checkErr func defined", err)
+					}
+					useMCMS := cfg.MCMSConfig != nil
+					if !useMCMS {
+						if tc.checkErr != nil {
+							tc.checkErr(t, useMCMS, err)
+							return
+						}
+					} else {
+						// when using mcms there are two kinds of errors:
+						// those from creating the proposal and those executing the proposal
+						// if we have a non-nil err here, its from creating the proposal
+						// so check it and do not proceed to applying the proposal
+						if err != nil {
+							tc.checkErr(t, useMCMS, err)
+							return
+						}
+						require.NotNil(t, csOut.Proposals) //nolint:staticcheck //SA1019 ignoring deprecated field for compatibility; we don't have tools to generate the new field
+						require.Len(t, csOut.Proposals, 1) //nolint:staticcheck //SA1019 ignoring deprecated field for compatibility; we don't have tools to generate the new field
+						applyErr := applyProposal(t, te, []commonchangeset.ChangesetApplication{
+							{
+								Changeset: commonchangeset.WrapChangeSet(changeset.UpdateDon),
+								Config:    &cfg,
+							},
+						})
+						if tc.checkErr != nil {
+							tc.checkErr(t, useMCMS, applyErr)
+							return
+						}
+					}
+					//			require.NoError(t, err)
+					//			require.Empty(t, csOut.Proposals)
+					//			require.Nil(t, csOut.AddressBook)
+
+					assertDonContainsCapabilities(t, te.ContractSets()[te.RegistrySelector].CapabilitiesRegistry, caps, p2pIDs)
+				})
+			}
 		})
-	})
-	t.Run("with mcms", func(t *testing.T) {
-		te := test.SetupTestEnv(t, test.TestConfig{
-			WFDonConfig:     test.DonConfig{N: 4},
-			AssetDonConfig:  test.DonConfig{N: 4},
-			WriterDonConfig: test.DonConfig{N: 4},
-			NumChains:       1,
-			UseMCMS:         true,
-		})
-
-		// contract set is already deployed with capabilities
-		// we have to keep track of the existing capabilities to add to the new ones
-		var p2pIDs []p2pkey.PeerID
-		for id := range te.WFNodes {
-			k, err := p2pkey.MakePeerID(id)
-			require.NoError(t, err)
-			p2pIDs = append(p2pIDs, k)
-		}
-
-		cfg := changeset.UpdateDonRequest{
-			RegistryChainSel: te.RegistrySelector,
-			P2PIDs:           p2pIDs,
-			CapabilityConfigs: []changeset.CapabilityConfig{
-				{
-					Capability: capA,
-					Config:     capACfgB,
-				},
-				{
-					Capability: capB,
-					Config:     capBCfgB,
-				},
-			},
-			MCMSConfig: &changeset.MCMSConfig{MinDuration: 0},
-		}
-
-		csOut, err := changeset.UpdateDon(te.Env, &cfg)
-		require.NoError(t, err)
-
-		require.Len(t, csOut.Proposals, 1)
-		require.Len(t, csOut.Proposals[0].Transactions, 1)          // append node capabilties cs, update don
-		require.Len(t, csOut.Proposals[0].Transactions[0].Batch, 3) // add capabilities, update nodes, update don
-		require.Nil(t, csOut.AddressBook)
-
-		// now apply the changeset such that the proposal is signed and execed
-		contracts := te.ContractSets()[te.RegistrySelector]
-		timelockContracts := map[uint64]*proposalutils.TimelockExecutionContracts{
-			te.RegistrySelector: {
-				Timelock:  contracts.Timelock,
-				CallProxy: contracts.CallProxy,
-			},
-		}
-		_, err = commonchangeset.ApplyChangesets(t, te.Env, timelockContracts, []commonchangeset.ChangesetApplication{
-			{
-				Changeset: commonchangeset.WrapChangeSet(changeset.UpdateDon),
-				Config:    &cfg,
-			},
-		})
-		require.NoError(t, err)
-		assertDonContainsCapabilities(t, te.ContractSets()[te.RegistrySelector].CapabilitiesRegistry, caps, p2pIDs)
-	})
+	}
 }
 
 func assertDonContainsCapabilities(t *testing.T, registry *kcr.CapabilitiesRegistry, want []kcr.CapabilitiesRegistryCapability, p2pIDs []p2pkey.PeerID) {
@@ -167,4 +177,17 @@ func capIDsFromCapCfgs(cfgs []kcr.CapabilitiesRegistryCapabilityConfiguration) [
 		out[i] = c.CapabilityId
 	}
 	return out
+}
+
+func applyProposal(t *testing.T, te test.TestEnv, applicable []commonchangeset.ChangesetApplication) error {
+	// now apply the changeset such that the proposal is signed and execed
+	contracts := te.ContractSets()[te.RegistrySelector]
+	timelockContracts := map[uint64]*proposalutils.TimelockExecutionContracts{
+		te.RegistrySelector: {
+			Timelock:  contracts.Timelock,
+			CallProxy: contracts.CallProxy,
+		},
+	}
+	_, err := commonchangeset.ApplyChangesets(t, te.Env, timelockContracts, applicable)
+	return err
 }
