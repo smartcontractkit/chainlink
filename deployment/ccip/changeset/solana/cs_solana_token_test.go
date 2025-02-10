4169167c3b9edbf3570d1236f7d02e14d8a695eb
Terry Tata <terry.tata@smartcontract.com>
2025-02-10 16:27:00 -0500

    try without parallel call

--- deployment/ccip/changeset/solana/cs_solana_token_test.go
+++ deployment/ccip/changeset/solana/cs_solana_token_test.go
@@ -21,7 +21,6 @@ import (
 )
 
 func TestSolanaTokenOps(t *testing.T) {
-	t.Parallel()
 	lggr := logger.TestLogger(t)
 	e := memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, memory.MemoryEnvironmentConfig{
 		SolChains: 1,
