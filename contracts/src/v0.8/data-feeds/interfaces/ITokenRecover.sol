b28c02f1269c7e361840a6706583d8ad912a18bc
Karen Stepanyan <karen.stepanyan@smartcontract.com>
2025-02-10 16:39:46 +0400

    change interface version

--- contracts/src/v0.8/data-feeds/interfaces/ITokenRecover.sol
+++ contracts/src/v0.8/data-feeds/interfaces/ITokenRecover.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
 import {IERC20} from "./../../vendor/openzeppelin-solidity/v5.0.2/contracts/interfaces/IERC20.sol";
 
