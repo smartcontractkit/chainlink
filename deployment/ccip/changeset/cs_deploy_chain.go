086b57773216d798ac52bfa8b03260df5b9446f3
jlaveracll <joaquin.lavera@smartcontract.com>
2025-02-10 15:54:36 -0300

    Update cs_deploy_chain.go

--- deployment/ccip/changeset/cs_deploy_chain.go
+++ deployment/ccip/changeset/cs_deploy_chain.go
@@ -230,6 +230,9 @@ func deployChainContractsForChains(
 	deployGrp := errgroup.Group{}
 
 	for chainSel, contractParams := range contractParamsPerChain {
+		if _, exists := existingState.SupportedChains()[chainSel]; !exists {
+			return fmt.Errorf("chain %d not supported", chainSel)
+		}
 		// already validated family
 		family, _ := chainsel.GetSelectorFamily(chainSel)
 		var deployFn func() error
