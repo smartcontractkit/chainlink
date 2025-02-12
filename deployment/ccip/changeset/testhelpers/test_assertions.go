4146521dd3823da23f5752ea51322698174f503b
AnieeG <anindita.ghosh@smartcontract.com>
2025-02-11 16:31:10 -0800

    import fix

--- deployment/ccip/changeset/testhelpers/test_assertions.go
+++ deployment/ccip/changeset/testhelpers/test_assertions.go
@@ -24,7 +24,7 @@ import (
 	commonchangeset "github.com/smartcontractkit/chainlink/deployment/common/changeset"
 
 	"github.com/smartcontractkit/chainlink/deployment"
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/fee_quoter"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/v1_6_0/fee_quoter"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/v1_6_0/offramp"
 )
 
