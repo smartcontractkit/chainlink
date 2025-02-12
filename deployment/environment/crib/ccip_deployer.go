e10fbd0ec6e88447b22799604a348ae4a3d4dc4e
0xAustinWang <austiwang+eth@gmail.com>
2025-02-13 02:06:37 +0800

    goimport

--- deployment/environment/crib/ccip_deployer.go
+++ deployment/environment/crib/ccip_deployer.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/v1_5_1/token_pool"
 	"math/big"
+
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/v1_5_1/token_pool"
 
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/globals"
 
