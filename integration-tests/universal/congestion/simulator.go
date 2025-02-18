963e1dba9a1e30e3d25c6781186997a8a544ce58
Dmytro Haidashenko <dmytro.haidashenko@smartcontract.com>
2025-02-18 11:48:59 +0100

    Rely on EIP-1559 and congestion to reach fees target

--- integration-tests/universal/congestion/simulator.go
+++ integration-tests/universal/congestion/simulator.go
@@ -3,6 +3,7 @@ package congestion
 import (
 	"context"
 	"fmt"
+	"math/big"
 	"sort"
 	"sync"
 	"testing"
@@ -22,33 +23,40 @@ import (
 	"github.com/smartcontractkit/chainlink/integration-tests/contracts"
 )
 
-const backoffPeriodOnError = time.Second
-const defaultTxPerBlock = 10
-const congestionMinTarget = 0.9
-const maxBlocksToCalcCongestion = 3
+const (
+	backoffPeriodOnError = time.Second
+	defaultTxPerBlock    = 10
+	defaultTip           = 5_000_000_000
+)
 
-// Input - defines configuration for chain congestion - state when block is filled with transactions
-// with high gas price.
+// Input - defines configuration for chain congestion - state when mem pool is filled with transactions and gas price increases.
+// Congestion is delayed by InitialDelay on the first iteration and by Period afterward.
+// During the ramp-up phase, which lasts RampUpDuration, the mem pool is filled with transactions whose fees gradually increase up to GasPriceIncreasePercent.
+// During the plateau phase, which lasts Duration, the mem pool is filled with transactions, but fees are constant.
+// During the cooldown phase, which lasts Duration, the mem pool is filled with transactions, and fees are gradually decreasing to the baseline.
 type Input struct {
-	InitialDelay            time.Duration `toml:"initial_delay"` // defines delay before the first surge
-	Period                  time.Duration `toml:"period"`        // defines delays between surges
-	Duration                time.Duration `toml:"duration"`      // defines duration of a surge
-	GasPriceIncreasePercent int64         `toml:"gas_price_increase_percent"`
-	NumberOfTxsPerBlock     *int          `toml:"number_of_txs_per_block"` // defines number of transaction to be produced to fill a block (default: defaultTxPerBlock)
-	PayloadSize             *int          `toml:"payload_size"`            // defines payload size of transactions to fill a block. Calculated automatically, if not specified
-	GasLimit                *uint64       `toml:"gas_limit"`               // calculated automatically if not specified
-	MinTxsInPool            *int          `toml:"min_txs_in_pool"`         // defines minimal number of transactions Simulator will try to maintain in the mem pool
+	InitialDelay          time.Duration `toml:"initial_delay"`    // defines delay before the first surge
+	Period                time.Duration `toml:"period"`           // defines delay between surges
+	RampUpDuration        time.Duration `toml:"ramp_up_duration"` // defines time it takes for gas price to pick
+	CoolDownDuration      time.Duration `toml:"cool_down_duration"`
+	Duration              time.Duration `toml:"duration"` // defines duration of plateau
+	GasPriceTargetPercent float64       `toml:"gas_price_target_percent"`
+	Tip                   int64         `toml:"tip"` // defines initial tip
+
+	// following values are optional and calculated automatically
+	NumberOfTxsPerBlock int    `toml:"number_of_txs_per_block"` // defines number of transaction to be produced to fill a block (default: defaultTxPerBlock)
+	PayloadSize         *int   `toml:"payload_size"`            // defines payload size that allows NumberOfTxsPerBlock transactions to fully fill a block.
+	GasLimit            uint64 `toml:"gas_limit"`               // calculated automatically if not specified
+	InitialBaseFee      uint64 `toml:"initial_base_fee"`
 }
 
 func (i *Input) defaults() {
-	if i.NumberOfTxsPerBlock == nil {
-		i.NumberOfTxsPerBlock = new(int)
-		*i.NumberOfTxsPerBlock = defaultTxPerBlock
+	if i.NumberOfTxsPerBlock == 0 {
+		i.NumberOfTxsPerBlock = defaultTxPerBlock
 	}
 
-	if i.MinTxsInPool == nil {
-		i.MinTxsInPool = new(int)
-		*i.MinTxsInPool = *i.NumberOfTxsPerBlock * 2
+	if i.Tip == 0 {
+		i.Tip = defaultTip
 	}
 }
 
@@ -69,12 +77,8 @@ type Simulator struct {
 
 func NewSimulator(t *testing.T, input Input, client *seth.Client) (*Simulator, error) {
 	input.defaults()
-	// required to calculate congestion
-	if client.HeaderCache == nil {
-		client.HeaderCache = seth.NewLFUBlockCache(maxBlocksToCalcCongestion)
-	}
 
-	if *input.MinTxsInPool > len(client.Addresses) {
+	if input.NumberOfTxsPerBlock > len(client.Addresses) {
 		return nil, fmt.Errorf("min_txs_in_pool cannot be greater than number of addresses available for seth.Client %d. Increase number of addresses", len(client.Addresses))
 	}
 
@@ -111,12 +115,21 @@ func (s *Simulator) start(ctx context.Context) error {
 		s.Input.PayloadSize = &payloadSize
 	}
 
-	if s.Input.GasLimit == nil {
+	if s.Input.GasLimit == 0 {
 		gasLimit, err := s.estimateGasLimit(ctx)
 		if err != nil {
 			return fmt.Errorf("failed to estimate gas limit: %v", err)
 		}
-		s.Input.GasLimit = &gasLimit
+		s.Input.GasLimit = gasLimit
+	}
+
+	if s.InitialBaseFee == 0 {
+		header, err := s.client.Client.HeaderByNumber(ctx, nil)
+		if err != nil {
+			return fmt.Errorf("failed to get header by number to capture inital base fee: %v", err)
+		}
+
+		s.InitialBaseFee = header.BaseFee.Uint64()
 	}
 
 	s.payload = []string{string(make([]byte, *s.Input.PayloadSize))}
@@ -134,16 +147,33 @@ func (s *Simulator) runTick(ctx context.Context) {
 	s.lggr.Info("starting congestion")
 	ctx, cancel := context.WithTimeout(ctx, s.Duration)
 	defer cancel()
-	work := make(chan struct{}, *s.Input.MinTxsInPool)
-	wg.Add(*s.Input.MinTxsInPool)
-	for i := range *s.Input.MinTxsInPool {
+	numberOfSenders := s.NumberOfTxsPerBlock * 2
+	work := make(chan struct{}, numberOfSenders)
+	wg.Add(numberOfSenders)
+	for i := range numberOfSenders {
 		go s.runSender(ctx, &wg, i, work)
 	}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		txsToSend := s.Input.NumberOfTxsPerBlock // no need to make sync as it's not access concurrently
+		prevBaseFeePercent := float64(0)
 		s.runOnNewBlockUntilCanceled(ctx, func(ctx context.Context, head *types.Header) error {
-			for range *s.Input.MinTxsInPool {
+			baseFeePercent := s.calculateBaseFeePercent(head)
+			if prevBaseFeePercent == 0 {
+				prevBaseFeePercent = baseFeePercent
+			}
+
+			distance := s.Input.GasPriceTargetPercent - baseFeePercent
+			speed := baseFeePercent - prevBaseFeePercent
+			prevBaseFeePercent = baseFeePercent
+			if distance > 0 && speed < 0 {
+				txsToSend++
+			} else if distance < 0 && speed > 0 {
+				txsToSend--
+			}
+			s.lggr.Infof("got new head - scheduling work: %.2f %.2f number of txs: %d congestion %.2f", baseFeePercent, s.Input.GasPriceTargetPercent, txsToSend, float64(head.GasUsed)/float64(head.GasLimit))
+			for range txsToSend {
 				select {
 				case work <- struct{}{}:
 				case <-ctx.Done():
@@ -170,7 +200,7 @@ func (s *Simulator) runSender(ctx context.Context, wg *sync.WaitGroup, key int, 
 			return
 		case <-send:
 			_, err := s.emitter.EmitLogStringsFromKeyAsync(s.payload, key, func(o *bind.TransactOpts) {
-				o.GasLimit = *s.Input.GasLimit
+				o.GasLimit = s.Input.GasLimit
 			})
 			if err != nil {
 				s.lggr.Errorw("failed to emit log to fill block", "error", err)
@@ -179,30 +209,41 @@ func (s *Simulator) runSender(ctx context.Context, wg *sync.WaitGroup, key int, 
 	}
 }
 
-func (s *Simulator) reportCongestion(_ context.Context, header *types.Header) error {
-	if s.congestionStartedAt == nil {
+func (s *Simulator) reportCongestion(ctx context.Context, header *types.Header) error {
+	/*if s.congestionStartedAt == nil {
 		s.congestionStartedAt = new(uint64)
 		*s.congestionStartedAt = header.Number.Uint64()
 	}
 
-	numberOfBlockToInclude := min(maxBlocksToCalcCongestion, header.Number.Uint64()-*s.congestionStartedAt+1)
-	congestion, err := s.client.CalculateNetworkCongestionMetric(numberOfBlockToInclude, seth.CongestionStrategy_Simple)
-	if err != nil {
-		return fmt.Errorf("failed to calculate congestion metric: %v", err)
-	}
-
-	block, err := s.client.Client.BlockByNumber(context.Background(), header.Number)
+	congestion := float64(header.GasUsed) / float64(header.GasLimit)
+	baseFeePercent := s.calculateBaseFeePercent(header)
+	block, err := s.client.Client.BlockByNumber(ctx, header.Number)
 	if err != nil {
 		return fmt.Errorf("failed to get block by number: %w", err)
 	}
 
-	if congestion < congestionMinTarget {
-		s.lggr.Errorf("at block %d congestion is %.2f, which is lower than taget %.2f. Number of txs %d", header.Number.Uint64(), congestion, congestionMinTarget, block.Transactions().Len())
-		return nil
+	tipPercent := s.gasTipPercent(block)
+
+	s.lggr.Infof("at block %d congestion is %.2f txs: %d, tip: %.2f%%, baseFee: %.2f%%", header.Number.Uint64(), congestion, block.Transactions().Len(), tipPercent, baseFeePercent)*/
+	return nil
+}
+
+func (s *Simulator) calculateBaseFeePercent(header *types.Header) float64 {
+	return float64(header.BaseFee.Uint64()) / float64(s.Input.InitialBaseFee) * 100
+}
+
+// calculates gas tip in percents of s.Tip
+func (s *Simulator) gasTipPercent(block *types.Block) float64 {
+	if block.Transactions().Len() == 0 {
+		return 0
+	}
+	total := big.NewInt(0)
+	for _, tx := range block.Transactions() {
+		total = total.Add(tx.EffectiveGasTipValue(block.BaseFee()), total)
 	}
 
-	s.lggr.Infof("at block %d congestion is %.2f txs: %d", header.Number.Uint64(), congestion, block.Transactions().Len())
-	return nil
+	avg := big.NewInt(0).Div(total, big.NewInt(int64(block.Transactions().Len())))
+	return float64(avg.Uint64()) / float64(s.Tip) * 100
 }
 
 func (s *Simulator) runOnNewBlockUntilCanceled(ctx context.Context, onNewBlock func(ctx context.Context, head *types.Header) error) {
@@ -260,7 +301,7 @@ func (s *Simulator) estimatePayloadSizeToFillBlock(ctx context.Context) (payload
 	}
 
 	gasLimit := block.GasLimit()
-	maxPayloadSize := gasLimit / 8 / uint64(*s.NumberOfTxsPerBlock) // assume that LogDataGas is 8
+	maxPayloadSize := gasLimit / 8 / uint64(s.NumberOfTxsPerBlock) // assume that LogDataGas is 8
 	const step = 1000
 	result := sort.Search(int(maxPayloadSize/step), func(payloadSize int) bool {
 		payloadSize *= step
@@ -277,11 +318,11 @@ func (s *Simulator) estimatePayloadSizeToFillBlock(ctx context.Context) (payload
 
 		txsToFill := int(gasLimit / receipt.GasUsed)
 		s.lggr.Infof("emitted payload size: %d that results in %d transactions to fill a block: %v", payloadSize, txsToFill, err)
-		if s.Input.GasLimit == nil {
-			s.Input.GasLimit = &receipt.GasUsed
+		if s.Input.GasLimit == 0 {
+			s.Input.GasLimit = receipt.GasUsed
 		}
 
-		return txsToFill <= *s.NumberOfTxsPerBlock
+		return txsToFill <= s.NumberOfTxsPerBlock
 	})
 
 	return result * step, nil
