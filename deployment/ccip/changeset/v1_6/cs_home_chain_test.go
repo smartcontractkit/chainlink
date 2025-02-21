dc9ba396ac11aa961c9d3c7c8dc3ccf0db341a45
AnieeG <anindita.ghosh@smartcontract.com>
2025-02-21 11:53:21 -0800

    fix lint

--- deployment/ccip/changeset/v1_6/cs_home_chain_test.go
+++ deployment/ccip/changeset/v1_6/cs_home_chain_test.go
@@ -4,9 +4,10 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/smartcontractkit/chainlink-integrations/evm/utils"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
+
+	"github.com/smartcontractkit/chainlink-integrations/evm/utils"
 
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
