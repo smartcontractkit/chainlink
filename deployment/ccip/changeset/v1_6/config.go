06b8a601e78ed8cee53ec97f44d6ac2a0d23bbfc
AnieeG <anindita.ghosh@smartcontract.com>
2025-02-25 14:59:00 -0800

    go import

--- deployment/ccip/changeset/v1_6/config.go
+++ deployment/ccip/changeset/v1_6/config.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
+
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-ccip/pluginconfig"
 
