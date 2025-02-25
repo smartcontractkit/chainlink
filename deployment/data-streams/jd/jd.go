f8620d30985b9262a14f497a5cbd79ae8c0834ec
giogam <151543+giogam@users.noreply.github.com>
2025-02-25 13:55:53 +0100

    feat(data-streams): adds basic JD functionality

---
+++ deployment/data-streams/jd/jd.go
@@ -0,0 +1,60 @@
+package jd
+
+import (
+	"context"
+	"fmt"
+
+	nodeapiv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/node"
+	jdtypesv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/shared/ptypes"
+	"github.com/smartcontractkit/chainlink/deployment"
+
+	"github.com/AlekSi/pointer"
+)
+
+const (
+	ProductLabel = "data-streams"
+)
+
+// Source for a set of JD filters to apply when fetching a DON.
+// Should uniquely identify a set of nodes in JD which belong to a single DON.
+type DONFilter struct {
+	DONID    uint64
+	DONName  string
+	EnvLabel string
+}
+
+func (f *DONFilter) bootstrappersFilter() *nodeapiv1.ListNodesRequest_Filter {
+	return &nodeapiv1.ListNodesRequest_Filter{
+		Selectors: []*jdtypesv1.Selector{
+			{
+				Key: fmt.Sprintf("don-%s-%s", f.DONID, f.DONName),
+				Op:  jdtypesv1.SelectorOp_EXIST,
+			},
+			{
+				Key:   "nodeType",
+				Op:    jdtypesv1.SelectorOp_EQ,
+				Value: pointer.String("bootstrap"),
+			},
+			{
+				Key:   "environment",
+				Op:    jdtypesv1.SelectorOp_EQ,
+				Value: &f.EnvLabel,
+			},
+			{
+				Key:   "product",
+				Op:    jdtypesv1.SelectorOp_EQ,
+				Value: pointer.String("data-streams"),
+			},
+		},
+	}
+}
+
+func FetchDONBootstrappersFromJD(ctx context.Context, jd deployment.OffchainClient, filter *DONFilter) (nodes []*nodeapiv1.Node, err error) {
+	jdFilter := filter.bootstrappersFilter()
+	resp, err := jd.ListNodes(ctx, &nodeapiv1.ListNodesRequest{Filter: jdFilter})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list bootstrap nodes for DON %s - %s: %w", filter.DONID, filter.DONName, err)
+	}
+
+	return resp.Nodes, nil
+}
