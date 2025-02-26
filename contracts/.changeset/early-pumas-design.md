7efc78d348df45e209c2eafd94f4d01a6a49c7c0
Rens Rooimans <github@rensrooimans.nl>
2025-02-26 11:44:11 +0100

    rename EVMExtraArgsV2 to GenericExtraArgsV2

---
+++ contracts/.changeset/early-pumas-design.md
@@ -0,0 +1,5 @@
+---
+'@chainlink/contracts': patch
+---
+
+rename EVMExtraArgsV2 to GenericExtraArgsV2
--- contracts/src/v0.8/ccip/libraries/Client.sol
+++ contracts/src/v0.8/ccip/libraries/Client.sol
@@ -26,7 +26,7 @@ library Client {
     bytes extraArgs; // Populate this with _argsToBytes(EVMExtraArgsV2).
   }
 
-  // bytes4(keccak256("CCIP EVMExtraArgsV1"));
+  // Tag to indicate only a gas limit. Only usable for EVM as destination chain.
   bytes4 public constant EVM_EXTRA_ARGS_V1_TAG = 0x97a657c9;
 
   struct EVMExtraArgsV1 {
@@ -39,23 +39,24 @@ library Client {
     return abi.encodeWithSelector(EVM_EXTRA_ARGS_V1_TAG, extraArgs);
   }
 
-  // bytes4(keccak256("CCIP EVMExtraArgsV2"));
-  bytes4 public constant EVM_EXTRA_ARGS_V2_TAG = 0x181dcf10;
-
-  // bytes4(keccak256("CCIP SVMExtraArgsV1"));
-  bytes4 public constant SVM_EXTRA_ARGS_V1_TAG = 0x1f3b3aba;
-
-  /// @dev The maximum number of accounts that can be passed in SVMExtraArgs.
-  uint256 public constant SVM_EXTRA_ARGS_MAX_ACCOUNTS = 64;
+  // Tag to indicate a gas limit (or dest chain equivalent processing units) and Out Of Order Execution. This tag is
+  // available for multiple chain families. If there is no chain family specific tag, this is the default available
+  // for a chain.
+  // Note: not available for Solana VM based chains.
+  bytes4 public constant GENERIC_EXTRA_ARGS_V2_TAG = 0x181dcf10;
 
   /// @param gasLimit: gas limit for the callback on the destination chain.
   /// @param allowOutOfOrderExecution: if true, it indicates that the message can be executed in any order relative to
   /// other messages from the same sender. This value's default varies by chain. On some chains, a particular value is
   /// enforced, meaning if the expected value is not set, the message request will revert.
-  struct EVMExtraArgsV2 {
+  /// @dev Fully compatible with the previously existing EVMExtraArgsV2.
+  struct GenericExtraArgsV2 {
     uint256 gasLimit;
     bool allowOutOfOrderExecution;
   }
+
+  // Extra args tag for chains that use the Solana VM.
+  bytes4 public constant SVM_EXTRA_ARGS_V1_TAG = 0x1f3b3aba;
 
   struct SVMExtraArgsV1 {
     uint32 computeUnits;
@@ -65,10 +66,13 @@ library Client {
     bytes32[] accounts;
   }
 
+  /// @dev The maximum number of accounts that can be passed in SVMExtraArgs.
+  uint256 public constant SVM_EXTRA_ARGS_MAX_ACCOUNTS = 64;
+
   function _argsToBytes(
-    EVMExtraArgsV2 memory extraArgs
+    GenericExtraArgsV2 memory extraArgs
   ) internal pure returns (bytes memory bts) {
-    return abi.encodeWithSelector(EVM_EXTRA_ARGS_V2_TAG, extraArgs);
+    return abi.encodeWithSelector(GENERIC_EXTRA_ARGS_V2_TAG, extraArgs);
   }
 
   function _svmArgsToBytes(
