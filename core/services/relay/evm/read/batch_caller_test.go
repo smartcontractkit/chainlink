2bb19ed06ddebfbe0cc42376fe11bb1fe0766325
dimkouv <dimitrios.kouveris@smartcontract.com>
2025-02-26 10:45:50 +0200

    rm rand test

--- core/services/relay/evm/read/batch_caller_test.go
+++ core/services/relay/evm/read/batch_caller_test.go
@@ -116,7 +116,6 @@ func TestDefaultEvmBatchCaller_batchCallLimit(t *testing.T) {
 		{numCalls: 10, batchSize: 100, parallelRpcCallsLimit: 10},
 		{numCalls: 1, batchSize: 100, parallelRpcCallsLimit: 10},
 		{numCalls: 1000, batchSize: 10, parallelRpcCallsLimit: 2},
-		{numCalls: rand.Uint() % 1000, batchSize: rand.Uint() % 500, parallelRpcCallsLimit: rand.Uint() % 500},
 	}
 
 	type MethodParam struct {
