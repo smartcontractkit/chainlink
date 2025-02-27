bd3459b4b3c1b306d6379b36862f0a95629220ff
Pablo <pablo.estrada@smartcontract.com>
2025-02-27 16:51:31 -0600

    fix: changeset params for transfer ownership

--- deployment/ccip/changeset/solana/cs_chain_contracts_test.go
+++ deployment/ccip/changeset/solana/cs_chain_contracts_test.go
@@ -233,7 +233,7 @@ func TestSetOcr3(t *testing.T) {
 	evmSelectors := tenv.Env.AllChainSelectors()
 	homeChainSel := evmSelectors[0]
 	solChainSelectors := tenv.Env.AllChainSelectorsSolana()
-	_, _ = testhelpers.TransferOwnershipSolana(t, &tenv.Env, solChainSelectors[0], true, true, true, true)
+	_, _ = testhelpers.TransferOwnershipSolana(t, &tenv.Env, solChainSelectors[0], true, true, true, true, nil, nil)
 
 	tenv.Env, err = commonchangeset.ApplyChangesetsV2(t, tenv.Env, []commonchangeset.ConfiguredChangeSet{
 		commonchangeset.Configure(
@@ -409,7 +409,7 @@ func TestTokenAdminRegistry(t *testing.T) {
 
 			var mcmsConfig *ccipChangesetSolana.MCMSConfigSolana
 			if test.Mcms {
-				_, _ = testhelpers.TransferOwnershipSolana(t, &e, solChain, true, true, true, true)
+				_, _ = testhelpers.TransferOwnershipSolana(t, &e, solChain, true, true, true, true, nil, nil)
 				mcmsConfig = &ccipChangesetSolana.MCMSConfigSolana{
 					MCMS: &ccipChangeset.MCMSConfig{
 						MinDelay: 1 * time.Second,
@@ -542,7 +542,7 @@ func TestPoolLookupTable(t *testing.T) {
 			var mcmsConfig *ccipChangesetSolana.MCMSConfigSolana
 			newAdmin := tenv.Env.SolChains[solChain].DeployerKey.PublicKey()
 			if test.Mcms {
-				_, _ = testhelpers.TransferOwnershipSolana(t, &tenv.Env, solChain, true, true, true, true)
+				_, _ = testhelpers.TransferOwnershipSolana(t, &tenv.Env, solChain, true, true, true, true, nil, nil)
 				mcmsConfig = &ccipChangesetSolana.MCMSConfigSolana{
 					MCMS: &ccipChangeset.MCMSConfig{
 						MinDelay: 1 * time.Second,
