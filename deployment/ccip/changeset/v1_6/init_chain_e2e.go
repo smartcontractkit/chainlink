293ce966e32f2ced59f502ef67d0218b42a36a6a
stackman27 <sishir.giri@smartcontract.com>
2025-03-06 15:08:52 -0500

    setup

---
+++ deployment/ccip/changeset/v1_6/init_chain_e2e.go
@@ -0,0 +1,14 @@
+package v1_6
+
+import "github.com/smartcontractkit/chainlink/deployment"
+
+var (
+	_ deployment.ChangeSet[InitChaine2eConfig] = InitChaine2eChangeset
+)
+
+func InitChain() {
+	e.deployment.Environment 
+	cfg.InitChaine2eConfig
+} (deployment.ChaingesetOutput, error) {
+	
+}
