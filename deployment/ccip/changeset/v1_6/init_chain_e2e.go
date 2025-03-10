e3d8990c608bd064cf9b092b6e1ddc0d9df2e83b
stackman27 <sishir.giri@smartcontract.com>
2025-03-10 11:38:56 -0700

    inmemory + existingaddressbook

--- deployment/ccip/changeset/v1_6/init_chain_e2e.go
+++ deployment/ccip/changeset/v1_6/init_chain_e2e.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/smartcontractkit/ccip-owner-contracts/pkg/proposal/timelock"
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
-	commonchangeset "github.com/smartcontractkit/chainlink/deployment/common/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/common/types"
 )
 
@@ -20,7 +20,8 @@ type InitChaine2eConfig struct {
 	McmsConfig        map[uint64]types.MCMSWithTimelockConfigV2
 	// RMNRemoteConfigs       map[uint64]RMNRemoteConfig
 	PreReqConfig           changeset.DeployPrerequisiteConfig
-	ContractParamsPerChain map[uint64]ChainContractParams
+	ContractParamsPerChain DeployChainContractsConfig
+	UpdateChainConfig      UpdateChainConfigConfig
 }
 
 func InitChaine2eChangeset(env deployment.Environment, cfg InitChaine2eConfig) (deployment.ChangesetOutput, error) {
@@ -35,46 +36,88 @@ func InitChaine2eChangeset(env deployment.Environment, cfg InitChaine2eConfig) (
 	// batches := make([]mcmstypes.BatchOperation, 0)
 
 	// Correct ordering for new chain integration
-	err := commonchangeset.DeployInternalMCMSWithTimelockV2ForEVM(env, env.Logger, addressBook, cfg.McmsConfig)
+	// err := commonchangeset.DeployInternalMCMSWithTimelockV2ForEVM(env, env.Logger, addressBook, cfg.McmsConfig)
+	// if err != nil {
+	// 	return deployment.ChangesetOutput{AddressBook: addressBook}, err
+	// }
+
+	// err = MergeAddress(env, env.ExistingAddresses, addressBook)
+	// if err != nil {
+	// 	return deployment.ChangesetOutput{}, err
+	// }
+
+	// addressBook1 := deployment.NewMemoryAddressBook()
+	// err = changeset.DeployPrerequisiteChainContracts(env, addressBook1, cfg.PreReqConfig)
+	// if err != nil {
+	// 	env.Logger.Errorw("Failed to deploy prerequisite contracts", "err", err, "addressBook", addressBook1)
+	// 	return deployment.ChangesetOutput{
+	// 		AddressBook: addressBook1,
+	// 	}, fmt.Errorf("failed to deploy prerequisite contracts: %w", err)
+	// }
+
+	// err = MergeAddress(env, env.ExistingAddresses, addressBook1)
+	// if err != nil {
+	// 	return deployment.ChangesetOutput{}, err
+	// }
+
+	// // Note: simpler to declare new memory addressbook for each changeset rather than filtering address from addressbook to merge
+	// addressBook2 := deployment.NewMemoryAddressBook()
+	// err = deployChainContractsForChains(env, addressBook2, cfg.HomeChainSelector, cfg.ContractParamsPerChain)
+	// if err != nil {
+	// 	env.Logger.Errorw("Failed to deploy CCIP contracts", "err", err, "newAddresses", addressBook2)
+	// 	return deployment.ChangesetOutput{AddressBook: addressBook2}, deployment.MaybeDataErr(err)
+	// }
+
+	// err = MergeAddress(env, env.ExistingAddresses, addressBook2)
+	// if err != nil {
+	// 	return deployment.ChangesetOutput{}, err
+	// }
+	fmt.Println("ENV ADDRESS 0: ", env.ExistingAddresses)
+
+	output, err := changeset.DeployPrerequisitesChangeset(env, cfg.PreReqConfig)
 	if err != nil {
-		return deployment.ChangesetOutput{AddressBook: addressBook}, err
+		return deployment.ChangesetOutput{}, fmt.Errorf("Error running DeployPrerequisiteChainContracts: ", err)
 	}
 
-	err = MergeAddress(env, env.ExistingAddresses, addressBook)
+	fmt.Println("ENV ADDRESS 1: ", env.ExistingAddresses)
+	err = env.ExistingAddresses.Merge(output.AddressBook)
 	if err != nil {
 		return deployment.ChangesetOutput{}, err
 	}
 
-	addressBook1 := deployment.NewMemoryAddressBook()
-	err = changeset.DeployPrerequisiteChainContracts(env, addressBook1, cfg.PreReqConfig)
-	if err != nil {
-		env.Logger.Errorw("Failed to deploy prerequisite contracts", "err", err, "addressBook", addressBook1)
-		return deployment.ChangesetOutput{
-			AddressBook: addressBook1,
-		}, fmt.Errorf("failed to deploy prerequisite contracts: %w", err)
-	}
-
-	err = MergeAddress(env, env.ExistingAddresses, addressBook1)
+	err = addressBook.Merge(output.AddressBook)
 	if err != nil {
 		return deployment.ChangesetOutput{}, err
 	}
 
-	// Note: simpler to declare new memory addressbook for each changeset rather than filtering address from addressbook to merge
-	addressBook2 := deployment.NewMemoryAddressBook()
-	err = deployChainContractsForChains(env, addressBook2, cfg.HomeChainSelector, cfg.ContractParamsPerChain)
+	fmt.Println("ENV ADDRESS 2: ", env.ExistingAddresses)
+	output, err = DeployChainContractsChangeset(env, cfg.ContractParamsPerChain)
 	if err != nil {
-		env.Logger.Errorw("Failed to deploy CCIP contracts", "err", err, "newAddresses", addressBook2)
-		return deployment.ChangesetOutput{AddressBook: addressBook2}, deployment.MaybeDataErr(err)
+		return deployment.ChangesetOutput{}, fmt.Errorf("Error running DeployChainContractsChangeset: ", err)
 	}
-
-	err = MergeAddress(env, env.ExistingAddresses, addressBook2)
+	err = env.ExistingAddresses.Merge(output.AddressBook)
 	if err != nil {
 		return deployment.ChangesetOutput{}, err
 	}
 
+	err = addressBook.Merge(output.AddressBook)
+	if err != nil {
+		return deployment.ChangesetOutput{}, err
+	}
+
+	fmt.Println("ENV ADDRESS 3: ", env.ExistingAddresses)
+	// Generate an MCMs proposal
+	// output, err := UpdateChainConfigChangeset(env, cfg.UpdateChainConfig)
+	// if err != nil {
+	// 	return deployment.ChangesetOutput{}, fmt.Errorf("Error running UpdateChainConfigChangeset: ", err)
+	// }
+
+	// fmt.Println(output)
 	// TODO handle MCMS proposals
 
-	return deployment.ChangesetOutput{}, nil
+	return deployment.ChangesetOutput{
+		Proposals:   []timelock.MCMSWithTimelockProposal{},
+		AddressBook: addressBook}, nil
 }
 
 func MergeAddress(env deployment.Environment, existingAddressBook, newAddresses deployment.AddressBook) error {
