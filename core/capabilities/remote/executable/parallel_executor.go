23acaf78601490f9a0d78e170a6978916e263588
Matthew Pendrey <matthew.pendrey@gmail.com>
2025-03-11 10:52:10 +0000

    task semaphore

--- core/capabilities/remote/executable/parallel_executor.go
+++ core/capabilities/remote/executable/parallel_executor.go
@@ -7,34 +7,18 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
 )
 
-type task struct {
-	fnCtx *context.Context
-	fn    func(ctx context.Context)
-}
-
 type parallelExecutor struct {
-	tasksCh  chan task
 	wg       sync.WaitGroup
 	stopChan services.StopChan
+
+	taskSemaphore chan struct{}
 }
 
 func newParallelExecutor(maxParallelTasks int) *parallelExecutor {
 	executor := &parallelExecutor{
-		tasksCh:  make(chan task),
-		stopChan: make(services.StopChan),
-		wg:       sync.WaitGroup{},
-	}
-
-	for i := 0; i < maxParallelTasks; i++ {
-		executor.wg.Add(1)
-		go func() {
-			for t := range executor.tasksCh {
-				ctxWithStop, cancel := executor.stopChan.Ctx(*t.fnCtx)
-				t.fn(ctxWithStop)
-				cancel()
-			}
-			executor.wg.Done()
-		}()
+		stopChan:      make(services.StopChan),
+		wg:            sync.WaitGroup{},
+		taskSemaphore: make(chan struct{}, maxParallelTasks),
 	}
 
 	return executor
@@ -44,15 +28,25 @@ func newParallelExecutor(maxParallelTasks int) *parallelExecutor {
 // is reached, the function will block until a slot is available or the context is cancelled.
 func (t *parallelExecutor) ExecuteTask(ctx context.Context, fn func(ctx context.Context)) error {
 	select {
-	case t.tasksCh <- task{fnCtx: &ctx, fn: fn}:
+	case t.taskSemaphore <- struct{}{}:
+		t.wg.Add(1)
+		go func() {
+			ctxWithStop, cancel := t.stopChan.Ctx(ctx)
+			fn(ctxWithStop)
+			cancel()
+			<-t.taskSemaphore
+			t.wg.Done()
+		}()
+	case <-t.stopChan:
 		return nil
 	case <-ctx.Done():
 		return ctx.Err()
 	}
+
+	return nil
 }
 
 func (t *parallelExecutor) Close() {
-	close(t.tasksCh)
 	close(t.stopChan)
 	t.wg.Wait()
 }
