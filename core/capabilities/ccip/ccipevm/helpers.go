e3866de8cb6cfbf94d150ac075854c717f31e60d
Joe Huang <joe.huang@smartcontract.com>
2025-03-13 11:22:30 -0500

    remove unused

--- core/capabilities/ccip/ccipevm/helpers.go
+++ core/capabilities/ccip/ccipevm/helpers.go
@@ -1,7 +1,6 @@
 package ccipevm
 
 import (
-	"bytes"
 	"fmt"
 	"math/big"
 
@@ -19,34 +18,6 @@ const (
 	evmV2DecodeName    = "decodeEVMExtraArgsV2"
 	evmDestExecDataKey = "destGasAmount"
 )
-
-// decodeExtraArgsV1V2 decodes the given EVM Extra Args and extracts the gas limit
-// that was specified.
-func decodeExtraArgsV1V2(extraArgs []byte) (gasLimit *big.Int, err error) {
-	if len(extraArgs) < 4 {
-		return nil, fmt.Errorf("extra args too short: %d, should be at least 4 bytes long (i.e the extraArgs tag)", len(extraArgs))
-	}
-
-	var method string
-	if bytes.Equal(extraArgs[:4], evmExtraArgsV1Tag) {
-		method = evmV1DecodeName
-	} else if bytes.Equal(extraArgs[:4], evmExtraArgsV2Tag) {
-		method = evmV2DecodeName
-	} else {
-		return nil, fmt.Errorf("unknown extra args tag: %x", extraArgs)
-	}
-	ifaces, err := messageHasherABI.Methods[method].Inputs.UnpackValues(extraArgs[4:])
-	if err != nil {
-		return nil, fmt.Errorf("abi decode extra args v1: %w", err)
-	}
-	// gas limit is always the first argument, and allow OOO isn't set explicitly
-	// on the message.
-	_, ok := ifaces[0].(*big.Int)
-	if !ok {
-		return nil, fmt.Errorf("expected *big.Int, got %T", ifaces[0])
-	}
-	return ifaces[0].(*big.Int), nil
-}
 
 // abiEncodeMethodInputs encodes the inputs for a method call.
 // example abi: `[{ "name" : "method", "type": "function", "inputs": [{"name": "a", "type": "uint256"}]}]`
