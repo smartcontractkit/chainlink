b77c99494598774df1dff61b172d8f6e562dfb16
Justin Kaseman <justinkaseman@live.com>
2025-03-13 13:55:02 -0700

    (fix): Amend log in encryptSecrets

--- core/services/workflows/syncer/handler.go
+++ core/services/workflows/syncer/handler.go
@@ -483,7 +483,7 @@ func (h *eventHandler) workflowRegisteredEvent(
 		// sanity check by decoding the secrets
 		_, decryptErr := h.decryptSecrets(secrets, string(payload.WorkflowOwner))
 		if decryptErr != nil {
-			return fmt.Errorf("failed to decrypt secrets %s: %w", payload.SecretsURL, err)
+			return fmt.Errorf("failed to decrypt secrets %s: %w", payload.SecretsURL, decryptErr)
 		}
 		secrets = fetchedSecrets
 	}
