9db3c51c59401967afdf473e71cecc8614b88d80
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2025-03-13 17:17:41 +0100

    test with failing tests

--- core/utils/crypto/private_key_test.go
+++ core/utils/crypto/private_key_test.go
@@ -3,7 +3,9 @@ package crypto
 import (
 	"crypto/ed25519"
 	"encoding/json"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/stretchr/testify/assert"
@@ -11,6 +13,27 @@ import (
 
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
+
+func TestRandomFlaky(t *testing.T) {
+	t.Parallel()
+
+	// Seed random number generator with current time
+	seed := time.Now().UnixNano()
+	t.Logf("Using seed: %d", seed)
+	r := rand.New(rand.NewSource(seed))
+
+	// Generate a random number between 0 and 1
+	randomValue := r.Float64()
+
+	t.Logf("Random value generated: %f", randomValue)
+	// Import at the top of the file:
+	// import "github.com/stretchr/testify/require"
+
+	// Using require to check that randomValue is < 0.5
+	require.Less(t, randomValue, 0.2, "Random value should be less than 0.2")
+
+	t.Log("This test randomly passed")
+}
 
 func Test_EncryptedPrivateKey(t *testing.T) {
 	t.Parallel()
