ddf186e6555fe5d2da6ec4d9d1cdf94aeffb1d28
pavel-raykov <pavel.raykov@smartcontract.com>
2025-03-14 17:27:41 +0100

    Minor

--- core/gethwrappers/generation/generate_automation/wrap.go
+++ core/gethwrappers/generation/generate_automation/wrap.go
@@ -6,6 +6,9 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/generation/generate/genwrapper"
 )
 
+// Multiple legacy automation classes named X_{digits}, while being in X_{digits} folder,
+// drop the {digits} in the output wrapper go class. Once such classes are removed, we can drop the current wrap.go
+// and switch to core/gethwrappers/generation/wrap.go.
 func main() {
 	rootDir := "../../contracts/solc/"
 	project := "automation"
