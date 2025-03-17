4cae4b4e84cba226dde70bcf0c9a78268f7f3972
Terry Tata <terry.tata@smartcontract.com>
2025-03-17 11:40:20 -0400

    fix params

--- deployment/ccip/changeset/solana/transfer_ccip_to_mcms_with_timelock_test.go
+++ deployment/ccip/changeset/solana/transfer_ccip_to_mcms_with_timelock_test.go
@@ -344,8 +344,8 @@ func TestTransferCCIPToMCMSWithTimelockSolana(t *testing.T) {
 			FeeQuoter:             true,
 			OffRamp:               true,
 			RMNRemote:             true,
-			BurnMintTokenPools:    []solana.PublicKey{burnMintPoolConfigPDA},
-			LockReleaseTokenPools: []solana.PublicKey{lockReleasePoolConfigPDA},
+			BurnMintTokenPools:    map[solana.PublicKey]solana.PublicKey{tokenAddressBurnMint: tokenAddressBurnMint},
+			LockReleaseTokenPools: map[solana.PublicKey]solana.PublicKey{tokenAddressLockRelease: tokenAddressLockRelease},
 		})
 
 	// 5. Now verify on-chain that each contract’s “config account” authority is the Timelock PDA.
