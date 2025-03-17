25ff4de6f17d54e1df0e3f81407bb73a9e4aac88
Will Winder <wwinder.unh@gmail.com>
2025-03-17 15:41:25 -0400

    Update integration-tests/smoke/ccip/ccip_batching_test.go
    Co-authored-by: Makram <makramkd@users.noreply.github.com>

--- integration-tests/smoke/ccip/ccip_batching_test.go
+++ integration-tests/smoke/ccip/ccip_batching_test.go
@@ -282,7 +282,7 @@ func Test_CCIPBatching_SingleSource(t *testing.T) {
 	ccipBatchingSingleSource(t)
 }
 
-func Test_CCIPBatching_SingleSource_MultiplReports(t *testing.T) {
+func Test_CCIPBatching_SingleSource_MultipleReports(t *testing.T) {
 	opt := testhelpers.WithOCRConfigOverride(func(params v1_6.CCIPOCRParams) v1_6.CCIPOCRParams {
 		params.CommitOffChainConfig.MultipleReportsEnabled = true
 		params.CommitOffChainConfig.MaxMerkleRootsPerReport = 1
