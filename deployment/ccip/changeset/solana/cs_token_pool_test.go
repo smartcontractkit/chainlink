99be5c8c9db75c58f67821295aa771d7ea42ace4
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-03-21 14:15:18 +0000

    did not save

--- deployment/ccip/changeset/solana/cs_token_pool_test.go
+++ deployment/ccip/changeset/solana/cs_token_pool_test.go
@@ -82,7 +82,7 @@ func doTestTokenPool(t *testing.T, mcms bool) {
 	)
 	var mcmsConfig *ccipChangesetSolana.MCMSConfigSolana
 	if mcms {
-		_, _ = testhelpers.TransferOwnershipSolana(t, &e, solChain, false,
+		_, _ = testhelpers.TransferOwnershipSolana(t, &e, solChain, true,
 			ccipChangesetSolana.CCIPContractsToTransfer{
 				Router:    true,
 				FeeQuoter: true,
