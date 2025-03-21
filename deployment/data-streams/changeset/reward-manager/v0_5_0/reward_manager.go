e3d3cc1efe40afd9218230bae5b8250e4b336703
ChrisAmora <27789416+ChrisAmora@users.noreply.github.com>
2025-03-21 11:55:44 -0300

    feat: use view state

--- deployment/data-streams/changeset/reward-manager/v0_5_0/reward_manager.go
+++ deployment/data-streams/changeset/reward-manager/v0_5_0/reward_manager.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/smartcontractkit/chainlink/deployment"
-	"github.com/smartcontractkit/chainlink/deployment/data-streams/changeset/types"
+	"github.com/smartcontractkit/chainlink/deployment/data-streams/changeset"
 	rewardManager "github.com/smartcontractkit/chainlink/v2/core/gethwrappers/llo-feeds/generated/reward_manager_v0_5_0"
 )
 
@@ -25,27 +25,21 @@ func loadRewardManagerState(
 		return nil, err
 	}
 
-	tv, found := addresses[contractAddr]
+	chainState, err := changeset.LoadChainState(e.Logger, chain, addresses)
+	if err != nil {
+		e.Logger.Errorw("Failed to load chain state", "err", err)
+		return nil, err
+	}
+
+	conf, found := chainState.RewardManagers[common.HexToAddress(contractAddr)]
+
 	if !found {
 		return nil, fmt.Errorf(
-			"unable to find RewardManager contract on chain %s (selector %d)",
+			"unable to find RewardManager contract on chain %s (selector %d, address %s)",
 			chain.Name(),
 			chain.Selector,
+			contractAddr,
 		)
-	}
-
-	if tv.Type != types.RewardManager || tv.Version != deployment.Version0_5_0 {
-		return nil, fmt.Errorf(
-			"unexpected contract type %s for RewardManager on chain %s (selector %d)",
-			tv,
-			chain.Name(),
-			chain.Selector,
-		)
-	}
-
-	conf, err := rewardManager.NewRewardManager(common.HexToAddress(contractAddr), chain.Client)
-	if err != nil {
-		return nil, err
 	}
 
 	return conf, nil
