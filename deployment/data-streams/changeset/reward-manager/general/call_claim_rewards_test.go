5846f5ee7a42fbd5740563ba34c875e6d9889795
ChrisAmora <27789416+ChrisAmora@users.noreply.github.com>
2025-03-25 19:32:21 -0300

    feat: add more mcms tests

--- deployment/data-streams/changeset/reward-manager/general/call_claim_rewards_test.go
+++ deployment/data-streams/changeset/reward-manager/general/call_claim_rewards_test.go
@@ -3,61 +3,66 @@ package general
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink/deployment/common/proposalutils"
 	"github.com/stretchr/testify/require"
 
 	commonChangesets "github.com/smartcontractkit/chainlink/deployment/common/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/data-streams/changeset/testutil"
-	"github.com/smartcontractkit/chainlink/deployment/data-streams/changeset/types"
 )
 
-func TestCallClaimRewards(t *testing.T) {
+func runClaimRewardsTest(t *testing.T, useMCMS bool) {
 	e := testutil.NewMemoryEnv(t, true)
+	chainSelector := testutil.TestChain.Selector
 
 	e, rewardManagerAddr, _ := DeployRewardManagerAndLink(t, e)
 
 	var poolID [32]byte
 	copy(poolID[:], []byte("poolId"))
 
-	_, err := commonChangesets.Apply(t, e, nil,
+	var timelocks map[uint64]*proposalutils.TimelockExecutionContracts
+	if useMCMS {
+		e, _, timelocks = testutil.DeployMCMS(t, e, map[uint64][]common.Address{
+			chainSelector: {rewardManagerAddr},
+		})
+	}
+
+	_, err := commonChangesets.Apply(
+		t, e, timelocks,
 		commonChangesets.Configure(
 			ClaimRewardsChangeset,
 			ClaimRewardsConfig{
 				ConfigsByChain: map[uint64][]ClaimRewards{
-					testutil.TestChain.Selector: {ClaimRewards{
+					chainSelector: {{
 						RewardManagerAddress: rewardManagerAddr,
 						PoolIDs:              [][32]byte{poolID},
 					}},
 				},
+				MCMSConfig: testutil.GetMCMSConfig(useMCMS),
 			},
 		),
 	)
-	// Need Configured Fee Manager For ClaimRewards Event
 	require.NoError(t, err)
 }
 
-func TestCallClaimRewardsMCMS(t *testing.T) {
-	e := testutil.NewMemoryEnv(t, true)
+func TestClaimRewards(t *testing.T) {
+	testCases := []struct {
+		name    string
+		useMCMS bool
+	}{
+		{
+			name:    "Without MCMS",
+			useMCMS: false,
+		},
+		{
+			name:    "With MCMS",
+			useMCMS: true,
+		},
+	}
 
-	e, rewardManagerAddr, _ := DeployRewardManagerAndLink(t, e)
-
-	var poolID [32]byte
-	copy(poolID[:], []byte("poolId"))
-
-	e, _, timelocks := testutil.DeployMCMS(t, e)
-
-	_, err := commonChangesets.Apply(t, e, timelocks,
-		commonChangesets.Configure(
-			ClaimRewardsChangeset,
-			ClaimRewardsConfig{
-				ConfigsByChain: map[uint64][]ClaimRewards{
-					testutil.TestChain.Selector: {ClaimRewards{
-						RewardManagerAddress: rewardManagerAddr,
-						PoolIDs:              [][32]byte{poolID},
-					}},
-				},
-				MCMSConfig: &types.MCMSConfig{MinDelay: 0, OverrideRoot: true},
-			},
-		),
-	)
-	require.NoError(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			runClaimRewardsTest(t, tc.useMCMS)
+		})
+	}
 }
