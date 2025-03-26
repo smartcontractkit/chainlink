be506f302ea13bc50a9f2493635574c5cd7fbc31
Josh <josh.weintraub@smartcontract.com>
2025-03-26 15:34:28 -0400

    comment fix

--- contracts/src/v0.8/ccip/pools/USDC/BurnMintWithLockReleaseFlagTokenPool.sol
+++ contracts/src/v0.8/ccip/pools/USDC/BurnMintWithLockReleaseFlagTokenPool.sol
@@ -28,8 +28,8 @@ contract BurnMintWithLockReleaseFlagTokenPool is BurnMintTokenPool {
   ) public virtual override returns (Pool.ReleaseOrMintOutV1 memory) {
     _validateReleaseOrMint(releaseOrMintIn);
 
-    // Since the remote token is always canonical USDC, the decimals should always, the decimals should always be 6
-    // for remote tokens, which enables potentially local non-canonical USDC with different decimals to be minted.
+    // Since the remote token is always canonical USDC, the decimals should always be 6 for remote tokens, 
+    // which enables potentially local non-canonical USDC with different decimals to be minted.
     uint256 localAmount = _calculateLocalAmount(releaseOrMintIn.amount, 6);
 
     IBurnMintERC20(address(i_token)).mint(releaseOrMintIn.receiver, localAmount);
