5e2c3822c97edcdb12622583c37e80167d5a308f
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2025-03-28 09:06:33 +0100

    update test

--- integration-tests/smoke/flakeguard_test.go
+++ integration-tests/smoke/flakeguard_test.go
@@ -12,9 +12,9 @@ func TestRandomFlaky_JUST_FOR_TESTING_FLAKEGUARD(t *testing.T) {
 	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
 
-	// Generate a random number between 0 and 9
-	// If the number is < 6, the test will fail (60% failure rate)
+	// Generate a random number between 0 and 9 (inclusive)
 	randomValue := rand.Intn(10)
 
-	require.True(t, randomValue >= 3, "This test is designed to fail 30%% of the time. Got value: %d", randomValue)
+	// Fails when randomValue is 0, 1, or 2
+	require.True(t, randomValue >= 5, "Got value: %d", randomValue)
 }
