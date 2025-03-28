84164a5a579626bbd4519a7f4698e5787f6c5f7b
lukaszcl <120112546+lukaszcl@users.noreply.github.com>
2025-03-28 08:39:48 +0100

    fail randomly

--- integration-tests/smoke/flakeguard_test.go
+++ integration-tests/smoke/flakeguard_test.go
@@ -1,11 +1,20 @@
 package smoke
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestRandomFlaky_JUST_FOR_TESTING_FLAKEGUARD(t *testing.T) {
-	require.True(t, false, "This test is just for testing the flakeguard")
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate a random number between 0 and 9
+	// If the number is < 6, the test will fail (60% failure rate)
+	randomValue := rand.Intn(10)
+
+	require.True(t, randomValue >= 6, "This test is designed to fail 60%% of the time. Got value: %d", randomValue)
 }
