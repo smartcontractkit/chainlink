4e828fecab9ffc27b9c6482965a52ba007a17c90
ilija <pavlovicilija42@gmail.com>
2025-03-31 22:44:03 +0200

    Solana Contract reader RMN MethodNameGetARM cfg changes

--- core/capabilities/ccip/configs/solana/contract_reader.go
+++ core/capabilities/ccip/configs/solana/contract_reader.go
@@ -363,9 +363,18 @@ func DestContractReaderConfig() (config.ContractReader, error) {
 						PDADefinition: solanacodec.PDATypeDef{
 							Prefix: []byte("config"),
 						},
+						OutputModifications: codec.ModifiersConfig{
+							// create a field to extract it
+							&codec.HardCodeModifierConfig{
+								OffChainValues: map[string]any{"RmnRemoteAddress": ""},
+							},
+							&codec.PropertyExtractorConfig{
+								FieldName: "RmnRemoteAddress",
+							},
+						},
 						ResponseAddressHardCoder: &codec.HardCodeModifierConfig{
-							// type doesn't matter it will be overridden with address internally
-							OffChainValues: map[string]any{"RmnRemoteAddress": ""},
+							// type doesn't matter it will be overridden with address internally, key is "" because it's a primitive value and not a field
+							OffChainValues: map[string]any{"": ""},
 						},
 					},
 				},
