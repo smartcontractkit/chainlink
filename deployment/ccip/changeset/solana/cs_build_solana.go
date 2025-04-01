14b9b598960a531bf923c785db7797a35a07a5cd
Terry Tata <terry.tata@smartcontract.com>
2025-04-01 05:35:36 -0400

    lint

--- deployment/ccip/changeset/solana/cs_build_solana.go
+++ deployment/ccip/changeset/solana/cs_build_solana.go
@@ -204,9 +204,8 @@ func generateVanityKeys(e deployment.Environment, keys map[deployment.ContractTy
 		destination := filepath.Join(cloneDir, deployDir, getTypeToProgramDeployName()[program]+"-keypair.json")
 		if err := os.Rename(absPath, destination); err != nil {
 			return fmt.Errorf("failed to move generated vanity key from %s to %s: %w", absPath, destination, err)
-		} else {
-			fmt.Println("File copied to:", destination)
 		}
+		fmt.Println("File copied to:", destination)
 	}
 	return nil
 }
