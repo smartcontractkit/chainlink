4817fc52490bb47ba58de7ae54231da36a681e5b
vyzaldysanchez <vyzaldysanchez@gmail.com>
2025-04-01 14:24:05 -0400

    Adds `GetOwnerTypeAndVersion` tests

--- deployment/keystone/changeset/contracts_test.go
+++ deployment/keystone/changeset/contracts_test.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
 
 	chainsel "github.com/smartcontractkit/chain-selectors"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-integrations/evm/testutils"
 
 	"github.com/smartcontractkit/chainlink/deployment"
+	"github.com/smartcontractkit/chainlink/deployment/common/types"
 	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 	"github.com/smartcontractkit/chainlink/deployment/keystone/changeset"
 
@@ -59,7 +62,7 @@ func TestGetOwnableContract(t *testing.T) {
 		// No target address provided
 		_, err = changeset.GetOwnableContract[*forwarder.KeystoneForwarder](addrBook, chain, nil)
 
-		assert.Error(t, err)
+		require.Error(t, err)
 		assert.Contains(t, err.Error(), "multiple contracts")
 		assert.Contains(t, err.Error(), "must provide a `targetAddr`")
 	})
@@ -79,7 +82,7 @@ func TestGetOwnableContract(t *testing.T) {
 		// No target address provided
 		_, err = changeset.GetOwnableContract[*forwarder.KeystoneForwarder](addrBook, chain, nil)
 
-		assert.Error(t, err)
+		require.Error(t, err)
 		assert.Contains(t, err.Error(), "no contract of type")
 	})
 
@@ -99,7 +102,86 @@ func TestGetOwnableContract(t *testing.T) {
 
 		_, err = changeset.GetOwnableContract[*capabilities_registry.CapabilitiesRegistry](addrBook, chain, &nonExistentAddrStr)
 
-		assert.Error(t, err)
+		require.Error(t, err)
 		assert.Contains(t, err.Error(), "not found in address book")
+	})
+}
+
+func TestGetOwnerTypeAndVersion(t *testing.T) {
+	t.Parallel()
+
+	lggr := logger.Test(t)
+	cfg := memory.MemoryEnvironmentConfig{
+		Nodes:  1, // nodes unused but required in config
+		Chains: 3,
+	}
+
+	t.Run("finds owner in address book", func(t *testing.T) {
+		t.Parallel()
+
+		env := memory.NewMemoryEnvironment(t, lggr, zapcore.DebugLevel, cfg)
+		chain := env.Chains[env.AllChainSelectors()[0]]
+		resp, err := changeset.DeployCapabilityRegistry(env, chain.Selector)
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+		err = env.ExistingAddresses.Merge(resp.AddressBook)
+		require.NoError(t, err)
+		addrs, err := resp.AddressBook.AddressesForChain(chain.Selector)
+		require.NoError(t, err)
+		require.Len(t, addrs, 1)
+		// Get the first address from the map
+		var targetAddrStr string
+		for addr := range addrs {
+			targetAddrStr = addr
+			break // Just take the first one
+		}
+
+		addrBook := env.ExistingAddresses
+
+		contract, err := changeset.GetOwnableContract[*capabilities_registry.CapabilitiesRegistry](addrBook, chain, &targetAddrStr)
+		require.NoError(t, err)
+		owner, err := (*contract).Owner(nil)
+		require.NoError(t, err)
+		mockAddresses := map[string]deployment.TypeAndVersion{
+			owner.Hex(): {Type: types.RBACTimelock, Version: deployment.Version1_0_0},
+		}
+		err = addrBook.Save(chain.Selector, owner.Hex(), mockAddresses[owner.Hex()])
+		require.NoError(t, err)
+
+		tv, err := changeset.GetOwnerTypeAndVersion(*contract, addrBook, chain)
+		require.NoError(t, err)
+		assert.Equal(t, types.RBACTimelock, tv.Type)
+		assert.Equal(t, deployment.Version1_0_0, tv.Version)
+	})
+
+	t.Run("errors when owner not in address book", func(t *testing.T) {
+		t.Parallel()
+
+		env := memory.NewMemoryEnvironment(t, lggr, zapcore.DebugLevel, cfg)
+		chain := env.Chains[env.AllChainSelectors()[0]]
+		resp, err := changeset.DeployCapabilityRegistry(env, chain.Selector)
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+		err = env.ExistingAddresses.Merge(resp.AddressBook)
+		require.NoError(t, err)
+		addrs, err := resp.AddressBook.AddressesForChain(chain.Selector)
+		require.NoError(t, err)
+		require.Len(t, addrs, 1)
+		// Get the first address from the map
+		var targetAddrStr string
+		for addr := range addrs {
+			targetAddrStr = addr
+			break // Just take the first one
+		}
+
+		addrBook := env.ExistingAddresses
+
+		contract, err := changeset.GetOwnableContract[*capabilities_registry.CapabilitiesRegistry](addrBook, chain, &targetAddrStr)
+		require.NoError(t, err)
+
+		_, err = changeset.GetOwnerTypeAndVersion(*contract, addrBook, chain)
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "not found in address book")
 	})
 }
