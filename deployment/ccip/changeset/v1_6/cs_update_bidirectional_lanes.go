f63a767dece487458de99b8c638c4cf88cb99b19
Kyle Martin <kyle.martin@smartcontract.com>
2025-04-03 08:33:22 -0400

    Fix build

--- deployment/ccip/changeset/v1_6/cs_update_bidirectional_lanes.go
+++ deployment/ccip/changeset/v1_6/cs_update_bidirectional_lanes.go
@@ -232,7 +232,7 @@ func updateBidirectionalLanesLogic(e deployment.Environment, c UpdateBidirection
 	if err != nil {
 		return deployment.ChangesetOutput{}, fmt.Errorf("failed to load onchain state: %w", err)
 	}
-	proposal, err := aggregateProposals(e, state, proposals, nil, "Update multiple bidirectional lanes", c.MCMSConfig)
+	proposal, err := proposalutils.AggregateProposals(e, state.EVMMCMSStateByChain(), proposals, nil, "Update multiple bidirectional lanes", c.MCMSConfig)
 	if err != nil {
 		return deployment.ChangesetOutput{}, fmt.Errorf("failed to aggregate proposals: %w", err)
 	}
