1ff5ca0eeee5f9f6c938d736be58470c277d73f8
patrickhuie19 <patrick.huie@smartcontract.com>
2025-04-04 12:57:00 -0400

    separated out protos

--- core/services/workflows/pb/generate.go
+++ core/services/workflows/pb/generate.go
@@ -1,2 +1,6 @@
-//go:generate protoc --go_out=. --go_opt=paths=source_relative events.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative events-metadata.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative events-workflow-started.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative events-workflow-finished.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative events-capability-started.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative events-capability-finished.proto
 package pb
