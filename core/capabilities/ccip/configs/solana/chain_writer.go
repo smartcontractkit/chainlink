3551f4fe1dba5a59b1fe8254a6181eb4062bbed0
Joe Huang <joe.huang@smartcontract.com>
2025-04-07 18:54:53 -0500

    move line

--- core/capabilities/ccip/configs/solana/chain_writer.go
+++ core/capabilities/ccip/configs/solana/chain_writer.go
@@ -241,7 +241,6 @@ func getExecuteMethodConfig(fromAddress string, offrampProgramAddress string) ch
 			// are appended to the accounts list in the CCIPExecute args transform for each token transfer
 		},
 		DebugIDLocation: "Info.AbstractReports.Messages.Header.MessageID",
-		// logic receiver and user defined messaging accounts are appended in the CCIPExecute args transform
 	}
 }
 
