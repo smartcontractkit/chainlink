555d92b19095c2ffe16c0d83d53cecb7f6041368
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-04-07 09:17:12 +0100

    fix: correct outputs

--- core/services/ocr2/plugins/ccip/transmitter/trontransmitter.go
+++ core/services/ocr2/plugins/ccip/transmitter/trontransmitter.go
@@ -98,7 +98,7 @@ func (t *tronTransmitter) CreateEthTransaction(ctx context.Context, toAddress co
 		// TODO: Add idempotency key generation
 	}
 
-	report, rs, ss, vs, err := t.unpackPayload(payload)
+	_, report, rs, ss, vs, err := t.unpackPayload(payload)
 	if err != nil {
 		return fmt.Errorf("failed to unpack payload: %w", err)
 	}
@@ -143,22 +143,21 @@ func (t *tronTransmitter) IsExecTransmitter() bool {
 	return !t.IsCommitTransmitter()
 }
 
-func (t *tronTransmitter) unpackPayload(payload []byte) (report []byte, rs [][32]byte, ss [][32]byte, vs [32]byte, err error) {
+func (t *tronTransmitter) unpackPayload(payload []byte) (rawReportCtx [3][32]byte, report []byte, rs [][32]byte, ss [][32]byte, vs [32]byte, err error) {
 	// Due to the quirks of the Tron API to send a transaction, we need to specify each parameter of the transmit function
 	// To avoid modifying the OCRContractTransmitter, we can just unpack the payload here
 
 	// Skip the first 4 bytes which is the function selector
 	result, err := t.contractABI.Methods["transmit"].Inputs.Unpack(payload[4:])
 	if err != nil {
-		return nil, nil, nil, [32]byte{}, fmt.Errorf("failed to unpack payload: %w", err)
+		return [3][32]byte{}, nil, nil, nil, [32]byte{}, fmt.Errorf("failed to unpack payload: %w", err)
 	}
 
-	report = result[0].([]byte)
-	rs = result[1].([][32]byte)
-	report = result[0].([]byte)
-	rs = result[1].([][32]byte)
-	ss = result[2].([][32]byte)
-	vs = result[3].([32]byte)
+	rawReportCtx = result[0].([3][32]byte)
+	report = result[1].([]byte)
+	rs = result[2].([][32]byte)
+	ss = result[3].([][32]byte)
+	vs = result[4].([32]byte)
 
-	return report, rs, ss, vs, nil
+	return rawReportCtx, report, rs, ss, vs, nil
 }
