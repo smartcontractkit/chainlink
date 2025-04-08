56fead7b32e9a08ae14e6ea2dcdfd3e747877196
Matthew Pendrey <matthew.pendrey@gmail.com>
2025-04-08 14:49:36 +0100

    stopped error on executor

--- core/capabilities/remote/executable/parallel_executor.go
+++ core/capabilities/remote/executable/parallel_executor.go
@@ -2,6 +2,7 @@ package executable
 
 import (
 	"context"
+	"errors"
 	"sync"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
@@ -30,7 +31,7 @@ func newParallelExecutor(maxParallelTasks int) *parallelExecutor {
 func (t *parallelExecutor) ExecuteTask(ctx context.Context, fn func(ctx context.Context)) error {
 	select {
 	case t.taskSemaphore <- struct{}{}:
-		t.IfNotStopped(func() {
+		stopped := t.IfNotStopped(func() {
 			t.wg.Add(1)
 			go func() {
 				ctxWithStop, cancel := t.stopChan.Ctx(ctx)
@@ -40,6 +41,10 @@ func (t *parallelExecutor) ExecuteTask(ctx context.Context, fn func(ctx context.
 				t.wg.Done()
 			}()
 		})
+
+		if stopped {
+			return errors.New("executor stopped")
+		}
 	case <-t.stopChan:
 		return nil
 	case <-ctx.Done():
