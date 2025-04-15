a0e5dd0fb62ff0af178ab0742091d2894880cbe2
cfal <cfal@users.noreply.github.com>
2025-04-16 01:24:00 +0400

    core/capabilities/ccip/ccipaptos: update for contract changes, dont depend on generated bindings

--- core/capabilities/ccip/ccipaptos/extradatadecoder.go
+++ core/capabilities/ccip/ccipaptos/extradatadecoder.go
@@ -2,27 +2,24 @@ package ccipaptos
 
 import (
 	"fmt"
-	"reflect"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-
-	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/abihelpers"
 
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	ccipcommon "github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/common"
-
-	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/v1_6_0/ccip_aptos_utils"
 )
 
 // ExtraDataDecoder is a concrete implementation of ccipcommon.ExtraDataDecoder
 // Compatible with ccip::fee_quoter version 1.6.0
 type ExtraDataDecoder struct{}
 
+var _ ccipcommon.ExtraDataDecoder = (*ExtraDataDecoder)(nil)
+
 const (
 	aptosDestExecDataKey = "destGasAmount"
 )
-
-var aptosUtilsABI = abihelpers.MustParseABI(ccip_aptos_utils.AptosUtilsABI)
 
 var (
 	// bytes4 public constant EVM_EXTRA_ARGS_V1_TAG = 0x97a657c9;
@@ -33,18 +30,71 @@ var (
 
 	// bytes4 public constant SVM_EXTRA_EXTRA_ARGS_V1_TAG = 0x1f3b3aba
 	svmExtraArgsV1Tag = hexutil.MustDecode("0x1f3b3aba")
+
+	uint32Type     = mustNewType("uint32")
+	uint64Type     = mustNewType("uint64")
+	uint256Type    = mustNewType("uint256")
+	boolType       = mustNewType("bool")
+	bytes32Type    = mustNewType("bytes32")
+	bytes32ArrType = mustNewType("bytes32[]")
+
+	// Arguments for decoding destGasAmount
+	destGasAmountArguments = abi.Arguments{
+		{Name: aptosDestExecDataKey, Type: uint32Type},
+	}
+
+	// Arguments matching the fields of EVMExtraArgsV1 struct
+	evmExtraArgsV1Fields = abi.Arguments{
+		{Name: "gasLimit", Type: uint256Type},
+	}
+
+	// Arguments matching the fields of GenericExtraArgsV2 struct
+	genericExtraArgsV2Fields = abi.Arguments{
+		{Name: "gasLimit", Type: uint256Type},
+		{Name: "allowOutOfOrderExecution", Type: boolType},
+	}
+
+	// Arguments matching the fields of SVMExtraArgsV1 struct
+	svmExtraArgsV1Fields = abi.Arguments{
+		{Name: "computeUnits", Type: uint32Type},
+		{Name: "accountIsWritableBitmap", Type: uint64Type},
+		{Name: "allowOutOfOrderExecution", Type: boolType},
+		{Name: "tokenReceiver", Type: bytes32Type},
+		{Name: "accounts", Type: bytes32ArrType},
+	}
 )
+
+func mustNewType(typeStr string) abi.Type {
+	t, err := abi.NewType(typeStr, "", nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create ABI type %s: %v", typeStr, err))
+	}
+	return t
+}
 
 // DecodeDestExecDataToMap reformats bytes into a chain agnostic map[string]interface{} representation for dest exec data
 func (d ExtraDataDecoder) DecodeDestExecDataToMap(destExecData cciptypes.Bytes) (map[string]any, error) {
-	destGasAmount, err := abiDecodeUint32(destExecData)
+	args := make(map[string]any)
+	err := destGasAmountArguments.UnpackIntoMap(args, destExecData)
 	if err != nil {
+		if len(destExecData) != 32 {
+			return nil, fmt.Errorf("decode dest gas amount: expected 32 bytes for uint32, got %d: %w", len(destExecData), err)
+		}
+
+		var val big.Int
+		val.SetBytes(destExecData)
+		if val.Cmp(big.NewInt(0xFFFFFFFF)) > 0 {
+			return nil, fmt.Errorf("decode dest gas amount: value %s exceeds uint32 max: %w", val.String(), err)
+		}
+
 		return nil, fmt.Errorf("decode dest gas amount: %w", err)
 	}
 
-	return map[string]any{
-		aptosDestExecDataKey: destGasAmount,
-	}, nil
+	if _, ok := args[aptosDestExecDataKey]; !ok {
+		return nil, fmt.Errorf("failed to unpack key '%s' for dest gas amount", aptosDestExecDataKey)
+	}
+
+	return args, nil
 }
 
 // DecodeExtraArgsToMap reformats bytes into a chain agnostic map[string]any representation for extra args
@@ -53,98 +103,99 @@ func (d ExtraDataDecoder) DecodeExtraArgsToMap(extraArgs cciptypes.Bytes) (map[s
 		return nil, fmt.Errorf("extra args too short: %d, should be at least 4 (i.e the extraArgs tag)", len(extraArgs))
 	}
 
-	var method string
-	var argName string
-	switch string(extraArgs[:4]) {
+	var decoderArgs abi.Arguments
+	tag := string(extraArgs[:4])
+	argsData := extraArgs[4:]
+
+	switch tag {
 	case string(evmExtraArgsV1Tag):
-		method = "exposeEVMExtraArgsV1"
-		argName = "evmExtraArgsV1"
+		decoderArgs = evmExtraArgsV1Fields
 	case string(genericExtraArgsV2Tag):
-		method = "exposeGenericExtraArgsV2"
-		argName = "evmExtraArgsV2"
+		decoderArgs = genericExtraArgsV2Fields
 	case string(svmExtraArgsV1Tag):
-		method = "exposeSVMExtraArgsV1"
-		argName = "svmExtraArgsV1"
+		decoderArgs = svmExtraArgsV1Fields
 	default:
-		return nil, fmt.Errorf("unknown extra args tag: %x", extraArgs)
+		return nil, fmt.Errorf("unknown extra args tag: %x", extraArgs[:4])
+	}
+
+	unpackedArgs := make(map[string]any)
+	err := decoderArgs.UnpackIntoMap(unpackedArgs, argsData)
+	if err != nil {
+		return nil, fmt.Errorf("abi decode extra args (tag %x): %w", extraArgs[:4], err)
 	}
 
 	output := make(map[string]any)
-	args := make(map[string]any)
-	err := aptosUtilsABI.Methods[method].Inputs.UnpackIntoMap(args, extraArgs[4:])
-	if err != nil {
-		return nil, fmt.Errorf("abi decode extra args %v: %w", method, err)
-	}
 
-	argValue, exists := args[argName]
-	if !exists {
-		return nil, fmt.Errorf("failed to get arg value for %s", argName)
-	}
-
-	val := reflect.ValueOf(argValue)
-	if val.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("expected struct for %s, got %T", argName, argValue)
-	}
-
-	switch argName {
-	case "evmExtraArgsV1":
-		gasLimitField := val.FieldByName("GasLimit")
-		if gasLimitField.IsValid() {
-			output["gasLimit"] = gasLimitField.Interface()
+	switch tag {
+	case string(evmExtraArgsV1Tag):
+		if gasLimit, ok := unpackedArgs["gasLimit"].(*big.Int); ok {
+			output["gasLimit"] = gasLimit
+		} else if unpackedArgs["gasLimit"] != nil {
+			return nil, fmt.Errorf("unexpected type for gasLimit in EVMExtraArgsV1: %T", unpackedArgs["gasLimit"])
 		} else {
-			output["gasLimit"] = nil
+			output["gasLimit"] = nil // Field not present or nil
 		}
-	case "evmExtraArgsV2":
-		gasLimitField := val.FieldByName("GasLimit")
-		if gasLimitField.IsValid() {
-			output["gasLimit"] = gasLimitField.Interface()
+
+	case string(genericExtraArgsV2Tag):
+		if gasLimit, ok := unpackedArgs["gasLimit"].(*big.Int); ok {
+			output["gasLimit"] = gasLimit
+		} else if unpackedArgs["gasLimit"] != nil {
+			return nil, fmt.Errorf("unexpected type for gasLimit in GenericExtraArgsV2: %T", unpackedArgs["gasLimit"])
 		} else {
 			output["gasLimit"] = nil
 		}
 
-		allowOutOfOrderField := val.FieldByName("AllowOutOfOrderExecution")
-		if allowOutOfOrderField.IsValid() {
-			output["allowOutOfOrderExecution"] = allowOutOfOrderField.Interface()
+		if allow, ok := unpackedArgs["allowOutOfOrderExecution"].(bool); ok {
+			output["allowOutOfOrderExecution"] = allow
+		} else if unpackedArgs["allowOutOfOrderExecution"] != nil {
+			return nil, fmt.Errorf("unexpected type for allowOutOfOrderExecution in GenericExtraArgsV2: %T", unpackedArgs["allowOutOfOrderExecution"])
 		} else {
+			// Default to false if not present, consistent with original code.
+			// Note: ABI decoding of bool usually doesn't result in nil, but checking doesn't hurt.
 			output["allowOutOfOrderExecution"] = false
 		}
-	case "svmExtraArgsV1":
-		computeUnitsField := val.FieldByName("ComputeUnits")
-		if computeUnitsField.IsValid() {
-			output["computeUnits"] = computeUnitsField.Interface()
+
+	case string(svmExtraArgsV1Tag):
+		if computeUnits, ok := unpackedArgs["computeUnits"].(uint32); ok {
+			output["computeUnits"] = computeUnits
+		} else if unpackedArgs["computeUnits"] != nil {
+			return nil, fmt.Errorf("unexpected type for computeUnits in SVMExtraArgsV1: %T", unpackedArgs["computeUnits"])
 		} else {
 			output["computeUnits"] = nil
 		}
 
-		bitmapField := val.FieldByName("AccountIsWritableBitmap")
-		if bitmapField.IsValid() {
-			output["accountIsWritableBitmap"] = bitmapField.Interface()
+		if bitmap, ok := unpackedArgs["accountIsWritableBitmap"].(uint64); ok {
+			output["accountIsWritableBitmap"] = bitmap
+		} else if unpackedArgs["accountIsWritableBitmap"] != nil {
+			return nil, fmt.Errorf("unexpected type for accountIsWritableBitmap in SVMExtraArgsV1: %T", unpackedArgs["accountIsWritableBitmap"])
 		} else {
 			output["accountIsWritableBitmap"] = nil
 		}
 
-		allowOutOfOrderField := val.FieldByName("AllowOutOfOrderExecution")
-		if allowOutOfOrderField.IsValid() {
-			output["allowOutOfOrderExecution"] = allowOutOfOrderField.Interface()
+		if allow, ok := unpackedArgs["allowOutOfOrderExecution"].(bool); ok {
+			output["allowOutOfOrderExecution"] = allow
+		} else if unpackedArgs["allowOutOfOrderExecution"] != nil {
+			return nil, fmt.Errorf("unexpected type for allowOutOfOrderExecution in SVMExtraArgsV1: %T", unpackedArgs["allowOutOfOrderExecution"])
 		} else {
 			output["allowOutOfOrderExecution"] = false
 		}
 
-		tokenReceiverField := val.FieldByName("TokenReceiver")
-		if tokenReceiverField.IsValid() {
-			output["tokenReceiver"] = tokenReceiverField.Interface()
+		if tokenReceiver, ok := unpackedArgs["tokenReceiver"].([32]byte); ok {
+			output["tokenReceiver"] = tokenReceiver
+		} else if unpackedArgs["tokenReceiver"] != nil {
+			return nil, fmt.Errorf("unexpected type for tokenReceiver in SVMExtraArgsV1: %T", unpackedArgs["tokenReceiver"])
 		} else {
 			output["tokenReceiver"] = nil
 		}
 
-		accountsField := val.FieldByName("Accounts")
-		if accountsField.IsValid() {
-			output["accounts"] = accountsField.Interface()
+		if accounts, ok := unpackedArgs["accounts"].([][32]byte); ok {
+			output["accounts"] = accounts
+		} else if unpackedArgs["accounts"] != nil {
+			return nil, fmt.Errorf("unexpected type for accounts in SVMExtraArgsV1: %T", unpackedArgs["accounts"])
 		} else {
 			output["accounts"] = nil
 		}
 	}
+
 	return output, nil
 }
-
-var _ ccipcommon.ExtraDataDecoder = (*ExtraDataDecoder)(nil)
