2189ddaba1cf028288dca4c171584a27a5495002
cfal <cfal@users.noreply.github.com>
2025-04-18 02:19:21 +0400

    core/capabilities/ccip/configs/aptos: add ccip CW/CR configs

---
+++ core/capabilities/ccip/configs/aptos/chain_writer.go
@@ -0,0 +1,70 @@
+package aptosconfig
+
+import (
+	"fmt"
+
+	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
+
+	"github.com/smartcontractkit/chainlink-aptos/relayer/chainreader"
+	"github.com/smartcontractkit/chainlink-aptos/relayer/chainwriter"
+	"github.com/smartcontractkit/chainlink-aptos/relayer/utils"
+)
+
+func GetChainWriterConfig(publicKeyStr string) (chainwriter.ChainWriterConfig, error) {
+	fromAddress, err := utils.HexPublicKeyToAddress(publicKeyStr)
+	if err != nil {
+		return chainwriter.ChainWriterConfig{}, fmt.Errorf("failed to parse Aptos address from public key %s: %w", publicKeyStr, err)
+	}
+
+	fmt.Printf("DEBUG: Aptos GetChainWriterConfig: fromAddressStr=%s, pubKeyStr=%s\n", fromAddress.String(), publicKeyStr)
+
+	return chainwriter.ChainWriterConfig{
+		Modules: map[string]*chainwriter.ChainWriterModule{
+			consts.ContractNameOffRamp: {
+				Name: "offramp",
+				Functions: map[string]*chainwriter.ChainWriterFunction{
+					consts.MethodCommit: {
+						Name:        "commit",
+						PublicKey:   publicKeyStr,
+						FromAddress: fromAddress.String(),
+						Params: []chainreader.AptosFunctionParam{
+							{
+								Name:     "ReportContext",
+								Type:     "vector<vector<u8>>",
+								Required: true,
+							},
+							{
+								Name:     "Report",
+								Type:     "vector<u8>",
+								Required: true,
+							},
+							{
+								Name:     "Signatures",
+								Type:     "vector<vector<u8>>",
+								Required: true,
+							},
+						},
+					},
+					consts.MethodExecute: {
+						Name:        "execute",
+						PublicKey:   publicKeyStr,
+						FromAddress: fromAddress.String(),
+						Params: []chainreader.AptosFunctionParam{
+							{
+								Name:     "ReportContext",
+								Type:     "vector<vector<u8>>",
+								Required: true,
+							},
+							{
+								Name:     "Report",
+								Type:     "vector<u8>",
+								Required: true,
+							},
+						},
+					},
+				},
+			},
+		},
+		FeeStrategy: chainwriter.DefaultFeeStrategy,
+	}, nil
+}
