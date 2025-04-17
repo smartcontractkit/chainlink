92f018061d9da693cb9240109313509653ed76ce
cfal <cfal@users.noreply.github.com>
2025-04-18 02:56:17 +0400

    core/capabilities/ccip/ocrimpls/svm_contract_transmitter_factory.go: add aptos for extra data codec

--- core/capabilities/ccip/ocrimpls/svm_contract_transmitter_factory.go
+++ core/capabilities/ccip/ocrimpls/svm_contract_transmitter_factory.go
@@ -9,6 +9,7 @@ import (
 	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
+	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/ccipaptos"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/ccipevm"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/ccipsolana"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
@@ -179,7 +180,7 @@ func (f *SVMContractTransmitterFactory) NewExecTransmitter(
 		offrampAddress: offrampAddress,
 		toCalldataFn:   SVMExecCalldataFunc,
 		extraDataCodec: ccipcommon.NewExtraDataCodec(
-			ccipcommon.NewExtraDataCodecParams(ccipevm.ExtraDataDecoder{}, ccipsolana.ExtraDataDecoder{}),
+			ccipcommon.NewExtraDataCodecParams(ccipevm.ExtraDataDecoder{}, ccipsolana.ExtraDataDecoder{}, ccipaptos.ExtraDataDecoder{}),
 		),
 	}
 }
