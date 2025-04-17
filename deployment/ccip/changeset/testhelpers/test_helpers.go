a2592324ac4527da80815134e2b3609dcbb9cf34
Damjan Smickovski <smickovskid@outlook.com>
2025-04-17 12:14:29 +0200

    Logging linefix

--- deployment/ccip/changeset/testhelpers/test_helpers.go
+++ deployment/ccip/changeset/testhelpers/test_helpers.go
@@ -828,7 +828,7 @@ func AddLane(
 }
 
 func AddLaneSolanaChangesets(e *DeployedEnv, solChainSelector, remoteChainSelector uint64, remoteFamily string) []commoncs.ConfiguredChangeSet {
-	e.Env.Logger.Infof("Adding lane for solana chain selector", solChainSelector, "to remote chain selector", remoteChainSelector)
+	e.Env.Logger.Infof("Adding lane for solana chain selector %v to remote chain selector %v", solChainSelector, remoteChainSelector)
 	chainFamilySelector := [4]uint8{}
 	if remoteFamily == chainsel.FamilyEVM {
 		// bytes4(keccak256("CCIP ChainFamilySelector EVM"))
