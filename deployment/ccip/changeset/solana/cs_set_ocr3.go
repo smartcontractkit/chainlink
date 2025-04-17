75c399537e88760cadf0e2d70b48caa192f32e49
Terry Tata <terry.tata@smartcontract.com>
2025-04-17 11:20:33 -0400

    fix batching

--- deployment/ccip/changeset/solana/cs_set_ocr3.go
+++ deployment/ccip/changeset/solana/cs_set_ocr3.go
@@ -97,7 +97,6 @@ func SetOCR3ConfigSolana(e deployment.Environment, cfg v1_6.SetOCR3OffRampConfig
 		proposers[remote] = mcmsSolana.ContractAddress(mcmState.McmProgram, mcmsSolana.PDASeed(mcmState.ProposerMcmSeed))
 		inspectors[remote] = mcmsSolana.NewInspector(chain.Client)
 
-		var txns []mcmsTypes.Transaction
 		offRampConfigPDA := state.SolChains[remote].OffRampConfigPDA
 		offRampStatePDA := state.SolChains[remote].OffRampStatePDA
 		solOffRamp.SetProgramID(state.SolChains[remote].OffRamp)
