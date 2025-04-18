a7180b6b19ffb07e8f6afcb5b0b1903264e6ea85
Terry Tata <terry.tata@smartcontract.com>
2025-04-18 05:17:20 -0400

    hex encode onramp

--- deployment/ccip/view/solana/offramp.go
+++ deployment/ccip/view/solana/offramp.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gagliardetto/solana-go"
-	"github.com/mr-tron/base58"
 
 	solOffRamp "github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_offramp"
 	solState "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/state"
@@ -78,7 +78,7 @@ func GenerateOffRampView(chain deployment.SolChain, program solana.PublicKey, re
 			IsEnabled:                 chainStateAccount.Config.IsEnabled,
 			IsRmnVerificationDisabled: chainStateAccount.Config.IsRmnVerificationDisabled,
 			LaneCodeVersion:           chainStateAccount.Config.LaneCodeVersion.String(),
-			OnRamp:                    base58.Encode(chainStateAccount.Config.OnRamp.Bytes[:]),
+			OnRamp:                    common.BytesToAddress(chainStateAccount.Config.OnRamp.Bytes[:]).String(),
 		}
 	}
 	return view, nil
