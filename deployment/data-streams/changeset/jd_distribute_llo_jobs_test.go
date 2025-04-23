bf3a7ea00bd47cf8f6db52e7621ea585b49c533b
Ivaylo Novakov <ivaylo.novakov@smartcontract.com>
2025-04-23 18:44:58 +0300

    More linter fixes because the local execution of the linter is completely unhelpful.

--- deployment/data-streams/changeset/jd_distribute_llo_jobs_test.go
+++ deployment/data-streams/changeset/jd_distribute_llo_jobs_test.go
@@ -73,7 +73,7 @@ func TestDistributeLLOJobSpecs(t *testing.T) {
 
 	// insert a Configurator address for the given DON
 	configuratorAddr := "0x4170ed0880ac9a755fd29b2688956bd959f923f4"
-	err = env.ExistingAddresses.Save(chainSelector, configuratorAddr, //nolint: staticcheck
+	err = env.ExistingAddresses.Save(chainSelector, configuratorAddr, //nolint: staticcheck // I don't care that ExistingAddresses is deprecated. We will fix it later.
 		deployment.TypeAndVersion{
 			Type:    "Configurator",
 			Version: deployment.Version1_0_0,
