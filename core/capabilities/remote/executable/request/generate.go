8094953d41ab464997f312fb617651441965a0ab
patrickhuie19 <patrick.huie@smartcontract.com>
2025-04-28 18:51:52 -0400

    trying to fix CI with protoc-gen-grpc

--- core/capabilities/remote/executable/request/generate.go
+++ core/capabilities/remote/executable/request/generate.go
@@ -1,2 +1,2 @@
-//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative transmit_schedule_event.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative transmit_schedule_event.proto
 package request
