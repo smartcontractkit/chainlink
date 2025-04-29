08bd804fad7fb2b6ce64aa0fc8dce3324b639c65
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2025-04-29 12:58:39 -0500

    changed usage of temporary directory from os.CreateTemp to using the testing package

--- core/internal/testutils/wasmtest/wasm.go
+++ core/internal/testutils/wasmtest/wasm.go
@@ -8,23 +8,19 @@ import (
 	"testing"
 
 	"github.com/andybalholm/brotli"
-	"github.com/google/uuid"
 
 	"github.com/stretchr/testify/require"
 )
 
 func CreateTestBinary(outputPath string, compress bool, t *testing.T) []byte {
-	tempFile, err := os.CreateTemp("", uuid.New().String()+".wasm")
-	require.NoError(t, err)
-	defer os.Remove(tempFile.Name())
-
-	cmd := exec.Command("go", "build", "-o", tempFile.Name(), "github.com/smartcontractkit/chainlink/v2/"+outputPath) // #nosec
+	filePath := t.TempDir() + "/test_binary.wasm"
+	cmd := exec.Command("go", "build", "-o", filePath, "github.com/smartcontractkit/chainlink/v2/"+outputPath) // #nosec
 	cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
 
 	output, err := cmd.CombinedOutput()
 	require.NoError(t, err, string(output))
 
-	binary, err := os.ReadFile(tempFile.Name())
+	binary, err := os.ReadFile(filePath)
 	require.NoError(t, err)
 
 	if !compress {
