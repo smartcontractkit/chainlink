b77a1d7449072644b45e9132af7c89d743ed4c57
Terry Tata <terry.tata@smartcontract.com>
2025-05-01 10:37:25 -0400

    bump

--- deployment/ccip/changeset/solana/cs_deploy_chain.go
+++ deployment/ccip/changeset/solana/cs_deploy_chain.go
@@ -149,7 +149,7 @@ func (c DeployChainContractsConfig) Validate(e deployment.Environment) error {
 func DeployChainContractsChangeset(e deployment.Environment, c DeployChainContractsConfig) (deployment.ChangesetOutput, error) {
 	e.SolChains[c.ChainSelector].CloseBuffers(e.Logger)
 	if true {
-		return deployment.ChangesetOutput{}, fmt.Errorf("deploying chain contracts is not supported in this environment")
+		return deployment.ChangesetOutput{}, errors.New("deploying chain contracts is not supported in this environment")
 	}
 	if err := c.Validate(e); err != nil {
 		return deployment.ChangesetOutput{}, fmt.Errorf("invalid DeployChainContractsConfig: %w", err)
