eff56e436cc96fbd8ce4274229b2d5e15e3f2cde
Chris De Leon <christopher.deleon@smartcontract.com>
2025-04-30 17:25:58 -0700

    fix: infer fee token program ID

--- deployment/ccip/changeset/testhelpers/test_helpers.go
+++ deployment/ccip/changeset/testhelpers/test_helpers.go
@@ -84,7 +84,9 @@ import (
 
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/abihelpers"
 
+	solbinary "github.com/gagliardetto/binary"
 	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/programs/token"
 	"github.com/gagliardetto/solana-go/rpc"
 )
 
@@ -542,14 +544,23 @@ func SendRequestSol(
 	sender := c.DeployerKey
 
 	// if fee token is 0, fallback to WSOL
-	feeTokenProgramID := solana.Token2022ProgramID
 	if message.FeeToken.IsZero() {
-		feeTokenProgramID = solana.TokenProgramID
 		message.FeeToken = s.WSOL
 	}
+	feeToken := message.FeeToken
 
 	e.Logger.Infof("Sending CCIP request from chain selector %d to chain selector %d from sender %s",
 		cfg.SourceChain, cfg.DestChain, sender.PublicKey().String())
+
+	feeTokenInfo, err := client.GetAccountInfo(ctx, feeToken)
+	if err != nil {
+		return nil, err
+	}
+	var mint token.Mint
+	if err := solbinary.NewBinDecoder(feeTokenInfo.Bytes()).Decode(&mint); err != nil {
+		return nil, fmt.Errorf("the provided fee token is not a valid token: (err = %w)", err)
+	}
+	feeTokenProgramID := feeTokenInfo.Value.Owner
 
 	destinationChainStatePDA, err := solstate.FindDestChainStatePDA(destinationChainSelector, s.Router)
 	if err != nil {
@@ -560,8 +571,6 @@ func SendRequestSol(
 	if err != nil {
 		return nil, err
 	}
-
-	feeToken := message.FeeToken
 
 	linkFqBillingConfigPDA, _, err := solstate.FindFqBillingTokenConfigPDA(s.LinkToken, s.FeeQuoter)
 	if err != nil {
