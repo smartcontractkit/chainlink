be5ac261c5f76407c662597b315c8954b7720e4c
Michael Street <5597260+MStreet3@users.noreply.github.com>
2025-05-08 09:08:05 -0400

    fix: lint errors

--- core/utils/safe/safe.go
+++ core/utils/safe/safe.go
@@ -1,12 +1,10 @@
 package safe
 
-import (
-	"fmt"
-)
+import "errors"
 
 func IntToUint64(n int) (uint64, error) {
 	if n < 0 {
-		return 0, fmt.Errorf("cannot convert negative int to uint64")
+		return 0, errors.New("cannot convert negative int to uint64")
 	}
 
 	return uint64(n), nil
