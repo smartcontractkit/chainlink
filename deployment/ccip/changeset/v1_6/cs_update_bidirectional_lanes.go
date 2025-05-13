c7f43944aee475dc493acc560df941bcb0963a8b
James Kong <jkongie@gmail.com>
2025-05-13 17:14:04 +0800

    feat: updates AggregateProposal to remove dependecy on legacy proposals
    This commit updates the `AggregateProposal` function to remove the
    functionality to aggregate legacy proposals. The function now only
    aggregates new mcms lib proposals.
    
    This change also requires changes to a Changeset to replace the creation
    of legacy proposals with the new mcms lib proposals.

--- deployment/ccip/changeset/v1_6/cs_update_bidirectional_lanes.go
+++ deployment/ccip/changeset/v1_6/cs_update_bidirectional_lanes.go
@@ -235,7 +235,7 @@ func updateBidirectionalLanesLogic(e deployment.Environment, c UpdateBidirection
 	if err != nil {
 		return cldf.ChangesetOutput{}, fmt.Errorf("failed to load onchain state: %w", err)
 	}
-	proposal, err := proposalutils.AggregateProposals(e, state.EVMMCMSStateByChain(), nil, proposals, nil, "Update multiple bidirectional lanes", c.MCMSConfig)
+	proposal, err := proposalutils.AggregateProposals(e, state.EVMMCMSStateByChain(), nil, proposals, "Update multiple bidirectional lanes", c.MCMSConfig)
 	if err != nil {
 		return cldf.ChangesetOutput{}, fmt.Errorf("failed to aggregate proposals: %w", err)
 	}
