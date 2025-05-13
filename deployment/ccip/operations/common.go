d27a77d41ed4ccb66ec97db3ac34264feef3ac07
AnieeG <anindita.ghosh@smartcontract.com>
2025-05-13 15:54:23 -0700

    changes

--- deployment/ccip/operations/common.go
+++ deployment/ccip/operations/common.go
@@ -2,8 +2,10 @@ package operations
 
 import (
 	"github.com/smartcontractkit/chainlink/deployment"
+	"github.com/smartcontractkit/chainlink/deployment/ccip/shared/stateview"
 )
 
 type OpDependencies struct {
-	Env deployment.Environment
+	Env          deployment.Environment
+	CurrentState stateview.CCIPOnChainState
 }
