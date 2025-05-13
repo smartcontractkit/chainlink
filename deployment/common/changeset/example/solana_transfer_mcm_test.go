5c5a6b029dbcbc203e88549e0640a77ac73b4767
Ilan <ilan.olkies@smartcontract.com>
2025-05-13 19:59:10 -0300

    Revert "Fix another test"
    This reverts commit 518b139dfef3820a639330951d2b0f4a7434039f.

--- deployment/common/changeset/example/solana_transfer_mcm_test.go
+++ deployment/common/changeset/example/solana_transfer_mcm_test.go
@@ -181,7 +181,7 @@ func TestTransferFromTimelockConfig_VerifyPreconditions(t *testing.T) {
 					To:     receiverKey,
 				}},
 			},
-			expectedError: "failed to get existing addresses: chain not found: chain selector 12463857294658392847",
+			expectedError: "failed to get existing addresses: chain selector 12463857294658392847: chain not found",
 		},
 		{
 			name: "empty from field",
