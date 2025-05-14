8c12c8fd4cf55d5abbb571ff363ce856848126ee
Terry Tata <terry.tata@smartcontract.com>
2025-05-14 09:38:17 -0400

    e2e token

--- deployment/ccip/changeset/solana/cs_e2e.go
+++ deployment/ccip/changeset/solana/cs_e2e.go
@@ -12,13 +12,13 @@ import (
 var _ cldf.ChangeSet[E2ETokenPoolConfig] = E2ETokenPool
 
 type E2ETokenPoolConfig struct {
-	AddTokenPoolAndLookupTable              []TokenPoolConfig
-	AddTokenPoolLookupTableConfig           []TokenPoolLookupTableConfig
-	RegisterTokenAdminRegistry              []RegisterTokenAdminRegistryConfig
-	AcceptAdminRoleTokenAdminRegistryConfig []AcceptAdminRoleTokenAdminRegistryConfig
-	SetPool                                 []SetPoolConfig
-	RemoteChainTokenPoolConfig              []RemoteChainTokenPoolConfig
-	ConfigureTokenPoolContractsChangesets   []v1_5_1.ConfigureTokenPoolContractsConfig
+	AddTokenPoolAndLookupTable            []TokenPoolConfig
+	AddTokenPoolLookupTable               []TokenPoolLookupTableConfig
+	RegisterTokenAdminRegistry            []RegisterTokenAdminRegistryConfig
+	AcceptAdminRoleTokenAdminRegistry     []AcceptAdminRoleTokenAdminRegistryConfig
+	SetPool                               []SetPoolConfig
+	RemoteChainTokenPool                  []RemoteChainTokenPoolConfig
+	ConfigureTokenPoolContractsChangesets []v1_5_1.ConfigureTokenPoolContractsConfig
 }
 
 func E2ETokenPool(e cldf.Environment, cfg E2ETokenPoolConfig) (cldf.ChangesetOutput, error) {
@@ -41,7 +41,7 @@ func E2ETokenPool(e cldf.Environment, cfg E2ETokenPoolConfig) (cldf.ChangesetOut
 			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
 		}
 	}
-	for _, tokenPoolConfig := range cfg.AddTokenPoolLookupTableConfig {
+	for _, tokenPoolConfig := range cfg.AddTokenPoolLookupTable {
 		output, err := AddTokenPoolLookupTable(e, tokenPoolConfig)
 		if err != nil {
 			return cldf.ChangesetOutput{}, fmt.Errorf("failed to add token pool lookup table: %w", err)
@@ -67,7 +67,7 @@ func E2ETokenPool(e cldf.Environment, cfg E2ETokenPoolConfig) (cldf.ChangesetOut
 			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
 		}
 	}
-	for _, acceptConfig := range cfg.AcceptAdminRoleTokenAdminRegistryConfig {
+	for _, acceptConfig := range cfg.AcceptAdminRoleTokenAdminRegistry {
 		output, err := AcceptAdminRoleTokenAdminRegistry(e, acceptConfig)
 		if err != nil {
 			return cldf.ChangesetOutput{}, fmt.Errorf("failed to accept admin role token admin registry: %w", err)
@@ -93,7 +93,7 @@ func E2ETokenPool(e cldf.Environment, cfg E2ETokenPoolConfig) (cldf.ChangesetOut
 			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
 		}
 	}
-	for _, remoteChainConfig := range cfg.RemoteChainTokenPoolConfig {
+	for _, remoteChainConfig := range cfg.RemoteChainTokenPool {
 		output, err := SetupTokenPoolForRemoteChain(e, remoteChainConfig)
 		if err != nil {
 			return cldf.ChangesetOutput{}, fmt.Errorf("failed to remote chain token pool config: %w", err)
@@ -110,6 +110,62 @@ func E2ETokenPool(e cldf.Environment, cfg E2ETokenPoolConfig) (cldf.ChangesetOut
 		output, err := v1_5_1.ConfigureTokenPoolContractsChangeset(e, configureTokenPoolConfig)
 		if err != nil {
 			return cldf.ChangesetOutput{}, fmt.Errorf("failed to configure token pool contracts: %w", err)
+		}
+		err = finalOutput.AddressBook.Merge(output.AddressBook) //nolint:staticcheck // Addressbook is deprecated, but we still use it for the time being
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to merge address book: %w", err)
+		}
+		if len(output.MCMSTimelockProposals) > 0 {
+			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
+		}
+	}
+
+	return finalOutput, nil
+}
+
+type E2ETokenConfig struct {
+	DeploySolanaToken   []DeploySolanaTokenConfig
+	UploadTokenMetadata []UploadTokenMetadataConfig
+	SetTokenAuthority   []SetTokenAuthorityConfig
+}
+
+func E2EToken(e cldf.Environment, cfg E2ETokenConfig) (cldf.ChangesetOutput, error) {
+	finalOutput := cldf.ChangesetOutput{}
+	defer func(e cldf.Environment) {
+		e.Logger.Info("E2EToken changeset completed")
+		e.Logger.Info("Final output: ", finalOutput.AddressBook) //nolint:staticcheck // Addressbook is deprecated, but we still use it for the time being
+	}(e)
+
+	for _, config := range cfg.DeploySolanaToken {
+		output, err := DeploySolanaToken(e, config)
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to deploy solana token: %w", err)
+		}
+		err = finalOutput.AddressBook.Merge(output.AddressBook) //nolint:staticcheck // Addressbook is deprecated, but we still use it for the time being
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to merge address book: %w", err)
+		}
+		if len(output.MCMSTimelockProposals) > 0 {
+			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
+		}
+	}
+	for _, config := range cfg.UploadTokenMetadata {
+		output, err := UploadTokenMetadata(e, config)
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to upload token metadata: %w", err)
+		}
+		err = finalOutput.AddressBook.Merge(output.AddressBook) //nolint:staticcheck // Addressbook is deprecated, but we still use it for the time being
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to merge address book: %w", err)
+		}
+		if len(output.MCMSTimelockProposals) > 0 {
+			finalOutput.MCMSTimelockProposals = append(finalOutput.MCMSTimelockProposals, output.MCMSTimelockProposals...)
+		}
+	}
+	for _, config := range cfg.SetTokenAuthority {
+		output, err := SetTokenAuthority(e, config)
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to register token admin registry: %w", err)
 		}
 		err = finalOutput.AddressBook.Merge(output.AddressBook) //nolint:staticcheck // Addressbook is deprecated, but we still use it for the time being
 		if err != nil {
