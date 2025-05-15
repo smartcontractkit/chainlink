ba157ff3abd74e8ad0c0ecce73ac00c24fe64057
AnieeG <anindita.ghosh@smartcontract.com>
2025-05-15 15:10:26 -0700

    changes

--- deployment/ccip/operations/common.go
+++ deployment/ccip/operations/common.go
@@ -1,11 +1,68 @@
 package operations
 
 import (
-	"github.com/smartcontractkit/chainlink/deployment"
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/mcms"
+
 	"github.com/smartcontractkit/chainlink/deployment/ccip/shared/stateview"
+	"github.com/smartcontractkit/chainlink/deployment/common/proposalutils"
 )
 
 type OpDependencies struct {
-	Env          deployment.Environment
+	Env          cldf.Environment
 	CurrentState stateview.CCIPOnChainState
 }
+
+type OpOutput struct {
+	Proposals   []mcms.TimelockProposal
+	AddressBook cldf.AddressBook
+}
+
+func (o *OpOutput) Merge(other OpOutput, env cldf.Environment) error {
+	if o.AddressBook == nil {
+		o.AddressBook = other.AddressBook
+	} else if other.AddressBook != nil {
+		if err := o.AddressBook.Merge(other.AddressBook); err != nil {
+			return fmt.Errorf("failed to merge address book: %w", err)
+		}
+		if err := env.ExistingAddresses.Merge(other.AddressBook); err != nil {
+			return fmt.Errorf("failed to merge existing addresses to environment: %w", err)
+		}
+	}
+	if len(other.Proposals) == 0 {
+		o.Proposals = append(o.Proposals, other.Proposals...)
+	}
+	return nil
+}
+
+type postOpsInput struct {
+	SolanaChainSelector uint64
+	EVMChainSelector    uint64
+	MCMSConfig          *proposalutils.TimelockConfig
+	Proposals           []mcmslib.TimelockProposal
+}
+
+var (
+	PostOps = operations.NewOperation(
+		"postOpsToAggregateProposals",
+		semver.MustParse("1.0.0"),
+		"Post ops to aggregate proposals",
+		func(b operations.Bundle, deps OpDependencies, input postOpsInput) ([]mcmslib.TimelockProposal, error) {
+			allProposals := input.Proposals
+			proposal, err := proposalutils.AggregateProposals(
+				deps.Env, deps.EVMMCMSState, deps.SolanaMCMSState, allProposals,
+				"Adding EVM and Solana lane", input.MCMSConfig)
+			if err != nil {
+				return nil, err
+			}
+			if proposal != nil {
+				input.Proposals = []mcmslib.TimelockProposal{*proposal}
+			}
+			return input.Proposals, nil
+		},
+	)
+)
--- deployment/ccip/operations/evm/v1_6/ops_deploy_chain.go
+++ deployment/ccip/operations/evm/v1_6/ops_deploy_chain.go
@@ -2,7 +2,6 @@ package v1_6
 
 import (
 	"github.com/Masterminds/semver/v3"
-	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
 
 	ccipoperations "github.com/smartcontractkit/chainlink/deployment/ccip/operations"
@@ -13,7 +12,7 @@ var (
 		"DeployRMNRemote",
 		semver.MustParse("1.0.0"),
 		"Deploys RMNRemote 1.6 contract on the specified evm chain(s)",
-		func(b operations.Bundle, deps ccipoperations.OpDependencies, _ any) (deployment.AddressBook, error) {
+		func(b operations.Bundle, deps ccipoperations.OpDependencies, _ any) (ccipoperations.OpOutput, error) {
 
 		})
 )
