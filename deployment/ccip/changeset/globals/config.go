c1d8e9147a48d33b41ab7bd8963e30380cd2caad
nogo <0xnogo@gmail.com>
2025-05-23 18:32:33 +0700

    change var name

--- deployment/ccip/changeset/globals/config.go
+++ deployment/ccip/changeset/globals/config.go
@@ -21,13 +21,13 @@ const (
 	RemoteGasPriceBatchWriteFrequency = 20 * time.Minute
 	TokenPriceBatchWriteFrequency     = 2 * time.Hour
 	// Building batches with 6.5m and transmit with 8m to account for overhead.
-	BatchGasLimit                  = 6_500_000
-	InflightCacheExpiry            = 1 * time.Minute
-	RootSnoozeTime                 = 5 * time.Minute
-	BatchingStrategyID             = 0
-	OptimisticConfirmations        = 1
-	TransmissionDelayMultiplier    = 15 * time.Second
-	DefaultMaxCommitReportsToFetch = 250
+	BatchGasLimit               = 6_500_000
+	InflightCacheExpiry         = 1 * time.Minute
+	RootSnoozeTime              = 5 * time.Minute
+	BatchingStrategyID          = 0
+	OptimisticConfirmations     = 1
+	TransmissionDelayMultiplier = 15 * time.Second
+	MaxCommitReportsToFetch     = 250
 	// ======================================
 
 	// ========= Onchain consts =========
@@ -87,7 +87,7 @@ var (
 		TransmissionDelayMultiplier: TransmissionDelayMultiplier,
 		MaxReportMessages:           0,
 		MaxSingleChainReports:       0,
-		MaxCommitReportsToFetch:     DefaultMaxCommitReportsToFetch,
+		MaxCommitReportsToFetch:     MaxCommitReportsToFetch,
 		// Remaining fields cannot be statically set:
 		// TokenDataObservers: , // Must be configured in CLD
 	}
--- deployment/ccip/changeset/v1_6/config.go
+++ deployment/ccip/changeset/v1_6/config.go
@@ -71,7 +71,7 @@ var (
 			RootSnoozeTime:            *config.MustNewDuration(globals.RootSnoozeTime),
 			MessageVisibilityInterval: *config.MustNewDuration(globals.PermissionLessExecutionThreshold),
 			BatchingStrategyID:        globals.BatchingStrategyID,
-			MaxCommitReportsToFetch:   globals.DefaultMaxCommitReportsToFetch,
+			MaxCommitReportsToFetch:   globals.MaxCommitReportsToFetch,
 		},
 	}
 )
