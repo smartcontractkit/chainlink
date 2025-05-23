7488eff43e1dea9da65b19057aaabd433a3d1a0c
krehermann <16602512+krehermann@users.noreply.github.com>
2025-05-23 13:12:30 -0600

    fixes deployment logic to support multiple sets of mcms

--- deployment/environment/devenv/chain.go
+++ deployment/environment/devenv/chain.go
@@ -52,6 +52,7 @@ type ChainConfig struct {
 	DeployerKey        *bind.TransactOpts       // key to deploy and configure contracts on the chain
 	SolDeployerKey     solana.PrivateKey
 	Users              []*bind.TransactOpts // map of addresses to their transact opts to interact with the chain as users
+	MultiClientOpts    func(c *cldf.MultiClient)
 }
 
 func (c *ChainConfig) SetUsers(pvtkeys []string) error {
@@ -157,7 +158,7 @@ func NewChains(logger logger.Logger, configs []ChainConfig) (map[uint64]cldf.Cha
 
 			switch chainCfg.ChainType {
 			case EVMChainType:
-				ec, err := cldf.NewMultiClient(logger, rpcConf)
+				ec, err := cldf.NewMultiClient(logger, rpcConf, chainCfg.MultiClientOpts)
 				if err != nil {
 					return fmt.Errorf("failed to create multi client: %w", err)
 				}
