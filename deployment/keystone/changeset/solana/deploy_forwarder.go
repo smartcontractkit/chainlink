dfa0ae57d12f036fe91f87baf56e0f572ae27327
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2025-05-23 13:47:33 -0400

    add initialize to forwarder

--- deployment/keystone/changeset/solana/deploy_forwarder.go
+++ deployment/keystone/changeset/solana/deploy_forwarder.go
@@ -1,6 +1,10 @@
 package solana
 
 import (
+	"fmt"
+
+	"github.com/gagliardetto/solana-go"
+	solanaUtils "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	cldfdeployment "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	ks_forwarder "github.com/smartcontractkit/chainlink-solana/contracts/generated/keystone_forwarder"
@@ -28,7 +32,23 @@ func DeployForwarder(env cldf.Environment, req *DeployRequest) (cldf.ChangesetOu
 		return cldf.ChangesetOutput{}, err
 	}
 
+	// initialize
 	ks_forwarder.SetProgramID(address)
+
+	key, err := solana.NewRandomPrivateKey()
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to create random keys: %w", err)
+	}
+
+	instruction, err := ks_forwarder.NewInitializeInstruction(key.PublicKey(), chain.DeployerKey.PublicKey(), solana.SystemProgramID).ValidateAndBuild()
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to build and validate initialize instruction %w", err)
+	}
+
+	instructions := []solana.Instruction{instruction}
+	if err = chain.Confirm(instructions, solanaUtils.AddSigners(key)); err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to confirm ")
+	}
 
 	return cldf.ChangesetOutput{
 		AddressBook: ab,
