81aaf33550db55b7d01712c508dfa3174f04795c
Graham Goh <graham.goh@smartcontract.com>
2025-05-27 15:27:54 +1000

    fix(aptos): migrate to new aptos packge
    Instead of cldf.AptosChain which is an alias, we will now import cldf_aptos.Chain directly
    
    JIRA: https://smartcontract-it.atlassian.net/browse/CLD-276

--- deployment/ccip/shared/stateview/aptos/state.go
+++ deployment/ccip/shared/stateview/aptos/state.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aptos-labs/aptos-go-sdk"
 	module_offramp "github.com/smartcontractkit/chainlink-aptos/bindings/ccip_offramp/offramp"
+	cldf_aptos "github.com/smartcontractkit/chainlink-deployments-framework/chain/aptos"
 
 	"github.com/smartcontractkit/chainlink-aptos/bindings/bind"
 	"github.com/smartcontractkit/chainlink-aptos/bindings/ccip_offramp"
@@ -70,7 +71,7 @@ func loadAptosChainStateFromAddresses(addresses map[string]cldf.TypeAndVersion) 
 	return chainState, nil
 }
 
-func GetOfframpDynamicConfig(c cldf.AptosChain, ccipAddress aptos.AccountAddress) (module_offramp.DynamicConfig, error) {
+func GetOfframpDynamicConfig(c cldf_aptos.Chain, ccipAddress aptos.AccountAddress) (module_offramp.DynamicConfig, error) {
 	offrampBind := ccip_offramp.Bind(ccipAddress, c.Client)
 	return offrampBind.Offramp().GetDynamicConfig(&bind.CallOpts{})
 }
