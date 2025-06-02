7b2e02914ff015d87dee2fabbc2c4b08ba2a7156
Karen Stepanyan <karen.stepanyan@smartcontract.com>
2025-06-02 18:57:54 +0400

    remove platform deployment code

--- deployment/data-feeds/changeset/aptos/deploy.go
+++ deployment/data-feeds/changeset/aptos/deploy.go
@@ -6,41 +6,9 @@ import (
 	"github.com/aptos-labs/aptos-go-sdk"
 
 	modulefeeds "github.com/smartcontractkit/chainlink-aptos/bindings/data_feeds"
-	moduleplatform "github.com/smartcontractkit/chainlink-aptos/bindings/platform"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/data-feeds/changeset/types"
 )
-
-func DeployPlatform(chain cldf.AptosChain, owner aptos.AccountAddress, labels []string) (*types.DeployPlatformResponse, error) {
-	address, pendingTX, platformModule, err := moduleplatform.DeployToObject(chain.DeployerSigner, chain.Client, owner)
-	if err != nil {
-		return nil, fmt.Errorf("failed to deploy ChainlinkPlatform: %w", err)
-	}
-
-	_, err = chain.Client.WaitForTransaction(pendingTX.Hash)
-	if err != nil {
-		return nil, fmt.Errorf("failed to confirm ChainlinkPlatform: %w", err)
-	}
-
-	// ChainlinkPlatform package contracts don't implement typeAndVersion interface, so we have to set it manually
-	tvStr := "ChainlinkPlatform 1.0.0"
-	tv, err := cldf.TypeAndVersionFromString(tvStr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse type and version from %s, %w", pendingTX.Hash, err)
-	}
-
-	for _, label := range labels {
-		tv.Labels.Add(label)
-	}
-
-	resp := &types.DeployPlatformResponse{
-		Address:  address,
-		Tx:       pendingTX.Hash,
-		Tv:       tv,
-		Contract: &platformModule,
-	}
-	return resp, nil
-}
 
 func DeployDataFeeds(chain cldf.AptosChain, owner aptos.AccountAddress, platform aptos.AccountAddress, secondaryPlatform aptos.AccountAddress, labels []string) (*types.DeployDataFeedsResponse, error) {
 	address, pendingTX, feedsModule, err := modulefeeds.DeployToObject(chain.DeployerSigner, chain.Client, owner, platform, owner, secondaryPlatform)
