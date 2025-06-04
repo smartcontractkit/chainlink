8e49bb9fdca6f68b88a43c00065b02aa2257cac2
joaoluisam <joao.luismoreira@smartcontract.com>
2025-06-04 10:10:47 +0100

    split suites to run in parallel

---
+++ integration-tests/smoke/ccip/ccip_aptos2evm_messaging_test.go
@@ -0,0 +1,89 @@
+package ccip
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/maps"
+
+	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/testhelpers"
+	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/testhelpers/messagingtest"
+	"github.com/smartcontractkit/chainlink/deployment/ccip/shared/stateview"
+	testsetups "github.com/smartcontractkit/chainlink/integration-tests/testsetups/ccip"
+)
+
+func Test_CCIP_Messaging_Aptos2EVM(t *testing.T) {
+	ctx := testhelpers.Context(t)
+	e, _, _ := testsetups.NewIntegrationEnvironment(
+		t,
+		testhelpers.WithNumOfChains(2),
+		testhelpers.WithAptosChains(1),
+	)
+	evmChainSelectors := e.Env.AllChainSelectors()
+	aptosChainSelectors := maps.Keys(e.Env.BlockChains.AptosChains())
+
+	fmt.Println("EVM: ", evmChainSelectors)
+	fmt.Println("Aptos: ", aptosChainSelectors)
+
+	state, err := stateview.LoadOnchainState(e.Env)
+	require.NoError(t, err)
+
+	sourceChain := aptosChainSelectors[0]
+	destChain := evmChainSelectors[1]
+
+	t.Log("Source chain (Aptos): ", sourceChain, "Dest chain (EVM): ", destChain)
+
+	testhelpers.AddLaneWithDefaultPricesAndFeeQuoterConfig(t, &e, state, sourceChain, destChain, false)
+
+	var (
+		replayed      bool
+		nonce         uint64
+		senderAddress = e.Env.BlockChains.AptosChains()[sourceChain].DeployerSigner.AccountAddress()
+		sender        = common.LeftPadBytes(senderAddress[:], 32)
+		out           messagingtest.TestCaseOutput
+		setup         = messagingtest.NewTestSetupWithDeployedEnv(
+			t,
+			e,
+			state,
+			sourceChain,
+			destChain,
+			sender,
+			false, // testRouter
+		)
+	)
+
+	t.Run("Message to EVM", func(t *testing.T) {
+		latestHead, err := testhelpers.LatestBlock(ctx, e.Env, destChain)
+		require.NoError(t, err)
+		message := []byte("Hello EVM, from Aptos!")
+		out = messagingtest.Run(t,
+			messagingtest.TestCase{
+				TestSetup:              setup,
+				Replayed:               replayed,
+				Nonce:                  &nonce,
+				ValidationType:         messagingtest.ValidationTypeExec,
+				FeeToken:               "0xa",
+				Receiver:               state.Chains[destChain].Receiver.Address().Bytes(),
+				MsgData:                message,
+				ExtraArgs:              nil,
+				ExpectedExecutionState: testhelpers.EXECUTION_STATE_SUCCESS,
+				ExtraAssertions: []func(t *testing.T){
+					func(t *testing.T) {
+						iter, err := state.Chains[destChain].Receiver.FilterMessageReceived(&bind.FilterOpts{
+							Context: ctx,
+							Start:   latestHead,
+						})
+						require.NoError(t, err)
+						require.True(t, iter.Next())
+						// Todo: MessageReceived doesn't emit the data unfortunately, so can't check that.
+					},
+				},
+			},
+		)
+	})
+
+	fmt.Printf("out: %v\n", out)
+}
