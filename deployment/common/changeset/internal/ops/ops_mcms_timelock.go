eb60890d9d3f6ef697adccd78ae8981eece806b7
simsonraj <simsonraj.easvarasakthi@smartcontract.com>
2025-06-04 22:34:35 +0530

    fix zksync deployment

--- deployment/common/changeset/internal/ops/ops_mcms_timelock.go
+++ deployment/common/changeset/internal/ops/ops_mcms_timelock.go
@@ -8,14 +8,16 @@ import (
 
 	"github.com/smartcontractkit/chainlink/deployment"
 
+	"github.com/ethereum/go-ethereum/core/types"
+	bindings "github.com/smartcontractkit/ccip-owner-contracts/pkg/gethwrappers"
 	evmMcms "github.com/smartcontractkit/mcms/sdk/evm"
-	"github.com/smartcontractkit/mcms/sdk/evm/bindings"
 	mcmsTypes "github.com/smartcontractkit/mcms/types"
 
 	cldf_evm "github.com/smartcontractkit/chainlink-deployments-framework/chain/evm"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	mcmsnew_zksync "github.com/smartcontractkit/chainlink/deployment/common/changeset/internal/evm/zksync"
 )
 
 type OpEVMMCMSDeps struct {
@@ -38,7 +40,6 @@ type OpEVMSetConfigMCMSInput struct {
 
 type OpEVMDeployMCMSOutput struct {
 	Address common.Address `json:"address"`
-	Tx      common.Hash    `json:"tx"`
 }
 
 type OpEVMSetConfigMCMSOutput struct {
@@ -54,10 +55,25 @@ var OpEVMDeployMCMS = operations.NewOperation(
 
 		mcm, err := cldf.DeployContract(b.Logger, deps.Chain, deps.AddrBook,
 			func(chain cldf_evm.Chain) cldf.ContractDeploy[*bindings.ManyChainMultiSig] {
-				addr, tx, _, err2 := ccip_owner.DeployManyChainMultiSig(
-					deps.Chain.DeployerKey,
-					deps.Backend,
+				var (
+					mcmAddr common.Address
+					tx      *types.Transaction
+					mcm     *bindings.ManyChainMultiSig
+					err2    error
 				)
+				if chain.IsZkSyncVM {
+					mcmAddr, _, mcm, err2 = mcmsnew_zksync.DeployManyChainMultiSigZk(
+						nil,
+						chain.ClientZkSyncVM,
+						chain.DeployerKeyZkSyncVM,
+						chain.Client,
+					)
+				} else {
+					mcmAddr, tx, mcm, err2 = ccip_owner.DeployManyChainMultiSig(
+						deps.Chain.DeployerKey,
+						deps.Backend,
+					)
+				}
 
 				tv := cldf.NewTypeAndVersion(input.ContractType, deployment.Version1_0_0)
 				for _, option := range deps.Options {
@@ -65,7 +81,7 @@ var OpEVMDeployMCMS = operations.NewOperation(
 				}
 
 				return cldf.ContractDeploy[*bindings.ManyChainMultiSig]{
-					Address: addr, Contract: nil, Tx: tx, Tv: tv, Err: err2,
+					Address: mcmAddr, Contract: mcm, Tx: tx, Tv: tv, Err: err2,
 				}
 			})
 
@@ -78,18 +94,6 @@ var OpEVMDeployMCMS = operations.NewOperation(
 			return out, err
 		}
 
-		// Confirm the transaction
-		if _, err = deps.Chain.Confirm(mcm.Tx); err != nil {
-			b.Logger.Errorw("Failed to confirm deployment",
-				"chainSelector", deps.Chain.ChainSelector(),
-				"chainName", deps.Chain.Name(),
-				"contractAddr", mcm.Address.String(),
-				"err", err,
-			)
-
-			return out, err
-		}
-
 		tv := cldf.NewTypeAndVersion(input.ContractType, deployment.Version1_0_0)
 		for _, option := range deps.Options {
 			option(&tv)
@@ -97,7 +101,6 @@ var OpEVMDeployMCMS = operations.NewOperation(
 
 		return OpEVMDeployMCMSOutput{
 			Address: mcm.Address,
-			Tx:      mcm.Tx.Hash(),
 		}, nil
 	})
 
