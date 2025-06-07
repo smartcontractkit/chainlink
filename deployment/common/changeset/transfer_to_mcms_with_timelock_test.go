6c3a172cc3fdd5bf2b5410ea46fc421918000bd3
simsonraj <simsonraj.easvarasakthi@smartcontract.com>
2025-06-07 23:59:51 +0530

    removed the new tests

--- deployment/common/changeset/transfer_to_mcms_with_timelock_test.go
+++ deployment/common/changeset/transfer_to_mcms_with_timelock_test.go
@@ -9,18 +9,13 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	owner_helpers "github.com/smartcontractkit/ccip-owner-contracts/pkg/gethwrappers"
-
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
 
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 
 	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
-	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
 
-	evminternal "github.com/smartcontractkit/chainlink/deployment/common/changeset/internal/evm"
-	"github.com/smartcontractkit/chainlink/deployment/common/changeset/internal/seqs"
 	"github.com/smartcontractkit/chainlink/deployment/common/proposalutils"
 	"github.com/smartcontractkit/chainlink/deployment/common/types"
 	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
@@ -219,90 +214,4 @@ func TestRenounceTimelockDeployer(t *testing.T) {
 
 	// Check that the admin is the timelock
 	require.Equal(t, tl.Address(), admin)
-}
-
-func TestGrantRolesForTimelockWhenDeployerisAdmin(t *testing.T) {
-	lggr := logger.TestLogger(t)
-	tenv := memory.NewMemoryEnvironment(t, lggr, 0, memory.MemoryEnvironmentConfig{
-		Chains: 1,
-		Nodes:  1,
-	})
-	chain1 := tenv.BlockChains.ListChainSelectors(cldf_chain.WithFamily(chain_selectors.FamilyEVM))[0]
-	ab := cldf.NewMemoryAddressBook()
-	mcmsTimelockContracts, err := evminternal.DeployMCMSWithTimelockContractsEVM(tenv,
-		tenv.BlockChains.EVMChains()[chain1],
-		ab, proposalutils.SingleGroupTimelockConfigV2(t), nil)
-	require.NoError(t, err)
-
-	addresses, err := ab.AddressesForChain(chain1)
-	require.NoError(t, err)
-	require.Len(t, addresses, 5)
-
-	mcmsTxs, err := evminternal.GrantRolesForTimelock(tenv, tenv.BlockChains.EVMChains()[chain1], mcmsTimelockContracts, false)
-	require.NoError(t, err)
-	require.Empty(t, mcmsTxs, "expected 0 transactions to be created for granting roles as Deployer is the admin still")
-}
-
-func TestGrantRolesForTimelockWhenDeployerisNotAdmin(t *testing.T) {
-	lggr := logger.TestLogger(t)
-	tenv := memory.NewMemoryEnvironment(t, lggr, 0, memory.MemoryEnvironmentConfig{
-		Chains: 1,
-		Nodes:  1,
-	})
-	chain1 := tenv.BlockChains.ListChainSelectors(cldf_chain.WithFamily(chain_selectors.FamilyEVM))[0]
-	// Deploy MCMS with Timelock V2
-	tenv, err := Apply(t, tenv,
-		Configure(
-			cldf.CreateLegacyChangeSet(DeployMCMSWithTimelockV2),
-			map[uint64]types.MCMSWithTimelockConfigV2{
-				chain1: proposalutils.SingleGroupTimelockConfigV2(t),
-			},
-		),
-	)
-	require.NoError(t, err)
-
-	addrs, err := tenv.ExistingAddresses.AddressesForChain(chain1)
-	require.NoError(t, err)
-
-	state, err := MaybeLoadMCMSWithTimelockChainState(tenv.BlockChains.EVMChains()[chain1], addrs)
-	require.NoError(t, err)
-
-	mcmsTimelockContracts := &state.MCMSWithTimelockContracts
-
-	// Revoke Deployer
-	tenv, err = Apply(t, tenv,
-		Configure(
-			cldf.CreateLegacyChangeSet(RenounceTimelockDeployer),
-			RenounceTimelockDeployerConfig{
-				ChainSel: chain1,
-			},
-		),
-	)
-	require.NoError(t, err)
-	ab := cldf.NewMemoryAddressBook()
-	// Deploy new Proposer MCMS
-	report, err := operations.ExecuteSequence(
-		tenv.OperationsBundle,
-		seqs.SeqEVMDeployMCMWithConfig,
-		seqs.SeqDeployMCMWithConfigDeps{
-			Chain:    tenv.BlockChains.EVMChains()[chain1],
-			AddrBook: ab,
-		},
-		seqs.SeqDeployMCMWithConfigInput{
-			ContractType:  types.ProposerManyChainMultisig,
-			MCMConfig:     proposalutils.SingleGroupTimelockConfigV2(t).Proposer,
-			ChainSelector: chain1,
-		},
-	)
-	require.NoError(t, err)
-	require.NotEmpty(t, report.Output.Address, "expected address to be set in output")
-
-	proposer, err := owner_helpers.NewManyChainMultiSig(report.Output.Address, tenv.BlockChains.EVMChains()[chain1].Client)
-	require.NoError(t, err)
-
-	// Check if the new proposer grant roles produces MCMS Output when the deployer is not the admin
-	(*mcmsTimelockContracts).ProposerMcm = proposer
-	mcmsTxs, err := evminternal.GrantRolesForTimelock(tenv, tenv.BlockChains.EVMChains()[chain1], *mcmsTimelockContracts, false)
-	require.NoError(t, err)
-	require.Len(t, mcmsTxs, 2, "expected 2 mcms transaction to be created for granting Proposer & canceller roles when deployer is not the admin")
 }
