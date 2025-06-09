2ba8a9f0f35a6f1e35c37e858d09d52eb7308122
amit-momin <amit.momin@smartcontract.com>
2025-06-09 14:54:23 -0500

    Removed multi exec reports counter check

--- integration-tests/smoke/ccip/ccip_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_messaging_test.go
@@ -396,13 +396,6 @@ func Test_CCIPMessaging_EVM2Solana(t *testing.T) {
 		})
 		require.NoError(t, err)
 
-		// check that counter is 0
-		var receiverCounterAccount soltesthelpers.ReceiverCounter
-		err = solcommon.GetAccountDataBorshInto(ctx, solChains[destChain].Client, receiverTargetAccountPDA, solconfig.DefaultCommitment, &receiverCounterAccount)
-		require.NoError(t, err, "failed to get account info")
-		// Already sent a message earlier
-		require.Equal(t, uint8(0), receiverCounterAccount.Value)
-
 		numMessages := 5
 		_ = mt.Run(
 			t,
