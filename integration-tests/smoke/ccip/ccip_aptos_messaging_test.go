921d7fe29523ee811c0f93a773a24d8897a020f2
joaoluisam <joao.luismoreira@smartcontract.com>
2025-06-11 22:11:45 +0100

    Try max gas and see what happens

--- integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
@@ -402,7 +402,7 @@ func Test_CCIP_Messaging_Aptos2EVM(t *testing.T) {
 				Receiver:       ccipReceiverAddress,
 				MsgData:        message,
 				// Just ensuring enough gas is provided to execute the message, doesn't matter if it's way too much
-				ExtraArgs:              testhelpers.MakeEVMExtraArgsV2(uint64(300_000), false),
+				ExtraArgs:              testhelpers.MakeEVMExtraArgsV2(uint64(defaultDestinationChainConfig.MaxPerMsgGasLimit), false),
 				ExpectedExecutionState: testhelpers.EXECUTION_STATE_SUCCESS,
 				ExtraAssertions: []func(t *testing.T){
 					func(t *testing.T) { assertEvmMessageReceived(t, ctx, state, destChain, latestHead, message) },
