463a8843e9daea6fe4766668b6f71667ed8462e0
nogo <0xnogo@gmail.com>
2025-06-13 00:29:21 +0700

    AddLaneWithEnforceOutOfOrder

--- integration-tests/smoke/ccip/ccip_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_messaging_test.go
@@ -372,7 +372,7 @@ func Test_CCIPMessaging_EVM2Solana(t *testing.T) {
 		", dest chain selector:", destChain,
 	)
 	// connect a single lane, source to dest
-	testhelpers.AddLaneWithDefaultPricesAndFeeQuoterConfig(t, &e, state, sourceChain, destChain, false)
+	testhelpers.AddLaneWithEnforceOutOfOrder(t, &e, state, sourceChain, destChain, false)
 
 	var (
 		// nonce    uint64 // Nonce not used as Solana check is skipped
