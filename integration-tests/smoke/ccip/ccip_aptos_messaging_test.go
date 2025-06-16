4e21a6af19c39fcdace75947607ff3b07d8804af
joaoluisam <joao.luismoreira@smartcontract.com>
2025-06-16 15:12:38 +0100

    Update out of order execution to false in aptos to 2 evm

--- integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
@@ -412,7 +412,7 @@ func Test_CCIP_Messaging_Aptos2EVM(t *testing.T) {
 				Receiver:       ccipReceiverAddress,
 				MsgData:        message,
 				// Just ensuring enough gas is provided to execute the message, doesn't matter if it's way too much
-				ExtraArgs:              testhelpers.MakeBCSEVMExtraArgsV2(big.NewInt(300000), true),
+				ExtraArgs:              testhelpers.MakeBCSEVMExtraArgsV2(big.NewInt(300000), false),
 				ExpectedExecutionState: testhelpers.EXECUTION_STATE_SUCCESS,
 				ExtraAssertions: []func(t *testing.T){
 					func(t *testing.T) { assertEvmMessageReceived(t, ctx, state, destChain, latestHead, message) },
