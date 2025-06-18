7a73e885edf67eb06d13fbc6830f99fb88ade1d9
dimkouv <dimitrios.kouveris@smartcontract.com>
2025-06-18 15:18:59 +0300

    add test

--- integration-tests/smoke/ccip/ccip_topologies_test.go
+++ integration-tests/smoke/ccip/ccip_topologies_test.go
@@ -2,6 +2,7 @@ package ccip
 
 import (
 	"context"
+	"sort"
 	"sync"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/maps"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_6_0/ccip_home"
 	"github.com/smartcontractkit/chainlink-deployments-framework/chain"
@@ -19,7 +21,7 @@ import (
 	testsetups "github.com/smartcontractkit/chainlink/integration-tests/testsetups/ccip"
 )
 
-func Test_CCIPTopologies_EVM2EVM_RoleDON_AllSupportSource_SomeSupportDest(t *testing.T) {
+func Test_CCIPMessaging_EVM2EVM_RoleDON(t *testing.T) {
 	// Setup 3 chains and a single lane between chains that doesn't
 	// include the home chain.
 	// We need at least 7 nodes to set up a suitable role DON.
@@ -165,6 +167,110 @@ func Test_CCIPTopologies_EVM2EVM_RoleDON_AllSupportSource_SomeSupportDest(t *tes
 	})
 
 	_ = out
+
+	monitorCancel()
+	wg.Wait()
+	// there should be no re-executions.
+	require.Equal(t, int32(0), ms.reExecutionsObserved.Load())
+}
+
+func Test_CCIPTopologies_EVM2EVM_RoleDON_AllSupportSource_SomeSupportDest(t *testing.T) {
+	// fix the chain ids for the test so we can appropriately set finality depth numbers on the destination chain.
+	chains := []chainsel.Chain{
+		chainsel.TEST_90000001,
+		chainsel.TEST_90000002,
+		chainsel.TEST_90000003,
+	}
+	sort.Slice(chains, func(i, j int) bool { return chains[i].Selector < chains[j].Selector })
+	homeChainSel := chains[0].Selector
+	sourceChainSel := chains[1].Selector
+	destChainSel := chains[2].Selector
+
+	const (
+		fRoleDON     = 2
+		nRoleDON     = 3*fRoleDON + 1
+		fChainSource = 2
+		fChainDest   = 1
+	)
+
+	// Setup 2 chains and a single lane.
+	ctx := testhelpers.Context(t)
+	e, _, _ := testsetups.NewIntegrationEnvironment(
+		t,
+		testhelpers.WithNumOfChains(len(chains)),
+		testhelpers.WithNumOfNodes(nRoleDON),
+		testhelpers.WithRoleDONTopology(cciptesthelpertypes.NewRandomTopology(
+			cciptesthelpertypes.RandomTopologyArgs{
+				FChainToNumChains: map[int]int{
+					fChainSource: 1, // 1 chain with fChain fChainSource
+					fChainDest:   1, // 1 chain with fChain fChainDest
+				},
+				Seed: 42, // for reproducible setups.
+			},
+		)),
+	)
+
+	state, err := stateview.LoadOnchainState(e.Env)
+	require.NoError(t, err)
+
+	allChainSelectors := maps.Keys(e.Env.BlockChains.EVMChains())
+	require.Len(t, allChainSelectors, 3)
+
+	require.Contains(t, allChainSelectors, homeChainSel)
+	require.Contains(t, allChainSelectors, sourceChainSel)
+	require.Contains(t, allChainSelectors, destChainSel)
+
+	t.Log("All chain selectors:", allChainSelectors,
+		", home chain selector:", e.HomeChainSel,
+		", feed chain selector:", e.FeedChainSel,
+		", source chain selector:", sourceChainSel,
+		", dest chain selector:", destChainSel,
+	)
+	// connect a single lane, source to dest
+	testhelpers.AddLaneWithDefaultPricesAndFeeQuoterConfig(
+		t, &e, state, sourceChainSel, destChainSel, false)
+
+	var (
+		nonce  uint64
+		sender = common.LeftPadBytes(e.Env.BlockChains.EVMChains()[sourceChainSel].DeployerKey.From.Bytes(), 32)
+		setup  = mt.NewTestSetupWithDeployedEnv(
+			t,
+			e,
+			state,
+			sourceChainSel,
+			destChainSel,
+			sender,
+			false, // testRouter
+		)
+	)
+
+	monitorCtx, monitorCancel := context.WithCancel(ctx)
+	ms := &monitorState{}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		monitorReExecutions(monitorCtx, t, state, destChainSel, ms)
+	}()
+
+	t.Run("data message to eoa", func(t *testing.T) {
+		_ = mt.Run(
+			t,
+			mt.TestCase{
+				ValidationType:         mt.ValidationTypeExec,
+				TestSetup:              setup,
+				Nonce:                  &nonce,
+				Receiver:               common.HexToAddress("0xdead").Bytes(),
+				MsgData:                []byte("hello eoa"),
+				ExtraArgs:              nil,                                 // default extraArgs
+				ExpectedExecutionState: testhelpers.EXECUTION_STATE_SUCCESS, // success because offRamp won't call an EOA
+				ExtraAssertions: []func(t *testing.T){
+					func(t *testing.T) {
+					},
+				},
+			},
+		)
+	})
 
 	monitorCancel()
 	wg.Wait()
