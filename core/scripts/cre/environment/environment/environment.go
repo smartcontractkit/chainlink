b7eebcff5144b2121f7898afc52be06e762180c4
Chris <104409744+vreff@users.noreply.github.com>
2025-06-19 15:00:45 -0400

    Fix lint 2

--- core/scripts/cre/environment/environment/environment.go
+++ core/scripts/cre/environment/environment/environment.go
@@ -182,7 +182,7 @@ var StartCmdGenerateSettingsFile = func(homeChainOut *creenv.BlockchainOutput, o
 	creCLISettingsFile, settingsErr := crecli.PrepareCRECLISettingsFile(
 		crecli.CRECLIProfile,
 		homeChainOut.SethClient.MustGetRootKeyAddress(),
-		output.CldEnvironment.ExistingAddresses, //nolint:staticcheck // won't migrate now
+		output.CldEnvironment.ExistingAddresses,
 		output.DonTopology.WorkflowDonID,
 		homeChainOut.ChainSelector,
 		rpcs,
