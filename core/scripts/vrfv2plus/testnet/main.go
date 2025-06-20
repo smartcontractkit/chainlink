af7c05a67471a48754c93d57784a0944214e3822
YikJiun Lee <yikjiun.lee@smartcontract.com>
2025-06-20 10:47:29 +0800

    set default batch size for batch bhs backwards to 162

--- core/scripts/vrfv2plus/testnet/main.go
+++ core/scripts/vrfv2plus/testnet/main.go
@@ -389,7 +389,7 @@ func main() {
 		bhsAddr := cmd.String("bhs-address", "", "address of the bhs contract")
 		startBlock := cmd.Int64("start-block", -1, "block number to start from. Must be in the BHS already.")
 		endBlock := cmd.Int64("end-block", -1, "block number to end at. Must be less than startBlock")
-		batchSize := cmd.Int64("batch-size", -1, "batch size")
+		batchSize := cmd.Int64("batch-size", 162, "batch size") // the largest safe value that doesn't exceed the 131072 bytes limit, so far.
 		gasMultiplier := cmd.Int64("gas-price-multiplier", 1, "gas price multiplier to use, defaults to 1 (no multiplication)")
 		helpers.ParseArgs(cmd, os.Args[2:], "batch-bhs-address", "bhs-address", "end-block", "batch-size")
 
