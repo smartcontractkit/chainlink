a2cc4bfd7e007c855c4061b171ef547443156a7f
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-06-23 12:53:59 -0400

    Introduce Flaky Tests

--- core/capabilities/flaky_test.go
+++ core/capabilities/flaky_test.go
@@ -3,14 +3,11 @@ package capabilities
 import (
 	"math/rand"
 	"testing"
-
-	"github.com/smartcontractkit/flakeguard"
 )
 
 func TestFlakyOriginallyTenPercent(t *testing.T) {
 	t.Parallel()
-
-	flakeguard.Quarantine(t, "flaky originally ten percent")
+	// flakeguard.Quarantine(t, "flaky originally ten percent")
 
 	if rand.Intn(10) == 0 {
 		t.Log("I flake 10% of the time")
@@ -20,7 +17,7 @@ func TestFlakyOriginallyTenPercent(t *testing.T) {
 
 func TestFlakyOriginallyTwentyFivePercent(t *testing.T) {
 	t.Parallel()
-	flakeguard.Quarantine(t, "flaky originally twenty five percent")
+	// flakeguard.Quarantine(t, "flaky originally twenty five percent")
 
 	if rand.Intn(4) == 0 {
 		t.Log("I flake 25% of the time")
@@ -30,7 +27,7 @@ func TestFlakyOriginallyTwentyFivePercent(t *testing.T) {
 
 func TestFlakyOriginallyFiftyPercent(t *testing.T) {
 	t.Parallel()
-	flakeguard.Quarantine(t, "flaky originally fifty percent")
+	// flakeguard.Quarantine(t, "flaky originally fifty percent")
 
 	if rand.Intn(2) == 0 {
 		t.Log("I flake 50% of the time")
@@ -40,16 +37,20 @@ func TestFlakyOriginallyFiftyPercent(t *testing.T) {
 
 func TestFlakyOriginallySeventyFivePercent(t *testing.T) {
 	t.Parallel()
-	flakeguard.Quarantine(t, "flaky originally seventy five percent")
+	// flakeguard.Quarantine(t, "flaky originally seventy five percent")
+
 	if rand.Intn(4) != 0 {
 		t.Log("I flake 75% of the time")
 		t.FailNow()
 	}
 }
 
-func TestPassOriginally(t *testing.T) {
+func TestFlakyOriginallyNinetyPercent(t *testing.T) {
 	t.Parallel()
+	// flakeguard.Quarantine(t, "flaky originally ninety percent")
 
-	flakeguard.Quarantine(t, "pass originally")
-	t.Log("I pass")
+	if rand.Intn(10) != 0 {
+		t.Log("I flake 90% of the time")
+		t.FailNow()
+	}
 }
