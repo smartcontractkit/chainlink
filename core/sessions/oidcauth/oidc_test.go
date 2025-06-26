b18414301d928557cb56bf67e22e651c2167e581
Harry Anderson <harry.anderson@smartcontract.com>
2025-06-26 16:37:43 +1000

    test: stup test

---
+++ core/sessions/oidcauth/oidc_test.go
@@ -0,0 +1,45 @@
+package oidcauth_test
+
+import (
+	// "errors"
+	// "fmt"
+	"testing"
+	// "time"
+
+	// "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/mock"
+	// "github.com/stretchr/testify/require"
+
+	"github.com/jmoiron/sqlx"
+
+	// "github.com/smartcontractkit/chainlink/v2/core/internal/cltest"
+	// "github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/pgtest"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
+	"github.com/smartcontractkit/chainlink/v2/core/logger/audit"
+	"github.com/smartcontractkit/chainlink/v2/core/sessions"
+	"github.com/smartcontractkit/chainlink/v2/core/sessions/oidcauth"
+	// "github.com/smartcontractkit/chainlink/v2/core/sessions/oidc/mocks"
+)
+
+// Setup oidc Auth authenticator
+func setupAuthenticationProvider(t *testing.T) (*sqlx.DB, sessions.AuthenticationProvider) {
+	t.Helper()
+
+	cfg := oidcauth.TestConfig{}
+	db := pgtest.NewSqlxDB(t)
+	oidcAuthProvider, err := oidcauth.NewTestOIDCAuthenticator(db, &cfg, logger.TestLogger(t), &audit.AuditLoggerService{})
+	if err != nil {
+		t.Fatalf("Error constructing NewTestoidcAuthenticator: %v\n", err)
+	}
+
+	return db, oidcAuthProvider
+}
+
+func TestORM_FindUser_Empty(t *testing.T) {
+	t.Parallel()
+	// ctx := testutils.Context(t)
+	// Init OIDC authenticator
+	_, oidcAuthProvider := setupAuthenticationProvider(t)
+	println("%v", oidcAuthProvider)
+}
