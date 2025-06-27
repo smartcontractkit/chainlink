d0bb4e9698334ecd57c73f97e77b557fdd367d72
Mateusz Sekara <mateusz.sekara@gmail.com>
2025-06-27 14:21:44 +0200

    Passing chain family to contract reader metrics

--- core/capabilities/ccip/ccip_integration_tests/integrationhelpers/integration_helpers.go
+++ core/capabilities/ccip/ccip_integration_tests/integrationhelpers/integration_helpers.go
@@ -19,6 +19,8 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient/simulated"
 	"github.com/stretchr/testify/require"
 
+	chainsel "github.com/smartcontractkit/chain-selectors"
+
 	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	ccipreader "github.com/smartcontractkit/chainlink-ccip/pkg/reader"
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
@@ -139,7 +141,7 @@ func NewTestUniverse(ctx context.Context, t *testing.T, lggr logger.Logger) Test
 	t.Cleanup(func() { require.NoError(t, lp.Close()) })
 
 	cr := NewReader(t, lp, headTracker, cl, ccAddress, configsevm.HomeChainReaderConfigRaw)
-	hcr := NewHomeChainReader(t, cr, ccAddress, "evm", strconv.Itoa(chainID))
+	hcr := NewHomeChainReader(t, cr, ccAddress, chainsel.FamilyEVM, strconv.Itoa(chainID))
 	return TestUniverse{
 		Transactor:         transactor,
 		Backend:            backend,
