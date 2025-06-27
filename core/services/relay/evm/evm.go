234824c5353c59f03182f42972184a016097ba26
Akhil Chainani <akhil.chainani@smartcontract.com>
2025-06-27 12:04:09 -0400

    revert relayer changes

--- core/services/relay/evm/evm.go
+++ core/services/relay/evm/evm.go
@@ -1004,16 +1004,8 @@ func (r *Relayer) EVM() (commontypes.EVMService, error) {
 	return r, nil
 }
 
-func (r *Relayer) CalculateTransactionFee(ctx context.Context, receipt evm.ReceiptGasInfo) (*evm.TransactionFee, error) {
-	return nil, nil
-}
-
 func (r *Relayer) IsTxFinalized(ctx context.Context, hash evm.Hash, timeout time.Duration) (bool, error) {
 	return false, nil
-}
-
-func (r *Relayer) SubmitTransaction(ctx context.Context, tx evm.SubmitTransactionRequest) (*evm.TransactionResult, error) {
-	return nil, nil
 }
 
 func (r *Relayer) NewMedianProvider(ctx context.Context, rargs commontypes.RelayArgs, pargs commontypes.PluginArgs) (commontypes.MedianProvider, error) {
