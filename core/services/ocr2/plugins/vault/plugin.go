34d343db708c1ce0b6b4d4ab153295a34a891f59
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2025-06-30 17:21:40 +0100

    [PRIV-78] Wire up OCR instance

---
+++ core/services/ocr2/plugins/vault/plugin.go
@@ -0,0 +1,22 @@
+package vault
+
+import (
+	"context"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/requests"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
+)
+
+func NewReportingPluginFactory(store *requests.Store[*Request]) *ReportingPluginFactory {
+	return &ReportingPluginFactory{
+		store: store,
+	}
+}
+
+type ReportingPluginFactory struct {
+	store *requests.Store[*Request]
+}
+
+func (r *ReportingPluginFactory) NewReportingPlugin(ctx context.Context, config ocr3types.ReportingPluginConfig) (ocr3types.ReportingPlugin[[]byte], ocr3types.ReportingPluginInfo, error) {
+	return nil, ocr3types.ReportingPluginInfo{}, nil
+}
