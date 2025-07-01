c93dec08101cb93341c2d2dd264b0cb1798add2d
joaoluisam <joao.luismoreira@smartcontract.com>
2025-07-01 10:24:08 +0100

    try remove max gas limit args

--- integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
@@ -188,7 +188,8 @@ func Test_CCIP_Messaging_EVM2Aptos(t *testing.T) {
 				Receiver:       ccipChainState.ReceiverAddress[:],
 				MsgData:        message,
 				// true for out of order execution, which is necessary and enforced for Aptos
-				ExtraArgs:              testhelpers.MakeEVMExtraArgsV2(uint64(srcFeeQuoterDestChainConfig.MaxPerMsgGasLimit-300), true),
+				// ExtraArgs:              testhelpers.MakeEVMExtraArgsV2(uint64(srcFeeQuoterDestChainConfig.MaxPerMsgGasLimit-300), true),
+				ExtraArgs:              testhelpers.MakeEVMExtraArgsV2(100000, true),
 				ExpectedExecutionState: testhelpers.EXECUTION_STATE_SUCCESS,
 				FeeToken:               nativeFeeToken,
 				ExtraAssertions: []func(t *testing.T){
