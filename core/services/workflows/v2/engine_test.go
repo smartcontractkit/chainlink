95c9def58b16b13c79a264a6a5fdef38d4ca7da7
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2025-07-01 17:43:21 +0100

    [PRIV-97] Vault message refactor

--- core/services/workflows/v2/engine_test.go
+++ core/services/workflows/v2/engine_test.go
@@ -18,6 +18,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	ragetypes "github.com/smartcontractkit/libocr/ragep2p/types"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/beholder/beholdertest"
