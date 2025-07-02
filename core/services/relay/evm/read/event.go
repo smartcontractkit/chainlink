d9f7211c2edcaa1dfdda8173179ac923ff5a4cb5
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2025-07-02 00:56:21 -0400

    rm unused

--- core/services/relay/evm/read/event.go
+++ core/services/relay/evm/read/event.go
@@ -2,9 +2,7 @@ package read
 
 import (
 	"context"
-	"crypto/sha3"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"reflect"
 	"strconv"
@@ -400,29 +398,6 @@ func (b *EventBinding) getLatestLog(ctx context.Context, address common.Address,
 	}
 
 	return &logs[0], err
-}
-
-func (b *EventBinding) deriveName() string {
-	filter := b.registrar.filter
-	s := struct {
-		Addresses evmtypes.AddressArray
-		EventSigs evmtypes.HashArray // list of possible values for eventsig (aka topic1)
-		Topic2    evmtypes.HashArray // list of possible values for topic2
-		Topic3    evmtypes.HashArray // list of possible values for topic3
-		Topic4    evmtypes.HashArray // list of possible values for topic4
-	}{
-		filter.Addresses,
-		filter.EventSigs,
-		filter.Topic2,
-		filter.Topic3,
-		filter.Topic4,
-	}
-
-	data, _ := json.Marshal(s) // the structure is json-safe, fine to ignore
-
-	hash := sha3.Sum256(data)
-
-	return fmt.Sprintf("%s.%s.%x", b.contractName, b.eventName, hash[:])
 }
 
 func (b *EventBinding) decodeLogsIntoSequences(ctx context.Context, logs []logpoller.Log, into any) ([]commontypes.Sequence, error) {
