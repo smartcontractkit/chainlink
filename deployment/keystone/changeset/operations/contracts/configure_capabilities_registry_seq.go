65885f99f17e44139751cbce936635ff57050aac
vyzaldysanchez <vyzaldysanchez@gmail.com>
2025-07-02 22:30:25 -0400

    CRE-483 - Fixes lint

--- deployment/keystone/changeset/operations/contracts/configure_capabilities_registry_seq.go
+++ deployment/keystone/changeset/operations/contracts/configure_capabilities_registry_seq.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/ethereum/go-ethereum/common"
-	chainsel "github.com/smartcontractkit/chain-selectors"
-	capabilities_registry "github.com/smartcontractkit/chainlink-evm/gethwrappers/keystone/generated/capabilities_registry_1_1_0"
 
+	chainsel "github.com/smartcontractkit/chain-selectors"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	capabilities_registry "github.com/smartcontractkit/chainlink-evm/gethwrappers/keystone/generated/capabilities_registry_1_1_0"
 
 	"github.com/smartcontractkit/chainlink/deployment/keystone/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/keystone/changeset/internal"
