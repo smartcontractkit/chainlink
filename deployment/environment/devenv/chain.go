a90a8017c4c362a28046fc022a9f90134076bc85
Damjan Smickovski <smickovskid@outlook.com>
2025-07-03 10:51:41 +0200

    Lint

--- deployment/environment/devenv/chain.go
+++ deployment/environment/devenv/chain.go
@@ -26,6 +26,7 @@ import (
 	cldf_chain_utils "github.com/smartcontractkit/chainlink-deployments-framework/chain/utils"
 
 	"github.com/aptos-labs/aptos-go-sdk"
+
 	solCommonUtil "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	cldf_aptos "github.com/smartcontractkit/chainlink-deployments-framework/chain/aptos"
