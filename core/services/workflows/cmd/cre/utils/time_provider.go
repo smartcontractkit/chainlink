e6d120062cef2b99b287fc68d4bcef052e70be8e
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-07-14 13:33:40 -0400

    lint

--- core/services/workflows/cmd/cre/utils/time_provider.go
+++ core/services/workflows/cmd/cre/utils/time_provider.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/smartcontractkit/chainlink/v2/core/services/workflows/v2"
+	v2 "github.com/smartcontractkit/chainlink/v2/core/services/workflows/v2"
 )
 
 type LocalTimeProvider struct{}
