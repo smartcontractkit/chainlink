defd087d323d97e415d249399c854caef41cc3b6
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-07-14 10:49:21 -0400

    Update standalone_engine.go

--- core/services/workflows/cmd/cre/utils/standalone_engine.go
+++ core/services/workflows/cmd/cre/utils/standalone_engine.go
@@ -169,7 +169,7 @@ func NewStandaloneEngine(
 		Request:         &sdkpb.ExecuteRequest_Subscribe{},
 		MaxResponseSize: uint64(cfg.LocalLimits.ModuleExecuteMaxResponseSizeBytes),
 		Config:          config,
-	}, v2.NewDisallowedExecutionHelper(lggr, nil, v2.TimeProvider{}, secretsFetcher))
+	}, v2.NewDisallowedExecutionHelper(lggr, nil, v2.NewTimeProvider(dontimeStore, defaultWorkflowID, lggr), secretsFetcher))
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to execute subscribe: %w", err)
 	}
