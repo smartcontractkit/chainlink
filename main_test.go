a8b8231b857f3606a5bbc32bb03853e47b6feb1b
krehermann <16602512+krehermann@users.noreply.github.com>
2025-07-15 11:30:06 -0600

    fix main test

--- main_test.go
+++ main_test.go
@@ -74,7 +74,7 @@ func TestScripts(t *testing.T) {
 				Dir:             path,
 				Setup:           commonEnv(t),
 				ContinueOnError: true,
-				UpdateScripts:   true, // uncomment to update golden files
+				// UpdateScripts:   true, // uncomment to update golden files
 			})
 		})
 		return nil
