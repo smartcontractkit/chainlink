d8d050e15e86f34296d66cbd3338bc7fa6bfda8c
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-07-30 13:18:49 -0400

    Update time_provider.go

--- core/services/workflows/cmd/cre/utils/time_provider.go
+++ core/services/workflows/cmd/cre/utils/time_provider.go
@@ -3,13 +3,9 @@ package utils
 import (
 	"context"
 	"time"
-
-	v2 "github.com/smartcontractkit/chainlink/v2/core/services/workflows/v2" //nolint:revive // required alias for v2 module
 )
 
 type LocalTimeProvider struct{}
-
-var _ v2.TimeProvider = &LocalTimeProvider{}
 
 func (t *LocalTimeProvider) GetNodeTime() time.Time {
 	return time.Now()
