27b5e47e44a1b9b2fcebeae15b75c9542e7f0f68
Oliver Townsend <oliver.townsend@smartcontract.com>
2025-07-31 21:54:22 -0400

    CCIP - wrap readers used for default accessors in observed and extended reader interfaces

--- core/capabilities/ccip/ccipsolana/chain_accessor_factory.go
+++ core/capabilities/ccip/ccipsolana/chain_accessor_factory.go
@@ -2,7 +2,6 @@ package ccipsolana
 
 import (
 	"github.com/smartcontractkit/chainlink-ccip/pkg/chainaccessor"
-	"github.com/smartcontractkit/chainlink-ccip/pkg/contractreader"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/common"
 )
@@ -14,10 +13,18 @@ type SolanaChainAccessorFactory struct{}
 func (f SolanaChainAccessorFactory) NewChainAccessor(
 	params common.ChainAccessorFactoryParams,
 ) (ccipocr3.ChainAccessor, error) {
+	reader, err := common.WrapContractReaderForDefaultAccessor(
+		params.ContractReader,
+		params.ChainSelector,
+		params.Lggr,
+	)
+	if err != nil {
+		return nil, err
+	}
 	return chainaccessor.NewDefaultAccessor(
 		params.Lggr,
 		params.ChainSelector,
-		contractreader.NewExtendedContractReader(params.ContractReader),
+		reader,
 		params.ContractWriter,
 		params.AddrCodec,
 	)
