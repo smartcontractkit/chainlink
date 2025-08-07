86361d63e07020f4e5b277db79199d4a44ef3553
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2025-08-07 11:01:03 -0700

    [PRODCRE-747] Fix duplicate signer keys in integration testing framework

--- core/capabilities/integration_tests/framework/don.go
+++ core/capabilities/integration_tests/framework/don.go
@@ -147,7 +147,6 @@ func NewDON(ctx context.Context, t *testing.T, lggr logger.Logger, donConfig Don
 	don := &DON{t: t, lggr: logger.Named(lggr, donConfig.name), config: donConfig, capabilitiesRegistry: donContext.capabilityRegistry,
 		workflowRegistry: donContext.workflowRegistry}
 
-	var newOracleFactoryFn standardcapabilities.NewOracleFactoryFn
 	if supportsOCR {
 		// This is required to support the non standard OCR3 capability - will be removed when required OCR3 behaviour is implemented as standard capabilities
 		don.fakeLibOcr = NewFakeLibOCR(t, lggr, donConfig.F, protocolRoundInterval)
@@ -174,6 +173,7 @@ func NewDON(ctx context.Context, t *testing.T, lggr logger.Logger, donConfig Don
 		}
 		don.nodes = append(don.nodes, cn)
 
+		var newOracleFactoryFn standardcapabilities.NewOracleFactoryFn
 		if supportsOCR {
 			factory := newFakeOracleFactoryFactory(t, lggr, donConfig.KeyBundles[i], len(donConfig.Members), donConfig.F,
 				protocolRoundInterval)
