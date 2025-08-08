239322aadb99391ae864b6554fbdbe98849d72de
Geert G <117188496+cll-gg@users.noreply.github.com>
2025-08-08 11:24:57 +0100

    Remove unused code

--- core/capabilities/integration_tests/keystone/contracts_setup.go
+++ core/capabilities/integration_tests/keystone/contracts_setup.go
@@ -1,19 +1,9 @@
 package keystone
 
 import (
-	"bytes"
-	"context"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/cockroachdb/errors"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient/simulated"
 	"github.com/stretchr/testify/require"
 
 	data_feeds_cache "github.com/smartcontractkit/chainlink-evm/gethwrappers/data-feeds/generated/data_feeds_cache"
@@ -77,7 +67,7 @@ func SetupDataFeedsCacheContract(t *testing.T, backend *framework.EthBlockchain,
 	feedIDBytes := [16]byte{}
 	copy(feedIDBytes[:], common.FromHex("0x04de41ba4fc9d91ad900000000000000")) // Data ID for secure mint report for chain selector 16015286601757825753 (ethereum-testnet-sepolia)
 
-	tx, err := dataFeedsCache.SetDecimalFeedConfigs(backend.TransactionOpts(), [][16]byte{feedIDBytes}, []string{"securemint"},
+	_, err = dataFeedsCache.SetDecimalFeedConfigs(backend.TransactionOpts(), [][16]byte{feedIDBytes}, []string{"securemint"},
 		[]data_feeds_cache.DataFeedsCacheWorkflowMetadata{
 			{
 				AllowedSender:        forwarderAddress,
@@ -85,79 +75,8 @@ func SetupDataFeedsCacheContract(t *testing.T, backend *framework.EthBlockchain,
 				AllowedWorkflowName:  nameBytes,
 			},
 		})
-	if err != nil {
-		t.Logf("Failed to set decimal feed configs: %v", err)
-		rpcError := decodeDFCacheError(t, backend, tx)
-		t.Fatalf("failed to set decimal feed configs: %v, rpc error: %s", err, rpcError)
-	}
+	require.NoError(t, err)
 	backend.Commit()
 
 	return addr, dataFeedsCache
-}
-
-func decodeDFCacheError(t *testing.T, backend *framework.EthBlockchain, tx *types.Transaction) string {
-	t.Helper()
-
-	contractAbi, txError := data_feeds_cache.DataFeedsCacheMetaData.GetAbi()
-	require.NoError(t, txError)
-
-	errString := "0xd86ad9cf0000000000000000000000000acba7ba442154d7b1d11ffa5ecb40e9457fca05"
-
-	errString = strings.TrimPrefix(errString, "0x")
-	errBytes, err := hex.DecodeString(errString)
-	require.NoError(t, err)
-
-	selector := errBytes[:4]
-	for name, errType := range contractAbi.Errors {
-		if bytes.Equal(selector, errType.ID.Bytes()) {
-			args, err := errType.Inputs.Unpack(errBytes[4:])
-			require.NoError(t, err)
-			return fmt.Sprintf("Matched error: %s, args: %v", name, args)
-		}
-
-	}
-
-	return fmt.Sprintf("No custom DF Cache error found for %v.", txError)
-}
-
-func getTxError(t *testing.T, client simulated.Client, from common.Address, tx *types.Transaction) (string, error) {
-	t.Helper()
-
-	re, err := client.TransactionReceipt(t.Context(), tx.Hash())
-	require.NoError(t, err, "error getting transaction receipt")
-	if err != nil {
-		return "", errors.Wrap(err, "error getting transaction receipt")
-	}
-
-	call := ethereum.CallMsg{
-		From:     from,
-		To:       tx.To(),
-		Data:     tx.Data(),
-		Value:    tx.Value(),
-		Gas:      tx.Gas(),
-		GasPrice: tx.GasPrice(),
-	}
-	_, err = client.CallContract(context.Background(), call, re.BlockNumber)
-	if err == nil {
-		panic("no error calling contract")
-	}
-
-	return parseError(err)
-}
-
-func parseError(txError error) (string, error) {
-	b, err := json.Marshal(txError)
-	if err != nil {
-		return "", err
-	}
-	var callErr struct {
-		Code    int
-		Data    string `json:"data"`
-		Message string `json:"message"`
-	}
-	if json.Unmarshal(b, &callErr) != nil {
-		return "", err
-	}
-
-	return callErr.Data, nil
 }
