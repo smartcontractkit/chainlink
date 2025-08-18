4b684ffa531841c2d21a9e6a05d0e959fd4d782c
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-08-18 14:40:38 +0100

    use qualifier

--- deployment/keystone/changeset/operations/contracts/deploy_forwarder_op.go
+++ deployment/keystone/changeset/operations/contracts/deploy_forwarder_op.go
@@ -30,7 +30,7 @@ var DeployKeystoneForwarderOp = operations.NewOperation[DeployForwarderOpInput, 
 	semver.MustParse("1.0.0"),
 	"Deploy KeystoneForwarder Contract",
 	func(b operations.Bundle, deps DeployForwarderOpDeps, input DeployForwarderOpInput) (DeployForwarderOpOutput, error) {
-		forwarderOutput, err := changeset.DeployForwarder(*deps.Env, changeset.DeployForwarderRequest{ChainSelectors: []uint64{input.ChainSelector}})
+		forwarderOutput, err := changeset.DeployForwarder(*deps.Env, changeset.DeployForwarderRequest{ChainSelectors: []uint64{input.ChainSelector}, Qualifier: input.Qualifier})
 		if err != nil {
 			return DeployForwarderOpOutput{}, err
 		}
