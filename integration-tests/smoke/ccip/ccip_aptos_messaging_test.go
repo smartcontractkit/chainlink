8339591bedbfe0a9d4f2441a01915c31519be14a
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-08-20 13:07:32 +0100

    chore: test

--- integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
+++ integration-tests/smoke/ccip/ccip_aptos_messaging_test.go
@@ -41,7 +41,8 @@ func Test_CCIP_Messaging_EVM2Aptos(t *testing.T) {
 	lggr := logger.TestLogger(t)
 	e, _, _ := testsetups.NewIntegrationEnvironment(
 		t,
-		testhelpers.WithNumOfChains(2),
+		testhelpers.WithNumOfChains(1),
+		testhelpers.WithChainIDs([]uint64{4}),
 		testhelpers.WithAptosChains(1),
 	)
 
