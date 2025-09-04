e2502da0a7525e122b79ab85180296e7c74d2882
vyzaldysanchez <vyzaldysanchez@gmail.com>
2025-09-03 21:47:10 -0400

    Fixes lint

--- deployment/cre/jobs/types/job_spec.go
+++ deployment/cre/jobs/types/job_spec.go
@@ -9,30 +9,23 @@ import (
 type JobSpecInput map[string]interface{}
 
 func (j JobSpecInput) ToStandardCapabilityJob(jobName string) (pkg.StandardCapabilityJob, error) {
-	var cmd, config, externalJobID string
-	var oracleFactory pkg.OracleFactory
-
-	if c, ok := j["command"].(string); ok {
-		cmd = c
-	} else {
+	cmd, ok := j["command"].(string)
+	if !ok || cmd == "" {
 		return pkg.StandardCapabilityJob{}, errors.New("command is required and must be a string")
 	}
 
-	if c, ok := j["config"].(string); ok {
-		config = c
-	} else {
+	config, ok := j["config"].(string)
+	if !ok || config == "" {
 		return pkg.StandardCapabilityJob{}, errors.New("config is required and must be a string")
 	}
 
-	if ej, ok := j["externalJobID"].(string); ok {
-		externalJobID = ej
-	} else {
+	externalJobID, ok := j["externalJobID"].(string)
+	if !ok || externalJobID == "" {
 		return pkg.StandardCapabilityJob{}, errors.New("externalJobID is required and must be a string")
 	}
 
-	if of, ok := j["oracleFactory"].(pkg.OracleFactory); ok {
-		oracleFactory = of
-	} else {
+	oracleFactory, ok := j["oracleFactory"].(pkg.OracleFactory)
+	if !ok {
 		return pkg.StandardCapabilityJob{}, errors.New("oracleFactory is required and must be of type OracleFactory")
 	}
 
