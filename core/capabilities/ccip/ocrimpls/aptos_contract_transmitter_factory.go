0a6a0285f8d1b4d86752857b92cf4087a1356c37
Joe Huang <joe.huang@smartcontract.com>
2025-09-03 23:23:25 -0300

    golint

--- core/capabilities/ccip/ocrimpls/aptos_contract_transmitter_factory.go
+++ core/capabilities/ccip/ocrimpls/aptos_contract_transmitter_factory.go
@@ -1,9 +1,10 @@
 package ocrimpls
 
 import (
-	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
--- core/capabilities/ccip/ocrimpls/svm_contract_transmitter_factory.go
+++ core/capabilities/ccip/ocrimpls/svm_contract_transmitter_factory.go
@@ -3,9 +3,10 @@ package ocrimpls
 import (
 	"fmt"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
