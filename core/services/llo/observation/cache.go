4ebf895a4ea2ec31f60d98ccc6f627010c533551
Ivaylo Novakov <ivaylo.novakov@smartcontract.com>
2025-09-04 14:33:45 +0200

    Revert "Reapply "DS-631 Observation loop (#18843)" (#19079) (#19086)"
    This reverts commit 0b1cc20bde43e6d55b9e2f13f366256316293926.

--- core/services/llo/observation/cache.go
+++ core/services/llo/observation/cache.go
@@ -4,23 +4,28 @@ import (
 	"runtime"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	ocr2types "github.com/smartcontractkit/libocr/offchainreporting2/types"
 
 	llotypes "github.com/smartcontractkit/chainlink-common/pkg/types/llo"
 	"github.com/smartcontractkit/chainlink-data-streams/llo"
 )
 
 var (
+	registryMu = sync.Mutex{}
+	registry   = map[ocr2types.ConfigDigest]*Cache{}
+
 	promCacheHitCount = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "llo",
 		Subsystem: "datasource",
 		Name:      "cache_hit_count",
 		Help:      "Number of local observation cache hits",
 	},
-		[]string{"streamID"},
+		[]string{"configDigest", "streamID"},
 	)
 	promCacheMissCount = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "llo",
@@ -28,9 +33,22 @@ var (
 		Name:      "cache_miss_count",
 		Help:      "Number of local observation cache misses",
 	},
-		[]string{"streamID", "reason"},
+		[]string{"configDigest", "streamID", "reason"},
 	)
 )
+
+func GetCache(configDigest ocr2types.ConfigDigest) *Cache {
+	registryMu.Lock()
+	defer registryMu.Unlock()
+
+	cache, ok := registry[configDigest]
+	if !ok {
+		cache = NewCache(configDigest, 500*time.Millisecond, time.Minute)
+		registry[configDigest] = cache
+	}
+
+	return cache
+}
 
 // Cache of stream values.
 // It maintains a cache of stream values fetched from adapters until the last
@@ -40,16 +58,20 @@ var (
 // The cache is cleaned up periodically to remove decommissioned stream values
 // if the provided cleanupInterval is greater than 0.
 type Cache struct {
-	mu              sync.RWMutex
+	mu sync.RWMutex
+
+	configDigestStr string
 	values          map[llotypes.StreamID]item
 	maxAge          time.Duration
 	cleanupInterval time.Duration
 
-	closeChan chan struct{}
+	lastTransmissionSeqNr atomic.Uint64
+	closeChan             chan struct{}
 }
 
 type item struct {
 	value     llo.StreamValue
+	seqNr     uint64
 	createdAt time.Time
 }
 
@@ -57,8 +79,9 @@ type item struct {
 //
 // maxAge is the maximum age of a stream value to keep in the cache.
 // cleanupInterval is the interval to clean up the cache.
-func NewCache(maxAge time.Duration, cleanupInterval time.Duration) *Cache {
+func NewCache(configDigest ocr2types.ConfigDigest, maxAge time.Duration, cleanupInterval time.Duration) *Cache {
 	c := &Cache{
+		configDigestStr: configDigest.Hex(),
 		values:          make(map[llotypes.StreamID]item),
 		maxAge:          maxAge,
 		cleanupInterval: cleanupInterval,
@@ -87,11 +110,16 @@ func NewCache(maxAge time.Duration, cleanupInterval time.Duration) *Cache {
 	return c
 }
 
+// SetLastTransmissionSeqNr sets the last transmission sequence number.
+func (c *Cache) SetLastTransmissionSeqNr(seqNr uint64) {
+	c.lastTransmissionSeqNr.Store(seqNr)
+}
+
 // Add adds a stream value to the cache.
-func (c *Cache) Add(id llotypes.StreamID, value llo.StreamValue) {
+func (c *Cache) Add(id llotypes.StreamID, value llo.StreamValue, seqNr uint64) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.values[id] = item{value: value, createdAt: time.Now()}
+	c.values[id] = item{value: value, seqNr: seqNr, createdAt: time.Now()}
 }
 
 func (c *Cache) Get(id llotypes.StreamID) (llo.StreamValue, bool) {
@@ -101,16 +129,21 @@ func (c *Cache) Get(id llotypes.StreamID) (llo.StreamValue, bool) {
 	label := strconv.FormatUint(uint64(id), 10)
 	item, ok := c.values[id]
 	if !ok {
-		promCacheMissCount.WithLabelValues(label, "notFound").Inc()
+		promCacheMissCount.WithLabelValues(c.configDigestStr, label, "notFound").Inc()
+		return nil, false
+	}
+
+	if item.seqNr <= c.lastTransmissionSeqNr.Load() {
+		promCacheMissCount.WithLabelValues(c.configDigestStr, label, "seqNr").Inc()
 		return nil, false
 	}
 
 	if time.Since(item.createdAt) >= c.maxAge {
-		promCacheMissCount.WithLabelValues(label, "maxAge").Inc()
+		promCacheMissCount.WithLabelValues(c.configDigestStr, label, "maxAge").Inc()
 		return nil, false
 	}
 
-	promCacheHitCount.WithLabelValues(label).Inc()
+	promCacheHitCount.WithLabelValues(c.configDigestStr, label).Inc()
 	return item.value, true
 }
 
@@ -118,8 +151,9 @@ func (c *Cache) cleanup() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
+	lastTransmissionSeqNr := c.lastTransmissionSeqNr.Load()
 	for id, item := range c.values {
-		if time.Since(item.createdAt) >= c.maxAge {
+		if item.seqNr <= lastTransmissionSeqNr || time.Since(item.createdAt) >= c.maxAge {
 			delete(c.values, id)
 		}
 	}
