b34909f2027c9d4710ec3af005e1a1aaa61ef33f
amit-momin <amit.momin@smartcontract.com>
2025-09-10 17:31:03 -0500

    Enabled ccip provider support flag for solana

--- core/capabilities/ccip/ccipsolana/pluginconfig.go
+++ core/capabilities/ccip/ccipsolana/pluginconfig.go
@@ -25,6 +25,7 @@ func InitializePluginConfig(lggr logger.Logger, extraDataCodec ccipocr3.ExtraDat
 		ChainRW:                    ChainRWProvider{},
 		ExtraDataCodec:             ExtraDataDecoder{},
 		PriceOnlyCommitFn:          consts.MethodCommitPriceOnly,
+		CCIPProviderSupported:      true,
 	}
 }
 
