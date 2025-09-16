95e5f7c40db5066c1d7c348c1d23164ffb9df17d
Agustina Aldasoro <agustina.aldasoro@smartcontract.com>
2025-09-16 19:17:50 -0300

    Bump versions for testing upgrade

--- deployment/ccip/changeset/solana_v0_1_1/cs_deploy_chain_test.go
+++ deployment/ccip/changeset/solana_v0_1_1/cs_deploy_chain_test.go
@@ -183,7 +183,7 @@ func TestUpgrade(t *testing.T) {
 	solChainSelectors := e.BlockChains.ListChainSelectors(cldf_chain.WithFamily(chain_selectors.FamilySolana))
 	e, _, err := commonchangeset.ApplyChangesets(t, e, initialDeployCS(t, e,
 		&ccipChangesetSolana.BuildSolanaConfig{
-			SolanaContractVersion: ccipChangesetSolana.VersionSolanaV0_1_0,
+			SolanaContractVersion: ccipChangesetSolana.VersionSolanaV0_1_1TokenPools,
 			DestinationDir:        e.BlockChains.SolanaChains()[solChainSelectors[0]].ProgramsPath,
 			LocalBuild: ccipChangesetSolana.LocalBuildConfig{
 				BuildLocally:        true,
@@ -321,7 +321,7 @@ func TestUpgrade(t *testing.T) {
 					},
 				},
 				BuildConfig: &ccipChangesetSolana.BuildSolanaConfig{
-					SolanaContractVersion: ccipChangesetSolana.VersionSolanaV0_1_1,
+					SolanaContractVersion: ccipChangesetSolana.VersionSolanaV0_1_2,
 					DestinationDir:        e.BlockChains.SolanaChains()[solChainSelectors[0]].ProgramsPath,
 					LocalBuild: ccipChangesetSolana.LocalBuildConfig{
 						BuildLocally: true,
