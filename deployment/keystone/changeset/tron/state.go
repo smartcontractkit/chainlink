cfb1415ceb981c978ae4ec3cb0f9c4924954058e
Silas Lenihan <sjl@lenihan.net>
2025-09-19 09:26:03 -0400

    fixed test

--- deployment/keystone/changeset/tron/state.go
+++ deployment/keystone/changeset/tron/state.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/fbsobreira/gotron-sdk/pkg/address"
 
+	chain_selectors "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
@@ -40,8 +41,17 @@ func LoadTronContractsState(logger logger.Logger, addresses []datastore.AddressR
 
 	for _, addr := range addresses {
 		if addr.Type == ForwarderContract {
-			// Convert from Ethereum format back to Tron address
-			forwarderAddress := address.EVMAddressToAddress(common.HexToAddress(addr.Address))
+			var forwarderAddress address.Address
+			isEvm, _ := chain_selectors.IsEvm(addr.ChainSelector)
+			if isEvm {
+				forwarderAddress = address.EVMAddressToAddress(common.HexToAddress(addr.Address))
+			} else {
+				var err error
+				forwarderAddress, err = address.Base58ToAddress(addr.Address)
+				if err != nil {
+					return nil, err
+				}
+			}
 			state.Forwarder = forwarderAddress
 
 			return &state, nil
