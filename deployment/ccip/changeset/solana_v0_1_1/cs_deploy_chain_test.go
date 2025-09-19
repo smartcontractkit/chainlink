5a14c9f74e44fd0fab083604f84e54db99c0da91
PabloMansanet <pablo.mansanet@smartcontract.com>
2025-09-19 12:32:16 +0200

    Revert change to CS deploy chain test

--- deployment/ccip/changeset/solana_v0_1_1/cs_deploy_chain_test.go
+++ deployment/ccip/changeset/solana_v0_1_1/cs_deploy_chain_test.go
@@ -522,7 +522,7 @@ func TestIDL(t *testing.T) {
 	// Test transferring ownership of the IDL back to the deployer key and then close and recreate the PDA
 	e, _, err = commonchangeset.ApplyChangesets(t, e, []commonchangeset.ConfiguredChangeSet{
 		commonchangeset.Configure(
-			cldf.CreateLegacyChangeSet(ccipChangesetSolana.SetAuthorityIDL),
+			cldf.CreateLegacyChangeSet(ccipChangesetSolana.SetAuthorityIDLByMCMs),
 			ccipChangesetSolana.IDLConfig{
 				ChainSelector: solChain,
 				BurnMintTokenPoolMetadata: []string{
