9b1bc7787173bd3cc49de8cac539cdc60a4cf679
Alexandr Yepishev <alexandr.yepishev@smartcontract.com>
2025-09-21 12:04:22 +0100

    Remove regression test from smoke tests

--- system-tests/tests/smoke/cre/v2_cron_beholder_test.go
+++ system-tests/tests/smoke/cre/v2_cron_beholder_test.go
@@ -31,35 +31,3 @@ func ExecuteCronBeholderTest(t *testing.T, testEnv *TestEnvironment) {
 	require.NoError(t, err, "Cron (Beholder) test failed")
 	testLogger.Info().Msg("Cron (Beholder) test completed")
 }
-
-// regression
-var cronInvalidSchedulesTests = []struct {
-	name            string
-	invalidSchedule string
-}{
-	{"below default limit (30s)", "*/29 * * * * *"},
-	{"negative", "*/-1 * * * * *"},
-	{"inappropriately formatted", "*MON/1 * * * * *"},
-}
-
-func CronBeholderFailWithInvalidScheduleTest(t *testing.T, testEnv *TestEnvironment, invalidSchedule string) {
-	testLogger := framework.L
-	workflowFileLocation := "../../../../core/scripts/cre/environment/examples/workflows/v2/cron/main.go"
-	workflowName := "cronbeholder"
-
-	listenerCtx, messageChan, kafkaErrChan := startBeholder(t, testLogger, testEnv)
-
-	testLogger.Info().Msg("Creating Cron workflow configuration file...")
-	workflowConfig := crontypes.WorkflowConfig{
-		Schedule: invalidSchedule,
-	}
-	compileAndDeployWorkflow(t, testEnv, testLogger, workflowName, &workflowConfig, workflowFileLocation)
-
-	testLogger.Warn().Msgf("Expecting Cron workflow to fail with invalid schedule: %s", invalidSchedule)
-	expectedBeholderLog := "expecting the error from a Beholder messages test validator" // no empty string, because it may match a message
-	timeout := 1 * time.Minute
-	expectedError := assertBeholderMessage(listenerCtx, t, expectedBeholderLog, testLogger, messageChan, kafkaErrChan, timeout)
-	require.Error(t, expectedError, "Cron (Beholder) test failed. This test expects to fail with an error, but did not.")
-
-	testLogger.Info().Msg("Cron (Beholder) fail test completed")
-}
