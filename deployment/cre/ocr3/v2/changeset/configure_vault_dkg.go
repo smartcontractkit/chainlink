6349e9b7615323c276bef03cfbb0797f53b744ab
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2025-09-29 19:49:37 +0100

    [PRIV-188] Configure Vault contracts

---
+++ deployment/cre/ocr3/v2/changeset/configure_vault_dkg.go
@@ -0,0 +1,115 @@
+package changeset
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/smdkg/dkgocr/dkgocrtypes"
+
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/chainlink/deployment/cre/jobs/pkg"
+	"github.com/smartcontractkit/chainlink/deployment/cre/ocr3"
+	"github.com/smartcontractkit/chainlink/deployment/cre/ocr3/v2/changeset/operations/contracts"
+)
+
+var _ cldf.ChangeSetV2[ConfigureVaultDKGInput] = ConfigureVaultDKG{}
+
+type ConfigureVaultDKGInput struct {
+	ContractChainSelector uint64 `json:"contract_chain_selector" yaml:"contract_chain_selector"`
+	ContractQualifier     string `json:"contract_qualifier" yaml:"contract_qualifier"`
+
+	DON          DKGDon             `json:"don" yaml:"don"`
+	OracleConfig *ocr3.OracleConfig `json:"oracle_config" yaml:"oracle_config"`
+	DryRun       bool               `json:"dry_run" yaml:"dry_run"`
+
+	MCMSConfig *ocr3.MCMSConfig `json:"mcms_config" yaml:"mcms_config"`
+}
+
+type DKGDon struct {
+	contracts.DonNodeSet
+	RecipientPublicKeys []string `json:"recipient_public_keys" yaml:"recipient_public_keys"`
+}
+
+type ConfigureVaultDKG struct{}
+
+func (l ConfigureVaultDKG) VerifyPreconditions(_ cldf.Environment, input ConfigureVaultDKGInput) error {
+	if input.ContractChainSelector == 0 {
+		return errors.New("contract chain selector is required")
+	}
+	if input.ContractQualifier == "" {
+		return errors.New("contract qualifier is required")
+	}
+	if input.DON.Name == "" {
+		return errors.New("don name is required")
+	}
+	if len(input.DON.NodeIDs) == 0 {
+		return errors.New("at least one don node ID is required")
+	}
+	if len(input.DON.RecipientPublicKeys) == 0 {
+		return errors.New("at least one recipient public key is required")
+	}
+	if len(input.DON.NodeIDs) != len(input.DON.RecipientPublicKeys) {
+		return errors.New("the number of don node IDs must match the number of recipient public keys")
+	}
+	if input.OracleConfig == nil {
+		return errors.New("oracle config is required")
+	}
+	return nil
+}
+
+func (l ConfigureVaultDKG) Apply(e cldf.Environment, input ConfigureVaultDKGInput) (cldf.ChangesetOutput, error) {
+	e.Logger.Infow("Configuring Vault DKG contract with DON", "donName", input.DON.Name, "nodes", input.DON.NodeIDs, "dryRun", input.DryRun)
+
+	contractRefKey := pkg.GetOCR3CapabilityAddressRefKey(input.ContractChainSelector, input.ContractQualifier)
+	contractAddrRef, err := e.DataStore.Addresses().Get(contractRefKey)
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to get OCR3 contract address for chain selector %d and qualifier %s: %w", input.ContractChainSelector, input.ContractQualifier, err)
+	}
+	contractAddr := common.HexToAddress(contractAddrRef.Address)
+	cfg, err := dkgReportingPluginConfig(input.DON, input.OracleConfig.MaxFaultyOracles+1) // validate config can be created
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to create DKG reporting plugin config: %w", err)
+	}
+	report, err := operations.ExecuteOperation(e.OperationsBundle, contracts.ConfigureDKG, contracts.ConfigureDKGDeps{
+		WriteGeneratedConfig: io.Discard,
+		Env:                  &e,
+	}, contracts.ConfigureDKGInput{
+		ContractAddress:       &contractAddr,
+		ChainSelector:         input.ContractChainSelector,
+		DON:                   input.DON.DonNodeSet,
+		Config:                input.OracleConfig,
+		DryRun:                input.DryRun,
+		MCMSConfig:            input.MCMSConfig,
+		ReportingPluginConfig: cfg,
+	})
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to configure OCR3 contract: %w", err)
+	}
+
+	return cldf.ChangesetOutput{
+		MCMSTimelockProposals: report.Output.MCMSTimelockProposals,
+		Reports:               []operations.Report[any, any]{report.ToGenericReport()},
+	}, nil
+}
+
+func dkgReportingPluginConfig(don DKGDon, threshold int) (dkgocrtypes.ReportingPluginConfig, error) {
+	keys := []dkgocrtypes.P256ParticipantPublicKey{}
+	for _, k := range don.RecipientPublicKeys {
+		bk, err := hex.DecodeString(k)
+		if err != nil {
+			return dkgocrtypes.ReportingPluginConfig{}, fmt.Errorf("failed to decode recipient public key %s: %w", k, err)
+		}
+		keys = append(keys, dkgocrtypes.P256ParticipantPublicKey(bk))
+	}
+	cfg := dkgocrtypes.ReportingPluginConfig{
+		T:                   threshold,
+		DealerPublicKeys:    keys,
+		RecipientPublicKeys: keys,
+	}
+
+	return cfg, nil
+}
