b19140364834ccfb4cdbaeef795654362fda3d99
vyzaldysanchez <vyzaldysanchez@gmail.com>
2025-10-02 16:30:08 -0400

    Improvements

--- deployment/cre/workflow_registry/v2/changeset/operations/contracts/admin_workflow_registry_ops.go
+++ deployment/cre/workflow_registry/v2/changeset/operations/contracts/admin_workflow_registry_ops.go
@@ -164,6 +164,7 @@ type AdminPauseAllByOwnerOpInput struct {
 	ChainSelector uint64           `json:"chainSelector"`
 	Qualifier     string           `json:"qualifier"` // Qualifier to identify the specific workflow registry
 	Owner         common.Address   `json:"owner"`
+	Limit         *big.Int         `json:"limit"`
 	MCMSConfig    *ocr3.MCMSConfig `json:"mcmsConfig,omitempty"`
 }
 
@@ -202,7 +203,7 @@ var AdminPauseAllByOwnerOp = operations.NewOperation(
 
 		// Execute the transaction using the strategy
 		proposals, err := strategy.Apply(func(opts *bind.TransactOpts) (*types.Transaction, error) {
-			tx, err := registry.AdminPauseAllByOwner(opts, input.Owner, big.NewInt(500))
+			tx, err := registry.AdminPauseAllByOwner(opts, input.Owner, input.Limit)
 			if err != nil {
 				return nil, fmt.Errorf("failed to call AdminPauseAllByOwner: %w", err)
 			}
@@ -230,6 +231,7 @@ type AdminPauseAllByDONOpInput struct {
 	ChainSelector uint64           `json:"chainSelector"`
 	Qualifier     string           `json:"qualifier"` // Qualifier to identify the specific workflow registry
 	DONFamily     string           `json:"donFamily"`
+	Limit         *big.Int         `json:"limit"`
 	MCMSConfig    *ocr3.MCMSConfig `json:"mcmsConfig,omitempty"`
 }
 
@@ -268,7 +270,7 @@ var AdminPauseAllByDONOp = operations.NewOperation(
 
 		// Execute the transaction using the strategy
 		proposals, err := strategy.Apply(func(opts *bind.TransactOpts) (*types.Transaction, error) {
-			tx, err := registry.AdminPauseAllByDON(opts, input.DONFamily, big.NewInt(500))
+			tx, err := registry.AdminPauseAllByDON(opts, input.DONFamily, input.Limit)
 			if err != nil {
 				return nil, fmt.Errorf("failed to call AdminPauseAllByDON: %w", err)
 			}
