4bad7d8fd61b4ee9959adaffcf83b19dd4a632a3
James Kong <jkongie@gmail.com>
2025-10-07 18:07:51 +0800

    chore: update tron data-feeds changesets tests to use test runtime
    This updates the `deployment/data-feeds` package to use the test runtime
    instead of the memory environment. This is limited to changesets which
    only interact with Tron chains, while other non Tron changesets remain
    using the memory environment and will be updated in a separate PR.

--- deployment/data-feeds/changeset/tron/deploy_aggregator_proxy_test.go
+++ deployment/data-feeds/changeset/tron/deploy_aggregator_proxy_test.go
@@ -6,60 +6,49 @@ import (
 	"github.com/fbsobreira/gotron-sdk/pkg/address"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zapcore"
 
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 
-	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	cldf_tron "github.com/smartcontractkit/chainlink-deployments-framework/chain/tron"
+	"github.com/smartcontractkit/chainlink-deployments-framework/engine/test/environment"
+	"github.com/smartcontractkit/chainlink-deployments-framework/engine/test/runtime"
 	"github.com/smartcontractkit/chainlink/deployment/data-feeds/changeset/tron"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
-
-	commonChangesets "github.com/smartcontractkit/chainlink/deployment/common/changeset"
-
 	"github.com/smartcontractkit/chainlink/deployment/data-feeds/changeset/types"
-	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 )
 
 func TestDeployAggregatorProxy(t *testing.T) {
 	t.Parallel()
-	lggr := logger.Test(t)
-	cfg := memory.MemoryEnvironmentConfig{
-		TronChains: 1,
-	}
-	env := memory.NewMemoryEnvironment(t, lggr, zapcore.DebugLevel, cfg)
 
-	chainSelector := env.BlockChains.ListChainSelectors(cldf_chain.WithFamily(chain_selectors.FamilyTron))[0]
+	selector := chain_selectors.TRON_DEVNET.Selector
+	rt, err := runtime.New(t.Context(), runtime.WithEnvOpts(
+		environment.WithTronContainer(t, []uint64{selector}),
+	))
+	require.NoError(t, err)
 
 	deployOptions := cldf_tron.DefaultDeployOptions()
 	deployOptions.FeeLimit = 1_000_000_000
 
-	newEnv, err := commonChangesets.Apply(t, env, commonChangesets.Configure(
-		tron.DeployCacheChangeset,
-		types.DeployTronConfig{
-			ChainsToDeploy: []uint64{chainSelector},
+	err = rt.Exec(
+		runtime.ChangesetTask(tron.DeployCacheChangeset, types.DeployTronConfig{
+			ChainsToDeploy: []uint64{selector},
 			Labels:         []string{"data-feeds"},
 			Qualifier:      "tron",
 			DeployOptions:  deployOptions,
-		},
-	))
+		}),
+	)
 	require.NoError(t, err)
 
 	accessControllerAddress, err := address.Base58ToAddress("TYS5HCEnSU23FgSirvxqVqfwDoD5xHd9Bz")
 	require.NoError(t, err)
 
-	resp, err := commonChangesets.Apply(t, newEnv,
-		commonChangesets.Configure(
-			tron.DeployAggregatorProxyChangeset,
-			types.DeployAggregatorProxyTronConfig{
-				ChainsToDeploy:   []uint64{chainSelector},
-				AccessController: []address.Address{accessControllerAddress},
-				Qualifier:        "tron",
-				DeployOptions:    deployOptions,
-			},
-		),
+	err = rt.Exec(
+		runtime.ChangesetTask(tron.DeployAggregatorProxyChangeset, types.DeployAggregatorProxyTronConfig{
+			ChainsToDeploy:   []uint64{selector},
+			AccessController: []address.Address{accessControllerAddress},
+			Qualifier:        "tron",
+			DeployOptions:    deployOptions,
+		}),
 	)
 	require.NoError(t, err)
-	require.NotNil(t, resp)
 }
