e033632489f6599dd55cf6ae9377db77002df5b0
Pavel <177363085+pkcll@users.noreply.github.com>
2025-10-07 21:23:54 -0400

    revert changes in core/logger/test_logger.go

--- core/logger/test_logger.go
+++ core/logger/test_logger.go
@@ -42,18 +42,9 @@ func testLogger(tb testing.TB, core zapcore.Core, lvl zapcore.Level) SugaredLogg
 		}))
 	}
 	opts = append(opts, zaptest.WrapOptions(zapOpts...))
-
-	// Get the actual core from the zap logger to initialize AtomicCore
-	zlog := zaptest.NewLogger(tb, opts...)
-	actualCore := zlog.Core()
-
-	// Initialize AtomicCore
-	atomicCore := &AtomicCore{}
-	atomicCore.Store(&actualCore)
-
-	zl := &zapLogger{
+	l := &zapLogger{
 		level:         a,
-		SugaredLogger: zlog.Sugar(),
+		SugaredLogger: zaptest.NewLogger(tb, opts...).Sugar(),
 	}
-	return Sugared(zl.With("version", verShaNameStatic()))
+	return Sugared(l.With("version", verShaNameStatic()))
 }
