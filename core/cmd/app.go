f5037506b9f048f19a38bffab7e5350838a6b7cb
Pavel <177363085+pkcll@users.noreply.github.com>
2025-10-07 21:33:40 -0400

    add comment

--- core/cmd/app.go
+++ core/cmd/app.go
@@ -274,13 +274,14 @@ func NewApp(s *Shell) *cli.App {
 					SentryEnabled:  s.Config.Sentry().DSN() != "",
 				}
 
-				// Atomic core that can be swapped out later for OTel support
+				// Noop atomic core that can be swapped out later for OTel support
 				atomicCore := logger.NewAtomicCore()
 
 				l, closeFn := lggrCfg.NewWithCores(atomicCore)
 
 				s.Logger = l
 				s.CloseLogger = closeFn
+				// s.SetOtelCore is a hook that can be used to set the OTel core
 				s.SetOtelCore = atomicCore.Store
 
 				return nil
