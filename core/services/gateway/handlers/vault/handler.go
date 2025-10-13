971373584aa91c54b691827f3138429a039231aa
Prashant Yadav <prashant.yadav@smartcontract.com>
2025-10-13 08:57:20 -0700

    active request overwritten fix

--- core/services/gateway/handlers/vault/handler.go
+++ core/services/gateway/handlers/vault/handler.go
@@ -282,6 +282,10 @@ func (h *handler) HandleJSONRPCUserMessage(ctx context.Context, req jsonrpc.Requ
 	req.ID = owner + "::" + req.ID
 
 	h.lggr.Infow("handling authorized vault request", "method", req.Method, "requestID", req.ID, "owner", owner)
+	if h.getActiveRequest(req.ID) != nil {
+		h.lggr.Errorw("request id already exists", "requestID", req.ID, "owner", owner)
+		return errors.New("request ID already exists" + req.ID)
+	}
 	ar := h.newActiveRequest(req, callback)
 
 	switch req.Method {
