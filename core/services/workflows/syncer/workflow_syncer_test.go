820124313f96a83e847ea3ea07a52265051fde10
Erik Burton <erik.burton@smartcontract.com>
2025-10-14 13:20:36 -0700

    test: increase time for Test_StratReconciliation_InitialStateSync

--- core/services/workflows/syncer/workflow_syncer_test.go
+++ core/services/workflows/syncer/workflow_syncer_test.go
@@ -794,7 +794,7 @@ func Test_StratReconciliation_InitialStateSync(t *testing.T) {
 
 		require.Eventually(t, func() bool {
 			return len(testEventHandler.GetEvents()) == numberWorkflows
-		}, 30*time.Second, 1*time.Second)
+		}, 60*time.Second, 1*time.Second)
 
 		for _, event := range testEventHandler.GetEvents() {
 			assert.Equal(t, WorkflowRegisteredEvent, event.EventType)
