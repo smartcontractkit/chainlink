ebe06465a05c47aa3070f25ca5dd85722f4853ca
pavel-raykov <pavel.raykov@smartcontract.com>
2025-10-23 20:02:50 +0200

    Minor.

---
+++ core/store/migrate/migrations/0280_create_keystore_table.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS encrypted_keystore (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW(),
+    encrypted_data BYTEA NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION encrypted_keystore_set_updated_at() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER encrypted_keystore_set_updated_at_trigger
+    BEFORE UPDATE ON encrypted_keystore
+    FOR EACH ROW
+EXECUTE FUNCTION encrypted_keystore_set_updated_at();
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER IF EXISTS encrypted_keystore_set_updated_at_trigger on encrypted_keystore;
+DROP FUNCTION IF EXISTS encrypted_keystore_set_updated_at;
+DROP TABLE IF EXISTS encrypted_keystore;
+-- +goose StatementEnd
