81da3d9c58285b98bc929ed1e6a5cc41a4c17de5
Lei <lei.shi@smartcontract.com>
2025-10-28 10:47:57 -0700

    fix block number bug in balanceAt

--- core/capabilities/fakes/evm_chain.go
+++ core/capabilities/fakes/evm_chain.go
@@ -302,10 +302,15 @@ func (fc *FakeEVMChain) BalanceAt(ctx context.Context, metadata commonCap.Reques
 
 	// Prepare balance at request
 	address := common.Address(input.Account)
-	blockNumber := new(big.Int).SetBytes(input.BlockNumber.AbsVal)
+
+	// Convert proto big-int to *big.Int; nil ⇒ latest (handled by geth toBlockNumArg)
+	var blockArg *big.Int
+	if input != nil {
+		blockArg = pb.NewIntFromBigInt(input.BlockNumber) // returns nil if input.BlockNumber is nil
+	}
 
 	// Get balance at block number
-	balance, err := fc.gethClient.BalanceAt(ctx, address, blockNumber)
+	balance, err := fc.gethClient.BalanceAt(ctx, address, blockArg)
 	if err != nil {
 		return nil, err
 	}
