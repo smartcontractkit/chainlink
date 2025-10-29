bb615c74c1041b7557e18809d75c3989663c2e17
pavel-raykov <pavel.raykov@smartcontract.com>
2025-10-29 14:59:28 +0100

    Minor.

--- core/store/migrate/migrations/0280_create_keystore_table.sql
+++ core/store/migrate/migrations/0280_create_keystore_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
--- This is a very basic storage table for the keystore https://github.com/smartcontractkit/chainlink-common/tree/main/keystore entries.
--- Support for versioning (and other automatic handling) can be conveniently done using triggers on the database level.
+-- This is a storage table for the keystore https://github.com/smartcontractkit/chainlink-common/tree/main/keystore entries.
+-- Support for versioning (and other automatic handling) can be conveniently done using triggers at the database level.
 -- Please do not implement such logic at the go library level.
 CREATE TABLE IF NOT EXISTS encrypted_keystore (
     id SERIAL PRIMARY KEY,
