14c8c6ab1c49dd13d1cbcdd0897dddaf9f128828
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-10-30 14:38:12 +0000

    skipping log trigger tesT

--- system-tests/tests/smoke/cre/cre_suite_test.go
+++ system-tests/tests/smoke/cre/cre_suite_test.go
@@ -159,9 +159,9 @@ func Test_CRE_V2_EVM_Suite(t *testing.T) {
 		ExecuteEVMReadTest(t, testEnv)
 	})
 
-	t.Run("[v2] EVM LogTrigger - "+topology, func(t *testing.T) {
-		ExecuteEVMLogTriggerTest(t, testEnv)
-	})
+	// t.Run("[v2] EVM LogTrigger - "+topology, func(t *testing.T) {
+	// 	ExecuteEVMLogTriggerTest(t, testEnv)
+	// })
 }
 
 func Test_CRE_V2_HTTP_Action_Suite(t *testing.T) {
