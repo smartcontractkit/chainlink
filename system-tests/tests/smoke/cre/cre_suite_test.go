6cbf3950e3e2e31900a8b8994acebd3d4d05cbd8
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-11-06 12:53:46 +0000

    quarantine log trigger test

--- system-tests/tests/smoke/cre/cre_suite_test.go
+++ system-tests/tests/smoke/cre/cre_suite_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink/system-tests/lib/cre/environment/blockchains"
 	t_helpers "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers"
+	"github.com/smartcontractkit/quarantine"
 )
 
 //////////// SMOKE TESTS /////////////
@@ -160,6 +161,7 @@ func Test_CRE_V2_EVM_Suite(t *testing.T) {
 	})
 
 	t.Run("[v2] EVM LogTrigger - "+topology, func(t *testing.T) {
+		quarantine.Flaky(t, "PLEX-1894")
 		ExecuteEVMLogTriggerTest(t, testEnv)
 	})
 }
