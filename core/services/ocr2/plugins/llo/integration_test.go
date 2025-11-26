052f341308ce48624fefb24c91cb2b329beea8d4
Martin Liu <martin.liu@smartcontract.com>
2025-11-26 18:09:08 +1100

    Use ABI encoder multiplier

--- core/services/ocr2/plugins/llo/integration_test.go
+++ core/services/ocr2/plugins/llo/integration_test.go
@@ -989,7 +989,7 @@ lloConfigMode = "bluegreen"
 					ExpirationWindow: expirationWindow,
 					FeedID:           timestampedStreamValueFeedID,
 					ABI: []lloevm.ABIEncoder{
-						newSingleABIEncoder("int192", nil),
+						newSingleABIEncoder("int192", standardMultiplier),
 						newSingleABIEncoder("uint64", millisToNanosMultiplier),
 					},
 				}),
@@ -1114,8 +1114,7 @@ dp -> bp_parse -> bp_decimal;
 		timestampedStreamValuePipeline := fmt.Sprintf(`
 ds1_payload [type=bridge name="%s" requestData="{\\"data\\":{\\"data\\":\\"foo\\"}}"];
 ds1_benchmark [type=jsonparse path="result,benchmarkPrice"];
-ds1_benchmark_decimal [type=multiply times=1000000000000000000];
-ds1_payload -> ds1_benchmark -> ds1_benchmark_decimal -> benchmark_price;
+ds1_payload -> ds1_benchmark -> benchmark_price;
 ds1_provider_indicated_time [type=jsonparse lax=true path="timestamps,providerIndicatedTimeUnixMs"];
 ds1_payload -> ds1_provider_indicated_time -> provider_indicated_time;
 ds1_data_received_time [type=jsonparse lax=true path="timestamps,providerDataReceivedUnixMs"];
@@ -1123,8 +1122,7 @@ ds1_payload -> ds1_data_received_time -> data_received_time;
 
 ds2_payload [type=bridge name="%s" requestData="{\\"data\\":{\\"data\\":\\"foo\\"}}"];
 ds2_benchmark [type=jsonparse path="result,benchmarkPrice2"];
-ds2_benchmark_decimal [type=multiply times=1000000000000000000];
-ds2_payload -> ds2_benchmark -> ds2_benchmark_decimal -> benchmark_price;
+ds2_payload -> ds2_benchmark -> benchmark_price;
 ds2_provider_indicated_time [type=jsonparse lax=true path="timestamps,providerIndicatedTimeUnixMs2"];
 ds2_payload -> ds2_provider_indicated_time -> provider_indicated_time;
 ds2_data_received_time [type=jsonparse lax=true path="timestamps,providerDataReceivedUnixMs"];
