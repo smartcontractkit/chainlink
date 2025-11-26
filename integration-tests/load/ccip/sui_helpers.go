1811061252a05c3b0de828b76f1caf911705ba54
Damjan Smickovski <smickovskid@outlook.com>
2025-11-26 21:27:57 +0100

    rebase

--- integration-tests/load/ccip/sui_helpers.go
+++ integration-tests/load/ccip/sui_helpers.go
@@ -1069,10 +1069,10 @@ func createSuiChainReader(
 		lggr,
 		env.BlockChains.SuiChains()[chainSelector].URL,
 		nil,
-		10*time.Second,
+		30*time.Second,
 		suiKeystore,
-		5,
-		"WaitForLocalExecution",
+		2000,
+		"WaitForEffectsCert",
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create PTB client: %w", err)
