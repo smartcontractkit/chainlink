d43bf17cccc5443ad547f6605b480a849175c4ee
Erik Burton <erik.burton@smartcontract.com>
2025-12-12 10:17:48 -0800

    test: config_test whitespace

--- core/services/chainlink/config_test.go
+++ core/services/chainlink/config_test.go
@@ -1827,7 +1827,7 @@ func TestNewGeneralConfig_SecretsOverrides(t *testing.T) {
 }
 
 func TestSecrets_Validate(t *testing.T) {
-	tests.BelongsToCISuite(t, "unit")
+	tests.BelongsToCISuite(t, "with-db")
 	for _, tt := range []struct {
 		name string
 		toml string
@@ -1847,25 +1847,25 @@ BackupURL = "foo-bar?password=asdf"
 AllowSimplePasswords = false`,
 			exp: `invalid secrets: 2 errors:
 	- Database: 2 errors:
-		- URL: invalid value (*****): missing or insufficiently complex password: DB URL must be authenticated; plaintext URLs are not allowed. Database should be secured by a password matching the following complexity requirements:
+		- URL: invalid value (*****): missing or insufficiently complex password: DB URL must be authenticated; plaintext URLs are not allowed. Database should be secured by a password matching the following complexity requirements: 
 	Must have a length of 16-50 characters
 	Must not comprise:
 		Leading or trailing whitespace (note that a trailing newline in the password file, if present, will be ignored)
-
-		- BackupURL: invalid value (*****): missing or insufficiently complex password:
+	
+		- BackupURL: invalid value (*****): missing or insufficiently complex password: 
 	Expected password complexity:
 	Must be at least 16 characters long
 	Must not comprise:
 		Leading or trailing whitespace
 		A user's API email
-
+	
 	Faults:
 		password is less than 16 characters long
-	. Database should be secured by a password matching the following complexity requirements:
+	. Database should be secured by a password matching the following complexity requirements: 
 	Must have a length of 16-50 characters
 	Must not comprise:
 		Leading or trailing whitespace (note that a trailing newline in the password file, if present, will be ignored)
-
+	
 	- Password.Keystore: empty: must be provided and non-empty`},
 
 		{name: "invalid-urls-allowed",
