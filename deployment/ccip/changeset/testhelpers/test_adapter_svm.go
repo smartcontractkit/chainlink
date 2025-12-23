aaa97926368f89fba63adca6d053b7d08cc1b785
Blaž Hrastnik <blaz@mxxn.io>
2025-12-23 10:43:26 +0900

    Padding is required on Solana too

--- deployment/ccip/changeset/testhelpers/test_adapter_svm.go
+++ deployment/ccip/changeset/testhelpers/test_adapter_svm.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gagliardetto/solana-go"
 	solrpc "github.com/gagliardetto/solana-go/rpc"
 	"github.com/stretchr/testify/require"
@@ -56,7 +57,7 @@ func (a *SVMAdapter) BuildMessage(components MessageComponents) (any, error) {
 	}
 
 	return ccip_router.SVM2AnyMessage{
-		Receiver:     components.Receiver,
+		Receiver:     common.LeftPadBytes(components.Receiver, 32),
 		TokenAmounts: nil,
 		Data:         components.Data,
 		FeeToken:     feeToken,
