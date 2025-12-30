11f504368a76cc7d648c116542d01f36dfcfa576
Prashant Yadav <prashant.yadav@smartcontract.com>
2025-12-29 17:48:54 -0800

    lint disable 2

--- deployment/cre/ocr3/v2/changeset/configure_vault_plugin.go
+++ deployment/cre/ocr3/v2/changeset/configure_vault_plugin.go
@@ -60,7 +60,7 @@ func (l ConfigureVaultPlugin) VerifyPreconditions(_ cldf.Environment, input Conf
 	if input.OracleConfig == nil {
 		return errors.New("oracle config is required")
 	}
-	if !((input.OracleConfig.PrevConfigDigest == "") == (input.OracleConfig.PrevSeqNr == uint64(0)) && (input.OracleConfig.PrevSeqNr == uint64(0)) == (input.OracleConfig.PrevHistoryDigest == "")) {
+	if !((input.OracleConfig.PrevConfigDigest == "") == (input.OracleConfig.PrevSeqNr == uint64(0)) && (input.OracleConfig.PrevSeqNr == uint64(0)) == (input.OracleConfig.PrevHistoryDigest == "")) { //nolint:staticcheck // ignore
 		return errors.New("PrevConfigDigest, PrevSeqNr, and PrevHistoryDigest must all be set or all be nil")
 	}
 	if (input.OracleConfig.PrevConfigDigest != "") && (input.OracleConfig.PrevSeqNr == uint64(0)) {
