929e28bb1b32439178d37141dfab3fce1173a802
skudasov <f4hrenh9it@gmail.com>
2026-01-15 11:48:38 +0100

    split configs

--- devenv/cmd/cl/completion.go
+++ devenv/cmd/cl/completion.go
@@ -57,6 +57,7 @@ func getSubCommands(parent string) []prompt.Suggest {
 	case "restart":
 		return []prompt.Suggest{
 			{Text: "env.toml", Description: "Spin up Anvil <> Anvil local chains, all services, 4 CL nodes"},
+			{Text: "env.toml,products/ocr2/basic.toml", Description: "2 Anvils, 4 CL Nodes, 1 OCRv2 product"},
 			{Text: "env.toml,env-cl-rebuild.toml", Description: "Spin up Anvil <> Anvil local chains, all services, 4 CL nodes (custom build)"},
 			{Text: "env.toml,env-geth.toml", Description: "Spin up Geth <> Geth local chains (clique), all services, 4 CL nodes"},
 			{Text: "env.toml,env-fuji-fantom.toml", Description: "Spin up testnets: Fuji <> Fantom, all services, 4 CL nodes"},
