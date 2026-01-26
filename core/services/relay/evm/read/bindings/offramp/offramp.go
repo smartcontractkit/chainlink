719cea2f9e63938afb8e97ac949578abc36ed6ca
pavel-raykov <pavel.raykov@smartcontract.com>
2026-01-26 11:18:51 +0100

    Minor.

--- core/services/relay/evm/read/bindings/offramp/offramp.go
+++ core/services/relay/evm/read/bindings/offramp/offramp.go
@@ -4,28 +4,11 @@
 package offramp
 
 import (
-	"errors"
 	"math/big"
-	"strings"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
-)
-
-var (
-	_ = errors.New
-	_ = big.NewInt
-	_ = strings.NewReader
-	_ = ethereum.NotFound
-	_ = bind.Bind
-	_ = common.Big1
-	_ = types.BloomLookup
-	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 type OffRampExecutionStateChanged struct {
