5c1962844128a1535a3230fb04ecf1efe1a42aec
Bartek Tofel <tofel.b@gmail.com>
2026-01-28 11:17:48 +0100

    remove stateful chip test sink

--- system-tests/tests/go.mod
+++ system-tests/tests/go.mod
@@ -204,7 +204,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chai2010/gettext-go v1.0.2 // indirect
 	github.com/chaos-mesh/chaos-mesh/api v0.0.0-20240821051457-da69c6d9617a // indirect
-	github.com/cloudevents/sdk-go/binding/format/protobuf/v2 v2.16.1 // indirect
+	github.com/cloudevents/sdk-go/binding/format/protobuf/v2 v2.16.1
 	github.com/cloudevents/sdk-go/v2 v2.16.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
@@ -237,7 +237,7 @@ require (
 	github.com/cosmos/cosmos-proto v1.0.0-beta.5 // indirect
 	github.com/cosmos/cosmos-sdk v0.50.14 // indirect
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
-	github.com/cosmos/gogoproto v1.7.0 // indirect
+	github.com/cosmos/gogoproto v1.7.0
 	github.com/cosmos/ics23/go v0.11.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.14.0 // indirect
 	github.com/cpuguy83/dockercfg v0.3.2 // indirect
@@ -568,7 +568,7 @@ require (
 	github.com/smartcontractkit/chainlink-ccip v0.1.1-solana.0.20260127052127-9e53e86176d3 // indirect
 	github.com/smartcontractkit/chainlink-ccip/chains/solana v0.0.0-20260119161343-499241536dea // indirect
 	github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings v0.0.0-20250912190424-fd2e35d7deb5 // indirect
-	github.com/smartcontractkit/chainlink-common/pkg/chipingress v0.0.11-0.20251211140724-319861e514c4 // indirect
+	github.com/smartcontractkit/chainlink-common/pkg/chipingress v0.0.11-0.20251211140724-319861e514c4
 	github.com/smartcontractkit/chainlink-evm v0.3.4-0.20260127143834-21d7db9955ee // indirect
 	github.com/smartcontractkit/chainlink-feeds v0.1.2-0.20250227211209-7cd000095135 // indirect
 	github.com/smartcontractkit/chainlink-framework/capabilities v0.0.0-20250818175541-3389ac08a563 // indirect
--- system-tests/tests/smoke/cre/v2_cron_beholder_test.go
+++ system-tests/tests/smoke/cre/v2_cron_beholder_test.go
@@ -2,14 +2,6 @@ package cre
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"slices"
-	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -26,9 +18,8 @@ import (
 
 	workflowevents "github.com/smartcontractkit/chainlink-protos/workflows/go/events"
 	t_helpers "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers"
+	chiptestsink "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers/chip-testsink"
 	ttypes "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers/configuration"
-	testsinkminimal "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers/testsink-minimal"
-	testsinkstateful "github.com/smartcontractkit/chainlink/system-tests/tests/test-helpers/testsink-stateful"
 )
 
 // smoke
@@ -40,7 +31,7 @@ func ExecuteCronBeholderTest_Stateless(t *testing.T, testEnv *ttypes.TestEnviron
 	expectedBeholderLog := "Amazing workflow user log"
 	logsFound := 0
 
-	var publishFn testsinkminimal.PublishFn = func(ctx context.Context, event *pb.CloudEvent) (*chippb.PublishResponse, error) {
+	var publishFn chiptestsink.PublishFn = func(ctx context.Context, event *pb.CloudEvent) (*chippb.PublishResponse, error) {
 		switch event.Type {
 		case "workflows.v1.UserLogs":
 			typedMsg := &workflowevents.UserLogs{}
@@ -76,7 +67,7 @@ func ExecuteCronBeholderTest_Stateless(t *testing.T, testEnv *ttypes.TestEnviron
 	}
 
 	startCh := make(chan struct{}, 1)
-	server, err := testsinkminimal.NewServer(testsinkminimal.Config{
+	server, err := chiptestsink.NewServer(chiptestsink.Config{
 		PublishFunc: publishFn,
 		GRPCListen:  ":50051",
 		Started:     startCh,
@@ -115,139 +106,6 @@ func ExecuteCronBeholderTest_Stateless(t *testing.T, testEnv *ttypes.TestEnviron
 				return
 			}
 			testLogger.Info().Msg("Waiting for expected user log message...")
-		}
-	}
-}
-
-func ExecuteCronBeholderTest_State(t *testing.T, testEnv *ttypes.TestEnvironment) {
-	testLogger := framework.L
-	workflowFileLocation := "../../../../core/scripts/cre/environment/examples/workflows/v2/cron/main.go"
-	workflowName := "cronbeholder"
-
-	expectedBeholderLog := "Amazing workflow user log"
-
-	startCh := make(chan struct{}, 1)
-	server, err := testsinkstateful.NewServer(testsinkstateful.Config{
-		GRPCListen: ":50051",
-		HTTPListen: ":8082",
-		Started:    startCh,
-	})
-	require.NoError(t, err, "failed to create new test sink server")
-
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- server.Run()
-	}()
-	waitForServerStart(t, startCh, errCh)
-
-	t.Cleanup(func() {
-		server.Shutdown(t.Context())
-	})
-
-	testLogger.Info().Msg("Creating Cron workflow configuration file...")
-	workflowConfig := crontypes.WorkflowConfig{
-		Schedule: "*/30 * * * * *", // every 30 seconds
-	}
-	_ = t_helpers.CompileAndDeployWorkflow(t, testEnv, testLogger, workflowName, &workflowConfig, workflowFileLocation)
-
-	var timeout time.Duration = 2 * time.Minute
-
-	ticker := time.NewTicker(2 * time.Second)
-	ctx, cancelFn := context.WithTimeout(t.Context(), timeout)
-	defer cancelFn()
-
-	var lastTimeStamp int
-	logsFound := 0
-	httpClient := &http.Client{Timeout: 30 * time.Second}
-	for {
-		select {
-		case <-ctx.Done():
-			require.GreaterOrEqual(t, logsFound, 1, "Cron (Beholder) test failed")
-		case <-ticker.C:
-			if logsFound > 0 {
-				ticker.Stop()
-				testLogger.Info().Msg("Cron (Beholder) test completed")
-				return
-			}
-
-			testLogger.Info().Msg("Waiting for expected user log message...")
-
-			query := url.Values{
-				"timestamp": {strconv.Itoa(lastTimeStamp)},
-				"limit":     {"100"},
-				"entity":    {"workflows.v1.UserLogs"},
-			}
-			reqURL := fmt.Sprintf("http://localhost:8082/events?%s", query.Encode())
-
-			req, err := http.NewRequestWithContext(ctx, http.MethodGet, reqURL, nil)
-			if err != nil {
-				testLogger.Error().Err(err).Msg("failed to create HTTP request")
-				continue
-			}
-
-			resp, err := httpClient.Do(req)
-			if err != nil {
-				testLogger.Error().Err(err).Msg("failed to perform HTTP request")
-				continue
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				body, _ := io.ReadAll(resp.Body)
-				testLogger.Error().Msgf("unexpected status %d: %s", resp.StatusCode, string(body))
-				continue
-			}
-
-			var results []testsinkstateful.ServedEvent
-			if err := json.NewDecoder(resp.Body).Decode(&results); err != nil {
-				testLogger.Error().Err(err).Msg("failed to decode response")
-				return
-			}
-
-			testLogger.Info().Msgf("Found %d events", len(results))
-
-			slices.SortFunc(results, func(a, b testsinkstateful.ServedEvent) int {
-				return a.Timestamp - b.Timestamp
-			})
-
-			for _, event := range results {
-				typedMsg := &workflowevents.UserLogs{}
-				raw, err := base64.StdEncoding.DecodeString(event.Body)
-				if err != nil {
-					testLogger.Error().Err(err).Str("schema", event.Schema).Msg("failed to base64 decode event body")
-					continue
-				}
-
-				if err := proto.Unmarshal(raw, typedMsg); err != nil {
-					testLogger.Error().Err(err).Str("schema", event.Schema).Msg("Failed to unmarshal protobuf; skipping")
-					continue
-				}
-
-				for _, logLine := range typedMsg.LogLines {
-					if strings.Contains(logLine.Message, expectedBeholderLog) {
-						testLogger.Info().
-							Str("expected_log", expectedBeholderLog).
-							Str("found_message", strings.TrimSpace(logLine.Message)).
-							Str("workflow_id", typedMsg.M.WorkflowExecutionID).
-							Msg("🎯 Found expected user log message!")
-
-						logsFound++
-
-						continue
-					}
-
-					testLogger.Warn().
-						Str("expected_log", expectedBeholderLog).
-						Str("found_message", strings.TrimSpace(logLine.Message)).
-						Msg("[soft assertion] Received UserLogs message, but it does not match expected log")
-				}
-			}
-
-			if len(results) == 0 {
-				continue
-			}
-
-			lastTimeStamp = results[len(results)-1].Timestamp
 		}
 	}
 }
