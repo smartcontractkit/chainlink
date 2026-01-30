988171bcba3ff08f6d522996baa762720c8fcfd5
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2026-01-30 15:18:46 -0500

    Update launcher_test.go

--- core/capabilities/launcher_test.go
+++ core/capabilities/launcher_test.go
@@ -41,6 +41,10 @@ type mockTrigger struct {
 	capabilities.CapabilityInfo
 }
 
+func (m *mockTrigger) AckEvent(ctx context.Context, triggerId string, eventId string) error {
+	return nil
+}
+
 func (m *mockTrigger) RegisterTrigger(ctx context.Context, request capabilities.TriggerRegistrationRequest) (<-chan capabilities.TriggerResponse, error) {
 	return nil, nil
 }
