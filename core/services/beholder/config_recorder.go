80143e307ae37a1f29887b7a07246ed9f53a9c93
kirqz23 <kiryll.kuzniecow@gmail.com>
2026-02-03 20:38:24 +0100

    Refactor to use service engine and gotick

--- core/services/beholder/config_recorder.go
+++ core/services/beholder/config_recorder.go
@@ -2,7 +2,6 @@ package beholder
 
 import (
 	"context"
-	"sync"
 	"time"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/beholder"
@@ -12,51 +11,31 @@ import (
 
 // ConfigRecorder periodically records Beholder config info metric.
 type ConfigRecorder struct {
-	services.StateMachine
-	chStop   services.StopChan
-	wgDone   sync.WaitGroup
+	services.Service
+	eng *services.Engine
+
 	interval time.Duration
 	logger   logger.Logger
 }
 
 func NewConfigRecorder(logger logger.Logger, interval time.Duration) *ConfigRecorder {
-	return &ConfigRecorder{
-		chStop:   make(services.StopChan),
+	cr := &ConfigRecorder{
 		interval: interval,
 		logger:   logger,
 	}
+	cr.Service, cr.eng = services.Config{
+		Name:  "BeholderConfigRecorder",
+		Start: cr.start,
+	}.NewServiceEngine(logger)
+	return cr
 }
 
-func (s *ConfigRecorder) Start(ctx context.Context) error {
-	return s.StartOnce("BeholderConfigRecorder", func() error {
-		s.wgDone.Add(1)
-		go s.run(ctx)
-		return nil
-	})
-}
-
-func (s *ConfigRecorder) Close() error {
-	return s.StopOnce("BeholderConfigRecorder", func() error {
-		close(s.chStop)
-		s.wgDone.Wait()
-		return nil
-	})
-}
-
-func (s *ConfigRecorder) run(ctx context.Context) {
-	defer s.wgDone.Done()
-	ticker := time.NewTicker(s.interval)
-	defer ticker.Stop()
-	for {
+func (s *ConfigRecorder) start(ctx context.Context) error {
+	ticker := services.TickerConfig{}.NewTicker(s.interval)
+	s.eng.GoTick(ticker, func(ctx context.Context) {
 		if err := beholder.GetClient().RecordConfigMetric(ctx); err != nil {
 			s.logger.Errorf("failed to record beholder config metric: %v", err)
 		}
-		select {
-		case <-ticker.C:
-		case <-s.chStop:
-			return
-		case <-ctx.Done():
-			return
-		}
-	}
+	})
+	return nil
 }
