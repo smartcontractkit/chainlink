af1824bd195f99a2cac9088be3fe096f59e08c99
Bartek Tofel <tofel.b@gmail.com>
2026-02-06 17:25:49 +0100

    migrate all other keepers smoke tests

--- devenv/products/keepers/core.go
+++ devenv/products/keepers/core.go
@@ -84,7 +84,7 @@ func deployContracts(l zerolog.Logger, chainClient *seth.Client, config *Keepers
 			return fmt.Errorf("error loading link token contract: %w", err)
 		}
 
-		registrar, err := deployKeeperRegistrar(chainClient, config.MustGetRegistryVersion(), linkInstance, registrarSettings, registryInstance)
+		registrar, err := DeployKeeperRegistrar(chainClient, config.MustGetRegistryVersion(), linkInstance, registrarSettings, registryInstance)
 		if err != nil {
 			return fmt.Errorf("error deploying registrar contract: %w", err)
 		}
@@ -94,57 +94,7 @@ func deployContracts(l zerolog.Logger, chainClient *seth.Client, config *Keepers
 	return nil
 }
 
-// func deployKeeperContracts(
-// 	t *testing.T,
-// 	registryVersion contracts.KeeperRegistryVersion,
-// 	registrySettings contracts.KeeperRegistrySettings,
-// 	numberOfUpkeeps int,
-// 	upkeepGasLimit uint32,
-// 	linkToken contracts.LinkToken,
-// 	client *seth.Client,
-// 	linkFundsForEachUpkeep *big.Int,
-// ) (contracts.KeeperRegistry, contracts.KeeperRegistrar, []contracts.KeeperConsumer, []*big.Int) {
-// 	ef, err := contracts.DeployMockLINKETHFeed(client, big.NewInt(2e18))
-// 	require.NoError(t, err, "Deploying mock ETH-Link feed shouldn't fail")
-// 	gf, err := contracts.DeployMockGASFeed(client, big.NewInt(2e11))
-// 	require.NoError(t, err, "Deploying mock gas feed shouldn't fail")
-
-// 	// Deploy the transcoder here, and then set it to the registry
-// 	transcoder, err := contracts.DeployUpkeepTranscoder(client)
-// 	require.NoError(t, err, "Deploying UpkeepTranscoder contract shouldn't fail")
-
-// 	registry, err := contracts.DeployKeeperRegistry(
-// 		client,
-// 		&contracts.KeeperRegistryOpts{
-// 			RegistryVersion: registryVersion,
-// 			LinkAddr:        linkToken.Address(),
-// 			ETHFeedAddr:     ef.Address(),
-// 			GasFeedAddr:     gf.Address(),
-// 			TranscoderAddr:  transcoder.Address(),
-// 			RegistrarAddr:   ZeroAddress.Hex(),
-// 			Settings:        registrySettings,
-// 		},
-// 	)
-// 	require.NoError(t, err, "Deploying KeeperRegistry shouldn't fail")
-
-// 	// Fund the registry with 1 LINK * amount of KeeperConsumerPerformance contracts
-// 	err = linkToken.Transfer(registry.Address(), big.NewInt(0).Mul(big.NewInt(1e18), big.NewInt(int64(numberOfUpkeeps))))
-// 	require.NoError(t, err, "Funding keeper registry contract shouldn't fail")
-
-// 	registrarSettings := contracts.KeeperRegistrarSettings{
-// 		AutoApproveConfigType: 2,
-// 		AutoApproveMaxAllowed: math.MaxUint16,
-// 		RegistryAddr:          registry.Address(),
-// 		MinLinkJuels:          big.NewInt(0),
-// 	}
-
-// 	// registrar := deployKeeperRegistrar(t, client, registryVersion, linkToken, registrarSettings, registry)
-// 	upkeeps, upkeepIds := automation.DeployLegacyConsumers(t, client, registry, registrar, linkToken, numberOfUpkeeps, linkFundsForEachUpkeep, upkeepGasLimit, false, false, false, nil)
-
-// 	return registry, registrar, upkeeps, upkeepIds
-// }
-
-func deployKeeperRegistrar(
+func DeployKeeperRegistrar(
 	client *seth.Client,
 	registryVersion contracts.KeeperRegistryVersion,
 	linkToken contracts.LinkToken,
@@ -169,47 +119,15 @@ func createJobs(
 	l zerolog.Logger,
 	chainlinkNodes []*clclient.ChainlinkClient,
 	config *Keepers,
-	// keeperRegistry contracts.KeeperRegistry,
-	// ocrConfig contracts.OCRv2Config,
 	evmChainID string,
 ) error {
-	// Send keeper jobs to registry and chainlink nodes
-	// primaryNode := chainlinkNodes[0]
-	// primaryNodeAddress, err := primaryNode.PrimaryEthAddress()
-	// if err != nil {
-	// 	l.Error().Err(err).Msg("Reading ETH Keys from Chainlink Client shouldn't fail")
-	// 	return err
-	// }
-
-	// nodeAddresses := make([]string, 0)
-	// for _, clNode := range chainlinkNodes {
-	// 	clNodeAddress, err := clNode.PrimaryEthAddress()
-	// 	if err != nil {
-	// 		l.Error().Err(err).Msg("Error retrieving chainlink node address")
-	// 		return err
-	// 	}
-	// 	nodeAddresses = append(nodeAddresses, clNodeAddress)
-	// }
-
-	// nodeAddressesStr, payees := make([]string, 0), make([]string, 0)
-	// for _, cla := range nodeAddresses {
-	// 	nodeAddressesStr = append(nodeAddressesStr, cla)
-	// 	payees = append(payees, primaryNodeAddress)
-	// }
-
-	// err = keeperRegistry.SetKeepers(nodeAddressesStr, payees, ocrConfig)
-	// if err != nil {
-	// 	l.Error().Err(err).Msg("Setting keepers in the registry shouldn't fail")
-	// 	return err
-	// }
-
 	for _, chainlinkNode := range chainlinkNodes {
 		chainlinkNodeAddress, err := chainlinkNode.PrimaryEthAddress()
 		if err != nil {
 			l.Error().Err(err).Msg("Error retrieving chainlink node address")
 			return err
 		}
-		_, err = chainlinkNode.MustCreateJob(&keeperJobSpec{
+		_, err = chainlinkNode.MustCreateJob(&KeeperJobSpec{
 			Name:                     "keeper-test-" + config.DeployedContracts.Registry,
 			ContractAddress:          config.DeployedContracts.Registry,
 			FromAddress:              chainlinkNodeAddress,
@@ -225,7 +143,7 @@ func createJobs(
 }
 
 // KeeperJobSpec represents a V2 keeper spec
-type keeperJobSpec struct {
+type KeeperJobSpec struct {
 	Name                     string `toml:"name"`
 	ContractAddress          string `toml:"contractAddress"`
 	FromAddress              string `toml:"fromAddress"` // Hex representation of the from address
@@ -234,10 +152,10 @@ type keeperJobSpec struct {
 }
 
 // Type returns the type of the job
-func (k *keeperJobSpec) Type() string { return "keeper" }
+func (k *KeeperJobSpec) Type() string { return "keeper" }
 
 // String representation of the job
-func (k *keeperJobSpec) String() (string, error) {
+func (k *KeeperJobSpec) String() (string, error) {
 	keeperTemplateString := `
 type                     = "keeper"
 schemaVersion            = 1
