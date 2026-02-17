8e87c2846817f920d7b42436dc2245931d6aabfa
JohnChangUK <johnchang9094@gmail.com>
2026-02-17 10:44:47 -0500

    Update chainlink-aptos

--- deployment/ccip/view/aptos/onramp.go
+++ deployment/ccip/view/aptos/onramp.go
@@ -104,7 +104,7 @@ func GenerateOnRampView(
 
 	destChainSpecificData := make(map[uint64]DestChainSpecificData, len(destinationChainSelectors))
 	for _, selector := range destinationChainSelectors {
-		_, allowlistEnabled, routerAddr, err := boundOnRamp.Onramp().GetDestChainConfig(nil, selector)
+		_, allowlistEnabled, routerAddr, routerStateAddr, err := boundOnRamp.Onramp().GetDestChainConfigV2(nil, selector)
 		if err != nil {
 			return OnRampView{}, fmt.Errorf("failed to get destChainConfig for selector %d of onRamp %s: %w", selector, onRampAddress.StringLong(), err)
 		}
