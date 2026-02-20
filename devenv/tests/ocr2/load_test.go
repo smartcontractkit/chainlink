b6d733ae4e4a37a0ac0d50085d0b35534fd91727
skudasov <f4hrenh9it@gmail.com>
2026-02-20 15:59:22 +0100

    quick DF1 smoke

--- devenv/tests/ocr2/load_test.go
+++ devenv/tests/ocr2/load_test.go
@@ -40,27 +40,13 @@ func TestOCR2Load(t *testing.T) {
 
 	anvilClient := rpc.New(in.Blockchains[0].Out.Nodes[0].ExternalHTTPUrl, nil)
 
-	// this config must be as close to production as possible
-	productionCfg := &ocr2.OCRv2SetConfigOptions{
-		RMax:                                    3,
-		DeltaProgress:                           20 * time.Second,
-		DeltaResend:                             20 * time.Second,
-		DeltaStage:                              15 * time.Second,
-		MaxDurationInitialization:               5 * time.Second,
-		MaxDurationQuery:                        5 * time.Second,
-		MaxDurationObservation:                  5 * time.Second,
-		MaxDurationReport:                       5 * time.Second,
-		MaxDurationShouldAcceptFinalizedReport:  5 * time.Second,
-		MaxDurationShouldTransmitAcceptedReport: 5 * time.Second,
-	}
-
 	testCases := []testcase{
 		{
 			name:               "clean",
 			roundCheckInterval: 5 * time.Second,
 			roundTimeout:       2 * time.Minute,
 			repeat:             60,
-			cfg:                productionCfg,
+			cfg:                DefaultProductionOCR2Config,
 			roundSettings: []*roundSettings{
 				{value: 1},
 				{value: 1e3},
