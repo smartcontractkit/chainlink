f10532ff5d4a2ced3bc833d1571d48a04a08b4ed
Tejaswi Nadahalli <tejaswi.nadahalli@smartcontract.com>
2026-03-04 17:48:57 +0100

    Add GIT_AUTH_TOKEN secret mount to Dockerfile, set confidential-relay gateway service name
    Dockerfile: mount GIT_AUTH_TOKEN during go mod download for private
    module access in Docker builds.
    
    gateway_job.go: set ServiceName "confidential" on the confidential-relay
    handler so the gateway can route requests from the enclave's
    RemoteDispatcher (which sends methods like "confidential.capability.execute").

--- deployment/cre/jobs/pkg/gateway_job.go
+++ deployment/cre/jobs/pkg/gateway_job.go
@@ -447,7 +447,8 @@ type confidentialRelayHandlerConfig struct {
 
 func newDefaultConfidentialRelayHandler() handler {
 	return handler{
-		Name: GatewayHandlerTypeConfidentialRelay,
+		Name:        GatewayHandlerTypeConfidentialRelay,
+		ServiceName: "confidential",
 		Config: confidentialRelayHandlerConfig{
 			NodeRateLimiter: nodeRateLimiterConfig{
 				GlobalBurst:    10,
