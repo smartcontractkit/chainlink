7f7c656d2188cfa8ace2bb63c0e9fc45b73703c3
skudasov <f4hrenh9it@gmail.com>
2026-03-10 21:45:12 +0100

    fix lint

--- devenv/tests/automation/load_test.go
+++ devenv/tests/automation/load_test.go
@@ -59,7 +59,7 @@ func TestLoad(t *testing.T) {
 			},
 		},
 	}
-	
+
 	t.Cleanup(func() {
 		_, cErr := framework.SaveContainerLogs(fmt.Sprintf("%s-%s", framework.DefaultCTFLogsDir, t.Name()))
 		require.NoError(t, cErr)
@@ -167,11 +167,11 @@ func TestLoad(t *testing.T) {
 			consumerABI, err := simple_log_upkeep_counter_wrapper.SimpleLogUpkeepCounterMetaData.GetAbi()
 			require.NoError(t, err, "Error getting consumer abi")
 
-			var bytes0 = [32]byte{
+			bytes0 := [32]byte{
 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 			}
 
-			var bytes1 = [32]byte{
+			bytes1 := [32]byte{
 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
 			}
 
@@ -356,9 +356,7 @@ func TestLoad(t *testing.T) {
 					}
 					err = errors.New("initial error") // to ensure our for loop runs at least once
 					for err != nil {
-						var (
-							logsInBatch []types.Log
-						)
+						var logsInBatch []types.Log
 						ctx2, cancel := context.WithTimeout(t.Context(), timeout)
 						logsInBatch, err = a.ChainClient.Client.FilterLogs(ctx2, filterQuery)
 						cancel()
@@ -421,9 +419,7 @@ func TestLoad(t *testing.T) {
 					}
 					err = errors.New("initial error") // to ensure our for loop runs at least once
 					for err != nil {
-						var (
-							logsInBatch []types.Log
-						)
+						var logsInBatch []types.Log
 						ctx2, cancel := context.WithTimeout(t.Context(), timeout)
 						logsInBatch, err = chainClient.Client.FilterLogs(ctx2, filterQuery)
 						cancel()
