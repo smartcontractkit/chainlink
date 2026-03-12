69adace93322af349a64f577b8e1ec6ba1547a37
Erik Burton <erik.burton@smartcontract.com>
2026-03-11 17:46:10 -0700

    fix: linting, add key id log for csa test

--- core/cmd/export_test.go
+++ core/cmd/export_test.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
-	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
 	"github.com/urfave/cli"
+
+	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
 )
 
 func NewAppWithOptsForTest(s *Shell, opts chainlink.GeneralConfigOpts) *cli.App {
