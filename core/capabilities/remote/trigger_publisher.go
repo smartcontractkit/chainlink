af749c053334c92d2d465ad054ef7471b5acccce
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2026-03-13 15:38:15 -0400

    Update trigger_publisher.go

--- core/capabilities/remote/trigger_publisher.go
+++ core/capabilities/remote/trigger_publisher.go
@@ -285,7 +285,8 @@ func (p *triggerPublisher) Receive(_ context.Context, msg *types.MessageBody) {
 
 		reg.cancel()
 		ctx, cancel := p.stopCh.NewCtx()
-		if err := p.cfg.Load().underlying.UnregisterTrigger(ctx, reg.request); err != nil {
+		err = p.cfg.Load().underlying.UnregisterTrigger(ctx, reg.request)
+		if err != nil {
 			p.lggr.Errorw("failed to unregister trigger on underlying", "workflowID", key.workflowID, "triggerID", key.triggerID, "err", err)
 		}
 		cancel()
