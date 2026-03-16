c39b704c945a789785532e8234888e4195afe630
pavel-raykov <pavel.raykov@smartcontract.com>
2026-03-16 17:18:38 +0100

    commit store 1.5.0

--- core/services/ocr2/plugins/ccip/internal/ccipdata/factory/commit_store.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/factory/commit_store.go
@@ -10,13 +10,13 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/ccipcalc"
 	ccipdata2 "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/ccipdata"
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/ccipdata/v1_2_0"
+	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/ccipdata/v1_5_0"
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/types"
 	ccipconfig "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/version"
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/versionfinder"
 
 	"github.com/smartcontractkit/chainlink-evm/pkg/client"
 	"github.com/smartcontractkit/chainlink-evm/pkg/logpoller"
-	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/v1_5_0"
 )
 
 func NewCommitStoreReader(ctx context.Context, lggr logger.Logger, versionFinder versionfinder.VersionFinder, address cciptypes.Address, ec client.Client, lp logpoller.LogPoller, feeEstimatorConfig ccipdata2.FeeEstimatorConfigReader) (types.CommitStoreReader, error) {
