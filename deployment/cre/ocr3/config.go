790eedfd2a091a5c342d79b35fdb5011b16749fb
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2026-03-16 13:32:51 -0400

    include extra signer families by default

--- deployment/cre/ocr3/config.go
+++ deployment/cre/ocr3/config.go
@@ -386,7 +386,7 @@ func toNodeKeys(o *deployment.Node, registryChainSel uint64, extraSignerFamilies
 	var solanaOnchainPublickey string
 	for details, cfg := range o.SelToOCRConfig {
 		if family, err := chainsel.GetSelectorFamily(details.ChainSelector); err == nil {
-			if !familySet[family] {
+			if len(familySet) > 0 && !familySet[family] {
 				continue
 			}
 			if family == chainsel.FamilyAptos {
