0ddc2ccc02bdde1f79d7df047beff1ca8c67eeb3
pavel-raykov <pavel.raykov@smartcontract.com>
2026-03-17 10:34:33 +0100

    minor

--- core/services/relay/evm/ccip/rpclib/rpclibmocks/evm_mock.go
+++ core/services/relay/evm/ccip/rpclib/rpclibmocks/evm_mock.go
@@ -5,7 +5,7 @@ package rpclibmocks
 import (
 	context "context"
 
-	rpclib "github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/rpclib"
+	rpclib "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/ccip/internal/rpclib"
 	mock "github.com/stretchr/testify/mock"
 )
 
