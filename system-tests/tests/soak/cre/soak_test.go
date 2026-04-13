ef63143bcc1c771064e28d369886a87ce3919de7
Bartek Tofel <tofel.b@gmail.com>
2026-04-13 17:56:00 +0200

    try out admin profile dump

--- system-tests/tests/soak/cre/soak_test.go
+++ system-tests/tests/soak/cre/soak_test.go
@@ -38,7 +38,7 @@ Environment variables:
   - CTF_CONFIGS: path to the environment config TOML file.
 */
 func Test_CRE_PoR_MemoryLeakSoak(t *testing.T) {
-	const numWorkflows = 20
+	const numWorkflows = 2
 
 	start := time.Now()
 
