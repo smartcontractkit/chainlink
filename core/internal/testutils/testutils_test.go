5834061f1593946cc7aaebaf96c68ad8aea64db6
Adam Hamrick <adam.hamrick@smartcontract.com>
2026-05-04 12:29:59 -0400

    timeout test

--- core/internal/testutils/testutils_test.go
+++ core/internal/testutils/testutils_test.go
@@ -13,3 +13,10 @@ func TestDefaultTimeoutWaitTimeoutMax(t *testing.T) {
 	require.GreaterOrEqual(t, got, DefaultWaitTimeout)
 	require.Greater(t, got, time.Duration(0))
 }
+
+func TestWaitTimeoutCustom(t *testing.T) {
+	got := WaitTimeoutCustom(t, 10*time.Second)
+
+	require.LessOrEqual(t, got, 10*time.Second)
+	require.Greater(t, got, time.Duration(0))
+}
