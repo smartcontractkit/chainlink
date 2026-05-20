44e5d1c6ef4fc05d4b30f5fa440cca0924939055
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2026-05-20 14:22:57 -0400

    Update durable_event_store_orm_test.go

--- core/services/beholder/durable_event_store_orm_test.go
+++ core/services/beholder/durable_event_store_orm_test.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/pkg/beholder/beholderstore"
 
-	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/pgtest"
 )
 
