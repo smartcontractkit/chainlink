94226cbd4bc71e026841a9c5a77234ab98718fc3
James Kong <james.kong@smartcontract.com>
2026-05-22 11:23:34 +0800

    Remove `ValidateSelectorsInEnvironment` from helpers.go. Not used

--- deployment/helpers.go
+++ deployment/helpers.go
@@ -10,15 +10,6 @@ import (
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 )
 
-func ValidateSelectorsInEnvironment(e cldf.Environment, chains []uint64) error {
-	for _, chain := range chains {
-		if !e.BlockChains.Exists(chain) {
-			return fmt.Errorf("chain %d not found in environment", chain)
-		}
-	}
-	return nil
-}
-
 func IsAddressListUnique(addresses []common.Address) bool {
 	addressSet := make(map[common.Address]struct{})
 	for _, address := range addresses {
