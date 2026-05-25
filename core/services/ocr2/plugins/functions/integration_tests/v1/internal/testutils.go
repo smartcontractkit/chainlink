d4fbb66a91103776c794367b6ebc71ff1139f9b6
Bartek Tofel <tofel.b@gmail.com>
2026-05-25 09:10:59 +0200

    use assert.Eventually

--- core/services/ocr2/plugins/functions/integration_tests/v1/internal/testutils.go
+++ core/services/ocr2/plugins/functions/integration_tests/v1/internal/testutils.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 
@@ -18,6 +19,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth/ethconfig"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/freeport"
@@ -617,46 +619,19 @@ func ClientTestRequests(t *testing.T, owner *bind.TransactOpts, b evmtypes.Backe
 		require.NoError(t, err)
 		client.FinalizeLatest(t, b)
 	}
-
 	// validate that all client contracts got correct responses to their requests
-	type pollResult struct {
-		idx    int
-		answer [32]byte
-		err    error
-	}
-	results := make(chan pollResult, len(clientContracts))
+	var wg sync.WaitGroup
 	for i := range clientContracts {
 		ic := i
-		go func() {
-			ticker := time.NewTicker(time.Second)
-			defer ticker.Stop()
-			timer := time.NewTimer(timeout)
-			defer timer.Stop()
-			for {
+		wg.Go(func() {
+			assert.Eventually(t, func() bool {
 				answer, err := clientContracts[ic].Contract.SLastResponse(nil)
 				if err != nil {
-					results <- pollResult{ic, [32]byte{}, err}
-					return
+					return false
 				}
-				if answer == GetExpectedResponse(requestSources[ic]) {
-					results <- pollResult{ic, answer, nil}
-					return
-				}
-				select {
-				case <-ticker.C:
-				case <-timer.C:
-					results <- pollResult{ic, [32]byte{}, fmt.Errorf("contract %d response timed out after %s", ic, timeout)}
-					return
-				}
-			}
-		}()
+				return answer == GetExpectedResponse(requestSources[ic])
+			}, timeout, 1*time.Second, "unexpected response for client contract at index %d", ic)
+		})
 	}
-	// require/gomega assertions call t.FailNow which must run on the test goroutine;
-	// calling them from spawned goroutines panics or behaves unpredictably, so we
-	// poll without assertions in the goroutines and fail here on the main goroutine.
-	for range clientContracts {
-		r := <-results
-		require.NoError(t, r.err)
-		require.Equal(t, GetExpectedResponse(requestSources[r.idx]), r.answer)
-	}
+	wg.Wait()
 }
