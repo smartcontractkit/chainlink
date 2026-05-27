e86d5bcc4c20bbccdf8e12d34e4193dffd4f4b05
Dmytro Haidashenko <dmytro.haidashenko@smartcontract.com>
2026-05-27 18:58:15 +0200

    improve workflow test assertion logging

--- system-tests/tests/test-helpers/workflows/testing/main.go
+++ system-tests/tests/test-helpers/workflows/testing/main.go
@@ -2,36 +2,47 @@ package testing
 
 import (
 	"fmt"
-	"log/slog"
-	"strings"
+	"sync/atomic"
 )
 
-type T struct {
-	*slog.Logger
+type Logger interface {
+	Error(msg string, args ...any)
 }
 
-func NewT(logger *slog.Logger) *T {
-	return &T{Logger: logger}
+type T struct {
+	lggr  Logger
+	msgID atomic.Int32
+}
+
+func New(lggr Logger) *T {
+	return &T{lggr: lggr}
 }
 
 func (t *T) Errorf(format string, args ...interface{}) {
 	// if the log was produced by require/assert we need to split it, as engine does not allow logs longer than 1k bytes
-	if len(args) > 0 {
-		if msg, ok := args[0].(string); ok && strings.Contains(msg, "Error:") && strings.Contains(msg, "Error Trace:") {
-			var out []string
-			for _, line := range strings.Split(msg, "\n") {
-				if strings.Contains(line, "Error Trace") {
-					continue
-				}
-
-				out = append(out, line)
-			}
-
-			t.Logger.Error(strings.Join(out, ";"))
-			return
-		}
+	logLine := fmt.Sprintf(format, args...)
+	const maxLength = 1000
+	if len(logLine) <= maxLength {
+		t.lggr.Error(fmt.Sprintf(format, args...))
 	}
-	t.Logger.Error(fmt.Sprintf(format, args...))
+	id := t.msgID.Add(1)
+	prefix := fmt.Sprintf("MsgID=%d ", id)
+	prefix = prefix + " Part=%d/%d "
+	prefixLength := len(prefix) // since parts counter in string are represented by %d we allocate 2 digits for each, which should be enough
+	partLen := maxLength - prefixLength
+	parts := len(logLine) / (partLen)
+	if len(logLine)%partLen != 0 {
+		parts++
+	}
+	i := 0
+	start := 0
+	for start < len(logLine) {
+		i++
+		partPrefix := fmt.Sprintf(prefix, id, i, parts)
+		end := min(start+partLen, len(logLine))
+		t.lggr.Error(fmt.Sprintf(partPrefix+format, logLine[start:end]))
+		start = end
+	}
 }
 
 func (t *T) FailNow() {
