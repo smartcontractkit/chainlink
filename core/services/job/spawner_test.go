550df2032fb64645ffb9dd38d2c6e1cd57d26bcd
pavel-raykov <pavel.raykov@smartcontract.com>
2026-05-27 09:46:28 +0200

    Minor.

--- core/services/job/spawner_test.go
+++ core/services/job/spawner_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/mailbox/mailboxtest"
+	"github.com/smartcontractkit/chainlink-evm/pkg/chains/legacyevm"
 	"github.com/smartcontractkit/chainlink-evm/pkg/keys"
 	evmrelayer "github.com/smartcontractkit/chainlink-evm/pkg/relay"
 	evmtypes "github.com/smartcontractkit/chainlink-evm/pkg/types"
@@ -72,10 +73,11 @@ func clearDB(t *testing.T, db *sqlx.DB) {
 
 type relayGetter struct {
 	r *evmrelayer.Relayer
+	c legacyevm.Chain
 }
 
 func (g *relayGetter) Get(id types.RelayID) (loop.Relayer, error) {
-	return coreevmrelayer.NewLegacyAdapter(g.r, nil), nil
+	return coreevmrelayer.NewLegacyAdapter(g.r, g.c), nil
 }
 
 func (g *relayGetter) GetIDToRelayerMap() map[types.RelayID]loop.Relayer {
@@ -324,6 +326,7 @@ func TestSpawner_CreateJobDeleteJob(t *testing.T) {
 
 		testRelayGetter := &relayGetter{
 			r: evmRelayer,
+			c: chain,
 		}
 
 		jobOCR2Keeper := makeOCR2Keeper21JobSpec(t, keyStore, address, chain.ID())
