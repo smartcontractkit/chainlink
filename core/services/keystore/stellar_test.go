2da45472b9d3a42b8f4c699c2d694964a4163459
ilija42 <pavlovicilija42@gmail.com>
2026-06-05 16:23:27 +0200

    lint

--- core/services/keystore/stellar_test.go
+++ core/services/keystore/stellar_test.go
@@ -60,7 +60,7 @@ func Test_StellarKeyStore_E2E(t *testing.T) {
 		exportJSON, err := ks.Export(key.ID(), cltest.Password)
 		require.NoError(t, err)
 		_, err = ks.Export("non-existent", cltest.Password)
-		assert.Error(t, err)
+		require.Error(t, err)
 		_, err = ks.Delete(ctx, key.ID())
 		require.NoError(t, err)
 		_, err = ks.Get(key.ID())
@@ -68,9 +68,9 @@ func Test_StellarKeyStore_E2E(t *testing.T) {
 		importedKey, err := ks.Import(ctx, exportJSON, cltest.Password)
 		require.NoError(t, err)
 		_, err = ks.Import(ctx, exportJSON, cltest.Password)
-		assert.Error(t, err)
+		require.Error(t, err)
 		_, err = ks.Import(ctx, []byte(""), cltest.Password)
-		assert.Error(t, err)
+		require.Error(t, err)
 		require.Equal(t, key.ID(), importedKey.ID())
 		retrievedKey, err := ks.Get(key.ID())
 		require.NoError(t, err)
@@ -85,14 +85,14 @@ func Test_StellarKeyStore_E2E(t *testing.T) {
 		err = ks.Add(ctx, newKey)
 		require.NoError(t, err)
 		err = ks.Add(ctx, newKey)
-		assert.Error(t, err)
+		require.Error(t, err)
 		keys, err := ks.GetAll()
 		require.NoError(t, err)
 		require.Len(t, keys, 1)
 		_, err = ks.Delete(ctx, newKey.ID())
 		require.NoError(t, err)
 		_, err = ks.Delete(ctx, newKey.ID())
-		assert.Error(t, err)
+		require.Error(t, err)
 		keys, err = ks.GetAll()
 		require.NoError(t, err)
 		require.Empty(t, keys)
@@ -104,10 +104,10 @@ func Test_StellarKeyStore_E2E(t *testing.T) {
 		defer reset()
 		ctx := testutils.Context(t)
 		err := ks.EnsureKey(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		err = ks.EnsureKey(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		keys, err := ks.GetAll()
 		require.NoError(t, err)
@@ -123,7 +123,7 @@ func Test_StellarKeyStore_E2E(t *testing.T) {
 
 		// sign unknown ID
 		_, err = ks.Sign(testutils.Context(t), "not-real", nil)
-		assert.Error(t, err)
+		require.Error(t, err)
 
 		// sign known key
 		payload := []byte{1}
