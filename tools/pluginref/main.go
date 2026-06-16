0013ae32500ab82a44733aa0b1803a20ec1b347e
Bartek Tofel <tofel.b@gmail.com>
2026-06-16 17:50:41 +0200

    use a small tool to be safe and cover more cases

---
+++ tools/pluginref/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var (
+		pluginFile string
+		pluginName string
+	)
+
+	rootCmd := &cobra.Command{
+		Use:   "pluginref",
+		Short: "Resolve a plugin gitRef from YAML to a git rev-parse ref",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if pluginFile == "" {
+				return fmt.Errorf("--plugin-file is required")
+			}
+			if pluginName == "" {
+				return fmt.Errorf("--plugin is required")
+			}
+
+			gitRef, err := gitRefForPlugin(pluginFile, pluginName)
+			if err != nil {
+				return err
+			}
+
+			commitRef, err := commitRefForGitRef(gitRef)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(commitRef)
+			return nil
+		},
+	}
+
+	rootCmd.Flags().StringVar(&pluginFile, "plugin-file", "", "Path to plugins YAML file")
+	rootCmd.Flags().StringVar(&pluginName, "plugin", "", "Plugin name (YAML key under plugins:)")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
