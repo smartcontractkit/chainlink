475bedb94a754638a2e3f47cffa50c3d4f317d0f
pavel-raykov <pavel.raykov@smartcontract.com>
2026-06-24 15:24:08 +0200

    Minor.

--- core/services/job/job_orm_test.go
+++ core/services/job/job_orm_test.go
@@ -963,7 +963,7 @@ func TestORM_CreateJob_OCR2_Sending_Keys_Transmitter_Keys_Validations(t *testing
 }
 
 func TestORM_ValidateKeyStoreMatch(t *testing.T) {
-	ctx := testing.TB.Context(t)
+	ctx := t.Context()
 	config := configtest.NewGeneralConfig(t, func(c *chainlink.Config, s *chainlink.Secrets) {})
 
 	keyStore := cltest.NewKeyStore(t, pgtest.NewSqlxDB(t))
