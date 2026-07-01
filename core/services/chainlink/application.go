3180a8b847fa58c6009f2479b5bcf45d43ebc733
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-07-01 11:48:06 -0300

    set timeout

--- core/services/chainlink/application.go
+++ core/services/chainlink/application.go
@@ -393,6 +393,7 @@ func NewApplication(ctx context.Context, opts ApplicationOpts) (Application, err
 			RetransmitEnabled: true, // host process owns retransmit
 			EmitterConfig:     &emitterCfg,
 			Meter:             meter,
+			MaxPublishTimeout: 10 * time.Second,
 		}
 		pgStore := durableemitter.NewPgDurableEventStore(opts.DS)
 		durableEmitter, setupErr := durableemitter.Setup(pgStore, durableCfg, globalLogger)
