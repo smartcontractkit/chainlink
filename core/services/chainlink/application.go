cc75a3c3695c688c0626ae178606753eed33bc1f
Tarcísio Zotelli Ferraz <tar-2008.ferraz@hotmail.com>
2026-07-01 15:43:52 -0300

    update batch size

--- core/services/chainlink/application.go
+++ core/services/chainlink/application.go
@@ -379,7 +379,7 @@ func NewApplication(ctx context.Context, opts ApplicationOpts) (Application, err
 
 	// Wire DurableEmitter for persistent chip ingress delivery when enabled
 	if cfg.Telemetry().DurableEmitterEnabled() && cfg.Telemetry().ChipIngressEndpoint() != "" {
-		maxBatchSize := 10_000
+		maxBatchSize := 1000
 		emitterCfg := durableemitter.DefaultConfig()
 		emitterCfg.RetransmitBatchSize = maxBatchSize
 		emitterCfg.Metrics = &durableemitter.DurableEmitterMetricsConfig{}
