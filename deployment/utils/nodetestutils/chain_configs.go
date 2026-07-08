1c2a7b1591d34536ad0d193db7f485ef11cfbf7d
faisal-link <faisal.altameemi@smartcontract.com>
2026-07-08 21:54:36 +0400

    update testutils ChainPoller configs for Sui

--- deployment/utils/nodetestutils/chain_configs.go
+++ deployment/utils/nodetestutils/chain_configs.go
@@ -64,6 +64,13 @@ func createSuiChainConfig(chainID string, chain cldf_sui.Chain) chainlink.RawCon
 		node["GrpcToken"] = defaultSuiGrpcToken
 	}
 	chainConfig["Nodes"] = []any{node}
+	chainConfig["ChainPoller"] = map[string]any{
+		"PollingIntervalSecs":     uint64(2),
+		"SyncTimeoutSecs":         uint64(60),
+		"ChannelBufferSize":       uint64(32),
+		"BackfillCheckpointCount": uint64(100),
+		"StartCheckpointSequence": uint64(357495000),
+	}
 
 	return chainConfig
 }
