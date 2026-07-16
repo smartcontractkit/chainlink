f67d97395a3ce81d1f67dec1c8b0f47c1a0b303e
Rens Rooimans <github@rensrooimans.nl>
2026-07-16 03:12:39 -0700

    Update expected TOML parse-error message for go-toml/v2 %#U formatting

--- core/services/chainlink/config_test.go
+++ core/services/chainlink/config_test.go
@@ -1621,7 +1621,7 @@ func TestNewGeneralConfig_ParsingError_InvalidSyntax(t *testing.T) {
 		SecretsStrings: []string{secretsFullTOML},
 	}
 	_, err := opts.New()
-	assert.EqualError(t, err, "failed to decode config TOML: toml: invalid character at start of key: {")
+	assert.EqualError(t, err, "failed to decode config TOML: toml: invalid character at start of key: U+007B '{'")
 }
 
 func TestNewGeneralConfig_ParsingError_DuplicateField(t *testing.T) {
