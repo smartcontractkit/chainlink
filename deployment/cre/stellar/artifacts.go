1d4c13cfef6664278ef644b10754b48f1233ac86
ilija42 <pavlovicilija42@gmail.com>
2026-07-27 18:18:25 +0200

    bump stellar wasm artifacts

--- deployment/cre/stellar/artifacts.go
+++ deployment/cre/stellar/artifacts.go
@@ -1,18 +1,19 @@
 package stellar
 
-import "github.com/smartcontractkit/chainlink-stellar/deployment/cre"
-
 // Artifact filenames produced by `stellar contract build` in the chainlink-stellar
 // cargo workspace (target/wasm32v1-none/release/). These are duplicated from
 // chainlink-stellar/deployment/cre/artifacts.go so callers in the deployment
 // module and downstream system-tests/lib do not need to import chainlink-stellar.
 const (
 	// ReadFixtureWasm is the CRE ReadContract test fixture (contracts/cre/test/read_fixture).
-	ReadFixtureWasm = cre.ReadFixtureWasm
+	ReadFixtureWasm = "read_fixture.wasm"
 
 	// ForwarderWasm is the CRE forwarder (contracts/cre/forwarder).
-	ForwarderWasm = cre.ForwarderWasm
+	ForwarderWasm = "forwarder.wasm"
 
 	// ReceiverWasm is the CRE test receiver (contracts/cre/test/receiver).
-	ReceiverWasm = cre.ReceiverWasm
+	ReceiverWasm = "receiver.wasm"
+
+	// RejectingReceiverWasm is the CRE test receiver that always rejects on_report.
+	RejectingReceiverWasm = "rejecting_receiver.wasm"
 )
