5c27ba710fe5ee2c1b8b0e61fedc84f8388e060a
Bartek Tofel <bartek.tofel@smartcontract.com>
2026-08-19 13:36:58 +0000

    remove CRE reconciller (moved to a dedicated repo) (#23454)
    * remove CRE reconciller (moved to a dedicated repo)
    
    * go mod tidy

--- core/scripts/cre/reconciler/internal/infra/consts.go
+++
@@ -1,7 +0,0 @@
-package infra
-
-const (
-	NodeAPIPort    = 6688 // chainlink node admin/REST API
-	OCRPeeringPort = 5001 // P2P V2 listen / OCR peering
-	GatewayWSPort  = 5003 // gateway node websocket endpoint
-)
