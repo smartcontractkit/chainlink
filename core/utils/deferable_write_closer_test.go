d5e4c6413bf3be54d76a1af5fd5124ce0cdfb1d6
Adam Hamrick <adam.hamrick@smartcontract.com>
2026-08-21 21:27:20 -0400

    lint(core/utils): fix violations (batch_012)

--- core/utils/deferable_write_closer_test.go
+++ core/utils/deferable_write_closer_test.go
@@ -18,23 +18,23 @@ func TestDeferableWriteCloser_Close(t *testing.T) {
 	wc := NewDeferableWriteCloser(f)
 	wantStr := "wanted"
 	_, err = io.WriteString(wc, wantStr)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer func() {
 		assert.NoError(t, wc.Close())
 	}()
 
-	assert.NoError(t, wc.Close())
+	require.NoError(t, wc.Close())
 	assert.True(t, wc.closed)
 	// safe to close multiple times
-	assert.NoError(t, wc.Close())
+	require.NoError(t, wc.Close())
 
 	_, err = io.WriteString(f, "after close")
-	assert.ErrorIs(t, err, os.ErrClosed)
+	require.ErrorIs(t, err, os.ErrClosed)
 
 	_, err = io.WriteString(f, "write to wc after close")
-	assert.ErrorIs(t, err, os.ErrClosed)
+	require.ErrorIs(t, err, os.ErrClosed)
 
 	r, err := os.ReadFile(f.Name())
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, wantStr, string(r))
 }
