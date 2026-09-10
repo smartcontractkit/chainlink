a02cc6e2281ca917a0b57d12ef3d88f2adbaf69d
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2026-09-10 13:32:20 -0700

    [CRE] Remove legacy workflow spec factories, v1 artifacts and workflow job paths (L1)
    - delete YAMLSpecFactory, WasmFileSpecFactory, WorkflowSpecFactory registry and tests
    - strip WorkflowSpec.Validate/SDKSpec/RawSpec/GetConfig + legacy sdk import; WorkflowSpec is now a plain row model for workflow_specs_v2
    - orm: drop workflow_specs insert, FindJobIDByWorkflow and delete cascade; CreateJob now rejects Workflow with ErrJobTypeRemoved
    - feeds: drop workflow auto-approval lookup and workflow job deletion/cancellation paths
    - delete prod-dead artifacts v1 package; rework job_orm_test and feeds tests
    - remove testdata/wasm legacy guest module, testdata/config.json and .gitignore entries
    - drop stale core/capabilities/compute/** path from CI wasm cache key
    - cmd/cre README: remove dead legacy data_feeds example section
    - system-tests: accept() no longer tolerates already-approved workflow specs

--- core/services/job/job_orm_test.go
+++ core/services/job/job_orm_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/uuid"
 	"github.com/lib/pq"
-	"github.com/pelletier/go-toml/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/guregu/null.v4"
@@ -19,7 +18,6 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/sqlutil"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/jsonserializable"
-	pkgworkflows "github.com/smartcontractkit/chainlink-common/pkg/workflows"
 	"github.com/smartcontractkit/chainlink-evm/pkg/assets"
 	configtoml "github.com/smartcontractkit/chainlink-evm/pkg/config/toml"
 	"github.com/smartcontractkit/chainlink-evm/pkg/keys"
@@ -48,7 +46,6 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/standardcapabilities"
 	"github.com/smartcontractkit/chainlink/v2/core/services/streams"
 	"github.com/smartcontractkit/chainlink/v2/core/services/vrf/vrfcommon"
-	"github.com/smartcontractkit/chainlink/v2/core/services/workflows/artifacts"
 	"github.com/smartcontractkit/chainlink/v2/core/testdata/testspecs"
 	"github.com/smartcontractkit/chainlink/v2/core/utils/testutils/heavyweight"
 )
@@ -708,6 +705,18 @@ func TestORM_CreateJob_EVMChainID_Validation(t *testing.T) {
 		err := jobORM.CreateJob(t.Context(), &jb)
 		require.Error(t, err)
 		assert.ErrorIs(t, err, job.ErrJobTypeRemoved)
+	})
+
+	t.Run("workflow job creation is rejected", func(t *testing.T) {
+		t.Parallel()
+		jb := job.Job{
+			Type:          job.Workflow,
+			WorkflowSpec:  &job.WorkflowSpec{},
+			SchemaVersion: 1,
+		}
+		err := jobORM.CreateJob(t.Context(), &jb)
+		require.ErrorIs(t, err, job.ErrJobTypeRemoved)
+		require.ErrorContains(t, err, `cannot create job of type "workflow"`)
 	})
 
 	t.Run("evm chain id validation for vrf works", func(t *testing.T) {
@@ -1880,215 +1889,6 @@ func Test_CountPipelineRunsByJobID(t *testing.T) {
 		require.NoError(t, err2)
 		require.Equal(t, int32(1), count)
 	})
-}
-
-func Test_ORM_FindJobByWorkflow(t *testing.T) {
-	addr1 := "0x0123456789012345678901234567890123456789"
-	addr2 := "0xabcdefabcdefabcdefabcdefabcdefabcdefabcd"
-	t.Parallel()
-	type fields struct {
-		ds sqlutil.DataSource
-	}
-	type args struct {
-		spec   *job.WorkflowSpec
-		before func(t *testing.T, o job.ORM, s *job.WorkflowSpec) int32
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "wf not job found",
-			fields: fields{
-				ds: pgtest.NewSqlxDB(t),
-			},
-			args: args{
-				// before is nil, so no job is inserted
-				spec: &job.WorkflowSpec{
-					ID:       1,
-					Workflow: pkgworkflows.WFYamlSpec(t, "workflow00", addr1),
-				},
-			},
-			wantErr: true,
-		},
-
-		{
-			name: "wf job found",
-			fields: fields{
-				ds: pgtest.NewSqlxDB(t),
-			},
-			args: args{
-				spec: &job.WorkflowSpec{
-					ID:       1,
-					Workflow: pkgworkflows.WFYamlSpec(t, "workflow01", addr1),
-					SpecType: job.YamlSpec,
-				},
-				before: mustInsertWFJob,
-			},
-			wantErr: false,
-		},
-
-		{
-			name: "wf wrong name",
-			fields: fields{
-				ds: pgtest.NewSqlxDB(t),
-			},
-			args: args{
-				spec: &job.WorkflowSpec{
-					ID:       1,
-					Workflow: pkgworkflows.WFYamlSpec(t, "workflow02", addr1),
-				},
-				before: func(t *testing.T, o job.ORM, s *job.WorkflowSpec) int32 {
-					var c job.WorkflowSpec
-					c.ID = s.ID
-					c.Workflow = pkgworkflows.WFYamlSpec(t, "workflow99", addr1) // insert with mismatched name
-					c.SpecType = job.YamlSpec
-					c.SecretsID = s.SecretsID
-					return mustInsertWFJob(t, o, &c)
-				},
-			},
-			wantErr: true,
-		},
-		{
-			name: "wf wrong owner",
-			fields: fields{
-				ds: pgtest.NewSqlxDB(t),
-			},
-			args: args{
-				spec: &job.WorkflowSpec{
-					ID:       1,
-					Workflow: pkgworkflows.WFYamlSpec(t, "workflow03", addr1),
-				},
-				before: func(t *testing.T, o job.ORM, s *job.WorkflowSpec) int32 {
-					var c job.WorkflowSpec
-					c.ID = s.ID
-					c.Workflow = pkgworkflows.WFYamlSpec(t, "workflow03", addr2) // insert with mismatched owner
-					c.SecretsID = s.SecretsID
-					return mustInsertWFJob(t, o, &c)
-				},
-			},
-			wantErr: true,
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ctx := t.Context()
-			ks := cltest.NewKeyStore(t, tt.fields.ds)
-
-			secretsORM := artifacts.NewWorkflowRegistryDS(tt.fields.ds, logger.TestLogger(t))
-
-			sid, err := secretsORM.Create(ctx, "some-url.com", fmt.Sprintf("some-hash-%d", i), "some-contentz")
-			require.NoError(t, err)
-			tt.args.spec.SecretsID = sql.NullInt64{Int64: sid, Valid: true}
-
-			pipelineORM := pipeline.NewORM(tt.fields.ds, logger.TestLogger(t), configtest.NewTestGeneralConfig(t).JobPipeline().MaxSuccessfulRuns())
-			bridgesORM := bridges.NewORM(tt.fields.ds)
-			o := NewTestORM(t, tt.fields.ds, pipelineORM, bridgesORM, ks)
-
-			var wantJobID int32
-			if tt.args.before != nil {
-				wantJobID = tt.args.before(t, o, tt.args.spec)
-			}
-
-			gotJ, err := o.FindJobIDByWorkflow(ctx, *tt.args.spec)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("orm.FindJobByWorkflow() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			if err == nil {
-				assert.Equal(t, wantJobID, gotJ, "mismatch job id")
-			}
-		})
-	}
-}
-
-func Test_ORM_FindJobByWorkflow_Multiple(t *testing.T) {
-	addr1 := "0x012345678901234567890123456789012345ffff"
-	addr2 := "0xabcdefabcdefabcdefabcdefabcdefabcdef0000"
-	t.Parallel()
-	t.Run("multiple jobs", func(t *testing.T) {
-		db := pgtest.NewSqlxDB(t)
-		o := NewTestORM(t,
-			db,
-			pipeline.NewORM(db,
-				logger.TestLogger(t),
-				configtest.NewTestGeneralConfig(t).JobPipeline().MaxSuccessfulRuns()),
-			bridges.NewORM(db),
-			cltest.NewKeyStore(t, db))
-		ctx := t.Context()
-		secretsORM := artifacts.NewWorkflowRegistryDS(db, logger.TestLogger(t))
-
-		sids := make([]int64, 0, 3)
-		for i := range 3 {
-			sid, err := secretsORM.Create(ctx, "some-url.com", fmt.Sprintf("some-hash-%d", i), "some-contentz")
-			require.NoError(t, err)
-			sids = append(sids, sid)
-		}
-
-		wfYaml1 := pkgworkflows.WFYamlSpec(t, "workflow00", addr1)
-		s1 := job.WorkflowSpec{
-			Workflow:  wfYaml1,
-			SpecType:  job.YamlSpec,
-			SecretsID: sql.NullInt64{Int64: sids[0], Valid: true},
-		}
-		wantJobID1 := mustInsertWFJob(t, o, &s1)
-
-		wfYaml2 := pkgworkflows.WFYamlSpec(t, "workflow01", addr1)
-		s2 := job.WorkflowSpec{
-			Workflow:  wfYaml2,
-			SpecType:  job.YamlSpec,
-			SecretsID: sql.NullInt64{Int64: sids[1], Valid: true},
-		}
-		wantJobID2 := mustInsertWFJob(t, o, &s2)
-
-		wfYaml3 := pkgworkflows.WFYamlSpec(t, "workflow00", addr2)
-		s3 := job.WorkflowSpec{
-			Workflow:  wfYaml3,
-			SpecType:  job.YamlSpec,
-			SecretsID: sql.NullInt64{Int64: sids[2], Valid: true},
-		}
-		wantJobID3 := mustInsertWFJob(t, o, &s3)
-
-		expectedIDs := []int32{wantJobID1, wantJobID2, wantJobID3}
-		for i, s := range []job.WorkflowSpec{s1, s2, s3} {
-			gotJ, err := o.FindJobIDByWorkflow(ctx, s)
-			require.NoError(t, err)
-			assert.Equal(t, expectedIDs[i], gotJ, "mismatch job id case %d, spec %v", i, s)
-			j, err := o.FindJob(ctx, expectedIDs[i])
-			require.NoError(t, err)
-			assert.NotNil(t, j)
-			t.Logf("found job %v", j)
-			assert.Equal(t, j.WorkflowSpec.Workflow, s.Workflow)
-			assert.Equal(t, j.WorkflowSpec.WorkflowID, s.WorkflowID)
-			assert.Equal(t, j.WorkflowSpec.WorkflowOwner, s.WorkflowOwner)
-			assert.Equal(t, j.WorkflowSpec.WorkflowName, s.WorkflowName)
-			assert.Equal(t, job.YamlSpec, j.WorkflowSpec.SpecType)
-		}
-	})
-}
-
-func mustInsertWFJob(t *testing.T, orm job.ORM, s *job.WorkflowSpec) int32 {
-	t.Helper()
-	err := s.Validate(t.Context())
-	require.NoError(t, err, "failed to validate spec %v", s)
-	ctx := t.Context()
-	_, err = toml.Marshal(s)
-	require.NoError(t, err, "failed to TOML marshal workflow spec %v", s)
-	j := job.Job{
-		Type:          job.Workflow,
-		WorkflowSpec:  s,
-		ExternalJobID: uuid.New(),
-		Name:          null.StringFrom(s.WorkflowOwner + "_" + s.WorkflowName),
-		SchemaVersion: 1,
-	}
-
-	err = orm.CreateJob(ctx, &j)
-	require.NoError(t, err, "failed to insert job with wf spec %+v %s", s, err)
-	return j.ID
 }
 
 func mustInsertPipelineRun(t *testing.T, orm pipeline.ORM, j job.Job) pipeline.Run {
