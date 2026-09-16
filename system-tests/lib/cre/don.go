511a53edf4851ba747a89bf0a5caeff01c9fa055
Adam Hamrick <adam.hamrick@smartcontract.com>
2026-09-16 12:59:52 -0400

    lint(integration-tests): fix violations

--- system-tests/lib/cre/don.go
+++ system-tests/lib/cre/don.go
@@ -790,7 +790,7 @@ func (n *Node) CreateJobDistributor(ctx context.Context, jd *jd.JobDistributor) 
 	}
 	return n.Clients.GQLClient.CreateJobDistributor(ctx, client.JobDistributorInput{
 		Name:      "Job Distributor",
-		Uri:       jd.WSRPC,
+		URI:       jd.WSRPC,
 		PublicKey: csaKey,
 	})
 }
