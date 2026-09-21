101dbab1a3f439ad8b9b7092ef3fd9a8f6098592
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2026-09-21 10:56:21 +0100

    Framework: update v2 workflow registry

--- core/capabilities/integration_tests/framework/don.go
+++ core/capabilities/integration_tests/framework/don.go
@@ -86,13 +86,13 @@ func (c DonContext) WaitForCapabilitiesToBeExposed(t *testing.T, dons ...*DON) {
 	}, 1*time.Minute, 1*time.Second, "timeout waiting for capabilities to be exposed")
 }
 
-func (c DonContext) WaitForWorkflowRegistryMetadata(t *testing.T, workflowName string, owner string, workflowID [32]byte) {
+func (c DonContext) WaitForWorkflowRegistryMetadata(t *testing.T, workflowName string, tag string, owner string, workflowID [32]byte) {
 	require.Eventually(t, func() bool {
-		wf, err := c.workflowRegistry.contract.GetWorkflowMetadata(&bind.CallOpts{}, common.HexToAddress(owner), workflowName)
+		wf, err := c.workflowRegistry.contract.GetWorkflow(&bind.CallOpts{}, common.HexToAddress(owner), workflowName, tag)
 		if err != nil {
 			return false
 		}
-		return wf.WorkflowID == workflowID
+		return wf.WorkflowId == workflowID
 	}, 1*time.Minute, 5*time.Second, "timeout waiting for workflow")
 }
 
@@ -202,11 +202,12 @@ func (d *DON) Initialise() {
 	d.id = &d.config.ID
 
 	if d.config.AcceptsWorkflows && d.workflowRegistry != nil {
-		d.workflowRegistry.UpdateAllowedDons([]uint32{d.config.ID})
+		d.workflowRegistry.UpdateAllowedDons([]string{d.config.name})
 		d.nodeConfigModifiers = append(d.nodeConfigModifiers, func(c *chainlink.Config, node *capabilityNode) {
 			workflowRegistryAddressStr := d.workflowRegistry.addr.String()
 			c.Capabilities.WorkflowRegistry.Address = &workflowRegistryAddressStr
 			c.Capabilities.WorkflowRegistry.ChainID = new(fmt.Sprintf("%d", testutils.SimulatedChainID))
+			c.Capabilities.WorkflowRegistry.ContractVersion = new("2.0.0")
 		})
 	}
 	d.initialised = true
@@ -368,12 +369,12 @@ func (d *DON) AddWorkflow(workflow Workflow) error {
 		return errors.New("cannot add workflow to non-initialised DON")
 	}
 
-	d.workflowRegistry.RegisterWorkflow(workflow, *d.id)
+	d.workflowRegistry.RegisterWorkflow(workflow, d.config.name)
 
 	return nil
 }
 
-func (d *DON) UpdateWorkflow(workflow UpdatedWorkflow) error {
+func (d *DON) UpdateWorkflow(workflow Workflow) error {
 	if !d.config.AcceptsWorkflows {
 		return errors.New("cannot add workflow to non-workflow DON")
 	}
@@ -382,13 +383,9 @@ func (d *DON) UpdateWorkflow(workflow UpdatedWorkflow) error {
 		return errors.New("cannot add workflow to non-initialised DON")
 	}
 
-	d.workflowRegistry.UpdateWorkflow(workflow, *d.id)
+	d.workflowRegistry.UpdateWorkflow(workflow, d.config.name)
 
 	return nil
-}
-
-func (d *DON) ComputeHashKey(owner string, field string) [32]byte {
-	return d.workflowRegistry.ComputeHashKey(owner, field)
 }
 
 type TriggerFactory interface {
--- core/capabilities/integration_tests/framework/ethereum.go
+++ core/capabilities/integration_tests/framework/ethereum.go
@@ -2,6 +2,7 @@ package framework
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"os"
 	"sync"
 	"testing"
@@ -9,8 +10,10 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth/ethconfig"
 	gethlog "github.com/ethereum/go-ethereum/log"
+	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
 	"github.com/smartcontractkit/chainlink-evm/pkg/assets"
@@ -23,6 +26,7 @@ type EthBlockchain struct {
 	services.StateMachine
 	evmtypes.Backend
 	transactionOpts *bind.TransactOpts
+	ownerKey        *ecdsa.PrivateKey
 
 	blockTimeProcessingTime time.Duration
 
@@ -31,14 +35,20 @@ type EthBlockchain struct {
 }
 
 func NewEthBlockchain(t *testing.T, initialEth int, blockTimeProcessingTime time.Duration) *EthBlockchain {
-	transactOpts := testutils.MustNewSimTransactor(t) // config contract deployer and owner
+	// The owner key is retained so that contracts requiring off-chain signatures from the deployer,
+	// such as the workflow registry's owner link, can be driven from tests.
+	ownerKey, err := crypto.GenerateKey()
+	require.NoError(t, err)
+	transactOpts, err := bind.NewKeyedTransactorWithChainID(ownerKey, testutils.SimulatedChainID) // contract deployer and owner
+	require.NoError(t, err)
+
 	genesisData := types.GenesisAlloc{transactOpts.From: {Balance: assets.Ether(initialEth).ToInt()}}
 	backend := cltest.NewSimulatedBackend(t, genesisData, ethconfig.Defaults.Miner.GasCeil)
 	gethlog.SetDefault(gethlog.NewLogger(gethlog.NewTerminalHandlerWithLevel(os.Stderr, gethlog.LevelWarn, true)))
 	backend.Commit()
 
 	return &EthBlockchain{Backend: backend, stopCh: make(services.StopChan),
-		blockTimeProcessingTime: blockTimeProcessingTime, transactionOpts: transactOpts}
+		blockTimeProcessingTime: blockTimeProcessingTime, transactionOpts: transactOpts, ownerKey: ownerKey}
 }
 
 func (b *EthBlockchain) Start(ctx context.Context) error {
@@ -73,4 +83,11 @@ func (b *EthBlockchain) Close() error {
 
 func (b *EthBlockchain) TransactionOpts() *bind.TransactOpts {
 	return b.transactionOpts
+}
+
+// SignHash signs a 32 byte hash with the key owning the contracts deployed against this backend.
+// The returned signature has a recovery ID of 0 or 1, callers needing an EIP-191 signature must
+// add 27 to it.
+func (b *EthBlockchain) SignHash(hash []byte) ([]byte, error) {
+	return crypto.Sign(hash, b.ownerKey)
 }
