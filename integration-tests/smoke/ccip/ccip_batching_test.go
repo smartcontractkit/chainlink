6af8ce3882d9f8ed58ec37a00bd7901dfdb53bc2
Adam Hamrick <adam.hamrick@smartcontract.com>
2026-09-24 16:34:34 -0400

    drop ccipocr3 common alias

--- integration-tests/smoke/ccip/ccip_batching_test.go
+++ integration-tests/smoke/ccip/ccip_batching_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/maps"
 
-	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/merklemulti"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	cldf_evm "github.com/smartcontractkit/chainlink-deployments-framework/chain/evm"
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
